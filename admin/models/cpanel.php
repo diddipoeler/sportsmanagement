d39a58ed551b7122eda5d4b1730e543d7ccbfe35
diddipoeler <famploeger@arcor.de>
2013-04-08 20:55:42 +0200

    backend: controlpanel

--- admin/models/cpanel.php
+++ admin/models/cpanel.php
@@ -16,7 +16,8 @@ public function getVersion()
 	   $mainframe =& JFactory::getApplication();
 	   $this->_db->setQuery('SELECT manifest_cache FROM #__extensions WHERE name = "com_sportsmanagement"');
        $manifest_cache = json_decode( $this->_db->loadResult(), true );
-	$mainframe->enqueueMessage(JText::_('manifest_cache<br><pre>'.print_r($manifest_cache,true).'</pre>'   ),'');	
+	$mainframe->enqueueMessage(JText::_('manifest_cache<br><pre>'.print_r($manifest_cache,true).'</pre>'   ),'');
+    return $manifest_cache['version'];	
 	}
 
 
