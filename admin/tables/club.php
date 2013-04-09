df8632b57fb52c68cb6a422a1043869ea6eef2f9
diddipoeler <famploeger@arcor.de>
2013-04-09 11:00:52 +0200

    backend: clubs

---
+++ admin/tables/club.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * @copyright	Copyright (C) 2006-2013 JoomLeague.net. All rights reserved.
+ * @license		GNU/GPL, see LICENSE.php
+ * Joomla! is free software. This version may have been modified pursuant
+ * to the GNU General Public License, and as distributed it includes or
+ * is derivative of works licensed under the GNU General Public License or
+ * other free or open source software licenses.
+ * See COPYRIGHT.php for copyright notices and details.
+ */
+
+// Check to ensure this file is included in Joomla!
+defined( '_JEXEC' ) or die( 'Restricted access' );
+
+// Include library dependencies
+jimport( 'joomla.filter.input' );
+
+/**
+* Project Table class
+*
+* @package		Joomleague
+* @since 0.1
+*/
+class TableClub extends JTable
+{
+	/**
+	 * Constructor
+	 *
+	 * @param object Database connector object
+	 * @since 1.0
+	 */
+	function __construct(& $db)
+	{
+		parent::__construct( '#__sportsmanagement_club', 'id', $db );
+	}
+
+	/**
+	 * Overloaded check method to ensure data integrity
+	 *
+	 * @access public
+	 * @return boolean True on success
+	 * @since 1.0
+	 */
+	function check()
+	{	
+		// setting alias
+		if ( empty( $this->alias ) )
+		{
+			$this->alias = JFilterOutput::stringURLSafe( $this->name );
+		}
+		else {
+			$this->alias = JFilterOutput::stringURLSafe( $this->alias ); // make sure the user didn't modify it to something illegal...
+		}
+		return true;
+	}
+
+}
+?>
