8a159db7adbef7b7a6e29b2ac766a5461f3d0748
diddipoeler <famploeger@arcor.de>
2013-04-09 11:38:07 +0200

    backend: clubs

---
+++ admin/controllers/clubs.php
@@ -0,0 +1,22 @@
+<?php
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+ 
+// import Joomla controlleradmin library
+jimport('joomla.application.component.controlleradmin');
+ 
+/**
+ * HelloWorlds Controller
+ */
+class sportsmanagementControllerclubs extends JControllerAdmin
+{
+	/**
+	 * Proxy for getModel.
+	 * @since	1.6
+	 */
+	public function getModel($name = 'club', $prefix = 'sportsmanagementModel') 
+	{
+		$model = parent::getModel($name, $prefix, array('ignore_request' => true));
+		return $model;
+	}
+}
