8ca7d3420cff2b9fab8b8eca7bab01bf79a0bb88
diddipoeler <famploeger@arcor.de>
2013-10-29 14:36:57 +0100

    update

---
+++ admin/controllers/projectposition.php
@@ -0,0 +1,16 @@
+<?php
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+ 
+// import Joomla controllerform library
+jimport('joomla.application.component.controllerform');
+ 
+/**
+ * HelloWorld Controller
+ */
+class sportsmanagementControllerprojectposition extends JControllerForm
+{
+
+
+
+}
