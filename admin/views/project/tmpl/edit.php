f3aad788d61bec44a989201ba3c92ff377e21e41
diddipoeler <famploeger@arcor.de>
2013-10-29 11:36:30 +0100

    update

--- admin/views/project/tmpl/edit.php
+++ admin/views/project/tmpl/edit.php
@@ -1,38 +1,41 @@
-<?php defined('_JEXEC') or die('Restricted access');
+<?php 
+defined('_JEXEC') or die('Restricted access');
+JHtml::_('behavior.tooltip');
+JHtml::_('behavior.formvalidation');
 
 ?>
-<form action="index.php" method="post" id="adminForm">
+<form action="<?php echo JRoute::_('index.php?option=com_sportsmanagement&layout=edit&id='.(int) $this->item->id); ?>" method="post" id="adminForm" name="adminForm">
 <fieldset class="adminform">
-			<legend><?php echo JText::sprintf('COM_JOOMLEAGUE_ADMIN_PROJECT_LEGEND_DESC','<i>'.$this->project->name.'</i>'); ?></legend>
+			<legend><?php echo JText::sprintf('COM_SPORTSMANAGEMENT_ADMIN_PROJECT_LEGEND_DESC','<i>'.$this->project->name.'</i>'); ?></legend>
 	<div class="col50">
 	<?php
 	echo JHTML::_('tabs.start','tabs', array('useCookie'=>1));
-	echo JHTML::_('tabs.panel',JText::_('COM_JOOMLEAGUE_TABS_DETAILS'), 'panel1');
+	echo JHTML::_('tabs.panel',JText::_('COM_SPORTSMANAGEMENT_TABS_DETAILS'), 'panel1');
 	echo $this->loadTemplate('details');
 
-	echo JHTML::_('tabs.panel',JText::_('COM_JOOMLEAGUE_TABS_DATE'), 'panel2');
+	echo JHTML::_('tabs.panel',JText::_('COM_SPORTSMANAGEMENT_TABS_DATE'), 'panel2');
 	echo $this->loadTemplate('date');
 
-	echo JHTML::_('tabs.panel',JText::_('COM_JOOMLEAGUE_TABS_PROJECT'), 'panel3');
+	echo JHTML::_('tabs.panel',JText::_('COM_SPORTSMANAGEMENT_TABS_PROJECT'), 'panel3');
 	echo $this->loadTemplate('project');
 
-	echo JHTML::_('tabs.panel',JText::_('COM_JOOMLEAGUE_TABS_COMPETITION'), 'panel4');
+	echo JHTML::_('tabs.panel',JText::_('COM_SPORTSMANAGEMENT_TABS_COMPETITION'), 'panel4');
 	echo $this->loadTemplate('competition');
 
-	echo JHTML::_('tabs.panel',JText::_('COM_JOOMLEAGUE_TABS_FAVORITE'), 'panel5');
+	echo JHTML::_('tabs.panel',JText::_('COM_SPORTSMANAGEMENT_TABS_FAVORITE'), 'panel5');
 	echo $this->loadTemplate('favorite');
 
-	echo JHTML::_('tabs.panel',JText::_('COM_JOOMLEAGUE_TABS_PICTURE'), 'panel6');
+	echo JHTML::_('tabs.panel',JText::_('COM_SPORTSMANAGEMENT_TABS_PICTURE'), 'panel6');
 	echo $this->loadTemplate('picture');
 
-	echo JHTML::_('tabs.panel',JText::_('COM_JOOMLEAGUE_TABS_EXTENDED'), 'panel7');
+	echo JHTML::_('tabs.panel',JText::_('COM_SPORTSMANAGEMENT_TABS_EXTENDED'), 'panel7');
 	echo $this->loadTemplate('extended');
 
 	echo JHTML::_('tabs.end');
 	?>
 	<div class="clr"></div>
-	<input type="hidden" name="option" value="com_joomleague" /> 
-	<input type="hidden" name="task" value="" /> 
+	
+	<input type="hidden" name="task" value="project.edit" /> 
 	<input type="hidden"name="oldseason" value="<?php echo $this->project->season_id; ?>" />
 	<input type="hidden" name="oldleague" value="<?php echo $this->project->league_id; ?>" /> 
 	<input type="hidden" name="cid[]" value="<?php echo $this->project->id; ?>" />
