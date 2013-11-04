9e5527f626ef4742d2d01e54e93a8a97f189b71b
diddipoeler <famploeger@arcor.de>
2013-11-04 15:44:14 +0100

    matches

--- admin/controllers/match.php
+++ admin/controllers/match.php
@@ -11,5 +11,26 @@ jimport('joomla.application.component.controllerform');
 class sportsmanagementControllermatch extends JControllerForm
 {
 
+//	add a match to round
+	function addmatch()
+	{
+		$option = JRequest::getCmd('option');
+		$mainframe = JFactory::getApplication();
+		$post = JRequest::get('post');
+		$post['project_id'] = $mainframe->getUserState( "$option.pid", '0' );
+		$post['round_id'] = $mainframe->getUserState( "$option.rid", '0' );
+		$model = $this->getModel('match');
+		if ($model->store($post))
+		{
+			$msg = JText::_('COM_JOOMLEAGUE_ADMIN_MATCH_CTRL_ADD_MATCH');
+		}
+		else
+		{
+			$msg = JText::_('COM_JOOMLEAGUE_ADMIN_MATCH_CTRL_ERROR_ADD_MATCH').$model->getError();
+		}
+		$link = 'index.php?option=com_joomleague&view=matches';
+		$this->setRedirect($link,$msg);
+	}
+    
 
 }
