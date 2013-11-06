4f6bcc1aea047a95f97c7f5e9ce0bbec9315ccb3
diddipoeler <famploeger@arcor.de>
2013-11-06 08:48:33 +0100

    update

--- admin/views/cpanel/tmpl/default.php
+++ admin/views/cpanel/tmpl/default.php
@@ -39,10 +39,10 @@ defined('_JEXEC') or die('Restricted access');
         <?php echo $this->addIcon('association.jpg','index.php?option=com_sportsmanagement&view=jlextassociations', JText::_('COM_SPORTSMANAGEMENT_D_MENU_ASSOCIATIONS'));?>
         <?php echo $this->addIcon('applications.gif','index.php?option=com_sportsmanagement&view=extrafields', JText::_('COM_SPORTSMANAGEMENT_D_MENU_EXTRAFIELDS'));?>
 				
-		<?php echo $this->addIcon('applications.gif','index.php?option=com_sportsmanagement&view=jlxmlimports', JText::_('COM_SPORTSMANAGEMENT_M_MENU_XML_IMPORT'));?>
-                <?php echo $this->addIcon('configuration.gif','index.php?option=com_sportsmanagement&view=databasetools', JText::_('COM_SPORTSMANAGEMENT_M_MENU_TOOLS'));?>
-                <?php echo $this->addIcon('applications.gif','index.php?option=com_sportsmanagement&view=updates', JText::_('COM_SPORTSMANAGEMENTM_MENU_UPDATES'));?>
-                <?php echo $this->addIcon('icon-48-rosterposition.png','index.php?option=com_sportsmanagement&view=rosterpositions', JText::_('COM_SPORTSMANAGEMENT_ROSTER_POSITION'));?>		
+		<?php echo $this->addIcon('applications.gif','index.php?option=com_sportsmanagement&view=jlxmlimports', JText::_('COM_SPORTSMANAGEMENT_D_MENU_XML_IMPORT'));?>
+                <?php echo $this->addIcon('configuration.gif','index.php?option=com_sportsmanagement&view=databasetools', JText::_('COM_SPORTSMANAGEMENT_D_MENU_TOOLS'));?>
+                <?php echo $this->addIcon('applications.gif','index.php?option=com_sportsmanagement&view=updates', JText::_('COM_SPORTSMANAGEMENT_D_MENU_UPDATES'));?>
+                <?php echo $this->addIcon('icon-48-rosterposition.png','index.php?option=com_sportsmanagement&view=rosterpositions', JText::_('COM_SPORTSMANAGEMENT_D_MENU_ROSTER_POSITION'));?>		
 			</div>
 		</td>
 		<td width="45%" valign="top">
--- admin/views/rounds/tmpl/massadd.php
+++ admin/views/rounds/tmpl/massadd.php
@@ -33,7 +33,7 @@ if($close == 1) {
         
         <fieldset>
 		<div class="fltrt">
-			<button type="button" onclick="this.form.submit();window.parent.SqueezeBox.close();">
+			<button type="button" onclick="Joomla.submitform('round.massadd', this.form);">
 				<?php echo JText::_('JSAVE');?></button>
 			<button id="cancel" type="button" onclick="<?php echo JRequest::getBool('refresh', 0) ? 'window.parent.location.href=window.parent.location.href;' : '';?>  window.parent.SqueezeBox.close();">
 				<?php echo JText::_('JCANCEL');?></button>
@@ -46,9 +46,13 @@ if($close == 1) {
 			<?php echo JHTML::_('form.token')."\n"; ?>
 			<table class='admintable'><tbody><tr>
 				<td class='key' nowrap='nowrap'><?php echo JText::_('COM_SPORTSMANAGEMENT_ADMIN_ROUNDS_MASSADD_COUNT'); ?></td>
-				<td><input type='text' name='add_round_count' id='add_round_count' value='0' size='3' class='inputbox' /></td>
+				<td>
+                <input type='text' name='add_round_count' id='add_round_count' value='0' size='3' class='inputbox' />
+                </td>
 				
 			</tr></tbody></table>
+            <input type='hidden' name='project_id' value='<?php echo $this->projectws->id; ?>' />
+            <input type="hidden" name="task" value="" />
 		</form>
 	</fieldset>
 </div>
