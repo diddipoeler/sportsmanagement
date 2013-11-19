62f48062778cbea292d015699febcdfa83e80705
diddipoeler <famploeger@arcor.de>
2013-11-19 16:02:17 +0100

    update backend

--- admin/sql/install.mysql.utf8.sql
+++ admin/sql/install.mysql.utf8.sql
@@ -780,6 +780,48 @@ ENGINE = MyISAM
 DEFAULT CHARSET = utf8;
 
 
+
+-- -----------------------------------------------------
+-- Table `#__sportsmanagement_season_team_id`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `#__sportsmanagement_season_team_id` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT ,
+  `team_id` INT(11) NOT NULL DEFAULT '0' ,
+  `season_id` INT(11) NOT NULL DEFAULT '0' ,
+  `checked_out` INT(11) NOT NULL DEFAULT '0' ,
+  `checked_out_time` DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00' ,
+  `modified` DATETIME NULL ,
+  `modified_by` INT NULL ,
+  PRIMARY KEY (`id`) ,
+  UNIQUE KEY `combi` (`team_id`,`season_id`) )
+ENGINE = MyISAM
+DEFAULT CHARSET = utf8;
+
+-- -----------------------------------------------------
+-- Table `#__sportsmanagement_season_person_id`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `#__sportsmanagement_season_person_id` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT ,
+  `person_id` INT(11) NOT NULL DEFAULT '0' ,
+  `season_id` INT(11) NOT NULL DEFAULT '0' ,
+  `checked_out` INT(11) NOT NULL DEFAULT '0' ,
+  `checked_out_time` DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00' ,
+  `modified` DATETIME NULL ,
+  `modified_by` INT NULL ,
+  PRIMARY KEY (`id`) ,
+  UNIQUE KEY `combi` (`person_id`,`season_id`) )
+ENGINE = MyISAM
+DEFAULT CHARSET = utf8;
+
+
+
+
+
+
+
+
+
+
 -- -----------------------------------------------------
 -- Table `#__sportsmanagement_sports_type`
 -- -----------------------------------------------------
