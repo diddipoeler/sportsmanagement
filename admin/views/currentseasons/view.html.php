b3940d37c512fe21e12df4f24a5fe9198b4f2bc2
DonClumsy <keller_tim@rocketmail.com>
2013-12-26 18:57:00 +0100

    Update view.html.php

--- admin/views/currentseasons/view.html.php
+++ admin/views/currentseasons/view.html.php
@@ -68,9 +68,14 @@ $this->addToolbar();
 	* @since	1.6
 	*/
 	protected function addToolbar()
-	{ 
+	{  		// Get a refrence of the page instance in joomla
+		$document	= JFactory::getDocument();
+        // Set toolbar items for the page
+        $stylelink = '<link rel="stylesheet" href="'.JURI::root().'administrator/components/com_sportsmanagement/assets/css/jlextusericons.css'.'" type="text/css" />' ."\n";
+        $document->addCustomTag($stylelink);
+        
 	// Set toolbar items for the page
-		JToolBarHelper::title(JText::_('COM_SPORTSMANAGEMENT_ADMIN_PROJECTS_TITLE'),'ProjectSettings');
+		JToolBarHelper::title(JText::_('COM_SPORTSMANAGEMENT_ADMIN_PROJECTS_TITLE'),'projectsettings');
 		JToolBarHelper::divider();
 		
 		sportsmanagementHelper::ToolbarButtonOnlineHelp();
