da2451caa7c8aa02632ba650fe2a03f862bf4998
DonClumsy <keller_tim@rocketmail.com>
2013-12-30 13:54:34 +0100

    Update view.html.php

--- admin/views/positions/view.html.php
+++ admin/views/positions/view.html.php
@@ -92,8 +92,13 @@ class sportsmanagementViewPositions extends JView
 	*/
 	protected function addToolbar()
 	{
+	// Get a refrence of the page instance in joomla
+        $document = JFactory::getDocument();
+        // Set toolbar items for the page
+        $stylelink = '<link rel="stylesheet" href="'.JURI::root().'administrator/components/com_sportsmanagement/assets/css/jlextusericons.css'.'" type="text/css" />' ."\n";
+        $document->addCustomTag($stylelink);
 		// Set toolbar items for the page
-		JToolBarHelper::title(JText::_('COM_SPORTSMANAGEMENT_ADMIN_POSITIONS_TITLE'),'Positions');
+		JToolBarHelper::title(JText::_('COM_SPORTSMANAGEMENT_ADMIN_POSITIONS_TITLE'),'positions');
 
 // 		JToolBarHelper::publishList('position.publish');
 // 		JToolBarHelper::unpublishList('position.unpublish');
