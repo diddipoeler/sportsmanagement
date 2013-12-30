3e7803fd8c6fca614b292c689eca4300162c577a
DonClumsy <keller_tim@rocketmail.com>
2013-12-30 10:16:01 +0100

    Update view.html.php

--- admin/views/season/view.html.php
+++ admin/views/season/view.html.php
@@ -47,12 +47,17 @@ class sportsmanagementViewSeason extends JView
 	 */
 	protected function addToolBar() 
 	{
+	// Get a refrence of the page instance in joomla
+        $document = JFactory::getDocument();
+        // Set toolbar items for the page
+        $stylelink = '<link rel="stylesheet" href="'.JURI::root().'administrator/components/com_sportsmanagement/assets/css/jlextusericons.css'.'" type="text/css" />' ."\n";
+        $document->addCustomTag($stylelink);
 		JRequest::setVar('hidemainmenu', true);
 		$user = JFactory::getUser();
 		$userId = $user->id;
 		$isNew = $this->item->id == 0;
 		$canDo = sportsmanagementHelper::getActions($this->item->id);
-		JToolBarHelper::title($isNew ? JText::_('COM_SPORTSMANAGEMENT_ADMIN_SEASON_ADD_NEW') : JText::_('COM_SPORTSMANAGEMENT_ADMIN_SEASON_EDIT'), 'helloworld');
+		JToolBarHelper::title($isNew ? JText::_('COM_SPORTSMANAGEMENT_ADMIN_SEASON_ADD_NEW') : JText::_('COM_SPORTSMANAGEMENT_ADMIN_SEASON_EDIT'), 'season');
 		// Built the actions for new and existing records.
 		if ($isNew) 
 		{
