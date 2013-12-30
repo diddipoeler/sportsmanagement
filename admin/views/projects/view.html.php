1cd3b30cf96e336ef0b1483595c1e4ce8ac64033
DonClumsy <keller_tim@rocketmail.com>
2013-12-30 10:03:40 +0100

    Update view.html.php

--- admin/views/projects/view.html.php
+++ admin/views/projects/view.html.php
@@ -118,9 +118,14 @@ class sportsmanagementViewProjects extends JView
 	* @since	1.6
 	*/
 	protected function addToolbar()
-	{ 
+	{
+	// Get a refrence of the page instance in joomla
+        $document = JFactory::getDocument();
+        // Set toolbar items for the page
+        $stylelink = '<link rel="stylesheet" href="'.JURI::root().'administrator/components/com_sportsmanagement/assets/css/jlextusericons.css'.'" type="text/css" />' ."\n";
+        $document->addCustomTag($stylelink);
 		// Set toolbar items for the page
-		JToolBarHelper::title(JText::_('COM_SPORTSMANAGEMENT_ADMIN_PROJECTS_TITLE'),'ProjectSettings');
+		JToolBarHelper::title(JText::_('COM_SPORTSMANAGEMENT_ADMIN_PROJECTS_TITLE'),'projects');
 		JToolBarHelper::publishList('project.publish');
 		JToolBarHelper::unpublishList('project.unpublish');
 		JToolBarHelper::divider();
