31263689f080cb9106017cb38e3f20af50c387a8
DonClumsy <keller_tim@rocketmail.com>
2014-01-03 22:54:52 +0100

    Update default.php

--- admin/views/predictions/tmpl/default.php
+++ admin/views/predictions/tmpl/default.php
@@ -25,9 +25,9 @@ defined('_JEXEC') or die('Restricted access');
 		<td width="100%" valign="top">
 			<div id="cpanel">
 				<?php echo $this->addIcon('tippspiel.png','index.php?option=com_sportsmanagement&view=predictiongames', JText::_('COM_SPORTSMANAGEMENT_EXT_PREDICTION_GAMES'));?>
-                <?php echo $this->addIcon('groups.gif','index.php?option=com_sportsmanagement&view=predictiongroups', JText::_('COM_SPORTSMANAGEMENT_EXT_PREDICTION_GROUPS'));?>
-                <?php echo $this->addIcon('userpoints.gif','index.php?option=com_sportsmanagement&view=predictionmembers', JText::_('COM_SPORTSMANAGEMENT_EXT_PREDICTION_MEMBERS'));?>
-                <?php echo $this->addIcon('templates.gif','index.php?option=com_sportsmanagement&view=predictiontemplates', JText::_('COM_SPORTSMANAGEMENT_EXT_PREDICTION_TEMPLATES'));?>
+                <?php echo $this->addIcon('tippspielgruppen.png','index.php?option=com_sportsmanagement&view=predictiongroups', JText::_('COM_SPORTSMANAGEMENT_EXT_PREDICTION_GROUPS'));?>
+                <?php echo $this->addIcon('tippspielmitglieder.png','index.php?option=com_sportsmanagement&view=predictionmembers', JText::_('COM_SPORTSMANAGEMENT_EXT_PREDICTION_MEMBERS'));?>
+                <?php echo $this->addIcon('tippspieltemplates','index.php?option=com_sportsmanagement&view=predictiontemplates', JText::_('COM_SPORTSMANAGEMENT_EXT_PREDICTION_TEMPLATES'));?>
 
 			</div>
 		</td>
