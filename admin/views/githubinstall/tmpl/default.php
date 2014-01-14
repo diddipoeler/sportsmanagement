1f49732fa417b1ff9e4deda715b09146891a5145
diddipoeler <famploeger@arcor.de>
2014-01-14 09:34:18 +0100

    update backend

--- admin/views/githubinstall/tmpl/default.php
+++ admin/views/githubinstall/tmpl/default.php
@@ -5,7 +5,7 @@ defined('_JEXEC') or die('Restricted access');
 
 ?>
 <form action="<?php echo $this->request_url; ?>" method="post" id="adminForm" name="adminForm">
-<p style='text-align:right;'><button type="button" onclick="Joomla.submitbutton('githubinstall.store')"><?php echo JText::_('JSAVE') ?></button></p>
+<p style='text-align:right;'><button type="button" onclick="Joomla.submitform('githubinstall.store')"><?php echo JText::_('JSAVE') ?></button></p>
 <input type="hidden" name="task"				value="" />
 	<input type="hidden" name="boxchecked"			value="0" />
 	<input type="hidden" name="filter_order"		value="" />
