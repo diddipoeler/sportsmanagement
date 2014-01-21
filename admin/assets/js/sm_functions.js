03600eb91efc54774bbc1d31b4a1ffa6e7969675
diddipoeler <famploeger@arcor.de>
2014-01-21 10:32:04 +0100

    update backend

--- admin/assets/js/sm_functions.js
+++ admin/assets/js/sm_functions.js
@@ -82,9 +82,9 @@ if (selected == 2)
 
 function registerhome(homepage,notes,homepagename,isadmin)
 	{
-var url='http://www.fussballineuropa.de/jlpaket.php';		
+var url='http://sportsmanagement.fussballineuropa.de/jsmpaket.php';		
 var data = 'homepage='+homepage+'&notes='+notes+'&homepagename='+homepagename+'&isadmin='+isadmin;
-var url2='http://www.fussballineuropa.de/jlpaket.php?'+'homepage='+homepage+'&notes='+notes+'&homepagename='+homepagename+'&isadmin='+isadmin;
+var url2='http://sportsmanagement.fussballineuropa.de/jsmpaket.php?'+'homepage='+homepage+'&notes='+notes+'&homepagename='+homepagename+'&isadmin='+isadmin;
 var request = new Request({
                         url: url2,
                         method:'post',
