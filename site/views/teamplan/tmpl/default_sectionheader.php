6af07fc04a145755b640528121c8b2f3de9661d0
diddipoeler <famploeger@arcor.de>
2014-01-22 12:47:45 +0100

    Update default_sectionheader.php

--- site/views/teamplan/tmpl/default_sectionheader.php
+++ site/views/teamplan/tmpl/default_sectionheader.php
@@ -28,7 +28,7 @@
 				<?php
 				if (!is_null($this->ptid))
 				{
-				$link=JoomleagueHelperRoute::getIcalRoute($this->project->id,$this->teams[$this->ptid]->team_id,null,null);
+				$link=sportsmanagementHelperRoute::getIcalRoute($this->project->id,$this->teams[$this->ptid]->team_id,null,null);
 				$text=JHTML::_('image','administrator/components/com_sportsmanagement/assets/images/calendar.png', JText::_('COM_SPORTSMANAGEMENT_TEAMPLAN_ICAL_EXPORT'));
 				$attribs	= array('title' => JText::_('COM_SPORTSMANAGEMENT_TEAMPLAN_ICAL_EXPORT'));
 				echo JHTML::_('link',$link,$text,$attribs);
