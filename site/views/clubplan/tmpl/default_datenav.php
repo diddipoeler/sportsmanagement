fa9a0b634555a071073ff8b7e903db4c36c8d4be
diddipoeler <famploeger@arcor.de>
2014-01-23 11:26:25 +0100

    update frontend

--- site/views/clubplan/tmpl/default_datenav.php
+++ site/views/clubplan/tmpl/default_datenav.php
@@ -1,4 +1,43 @@
-<?php defined('_JEXEC') or die('Restricted access');
+<?php 
+/** SportsManagement ein Programm zur Verwaltung für alle Sportarten
+* @version         1.0.05
+* @file                agegroup.php
+* @author                diddipoeler, stony, svdoldie und donclumsy (diddipoeler@arcor.de)
+* @copyright        Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+* @license                This file is part of SportsManagement.
+*
+* SportsManagement is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* SportsManagement is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with SportsManagement.  If not, see <http://www.gnu.org/licenses/>.
+*
+* Diese Datei ist Teil von SportsManagement.
+*
+* SportsManagement ist Freie Software: Sie können es unter den Bedingungen
+* der GNU General Public License, wie von der Free Software Foundation,
+* Version 3 der Lizenz oder (nach Ihrer Wahl) jeder späteren
+* veröffentlichten Version, weiterverbreiten und/oder modifizieren.
+*
+* SportsManagement wird in der Hoffnung, dass es nützlich sein wird, aber
+* OHNE JEDE GEWÄHELEISTUNG, bereitgestellt; sogar ohne die implizite
+* Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
+* Siehe die GNU General Public License für weitere Details.
+*
+* Sie sollten eine Kopie der GNU General Public License zusammen mit diesem
+* Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
+*
+* Note : All ini files need to be saved as UTF-8 without BOM
+*/
+
+defined('_JEXEC') or die('Restricted access');
 ?>
 <form name="adminForm" id="adminForm" method="post">
 	<?php $dateformat="%d-%m-%Y"; ?>
@@ -9,15 +48,23 @@
 				echo ' - '.JHTML::calendar(sportsmanagementHelper::convertDate($this->enddate,1),'enddate','enddate',$dateformat);
 				?><input type="submit" class="button" name="reload View" value="<?php echo JText::_('COM_SPORTSMANAGEMENT_GLOBAL_FILTER'); ?>" /></td>
 			<td><?php
-			if($this->club)
+			if ( $this->club )
 			{
-				$picture=$this->club->logo_middle;
-				echo sportsmanagementHelper::getPictureThumb($picture, 
-										$this->club->name,
-										50,
-										50,
-										2);
-			}
+				$picture=$this->club->logo_big;
+/**
+ * 				echo sportsmanagementHelper::getPictureThumb($picture, 
+ * 										$this->club->name,
+ * 										50,
+ * 										50,
+ * 										2);
+ */
+			
+?>                                    
+<a href="<?php echo JURI::root().$picture;?>" title="<?php echo $this->club->name;?>" class="modal">
+<img src="<?php echo JURI::root().$picture;?>" alt="<?php echo $this->club->name;?>" width="50" />
+</a>
+<?PHP            
+            }
 			?></td>
 		</tr>
 	</table>
