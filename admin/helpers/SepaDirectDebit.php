716f94774ee6ff841fa1c4772d7e2d2b8ef4f8fa
diddipoeler <famploeger@arcor.de>
2014-02-13 20:06:19 +0100

    Create SepaDirectDebit.php

---
+++ admin/helpers/SepaDirectDebit.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * PHP-Classs to generate SEPA Direct Debit XML Files (SEPA Basis-Lastschrift)
+ *  
+ * (c) 2013 by Roger Sennert, Blue Star Software
+ *
+ * Term of use:
+ * - You are NOT allowed to redistribute this class. 
+ * - You can use this class as long as you credit to us. 
+ *   When using in a web-service or in a web-app you have to add a notice to your Impressum (or something similar) 
+ *   that you are using this class AND link to http://www.bluestarsoftware.de
+ * - If you modify the code please send us your modifications
+ *   If we think that the modifications are useful we'll add them to this class and credit to you  
+ * 
+ * This class is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied 
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * 
+ * If you have any questions please use the contact form on our website: http://www.bluestarsoftware.de/en/contact.html      
