92c3a7b0494c92aad2ee5d8401403ed70f1da5e0
diddipoeler <famploeger@arcor.de>
2014-02-16 15:00:55 +0100

    update backend

--- admin/models/jlextsisimport.php
+++ admin/models/jlextsisimport.php
@@ -809,8 +809,17 @@ JFile::write($file, $xmlfile);
                     if (function_exists('curl_version'))
 {
     $curl = curl_init();
+    //Define header array for cURL requestes
+    $header = array('Contect-Type:application/xml');
     curl_setopt($curl, CURLOPT_URL, $linkresults);
+    curl_setopt($curl, CURLOPT_VERBOSE, 1);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
+    //curl_setopt($curl, CURLOPT_POST, 1);
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($curl, CURLOPT_HTTPHEADER , $header);
+    
+    
     $content = curl_exec($curl);
     curl_close($curl);
 }
@@ -838,8 +847,15 @@ else
             if (function_exists('curl_version'))
 {
     $curl = curl_init();
+    //Define header array for cURL requestes
+    $header = array('Contect-Type:application/xml');
     curl_setopt($curl, CURLOPT_URL, $linkresults);
+    curl_setopt($curl, CURLOPT_VERBOSE, 1);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
+    //curl_setopt($curl, CURLOPT_POST, 1);
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($curl, CURLOPT_HTTPHEADER , $header);
     $content = curl_exec($curl);
     curl_close($curl);
 }
