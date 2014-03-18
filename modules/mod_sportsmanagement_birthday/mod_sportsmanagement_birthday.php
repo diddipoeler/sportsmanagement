164875aff0dedd6dc3430e0a080c7bd5794b3adf
diddipoeler <famploeger@arcor.de>
2014-03-18 13:54:39 +0100

    frontend modul mod_sportsmanagement_birthday

---
+++ modules/mod_sportsmanagement_birthday/mod_sportsmanagement_birthday.php
@@ -0,0 +1,143 @@
+<?php
+/** SportsManagement ein Programm zur Verwaltung für alle Sportarten
+* @version         1.0.05
+* @file                agegroup.php
+* @author                diddipoeler, stony, svdoldie und donclumsy (diddipoeler@arcor.de)
+* @copyright        Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+* @license                This file is part of SportsManagement.
+*
+* SportsManagement is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* SportsManagement is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with SportsManagement.  If not, see <http://www.gnu.org/licenses/>.
+*
+* Diese Datei ist Teil von SportsManagement.
+*
+* SportsManagement ist Freie Software: Sie können es unter den Bedingungen
+* der GNU General Public License, wie von der Free Software Foundation,
+* Version 3 der Lizenz oder (nach Ihrer Wahl) jeder späteren
+* veröffentlichten Version, weiterverbreiten und/oder modifizieren.
+*
+* SportsManagement wird in der Hoffnung, dass es nützlich sein wird, aber
+* OHNE JEDE GEWÄHELEISTUNG, bereitgestellt; sogar ohne die implizite
+* Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
+* Siehe die GNU General Public License für weitere Details.
+*
+* Sie sollten eine Kopie der GNU General Public License zusammen mit diesem
+* Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
+*
+* Note : All ini files need to be saved as UTF-8 without BOM
+*/
+
+defined('_JEXEC') or die('Restricted access');
+
+require_once(JPATH_SITE.DS.'components'.DS.'com_sportsmanagement'.DS.'sportsmanagement.php');
+require_once(dirname(__FILE__).DS.'helper.php');
+$document = JFactory::getDocument();
+$show_debug_info = JComponentHelper::getParams('com_sportsmanagement')->get('show_debug_info',0) ;
+
+//add css file
+$document->addStyleSheet(JURI::base().'modules/mod_sportsmanagement_birthday/css/mod_sportsmanagement_birthday.css');
+
+// Prevent that result is null when either $players or $crew is null by casting each to an array.
+$persons = array_merge((array)$players, (array)$crew);
+if(count($persons)>1)   $persons = jl_birthday_sort($persons, array("n+days_to_birthday", "n".$params->get('sort_order')."age"), false);
+
+if ( $show_debug_info )
+{
+echo 'this->mod_sportsmanagement_birthday persons<br /><pre>~' . print_r($persons,true) . '~</pre><br />';
+echo 'this->mod_sportsmanagement_birthday params<br /><pre>~' . print_r($params,true) . '~</pre><br />';
+}
+
+$k=0;
+$counter=0;
+?>
+<table class="birthday">
+<?php
+if(count($persons) > 0) {
+	foreach ($persons AS $person) {
+		if (($params->get('limit')> 0) && ($counter == intval($params->get('limit')))) break;
+		$class = ($k == 0)? $params->get('sectiontableentry1') : $params->get('sectiontableentry2');
+
+		$thispic = "";
+		$flag = $params->get('show_player_flag')? JSMCountries::getCountryFlag($person['country']) . "&nbsp;" : "";
+		$text = htmlspecialchars(sportsmanagementHelper::formatName(null, $person['firstname'], 
+													$person['nickname'], 
+													$person['lastname'], 
+													$params->get("name_format")), ENT_QUOTES, 'UTF-8');
+		$usedname = $flag.$text;
+		
+		$person_link = "";
+		$person_type = $person['type'];
+		if($person_type==1) {
+			$person_link = sportsmanagementHelperRoute::getPlayerRoute($person['project_id'],
+																$person['team_id'],
+																$person['id']);
+		} else if($person_type==2) {
+			$person_link = sportsmanagementHelperRoute::getStaffRoute($person['project_id'],
+																$person['team_id'],
+																$person['id']);
+		} else if($person_type==3) {
+			$person_link = sportsmanagementHelperRoute::getRefereeRoute($person['project_id'],
+																$person['team_id'],
+																$person['id']);
+		}
+		$showname = JHTML::link( $person_link, $usedname );
+		?>
+	<tr class="<?php echo $params->get('heading_style');?>">
+		<td class="birthday"><?php echo $showname;?></td>
+	</tr>
+	<tr class="<?php echo $class;?>">
+		<td class="birthday">
+		<?php
+		if ($params->get('show_picture')==1) {
+			if (file_exists(JPATH_BASE.'/'.$person['picture'])&&$person['picture']!='') {
+				$thispic = $person['picture'];
+			}
+			elseif (file_exists(JPATH_BASE.'/'.$person['default_picture'])&&$person['default_picture']!='') {
+				$thispic = $person['default_picture'];
+			}
+			echo '<img src="'.JURI::base().'/'.$thispic.'" alt="'.$text.'" title="'.$text.'"';
+			if ($params->get('picture_width') != '') echo ' width="'.$params->get('picture_width').'"';
+			echo ' /><br />';
+
+		}
+		switch ($person['days_to_birthday']) {
+			case 0: $whenmessage = $params->get('todaymessage');break;
+			case 1: $whenmessage = $params->get('tomorrowmessage');break;
+			default: $whenmessage = str_replace('%DAYS_TO%', $person['days_to_birthday'], trim($params->get('futuremessage')));break;
+		}
+		$birthdaytext = htmlentities(trim(JText::_($params->get('birthdaytext'))), ENT_COMPAT , 'UTF-8');
+		$dayformat = htmlentities(trim($params->get('dayformat')));
+		$birthdayformat = htmlentities(trim($params->get('birthdayformat')));
+		$birthdaytext = str_replace('%WHEN%', $whenmessage, $birthdaytext);
+		$birthdaytext = str_replace('%AGE%', $person['age'], $birthdaytext);
+		$birthdaytext = str_replace('%DATE%', strftime($dayformat, strtotime($person['year'].'-'.$person['daymonth'])), $birthdaytext);
+		$birthdaytext = str_replace('%DATE_OF_BIRTH%', strftime($birthdayformat, strtotime($person['date_of_birth'])), $birthdaytext);
+		$birthdaytext = str_replace('%BR%', '<br />', $birthdaytext);
+		$birthdaytext = str_replace('%BOLD%', '<b>', $birthdaytext);
+		$birthdaytext = str_replace('%BOLDEND%', '</b>', $birthdaytext);
+			
+		echo $birthdaytext;
+		?></td>
+	</tr>
+	<?php
+	$k = 1 - $k;
+	$counter++;
+	}
+}
+else {
+?>
+<tr>
+	<td class="birthday"><?php echo''.str_replace('%DAYS%', $params->get('maxdays'), htmlentities(trim($params->get('not_found_text')))).''; ?></td>
+</tr>
+<?php } ?>
+</table>
