7d138eaa3317aadcb94f860b85c18f002df43cd0
stony <webmaster@sport4u.at>
2014-03-24 17:38:30 +0100

    update backend/frontend
    Signed-off-by: stony <webmaster@sport4u.at>

--- site/views/allteams/tmpl/default.php
+++ site/views/allteams/tmpl/default.php
@@ -1,33 +1,33 @@
 <?php 
-/** SportsManagement ein Programm zur Verwaltung für alle Sportarten
-* @version         1.0.05
-* @file                agegroup.php
-* @author                diddipoeler, stony, svdoldie und donclumsy (diddipoeler@arcor.de)
-* @copyright        Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
-* @license                This file is part of SportsManagement.
+/** Joomla Sports Management ein Programm zur Verwaltung für alle Sportarten
+* @version 1.0.26
+* @file components/sportsmanagement/views/allteams/tmpl/default.php
+* @author diddipoeler, stony, svdoldie und donclumsy (diddipoeler@arcor.de)
+* @copyright Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+* @license This file is part of Joomla Sports Management.
 *
-* SportsManagement is free software: you can redistribute it and/or modify
+* Joomla Sports Management is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
 * the Free Software Foundation, either version 3 of the License, or
 * (at your option) any later version.
 *
-* SportsManagement is distributed in the hope that it will be useful,
+* Joomla Sports Management is distributed in the hope that it will be useful,
 * but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 * GNU General Public License for more details.
 *
 * You should have received a copy of the GNU General Public License
-* along with SportsManagement.  If not, see <http://www.gnu.org/licenses/>.
+* along with Joomla Sports Management. If not, see <http://www.gnu.org/licenses/>.
 *
-* Diese Datei ist Teil von SportsManagement.
+* Diese Datei ist Teil von Joomla Sports Management.
 *
-* SportsManagement ist Freie Software: Sie können es unter den Bedingungen
+* Joomla Sports Management ist Freie Software: Sie können es unter den Bedingungen
 * der GNU General Public License, wie von der Free Software Foundation,
 * Version 3 der Lizenz oder (nach Ihrer Wahl) jeder späteren
 * veröffentlichten Version, weiterverbreiten und/oder modifizieren.
 *
-* SportsManagement wird in der Hoffnung, dass es nützlich sein wird, aber
-* OHNE JEDE GEWÄHELEISTUNG, bereitgestellt; sogar ohne die implizite
+* Joomla Sports Management wird in der Hoffnung, dass es nützlich sein wird, aber
+* OHNE JEDE GEWÄHRLEISTUNG, bereitgestellt; sogar ohne die implizite
 * Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
 * Siehe die GNU General Public License für weitere Details.
 *
@@ -68,7 +68,7 @@ function tableOrdering( order, dir, task )
 	<legend class="hidelabeltxt"><?php echo JText::_('JGLOBAL_FILTER_LABEL'); ?></legend>
 	<div class="filter-search">
 
-		<label class="filter_search-lbl" for="filter_search"><?php echo JText::_('COM_SPORTSMANAGEMENT_FILTER_LABEL').'&#160;'; ?></label>
+		<label class="filter_search-lbl" for="filter_search"><?php echo JText::_('JSEARCH_FILTER_LABEL').':&#160;'; ?></label>
 		<input type="text" name="filter_search" id="filter_search" value="<?php echo $this->escape($this->filter); ?>" class="inputbox" onchange="document.getElementById('adminForm').submit();" />
 
 		<button type="submit" class="button"><?php echo JText::_('JGLOBAL_FILTER_BUTTON'); ?></button>
