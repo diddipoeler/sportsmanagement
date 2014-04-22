1ed626d400706f3f46d4c559788bdb96cd460390
diddipoeler <famploeger@arcor.de>
2014-04-22 15:55:44 +0200

    update frontend

--- site/controllers/editperson.php
+++ site/controllers/editperson.php
@@ -55,6 +55,21 @@ jimport('joomla.application.component.controllerform');
 class sportsmanagementControllereditperson extends JControllerForm
 {
  
+ /**
+	 * Class Constructor
+	 *
+	 * @param	array	$config		An optional associative array of configuration settings.
+	 * @return	void
+	 * @since	1.5
+	 */
+	function __construct($config = array())
+	{
+		parent::__construct($config);
+
+		// Map the apply task to the save method.
+		$this->registerTask('apply', 'save');
+	}
+    
         public function getModel($name = '', $prefix = '', $config = array('ignore_request' => true))
         {
                 return parent::getModel($name, $prefix, array('ignore_request' => false));
@@ -87,20 +102,37 @@ class sportsmanagementControllereditperson extends JControllerForm
         
         public function save()
         {
-            $msg = 'save';
-            $this->setRedirect('index.php?option=com_sportsmanagement&view=close&tmpl=component',$msg);
+            $data	= JRequest::getVar('jform', array(), 'post', 'array');
+		    $id		= JRequest::getInt('id');
+        
+//            $msg = 'save';
+//            $this->setRedirect('index.php?option=com_sportsmanagement&view=close&tmpl=component',$msg);
+            
+            // Set the redirect based on the task.
+		switch ($this->getTask())
+		{
+			case 'apply':
+				$message = JText::_('COM_SPORTSMANAGEMENT_SAVE_SUCCESS');
+				$this->setRedirect('index.php?option=com_sportsmanagement&view=editperson&tmpl=component&id='.$id, $message);
+				break;
+
+			case 'save':
+			default:
+				$this->setRedirect('index.php?option=com_sportsmanagement&view=close&tmpl=component');
+				break;
+		}
 
  
                 return true;
         }
         
-        public function apply()
-        {
-            $msg = 'apply';
-            $this->setRedirect('index.php?option=com_sportsmanagement&view=close&tmpl=component',$msg);
-
- 
-                return true;
-        }
+//        public function apply()
+//        {
+////            $msg = 'apply';
+////            $this->setRedirect('index.php?option=com_sportsmanagement&view=close&tmpl=component',$msg);
+//
+// 
+//                return true;
+//        }
  
 }
