44612ae552af6732e1ad0ea13c4e7d93887887a4
stony <webmaster@sport4u.at>
2014-06-17 23:01:00 +0200

    bugtracker id 0000084

--- modules/mod_sportsmanagement_playgroundplan/mod_sportsmanagement_playgroundplan.php
+++ modules/mod_sportsmanagement_playgroundplan/mod_sportsmanagement_playgroundplan.php
@@ -41,7 +41,12 @@
 // no direct access
 defined('_JEXEC') or die('Restricted access');
 
+
 //require_once(JPATH_SITE.DS.'components'.DS.'com_sportsmanagement'.DS.'sportsmanagement.php');
+DEFINE( 'JSM_PATH','components/com_sportsmanagement' );
+
+require_once(JPATH_ADMINISTRATOR.DS.JSM_PATH.DS.'helpers'.DS.'sportsmanagement.php');
+require_once(JPATH_SITE.DS.JSM_PATH.DS.'helpers'.DS.'route.php'); 
 
 // get helper
 require_once (dirname(__FILE__).DS.'helper.php');
