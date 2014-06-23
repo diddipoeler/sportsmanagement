733a7dd1a60c9f287c6e37b3e4fa8a8220993bb2
diddipoeler <famploeger@arcor.de>
2014-06-23 10:03:16 +0200

    update frontend

--- site/helpers/imageselect.php
+++ site/helpers/imageselect.php
@@ -183,7 +183,7 @@ var $_foldertype = '';
 		//check if the imagefiletype is valid
 		$fileext	= JFile::getExt($file['name']);
 
-		$allowable	= array ( 'gif', 'jpg', 'png' );
+		$allowable	= array ( 'gif', 'jpg', 'png','GIF', 'JPG', 'PNG' );
 		if ( !in_array( $fileext, $allowable ) )
 		{
 			JError::raiseWarning( 100, JText::_( 'COM_SPORTSMANAGEMENT_ADMIN_IMAGEHANDLER_ERROR1' ) . ': ' . htmlspecialchars( $file['name'], ENT_COMPAT, 'UTF-8' ) );
