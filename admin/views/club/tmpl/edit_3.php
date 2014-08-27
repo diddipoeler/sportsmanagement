ff88434e822012886221dbe9684a0b2c48027d2d
diddipoeler <famploeger@arcor.de>
2014-08-27 16:00:33 +0200

    joomla 3

--- admin/views/club/tmpl/edit_3.php
+++ admin/views/club/tmpl/edit_3.php
@@ -52,46 +52,26 @@ JHtml::_('behavior.formvalidation');
 JHtml::_('behavior.modal');
 $params = $this->form->getFieldsets('params');
 
-$options = array(
-    'onActive' => 'function(title, description){
-        description.setStyle("display", "block");
-        title.addClass("open").removeClass("closed");
-    }',
-    'onBackground' => 'function(title, description){
-        description.setStyle("display", "none");
-        title.addClass("closed").removeClass("open");
-    }',
-    'startOffset' => 0,  // 0 starts on the first tab, 1 starts the second, etc...
-    'useCookie' => true, // this must not be a string. Don't use quotes.
-);
+//$options = array(
+//    'onActive' => 'function(title, description){
+//        description.setStyle("display", "block");
+//        title.addClass("open").removeClass("closed");
+//    }',
+//    'onBackground' => 'function(title, description){
+//        description.setStyle("display", "none");
+//        title.addClass("closed").removeClass("open");
+//    }',
+//    'startOffset' => 0,  // 0 starts on the first tab, 1 starts the second, etc...
+//    'useCookie' => true, // this must not be a string. Don't use quotes.
+//);
 
 // Get the form fieldsets.
 $fieldsets = $this->form->getFieldsets();
 ?>
 <form action="<?php echo JRoute::_('index.php?option=com_sportsmanagement&layout=edit&id='.(int) $this->item->id .'&tmpl='.$this->tmpl); ?>" method="post" name="adminForm" id="adminForm">
 
-<?PHP
-if ( $this->tmpl )
-{
-		?>
-			<fieldset>
-				<div class="fltrt">
-					<button type="button" onclick="Joomla.submitform('club.apply', this.form);">
-						<?php echo JText::_('JAPPLY');?></button>
-					<button type="button" onclick="Joomla.submitform('club.save', this.form);">
-						<?php echo JText::_('JSAVE');?></button>
-					<button id="cancel" type="button" onclick="<?php echo JRequest::getBool('refresh', 0) ? 'window.parent.location.href=window.parent.location.href;' : '';?>  window.parent.SqueezeBox.close();">
-						<?php echo JText::_('JCANCEL');?></button>
-				
-                
-                </div>
-				
-			</fieldset>
-<?PHP                
-}
-?> 
-<div class="width-60 fltlft">
-           
+
+
 <?PHP
                 if ( !$this->item->id )
                 {
@@ -104,109 +84,57 @@ if ( $this->tmpl )
                 <?PHP
                 }
                 ?>
+                
+<div class="form-horizontal">
+<?php echo JHtml::_('bootstrap.startTabSet', 'myTab', array('active' => 'details')); ?>
 
+<?PHP    
+foreach ($fieldsets as $fieldset) 
+{
+echo JHtml::_('bootstrap.addTab', 'myTab', $fieldset->name, JText::_($fieldset->label, true));    
 
-		<fieldset class="adminform">
-			<legend><?php echo JText::_('COM_SPORTSMANAGEMENT_TABS_DETAILS'); ?></legend>
-			<ul class="adminformlist">
-			<?php 
-            foreach($this->form->getFieldset('details') as $field) :
-            
-            //echo '<pre>'.print_r($field,true).'</pre>';
-            ?>
-				<li><?php echo $field->label; ?>
-				<?php echo $field->input; 
-                 
-                if ( $field->name == 'jform[country]' )
-                {
-                echo JSMCountries::getCountryFlag($field->value);    
-                }
-                
-                if ( $field->name == 'jform[standard_playground]' )
-                {
-                //echo sportsmanagementHelper::getPicturePlayground($field->value);
-                $picture = sportsmanagementHelper::getPicturePlayground($field->value);
-                //echo $picture;
-                //echo JHtml::image($picture, 'Playground', array('title' => 'Playground','width' => '50' )); 
-                //echo JHtml::_('image', $picture, 'Playground',array('title' => 'Playground','width' => '50' )); 
-?>
-<a href="<?php echo JURI::root().$picture;?>" title="<?php echo 'Playground';?>" class="modal">
-<img src="<?php echo JURI::root().$picture;?>" alt="<?php echo 'Playground';?>" width="50" />
-</a>
-<?PHP                   
-                }
-                
-                if ( $field->name == 'jform[website]' )
-                {
-                echo '<img style="" src="http://www.thumbshots.de/cgi-bin/show.cgi?url='.$field->value.'">';  
-                }
-                if ( $field->name == 'jform[twitter]' )
-                {
-                echo '<img style="" src="http://www.thumbshots.de/cgi-bin/show.cgi?url='.$field->value.'">';  
-                }
-                if ( $field->name == 'jform[facebook]' )
-                {
-                echo '<img style="" src="http://www.thumbshots.de/cgi-bin/show.cgi?url='.$field->value.'">';  
-                }
-                
-                $suchmuster = array ("jform[","]");
-                $ersetzen = array ('', '');
-                $var_onlinehelp = str_replace($suchmuster, $ersetzen, $field->name);
-                
-                switch ($var_onlinehelp)
-                {
-                    case 'id':
-                    break;
-                    default:
-                    if ( $field->type != 'Hidden')
-                    {
-                ?>
-                <a	rel="{handler: 'iframe',size: {x: <?php echo COM_SPORTSMANAGEMENT_MODAL_POPUP_WIDTH; ?>,y: <?php echo COM_SPORTSMANAGEMENT_MODAL_POPUP_HEIGHT; ?>}}"
-									href="<?php echo COM_SPORTSMANAGEMENT_HELP_SERVER.'SM-Backend-Felder:'.JRequest::getVar( "view").'-'.$var_onlinehelp; ?>"
-									 class="modal">
-									<?php
-									echo JHtml::_(	'image','media/com_sportsmanagement/jl_images/help.png',
-													JText::_('COM_SPORTSMANAGEMENT_HELP_LINK'),'title= "' .
-													JText::_('COM_SPORTSMANAGEMENT_HELP_LINK').'"');
-									?>
-								</a>
-                
-                <?PHP
-                }
-                break;
-                }
-                ?>
-                </li>
-			<?php 
-            
-            //echo $field->type;
-            
-            endforeach; 
-            ?>
-			</ul>
-		</fieldset>
-	</div>
+switch ($fieldset->name)
+{
+    case 'details':
+    ?>
+    <div class="row-fluid">
+			<div class="span9">
+				<div class="row-fluid form-horizontal-desktop">
+					<div class="span6">
+    <?PHP
+    foreach( $this->form->getFieldset($fieldset->name) as $field ) 
+    {
+        ?>
+					<div class="control-group">
+						<div class="control-label">
+							<?php echo $field->label; ?>
+						</div>
+						<div class="controls">
+							<?php echo $field->input; ?>
+						</div>
+					</div>
+				<?php
 
-<div class="width-40 fltrt">
-		<?php
-		echo JHtml::_('sliders.start');
-		foreach ($fieldsets as $fieldset) :
-			if ($fieldset->name == 'details') :
-				continue;
-			endif;
-			echo JHtml::_('sliders.panel', JText::_($fieldset->label), $fieldset->name);
-		if (isset($fieldset->description) && !empty($fieldset->description)) :
-				echo '<p class="tab-description">'.JText::_($fieldset->description).'</p>';
-			endif;
-		//echo $this->loadTemplate($fieldset->name);
-        $this->fieldset = $fieldset->name;
-        echo $this->loadTemplate('fieldsets');
-		endforeach; ?>
-		<?php echo JHtml::_('sliders.end'); ?>
+    }
+    ?>
+    </div>
+				</div>
+			</div>
+            </div>
+    <?PHP
+    break;
+    default:
+    $this->fieldset = $fieldset->name;
+    echo $this->loadTemplate('fieldsets');
+    break;
+}    
+echo JHtml::_('bootstrap.endTab');    
+}    
 
+?>    
 	
-	</div>
-
+<?php echo JHtml::_('bootstrap.endTabSet'); ?>
+</div> 
 
     
  <div class="clr"></div>
