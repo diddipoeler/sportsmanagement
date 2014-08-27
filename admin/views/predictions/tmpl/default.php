b21756f8242f2edebc1705ba727460abc4646eb3
diddipoeler <famploeger@arcor.de>
2014-08-27 09:55:29 +0200

    joomla 3

--- admin/views/predictions/tmpl/default.php
+++ admin/views/predictions/tmpl/default.php
@@ -41,6 +41,16 @@
 // Disallow direct access to this file
 defined('_JEXEC') or die('Restricted access');
 ?>
+
+<?php if (!empty( $this->sidebar)) : ?>
+	<div id="j-sidebar-container" class="span2">
+		<?php echo $this->sidebar; ?>
+	</div>
+	<div id="j-main-container" class="span10">
+<?php else : ?>
+	<div id="j-main-container">
+<?php endif;?>
+
 <table width="100%" border="0">
 	<tr>
 		<td width="100%" valign="top">
