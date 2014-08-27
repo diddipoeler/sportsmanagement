87329fc8f1202b2ac25eee1a9f1e10ccf7613fa1
diddipoeler <famploeger@arcor.de>
2014-08-27 14:23:17 +0200

    joomla 3

--- admin/views/smquotes/view.html.php
+++ admin/views/smquotes/view.html.php
@@ -97,10 +97,7 @@ class sportsmanagementViewsmquotes extends sportsmanagementView
 		$this->assignRef('pagination',$pagination);
 		$this->assign('request_url',$uri->toString());
         
-        if ( COM_SPORTSMANAGEMENT_JOOMLAVERSION != '2.5' )
-        {
-        sportsmanagementHelper::addSubmenu('menu');
-        }
+        
 		
 	}
 	
@@ -111,14 +108,14 @@ class sportsmanagementViewsmquotes extends sportsmanagementView
 	*/
 	protected function addToolbar()
 	{
-		// Get a refrence of the page instance in joomla
-		$document	= JFactory::getDocument();
-        // Set toolbar items for the page
-        $stylelink = '<link rel="stylesheet" href="'.JURI::root().'administrator/components/com_sportsmanagement/assets/css/jlextusericons.css'.'" type="text/css" />' ."\n";
-        $document->addCustomTag($stylelink);
-        
-        // Set toolbar items for the page
-		JToolBarHelper::title(JText::_('COM_SPORTSMANAGEMENT_ADMIN_QUOTES_TITLE'),'quotes');
+	//	// Get a refrence of the page instance in joomla
+//		$document	= JFactory::getDocument();
+//        // Set toolbar items for the page
+//        $stylelink = '<link rel="stylesheet" href="'.JURI::root().'administrator/components/com_sportsmanagement/assets/css/jlextusericons.css'.'" type="text/css" />' ."\n";
+//        $document->addCustomTag($stylelink);
+//        
+//        // Set toolbar items for the page
+//		JToolBarHelper::title(JText::_('COM_SPORTSMANAGEMENT_ADMIN_QUOTES_TITLE'),'quotes');
 		JToolBarHelper::addNew('smquote.add');
 		JToolBarHelper::editList('smquote.edit');
 		JToolBarHelper::custom('smquote.import','upload','upload',JText::_('JTOOLBAR_UPLOAD'),false);
