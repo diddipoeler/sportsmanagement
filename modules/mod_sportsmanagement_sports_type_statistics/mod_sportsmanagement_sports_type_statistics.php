7ffd1f3a07d0e376fe711073070511338892f727
diddipoeler <famploeger@arcor.de>
2015-05-07 09:00:30 +0200

    mod_joomleague_sports_type_statistics (bootstrap)

---
+++ modules/mod_sportsmanagement_sports_type_statistics/mod_sportsmanagement_sports_type_statistics.php
@@ -0,0 +1,70 @@
+<?php
+/** SportsManagement ein Programm zur Verwaltung für alle Sportarten
+* @version         1.0.05
+* @file                agegroups.php
+* @author                diddipoeler, stony und svdoldie (diddipoeler@arcor.de)
+* @copyright        Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+* @license                This file is part of SportsManagement.
+*
+*        SportsManagement is free software: you can redistribute it and/or modify
+*        it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation, either version 3 of the License, or
+*  (at your option) any later version.
+*
+*  SportsManagement is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  You should have received a copy of the GNU General Public License
+*  along with SportsManagement.  If not, see <http://www.gnu.org/licenses/>.
+*
+*  Diese Datei ist Teil von SportsManagement.
+*
+*  SportsManagement ist Freie Software: Sie können es unter den Bedingungen
+*  der GNU General Public License, wie von der Free Software Foundation,
+*  Version 3 der Lizenz oder (nach Ihrer Wahl) jeder späteren
+*  veröffentlichten Version, weiterverbreiten und/oder modifizieren.
+*
+*  SportsManagement wird in der Hoffnung, dass es nützlich sein wird, aber
+*  OHNE JEDE GEWÄHELEISTUNG, bereitgestellt; sogar ohne die implizite
+*  Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
+*  Siehe die GNU General Public License für weitere Details.
+*
+*  Sie sollten eine Kopie der GNU General Public License zusammen mit diesem
+*  Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
+*
+* Note : All ini files need to be saved as UTF-8 without BOM
+*/
+
+// no direct access
+defined('_JEXEC') or die('Restricted access');
+
+// get helper
+require_once (dirname(__FILE__).DS.'helper.php');
+
+//require_once(JPATH_SITE.DS.'components'.DS.'com_joomleague'.DS.'joomleague.core.php');
+$sportstypes = $params->get('sportstypes');
+$data = modJSMSportsHelper::getData($params);
+
+$document = JFactory::getDocument();
+//add css file
+$document->addStyleSheet(JURI::base().'modules/mod_sportsmanagement_sports_type_statistics/css/mod_sportsmanagement_sports_type_statistics.css');
+
+/**
+ * wenn die komponente im frontend nicht geladen oder aufgerufen wurde,
+ * dann muss die sprachdatei aus dem backend geladen werden.
+ * ansonsten wird der übersetzte text der sportart nicht angezeigt.
+ */
+if (! defined($data['sportstype'][0]->name) )
+{
+$langtag = JFactory::getLanguage();
+$extension = 'com_sportsmanagement';
+$base_dir = JPATH_ADMINISTRATOR;
+$language_tag = $langtag->getTag();
+$reload = true;
+$lang->load($extension, $base_dir, $language_tag, $reload);
+}
+
+
+require(JModuleHelper::getLayoutPath('mod_sportsmanagement_sports_type_statistics'));
