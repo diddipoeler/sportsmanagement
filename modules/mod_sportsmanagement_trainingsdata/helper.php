32c61554ec26171c016d3662069de5a1a7234059
diddipoeler <famploeger@arcor.de>
2015-05-28 07:58:04 +0200

    bugtracker 0000094

---
+++ modules/mod_sportsmanagement_trainingsdata/helper.php
@@ -0,0 +1,93 @@
+<?php
+/** SportsManagement ein Programm zur Verwaltung für alle Sportarten
+ * @version         1.0.05
+ * @file                agegroup.php
+ * @author                diddipoeler, stony, svdoldie und donclumsy (diddipoeler@arcor.de)
+ * @copyright        Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license                This file is part of SportsManagement.
+ *
+ * SportsManagement is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SportsManagement is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SportsManagement.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Diese Datei ist Teil von SportsManagement.
+ *
+ * SportsManagement ist Freie Software: Sie können es unter den Bedingungen
+ * der GNU General Public License, wie von der Free Software Foundation,
+ * Version 3 der Lizenz oder (nach Ihrer Wahl) jeder späteren
+ * veröffentlichten Version, weiterverbreiten und/oder modifizieren.
+ *
+ * SportsManagement wird in der Hoffnung, dass es nützlich sein wird, aber
+ * OHNE JEDE GEWÄHELEISTUNG, bereitgestellt; sogar ohne die implizite
+ * Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
+ * Siehe die GNU General Public License für weitere Details.
+ *
+ * Sie sollten eine Kopie der GNU General Public License zusammen mit diesem
+ * Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
+ *
+ * Note : All ini files need to be saved as UTF-8 without BOM
+ */
+
+// no direct access
+defined('_JEXEC') or die('Restricted access');
+
+
+
+/**
+ * modJSMTrainingsData
+ * 
+ * @package 
+ * @author abcde
+ * @copyright 2015
+ * @version $Id$
+ * @access public
+ */
+class modJSMTrainingsData
+{
+
+   
+    /**
+     * modJSMTrainingsData::getData()
+     * 
+     * @param mixed $params
+     * @return
+     */
+    public static function getData($params)
+    {
+        $app = JFactory::getApplication();
+        // JInput object
+        $jinput = $app->input;
+        $cfg_which_database = JRequest::getInt('cfg_which_database', 0);
+        // Get a db connection.
+        $db = sportsmanagementHelper::getDBConnection(true, $cfg_which_database);
+        // Create a new query object.
+        $query = $db->getQuery(true);
+        
+        $result = array();
+        
+        $query->select('*');
+        $query->from('#__sportsmanagement_team_trainingdata'); 
+        $query->where('team_id = '. $params->get('team') );  
+        $query->order('dayofweek ASC');
+		
+        //$app->enqueueMessage(JText::_(__METHOD__.' '.__LINE__.' params'.'<pre>'.print_r($params,true).'</pre>' ),'');
+        //$app->enqueueMessage(JText::_(__METHOD__.' '.__LINE__.' dump'.'<pre>'.print_r($query->dump(),true).'</pre>' ),'');
+        
+        $db->setQuery($query);
+		$result = $db->loadObjectList();
+
+        
+        return $result;
+
+    }
+
+}
