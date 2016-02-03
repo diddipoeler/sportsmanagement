86e6d37c0369b43ddf4166123ab378b63068c76c
diddipoeler <famploeger@arcor.de>
2016-02-03 12:20:50 +0100

    Update default.php

--- admin/views/githubinstall/tmpl/default.php
+++ admin/views/githubinstall/tmpl/default.php
@@ -1,32 +1,32 @@
 <?php 
-/** SportsManagement ein Programm zur Verwaltung für alle Sportarten
-* @version         1.0.05
-* @file                agegroup.php
-* @author                diddipoeler, stony, svdoldie und donclumsy (diddipoeler@arcor.de)
-* @copyright        Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
-* @license                This file is part of SportsManagement.
+/** Joomla Sports Management ein Programm zur Verwaltung für alle Sportarten
+* @version 1.0.26
+* @file		components/sportsmanagement/views/results/tmpl/default.php
+* @author diddipoeler, stony, svdoldie und donclumsy (diddipoeler@arcor.de)
+* @copyright Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+* @license This file is part of Joomla Sports Management.
 *
-* SportsManagement is free software: you can redistribute it and/or modify
+* Joomla Sports Management is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
 * the Free Software Foundation, either version 3 of the License, or
 * (at your option) any later version.
 *
-* SportsManagement is distributed in the hope that it will be useful,
+* Joomla Sports Management is distributed in the hope that it will be useful,
 * but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 * GNU General Public License for more details.
 *
 * You should have received a copy of the GNU General Public License
-* along with SportsManagement.  If not, see <http://www.gnu.org/licenses/>.
+* along with Joomla Sports Management. If not, see <http://www.gnu.org/licenses/>.
 *
-* Diese Datei ist Teil von SportsManagement.
+* Diese Datei ist Teil von Joomla Sports Management.
 *
-* SportsManagement ist Freie Software: Sie können es unter den Bedingungen
+* Joomla Sports Management ist Freie Software: Sie können es unter den Bedingungen
 * der GNU General Public License, wie von der Free Software Foundation,
 * Version 3 der Lizenz oder (nach Ihrer Wahl) jeder späteren
 * veröffentlichten Version, weiterverbreiten und/oder modifizieren.
 *
-* SportsManagement wird in der Hoffnung, dass es nützlich sein wird, aber
+* Joomla Sports Management wird in der Hoffnung, dass es nützlich sein wird, aber
 * OHNE JEDE GEWÄHRLEISTUNG, bereitgestellt; sogar ohne die implizite
 * Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
 * Siehe die GNU General Public License für weitere Details.
@@ -38,28 +38,48 @@
 */
 
 defined('_JEXEC') or die('Restricted access');
+$templatesToLoad = array('footer','listheader');
+sportsmanagementHelper::addTemplatePaths($templatesToLoad, $this);
+JHtml::_( 'behavior.tooltip' );
 
-//echo $this->github_link;
 
 ?>
+
 <form action="<?php echo $this->request_url; ?>" method="post" id="adminForm" name="adminForm">
-<p style='text-align:right;'>
 <?PHP
-foreach ($this->_success_text as $key => $value)
-		{
-			?>
-			<fieldset>
-				<legend><?php echo JText::_($key); ?></legend>
-				<table class='adminlist'><tr><td><?php echo $value; ?></td></tr></table>
-			</fieldset>
-			<?php
-		}  
-?>		
-<input class='button' type='submit' value='<?php echo JText::_('JSAVE'); ?>' />
-</p>
-<input type="hidden" name="task"				value="githubinstall.store" />
+// welche joomla version ?
+if(version_compare(JVERSION,'3.0.0','ge')) 
+{
+echo $this->loadTemplate('joomla3');
+}
+else
+{
+echo $this->loadTemplate('joomla2');    
+}
+
+//echo $this->loadTemplate('data');
+
+foreach ($this->_success_text as $key => $value) 
+		{ 
+			?> 
+			<fieldset> 
+				<legend><?php echo JText::_($key); ?></legend> 
+				<table class='adminlist'><tr><td><?php echo $value; ?></td></tr></table> 
+			</fieldset> 
+			<?php 
+		}   
+		 
+
+
+?>
+	<input type="hidden" name="task"				value="" />
 	<input type="hidden" name="boxchecked"			value="0" />
-	<input type="hidden" name="filter_order"		value="" />
+	<input type="hidden" name="filter_order"		value="<?php echo $this->sortColumn; ?>" />
 	<input type="hidden" name="filter_order_Dir"	value="" />
 	<?php echo JHtml::_( 'form.token' ); ?>
 </form>
+<?PHP
+echo "<div>";
+echo $this->loadTemplate('footer');
+echo "</div>";
+?>   
