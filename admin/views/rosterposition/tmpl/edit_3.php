d74c774e3a053f79d63695bb5b677f1ab1d21021
diddipoeler <famploeger@arcor.de>
2016-02-04 08:47:26 +0100

    bugtracker 0000270

--- admin/views/rosterposition/tmpl/edit_3.php
+++ admin/views/rosterposition/tmpl/edit_3.php
@@ -47,8 +47,8 @@ $params = $this->form->getFieldsets('params');
 // Get the form fieldsets.
 $fieldsets = $this->form->getFieldsets();
 
-echo ' params<br><pre>'.print_r($params,true).'</pre><br>';
-echo ' fieldsets<br><pre>'.print_r($fieldsets,true).'</pre><br>';
+//echo ' params<br><pre>'.print_r($params,true).'</pre><br>';
+//echo ' fieldsets<br><pre>'.print_r($fieldsets,true).'</pre><br>';
 
 
 ?>
