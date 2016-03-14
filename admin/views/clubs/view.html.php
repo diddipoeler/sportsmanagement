be94433050537d85089c5303063ff86e7d3f59d6
diddipoeler <famploeger@arcor.de>
2016-03-14 12:16:47 +0100

    Update view.html.php

--- admin/views/clubs/view.html.php
+++ admin/views/clubs/view.html.php
@@ -126,6 +126,10 @@ class sportsmanagementViewClubs extends sportsmanagementView
 
 	
 		$this->lists		= $lists;
+
+/**
+* dadurch werden die spaltenbreiten optimiert
+*/
 $this->document->addStyleSheet(JUri::root() .'administrator/components/com_sportsmanagement/assets/css/form_control.css', 'text/css');	
 		
         
