d0f9b774ecfb6dfefda14717b2eb36c27e03ebc7
diddipoeler <famploeger@arcor.de>
2016-03-29 13:06:31 +0200

    Update default.php

--- site/extensions/jsminlinehockey/admin/views/jsminlinehockey/tmpl/default.php
+++ site/extensions/jsminlinehockey/admin/views/jsminlinehockey/tmpl/default.php
@@ -25,12 +25,16 @@ sportsmanagementHelper::addTemplatePaths($templatesToLoad, $this);
 
 
 <fieldset style='text-align: center; '>
-				<input class='input_box' id='import_package' name='import_package' type='file' size='57' />
-				<input class='button' type='submit' value='<?php echo JText::_('COM_SPORTSMANAGEMENT_ADMIN_XML_IMPORT_UPLOAD_BUTTON'); ?>' />
-			</fieldset>
+<input type="radio" name="check" value="clubs" checked="checked"> Vereine
+<input type="radio" name="check" value="teams"> Mannschaften
+<input type="radio" name="check" value="players"> Spieler
+	
+<input class='input_box' id='import_package' name='import_package' type='file' size='57' />
+<input class='button' type='submit' value='<?php echo JText::_('COM_SPORTSMANAGEMENT_ADMIN_XML_IMPORT_UPLOAD_BUTTON'); ?>' />
+</fieldset>
 <input type='hidden' name='sent' value='1' />
 		
-		<input type='hidden' name='task' value='jsminlinehockey.save' />
+<input type='hidden' name='task' value='jsminlinehockey.save' />
                     
 </form>
 
