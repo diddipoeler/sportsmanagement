d0714af1f058d0378a6a7042f152a5de1a4d498f
diddipoeler <famploeger@arcor.de>
2016-05-11 10:46:20 +0200

    Update default_teams.php

--- admin/views/projectteams/tmpl/default_teams.php
+++ admin/views/projectteams/tmpl/default_teams.php
@@ -381,9 +381,15 @@ $link = 'index.php?option=com_sportsmanagement&view=club&layout=edit&tmpl=compon
                             ?>
                             <br>
 	<input<?php echo $inputappend; ?> type="text" size="25" class="form-control form-control-inline"
-	name="location<?php echo $row->club_id; ?>"
+	name="location<?php echo $row->id; ?>"
 	value="<?php echo $row->location; ?>"
-	onchange="document.getElementById('cb<?php echo $i; ?>').checked=true" />                            
+	onchange="document.getElementById('cb<?php echo $i; ?>').checked=true" />  
+	
+	<input<?php echo $inputappend; ?> type="hidden" size="25" class="form-control form-control-inline"
+	name="club_id<?php echo $row->club_id; ?>"
+	value="<?php echo $row->location; ?>"
+	onchange="document.getElementById('cb<?php echo $i; ?>').checked=true" />  
+	
                             </td>
                             
 							<td class="center"><?php
