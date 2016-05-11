4c3d07af1c2a231f8062f08868cdcca5c765485e
diddipoeler <famploeger@arcor.de>
2016-05-11 10:16:14 +0200

    Update projectteams.php

--- admin/models/projectteams.php
+++ admin/models/projectteams.php
@@ -226,7 +226,7 @@ class sportsmanagementModelProjectteams extends JModelList
 		$query->select('t.name,t.club_id');
 		$query->join('LEFT', '#__sportsmanagement_team AS t on st.team_id = t.id');
         // Join over the club
-		$query->select('c.email AS club_email,c.logo_big as club_logo,c.country,c.latitude,c.longitude');
+		$query->select('c.email AS club_email,c.logo_big as club_logo,c.country,c.latitude,c.longitude,c.location');
 		$query->join('LEFT', '#__sportsmanagement_club AS c on t.club_id = c.id');
         // Join over the playground
 		$query->join('LEFT', '#__sportsmanagement_playground AS plg on plg.id = tl.standard_playground');
