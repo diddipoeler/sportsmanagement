2a3ac378cd5a369ccb0e76801c88c546bba4fdbb
diddipoeler <famploeger@arcor.de>
2016-09-07 08:49:37 +0200

    Update updates.php
    #359

--- admin/models/updates.php
+++ admin/models/updates.php
@@ -1,9 +1,9 @@
 <?php
-/** SportsManagement ein Programm zur Verwaltung für alle Sportarten
+/** SportsManagement ein Programm zur Verwaltung fÃ¼r alle Sportarten
 * @version         1.0.05
 * @file                agegroup.php
 * @author                diddipoeler, stony, svdoldie und donclumsy (diddipoeler@arcor.de)
-* @copyright        Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+* @copyright        Copyright: Â© 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
 * @license                This file is part of SportsManagement.
 *
 * SportsManagement is free software: you can redistribute it and/or modify
@@ -21,15 +21,15 @@
 *
 * Diese Datei ist Teil von SportsManagement.
 *
-* SportsManagement ist Freie Software: Sie können es unter den Bedingungen
+* SportsManagement ist Freie Software: Sie kÃ¶nnen es unter den Bedingungen
 * der GNU General Public License, wie von der Free Software Foundation,
-* Version 3 der Lizenz oder (nach Ihrer Wahl) jeder späteren
-* veröffentlichten Version, weiterverbreiten und/oder modifizieren.
+* Version 3 der Lizenz oder (nach Ihrer Wahl) jeder spÃ¤teren
+* verÃ¶ffentlichten Version, weiterverbreiten und/oder modifizieren.
 *
-* SportsManagement wird in der Hoffnung, dass es nützlich sein wird, aber
-* OHNE JEDE GEWÄHELEISTUNG, bereitgestellt; sogar ohne die implizite
-* Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
-* Siehe die GNU General Public License für weitere Details.
+* SportsManagement wird in der Hoffnung, dass es nÃ¼tzlich sein wird, aber
+* OHNE JEDE GEWÃ„HELEISTUNG, bereitgestellt; sogar ohne die implizite
+* GewÃ¤hrleistung der MARKTFÃ„HIGKEIT oder EIGNUNG FÃœR EINEN BESTIMMTEN ZWECK.
+* Siehe die GNU General Public License fÃ¼r weitere Details.
 *
 * Sie sollten eine Kopie der GNU General Public License zusammen mit diesem
 * Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
@@ -103,7 +103,7 @@ class sportsmanagementModelUpdates extends JModelLegacy
 			$data['revision']=!empty($revision) ? $revision : $result->revision;			
 		}
 /**
-* wird nicht benötigt
+* wird nicht benÃ¶tigt
 */		
 //		if (!$tableVersion->bind($data))
 //		{
@@ -112,12 +112,12 @@ class sportsmanagementModelUpdates extends JModelLegacy
 //			return false;
 //		}
 		// Store the item to the database
-		if (!$store=$tableVersion->store())
-		{
-			echo $this->_db->getErrorMsg();
-			$this->setError($this->_db->getErrorMsg());
-			return false;
-		}
+//		if (!$store=$tableVersion->store())
+//		{
+//			echo $this->_db->getErrorMsg();
+//			$this->setError($this->_db->getErrorMsg());
+//			return false;
+//		}
 		return '';
 	}
 
