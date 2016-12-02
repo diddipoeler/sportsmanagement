fe8a08e06d76feabf6ed18a4dcc94f1703b142ea
diddipoeler <famploeger@arcor.de>
2016-12-02 11:04:21 +0100

    Update clubinfo.php
    clubinfo erweiterung der fusionen

--- site/models/clubinfo.php
+++ site/models/clubinfo.php
@@ -1,9 +1,9 @@
 <?php 
-/** SportsManagement ein Programm zur Verwaltung für alle Sportarten
+/** SportsManagement ein Programm zur Verwaltung fÃ¼r alle Sportarten
 * @version         1.0.05
 * @file                agegroup.php
 * @author                diddipoeler, stony, svdoldie und donclumsy (diddipoeler@arcor.de)
-* @copyright        Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+* @copyright        Copyright: Â© 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
 * @license                This file is part of SportsManagement.
 *
 * SportsManagement is free software: you can redistribute it and/or modify
@@ -21,15 +21,15 @@
 *
 * Diese Datei ist Teil von SportsManagement.
 *
-* SportsManagement ist Freie Software: Sie können es unter den Bedingungen
+* SportsManagement ist Freie Software: Sie kÃ¶nnen es unter den Bedingungen
 * der GNU General Public License, wie von der Free Software Foundation,
-* Version 3 der Lizenz oder (nach Ihrer Wahl) jeder späteren
-* veröffentlichten Version, weiterverbreiten und/oder modifizieren.
+* Version 3 der Lizenz oder (nach Ihrer Wahl) jeder spÃ¤teren
+* verÃ¶ffentlichten Version, weiterverbreiten und/oder modifizieren.
 *
-* SportsManagement wird in der Hoffnung, dass es nützlich sein wird, aber
-* OHNE JEDE GEWÄHELEISTUNG, bereitgestellt; sogar ohne die implizite
-* Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
-* Siehe die GNU General Public License für weitere Details.
+* SportsManagement wird in der Hoffnung, dass es nÃ¼tzlich sein wird, aber
+* OHNE JEDE GEWÃ„HELEISTUNG, bereitgestellt; sogar ohne die implizite
+* GewÃ¤hrleistung der MARKTFÃ„HIGKEIT oder EIGNUNG FÃœR EINEN BESTIMMTEN ZWECK.
+* Siehe die GNU General Public License fÃ¼r weitere Details.
 *
 * Sie sollten eine Kopie der GNU General Public License zusammen mit diesem
 * Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
@@ -68,7 +68,7 @@ class sportsmanagementModelClubInfo extends JModelLegacy
 	static $treedepthold = 0;
     
     static $cfg_which_database = 0;	
-
+static $tree_fusion = Array();
 	/**
 	 * sportsmanagementModelClubInfo::__construct()
 	 * 
@@ -90,6 +90,42 @@ class sportsmanagementModelClubInfo extends JModelLegacy
         parent::__construct( );
 	}
 
+static function fbTreeRecurse( $id, $indent, $list, &$children, $maxlevel=9999, $level=0, $type=1 ) 
+    {
+$app = JFactory::getApplication();
+//$app->enqueueMessage(JText::_(__METHOD__.' '.__LINE__.' children<br><pre>'.print_r($children,true).'</pre>'),'Notice');
+
+    if (isset($children[$id]) && $level <= $maxlevel) {
+        foreach ($children[$id] as $v) {
+            $id = $v->id;
+			if ( $type ) {
+                $pre     = '&nbsp;';
+                $spacer = '...';
+            } else {
+                $pre     = '- ';
+                $spacer = '&nbsp;&nbsp;';
+            }
+
+            if ( $v->new_club_id == 0 ) {
+                $txt     = $v->name;
+            } else {
+                $txt     = $pre . $v->name;
+            }
+            $pt = $v->new_club_id;
+            $list[$id] = $v;
+            $list[$id]->treename = $indent . $txt;
+            $list[$id]->children = !empty($children[$id]) ? count( $children[$id] ) : 0;
+            $list[$id]->section = ($v->new_club_id==0);
+
+            $list = self::fbTreeRecurse( $id, $indent . $spacer, $list, $children, $maxlevel, $level+1, $type );
+
+//$app->enqueueMessage(JText::_(__METHOD__.' '.__LINE__.' list <br><pre>'.print_r($list ,true).'</pre>'),'Notice');       
+       
+        }
+    }
+    return $list;
+}    	
+	
 	// limit count word
 	/**
 	 * sportsmanagementModelClubInfo::limitText()
@@ -548,6 +584,12 @@ $result = $db->execute();
 			
 	foreach ( $result as $row )
 	{
+		
+$pt = $row->new_club_id;
+$list = isset(self::$tree_fusion[$pt]) ? self::$tree_fusion[$pt] : array ();
+array_push($list, $row);
+self::$tree_fusion[$pt] = $list;
+		
   //$temp = '<ul><li>'.$row->name.'</li>';
   //$this->treedepthold = $this->treedepth;
   
