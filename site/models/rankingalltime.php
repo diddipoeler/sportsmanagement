0770c799d645e7e865f0d1b728272639fd354103
diddipoeler <famploeger@arcor.de>
2017-01-24 12:02:51 +0100

    Update rankingalltime.php
    #391

--- site/models/rankingalltime.php
+++ site/models/rankingalltime.php
@@ -1,9 +1,9 @@
 <?php 
-/** SportsManagement ein Programm zur Verwaltung für alle Sportarten
+/** SportsManagement ein Programm zur Verwaltung fÃ¼r alle Sportarten
 * @version         1.0.05
 * @file                agegroup.php
 * @author                diddipoeler, stony, svdoldie und donclumsy (diddipoeler@arcor.de)
-* @copyright        Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+* @copyright        Copyright: Â© 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
 * @license                This file is part of SportsManagement.
 *
 * SportsManagement is free software: you can redistribute it and/or modify
@@ -21,15 +21,15 @@
 *
 * Diese Datei ist Teil von SportsManagement.
 *
-* SportsManagement ist Freie Software: Sie können es unter den Bedingungen
+* SportsManagement ist Freie Software: Sie kÃ¶nnen es unter den Bedingungen
 * der GNU General Public License, wie von der Free Software Foundation,
-* Version 3 der Lizenz oder (nach Ihrer Wahl) jeder späteren
-* veröffentlichten Version, weiterverbreiten und/oder modifizieren.
+* Version 3 der Lizenz oder (nach Ihrer Wahl) jeder spÃ¤teren
+* verÃ¶ffentlichten Version, weiterverbreiten und/oder modifizieren.
 *
-* SportsManagement wird in der Hoffnung, dass es nützlich sein wird, aber
-* OHNE JEDE GEWÄHELEISTUNG, bereitgestellt; sogar ohne die implizite
-* Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
-* Siehe die GNU General Public License für weitere Details.
+* SportsManagement wird in der Hoffnung, dass es nÃ¼tzlich sein wird, aber
+* OHNE JEDE GEWÃ„HELEISTUNG, bereitgestellt; sogar ohne die implizite
+* GewÃ¤hrleistung der MARKTFÃ„HIGKEIT oder EIGNUNG FÃœR EINEN BESTIMMTEN ZWECK.
+* Siehe die GNU General Public License fÃ¼r weitere Details.
 *
 * Sie sollten eine Kopie der GNU General Public License zusammen mit diesem
 * Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
@@ -150,35 +150,42 @@ foreach ($newparams['data'] as $key => $value ) {
         }
         else
         {
-        //$strXmlFile = JPATH_SITE.DS.JSM_PATH.DS.'settings'.DS.'default'.DS.'rankingalltime.xml';    
+//$strXmlFile = JPATH_SITE.DS.JSM_PATH.DS.'settings'.DS.'default'.DS.'rankingalltime.xml';    
         $strXmlFile = JPATH_SITE.DS.JSM_PATH.DS.'views'.DS.'rankingalltime'.DS.'tmpl'.DS.'default.xml';
         //$xml = simplexml_load_file($strXmlFile);
         
-        $xml = JFactory::getXMLParser( 'Simple' );
-        $xml->loadFile($strXmlFile);
+//        $xml = JFactory::getXMLParser( 'Simple' );
+//        $xml->loadFile($strXmlFile);
+        $xml=JFactory::getXML($strXmlFile);
         $children = $xml->document->children();
-        
-        //$positions = $xml->document->field;
-        
-        // We can now step through each element of the file 
-foreach( $xml->document->fields as $field ) 
+
+ // We can now step through each element of the file 
+foreach( $xml->children() as $field ) 
 {
-//$app->enqueueMessage(JText::_(__METHOD__.' '.__LINE__.' field<br><pre>'.print_r($field->attributes() ,true).'</pre>'),'Notice');
+//$app->enqueueMessage(JText::_(__METHOD__.' '.__LINE__.' field<br><pre>'.print_r($field ,true).'</pre>'),'Notice');
+//$attr = $field->field->attributes();
+//$app->enqueueMessage(JText::_(__METHOD__.' '.__LINE__.' attr <br><pre>'.print_r($attr ,true).'</pre>'),'Notice'); 
 foreach( $field->fieldset  as $fieldset ) 
 {
-//$app->enqueueMessage(JText::_(__METHOD__.' '.__LINE__.' fieldset<br><pre>'.print_r($fieldset->attributes(),true).'</pre>'),'Notice');
+//$app->enqueueMessage(JText::_(__METHOD__.' '.__LINE__.' fieldset<br><pre>'.print_r($fieldset,true).'</pre>'),'Notice');
 
 foreach( $fieldset->field  as $param ) 
 {
 $attributes = $param->attributes();
-//$app->enqueueMessage(JText::_(__METHOD__.' '.__LINE__.' fieldset<br><pre>'.print_r($param->attributes(),true).'</pre>'),'Notice');
-$this->_params[$attributes['name']] = $attributes['default'];
+//$app->enqueueMessage(JText::_(__METHOD__.' '.__LINE__.' attributes<br><pre>'.print_r($attributes,true).'</pre>'),'Notice');
+//$app->enqueueMessage(JText::_(__METHOD__.' '.__LINE__.' attributes name<br><pre>'.print_r($attributes['name'],true).'</pre>'),'Notice');
+//$app->enqueueMessage(JText::_(__METHOD__.' '.__LINE__.' attributes default<br><pre>'.print_r($attributes['default'],true).'</pre>'),'Notice');
+//$app->enqueueMessage(JText::_(__METHOD__.' '.__LINE__.' attributes name<br><pre>'.print_r((string)$param->attributes()->name[0],true).'</pre>'),'Notice');
+
+$this->_params[(string)$param->attributes()->name[0]] = (string)$param->attributes()->default[0];
 
 }
 
-}    
+}  
 
    }
+		
+
         
 }        
 
