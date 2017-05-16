e1e5aa21965a9df4312aecd32cdd40a32e7d17a9
diddipoeler <famploeger@arcor.de>
2017-05-16 14:06:05 +0200

    Update view.html.php
    #438

--- site/views/predictionentry/view.html.php
+++ site/views/predictionentry/view.html.php
@@ -1,9 +1,9 @@
 <?php
-/** SportsManagement ein Programm zur Verwaltung für alle Sportarten
+/** SportsManagement ein Programm zur Verwaltung fÃ¼r alle Sportarten
 * @version         1.0.05
 * @file                agegroup.php
 * @author                diddipoeler, stony, svdoldie und donclumsy (diddipoeler@arcor.de)
-* @copyright        Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+* @copyright        Copyright: Â© 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
 * @license                This file is part of SportsManagement.
 *
 * SportsManagement is free software: you can redistribute it and/or modify
@@ -21,15 +21,15 @@
 *
 * Diese Datei ist Teil von SportsManagement.
 *
-* SportsManagement ist Freie Software: Sie können es unter den Bedingungen
+* SportsManagement ist Freie Software: Sie kÃ¶nnen es unter den Bedingungen
 * der GNU General Public License, wie von der Free Software Foundation,
-* Version 3 der Lizenz oder (nach Ihrer Wahl) jeder späteren
-* veröffentlichten Version, weiterverbreiten und/oder modifizieren.
+* Version 3 der Lizenz oder (nach Ihrer Wahl) jeder spÃ¤teren
+* verÃ¶ffentlichten Version, weiterverbreiten und/oder modifizieren.
 *
-* SportsManagement wird in der Hoffnung, dass es nützlich sein wird, aber
-* OHNE JEDE GEWÄHELEISTUNG, bereitgestellt; sogar ohne die implizite
-* Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
-* Siehe die GNU General Public License für weitere Details.
+* SportsManagement wird in der Hoffnung, dass es nÃ¼tzlich sein wird, aber
+* OHNE JEDE GEWÃ„HELEISTUNG, bereitgestellt; sogar ohne die implizite
+* GewÃ¤hrleistung der MARKTFÃ„HIGKEIT oder EIGNUNG FÃœR EINEN BESTIMMTEN ZWECK.
+* Siehe die GNU General Public License fÃ¼r weitere Details.
 *
 * Sie sollten eine Kopie der GNU General Public License zusammen mit diesem
 * Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
@@ -196,10 +196,10 @@ echo 'allow -> ' . $allow. '<br>';
 		$output .= '<input type="hidden" name="homes[' . $pid . '][' . $mid . ']" value="' . $tipp_home . '" />';
 		$output .= '<input type="hidden" name="aways[' . $pid . '][' . $mid . ']" value="' . $tipp_away . '" />';
 		$outputArray = array	(
-									JHTML::_('select.option','',	JText::_('JL_PRED_ENTRY_NO_TIPP'),	'id','name'),
-									JHTML::_('select.option','1',	JText::_('JL_PRED_ENTRY_HOME_WIN'),	'id','name'),
-									JHTML::_('select.option','0',	JText::_('JL_PRED_ENTRY_DRAW'),		'id','name'),
-									JHTML::_('select.option','2',	JText::_('JL_PRED_ENTRY_AWAY_WIN'),	'id','name')
+									JHTML::_('select.option','',JText::_('COM_SPORTSMANAGEMENT_PRED_ENTRY_NO_TIPP'),'id','name'),
+									JHTML::_('select.option','1',JText::_('COM_SPORTSMANAGEMENT_PRED_ENTRY_HOME_WIN'),'id','name'),
+									JHTML::_('select.option','0',JText::_('COM_SPORTSMANAGEMENT_PRED_ENTRY_DRAW'),'id','name'),
+									JHTML::_('select.option','2',JText::_('COM_SPORTSMANAGEMENT_PRED_ENTRY_AWAY_WIN'),'id','name')
 								);
 		$output .= JHTML::_('select.genericlist',$outputArray,'tipps['.$pid.']['.$mid.']','class="inputbox" size="1" ' . $disabled,'id','name',$tipp);
 		unset($outputArray);
