06b218657b312ce8f2eb9f14eb348eef97304b99
diddipoeler <famploeger@arcor.de>
2017-05-18 12:07:32 +0200

    Update router.php
    #420

--- site/router.php
+++ site/router.php
@@ -1,9 +1,9 @@
 <?php
-/** SportsManagement ein Programm zur Verwaltung für alle Sportarten
+/** SportsManagement ein Programm zur Verwaltung fÃ¼r alle Sportarten
  * @version         1.0.05
  * @file                agegroup.php
  * @author                diddipoeler, stony, svdoldie und donclumsy (diddipoeler@arcor.de)
- * @copyright        Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @copyright        Copyright: Â© 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
  * @license                This file is part of SportsManagement.
  *
  * SportsManagement is free software: you can redistribute it and/or modify
@@ -21,15 +21,15 @@
  *
  * Diese Datei ist Teil von SportsManagement.
  *
- * SportsManagement ist Freie Software: Sie können es unter den Bedingungen
+ * SportsManagement ist Freie Software: Sie kÃ¶nnen es unter den Bedingungen
  * der GNU General Public License, wie von der Free Software Foundation,
- * Version 3 der Lizenz oder (nach Ihrer Wahl) jeder späteren
- * veröffentlichten Version, weiterverbreiten und/oder modifizieren.
+ * Version 3 der Lizenz oder (nach Ihrer Wahl) jeder spÃ¤teren
+ * verÃ¶ffentlichten Version, weiterverbreiten und/oder modifizieren.
  *
- * SportsManagement wird in der Hoffnung, dass es nützlich sein wird, aber
- * OHNE JEDE GEWÄHELEISTUNG, bereitgestellt; sogar ohne die implizite
- * Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
- * Siehe die GNU General Public License für weitere Details.
+ * SportsManagement wird in der Hoffnung, dass es nÃ¼tzlich sein wird, aber
+ * OHNE JEDE GEWÃ„HELEISTUNG, bereitgestellt; sogar ohne die implizite
+ * GewÃ¤hrleistung der MARKTFÃ„HIGKEIT oder EIGNUNG FÃœR EINEN BESTIMMTEN ZWECK.
+ * Siehe die GNU General Public License fÃ¼r weitere Details.
  *
  * Sie sollten eine Kopie der GNU General Public License zusammen mit diesem
  * Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
@@ -424,7 +424,7 @@ function sportsmanagementBuildRoute(&$query)
                 unset($query['dir']);
             }
 /**
- * beim printbutton wird ein popup geöffnet
+ * beim printbutton wird ein popup geÃ¶ffnet
  */
             if (isset($query['tmpl'])) {
                 $segments[] = $query['tmpl'];
@@ -744,17 +744,18 @@ function sportsmanagementParseRoute($segments)
                 $vars['prediction_id'] = $segments[2];
             }
             if (isset($segments[3])) {
-                $vars['uid'] = $segments[3];
+                $vars['pggroup'] = $segments[3];
             }
             if (isset($segments[4])) {
-                $vars['pggroup'] = $segments[4];
+                $vars['pj'] = $segments[4];
             }
             if (isset($segments[5])) {
-                $vars['pj'] = $segments[5];
-            }
-            if (isset($segments[6])) {
                 $vars['r'] = $segments[5];
             }
+            if (isset($segments[6])) {
+                $vars['uid'] = $segments[6];
+            }
+            
 
             break;
 
@@ -859,7 +860,7 @@ function sportsmanagementParseRoute($segments)
                 $vars['dir'] = $segments[10];
             }
 /**
- * beim printbutton wird ein popup geöffnet
+ * beim printbutton wird ein popup geÃ¶ffnet
  */            
             if (isset($segments[11])) {
                 $vars['tmpl'] = $segments[11];
