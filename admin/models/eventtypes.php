f1ae4a5b8b06d22409083088a20c17bc76c88d83
diddipoeler <famploeger@arcor.de>
2017-05-31 10:17:49 +0200

    #428

--- admin/models/eventtypes.php
+++ admin/models/eventtypes.php
@@ -91,7 +91,7 @@ class sportsmanagementModelEventtypes extends JSMModelList
 		// Load the filter state.
 		$search = $this->getUserStateFromRequest($this->context.'.filter.search', 'filter_search');
 		$this->setState('filter.search', $search);
-		$published = $this->getUserStateFromRequest($this->context.'.filter.state', 'filter_published', '', 'string');
+		$published = $this->getUserStateFromRequest($this->context.'.filter.state', 'filter_state', '', 'string');
 		$this->setState('filter.state', $published);
         $temp_user_request = $this->getUserStateFromRequest($this->context.'.filter.sports_type', 'filter_sports_type', '');
 		$this->setState('filter.sports_type', $temp_user_request);
