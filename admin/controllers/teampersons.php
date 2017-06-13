648f0bbb058f699480ad0eebe871786774ebd33c
diddipoeler <famploeger@arcor.de>
2017-06-13 19:09:06 +0200

    #449

--- admin/controllers/teampersons.php
+++ admin/controllers/teampersons.php
@@ -39,6 +39,8 @@
 
 // No direct access to this file
 defined('_JEXEC') or die('Restricted access');
+
+use Joomla\Utilities\ArrayHelper;
  
 // import Joomla controlleradmin library
 jimport('joomla.application.component.controlleradmin');
