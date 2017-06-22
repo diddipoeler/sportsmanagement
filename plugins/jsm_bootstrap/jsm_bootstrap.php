e24c7dc1c43d0bf849a33562dda386ce22d14b52
diddipoeler <famploeger@arcor.de>
2017-06-22 11:50:16 +0200

    Update jsm_bootstrap.php
    #420

--- plugins/jsm_bootstrap/jsm_bootstrap.php
+++ plugins/jsm_bootstrap/jsm_bootstrap.php
@@ -1,9 +1,9 @@
 <?php
-/** SportsManagement ein Programm zur Verwaltung für alle Sportarten
+/** SportsManagement ein Programm zur Verwaltung fÃ¼r alle Sportarten
  * @version         1.0.05
  * @file                agegroup.php
  * @author                diddipoeler, stony, svdoldie und donclumsy (diddipoeler@arcor.de)
- * @copyright        Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @copyright        Copyright: Â© 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
  * @license                This file is part of SportsManagement.
  *
  * SportsManagement is free software: you can redistribute it and/or modify
@@ -21,15 +21,15 @@
  *
  * Diese Datei ist Teil von SportsManagement.
  *
- * SportsManagement ist Freie Software: Sie können es unter den Bedingungen
+ * SportsManagement ist Freie Software: Sie kÃ¶nnen es unter den Bedingungen
  * der GNU General Public License, wie von der Free Software Foundation,
- * Version 3 der Lizenz oder (nach Ihrer Wahl) jeder späteren
- * veröffentlichten Version, weiterverbreiten und/oder modifizieren.
+ * Version 3 der Lizenz oder (nach Ihrer Wahl) jeder spÃ¤teren
+ * verÃ¶ffentlichten Version, weiterverbreiten und/oder modifizieren.
  *
- * SportsManagement wird in der Hoffnung, dass es nützlich sein wird, aber
- * OHNE JEDE GEWÄHELEISTUNG, bereitgestellt; sogar ohne die implizite
- * Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
- * Siehe die GNU General Public License für weitere Details.
+ * SportsManagement wird in der Hoffnung, dass es nÃ¼tzlich sein wird, aber
+ * OHNE JEDE GEWÃ„HELEISTUNG, bereitgestellt; sogar ohne die implizite
+ * GewÃ¤hrleistung der MARKTFÃ„HIGKEIT oder EIGNUNG FÃœR EINEN BESTIMMTEN ZWECK.
+ * Siehe die GNU General Public License fÃ¼r weitere Details.
  *
  * Sie sollten eine Kopie der GNU General Public License zusammen mit diesem
  * Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
@@ -157,7 +157,37 @@ class PlgSystemjsm_bootstrap extends JPlugin
     public function onAfterDispatch()
     {
         $app = JFactory::getApplication();
+// Get a refrence of the page instance in joomla
+        $document = JFactory::getDocument();
+        // JInput object
+        $this->jinput = $app->input;
+        $this->option = $this->jinput->getCmd('option');
+        //$app->enqueueMessage(JText::_(__METHOD__.' '.__LINE__.'isEnabled <br><pre>'.print_r(JComponentHelper::isEnabled('com_k2', true),true).'</pre>'),'');
+$dontInclude = array(
+'/media/jui/js/jquery.js',
+'/media/jui/js/jquery.min.js',
+//'/media/jui/js/jquery-noconflict.js',
+'/media/jui/js/jquery-migrate.js',
+'/media/jui/js/jquery-migrate.min.js',
+'/media/jui/js/bootstrap.js',
+'/media/jui/js/bootstrap.min.js',
+'/media/system/js/core-uncompressed.js',
+'/media/system/js/tabs-state.js',
+'/media/system/js/core.js',
+//'/media/system/js/mootools-core.js',
+//'/media/system/js/mootools-core-uncompressed.js',
+);
 
+if ( $this->option == 'com_sportsmanagement' )
+{
+foreach($document->_scripts as $key => $script){
+    if(in_array($key, $dontInclude)){
+        unset($document->_scripts[$key]);
+    }
+}
+JFactory::getDocument()->addScript('http://ajax.googleapis.com/ajax/libs/jquery/2.2.1/jquery.min.js');
+}	    
+	    
         //$app->enqueueMessage(JText::_(__METHOD__.' '.__LINE__.'isEnabled <br><pre>'.print_r(JComponentHelper::isEnabled('com_k2', true),true).'</pre>'),'');
 
         // Get a refrence of the page instance in joomla
