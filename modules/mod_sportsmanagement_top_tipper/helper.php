e09c8aac7cf60ff75d66b72818c9e671a1764e84
diddipoeler <famploeger@arcor.de>
2017-07-07 08:41:42 +0200

    Update helper.php
    #464

--- modules/mod_sportsmanagement_top_tipper/helper.php
+++ modules/mod_sportsmanagement_top_tipper/helper.php
@@ -43,14 +43,6 @@
 defined('_JEXEC') or die('Restricted access');
 $database = JFactory::getDBO();
 
-//$option='com_sportsmanagement';
 
-// funktionen einbinden
-//require_once ( JPATH_SITE.DS.'components'.DS.$option.DS.'models'.DS.'predictionranking.php' );
-//require_once ( JPATH_SITE.DS.'components'.DS.$option.DS.'helpers'.DS.'route.php' );
-
-// $modelpg = &JLGModel::getInstance('PredictionRanking', 'JoomleagueModel');
-// $predictionGame[] = $modelpg->getPredictionGame();
-// echo 'prediction game -> <pre>'.print_r($predictionGame).'</pre><br>';
 		
 ?>
