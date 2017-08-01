984d4c67705f9105f0039b90d6f6cb302f7f34d7
diddipoeler <famploeger@arcor.de>
2017-08-01 11:27:07 +0200

    Update default_rankingnav.php
    #420

--- site/views/ranking/tmpl/default_rankingnav.php
+++ site/views/ranking/tmpl/default_rankingnav.php
@@ -42,25 +42,20 @@ defined( '_JEXEC' ) or die( 'Restricted access' );
 
 <form name="adminForm" id="adminForm" method="post"	action="<?php echo $this->action;?>">
 <table class="table">
-	<tr>
-	<?php
-	//echo " [" . $this->startdate. " / " . $this->enddate. "]";
+<tr>
+<?php
+echo "<td>".JHtml::_('select.genericlist', $this->lists['type'], 'type' , 'class="inputbox" size="1"', 'value', 'text', $this->type )."</td>";
+echo "<td>".JHtml::_('select.genericlist', $this->lists['frommatchday'], 'from' , 'class="inputbox" size="1"', 'value' ,'text' , $this->from )."</td>";
+echo "<td>".JHtml::_('select.genericlist', $this->lists['tomatchday'], 'to' , 'class="inputbox" size="1"', 'value', 'text', $this->to )."</td>";
 
-	//echo $this->pagenav.'<br>';
-	//echo $this->pagenav2.'<br>';
-
-	//echo JHtml::calendar( $this->startdate, 'startdate', 'startdate', $dateformat );
-	//echo " - " . JHtml::calendar( $this->enddate, 'enddate', 'enddate', $dateformat );
-	echo "<td>".JHtml::_('select.genericlist', $this->lists['type'], 'type' , 'class="inputbox" size="1"', 'value', 'text', $this->type )."</td>";
-	echo "<td>".JHtml::_('select.genericlist', $this->lists['frommatchday'], 'from' , 'class="inputbox" size="1"', 'value' ,'text' , $this->from )."</td>";
-	echo "<td>".JHtml::_('select.genericlist', $this->lists['tomatchday'], 'to' , 'class="inputbox" size="1"', 'value', 'text', $this->to )."</td>";
-
-	?>
-		<td><input type="submit" class="<?PHP echo $this->config['button_style']; ?>" name="reload View"
-			value="<?php echo JText::_('COM_SPORTSMANAGEMENT_RANKING_FILTER'); ?>"></td>
-	</tr>
+?>
+<td>
+<input type="submit" class="<?PHP echo $this->config['button_style']; ?>" name="reload View"
+			value="<?php echo JText::_('COM_SPORTSMANAGEMENT_RANKING_FILTER'); ?>">
+</td>
+</tr>
 </table>
-	<?php echo JHtml::_( 'form.token' ); ?>
-    </form>
+<?php echo JHtml::_( 'form.token' ); ?>
+</form>
 <br />
 
