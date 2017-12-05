973e3dabf0397db62048ec0b7ad77040d70ea788
diddipoeler <famploeger@arcor.de>
2017-12-05 08:02:20 +0100

    Update default_data.php
    #487

--- admin/views/smquotes/tmpl/default_data.php
+++ admin/views/smquotes/tmpl/default_data.php
@@ -173,10 +173,10 @@ JHtml::_('behavior.modal');
                         
 						<td class="order">
 							<span>
-								<?php echo $this->pagination->orderUpIcon($i,$i > 0,'smquotes.orderup','JLIB_HTML_MOVE_UP',$ordering); ?>
+								<?php echo $this->pagination->orderUpIcon($i,$i > 0,'smquotes.orderup','JLIB_HTML_MOVE_UP','obj.ordering'); ?>
 							</span>
 							<span>
-								<?php echo $this->pagination->orderDownIcon($i,$n,$i < $n,'smquotes.orderdown','JLIB_HTML_MOVE_DOWN',$ordering); ?>
+								<?php echo $this->pagination->orderDownIcon($i,$n,$i < $n,'smquotes.orderdown','JLIB_HTML_MOVE_DOWN','obj.ordering'); ?>
 								<?php $disabled=true ?	'' : 'disabled="disabled"'; ?>
 							</span>
 							<input	type="text" name="order[]" size="5" value="<?php echo $row->ordering;?>" <?php echo $disabled; ?>
