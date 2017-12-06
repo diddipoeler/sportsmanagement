a22f795c7972e547be37d99b43f759b1c6cdf4c2
diddipoeler <famploeger@arcor.de>
2017-12-06 10:04:13 +0100

    Update default_joomla_vier.php
    #488

--- site/views/ranking/tmpl/default_joomla_vier.php
+++ site/views/ranking/tmpl/default_joomla_vier.php
@@ -42,6 +42,14 @@ defined('_JEXEC') or die('Restricted access');
 JHtml::_('behavior.switcher');
 JHtml::_('behavior.modal');
 
+$this->startPane = 'startTabSet';
+$this->endPane = 'endTabSet';
+$this->addPanel = 'addTab';
+$this->endPanel = 'endTab';
+// Define tabs options for version of Joomla! 4.0
+        $tabsOptions = array(
+            "active" => "tab1_id" // It is the ID of the active tab.
+			); 
 // Make sure that in case extensions are written for mentioned (common) views,
 // that they are loaded i.s.o. of the template of this view
 $templatesToLoad = array('globalviews');
@@ -49,8 +57,31 @@ sportsmanagementHelper::addTemplatePaths($templatesToLoad, $this);
 
 echo $this->loadTemplate('projectheading');
 
-
+echo JHtml::_('bootstrap.'.$this->startPane, 'ID-Tabs-Group', $tabsOptions);
+echo JHtml::_('bootstrap.'.$this->addPanel, 'ID-Tabs-Group', 'tab1_id',JText::_($this->config['table_text_1']) ); 
+//echo '<h2>' . JText::_('COM_SPORTSMANAGEMENT_DESCRIPTION_1') .'</h2>';
 echo $this->loadTemplate('ranking');
+echo JHtml::_('bootstrap.'.$this->endPanel);
+echo JHtml::_('bootstrap.'.$this->addPanel, 'ID-Tabs-Group', 'tab2_id',JText::_($this->config['table_text_2'])); 
+//echo '<h2>' . JText::_('COM_SPORTSMANAGEMENT_DESCRIPTION_2') .'</h2>';
+echo $this->loadTemplate('ranking_home');
+echo JHtml::_('bootstrap.'.$this->endPanel);
+echo JHtml::_('bootstrap.'.$this->addPanel, 'ID-Tabs-Group', 'tab3_id',JText::_($this->config['table_text_3'])); 
+//echo '<h2>' . JText::_('COM_SPORTSMANAGEMENT_DESCRIPTION_3') .'</h2>';
+echo $this->loadTemplate('ranking_away');
+echo JHtml::_('bootstrap.'.$this->endPanel);
+echo JHtml::_('bootstrap.'.$this->addPanel, 'ID-Tabs-Group', 'tab4_id',JText::_($this->config['table_text_4'])); 
+//echo '<h2>' . JText::_('COM_SPORTSMANAGEMENT_DESCRIPTION_4') .'</h2>';
+echo $this->loadTemplate('ranking_first');
+echo JHtml::_('bootstrap.'.$this->endPanel);
+echo JHtml::_('bootstrap.'.$this->addPanel, 'ID-Tabs-Group', 'tab5_id',JText::_($this->config['table_text_5'])); 
+//echo '<h2>' . JText::_('COM_SPORTSMANAGEMENT_DESCRIPTION_5') .'</h2>';
+echo $this->loadTemplate('ranking_second');
+echo JHtml::_('bootstrap.'.$this->endPanel);
+echo JHtml::_('bootstrap.'.$this->endPane, 'ID-Tabs-Group');
+
+
+//echo $this->loadTemplate('ranking');
 
 
 
