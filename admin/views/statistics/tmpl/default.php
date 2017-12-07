dea7f498bd0ec35ed5314df37702bdde50ffb813
diddipoeler <famploeger@arcor.de>
2017-12-07 15:27:49 +0100

    Update default.php
    #487

--- admin/views/statistics/tmpl/default.php
+++ admin/views/statistics/tmpl/default.php
@@ -50,22 +50,13 @@ sportsmanagementHelper::addTemplatePaths($templatesToLoad, $this);
 <form action="<?php echo $this->request_url; ?>" method="post" id="adminForm" name="adminForm">
 
 <?PHP
-if(version_compare(JVERSION,'3.0.0','ge')) 
-{
-echo $this->loadTemplate('joomla3');
-}
-else
-{
-echo $this->loadTemplate('joomla2');    
-}
-
-echo $this->loadTemplate('data');
+echo $this->loadTemplate('joomla_version');
 ?>  
-	<input type="hidden" name="task"				value="" />
-	<input type="hidden" name="boxchecked"			value="0" />
-	<input type="hidden" name="filter_order"		value="<?php echo $this->sortColumn; ?>" />
-	<input type="hidden" name="filter_order_Dir"	value="<?php echo $this->sortDirection; ?>" />
-	<?php echo JHtml::_('form.token'); ?>
+<input type="hidden" name="task" value="" />
+<input type="hidden" name="boxchecked" value="0" />
+<input type="hidden" name="filter_order" value="<?php echo $this->sortColumn; ?>" />
+<input type="hidden" name="filter_order_Dir" value="<?php echo $this->sortDirection; ?>" />
+<?php echo JHtml::_('form.token'); ?>
 </form>
 <?PHP
 echo "<div>";
