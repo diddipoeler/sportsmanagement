eb218d1446a8d084c05adec005ea67ca0ac019c0
diddipoeler <famploeger@arcor.de>
2017-12-09 19:34:35 +0100

    #491

--- modules/mod_sportsmanagement_clubicons/tmpl/default.php
+++ modules/mod_sportsmanagement_clubicons/tmpl/default.php
@@ -47,33 +47,29 @@ defined('_JEXEC') or die('Restricted access');
 		$cnt = 0;
 		$after = 0;
 		$rest = count($data);
-		foreach($data->ranking AS $k => $ficken)
+		foreach($data->ranking AS $k => $value)
 		{
 			$val = $data->teams[$k];
 			$append = ($params->get('teamlink', 0) == 5 AND $params->get('newwindow', 0) == 1) ?
 				' target="_blank"': '';
-			echo '<td class="modjlclubicons">';
+			?>
+            <td class="">
+            <?PHP
   		if (!empty($val['link'])) { echo '<a href="'.$val['link'].'"'.$append.'>';}
       echo $val['logo'];
       if (!empty($val['link'])) { echo '</a>';}
-      echo '</td>';
+      ?>
+      </td>
+      <?PHP
       $cnt++;
       $modulo = intval($cnt%$params->get('iconsperrow', 20));
       if ($modulo == 0)
       {
-        echo '</tr><tr>';
-        $rest = $rest - $params->get('iconsperrow', 20);
-        if ($rest < $params->get('iconsperrow', 20))
-        {
-          $before = floor(($params->get('iconsperrow', 20)-$rest) / 2);
-          $after = $params->get('iconsperrow', 20) - $before - $rest;
-          for ($x=0;$x < $before;$x++) echo '<td>&nbsp;</td>';
-        }
+        ?>
+        </tr><tr>
+        <?PHP
       }
-      if ($cnt == count($data) && $after > 0)
-      {
-        for ($x=0;$x < $after;$x++) echo '<td>&nbsp;</td>';
-      }
+
 
 		}
 ?>
