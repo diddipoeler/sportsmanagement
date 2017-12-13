d3b4c8c76316811ca26f91ad4351fb61c257a2c0
diddipoeler <famploeger@arcor.de>
2017-12-13 16:01:26 +0100

    Update default_joomla4.php
    #487

--- admin/views/listheader/tmpl/default_joomla4.php
+++ admin/views/listheader/tmpl/default_joomla4.php
@@ -51,14 +51,59 @@ $project_id = $app->getUserState( "$option.pid", '0' );
 ?>
 
 <div class="row">
-		<div id="j-sidebar-container" class="col-md-2">
-			<?php echo $this->sidebar; ?>
-		</div>
-        <div class="col-md-10">
-			<div id="j-main-container" class="j-main-container">
-        
-        
-        
+<div id="j-sidebar-container" class="col-md-2">
+<?php echo $this->sidebar; ?>
+</div>
+<div class="col-md-10">
+<div id="j-main-container" class="j-main-container">
+<?php        
+
+switch ($view)
+{
+case 'githubinstall':
+break;
+case 'updates':
+break;
+case 'databasetools':
+break;
+case 'treetonodes':
+break;
+case 'treetomatchs':
+break;
+default:
+<?php		
+<div id="filter-bar" class="btn-toolbar">
+			<div class="filter-search btn-group pull-left">
+				<label for="filter_search" class="element-invisible"><?php echo JText::_('JSEARCH_FILTER_LABEL');?></label>
+				<input type="text" name="filter_search" id="filter_search" placeholder="<?php echo JText::_('JSEARCH_FILTER'); ?>" value="<?php echo $this->escape($this->state->get('filter.search')); ?>" class="hasTooltip" title="<?php echo JHtml::tooltipText('JGLOBAL_LOOKING_FOR'); ?>" />
+			</div>
+			<div class="btn-group pull-left">
+				<button type="submit" class="btn hasTooltip" title="<?php echo JHtml::tooltipText('JSEARCH_FILTER_SUBMIT'); ?>"><i class="icon-search"></i></button>
+				<button type="button" class="btn hasTooltip" title="<?php echo JHtml::tooltipText('JSEARCH_FILTER_CLEAR'); ?>" onclick="document.id('filter_search').value='';this.form.submit();"><i class="icon-remove"></i></button>
+			           
+            </div>
+            	<?php
+                $startRange = JComponentHelper::getParams($jinput->getCmd('option'))->get('character_filter_start_hex', '0');
+		$endRange = JComponentHelper::getParams($jinput->getCmd('option'))->get('character_filter_end_hex', '0');
+   
+		for ($i=$startRange; $i <= $endRange; $i++)
+		{
+			
+            //printf("<a href=\"javascript:searchPerson('%s')\">%s</a>&nbsp;&nbsp;&nbsp;&nbsp;",chr($i),chr($i));
+            printf("<a href=\"javascript:searchPerson('%s')\">%s</a>&nbsp;&nbsp;&nbsp;&nbsp;",'&#'.$i.';','&#'.$i.';');
+			}
+                
+				?>
+			<div class="btn-group pull-right hidden-phone">
+				<label for="limit" class="element-invisible"><?php echo JText::_('JFIELD_PLG_SEARCH_SEARCHLIMIT_DESC');?></label>
+				<?php echo $this->pagination->getLimitBox(); ?>
+			</div>
+			
+			
+</div>
+?>
+break;		
+?>        
         
         
         
