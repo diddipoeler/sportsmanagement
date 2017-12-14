ee79115e4d5f9db6dd36f6d6779e73c21e7d4b6d
diddipoeler <famploeger@arcor.de>
2017-12-14 09:10:22 +0100

    Update helper.php
    #494

--- modules/mod_sportsmanagement_clubicons/helper.php
+++ modules/mod_sportsmanagement_clubicons/helper.php
@@ -1,9 +1,9 @@
 <?php
-/** SportsManagement ein Programm zur Verwaltung für alle Sportarten
+/** SportsManagement ein Programm zur Verwaltung fÃ¼r alle Sportarten
 * @version         1.0.05
 * @file                agegroup.php
 * @author                diddipoeler, stony, svdoldie und donclumsy (diddipoeler@arcor.de)
-* @copyright        Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+* @copyright        Copyright: Â© 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
 * @license                This file is part of SportsManagement.
 *
 * SportsManagement is free software: you can redistribute it and/or modify
@@ -21,15 +21,15 @@
 *
 * Diese Datei ist Teil von SportsManagement.
 *
-* SportsManagement ist Freie Software: Sie können es unter den Bedingungen
+* SportsManagement ist Freie Software: Sie kÃ¶nnen es unter den Bedingungen
 * der GNU General Public License, wie von der Free Software Foundation,
-* Version 3 der Lizenz oder (nach Ihrer Wahl) jeder späteren
-* veröffentlichten Version, weiterverbreiten und/oder modifizieren.
+* Version 3 der Lizenz oder (nach Ihrer Wahl) jeder spÃ¤teren
+* verÃ¶ffentlichten Version, weiterverbreiten und/oder modifizieren.
 *
-* SportsManagement wird in der Hoffnung, dass es nützlich sein wird, aber
-* OHNE JEDE GEWÄHELEISTUNG, bereitgestellt; sogar ohne die implizite
-* Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
-* Siehe die GNU General Public License für weitere Details.
+* SportsManagement wird in der Hoffnung, dass es nÃ¼tzlich sein wird, aber
+* OHNE JEDE GEWÃ„HELEISTUNG, bereitgestellt; sogar ohne die implizite
+* GewÃ¤hrleistung der MARKTFÃ„HIGKEIT oder EIGNUNG FÃœR EINEN BESTIMMTEN ZWECK.
+* Siehe die GNU General Public License fÃ¼r weitere Details.
 *
 * Sie sollten eine Kopie der GNU General Public License zusammen mit diesem
 * Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
@@ -95,7 +95,7 @@ class modJSMClubiconsHelper
 		//sportsmanagementModelProject::setProjectId($project_id);
         sportsmanagementModelProject::$projectid = $project_id;
         sportsmanagementModelProject::$cfg_which_database = $this->params->get('cfg_which_database');
-		$this->project = sportsmanagementModelProject::getProject();
+		$this->project = sportsmanagementModelProject::getProject($this->params->get('cfg_which_database'));
 
 		$ranking = JSMRanking::getInstance($this->project,$this->params->get('cfg_which_database'));
         sportsmanagementModelRanking::$projectid = $project_id;
@@ -105,11 +105,11 @@ class modJSMClubiconsHelper
 
 		if ($this->params->get( 'logotype' ) == 'logo_small')
 		{
-			$teams = sportsmanagementModelProject::getTeamsIndexedByPtid();
+			$teams = sportsmanagementModelProject::getTeamsIndexedByPtid($divisionid,'name',$this->params->get('cfg_which_database'));
 		}
 		else //get the teams cause we don't have logo_middle and big in ranking model's getTeams:
 		{
-			$teams = sportsmanagementModelProject::getTeams($divisionid);
+			$teams = sportsmanagementModelProject::getTeams($divisionid,'name',$this->params->get('cfg_which_database'));
 		}
 		
         //$app->enqueueMessage(JText::_(__METHOD__.' '.__LINE__.' projectid<br><pre>'.print_r($teams,true).'</pre>'),'');
