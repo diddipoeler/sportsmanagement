7bef3cb7eabd91f73edbc20cc0a1cace713fe9b2
diddipoeler <famploeger@arcor.de>
2017-12-19 14:16:59 +0100

    Update edit_3.php
    #487

--- admin/views/fieldsets/tmpl/edit_3.php
+++ admin/views/fieldsets/tmpl/edit_3.php
@@ -48,7 +48,7 @@ $params = $this->form->getFieldsets('params');
 $fieldsets = $this->form->getFieldsets();
 
 ?>
-<form action="<?php echo JRoute::_('index.php?option=com_sportsmanagement&view='.$this->view.'&layout=edit&id='.(int) $this->item->id); ?>" method="post" name="adminForm" id="adminForm" class="form-validate">
+<form action="<?php echo JRoute::_('index.php?option=com_sportsmanagement&view='.$this->view.'&layout=edit&id='.(int) $this->item->id.'&tmpl='.$this->tmpl); ?>" method="post" name="adminForm" id="adminForm" class="form-validate">
 <?PHP
 if ( !$this->item->id && $this->view == 'club' )
                 {
