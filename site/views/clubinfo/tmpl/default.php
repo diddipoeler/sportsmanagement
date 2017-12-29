12bee71ab6a2aac4f9bcc69bd31c7ddc509a1c8e
diddipoeler <famploeger@arcor.de>
2017-12-29 21:42:59 +0100

    #488

--- site/views/clubinfo/tmpl/default.php
+++ site/views/clubinfo/tmpl/default.php
@@ -1,43 +1,17 @@
 <?php 
-/** SportsManagement ein Programm zur Verwaltung f?r alle Sportarten
-* @version         1.0.05
-* @file                agegroup.php
-* @author                diddipoeler, stony, svdoldie und donclumsy (diddipoeler@arcor.de)
-* @copyright        Copyright: ? 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
-* @license                This file is part of SportsManagement.
-*
-* SportsManagement is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* SportsManagement is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with SportsManagement.  If not, see <http://www.gnu.org/licenses/>.
-*
-* Diese Datei ist Teil von SportsManagement.
-*
-* SportsManagement ist Freie Software: Sie k?nnen es unter den Bedingungen
-* der GNU General Public License, wie von der Free Software Foundation,
-* Version 3 der Lizenz oder (nach Ihrer Wahl) jeder sp?teren
-* ver?ffentlichten Version, weiterverbreiten und/oder modifizieren.
-*
-* SportsManagement wird in der Hoffnung, dass es n?tzlich sein wird, aber
-* OHNE JEDE GEW?HELEISTUNG, bereitgestellt; sogar ohne die implizite
-* Gew?hrleistung der MARKTF?HIGKEIT oder EIGNUNG F?R EINEN BESTIMMTEN ZWECK.
-* Siehe die GNU General Public License f?r weitere Details.
-*
-* Sie sollten eine Kopie der GNU General Public License zusammen mit diesem
-* Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
-*
-* Note : All ini files need to be saved as UTF-8 without BOM
-*/
+/** SportsManagement ein Programm zur Verwaltung für alle Sportarten
+ * @version   1.0.05
+ * @file      default.php
+ * @author    diddipoeler, stony, svdoldie und donclumsy (diddipoeler@arcor.de)
+ * @copyright Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license   This file is part of SportsManagement.
+ */
 
 defined( '_JEXEC' ) or die( 'Restricted access' ); 
+
+//echo '<pre>'.print_r($this->config,true).'</pre>';
+
+//echo 'joomla-version -> '.'JSM_JVERSION.'<br>';
 
 // Make sure that in case extensions are written for mentioned (common) views,
 // that they are loaded i.s.o. of the template of this view
@@ -114,18 +88,27 @@ if ( $this->config['show_club_rssfeed'] )
 {
 if ( $this->rssfeeditems )
 {
-$this->output['COM_SPORTSMANAGEMENT_CLUBINFO_RSSFEED'] = 'rssfeed';  
+    
+if(version_compare(JSM_JVERSION,'4','eq')) 
+{
+$this->output['COM_SPORTSMANAGEMENT_CLUBINFO_RSSFEED'] = 'rssfeed_4';    
+}    
+else
+{
+$this->output['COM_SPORTSMANAGEMENT_CLUBINFO_RSSFEED'] = 'rssfeed';
+}
+  
 }
 }
   
 /**
-   * je nach einstellung der templates im backend, wird das template
-   * aus dem verzeichnis globalviews geladen.
-   * hat den vorteil, das weniger programmcode erzeugt wird.
-   * no_tabs
-   * show_tabs
-   * show_slider
-   */  
+ * je nach einstellung der templates im backend, wird das template
+ * aus dem verzeichnis globalviews geladen.
+ * hat den vorteil, das weniger programmcode erzeugt wird.
+ * no_tabs
+ * show_tabs
+ * show_slider
+ */  
 echo $this->loadTemplate($this->config['show_clubinfo_tabs']);
 
 ?>
---
+++ site/views/clubinfo/tmpl/default_rssfeed_4.php
@@ -0,0 +1,93 @@
+<?php 
+/** SportsManagement ein Programm zur Verwaltung für alle Sportarten
+ * @version   1.0.05
+ * @file      default_rssfeed.php
+ * @author    diddipoeler, stony, svdoldie und donclumsy (diddipoeler@arcor.de)
+ * @copyright Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license   This file is part of SportsManagement.
+ */ 
+
+// no direct access
+defined('_JEXEC') or die('Restricted access'); ?>
+
+<?php
+//$rssitems_colums = $this->overallconfig['rssitems_colums'] ;
+
+foreach ($this->rssfeeditems as $feed) 
+{
+	if( $feed != false )
+	{
+		// Image handling
+		$iUrl   = $feed->image ? $feed->image : null;
+		$iTitle = $feed->imagetitle ? $feed->imagetitle : null;
+		?>
+		<div style="direction: <?php echo $rssrtl ? 'rtl' :'ltr'; ?>; text-align: <?php echo $rssrtl ? 'right' :'left'; ?> !important"  class="feed">
+		<?php
+		// Feed description
+		if ($feed->title !== null && $params->get('rsstitle', 1))
+		{
+			?>
+					<h2 class="<?php echo $direction; ?>">
+						<a href="<?php echo htmlspecialchars($rssurl, ENT_COMPAT, 'UTF-8'); ?>" target="_blank">
+						<?php echo $feed->title; ?></a>
+					</h2>
+			<?php
+		}
+		// Feed description
+		if ($params->get('rssdesc', 1))
+		{
+		?>
+			<?php echo $feed->description; ?>
+			<?php
+		}
+		// Feed image
+		if ($iUrl && $params->get('rssimage', 1)) :
+		?>
+			<img src="<?php echo $iUrl; ?>" alt="<?php echo @$iTitle; ?>">
+		<?php endif; ?>
+
+
+	<!-- Show items -->
+	<?php if (!empty($feed))
+	{ ?>
+		<ul class="newsfeed">
+		<?php for ($i = 0, $max = min(count($feed), $params->get('rssitems', 5)); $i < $max; $i++) { ?>
+			<?php
+				$uri   = (!empty($feed[$i]->uri) || $feed[$i]->uri !== null) ? trim($feed[$i]->uri) : trim($feed[$i]->guid);
+				$uri   = strpos($uri, 'http') !== 0 ? $params->get('rsslink') : $uri;
+				$text  = !empty($feed[$i]->content) || $feed[$i]->content !== null ? trim($feed[$i]->content) : trim($feed[$i]->description);
+				$title = trim($feed[$i]->title);
+			?>
+				<li>
+					<?php if (!empty($uri)) : ?>
+						<span class="feed-link">
+						<a href="<?php echo htmlspecialchars($uri, ENT_COMPAT, 'UTF-8'); ?>" target="_blank">
+						<?php echo $feed[$i]->title; ?></a></span>
+					<?php else : ?>
+						<span class="feed-link"><?php echo $title; ?></span>
+					<?php endif; ?>
+
+					<?php if (!empty($text) && $params->get('rssitemdesc')) : ?>
+						<div class="feed-item-description">
+						<?php
+							// Strip the images.
+							$text = JFilterOutput::stripImages($text);
+
+							$text = JHtml::_('string.truncate', $text, $params->get('word_count'));
+							echo str_replace('&apos;', "'", $text);
+						?>
+						</div>
+					<?php endif; ?>
+				</li>
+		<?php } ?>
+		</ul>
+	<?php } ?>
+	</div>
+	<?php
+
+
+
+	} 
+} 
+?>
+
