70ac2432630fce890455fd11eeb5a7c1e5f67866
diddipoeler <famploeger@arcor.de>
2017-12-29 21:12:28 +0100

    #487

--- admin/models/databasetool.php
+++ admin/models/databasetool.php
@@ -1,41 +1,11 @@
 <?php
-/** SportsManagement ein Programm zur Verwaltung f�r alle Sportarten
-* @version         1.0.05
-* @file                agegroup.php
-* @author                diddipoeler, stony, svdoldie und donclumsy (diddipoeler@arcor.de)
-* @copyright        Copyright: � 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
-* @license                This file is part of SportsManagement.
-*
-* SportsManagement is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* SportsManagement is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with SportsManagement.  If not, see <http://www.gnu.org/licenses/>.
-*
-* Diese Datei ist Teil von SportsManagement.
-*
-* SportsManagement ist Freie Software: Sie k�nnen es unter den Bedingungen
-* der GNU General Public License, wie von der Free Software Foundation,
-* Version 3 der Lizenz oder (nach Ihrer Wahl) jeder sp�teren
-* ver�ffentlichten Version, weiterverbreiten und/oder modifizieren.
-*
-* SportsManagement wird in der Hoffnung, dass es n�tzlich sein wird, aber
-* OHNE JEDE GEW�HELEISTUNG, bereitgestellt; sogar ohne die implizite
-* Gew�hrleistung der MARKTF�HIGKEIT oder EIGNUNG F�R EINEN BESTIMMTEN ZWECK.
-* Siehe die GNU General Public License f�r weitere Details.
-*
-* Sie sollten eine Kopie der GNU General Public License zusammen mit diesem
-* Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
-*
-* Note : All ini files need to be saved as UTF-8 without BOM
-*/
+/** SportsManagement ein Programm zur Verwaltung für alle Sportarten
+ * @version   1.0.05
+ * @file      default_rssfeed.php
+ * @author    diddipoeler, stony, svdoldie und donclumsy (diddipoeler@arcor.de)
+ * @copyright Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license   This file is part of SportsManagement.
+ */ 
 
 // No direct access to this file
 defined('_JEXEC') or die('Restricted access');
@@ -69,35 +39,7 @@ class sportsmanagementModeldatabasetool extends JSMModelLegacy
     static $db_num_rows = 0;
     static $jsmtables = null;
     static $bar_value = 0;
-    
-    
-    ///**
-//     * sportsmanagementModeldatabasetool::__construct()
-//     * 
-//     * @param mixed $config
-//     * @return void
-//     */
-//    public function __construct($config = array())
-//        {   
-//
-//        parent::__construct($config);
-//        $getDBConnection = sportsmanagementHelper::getDBConnection();
-//        parent::setDbo($getDBConnection);
-//        $this->jsmdb = sportsmanagementHelper::getDBConnection();
-//        parent::setDbo($this->jsmdb);
-//        $this->jsmquery = $this->jsmdb->getQuery(true);
-//        $this->jsmsubquery1 = $this->jsmdb->getQuery(true); 
-//        $this->jsmsubquery2 = $this->jsmdb->getQuery(true); 
-//        $this->jsmsubquery3 = $this->jsmdb->getQuery(true);  
-//        // Reference global application object
-//        $this->jsmapp = JFactory::getApplication();
-//        // JInput object
-//        $this->jsmjinput = $this->jsmapp->input;
-//        $this->jsmoption = $this->jsmjinput->getCmd('option');
-//        $this->jsmdocument = JFactory::getDocument();
-//        $this->jsmuser = JFactory::getUser(); 
-//        }
-        
+      
     /**
      * sportsmanagementModeldatabasetool::runJoomlaQuery()
      * 
