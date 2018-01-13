831e9221c6a1cc56d89f9f2dfff46a76c7990d4c
diddipoeler <famploeger@arcor.de>
2018-01-13 10:17:27 +0100

    #488

--- site/views/teaminfo/tmpl/default_teaminfo.php
+++ site/views/teaminfo/tmpl/default_teaminfo.php
@@ -1,41 +1,13 @@
 <?php
-/** SportsManagement ein Programm zur Verwaltung f?r alle Sportarten
-* @version         1.0.05
-* @file                agegroup.php
-* @author                diddipoeler, stony, svdoldie und donclumsy (diddipoeler@arcor.de)
-* @copyright        Copyright: ? 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
-* @license                This file is part of SportsManagement.
-*
-* SportsManagement is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* SportsManagement is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with SportsManagement.  If not, see <http://www.gnu.org/licenses/>.
-*
-* Diese Datei ist Teil von SportsManagement.
-*
-* SportsManagement ist Freie Software: Sie k?nnen es unter den Bedingungen
-* der GNU General Public License, wie von der Free Software Foundation,
-* Version 3 der Lizenz oder (nach Ihrer Wahl) jeder sp?teren
-* ver?ffentlichten Version, weiterverbreiten und/oder modifizieren.
-*
-* SportsManagement wird in der Hoffnung, dass es n?tzlich sein wird, aber
-* OHNE JEDE GEW?HELEISTUNG, bereitgestellt; sogar ohne die implizite
-* Gew?hrleistung der MARKTF?HIGKEIT oder EIGNUNG F?R EINEN BESTIMMTEN ZWECK.
-* Siehe die GNU General Public License f?r weitere Details.
-*
-* Sie sollten eine Kopie der GNU General Public License zusammen mit diesem
-* Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
-*
-* Note : All ini files need to be saved as UTF-8 without BOM
-*/
+/** SportsManagement ein Programm zur Verwaltung für alle Sportarten
+ * @version   1.0.05
+ * @file      deafult_teaminfo.php
+ * @author    diddipoeler, stony, svdoldie und donclumsy (diddipoeler@arcor.de)
+ * @copyright Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license   This file is part of SportsManagement.
+ * @package   sportsmanagement
+ * @subpackage teaminfo
+ */
 
 defined('_JEXEC') or die('Restricted access');
 
@@ -50,28 +22,18 @@ if (!isset($this->team))
 else
 {
 
-//	if($this->config['show_club_info'] || $this->config['show_team_info'])
-//	{
 	?>
         <div class="<?php echo COM_SPORTSMANAGEMENT_BOOTSTRAP_DIV_CLASS; ?>" id="default_teaminfo">
         <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6" id="default_teaminfo-left">
         <?PHP
-//	}
-//	else
-//	{
+
 		 ?>
-<!--        <div class="<?php echo COM_SPORTSMANAGEMENT_BOOTSTRAP_DIV_CLASS; ?>"> -->
-<!--        <div style="text-align:center; width:100%;"> -->
+
         <?PHP
-//	}
 	
     //dynamic object property string
 	$pic = $this->config['show_picture'];
     $picture = $this->team->$pic;
-        if ( !JFile::exists(JPATH_SITE.DS.$picture) )
-        {
-        $picture = sportsmanagementHelper::getDefaultPlaceholder("team");
-        } 
         
 echo sportsmanagementHelperHtml::getBootstrapModalImage('teaminfo'.$this->team->id,$picture,$this->team->name,$this->config['team_picture_width']);
 
@@ -107,17 +69,6 @@ echo JText::sprintf('COM_SPORTSMANAGEMENT_COPYRIGHT_INFO','<i>'.$this->team->cr_
 															$this->club->location,
 															$this->club->country,
 															'COM_SPORTSMANAGEMENT_TEAMINFO_CLUB_ADDRESS_FORM' );
-
-//				$dummy = explode('<br />', $dummy);
-//				for ($i = 0; $i < count($dummy); $i++)
-//				{
-//					if ($i > 0)
-//					{
-//						echo'<span class="clubinfo_listing_item">&nbsp;</span>';
-//					}
-//					echo'<span class="clubinfo_listing_value">'.$dummy[$i].'</span>';
-//				}
-//				echo '</div>';
                 
                 echo $addressString;
 			}
