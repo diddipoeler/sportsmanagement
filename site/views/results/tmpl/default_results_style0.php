5f405de4f4d6a166c567b3c918f6b779a302c484
diddipoeler <famploeger@arcor.de>
2018-01-19 14:40:35 +0100

    #488

--- site/views/results/tmpl/default_results_style0.php
+++ site/views/results/tmpl/default_results_style0.php
@@ -1,41 +1,13 @@
 <?php 
-/** Joomla Sports Management ein Programm zur Verwaltung für alle Sportarten
-* @version 1.0.26
-* @file		components/sportsmanagement/views/results/tmpl/default_results_style0.php
-* @author diddipoeler, stony, svdoldie und donclumsy (diddipoeler@arcor.de)
-* @copyright Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
-* @license This file is part of Joomla Sports Management.
-*
-* Joomla Sports Management is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* Joomla Sports Management is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with Joomla Sports Management. If not, see <http://www.gnu.org/licenses/>.
-*
-* Diese Datei ist Teil von Joomla Sports Management.
-*
-* Joomla Sports Management ist Freie Software: Sie können es unter den Bedingungen
-* der GNU General Public License, wie von der Free Software Foundation,
-* Version 3 der Lizenz oder (nach Ihrer Wahl) jeder späteren
-* veröffentlichten Version, weiterverbreiten und/oder modifizieren.
-*
-* Joomla Sports Management wird in der Hoffnung, dass es nützlich sein wird, aber
-* OHNE JEDE GEWÄHRLEISTUNG, bereitgestellt; sogar ohne die implizite
-* Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
-* Siehe die GNU General Public License für weitere Details.
-*
-* Sie sollten eine Kopie der GNU General Public License zusammen mit diesem
-* Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
-*
-* Note : All ini files need to be saved as UTF-8 without BOM
-*/
+/** SportsManagement ein Programm zur Verwaltung für alle Sportarten
+ * @version   1.0.05
+ * @file      default_results_style0.php
+ * @author    diddipoeler, stony, svdoldie und donclumsy (diddipoeler@arcor.de)
+ * @copyright Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license   This file is part of SportsManagement.
+ * @package   sportsmanagement
+ * @subpackage results
+ */
 
 defined( '_JEXEC' ) or die( 'Restricted access' );
 ?>
@@ -310,7 +282,7 @@ $report_link = sportsmanagementHelperRoute::getSportsmanagementRoute('nextmatch'
 
 //echo sportsmanagementHelperHtml::getBootstrapModalImage('match_summary'.$game->id,$imgsummary,$imgTitle,'20');        
 //echo sportsmanagementHelperHtml::getBootstrapModalImage('match_content'.$game->id,$imgsummary,$imgTitle,'20');
-echo sportsmanagementHelperHtml::getBootstrapModalImage('match_content'.$game->id,$imgcontent,$imgTitle,'20',JURI::base().'index.php?tmpl=component&option=com_content&view=article&id='.$game->content_id);                
+echo sportsmanagementHelperHtml::getBootstrapModalImage('match_content'.$game->id,$imgcontent,$imgTitle,'20',JURI::base().'index.php?tmpl=component&option=com_content&view=article&id='.$game->content_id,$this->modalwidth,$this->modalheight);                
         ?>
 
             </td>
