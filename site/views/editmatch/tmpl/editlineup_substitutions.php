d0c8e94ccfa44634f661f13801ff66340c6b38a1
diddipoeler <famploeger@arcor.de>
2018-02-07 09:40:53 +0100

    Update editlineup_substitutions.php
    #551

--- site/views/editmatch/tmpl/editlineup_substitutions.php
+++ site/views/editmatch/tmpl/editlineup_substitutions.php
@@ -1,5 +1,13 @@
 <?php
-
+/** SportsManagement ein Programm zur Verwaltung für Sportarten
+ * @version   1.0.05
+ * @file      editlineup_substitutions.php
+ * @author    diddipoeler, stony, svdoldie und donclumsy (diddipoeler@arcor.de)
+ * @copyright Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license   This file is part of SportsManagement.
+ * @package   sportsmanagement
+ * @subpackage editmatch
+ */
 
 defined('_JEXEC') or die('Restricted access');
 
