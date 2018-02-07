51c87fbfa1c9f1f6452ac3ca09ab6a8017a6060e
diddipoeler <famploeger@arcor.de>
2018-02-07 09:16:38 +0100

    Update editlineup_staff.php
    #551

--- site/views/editmatch/tmpl/editlineup_staff.php
+++ site/views/editmatch/tmpl/editlineup_staff.php
@@ -1,41 +1,13 @@
 <?php 
-/** SportsManagement ein Programm zur Verwaltung für alle Sportarten
-* @version         1.0.05
-* @file                agegroup.php
-* @author                diddipoeler, stony, svdoldie und donclumsy (diddipoeler@arcor.de)
-* @copyright        Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
-* @license                This file is part of SportsManagement.
-*
-* SportsManagement is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* SportsManagement is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with SportsManagement.  If not, see <http://www.gnu.org/licenses/>.
-*
-* Diese Datei ist Teil von SportsManagement.
-*
-* SportsManagement ist Freie Software: Sie können es unter den Bedingungen
-* der GNU General Public License, wie von der Free Software Foundation,
-* Version 3 der Lizenz oder (nach Ihrer Wahl) jeder späteren
-* veröffentlichten Version, weiterverbreiten und/oder modifizieren.
-*
-* SportsManagement wird in der Hoffnung, dass es nützlich sein wird, aber
-* OHNE JEDE GEWÄHELEISTUNG, bereitgestellt; sogar ohne die implizite
-* Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
-* Siehe die GNU General Public License für weitere Details.
-*
-* Sie sollten eine Kopie der GNU General Public License zusammen mit diesem
-* Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
-*
-* Note : All ini files need to be saved as UTF-8 without BOM
-*/
+/** SportsManagement ein Programm zur Verwaltung fÃ¼r Sportarten
+ * @version   1.0.05
+ * @file      editlineup_staff.php
+ * @author    diddipoeler, stony, svdoldie und donclumsy (diddipoeler@arcor.de)
+ * @copyright Copyright: Â© 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license   This file is part of SportsManagement.
+ * @package   sportsmanagement
+ * @subpackage editmatch
+ */
 
 defined('_JEXEC') or die('Restricted access');
 ?>
@@ -76,8 +48,8 @@ defined('_JEXEC') or die('Restricted access');
 										<br />
 										
                                         
-                                        <input id="moveright" type="button" value="Move Right" onclick="move_list_items('staff','staffposition<?php echo $position_id;?>');" />
-                                        <input id="moveleft" type="button" value="Move Left" onclick="move_list_items('staffposition<?php echo $position_id;?>','staff');" />
+                                        <input id="moveright" type="button" value="<?php echo JText::_('COM_SPORTSMANAGEMENT_GLOBAL_RIGHT'); ?>" onclick="move_list_items('staff','staffposition<?php echo $position_id;?>');" />
+                                        <input id="moveleft" type="button" value="<?php echo JText::_('COM_SPORTSMANAGEMENT_GLOBAL_LEFT'); ?>" onclick="move_list_items('staffposition<?php echo $position_id;?>','staff');" />
                                         
                                         
                                         
@@ -93,7 +65,7 @@ defined('_JEXEC') or die('Restricted access');
 										<input	type="button" id="move_up('staffposition<?php echo $position_id;?>');" class="inputbox smove-up"
 												value="<?php echo JText::_('COM_SPORTSMANAGEMENT_GLOBAL_UP'); ?>" /><br />
 										<input	type="button" id="move_down('staffposition<?php echo $position_id;?>');" class="inputbox smove-down"
-												value="<?php echo JText::_('JGLOBAL_DOWN'); ?>" />
+												value="<?php echo JText::_('COM_SPORTSMANAGEMENT_GLOBAL_DOWN'); ?>" />
 									</td>
 								</tr>
 								<?php
