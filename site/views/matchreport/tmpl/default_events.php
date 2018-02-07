1fe55083c5f5304b5f8aa3b0074a279324fcded7
diddipoeler <famploeger@arcor.de>
2018-02-07 15:13:28 +0100

    Update default_events.php
    #551

--- site/views/matchreport/tmpl/default_events.php
+++ site/views/matchreport/tmpl/default_events.php
@@ -1,9 +1,9 @@
 <?php 
-/** SportsManagement ein Programm zur Verwaltung für alle Sportarten
+/** SportsManagement ein Programm zur Verwaltung fÃ¼r alle Sportarten
  * @version   1.0.05
  * @file      default_events.php
  * @author    diddipoeler, stony, svdoldie und donclumsy (diddipoeler@arcor.de)
- * @copyright Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @copyright Copyright: Â© 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
  * @license   This file is part of SportsManagement.
  * @package   sportsmanagement
  * @subpackage matchreport
@@ -15,6 +15,11 @@ defined( '_JEXEC' ) or die( 'Restricted access' );
 
 <h2>
 <?php 
+if ( $this->config['show_timeline'] && !$this->config['show_timeline_under_results'] )
+{
+echo $this->loadTemplate('timeline');
+}
+	
 echo JText::_('COM_SPORTSMANAGEMENT_MATCHREPORT_EVENTS'); 
 ?>
 </h2>		
