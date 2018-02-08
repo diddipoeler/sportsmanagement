650090964760dac76c3ab79e55baf8e964d0d05a
diddipoeler <famploeger@arcor.de>
2018-02-08 12:20:27 +0100

    Update projectpositions.php
    #563

--- admin/models/projectpositions.php
+++ admin/models/projectpositions.php
@@ -1,41 +1,12 @@
 <?php
 /** SportsManagement ein Programm zur Verwaltung für alle Sportarten
-* @version         1.0.05
-* @file                agegroup.php
-* @author                diddipoeler, stony, svdoldie und donclumsy (diddipoeler@arcor.de)
-* @copyright        Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
-* @license                This file is part of SportsManagement.
-*
-* SportsManagement is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* SportsManagement is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with SportsManagement.  If not, see <http://www.gnu.org/licenses/>.
-*
-* Diese Datei ist Teil von SportsManagement.
-*
-* SportsManagement ist Freie Software: Sie können es unter den Bedingungen
-* der GNU General Public License, wie von der Free Software Foundation,
-* Version 3 der Lizenz oder (nach Ihrer Wahl) jeder späteren
-* veröffentlichten Version, weiterverbreiten und/oder modifizieren.
-*
-* SportsManagement wird in der Hoffnung, dass es nützlich sein wird, aber
-* OHNE JEDE GEWÄHELEISTUNG, bereitgestellt; sogar ohne die implizite
-* Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
-* Siehe die GNU General Public License für weitere Details.
-*
-* Sie sollten eine Kopie der GNU General Public License zusammen mit diesem
-* Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
-*
-* Note : All ini files need to be saved as UTF-8 without BOM
-*/
+ * @version   1.0.05
+ * @file      projectpositions.php
+ * @author    diddipoeler, stony, svdoldie und donclumsy (diddipoeler@arcor.de)
+ * @copyright Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license   This file is part of SportsManagement.
+ * @subpackage projectpositions
+ */
 
 // Check to ensure this file is included in Joomla!
 defined('_JEXEC') or die('Restricted access');
@@ -159,9 +130,6 @@ class sportsmanagementModelProjectpositions extends JSMModelList
         $query->select('('.$subQuery2.') AS countStats');
         
         $query->where('pt.project_id = '.$this->getState('filter.pid') );
-
-//        $query->where(self::_buildContentWhere());
-//		$query->order(self::_buildContentOrderBy());
         
         $query->order($db->escape($this->getState('list.ordering', 'po.name')).' '.
                 $db->escape($this->getState('list.direction', 'ASC')));
@@ -177,7 +145,13 @@ $app->enqueueMessage(JText::_(__METHOD__.' '.__LINE__.' <br><pre>'.print_r($quer
     
     
    
-    /**
+   function updateprojectpositions($items=NULL)
+   {
+	   
+	   
+   }
+	
+   /**
      * sportsmanagementModelProjectpositions::insertStandardProjectPositions()
      * 
      * @param integer $project_id
@@ -226,34 +200,6 @@ $app->enqueueMessage(JText::_(__METHOD__.' '.__LINE__.' <br><pre>'.print_r($quer
     
         
     }
-
-//	function _buildContentOrderBy()
-//	{
-//		$option = JFactory::getApplication()->input->getCmd('option');
-//		$app = JFactory::getApplication();
-//		$filter_order		= $app->getUserStateFromRequest($option.'.'.$this->_identifier.'.po_filter_order','filter_order','po.name','cmd');
-//		$filter_order_Dir	= $app->getUserStateFromRequest($option.'.'.$this->_identifier.'.po_filter_order_Dir','filter_order_Dir','','word');
-//
-//		if ($filter_order=='po.name')
-//		{
-//			$orderby=' po.parent_id,po.name '.$filter_order_Dir;
-//		}
-//		else
-//		{
-//			$orderby=' '.$filter_order.' '.$filter_order_Dir.',po.name ';
-//		}
-//		return $orderby;
-//	}
-
-//	function _buildContentWhere()
-//	{
-//		$option = JFactory::getApplication()->input->getCmd('option');
-//		$app = JFactory::getApplication();
-//		$where =' pt.project_id='.$this->_project_id;
-//		return $where;
-//	}
-
-	
 
 	/**
 	 * Method to return the positions which are subpositions and are equal to a sportstype array (id,name)
