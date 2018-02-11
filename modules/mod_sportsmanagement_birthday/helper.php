eba44bc17692751e0b314f92a032aef88b601520
Appukonrad <appukonrad@gmail.com>
2018-02-11 12:56:48 +0100

    fix count() warning, fix php brackets

--- modules/mod_sportsmanagement_birthday/helper.php
+++ modules/mod_sportsmanagement_birthday/helper.php
@@ -1,42 +1,42 @@
 <?php
-/** SportsManagement ein Programm zur Verwaltung für alle Sportarten
-* @version         1.0.05
-* @file                agegroup.php
-* @author                diddipoeler, stony, svdoldie und donclumsy (diddipoeler@arcor.de)
-* @copyright        Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
-* @license                This file is part of SportsManagement.
-*
-* SportsManagement is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* SportsManagement is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with SportsManagement.  If not, see <http://www.gnu.org/licenses/>.
-*
-* Diese Datei ist Teil von SportsManagement.
-*
-* SportsManagement ist Freie Software: Sie können es unter den Bedingungen
-* der GNU General Public License, wie von der Free Software Foundation,
-* Version 3 der Lizenz oder (nach Ihrer Wahl) jeder späteren
-* veröffentlichten Version, weiterverbreiten und/oder modifizieren.
-*
-* SportsManagement wird in der Hoffnung, dass es nützlich sein wird, aber
-* OHNE JEDE GEWÄHELEISTUNG, bereitgestellt; sogar ohne die implizite
-* Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
-* Siehe die GNU General Public License für weitere Details.
-*
-* Sie sollten eine Kopie der GNU General Public License zusammen mit diesem
-* Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
-*
-* Note : All ini files need to be saved as UTF-8 without BOM
-*/
 
+/** SportsManagement ein Programm zur Verwaltung für alle Sportarten
+ * @version         1.0.05
+ * @file                agegroup.php
+ * @author                diddipoeler, stony, svdoldie und donclumsy (diddipoeler@arcor.de)
+ * @copyright        Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license                This file is part of SportsManagement.
+ *
+ * SportsManagement is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SportsManagement is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SportsManagement.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Diese Datei ist Teil von SportsManagement.
+ *
+ * SportsManagement ist Freie Software: Sie können es unter den Bedingungen
+ * der GNU General Public License, wie von der Free Software Foundation,
+ * Version 3 der Lizenz oder (nach Ihrer Wahl) jeder späteren
+ * veröffentlichten Version, weiterverbreiten und/oder modifizieren.
+ *
+ * SportsManagement wird in der Hoffnung, dass es nützlich sein wird, aber
+ * OHNE JEDE GEWÄHELEISTUNG, bereitgestellt; sogar ohne die implizite
+ * Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
+ * Siehe die GNU General Public License für weitere Details.
+ *
+ * Sie sollten eine Kopie der GNU General Public License zusammen mit diesem
+ * Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
+ *
+ * Note : All ini files need to be saved as UTF-8 without BOM
+ */
 // no direct access
 defined('_JEXEC') or die('Restricted access');
 $mainframe = JFactory::getApplication();
@@ -46,94 +46,93 @@ $crew = array();
 
 //$mainframe->enqueueMessage(JText::_(__FILE__.' '.__LINE__.' params<br><pre>'.print_r($params,true).'</pre>'),'');
 
-if(!function_exists('jl_birthday_sort'))
-{
-	// snippet taken from http://de3.php.net/manual/en/function.uasort.php
-	/**
-	 * jl_birthday_sort()
-	 * 
-	 * @param mixed $array
-	 * @param mixed $arguments
-	 * @param bool $keys
-	 * @return
-	 */
-	function jl_birthday_sort ($array, $arguments = array(), $keys = true) {
-		$code = "\$result=0;";
-		foreach ($arguments as $argument) {
-			$field = substr($argument, 2, strlen($argument));
-			$type = $argument[0];
-			$order = $argument[1];
-			$code .= "if (!Is_Numeric(\$result) || \$result == 0) ";
-			if (strtolower($type) == "n") $code .= $order == "-" ? "\$result = (intval(\$a['{$field}']) > intval(\$b['{$field}']) ? -1 : (intval(\$a['{$field}']) < intval(\$b['{$field}']) ? 1 : 0));" : "\$result = (intval(\$a['{$field}']) > intval(\$b['{$field}']) ? 1 : (intval(\$a['{$field}']) < intval(\$b['{$field}']) ? -1 : 0));";
-			else $code .= $order == "-" ? "\$result = strcoll(\$a['{$field}'], \$b['{$field}']) * -1;" : "\$result = strcoll(\$a['{$field}'], \$b['{$field}']);";
-		}
-		$code .= "return \$result;";
-		$compare = create_function('$a, $b', $code);
-		if ($keys) uasort($array, $compare);
-		else usort($array, $compare);
-		return $array;
-	}
+if (!function_exists('jl_birthday_sort')) {
+
+    // snippet taken from http://de3.php.net/manual/en/function.uasort.php
+    /**
+     * jl_birthday_sort()
+     * 
+     * @param mixed $array
+     * @param mixed $arguments
+     * @param bool $keys
+     * @return
+     */
+    function jl_birthday_sort($array, $arguments = array(), $keys = true) {
+        $code = "\$result=0;";
+        foreach ($arguments as $argument) {
+            $field = substr($argument, 2, strlen($argument));
+            $type = $argument[0];
+            $order = $argument[1];
+            $code .= "if (!Is_Numeric(\$result) || \$result == 0) ";
+            if (strtolower($type) == "n") {
+                $code .= $order == "-" ? "\$result = (intval(\$a['{$field}']) > intval(\$b['{$field}']) ? -1 : (intval(\$a['{$field}']) < intval(\$b['{$field}']) ? 1 : 0));" : "\$result = (intval(\$a['{$field}']) > intval(\$b['{$field}']) ? 1 : (intval(\$a['{$field}']) < intval(\$b['{$field}']) ? -1 : 0));";
+            } else {
+                $code .= $order == "-" ? "\$result = strcoll(\$a['{$field}'], \$b['{$field}']) * -1;" : "\$result = strcoll(\$a['{$field}'], \$b['{$field}']);";
+            }
+        }
+        $code .= "return \$result;";
+        $compare = create_function('$a, $b', $code);
+
+        if ($keys) {
+            uasort($array, $compare);
+        } else {
+            usort($array, $compare);
+        }
+        return $array;
+    }
+
 }
 
-$usedp = $params->get('projects','0');
-$p = (is_array($usedp)) ? implode(",",array_map('intval', $usedp)) : (int) $usedp;
+$usedp = $params->get('projects', '0');
+$p = (is_array($usedp)) ? implode(",", array_map('intval', $usedp)) : (int) $usedp;
 
 
 //$usedp = $params->get('teams','0');
 //$usedteams = (is_array($usedp)) ? implode(",", $usedp) : $usedp;
-
 //$mainframe->enqueueMessage(JText::_(__FILE__.' '.__LINE__.' usedteams<br><pre>'.print_r($usedteams,true).'</pre>'),'');
 
 $usedteams = "";
 
 // get favorite team(s), we have to make a function for this
-if ($params->get('use_fav')==1)
-{
+if ($params->get('use_fav') == 1) {
     $query = $database->getQuery(true);
     $query->select('fav_team');
     $query->from('#__sportsmanagement_project');
-            
-	if ($p!='' && $p>0) 
-    $query->where('id IN ('.$p.')');
 
-    ;
+    if ($p != '' && $p > 0) {
+        $query->where('id IN (' . $p . ')');
+    }
 
-	$database->setQuery($query);
-    
+    $database->setQuery($query);
+
     //$mainframe->enqueueMessage(JText::_(__FILE__.' '.__LINE__.' <br><pre>'.print_r($query->dump(),true).'</pre>'),'');
-    
-	$temp = $database->loadResultArray();
-    
-    if ( !$temp )
-    {
+
+    $temp = $database->loadResultArray();
+
+    if (!$temp) {
         //$mainframe->enqueueMessage(JText::_(__FILE__.' '.__LINE__.' <br><pre>'.print_r($database->getErrorMsg(),true).'</pre>'),'Error');
     }
 
-	if ( count($temp) > 0 )
-	{
-		$usedteams = join(',', array_filter($temp));
-	}
-}
-else
-{
-	//$usedteams = $params->get('teams');
-    $usedp = $params->get('teams','0');
-    $usedteams = (is_array($usedp)) ? implode(",",array_map('intval', $usedp)) : (int) $usedp;
-
+    if (!empty($temp)) {
+        $usedteams = join(',', array_filter($temp));
+    }
+} else {
+    //$usedteams = $params->get('teams');
+    $usedp = $params->get('teams', '0');
+    $usedteams = (is_array($usedp)) ? implode(",", array_map('intval', $usedp)) : (int) $usedp;
 }
 
-$birthdaytext='';
+$birthdaytext = '';
 
 // get player info, we have to make a function for this
 $dateformat = "DATE_FORMAT(p.birthday,'%Y-%m-%d') AS date_of_birth";
 
-if ($params->get('use_which') <= 1)
-{
+if ($params->get('use_which') <= 1) {
     $query = $database->getQuery(true);
     $query->clear();
-         
-    $query->select('p.id, p.birthday, p.firstname, p.nickname, p.lastname,p.picture AS default_picture, p.country,DATE_FORMAT(p.birthday, \'%m-%d\')AS daymonth');        
-	$query->select('YEAR( CURRENT_DATE( ) ) as year');
+
+    $query->select('p.id, p.birthday, p.firstname, p.nickname, p.lastname,p.picture AS default_picture, p.country,DATE_FORMAT(p.birthday, \'%m-%d\')AS daymonth');
+    $query->select('YEAR( CURRENT_DATE( ) ) as year');
     $query->select('(YEAR( CURRENT_DATE( ) ) - YEAR( p.birthday ) +	IF(DATE_FORMAT(CURDATE(), \'%m.%d\') > DATE_FORMAT(p.birthday, \'%m.%d\'), 1, 0)) AS age');
     $query->select($dateformat);
     $query->select('stp.picture');
@@ -141,68 +140,61 @@ if ($params->get('use_which') <= 1)
     $query->select('\'person\' AS func_to_call, \'\' project_id, \'\' team_id');
     $query->select('\'pid\' AS id_to_append, 1 AS type');
     $query->select('st.team_id, pt.project_id');
-    
+
     $query->select('CONCAT_WS(\':\',pro.id,pro.alias) AS project_slug');
     $query->select('CONCAT_WS(\':\',p.id,p.alias) AS person_slug');
     $query->select('CONCAT_WS(\':\',t.id,t.alias) AS team_slug');
-    
+
     $query->from('#__sportsmanagement_person AS p ');
-    $query->join('INNER','#__sportsmanagement_season_team_person_id as stp ON stp.person_id = p.id ');
-    $query->join('INNER','#__sportsmanagement_season_team_id as st ON st.team_id = stp.team_id ');
-    $query->join('INNER','#__sportsmanagement_project_team as pt ON st.id = pt.team_id ');
-    $query->join('INNER','#__sportsmanagement_project AS pro ON pro.id = pt.project_id');
-    $query->join('INNER','#__sportsmanagement_team AS t ON t.id = st.team_id');
+    $query->join('INNER', '#__sportsmanagement_season_team_person_id as stp ON stp.person_id = p.id ');
+    $query->join('INNER', '#__sportsmanagement_season_team_id as st ON st.team_id = stp.team_id ');
+    $query->join('INNER', '#__sportsmanagement_project_team as pt ON st.id = pt.team_id ');
+    $query->join('INNER', '#__sportsmanagement_project AS pro ON pro.id = pt.project_id');
+    $query->join('INNER', '#__sportsmanagement_team AS t ON t.id = st.team_id');
     $query->where('p.published = 1 AND p.birthday != \'0000-00-00\'');
     $query->where('stp.persontype = 1');
 
-    if ( $params->get('agegrouplist') ) 
-    {
-	$query->where('p.agegroup_id = '.$params->get('agegrouplist') );    
+    if ($params->get('agegrouplist')) {
+        $query->where('p.agegroup_id = ' . $params->get('agegrouplist'));
     }
-	if ( $usedteams != '' ) 
-    $query->where('st.team_id IN ('.$usedteams.')');
 
-
-	if ( $p != '' && $p>0 ) 
-    $query->where('pt.project_id IN ('.$p.')');
-
-
-	$query->group('p.id');
-
-	$maxDays = $params->get('maxdays');
-	if ((strlen($maxDays) > 0) && (intval($maxDays) >= 0))
-	{
-        $query->having('days_to_birthday <= '. intval($maxDays));
-	}
-
-	$query->order('days_to_birthday ASC');
-
-	if ($params->get('limit') > 0)
-    {
-    $database->setQuery($query,0,$params->get('limit'));    
+    if ($usedteams != '') {
+        $query->where('st.team_id IN (' . $usedteams . ')');
     }
-    else
-    {
-    $database->setQuery($query);    
-    }    
+
+    if ($p != '' && $p > 0) {
+        $query->where('pt.project_id IN (' . $p . ')');
+    }
+
+    $query->group('p.id');
+
+    $maxDays = $params->get('maxdays');
+    if ((strlen($maxDays) > 0) && (intval($maxDays) >= 0)) {
+        $query->having('days_to_birthday <= ' . intval($maxDays));
+    }
+
+    $query->order('days_to_birthday ASC');
+
+    if ($params->get('limit') > 0) {
+        $database->setQuery($query, 0, $params->get('limit'));
+    } else {
+        $database->setQuery($query);
+    }
     //$query->setLimit($params->get('limit'));
-
-	//$database->setQuery($query);
-	//echo("<hr>".$database->getQuery($query));
-    
+    //$database->setQuery($query);
+    //echo("<hr>".$database->getQuery($query));
     //$mainframe->enqueueMessage(JText::_(__FILE__.' '.__LINE__.' <br><pre>'.print_r($query->dump(),true).'</pre>'),'');
-    
-	$players = $database->loadAssocList();
+
+    $players = $database->loadAssocList();
 }
 
 //get staff info, we have to make a function for this
-if ($params->get('use_which') == 2 || $params->get('use_which') == 0)
-{
+if ($params->get('use_which') == 2 || $params->get('use_which') == 0) {
     $query = $database->getQuery(true);
     $query->clear();
 
-	$query->select('p.id, p.birthday, p.firstname, p.nickname, p.lastname,p.picture AS default_picture, p.country,DATE_FORMAT(p.birthday, \'%m-%d\')AS daymonth');        
-	$query->select('YEAR( CURRENT_DATE( ) ) as year');
+    $query->select('p.id, p.birthday, p.firstname, p.nickname, p.lastname,p.picture AS default_picture, p.country,DATE_FORMAT(p.birthday, \'%m-%d\')AS daymonth');
+    $query->select('YEAR( CURRENT_DATE( ) ) as year');
     $query->select('(YEAR( CURRENT_DATE( ) ) - YEAR( p.birthday ) +	IF(DATE_FORMAT(CURDATE(), \'%m.%d\') > DATE_FORMAT(p.birthday, \'%m.%d\'), 1, 0)) AS age');
     $query->select($dateformat);
     $query->select('stp.picture');
@@ -210,63 +202,55 @@ if ($params->get('use_which') == 2 || $params->get('use_which') == 0)
     $query->select('\'staff\' AS func_to_call, \'\' project_id, \'\' team_id');
     $query->select('st.team_id, pt.project_id');
     $query->select('\'tsid\' AS id_to_append, 2 AS type');
-    
+
     $query->select('CONCAT_WS(\':\',pro.id,pro.alias) AS project_slug');
     $query->select('CONCAT_WS(\':\',p.id,p.alias) AS person_slug');
     $query->select('CONCAT_WS(\':\',t.id,t.alias) AS team_slug');
-    
+
     $query->from('#__sportsmanagement_person AS p ');
-    $query->join('INNER','#__sportsmanagement_season_team_person_id as stp ON stp.person_id = p.id ');
-    $query->join('INNER','#__sportsmanagement_season_team_id as st ON st.team_id = stp.team_id ');
-    $query->join('INNER','#__sportsmanagement_project_team as pt ON st.id = pt.team_id ');
-    $query->join('INNER','#__sportsmanagement_project AS pro ON pro.id = pt.project_id'); 
-    $query->join('INNER','#__sportsmanagement_team AS t ON t.id = st.team_id');
-    
+    $query->join('INNER', '#__sportsmanagement_season_team_person_id as stp ON stp.person_id = p.id ');
+    $query->join('INNER', '#__sportsmanagement_season_team_id as st ON st.team_id = stp.team_id ');
+    $query->join('INNER', '#__sportsmanagement_project_team as pt ON st.id = pt.team_id ');
+    $query->join('INNER', '#__sportsmanagement_project AS pro ON pro.id = pt.project_id');
+    $query->join('INNER', '#__sportsmanagement_team AS t ON t.id = st.team_id');
+
     $query->where('p.published = 1 AND p.birthday != \'0000-00-00\'');
     $query->where('stp.persontype = 2');
-    if ( $params->get('agegrouplist') ) 
-    {
-	$query->where('p.agegroup_id = '.$params->get('agegrouplist') );    
+    if ($params->get('agegrouplist')) {
+        $query->where('p.agegroup_id = ' . $params->get('agegrouplist'));
     }
     // Exclude players from the staff query to avoid duplicate persons (if a person is both player and staff)
-	if(count($players) > 0)
-	{
-		$ids = "0";
-		foreach ($players AS $player)
-		{
-			$ids .= "," . $player['id'];
-		}
-        $query->where('p.id NOT IN ('.$ids.')');
-	}
-
-	if ($usedteams!='') 
-    $query->where('st.team_id IN ('.$usedteams.')');
-
-	if ($p!='' && $p>0) 
-    $query->where('pt.project_id IN ('.$p.')');
-
-	$query->group('p.id');
-
-	$maxDays = $params->get('maxdays');
-	if ((strlen($maxDays) > 0) && (intval($maxDays) >= 0))
-	{
-        $query->having('days_to_birthday <= '. intval($maxDays));
-	}
-
-	$query->order('days_to_birthday ASC');
-
-	if ($params->get('limit') > 0)
-    {
-    $database->setQuery($query,0,$params->get('limit'));    
+    if (count($players) > 0) {
+        $ids = "0";
+        foreach ($players AS $player) {
+            $ids .= "," . $player['id'];
+        }
+        $query->where('p.id NOT IN (' . $ids . ')');
     }
-    else
-    {
-    $database->setQuery($query);    
-    }  
-    
+
+    if ($usedteams != '') {
+        $query->where('st.team_id IN (' . $usedteams . ')');
+    }
+    if ($p != '' && $p > 0) {
+        $query->where('pt.project_id IN (' . $p . ')');
+    }
+    $query->group('p.id');
+
+    $maxDays = $params->get('maxdays');
+    if ((strlen($maxDays) > 0) && (intval($maxDays) >= 0)) {
+        $query->having('days_to_birthday <= ' . intval($maxDays));
+    }
+
+    $query->order('days_to_birthday ASC');
+
+    if ($params->get('limit') > 0) {
+        $database->setQuery($query, 0, $params->get('limit'));
+    } else {
+        $database->setQuery($query);
+    }
+
     //$mainframe->enqueueMessage(JText::_(__FILE__.' '.__LINE__.' <br><pre>'.print_r($query->dump(),true).'</pre>'),'');
-    
-	//echo("<hr>".$database->getQuery($query));
-	$crew = $database->loadAssocList();
+    //echo("<hr>".$database->getQuery($query));
+    $crew = $database->loadAssocList();
 }
 ?>
