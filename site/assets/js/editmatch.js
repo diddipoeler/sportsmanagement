2208036251e000308cf4250e565c4a9d50ceda49
diddipoeler <famploeger@arcor.de>
2018-02-12 09:29:44 +0100

    Create editmatch.js
    #563

---
+++ site/assets/js/editmatch.js
@@ -0,0 +1,21 @@
+jQuery(document).ready(function()  {
+
+// neuen wechsel speichern     
+//jQuery("#save-new-subst").addEvent('click', save_new_subst);
+//document.getElementById('save-new-subst').onclick = save_new_subst();
+//jQuery("#ajaxresponse").html('hallo');
+//alert('hallo');
+});
+
+
+
+// hier sind die funktionen
+function save_new_subst(matchid)
+{
+//jQuery("#ajaxresponse").html(matchid);
+//jQuery("#ajaxresponse").html('hallo');
+//alert(baseajaxurl);
+
+
+
+}
