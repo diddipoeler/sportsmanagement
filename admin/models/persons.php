9469dfb94d3dd5618aaaeeecabf6647b78896682
diddipoeler <famploeger@arcor.de>
2018-02-13 09:16:07 +0100

    Update persons.php
    #563

--- admin/models/persons.php
+++ admin/models/persons.php
@@ -1,10 +1,12 @@
 <?php
-/** SportsManagement ein Programm zur Verwaltung für alle Sportarten
+/** SportsManagement ein Programm zur Verwaltung für Sportarten
  * @version   1.0.05
  * @file      persons.php
  * @author    diddipoeler, stony, svdoldie und donclumsy (diddipoeler@arcor.de)
  * @copyright Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
  * @license   This file is part of SportsManagement.
+ * @package   sportsmanagement
+ * @subpackage persons
  */
 
 // Check to ensure this file is included in Joomla!
