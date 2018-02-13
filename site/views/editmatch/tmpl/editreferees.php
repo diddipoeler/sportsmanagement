c0de422a3b420bdffccd80a31aaf6a0794b14d1e
diddipoeler <famploeger@arcor.de>
2018-02-13 11:07:32 +0100

    Update editreferees.php
    #563

--- site/views/editmatch/tmpl/editreferees.php
+++ site/views/editmatch/tmpl/editreferees.php
@@ -28,14 +28,11 @@ $params = $this->form->getFieldsets('params');
 
 ?>
 <div id="lineup">
-	<form  action="<?php echo JRoute::_('index.php?option=com_sportsmanagement');?>" id='component-form' method='post' style='display:inline' name='adminform' >
+	<form  action="<?php echo JRoute::_('index.php?option=com_sportsmanagement');?>" id='editreferees' method='post' style='display:inline' name='editreferees' >
 	<fieldset>
 		<div class="fltrt">
-<!--
-					<button type="button" onclick="Joomla.submitform('editmatch.save', this.form);">
-						<?php echo JText::_('JSAVE');?></button>
-                        -->
-<input type='submit' name='save' value='<?php echo JText::_('JSAVE' );?>' />			
+<button type="button" onclick="jQuery('select.position-starters option').prop('selected', 'selected');Joomla.submitform('editmatch.save', this.form);">
+<?php echo JText::_('JSAVE');?></button>			
 		</div>
 		<div class="configuration" >
 			<?php echo JText::sprintf('COM_SPORTSMANAGEMENT_ADMIN_MATCH_ER_TITLE'); ?>
