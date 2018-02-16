c12c68c7a1ba1d4ed77447d186b2383c1ed87a0b
diddipoeler <famploeger@arcor.de>
2018-02-16 09:47:12 +0100

    Create default_played_matches.php
    #563

---
+++ site/views/playground/tmpl/default_played_matches.php
@@ -0,0 +1,11 @@
+<?php 
+/** SportsManagement ein Programm zur Verwaltung für alle Sportarten
+ * @version   1.0.05
+ * @file      default_played_matches.php
+ * @author    diddipoeler, stony, svdoldie und donclumsy (diddipoeler@arcor.de)
+ * @copyright Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license   This file is part of SportsManagement.
+ * @subpackage playground
+ */
+defined( '_JEXEC' ) or die( 'Restricted access' ); 
+?>
