452ead66dbc58994356ec4efc5bc759e9ac4fb85
diddipoeler <famploeger@arcor.de>
2018-02-16 15:35:58 +0100

    #563

--- site/views/results/tmpl/form_row.php
+++ site/views/results/tmpl/form_row.php
@@ -14,15 +14,8 @@ defined('_JEXEC') or die('Restricted access');
 if ( $this->overallconfig['use_jquery_modal'] )
 {
 ?>
-<style>
-  
-
-</style>
-
-
-<script type="text/javascript" language="javascript" >
-    
-</script>
+<link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap3-dialog/1.34.9/css/bootstrap-dialog.min.css" rel="stylesheet" type="text/css" />
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap3-dialog/1.34.9/js/bootstrap-dialog.min.js"></script>
 
 <?php 
 }	
