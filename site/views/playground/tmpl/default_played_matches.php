accc456050634fc4bafbac1ddb1ae74b026595b1
diddipoeler <famploeger@arcor.de>
2018-02-16 10:02:05 +0100

    Update default_played_matches.php
    #563

--- site/views/playground/tmpl/default_played_matches.php
+++ site/views/playground/tmpl/default_played_matches.php
@@ -9,3 +9,95 @@
  */
 defined( '_JEXEC' ) or die( 'Restricted access' ); 
 ?>
+
+<?php
+
+if ( $this->playedgames )
+{
+	?>
+	<!-- Playground next games -->
+<h2><?php echo JText::_('COM_SPORTSMANAGEMENT_PLAYGROUND_PREV_GAMES'); ?></h2>
+		<div class="row-fluid">
+					<table class="<?php echo $this->config['matches_table_class']; ?>" >
+						<?php
+						//sort games by dates
+						$gamesByDate = Array();
+						foreach ( $this->playedgames as $game )
+						{
+							$gamesByDate[substr( $game->match_date, 0, 10 )][] = $game;
+						}
+						// $teams = $this->project->getTeamsFromMatches( $this->games );
+						$colspan = 5;
+						if ($this->config['show_logo'] == 1) 
+                        {
+							$colspan = 7;
+						}
+						foreach ( $gamesByDate as $date => $games )
+						{
+							?>
+							<tr>
+								<td align="left" colspan="<?php echo $colspan; ?>" class="sectiontableheader">
+									<?php
+									echo JHtml::date($date, JText::_('COM_SPORTSMANAGEMENT_GLOBAL_MATCHDAYDATE'));
+									?>
+								</td>
+							</tr>
+							<?php
+							foreach ( $games as $game )
+							{
+								$home = $this->gamesteams[$game->team1];
+								$away = $this->gamesteams[$game->team2];
+								?>
+								<tr class="sectiontableentry1">
+									<td>
+										<?php
+										echo substr( $game->match_date, 11, 5 );
+										?>
+									</td>
+									<td class="nowrap">
+										<?php
+										echo $game->project_name;
+										?>
+									</td>
+									<?php
+									if ($this->config['show_logo'] == 1) {
+										//$model = $this->getModel();
+										$home_logo = sportsmanagementModelteam::getTeamLogo($home->id);
+										$away_logo = sportsmanagementModelteam::getTeamLogo($away->id);
+										$teamA = '<td align="right" valign="top" class="nowrap">';
+										$teamA .= " " . sportsmanagementModelProject::getClubIconHtml( $home_logo[0], 1 );
+										$teamA .= '</td>';
+										echo $teamA;
+									}
+									?>
+									<td class="nowrap">
+										<?php
+										echo $home->name;
+										?>
+									</td>
+									<td class="nowrap">-</td>
+									<?php
+									if ($this->config['show_logo'] == 1) {
+										$teamB = '<td align="right" valign="top" class="nowrap">';
+										$teamB .= " " . sportsmanagementModelProject::getClubIconHtml( $away_logo[0], 1 );
+										$teamB .= '</td>';
+										echo $teamB;
+									}
+									?>
+									<td class="nowrap">
+										<?php
+										echo $away->name;
+										?>
+									</td>
+								</tr>
+								<?php
+							}
+						}
+						?>
+					</table>
+			</div>
+	
+	<!-- End of playground next games -->
+	<?php
+}
+?>
