a7ccb84ebb9508c915432579a743b6327512e935
diddipoeler <famploeger@arcor.de>
2018-02-16 11:27:59 +0100

    Update countries.php
    #563

--- site/helpers/countries.php
+++ site/helpers/countries.php
@@ -1,42 +1,14 @@
 <?php
-
-/** SportsManagement ein Programm zur Verwaltung für alle Sportarten
- * @version         1.0.05
- * @file                agegroup.php
- * @author                diddipoeler, stony, svdoldie und donclumsy (diddipoeler@arcor.de)
- * @copyright        Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license                This file is part of SportsManagement.
- *
- * SportsManagement is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * SportsManagement is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with SportsManagement.  If not, see <http://www.gnu.org/licenses/>.
- *
- * Diese Datei ist Teil von SportsManagement.
- *
- * SportsManagement ist Freie Software: Sie können es unter den Bedingungen
- * der GNU General Public License, wie von der Free Software Foundation,
- * Version 3 der Lizenz oder (nach Ihrer Wahl) jeder späteren
- * veröffentlichten Version, weiterverbreiten und/oder modifizieren.
- *
- * SportsManagement wird in der Hoffnung, dass es nützlich sein wird, aber
- * OHNE JEDE GEWÄHELEISTUNG, bereitgestellt; sogar ohne die implizite
- * Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
- * Siehe die GNU General Public License für weitere Details.
- *
- * Sie sollten eine Kopie der GNU General Public License zusammen mit diesem
- * Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
- *
- * Note : All ini files need to be saved as UTF-8 without BOM
+/** SportsManagement ein Programm zur Verwaltung für Sportarten
+ * @version   1.0.05
+ * @file      countries.php
+ * @author    diddipoeler, stony, svdoldie und donclumsy (diddipoeler@arcor.de)
+ * @copyright Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license   This file is part of SportsManagement.
+ * @package   sportsmanagement
+ * @subpackage helpers
  */
+
 // no direct access
 defined('_JEXEC') or die('Restricted access');
 jimport('joomla.utilities.arrayhelper');
@@ -103,16 +75,11 @@ class JSMCountries {
      * @param string $text_tag
      * @return
      */
-    public static function getCountryOptions($value_tag = 'value', $text_tag = 'text') {
+    public static function getCountryOptions($value_tag = 'value', $text_tag = 'text', $useflag = 0) {
         $app = JFactory::getApplication();
         // JInput object
         $jinput = $app->input;
         $option = $jinput->getCmd('option');
-        // welche tabelle soll genutzt werden
-//$params = JComponentHelper::getParams( 'com_sportsmanagement' );
-//$database_table	= $params->get( 'cfg_which_database_table' );
-        // Get a db connection.
-//$db = sportsmanagementHelper::getDBConnection(TRUE, $app->getUserState( "com_sportsmanagement.cfg_which_database", FALSE ) );
         $db = sportsmanagementHelper::getDBConnection();
 
 // Create a new query object.
@@ -121,12 +88,8 @@ class JSMCountries {
         $query->select('alpha3,name');
         // From table
         $query->from('#__sportsmanagement_countries');
-        //$query->from('#__SPORTSMANAGEMENT_countries');
-        // Reset the query using our newly populated query object.
         $db->setQuery($query);
         $countries = $db->loadAssocList();
-
-        //$app->enqueueMessage(JText::_(get_class($this).' '.__FUNCTION__.'<br><pre>'.print_r($countries,true).'</pre>'),'');
 
         $options = array();
         foreach ($countries AS $k) {
