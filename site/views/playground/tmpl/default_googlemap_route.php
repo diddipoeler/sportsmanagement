c25fd4aff6f03694eeb0abe30f4183349675a198
diddipoeler <famploeger@arcor.de>
2018-02-18 12:27:25 +0100

    #366

--- site/views/playground/tmpl/default_googlemap_route.php
+++ site/views/playground/tmpl/default_googlemap_route.php
@@ -5,9 +5,17 @@
  * @author    diddipoeler, stony, svdoldie und donclumsy (diddipoeler@arcor.de)
  * @copyright Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
  * @license   This file is part of SportsManagement.
+ * @package   sportsmanagement
  * @subpackage playground
  */
 
 defined( '_JEXEC' ) or die( 'Restricted access' );
 
 ?>
+
+<?php echo JText::_('COM_SPORTSMANAGEMENT_PLAYGROUND_GOOGLE_ROUTE'); ?>
+<div class="row-fluid">
+<?php 
+
+?>
+</div>
