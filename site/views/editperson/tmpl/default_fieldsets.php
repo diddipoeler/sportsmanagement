9dc1116be4f1e34ad1a883b3ad3374b0a236a359
diddipoeler <famploeger@arcor.de>
2018-02-19 09:21:23 +0100

    Update default_fieldsets.php
    #563

--- site/views/editperson/tmpl/default_fieldsets.php
+++ site/views/editperson/tmpl/default_fieldsets.php
@@ -1,41 +1,14 @@
 <?php
-/** SportsManagement ein Programm zur Verwaltung für alle Sportarten
-* @version         1.0.05
-* @file                agegroup.php
-* @author                diddipoeler, stony, svdoldie und donclumsy (diddipoeler@arcor.de)
-* @copyright        Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
-* @license                This file is part of SportsManagement.
-*
-* SportsManagement is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* SportsManagement is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with SportsManagement.  If not, see <http://www.gnu.org/licenses/>.
-*
-* Diese Datei ist Teil von SportsManagement.
-*
-* SportsManagement ist Freie Software: Sie können es unter den Bedingungen
-* der GNU General Public License, wie von der Free Software Foundation,
-* Version 3 der Lizenz oder (nach Ihrer Wahl) jeder späteren
-* veröffentlichten Version, weiterverbreiten und/oder modifizieren.
-*
-* SportsManagement wird in der Hoffnung, dass es nützlich sein wird, aber
-* OHNE JEDE GEWÄHELEISTUNG, bereitgestellt; sogar ohne die implizite
-* Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
-* Siehe die GNU General Public License für weitere Details.
-*
-* Sie sollten eine Kopie der GNU General Public License zusammen mit diesem
-* Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
-*
-* Note : All ini files need to be saved as UTF-8 without BOM
-*/ 
+/** SportsManagement ein Programm zur Verwaltung fÃ¼r Sportarten
+ * @version   1.0.05
+ * @file      default_fieldsets.php
+ * @author    diddipoeler, stony, svdoldie und donclumsy (diddipoeler@arcor.de)
+ * @copyright Copyright: Â© 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license   This file is part of SportsManagement.
+ * @package   sportsmanagement
+ * @subpackage editperson
+ */
+
 defined('_JEXEC') or die('Restricted access');
 
 switch($this->fieldset)
@@ -99,7 +72,7 @@ case 'training':
 break;
 
 
-// für extra felder
+// fÃ¼r extra felder
 case 'extra_fields':
 ?>
 	<fieldset class="adminform">
@@ -128,7 +101,7 @@ case 'extra_fields':
 
 break;
 
-// für google maps    
+// fÃ¼r google maps    
 case 'maps':
 $document = JFactory::getDocument();
 $document->addScript('http://maps.google.com/maps/api/js?&sensor=false');
@@ -174,7 +147,7 @@ function initialize() {
 <?PHP
 break;
 
-// für die extended daten
+// fÃ¼r die extended daten
 case 'extended':
 if ( isset($this->extended) )
 {
