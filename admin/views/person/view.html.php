d748dc7d43af3fba3eebe0b2e3128ef2c535cbf4
diddipoeler <famploeger@arcor.de>
2018-02-20 10:43:00 +0100

    Update view.html.php
    Code.

--- admin/views/person/view.html.php
+++ admin/views/person/view.html.php
@@ -1,41 +1,13 @@
 <?php
-/** SportsManagement ein Programm zur Verwaltung für alle Sportarten
-* @version         1.0.05
-* @file                agegroup.php
-* @author                diddipoeler, stony, svdoldie und donclumsy (diddipoeler@arcor.de)
-* @copyright        Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
-* @license                This file is part of SportsManagement.
-*
-* SportsManagement is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* SportsManagement is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with SportsManagement.  If not, see <http://www.gnu.org/licenses/>.
-*
-* Diese Datei ist Teil von SportsManagement.
-*
-* SportsManagement ist Freie Software: Sie können es unter den Bedingungen
-* der GNU General Public License, wie von der Free Software Foundation,
-* Version 3 der Lizenz oder (nach Ihrer Wahl) jeder späteren
-* veröffentlichten Version, weiterverbreiten und/oder modifizieren.
-*
-* SportsManagement wird in der Hoffnung, dass es nützlich sein wird, aber
-* OHNE JEDE GEWÄHRLEISTUNG, bereitgestellt; sogar ohne die implizite
-* Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
-* Siehe die GNU General Public License für weitere Details.
-*
-* Sie sollten eine Kopie der GNU General Public License zusammen mit diesem
-* Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
-*
-* Note : All ini files need to be saved as UTF-8 without BOM
-*/
+/** SportsManagement ein Programm zur Verwaltung für Sportarten
+ * @version   1.0.05
+ * @file      view.html.php
+ * @author    diddipoeler, stony, svdoldie und donclumsy (diddipoeler@arcor.de)
+ * @copyright Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license   This file is part of SportsManagement.
+ * @package   sportsmanagement
+ * @subpackage person
+ */
 
 // No direct access to this file
 defined('_JEXEC') or die('Restricted access');
@@ -63,19 +35,12 @@ class sportsmanagementViewPerson extends sportsmanagementView
 	{
 		
 	$starttime = microtime();
-        
-
-        
-        //$app->enqueueMessage(JText::_(__METHOD__.' '.__LINE__.' item<br><pre>'.print_r($item,true).'</pre>'),'');
-        //$app->enqueueMessage(JText::_(__METHOD__.' '.__FUNCTION__.' form<br><pre>'.print_r($form,true).'</pre>'),'');
-        
+       
         if ( COM_SPORTSMANAGEMENT_SHOW_QUERY_DEBUG_INFO )
         {
         $app->enqueueMessage(JText::_(__METHOD__.' '.__LINE__.' Ausfuehrungszeit query<br><pre>'.print_r(sportsmanagementModeldatabasetool::getQueryTime($starttime, microtime()),true).'</pre>'),'Notice');
         }
-        
 
- 
 		// Check for errors.
 		if (count($errors = $this->get('Errors'))) 
 		{
@@ -86,13 +51,6 @@ class sportsmanagementViewPerson extends sportsmanagementView
                 
         // name für den titel setzen
         $this->item->name = $this->item->lastname.' - '.$this->item->firstname;
-        
-//        $this->form->setValue('address_country', 'request', $this->item->address_country);
-//        $this->form->setValue('zipcode', 'request', $this->item->zipcode);
-//        $this->form->setValue('location', 'request', $this->item->location);
-//        $this->form->setValue('address', 'request', $this->item->address);
-//        $this->form->setValue('state', 'request', $this->item->state);
-        
         
         $this->form->setValue('sports_type_id', 'request', $this->item->sports_type_id);
         $this->form->setValue('position_id', 'request', $this->item->position_id);
@@ -121,28 +79,22 @@ class sportsmanagementViewPerson extends sportsmanagementView
         }
         
         $extended = sportsmanagementHelper::getExtended($this->item->extended, 'person');
-		$this->extended =  $extended;
+	$this->extended =  $extended;
         $extendeduser = sportsmanagementHelper::getExtendedUser($this->item->extendeduser, 'person');		
-		$this->extendeduser = $extendeduser;
-
-		//$this->assign('cfg_which_media_tool', JComponentHelper::getParams($option)->get('cfg_which_media_tool',0) );
- 
+	$this->extendeduser = $extendeduser;
 	$this->checkextrafields = sportsmanagementHelper::checkUserExtraFields();
-    $lists = array();
+	$lists = array();
 	if ( $this->checkextrafields )
 	{
-//	$lists = array();
 	$lists['ext_fields'] = sportsmanagementHelper::getUserExtraFields($this->item->id);
-            //$app->enqueueMessage(JText::_('view -> '.'<pre>'.print_r($lists['ext_fields'],true).'</pre>' ),'');
         }
         
 	$this->lists = $lists;
 	unset($lists);
     
 	$person_age = sportsmanagementHelper::getAge($this->form->getValue('birthday'),$this->form->getValue('deathday'));
-//    $app->enqueueMessage(JText::_('personagegroup person_age<br><pre>'.print_r($person_age,true).'</pre>'   ),'');
 	$person_range = $this->model->getAgeGroupID($person_age);
-//    $app->enqueueMessage(JText::_('personagegroup person_range<br><pre>'.print_r($person_range,true).'</pre>'   ),'');
+
 	if ( $person_range )
  	{
 	$this->form->setValue('agegroup_id', null, $person_range);
