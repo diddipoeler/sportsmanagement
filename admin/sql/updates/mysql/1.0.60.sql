5a3315d60593659e91a4f62f823871efef1968ec
diddipoeler <famploeger@arcor.de>
2018-02-20 11:45:21 +0100

    Update 1.0.60.sql
    1.0.60

--- admin/sql/updates/mysql/1.0.60.sql
+++ admin/sql/updates/mysql/1.0.60.sql
@@ -1,1 +1,5 @@
+ALTER TABLE `#__sportsmanagement_team` ADD `email` VARCHAR(250) NULL;
 
+
+INSERT INTO `#__sportsmanagement_version_history` (`id`, `date`, `text`, `version`) VALUES
+(NULL, '2018-02-20', 'COM_SPORTSMANAGEMENT_DB_UPDATE_2018_02_20', '1.0.60');
