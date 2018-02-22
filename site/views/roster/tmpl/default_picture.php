d92c66848226d7473acddbd5fef05f1257d83ac8
diddipoeler <famploeger@arcor.de>
2018-02-22 07:46:43 +0100

    Update default_picture.php
    Code

--- site/views/roster/tmpl/default_picture.php
+++ site/views/roster/tmpl/default_picture.php
@@ -1,9 +1,9 @@
 <?php 
-/** SportsManagement ein Programm zur Verwaltung für alle Sportarten
+/** SportsManagement ein Programm zur Verwaltung fÃ¼r alle Sportarten
  * @version   1.0.05
  * @file      default_picture.php
  * @author    diddipoeler, stony, svdoldie und donclumsy (diddipoeler@arcor.de)
- * @copyright Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @copyright Copyright: Â© 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
  * @license   This file is part of SportsManagement.
  * @package   sportsmanagement
  * @subpackage roster
@@ -26,46 +26,13 @@ defined( '_JEXEC' ) or die( 'Restricted access' ); ?>
 					{
 						$picture = $this->team->picture;
 					}
-					if ( !file_exists( $picture ) )
-					{
-						$picture = sportsmanagementHelper::getDefaultPlaceholder("team");
-					}					
+										
 					$imgTitle = JText::sprintf( 'COM_SPORTSMANAGEMENT_ROSTER_PICTURE_TEAM', $this->team->name );
            
-      
+echo sportsmanagementHelperHtml::getBootstrapModalImage('roster'.$this->team->name,$picture,$this->team->name,$this->config['team_picture_width']);      
       ?>
 
-
-<a href="#" title="<?php echo $this->team->name;?>" data-toggle="modal" data-target=".teamroster">
-<img src="<?php echo $picture;?>" alt="<?php echo $this->team->name;?>" width="<?php echo $this->config['team_picture_width'];?>" />
-</a>
-
-
-<div id="" style="display: none;" class="modal fade teamroster" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-lg">
-<!--  <div class="modal-dialog"> -->
-    <div class="modal-content">
-    
-    <div class="modal-header">
-          <button type="button" class="close" data-dismiss="modal" aria-hidden="true">X</button>
-          <h4 class="modal-title" id="myLargeModalLabel"><?php echo $this->team->name;?></h4>
-        </div>
-        
-        <div class="modal-body">
-            <img src="<?php echo $picture;?>" class="img-responsive img-rounded center-block">
-        </div>
-        <div class="modal-footer">
-<button class="btn" data-dismiss="modal" aria-hidden="true"><?php echo JText::_('JLIB_HTML_BEHAVIOR_CLOSE');?> </button>
-</div>
-    </div>
-<!--  </div> -->
-  </div>
-</div>    
-     
-
-    <?php
-      	
-                    ?>
+   
 				</td>
 			</tr>
 		</table>
