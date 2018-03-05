e0270bcb122867454c575af6075bfa169aa8b5eb
diddipoeler <famploeger@arcor.de>
2018-03-05 07:56:10 +0100

    Update default_roster.php
    #641

--- site/views/matchreport/tmpl/default_roster.php
+++ site/views/matchreport/tmpl/default_roster.php
@@ -22,8 +22,8 @@ if (!empty($this->matchplayerpositions))
 ?>
 
 <h2><?php echo JText::_('COM_SPORTSMANAGEMENT_MATCHREPORT_STARTING_LINE-UP'); ?></h2>		
-	<table class="table table-responsive">
-		<?php
+<table class="table table-responsive">
+<?php
 		foreach ($this->matchplayerpositions as $pos)
 		{
 			$personCount=0;
@@ -52,11 +52,12 @@ if (!empty($this->matchplayerpositions))
 								  {
                   $player->jerseynumber = $player->trikot_number;
                   }
-                                    if ( $player->position_id == $pos->position_id && $player->ptid == $this->match->projectteam1_id )
-									{
-										?>
-										<li <?php echo ($this->config['show_player_picture'] == 2 ? 'class="list_pictureonly_left"' : 'class="list"') ?>>
-											<?php
+
+if ( $player->position_id == $pos->position_id && $player->ptid == $this->match->projectteam1_id )
+{
+?>
+<li <?php echo ($this->config['show_player_picture'] == 2 ? 'class="list_pictureonly_left"' : 'class="list"') ?>>
+<?php
 /**
 * ist der spieler ein spielführer ?
 */                  
@@ -72,41 +73,37 @@ $routeparameter['p'] = $this->project->slug;
 $routeparameter['tid'] = $player->team_slug;
 $routeparameter['pid'] = $player->person_slug;
 $player_link = sportsmanagementHelperRoute::getSportsmanagementRoute('player',$routeparameter);
+$prefix = $player->jerseynumber ? $player->jerseynumber."." : null;
+$match_player = sportsmanagementHelper::formatName($prefix,$player->firstname,$player->nickname,$player->lastname, $this->config["name_format"]);
+$isFavTeam = in_array( $player->team_id, explode(",",$this->project->fav_team));
 
-											//$player_link = sportsmanagementHelperRoute::getPlayerRoute($this->project->slug,$player->team_slug,$player->person_slug);
-											$prefix = $player->jerseynumber ? $player->jerseynumber."." : null;
-											$match_player = sportsmanagementHelper::formatName($prefix,$player->firstname,$player->nickname,$player->lastname, $this->config["name_format"]);
-											$isFavTeam = in_array( $player->team_id, explode(",",$this->project->fav_team));
+if ( ($this->config['show_player_profile_link'] == 1) || (($this->config['show_player_profile_link'] == 2) && ($isFavTeam)) )
+{
+if ($this->config['show_player_picture'] == 2) {
+echo '';
+} else {
+											
+if ( $this->config['show_player_profile_link_alignment'] == 0 )
+{
+echo JHtml::link($player_link,$match_player.HTMLHelper::image(JURI::root().'images/com_sportsmanagement/database/teamplayers/shirt.php?text='.$player->jerseynumber,$player->jerseynumber,array('title'=> $player->jerseynumber)));
+}
+}
+} else {
+if ($this->config['show_player_picture'] == 2) {
+echo '';
+} else {
+echo $match_player;
+}
+}
 
-                                            if ( ($this->config['show_player_profile_link'] == 1) || (($this->config['show_player_profile_link'] == 2) && ($isFavTeam)) )
-                                            {
-												if ($this->config['show_player_picture'] == 2) {
-													echo '';
-												} else {
-												
-                        if ( $this->config['show_player_profile_link_alignment'] == 0 )
-												{
-                        echo JHtml::link($player_link,$match_player.HTMLHelper::image(JURI::root().'images/com_sportsmanagement/database/teamplayers/shirt.php?text='.$player->jerseynumber,$player->jerseynumber,array('title'=> $player->jerseynumber)));
-                        }
-													//echo JHtml::link($player_link,$match_player);
-                          //echo JHtml::link($player_link,$match_player.JHtml::image(JURI::root().'images/com_sportsmanagement/database/teamplayers/shirt.php?text='.$player->jerseynumber,$player->jerseynumber,array('title'=> $player->jerseynumber)));
-												}
-                                            } else {
-												if ($this->config['show_player_picture'] == 2) {
-													echo '';
-												} else {
-													echo $match_player;
-												}
-                                            }
-
-                                            if (($this->config['show_player_picture'] == 1) || ($this->config['show_player_picture'] == 2))
-                                            {
-                                                $imgTitle=($this->config['show_player_profile_link'] == 1) ? JText::sprintf('COM_SPORTSMANAGEMENT_MATCHREPORT_PIC', $match_player) : $match_player;
-                                                $picture=$player->picture;
-                                                if ((empty($picture)) || ($picture == sportsmanagementHelper::getDefaultPlaceholder("player") ) || !curl_init( $picture ) )
-                                                {
-                                                    $picture = $player->ppic;
-                                                }
+if (($this->config['show_player_picture'] == 1) || ($this->config['show_player_picture'] == 2))
+{
+$imgTitle=($this->config['show_player_profile_link'] == 1) ? JText::sprintf('COM_SPORTSMANAGEMENT_MATCHREPORT_PIC', $match_player) : $match_player;
+$picture=$player->picture;
+if ((empty($picture)) || ($picture == sportsmanagementHelper::getDefaultPlaceholder("player") ) || !curl_init( $picture ) )
+{
+$picture = $player->ppic;
+}
                                                 if ( !curl_init( $picture ) )
                                                 {
                                                     $picture = sportsmanagementHelper::getDefaultPlaceholder("player");
