1f9ace6f2b5796dea4714baf3a5ce6be9b5040ab
diddipoeler <famploeger@arcor.de>
2018-03-10 12:05:57 +0100

    Code

--- admin/controllers/smextxmleditor.php
+++ admin/controllers/smextxmleditor.php
@@ -2,7 +2,7 @@
 /** SportsManagement ein Programm zur Verwaltung für Sportarten
  * @version   1.0.05
  * @file      smextxmleditor.php
- * @author    diddipoeler, stony, svdoldie und donclumsy (diddipoeler@arcor.de)
+ * @author    diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
  * @copyright Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
  * @license   This file is part of SportsManagement.
  * @package   sportsmanagement
