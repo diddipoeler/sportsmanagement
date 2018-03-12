86ffb452269ad0e865f94f81489a5de3a77f342b
diddipoeler <famploeger@arcor.de>
2018-03-12 11:49:32 +0100

    Update jlextlmoimports.php
    Code

--- admin/models/jlextlmoimports.php
+++ admin/models/jlextlmoimports.php
@@ -1,41 +1,13 @@
 <?php
-/** SportsManagement ein Programm zur Verwaltung für alle Sportarten
-* @version         1.0.05
-* @file                agegroup.php
-* @author                diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
-* @copyright        Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
-* @license                This file is part of SportsManagement.
-*
-* SportsManagement is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* SportsManagement is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with SportsManagement.  If not, see <http://www.gnu.org/licenses/>.
-*
-* Diese Datei ist Teil von SportsManagement.
-*
-* SportsManagement ist Freie Software: Sie können es unter den Bedingungen
-* der GNU General Public License, wie von der Free Software Foundation,
-* Version 3 der Lizenz oder (nach Ihrer Wahl) jeder späteren
-* veröffentlichten Version, weiterverbreiten und/oder modifizieren.
-*
-* SportsManagement wird in der Hoffnung, dass es nützlich sein wird, aber
-* OHNE JEDE GEWÄHELEISTUNG, bereitgestellt; sogar ohne die implizite
-* Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
-* Siehe die GNU General Public License für weitere Details.
-*
-* Sie sollten eine Kopie der GNU General Public License zusammen mit diesem
-* Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
-*
-* Note : All ini files need to be saved as UTF-8 without BOM
-*/
+/** SportsManagement ein Programm zur Verwaltung fÃ¼r Sportarten
+ * @version   1.0.05
+ * @file      jlextlmoimports.php
+ * @author    diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright Copyright: Â© 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license   This file is part of SportsManagement.
+ * @package   sportsmanagement
+ * @subpackage models
+ */
 
 // Check to ensure this file is included in Joomla!
 defined( '_JEXEC' ) or die( 'Restricted access' );
@@ -730,7 +702,7 @@ if ( $timestamp )
     foreach ($parse['Teams'] AS $key => $value)
 		{
 		
-// der clubname muss um die mannschaftsnummer verkürzt werden
+// der clubname muss um die mannschaftsnummer verkÃ¼rzt werden
 if ( substr($value, -4, 4) == ' III')
 {
 $convert = array (
@@ -902,11 +874,11 @@ $countjoomleagueteams = $this->_db->loadResult();
 
 if (  $countlmoteams != $countjoomleagueteams  )
 {
-$app->enqueueMessage(JText::_('Die Anzahl der Teams im Projekt '.$project.' stimmt nicht überein!'),'Error');
+$app->enqueueMessage(JText::_('Die Anzahl der Teams im Projekt '.$project.' stimmt nicht Ã¼berein!'),'Error');
 }
 else
 {
-$app->enqueueMessage(JText::_('Die Anzahl der Teams im Projekt '.$project.' stimmt überein!'),'Notice');
+$app->enqueueMessage(JText::_('Die Anzahl der Teams im Projekt '.$project.' stimmt Ã¼berein!'),'Notice');
 }
     
     }
@@ -920,7 +892,7 @@ $app->enqueueMessage(JText::_('Die Anzahl der Teams im Projekt '.$project.' stim
 // echo '</pre><br>';
 
 /**
- * das ganze für den standardimport aufbereiten
+ * das ganze fÃ¼r den standardimport aufbereiten
  */
 $output = '<?xml version="1.0" encoding="utf-8"?>' . "\n";
 // open the project
