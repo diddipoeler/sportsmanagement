76b0a286265b2c7594939379dfb6c60fa97a7f5f
diddipoeler <famploeger@arcor.de>
2018-04-01 14:46:55 +0200

    Code

--- site/views/nextmatch/tmpl/default_nextmatch.php
+++ site/views/nextmatch/tmpl/default_nextmatch.php
@@ -1,41 +1,13 @@
 <?php 
 /** SportsManagement ein Programm zur Verwaltung für alle Sportarten
-* @version         1.0.05
-* @file                agegroup.php
-* @author                diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
-* @copyright        Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
-* @license                This file is part of SportsManagement.
-*
-* SportsManagement is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* SportsManagement is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with SportsManagement.  If not, see <http://www.gnu.org/licenses/>.
-*
-* Diese Datei ist Teil von SportsManagement.
-*
-* SportsManagement ist Freie Software: Sie können es unter den Bedingungen
-* der GNU General Public License, wie von der Free Software Foundation,
-* Version 3 der Lizenz oder (nach Ihrer Wahl) jeder späteren
-* veröffentlichten Version, weiterverbreiten und/oder modifizieren.
-*
-* SportsManagement wird in der Hoffnung, dass es nützlich sein wird, aber
-* OHNE JEDE GEWÄHELEISTUNG, bereitgestellt; sogar ohne die implizite
-* Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
-* Siehe die GNU General Public License für weitere Details.
-*
-* Sie sollten eine Kopie der GNU General Public License zusammen mit diesem
-* Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
-*
-* Note : All ini files need to be saved as UTF-8 without BOM
-*/
+ * @version   1.0.05
+ * @file      default_nextmatch.php
+ * @author    diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license   This file is part of SportsManagement.
+ * @package   sportsmanagement
+ * @subpackage nextmatch
+ */
 
 defined( '_JEXEC' ) or die( 'Restricted access' ); 
 
@@ -54,13 +26,15 @@ defined( '_JEXEC' ) or die( 'Restricted access' );
 			$pic = $this->config['show_picture'];
         
         $picture = COM_SPORTSMANAGEMENT_PICTURE_SERVER.$this->teams[0]->$pic;
-        //echo $picture.'<br>';
-//        if ( !sportsmanagementHelper::existPicture($picture) )
-//        {
-//        $picture = sportsmanagementHelper::getDefaultPlaceholder($pic);
-//        }   
         
-echo sportsmanagementHelperHtml::getBootstrapModalImage('nextmatch'.$this->teams[0]->id,$picture,$this->teams[0]->name,$this->config['club_logo_width'])
+echo sportsmanagementHelperHtml::getBootstrapModalImage('nextmatch'.$this->teams[0]->id,
+$picture,
+$this->teams[0]->name,
+$this->config['club_logo_width'],
+'',
+$this->modalwidth,
+$this->modalheight,
+$this->overallconfig['use_jquery_modal'])
                              
 		?>
 
@@ -69,13 +43,15 @@ echo sportsmanagementHelperHtml::getBootstrapModalImage('nextmatch'.$this->teams
 		<td class="teamlogo"><?php
 
         $picture = COM_SPORTSMANAGEMENT_PICTURE_SERVER.$this->teams[1]->$pic;
-        //echo $picture.'<br>';
-//        if ( !sportsmanagementHelper::existPicture($picture) )
-//        {
-//        $picture = sportsmanagementHelper::getDefaultPlaceholder($pic);
-//        }                         
 
-echo sportsmanagementHelperHtml::getBootstrapModalImage('nextmatch'.$this->teams[1]->id,$picture,$this->teams[1]->name,$this->config['club_logo_width'])
+echo sportsmanagementHelperHtml::getBootstrapModalImage('nextmatch'.$this->teams[1]->id,
+$picture,
+$this->teams[1]->name,
+$this->config['club_logo_width'],
+'',
+$this->modalwidth,
+$this->modalheight,
+$this->overallconfig['use_jquery_modal'])
         
 		?>
         
@@ -91,13 +67,15 @@ echo sportsmanagementHelperHtml::getBootstrapModalImage('nextmatch'.$this->teams
 			<?php
         
         $picture = COM_SPORTSMANAGEMENT_PICTURE_SERVER.$this->teams[0]->projectteam_picture;
-        //echo $picture.'<br>';
-//        if ( !sportsmanagementHelper::existPicture($picture) )
-//        {
-//        $picture = sportsmanagementHelper::getDefaultPlaceholder('projectteam_picture');
-//        }   
         
-echo sportsmanagementHelperHtml::getBootstrapModalImage('nextmatch_team'.$this->teams[0]->id,$picture,$this->teams[0]->name,$this->config['team_picture_width'])
+echo sportsmanagementHelperHtml::getBootstrapModalImage('nextmatch_team'.$this->teams[0]->id,
+$picture,
+$this->teams[0]->name,
+$this->config['team_picture_width'],
+'',
+$this->modalwidth,
+$this->modalheight,
+$this->overallconfig['use_jquery_modal'])
                              
 		?>
 
@@ -106,13 +84,15 @@ echo sportsmanagementHelperHtml::getBootstrapModalImage('nextmatch_team'.$this->
 		<td class="teampicture"><?php
 
         $picture = COM_SPORTSMANAGEMENT_PICTURE_SERVER.$this->teams[1]->projectteam_picture;
-        //echo $picture.'<br>';
-//        if ( !sportsmanagementHelper::existPicture($picture) )
-//        {
-//        $picture = sportsmanagementHelper::getDefaultPlaceholder('projectteam_picture');
-//        }                         
 
-echo sportsmanagementHelperHtml::getBootstrapModalImage('nextmatch_team'.$this->teams[1]->id,$picture,$this->teams[1]->name,$this->config['team_picture_width'])
+echo sportsmanagementHelperHtml::getBootstrapModalImage('nextmatch_team'.$this->teams[1]->id,
+$picture,
+$this->teams[1]->name,
+$this->config['team_picture_width'],
+'',
+$this->modalwidth,
+$this->modalheight,
+$this->overallconfig['use_jquery_modal'])
         
 		?>
         
