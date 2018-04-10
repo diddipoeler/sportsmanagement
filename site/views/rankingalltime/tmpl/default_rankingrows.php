45624c594f42550ef40a60348f066fdbc6068ed1
diddipoeler <famploeger@arcor.de>
2018-04-10 10:12:08 +0200

    Update default_rankingrows.php
    Code

--- site/views/rankingalltime/tmpl/default_rankingrows.php
+++ site/views/rankingalltime/tmpl/default_rankingrows.php
@@ -1,41 +1,14 @@
 <?php
 /** SportsManagement ein Programm zur Verwaltung für alle Sportarten
-* @version         1.0.05
-* @file                agegroup.php
-* @author                diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
-* @copyright        Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
-* @license                This file is part of SportsManagement.
-*
-* SportsManagement is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* SportsManagement is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with SportsManagement.  If not, see <http://www.gnu.org/licenses/>.
-*
-* Diese Datei ist Teil von SportsManagement.
-*
-* SportsManagement ist Freie Software: Sie können es unter den Bedingungen
-* der GNU General Public License, wie von der Free Software Foundation,
-* Version 3 der Lizenz oder (nach Ihrer Wahl) jeder späteren
-* veröffentlichten Version, weiterverbreiten und/oder modifizieren.
-*
-* SportsManagement wird in der Hoffnung, dass es nützlich sein wird, aber
-* OHNE JEDE GEWÄHELEISTUNG, bereitgestellt; sogar ohne die implizite
-* Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
-* Siehe die GNU General Public License für weitere Details.
-*
-* Sie sollten eine Kopie der GNU General Public License zusammen mit diesem
-* Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
-*
-* Note : All ini files need to be saved as UTF-8 without BOM
-*/
+ * @version   1.0.05
+ * @file      default_rankingrows.php
+ * @author    diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license   This file is part of SportsManagement.
+ * @package   sportsmanagement
+ * @subpackage rankingalltime
+ */
+
 defined( '_JEXEC' ) or die( 'Restricted access' );
 
 JHTML::_('behavior.tooltip');
@@ -177,7 +150,7 @@ foreach( $current as $ptid => $team )
         }  
         else 
         {
-			$pic = $config['show_logo_small_table'];
+	$pic = $config['show_logo_small_table'];
             switch ($pic)
             {
                 case 'logo_small';
@@ -191,12 +164,7 @@ foreach( $current as $ptid => $team )
                 break;
                 
             }
-            /*
-			echo sportsmanagementHelper::getPictureThumb($team->team->$pic,
-					$team->team->name,
-					$config['team_picture_width'],
-					$config['team_picture_height'],3);
-           */         
+                  
 		}
 
 		echo '</td>';
