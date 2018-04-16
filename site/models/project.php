f6b28fa78ccd3b48f13631d80a0588f3f4d3f8d7
diddipoeler <famploeger@arcor.de>
2018-04-16 08:02:18 +0200

    Update project.php
    Code

--- site/models/project.php
+++ site/models/project.php
@@ -1721,9 +1721,14 @@ catch (Exception $e)
           $query->from('#__sportsmanagement_project_position AS ppos');
           $query->join('INNER','#__sportsmanagement_position AS pos ON ppos.position_id = pos.id');
           $query->where('ppos.project_id = '.(int)self::$projectid );
-
+try{
 			$db->setQuery($query);
 			self::$_positions = $db->loadObjectList('id');
+			}
+catch (Exception $e)
+{
+    $app->enqueueMessage(JText::_(__METHOD__.' '.__LINE__.' '.$e->getMessage()), 'error');
+}
 		}
 		return self::$_positions;
 	}
