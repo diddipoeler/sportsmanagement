a1de3916d4f79ffe761d8029399420c2ab5ebe14
diddipoeler <famploeger@arcor.de>
2018-04-26 12:15:14 +0200

    Create view.html.php
    teamstree

---
+++ site/views/teamstree/view.html.php
@@ -0,0 +1,14 @@
+<?php 
+/** SportsManagement ein Programm zur Verwaltung für alle Sportarten
+ * @version   1.0.05
+ * @file      view.html.php
+ * @author    diddipoeler, stony, svdoldie und donclumsy (diddipoeler@arcor.de)
+ * @copyright Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license   This file is part of SportsManagement.
+ * @package   sportsmanagement
+ * @subpackage teamstree
+ */
+defined( '_JEXEC' ) or die( 'Restricted access' );
+jimport( 'joomla.application.component.view' );
+
+?>
