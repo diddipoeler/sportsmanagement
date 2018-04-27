3eef9ef9811f94c1b5acec44d07ba537b443a364
diddipoeler <famploeger@arcor.de>
2018-04-27 10:56:53 +0200

    teamstree

--- site/views/teamstree/view.html.php
+++ site/views/teamstree/view.html.php
@@ -11,4 +11,53 @@
 defined( '_JEXEC' ) or die( 'Restricted access' );
 jimport( 'joomla.application.component.view' );
 
+
+/**
+ * sportsmanagementViewTeamsTree
+ * 
+ * @package 
+ * @author Dieter Plöger
+ * @copyright 2018
+ * @version $Id$
+ * @access public
+ */
+class sportsmanagementViewTeamsTree extends sportsmanagementView
+{
+
+	/**
+	 * sportsmanagementViewTeamsTree::init()
+	 * 
+	 * @return void
+	 */
+	function init()
+	{
+
+$this->teams = sportsmanagementModelProject::getTeams($this->jinput->getInt( "division", 0 ),'name',$this->jinput->getInt('cfg_which_database',0));
+
+$javascript = "\n";	
+$javascript .= "
+jQuery(function ($) {
+    $('.tree li:has(ul)').addClass('parent_li').find(' > span').attr('title', 'Collapse this branch');
+    $('.tree li.parent_li > span').on('click', function (e) {
+        var children = $(this).parent('li.parent_li').find(' > ul > li');
+        if (children.is(\":visible\")) {
+            children.hide('fast');
+            $(this).attr('title', 'Expand this branch').find(' > i').addClass('icon-plus-sign').removeClass('icon-minus-sign');
+        } else {
+            children.show('fast');
+            $(this).attr('title', 'Collapse this branch').find(' > i').addClass('icon-minus-sign').removeClass('icon-plus-sign');
+        }
+        e.stopPropagation();
+    });
+});
+
+
+";	
+	
+$this->document->addScriptDeclaration( $javascript );
+		$this->document->addStyleSheet(JURI::base().'components/'.$this->option.'/assets/css/bootstrap-tree2.css');	
+        
+}
+
+}
 ?>
