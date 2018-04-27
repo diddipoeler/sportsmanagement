d7b955efd1f4c880b51e594e66f615c57cceef61
diddipoeler <famploeger@arcor.de>
2018-04-27 18:35:38 +0200

    Code

--- modules/mod_sportsmanagement_ajax_top_navigation_menu/helper.php
+++ modules/mod_sportsmanagement_ajax_top_navigation_menu/helper.php
@@ -652,10 +652,9 @@ return $user->username ;
 		$options = array(JHTML::_('select.option', 0, JText::_($this->getParam('seasons_text'))));
         
         $query->select('s.id AS value, s.name AS text');
-            $query->from('#__sportsmanagement_season AS s');
-            $query->where('s.published = 1');
-            $query->order('s.name DESC');
-
+        $query->from('#__sportsmanagement_season AS s');
+        $query->where('s.published = 1');
+        $query->order('s.name DESC');
                 
 		$db->setQuery($query);
         
@@ -1139,7 +1138,7 @@ $routeparameter['cfg_which_database'] = JRequest::getInt('cfg_which_database',0)
 $routeparameter['s'] = JRequest::getInt('s',0);
 $routeparameter['p'] = $this->_project_slug;
 $routeparameter['division'] = $this->_division_id;
-$link = sportsmanagementHelperRoute::getSportsmanagementRoute('teams',$routeparameter);            
+$link = sportsmanagementHelperRoute::getSportsmanagementRoute($view,$routeparameter);            
 				break;    
 				
 			case "treetonode":
