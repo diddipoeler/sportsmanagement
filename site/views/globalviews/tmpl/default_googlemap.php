6e0260a2c1096964319893e5bb56e6e833d7688a
diddipoeler <famploeger@arcor.de>
2018-05-02 15:26:00 +0200

    Update default_googlemap.php
    Code

--- site/views/globalviews/tmpl/default_googlemap.php
+++ site/views/globalviews/tmpl/default_googlemap.php
@@ -193,7 +193,7 @@ $row->team_name= str_replace($find, $replace, $row->team_name);
 // logo_big
 //$row->team_name = $row->team_name.' '."<img src='".JURI::root().$row->logo_big."' width='50'>";
 //$map_markes[] = "[".$zaehler.",".$latitude.",".$longitude.",'".$row->team_name."']";
-$map_markes[] = "[".$zaehler.",".$latitude.",".$longitude.",'".$row->team_name."','".JURI::root().$row->logo_big."']";
+$map_markes[] = "[".$zaehler.",".$latitude.",".$longitude.",'".$row->team_name."','".$row->logo_big."']";
 $zaehler++;
 }
 
