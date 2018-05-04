b8547db783f1cf642e057daeaa8c183af1dd4de3
diddipoeler <famploeger@arcor.de>
2018-05-04 18:44:15 +0200

    Update default.php
    teamstree

--- site/views/teamstree/tmpl/default.php
+++ site/views/teamstree/tmpl/default.php
@@ -1,9 +1,9 @@
 <?php
-/** SportsManagement ein Programm zur Verwaltung für alle Sportarten
+/** SportsManagement ein Programm zur Verwaltung fÃ¼r alle Sportarten
  * @version   1.0.05
  * @file      default.php
  * @author    diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @copyright Copyright: Â© 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
  * @license   This file is part of SportsManagement.
  * @package   sportsmanagement
  * @subpackage teamstree
@@ -36,7 +36,7 @@ foreach( $this->familyteamstree as $rowclub => $rowvalue )
                                     <?php
                                 }
                                 ?>	
-                                <a href="#"><?PHP echo JHTML::image($this->familyclub[$rowclub]->logo_big, $this->familyclub[$rowclub]->club_name, 'width="30"') . ' ' . $this->familyclub[$rowclub]->club_name; ?></a>
+                                <a href="<?php echo $this->familyclub[$rowclub]->clublink;?>"><?PHP echo JHTML::image($this->familyclub[$rowclub]->logo_big, $this->familyclub[$rowclub]->club_name, 'width="30"') . ' ' . $this->familyclub[$rowclub]->club_name; ?></a>
                                 <?php
                                 echo $rowvalue;
                                 ?>
