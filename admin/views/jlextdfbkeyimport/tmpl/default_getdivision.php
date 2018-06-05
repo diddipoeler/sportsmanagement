44357bc63045e14f695649cb13d6a4d03999ae8d
diddipoeler <famploeger@arcor.de>
2018-06-05 14:14:07 +0200

    Create default_getdivision.php
    Code

---
+++ admin/views/jlextdfbkeyimport/tmpl/default_getdivision.php
@@ -0,0 +1,14 @@
+<?php
+/** SportsManagement ein Programm zur Verwaltung für Sportarten
+ * @version   1.0.05
+ * @file      default_getdivision.php
+ * @author    diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license   This file is part of SportsManagement.
+ * @package   sportsmanagement
+ * @subpackage jlextdfbkeyimport
+ */
+defined( '_JEXEC' ) or die( 'Restricted access' );
+JHtml::_( 'behavior.tooltip' );
+
+?>
