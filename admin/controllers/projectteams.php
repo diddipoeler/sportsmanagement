b36b31ebcfbb41a43c361e9ec00a83e29b89c7db
diddipoeler <famploeger@arcor.de>
2018-06-06 18:09:40 +0200

    Code

--- admin/controllers/projectteams.php
+++ admin/controllers/projectteams.php
@@ -1,41 +1,13 @@
 <?php
-/** SportsManagement ein Programm zur Verwaltung für alle Sportarten
-* @version         1.0.05
-* @file                agegroup.php
-* @author                diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
-* @copyright        Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
-* @license                This file is part of SportsManagement.
-*
-* SportsManagement is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* SportsManagement is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with SportsManagement.  If not, see <http://www.gnu.org/licenses/>.
-*
-* Diese Datei ist Teil von SportsManagement.
-*
-* SportsManagement ist Freie Software: Sie können es unter den Bedingungen
-* der GNU General Public License, wie von der Free Software Foundation,
-* Version 3 der Lizenz oder (nach Ihrer Wahl) jeder späteren
-* veröffentlichten Version, weiterverbreiten und/oder modifizieren.
-*
-* SportsManagement wird in der Hoffnung, dass es nützlich sein wird, aber
-* OHNE JEDE GEWÄHELEISTUNG, bereitgestellt; sogar ohne die implizite
-* Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
-* Siehe die GNU General Public License für weitere Details.
-*
-* Sie sollten eine Kopie der GNU General Public License zusammen mit diesem
-* Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
-*
-* Note : All ini files need to be saved as UTF-8 without BOM
-*/
+/** SportsManagement ein Programm zur Verwaltung für Sportarten
+ * @version   1.0.05
+ * @file      projectteams.php
+ * @author    diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license   This file is part of SportsManagement.
+ * @package   sportsmanagement
+ * @subpackage controllers
+ */
 
 // No direct access to this file
 defined('_JEXEC') or die('Restricted access');
@@ -98,8 +70,9 @@ class sportsmanagementControllerprojectteams extends JControllerAdmin
   function matchgroups()
 	{
 	   $model = $this->getModel();
+       $post = JFactory::getApplication()->input->post->getArray(array());
        $model->matchgroups();
-       $this->setRedirect(JRoute::_('index.php?option='.$this->option.'&view='.$this->view_list, false));
+       $this->setRedirect(JRoute::_('index.php?option='.$this->option.'&view='.$this->view_list.'&pid='.$post['pid'], false));
     } 
     
     
@@ -111,8 +84,9 @@ class sportsmanagementControllerprojectteams extends JControllerAdmin
     function setseasonid()
 	{
 	   $model = $this->getModel();
+       $post = JFactory::getApplication()->input->post->getArray(array());
        $model->setseasonid();
-       $this->setRedirect(JRoute::_('index.php?option='.$this->option.'&view='.$this->view_list, false));
+       $this->setRedirect(JRoute::_('index.php?option='.$this->option.'&view='.$this->view_list.'&pid='.$post['pid'], false));
     } 
     
     
@@ -124,8 +98,9 @@ class sportsmanagementControllerprojectteams extends JControllerAdmin
     function use_table_yes()
     {
        $model = $this->getModel();
+       $post = JFactory::getApplication()->input->post->getArray(array());
        $model->setusetable(1);
-       $this->setRedirect(JRoute::_('index.php?option='.$this->option.'&view='.$this->view_list, false));
+       $this->setRedirect(JRoute::_('index.php?option='.$this->option.'&view='.$this->view_list.'&pid='.$post['pid'], false));
     }
     
     /**
@@ -136,8 +111,9 @@ class sportsmanagementControllerprojectteams extends JControllerAdmin
     function use_table_no()
     {
        $model = $this->getModel();
+       $post = JFactory::getApplication()->input->post->getArray(array());
        $model->setusetable(0);
-       $this->setRedirect(JRoute::_('index.php?option='.$this->option.'&view='.$this->view_list, false));
+       $this->setRedirect(JRoute::_('index.php?option='.$this->option.'&view='.$this->view_list.'&pid='.$post['pid'], false));
     }
     
     /**
@@ -148,8 +124,9 @@ class sportsmanagementControllerprojectteams extends JControllerAdmin
     function use_table_points_yes()
     {
        $model = $this->getModel();
+       $post = JFactory::getApplication()->input->post->getArray(array());
        $model->setusetablepoints(1);
-       $this->setRedirect(JRoute::_('index.php?option='.$this->option.'&view='.$this->view_list, false));
+       $this->setRedirect(JRoute::_('index.php?option='.$this->option.'&view='.$this->view_list.'&pid='.$post['pid'], false));
     }
     
     /**
@@ -160,8 +137,9 @@ class sportsmanagementControllerprojectteams extends JControllerAdmin
     function use_table_points_no()
     {
        $model = $this->getModel();
+       $post = JFactory::getApplication()->input->post->getArray(array());
        $model->setusetablepoints(0);
-       $this->setRedirect(JRoute::_('index.php?option='.$this->option.'&view='.$this->view_list, false));
+       $this->setRedirect(JRoute::_('index.php?option='.$this->option.'&view='.$this->view_list.'&pid='.$post['pid'], false));
     }
     
   /**
@@ -174,8 +152,9 @@ class sportsmanagementControllerprojectteams extends JControllerAdmin
     function saveshort()
 	{
 	   $model = $this->getModel();
+       $post = JFactory::getApplication()->input->post->getArray(array());
        $model->saveshort();
-       $this->setRedirect(JRoute::_('index.php?option='.$this->option.'&view='.$this->view_list, false));
+       $this->setRedirect(JRoute::_('index.php?option='.$this->option.'&view='.$this->view_list.'&pid='.$post['pid'], false));
     } 
   
   /**
