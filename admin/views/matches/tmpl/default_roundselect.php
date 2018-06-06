cc0c8fb6b3c8ccfe2135fc22867ffb7c5734889e
diddipoeler <famploeger@arcor.de>
2018-06-06 12:44:52 +0200

    Update default_roundselect.php
    Code

--- admin/views/matches/tmpl/default_roundselect.php
+++ admin/views/matches/tmpl/default_roundselect.php
@@ -1,7 +1,7 @@
 <?php
 /** SportsManagement ein Programm zur Verwaltung für alle Sportarten
  * @version   1.0.05
- * @file      defaul_roundselect.php
+ * @file      default_roundselect.php
  * @author    diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
  * @copyright Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
  * @license   This file is part of SportsManagement.
