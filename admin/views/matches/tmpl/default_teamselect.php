686a526054ab48e2d3da722367bd2c024da26576
diddipoeler <famploeger@arcor.de>
2018-06-06 12:57:52 +0200

    Create default_teamselect.php
    Code

---
+++ admin/views/matches/tmpl/default_teamselect.php
@@ -0,0 +1,12 @@
+<?php
+/** SportsManagement ein Programm zur Verwaltung für alle Sportarten
+ * @version   1.0.05
+ * @file      default_teamselect.php
+ * @author    diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license   This file is part of SportsManagement.
+ * @package   sportsmanagement
+ * @subpackage matches
+ */
+defined( '_JEXEC' ) or die( 'Restricted access' );
+?>
