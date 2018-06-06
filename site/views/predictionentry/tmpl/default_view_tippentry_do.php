0e588496c0c15b79a3e163b12d21ecf869801af9
diddipoeler <famploeger@arcor.de>
2018-06-06 17:40:54 +0200

    Code

--- site/views/predictionentry/tmpl/default_view_tippentry_do.php
+++ site/views/predictionentry/tmpl/default_view_tippentry_do.php
@@ -343,6 +343,12 @@ echo '<br />this->use_tipp_admin<pre>~' . print_r($this->config['use_tipp_admin'
 // 								echo ' - ';
 // 								echo JHTML::date(date("Y-m-d H:i:s",$matchTimeDate),$this->config['time_format']); 
                 //echo $result->match_date;
+//Versus
+$jdate = JFactory::getDate($result->match_date);
+$jdate->setTimezone("Europa/Berlin");
+echo $jdate->format('d.m.Y H:i'); //outputs 01:00:00
+echo $jdate->format('d.m.Y H:i',true); //outputs 04:00:00 (correct)
+                
                 echo JHtml::date($result->match_date, 'd.m.Y H:i', true);
 								?>
 							</td>
