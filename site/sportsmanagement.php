9f668e4dded9ad265e83e3993c3603ff21250fbd
diddipoeler <famploeger@arcor.de>
2018-06-10 12:59:52 +0200

    Code

--- site/sportsmanagement.php
+++ site/sportsmanagement.php
@@ -123,9 +123,7 @@ break;
 case 'predictionrules':
 case 'predictionranking':
 case 'predictionusers':
-require_once(JPATH_SITE . DS . JSM_PATH . DS . 'models' . DS . 'prediction.php');
-require_once(JPATH_SITE . DS . JSM_PATH . DS . 'helpers' . DS . 'predictionroute.php' );
-break;
+case 'predictionuser':
 case 'predictionentry':
 case 'predictionresults':
 require_once(JPATH_SITE . DS . JSM_PATH . DS . 'models' . DS . 'project.php' );
