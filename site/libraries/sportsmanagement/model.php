e1bec50664ea55edc6aabb72a4d265098051ae86
diddipoeler <famploeger@arcor.de>
2018-06-18 13:54:34 +0200

    Update model.php
    Code

--- site/libraries/sportsmanagement/model.php
+++ site/libraries/sportsmanagement/model.php
@@ -18,8 +18,17 @@ jimport('joomla.application.component.model');
 
 class JSMModelList extends JModelList
 {
-    
-
+// Reference global application object
+$this->jsmapp = JFactory::getApplication();
+// JInput object
+$this->jsmjinput = $this->jsmapp->input;
+$this->jsmoption = $this->jsmjinput->getCmd('option');
+$this->jsmview = $this->jsmjinput->getCmd('view');    
+var $_identifier = $this->jsmview;
+var $limitstart = 0;
+var $limit = 0;
+var $_total = null;
+var $_pagination = null;    
     
 }
 
