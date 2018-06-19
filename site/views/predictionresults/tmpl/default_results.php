0e58769e278c359a54eebe04f4c9b8251baf3ff3
diddipoeler <famploeger@arcor.de>
2018-06-19 11:25:28 +0200

    Update default_results.php
    tippspiel

--- site/views/predictionresults/tmpl/default_results.php
+++ site/views/predictionresults/tmpl/default_results.php
@@ -135,20 +135,15 @@ $link = sportsmanagementHelperRoute::getSportsmanagementRoute('results',$routepa
 				</tr>
 
 <tfoot>
-<div class="pred_ranking">
-<?php 
-//echo $this->pagination->getListFooter(); 
-echo $this->pagination->getLimitBox();
-?>
+<div class="pagination">
+    <p class="counter">
+        <?php echo $this->pagination->getPagesCounter(); ?>
+    </p>
+    <p class="counter">
+        <?php echo $this->pagination->getResultsCounter(); ?>
+    </p>
+    <?php echo $this->pagination->getPagesLinks(); ?>
 </div>
-	
-<div class="pageslinks">
-	<?php echo $this->pagination->getPagesLinks(); ?>
-</div>
-
-<p class="pagescounter">
-	<?php echo $this->pagination->getPagesCounter(); ?>
-</p>
 	
 </tfoot>  
                 
