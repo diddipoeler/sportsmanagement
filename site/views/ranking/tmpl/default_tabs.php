2e06a29b6da3297a42adf300ca1a64c1c481a8e9
diddipoeler <famploeger@arcor.de>
2018-06-20 09:54:15 +0200

    Update default_tabs.php
    Code

--- site/views/ranking/tmpl/default_tabs.php
+++ site/views/ranking/tmpl/default_tabs.php
@@ -11,9 +11,47 @@
 
 defined('_JEXEC') or die('Restricted access');
   
-echo JHtml::_('bootstrap.startTabSet', 'defaulttabs', array('active' => 'ranking')); //start tab set
-echo JHtml::_('bootstrap.addTab', 'defaulttabs', 'ranking', JText::_('ranking'));
-echo $this->loadTemplate('ranking');
+echo JHtml::_('bootstrap.startTabSet', 'defaulttabs', array('active' => 'rankingall')); //start tab set
+echo JHtml::_('bootstrap.addTab', 'defaulttabs', 'rankingall', JText::_('ranking'));
+
+if ($this->config['show_table_1'] ||
+        $this->config['show_table_2'] ||
+        $this->config['show_table_3'] ||
+        $this->config['show_table_4'] ||
+        $this->config['show_table_5']) {
+
+echo JHtml::_('bootstrap.startTabSet', 'defaulttabsranking', array('active' => 'show_table_1')); //start tab set  
+if ($this->config['show_table_1']) {
+echo JHtml::_('bootstrap.addTab', 'defaulttabsranking', 'show_table_1', JText::_($this->config['table_text_1']));
+echo $this->loadTemplate('ranking');  
+echo JHtml::_('bootstrap.endTab');  
+}  
+if ($this->config['show_table_2']) {
+echo JHtml::_('bootstrap.addTab', 'defaulttabsranking', 'show_table_2', JText::_($this->config['table_text_2']));
+echo $this->loadTemplate('ranking_home');  
+echo JHtml::_('bootstrap.endTab');   
+} 
+if ($this->config['show_table_3']) {
+echo JHtml::_('bootstrap.addTab', 'defaulttabsranking', 'show_table_3', JText::_($this->config['table_text_3']));
+echo $this->loadTemplate('ranking_away');  
+echo JHtml::_('bootstrap.endTab');   
+} 
+if ($this->config['show_table_4']) {
+echo JHtml::_('bootstrap.addTab', 'defaulttabsranking', 'show_table_4', JText::_($this->config['table_text_4']));
+echo $this->loadTemplate('ranking_first');  
+echo JHtml::_('bootstrap.endTab');   
+} 
+if ($this->config['show_table_5']) {
+echo JHtml::_('bootstrap.addTab', 'defaulttabsranking', 'show_table_5', JText::_($this->config['table_text_5']));
+echo $this->loadTemplate('ranking_second');  
+echo JHtml::_('bootstrap.endTab');   
+}   
+echo JHtml::_('bootstrap.endTabSet'); //end tab set    
+}
+
+
+
+
 if ($this->config['show_colorlegend']) {
 echo $this->loadTemplate('colorlegend');
 }
