8ea6d2b0ccb6f0c36d5e25d9685e84bd7c2106d6
diddipoeler <famploeger@arcor.de>
2018-06-20 08:58:46 +0200

    Create default_tabs.php
    Code

---
+++ site/views/ranking/tmpl/default_tabs.php
@@ -0,0 +1,15 @@
+<?php
+/** SportsManagement ein Programm zur Verwaltung fűr alle Sportarten
+ * @version   1.0.05
+ * @file      deafult_tabs.php
+ * @author    diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license   This file is part of SportsManagement.
+ * @package   sportsmanagement
+ * @subpackage ranking
+ */
+  defined('_JEXEC') or die('Restricted access');
+  
+  
+  
+  ?>
