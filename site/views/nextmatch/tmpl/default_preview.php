73c5efc5c225ddec323b4afdd4baec5fd63102f2
diddipoeler <famploeger@arcor.de>
2018-06-21 14:53:02 +0200

    Update default_preview.php
    jcomments

--- site/views/nextmatch/tmpl/default_preview.php
+++ site/views/nextmatch/tmpl/default_preview.php
@@ -1,15 +1,17 @@
 <?php 
-/** SportsManagement ein Programm zur Verwaltung für alle Sportarten
+/** SportsManagement ein Programm zur Verwaltung fÃ¼r alle Sportarten
  * @version   1.0.05
  * @file      default_preview.php
  * @author    diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @copyright Copyright: Â© 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
  * @license   This file is part of SportsManagement.
  * @package   sportsmanagement
  * @subpackage nextmatch
  */
 
-defined( '_JEXEC' ) or die( 'Restricted access' ); ?>
+defined( '_JEXEC' ) or die( 'Restricted access' ); 
+use Joomla\Registry\Registry;
+?>
 <!-- START of match preview -->
 <?php
 
@@ -47,4 +49,39 @@ if (!empty($this->match->preview))
 
 <?php
 }
+$dispatcher = JDispatcher::getInstance();
+$comments = '';
+if(file_exists(JPATH_ROOT.'/components/com_jcomments/classes/config.php'))
+		{
+			require_once JPATH_ROOT.'/components/com_jcomments/classes/config.php';
+			require_once JPATH_ROOT.'/components/com_jcomments/jcomments.class.php';
+			require_once JPATH_ROOT.'/components/com_jcomments/models/jcomments.php';
+		}
 
+		// load joomleague comments plugin files
+		JPluginHelper::importPlugin('content','sportsmanagement_comments');
+
+		// get joomleague comments plugin params
+		$plugin = JPluginHelper::getPlugin('content', 'sportsmanagement_comments');
+
+	if (is_object($plugin)) {
+		$pluginParams = new Registry($plugin->params);
+	}
+	else {
+		$pluginParams = new Registry('');
+	}
+	$separate_comments 	= $pluginParams->get( 'separate_comments', 0 );
+
+	if ($separate_comments) {
+		// Comments integration trigger when separate_comments in plugin is set to yes/1
+		if ($dispatcher->trigger( 'onNextMatchComments', array( &$this->match, $this->teams[0]->name .' - '. $this->teams[1]->name, &$comments ) )) {
+			echo $comments;
+		}
+	}
+	else {
+		// Comments integration trigger when separate_comments in plugin is set to no/0
+		if ($dispatcher->trigger( 'onMatchComments', array( &$this->match, $this->teams[0]->name .' - '. $this->teams[1]->name, &$comments ) )) {
+			echo $comments;
+		}
+	}
+?>
