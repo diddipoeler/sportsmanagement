ddb8c914378eb35e924b8057f274bdbb8bd5045e
diddipoeler <famploeger@arcor.de>
2018-06-25 10:07:08 +0200

    Update teamplan.php
    Code

--- site/models/teamplan.php
+++ site/models/teamplan.php
@@ -364,6 +364,7 @@ try{
 		if ($getplayground)
 		{
             $query->select('playground.name AS playground_name,playground.short_name AS playground_short_name');
+	$query->select('CONCAT_WS( \':\', playground.id, playground.alias ) AS playground_slug');	
             $query->join('LEFT',' #__sportsmanagement_playground AS playground ON playground.id = m.playground_id ');
 		}
 		
