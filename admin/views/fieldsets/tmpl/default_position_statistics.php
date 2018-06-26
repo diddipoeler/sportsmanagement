5ad78e8cdd2394fbca71487e59fa75f04054c52b
diddipoeler <famploeger@arcor.de>
2018-06-26 12:29:26 +0200

    Update default_position_statistics.php
    Code

--- admin/views/fieldsets/tmpl/default_position_statistics.php
+++ admin/views/fieldsets/tmpl/default_position_statistics.php
@@ -18,21 +18,21 @@ defined('_JEXEC') or die('Restricted access');
 			<td style="width:auto;"><b><?php echo JText::_('COM_SPORTSMANAGEMENT_ADMIN_POSITION_EXISTING_STATISTICS'); ?></b><br /><?php echo $this->lists['statistic']; ?></td>
 			<td style="width:auto;">
 				<input  type="button" class="inputbox"
-						onclick="move_list_items('statistic','position_statistic');selectallstatistic();"
+						onclick="move_list_items('statistic','position_statistic');jQuery('#position_statistic option').prop('selected', true);"
 						value="&gt;&gt;" />
 				<br /><br />
 				<input  type="button" class="inputbox"
-						onclick="move_list_items('position_statistic','statistic');selectallstatistic();"
+						onclick="move_list_items('position_statistic','statistic');jQuery('#position_statistic option').prop('selected', true);"
 						value="&lt;&lt;" />
 			</td>
 			<td style="width:auto;"><b><?php echo JText::_('COM_SPORTSMANAGEMENT_ADMIN_POSITION_ASSIGNED_STATS_TO_POS'); ?></b><br /><?php echo $this->lists['position_statistic']; ?></td>
 			<td align='center' style="width:auto;">
 				<input  type="button" class="inputbox"
-						onclick="move_up('position_statistic');selectallstatistic();"
+						onclick="move_up('position_statistic');jQuery('#position_statistic option').prop('selected', true);"
 						value="<?php echo JText::_('COM_SPORTSMANAGEMENT_GLOBAL_UP'); ?>" />
 				<br /><br />
 				<input type="button" class="inputbox"
-					   onclick="move_down('position_statistic');selectallstatistic();"
+					   onclick="move_down('position_statistic');jQuery('#position_statistic option').prop('selected', true);"
 					   value="<?php echo JText::_('COM_SPORTSMANAGEMENT_GLOBAL_DOWN'); ?>" />
 			</td>
 			<td style="width:auto;">
