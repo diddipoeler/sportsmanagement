fa3560a6d802560cda97627fbc808792509b9c7f
diddipoeler <famploeger@arcor.de>
2018-06-27 14:05:39 +0200

    Update default_stats.php
    statistic

--- site/views/statsrankingteams/tmpl/default_stats.php
+++ site/views/statsrankingteams/tmpl/default_stats.php
@@ -9,4 +9,23 @@
  * @subpackage statsrankingteams
  */
 
+defined( '_JEXEC' ) or die( 'Restricted access' );
 ?>
+<table class="<?php	echo $this->config['table_class'];	?>">
+<thead>
+<tr class="">
+<th class="td_r rank"><?php	echo JText::_( 'COM_SPORTSMANAGEMENT_STATSRANKING_RANK' );	?></th>  
+<th class="td_l"><?php	echo JText::_( 'COM_SPORTSMANAGEMENT_STATSRANKING_TEAM' );	?></th>  
+<?php  
+foreach ( $this->stats AS $rows )
+{
+?>  
+<th class="td_r" class="nowrap"><?php	echo JText::_($rows->name); ?></th>
+<?php  
+}
+  
+  
+?>  
+</tr>
+</thead>  
+</table>  
