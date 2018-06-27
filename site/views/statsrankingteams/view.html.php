370708478c93282e6edacab217c0062cd2174045
diddipoeler <famploeger@arcor.de>
2018-06-27 11:28:02 +0200

    Update view.html.php
    statistic

--- site/views/statsrankingteams/view.html.php
+++ site/views/statsrankingteams/view.html.php
@@ -1,4 +1,35 @@
 <?php
-
-
+/** SportsManagement ein Programm zur Verwaltung für alle Sportarten
+ * @version   1.0.05
+ * @file      view.html.php
+ * @author    diddipoeler, stony, svdoldie und donclumsy (diddipoeler@arcor.de)
+ * @copyright Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license   This file is part of SportsManagement.
+ * @package   sportsmanagement
+ * @subpackage statsrankingteam
+ */
+defined( '_JEXEC' ) or die( 'Restricted access' );
+//jimport( 'joomla.application.component.view' );
+/**
+ * sportsmanagementViewStatsRanking
+ * 
+ * @package 
+ * @author diddi
+ * @copyright 2014
+ * @version $Id$
+ * @access public
+ */
+class sportsmanagementViewStatsRankingTeam extends sportsmanagementView
+{
+  
+function init()
+	{  
+  	$this->document->addScript(JUri::root(true) . '/components/' . $this->option . '/assets/js/smsportsmanagement.js');
+        sportsmanagementModelProject::setProjectID($this->jinput->getInt('p',0),$this->cfg_which_database);
+  
+  $teams = sportsmanagementModelProject::getTeamsIndexedById(0,'name',$this->cfg_which_database);
+  
+}
+  
+}
 ?>
