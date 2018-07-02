ade63d394ce5208063a2f52b74cb30c28a83620c
diddipoeler <famploeger@arcor.de>
2018-07-02 14:33:01 +0200

    Update default.php
    Code

--- admin/views/teampersons/tmpl/default.php
+++ admin/views/teampersons/tmpl/default.php
@@ -85,6 +85,7 @@ echo $this->loadTemplate('joomla_version');
 <!--	</fieldset> -->
 <input type="hidden" name="project_team_id" value="<?php echo $this->project_team_id; ?>" />
 <input type="hidden" name="team_id" value="<?php echo $this->team_id; ?>" />
+<input type="hidden" name="season_id" value="<?php echo $this->season_id; ?>" />	
 <input type="hidden" name="pid" value="<?php echo $this->project_id; ?>" />
 <input type="hidden" name="persontype" value="<?php echo $this->_persontype; ?>" />
 <input type="hidden" name="search_mode" value="<?php echo $this->lists['search_mode'];?>" id="search_mode" />
