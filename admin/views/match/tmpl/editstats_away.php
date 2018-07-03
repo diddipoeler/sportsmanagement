d85313fafadfaf7fcee6f501482abee9f1c0f78a
diddipoeler <famploeger@arcor.de>
2018-07-03 16:13:22 +0200

    Update editstats_away.php
    Code

--- admin/views/match/tmpl/editstats_away.php
+++ admin/views/match/tmpl/editstats_away.php
@@ -19,7 +19,7 @@ $j = 0;
 <fieldset class="adminform">
 	<legend><?php echo JText::_( $this->teams->team2 );?></legend>
 	<?php foreach ($this->positions as $position): ?>
-	<h3><?php echo $position->text; ?></h3>
+	<h3><?php echo JText::_($position->text); ?></h3>
 	<table class='adminlist'>
 		<thead>
 			<tr>
