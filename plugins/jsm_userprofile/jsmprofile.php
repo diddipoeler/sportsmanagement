1d918d1273678590006f83907d1695e2ad290da8
diddipoeler <famploeger@arcor.de>
2018-07-05 10:52:46 +0200

    Update jsmprofile.php
    Code

--- plugins/jsm_userprofile/jsmprofile.php
+++ plugins/jsm_userprofile/jsmprofile.php
@@ -1,41 +1,13 @@
 <?php 
-/** SportsManagement ein Programm zur Verwaltung für alle Sportarten
-* @version         1.0.05
-* @file                agegroup.php
-* @author                diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
-* @copyright        Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
-* @license                This file is part of SportsManagement.
-*
-* SportsManagement is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* SportsManagement is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with SportsManagement.  If not, see <http://www.gnu.org/licenses/>.
-*
-* Diese Datei ist Teil von SportsManagement.
-*
-* SportsManagement ist Freie Software: Sie können es unter den Bedingungen
-* der GNU General Public License, wie von der Free Software Foundation,
-* Version 3 der Lizenz oder (nach Ihrer Wahl) jeder späteren
-* veröffentlichten Version, weiterverbreiten und/oder modifizieren.
-*
-* SportsManagement wird in der Hoffnung, dass es nützlich sein wird, aber
-* OHNE JEDE GEWÄHELEISTUNG, bereitgestellt; sogar ohne die implizite
-* Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
-* Siehe die GNU General Public License für weitere Details.
-*
-* Sie sollten eine Kopie der GNU General Public License zusammen mit diesem
-* Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
-*
-* Note : All ini files need to be saved as UTF-8 without BOM
-*/
+/** SportsManagement ein Programm zur Verwaltung fÃ¼r alle Sportarten
+ * @version   1.0.05
+ * @file      jsmprofile.php
+ * @author    diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright Copyright: Â© 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license   This file is part of SportsManagement.
+ * @package   sportsmanagement
+ * @subpackage plugins
+ */
 
 defined('JPATH_BASE') or die;
  
@@ -62,7 +34,7 @@ class plgUserjsmprofile extends JPlugin
         $app = JFactory::getApplication();
         // Check we are manipulating a valid form.
         //if (!in_array($context, array('com_users.profile','com_users.registration','com_users.user','com_admin.profile')))
-        if (!in_array($context, array('com_users.user','com_admin.profile')))
+        if (!in_array($context, array('com_users.user','com_users.profile','com_admin.profile')))
         {
             return true;
         }
@@ -80,17 +52,6 @@ class plgUserjsmprofile extends JPlugin
         $results = $db->loadRowList();
         
         //$app->enqueueMessage(JText::_(__METHOD__.' '.__LINE__.' results<br><pre>'.print_r($results,true).'</pre>'),'');
-        /*
-Array
-(
-    [0] => Array
-        (
-            [0] => jsmprofile.databaseaccess
-            [1] => 0
-        )
-
-)
-        */
  
         // Check for a database error.
         if ($db->getErrorNum()) {
