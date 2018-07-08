be0fb3b9aff40b262c47d7a79095dde382915df7
diddipoeler <famploeger@arcor.de>
2018-07-08 17:54:58 +0200

    Code

--- site/views/player/tmpl/default_playfield.php
+++ site/views/player/tmpl/default_playfield.php
@@ -32,7 +32,7 @@ if ( isset($this->person_position) )
 {
 ?>
 <div style="position:relative;height:170px;background-image:url(<?PHP echo $backimage;?>);background-repeat:no-repeat;">
-<img src="<?PHP echo $hauptimage;?>" class="<?PHP echo $this->person_position;?>" alt="<?PHP echo $this->teamPlayer->position_name; ?>" title="<?PHP echo $this->teamPlayer->position_name; ?>" />
+<img src="<?PHP echo $hauptimage;?>" class="<?PHP echo $this->person_position;?>" alt="<?PHP echo JText::_($this->teamPlayer->position_name); ?>" title="<?PHP echo JText::_($this->teamPlayer->position_name); ?>" />
 
 <?PHP
 
