1bccbd1777d39597cb61bb2ced5d7a981c06a6dc
diddipoeler <famploeger@arcor.de>
2018-07-19 09:40:01 +0200

    Update default_teams.php
    #698

--- site/views/clubinfo/tmpl/default_teams.php
+++ site/views/clubinfo/tmpl/default_teams.php
@@ -1,51 +1,19 @@
 <?php 
-/** SportsManagement ein Programm zur Verwaltung f?r alle Sportarten
-* @version         1.0.05
-* @file                agegroup.php
-* @author                diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
-* @copyright        Copyright: ? 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
-* @license                This file is part of SportsManagement.
-*
-* SportsManagement is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* SportsManagement is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with SportsManagement.  If not, see <http://www.gnu.org/licenses/>.
-*
-* Diese Datei ist Teil von SportsManagement.
-*
-* SportsManagement ist Freie Software: Sie k?nnen es unter den Bedingungen
-* der GNU General Public License, wie von der Free Software Foundation,
-* Version 3 der Lizenz oder (nach Ihrer Wahl) jeder sp?teren
-* ver?ffentlichten Version, weiterverbreiten und/oder modifizieren.
-*
-* SportsManagement wird in der Hoffnung, dass es n?tzlich sein wird, aber
-* OHNE JEDE GEW?HELEISTUNG, bereitgestellt; sogar ohne die implizite
-* Gew?hrleistung der MARKTF?HIGKEIT oder EIGNUNG F?R EINEN BESTIMMTEN ZWECK.
-* Siehe die GNU General Public License f?r weitere Details.
-*
-* Sie sollten eine Kopie der GNU General Public License zusammen mit diesem
-* Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
-*
-* Note : All ini files need to be saved as UTF-8 without BOM
-*/
+/** SportsManagement ein Programm zur Verwaltung für alle Sportarten
+ * @version   1.0.05
+ * @file      default_teams.php
+ * @author    diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license   This file is part of SportsManagement.
+ */
 
 defined( '_JEXEC' ) or die( 'Restricted access' ); ?>
 
-<div class="row-fluid">
+<div class="row-fluid" id="default_teams">
 <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">	
 <h4>
 <?php echo JText::_('COM_SPORTSMANAGEMENT_CLUBINFO_TEAMS'); ?>
 </h4>
-	
-	<table class="table">
 	
 	<?php
 	$params = array();
@@ -63,7 +31,7 @@ defined( '_JEXEC' ) or die( 'Restricted access' ); ?>
        $routeparameter['ptid'] = $team->ptid;
                 $link = sportsmanagementHelperRoute::getSportsmanagementRoute('teaminfo',$routeparameter);
 				?>
-                <address>
+                <div>
 			
 					<?php
 						
@@ -129,11 +97,11 @@ defined( '_JEXEC' ) or die( 'Restricted access' ); ?>
             	
 			}
             ?>
-            </address>
+            </div>
             <?PHP
 		}
 	?>
 	
-	</table>
+	
 </div>
 </div>
