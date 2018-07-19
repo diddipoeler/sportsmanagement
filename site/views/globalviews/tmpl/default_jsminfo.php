d0be01e6695f8985d6fb275ed9f3d9c822524cf8
diddipoeler <famploeger@arcor.de>
2018-07-19 07:49:06 +0200

    Update default_jsminfo.php
    Code

--- site/views/globalviews/tmpl/default_jsminfo.php
+++ site/views/globalviews/tmpl/default_jsminfo.php
@@ -1,1 +1,15 @@
+<?php 
+/** SportsManagement ein Programm zur Verwaltung für alle Sportarten
+ * @version   1.0.05
+ * @file      default_jsminfo.php
+ * @author    diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license   This file is part of SportsManagement.
+ * @package   sportsmanagement
+ * @subpackage globalviews
+ */
+defined('_JEXEC') or die('Restricted access');
 
+
+
+?>
