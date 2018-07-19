78a454e02026398d6edc30b7077ee2603be38bbe
diddipoeler <famploeger@arcor.de>
2018-07-19 10:47:54 +0200

    Update default_selectround.php
    #698

--- site/views/resultsranking/tmpl/default_selectround.php
+++ site/views/resultsranking/tmpl/default_selectround.php
@@ -1,15 +1,18 @@
 <?php 
-/** SportsManagement ein Programm zur Verwaltung für alle Sportarten
+/** SportsManagement ein Programm zur Verwaltung fÃ¼r alle Sportarten
  * @version   1.0.05
  * @file      default_selectround.php
  * @author    diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @copyright Copyright: Â© 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
  * @license   This file is part of SportsManagement.
  * @package   sportsmanagement
  * @subpackage resultsranking
  */
 
 defined( '_JEXEC' ) or die( 'Restricted access' );
-
+?>
+<div class="row" id="selectround">
+<?php
 echo JHTML::_('select.genericlist', $this->matchdaysoptions, 'select-round', 'onchange="joomleague_changedoc(this);" style="float:right;"', 'value', 'text', $this->currenturl);
 ?>
+</div>
