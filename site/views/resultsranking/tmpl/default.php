88887f5506f1b9305e279398383319eebb5d95b0
diddipoeler <famploeger@arcor.de>
2018-07-27 18:26:00 +0200

    Code

--- site/views/resultsranking/tmpl/default.php
+++ site/views/resultsranking/tmpl/default.php
@@ -24,63 +24,58 @@ $this->kmlfile = $this->project->id.'-ranking.kml';
 ?>
 <div class="container" id="resultsranking">
 
-	<?php 
-    if ( COM_SPORTSMANAGEMENT_SHOW_DEBUG_INFO )
+<?php 
+if ( COM_SPORTSMANAGEMENT_SHOW_DEBUG_INFO )
 {
-    echo $this->loadTemplate('debug');
+echo $this->loadTemplate('debug');
 }
 
-	echo $this->loadTemplate('projectheading');
+echo $this->loadTemplate('projectheading');
 
-	if ($this->config['show_matchday_dropdown'])
-	{
-		echo $this->loadTemplate('selectround');
-	}
+if ($this->config['show_matchday_dropdown'])
+{
+echo $this->loadTemplate('selectround');
+}
 
-// diddipoeler
-  // aufbau der templates
-  $this->output = array();
+/**
+ * diddipoeler
+ * aufbau der templates
+ */
+$this->output = array();
   
-  if ( $this->params->get('what_to_show_first', 0) == 0 )
-  {
-  $this->output['COM_SPORTSMANAGEMENT_RESULTS_ROUND_RESULTS'] = 'results';
-  $this->output['COM_SPORTSMANAGEMENT_RANKING_PAGE_TITLE'] = 'ranking';
-  }
-  else
-  {
-  $this->output['COM_SPORTSMANAGEMENT_RANKING_PAGE_TITLE'] = 'ranking';
-  $this->output['COM_SPORTSMANAGEMENT_RESULTS_ROUND_RESULTS'] = 'results';  
-  }
-  
-  
+if ( $this->params->get('what_to_show_first', 0) == 0 )
+{
+$this->output['COM_SPORTSMANAGEMENT_RESULTS_ROUND_RESULTS'] = 'results';
+$this->output['COM_SPORTSMANAGEMENT_RANKING_PAGE_TITLE'] = 'ranking';
+}
+else
+{
+$this->output['COM_SPORTSMANAGEMENT_RANKING_PAGE_TITLE'] = 'ranking';
+$this->output['COM_SPORTSMANAGEMENT_RESULTS_ROUND_RESULTS'] = 'results';  
+}
   
 echo $this->loadTemplate('show_tabs');
-   
     
-    if ($this->config['show_colorlegend'])
-		{
-			echo $this->loadTemplate('colorlegend');
-		}
+if ($this->config['show_colorlegend'])
+{
+echo $this->loadTemplate('colorlegend');
+}
 		
-		if ( $this->config['show_explanation'] )
-		{
-			echo $this->loadTemplate('explanation');
-		}
-        if ( $this->config['show_ranking_maps'] )
-	{ 
-		echo $this->loadTemplate('googlemap');
-	}   
+if ( $this->config['show_explanation'] )
+{
+echo $this->loadTemplate('explanation');
+}
+
+if ( $this->config['show_ranking_maps'] )
+{ 
+echo $this->loadTemplate('googlemap');
+}   
     	
-	if ( $this->config['show_pagnav'] )
-	{
-		echo $this->loadTemplate('pagnav');
-	}
-?>
-	
-    <?PHP
+if ( $this->config['show_pagnav'] )
+{
+echo $this->loadTemplate('pagnav');
+}
+
 echo $this->loadTemplate('jsminfo');
-	?>
-	
-    <?PHP
-	?>
+?>
 </div>
