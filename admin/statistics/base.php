206c86c6dd8884e07b10bdd28cc7a0e8cc1cfcdb
diddipoeler <famploeger@arcor.de>
2018-07-28 17:07:03 +0200

    #716

--- admin/statistics/base.php
+++ admin/statistics/base.php
@@ -444,7 +444,7 @@ class SMStatistic extends JObject
 
 		if (!class_exists($classname))
 		{
-			$file = JPATH_COMPONENT_ADMINISTRATOR .DS . 'statistics' . DS .$class.'.php';
+			$file = 'administrator'.DS.'components'.DS.'con_sportsmanagement'.DS.'statistics'.DS.$class.'.php';
 			if (!file_exists($file)) {
 				JError::raiseError(0, $classname .': '. JText::_('STATISTIC CLASS NOT DEFINED'));
 			}
