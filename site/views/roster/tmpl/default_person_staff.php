89a1229799fa4458dbd79406712d90e5aa454445
diddipoeler <famploeger@arcor.de>
2018-08-18 10:30:15 +0200

    #663

--- site/views/roster/tmpl/default_person_staff.php
+++ site/views/roster/tmpl/default_person_staff.php
@@ -77,11 +77,38 @@ $this->overallconfig['use_jquery_modal']);
 						</span><!-- /.jl_roster_persondetails_label -->
 						<span class="jl_roster_persondetails_data">
 <?php
-						if (!empty($this->row->parentname))
-						{
-						echo Text::sprintf('COM_SPORTSMANAGEMENT_ROSTER_MEMBER_OF',Text::_($this->row->parentname));
-						}
-						echo '&nbsp;' . Text::_( $this->row->position );
+//						if (!empty($this->row->parentname))
+//						{
+//						echo Text::sprintf('COM_SPORTSMANAGEMENT_ROSTER_MEMBER_OF',Text::_($this->row->parentname));
+//						}
+//						echo '&nbsp;' . Text::_( $this->row->position );
+                        $staff_position = '';
+				switch ($this->config['staff_position_format'])
+				{
+					case 2:	 // show member with text
+								$staff_position = Text::sprintf('COM_SPORTSMANAGEMENT_ROSTER_MEMBER_OF',Text::_($row->parentname));
+								break;
+
+					case 3:	 // show function with text
+								$staff_position .= Text::sprintf('COM_SPORTSMANAGEMENT_ROSTER_FUNCTION_IS',Text::_($row->position));
+								break;
+
+					case 4:	 // show only function
+								$staff_position = Text::_($row->parentname);
+								break;
+
+					case 5:	 // show only position
+								$staff_position = Text::_($row->position);
+								break;
+
+					default: // show member+function with text
+								$staff_position = Text::sprintf('COM_SPORTSMANAGEMENT_ROSTER_MEMBER_OF',Text::_($row->parentname));
+								$staff_position .= '<br />';
+								$staff_position .= Text::sprintf('COM_SPORTSMANAGEMENT_ROSTER_FUNCTION_IS',Text::_($row->position));
+								break;
+				}
+				echo $staff_position;
+                        
 						?>
 						</span><!-- /.jl_roster_persondetails_data -->
 					</div>
