5b6d994f11b8e3a34a9f137de0349237c3e62f1b
diddipoeler <famploeger@arcor.de>
2018-08-20 11:42:21 +0200

    Update view.html.php
    #742

--- site/views/results/view.html.php
+++ site/views/results/view.html.php
@@ -136,7 +136,7 @@ if ( ($this->overallconfig['show_project_rss_feed']) == 1 )
 		$rss = array('type' => 'application/rss+xml', 'title' => Text::_('COM_SPORTSMANAGEMENT_RESULTS_RSSFEED'));
 
 		// add the links
-		$document->addHeadLink(Route::::_($feed.'&type=rss'), 'alternate', 'rel', $rss);
+		$document->addHeadLink(Route::_($feed.'&type=rss'), 'alternate', 'rel', $rss);
         $view = $jinput->getVar( "view") ;
         //$stylelink = '<link rel="stylesheet" href="'.JURI::root().'components/'.$option.'/assets/css/bootstrap-dialog.min.css'.'" type="text/css" />' ."\n";
         
