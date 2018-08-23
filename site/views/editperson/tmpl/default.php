9a29ed3c714977ac8952f6e984bf0c56594dc79f
diddipoeler <famploeger@arcor.de>
2018-08-23 12:51:41 +0200

    Update default.php
    #664

--- site/views/editperson/tmpl/default.php
+++ site/views/editperson/tmpl/default.php
@@ -51,112 +51,18 @@ $fieldsets = $this->form->getFieldsets();
   ?>
   </legend>
   </fieldset>
-<?php echo HTMLHelper::_('bootstrap.startTabSet', 'myTab', array('active' => 'details')); ?>    
-<div class="width-60 fltlft">
-		<fieldset class="adminform">
-			<legend><?php echo Text::_('COM_SPORTSMANAGEMENT_TABS_DETAILS'); ?></legend>
-			<ul class="adminformlist">
-			<?php 
-            foreach($this->form->getFieldset('details') as $field) :
-            echo HTMLHelper::_('bootstrap.addTab', 'myTab', $field->name, Text::_($field->label, true)); 
-            //echo '<pre>'.print_r($field,true).'</pre>';
-            ?>
-				<li><?php echo $field->label; ?>
-				<?php echo $field->input; 
-                 
-                if ( $field->name == 'country' )
-                {
-                echo JSMCountries::getCountryFlag($field->value);    
-                }
-                
-                if ( $field->name == 'standard_playground' )
-                {
-                //echo sportsmanagementHelper::getPicturePlayground($field->value);
-                $picture = sportsmanagementHelper::getPicturePlayground($field->value);
-                //echo $picture;
-                //echo JHtml::image($picture, 'Playground', array('title' => 'Playground','width' => '50' )); 
-                //echo JHtml::_('image', $picture, 'Playground',array('title' => 'Playground','width' => '50' )); 
-?>
-<a href="<?php echo JURI::root().$picture;?>" title="<?php echo 'Playground';?>" class="modal">
-<img src="<?php echo JURI::root().$picture;?>" alt="<?php echo 'Playground';?>" width="50" />
-</a>
-<?PHP                   
-                }
-                
-                if ( $field->name == 'website' )
-                {
-                echo '<img style="" src="http://www.thumbshots.de/cgi-bin/show.cgi?url='.$field->value.'">';  
-                }
-                if ( $field->name == 'twitter' )
-                {
-                echo '<img style="" src="http://www.thumbshots.de/cgi-bin/show.cgi?url='.$field->value.'">';  
-                }
-                if ( $field->name == 'facebook' )
-                {
-                echo '<img style="" src="http://www.thumbshots.de/cgi-bin/show.cgi?url='.$field->value.'">';  
-                }
-                
-                $suchmuster = array ("jform[","]");
-                $ersetzen = array ('', '');
-                $var_onlinehelp = str_replace($suchmuster, $ersetzen, $field->name);
-                
-                switch ($var_onlinehelp)
-                {
-                    case 'id':
-                    break;
-                    default:
-                    if ( $field->type != 'Hidden')
-                    {
-                ?>
-                <a	rel="{handler: 'iframe',size: {x: <?php echo COM_SPORTSMANAGEMENT_MODAL_POPUP_WIDTH; ?>,y: <?php echo COM_SPORTSMANAGEMENT_MODAL_POPUP_HEIGHT; ?>}}"
-									href="<?php echo COM_SPORTSMANAGEMENT_HELP_SERVER.'SM-Backend-Felder:'.JFactory::getApplication()->input->getVar( "view").'-'.$var_onlinehelp; ?>"
-									 class="modal">
-									<?php
-									echo HTMLHelper::_(	'image','media/com_sportsmanagement/jl_images/help.png',
-													Text::_('COM_SPORTSMANAGEMENT_HELP_LINK'),'title= "' .
-													Text::_('COM_SPORTSMANAGEMENT_HELP_LINK').'"');
-									?>
-								</a>
-                
-                <?PHP
-                }
-                break;
-                }
-                ?>
-                </li>
-			<?php 
-            
-            //echo $field->type;
-            echo HTMLHelper::_('bootstrap.endTab');
-            endforeach; 
-            ?>
-			</ul>
-		</fieldset>
-	</div>		
+ 
+<?php 
+echo HTMLHelper::_('bootstrap.startTabSet', 'myTab', array('active' => 'details'));    
+foreach ($fieldsets as $fieldset) :
+echo HTMLHelper::_('bootstrap.addTab', 'myTab', $fieldset->name, Text::_($fieldset->label, true)); 
+echo HTMLHelper::_('bootstrap.endTab');
+endforeach; 
 
-<div class="width-40 fltrt">
-		<?php
-		
-		foreach ($fieldsets as $fieldset) :
-	echo HTMLHelper::_('bootstrap.addTab', 'myTab', $fieldset->name, Text::_($fieldset->label, true));  
-			if ( $fieldset->name == 'details' ||  $fieldset->name == 'seasons' ) :
-				continue;
-			endif;
-			//echo HTMLHelper::_('sliders.panel', Text::_($fieldset->label), $fieldset->name);
-		if (isset($fieldset->description) && !empty($fieldset->description)) :
-				echo '<p class="tab-description">'.Text::_($fieldset->description).'</p>';
-			endif;
-		//echo $this->loadTemplate($fieldset->name);
-        $this->fieldset = $fieldset->name;
-        echo $this->loadTemplate('fieldsets');
-	echo HTMLHelper::_('bootstrap.endTab');
-		endforeach; ?>
-		<?php   ?>
-
+echo HTMLHelper::_('bootstrap.endTabSet');
+?>	
 	
-	</div>
-
-<?php echo HTMLHelper::_('bootstrap.endTabSet'); ?>
+	
 <div class="clr"></div>
 
     
