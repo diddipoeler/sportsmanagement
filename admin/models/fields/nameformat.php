0d9ba39c1c477bff385ed145a6be390d63479b1c
diddipoeler <famploeger@arcor.de>
2018-08-26 17:35:16 +0200

    Joomla 3/4

--- admin/models/fields/nameformat.php
+++ admin/models/fields/nameformat.php
@@ -1,17 +1,19 @@
 <?php
-/** 
-* Modified by Sports Management Entwickler: diddipoeler, stony, svdoldie und  donclumsy
-* http://fussballineuropa.de/
-* Email: diddipoeler@gmx.de
-* Date: 2014
-* Release: 1.0.47
-* License : http://www.gnu.org/copyleft/gpl.html GNU/GPL 
-*/
-
+/** SportsManagement ein Programm zur Verwaltung für Sportarten
+ * @version   1.0.05
+ * @file      nameformat.php
+ * @author    diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license   This file is part of SportsManagement.
+ * @package   sportsmanagement
+ * @subpackage fields
+ */
 
 defined('_JEXEC') or die('Restricted access');
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Form\FormField;
+use Joomla\CMS\Form\FormHelper;
 
 /**
  * FormFieldNameFormat
