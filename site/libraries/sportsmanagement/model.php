664a9b57e002dc674ecee4193e25f658c71ef30e
diddipoeler <famploeger@arcor.de>
2018-09-04 13:38:23 +0200

    Update model.php
    #767

--- site/libraries/sportsmanagement/model.php
+++ site/libraries/sportsmanagement/model.php
@@ -35,6 +35,18 @@ $this->jsmview = $this->jsmjinput->getCmd('view');
        parent::__construct($config);
        }
     
+	/**
+	 * Method to get the record form.
+	 *
+	 * @param	array	$data		Data for the form.
+	 * @param	boolean	$loadData	True if the form is to load its own data (default case), false if not.
+	 * @return	mixed	A JForm object on success, false on failure
+	 * @since	1.6
+	 */
+	public function getForm($data = array(), $loadData = true) 
+	{
+		
+	}
     
 }
 
