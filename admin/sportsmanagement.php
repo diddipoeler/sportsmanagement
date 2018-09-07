8a69b9153a4a7fc054453601bcf3c5e5b44d4267
diddipoeler <famploeger@arcor.de>
2018-09-07 08:44:06 +0200

    Update sportsmanagement.php
    Joomla 4 Github

--- admin/sportsmanagement.php
+++ admin/sportsmanagement.php
@@ -50,6 +50,7 @@ if(version_compare( $baseVersion,'4.0','ge'))
 {
 // Joomla! 4.0 code here
 defined('JSM_JVERSION') or define('JSM_JVERSION', 4);
+JLoader::import('components.com_sportsmanagement.libraries.github.github', JPATH_ADMINISTRATOR);	
 }
 if(version_compare($baseVersion,'3.0','ge')) 
 {
