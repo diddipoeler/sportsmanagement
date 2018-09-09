fddf01ef19092e5185c849ceb762bded26e2b06f
diddipoeler <famploeger@arcor.de>
2018-09-09 15:30:28 +0200

    Joomla 4

--- admin/libraries/sportsmanagement/view.php
+++ admin/libraries/sportsmanagement/view.php
@@ -85,8 +85,7 @@ class sportsmanagementView extends HtmlView
 			$this->app->enqueueMessage(implode("\n",$errors));
 			return false;
 		}
-        $this->modalheight = ComponentHelper::getParams($this->jinput->getCmd('option'))->get('modal_popup_height', 600);
-        $this->modalwidth = ComponentHelper::getParams($this->jinput->getCmd('option'))->get('modal_popup_width', 900);
+        
         $this->layout = $this->getLayout();
         
     if(version_compare(JVERSION,'3.0.0','ge')) 
@@ -110,6 +109,8 @@ class sportsmanagementView extends HtmlView
         $this->format = $this->jinput->getCmd('format');
         $this->view = $this->jinput->getCmd('view', 'cpanel');
         $this->tmpl = $this->jinput->getCmd('tmpl', '');
+        $this->modalheight = ComponentHelper::getParams($this->jinput->getCmd('option'))->get('modal_popup_height', 600);
+        $this->modalwidth = ComponentHelper::getParams($this->jinput->getCmd('option'))->get('modal_popup_width', 900);
 	$this->project_id = $this->jinput->get('pid');
 	$this->jsmmessage = '';
 	$this->jsmmessagetype = 'notice';
