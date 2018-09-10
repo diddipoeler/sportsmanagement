b8ff59160a042f885727caaba692e50038746fa1
diddipoeler <famploeger@arcor.de>
2018-09-10 12:20:01 +0200

    Update assignteams.php
    Joomla 4

--- admin/layouts/assignteams.php
+++ admin/layouts/assignteams.php
@@ -1,1 +1,9 @@
+<?php
+defined('JPATH_BASE') or die;
+JHtml::_('behavior.core');
+?>
 
+<button data-toggle="modal" onclick="jQuery( '#collapseModalassignTeams' ).modal('show');" class="btn btn-small">
+	<span class="icon-checkbox-partial" aria-hidden="true"></span>
+	<?php echo JText::_('COM_SPORTSMANAGEMENT_ADMIN_PROJECTTEAMS_BUTTON_ASSIGN'); ?>
+</button>
