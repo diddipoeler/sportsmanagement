e4cd73e438b119d21d211bed57eb770aeab5e6cb
diddipoeler <famploeger@arcor.de>
2018-09-11 07:40:06 +0200

    Update default_results.php
    #791

--- site/views/results/tmpl/default_results.php
+++ site/views/results/tmpl/default_results.php
@@ -1,9 +1,9 @@
 <?php 
-/** SportsManagement ein Programm zur Verwaltung für alle Sportarten
+/** SportsManagement ein Programm zur Verwaltung fÃ¼r alle Sportarten
  * @version   1.0.05
  * @file      default_results.php
  * @author    diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @copyright Copyright: Â© 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
  * @license   This file is part of SportsManagement.
  * @subpackage results
  */
@@ -12,47 +12,47 @@ defined( '_JEXEC' ) or die( 'Restricted access' );
 
 if ( !isset ( $this->project ) )
 {
-	JError::raiseWarning( 'ERROR_CODE', JText::_( 'Error: ProjectID was not submitted in URL or selected project was not found in database!' ) );
+JError::raiseWarning( 'ERROR_CODE', JText::_( 'Error: ProjectID was not submitted in URL or selected project was not found in database!' ) );
 }
 else
 {
-	?>
-	<div class="<?php echo COM_SPORTSMANAGEMENT_BOOTSTRAP_DIV_CLASS; ?>">
-		<br />
-		<?php
-		if ( count( $this->matches ) > 0 )
-		{
-			switch ( $this->config['result_style'] )
-			{
-				case 4:
-							{
-								echo $this->loadTemplate('results_style_dfcday');
-							}
-							break;
-        case 3:
-							{
-								echo $this->loadTemplate('results_style3');
-							}
-							break;
+?>
+<div class="<?php echo $this->divclassrow; ?>">
+<br />
+<?php
+if ( count( $this->matches ) > 0 )
+{
 
-				case 0:
-				case 1:
-				case 2:				
-				default:
-							{
-								echo $this->loadTemplate('results_style0');
-							}
-							break;
-			}
-		}
-		?>
-	</div>
-	<!-- Main END -->
-	<?php
-	if ( $this->config['show_dnp_teams'] ) 
-    { 
-        echo $this->loadTemplate('freeteams'); 
-        }
+switch ( $this->config['result_style'] )
+{
+case 4:
+{
+echo $this->loadTemplate('results_style_dfcday');
+}
+break;
+case 3:
+{
+echo $this->loadTemplate('results_style3');
+}
+break;
+case 0:
+case 1:
+case 2:				
+default:
+{
+echo $this->loadTemplate('results_style0');
+}
+break;
+}
+}
+?>
+</div>
+<!-- Main END -->
+<?php
+if ( $this->config['show_dnp_teams'] ) 
+{ 
+echo $this->loadTemplate('freeteams'); 
+}
         
 }
 ?>
