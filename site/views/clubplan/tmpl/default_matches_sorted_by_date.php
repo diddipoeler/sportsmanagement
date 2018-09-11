cca861997f36e3526e042192873799197c7855d9
diddipoeler <famploeger@arcor.de>
2018-09-11 09:22:36 +0200

    Update default_matches_sorted_by_date.php
    #791

--- site/views/clubplan/tmpl/default_matches_sorted_by_date.php
+++ site/views/clubplan/tmpl/default_matches_sorted_by_date.php
@@ -1,9 +1,9 @@
 <?php 
-/** SportsManagement ein Programm zur Verwaltung für alle Sportarten
+/** SportsManagement ein Programm zur Verwaltung fÃ¼r alle Sportarten
  * @version   1.0.05
  * @file      default_matches_sorted_by_date.php
  * @author    diddipoeler, stony, svdoldie und donclumsy (diddipoeler@arcor.de)
- * @copyright Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @copyright Copyright: Â© 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
  * @license   This file is part of SportsManagement.
  * @package   sportsmanagement
  * @subpackage clubplan
@@ -16,7 +16,7 @@ use Joomla\CMS\Factory;
 ?>
 
 <!-- START: matches -->
-<div class="row table-responsive" id="clubplanmatchessbd">
+<div class="<?php echo $this->divclassrow;?> table-responsive" id="clubplanmatchessbd">
 	<?php
 		//sort matches by dates
 		$gamesByDate = Array();
