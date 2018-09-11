c273941508b5bb554b0d04e53f7b9a28a2300afc
diddipoeler <famploeger@arcor.de>
2018-09-11 15:23:22 +0200

    Update default.php
    #664

--- site/views/editclub/tmpl/default.php
+++ site/views/editclub/tmpl/default.php
@@ -13,18 +13,6 @@ defined('_JEXEC') or die('Restricted access');
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\HTML\HTMLHelper;
 
-//echo ' club<br><pre>'.print_r($this->item,true).'</pre>';
-
-//JFactory::getLanguage()->load('com_sportsmanagement', JPATH_ADMINISTRATOR);
-
-
-//HTMLHelper::_('behavior.modal');
-//jimport('joomla.html.pane');
-//jimport('joomla.html.html.tabs' );
-
-//echo 'form<pre>'.print_r($this->form , true).'</pre><br>';
-//echo 'club<pre>'.print_r($this->club , true).'</pre><br>';
-
 // Get the form fieldsets.
 $fieldsets = $this->form->getFieldsets();
 
@@ -81,40 +69,6 @@ break;
 endforeach; 
 
 echo HTMLHelper::_('bootstrap.endTabSet');
-
-
-
-/*
-$options = array(
-    'onActive' => 'function(title, description){
-        description.setStyle("display", "block");
-        title.addClass("open").removeClass("closed");
-    }',
-    'onBackground' => 'function(title, description){
-        description.setStyle("display", "none");
-        title.addClass("closed").removeClass("open");
-    }',
-    'startOffset' => 0,  // 0 starts on the first tab, 1 starts the second, etc...
-    'useCookie' => true, // this must not be a string. Don't use quotes.
-);
- 
-echo HTMLHelper::_('tabs.start', 'tab_group_id', $options);
- 
-echo HTMLHelper::_('tabs.panel', Text::_('PANEL_1_TITLE'), 'panel_1_id');
-//echo 'Panel 1 content can go here.';
-echo $this->loadTemplate('details');
- 
-echo HTMLHelper::_('tabs.panel', Text::_('PANEL_2_TITLE'), 'panel_2_id');
-//echo 'Panel 2 content can go here.';
-echo $this->loadTemplate('picture');
-
-echo HTMLHelper::_('tabs.panel', Text::_('PANEL_3_TITLE'), 'panel_3_id');
-//echo 'Panel 3 content can go here.';
-//echo $this->loadTemplate('extended');
- 
-echo HTMLHelper::_('tabs.end');
-*/
-
 
 ?>
 <div class="clr"></div>
