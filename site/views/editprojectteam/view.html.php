99b8901dbcc6a31b3b812ea753d03e15adb7b1c8
diddipoeler <famploeger@arcor.de>
2018-09-12 09:24:51 +0200

    Add files via upload
    Edit Frontend

--- site/views/editprojectteam/view.html.php
+++ site/views/editprojectteam/view.html.php
@@ -1,1 +1,45 @@
+<?php
+/** SportsManagement ein Programm zur Verwaltung für Sportarten
+ * @version   1.0.05
+ * @file      view.html.php
+ * @author    diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license   This file is part of SportsManagement.
+ * @package   sportsmanagement
+ * @subpackage editclub
+ */
+
+// Check to ensure this file is included in Joomla!
+defined('_JEXEC') or die('Restricted access');
+use Joomla\CMS\Factory;
+use Joomla\CMS\Component\ComponentHelper;
+
+
+class sportsmanagementViewEditprojectteam extends sportsmanagementView
+{
+
+	
+	/**
+	 * sportsmanagementViewEditClub::init()
+	 * 
+	 * @return void
+	 */
+	function init()
+	{
+	
+$this->item = $this->model->getData();
+		$lists = array();
+
+		$this->form = $this->get('Form');	
+		$extended = sportsmanagementHelper::getExtended($this->item->extended, 'projectteam');
+		$this->extended = $extended;
+        $this->lists = $lists;
+
+        $this->cfg_which_media_tool = ComponentHelper::getParams($this->option)->get('cfg_which_media_tool',0);
+	
+	}
+
+	
+}
+?>
 
---
+++ site/views/editteam/tmpl/default.php
@@ -0,0 +1,88 @@
+<?php 
+/** SportsManagement ein Programm zur Verwaltung für Sportarten
+ * @version   1.0.05
+ * @file      default.php
+ * @author    diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license   This file is part of SportsManagement.
+ * @package   sportsmanagement
+ * @subpackage editperson
+ */
+
+defined('_JEXEC') or die('Restricted access');
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Router\Route;
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Factory;
+
+// Load the tooltip behavior.
+HTMLHelper::_('behavior.tooltip');
+HTMLHelper::_('behavior.formvalidation');
+
+// Get the form fieldsets.
+$fieldsets = $this->form->getFieldsets();
+
+//echo ' person<br><pre>'.print_r($this->item,true).'</pre>';
+
+?>
+<script type="text/javascript">
+	Joomla.submitbutton = function(task)
+	{
+		if (document.formvalidator.isValid(document.id('editperson'))) {
+			Joomla.submitform(task, document.getElementById('editperson'));
+		}
+	}
+</script>
+<form name="adminForm" id="adminForm" method="post" action="<?php echo $this->uri->toString(); ?>">
+<?php
+
+		?>
+	<fieldset class="adminform">
+	<div class="fltrt">
+					<button type="button" onclick="Joomla.submitform('editperson.apply', this.form);">
+						<?php echo Text::_('COM_SPORTSMANAGEMENT_GLOBAL_SAVE');?></button>
+					<button type="button" onclick="Joomla.submitform('editperson.save', this.form);">
+						<?php echo Text::_('COM_SPORTSMANAGEMENT_GLOBAL_SAVECLOSE');?></button>
+					<button type="button" onclick="Joomla.submitform('editperson.cancel', this.form);">
+						<?php echo Text::_('JCANCEL');?></button>
+				</div>
+	<legend>
+  <?php 
+  echo Text::sprintf('COM_SPORTSMANAGEMENT_PERSON_LEGEND_DESC','<i>'.$this->item->firstname.'</i>','<i>'.$this->item->lastname.'</i>');
+  ?>
+  </legend>
+  </fieldset>
+ 
+<?php 
+echo HTMLHelper::_('bootstrap.startTabSet', 'myTab', array('active' => 'details'));    
+foreach ($fieldsets as $fieldset) :
+
+switch ( $fieldset->name )
+{
+case 'details':
+//case 'picture':
+//case 'extended':
+echo HTMLHelper::_('bootstrap.addTab', 'myTab', $fieldset->name, Text::_($fieldset->label, true));
+echo $this->loadTemplate($fieldset->name);
+echo HTMLHelper::_('bootstrap.endTab');
+break;    
+}
+
+endforeach; 
+
+echo HTMLHelper::_('bootstrap.endTabSet');
+?>	
+	
+	
+<div class="clr"></div>
+<input type="hidden" name="assignperson" value="0" id="assignperson" />
+<input type="hidden" name="option" value="com_sportsmanagement" /> 
+<input type="hidden" name="id" value="<?php echo $this->item->id; ?>" /> 
+<input type="hidden" name="pid" value="<?php echo $this->item->id; ?>" /> 	
+	
+<input type="hidden" name="p" value="<?php echo Factory::getApplication()->input->getInt('p',0); ?>" /> 
+<input type="hidden" name="tid" value="<?php echo Factory::getApplication()->input->getInt('tid',0); ?>" /> 
+	
+<input type="hidden" name="task" value="" />
+<?php echo HTMLHelper::_('form.token')."\n"; ?>
+</form>
