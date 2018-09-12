db7abb45908e3a22b5683f879ddc8b1bbbe70613
diddipoeler <famploeger@arcor.de>
2018-09-12 13:50:10 +0200

    Update default_ranking_first.php
    #791

--- site/views/ranking/tmpl/default_ranking_first.php
+++ site/views/ranking/tmpl/default_ranking_first.php
@@ -21,6 +21,7 @@ defined('_JEXEC') or die('Restricted access');
 foreach ($this->firstRank as $division => $cu_rk) {
     if ($division) {
         ?>
+<div class="<?php echo $this->divclassrow;?> table-responsive">
         <table class="<?PHP echo $this->config['table_class']; ?>">
             <tr>
                 <td class="contentheading">
@@ -34,7 +35,8 @@ foreach ($this->firstRank as $division => $cu_rk) {
                 </td>
             </tr>
         </table>
-        <div class="table-responsive">
+</div>
+        <div class="<?php echo $this->divclassrow;?> table-responsive">
             <table class="<?PHP echo $this->config['table_class']; ?>">
         <?php
         foreach ($cu_rk as $ptid => $team) {
