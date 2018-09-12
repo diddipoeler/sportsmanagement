0fd372a7e2e88e8f1d07a0820eeebe168236daa5
diddipoeler <famploeger@arcor.de>
2018-09-12 08:39:25 +0200

    Update default.php
    #791

--- site/views/matchreport/tmpl/default.php
+++ site/views/matchreport/tmpl/default.php
@@ -71,7 +71,7 @@ if (!empty($this->matchplayerpositions ))
 
 ?>
 
-<div class="container">
+<div class="<?php echo $this->divclasscontainer;?>">
 
 <?php
 
@@ -79,113 +79,109 @@ if ( COM_SPORTSMANAGEMENT_SHOW_DEBUG_INFO )
 {
 echo $this->loadTemplate('debug');
 }
-	echo $this->loadTemplate('projectheading');
+echo $this->loadTemplate('projectheading');
 
-	if ( $this->config['show_sectionheader'] )
-	{
-		echo $this->loadTemplate('sectionheader');
-	}
+if ( $this->config['show_sectionheader'] )
+{
+echo $this->loadTemplate('sectionheader');
+}
 
-	if ( $this->config['show_result'] )
-	{
-		echo $this->loadTemplate('result');
-	}
+if ( $this->config['show_result'] )
+{
+echo $this->loadTemplate('result');
+}
     
-    if ( !empty( $this->matchevents ) )
-	{
+if ( !empty( $this->matchevents ) )
+{
 	      
-    }
+}
     
-  // ################################################################
-  // diddipoeler
-  // aufbau der templates
-  $output = array();
-  if ( $this->config['show_details'] )
-	{
-		$output['COM_SPORTSMANAGEMENT_MATCHREPORT_DETAILS'] = 'details';
-	}
-  if ( $this->config['show_extended'] && $this->extended )
-	{
-        $output['COM_SPORTSMANAGEMENT_TABS_EXTENDED'] = 'extended';
-	}
-	if ( $this->config['show_roster'] )
-	{
-        if ( $this->matchplayerpositions )
-        {
-        $output['COM_SPORTSMANAGEMENT_MATCHREPORT_STARTING_LINE_UP_PLAYER'] = $this->config['show_roster_card'];
-        }
-        if ( $this->matchstaffpositions )
-        {
-        $output['COM_SPORTSMANAGEMENT_MATCHREPORT_STARTING_LINE_UP_STAFF'] = 'staff';
-        }
-        if ( $this->matchplayerpositions )
-        {
-        $output['COM_SPORTSMANAGEMENT_MATCHREPORT_SUBSTITUTES'] = 'subst';
-        }
-	}
-    if ( $this->config['show_roster_playground'] )
-	{
-        $output['COM_SPORTSMANAGEMENT_MATCHREPORT_STARTING_PLAYGROUND'] = 'rosterplayground';
-	}
-    if ( !empty( $this->matchevents ) )
-	{
+// ################################################################
+// diddipoeler
+// aufbau der templates
+$output = array();
+if ( $this->config['show_details'] )
+{
+$output['COM_SPORTSMANAGEMENT_MATCHREPORT_DETAILS'] = 'details';
+}
+if ( $this->config['show_extended'] && $this->extended )
+{
+$output['COM_SPORTSMANAGEMENT_TABS_EXTENDED'] = 'extended';
+}
+if ( $this->config['show_roster'] )
+{
+if ( $this->matchplayerpositions )
+{
+$output['COM_SPORTSMANAGEMENT_MATCHREPORT_STARTING_LINE_UP_PLAYER'] = $this->config['show_roster_card'];
+}
+if ( $this->matchstaffpositions )
+{
+$output['COM_SPORTSMANAGEMENT_MATCHREPORT_STARTING_LINE_UP_STAFF'] = 'staff';
+}
+if ( $this->matchplayerpositions )
+{
+$output['COM_SPORTSMANAGEMENT_MATCHREPORT_SUBSTITUTES'] = 'subst';
+}
+}
+if ( $this->config['show_roster_playground'] )
+{
+$output['COM_SPORTSMANAGEMENT_MATCHREPORT_STARTING_PLAYGROUND'] = 'rosterplayground';
+}
+if ( !empty( $this->matchevents ) )
+{
 		
-        if ( $this->config['show_events'] )
-		{
-			switch ($this->config['use_tabs_events'])
-			{
-				case 0:
-					// No tabs 
-					if ( !empty( $this->eventtypes ) ) 
-                    {
-						$output['COM_SPORTSMANAGEMENT_MATCHREPORT_EVENTS'] = 'events';
-					}
-					break;
-				case 1:
-					// Tabs
-					if ( !empty( $this->eventtypes ) ) 
-                    {
-						//echo $this->loadTemplate('events_tabs');
-                        $output['COM_SPORTSMANAGEMENT_MATCHREPORT_EVENTS'] = 'events_tabs';
-					}
-					break;
-				case 2:
-					// Table/Ticker layout 
-                    $output['COM_SPORTSMANAGEMENT_MATCHREPORT_EVENTS'] = 'events_ticker';
-					break;
-			}
-                    
-                    
-                    
-                    
-        }
+if ( $this->config['show_events'] )
+{
+switch ($this->config['use_tabs_events'])
+{
+case 0:
+// No tabs 
+if ( !empty( $this->eventtypes ) ) 
+{
+$output['COM_SPORTSMANAGEMENT_MATCHREPORT_EVENTS'] = 'events';
+}
+break;
+case 1:
+// Tabs
+if ( !empty( $this->eventtypes ) ) 
+{
+//echo $this->loadTemplate('events_tabs');
+$output['COM_SPORTSMANAGEMENT_MATCHREPORT_EVENTS'] = 'events_tabs';
+}
+break;
+case 2:
+// Table/Ticker layout 
+$output['COM_SPORTSMANAGEMENT_MATCHREPORT_EVENTS'] = 'events_ticker';
+break;
+}
+}
         
-    }    
-    if ( $this->config['show_stats'] && ( $hasMatchPlayerStats || $hasMatchStaffStats ) )
-	{
-        $output['COM_SPORTSMANAGEMENT_MATCHREPORT_STATISTICS'] = 'stats';
-	}
+}    
+if ( $this->config['show_stats'] && ( $hasMatchPlayerStats || $hasMatchStaffStats ) )
+{
+$output['COM_SPORTSMANAGEMENT_MATCHREPORT_STATISTICS'] = 'stats';
+}
 
-	if ( $this->config['show_summary'] && $this->match->summary )
-	{
-        $output['COM_SPORTSMANAGEMENT_MATCHREPORT_MATCH_SUMMARY'] = 'summary';
-	}
+if ( $this->config['show_summary'] && $this->match->summary )
+{
+$output['COM_SPORTSMANAGEMENT_MATCHREPORT_MATCH_SUMMARY'] = 'summary';
+}
     
-    if ( $this->config['show_article'] )
-	{
-        $output['COM_SPORTSMANAGEMENT_MATCHREPORT_MATCH_ARTICLE'] = 'article';
-	}
+if ( $this->config['show_article'] )
+{
+$output['COM_SPORTSMANAGEMENT_MATCHREPORT_MATCH_ARTICLE'] = 'article';
+}
     
-    if ( $this->config['show_commentary'] && $this->matchcommentary )
-	{
-        $output['COM_SPORTSMANAGEMENT_MATCHREPORT_MATCH_COMMENTARY'] = 'commentary';
-	}
+if ( $this->config['show_commentary'] && $this->matchcommentary )
+{
+$output['COM_SPORTSMANAGEMENT_MATCHREPORT_MATCH_COMMENTARY'] = 'commentary';
+}
 	
-	if ( $this->config['show_pictures'] && isset($this->matchimages) )
-	{
-        $output['COM_SPORTSMANAGEMENT_MATCHREPORT_MATCH_PICTURES'] = 'pictures';
-	} 
-    break;
+if ( $this->config['show_pictures'] && isset($this->matchimages) )
+{
+$output['COM_SPORTSMANAGEMENT_MATCHREPORT_MATCH_PICTURES'] = 'pictures';
+} 
+break;
     
 }       
 
