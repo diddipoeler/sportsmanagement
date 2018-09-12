e51e9160480d6ce6b93a5db88d2d8e711e7d9c61
diddipoeler <famploeger@arcor.de>
2018-09-12 09:19:19 +0200

    Add files via upload
    Edit Frontend

--- site/models/editprojectteam.php
+++ site/models/editprojectteam.php
@@ -1,1 +1,127 @@
+<?php
+/** SportsManagement ein Programm zur Verwaltung für Sportarten
+ * @version   1.0.05
+ * @file      editperson.php
+ * @author    diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license   This file is part of SportsManagement.
+ * @package   sportsmanagement
+ * @subpackage editperson
+ */
+
+// Check to ensure this file is included in Joomla!
+defined('_JEXEC') or die('Restricted access');
+use Joomla\CMS\Factory;
+use Joomla\CMS\Component\ComponentHelper;
+use Joomla\CMS\MVC\Model\AdminModel;
+use Joomla\CMS\Table\Table;
+
+require_once(JPATH_ROOT.DS.'components'.DS.'com_sportsmanagement'.DS. 'helpers' . DS . 'imageselect.php');
+
+
+
+class sportsmanagementModelEditprojectteam extends AdminModel
+{
+  
+  /* interfaces */
+	var $latitude	= null;
+	var $longitude	= null;
+	
+	
+    
+    function updItem($data)
+    {
+        $app = Factory::getApplication();
+        
+        foreach( $data['request'] as $key => $value)
+        {
+            $data[$key] = $value;
+        }
+        
+        // Specify which columns are to be ignored. This can be a string or an array.
+        //$ignore = 'id';
+        $ignore = '';
+        // Get the table object from the model.
+        $table = $this->getTable( 'projectteam' );
+        // Bind the array to the table object.
+        $table->bind( $data, $ignore );
+        
+        if ( !$table->store() )
+        {
+            JError::raiseError(500, $db->getErrorMsg());
+        }
+        else
+        {
+            
+        }
+
+    }
+    
+    
+    
+	function getData()
+	{
+	   $this->_id = Factory::getApplication()->input->getInt('pid',0);
+
+    $this->_data = $this->getTable( 'projectteam', 'sportsmanagementTable' );
+			$this->_data->load( $this->_id );
+			return $this->_data;
+	}
+
+
+	/**
+	 * Returns a Table object, always creating it
+	 *
+	 * @param	type	The table type to instantiate
+	 * @param	string	A prefix for the table class name. Optional.
+	 * @param	array	Configuration array for model. Optional.
+	 * @return	JTable	A database object
+	 * @since	1.6
+	 */
+	public function getTable($type = 'projectteam', $prefix = 'sportsmanagementTable', $config = array())
+	{
+		return Table::getInstance($type, $prefix, $config);
+	}
+	
+	/**
+	 * Method to get the record form.
+	 *
+	 * @param	array	$data		Data for the form.
+	 * @param	boolean	$loadData	True if the form is to load its own data (default case), false if not.
+	 * @return	mixed	A JForm object on success, false on failure
+	 * @since	1.7
+	 */
+	public function getForm($data = array(), $loadData = true)
+	{
+		$cfg_which_media_tool = ComponentHelper::getParams(Factory::getApplication()->input->getCmd('option'))->get('cfg_which_media_tool',0);
+        $app = Factory::getApplication('site');
+        // Get the form.
+		$form = $this->loadForm('com_sportsmanagement.'.$this->name, $this->name,array('load_data' => $loadData) );
+		if (empty($form))
+		{
+			return false;
+		}
+       
+		return $form;
+	}
+	
+	/**
+	 * Method to get the data that should be injected in the form.
+	 *
+	 * @return	mixed	The data for the form.
+	 * @since	1.7
+	 */
+	protected function loadFormData()
+	{
+		// Check the session for previously entered form data.
+		$data = Factory::getApplication()->getUserState('com_sportsmanagement.edit.'.$this->name.'.data', array());
+		if (empty($data))
+		{
+			$data = $this->getData();
+		}
+		return $data;
+	}
+    
+}
+
 
