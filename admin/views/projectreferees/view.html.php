5e00ac4085030e41b2c94c30387d0f790d0d4ba7
diddipoeler <famploeger@arcor.de>
2018-09-17 07:53:58 +0200

    Update view.html.php
    #796

--- admin/views/projectreferees/view.html.php
+++ admin/views/projectreferees/view.html.php
@@ -94,7 +94,7 @@ class sportsmanagementViewprojectreferees extends sportsmanagementView {
         $this->title = Text::_('COM_SPORTSMANAGEMENT_ADMIN_PREF_TITLE');
 
         JToolbarHelper::apply('projectreferees.saveshort', Text::_('COM_SPORTSMANAGEMENT_ADMIN_PREF_APPLY'));
-        sportsmanagementHelper::ToolbarButton('assignplayers', 'upload', Text::_('COM_SPORTSMANAGEMENT_ADMIN_PREF_ASSIGN'), 'persons', 3);
+        sportsmanagementHelper::ToolbarButton('assignpersons', 'upload', Text::_('COM_SPORTSMANAGEMENT_ADMIN_PREF_ASSIGN'), 'persons', 3);
         JToolbarHelper::deleteList('', 'projectreferees.delete');
         JToolbarHelper::checkin('projectreferees.checkin');
         parent::addToolbar();
