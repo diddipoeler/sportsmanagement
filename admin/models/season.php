a45722e3e6c121f2c2248f81fdb3377feae3c2ac
diddipoeler <famploeger@arcor.de>
2018-09-18 11:09:39 +0200

    Update season.php
    #796

--- admin/models/season.php
+++ admin/models/season.php
@@ -50,11 +50,11 @@ class sportsmanagementModelseason extends JSMModelAdmin
     function saveshortpersons()
     {
         // Reference global application object
-        $app = JFactory::getApplication();
+        //$app = JFactory::getApplication();
         // JInput object
-        $jinput = $app->input;
-        $option = $jinput->getCmd('option');
-        $db = sportsmanagementHelper::getDBConnection();
+        //$jinput = $app->input;
+        //$option = $jinput->getCmd('option');
+        //$db = sportsmanagementHelper::getDBConnection();
         
         //$date = JFactory::getDate();
 //	   $user = JFactory::getUser();
@@ -63,10 +63,10 @@ class sportsmanagementModelseason extends JSMModelAdmin
        
         //$post = JFactory::getApplication()->input->post->getArray(array());
         //$post = $jinput->post;
-        $pks = $jinput->getVar('cid', null, 'post', 'array');
-        $teams = $jinput->getVar('team_id', null, 'post', 'array');
-        $season_id = $jinput->getVar('season_id', 0, 'post', 'array');
-        $persontype = $jinput->getVar('persontype', 0, 'post', 'array');
+        $pks = $this->jsmjinput->getVar('cid', null, 'post', 'array');
+        $teams = $this->jsmjinput->getVar('team_id', null, 'post', 'array');
+        $season_id = $this->jsmjinput->getVar('season_id', 0, 'post', 'array');
+        $persontype = $this->jsmjinput->getVar('persontype', 0, 'post', 'array');
         
         //$app->enqueueMessage(__METHOD__.' '.__LINE__.' pks<br><pre>'.print_r($pks, true).'</pre><br>','');
         //$app->enqueueMessage(__METHOD__.' '.__LINE__.' teams<br><pre>'.print_r($teams, true).'</pre><br>','');
@@ -74,46 +74,47 @@ class sportsmanagementModelseason extends JSMModelAdmin
         foreach ( $pks as $key => $value )
         {
         // Create a new query object.
-        $query = $db->getQuery(true);
+        //$query = $db->getQuery(true);
+	$this->jsmquery->clear();	
         // Insert columns.
         $columns = array('person_id','season_id','modified','modified_by');
         // Insert values.
         $values = array($value,$season_id,$db->Quote(''.$modified.''),$modified_by);
         // Prepare the insert query.
-        $query
-            ->insert($db->quoteName('#__sportsmanagement_season_person_id'))
-            ->columns($db->quoteName($columns))
+        $this->jsmquery
+            ->insert($this->jsmdb->quoteName('#__sportsmanagement_season_person_id'))
+            ->columns($this->jsmdb->quoteName($columns))
             ->values(implode(',', $values));
             try{
         // Set the query using our newly populated query object and execute it.
-        $db->setQuery($query);
-	$db->execute();
+        $this->jsmdb->setQuery($this->jsmquery);
+	$this->jsmdb->execute();
         }
 catch (Exception $e) {
-$app->enqueueMessage(__METHOD__.' '.__LINE__.' '. Text::_($e->getMessage()),'Error');
-$app->enqueueMessage(__METHOD__.' '.__LINE__.' '. Text::_($e->getCode()),'Error');  
+$this->jsmapp->enqueueMessage(__METHOD__.' '.__LINE__.' '. Text::_($e->getMessage()),'Error');
+$this->jsmapp->enqueueMessage(__METHOD__.' '.__LINE__.' '. Text::_($e->getCode()),'Error');  
 }
 
         if ( isset($teams) && $persontype != 3 )
         {
-        $query->clear();
+        $this->jsmquery->clear();
         // Insert columns.
         $columns = array('person_id','season_id','team_id','published','persontype','modified','modified_by'   );
         // Insert values.
         $values = array($value,$season_id,$teams,'1',$persontype,$db->Quote(''.$modified.''),$modified_by);
         // Prepare the insert query.
-        $query
-            ->insert($db->quoteName('#__sportsmanagement_season_team_person_id'))
-            ->columns($db->quoteName($columns))
+        $this->jsmquery
+            ->insert($this->jsmdb->quoteName('#__sportsmanagement_season_team_person_id'))
+            ->columns($this->jsmdb->quoteName($columns))
             ->values(implode(',', $values));
             try{
         // Set the query using our newly populated query object and execute it.
-        $db->setQuery($query);
-$db->execute();
+        $this->jsmdb->setQuery($this->jsmquery);
+$this->jsmdb->execute();
         }
 catch (Exception $e) {
-$app->enqueueMessage(__METHOD__.' '.__LINE__.' '. Text::_($e->getMessage()),'Error');
-$app->enqueueMessage(__METHOD__.' '.__LINE__.' '. Text::_($e->getCode()),'Error');  
+$this->jsmapp->enqueueMessage(__METHOD__.' '.__LINE__.' '. Text::_($e->getMessage()),'Error');
+$this->jsmapp->enqueueMessage(__METHOD__.' '.__LINE__.' '. Text::_($e->getCode()),'Error');  
 }
 		
         
