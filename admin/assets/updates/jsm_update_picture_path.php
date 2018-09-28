20c402d052bc46285481761c76e51f68a082c6b3
diddipoeler <famploeger@arcor.de>
2018-09-28 15:26:04 +0200

    Code

---
+++ admin/assets/updates/jsm_update_picture_path.php
@@ -0,0 +1,17 @@
+<?php
+/** SportsManagement ein Programm zur Verwaltung für Sportarten
+ * @version   1.0.05
+ * @file      jsm_update_picture_path.php
+ * @author    diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license   This file is part of SportsManagement.
+ * @package   sportsmanagement
+ * @subpackage updates
+ */
+ 
+// Check to ensure this file is included in Joomla!
+defined('_JEXEC') or die('Restricted access');
+
+
+
+?>
