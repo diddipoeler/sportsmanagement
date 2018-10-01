7d0cc7d06d2a7353591c12f5d5f4d085608a50ab
diddipoeler <famploeger@arcor.de>
2018-10-01 14:15:10 +0200

    Update default.php
    Code

--- site/views/results/tmpl/default.php
+++ site/views/results/tmpl/default.php
@@ -33,7 +33,7 @@ if ( $this->config['show_matchday_pagenav'] == 2 || $this->config['show_matchday
 {
 echo $this->loadTemplate('pagnav');
 }
-
+//echo $this->loadTemplate('pagination');
 echo $this->loadTemplate('results');
 
 if ( $this->config['show_matchday_pagenav'] == 1 || $this->config['show_matchday_pagenav'] == 3 )
