39b3e810ac16128dc4fe5e9ac969835a200b1c2f
diddipoeler <famploeger@arcor.de>
2018-10-01 11:31:22 +0200

    Create default_pagination.php
    Code

---
+++ site/views/results/tmpl/default_pagination.php
@@ -0,0 +1,13 @@
+<?php 
+/** SportsManagement ein Programm zur Verwaltung für alle Sportarten
+ * @version   1.0.05
+ * @file      default_pagination.php
+ * @author    diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license   This file is part of SportsManagement.
+ * @subpackage results
+ */
+defined('_JEXEC') or die('Restricted access');
+
+
+?>
