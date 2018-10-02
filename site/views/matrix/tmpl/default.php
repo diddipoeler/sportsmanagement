bdc5b1ffc6401b8684ed4ba5583787db9cf881c3
diddipoeler <famploeger@arcor.de>
2018-10-02 09:28:55 +0200

    Update default.php
    Code

--- site/views/matrix/tmpl/default.php
+++ site/views/matrix/tmpl/default.php
@@ -18,55 +18,52 @@ defined( '_JEXEC' ) or die( 'Restricted access' );
 $templatesToLoad = array('globalviews');
 sportsmanagementHelper::addTemplatePaths($templatesToLoad, $this);
 ?>
-<div class="container" id="matrix" >
-
-	<?php 
-    if ( COM_SPORTSMANAGEMENT_SHOW_DEBUG_INFO )
+<div class="<?php echo $this->divclasscontainer;?>" id="matrix" >
+<?php 
+if ( COM_SPORTSMANAGEMENT_SHOW_DEBUG_INFO )
 {
-    echo $this->loadTemplate('debug');
+echo $this->loadTemplate('debug');
 }
-	echo $this->loadTemplate('projectheading');
+echo $this->loadTemplate('projectheading');
 
-	if (($this->config['show_matrix'])==1)
-	{
+if ( $this->config['show_matrix'] )
+{
 	
-  if (($this->config['show_sectionheader'])==1)
-	{ 
-		echo $this->loadTemplate('sectionheader');
-	}
+if ( $this->config['show_sectionheader'] )
+{ 
+echo $this->loadTemplate('sectionheader');
+}
   
-  if(isset($this->divisions) && count($this->divisions) > 1) 
-  {
-  echo $this->loadTemplate('matrix_division').'<br />';
-  }
-  else 
-  {
-    if ( isset($this->config['show_matrix_russia']) )
-    {
- 	if (($this->config['show_matrix_russia'])==1)
-	{
-	echo $this->loadTemplate('matrix_russia');
-    }   
-    else
-    {
-     echo $this->loadTemplate('matrix');
-     }
-     }
-     else
-     {
-        echo $this->loadTemplate('matrix');
-     }
-  }
+if(isset($this->divisions) && count($this->divisions) > 1) 
+{
+echo $this->loadTemplate('matrix_division').'<br />';
+}
+else 
+{
+if ( isset($this->config['show_matrix_russia']) )
+{
+if ( $this->config['show_matrix_russia'] )
+{
+echo $this->loadTemplate('matrix_russia');
+}   
+else
+{
+echo $this->loadTemplate('matrix');
+}
+}
+else
+{
+echo $this->loadTemplate('matrix');
+}
+}
 	
-	
-	}
-    
+}
 
-	if ($this->config['show_help']==1)
-	{
-		echo $this->loadTemplate('hint');
-	}
+if ($this->config['show_help']==1)
+{
+echo $this->loadTemplate('hint');
+}
 
-	echo $this->loadTemplate('jsminfo');
-	?>
+echo $this->loadTemplate('jsminfo');
+?>
 </div>
