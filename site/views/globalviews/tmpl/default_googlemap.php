c9f07483e51ae1ca1e351d39a450251cb2f59e70
diddipoeler <famploeger@arcor.de>
2018-10-09 15:56:47 +0200

    Update default_googlemap.php
    #821

--- site/views/globalviews/tmpl/default_googlemap.php
+++ site/views/globalviews/tmpl/default_googlemap.php
@@ -47,7 +47,7 @@ case 'clubinfo':
          mapLink =
              '<a href="http://openstreetmap.org">OpenStreetMap</a>';
          L.tileLayer(
-             'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+             'https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
              attribution: '&copy; ' + mapLink + ' Contributors',
              maxZoom: 18,
              }).addTo(map);
