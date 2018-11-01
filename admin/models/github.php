370fb19789180d51dd6d37e092b599d6e91e4069
sigal <stony@sport4u.at>
2018-11-01 19:28:22 +0100

    Update github.php

--- admin/models/github.php
+++ admin/models/github.php
@@ -1,41 +1,13 @@
 <?php
-/** Joomla Sports Management ein Programm zur Verwaltung für alle Sportarten
-* @version 1.0.26
-* @file		components/sportsmanagement/models/cpanel.php
-* @author diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
-* @copyright Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
-* @license This file is part of Joomla Sports Management.
-*
-* Joomla Sports Management is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* Joomla Sports Management is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with Joomla Sports Management. If not, see <http://www.gnu.org/licenses/>.
-*
-* Diese Datei ist Teil von Joomla Sports Management.
-*
-* Joomla Sports Management ist Freie Software: Sie können es unter den Bedingungen
-* der GNU General Public License, wie von der Free Software Foundation,
-* Version 3 der Lizenz oder (nach Ihrer Wahl) jeder späteren
-* veröffentlichten Version, weiterverbreiten und/oder modifizieren.
-*
-* Joomla Sports Management wird in der Hoffnung, dass es nützlich sein wird, aber
-* OHNE JEDE GEWÄHRLEISTUNG, bereitgestellt; sogar ohne die implizite
-* Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
-* Siehe die GNU General Public License für weitere Details.
-*
-* Sie sollten eine Kopie der GNU General Public License zusammen mit diesem
-* Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
-*
-* Note : All ini files need to be saved as UTF-8 without BOM
-*/
+/** SportsManagement ein Programm zur Verwaltung für Sportarten
+ * @version   1.0.05
+ * @file      github.php
+ * @author    diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license   This file is part of SportsManagement.
+ * @package   sportsmanagement
+ * @subpackage models
+ */
 
 /**
  * github icons
@@ -47,6 +19,7 @@ defined('_JEXEC') or die('Restricted access');
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\Factory;
 use Joomla\CMS\Component\ComponentHelper;
+use Joomla\Registry\Registry;
  
 // import Joomla  library
 //jimport('joomla.github.issues');
@@ -186,7 +159,7 @@ return false;
     {
     $github_user = ComponentHelper::getParams($this->option)->get('cfg_github_username','');
         $github_repo = ComponentHelper::getParams($this->option)->get('cfg_github_repository','');    
-    $gh_options = new JRegistry();
+    $gh_options = new Registry();
 // If an API token is set in the params, use it for authentication 
  		if ( $this->post['gh_token'] ) 
  		{ 
@@ -231,7 +204,7 @@ return $github->issues->create($github_user, $github_repo, $this->post['title'],
         $params = \ComponentHelper::getParams($option);
 
 
-$gh_options = new JRegistry();
+$gh_options = new Registry();
 // If an API token is set in the params, use it for authentication 
  		if ($params->get('gh_token', '')) 
  		{ 
