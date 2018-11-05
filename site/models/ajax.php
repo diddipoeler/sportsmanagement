17722720f4acb40f95ec79bdadb4cffc1b628e0b
diddipoeler <famploeger@arcor.de>
2018-11-05 13:54:39 +0100

    Update ajax.php
    Code

--- site/models/ajax.php
+++ site/models/ajax.php
@@ -14,6 +14,7 @@ defined('_JEXEC') or die('Restricted access');
 use Joomla\CMS\Factory;
 use Joomla\CMS\MVC\Model\BaseDatabaseModel;
 use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Language\Text;
 
 /**
  * sportsmanagementModelAjax
