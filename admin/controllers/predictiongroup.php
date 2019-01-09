a591941267190e894183654d1f1fb1409d89e174
sigal <stony@sport4u.at>
2019-01-09 22:11:34 +0100

    Code

--- admin/controllers/predictiongroup.php
+++ admin/controllers/predictiongroup.php
@@ -12,7 +12,7 @@
 // Check to ensure this file is included in Joomla!
 defined('_JEXEC') or die('Restricted access');
 // import Joomla controllerform library
-jimport('joomla.application.component.controllerform');
+//jimport('joomla.application.component.controllerform');
  
 
 /**
--- admin/controllers/sportstype.php
+++ admin/controllers/sportstype.php
@@ -13,7 +13,7 @@
 defined('_JEXEC') or die('Restricted access');
  
 // import Joomla controllerform library
-jimport('joomla.application.component.controllerform');
+//jimport('joomla.application.component.controllerform');
 
 /**
  * sportsmanagementControllersportstype
