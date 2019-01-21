eb2ca85f1b77c19fa1649d0adeade4e9c620c22a
diddipoeler <famploeger@arcor.de>
2019-01-21 13:04:31 +0100

    Update default.php
    Code

--- site/views/results/tmpl/default.php
+++ site/views/results/tmpl/default.php
@@ -9,13 +9,19 @@
  */
 
 defined('_JEXEC') or die('Restricted access');
+use Joomla\CMS\Component\ComponentHelper;
+if ( ComponentHelper::getParams('com_sportsmanagement')->get('show_debug_info_frontend') )
+{
+echo __METHOD__.' '.__LINE__.' divclasscontainer<br><pre>'.print_r($this->divclasscontainer,true).'</pre>'; 
+echo __METHOD__.' '.__LINE__.' divclassrow<br><pre>'.print_r($this->divclassrow,true).'</pre>'; 	
+}
 
 // Make sure that in case extensions are written for mentioned (common) views,
 // that they are loaded i.s.o. of the template of this view
 $templatesToLoad = array('globalviews');
 sportsmanagementHelper::addTemplatePaths($templatesToLoad, $this);
 ?>
-<div class="<?php echo $this->divclasscontainer;?>">
+<div class="<?php echo $this->divclasscontainer;?>" id="results">
 <?php
     
 if ( COM_SPORTSMANAGEMENT_SHOW_DEBUG_INFO )
