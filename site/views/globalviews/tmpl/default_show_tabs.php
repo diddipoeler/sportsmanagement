0033a1738b7bede0ac100b58056262e49c47adc0
diddipoeler <famploeger@arcor.de>
2019-01-21 14:25:45 +0100

    Update default_show_tabs.php
    Code

--- site/views/globalviews/tmpl/default_show_tabs.php
+++ site/views/globalviews/tmpl/default_show_tabs.php
@@ -72,7 +72,42 @@ echo HTMLHelper::_('bootstrap.addTab', 'myTab4', $text, Text::_($text));
 <?PHP
 if ( $this->params->get('show_allranking', 0) && $this->view == 'resultsranking' && $template == 'ranking' ) 
 {
-   
+/**
+ * sollen überhaupt reiter angezeigt werden ?
+ */
+if ($this->config['show_table_1'] ||
+        $this->config['show_table_2'] ||
+        $this->config['show_table_3'] ||
+        $this->config['show_table_4'] ||
+        $this->config['show_table_5']) {
+echo HTMLHelper::_('bootstrap.startTabSet', 'defaulttabsranking', array('active' => 'show_table_1' )); //start tab set    
+if ($this->config['show_table_1']) {
+echo HTMLHelper::_('bootstrap.addTab', 'defaulttabsranking', 'show_table_1', Text::_($this->config['table_text_1']));
+echo $this->loadTemplate('ranking');  
+echo HTMLHelper::_('bootstrap.endTab');  
+}  
+if ($this->config['show_table_2']) {
+echo HTMLHelper::_('bootstrap.addTab', 'defaulttabsranking', 'show_table_2', Text::_($this->config['table_text_2']));
+echo $this->loadTemplate('ranking_home');  
+echo HTMLHelper::_('bootstrap.endTab');   
+} 
+if ($this->config['show_table_3']) {
+echo HTMLHelper::_('bootstrap.addTab', 'defaulttabsranking', 'show_table_3', Text::_($this->config['table_text_3']));
+echo $this->loadTemplate('ranking_away');  
+echo HTMLHelper::_('bootstrap.endTab');   
+} 
+if ($this->config['show_table_4']) {
+echo HTMLHelper::_('bootstrap.addTab', 'defaulttabsranking', 'show_table_4', Text::_($this->config['table_text_4']));
+echo $this->loadTemplate('ranking_first');  
+echo HTMLHelper::_('bootstrap.endTab');   
+} 
+if ($this->config['show_table_5']) {
+echo HTMLHelper::_('bootstrap.addTab', 'defaulttabsranking', 'show_table_5', Text::_($this->config['table_text_5']));
+echo $this->loadTemplate('ranking_second');  
+echo HTMLHelper::_('bootstrap.endTab');   
+}       
+echo HTMLHelper::_('bootstrap.endTabSet'); //end tab set     
+}   
 }   
 else   
 {   
