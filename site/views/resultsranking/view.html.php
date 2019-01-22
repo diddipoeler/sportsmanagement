2d224065958416ce948e9c6ff86a3dabafcc2a13
diddipoeler <famploeger@arcor.de>
2019-01-22 18:21:53 +0100

    #864

--- site/views/resultsranking/view.html.php
+++ site/views/resultsranking/view.html.php
@@ -185,6 +185,7 @@ $this->currentRanking = $rankingmodel::$currentRanking;
         
       if ( $this->params->get('show_map', 0) )
 	  {
+	  $this->mapconfig = sportsmanagementModelProject::getTemplateConfig('map',$this->jinput->getInt('cfg_which_database',0)); 
 	  $this->geo = new JSMsimpleGMapGeocoder();
 	  $this->geo->genkml3($project->id,$this->allteams);
   
