2d13cc2e62d77a68c960edd78b286d43bcf4f673
diddipoeler <famploeger@arcor.de>
2019-01-31 09:57:02 +0100

    Update imagehandler.php
    Code

--- site/models/imagehandler.php
+++ site/models/imagehandler.php
@@ -1,5 +1,13 @@
 <?php
-
+/** SportsManagement ein Programm zur Verwaltung für alle Sportarten
+ * @version   1.0.05
+ * @file      imagehandler.php
+ * @author    diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license   This file is part of SportsManagement.
+ * @package   sportsmanagement
+ * @subpackage imagehandler
+ */
 
 defined( '_JEXEC' ) or die( 'Restricted access' );
 use Joomla\CMS\Factory;
@@ -230,61 +238,6 @@ class sportsmanagementModelImagehandler extends BaseDatabaseModel
 			}
 		}
 	}
-	
-	/**
-	 * Method to get the record form.
-	 *
-	 * @param	array	$data		Data for the form.
-	 * @param	boolean	$loadData	True if the form is to load its own data (default case), false if not.
-	 * @return	mixed	A JForm object on success, false on failure
-	 * @since	1.7
-	 */
-	/*
-    public function getForm($data = array(), $loadData = true)
-	{
-		// Get the form.
-		$form = $this->loadForm('com_joomleague.'.$this->name, $this->name,
-				array('load_data' => $loadData) );
-		if (empty($form))
-		{
-			return false;
-		}
-		return $form;
-	}
-	*/
-    
-	/**
-	 * Method to get the data that should be injected in the form.
-	 *
-	 * @return	mixed	The data for the form.
-	 * @since	1.7
-	 */
-     /*
-	protected function loadFormData()
-	{
-		// Check the session for previously entered form data.
-		$data = Factory::getApplication()->getUserState('com_joomleague.edit.'.$this->name.'.data', array());
-		if (empty($data))
-		{
-			$data = $this->getData();
-		}
-		return $data;
-	}
-    /*
-	/**
-	 * Returns a Table object, always creating it
-	 *
-	 * @param	type	The table type to instantiate
-	 * @param	string	A prefix for the table class name. Optional.
-	 * @param	array	Configuration array for model. Optional.
-	 * @return	Table	A database object
-	 * @since	1.6
-	 */
-     /*
-	public function getTable($type = 'club', $prefix = 'table', $config = array())
-	{
-		return Table::getInstance($type, $prefix, $config);
-	}
-    */
+
 }
 ?>
