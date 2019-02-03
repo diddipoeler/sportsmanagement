28f7405e0f5694825108c0956e695b492d2c2fbd
diddipoeler <famploeger@arcor.de>
2019-02-03 12:46:30 +0100

    Code

--- admin/views/jlextindividualsportes/view.html.php
+++ admin/views/jlextindividualsportes/view.html.php
@@ -41,9 +41,7 @@ switch ($this->getLayout()) {
             case 'default_4';
             $this->_displayDefault($tpl);
             break;
-            
             }
-		
 
 	}
 
@@ -55,41 +53,41 @@ switch ($this->getLayout()) {
 	 */
 	function _displayDefault($tpl)
 	{
-		$app = Factory::getApplication();
-		$jinput = $app->input;
-		$option = $jinput->getCmd('option');
-        $model = $this->getModel();
-		$uri = Factory::getURI();
+		//$app = Factory::getApplication();
+		//$jinput = $app->input;
+		//$option = $jinput->getCmd('option');
+        //$model = $this->getModel();
+		//$uri = Factory::getURI();
 
 		$this->state = $this->get('State'); 
         $this->sortDirection = $this->state->get('list.direction');
         $this->sortColumn = $this->state->get('list.ordering');
         
-		$cid = $jinput->request->get('cid', null, array() );
+		$cid = $this->jinput->request->get('cid', null, array() );
 		
-        $project_id	= $app->getUserState( "$option.pid", '0' );
-		$match_id = $input->getInt('id', 0);
-        $rid = $input->getInt('rid', 0);
-		$projectteam1_id = $jinput->getInt('team1', 0);
-		$projectteam2_id = $jinput->getInt('team2', 0);
+        $project_id	= $this->app->getUserState( "$this->option.pid", '0' );
+		$match_id = $this->jinput->getInt('id', 0);
+        $rid = $this->jinput->getInt('rid', 0);
+		$projectteam1_id = $this->jinput->getInt('team1', 0);
+		$projectteam2_id = $this->jinput->getInt('team2', 0);
         
         $mdlProject = BaseDatabaseModel::getInstance("Project", "sportsmanagementModel");
 	    $projectws = $mdlProject->getProject($project_id);
         $mdlRound = BaseDatabaseModel::getInstance("Round", "sportsmanagementModel");
 		$roundws = $mdlRound->getRound($rid);
         
-        //$app->enqueueMessage(__FILE__.' '.get_class($this).' '.__FUNCTION__.' projectws<br><pre>'.print_r($projectws, true).'</pre><br>','');
+        //$this->app->enqueueMessage(__FILE__.' '.get_class($this).' '.__FUNCTION__.' projectws<br><pre>'.print_r($projectws, true).'</pre><br>','');
         
-        $model->checkGames($projectws, $match_id, $rid, $projectteam1_id, $projectteam2_id);
+        $this->model->checkGames($projectws, $match_id, $rid, $projectteam1_id, $projectteam2_id);
         
-        //$app->enqueueMessage(Text::_(get_class($this).' '.__FUNCTION__.' ' .  ' match_id<br><pre>'.print_r($match_id,true).'</pre>'),'');
+        //$this->app->enqueueMessage(Text::_(get_class($this).' '.__FUNCTION__.' ' .  ' match_id<br><pre>'.print_r($match_id,true).'</pre>'),'');
         
         $matches = $this->get('Items');
 		$total = $this->get('Total');
 		$pagination = $this->get('Pagination');
         
         $teams[] = HTMLHelper::_('select.option', '0', Text::_('COM_SPORTSMANAGEMENT_GLOBAL_SELECT_TEAM_PLAYER'));
-        if ($projectteams = $model->getPlayer($projectteam1_id, $project_id))
+        if ($projectteams = $this->model->getPlayer($projectteam1_id, $project_id))
 		{
 			$teams = array_merge($teams, $projectteams);
 		}
@@ -99,7 +97,7 @@ switch ($this->getLayout()) {
             
             
          $teams[] = HTMLHelper::_('select.option', '0', Text::_('COM_SPORTSMANAGEMENT_GLOBAL_SELECT_TEAM_PLAYER'));
-         if ($projectteams = $model->getPlayer($projectteam2_id, $project_id))
+         if ($projectteams = $this->model->getPlayer($projectteam2_id, $project_id))
 		{
 			$teams = array_merge($teams, $projectteams);
 		}
@@ -109,14 +107,14 @@ switch ($this->getLayout()) {
         
         if ( COM_SPORTSMANAGEMENT_SHOW_DEBUG_INFO )
         {
-        $app->enqueueMessage(Text::_(get_class($this).' '.__FUNCTION__.' ' .  ' lists<br><pre>'.print_r($lists,true).'</pre>'),'');
+        $this->app->enqueueMessage(Text::_(get_class($this).' '.__FUNCTION__.' ' .  ' lists<br><pre>'.print_r($lists,true).'</pre>'),'');
         }
         
         $this->matches	= $matches;
         $this->pagination	= $pagination;
-        $this->request_url	= $uri->toString();
+        //$this->request_url	= $uri->toString();
         
-        $this->ProjectTeams	= $model->getProjectTeams($project_id);
+        $this->ProjectTeams	= $this->model->getProjectTeams($project_id);
         
         $this->match_id	= $match_id;
         $this->rid	= $rid;
@@ -127,9 +125,9 @@ switch ($this->getLayout()) {
         $this->projectws	= $projectws;
         $this->roundws	= $roundws;
         
-        if ( $result = $model->getPlayer($projectteam1_id, $project_id) )
+        if ( $result = $this->model->getPlayer($projectteam1_id, $project_id) )
         {
-        $this->getHomePlayer = $model->getPlayer($projectteam1_id, $project_id);    
+        $this->getHomePlayer = $this->model->getPlayer($projectteam1_id, $project_id);    
         }
         else
         {
@@ -140,9 +138,9 @@ switch ($this->getLayout()) {
             $this->getHomePlayer = $exportplayer;
         }
         
-        if ( $result = $model->getPlayer($projectteam2_id, $project_id) )
+        if ( $result = $this->model->getPlayer($projectteam2_id, $project_id) )
         {
-        $this->getAwayPlayer	= $model->getPlayer($projectteam2_id, $project_id);    
+        $this->getAwayPlayer = $this->model->getPlayer($projectteam2_id, $project_id);    
         }
         else
         {
