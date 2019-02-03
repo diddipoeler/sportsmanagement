d933ccfc5045a194b77bdcea2fd800c55fd7f050
diddipoeler <famploeger@arcor.de>
2019-02-03 15:38:39 +0100

    Code

--- modules/mod_sportsmanagement_liveticker/mod_sportsmanagement_liveticker.php
+++ modules/mod_sportsmanagement_liveticker/mod_sportsmanagement_liveticker.php
@@ -39,21 +39,15 @@ require_once(JPATH_SITE . DS . JSM_PATH . DS . 'helpers' . DS . 'countries.php')
 }
 if ( !class_exists('sportsmanagementHelper') ) 
 {
-//add the classes for handling
+/**
+ * add the classes for handling
+ */
 $classpath = JPATH_ADMINISTRATOR.DS.JSM_PATH.DS.'helpers'.DS.'sportsmanagement.php';
 JLoader::register('sportsmanagementHelper', $classpath);
 BaseDatabaseModel::getInstance("sportsmanagementHelper", "sportsmanagementModel");
 }
 
-
-
 require_once (dirname(__FILE__).DS.'helper.php');
-
-/*
-// No conflict
-$document = Factory::getDocument();
-$document->addScript(Uri::root(true).'/administrator/components/com_sportsmanagement/assets/js/jl2.noconflict.js');
-*/
 
 $document = Factory::getDocument();
 $document->addScript(Uri::base().'modules/'.$module->module.'/js/turtushout.js');
@@ -66,7 +60,6 @@ $use_secret_salt    = $params->get( 'use_secret_salt', true );
 $secret_salt        = $params->get( 'secret_salt', 'tGbd8mfTb4p3f1_aAQpn84Qds' );
 $add_timeout        = $params->get( 'add_timeout' , 240);
 $update_timeout     = $params->get( 'update_timeout' , 240);
-
 $display_username   = $params->get( 'display_username', 1 );
 $display_title      = $params->get( 'display_title', 1 );
 $display_num        = $params->get( 'display_num', 5 );
@@ -75,7 +68,6 @@ $display_welcome    = $params->get( 'display_welcome', 1 );
 $size	            = $params->get( 'size' );
 $cols	            = $params->get( 'cols' );
 $rows	            = $params->get( 'rows' );
-
 $use_css            = $params->get( 'use_css', 'simple' );
 $class	            = $params->get( 'moduleclass_sfx', '' );
 
@@ -100,7 +92,9 @@ $document->addScript(Uri::base().'modules/'.$module->module.'/js/jquery-1.2.3.pa
 
 if ($use_css) 
 {
-//add css file
+/**
+ * add css file
+ */
 $document->addStyleSheet(Uri::base().'modules/'.$module->module.'/css/'.$use_css);
 }
     
@@ -159,20 +153,15 @@ if (!$is_ajaxed || ($action == "turtushout_shouts"))
 	$list_html = "";
 	
 $list_html .= "<div class='turtushout-entry'>";
-
 $list_html .=  "<div class='turtushout-name'>";
 $list_html .=  "<table class=\"".$table_class."\">";
-
 $list_html .=  "<thead>" ;
 $list_html .=  "<tr>" ;
 $list_html .= "<td colspan=\"\" align=\"middle\" >" . "aktuelle Zeit" . "</td>";
-	
 $date = new DateTime();
 $config = Factory::getConfig();
 $date->setTimezone(new DateTimeZone($config->get('offset')));	
 $list_html .= "<td colspan=\"8\" align=\"left\" >" . $date->format('H:i:s'). "</td>";	
-//$list_html .= "<td colspan=\"8\" align=\"left\" >" . date("H:i:s",time()). "</td>";
-//$list_html .= "<td colspan=\"8\" align=\"left\" >" . Factory::getDate()->format('%H:%M'). "</td>";	
 $list_html .= "</tr>" ;
 $list_html .=  "<tr>" ;
 
@@ -181,12 +170,10 @@ if ( $display_liganame && $display_ligaflagge )
 $list_html .= "<td colspan=\"2\">" . "Liga" . "</td>";
 }
 
-
 if ( ( !$display_liganame && $display_ligaflagge ) || ( $display_liganame && !$display_ligaflagge ) )
 {
 $list_html .= "<td colspan=\"1\">" . "Liga" . "</td>";
 }
-
 
 if ( $display_anstoss )
 {
@@ -198,30 +185,36 @@ if ( $display_abpfiff )
 $list_html .= "<td>" . "Abpfiff" . "</td>";
 }
 
-// wappen und teamname
+/**
+ * wappen und teamname
+ */
 if ( $display_teamwappen && $display_teamname < 3 )
 {
 $list_html .= "<td colspan=\"4\" align=\"middle\" >" . "Paarung" . "</td>";
 }
-// kein wappen und teamname
+/**
+ * kein wappen und teamname
+ */
 if ( !$display_teamwappen && $display_teamname < 3 )
 {
 $list_html .= "<td colspan=\"2\" align=\"middle\" >" . "Paarung" . "</td>";
 }
-// wappen und kein teamname
+/**
+ * wappen und kein teamname
+ */
 if ( $display_teamwappen && $display_teamname == 3 )
 {
 $list_html .= "<td colspan=\"2\" align=\"middle\" >" . "Paarung" . "</td>";
 }
-// kein wappen und kein teamname
+/**
+ * kein wappen und kein teamname
+ */
 if ( !$display_teamwappen && $display_teamname == 3 )
 {
 }
 
-
 $list_html .= "<td colspan=\"2\">" . "Ergebnis" . "</td>";
 $list_html .= "</tr>" ;
-
 $list_html .=  "</thead>" ;
 
 	for ($i = 0, $ic = count($list); $i<$ic; $i++)
@@ -237,17 +230,19 @@ $abpfiff = date('H:i:s', strtotime($anstoss) + ($list[$i]->game_regular_time + $
 $matchpart1_pic = (trim($list[$i]->wappenheim) != "")? sprintf('<img src="%s" alt="%s" width="20"/>', $list[$i]->wappenheim, $list[$i]->heim) : "";
 $matchpart2_pic = (trim($list[$i]->wappengast) != "")? sprintf('<img src="%s" alt="%s" width="20"/>', $list[$i]->wappengast, $list[$i]->gast) : "";
 
-//$isoflag =  strtolower($list[$i]->alpha2);
-
 $list_html .=  "<tr>" ;
 
-// ligaflagge
+/**
+ * ligaflagge
+ */
 if ( $display_ligaflagge )
 {
 $list_html .= "<td>" . "<img src=\"".$list[$i]->country_picture."\" alt=\"".$list[$i]->countries_iso_code_3."\" title=\"".$list[$i]->countries_iso_code_3."\" hspace=\"2\" /> " . "</td>";
 }
 
-// liganame
+/**
+ * liganame
+ */
 if ( $display_liganame )
 {
 $list_html .= "<td>" . $list[$i]->name . "</td>";
@@ -283,15 +278,10 @@ $list_html .= "<td>" . $list[$i]->heim_short_name . "</td>";
 if ( $display_teamname == 3 )
 {
 }
-
-
-
 if ( $display_teamwappen )
 {
 $list_html .= "<td>" . $matchpart2_pic . "</td>";
 }
-
-
 if ( $display_teamname == 0 )
 {
 $list_html .= "<td>" . $list[$i]->gast . "</td>";
