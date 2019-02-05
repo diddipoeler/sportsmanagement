8e52e2995a398add633329bac510c16f2848a893
diddipoeler <famploeger@arcor.de>
2019-02-05 15:48:32 +0100

    Update default.php
    Dart

--- site/views/eventsranking/tmpl/default.php
+++ site/views/eventsranking/tmpl/default.php
@@ -1,9 +1,9 @@
 <?php 
-/** SportsManagement ein Programm zur Verwaltung für alle Sportarten
+/** SportsManagement ein Programm zur Verwaltung fÃ¼r alle Sportarten
  * @version   1.0.05
  * @file      default.php
  * @author    diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @copyright Copyright: Â© 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
  * @license   This file is part of SportsManagement.
  * @package   sportsmanagement
  * @subpackage eventsranking
@@ -16,18 +16,18 @@ defined('_JEXEC') or die('Restricted access');
 $templatesToLoad = array('globalviews');
 sportsmanagementHelper::addTemplatePaths($templatesToLoad, $this);
 ?>
-<div class="container-fluid" id="eventsranking">
+<div class="<?php echo $this->divclasscontainer;?>" id="eventsranking">
 <?php
-if ($this->config['show_sectionheader']==1)
+if ( $this->config['show_sectionheader'] )
 {
-	echo $this->loadTemplate('sectionheader');
+echo $this->loadTemplate('sectionheader');
 }
 
 echo $this->loadTemplate('projectheading');
 
-if (($this->config['show_eventsstats'])==1)
+if ( $this->config['show_eventsstats'] )
 {
-	echo $this->loadTemplate('eventsrank');
+echo $this->loadTemplate('eventsrank');
 }
 echo $this->loadTemplate('jsminfo');
 ?>
