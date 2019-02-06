a9f6301025c069cfde4d14edff62dade5640a4aa
diddipoeler <famploeger@arcor.de>
2019-02-06 12:47:05 +0100

    Update uploaddraganddrop.php
    #873

--- admin/views/imagehandler/tmpl/uploaddraganddrop.php
+++ admin/views/imagehandler/tmpl/uploaddraganddrop.php
@@ -15,16 +15,16 @@ use Joomla\CMS\Language\Text;
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Uri\Uri;
 
-$this->document->addScript(Uri::root() . '/components/com_sportsmanagement/assets/js/fileupload/vendor/jquery.ui.widget.js');
-$this->document->addScript(Uri::root() . '/components/com_sportsmanagement/assets/js/fileupload/jquery.iframe-transport.js');
-$this->document->addScript(Uri::root() . '/components/com_sportsmanagement/assets/js/fileupload/jquery.fileupload.js');
-$this->document->addScript(Uri::root() . '/components/com_sportsmanagement/assets/js/fileupload/jquery.fileupload-process.js');
-$this->document->addScript(Uri::root() . '/components/com_sportsmanagement/assets/js/fileupload/jquery.fileupload-image.js');
-$this->document->addScript(Uri::root() . '/components/com_sportsmanagement/assets/js/fileupload/jquery.fileupload-audio.js');
-$this->document->addScript(Uri::root() . '/components/com_sportsmanagement/assets/js/fileupload/jquery.fileupload-video.js');
-$this->document->addScript(Uri::root() . '/components/com_sportsmanagement/assets/js/fileupload/jquery.fileupload-validate.js');
+$this->document->addScript(Uri::root() . 'administrator/components/com_sportsmanagement/assets/js/fileupload/vendor/jquery.ui.widget.js');
+$this->document->addScript(Uri::root() . 'administrator/components/com_sportsmanagement/assets/js/fileupload/jquery.iframe-transport.js');
+$this->document->addScript(Uri::root() . 'administrator/components/com_sportsmanagement/assets/js/fileupload/jquery.fileupload.js');
+$this->document->addScript(Uri::root() . 'administrator/components/com_sportsmanagement/assets/js/fileupload/jquery.fileupload-process.js');
+$this->document->addScript(Uri::root() . 'administrator/components/com_sportsmanagement/assets/js/fileupload/jquery.fileupload-image.js');
+$this->document->addScript(Uri::root() . 'administrator/components/com_sportsmanagement/assets/js/fileupload/jquery.fileupload-audio.js');
+$this->document->addScript(Uri::root() . 'administrator/components/com_sportsmanagement/assets/js/fileupload/jquery.fileupload-video.js');
+$this->document->addScript(Uri::root() . 'administrator/components/com_sportsmanagement/assets/js/fileupload/jquery.fileupload-validate.js');
 
-$this->document->addStyleSheet(Uri::root() .'components/com_sportsmanagement/assets/css/fileupload/style.css', 'text/css');
-$this->document->addStyleSheet(Uri::root() .'components/com_sportsmanagement/assets/css/fileupload/jquery.fileupload.css', 'text/css');
+$this->document->addStyleSheet(Uri::root() .'administrator/components/com_sportsmanagement/assets/css/fileupload/style.css', 'text/css');
+$this->document->addStyleSheet(Uri::root() .'administrator/components/com_sportsmanagement/assets/css/fileupload/jquery.fileupload.css', 'text/css');
 
 ?>
