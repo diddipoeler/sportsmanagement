cdd66ee61c43add6a66ef5714db83a89fdf34dec
diddipoeler <famploeger@arcor.de>
2019-02-07 11:02:58 +0100

    Update uploaddraganddrop.php
    #873

--- admin/views/imagehandler/tmpl/uploaddraganddrop.php
+++ admin/views/imagehandler/tmpl/uploaddraganddrop.php
@@ -1,3 +1,20 @@
+<?php
+/** SportsManagement ein Programm zur Verwaltung für alle Sportarten
+ * @version   1.0.05
+ * @file      uploaddraganddrop.php
+ * @author    diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license   This file is part of SportsManagement.
+ * @package   sportsmanagement
+ * @subpackage imagehandler
+ */
+ 
+// no direct access
+defined( '_JEXEC' ) or die( 'Restricted access' );
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\HTML\HTMLHelper;
+?>
+
 <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-fileinput/4.4.9/css/fileinput.min.css" media="all" rel="stylesheet" type="text/css" />
 <!-- piexif.min.js is needed for auto orienting image files OR when restoring exif data in resized images and when you 
     wish to resize images before upload. This must be loaded before fileinput.min.js -->
