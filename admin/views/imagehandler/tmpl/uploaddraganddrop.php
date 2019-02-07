cc16484f17a2dfece295767de442f306f72892cd
diddipoeler <famploeger@arcor.de>
2019-02-07 13:26:10 +0100

    Update uploaddraganddrop.php
    #873

--- admin/views/imagehandler/tmpl/uploaddraganddrop.php
+++ admin/views/imagehandler/tmpl/uploaddraganddrop.php
@@ -1,7 +1,7 @@
 <?php
 /** SportsManagement ein Programm zur Verwaltung für alle Sportarten
  * @version   1.0.05
- * @file      uploaddraganddrop.php
+ * @file      upload.php
  * @author    diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
  * @copyright Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
  * @license   This file is part of SportsManagement.
@@ -15,62 +15,51 @@ use Joomla\CMS\Language\Text;
 use Joomla\CMS\HTML\HTMLHelper;
 ?>
 
-<link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-fileinput/4.4.9/css/fileinput.min.css" media="all" rel="stylesheet" type="text/css" />
-<!-- piexif.min.js is needed for auto orienting image files OR when restoring exif data in resized images and when you 
-    wish to resize images before upload. This must be loaded before fileinput.min.js -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-fileinput/4.4.9/js/plugins/piexif.min.js" type="text/javascript"></script>
-<!-- sortable.min.js is only needed if you wish to sort / rearrange files in initial preview. 
-    This must be loaded before fileinput.min.js -->  
-<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-fileinput/4.4.9/js/plugins/sortable.min.js" type="text/javascript"></script>
-<!-- purify.min.js is only needed if you wish to purify HTML content in your preview for 
-    HTML files. This must be loaded before fileinput.min.js -->  
-<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-fileinput/4.4.9/js/plugins/purify.min.js" type="text/javascript"></script>
-<!-- popper.min.js below is needed if you use bootstrap 4.x. You can also use the bootstrap js 
-   3.3.x versions without popper.min.js. -->  
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"></script>
-<!-- the main fileinput plugin file -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-fileinput/4.4.9/js/fileinput.min.js"></script>
-<!-- optionally if you need a theme like font awesome theme you can include it as mentioned below -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-fileinput/4.4.9/themes/fa/theme.js"></script>
-<!-- optionally if you need translation for your language then include  locale file as mentioned below -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-fileinput/4.4.9/js/locales/de.js"></script>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" crossorigin="anonymous">
+    <link href="components/com_sportsmanagement/views/imagehandler/tmpl/css/fileinput.css" media="all" rel="stylesheet" type="text/css"/>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" crossorigin="anonymous">
+    <link href="components/com_sportsmanagement/views/imagehandler/tmpl/themes/explorer-fas/theme.css" media="all" rel="stylesheet" type="text/css"/>
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+    <script src="components/com_sportsmanagement/views/imagehandler/tmpl/js/plugins/sortable.js" type="text/javascript"></script>
+    <script src="components/com_sportsmanagement/views/imagehandler/tmpl/js/fileinput.js" type="text/javascript"></script>
+    <script src="components/com_sportsmanagement/views/imagehandler/tmpl/js/locales/de.js" type="text/javascript"></script>
+    
+    <script src="components/com_sportsmanagement/views/imagehandler/tmpl/themes/fas/theme.js" type="text/javascript"></script>
+    <script src="components/com_sportsmanagement/views/imagehandler/tmpl/themes/explorer-fas/theme.js" type="text/javascript"></script>
   
-<div class="container kv-main">
-    <div class="page-header">
-        <h1>File upload demo
-        </h1>
-    </div>
-
- <form action="<?php echo $this->request_url; ?>" enctype="multipart/form-data" id="adminForm" name="adminForm"> 
-         <div class="file-loading"> 
-             <input id="kv-explorer" type="file" multiple> 
-         </div> 
-         <br> 
-         <div class="file-loading"> 
-             <input id="userfile" class="file" type="file" multiple data-min-file-count="1" data-theme="fas"> 
-         </div> 
-         <br> 
-         <button type="submit" class="btn btn-primary">Submit</button> 
-         <button type="reset" class="btn btn-outline-secondary">Reset</button> 
+  
+<div class="container my-4">
+<form action="<?php echo $this->request_url; ?>" enctype="multipart/form-data" id="adminForm" name="adminForm" method="post">
+        
+  <div class="form-group">
+            <div class="file-loading">
+                <input name="userfile" id="userfile" type="file" class="file" data-overwrite-initial="false" data-theme="fas">
+            </div>
+        </div>
   
   <input type="hidden" name="option" value="com_sportsmanagement" />
 <input type="hidden" name="task" value="imagehandler.upload" />
 <input type="hidden" name="folder" value="<?php echo $this->folder;?>" />
 <?php echo HTMLHelper::_( 'form.token' ); ?>
-     </form> 
+</form>
 
+  
 
+</div>  
+  
+<script>  
+ jQuery("#userfile").fileinput({
+        theme: 'fas',
+  language: 'de',
+        allowedFileExtensions: ['jpg', 'png', 'gif'],
+        overwriteInitial: false,
+        maxFileSize: 1000,
+        maxFilesNum: 10,
+        //allowedFileTypes: ['image', 'video', 'flash'],
+        slugCallback: function (filename) {
+            return filename.replace('(', '_').replace(']', '_');
+        }
+    });  
 
-
-</div>
-  
-  
-<script> 
-             
-</script> 
-  
-  
-  
-  
-  
-  
+</script>
