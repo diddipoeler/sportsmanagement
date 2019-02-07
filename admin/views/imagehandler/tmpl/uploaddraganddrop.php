455a3f7d7466c7692f270ce9a54304e2890ed4ad
diddipoeler <famploeger@arcor.de>
2019-02-07 10:56:19 +0100

    Update uploaddraganddrop.php
    #873

--- admin/views/imagehandler/tmpl/uploaddraganddrop.php
+++ admin/views/imagehandler/tmpl/uploaddraganddrop.php
@@ -1,193 +1,59 @@
-<?php
-/** SportsManagement ein Programm zur Verwaltung für alle Sportarten
- * @version   1.0.05
- * @file      uploaddraganddrop.php
- * @author    diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license   This file is part of SportsManagement.
- * @package   sportsmanagement
- * @subpackage imagehandler
- * https://github.com/blueimp/jQuery-File-Upload/wiki
- */
- 
-// no direct access
-defined( '_JEXEC' ) or die( 'Restricted access' );
-use Joomla\CMS\Language\Text;
-use Joomla\CMS\HTML\HTMLHelper;
-use Joomla\CMS\Uri\Uri;
-
-$this->document->addScript(Uri::root() . 'administrator/components/com_sportsmanagement/assets/js/fileupload/vendor/jquery.ui.widget.js');
-
-$this->document->addScript('https://blueimp.github.io/JavaScript-Load-Image/js/load-image.all.min.js');
-$this->document->addScript('https://blueimp.github.io/JavaScript-Canvas-to-Blob/js/canvas-to-blob.min.js');
-
-$this->document->addScript(Uri::root() . 'administrator/components/com_sportsmanagement/assets/js/fileupload/jquery.iframe-transport.js');
-$this->document->addScript(Uri::root() . 'administrator/components/com_sportsmanagement/assets/js/fileupload/jquery.fileupload.js');
-$this->document->addScript(Uri::root() . 'administrator/components/com_sportsmanagement/assets/js/fileupload/jquery.fileupload-process.js');
-$this->document->addScript(Uri::root() . 'administrator/components/com_sportsmanagement/assets/js/fileupload/jquery.fileupload-image.js');
-$this->document->addScript(Uri::root() . 'administrator/components/com_sportsmanagement/assets/js/fileupload/jquery.fileupload-audio.js');
-$this->document->addScript(Uri::root() . 'administrator/components/com_sportsmanagement/assets/js/fileupload/jquery.fileupload-video.js');
-$this->document->addScript(Uri::root() . 'administrator/components/com_sportsmanagement/assets/js/fileupload/jquery.fileupload-validate.js');
-
-$this->document->addStyleSheet(Uri::root() .'administrator/components/com_sportsmanagement/assets/css/fileupload/style.css', 'text/css');
-$this->document->addStyleSheet(Uri::root() .'administrator/components/com_sportsmanagement/assets/css/fileupload/jquery.fileupload.css', 'text/css');
-
-echo 'folder '.$this->folder;
-
-$uploadhandler = Uri::root() .'images/com_sportsmanagement/database/'.$this->folder;
-echo 'uploadhandler  '.$uploadhandler ;
-?>
-
-
-
-<script>
-/*jslint unparam: true, regexp: true */
-/*global window, $ */
-jQuery(function ($) {
-    'use strict';
-    // Change this to the location of your server-side upload handler:
-    var url = window.location.hostname === 'administrator/components/com_sportsmanagement/views/imagehandler/tmpl/',
-        uploadButton = $('<button/>')
-            .addClass('btn btn-primary')
-            .prop('disabled', true)
-            .text('Processing...')
-            .on('click', function () {
-                var $this = $(this),
-                    data = $this.data();
-                $this
-                    .off('click')
-                    .text('Abort')
-                    .on('click', function () {
-                        $this.remove();
-                        data.abort();
-                    });
-                data.submit().always(function () {
-                    $this.remove();
-                });
-            });
-    $('#fileupload').fileupload({
-        url: url,
-        dataType: 'json',
-        autoUpload: true,
-        acceptFileTypes: /(\.|\/)(gif|jpe?g|png)$/i,
-        maxFileSize: 999000,
-        // Enable image resizing, except for Android and Opera,
-        // which actually support image resizing, but fail to
-        // send Blob objects via XHR requests:
-        disableImageResize: /Android(?!.*Chrome)|Opera/
-            .test(window.navigator.userAgent),
-        previewMaxWidth: 100,
-        previewMaxHeight: 100,
-        previewCrop: true
-    }).on('fileuploadadd', function (e, data) {
-        data.context = $('<div/>').appendTo('#files');
-        $.each(data.files, function (index, file) {
-            var node = $('<p/>')
-                    .append($('<span/>').text(file.name));
-            if (!index) {
-                node
-                    .append('<br>')
-                    .append(uploadButton.clone(true).data(data));
-            }
-            node.appendTo(data.context);
-        });
-    }).on('fileuploadprocessalways', function (e, data) {
-        var index = data.index,
-            file = data.files[index],
-            node = $(data.context.children()[index]);
-        if (file.preview) {
-            node
-                .prepend('<br>')
-                .prepend(file.preview);
-        }
-        if (file.error) {
-            node
-                .append('<br>')
-                .append($('<span class="text-danger"/>').text(file.error));
-        }
-        if (index + 1 === data.files.length) {
-            data.context.find('button')
-                .text('Upload')
-                .prop('disabled', !!data.files.error);
-        }
-    }).on('fileuploadprogressall', function (e, data) {
-        var progress = parseInt(data.loaded / data.total * 100, 10);
-        $('#progress .progress-bar').css(
-            'width',
-            progress + '%'
-        );
-    }).on('fileuploaddone', function (e, data) {
-        $.each(data.result.files, function (index, file) {
-            if (file.url) {
-                var link = $('<a>')
-                    .attr('target', '_blank')
-                    .prop('href', file.url);
-                $(data.context.children()[index])
-                    .wrap(link);
-            } else if (file.error) {
-                var error = $('<span class="text-danger"/>').text(file.error);
-                $(data.context.children()[index])
-                    .append('<br>')
-                    .append(error);
-            }
-        });
-    }).on('fileuploadfail', function (e, data) {
-        $.each(data.files, function (index) {
-            var error = $('<span class="text-danger"/>').text('File upload failed.');
-            $(data.context.children()[index])
-                .append('<br>')
-                .append(error);
-        });
-    }).prop('disabled', !$.support.fileInput)
-        .parent().addClass($.support.fileInput ? undefined : 'disabled');
-});
-</script>
-
-<div class="container">
-    <h1>jQuery File Upload Demo</h1>
-    <h2 class="lead">Basic Plus version</h2>
-    <ul class="nav nav-tabs">
-        
-        <li class="active"><a href="basic-plus.html">Basic Plus</a></li>
-        
-    </ul>
-    <br>
-    <blockquote>
-        <p>File Upload widget with multiple file selection, drag&amp;drop support, progress bar, validation and preview images, audio and video for jQuery.<br>
-        Supports cross-domain, chunked and resumable file uploads and client-side image resizing.<br>
-        Works with any server-side platform (PHP, Python, Ruby on Rails, Java, Node.js, Go etc.) that supports standard HTML form file uploads.</p>
-    </blockquote>
-    <br>
-    <!-- The fileinput-button span is used to style the file input field as button -->
-    <span class="btn btn-success fileinput-button">
-        <i class="glyphicon glyphicon-plus"></i>
-        <span>Add files...</span>
-        <!-- The file input field used as target for the file upload widget -->
-        <input id="fileupload" type="file" name="files[]" multiple>
-    </span>
-    <br>
-    <br>
-    <!-- The global progress bar -->
-    <div id="progress" class="progress">
-        <div class="progress-bar progress-bar-success"></div>
+<link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-fileinput/4.4.9/css/fileinput.min.css" media="all" rel="stylesheet" type="text/css" />
+<!-- piexif.min.js is needed for auto orienting image files OR when restoring exif data in resized images and when you 
+    wish to resize images before upload. This must be loaded before fileinput.min.js -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-fileinput/4.4.9/js/plugins/piexif.min.js" type="text/javascript"></script>
+<!-- sortable.min.js is only needed if you wish to sort / rearrange files in initial preview. 
+    This must be loaded before fileinput.min.js -->  
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-fileinput/4.4.9/js/plugins/sortable.min.js" type="text/javascript"></script>
+<!-- purify.min.js is only needed if you wish to purify HTML content in your preview for 
+    HTML files. This must be loaded before fileinput.min.js -->  
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-fileinput/4.4.9/js/plugins/purify.min.js" type="text/javascript"></script>
+<!-- popper.min.js below is needed if you use bootstrap 4.x. You can also use the bootstrap js 
+   3.3.x versions without popper.min.js. -->  
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"></script>
+<!-- the main fileinput plugin file -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-fileinput/4.4.9/js/fileinput.min.js"></script>
+<!-- optionally if you need a theme like font awesome theme you can include it as mentioned below -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-fileinput/4.4.9/themes/fa/theme.js"></script>
+<!-- optionally if you need translation for your language then include  locale file as mentioned below -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-fileinput/4.4.9/js/locales/de.js"></script>
+  
+<div class="container kv-main">
+    <div class="page-header">
+        <h1>File upload demo
+        </h1>
     </div>
-    <!-- The container for the uploaded files -->
-    <div id="files" class="files"></div>
-    <br>
-    
+
+ <form action="<?php echo $this->request_url; ?>" enctype="multipart/form-data" id="adminForm" name="adminForm"> 
+         <div class="file-loading"> 
+             <input id="kv-explorer" type="file" multiple> 
+         </div> 
+         <br> 
+         <div class="file-loading"> 
+             <input id="userfile" class="file" type="file" multiple data-min-file-count="1" data-theme="fas"> 
+         </div> 
+         <br> 
+         <button type="submit" class="btn btn-primary">Submit</button> 
+         <button type="reset" class="btn btn-outline-secondary">Reset</button> 
+  
+  <input type="hidden" name="option" value="com_sportsmanagement" />
+<input type="hidden" name="task" value="imagehandler.upload" />
+<input type="hidden" name="folder" value="<?php echo $this->folder;?>" />
+<?php echo HTMLHelper::_( 'form.token' ); ?>
+     </form> 
+
+
+
+
 </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  
+  
+<script> 
+             
+</script> 
+  
+  
+  
+  
+  
+  
