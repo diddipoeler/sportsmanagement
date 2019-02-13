22e8c11b85dfdede11f0792f165e9915cb150278
diddipoeler <famploeger@arcor.de>
2019-02-13 19:41:50 +0100

    #822

--- site/views/editmatch/tmpl/editreferees.php
+++ site/views/editmatch/tmpl/editreferees.php
@@ -27,7 +27,9 @@ $params = $this->form->getFieldsets('params');
 	<fieldset>
 		<div class="fltrt">
 <button type="button" onclick="jQuery('select.position-starters option').prop('selected', 'selected');Joomla.submitform('editmatch.saveReferees', this.form);">
-<?php echo Text::_('JSAVE');?></button>			
+<?php echo Text::_('JSAVE');?></button>	
+<button type="button" onclick="Joomla.submitform('editmatch.cancel', this.form);">
+<?php echo Text::_('JCANCEL');?></button>		
 		</div>
 		<div class="configuration" >
 			<?php echo Text::sprintf('COM_SPORTSMANAGEMENT_ADMIN_MATCH_ER_TITLE'); ?>
@@ -97,21 +99,19 @@ $params = $this->form->getFieldsets('params');
 		</fieldset>
 		<br/>
 		<br/>
-		<input type="hidden" name="task" value="" />
-		<input type="hidden" name="view" value="" />
-		<input type="hidden" name="close" id="close" value="0" />
-		<input type="hidden" name="id" value="<?php echo $this->match->id; ?>" />
-		<input type="hidden" name="project_id" value="<?php echo $this->project_id; ?>" />
-		<input type="hidden" name="changes_check" value="0" id="changes_check" />
-		<input type="hidden" name="p" value="<?php echo sportsmanagementModelEditMatch::$projectid; ?>" />
-	
-	<input type="hidden" name="r" value="<?php echo sportsmanagementModelEditMatch::$roundid; ?>" />
-	<input type="hidden" name="s" value="<?php echo sportsmanagementModelEditMatch::$seasonid; ?>" />
-	<input type="hidden" name="division" value="<?php echo sportsmanagementModelEditMatch::$divisionid; ?>" />
+<input type="hidden" name="task" value="" />
+<input type="hidden" name="view" value="" />
+<input type="hidden" name="close" id="close" value="0" />
+<input type="hidden" name="id" value="<?php echo $this->match->id; ?>" />
+<input type="hidden" name="project_id" value="<?php echo $this->project_id; ?>" />
+<input type="hidden" name="changes_check" value="0" id="changes_check" />
+<input type="hidden" name="p" value="<?php echo sportsmanagementModelEditMatch::$projectid; ?>" />
+<input type="hidden" name="r" value="<?php echo sportsmanagementModelEditMatch::$roundid; ?>" />
+<input type="hidden" name="s" value="<?php echo sportsmanagementModelEditMatch::$seasonid; ?>" />
+<input type="hidden" name="division" value="<?php echo sportsmanagementModelEditMatch::$divisionid; ?>" />
 <input type="hidden" name="cfg_which_database" value="<?php echo sportsmanagementModelEditMatch::$cfg_which_database; ?>" />
-		
-		<input type="hidden" name="positionscount" value="<?php echo count($this->positions); ?>" id="positioncount" />
-        <input type="hidden" name="component" value="com_sportsmanagement" />
-		<?php echo HTMLHelper::_('form.token')."\n"; ?>
-	</form>
+<input type="hidden" name="positionscount" value="<?php echo count($this->positions); ?>" id="positioncount" />
+<input type="hidden" name="component" value="com_sportsmanagement" />
+<?php echo HTMLHelper::_('form.token')."\n"; ?>
+</form>
 </div>
