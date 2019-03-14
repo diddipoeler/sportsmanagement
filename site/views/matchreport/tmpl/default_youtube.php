ed61a6a4a6f0853f65e11656626ab04cc91b1319
diddipoeler <famploeger@arcor.de>
2019-03-14 09:12:23 +0100

    Update default_youtube.php
    youtube

--- site/views/matchreport/tmpl/default_youtube.php
+++ site/views/matchreport/tmpl/default_youtube.php
@@ -1,1 +1,16 @@
+<?php 
+/** SportsManagement ein Programm zur Verwaltung für alle Sportarten
+ * @version   1.0.05
+ * @file      default_youtube.php
+ * @author    diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license   This file is part of SportsManagement.
+ * @package   sportsmanagement
+ * @subpackage matchreport
+ */
 
+
+
+
+
+?>
