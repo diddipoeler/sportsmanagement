2370a4a6f5010e648dfac2f61e4102a6aada4439
diddipoeler <famploeger@arcor.de>
2019-03-14 09:17:36 +0100

    Update default_youtube.php
    youtube

--- site/views/matchreport/tmpl/default_youtube.php
+++ site/views/matchreport/tmpl/default_youtube.php
@@ -14,3 +14,20 @@
 
 
 ?>
+
+<h4><?php echo Text::_('COM_SPORTSMANAGEMENT_MATCHREPORT_YOUTUBE'); ?></h4>
+<div class="panel-group" id="showyoutube">    
+
+<div class="panel panel-default">
+<div class="panel-heading">
+<h4 class="panel-title">
+<a data-toggle="collapse" data-parent="#showyoutube" href="#countall"><?php echo Text::_('COM_SPORTSMANAGEMENT_MATCHREPORT_YOUTUBE_SHOW'); ?></a>
+</h4>
+</div>  
+<div id="countall" class="panel-collapse collapse">
+<div class="panel-body">
+
+  
+</div>
+</div>
+</div>
