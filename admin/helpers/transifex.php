9ffc88a0947b8449a6f0f35512ac7cd88a8b9737
diddipoeler <famploeger@arcor.de>
2019-03-18 09:20:45 +0100

    Update transifex.php
    transifex

--- admin/helpers/transifex.php
+++ admin/helpers/transifex.php
@@ -1,1 +1,17 @@
+<?php
+/** SportsManagement ein Programm zur Verwaltung für alle Sportarten
+ * @version   1.0.05
+ * @file      transifex.php
+ * @author    diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license   This file is part of SportsManagement.
+ * @package   sportsmanagement
+ * @subpackage helpers
+ * 
+ */
+ 
+defined('_JEXEC') or die;
 
+
+
+?>
