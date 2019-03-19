6935ce177b5afb72c093f9bb46355252e40541ee
diddipoeler <famploeger@arcor.de>
2019-03-19 15:56:49 +0100

    Update transifex.php
    transifex

--- admin/controllers/transifex.php
+++ admin/controllers/transifex.php
@@ -43,7 +43,7 @@ $lang = Factory::getLanguage();
 $langtag = $lang->getTag();	
 $this->jsmapp->enqueueMessage(Text::_(__METHOD__.' '.__LINE__.' langtag<br><pre>'.print_r($langtag,true).'</pre>'),'');	
 $code = sportsmanagementHelperTransifex::getLangCode($langtag);
-$this->jsmapp->enqueueMessage(Text::_(__METHOD__.' '.__LINE__.' code<br><pre>'.print_r(v,true).'</pre>'),'');		
+$this->jsmapp->enqueueMessage(Text::_(__METHOD__.' '.__LINE__.' code<br><pre>'.print_r($code,true).'</pre>'),'');		
 	
 $result = sportsmanagementHelperTransifex::getData('');
 $this->jsmapp->enqueueMessage(Text::_(__METHOD__.' '.__LINE__.' result<br><pre>'.print_r($result,true).'</pre>'),'');
