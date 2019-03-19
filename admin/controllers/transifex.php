ed7434b841302f5a8c89aaa8fa77e2afed62bbd5
diddipoeler <famploeger@arcor.de>
2019-03-19 12:34:34 +0100

    Update transifex.php
    transifex

--- admin/controllers/transifex.php
+++ admin/controllers/transifex.php
@@ -1,1 +1,36 @@
+<?php
+/** SportsManagement ein Programm zur Verwaltung für Sportarten
+ * @version   1.0.05
+ * @file      transifex.php
+ * @author    diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license   This file is part of SportsManagement.
+ * @package   sportsmanagement
+ * @subpackage controllers
+ */
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
 
+/**
+ * sportsmanagementControllertransifex
+ * 
+ * @package   
+ * @author 
+ * @copyright diddi
+ * @version 2014
+ * @access public
+ */
+class sportsmanagementControllertransifex extends JSMControllerForm
+{
+    /**
+	 * Class Constructor
+	 *
+	 * @param	array	$config		An optional associative array of configuration settings.
+	 * @return	void
+	 * @since	1.5
+	 */
+	function __construct($config = array())
+	{
+		parent::__construct($config);
+    }    
+}
