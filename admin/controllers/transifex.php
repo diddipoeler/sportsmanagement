1fafb55fa637722e0517a5348585c4fe844d2640
diddipoeler <famploeger@arcor.de>
2019-03-19 14:13:17 +0100

    Update transifex.php
    transifex

--- admin/controllers/transifex.php
+++ admin/controllers/transifex.php
@@ -41,13 +41,20 @@ function gettransifexinfo()
 
 $result = sportsmanagementHelperTransifex::getData('');
 $this->jsmapp->enqueueMessage(Text::_(__METHOD__.' '.__LINE__.' result<br><pre>'.print_r($result,true).'</pre>'),'');
+$json_decode = json_decode($result['data']);
+$this->jsmapp->enqueueMessage(Text::_(__METHOD__.' '.__LINE__.' json_decode<br><pre>'.print_r($json_decode,true).'</pre>'),'');
 	
 $transifexlanguages = sportsmanagementHelperTransifex::getData('languages');	
 $this->jsmapp->enqueueMessage(Text::_(__METHOD__.' '.__LINE__.' transifexlanguages<br><pre>'.print_r($transifexlanguages,true).'</pre>'),'');	
-
+$json_decode = json_decode($transifexlanguages['data']);
+$this->jsmapp->enqueueMessage(Text::_(__METHOD__.' '.__LINE__.' json_decode<br><pre>'.print_r($json_decode,true).'</pre>'),'');
 	
 $transifexresources = sportsmanagementHelperTransifex::getData('resources');	
 $this->jsmapp->enqueueMessage(Text::_(__METHOD__.' '.__LINE__.' transifexresources<br><pre>'.print_r($transifexresources,true).'</pre>'),'');		
+$json_decode = json_decode($transifexresources['data']);
+$this->jsmapp->enqueueMessage(Text::_(__METHOD__.' '.__LINE__.' json_decode<br><pre>'.print_r($json_decode,true).'</pre>'),'');
+	
+	
 	
 }
 	
