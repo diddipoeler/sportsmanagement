cf008b4da2e875bccfb2829440e5ebda8f11c883
diddipoeler <famploeger@arcor.de>
2019-03-20 11:53:17 +0100

    Update view.html.php
    transifex

--- admin/views/transifex/view.html.php
+++ admin/views/transifex/view.html.php
@@ -90,7 +90,7 @@ $translatefiles[] = $object;
 //$this->app->enqueueMessage(Text::_(__METHOD__.' '.__LINE__.' translatefiles<br><pre>'.print_r($translatefiles  ,true).'</pre>'),'');        
 
 $this->language = sportsmanagementHelperTransifex::updatelanguage($translatefiles,$langtag);		
-$this->app->enqueueMessage(Text::_(__METHOD__.' '.__LINE__.' language<br><pre>'.print_r($this->language  ,true).'</pre>'),'');        		
+//$this->app->enqueueMessage(Text::_(__METHOD__.' '.__LINE__.' language<br><pre>'.print_r($this->language  ,true).'</pre>'),'');        		
 		
 	}
     
