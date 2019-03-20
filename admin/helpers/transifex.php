dc7a6326fe63abaea1a7cc6ef8ea15d91a4df20a
diddipoeler <famploeger@arcor.de>
2019-03-20 07:31:45 +0100

    Update transifex.php
    transifex

--- admin/helpers/transifex.php
+++ admin/helpers/transifex.php
@@ -13,6 +13,8 @@
 defined('_JEXEC') or die;
 use Joomla\CMS\Factory;
 use Joomla\CMS\Language\Text;
+use Joomla\CMS\Filesystem\Folder;
+use Joomla\CMS\Filesystem\File;
 
 /**
  * sportsmanagementHelperTransifex
