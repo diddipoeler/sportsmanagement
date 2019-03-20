e6a6681102a5ae3e10bac60ff4cdd27fcbe92652
diddipoeler <famploeger@arcor.de>
2019-03-20 10:55:15 +0100

    Update default_data.php
    transifex

--- admin/views/transifex/tmpl/default_data.php
+++ admin/views/transifex/tmpl/default_data.php
@@ -17,7 +17,50 @@ use Joomla\CMS\Router\Route;
 HTMLHelper::_('behavior.tooltip');
 HTMLHelper::_('behavior.modal');
 ?>
-<div id="editcell">
-
+<div id="j-main-container">
+<table class="table table-striped" id="contentList">
+				<thead>
+					<tr>
+						
+						<th class="title nowrap">
+							<?php echo JHtml::_('searchtools.sort', 'JGLOBAL_TITLE', 'a.title', $listDirn, $listOrder); ?>
+						</th>
+						<th class="title nowrap hidden-phone hidden-tablet">
+							<?php echo JHtml::_('searchtools.sort', 'COM_LANGUAGES_HEADING_TITLE_NATIVE', 'a.title_native', $listDirn, $listOrder); ?>
+						</th>
+						<th width="1%" class="nowrap">
+							<?php echo JHtml::_('searchtools.sort', 'COM_LANGUAGES_HEADING_LANG_TAG', 'a.lang_code', $listDirn, $listOrder); ?>
+						</th>
+						<th width="1%" class="nowrap">
+							<?php echo JHtml::_('searchtools.sort', 'COM_LANGUAGES_HEADING_LANG_CODE', 'a.sef', $listDirn, $listOrder); ?>
+						</th>
+						<th width="8%" class="nowrap hidden-phone">
+							<?php echo JHtml::_('searchtools.sort', 'COM_LANGUAGES_HEADING_LANG_IMAGE', 'a.image', $listDirn, $listOrder); ?>
+						</th>
+						<th width="5%" class="nowrap hidden-phone">
+							<?php echo JHtml::_('searchtools.sort', 'JGRID_HEADING_ACCESS', 'a.access', $listDirn, $listOrder); ?>
+						</th>
+						<th width="5%" class="nowrap hidden-phone">
+							<?php echo JHtml::_('searchtools.sort', 'COM_LANGUAGES_HEADING_HOMEPAGE', 'l.home', $listDirn, $listOrder); ?>
+						</th>
+						<th width="1%" class="nowrap hidden-phone hidden-tablet">
+							<?php echo JHtml::_('searchtools.sort', 'JGRID_HEADING_ID', 'a.lang_id', $listDirn, $listOrder); ?>
+						</th>
+					</tr>
+				</thead>
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+</table>	
 </div>
 	
