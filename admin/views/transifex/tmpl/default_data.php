bcfff58bd13c77c628935194917b15d4ec0cd70a
diddipoeler <famploeger@arcor.de>
2019-03-20 11:02:02 +0100

    Update default_data.php
    transifex

--- admin/views/transifex/tmpl/default_data.php
+++ admin/views/transifex/tmpl/default_data.php
@@ -19,48 +19,56 @@ HTMLHelper::_('behavior.modal');
 ?>
 <div id="j-main-container">
 <table class="table table-striped" id="contentList">
-				<thead>
-					<tr>
+<thead>
+<tr>
 						
-						<th class="title nowrap">
-							<?php echo JHtml::_('searchtools.sort', 'JGLOBAL_TITLE', 'a.title', $listDirn, $listOrder); ?>
-						</th>
-						<th class="title nowrap hidden-phone hidden-tablet">
-							<?php echo JHtml::_('searchtools.sort', 'COM_LANGUAGES_HEADING_TITLE_NATIVE', 'a.title_native', $listDirn, $listOrder); ?>
-						</th>
-						<th width="1%" class="nowrap">
-							<?php echo JHtml::_('searchtools.sort', 'COM_LANGUAGES_HEADING_LANG_TAG', 'a.lang_code', $listDirn, $listOrder); ?>
-						</th>
-						<th width="1%" class="nowrap">
-							<?php echo JHtml::_('searchtools.sort', 'COM_LANGUAGES_HEADING_LANG_CODE', 'a.sef', $listDirn, $listOrder); ?>
-						</th>
-						<th width="8%" class="nowrap hidden-phone">
-							<?php echo JHtml::_('searchtools.sort', 'COM_LANGUAGES_HEADING_LANG_IMAGE', 'a.image', $listDirn, $listOrder); ?>
-						</th>
-						<th width="5%" class="nowrap hidden-phone">
-							<?php echo JHtml::_('searchtools.sort', 'JGRID_HEADING_ACCESS', 'a.access', $listDirn, $listOrder); ?>
-						</th>
-						<th width="5%" class="nowrap hidden-phone">
-							<?php echo JHtml::_('searchtools.sort', 'COM_LANGUAGES_HEADING_HOMEPAGE', 'l.home', $listDirn, $listOrder); ?>
-						</th>
-						<th width="1%" class="nowrap hidden-phone hidden-tablet">
-							<?php echo JHtml::_('searchtools.sort', 'JGRID_HEADING_ID', 'a.lang_id', $listDirn, $listOrder); ?>
-						</th>
-					</tr>
-				</thead>
+<th class="title nowrap">
+<?php echo JHtml::_('searchtools.sort', 'JGLOBAL_TITLE', 'a.title', $listDirn, $listOrder); ?>
+</th>
+<th class="title nowrap hidden-phone hidden-tablet">
+<?php echo JHtml::_('searchtools.sort', 'COM_LANGUAGES_HEADING_TITLE_NATIVE', 'a.title_native', $listDirn, $listOrder); ?>
+</th>
+<th width="1%" class="nowrap">
+<?php echo JHtml::_('searchtools.sort', 'COM_LANGUAGES_HEADING_LANG_TAG', 'a.lang_code', $listDirn, $listOrder); ?>
+</th>
+<th width="1%" class="nowrap">
+<?php echo JHtml::_('searchtools.sort', 'COM_LANGUAGES_HEADING_LANG_CODE', 'a.sef', $listDirn, $listOrder); ?>
+</th>
+<th width="8%" class="nowrap hidden-phone">
+<?php echo JHtml::_('searchtools.sort', 'COM_LANGUAGES_HEADING_LANG_IMAGE', 'a.image', $listDirn, $listOrder); ?>
+</th>
+<th width="5%" class="nowrap hidden-phone">
+<?php echo JHtml::_('searchtools.sort', 'JGRID_HEADING_ACCESS', 'a.access', $listDirn, $listOrder); ?>
+</th>
+<th width="5%" class="nowrap hidden-phone">
+<?php echo JHtml::_('searchtools.sort', 'COM_LANGUAGES_HEADING_HOMEPAGE', 'l.home', $listDirn, $listOrder); ?>
+</th>
+<th width="1%" class="nowrap hidden-phone hidden-tablet">
+<?php echo JHtml::_('searchtools.sort', 'JGRID_HEADING_ID', 'a.lang_id', $listDirn, $listOrder); ?>
+</th>
+</tr>
+</thead>
+	
+<?php
+foreach ($this->language as $i => $item) :
+?>	
+<tr class="row<?php echo $i % 2; ?>">	
+<td class="hidden-phone hidden-tablet">
+<?php echo $item->file; ?>
+</td>	
+<td class="hidden-phone hidden-tablet">
+<?php echo $item->languagetag; ?>
+</td>	
+<td class="hidden-phone hidden-tablet">
+<?php echo $item->completed; ?>
+</td>	
 	
 	
 	
 	
 	
-	
-	
-	
-	
-	
-	
-	
-	
+</tr>	
+<?php endforeach; ?>	
 </table>	
 </div>
 	
