40fea72223df969ebae133b99dac588bc2afab32
diddipoeler <famploeger@arcor.de>
2019-04-08 13:51:34 +0200

    Update default_pictures.php
    Code

--- site/views/matchreport/tmpl/default_pictures.php
+++ site/views/matchreport/tmpl/default_pictures.php
@@ -1,9 +1,9 @@
 <?php 
-/** SportsManagement ein Programm zur Verwaltung für alle Sportarten
+/** SportsManagement ein Programm zur Verwaltung fÃ¼r alle Sportarten
  * @version   1.0.05
  * @file      default_picture.php
  * @author    diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @copyright Copyright: Â© 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
  * @license   This file is part of SportsManagement.
  * @package   sportsmanagement
  * @subpackage matchreport
@@ -17,10 +17,10 @@ $rssitems_colums = $this->config['show_pictures_columns'] ;
 $pictures_width = $this->config['show_pictures_width'] ;
 
 if ($setItems > $actualItems) {
-			$totalItems = $actualItems;
-		} else {
-			$totalItems = $setItems;
-		}
+$totalItems = $actualItems;
+} else {
+$totalItems = $setItems;
+}
     
 ?>
 <div class="<?php echo $this->divclassrow;?> table-responsive" id="matchreport">
@@ -31,14 +31,13 @@ foreach ( $this->matchimages as $images )
 {
 
 if (($j % $rssitems_colums) == 0 ) : 
-							$row = 'row'.(floor($j / $rssitems_colums) % $rssitems_colums) ;
-
+$row = 'row'.(floor($j / $rssitems_colums) % $rssitems_colums) ;
 ?>
-					<tr class="<?php echo $row; ?>">
-					<?php endif; ?>
-					<td class="item" style="width:<?php echo floor(99/$rssitems_colums)."%";?>">
-          <a href="<?php echo $images->sitepath.DS.$images->name;?>" alt="<?php echo $images->name;?>" title="<?php echo $images->name;?>" class="highslide" onclick="return hs.expand(this)">
-					<?php
+<tr class="<?php echo $row; ?>">
+<?php endif; ?>
+<td class="item" style="width:<?php echo floor(99/$rssitems_colums)."%";?>">
+<a href="<?php echo $images->sitepath.DS.$images->name;?>" alt="<?php echo $images->name;?>" title="<?php echo $images->name;?>" class="highslide" onclick="return hs.expand(this)">
+<?php
 echo sportsmanagementHelperHtml::getBootstrapModalImage($images->name,
 $images->sitepath.DS.$images->name,
 $images->name,
@@ -47,14 +46,13 @@ $pictures_width,
 $this->modalwidth,
 $this->modalheight,
 $this->overallconfig['use_jquery_modal']);					
-					?>
-          </a>
-					</td>
-					<?php if (($j % $rssitems_colums) == ($rssitems_colums-1) ) : ?>
-					</tr>
-					<?php endif; ?>
-					<?php						
-						
+?>
+</a>
+</td>
+<?php if (($j % $rssitems_colums) == ($rssitems_colums-1) ) : ?>
+</tr>
+<?php endif; ?>
+<?php						
 $j++;
 }
 ?>
