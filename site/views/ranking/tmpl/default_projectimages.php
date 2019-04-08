ede8dc6f68b4196de31470e8f0589548133eeb80
diddipoeler <famploeger@arcor.de>
2019-04-08 13:47:29 +0200

    Update and rename default_projectimages to default_projectimages.php
    Code

---
+++ site/views/ranking/tmpl/default_projectimages.php
@@ -0,0 +1,16 @@
+<?php
+/** SportsManagement ein Programm zur Verwaltung fűr alle Sportarten
+ * @version   1.0.05
+ * @file      default_projectimages.php
+ * @author    diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license   This file is part of SportsManagement.
+ * @package   sportsmanagement
+ * @subpackage ranking
+ */
+defined('_JEXEC') or die('Restricted access');
+use Joomla\CMS\Language\Text;
+?>
+<div class="<?php echo $this->divclassrow;?>" id="projectimages">
+
+</div>
