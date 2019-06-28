d78c2d139989d9a5057bcb878e6aaac7f939cac7
diddipoeler <famploeger@arcor.de>
2019-06-28 16:30:00 +0200

    JED Checker

--- admin/libraries/google-php/Google/Client.php
+++ admin/libraries/google-php/Google/Client.php
@@ -1,8 +1,8 @@
 <?php
-/*
+/**
  * Copyright 2010 Google Inc.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * @license Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
