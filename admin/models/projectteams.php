854c1ad34b23e42b2394e29ba17b9797df3b750e
diddipoeler <famploeger@arcor.de>
2019-07-12 14:33:09 +0200

    #905

--- admin/models/projectteams.php
+++ admin/models/projectteams.php
@@ -501,7 +501,8 @@ function getCountryTeams()
 		$this->jsmquery->clear();
         // Select some fields
 		//$this->jsmquery->select('t.id AS value,t.name AS text,t.info,c.logo_big as picture');
-        $this->jsmquery->select('t.id AS value,t.name AS text,a.name as info,c.logo_big as picture');
+        //$this->jsmquery->select('t.id AS value,t.name AS text,a.name as info,c.logo_big as picture');
+        $this->jsmquery->select('t.id AS value,CONCAT(t.name," (", a.name,")") AS text,c.logo_big as picture');
         // From table
 		$this->jsmquery->from('#__sportsmanagement_team AS t');
         $this->jsmquery->join('INNER', '#__sportsmanagement_club AS c ON c.id = t.club_id');
