22a7fd628c7a43beae37ac0424d07feb0dc70b6e
diddipoeler <famploeger@arcor.de>
2019-08-02 17:53:17 +0200

    #917

--- admin/views/jsmgcalendars/view.html.php
+++ admin/views/jsmgcalendars/view.html.php
@@ -25,7 +25,6 @@ use Joomla\CMS\Toolbar\ToolbarHelper;
 class sportsmanagementViewjsmgcalendars extends sportsmanagementView 
 {
 
-
 /**
  * sportsmanagementViewjsmgcalendars::init()
  * 
@@ -45,15 +44,13 @@ public function init ()
     {
 		$jinput = Factory::getApplication()->input;
         $option = $jinput->getCmd('option');
-        $canDo = jsmGCalendarUtil::getActions();
-		if ($canDo->get('core.create')) {
+        //$canDo = jsmGCalendarUtil::getActions();
+		//if ($canDo->get('core.create')) {
 			ToolbarHelper::addNew('jsmgcalendar.add', 'JTOOLBAR_NEW');
 			ToolbarHelper::custom('jsmgcalendarimport.import', 'upload.png', 'upload.png', 'COM_SPORTSMANAGEMENT_JSMGCALENDAR_VIEW_GCALENDARS_BUTTON_IMPORT', false);
-		}
-
+		//}
        
         $this->icon = 'google-calendar-48-icon.png';
-        
 
 		parent::addToolbar();
 	}
