c3407d93dc8f3f51b524d663b70a53b7996266c4
diddipoeler <famploeger@arcor.de>
2019-11-12 08:02:47 +0100

    Update default.php
    joomla 4

--- site/views/rivals/tmpl/default.php
+++ site/views/rivals/tmpl/default.php
@@ -1,9 +1,9 @@
 <?php 
-/** SportsManagement ein Programm zur Verwaltung für alle Sportarten
+/** SportsManagement ein Programm zur Verwaltung fÃ¼r alle Sportarten
  * @version   1.0.05
  * @file      default.php
  * @author    diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @copyright Copyright: Â© 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
  * @license   GNU General Public License version 2 or later; see LICENSE.txt
  * @package   sportsmanagement
  * @subpackage rivals
@@ -19,7 +19,7 @@ $templatesToLoad = array('globalviews');
 sportsmanagementHelper::addTemplatePaths($templatesToLoad, $this);
 
 ?>
-<div class="container-fluid" id="">
+<div class="<?php echo $this->divclasscontainer;?>" id="rivals" >
 <?php
 if ( $this->config['show_sectionheader'] )
 {
