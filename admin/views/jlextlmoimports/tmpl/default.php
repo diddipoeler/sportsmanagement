3e5bb3c04576182a49166d0841774d052454aeab
diddipoeler <famploeger@arcor.de>
2019-11-18 17:08:00 +0100

    #934

--- admin/views/jlextlmoimports/tmpl/default.php
+++ admin/views/jlextlmoimports/tmpl/default.php
@@ -46,7 +46,7 @@ sportsmanagementHelper::addTemplatePaths($templatesToLoad, $this);
 </tr>				
 <tr>
 <td style='background-color:#EEEEEE'>
-<select name='copyTemplate' id='copyTemplate'>
+<select name='copyTemplate' id='copyTemplate' style="display:none">
 <option value="0" selected><?php echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_XML_IMPORT_TEMPLATES_USEOWN'); ?></option>
 <?php
 foreach ($this->templates AS $row)
