1b448d6e0ed9fcc7a84952f7b781b8c78a95edb0
diddipoeler <famploeger@arcor.de>
2019-11-19 14:58:32 +0100

    Update edit_extended.php
    code

--- admin/views/match/tmpl/edit_extended.php
+++ admin/views/match/tmpl/edit_extended.php
@@ -1,4 +1,14 @@
-<?php defined('_JEXEC') or die('Restricted access');
+<?php 
+/** SportsManagement ein Programm zur Verwaltung für alle Sportarten
+ * @version   1.0.05
+ * @file      edit_extended.php
+ * @author    diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license   GNU General Public License version 2 or later; see LICENSE.txt
+ * @package   sportsmanagement
+ * @subpackage match
+ */
+defined('_JEXEC') or die('Restricted access');
 use Joomla\CMS\Language\Text;
 foreach ($this->extended->getFieldsets() as $fieldset)
 {
