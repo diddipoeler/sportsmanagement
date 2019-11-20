85112b8c8c871a9682dc5141a605127f052d58cb
diddipoeler <famploeger@arcor.de>
2019-11-20 17:55:43 +0100

    code

--- admin/views/extensions/tmpl/default_4.php
+++ admin/views/extensions/tmpl/default_4.php
@@ -1,7 +1,7 @@
 <?php
 /** SportsManagement ein Programm zur Verwaltung für Sportarten
  * @version   1.0.05
- * @file      default_3.php
+ * @file      default_4.php
  * @author    diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
  * @copyright Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
  * @license   GNU General Public License version 2 or later; see LICENSE.txt
