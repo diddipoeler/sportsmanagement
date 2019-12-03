dff80f2e463ccd361c4855af01a1f28937e4802f
diddipoeler <famploeger@arcor.de>
2019-12-03 07:56:22 +0100

    Update greeting.php
    code

--- admin/models/rules/greeting.php
+++ admin/models/rules/greeting.php
@@ -1,14 +1,22 @@
 <?php
+/** SportsManagement ein Programm zur Verwaltung für Sportarten
+ * @version   1.0.05
+ * @file      superiorzero.php
+ * @author    diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license   GNU General Public License version 2 or later; see LICENSE.txt
+ * @package   sportsmanagement
+ * @subpackage rules
+ */
 
 defined('_JEXEC') or die('Restricted access');
- 
-// import Joomla formrule library
-jimport('joomla.form.formrule');
+
+use Joomla\CMS\Form\FormRule;
  
 /**
  * Form Rule class for the Joomla Framework.
  */
-class JFormRuleGreeting extends JFormRule
+class JFormRuleGreeting extends FormRule
 {
 	/**
 	 * The regular expression.
