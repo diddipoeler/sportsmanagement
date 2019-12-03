0b04208d41b40ca838989feb6b0129b73c00c604
diddipoeler <famploeger@arcor.de>
2019-12-03 07:58:47 +0100

    Update jevents.php
    code

--- modules/mod_sportsmanagement_calendar/connectors/jevents.php
+++ modules/mod_sportsmanagement_calendar/connectors/jevents.php
@@ -1,4 +1,14 @@
 <?php
+/** SportsManagement ein Programm zur Verwaltung für Sportarten
+ * @version   1.0.05
+ * @file      jevents.php
+ * @author    diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license   GNU General Public License version 2 or later; see LICENSE.txt
+ * @package   mod_sportsmanagement_calendar
+ * @subpackage rules
+ */
+
 defined('_JEXEC') or die('Restricted access');
 use Joomla\CMS\Factory;
 use Joomla\CMS\Router\Route;
