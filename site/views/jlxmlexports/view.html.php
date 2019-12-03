f2041add8246a5c717d8a0019b463e7887644d29
diddipoeler <famploeger@arcor.de>
2019-12-03 08:02:46 +0100

    Update view.html.php
    code

--- site/views/jlxmlexports/view.html.php
+++ site/views/jlxmlexports/view.html.php
@@ -1,4 +1,15 @@
-<?php defined( '_JEXEC' ) or die( 'Restricted access' );
+<?php 
+/** SportsManagement ein Programm zur Verwaltung für Sportarten
+ * @version   1.0.05
+ * @file      view.html.php
+ * @author    diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license   GNU General Public License version 2 or later; see LICENSE.txt
+ * @package   mod_sportsmanagement_calendar
+ * @subpackage jlxmlexports
+ */
+
+defined( '_JEXEC' ) or die( 'Restricted access' );
 
 use Joomla\CMS\Factory;
 
