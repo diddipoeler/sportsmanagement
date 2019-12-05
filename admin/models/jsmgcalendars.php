c67e600a38de8484b2ca8774c61b7b3ceadca795
diddipoeler <famploeger@arcor.de>
2019-12-05 15:50:31 +0100

    Update jsmgcalendars.php
    code

--- admin/models/jsmgcalendars.php
+++ admin/models/jsmgcalendars.php
@@ -34,6 +34,11 @@ class sportsmanagementModeljsmGCalendars extends ListModel
 
 	function check_google_api()
 	{
+$importFile = JPATH_ADMINISTRATOR.DIRECTORY_SEPARATOR.JSM_PATH.DIRECTORY_SEPARATOR.'libraries'.DIRECTORY_SEPARATOR.'google-php/Google/autoload.php';
+if (File::exists ( $importFile )) {
+Log::add(Text::_('Google API vorhanden'), Log::NOTICE, 'jsmerror');		
+}
+		
 		
 	}
 	
