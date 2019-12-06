a899a9b784dc2a3609c1d2b19cd082b84982b8cd
diddipoeler <famploeger@arcor.de>
2019-12-06 17:45:46 +0100

    #942

--- admin/views/match/tmpl/edit_matchdetails.php
+++ admin/views/match/tmpl/edit_matchdetails.php
@@ -24,15 +24,15 @@ use Joomla\CMS\Uri\Uri;
 });
 function toggle_altdecision() {
 	if (document.getElementById('jform_alt_decision').value == 0) {
-		document.getElementById'alt_decision_enter').style.display='none';
-		document.getElementById('team1_result_decision').disabled=true;
-		document.getElementById('team2_result_decision').disabled=true;
-		document.getElementById('decision_info').disabled=true;
+		document.getElementById'alt_decision_enter').style.display = 'none';
+		document.getElementById('team1_result_decision').disabled = true;
+		document.getElementById('team2_result_decision').disabled = true;
+		document.getElementById('decision_info').disabled = true;
 	} else {
-		document.getElementById('alt_decision_enter').style.display='block';
-		document.getElementById('team1_result_decision').disabled=false;
-		document.getElementById('team2_result_decision').disabled=false;
-		document.getElementById('decision_info').disabled=false;
+		document.getElementById('alt_decision_enter').style.display = 'block';
+		document.getElementById('team1_result_decision').disabled = false;
+		document.getElementById('team2_result_decision').disabled = false;
+		document.getElementById('decision_info').disabled = false;
 	}
 }
 </script>
