bdc7a6ff24fc9f757977f17bb0c16bc863baf535
diddipoeler <famploeger@arcor.de>
2019-12-14 13:25:13 +0100

    tree

--- site/views/jltournamenttree/view.html.php
+++ site/views/jltournamenttree/view.html.php
@@ -32,60 +32,61 @@ class sportsmanagementViewjltournamenttree extends sportsmanagementView {
      * @return void
      */
     function init() {
-        $option = Factory::getApplication()->input->getCmd('option');
-        // Get a refrence of the page instance in joomla
-        $document = Factory::getDocument();
-        if (version_compare(JSM_JVERSION, '4', 'eq')) {
-            $uri = Uri::getInstance();
-        } else {
-            $uri = Factory::getURI();
-        }
-        $app = Factory::getApplication();
-
-        $this->app = Factory::getApplication();
-        $this->jinput = $this->app->input;
-        //$this->projectid = Factory::getApplication()->input->getInt( "p", 0 );
-        sportsmanagementModelProject::setProjectID($this->jinput->getInt('p', 0), sportsmanagementModelProject::$cfg_which_database);
-        $this->project = sportsmanagementModelProject::getProject();
+//        $option = Factory::getApplication()->input->getCmd('option');
+//        // Get a refrence of the page instance in joomla
+//        $document = Factory::getDocument();
+//        if (version_compare(JSM_JVERSION, '4', 'eq')) {
+//            $uri = Uri::getInstance();
+//        } else {
+//            $uri = Factory::getURI();
+//        }
+//        $app = Factory::getApplication();
+//
+//        $this->app = Factory::getApplication();
+//        $this->jinput = $this->app->input;
+//        //$this->projectid = Factory::getApplication()->input->getInt( "p", 0 );
+//        sportsmanagementModelProject::setProjectID($this->jinput->getInt('p', 0), sportsmanagementModelProject::$cfg_which_database);
+//        $this->project = sportsmanagementModelProject::getProject();
 
         if ($this->project->project_type == 'TOURNAMENT_MODE') {
-            $model = $this->getModel();
-            $bracket_request = Factory::getApplication()->input->get();
-            $this->logo = $bracket_request['tree_logo'];
-            $this->color_from = $model->getColorFrom();
-            $this->color_to = $model->getColorTo();
-            $this->font_size = $model->getFontSize();
 
-            if (!$this->font_size) {
-                $this->font_size = '14';
-            }
+//            $model = $this->getModel();
+//            $bracket_request = Factory::getApplication()->input->get();
+//            $this->logo = $bracket_request['tree_logo'];
+//            $this->color_from = $model->getColorFrom();
+//            $this->color_to = $model->getColorTo();
+//            $this->font_size = $model->getFontSize();
+//
+//            if (!$this->font_size) {
+//                $this->font_size = '14';
+//            }
+//
+//            if (!$this->color_from) {
+//                $this->color_from = '#FFFFFF';
+//            }
+//            if (!$this->color_to) {
+//                $this->color_to = '#0000FF';
+//            }
+//
+//
+//            $this->rounds = $model->getTournamentRounds();
+//            $this->projectname = $model->getTournamentName();
+//            $this->bracket_rounds = $model->getTournamentBracketRounds($this->rounds);
+//            $this->bracket_teams = $model->getTournamentMatches($this->rounds);
+//            $this->bracket_results = $model->getTournamentResults($this->rounds);
+//            $this->which_first_round = $model->getWhichShowFirstRound();
+//            $this->jl_tree_bracket_round_width = $model->getTreeBracketRoundWidth();
+//            $this->jl_tree_bracket_teamb_width = $model->getTreeBracketTeambWidth();
+//            $this->jl_tree_bracket_width = $model->getTreeBracketWidth();
 
-            if (!$this->color_from) {
-                $this->color_from = '#FFFFFF';
-            }
-            if (!$this->color_to) {
-                $this->color_to = '#0000FF';
-            }
-
-
-            $this->rounds = $model->getTournamentRounds();
-            $this->projectname = $model->getTournamentName();
-            $this->bracket_rounds = $model->getTournamentBracketRounds($this->rounds);
-            $this->bracket_teams = $model->getTournamentMatches($this->rounds);
-            $this->bracket_results = $model->getTournamentResults($this->rounds);
-            $this->which_first_round = $model->getWhichShowFirstRound();
-            $this->jl_tree_bracket_round_width = $model->getTreeBracketRoundWidth();
-            $this->jl_tree_bracket_teamb_width = $model->getTreeBracketTeambWidth();
-            $this->jl_tree_bracket_width = $model->getTreeBracketWidth();
-
-            $document->addScript(Uri::base() . 'components/' . $option . '/assets/js/jquery.json-2.3.min.js');
-            $document->addScript(Uri::base() . 'components/' . $option . '/assets/js/jquery.bracket-3.js');
+            $this->document->addScript(Uri::base() . 'components/' . $option . '/assets/js/jquery.json-2.3.min.js');
+            $this->document->addScript(Uri::base() . 'components/' . $option . '/assets/js/jquery.bracket-3.js');
 
 // Add customstyles
             $stylelink = '<link rel="stylesheet" href="' . Uri::base() . 'components/' . $option . '/assets/css/jquery.bracket-3.css' . '" type="text/css" />' . "\n";
-            $document->addCustomTag($stylelink);
+            $this->document->addCustomTag($stylelink);
             $stylelink = '<link rel="stylesheet" href="' . Uri::base() . 'components/' . $option . '/assets/css/jquery.bracket-site.css' . '" type="text/css" />' . "\n";
-            $document->addCustomTag($stylelink);
+            $this->document->addCustomTag($stylelink);
 
 
             /*
