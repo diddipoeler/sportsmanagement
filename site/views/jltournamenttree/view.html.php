53701e8cb4df5c8ca256d1a3de7cc9d0c4a0a286
diddipoeler <famploeger@arcor.de>
2019-12-15 16:37:10 +0100

    tree

--- site/views/jltournamenttree/view.html.php
+++ site/views/jltournamenttree/view.html.php
@@ -32,45 +32,13 @@ class sportsmanagementViewjltournamenttree extends sportsmanagementView {
      * @return void
      */
     function init() {
-//        $option = Factory::getApplication()->input->getCmd('option');
-//        // Get a refrence of the page instance in joomla
-//        $document = Factory::getDocument();
-//        if (version_compare(JSM_JVERSION, '4', 'eq')) {
-//            $uri = Uri::getInstance();
-//        } else {
-//            $uri = Factory::getURI();
-//        }
-//        $app = Factory::getApplication();
-//
-//        $this->app = Factory::getApplication();
-//        $this->jinput = $this->app->input;
-//        //$this->projectid = Factory::getApplication()->input->getInt( "p", 0 );
-//        sportsmanagementModelProject::setProjectID($this->jinput->getInt('p', 0), sportsmanagementModelProject::$cfg_which_database);
-//        $this->project = sportsmanagementModelProject::getProject();
 
         if ($this->project->project_type == 'TOURNAMENT_MODE') {
 $this->rounds = $this->model->getTournamentRounds();
 
-//            $model = $this->getModel();
-//            $bracket_request = Factory::getApplication()->input->get();
-//            $this->logo = $bracket_request['tree_logo'];
             $this->color_from = $this->model->getColorFrom();
             $this->color_to = $this->model->getColorTo();
             $this->font_size = $this->model->getFontSize();
-//
-//            if (!$this->font_size) {
-//                $this->font_size = '14';
-//            }
-//
-//            if (!$this->color_from) {
-//                $this->color_from = '#FFFFFF';
-//            }
-//            if (!$this->color_to) {
-//                $this->color_to = '#0000FF';
-//            }
-//
-//
-//            $this->rounds = $model->getTournamentRounds();
             $this->projectname = $this->project->name;
             $this->bracket_rounds = $this->model->getTournamentBracketRounds($this->rounds);
             $this->bracket_teams = $this->model->getTournamentMatches($this->rounds);
@@ -88,26 +56,8 @@ $this->rounds = $this->model->getTournamentRounds();
             $this->document->addCustomTag($stylelink);
             $stylelink = '<link rel="stylesheet" href="' . Uri::base() . 'components/' . $this->option . '/assets/css/jquery.bracket-site.css' . '" type="text/css" />' . "\n";
             $this->document->addCustomTag($stylelink);
-
-
-            /*
-              $style = 'div.jQBracket {'
-              . '  font-family: "Arial";'
-              . '  font-size: 14px;'
-              . '  float: left;'
-              . '  clear: both;'
-              . '  position: relative;'
-              . '  background-color: #333333;'
-              . '  background: -webkit-gradient(linear, 0% 0%, 0% 100%, from('.$bracket_request['color_from'].'), to('.$bracket_request['color_to'].'));'
-              . '  background: -moz-linear-gradient(-90deg, '.$bracket_request['color_from'].', '.$bracket_request['color_to'].');'
-              . '  }';
-
-              $document->addStyleDeclaration( $style );
-             */
         }
 
-
-        //parent::display($tpl);
     }
 
 }
