5e149f4910185049a6e02cb78cf2011c07fe75c0
diddipoeler <famploeger@arcor.de>
2019-12-18 07:39:06 +0100

    Update smextxmleditor.php
    #952

--- admin/models/smextxmleditor.php
+++ admin/models/smextxmleditor.php
@@ -1,9 +1,9 @@
 <?php
-/** SportsManagement ein Programm zur Verwaltung für Sportarten
+/** SportsManagement ein Programm zur Verwaltung fÃ¼r Sportarten
  * @version   1.0.05
  * @file      smextxmleditor.php
  * @author    diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @copyright Copyright: Â© 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
  * @license   GNU General Public License version 2 or later; see LICENSE.txt
  * @package   sportsmanagement
  * @subpackage models
@@ -27,58 +27,11 @@ use Joomla\CMS\Log\Log;
  * @version $Id$
  * @access public
  */
-class sportsmanagementModelsmextxmleditor extends AdminModel
+class sportsmanagementModelsmextxmleditor extends JSMModelAdmin
 {
 
-    /**
-	 * Method to get the record form.
-	 *
-	 * @param	array	$data		Data for the form.
-	 * @param	boolean	$loadData	True if the form is to load its own data (default case), false if not.
-	 * @return	mixed	A JForm object on success, false on failure
-	 * @since	1.6
-	 */
-	public function getForm($data = array(), $loadData = true) 
-	{
-		// Reference global application object
-        $app = Factory::getApplication();
-        // JInput object
-        $jinput = $app->input;
-        $option = $jinput->getCmd('option');
-        $cfg_which_media_tool = ComponentHelper::getParams($option)->get('cfg_which_media_tool',0);
-        // Get the form.
-		$form = $this->loadForm('com_sportsmanagement.smextxmleditor', 'smextxmleditor', array('control' => 'jform', 'load_data' => $loadData));
-		if (empty($form)) 
-		{
-			return false;
-		}
-        /*        
-        $form->setFieldAttribute('picture', 'default', ComponentHelper::getParams($option)->get('ph_icon',''));
-        $form->setFieldAttribute('picture', 'directory', 'com_sportsmanagement/database/agegroups');
-        $form->setFieldAttribute('picture', 'type', $cfg_which_media_tool);
-        */
-		return $form;
-	}
     
-    /**
-	 * Method to get the data that should be injected in the form.
-	 *
-	 * @return	mixed	The data for the form.
-	 * @since	1.6
-	 */
-	protected function loadFormData()
-	{
-		// Check the session for previously entered form data.
-		//$data = Factory::getApplication()->getUserState('com_templates.edit.source.data', array());
-        $data = Factory::getApplication()->getUserState('com_sportsmanagement.edit.source.data', array());
-
-		if (empty($data)) {
-			$data = $this->getSource();
-		}
-
-		return $data;
-	}
-    
+	
     
   /**
 	 * Method to store the source file contents.
