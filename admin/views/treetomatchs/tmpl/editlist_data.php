ac73bb9667bb45214614292ac58ac12f0096d2aa
diddipoeler <famploeger@arcor.de>
2019-12-19 10:26:40 +0100

    Update editlist_data.php
    tree

--- admin/views/treetomatchs/tmpl/editlist_data.php
+++ admin/views/treetomatchs/tmpl/editlist_data.php
@@ -8,57 +8,42 @@
  * @package   sportsmanagement
  * @subpackage treetomatchs
  */
+
 defined( '_JEXEC' ) or die( 'Restricted access' );
 use Joomla\CMS\Language\Text;
 ?>
-
-
-	
-
-			<legend>
-				<?php
-				echo Text::sprintf( 'COM_SPORTSMANAGEMENT_ADMIN_TREETOMATCH_ASSIGN_TITLE', '<i>' . $this->projectws->name . '</i>');
-				?>
-			</legend>
-			<table class="table" border="0">
-				<tr>
-					<td>
-						<b>
-							<?php
-							echo Text::_( 'COM_SPORTSMANAGEMENT_ADMIN_TREETOMATCH_ASSIGN_AVAIL_MATCHES' );
-							?>
-						</b><br />
-						<?php
-						echo $this->lists['matches'];
-						?>
-					</td>
-					<td style="">
-						
+<legend>
+<?php
+echo Text::sprintf( 'COM_SPORTSMANAGEMENT_ADMIN_TREETOMATCH_ASSIGN_TITLE', '<i>' . $this->projectws->name . '</i>');
+?>
+</legend>
+<table class="table" border="0">
+<tr>
+<td>
+<b>
+<?php
+echo Text::_( 'COM_SPORTSMANAGEMENT_ADMIN_TREETOMATCH_ASSIGN_AVAIL_MATCHES' );
+?>
+</b><br />
+<?php
+echo $this->lists['matches'];
+?>
+</td>
+<td style="">
 <input id="moveright" type="button" value="<?php echo Text::_('&gt;&gt;'); ?>" onclick="move_list_items('matcheslist','node_matcheslist');" />                        
 <input id="moveleft" type="button" value="<?php echo Text::_('&lt;&lt;'); ?>" onclick="move_list_items('node_matcheslist','matcheslist');" />                        
-                        <!--
-                        &nbsp;&nbsp;
-						<input	type="button" class="inputbox"
-								onclick="handleLeftToRight();"
-								value="&gt;&gt;" />
-						&nbsp;&nbsp;<br />&nbsp;&nbsp;
-					 	<input	type="button" class="inputbox"
-					 			onclick="handleRightToLeft();"
-								value="&lt;&lt;" />
-						&nbsp;&nbsp;
-                        -->
-					</td>
-					<td>
-						<b>
-							<?php
-							echo Text::_( 'COM_SPORTSMANAGEMENT_ADMIN_TREETOMATCH_ASSIGN_NODE_MATCHES' );
-							?>
-						</b><br />
-						<?php
-						echo $this->lists['node_matches'];
-						?>
-					</td>
-			   </tr>
-			</table>
+</td>
+<td>
+<b>
+<?php
+echo Text::_( 'COM_SPORTSMANAGEMENT_ADMIN_TREETOMATCH_ASSIGN_NODE_MATCHES' );
+?>
+</b><br />
+<?php
+echo $this->lists['node_matches'];
+?>
+</td>
+</tr>
+</table>
 
 		
