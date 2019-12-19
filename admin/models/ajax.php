b7d2783320553a57f020f47009f2440128912147
diddipoeler <famploeger@arcor.de>
2019-12-19 13:06:26 +0100

    Update ajax.php
    birthday

--- admin/models/ajax.php
+++ admin/models/ajax.php
@@ -1060,7 +1060,7 @@ $option = $app->input->getCmd('option');
 		if ( $season_id )
         {
 	$season_id = explode(",", $season_id);		
-        if ( is_array( $season_id )
+        if ( is_array( $season_id ) )
         {
         $ids = implode(",",array_map('intval', $season_id));
         $query->where('p.season_id IN (' . $ids .')' );    
