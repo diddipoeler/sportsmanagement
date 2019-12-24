f0fc8acc4e74feab0ee22aa1bca9775c30d19f35
diddipoeler <famploeger@arcor.de>
2019-12-24 13:04:46 +0100

    code

--- site/views/editmatch/tmpl/editlineup.php
+++ site/views/editmatch/tmpl/editlineup.php
@@ -13,7 +13,7 @@ defined('_JEXEC') or die('Restricted access');
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\Router\Route;
 use Joomla\CMS\HTML\HTMLHelper;
-HTMLHelper::_('behavior.framework'); 
+ 
 $params = $this->form->getFieldsets('params');
 ?>
 <script type="text/javascript">
