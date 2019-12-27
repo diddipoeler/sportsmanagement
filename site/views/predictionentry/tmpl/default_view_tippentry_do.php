9142c2bd29fc1cfc462e0f4de60963da358b6b98
diddipoeler <famploeger@arcor.de>
2019-12-27 14:32:08 +0100

    #916

--- site/views/predictionentry/tmpl/default_view_tippentry_do.php
+++ site/views/predictionentry/tmpl/default_view_tippentry_do.php
@@ -181,13 +181,11 @@ if ( $predictionProject->use_cards )
 <?php echo Text::_( 'COM_SPORTSMANAGEMENT_ADMIN_PGAMES_USE_YELLOW_CARDS' ); ?>
 <input type="text" id="yellowcards_<?php echo (int)sportsmanagementModelPrediction::$roundID; ?>" name="yellowcards[<?php echo $predictionProject->project_id; ?>][<?php echo (int)sportsmanagementModelPrediction::$roundID; ?>]"
 value="<?php echo 0; ?>" />
-</td>
-<td>
+
 <?php echo Text::_( 'COM_SPORTSMANAGEMENT_ADMIN_PGAMES_USE_YELLOW_RED_CARDS' ); ?>
 <input type="text" id="yellowredcards_<?php echo (int)sportsmanagementModelPrediction::$roundID; ?>" name="yellowredcards[<?php echo $predictionProject->project_id; ?>][<?php echo (int)sportsmanagementModelPrediction::$roundID; ?>]"
 value="<?php echo 0; ?>" />
-</td>
-<td>
+
 <?php echo Text::_( 'COM_SPORTSMANAGEMENT_ADMIN_PGAMES_USE_RED_CARDS' ); ?>
 <input type="text" id="redcards_<?php echo (int)sportsmanagementModelPrediction::$roundID; ?>" name="redcards[<?php echo $predictionProject->project_id; ?>][<?php echo (int)sportsmanagementModelPrediction::$roundID; ?>]"
 value="<?php echo 0; ?>" />
