beb3e30bb8111928e81f11098f00d51e52537214
diddipoeler <famploeger@arcor.de>
2020-01-05 16:17:25 +0100

    code

--- admin/views/template/view.html.php
+++ admin/views/template/view.html.php
@@ -90,13 +90,18 @@ if ( ComponentHelper::getParams($this->option)->get('show_debug_info_backend') )
 {
 $this->app->enqueueMessage(Text::_(__METHOD__.' '.__LINE__.' teile2 -> '.TVarDumper::dump($teile2,10,TRUE).''),'');    
 $this->app->enqueueMessage(Text::_(__METHOD__.' '.__LINE__.' count -> '.TVarDumper::dump($count,10,TRUE).''),'');    
-$this->app->enqueueMessage(Text::_(__METHOD__.' '.__LINE__.' colors_ranking -> '.TVarDumper::dump($colors_ranking,10,TRUE).''),'');
 }            
               
             list($colors_ranking[$count]['von'], $colors_ranking[$count]['bis'], $colors_ranking[$count]['color'], $colors_ranking[$count]['text'] ) = $teile2;
             $count++;
               }
             }
+            
+if ( ComponentHelper::getParams($this->option)->get('show_debug_info_backend') )
+{
+$this->app->enqueueMessage(Text::_(__METHOD__.' '.__LINE__.' colors_ranking -> '.TVarDumper::dump($colors_ranking,10,TRUE).''),'');
+}                        
+            
             $this->form->setValue('colors_ranking', null, $colors_ranking);
             
             break;
