fb3665e2f72c94fcf1c9b5f6473bce2db6fe919d
diddipoeler <famploeger@arcor.de>
2020-01-07 20:27:38 +0100

    #969

---
+++ site/views/globalviews/tmpl/default_selectround.php
@@ -0,0 +1,19 @@
+<?php 
+/** SportsManagement ein Programm zur Verwaltung für alle Sportarten
+ * @version   1.0.05
+ * @file      default_selectround.php
+ * @author    diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license   GNU General Public License version 2 or later; see LICENSE.txt
+ * @package   sportsmanagement
+ * @subpackage resultsranking
+ */
+
+defined( '_JEXEC' ) or die( 'Restricted access' );
+use Joomla\CMS\HTML\HTMLHelper;
+?>
+<div class="row" id="selectround">
+<?php
+echo HTMLHelper::_('select.genericlist', $this->matchdaysoptions, 'select-round', 'onchange="sportsmanagement_changedoc(this);" style="float:right;"', 'value', 'text', $this->currenturl);
+?>
+</div>
