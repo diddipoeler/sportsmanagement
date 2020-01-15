bef02aacfa7ad3197db5dd3e810871b9adefcac2
diddipoeler <famploeger@arcor.de>
2020-01-15 10:19:45 +0100

    Update default.php
    code

--- site/views/rankingalltime/tmpl/default.php
+++ site/views/rankingalltime/tmpl/default.php
@@ -1,9 +1,9 @@
 <?php
-/** SportsManagement ein Programm zur Verwaltung für alle Sportarten
+/** SportsManagement ein Programm zur Verwaltung fÃ¼r alle Sportarten
  * @version   1.0.05
  * @file      default.php
  * @author    diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @copyright Copyright: Â© 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
  * @license   GNU General Public License version 2 or later; see LICENSE.txt
  * @package   sportsmanagement
  * @subpackage rankingalltime
@@ -11,20 +11,13 @@
 
 defined('_JEXEC') or die('Restricted access');
 
-/**
- * Make sure that in case extensions are written for mentioned (common) views,
- * that they are loaded i.s.o. of the template of this view
- */
 $templatesToLoad = array('globalviews');
 sportsmanagementHelper::addTemplatePaths($templatesToLoad, $this);
 
 ?>
-
 <div class="<?php echo $this->divclasscontainer;?>" id="defaultrankingalltime">
-	<?php
-
+<?php
 echo $this->loadTemplate('projectheading');
 echo $this->loadTemplate('ranking');
-
 ?>
 </div>
