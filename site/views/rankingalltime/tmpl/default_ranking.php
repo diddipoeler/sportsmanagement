31e9c7ddd5586454dac31022a0fa47e41296221f
diddipoeler <famploeger@arcor.de>
2020-01-15 11:10:37 +0100

    Update default_ranking.php
    code

--- site/views/rankingalltime/tmpl/default_ranking.php
+++ site/views/rankingalltime/tmpl/default_ranking.php
@@ -8,10 +8,66 @@
  * @package   sportsmanagement
  * @subpackage rankingalltime
  */
-
+  
+use Joomla\CMS\Language\Text; 
+use Joomla\CMS\Router\Route;
 defined( '_JEXEC' ) or die( 'Restricted access' );?>
+ 
 <div class="<?php echo $this->divclassrow;?> table-responsive">
 <!-- content -->
+  
+<div class="panel-group" id="projectnames">
+
+<div class="panel panel-default">
+<div class="panel-heading">
+<h4 class="panel-title">
+<a data-toggle="collapse" data-parent="#projectnames" href="#viewprojectnames"><?php echo Text::_('COM_SPORTSMANAGEMENT_ALLPROJECTS_PAGE_TITLE'); ?></a>
+</h4>
+</div>
+<div id="viewprojectnames" class="panel-collapse collapse">
+<div class="panel-body">
+<table class="table">
+  <?php
+ 
+  foreach($this->projectnames as $value )
+  {
+$createroute = array("option" => "com_sportsmanagement",
+	"view" => "ranking",
+        "cfg_which_database" => 0,
+        "s" => 0,
+	"p" => $value->project_slug,
+        "type" => 0,
+        "r" => 0,
+        "from" => 0,
+        "to" => 0,
+        "division" => 0, );
+$query = sportsmanagementHelperRoute::buildQuery( $createroute );
+$link = Route::_( 'index.php?' . $query, false );     
+    
+?>
+<tr>
+<td>
+<a href="<?PHP echo $link;  ?>" class="btn btn-primary btn-lg btn-block" role="button">  
+<?PHP
+echo Text::_( $value->name  );
+?>    
+</a>
+</td>
+</tr>
+<?php
+}
+?>
+</table>  
+</div>                      
+</div>                      
+</div>                        
+  
+</div>  
+  
+  
+  
+  
+  
 <?php
 foreach ( $this->currentRanking as $division => $cu_rk )
 {
