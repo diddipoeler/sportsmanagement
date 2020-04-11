f477b0cd545d7227933edd282231592025d29801
diddipoeler <famploeger@arcor.de>
2020-04-11 11:58:03 +0200

    update

--- plugins/jcemediabox/plugins/system/jcemediabox/jcemediabox.php
+++ plugins/jcemediabox/plugins/system/jcemediabox/jcemediabox.php
@@ -39,7 +39,7 @@ class plgSystemJCEMediabox extends JPlugin
     {
         JPlugin::loadLanguage('plg_system_jcemediabox', JPATH_ADMINISTRATOR);
 
-        $words = array('close', 'next', 'previous', 'cancel', 'numbers', 'numbers_count');
+        $words = array('close', 'next', 'previous', 'cancel', 'numbers', 'numbers_count', 'download');
 
         $v = array();
 
