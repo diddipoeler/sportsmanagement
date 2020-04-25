0b6aee495057cd2d4ed3c4e4cdfbfb1947423454
diddipoeler <famploeger@arcor.de>
2020-04-25 17:46:22 +0200

    update

--- site/views/nextmatch/view.html.php
+++ site/views/nextmatch/view.html.php
@@ -17,6 +17,7 @@ defined('_JEXEC') or die('Restricted access');
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Uri\Uri;
+use Joomla\CMS\Factory;
 
 /**
  * sportsmanagementViewNextMatch
