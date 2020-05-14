b9291fd0c1d1298091f7daf3ea2e3d7f64a18cfb
diddipoeler <diddipoeler@gmx.de>
2020-05-14 07:29:11 +0200

    Update finaltablerank.php
    code

--- admin/models/fields/finaltablerank.php
+++ admin/models/fields/finaltablerank.php
@@ -4,7 +4,7 @@
  * @version    1.0.05
  * @package    Sportsmanagement
  * @subpackage fields
- * @file       leaguelevel.php
+ * @file       finaltablerank.php
  * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
  * @copyright  Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
