cf526bda8a5ee494b73853cacb26255ccdbb54ae
diddipoeler <diddipoeler@gmx.de>
2020-05-14 09:53:56 +0200

    Update ranking.php
    code

--- site/helpers/ranking.php
+++ site/helpers/ranking.php
@@ -1,8 +1,6 @@
 <?php
 /**
- *
  * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- *
  * @version    1.0.05
  * @package    Sportsmanagement
  * @subpackage helpers
@@ -753,8 +751,6 @@ class JSMRanking
 	{
 		$app    = Factory::getApplication();
 		$option = $app->input->getCmd('option');
-
-		// Create a new query object.
 		$db        = sportsmanagementHelper::getDBConnection(true, $cfg_which_database);
 		$query     = $db->getQuery(true);
 		$starttime = microtime();
@@ -762,7 +758,7 @@ class JSMRanking
 		$query->select('pt.id AS ptid, pt.is_in_score, pt.start_points, pt.division_id');
 		$query->select('t.name, t.id as teamid, pt.neg_points_finally');
 		$query->select('pt.use_finally, pt.points_finally,pt.matches_finally,pt.won_finally,pt.draws_finally,pt.lost_finally');
-		$query->select('pt.homegoals_finally, pt.guestgoals_finally,pt.diffgoals_finally,pt.penalty_points');
+		$query->select('pt.homegoals_finally, pt.guestgoals_finally,pt.diffgoals_finally,pt.penalty_points,pt.finaltablerank');
 		$query->select('CONCAT_WS(\':\',pt.id,t.alias) AS ptid_slug');
 		$query->from('#__sportsmanagement_project_team AS pt ');
 		$query->join('INNER', '#__sportsmanagement_season_team_id AS st1 ON st1.id = pt.team_id');
