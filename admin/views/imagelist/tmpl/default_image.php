6a4d207fa5df3ebbe07652b2bbd685e8a93cf737
diddipoeler <famploeger@arcor.de>
2020-05-29 17:21:36 +0200

    #1072

--- admin/views/imagelist/tmpl/default_image.php
+++ admin/views/imagelist/tmpl/default_image.php
@@ -1,9 +1,18 @@
 <?php
-
+/**
+ * SportsManagement ein Programm zur Verwaltung für Sportarten
+ * @version    1.0.05
+ * @package    Sportsmanagement
+ * @subpackage imagelist
+ * @file       default_imagelist.php
+ * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright  Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 defined('_JEXEC') or die;
 
 use Joomla\Registry\Registry;
-
+use Joomla\CMS\Language\Text;
 $params     = new Registry;
 $dispatcher = JEventDispatcher::getInstance();
 $dispatcher->trigger('onContentBeforeDisplay', array('com_media.file', &$this->_tmp_img, &$params));
@@ -15,7 +24,7 @@ echo '<pre>'.print_r($this->_tmp_img,true).'</pre>';
 <li class="imgOutline thumbnail height-80 width-80 center">
 	<a class="img-preview" href="javascript:ImageManager.populateFields('<?php echo $this->_tmp_img->path_relative; ?>')" title="<?php echo $this->_tmp_img->name; ?>" >
 		<div class="height-50">
-			<?php echo JHtml::_('image', $this->baseURL . '/' . $this->_tmp_img->path_relative, JText::sprintf('COM_MEDIA_IMAGE_TITLE', $this->_tmp_img->name, JHtml::_('number.bytes', $this->_tmp_img->size)), array('width' => $this->_tmp_img->width_60, 'height' => $this->_tmp_img->height_60)); ?>
+			<?php echo JHtml::_('image', $this->_tmp_img->path_relative.'/'.$this->_tmp_img->file, Text::sprintf('COM_MEDIA_IMAGE_TITLE', $this->_tmp_img->name, JHtml::_('number.bytes', $this->_tmp_img->size)), array('width' => $this->_tmp_img->width_60, 'height' => $this->_tmp_img->height_60)); ?>
 		</div>
 		<div class="small">
 			<?php echo JHtml::_('string.truncate', $this->_tmp_img->name, 10, false); ?>
