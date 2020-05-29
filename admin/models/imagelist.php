851421dcf9d1dc6efca4f1c02c2111605fd7ebbe
diddipoeler <diddipoeler@gmx.de>
2020-05-29 08:57:55 +0200

    Create imagelist.php
    #1072

---
+++ admin/models/imagelist.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * SportsManagement ein Programm zur Verwaltung für Sportarten
+ * @version    1.0.05
+ * @package    Sportsmanagement
+ * @subpackage imagelist
+ * @file       imagelist.php
+ * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright  Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die('Restricted access');
+
+use Joomla\CMS\MVC\Model\BaseDatabaseModel;
+use Joomla\CMS\Component\ComponentHelper;
+use Joomla\CMS\Factory;
+use Joomla\CMS\Filter\InputFilter;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Uri\Uri;
+use Joomla\Filesystem\File;
+use Joomla\Filesystem\Folder;
+use Joomla\Filesystem\Path;
+
+/**
+ * sportsmanagementModelimagelist
+ *
+ * @package
+ * @author    diddi
+ * @copyright 2014
+ * @version   $Id$
+ * @access    public
+ */
+class sportsmanagementModelimagelist extends BaseDatabaseModel
+{
+
+public static function getFiles($path, $scopeName)
+{
+$filesOutput = [];
+$files = Folder::files($directory);
+$directories = Folder::folders($directory);
+
+echo '<pre>'.print_r($files,true).'</pre>';
+echo '<pre>'.print_r($directories,true).'</pre>';
+
+
+
+
+
+      
+}
+
+
+
+
+}
