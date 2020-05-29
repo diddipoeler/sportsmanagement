a5d7a09f00ff5a4211a022e73f789750627aa8c5
diddipoeler <diddipoeler@gmx.de>
2020-05-29 08:38:43 +0200

    Create view.html.php
    #1072

---
+++ admin/views/imagelist/view.html.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * SportsManagement ein Programm zur Verwaltung für Sportarten
+ * @version    1.0.05
+ * @package    Sportsmanagement
+ * @subpackage imagelist
+ * @file       view.html.php
+ * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright  Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * https://www.jqueryscript.net/form/Drag-Drop-File-Upload-Dialog-with-jQuery-Bootstrap.html
+ */
+defined('_JEXEC') or die('Restricted access');
+
+use Joomla\String\StringHelper;
+use Joomla\CMS\Application\WebApplication;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Factory;
+use Joomla\CMS\Component\ComponentHelper;
+
+/**
+ * sportsmanagementViewimagelist
+ *
+ * @package
+ * @author    diddi
+ * @copyright 2014
+ * @version   $Id$
+ * @access    public
+ */
+class sportsmanagementViewimagelist extends sportsmanagementView
+{
+
+
+	/**
+	 * sportsmanagementViewImagehandler::init()
+	 *
+	 * @return
+	 */
+	public function init()
+	{
+  
+  }
+  
+  }
