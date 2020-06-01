047bbc53eed95598e05f95c73a5e9b1fcc36b62e
diddipoeler <famploeger@arcor.de>
2020-06-01 10:12:57 +0200

    #1072

--- admin/views/imagelist/tmpl/default.php
+++ admin/views/imagelist/tmpl/default.php
@@ -20,8 +20,8 @@ use Joomla\CMS\Uri\Uri;
 use Joomla\CMS\Log\Log;
 $templatesToLoad = array('footer', 'listheader');
 sportsmanagementHelper::addTemplatePaths($templatesToLoad, $this);
-$modalheight = ComponentHelper::getParams(Factory::getApplication()->input->getCmd('option'))->get('modal_popup_height', 600);
-$modalwidth  = ComponentHelper::getParams(Factory::getApplication()->input->getCmd('option'))->get('modal_popup_width', 900);
+//$modalheight = ComponentHelper::getParams(Factory::getApplication()->input->getCmd('option'))->get('modal_popup_height', 600);
+//$modalwidth  = ComponentHelper::getParams(Factory::getApplication()->input->getCmd('option'))->get('modal_popup_width', 900);
 $link = 'index.php?option=com_sportsmanagement&view=imagehandler&layout=uploaddraganddrop&type=&field=&fieldid=&tmpl=component';
 //JHtml::_('script', 'media/popup-imagemanager.min.js', array('version' => 'auto', 'relative' => true));
 //JHtml::_('stylesheet', 'media/popup-imagemanager.css', array('version' => 'auto', 'relative' => true));
@@ -31,7 +31,7 @@ $link = 'index.php?option=com_sportsmanagement&view=imagehandler&layout=uploaddr
 ?>
 <div class="button2-left"><div class="blank">";
 <?php
-echo sportsmanagementHelper::getBootstrapModalImage('upload', '', Text::_('JLIB_HTML_BEHAVIOR_UPLOADER_CURRENT_TITLE'), '20', Uri::base() . $link, $modalwidth, $modalheight);
+echo sportsmanagementHelper::getBootstrapModalImage('upload'.$this->project_id, '', Text::_('JLIB_HTML_BEHAVIOR_UPLOADER_CURRENT_TITLE'), '20', Uri::base() . $link, $this->modalwidth , $this->modalheight );
 ?>
 </div></div>
 
