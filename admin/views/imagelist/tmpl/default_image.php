a9526689e983f99ab89cc562b16cd87ca47d4488
diddipoeler <famploeger@arcor.de>
2020-06-01 12:01:19 +0200

    #1072

--- admin/views/imagelist/tmpl/default_image.php
+++ admin/views/imagelist/tmpl/default_image.php
@@ -26,7 +26,7 @@ $dispatcher->trigger('onContentBeforeDisplay', array('com_media.file', &$this->_
 
 echo sportsmanagementHelper::getBootstrapModalImage(
 							$this->_tmp_img->name,
-							Uri::root() . $this->_tmp_img->path_relative.'/'.$this->_tmp_img->file,
+							Uri::root() . 'images/com_sportsmanagement/database/'.$this->_tmp_img->path_relative.'/'.$this->_tmp_img->file,
 							Text::_('COM_SPORTSMANAGEMENT_ADMIN_CLUBS_EDIT_DETAILS'),
 							$this->_tmp_img->width_60,
 							'',
@@ -45,7 +45,7 @@ echo sportsmanagementHelper::getBootstrapModalImage(
 <li class="imgOutline thumbnail height-80 width-80 center">
 	<a class="img-preview" href="" >
 		<div class="height-50">
-			<?php echo JHtml::_('image', $this->_tmp_img->path_relative.'/'.$this->_tmp_img->file, Text::sprintf('COM_MEDIA_IMAGE_TITLE', $this->_tmp_img->name, JHtml::_('number.bytes', $this->_tmp_img->size)), array('width' => $this->_tmp_img->width_60, 'height' => $this->_tmp_img->height_60)); ?>
+			<?php echo JHtml::_('image', 'images/com_sportsmanagement/database/'.$this->_tmp_img->path_relative.'/'.$this->_tmp_img->file, Text::sprintf('COM_MEDIA_IMAGE_TITLE', $this->_tmp_img->name, JHtml::_('number.bytes', $this->_tmp_img->size)), array('width' => $this->_tmp_img->width_60, 'height' => $this->_tmp_img->height_60)); ?>
 		</div>
 		<div class="small">
 			<?php echo JHtml::_('string.truncate', $this->_tmp_img->name, 10, false); ?>
