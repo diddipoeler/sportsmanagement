d04a14a696fc2667bb3b1b495d4a776b42752875
diddipoeler <diddipoeler@gmx.de>
2020-06-03 08:31:39 +0200

    Update imagelist.php
    #1072

--- admin/models/imagelist.php
+++ admin/models/imagelist.php
@@ -35,7 +35,7 @@ class sportsmanagementModelimagelist extends ListModel
 var $_identifier = "imagelist";
 var $limitstart = 0;
 var $limit = 0;
-static $filesOutput = array();
+static public $filesOutput = array();
 	
 public function __construct($config = array())
 	{
