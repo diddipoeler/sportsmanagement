93165bfd242224723efa9730292c1059b913c38a
diddipoeler <diddipoeler@gmx.de>
2020-06-03 14:53:09 +0200

    Update default.php
    #1072

--- admin/views/imagelist/tmpl/default.php
+++ admin/views/imagelist/tmpl/default.php
@@ -24,15 +24,18 @@ $link = 'index.php?option=com_sportsmanagement&view=imagehandler&layout=uploaddr
 //JHtml::_('script', 'media/popup-imagemanager.min.js', array('version' => 'auto', 'relative' => true));
 //JHtml::_('stylesheet', 'media/popup-imagemanager.css', array('version' => 'auto', 'relative' => true));
 
+//echo '<pre>'.print_r($this->uri,true).'</pre>';
+
 
 //echo '<pre>'.print_r($this->images,true).'</pre>';
 ?>
+<div class="container-fluid" id="allleagues">  
 <div class="button2-left"><div class="blank">
 <?php
 echo sportsmanagementHelper::getBootstrapModalImage('upload'.$this->project_id, '', Text::_('JLIB_HTML_BEHAVIOR_UPLOADER_CURRENT_TITLE'), '20', Uri::base() . $link, $this->modalwidth , $this->modalheight );
 ?>
 </div></div>
-
+<form name="adminForm" id="adminForm" action="<?php echo htmlspecialchars($this->uri->toString()); ?>"
 <?php if ( count($this->images) > 0 ) : ?>
 	<ul class="manager thumbnails">
 		<?php for ($i = 0, $n = count($this->images); $i < $n; $i++) :
@@ -54,3 +57,31 @@ echo sportsmanagementHelper::getBootstrapModalImage('upload'.$this->project_id, 
     </p>
 	<?php echo $this->pagination->getPagesLinks(); ?>
 </div>
+   <input type="hidden" name="limitstart" value=""/>
+  <input type="hidden" name="option" value="com_sportsmanagement"/>
+  
+  
+            <input type="hidden" name="view" value="imagelist"/>
+            <input type="hidden" name="imagelist" value="1"/>
+            <input type="hidden" name="asset" value="com_sportsmanagement"/>
+            <input type="hidden" name="folder" value="clubs/large"/>
+            <input type="hidden" name="author" value=""/>
+            <input type="hidden" name="fieldid" value="jform_logo_big"/>
+            <input type="hidden" name="tmpl" value="component"/>
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+</form>
+  </div>
