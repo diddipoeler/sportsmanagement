ff6b54869db2a62c11e390593d841bb8e27bf163
diddipoeler <famploeger@arcor.de>
2020-06-05 17:53:26 +0200

    joomla4

--- admin/views/databasetools/tmpl/default.php
+++ admin/views/databasetools/tmpl/default.php
@@ -1,8 +1,6 @@
 <?php
 /**
- *
  * SportsManagement ein Programm zur Verwaltung für Sportarten
- *
  * @version    1.0.05
  * @package    Sportsmanagement
  * @subpackage databasetools
@@ -11,20 +9,20 @@
  * @copyright  Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
-
 defined('_JEXEC') or die('Restricted access');
-
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\Router\Route;
 
 $templatesToLoad = array('footer', 'listheader');
 sportsmanagementHelper::addTemplatePaths($templatesToLoad, $this);
-HTMLHelper::_('behavior.tooltip');
+//HTMLHelper::_('behavior.tooltip');
 ?>
     <form action="<?php echo $this->request_url; ?>" method="post" id="adminForm" name="adminForm">
 
 		<?PHP
+        echo $this->loadTemplate('joomla_version');
+        /*
 		if (version_compare(JVERSION, '3.0.0', 'ge'))
 		{
 			echo $this->loadTemplate('joomla3');
@@ -33,157 +31,10 @@ HTMLHelper::_('behavior.tooltip');
 		{
 			echo $this->loadTemplate('joomla2');
 		}
+        */
 		?>
 
-        <!--	<div id="editcell"> -->
-        <table class="table">
-            <thead>
-            <tr>
-                <th class="title" class="nowrap">
-					<?php
-					echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_DBTOOLS_TOOL');
-					?>
-                </th>
-                <th class="title" class="nowrap">
-					<?php
-					echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_DBTOOLS_DESCR');
-					?>
-                </th>
-            </tr>
-            </thead>
-            <tfoot>
-            <tr>
-                <td colspan="2">
-					<?php
-					echo "&nbsp;";
-					?>
-                </td>
-            </tr>
-            </tfoot>
-            <tbody>
 
-            <tr>
-                <td class="nowrap" valign="top">
-					<?php
-					$link = Route::_('index.php?option=com_sportsmanagement&view=databasetool&task=databasetool.truncate');
-					?>
-
-                    <a href="<?php echo $link; ?>"
-                       title="<?php echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_DBTOOLS_TRUNCATE2'); ?>">
-						<?php
-						echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_DBTOOLS_TRUNCATE');
-						?>
-                    </a>
-                </td>
-                <td>
-					<?php
-					echo Text::_("COM_SPORTSMANAGEMENT_ADMIN_DBTOOLS_TRUNCATE_DESCR");
-					?>
-                </td>
-            </tr>
-
-            <tr>
-                <td class="nowrap" valign="top">
-					<?php
-					$link = Route::_('index.php?option=com_sportsmanagement&view=databasetool&task=databasetool.truncatejl');
-					?>
-
-                    <a href="<?php echo $link; ?>"
-                       title="<?php echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_DBTOOLS_TRUNCATE2JL'); ?>">
-						<?php
-						echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_DBTOOLS_TRUNCATEJL');
-						?>
-                    </a>
-                </td>
-                <td>
-					<?php
-					echo Text::_("COM_SPORTSMANAGEMENT_ADMIN_DBTOOLS_TRUNCATEJL_DESCR");
-					?>
-                </td>
-            </tr>
-
-            <tr>
-                <td class="nowrap" valign="top">
-					<?php
-					$link = Route::_('index.php?option=com_sportsmanagement&view=databasetool&task=databasetool.optimize');
-					?>
-                    <a href="<?php echo $link; ?>"
-                       title="<?php echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_DBTOOLS_OPTIMIZE2'); ?>">
-						<?php
-						echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_DBTOOLS_OPTIMIZE');
-						?>
-                    </a>
-                </td>
-                <td>
-					<?php
-					echo Text::_("COM_SPORTSMANAGEMENT_ADMIN_DBTOOLS_OPTIMIZE_DESCR");
-					?>
-                </td>
-            </tr>
-
-            <tr>
-                <td class="nowrap" valign="top">
-					<?php
-					$link = Route::_('index.php?option=com_sportsmanagement&view=databasetool&task=databasetool.repair');
-					?>
-
-                    <a href="<?php echo $link; ?>"
-                       title="<?php echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_DBTOOLS_REPAIR2'); ?>">
-						<?php
-						echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_DBTOOLS_REPAIR');
-						?>
-                    </a>
-                </td>
-                <td>
-					<?php
-					echo Text::_("COM_SPORTSMANAGEMENT_ADMIN_DBTOOLS_REPAIR_DESCR");
-					?>
-                </td>
-            </tr>
-
-            <tr>
-                <td class="nowrap" valign="top">
-					<?php
-					$link = Route::_('index.php?option=com_sportsmanagement&view=databasetool&task=databasetool.picturepath');
-					?>
-
-                    <a href="<?php echo $link; ?>"
-                       title="<?php echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_DBTOOLS_PICTURE_PATH_MIGRATION2'); ?>">
-						<?php
-						echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_DBTOOLS_PICTURE_PATH_MIGRATION');
-						?>
-                    </a>
-                </td>
-                <td>
-					<?php
-					echo Text::_("COM_SPORTSMANAGEMENT_ADMIN_DBTOOLS_PICTURE_PATH_MIGRATION_DESCR");
-					?>
-                </td>
-            </tr>
-
-            <tr>
-                <td class="nowrap" valign="top">
-					<?php
-					$link = Route::_('index.php?option=com_sportsmanagement&view=databasetool&task=databasetool.updatetemplatemasters');
-					?>
-
-                    <a href="<?php echo $link; ?>"
-                       title="<?php echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_DBTOOLS_UPDATE_TEMPLATE_MASTERS2'); ?>">
-						<?php
-						echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_DBTOOLS_UPDATE_TEMPLATE_MASTERS');
-						?>
-                    </a>
-                </td>
-                <td>
-					<?php
-					echo Text::_("COM_SPORTSMANAGEMENT_ADMIN_DBTOOLS_UPDATE_TEMPLATE_MASTERS_DESCR");
-					?>
-                </td>
-            </tr>
-
-            </tbody>
-        </table>
-        <!--	</div> -->
 
         <input type="hidden" name="task" value="databasetool.execute"/>
 		<?php echo HTMLHelper::_('form.token'); ?>
