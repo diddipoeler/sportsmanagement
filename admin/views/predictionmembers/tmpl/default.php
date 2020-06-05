fbd20933669a7acb1429974c3188d92c53129251
diddipoeler <diddipoeler@gmx.de>
2020-06-05 12:07:19 +0200

    Update default.php
    joomla4

--- admin/views/predictionmembers/tmpl/default.php
+++ admin/views/predictionmembers/tmpl/default.php
@@ -10,7 +10,7 @@
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 defined('_JEXEC') or die('Restricted access');
-//use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\HTML\HTMLHelper;
 
 $templatesToLoad = array('footer', 'listheader');
 sportsmanagementHelper::addTemplatePaths($templatesToLoad, $this);
