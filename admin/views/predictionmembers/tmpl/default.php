f6c2d7bb1eb97541b1e7a758a06650cc79d7ca86
diddipoeler <diddipoeler@gmx.de>
2020-06-05 12:05:10 +0200

    Update default.php
    joomla4

--- admin/views/predictionmembers/tmpl/default.php
+++ admin/views/predictionmembers/tmpl/default.php
@@ -1,8 +1,6 @@
 <?php
 /**
- *
  * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- *
  * @version    1.0.05
  * @package    Sportsmanagement
  * @subpackage predictionmembers
@@ -11,10 +9,8 @@
  * @copyright  Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
-
 defined('_JEXEC') or die('Restricted access');
-
-use Joomla\CMS\HTML\HTMLHelper;
+//use Joomla\CMS\HTML\HTMLHelper;
 
 $templatesToLoad = array('footer', 'listheader');
 sportsmanagementHelper::addTemplatePaths($templatesToLoad, $this);
