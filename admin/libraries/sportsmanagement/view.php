c83e81c11bf8b0ecbba557c810d5356e585065c7
diddipoeler <famploeger@arcor.de>
2020-06-10 15:24:39 +0200

    joomla4

--- admin/libraries/sportsmanagement/view.php
+++ admin/libraries/sportsmanagement/view.php
@@ -120,7 +120,7 @@ class sportsmanagementView extends HtmlView
 		$this->document->addStyleSheet(Uri::root() . 'components/com_sportsmanagement/assets/css/flex.css', 'text/css');
         if (version_compare(substr(JVERSION, 0, 3), '4.0', 'ge'))
         {
-        $this->document->addScript(Uri::root() . '/components/com_sportsmanagement/assets/js/joomla4functions.js');
+        $this->document->addScript(Uri::root() . 'administrator/components/com_sportsmanagement/assets/js/joomla4functions.js');
         }
 		$this->document->addScript(Uri::root() . '/components/com_sportsmanagement/assets/js/sm_functions.js');
 		$this->jinput         = $this->app->input;
