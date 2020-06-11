b9cfac05cebeb011c16f9983e7bfc9cb853eb596
diddipoeler <famploeger@arcor.de>
2020-06-11 10:40:40 +0200

    joomla4

--- admin/views/agegroups/tmpl/default_data.php
+++ admin/views/agegroups/tmpl/default_data.php
@@ -131,16 +131,16 @@ $this->dragable_group = 'data-dragable-group="<?php echo $item->catid; ?>"';
 		$k = 0;
 		for ($i = 0, $n = count($this->items); $i < $n; $i++)
 		{
-			$row        = &$this->items[$i];
+			$this->datarow        = &$this->items[$i];
 if (version_compare(substr(JVERSION, 0, 3), '4.0', 'ge'))
 {
-$this->dragable_group = 'data-dragable-group="'.$row->ordering.'"';
+$this->dragable_group = 'data-dragable-group="'.$this->datarow->ordering.'"';
 } 
-			$link       = Route::_('index.php?option=com_sportsmanagement&task=agegroup.edit&id=' . $row->id);
+			$link       = Route::_('index.php?option=com_sportsmanagement&task=agegroup.edit&id=' . $this->datarow->id);
 			$canEdit    = $this->user->authorise('core.edit', 'com_sportsmanagement');
-			$canCheckin = $this->user->authorise('core.manage', 'com_checkin') || $row->checked_out == $this->user->get('id') || $row->checked_out == 0;
-			$checked    = HTMLHelper::_('jgrid.checkedout', $i, $this->user->get('id'), $row->checked_out_time, 'agegroups.', $canCheckin);
-			$canChange  = $this->user->authorise('core.edit.state', 'com_sportsmanagement.agegroup.' . $row->id) && $canCheckin;
+			$canCheckin = $this->user->authorise('core.manage', 'com_checkin') || $this->datarow->checked_out == $this->user->get('id') || $this->datarow->checked_out == 0;
+			$checked    = HTMLHelper::_('jgrid.checkedout', $i, $this->user->get('id'), $this->datarow->checked_out_time, 'agegroups.', $canCheckin);
+			$canChange  = $this->user->authorise('core.edit.state', 'com_sportsmanagement.agegroup.' . $this->datarow->id) && $canCheckin;
 			?>
             <tr class="<?php echo "row$k"; ?>" <?php echo $this->dragable_group; ?>>
                 <td class="center">
@@ -150,50 +150,50 @@ $this->dragable_group = 'data-dragable-group="'.$row->ordering.'"';
                 </td>
                 <td class="center">
 					<?php
-					echo HTMLHelper::_('grid.id', $i, $row->id);
+					echo HTMLHelper::_('grid.id', $i, $this->datarow->id);
 					?>
                 </td>
 				<?php
 				$inputappend = '';
 				?>
                 <td class="center">
-					<?php if ($row->checked_out) : ?>
-						<?php echo HTMLHelper::_('jgrid.checkedout', $i, $row->editor, $row->checked_out_time, 'agegroups.', $canCheckin); ?>
+					<?php if ($this->datarow->checked_out) : ?>
+						<?php echo HTMLHelper::_('jgrid.checkedout', $i, $this->datarow->editor, $this->datarow->checked_out_time, 'agegroups.', $canCheckin); ?>
 					<?php endif; ?>
 					<?php if ($canEdit) : ?>
-                        <a href="<?php echo Route::_('index.php?option=com_sportsmanagement&task=agegroup.edit&id=' . (int) $row->id); ?>">
-							<?php echo $this->escape($row->name); ?></a>
+                        <a href="<?php echo Route::_('index.php?option=com_sportsmanagement&task=agegroup.edit&id=' . (int) $this->datarow->id); ?>">
+							<?php echo $this->escape($this->datarow->name); ?></a>
 					<?php else : ?>
-						<?php echo $this->escape($row->name); ?>
+						<?php echo $this->escape($this->datarow->name); ?>
 					<?php endif; ?>
                     <input tabindex="2" type="hidden" size="30" maxlength="64" class="form-control form-control-inline"
-                           name="name<?php echo $row->id; ?>" value="<?php echo $row->name; ?>"
+                           name="name<?php echo $this->datarow->id; ?>" value="<?php echo $this->datarow->name; ?>"
                            onchange="document.getElementById('cb<?php echo $i; ?>').checked = true"/>
 
 
 					<?php //echo $checked;  ?>
 
-					<?php //echo $row->name;  ?>
+					<?php //echo $this->datarow->name;  ?>
                     <p class="smallsub">
-						<?php echo Text::sprintf('JGLOBAL_LIST_ALIAS', $this->escape($row->alias)); ?></p>
+						<?php echo Text::sprintf('JGLOBAL_LIST_ALIAS', $this->escape($this->datarow->alias)); ?></p>
                 </td>
 				<?php ?>
 
 
-                <td><?php echo $row->age_from; ?></td>
-                <td><?php echo $row->age_to; ?></td>
-                <td><?php echo $row->deadline_day; ?></td>
-                <td class="center"><?php echo JSMCountries::getCountryFlag($row->country); ?></td>
+                <td><?php echo $this->datarow->age_from; ?></td>
+                <td><?php echo $this->datarow->age_to; ?></td>
+                <td><?php echo $this->datarow->deadline_day; ?></td>
+                <td class="center"><?php echo JSMCountries::getCountryFlag($this->datarow->country); ?></td>
 
                 <td class="center">
 					<?php
-					if (empty($row->picture) || !File::exists(JPATH_SITE . DIRECTORY_SEPARATOR . $row->picture))
+					if (empty($this->datarow->picture) || !File::exists(JPATH_SITE . DIRECTORY_SEPARATOR . $this->datarow->picture))
 					{
-						$imageTitle = Text::_('COM_SPORTSMANAGEMENT_ADMIN_PERSONS_NO_IMAGE') . $row->picture;
+						$imageTitle = Text::_('COM_SPORTSMANAGEMENT_ADMIN_PERSONS_NO_IMAGE') . $this->datarow->picture;
                         $image_attributes['title'] = $imageTitle;
 						echo HTMLHelper::_('image', 'administrator/components/com_sportsmanagement/assets/images/delete.png', $imageTitle, $image_attributes);
 					}
-                    elseif ($row->picture == sportsmanagementHelper::getDefaultPlaceholder("player"))
+                    elseif ($this->datarow->picture == sportsmanagementHelper::getDefaultPlaceholder("player"))
 					{
 						$imageTitle = Text::_('COM_SPORTSMANAGEMENT_ADMIN_PERSONS_DEFAULT_IMAGE');
                         $image_attributes['title'] = $imageTitle;
@@ -201,12 +201,12 @@ $this->dragable_group = 'data-dragable-group="'.$row->ordering.'"';
 					}
 					else
 					{
-						//$playerName = sportsmanagementHelper::formatName(null ,$row->firstname, $row->nickname, $row->lastname, 0);
-						//echo sportsmanagementHelper::getPictureThumb($row->picture, $playerName, 0, 21, 4);
+						//$playerName = sportsmanagementHelper::formatName(null ,$this->datarow->firstname, $this->datarow->nickname, $this->datarow->lastname, 0);
+						//echo sportsmanagementHelper::getPictureThumb($this->datarow->picture, $playerName, 0, 21, 4);
 						?>
-                        <a href="<?php echo Uri::root() . $row->picture; ?>" title="<?php echo $row->name; ?>"
+                        <a href="<?php echo Uri::root() . $this->datarow->picture; ?>" title="<?php echo $this->datarow->name; ?>"
                            class="modal">
-                            <img src="<?php echo Uri::root() . $row->picture; ?>" alt="<?php echo $row->name; ?>"
+                            <img src="<?php echo Uri::root() . $this->datarow->picture; ?>" alt="<?php echo $this->datarow->name; ?>"
                                  width="20"/>
                         </a>
 						<?PHP
@@ -214,58 +214,28 @@ $this->dragable_group = 'data-dragable-group="'.$row->ordering.'"';
 					?>
                 </td>
 
-                <td class=""><?php echo Text::_($row->sportstype); ?></td>
+                <td class=""><?php echo Text::_($this->datarow->sportstype); ?></td>
                 <td class="center">
                     <div class="btn-group">
-						<?php echo HTMLHelper::_('jgrid.published', $row->published, $i, 'agegroups.', $canChange, 'cb'); ?>
+						<?php echo HTMLHelper::_('jgrid.published', $this->datarow->published, $i, 'agegroups.', $canChange, 'cb'); ?>
 						<?php
 						// Create dropdown items and render the dropdown list.
 						if ($canChange)
 						{
-							HTMLHelper::_('actionsdropdown.' . ((int) $row->published === 2 ? 'un' : '') . 'archive', 'cb' . $i, 'agegroups');
-							HTMLHelper::_('actionsdropdown.' . ((int) $row->published === -2 ? 'un' : '') . 'trash', 'cb' . $i, 'agegroups');
-							echo HTMLHelper::_('actionsdropdown.render', $this->escape($row->name));
+							HTMLHelper::_('actionsdropdown.' . ((int) $this->datarow->published === 2 ? 'un' : '') . 'archive', 'cb' . $i, 'agegroups');
+							HTMLHelper::_('actionsdropdown.' . ((int) $this->datarow->published === -2 ? 'un' : '') . 'trash', 'cb' . $i, 'agegroups');
+							echo HTMLHelper::_('actionsdropdown.render', $this->escape($this->datarow->name));
 						}
 						?>
                     </div>
                 </td>
                 <td class="order">
-     
- <?php if ($this->saveOrder) : ?>
-                	<?php if ($this->sortDirection == 'asc') : ?>
-								<span><?php echo $this->pagination->orderUpIcon($i, $row->ordering - 1, $this->view.'.orderup', 'JLIB_HTML_MOVE_UP', $this->ordering); ?></span>
-								<span><?php echo $this->pagination->orderDownIcon($i, $n, $row->ordering + 1, $this->view.'.orderdown', 'JLIB_HTML_MOVE_DOWN', $this->ordering); ?></span>
-							<?php elseif ($this->sortDirection == 'desc') : ?>
-								<span><?php echo $this->pagination->orderUpIcon($i, $row->ordering - 1, $this->view.'.orderdown', 'JLIB_HTML_MOVE_UP', $this->ordering); ?></span>
-								<span><?php echo $this->pagination->orderDownIcon($i, $n, $row->ordering + 1, $this->view.'.orderup', 'JLIB_HTML_MOVE_DOWN', $this->ordering); ?></span>
-							<?php endif; ?>
-                <?php endif; ?>
-                        <?php $disabled = $this->saveOrder ? '' : 'disabled="disabled"';?>
-                    <input type="text" name="order[]" size="5"
-                           value="<?php echo $row->ordering; ?>" <?php echo $disabled; ?>
-                           class="form-control form-control-inline" style="text-align: center"/>
-                           <?php
-if (version_compare(substr(JVERSION, 0, 3), '4.0', 'ge'))
-{
-$iconClass = '';
-if (!$this->saveOrder)
-{
-$iconClass = ' inactive" title="' . Text::_('JORDERINGDISABLED');
-}
+<?php
+echo $this->loadTemplate('data_order');
 ?>
-<span class="sortable-handler <?php echo $iconClass ?>">
-<span class="fas fa-ellipsis-v" aria-hidden="true"></span>
-</span>
-<?php    
-}                  
-               
-               
-               
-               
-               //echo $this->loadTemplate('data_order');
-               ?>
+
                 </td>
-                <td class="center"><?php echo $row->id; ?></td>
+                <td class="center"><?php echo $this->datarow->id; ?></td>
             </tr>
 			<?php
 			$k = 1 - $k;
