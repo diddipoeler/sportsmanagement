81dc108458bb9050fd5e8f4d9f40b7f99aa2da66
diddipoeler <famploeger@arcor.de>
2020-06-22 18:45:50 +0200

    code

--- admin/views/leagues/tmpl/default_data.php
+++ admin/views/leagues/tmpl/default_data.php
@@ -121,62 +121,68 @@ $this->dragable_group = 'data-dragable-group="<?php echo $item->catid; ?>"';
         </td>
     </tr>
     </tfoot>
-    <tbody>
+    <tbody <?php if ( $this->saveOrder && version_compare(substr(JVERSION, 0, 3), '4.0', 'ge') ) :?> class="js-draggable" data-url="<?php echo $saveOrderingUrl; ?>" data-direction="<?php echo strtolower($this->sortDirection); ?>" data-nested="true"<?php endif; ?>>
 	<?php
 	$k = 0;
-	for ($i = 0, $n = count($this->items); $i < $n; $i++)
+	//for ($i = 0, $n = count($this->items); $i < $n; $i++)
+    foreach ($this->items as $i => $this->item)
 	{
-		$row        = &$this->items[$i];
-		$link       = Route::_('index.php?option=com_sportsmanagement&task=league.edit&id=' . $row->id);
+		//$row        = &$this->items[$i];
+$this->count_i = $i;
+if (version_compare(substr(JVERSION, 0, 3), '4.0', 'ge'))
+{
+$this->dragable_group = 'data-dragable-group="'.$this->item->ordering.'"';
+}         
+		$link       = Route::_('index.php?option=com_sportsmanagement&task=league.edit&id=' . $this->item->id);
 		$canEdit    = $this->user->authorise('core.edit', 'com_sportsmanagement');
-		$canCheckin = $this->user->authorise('core.manage', 'com_checkin') || $row->checked_out == $this->user->get('id') || $row->checked_out == 0;
-		$checked    = HTMLHelper::_('jgrid.checkedout', $i, $this->user->get('id'), $row->checked_out_time, 'leagues.', $canCheckin);
-		$canChange  = $this->user->authorise('core.edit.state', 'com_sportsmanagement.league.' . $row->id) && $canCheckin;
+		$canCheckin = $this->user->authorise('core.manage', 'com_checkin') || $this->item->checked_out == $this->user->get('id') || $this->item->checked_out == 0;
+		$checked    = HTMLHelper::_('jgrid.checkedout', $this->count_i, $this->user->get('id'), $this->item->checked_out_time, 'leagues.', $canCheckin);
+		$canChange  = $this->user->authorise('core.edit.state', 'com_sportsmanagement.league.' . $this->item->id) && $canCheckin;
 		?>
         <tr class="<?php echo "row$k"; ?>">
             <td class="center">
 				<?php
-				echo $this->pagination->getRowOffset($i);
+				echo $this->pagination->getRowOffset($this->count_i);
 				?>
             </td>
             <td class="center">
 				<?php
-				echo HTMLHelper::_('grid.id', $i, $row->id);
+				echo HTMLHelper::_('grid.id', $this->count_i, $this->item->id);
 				?>
             </td>
 			<?php
 			$inputappend = '';
 			?>
             <td class="center">
-				<?php if ($row->checked_out) : ?>
-					<?php echo HTMLHelper::_('jgrid.checkedout', $i, $row->editor, $row->checked_out_time, 'leagues.', $canCheckin); ?>
+				<?php if ($this->item->checked_out) : ?>
+					<?php echo HTMLHelper::_('jgrid.checkedout', $this->count_i, $this->item->editor, $this->item->checked_out_time, 'leagues.', $canCheckin); ?>
 				<?php endif; ?>
 				<?php if ($canEdit) : ?>
-                    <a href="<?php echo Route::_('index.php?option=com_sportsmanagement&task=league.edit&id=' . (int) $row->id); ?>">
-						<?php echo $this->escape($row->name); ?></a>
+                    <a href="<?php echo Route::_('index.php?option=com_sportsmanagement&task=league.edit&id=' . (int) $this->item->id); ?>">
+						<?php echo $this->escape($this->item->name); ?></a>
 				<?php else : ?>
-					<?php echo $this->escape($row->name); ?>
+					<?php echo $this->escape($this->item->name); ?>
 				<?php endif; ?>
                 <div class="small">
-					<?php echo Text::sprintf('JGLOBAL_LIST_ALIAS', $this->escape($row->alias)); ?>
+					<?php echo Text::sprintf('JGLOBAL_LIST_ALIAS', $this->escape($this->item->alias)); ?>
                 </div>
             </td>
-            <td><?php echo $row->short_name; ?></td>
+            <td><?php echo $this->item->short_name; ?></td>
             <td class="center">
 				<?php
-				echo JSMCountries::getCountryFlag($row->country);
-				$append = ' onchange="document.getElementById(\'cb' . $i . '\').checked=true" style="width:150px;background-color:#bbffff" size="1" ';
-				echo HTMLHelper::_('select.genericlist', $this->lists['nation'], 'country' . $row->id, 'class="form-control form-control-inline" ' . $append, 'value', 'text', $row->country);
+				echo JSMCountries::getCountryFlag($this->item->country);
+				$append = ' onchange="document.getElementById(\'cb' . $this->count_i . '\').checked=true" style="width:150px;background-color:#bbffff" size="1" ';
+				echo HTMLHelper::_('select.genericlist', $this->lists['nation'], 'country' . $this->item->id, 'class="form-control form-control-inline" ' . $append, 'value', 'text', $this->item->country);
 				?>
             </td>
-            <td class="center"><?php echo Text::_($row->sportstype); ?></td>
+            <td class="center"><?php echo Text::_($this->item->sportstype); ?></td>
             <td class="center">
 				<?php
 				$inputappend = '';
 				$append      = ' style="background-color:#bbffff"';
 				echo HTMLHelper::_(
-					'select.genericlist', $this->lists['agegroup'], 'agegroup' . $row->id, $inputappend . 'class="form-control form-control-inline" size="1" onchange="document.getElementById(\'cb' .
-					$i . '\').checked=true"' . $append, 'value', 'text', $row->agegroup_id
+					'select.genericlist', $this->lists['agegroup'], 'agegroup' . $this->item->id, $inputappend . 'class="form-control form-control-inline" size="1" onchange="document.getElementById(\'cb' .
+					$this->count_i . '\').checked=true"' . $append, 'value', 'text', $this->item->agegroup_id
 				);
 				?>
             </td>
@@ -184,10 +190,10 @@ $this->dragable_group = 'data-dragable-group="<?php echo $item->catid; ?>"';
 				<?php
 
 				$imageTitle = '';
-				$append     = ' onchange="document.getElementById(\'cb' . $i . '\').checked=true" style="background-color:#bbffff"';
-				if (isset($this->lists['association'][$row->country]))
+				$append     = ' onchange="document.getElementById(\'cb' . $this->count_i . '\').checked=true" style="background-color:#bbffff"';
+				if (isset($this->lists['association'][$this->item->country]))
 				{
-					echo HTMLHelper::_('select.genericlist', $this->lists['association'][$row->country], 'association' . $row->id, 'class="form-control form-control-inline" size="1"' . $append, 'value', 'text', $row->associations);
+					echo HTMLHelper::_('select.genericlist', $this->lists['association'][$this->item->country], 'association' . $this->item->id, 'class="form-control form-control-inline" size="1"' . $append, 'value', 'text', $this->item->associations);
 				}
 				else
 				{
@@ -198,13 +204,13 @@ $this->dragable_group = 'data-dragable-group="<?php echo $item->catid; ?>"';
             </td>
             <td class="center">
 				<?php
-				if (empty($row->picture))
+				if (empty($this->item->picture))
 				{
-					$imageTitle = Text::_('COM_SPORTSMANAGEMENT_ADMIN_PERSONS_NO_IMAGE') . COM_SPORTSMANAGEMENT_PICTURE_SERVER . $row->picture;
+					$imageTitle = Text::_('COM_SPORTSMANAGEMENT_ADMIN_PERSONS_NO_IMAGE') . COM_SPORTSMANAGEMENT_PICTURE_SERVER . $this->item->picture;
                     $image_attributes['title'] = $imageTitle;
 					echo HTMLHelper::_('image', 'administrator/components/com_sportsmanagement/assets/images/delete.png', $imageTitle, $image_attributes);
 				}
-                elseif ($row->picture == sportsmanagementHelper::getDefaultPlaceholder("player"))
+                elseif ($this->item->picture == sportsmanagementHelper::getDefaultPlaceholder("player"))
 				{
 					$imageTitle = Text::_('COM_SPORTSMANAGEMENT_ADMIN_PERSONS_DEFAULT_IMAGE');
                     $image_attributes['title'] = $imageTitle;
@@ -213,10 +219,10 @@ $this->dragable_group = 'data-dragable-group="<?php echo $item->catid; ?>"';
 				else
 				{
 					?>
-                    <a href="<?php echo COM_SPORTSMANAGEMENT_PICTURE_SERVER . $row->picture; ?>"
-                       title="<?php echo $row->name; ?>" class="modal">
-                        <img src="<?php echo COM_SPORTSMANAGEMENT_PICTURE_SERVER . $row->picture; ?>"
-                             alt="<?php echo $row->name; ?>" width="20"/>
+                    <a href="<?php echo COM_SPORTSMANAGEMENT_PICTURE_SERVER . $this->item->picture; ?>"
+                       title="<?php echo $this->item->name; ?>" class="modal">
+                        <img src="<?php echo COM_SPORTSMANAGEMENT_PICTURE_SERVER . $this->item->picture; ?>"
+                             alt="<?php echo $this->item->name; ?>" width="20"/>
                     </a>
 					<?PHP
 				}
@@ -232,18 +238,18 @@ $this->dragable_group = 'data-dragable-group="<?php echo $item->catid; ?>"';
 				);
 
 				$html   = array();
-				$html[] = '<fieldset id="published_act_season' . $row->id . '" class="' . $class . '" >';
+				$html[] = '<fieldset id="published_act_season' . $this->item->id . '" class="' . $class . '" >';
 				foreach ($options as $in => $option)
 				{
-					$checked = ($option->value == $row->published_act_season) ? ' checked="checked"' : '';
-					$btn     = ($option->value == $row->published_act_season && $row->published_act_season) ? ' active btn-success' : ' ';
-					$btn     = ($option->value == $row->published_act_season && !$row->published_act_season) ? ' active btn-danger' : $btn;
+					$checked = ($option->value == $this->item->published_act_season) ? ' checked="checked"' : '';
+					$btn     = ($option->value == $this->item->published_act_season && $this->item->published_act_season) ? ' active btn-success' : ' ';
+					$btn     = ($option->value == $this->item->published_act_season && !$this->item->published_act_season) ? ' active btn-danger' : $btn;
 
-					$onchange = ' onchange="document.getElementById(\'cb' . $i . '\').checked=true"';
-					$html[]   = '<input type="radio" style="display:none;" id="published_act_season' . $row->id . $in . '" name="published_act_season' . $row->id . '" value="'
+					$onchange = ' onchange="document.getElementById(\'cb' . $this->count_i . '\').checked=true"';
+					$html[]   = '<input type="radio" style="display:none;" id="published_act_season' . $this->item->id . $in . '" name="published_act_season' . $this->item->id . '" value="'
 						. $option->value . '"' . $onchange . ' />';
 
-					$html[] = '<label for="published_act_season' . $row->id . $in . '"' . $checked . ' class="btn' . $btn . '" >'
+					$html[] = '<label for="published_act_season' . $this->item->id . $in . '"' . $checked . ' class="btn' . $btn . '" >'
 						. Text::_($option->text) . '</label>';
 
 				}
@@ -253,14 +259,14 @@ $this->dragable_group = 'data-dragable-group="<?php echo $item->catid; ?>"';
             </td>
             <td class="center">
                 <div class="btn-group">
-					<?php echo HTMLHelper::_('jgrid.published', $row->published, $i, 'leagues.', $canChange, 'cb'); ?>
+					<?php echo HTMLHelper::_('jgrid.published', $this->item->published, $this->count_i, 'leagues.', $canChange, 'cb'); ?>
 					<?php
 					/**  Create dropdown items and render the dropdown list. */
 					if ($canChange)
 					{
-						HTMLHelper::_('actionsdropdown.' . ((int) $row->published === 2 ? 'un' : '') . 'archive', 'cb' . $i, 'leagues');
-						HTMLHelper::_('actionsdropdown.' . ((int) $row->published === -2 ? 'un' : '') . 'trash', 'cb' . $i, 'leagues');
-						echo HTMLHelper::_('actionsdropdown.render', $this->escape($row->name));
+						HTMLHelper::_('actionsdropdown.' . ((int) $this->item->published === 2 ? 'un' : '') . 'archive', 'cb' . $this->count_i, 'leagues');
+						HTMLHelper::_('actionsdropdown.' . ((int) $this->item->published === -2 ? 'un' : '') . 'trash', 'cb' . $this->count_i, 'leagues');
+						echo HTMLHelper::_('actionsdropdown.render', $this->escape($this->item->name));
 					}
 					?>
                 </div>
@@ -270,7 +276,7 @@ $this->dragable_group = 'data-dragable-group="<?php echo $item->catid; ?>"';
 echo $this->loadTemplate('data_order');
 ?>
 </td>
-            <td class="center"><?php echo $row->id; ?></td>
+            <td class="center"><?php echo $this->item->id; ?></td>
         </tr>
 		<?php
 		$k = 1 - $k;
