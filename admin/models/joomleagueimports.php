04a3f543a3a57f45fdadbc6aa7f6c21b5b7c815c
diddipoeler <diddipoeler@gmx.de>
2020-06-29 14:14:12 +0200

    Update joomleagueimports.php
    code

--- admin/models/joomleagueimports.php
+++ admin/models/joomleagueimports.php
@@ -199,7 +199,9 @@ class sportsmanagementModeljoomleagueimports extends ListModel
 		  //echo JText::sprintf('JLIB_DATABASE_ERROR_FUNCTION_FAILED', $e->getCode(), $e->getMessage()) . '<br />'; mysql_errno($this->connection)
           
           Log::add(Text::sprintf('JLIB_DATABASE_ERROR_FUNCTION_FAILED', $e->getCode(), $e->getMessage()) . '<br />', Log::NOTICE, 'jsmerror');
-//Log::add(Text::_('<pre>'.print_r($e,true).'</pre>' ), Log::ERROR, 'jsmerror');			
+			
+	$logmessage = sportsmanagementModeldatabasetool::getExeptionMessage($e->getCode(),$e->getMessage()); 		
+Log::add(Text::_('<pre>'.print_r($logmessage,true).'</pre>' ), Log::ERROR, 'jsmerror');			
           //Log::add(Text::_($db->getErrorNumber() ), Log::ERROR, 'jsmerror');
           //Log::add(Text::_(mysql_errno($db) ), Log::ERROR, 'jsmerror');
           //Log::add(Text::_('<pre>'.print_r($db,true).'</pre>' ), Log::ERROR, 'jsmerror');
