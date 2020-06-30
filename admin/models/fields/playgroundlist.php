992fbb49b110e9151e869489cec4d5cd3f0ff131
diddipoeler <diddipoeler@gmx.de>
2020-06-30 10:20:34 +0200

    Update playgroundlist.php
    #1079

--- admin/models/fields/playgroundlist.php
+++ admin/models/fields/playgroundlist.php
@@ -10,11 +10,9 @@
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Factory;
 use Joomla\CMS\Form\FormField;
 use Joomla\CMS\Form\FormHelper;
-use Joomla\CMS\Uri\Uri;
 
 FormHelper::loadFieldClass('list');
 
@@ -36,92 +34,6 @@ class JFormFieldplaygroundlist extends \JFormFieldList
 	 */
 	public $type = 'playgroundlist';
 
-  /**
-   * JFormFieldplaygroundlist::getInput()
-   * 
-   * @return
-   */
-  protected function getInput()
-	{
-	   $document = Factory::getDocument();
-       /** Some CSS */
-		$document->addStyleDeclaration(
-			'
-img.item {
-    padding-right: 10px;
-    vertical-align: middle;
-}
-img.car {
-    height: 25px;
-}'
-		);
-    /**
-		 *          Initialize variables.
-		 */
-		$options = array();
-        $script = array();
-$html = '';
-
-
-		$db    = Factory::getDbo();
-		$query = $db->getQuery(true);
-
-		$query->select('id AS value, name AS text, picture as teampicture');
-		$query->from('#__sportsmanagement_playground');
-		$query->order('name');
-		$db->setQuery($query);
-		$options = $db->loadObjectList();
-        
-//echo '<pre>'.print_r($options,true).'</pre>';
-//echo 'value<pre>'.print_r($this->value,true).'</pre>';
-//echo 'id<pre>'.print_r($this->id,true).'</pre>';
-//echo 'value<pre>'.print_r($this->name,true).'</pre>';
-    
-$script[] = "var teampicture = new Array;";        
-foreach ($options as $key => $value)
-			{
-				if (!$value->teampicture)
-				{
-					$value->teampicture = sportsmanagementHelper::getDefaultPlaceholder("playgrounds");
-				}
-
-				$script[] = 'teampicture[' . ($key) . ']=\'' . $value->teampicture . "';\n";
-			}        
-Factory::getDocument()->addScriptDeclaration(implode("\n", $script));        
-/**
- * String $opt - second parameter of formbehavior2::select2
- * for details http://ivaynberg.github.io/select2/
- */
-      $opt = ' allowClear: true,
-   width: "100%",
-
-   formatResult: function format(state)
-   {
-   var originalOption = state.element;
-   var picture;
-   //picture = "images/com_sportsmanagement/database/playgrounds/placeholder_stadium.png";
-   picture = teampicture[state.id];
-   if (!state.id)
-   return state.text;
-   return "<img class=\'item car\' src=\'' . Uri::root() . '" + picture + "\' />" + state.text;
-   },
- 
-   escapeMarkup: function(m) { return m; }
-';		
-
-      $append = '';
-		$html .= HTMLHelper::_('formbehavior2.select2', '.test1', $opt);
-      $html .= HTMLHelper::_(
-				'select.genericlist', $options, $this->name,
-				'style="width:225px;" class="test1" size="6"' . $append, 'value', 'text', $this->value, $this->id
-			);
-      
-      
-		return $html;
-  }
-  
-  
- 
 	/**
 	 * Method to get the field options.
 	 *
@@ -131,6 +43,25 @@ Factory::getDocument()->addScriptDeclaration(implode("\n", $script));
 	 */
 	protected function getOptions()
 	{
+		/**
+		 *          Initialize variables.
+		 */
+		$options = array();
 
+		$db    = Factory::getDbo();
+		$query = $db->getQuery(true);
+
+		$query->select('id AS value, name AS text');
+		$query->from('#__sportsmanagement_playground');
+		$query->order('name');
+		$db->setQuery($query);
+		$options = $db->loadObjectList();
+
+		/**
+		 *          Merge any additional options in the XML definition.
+		 */
+		$options = array_merge(parent::getOptions(), $options);
+
+		return $options;
 	}
 }
