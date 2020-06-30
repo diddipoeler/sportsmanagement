33636df952ca802c44d1d1cf1abd61f5928ff2e5
diddipoeler <diddipoeler@gmx.de>
2020-06-30 08:32:19 +0200

    Update playgrounds.php
    #1079

--- admin/models/playgrounds.php
+++ admin/models/playgrounds.php
@@ -1,8 +1,6 @@
 <?php
 /**
- *
  * SportsManagement ein Programm zur Verwaltung für Sportarten
- *
  * @version    1.0.05
  * @package    Sportsmanagement
  * @subpackage models
@@ -11,9 +9,7 @@
  * @copyright  Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
-
 defined('_JEXEC') or die('Restricted access');
-
 use Joomla\CMS\Language\Text;
 
 /**
@@ -100,13 +96,16 @@ class sportsmanagementModelPlaygrounds extends JSMModelList
 	 * @return array
 	 * @since  0.1
 	 */
-	function getPlaygrounds()
+	function getPlaygrounds($picture=false)
 	{
 		$starttime = microtime();
 		$results   = array();
 		$this->jsmquery->clear();
 		$this->jsmquery->select('id AS value, name AS text');
-
+if ( $picture )
+{
+$this->jsmquery->select('picture as playgroundpicture');	
+}
 		// From table
 		$this->jsmquery->from('#__sportsmanagement_playground');
 		$this->jsmquery->order('text ASC');
