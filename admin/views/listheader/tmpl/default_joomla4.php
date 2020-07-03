7214a4ddecc534022237076c334764e65eb4320f
diddipoeler <diddipoeler@gmx.de>
2020-07-03 12:03:52 +0200

    Update default_joomla4.php
    #1085

--- admin/views/listheader/tmpl/default_joomla4.php
+++ admin/views/listheader/tmpl/default_joomla4.php
@@ -1,8 +1,6 @@
 <?php
 /**
- *
  * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- *
  * @version    1.0.05
  * @package    Sportsmanagement
  * @subpackage listheader
@@ -11,7 +9,6 @@
  * @copyright  Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
-
 defined('_JEXEC') or die('Restricted access');
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\HTML\HTMLHelper;
@@ -45,7 +42,12 @@ if ($this->jsmmessage)
 {
 	echo $this->loadTemplate('info_message');
 }
-
+switch ($view)
+		{
+			case 'clubs':
+		echo JLayoutHelper::render('joomla.searchtools.default', array('view' => $this));
+				break;
+}
 switch ($view)
 {
 	case 'githubinstall':
@@ -61,6 +63,7 @@ switch ($view)
 	default:
 		?>  
 		<div id="filter-bar" class="btn-toolbar">
+			<!--
 			<div class="filter-search btn-group pull-left">
 				<label for="filter_search" class="element-invisible"><?php echo Text::_('JSEARCH_FILTER_LABEL');?></label>
 				<input type="text" name="filter_search" id="filter_search" placeholder="<?php echo Text::_('JSEARCH_FILTER'); ?>" value="<?php echo $this->escape($this->state->get('filter.search')); ?>" class="hasTooltip" title="<?php echo HTMLHelper::tooltipText('JGLOBAL_LOOKING_FOR'); ?>" />
@@ -70,6 +73,7 @@ switch ($view)
 				<button type="button" class="btn hasTooltip" title="<?php echo HTMLHelper::tooltipText('JSEARCH_FILTER_CLEAR'); ?>" onclick="document.id('filter_search').value='';this.form.submit();"><i class="icon-remove"></i></button>
 
 								 </div>
+			-->
 				<?php
 				$startRange = ComponentHelper::getParams($jinput->getCmd('option'))->get('character_filter_start_hex', '0');
 				$endRange = ComponentHelper::getParams($jinput->getCmd('option'))->get('character_filter_end_hex', '0');
@@ -80,11 +84,12 @@ switch ($view)
 				}
 
 								?>
+			<!--
 				<div class="btn-group pull-right hidden-phone">
 				<label for="limit" class="element-invisible"><?php echo Text::_('JFIELD_PLG_SEARCH_SEARCHLIMIT_DESC');?></label>
 				<?php echo $this->pagination->getLimitBox(); ?>
 				</div>
-
+-->
 		  
 				  </div>
 		<?php
