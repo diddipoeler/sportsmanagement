cb5930e4db88be366fe860ea9ac9a3ceb0bfdc4c
diddipoeler <famploeger@arcor.de>
2020-07-03 19:45:01 +0200

    #1085

--- admin/views/listheader/tmpl/default_joomla4.php
+++ admin/views/listheader/tmpl/default_joomla4.php
@@ -43,67 +43,14 @@ if ($this->jsmmessage)
 {
 	echo $this->loadTemplate('info_message');
 }
-
-switch ($view)
-{
-	case 'githubinstall':
-	break;
-	case 'updates':
-	break;
-	case 'databasetools':
-	break;
-	case 'treetonodes':
-	break;
-	case 'treetomatchs':
-	break;
-	default:
-
-		?>  
-		<!-- <div id="filter-bar" class="btn-toolbar"> -->
-			<!--
-			<div class="filter-search btn-group pull-left">
-				<label for="filter_search" class="element-invisible"><?php echo Text::_('JSEARCH_FILTER_LABEL');?></label>
-				<input type="text" name="filter_search" id="filter_search" placeholder="<?php echo Text::_('JSEARCH_FILTER'); ?>" value="<?php echo $this->escape($this->state->get('filter.search')); ?>" class="hasTooltip" title="<?php echo HTMLHelper::tooltipText('JGLOBAL_LOOKING_FOR'); ?>" />
-			</div>
-			<div class="btn-group pull-left">
-				<button type="submit" class="btn hasTooltip" title="<?php echo HTMLHelper::tooltipText('JSEARCH_FILTER_SUBMIT'); ?>"><i class="icon-search"></i></button>
-				<button type="button" class="btn hasTooltip" title="<?php echo HTMLHelper::tooltipText('JSEARCH_FILTER_CLEAR'); ?>" onclick="document.id('filter_search').value='';this.form.submit();"><i class="icon-remove"></i></button>
-
-								 </div>
-			-->
-				<?php
-		/*
-				$startRange = ComponentHelper::getParams($jinput->getCmd('option'))->get('character_filter_start_hex', '0');
-				$endRange = ComponentHelper::getParams($jinput->getCmd('option'))->get('character_filter_end_hex', '0');
-
-				for ($i = $startRange; $i <= $endRange; $i++)
-		{
-						  printf("<a href=\"javascript:searchPerson('%s')\">%s</a>&nbsp;&nbsp;&nbsp;&nbsp;", '&#' . $i . ';', '&#' . $i . ';');
-				}
-*/
-								?>
-			<!--
-				<div class="btn-group pull-right hidden-phone">
-				<label for="limit" class="element-invisible"><?php echo Text::_('JFIELD_PLG_SEARCH_SEARCHLIMIT_DESC');?></label>
-				<?php echo $this->pagination->getLimitBox(); ?>
-				</div>
--->
-		  
-				<!--  </div> -->
-		<?php
-	break;
-}
+?>
+<div id="filter-bar" class="btn-toolbar">
+<?php
 switch ($view)
 {
 case 'clubs':
 case 'playgrounds':
-?>
-<div id="filter-bar" class="btn-toolbar"> 
-<?php
-echo LayoutHelper::render('joomla.searchtools.default', ['view' => $this]);
-?>
-</div>
-<?php
+echo LayoutHelper::render('joomla.searchtools.default', array('view' => $this));
 break;
 case 'githubinstall':
 case 'updates':
@@ -113,15 +60,39 @@ case 'treetomatchs':
 break;
 default:
 ?>
-<div id="filter-bar" class="btn-toolbar"> 
+<div class="filter-search btn-group pull-left">
+<label for="filter_search" class="element-invisible"><?php echo Text::_('JSEARCH_FILTER_LABEL'); ?></label>
+                            <input type="text" name="filter_search" id="filter_search"
+                                 placeholder="<?php echo Text::_('JSEARCH_FILTER'); ?>"
+                                   value="<?php echo $this->escape($this->state->get('filter.search')); ?>"
+                                   class="hasTooltip"
+                                   title="<?php echo HTMLHelper::tooltipText('JGLOBAL_LOOKING_FOR'); ?>"/>
+                        </div>
+                        <div class="btn-group pull-left">
+                            <button type="submit" class="btn hasTooltip"
+                                    title="<?php echo HTMLHelper::tooltipText('JSEARCH_FILTER_SUBMIT'); ?>"><i
+                                        class="icon-search"></i></button>
+                            <button type="button" class="btn hasTooltip"
+                                    title="<?php echo HTMLHelper::tooltipText('JSEARCH_FILTER_CLEAR'); ?>"
+                                    onclick="document.id('filter_search').value='';this.form.submit();"><i
+                                        class="icon-remove"></i></button>
+
+                        </div>
 <div class="btn-group pull-right hidden-phone">
-<label for="limit" class="element-invisible"><?php echo Text::_('JFIELD_PLG_SEARCH_SEARCHLIMIT_DESC');?></label>
-<?php echo $this->pagination->getLimitBox(); ?>
-</div>
-</div>
+                            <label for="limit"
+                                   class="element-invisible"><?php echo Text::_('JFIELD_PLG_SEARCH_SEARCHLIMIT_DESC'); ?></label>
+							<?php echo $this->pagination->getLimitBox(); ?>
+                        </div>                        
 <?php
 break;
 }
+
+?>
+	    
+<?PHP
+?>
+ </div>
+
 
 
 
