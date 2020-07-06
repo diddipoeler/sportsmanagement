f0d074e6e73aad89c956e140a3f0335b7a39a003
diddipoeler <famploeger@arcor.de>
2020-07-06 17:17:19 +0200

    #1088

--- admin/views/teams/tmpl/default.php
+++ admin/views/teams/tmpl/default.php
@@ -1,8 +1,6 @@
 <?php
 /**
- *
  * SportsManagement ein Programm zur Verwaltung für Sportarten
- *
  * @version    1.0.05
  * @package    Sportsmanagement
  * @subpackage teams
@@ -11,9 +9,7 @@
  * @copyright  Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
-
 defined('_JEXEC') or die('Restricted access');
-
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\HTML\HTMLHelper;
 
@@ -50,6 +46,7 @@ sportsmanagementHelper::addTemplatePaths($templatesToLoad, $this);
 				echo Text::_('JSEARCH_FILTER_CLEAR');
 				?>
             </button>
+            <?php echo $this->pagination->getLimitBox(); ?>
 			<?php
 			echo $this->loadTemplate('data');
 		}
