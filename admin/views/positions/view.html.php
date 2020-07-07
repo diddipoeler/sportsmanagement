f5d4398aad5e0deb311b962b4fdaace529ad00b2
diddipoeler <diddipoeler@gmx.de>
2020-07-07 13:08:53 +0200

    Update view.html.php
    #1085

--- admin/views/positions/view.html.php
+++ admin/views/positions/view.html.php
@@ -1,8 +1,6 @@
 <?php
 /**
- *
  * SportsManagement ein Programm zur Verwaltung für Sportarten
- *
  * @version    1.0.05
  * @package    Sportsmanagement
  * @subpackage positions
@@ -11,15 +9,14 @@
  * @copyright  Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
-
-
 defined('_JEXEC') or die('Restricted access');
-
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Table\Table;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\MVC\Model\BaseDatabaseModel;
 use Joomla\CMS\Toolbar\ToolbarHelper;
+use Joomla\CMS\Form\Form;
+use Joomla\CMS\Log\Log;
 
 /**
  * sportsmanagementViewPositions
@@ -32,6 +29,22 @@ use Joomla\CMS\Toolbar\ToolbarHelper;
  */
 class sportsmanagementViewPositions extends sportsmanagementView
 {
+	/**
+	 * A \JForm instance with filter fields.
+	 *
+	 * @var    \JForm
+	 * @since  3.6.3
+	 */
+	public $filterForm;
+
+	/**
+	 * An array with active filters.
+	 *
+	 * @var    array
+	 * @since  3.6.3
+	 */
+	public $activeFilters;
+	
 	/**
 	 * sportsmanagementViewPositions::init()
 	 *
@@ -77,6 +90,11 @@ class sportsmanagementViewPositions extends sportsmanagementView
 		unset($sportstypes);
 
 		$this->lists = $lists;
+		$this->filterForm    = $this->model->getFilterForm();
+		$this->activeFilters = $this->model->getActiveFilters();
+	
+//Log::add(Text::_(__METHOD__ . ' ' . __LINE__ . ' ' . '<pre>'.print_r($this->filterForm ,true).'</pre>' ), Log::NOTICE, 'jsmerror');		
+//Log::add(Text::_(__METHOD__ . ' ' . __LINE__ . ' ' . '<pre>'.print_r($this->activeFilters ,true).'</pre>' ), Log::NOTICE, 'jsmerror');	
 
 	}
 
