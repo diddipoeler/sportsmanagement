50be662219d9ffdb4ffe9d18f987f8fbf63434ee
diddipoeler <diddipoeler@gmx.de>
2020-07-17 12:53:26 +0200

    Update default_teams.php
    code

--- site/views/teams/tmpl/default_teams.php
+++ site/views/teams/tmpl/default_teams.php
@@ -1,8 +1,6 @@
 <?php
 /**
- *
  * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- *
  * @version    1.0.05
  * @package    Sportsmanagement
  * @subpackage teams
@@ -11,9 +9,7 @@
  * @copyright  Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
-
 defined('_JEXEC') or die('Restricted access');
-
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Factory;
@@ -125,7 +121,8 @@ usort(
 					$picture = sportsmanagementHelper::getDefaultPlaceholder("clublogomedium");
 				}
 
-				$image              = HTMLHelper::image($picture, $clubTitle, array('title' => $clubTitle, ' border' => 0, ' width' => $this->config['team_logo_width']));
+//$image = HTMLHelper::image($picture, $clubTitle, array('title' => $clubTitle, ' border' => 0, ' width' => $this->config['team_logo_width']));
+$image = HTMLHelper::image($picture, $clubTitle, array('title' => $clubTitle, ' border' => 0, ' style' => 'width: auto;height:'.$this->config['team_logo_width'].'px'  ) );				
 				$mediumClubLogoLink = HTMLHelper::link($clubinfo_link, $image);
 			}
 			?>
