b1ff2130e445aac41a0fa255c6cff1bd425a3e15
diddipoeler <famploeger@arcor.de>
2020-07-18 12:16:24 +0200

    #1101

--- site/views/roster/tmpl/default_picture.php
+++ site/views/roster/tmpl/default_picture.php
@@ -1,8 +1,6 @@
 <?php
 /**
- *
  * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- *
  * @version    1.0.05
  * @package    Sportsmanagement
  * @subpackage roster
@@ -11,9 +9,7 @@
  * @copyright  Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
-
 defined('_JEXEC') or die('Restricted access');
-
 use Joomla\CMS\Language\Text;
 
 ?>
@@ -41,7 +37,7 @@ use Joomla\CMS\Language\Text;
 						'roster' . $this->team->name,
 						$picture,
 						$this->team->name,
-						$this->config['team_picture_width'],
+						$this->config['team_picture_height'],
 						'',
 						$this->modalwidth,
 						$this->modalheight,
--- site/views/roster/tmpl/default_staff.php
+++ site/views/roster/tmpl/default_staff.php
@@ -1,8 +1,6 @@
 <?php
 /**
- *
  * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- *
  * @version    1.0.05
  * @package    Sportsmanagement
  * @subpackage roster
@@ -11,9 +9,7 @@
  * @copyright  Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
-
 defined('_JEXEC') or die('Restricted access');
-
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Factory;
@@ -142,7 +138,7 @@ if (count($this->stafflist) > 0)
 								'staff' . $row->playerid,
 								$picture,
 								$playerName,
-								$this->config['staff_picture_width'],
+								$this->config['staff_picture_height'],
 								'',
 								$this->modalwidth,
 								$this->modalheight,
