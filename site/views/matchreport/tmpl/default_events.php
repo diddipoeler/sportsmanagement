b46521a58ad1222b8fb2640e3778843bac53a123
diddipoeler <famploeger@arcor.de>
2020-07-20 19:13:19 +0200

    #1104

--- site/views/matchreport/tmpl/default_events.php
+++ site/views/matchreport/tmpl/default_events.php
@@ -1,8 +1,6 @@
 <?php
 /**
- *
  * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- *
  * @version    1.0.05
  * @package    Sportsmanagement
  * @subpackage matchreport
@@ -11,15 +9,13 @@
  * @copyright  Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
-
 defined('_JEXEC') or die('Restricted access');
-
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\HTML\HTMLHelper;
 
 ?>
 <!-- START of match events -->
-<div class="<?php echo $this->divclassrow; ?> table-responsive" id="matchreport">
+<div class="<?php echo $this->divclassrow; ?> table-responsive" id="matchreport-events">
     <h2>
 		<?php
 		echo Text::_('COM_SPORTSMANAGEMENT_MATCHREPORT_EVENTS');
