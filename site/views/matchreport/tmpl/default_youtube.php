e70733a98ee425ca0c8dd6e525519fbbc2f64668
diddipoeler <diddipoeler@gmx.de>
2020-07-20 11:46:47 +0200

    Update default_youtube.php
    code

--- site/views/matchreport/tmpl/default_youtube.php
+++ site/views/matchreport/tmpl/default_youtube.php
@@ -34,7 +34,7 @@ function getYoutubeEmbedHtml($url)
 ?>
 
 <h4><?php echo Text::_('COM_SPORTSMANAGEMENT_MATCHREPORT_YOUTUBE'); ?></h4>
-<div class="panel-group" id="showyoutube">
+
 <div id="videobereich">
                     <iframe class="videoextern" width="640" height="360"
                             src="<?php echo $this->youtube; ?>"
