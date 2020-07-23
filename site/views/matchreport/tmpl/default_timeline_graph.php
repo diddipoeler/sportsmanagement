b24aaf8709f8b6f586d3cac0c4539fba84694703
diddipoeler <diddipoeler@gmx.de>
2020-07-23 06:54:20 +0200

    Create default_timeline_graph.php
    #1107

---
+++ site/views/matchreport/tmpl/default_timeline_graph.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * SportsManagement ein Programm zur Verwaltung für alle Sportarten
+ * @version    1.0.05
+ * @package    Sportsmanagement
+ * @subpackage matchreport
+ * @file       default_timeline_graph.php
+ * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright  Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
+defined('_JEXEC') or die('Restricted access');
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Factory;
+//echo $this->playgroundheight;
+?>
