82c796023da0026dff9c05d9d7c25eb6673aa51d
diddipoeler <diddipoeler@gmx.de>
2020-07-23 07:35:04 +0200

    Update default_timeline_graph.php
    #1107

--- site/views/matchreport/tmpl/default_timeline_graph.php
+++ site/views/matchreport/tmpl/default_timeline_graph.php
@@ -16,13 +16,27 @@ use Joomla\CMS\Language\Text;
 use Joomla\CMS\Factory;
 //echo $this->playgroundheight;
 ?>
-<script type="text/javascript" src="https://unpkg.com/browse/vis-timeline@5.0.0/dist/vis-timeline-graph2d.min.js"></script>
-  <link href="https://unpkg.com/browse/vis-timeline@5.0.0/dist/vis-timeline-graph2d.css" rel="stylesheet" type="text/css" />
+<script src="//unpkg.com/vis-timeline@7.3.7/standalone/umd/vis-timeline-graph2d.min.js"></script>
+  <link href="//unpkg.com/vis-timeline@7.3.7/styles/vis-timeline-graph2d.min.css" rel="stylesheet" type="text/css" />
   <style type="text/css">
     #visualization {
-      width: 600px;
+      width: 100%;
       height: 400px;
       border: 1px solid lightgray;
     }
   </style>
 
+<div id="visualization"></div>
+    
+<script type="text/javascript">
+  var container = document.getElementById('visualization');
+  var items = new vis.DataSet();
+  var customDate = new Date();
+  var options = {
+    start: 1,
+    end: 90
+  };
+  var timeline = new vis.Timeline(container, items, options);
+
+
+</script>
