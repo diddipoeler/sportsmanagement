6eb1151440305f7471e8410986e25a46bcbeed45
diddipoeler <diddipoeler@gmx.de>
2020-07-31 11:33:26 +0200

    Update default_players.php
    code

--- site/views/roster/tmpl/default_players.php
+++ site/views/roster/tmpl/default_players.php
@@ -8,6 +8,8 @@
  * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
  * @copyright  Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ *
+ * https://github.com/eKoopmans/html2pdf.js
  */
 defined('_JEXEC') or die('Restricted access');
 use Joomla\CMS\Language\Text;
