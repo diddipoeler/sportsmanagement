386e43e182a33507b29697677f23f0d7d5246c55
diddipoeler <famploeger@arcor.de>
2020-08-02 13:44:04 +0200

    #1129

--- site/views/teamplan/tmpl/default.php
+++ site/views/teamplan/tmpl/default.php
@@ -53,7 +53,17 @@ sportsmanagementHelper::addTemplatePaths($templatesToLoad, $this);
 <script type="text/javascript">
     jQuery(function ($) {
         $("#exportButton").click(function () {
-            
+var element = document.getElementById('teamplan');
+var opt = {
+  margin:       1,
+  filename:     'tablestaff.pdf',
+  image:        { type: 'jpeg', quality: 0.98 },
+  html2canvas:  { scale: 2 },
+  jsPDF:        { unit: 'in', format: 'A3', orientation: 'landscape' }
+};
+
+// New Promise-based usage:
+//html2pdf().set(opt).from(element).save();            
         });
     });
 </script>
