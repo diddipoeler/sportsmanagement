8bb8dae182171367d7282948798ba985f84e30b3
diddipoeler <famploeger@arcor.de>
2020-08-04 20:21:57 +0200

    #1139

--- admin/views/positions/tmpl/default_data.php
+++ admin/views/positions/tmpl/default_data.php
@@ -14,6 +14,7 @@ use Joomla\CMS\Language\Text;
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Uri\Uri;
 use Joomla\CMS\Router\Route;
+use Joomla\CMS\Session\Session;
 
 if (version_compare(substr(JVERSION, 0, 3), '4.0', 'ge'))
 {
