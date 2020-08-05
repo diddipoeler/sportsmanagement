cad4c29c17bb7f19184880363f4bd18b40a16f5f
diddipoeler <diddipoeler@gmx.de>
2020-08-05 10:37:13 +0200

    Update view.html.php
    #1139

--- admin/views/seasons/view.html.php
+++ admin/views/seasons/view.html.php
@@ -1,8 +1,6 @@
 <?php
 /**
- *
  * SportsManagement ein Programm zur Verwaltung für Sportarten
- *
  * @version    1.0.05
  * @package    Sportsmanagement
  * @subpackage seasons
@@ -11,14 +9,12 @@
  * @copyright  Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
-
-
 defined('_JEXEC') or die('Restricted access');
-
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Table\Table;
 use Joomla\CMS\Toolbar\ToolbarHelper;
+use Joomla\CMS\Factory;
 
 /**
  * sportsmanagementViewSeasons
@@ -31,7 +27,22 @@ use Joomla\CMS\Toolbar\ToolbarHelper;
  */
 class sportsmanagementViewSeasons extends sportsmanagementView
 {
+	/**
+	 * A \JForm instance with filter fields.
+	 *
+	 * @var    \JForm
+	 * @since  3.6.3
+	 */
+	public $filterForm;
 
+	/**
+	 * An array with active filters.
+	 *
+	 * @var    array
+	 * @since  3.6.3
+	 */
+	public $activeFilters;
+	
 	/**
 	 * sportsmanagementViewSeasons::init()
 	 *
@@ -87,6 +98,15 @@ class sportsmanagementViewSeasons extends sportsmanagementView
 			$this->setLayout('assignpersons');
 			break;
 		}
+try
+{		
+$this->filterForm    = $this->model->getFilterForm();
+$this->activeFilters = $this->model->getActiveFilters();
+}
+catch (Exception $e)
+{
+Factory::getApplication()->enqueueMessage(__METHOD__ . ' ' . __LINE__ . Text::_($e->getMessage()), 'Error');
+}
 
 	}
 
