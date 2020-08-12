6f0bfeb33dd321e29dfbb56d91926fd4b44e7d7a
diddipoeler <famploeger@arcor.de>
2020-08-12 18:15:46 +0200

    map

--- admin/views/playground/view.html.php
+++ admin/views/playground/view.html.php
@@ -1,8 +1,6 @@
 <?php
 /**
- *
  * SportsManagement ein Programm zur Verwaltung für Sportarten
- *
  * @version    1.0.05
  * @package    Sportsmanagement
  * @subpackage playground
@@ -11,9 +9,7 @@
  * @copyright  Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
-
 defined('_JEXEC') or die('Restricted access');
-
 use Joomla\CMS\Uri\Uri;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\Environment\Browser;
@@ -54,6 +50,7 @@ class sportsmanagementViewPlayground extends sportsmanagementView
 
 		if (version_compare(JSM_JVERSION, '4', 'eq'))
 		{
+		  $this->document->addScript(Uri::base() . 'components/' . $this->option . '/views/playground/tmpl/edit.js');
 		}
 		else
 		{
