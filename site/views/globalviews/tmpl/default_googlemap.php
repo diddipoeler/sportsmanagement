bf1f2a4b58f627e1fffc56083b41e59eb56befc8
diddipoeler <diddipoeler@gmx.de>
2020-08-12 08:09:19 +0200

    Update default_googlemap.php
    map

--- site/views/globalviews/tmpl/default_googlemap.php
+++ site/views/globalviews/tmpl/default_googlemap.php
@@ -1,8 +1,6 @@
 <?php
 /**
- *
  * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- *
  * @version    1.0.05
  * @package    Sportsmanagement
  * @subpackage globalviews
@@ -23,7 +21,6 @@
  */
 
 defined('_JEXEC') or die('Restricted access');
-
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Uri\Uri;
@@ -37,8 +34,18 @@ $map_type      = 'http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}';
 
 if ($this->config['use_which_map'])
 {
-	$this->document->addScript('https://unpkg.com/leaflet@1.3.4/dist/leaflet.js');
-	$this->document->addStyleSheet('https://unpkg.com/leaflet@1.3.4/dist/leaflet.css');
+?>
+
+<link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css"
+  integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
+  crossorigin=""/>
+<script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"
+  integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew=="
+  crossorigin=""></script>
+
+<?php	
+	//$this->document->addScript('https://unpkg.com/leaflet@1.3.4/dist/leaflet.js');
+	//$this->document->addStyleSheet('https://unpkg.com/leaflet@1.3.4/dist/leaflet.css');
 
 	$this->document->addScript('https://cdn.jsdelivr.net/npm/leaflet.locatecontrol@0.63.0/dist/L.Control.Locate.min.js');
 	$this->document->addStyleSheet('https://cdn.jsdelivr.net/npm/leaflet.locatecontrol@0.63.0/dist/L.Control.Locate.min.css');
