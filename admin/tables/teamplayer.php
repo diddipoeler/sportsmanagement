e7290a03c4fddb4902d86bc947a57cbb75843ef8
diddipoeler <famploeger@arcor.de>
2020-08-18 17:51:15 +0200

    joomla 4

--- admin/tables/teamplayer.php
+++ admin/tables/teamplayer.php
@@ -1,8 +1,6 @@
 <?php
 /**
- *
  * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- *
  * @version    1.0.05
  * @package    Sportsmanagement
  * @subpackage tables
@@ -11,8 +9,6 @@
  * @copyright  Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
-
-
 defined('_JEXEC') or die('Restricted access');
 
 /**
@@ -39,6 +35,5 @@ class sportsmanagementTableTeamPerson extends JSMTable
 		$db = sportsmanagementHelper::getDBConnection();
 		parent::__construct('#__sportsmanagement_season_team_person_id', 'id', $db);
 	}
-
 
 }
