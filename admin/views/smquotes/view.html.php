783e46ff7e91b87c5e774ae326092f4f3782b88f
diddipoeler <diddipoeler@gmx.de>
2020-08-19 09:28:11 +0200

    Update view.html.php
    code

--- admin/views/smquotes/view.html.php
+++ admin/views/smquotes/view.html.php
@@ -1,8 +1,6 @@
 <?php
 /**
- *
  * SportsManagement ein Programm zur Verwaltung für Sportarten
- *
  * @version    1.0.05
  * @package    Sportsmanagement
  * @subpackage smquotes
@@ -11,10 +9,7 @@
  * @copyright  Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
-
-
 defined('_JEXEC') or die('Restricted access');
-
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\Table\Table;
 use Joomla\CMS\Toolbar\ToolbarHelper;
@@ -39,9 +34,7 @@ class sportsmanagementViewsmquotes extends sportsmanagementView
 	 */
 	public function init()
 	{
-
 		$this->table = Table::getInstance('smquote', 'sportsmanagementTable');
-
 	}
 
 	/**
@@ -51,21 +44,14 @@ class sportsmanagementViewsmquotes extends sportsmanagementView
 	 */
 	protected function addToolbar()
 	{
-		// Set toolbar items for the page
 		$this->title = Text::_('COM_SPORTSMANAGEMENT_ADMIN_QUOTES_TITLE');
 		ToolbarHelper::addNew('smquote.add');
 		ToolbarHelper::editList('smquote.edit');
 		ToolbarHelper::custom('smquote.import', 'upload', 'upload', Text::_('JTOOLBAR_UPLOAD'), false);
-
 		ToolbarHelper::custom('smquotes.edittxt', 'featured.png', 'featured_f2.png', Text::_('JTOOLBAR_EDIT'), false);
-
 		$bar = Toolbar::getInstance('toolbar');
-
-		// $bar->appendButton('Link', 'info', 'Kategorie', 'index.php?option=com_categories&view=categories&extension=com_sportsmanagement');
 		$bar->appendButton('Link', 'info', 'Kategorie', 'index.php?option=com_categories&extension=com_sportsmanagement');
-
 		ToolbarHelper::archiveList('smquote.export', Text::_('JTOOLBAR_EXPORT'));
-
 		parent::addToolbar();
 	}
 }
