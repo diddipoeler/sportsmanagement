8653d4280e8677e32183d15ab8dfe952a203bcb8
diddipoeler <diddipoeler@gmx.de>
2020-08-19 12:10:07 +0200

    Update smquotestxt.php
    code

--- admin/tables/smquotestxt.php
+++ admin/tables/smquotestxt.php
@@ -1,1 +1,38 @@
+<?php
+/**
+ * SportsManagement ein Programm zur Verwaltung für alle Sportarten
+ * @version    1.0.05
+ * @package    Sportsmanagement
+ * @subpackage tables
+ * @file       smquote.php
+ * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright  Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
+defined('_JEXEC') or die('Restricted access');
 
+/**
+ * sportsmanagementTablesmquotestxt
+ *
+ * @package
+ * @author
+ * @copyright diddi
+ * @version   2020
+ * @access    public
+ */
+class sportsmanagementTablesmquotestxt extends JSMTable
+{
+	/**
+	 * Constructor
+	 *
+	 * @param   object Database connector object
+	 *
+	 * @since 1.0
+	 */
+	function __construct(&$db)
+	{
+		$db = sportsmanagementHelper::getDBConnection();
+		//parent::__construct('#__sportsmanagement_rquote', 'id', $db);
+	}
+
+}
