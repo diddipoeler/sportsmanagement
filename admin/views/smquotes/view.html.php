1b392cf87a6b7b3435ca99bad3afbc6ace38f904
diddipoeler <diddipoeler@gmx.de>
2020-08-19 09:37:06 +0200

    Update view.html.php
    code

--- admin/views/smquotes/view.html.php
+++ admin/views/smquotes/view.html.php
@@ -14,6 +14,8 @@ use Joomla\CMS\Language\Text;
 use Joomla\CMS\Table\Table;
 use Joomla\CMS\Toolbar\ToolbarHelper;
 use Joomla\CMS\Toolbar\Toolbar;
+use Joomla\CMS\Form\Form;
+use Joomla\CMS\Log\Log;
 
 /**
  * sportsmanagementViewsmquotes
@@ -26,6 +28,21 @@ use Joomla\CMS\Toolbar\Toolbar;
  */
 class sportsmanagementViewsmquotes extends sportsmanagementView
 {
+	/**
+	 * A \JForm instance with filter fields.
+	 *
+	 * @var    \JForm
+	 * @since  3.6.3
+	 */
+	public $filterForm;
+
+	/**
+	 * An array with active filters.
+	 *
+	 * @var    array
+	 * @since  3.6.3
+	 */
+	public $activeFilters;
 
 	/**
 	 * sportsmanagementViewsmquotes::init()
@@ -35,6 +52,18 @@ class sportsmanagementViewsmquotes extends sportsmanagementView
 	public function init()
 	{
 		$this->table = Table::getInstance('smquote', 'sportsmanagementTable');
+		
+try
+{		
+$this->filterForm    = $this->model->getFilterForm();
+$this->activeFilters = $this->model->getActiveFilters();
+}
+catch (Exception $e)
+{
+Log::add(Text::_(__METHOD__ . ' ' . __LINE__ . ' ' . $e->getCode()), Log::ERROR, 'jsmerror');
+Log::add(Text::_(__METHOD__ . ' ' . __LINE__ . ' ' . $e->getMessage()), Log::ERROR, 'jsmerror');	
+}	
+		
 	}
 
 	/**
