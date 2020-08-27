303c46db7d2a5937a3bf9bff4bdd1aa29e34c07f
diddipoeler <diddipoeler@gmx.de>
2020-08-27 12:32:06 +0200

    Update default_googlemap.php
    #1063

--- site/views/globalviews/tmpl/default_googlemap.php
+++ site/views/globalviews/tmpl/default_googlemap.php
@@ -160,6 +160,11 @@ if ($this->config['use_which_map'])
 			{
 				$this->showmap = true;
 				?>
+<span itemprop="name"><?php echo $this->club->name; ?></span>
+<div itemprop="geo" itemscope itemtype="http://schema.org/GeoCoordinates">
+    <meta itemprop="latitude" content="<?php echo $this->club->latitude; ?>" />
+    <meta itemprop="longitude" content="<?php echo $this->club->longitude; ?>" />
+  </div>
                 <script>
 
                     var planes = [
