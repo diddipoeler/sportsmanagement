6592cf0028ba5728697ea60d42fb8d09cf88d8e4
diddipoeler <diddipoeler@gmx.de>
2020-09-20 11:21:27 +0200

    code

--- admin/libraries/sportsmanagement/view.php
+++ admin/libraries/sportsmanagement/view.php
@@ -10,7 +10,7 @@
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 defined('_JEXEC') or die();
-//use Joomla\CMS\MVC\View\HtmlView;
+use Joomla\CMS\Table\Table;
 use Joomla\CMS\MVC\View\GenericDataException;
 use Joomla\CMS\MVC\View\HtmlView as BaseHtmlView;
 use Joomla\CMS\Language\Text;
