2f6e43f147de967323359c166b25a01dbca2c69d
diddipoeler <diddipoeler@gmx.de>
2020-09-21 13:59:07 +0200

    Update default_editdata.php
    code

--- admin/views/fieldsets/tmpl/default_editdata.php
+++ admin/views/fieldsets/tmpl/default_editdata.php
@@ -16,15 +16,14 @@ use Joomla\CMS\Language\Text;
 use Joomla\CMS\Uri\Uri;
 use Joomla\CMS\Factory;
 ?>
-<link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css"
+<link rel="stylesheet" href="https://unpkg.com/leaflet@<?php echo $this->leaflet_version; ?></dist/leaflet.css"
   integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
   crossorigin=""/>
-<script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"
+<script src="https://unpkg.com/leaflet@<?php echo $this->leaflet_version; ?>/dist/leaflet.js"
   integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew=="
   crossorigin=""></script>
 <?php
-//$this->document->addScript('https://unpkg.com/leaflet@1.3.4/dist/leaflet.js');
-//$this->document->addStyleSheet('https://unpkg.com/leaflet@1.3.4/dist/leaflet.css');
+
 $this->document->addScript('https://unpkg.com/leaflet-control-geocoder/dist/Control.Geocoder.js');
 
 $templatesToLoad = array('footer', 'fieldsets');
