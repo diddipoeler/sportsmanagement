365087e7bc5041f56260f4414f4a2fd8deb85014
diddipoeler <diddipoeler@gmx.de>
2020-09-21 07:51:33 +0200

    Update uploaddraganddrop.php
    code

--- admin/views/imagehandler/tmpl/uploaddraganddrop.php
+++ admin/views/imagehandler/tmpl/uploaddraganddrop.php
@@ -31,14 +31,14 @@ $languageCode = substr($lang->getTag(),0,2);
 //echo 'getTag <pre>'.print_r($lang->getTag(),true).'</pre>';
 //echo 'languageCode <pre>'.print_r($languageCode,true).'</pre>';
 
-$bootstrap_fileinput_version = '5.1.2';
+//$bootstrap_fileinput_version = '5.1.2';
 
 ?>
 
 <!-- bootstrap 4.x is supported. You can also use the bootstrap css 3.3.x versions -->
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
       crossorigin="anonymous">
-<link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-fileinput/<?php echo $bootstrap_fileinput_version; ?>/css/fileinput.min.css" media="all"
+<link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-fileinput/<?php echo $this->bootstrap_fileinput_version; ?>/css/fileinput.min.css" media="all"
       rel="stylesheet" type="text/css"/>
 <!-- if using RTL (Right-To-Left) orientation, load the RTL CSS file after fileinput.css by uncommenting below -->
 <!-- link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-fileinput/4.5.1/css/fileinput-rtl.min.css" media="all" rel="stylesheet" type="text/css" /-->
@@ -47,26 +47,31 @@ $bootstrap_fileinput_version = '5.1.2';
 <script src="https://code.jquery.com/jquery-3.3.1.min.js" crossorigin="anonymous"></script>
 <!-- piexif.min.js is needed for auto orienting image files OR when restoring exif data in resized images and when you
 	wish to resize images before upload. This must be loaded before fileinput.min.js -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-fileinput/<?php echo $bootstrap_fileinput_version; ?>/js/plugins/piexif.min.js"
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-fileinput/<?php echo $this->bootstrap_fileinput_version; ?>/js/plugins/piexif.min.js"
         type="text/javascript"></script>
 <!-- sortable.min.js is only needed if you wish to sort / rearrange files in initial preview.
 	This must be loaded before fileinput.min.js -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-fileinput/<?php echo $bootstrap_fileinput_version; ?>/js/plugins/sortable.min.js"
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-fileinput/<?php echo $this->bootstrap_fileinput_version; ?>/js/plugins/sortable.min.js"
         type="text/javascript"></script>
 <!-- purify.min.js is only needed if you wish to purify HTML content in your preview for
 	HTML files. This must be loaded before fileinput.min.js -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-fileinput/<?php echo $bootstrap_fileinput_version; ?>/js/plugins/purify.min.js"
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-fileinput/<?php echo $this->bootstrap_fileinput_version; ?>/js/plugins/purify.min.js"
         type="text/javascript"></script>
+
+<!-- popper.min.js below is needed if you use bootstrap 4.x. You can also use the bootstrap js 
+   3.3.x versions without popper.min.js. -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+
 <!-- bootstrap.min.js below is needed if you wish to zoom and preview file content in a detail modal
-	dialog. bootstrap 4.x is supported. You can also use the bootstrap js 3.3.x versions. -->
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.bundle.min.js"
-        crossorigin="anonymous"></script>
+    dialog. bootstrap 4.x is supported. You can also use the bootstrap js 3.3.x versions. -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" type="text/javascript"></script>
+
 <!-- the main fileinput plugin file -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-fileinput/<?php echo $bootstrap_fileinput_version; ?>/js/fileinput.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-fileinput/<?php echo $this->bootstrap_fileinput_version; ?>/js/fileinput.min.js"></script>
 <!-- following theme script is needed to use the Font Awesome 5.x theme (`fas`) -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-fileinput/<?php echo $bootstrap_fileinput_version; ?>/themes/fas/theme.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-fileinput/<?php echo $this->bootstrap_fileinput_version; ?>/themes/fa/theme.min.js"></script>
 <!-- optionally if you need translation for your language then include the locale file as mentioned below (replace LANG.js with your language locale) -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-fileinput/<?php echo $bootstrap_fileinput_version; ?>/js/locales/<?php echo $languageCode; ?>.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-fileinput/<?php echo $this->bootstrap_fileinput_version; ?>/js/locales/<?php echo $languageCode; ?>.js"></script>
 
 <div class="container my-4">
     <form action="<?php echo $this->request_url; ?>" enctype="multipart/form-data" id="adminForm" name="adminForm"
