272c63fcbb977e23dc84a5253019e3ad02ee3924
diddipoeler <diddipoeler@gmx.de>
2020-09-21 14:13:40 +0200

    Update default_editdata.php
    code

--- admin/views/fieldsets/tmpl/default_editdata.php
+++ admin/views/fieldsets/tmpl/default_editdata.php
@@ -17,10 +17,10 @@ use Joomla\CMS\Uri\Uri;
 use Joomla\CMS\Factory;
 ?>
 <link rel="stylesheet" href="https://unpkg.com/leaflet@<?php echo $this->leaflet_version;?>/dist/leaflet.css"
-  integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
+  integrity="<?php echo $this->leaflet_css_integrity;?>"
   crossorigin=""/>
 <script src="https://unpkg.com/leaflet@<?php echo $this->leaflet_version;?>/dist/leaflet.js"
-  integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew=="
+  integrity="<?php echo $this->leaflet_js_integrity;?>"
   crossorigin=""></script>
 <?php
 
