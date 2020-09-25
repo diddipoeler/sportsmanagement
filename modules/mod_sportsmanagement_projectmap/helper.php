2102de06fddced20639c1af495ae6e41fffd05ad
diddipoeler <diddipoeler@gmx.de>
2020-09-25 09:14:01 +0200

    Update helper.php
    code

--- modules/mod_sportsmanagement_projectmap/helper.php
+++ modules/mod_sportsmanagement_projectmap/helper.php
@@ -1,1 +1,29 @@
+<?php
+/**
+ * SportsManagement ein Programm zur Verwaltung für alle Sportarten
+ * @version    1.0.05
+ * @package    Sportsmanagement
+ * @subpackage mod_sportsmanagement_projectmap
+ * @file       helper.php
+ * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright  Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
+defined('_JEXEC') or die('Restricted access');
+use Joomla\CMS\Factory;
 
+JLoader::import('components.com_sportsmanagement.helpers.route', JPATH_SITE);
+
+
+class modJSMprojectmaphelper
+{
+
+	
+	public static function getData($season_ids)
+	{
+    
+    
+  }
+  
+  
+}
