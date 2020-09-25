14b2d866b74acdb51307b6e5656cd296bc0a26f4
diddipoeler <diddipoeler@gmx.de>
2020-09-25 09:22:39 +0200

    Update default.php
    code

--- modules/mod_sportsmanagement_projectmap/tmpl/default.php
+++ modules/mod_sportsmanagement_projectmap/tmpl/default.php
@@ -1,1 +1,19 @@
+<?php
+/**
+ * SportsManagement ein Programm zur Verwaltung für alle Sportarten
+ * @version    1.0.05
+ * @package    Sportsmanagement
+ * @subpackage mod_sportsmanagement_projectmap
+ * @file       default.php
+ * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright  Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * https://www.tutorialrepublic.com/codelab.php?topic=bootstrap&file=accordion
+ */
+defined('_JEXEC') or die('Restricted access');
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Router\Route;
+use Joomla\CMS\HTML\HTMLHelper;
+
+?>
 
