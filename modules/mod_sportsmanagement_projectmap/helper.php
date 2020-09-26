05216d837e3aec82f59a4f5589ea388762b70c80
diddipoeler <diddipoeler@gmx.de>
2020-09-26 11:37:14 +0200

    code

--- modules/mod_sportsmanagement_projectmap/helper.php
+++ modules/mod_sportsmanagement_projectmap/helper.php
@@ -14,7 +14,6 @@ use Joomla\CMS\Factory;
 
 JLoader::import('components.com_sportsmanagement.helpers.route', JPATH_SITE);
 
-
 /**
  * modJSMprojectmaphelper
  * 
@@ -26,7 +25,6 @@ JLoader::import('components.com_sportsmanagement.helpers.route', JPATH_SITE);
  */
 class modJSMprojectmaphelper
 {
-
 	
 	/**
 	 * modJSMprojectmaphelper::getData()
@@ -36,7 +34,38 @@ class modJSMprojectmaphelper
 	 */
 	public static function getData($season_ids)
 	{
-    
+		$app    = Factory::getApplication();
+		$date   = Factory::getDate();
+		$user   = Factory::getUser();
+		$db     = Factory::getDBO();
+		$query  = $db->getQuery(true);
+		$result = array();
+
+		$seasons = implode(",", $season_ids);
+		$query->select('pro.id,pro.name,CONCAT_WS(\':\',pro.id,pro.alias) AS project_slug,le.name as liganame,le.country');
+		$query->select('le.picture as league_picture,pro.picture as project_picture');
+		$query->select('CONCAT_WS(\':\',r.id,r.alias) AS roundcode');
+        
+        $query->select('c.alpha2 as country_alpha2,c.name as country_name,c.picture as country_picture,c.federation as country_federation');
+        
+		$query->from('#__sportsmanagement_project as pro');
+		$query->join('INNER', '#__sportsmanagement_league as le on le.id = pro.league_id');
+		$query->join('INNER', '#__sportsmanagement_round as r on r.id = pro.current_round');
+        $query->join('INNER', '#__sportsmanagement_countries as c on c.alpha3 = le.country');
+        
+		$query->where('le.published_act_season = 1 ');
+		$query->where('pro.season_id IN (' . $seasons . ')');
+		$query->order('le.country ASC, pro.name ASC');
+
+		$db->setQuery($query);
+		$result = $db->loadObjectList();
+		$db->disconnect(); // See: http://api.joomla.org/cms-3/classes/JDatabaseDriver.html#method_disconnect
+
+// echo '<pre>'.print_r($result,true).'</pre>';
+
+		return $result;
+        
+        
     
   }
   
