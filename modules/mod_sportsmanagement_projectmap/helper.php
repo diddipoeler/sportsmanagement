95373b856f7ab797846f3b015914440ae5dbb389
diddipoeler <diddipoeler@gmx.de>
2020-09-26 11:56:12 +0200

    code

--- modules/mod_sportsmanagement_projectmap/helper.php
+++ modules/mod_sportsmanagement_projectmap/helper.php
@@ -42,26 +42,31 @@ class modJSMprojectmaphelper
 		$result = array();
 
 		$seasons = implode(",", $season_ids);
-		$query->select('pro.id,pro.name,CONCAT_WS(\':\',pro.id,pro.alias) AS project_slug,le.name as liganame,le.country');
+		$query->select('MAX( pro.id ) as id,pro.name,CONCAT_WS(\':\',pro.id,pro.alias) AS project_slug,le.name as liganame,le.country');
 		$query->select('le.picture as league_picture,pro.picture as project_picture');
 		$query->select('CONCAT_WS(\':\',r.id,r.alias) AS roundcode');
         
         $query->select('c.alpha2 as country_alpha2,c.name as country_name,c.picture as country_picture,c.federation as country_federation');
-        
+        $query->select('f.name as federation_name,f.picture as federation_picture');
+      
 		$query->from('#__sportsmanagement_project as pro');
 		$query->join('INNER', '#__sportsmanagement_league as le on le.id = pro.league_id');
 		$query->join('INNER', '#__sportsmanagement_round as r on r.id = pro.current_round');
         $query->join('INNER', '#__sportsmanagement_countries as c on c.alpha3 = le.country');
+      
+      $query->join('INNER', '#__sportsmanagement_federations as f on f.id = c.federation');
         
 		$query->where('le.published_act_season = 1 ');
+      $query->where('le.league_level = 1 ');
 		$query->where('pro.season_id IN (' . $seasons . ')');
 		$query->order('le.country ASC, pro.name ASC');
+      $query->group('le.country');
 
 		$db->setQuery($query);
 		$result = $db->loadObjectList();
 		$db->disconnect(); // See: http://api.joomla.org/cms-3/classes/JDatabaseDriver.html#method_disconnect
 
-// echo '<pre>'.print_r($result,true).'</pre>';
+//echo '<pre>'.print_r($result,true).'</pre>';
 
 		return $result;
         
