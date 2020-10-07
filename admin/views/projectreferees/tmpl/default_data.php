c1e0246050660738303fdab0723343a938947b3b
diddipoeler <diddipoeler@gmx.de>
2020-10-07 14:51:17 +0200

    Update default_data.php
    #1125

--- admin/views/projectreferees/tmpl/default_data.php
+++ admin/views/projectreferees/tmpl/default_data.php
@@ -113,7 +113,7 @@ $this->dragable_group = 'data-dragable-group="none"';
 			$link       = Route::_('index.php?option=com_sportsmanagement&task=projectreferee.edit&id=' . $this->item->id . '&pid=' . $this->item->project_id . '&person_id=' . $this->item->person_id);
 			$canEdit    = $this->user->authorise('core.edit', 'com_sportsmanagement');
 			$canCheckin = $this->user->authorise('core.manage', 'com_checkin') || $this->item->checked_out == $this->user->get('id') || $this->item->checked_out == 0;
-			$checked    = HTMLHelper::_('jgrid.checkedout', $i, $this->user->get('id'), $this->item->checked_out_time, 'projectreferees.', $canCheckin);
+			$checked    = HTMLHelper::_('jgrid.checkedout', $this->count_i, $this->user->get('id'), $this->item->checked_out_time, 'projectreferees.', $canCheckin);
 
 			$inputappend = '';
 			?>
