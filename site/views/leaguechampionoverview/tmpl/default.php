661879ab820ed2011a24518e1565c7ca28e1c5aa
diddipoeler <diddipoeler@gmx.de>
2020-10-09 17:19:34 +0200

    code

---
+++ site/views/leaguechampionoverview/tmpl/default.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * SportsManagement ein Programm zur Verwaltung für alle Sportarten
+ * @version    1.0.05
+ * @package    Sportsmanagement
+ * @subpackage leaguechampionoverview
+ * @file       default.php
+ * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright  Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
+defined('_JEXEC') or die('Restricted access');
+$templatesToLoad = array('globalviews');
+sportsmanagementHelper::addTemplatePaths($templatesToLoad, $this);
+?>
+<div class="<?php echo $this->divclasscontainer; ?>" id="defaultleaguechampionoverview">
+	<?php
+	echo $this->loadTemplate('projectheading');
+//	echo $this->loadTemplate('ranking');
+	?>
+</div>
