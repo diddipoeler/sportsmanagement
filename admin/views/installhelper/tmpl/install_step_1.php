7b554aa4216fc6d3473a2a2053f96188d17f38cd
diddipoeler <diddipoeler@gmx.de>
2021-01-02 17:07:40 +0100

    wizard

--- admin/views/installhelper/tmpl/install_step_1.php
+++ admin/views/installhelper/tmpl/install_step_1.php
@@ -16,13 +16,15 @@ use Joomla\CMS\Component\ComponentHelper;
 
 $templatesToLoad = array('footer', 'listheader');
 sportsmanagementHelper::addTemplatePaths($templatesToLoad, $this);
+
+$this->document->addStyleSheet(Uri::root() . 'administrator/components/com_sportsmanagement/assets/css/extended-1.1.css', 'text/css');
 ?>
 <!--Tip Box-->
                 <div class="color-box space">
                     <div class="shadow">
                         <div class="info-tab tip-icon" title="Useful Tips"><i></i></div>
                         <div class="tip-box">
-                            <p><strong>Tip:</strong> The <code>.fade</code> and <code>.show</code> classes on the <code>.alert</code> element enable the fading transition effect while closing the alert boxes. If you don't want animation just removes these classes. Also, the class <code>.alert-dismissible</code> is required on the <code>.alert</code> element for proper positioning of the <code>.close</code> button. If your alert doesn't have a close button you can skip this class.</p>
+                            <p><strong>Tip:</strong></p>
                         </div>
                     </div>
                </div>
