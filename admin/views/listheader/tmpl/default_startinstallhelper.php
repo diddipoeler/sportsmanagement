4927812a129a004d20cc91df1d005d9b56ad1562
diddipoeler <diddipoeler@gmx.de>
2021-01-02 12:42:06 +0100

    wizard

--- admin/views/listheader/tmpl/default_startinstallhelper.php
+++ admin/views/listheader/tmpl/default_startinstallhelper.php
@@ -15,3 +15,20 @@ use Joomla\CMS\Language\Text;
 use Joomla\CMS\Component\ComponentHelper;
 
 ?>
+<section class="content-block" role="main">
+<div class="row-fluid">
+<div class="span9">
+<div class="well well-small">
+
+<div id="dashboard-icons" class="btn-group">
+                                <a class="btn" href="index.php?option=com_sportsmanagement&view=installhelper&step=1">
+                                    <img src="components/com_sportsmanagement/assets/icons/wizard-1.png"
+                                         alt="<?php echo Text::_('COM_SPORTSMANAGEMENT_D_MENU_INSTALLHELPER_STEP1') ?>"/><br/>
+                                    <span><?php echo Text::_('COM_SPORTSMANAGEMENT_D_MENU_INSTALLHELPER_STEP1') ?></span>
+                                </a>
+</div>                                
+
+</div>
+</div>
+</div>
+</section>
