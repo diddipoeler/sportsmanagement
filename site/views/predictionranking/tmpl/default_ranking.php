008ca33da42bfbf8c3c59827008ffc137d8e5fe5
jst71 <joerg@stelteronline.de>
2021-01-06 15:33:06 +0100

    diddipoeler#984: optimized presentation of final4 as 4 column table

--- site/views/predictionranking/tmpl/default_ranking.php
+++ site/views/predictionranking/tmpl/default_ranking.php
@@ -244,7 +244,7 @@ foreach (sportsmanagementModelPrediction::$_predictionProjectS AS $predictionPro
 					if ($this->config['show_final4_tip'])
 					{
 						?>
-                        <th style='text-align:center; vertical-align:top; '><?php echo Text::_('COM_SPORTSMANAGEMENT_PRED_RANK_FINAL4_TIP'); ?></th><?php
+                        <th colspan="4" style='text-align:center; vertical-align:top; '><?php echo Text::_('COM_SPORTSMANAGEMENT_PRED_RANK_FINAL4_TIP'); ?></th><?php
 					}
 
 					if ($this->config['show_tip_details'])
@@ -705,9 +705,9 @@ foreach (sportsmanagementModelPrediction::$_predictionProjectS AS $predictionPro
 									// soll der meistertipp angezeigt werden ? ende
 									if ($this->config['show_final4_tip'])
 									{
-										if (is_array($membersDataArray[$key]['final4_tipps'])) {?>
-											<td <?php echo $tdStyleStr; ?> ><?php
-                                            foreach ($membersDataArray[$key]['final4_tipps'] as $final4_tip) {
+										if (is_array($membersDataArray[$key]['final4_tipps'])) {
+                                            foreach ($membersDataArray[$key]['final4_tipps'] as $final4_tip) {?>
+												<td <?php echo $tdStyleStr; ?>><span style="display:inline;"><?php
                                                 if ($this->config['show_final4_tip_club_logo']) {
                                                     if ($showProjectID) {
                                                         $final4Logo = sportsmanagementModelProject::getTeaminfo( $final4_tip['final4_tipp'][1], 0);
@@ -730,11 +730,10 @@ foreach (sportsmanagementModelPrediction::$_predictionProjectS AS $predictionPro
                                                 echo $imgFile;
                                                 if ($this->config['show_final4_tip_result'] && ($final4_tip['final4_tipp_result'] !== false)) {
                                                     echo "<sub style='color:#7D0204;font-weight:bold;'>".$final4_tip['final4_tipp_result']."</sub>";
-												}
-												echo " ";
-                                            }?>
-											</td>
-											<?php
+												}?>
+												</span></td>
+												<?php
+                                            }
 										}
 										else
 										{
