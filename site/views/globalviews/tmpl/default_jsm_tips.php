ba24840c86577de9163e01a34bae17aa329692f0
diddipoeler <diddipoeler@gmx.de>
2021-01-22 14:35:22 +0100

    utf-8

--- site/views/globalviews/tmpl/default_jsm_tips.php
+++ site/views/globalviews/tmpl/default_jsm_tips.php
@@ -1,12 +1,12 @@
 <?php
 /**
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
+ * SportsManagement ein Programm zur Verwaltung fÃ¼r alle Sportarten
  * @version    1.0.05
  * @package    Sportsmanagement
  * @subpackage listheader
  * @file       default_jsm_tips.php
  * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @copyright  Copyright: Â© 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 defined('_JEXEC') or die('Restricted access');
@@ -20,7 +20,7 @@ if ( $this->tips )
 {
 $tips = implode("<br>",$this->tips);  
 ?>
-<!--Tip Box grün -->
+<!--Tip Box grÃ¼n -->
 <div class="color-box">
 					<div class="shadow">
 						<div class="info-tab tip-icon" title="<?php echo Text::_('COM_SPORTSMANAGEMENT_GLOBAL_TIP'); ?>"><i></i></div>
@@ -33,7 +33,7 @@ $tips = implode("<br>",$this->tips);
 						</div>
 					</div>
 </div>
-<!--Tip Box grün -->
+<!--Tip Box grÃ¼n -->
 
 <?php  
 }
