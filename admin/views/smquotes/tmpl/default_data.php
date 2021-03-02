1eef74b04b64d181114b7f246ba93ae5f0a826c0
diddipoeler <diddipoeler@gmx.de>
2021-03-02 12:05:22 +0100

    Update default_data.php
    joomla 4

--- admin/views/smquotes/tmpl/default_data.php
+++ admin/views/smquotes/tmpl/default_data.php
@@ -90,11 +90,11 @@ $this->dragable_group = 'data-dragable-group="none"';
 			$link       = Route::_('index.php?option=com_sportsmanagement&task=smquote.edit&id=' . $this->item->id);
 			$canEdit    = $this->user->authorise('core.edit', 'com_sportsmanagement');
 			$canCheckin = $this->user->authorise('core.manage', 'com_checkin') || $this->count_i->checked_out == $this->user->get('id') || $this->item->checked_out == 0;
-			$checked    = HTMLHelper::_('jgrid.checkedout', $i, $this->user->get('id'), $this->count_i->checked_out_time, 'smquotes.', $canCheckin);
+			$checked    = HTMLHelper::_('jgrid.checkedout', $this->count_i, $this->user->get('id'), $this->count_i->checked_out_time, 'smquotes.', $canCheckin);
 			$canChange  = $this->user->authorise('core.edit.state', 'com_sportsmanagement.smquote.' . $this->item->id) && $canCheckin;
 			?>
             <tr class="row<?php echo $this->count_i % 2; ?>" <?php echo $this->dragable_group; ?>>
-                <td class="center"><?php echo $this->pagination->getRowOffset($i); ?></td>
+                <td class="center"><?php echo $this->pagination->getRowOffset($this->count_i); ?></td>
                 <td class="center"><?php echo HTMLHelper::_('grid.id', $this->count_i, $this->item->id); ?></td>
 				<?php
 
