19b490522281c7fe61460dadcba5bbdf2566b0bc
diddipoeler <diddipoeler@gmx.de>
2021-03-03 11:03:07 +0100

    Update default_jsm_warnings.php
    joomla 4

--- admin/views/listheader/tmpl/default_jsm_warnings.php
+++ admin/views/listheader/tmpl/default_jsm_warnings.php
@@ -1,18 +1,26 @@
 <?php
 /**
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
+ * SportsManagement ein Programm zur Verwaltung fÃ¼r alle Sportarten
  * @version    1.0.05
  * @package    Sportsmanagement
  * @subpackage listheader
  * @file       default_jsm_warnings.php
  * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @copyright  Copyright: Â© 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 defined('_JEXEC') or die('Restricted access');
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
 
+if (version_compare(substr(JVERSION, 0, 3), '4.0', 'ge'))
+{
+$boxicon = 'icon-fw';
+}
+else	
+{
+$boxicon = 'warning-icon';	
+}
 ?>
 
 <?php
@@ -22,19 +30,15 @@ $warnings = implode("<br>",$this->warnings);
 ?>
 <!--Warning box rot -->
 <div class="color-box">
-					<div class="shadow">
-						<div class="info-tab warning-icon" title="<?php echo Text::_('COM_SPORTSMANAGEMENT_GLOBAL_WARNING'); ?>"><i></i></div>
-						<div class="warning-box">
-							<p><strong><?php echo Text::_('COM_SPORTSMANAGEMENT_GLOBAL_WARNING'); ?></strong>
-                            <?php echo $warnings; ?>
-                            
-                            </p>
-						</div>
-					</div>
+<div class="shadow">
+<div class="info-tab <?php echo $boxicon;?>" title="<?php echo Text::_('COM_SPORTSMANAGEMENT_GLOBAL_WARNING'); ?>"><i></i></div>
+<div class="warning-box">
+<p><strong><?php echo Text::_('COM_SPORTSMANAGEMENT_GLOBAL_WARNING'); ?></strong>
+<?php echo $warnings; ?>
+</p>
+</div>
+</div>
 </div>
 <!--Warning box rot -->
-
-
-
 <?php  
 }
