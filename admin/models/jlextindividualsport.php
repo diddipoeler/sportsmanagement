37d8633c1c0c0cde195538baaa5a4f431fb60131
diddipoeler <diddipoeler@gmx.de>
2021-03-13 15:23:44 +0100

    code

--- admin/models/jlextindividualsport.php
+++ admin/models/jlextindividualsport.php
@@ -104,8 +104,8 @@ class sportsmanagementModeljlextindividualsport extends JSMModelAdmin
 		case 'COM_SPORTSMANAGEMENT_ST_SMALL_BORE_RIFLE_ASSOCIATION':
         $ringetotal = 0;
         
-//        $this->jsmapp->enqueueMessage(Text::_(__METHOD__ . ' ' . ' ' . __LINE__ . ' ' . '<pre>'.print_r($pks,true).'</pre>'), 'error');
-//        $this->jsmapp->enqueueMessage(Text::_(__METHOD__ . ' ' . ' ' . __LINE__ . ' ' . '<pre>'.print_r($post,true).'</pre>'), 'error');
+        $this->jsmapp->enqueueMessage(Text::_(__METHOD__ . ' ' . ' ' . __LINE__ . ' ' . '<pre>'.print_r($pks,true).'</pre>'), 'error');
+        $this->jsmapp->enqueueMessage(Text::_(__METHOD__ . ' ' . ' ' . __LINE__ . ' ' . '<pre>'.print_r($post,true).'</pre>'), 'error');
         for ($x = 0; $x < count($pks); $x++)
 		{
 		$rowmatch                          = new stdClass;
