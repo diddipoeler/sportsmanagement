9b148b0b48f3b970ff3803032aeab89ece69f7ab
diddipoeler <diddipoeler@gmx.de>
2021-03-16 10:42:18 +0100

    code

--- admin/views/eventtypes/tmpl/default_data.php
+++ admin/views/eventtypes/tmpl/default_data.php
@@ -14,6 +14,7 @@ use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\Router\Route;
 use Joomla\CMS\Session\Session;
+use Joomla\CMS\Uri\Uri;
 
 $this->saveOrder = $this->sortColumn == 'obj.ordering';
 if (version_compare(substr(JVERSION, 0, 3), '4.0', 'ge'))
