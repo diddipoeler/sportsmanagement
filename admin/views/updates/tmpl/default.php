64270525372427cd5da6b18fce3b048e86fb4723
diddipoeler <diddipoeler@gmx.de>
2021-03-17 16:53:40 +0100

    code

--- admin/views/updates/tmpl/default.php
+++ admin/views/updates/tmpl/default.php
@@ -43,30 +43,15 @@ sportsmanagementHelper::addTemplatePaths($templatesToLoad, $this);
 </style>
 
 <form action="<?php echo $this->request_url; ?>" method="post" id="adminForm" name="adminForm">
-	<?PHP
-    echo $this->loadTemplate('joomla_version');
-  /*  
-	if (version_compare(JVERSION, '3.0.0', 'ge'))
-	{
-		echo $this->loadTemplate('joomla3');
-	}
-	else
-	{
-		echo $this->loadTemplate('joomla2');
-	}
-*/
-
-	?>
-
-
-
-
-    <input type="hidden" name="view" value="updates"/>
-    <input type="hidden" name="task" value=""/>
-    <input type="hidden" name="boxchecked" value="0"/>
-    <input type="hidden" name="filter_order" value="<?php echo $this->sortColumn; ?>"/>
-    <input type="hidden" name="filter_order_Dir" value="<?php echo $this->sortDirection; ?>"/>
-	<?php echo HTMLHelper::_('form.token') . "\n"; ?>
+<?PHP
+echo $this->loadTemplate('joomla_version');
+?>
+<input type="hidden" name="view" value="updates"/>
+<input type="hidden" name="task" value=""/>
+<input type="hidden" name="boxchecked" value="0"/>
+<input type="hidden" name="filter_order" value="<?php echo $this->sortColumn; ?>"/>
+<input type="hidden" name="filter_order_Dir" value="<?php echo $this->sortDirection; ?>"/>
+<?php echo HTMLHelper::_('form.token') . "\n"; ?>
 </form>
 <div>
 <?PHP
