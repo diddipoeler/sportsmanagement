12e0885f782fb96460b75d80c09767bae4e5773f
diddipoeler <diddipoeler@gmx.de>
2021-03-24 16:11:00 +0100

    Update default_person_staff.php
    #1252

--- site/views/roster/tmpl/default_person_staff.php
+++ site/views/roster/tmpl/default_person_staff.php
@@ -1,8 +1,6 @@
 <?php
 /**
- *
  * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- *
  * @version    1.0.05
  * @package    Sportsmanagement
  * @subpackage roster
@@ -11,9 +9,7 @@
  * @copyright  Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
-
 defined('_JEXEC') or die('Restricted access');
-
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Factory;
@@ -47,7 +43,7 @@ use Joomla\CMS\Factory;
 					'st' . $this->row->person_id,
 					$picture,
 					$personName,
-					$this->config['staff_picture_width'],
+					$this->config['player_picture_height'],
 					'',
 					$this->modalwidth,
 					$this->modalheight,
