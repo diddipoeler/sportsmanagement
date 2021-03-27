bd97e21e42c363e0e041116a1431efe08ccc8472
diddipoeler <diddipoeler@gmx.de>
2021-03-27 12:48:04 +0100

    code

--- script.php
+++ script.php
@@ -309,6 +309,10 @@ class com_sportsmanagementInstallerScript
 	function preflight($route,  $adapter)
 	{
 
+echo '<p> Aktion ' .  $route . '</p>';
+echo '<p> neue Version' .  $this->release . '</p>';
+echo '<p> alte Version' .  $this->oldRelease . '</p>';
+
 		if ($route == 'update')
 		{
 			$this->oldRelease = $this->getParam('version');
