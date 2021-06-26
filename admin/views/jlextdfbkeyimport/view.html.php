c7676488654bd088f7d4c32f831150733f7820e7
diddipoeler <diddipoeler@gmx.de>
2021-06-26 13:04:46 +0200

    code

--- admin/views/jlextdfbkeyimport/view.html.php
+++ admin/views/jlextdfbkeyimport/view.html.php
@@ -1,8 +1,6 @@
 <?php
 /**
- *
  * SportsManagement ein Programm zur Verwaltung für Sportarten
- *
  * @version    1.0.05
  * @package    Sportsmanagement
  * @subpackage jlextdfbkeyimport
@@ -11,9 +9,7 @@
  * @copyright  Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
-
 defined('_JEXEC') or die('Restricted access');
-
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Uri\Uri;
 use Joomla\CMS\Language\Text;
@@ -164,6 +160,7 @@ class sportsmanagementViewjlextdfbkeyimport extends sportsmanagementView
 				{
 					$procountry = $this->model->getCountry($this->project_id);
 					Log::add(Text::sprintf('COM_SPORTSMANAGEMENT_ADMIN_DFBKEYS_ERROR_6', $dfbteams, JSMCountries::getCountryFlag($procountry), $procountry), Log::WARNING, 'jsmerror');
+                    $this->tips[] = Text::sprintf('COM_SPORTSMANAGEMENT_ADMIN_DFBKEYS_ERROR_6', $dfbteams, JSMCountries::getCountryFlag($procountry), $procountry);
 					$this->app->redirect('index.php?option=' . $this->option . '&view=projects');
 				}
 
