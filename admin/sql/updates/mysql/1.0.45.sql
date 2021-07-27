a927daab656314d4cc25e48d1588473e39598ed5
diddipoeler <diddipoeler@gmx.de>
2021-07-27 12:44:44 +0200

    Update 1.0.45.sql
    code

--- admin/sql/updates/mysql/1.0.45.sql
+++ admin/sql/updates/mysql/1.0.45.sql
@@ -1,8 +1,8 @@
 ALTER TABLE  `#__sportsmanagement_team` ADD `standard_playground` INT(11) NULL DEFAULT NULL ;
 ALTER TABLE  `#__sportsmanagement_season_team_id` ADD `standard_playground` INT(11) NULL DEFAULT NULL ;
 
-ALTER TABLE  `#__sportsmanagement_project_position` ADD `checked_out` INT(11) NOT NULL DEFAULT '0' ;
-ALTER TABLE  `#__sportsmanagement_project_position` ADD `checked_out_time` DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00' ;
+-- ALTER TABLE  `#__sportsmanagement_project_position` ADD `checked_out` INT(11) NOT NULL DEFAULT '0' ;
+-- ALTER TABLE  `#__sportsmanagement_project_position` ADD `checked_out_time` DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00' ;
 ALTER TABLE  `#__sportsmanagement_project_position` ADD `jl_update` TINYINT(1) NOT NULL DEFAULT '0' ;
 
 ALTER TABLE  `#__sportsmanagement_person` ADD `jl_update` TINYINT(1) NOT NULL DEFAULT '0' ;
