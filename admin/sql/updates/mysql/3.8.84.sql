6b7d006dc01af9da38739b00387fe3f2c714e062
diddipoeler <diddipoeler@gmx.de>
2021-07-29 10:31:14 +0200

    Create 3.8.84.sql
    code

---
+++ admin/sql/updates/mysql/3.8.84.sql
@@ -0,0 +1,2 @@
+ALTER TABLE  `#__sportsmanagement_countries` CHANGE `countrymap_mapdata` `countrymap_mapdata` MEDIUMTEXT NULL DEFAULT NULL;
+ALTER TABLE  `#__sportsmanagement_countries` CHANGE `countrymap_mapinfo` `countrymap_mapinfo` MEDIUMTEXT NULL DEFAULT NULL;
