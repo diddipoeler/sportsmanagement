67d234ce3e8fbd6b313e67afc6f50fde397fc23b
diddipoeler <diddipoeler@gmx.de>
2021-10-13 14:30:11 +0200

    Update rosterposition.php
    #1326

--- admin/tables/rosterposition.php
+++ admin/tables/rosterposition.php
@@ -1,8 +1,6 @@
 <?php
 /**
- *
  * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- *
  * @version    1.0.05
  * @package    Sportsmanagement
  * @subpackage tables
@@ -11,10 +9,7 @@
  * @copyright  Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
-
-
 defined('_JEXEC') or die('Restricted access');
-
 use Joomla\CMS\Filter\OutputFilter;
 
 /**
@@ -51,6 +46,7 @@ class sportsmanagementTablerosterposition extends JSMTable
 	 */
 	function check()
 	{
+		/*
 		// Setting alias
 		if (empty($this->alias))
 		{
@@ -60,6 +56,7 @@ class sportsmanagementTablerosterposition extends JSMTable
 		{
 			$this->alias = OutputFilter::stringURLSafe($this->alias); // Make sure the user didn't modify it to something illegal...
 		}
+		*/
 
 		// Should check name unicity
 		return true;
