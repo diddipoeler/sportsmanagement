cbb8a41479f5aaba47e06bc9e1d88190d4c2fd27
diddipoeler <diddipoeler@gmx.de>
2021-10-13 15:07:21 +0200

    Update rosterposition.php
    #1326

--- admin/models/rosterposition.php
+++ admin/models/rosterposition.php
@@ -1,8 +1,6 @@
 <?php
 /**
- *
  * SportsManagement ein Programm zur Verwaltung für Sportarten
- *
  * @version    1.0.05
  * @package    Sportsmanagement
  * @subpackage models
@@ -11,9 +9,7 @@
  * @copyright  Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
-
 defined('_JEXEC') or die('Restricted access');
-
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\Factory;
 use Joomla\Registry\Registry;
@@ -61,7 +57,7 @@ class sportsmanagementModelrosterposition extends JSMModelAdmin
 		// Set the values
 		$data['modified']    = $date->toSql();
 		$data['modified_by'] = $user->get('id');
-		$data['alias']       = OutputFilter::stringURLSafe($data['name']);
+		//$data['alias']       = OutputFilter::stringURLSafe($data['name']);
 
 		// Zuerst sichern, damit wir bei einer neuanlage die id haben
 		if (parent::save($data))
