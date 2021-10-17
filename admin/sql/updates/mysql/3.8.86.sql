2a55c2f7038b85da576f6b7cf08494454083758d
diddipoeler <diddipoeler@gmx.de>
2021-10-17 13:26:32 +0200

    update

--- admin/sql/updates/mysql/3.8.86.sql
+++ admin/sql/updates/mysql/3.8.86.sql
@@ -1,4 +1,4 @@
-ALTER TABLE '#__sportsmanagement_project' ADD 'fav_team_send_mail' TINYINT(4) NOT NULL DEFAULT '1';
+ALTER TABLE `#__sportsmanagement_project` ADD `fav_team_send_mail` TINYINT(4) NOT NULL DEFAULT '1';
 
 INSERT INTO `#__sportsmanagement_version_history` (`id`, `date`, `text`, `version`) VALUES
 (NULL, '2021-10-17', 'COM_SPORTSMANAGEMENT_DB_UPDATE_2021-10-17', '3.8.86');
