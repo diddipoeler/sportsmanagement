3f71ec605010dadb33462fcbf6ee8bca90ff8622
diddipoeler <diddipoeler@gmx.de>
2021-11-03 13:36:57 +0100

    Update view.html.php
    code

--- site/views/teamstats/view.html.php
+++ site/views/teamstats/view.html.php
@@ -33,6 +33,7 @@ class sportsmanagementViewTeamStats extends sportsmanagementView
 	{
 		if ($this->config['show_goals_stats_flash'])
 		{
+			//$js = 'https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.3/Chart.js';
 			$js = 'https://cdnjs.cloudflare.com/ajax/libs/Chart.js/'.$this->chart_version.'/chart.min.js';
 			$this->document->addScript($js);
 		}
