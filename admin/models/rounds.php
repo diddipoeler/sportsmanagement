5c4eb5334c6e628c72da8149b3059088fca199a0
diddipoeler <diddipoeler@gmx.de>
2021-11-25 07:20:35 +0100

    Update rounds.php
    code

--- admin/models/rounds.php
+++ admin/models/rounds.php
@@ -547,12 +547,9 @@ class sportsmanagementModelRounds extends JSMModelList
 		$db     = sportsmanagementHelper::getDBConnection(true, $cfg_which_database);
 		$query  = $db->getQuery(true);
 
-		// Select some fields
 		$query->select('CONCAT_WS( \':\', id, alias ) AS value');
 		$query->select('name AS text');
 		$query->select('id, name, round_date_first, round_date_last, roundcode');
-
-		// From the table
 		$query->from('#__sportsmanagement_round');
 		$query->where('project_id = ' . $project_id);
 		$query->order('roundcode ' . $ordering);
@@ -564,8 +561,7 @@ class sportsmanagementModelRounds extends JSMModelList
 		}
 		catch (Exception $e)
 		{
-			$this->jsmapp->enqueueMessage(Text::_($e->getMessage()), 'error');
-
+			$app->enqueueMessage(Text::_($e->getMessage()), 'error');
 			return false;
 		}
 
