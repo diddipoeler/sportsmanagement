a02349b72d9a1d5afa651d10f59a13083812ce5d
diddipoeler <diddipoeler@gmx.de>
2022-03-04 18:05:48 +0100

    Update mod_sportsmanagement_ajax_top_navigation_menu.php
    joomla 4

--- modules/mod_sportsmanagement_ajax_top_navigation_menu/mod_sportsmanagement_ajax_top_navigation_menu.php
+++ modules/mod_sportsmanagement_ajax_top_navigation_menu/mod_sportsmanagement_ajax_top_navigation_menu.php
@@ -612,15 +612,27 @@ Factory::getDocument()->addScriptDeclaration(implode("\n", $script));
 if ( !array_key_exists($country_federation, $countryassocselect) ) {
 $countryassocselect[$country_federation] = array();
 }
+if ( !array_key_exists($countrysubsubassocselect, $countryassocselect) ) {
+$countrysubsubassocselect[$country_federation] = array();
+}
+if ( !array_key_exists($countrysubsubsubassocselect, $countryassocselect) ) {
+$countrysubsubsubassocselect[$country_federation] = array();
+}
+
+
+
+
+
 
 if ( !array_key_exists('assocs', $countryassocselect[$country_federation]) ) {
 $countryassocselect[$country_federation]['assocs'] = array();
 }
 
-if ( !is_array($countrysubsubassocselect) )
-{
-$countrysubsubassocselect = array();
-}
+//if ( !is_array($countrysubsubassocselect) )
+//{
+//$countrysubsubassocselect = array();
+//}
+
 if ( !array_key_exists('subassocs', $countrysubsubassocselect[$country_federation]) ) {
 $countrysubsubassocselect[$country_federation]['subassocs'] = array();
 }
