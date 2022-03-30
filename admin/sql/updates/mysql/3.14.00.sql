b15d9867ac93e2c6be4da734b7ea055a85fcf8b0
diddipoeler <diddipoeler@gmx.de>
2022-03-30 17:39:51 +0200

    neue Version

---
+++ admin/sql/updates/mysql/3.14.00.sql
@@ -0,0 +1,7 @@
+ALTER TABLE  `#__sportsmanagement_match` CHANGE `decision_info` `decision_info` VARCHAR(128) NULL DEFAULT NULL;
+ALTER TABLE  `#__sportsmanagement_match` CHANGE `cancel_reason` `cancel_reason` VARCHAR(32) NULL DEFAULT NULL;
+ALTER TABLE  `#__sportsmanagement_match` CHANGE `match_result_detail` `match_result_detail` VARCHAR(64) NULL DEFAULT NULL;
+
+
+INSERT INTO `#__sportsmanagement_version_history` (`id`, `date`, `text`, `version`) VALUES
+(NULL, '2022-03-30', 'COM_SPORTSMANAGEMENT_DB_UPDATE_2022-03-30', '3.13.00');
