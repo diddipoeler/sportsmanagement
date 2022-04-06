e931f0b78e83cb028d10850d2d169fb051f625b7
diddipoeler <diddipoeler@gmx.de>
2022-04-06 12:16:13 +0200

    Update currentseason.php
    #1212

--- admin/controllers/currentseason.php
+++ admin/controllers/currentseason.php
@@ -1,8 +1,6 @@
 <?php
 /**
- *
  * SportsManagement ein Programm zur Verwaltung für Sportarten
- *
  * @version    1.0.05
  * @package    Sportsmanagement
  * @subpackage controllers
@@ -11,10 +9,8 @@
  * @copyright  Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
-
 defined('_JEXEC') or die('Restricted access');
-
-jimport('joomla.application.component.controller');
+//jimport('joomla.application.component.controller');
 jimport('joomla.filesystem.file');
 
 /**
@@ -26,7 +22,7 @@ jimport('joomla.filesystem.file');
  * @version   2014
  * @access    public
  */
-class sportsmanagementControllercurrentseason extends JController
+class sportsmanagementControllercurrentseason extends JSMControllerAdmin
 {
 	protected $view_list = 'currentseasons';
 
