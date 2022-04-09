db17573ca339c94bcb665772ba5b7fd267f43acb
diddipoeler <diddipoeler@gmx.de>
2022-04-09 12:28:00 +0200

    #1439

--- admin/views/jsmgcalendar/view.html.php
+++ admin/views/jsmgcalendar/view.html.php
@@ -44,6 +44,8 @@ class sportsmanagementViewjsmgcalendar extends sportsmanagementView
 			$this->form->setValue('username', null, ComponentHelper::getParams(Factory::getApplication()->input->getCmd('option'))->get('google_mail_account', ''));
 			$this->form->setValue('password', null, ComponentHelper::getParams(Factory::getApplication()->input->getCmd('option'))->get('google_mail_password', ''));
 		}
+        
+        $this->extended = sportsmanagementHelper::getExtended($this->item->params, 'jsmgcalendar');
 
 	}
 
