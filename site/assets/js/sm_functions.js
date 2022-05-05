4689b84dc18486b8f1807dae026eb53b5051c845
diddipoeler <diddipoeler@gmx.de>
2022-05-05 15:34:59 +0200

    Update sm_functions.js
    code

--- site/assets/js/sm_functions.js
+++ site/assets/js/sm_functions.js
@@ -160,7 +160,10 @@ console.log("move_list_items sourceid team : "+team);
 //alert(destinationid);
 jQuery("#"+sourceid+"  option:selected").appendTo("#"+destinationid);
 	
-jQuery("#"+destinationtext+"").append(jQuery('<option></option>').attr("value", laengetext).text(team));	
+jQuery("#"+destinationtext+"").append(jQuery('<option></option>').attr("value", laengetext).text(team));
+	
+jQuery('#postteamname').append("<input type='hidden' size='40' name='postteamname["+laengetext+"]' value='"+team+"' >"); 
+	
 }
 
 //this will move all selected items from source list to destination list
