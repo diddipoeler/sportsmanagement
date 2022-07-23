b701cc5ae2a5fff7fd636d028b8ced98dc3629ec
diddipoeler <diddipoeler@gmx.de>
2022-07-23 17:11:52 +0200

    code

--- site/views/uefawertung/view.html.php
+++ site/views/uefawertung/view.html.php
@@ -52,7 +52,7 @@ $lists['coefficientyears'] = HTMLHelper::_(
 			'class="inputbox" onChange="this.form.submit();" style="width:120px"',
 			'id',
 			'name',
-			$this->jsmjinput->post->get('coefficientyear', '')
+			$this->jinput->post->get('coefficientyear', '')
 		);
 
 
