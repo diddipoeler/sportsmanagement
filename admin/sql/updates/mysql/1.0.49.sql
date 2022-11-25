f391bf92e7e1abc1898a614f2c050e1b5e3fe1cf
diddipoeler <diddipoeler@gmx.de>
2022-11-25 13:26:03 +0100

    Update 1.0.49.sql
    code

--- admin/sql/updates/mysql/1.0.49.sql
+++ admin/sql/updates/mysql/1.0.49.sql
@@ -1,5 +1,4 @@
 
 
 
-INSERT INTO `#__sportsmanagement_version_history` (`id`, `date`, `text`, `version`) VALUES
-(NULL, '2015-02-28', 'COM_SPORTSMANAGEMENT_DB_UPDATE_2015_04_01', '1.0.49');
+-- INSERT INTO `#__sportsmanagement_version_history` (`id`, `date`, `text`, `version`) VALUES (NULL, '2015-02-28', 'COM_SPORTSMANAGEMENT_DB_UPDATE_2015_04_01', '1.0.49');
