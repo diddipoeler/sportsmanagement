e3d4c09cb8d9916007349b2551801c6c51cffb07
diddipoeler <diddipoeler@gmx.de>
2022-11-25 12:50:22 +0100

    Update 1.0.27.sql
    code

--- admin/sql/updates/mysql/1.0.27.sql
+++ admin/sql/updates/mysql/1.0.27.sql
@@ -1,5 +1,5 @@
-ALTER TABLE  `#__sportsmanagement_club` ADD INDEX  `standard_playground` (  `standard_playground` );
-ALTER TABLE  `#__sportsmanagement_club` ADD INDEX  `country` (  `country` );
+-- ALTER TABLE  `#__sportsmanagement_club` ADD INDEX  `standard_playground` (  `standard_playground` );
+-- ALTER TABLE  `#__sportsmanagement_club` ADD INDEX  `country` (  `country` );
 
 ALTER TABLE  `#__sportsmanagement_league` ADD INDEX  `country` (  `country` );
 ALTER TABLE  `#__sportsmanagement_league` ADD INDEX  `sports_type_id` (  `sports_type_id` );
