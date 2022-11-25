e0dd246a72fe6d4e0ce17d7e0719c2783549623e
diddipoeler <diddipoeler@gmx.de>
2022-11-25 12:36:48 +0100

    Update 3.8.60.sql
    code

--- admin/sql/updates/mysql/3.8.60.sql
+++ admin/sql/updates/mysql/3.8.60.sql
@@ -1,4 +1,4 @@
-ALTER TABLE `#__sportsmanagement_match` ADD `ringetotal` int(11) DEFAULT NULL;
+-- ALTER TABLE `#__sportsmanagement_match` ADD `ringetotal` int(11) DEFAULT NULL;
 ALTER TABLE `#__sportsmanagement_match_single` ADD `ringetotal` int(11) DEFAULT NULL;
 
 INSERT INTO `#__sportsmanagement_version_history` (`id`, `date`, `text`, `version`) VALUES
