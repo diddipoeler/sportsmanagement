f73518da05aaa42bb4db5437738c72972f993e6c
diddipoeler <diddipoeler@gmx.de>
2022-11-25 13:17:58 +0100

    Update 1.0.33.sql
    code

--- admin/sql/updates/mysql/1.0.33.sql
+++ admin/sql/updates/mysql/1.0.33.sql
@@ -1,4 +1,3 @@
-ALTER TABLE  `#__sportsmanagement_match` CHANGE  `match_number`  `match_number` VARCHAR( 200 ) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL ;
+-- ALTER TABLE  `#__sportsmanagement_match` CHANGE  `match_number`  `match_number` VARCHAR( 200 ) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL ;
 
-INSERT INTO `#__sportsmanagement_version_history` (`id`, `date`, `text`, `version`) VALUES
-(NULL, '2014-04-21', 'COM_SPORTSMANAGEMENT_DB_UPDATE_2014_04_21', '1.0.33');
+-- INSERT INTO `#__sportsmanagement_version_history` (`id`, `date`, `text`, `version`) VALUES (NULL, '2014-04-21', 'COM_SPORTSMANAGEMENT_DB_UPDATE_2014_04_21', '1.0.33');
