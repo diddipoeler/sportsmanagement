2c5f619fd53897e5b7e611f0fec0144941f56c59
diddipoeler <diddipoeler@gmx.de>
2022-11-25 12:19:53 +0100

    Update 1.0.05.sql
    code

--- admin/sql/updates/mysql/1.0.05.sql
+++ admin/sql/updates/mysql/1.0.05.sql
@@ -1,4 +1,4 @@
-ALTER TABLE  `#__sportsmanagement_person` ADD  `season_ids` TEXT NULL ;
+-- ALTER TABLE  `#__sportsmanagement_person` ADD  `season_ids` TEXT NULL ;
 ALTER TABLE  `#__sportsmanagement_team` ADD  `season_ids` TEXT NULL ;
 
 -- -----------------------------------------------------
