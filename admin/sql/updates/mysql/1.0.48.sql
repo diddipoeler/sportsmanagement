7607ddd4b34f3b885b565be3bef4a5375c51ca10
diddipoeler <diddipoeler@gmx.de>
2022-11-25 13:25:44 +0100

    Update 1.0.48.sql
    code

--- admin/sql/updates/mysql/1.0.48.sql
+++ admin/sql/updates/mysql/1.0.48.sql
@@ -1,19 +1,19 @@
-ALTER TABLE  `#__sportsmanagement_person` ADD  `hits` INT( 11 ) NOT NULL DEFAULT  '0';
-ALTER TABLE  `#__sportsmanagement_person` ADD  `modified_hits` INT( 11 ) NOT NULL DEFAULT  '0';
-ALTER TABLE  `#__sportsmanagement_playground` ADD  `hits` INT( 11 ) NOT NULL DEFAULT  '0';
-ALTER TABLE  `#__sportsmanagement_playground` ADD  `modified_hits` INT( 11 ) NOT NULL DEFAULT  '0';
+-- ALTER TABLE  `#__sportsmanagement_person` ADD  `hits` INT( 11 ) NOT NULL DEFAULT  '0';
+-- ALTER TABLE  `#__sportsmanagement_person` ADD  `modified_hits` INT( 11 ) NOT NULL DEFAULT  '0';
+-- ALTER TABLE  `#__sportsmanagement_playground` ADD  `hits` INT( 11 ) NOT NULL DEFAULT  '0';
+-- ALTER TABLE  `#__sportsmanagement_playground` ADD  `modified_hits` INT( 11 ) NOT NULL DEFAULT  '0';
 
-ALTER TABLE  `#__sportsmanagement_club` ADD  `hits` INT( 11 ) NOT NULL DEFAULT  '0';
-ALTER TABLE  `#__sportsmanagement_club` ADD  `modified_hits` INT( 11 ) NOT NULL DEFAULT  '0';
-ALTER TABLE  `#__sportsmanagement_team` ADD  `hits` INT( 11 ) NOT NULL DEFAULT  '0';
-ALTER TABLE  `#__sportsmanagement_team` ADD  `modified_hits` INT( 11 ) NOT NULL DEFAULT  '0';
+-- ALTER TABLE  `#__sportsmanagement_club` ADD  `hits` INT( 11 ) NOT NULL DEFAULT  '0';
+-- ALTER TABLE  `#__sportsmanagement_club` ADD  `modified_hits` INT( 11 ) NOT NULL DEFAULT  '0';
+-- ALTER TABLE  `#__sportsmanagement_team` ADD  `hits` INT( 11 ) NOT NULL DEFAULT  '0';
+-- ALTER TABLE  `#__sportsmanagement_team` ADD  `modified_hits` INT( 11 ) NOT NULL DEFAULT  '0';
 
-ALTER TABLE  `#__sportsmanagement_season_team_person_id` ADD  `position_id` INT( 11 ) NOT NULL DEFAULT  '0';
-ALTER TABLE  `#__sportsmanagement_season_person_id` ADD  `position_id` INT( 11 ) NOT NULL DEFAULT  '0';
+-- ALTER TABLE  `#__sportsmanagement_season_team_person_id` ADD  `position_id` INT( 11 ) NOT NULL DEFAULT  '0';
+-- ALTER TABLE  `#__sportsmanagement_season_person_id` ADD  `position_id` INT( 11 ) NOT NULL DEFAULT  '0';
 
-ALTER TABLE  `#__sportsmanagement_match_player` ADD `captain` TINYINT(1) NOT NULL DEFAULT '0' ;
+-- ALTER TABLE  `#__sportsmanagement_match_player` ADD `captain` TINYINT(1) NOT NULL DEFAULT '0' ;
 
-ALTER TABLE  `#__sportsmanagement_match` ADD  `overtime` INT( 11 ) NOT NULL DEFAULT  '0';
+-- ALTER TABLE  `#__sportsmanagement_match` ADD  `overtime` INT( 11 ) NOT NULL DEFAULT  '0';
 
 --
 -- Tabellenstruktur für Tabelle `#__sportsmanagement_person_project_position`
@@ -28,5 +28,4 @@ CREATE TABLE IF NOT EXISTS `#__sportsmanagement_person_project_position` (
   UNIQUE KEY `combi` (`person_id`,`project_id`,`project_position_id`,`persontype`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
-INSERT INTO `#__sportsmanagement_version_history` (`id`, `date`, `text`, `version`) VALUES
-(NULL, '2015-02-28', 'COM_SPORTSMANAGEMENT_DB_UPDATE_2015_02_28', '1.0.48');
+-- INSERT INTO `#__sportsmanagement_version_history` (`id`, `date`, `text`, `version`) VALUES (NULL, '2015-02-28', 'COM_SPORTSMANAGEMENT_DB_UPDATE_2015_02_28', '1.0.48');
