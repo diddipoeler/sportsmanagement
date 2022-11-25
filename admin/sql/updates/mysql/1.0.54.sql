51f78663be8c5a7a0cecd6b68d55839f058beb3a
diddipoeler <diddipoeler@gmx.de>
2022-11-25 13:28:47 +0100

    Update 1.0.54.sql
    code

--- admin/sql/updates/mysql/1.0.54.sql
+++ admin/sql/updates/mysql/1.0.54.sql
@@ -1,5 +1,4 @@
-ALTER TABLE  `#__sportsmanagement_round` ADD `rdatefirst_timestamp` INT( 11 ) NOT NULL DEFAULT  0 ;
-ALTER TABLE  `#__sportsmanagement_round` ADD `rdatelast_timestamp` INT( 11 ) NOT NULL DEFAULT  0 ;
+-- ALTER TABLE  `#__sportsmanagement_round` ADD `rdatefirst_timestamp` INT( 11 ) NOT NULL DEFAULT  0 ;
+-- ALTER TABLE  `#__sportsmanagement_round` ADD `rdatelast_timestamp` INT( 11 ) NOT NULL DEFAULT  0 ;
 
-INSERT INTO `#__sportsmanagement_version_history` (`id`, `date`, `text`, `version`) VALUES
-(NULL, '2015-11-31', 'COM_SPORTSMANAGEMENT_DB_UPDATE_2016_02_11', '1.0.54');
+-- INSERT INTO `#__sportsmanagement_version_history` (`id`, `date`, `text`, `version`) VALUES (NULL, '2015-11-31', 'COM_SPORTSMANAGEMENT_DB_UPDATE_2016_02_11', '1.0.54');
