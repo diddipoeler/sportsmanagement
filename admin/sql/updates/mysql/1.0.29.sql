da7c8880cc0c09859abfe1242196f03ec3aba37d
diddipoeler <diddipoeler@gmx.de>
2022-11-25 13:16:06 +0100

    Update 1.0.29.sql
    code

--- admin/sql/updates/mysql/1.0.29.sql
+++ admin/sql/updates/mysql/1.0.29.sql
@@ -1,8 +1,7 @@
-ALTER TABLE  `#__sportsmanagement_club` CHANGE  `dissolved_year`  `dissolved_year` VARCHAR( 4 ) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL ;
-ALTER TABLE  `#__sportsmanagement_club` CHANGE  `founded_year`  `founded_year` VARCHAR( 4 ) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL ;
-
-ALTER TABLE  `#__sportsmanagement_project_team` ADD  `import` TINYINT(1)  NOT NULL DEFAULT '0' ;
-ALTER TABLE  `#__sportsmanagement_team` ADD INDEX  `sports_type_id` (  `sports_type_id` );
+-- ALTER TABLE  `#__sportsmanagement_club` CHANGE  `dissolved_year`  `dissolved_year` VARCHAR( 4 ) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL ;
+-- ALTER TABLE  `#__sportsmanagement_club` CHANGE  `founded_year`  `founded_year` VARCHAR( 4 ) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL ;
+-- ALTER TABLE  `#__sportsmanagement_project_team` ADD  `import` TINYINT(1)  NOT NULL DEFAULT '0' ;
+-- ALTER TABLE  `#__sportsmanagement_team` ADD INDEX  `sports_type_id` (  `sports_type_id` );
 
 --
 -- Tabellenstruktur für Tabelle `#__sportsmanagement_jl_tables`
