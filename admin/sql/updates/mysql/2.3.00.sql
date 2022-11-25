b4f7b4e03de98fa7d7f57d59495cd095fa062290
diddipoeler <diddipoeler@gmx.de>
2022-11-25 12:32:37 +0100

    Update 2.3.00.sql
    code

--- admin/sql/updates/mysql/2.3.00.sql
+++ admin/sql/updates/mysql/2.3.00.sql
@@ -1,7 +1,7 @@
 ALTER TABLE `#__sportsmanagement_playground` CHANGE `notes` `notes` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;
 ALTER TABLE `#__sportsmanagement_division` CHANGE `notes` `notes` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;
 ALTER TABLE `#__sportsmanagement_match_commentary` CHANGE `notes` `notes` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;
-ALTER TABLE `#__sportsmanagement_match_event` CHANGE `notes` `notes` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;
+-- ALTER TABLE `#__sportsmanagement_match_event` CHANGE `notes` `notes` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;
 ALTER TABLE `#__sportsmanagement_season_team_person_id` CHANGE `notes` `notes` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;
 ALTER TABLE `#__sportsmanagement_rquote` CHANGE `notes` `notes` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;
 
