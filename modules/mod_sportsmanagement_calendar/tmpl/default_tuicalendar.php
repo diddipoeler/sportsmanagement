85309687d2f65386a8191df35d9a339f45d22f4c
diddipoeler <diddipoeler@gmx.de>
2022-12-20 10:19:41 +0100

    Create default_tuicalendar.php
    #1473

---
+++ modules/mod_sportsmanagement_calendar/tmpl/default_tuicalendar.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * SportsManagement ein Programm zur Verwaltung für alle Sportarten
+ * @version    1.0.05
+ * @package    Sportsmanagement
+ * @subpackage mod_sportsmanagement_calendar
+ * @file       default_tuicalendar.php
+ * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright  Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
+defined('_JEXEC') or die('Restricted access');
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Uri\Uri;
+use Joomla\CMS\Factory;
+
+//$display = ($params->get('update_module') == 1) ? 'block' : 'none';
+//$show_teamlist = ($params->get('show_teamslist') == 1) ? 'show' : 'hidden';
+
+$cal       = new JSMCalendar; // This object creates the html for the calendar
+$cal::getMatches($month, $year);
+
+//echo '<pre>'.print_r($cal::$matches,true).'</pre>';
+
+?>
+
+<link rel="stylesheet" href="https://uicdn.toast.com/calendar/latest/toastui-calendar.min.css" />
+<script src="https://uicdn.toast.com/calendar/latest/toastui-calendar.min.js"></script>
