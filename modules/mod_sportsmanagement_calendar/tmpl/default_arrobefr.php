4221ac9871e6478538c1a73adc36df6a5399b72d
diddipoeler <diddipoeler@gmx.de>
2022-12-28 11:08:04 +0100

    #1473

---
+++ modules/mod_sportsmanagement_calendar/tmpl/default_arrobefr.php
@@ -0,0 +1,409 @@
+<?php
+/**
+ * SportsManagement ein Programm zur Verwaltung für alle Sportarten
+ * @version    1.0.05
+ * @package    Sportsmanagement
+ * @subpackage mod_sportsmanagement_calendar
+ * @file       default_tuicalendar.php
+ * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright  Copyright: © 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ *
+ * https://codesandbox.io/s/toast-ui-calendar-for-vanillajs-wz2s3?file=/index.html:212-246
+ * https://codesandbox.io/examples/package/tui-date-picker
+ * https://stackoverflow.com/questions/71907057/how-to-send-json-value-to-tui-calendar-using-ajax
+ * 
+ * https://github.com/nhn/tui.calendar/tree/56f36b0ae0ac2331983fd2335017ae0fc7e24d7b/examples
+ * https://github.com/nhn/tui.calendar/tree/56f36b0ae0ac2331983fd2335017ae0fc7e24d7b
+ * 
+ * https://ui.toast.com/tui-calendar
+ * https://github.com/nhn/tui.calendar
+ * popup
+ * https://codepen.io/AnzilkhaN/pen/GRJqPVK
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+defined('_JEXEC') or die('Restricted access');
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Uri\Uri;
+use Joomla\CMS\Factory;
+
+$app = Factory::getApplication('site');
+$post   = Factory::getApplication()->input->post->getArray(array()); 
+$doc              = Factory::getDocument();
+// JInput object
+$jinput = $app->input;
+
+//echo 'post <pre>'.print_r($post,true).'</pre>';
+
+//$display = ($params->get('update_module') == 1) ? 'block' : 'none';
+//$show_teamlist = ($params->get('show_teamslist') == 1) ? 'show' : 'hidden';
+
+$cal       = new JSMCalendar; // This object creates the html for the calendar
+$cal::$matches = array();
+$matches = $cal::getMatches($month, $year);
+
+//$event_month = $month;
+//$event_year = $year;
+
+//echo __LINE__.'<pre>'.print_r($month,true).'</pre>';
+//echo __LINE__.'<pre>'.print_r($year,true).'</pre>';
+
+//echo '<pre>'.print_r($cal::$matches,true).'</pre>';
+
+foreach ( $matches as $row )
+{
+$event = "";
+  //$theStart_date = date(DATE_ATOM, strtotime($row['date']));
+  //echo __LINE__.'<pre>'.print_r($theStart_date,true).'</pre>';
+  
+  //$time = date("c", $row['timestamp']);
+$time = date("Y-m-d\TH:i:s", $row['timestamp']);
+  //echo __LINE__.'<pre>'.print_r($time,true).'</pre>';
+  
+  
+  //$row['date'] = preg_replace(' ', 'T', $row['date']);
+  
+$event .= "{id: '".$row['matchcode']."',";
+$event .= "calendarId: '1',";
+$event .= "category: 'time',";
+$event .= "dueDateClass: '',";
+$event .= "isReadOnly: 'true',";
+
+$event .= "isAllDay: false, "; 
+$event .= "goingDuration: 30, ";
+$event .= "comingDuration: 30, ";
+$event .= "color: '#ffffff', ";
+$event .= "bgColor: '#69BB2D', ";
+$event .= "dragBgColor: '#69BB2D',"; 
+$event .= "borderColor: '#69BB2D',  "; 
+$event .= "customStyle: 'cursor: default;',"; 
+$event .= "isPending: false, ";
+$event .= "isFocused: false, ";
+$event .= "isPrivate: false, ";
+$event .= "isVisible: true,";
+$event .= "location: '".$row['leaguecountry'] ." ". $row['leaguename']."', ";
+$event .= "attendees: '', ";
+$event .= "recurrenceRule: '',";
+
+$event .= "title: '".$row['homename'].' - '.$row['awayname'].' '.$row['result']   ."',";
+$event .= "start: '".$time."',";
+$event .= "end: '".$time."',  }";
+$events[] = $event;
+}
+
+//echo '<pre>'.print_r($events,true).'</pre>';
+$calendeer_events = implode(",",$events);
+//echo '<pre>'.print_r($calendeer_events,true).'</pre>';
+
+?>
+
+
+  
+<script src="https://cdn.jsdelivr.net/npm/arrobefr-jquery-calendar-bs4@1.0.3/dist/js/jquery-calendar.min.js"></script>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/arrobefr-jquery-calendar-bs4@1.0.3/dist/css/jquery-calendar.min.css">
+  
+  
+<?php
+
+  
+
+  
+?>
+ <script>
+    $(document).ready(function(){
+      moment.locale('en');
+      var now = moment();
+
+      /**
+       * Many events
+       */
+      var events = [
+        {
+          start: now.startOf('week').add(9, 'h').format('X'),
+          end: now.startOf('week').add(10, 'h').format('X'),
+          title: '1',
+          content: 'Hello World! <br> <p>Foo Bar</p>',
+          category:'Professionnal'
+        },
+        {
+          start: now.startOf('week').add(10, 'h').format('X'),
+          end: now.startOf('week').add(11, 'h').format('X'),
+          title: '2',
+          content: 'Hello World! <br> <p>Foo Bar</p>',
+          category:'Professionnal'
+        },
+        {
+          start: now.startOf('week').add(11, 'h').format('X'),
+          end: now.startOf('week').add(12, 'h').format('X'),
+          title: '3',
+          content: 'Hello World! <br> <p>Foo Bar</p>',
+          category:'Personnal'
+        },
+        {
+          start: now.startOf('week').add(1, 'days').add(9, 'h').format('X'),
+          end: now.startOf('week').add(1, 'days').add(10, 'h').format('X'),
+          title: '4',
+          content: 'Hello World! <br> <p>Foo Bar</p>',
+          category:'Personnal'
+        },
+        {
+          start: now.startOf('week').add(1, 'days').add(9, 'h').add(30, 'm').format('X'),
+          end: now.startOf('week').add(1, 'days').add(10, 'h').add(30, 'm').format('X'),
+          title: '5',
+          content: 'Hello World! <br> <p>Foo Bar</p>',
+          category:'Arrobe'
+        },
+        {
+          start: now.startOf('week').add(1, 'days').add(11, 'h').format('X'),
+          end: now.startOf('week').add(1, 'days').add(12, 'h').format('X'),
+          title: '6',
+          content: 'Hello World! <br> <p>Foo Bar</p>',
+          category:'Another category'
+        },
+        {
+          start: now.startOf('week').add(2, 'days').add(9, 'h').format('X'),
+          end: now.startOf('week').add(2, 'days').add(10, 'h').format('X'),
+          title: '7',
+          content: 'Hello World! <br> <p>Foo Bar</p>',
+          category:'Personnal'
+        },
+        {
+          start: now.startOf('week').add(2, 'days').add(9, 'h').add(30, 'm').format('X'),
+          end: now.startOf('week').add(2, 'days').add(10, 'h').add(30, 'm').format('X'),
+          title: '8',
+          content: 'Hello World! <br> <p>Foo Bar</p>',
+          category:'Professionnal'
+        },
+        {
+          start: now.startOf('week').add(2, 'days').add(10, 'h').format('X'),
+          end: now.startOf('week').add(2, 'days').add(11, 'h').format('X'),
+          title: '9',
+          content: 'Hello World! <br> <p>Foo Bar</p>',
+          category:'Personnal'
+        },
+        {
+          start: now.startOf('week').add(3, 'days').add(9, 'h').format('X'),
+          end: now.startOf('week').add(3, 'days').add(10, 'h').format('X'),
+          title: '10',
+          content: 'Hello World! <br> <p>Foo Bar</p>',
+          category:'Professionnal'
+        },
+        {
+          start: now.startOf('week').add(3, 'days').add(9, 'h').add(15, 'm').format('X'),
+          end: now.startOf('week').add(3, 'days').add(10, 'h').add(15, 'm').format('X'),
+          title: '11',
+          content: 'Hello World! <br> <p>Foo Bar</p>',
+          category:'Personnal'
+        },
+        {
+          start: now.startOf('week').add(3, 'days').add(9, 'h').add(30, 'm').format('X'),
+          end: now.startOf('week').add(3, 'days').add(10, 'h').add(30, 'm').format('X'),
+          title: '12',
+          content: 'Hello World! <br> <p>Foo Bar</p>',
+          category:'Anything else'
+        },
+        {
+          start: now.startOf('week').add(4, 'days').add(9, 'h').format('X'),
+          end: now.startOf('week').add(4, 'days').add(12, 'h').format('X'),
+          title: '13',
+          content: 'Hello World! <br> <p>Foo Bar</p>',
+          category:'Private'
+        },
+        {
+          start: now.startOf('week').add(4, 'days').add(9, 'h').add(30, 'm').format('X'),
+          end: now.startOf('week').add(4, 'days').add(10, 'h').format('X'),
+          title: '14',
+          content: 'Hello World! <br> <p>Foo Bar</p>',
+          category:'No more creative category name'
+        },
+        {
+          start: now.startOf('week').add(4, 'days').add(11, 'h').format('X'),
+          end: now.startOf('week').add(4, 'days').add(11, 'h').add(30, 'm').format('X'),
+          title: '15',
+          content: 'Hello World! <br> <p>Foo Bar</p>',
+          category:'Professionnal'
+        },
+        {
+         start: now.startOf('week').add(5, 'days').add(10, 'h').format('X'),
+         end: now.startOf('week').add(5, 'days').add(12, 'h').format('X'),
+         title: '17',
+         content: 'Hello World! <br> <p>Foo Bar</p>',
+         category:'Private'
+       },
+       {
+         start: now.startOf('week').add(5, 'days').add(9, 'h').add(30, 'm').format('X'),
+         end: now.startOf('week').add(5, 'days').add(10, 'h').add(30, 'm').format('X'),
+         title: '16',
+         content: 'Hello World! <br> <p>Foo Bar</p>',
+         category:'Course'
+       },
+       {
+         start: now.startOf('week').add(5, 'days').add(11, 'h').add(30, 'm').format('X'),
+         end: now.startOf('week').add(5, 'days').add(12, 'h').add(30, 'm').format('X'),
+         title: '18',
+         content: 'Hello World! <br> <p>Foo Bar</p>',
+         category:'No more creative category name'
+       },
+       {
+         start: now.startOf('week').add(5, 'days').add(12, 'h').format('X'),
+         end: now.startOf('week').add(5, 'days').add(13, 'h').format('X'),
+         title: '19',
+         content: 'Hello World! <br> <p>Foo Bar</p>',
+         category:'Another one'
+       },
+       {
+         start: now.startOf('week').add(5, 'days').add(12, 'h').add(15, 'm').format('X'),
+         end: now.startOf('week').add(5, 'days').add(13, 'h').format('X'),
+         title: '21',
+         content: 'Hello World! <br> <p>Foo Bar</p>',
+         category:'One again'
+       },
+       {
+         start: now.startOf('week').add(5, 'days').add(12, 'h').add(45, 'm').format('X'),
+         end: now.startOf('week').add(5, 'days').add(13, 'h').add(45, 'm').format('X'),
+         title: '22',
+         content: 'Hello World! <br> <p>Foo Bar</p>',
+         category:'Encore'
+       },
+       {
+         start: now.startOf('week').add(5, 'days').add(13, 'h').add(45, 'm').format('X'),
+         end: now.startOf('week').add(5, 'days').add(14, 'h').format('X'),
+         title: '23',
+         content: 'Hello World! <br> <p>Foo Bar</p>',
+         category:'Professionnal'
+       },
+       {
+         start: now.startOf('week').add(5, 'days').add(12, 'h').format('X'),
+         end: now.startOf('week').add(5, 'days').add(14, 'h').add(30, 'm').format('X'),
+         title: '20',
+         content: 'Hello World! <br> <p>Foo Bar</p>',
+         category:'Private'
+       },
+       {
+         start: now.startOf('week').add(5, 'days').add(13, 'h').add(45, 'm').format('X'),
+         end: now.startOf('week').add(5, 'days').add(15, 'h').format('X'),
+         title: '27',
+         content: 'Hello World! <br> <p>Foo Bar</p>',
+         category:'Encore'
+       },
+       {
+         start: now.startOf('week').add(5, 'days').add(14, 'h').add(30, 'm').format('X'),
+         end: now.startOf('week').add(5, 'days').add(15, 'h').format('X'),
+         title: '28',
+         content: 'Hello World! <br> <p>Foo Bar</p>',
+         category:'Private'
+       },
+       {
+         start: now.startOf('week').add(6, 'days').add(9, 'h').format('X'),
+         end: now.startOf('week').add(6, 'days').add(11, 'h').format('X'),
+         title: '24',
+         content: 'Hello World! <br> <p>Foo Bar</p>',
+         category:'Foo'
+       },
+       {
+         start: now.startOf('week').add(6, 'days').add(9, 'h').format('X'),
+         end: now.startOf('week').add(6, 'days').add(11, 'h').format('X'),
+         title: '25',
+         content: 'Hello World! <br> <p>Foo Bar</p>',
+         category:'Bar'
+       },
+       {
+         start: now.startOf('week').add(6, 'days').add(9, 'h').format('X'),
+         end: now.startOf('week').add(6, 'days').add(11, 'h').format('X'),
+         title: '26',
+         content: 'Hello World! <br> <p>Foo Bar</p>',
+         category:'Baz'
+       },
+      ];
+
+      /**
+       * A daynote
+       */
+      var daynotes = [
+        {
+          time: now.startOf('week').add(15, 'h').add(30, 'm').format('X'),
+          title: 'Leo\'s holiday',
+          content: 'yo',
+          category: 'holiday'
+        }
+      ];
+
+      /**
+       * Init the calendar
+       */
+      var calendar = $('#calendar').Calendar({
+        locale: 'en',
+        weekday: {
+          timeline: {
+            intervalMinutes: 30,
+            fromHour: 9
+          }
+        },
+        events: events,
+        daynotes: daynotes
+      }).init();
+
+      /**
+       * Listening for events
+       */
+
+      $('#calendar').on('Calendar.init', function(event, instance, before, current, after){
+        console.log('event : Calendar.init');
+        console.log(instance);
+        console.log(before);
+        console.log(current);
+        console.log(after);
+      });
+      $('#calendar').on('Calendar.daynote-mouseenter', function(event, instance, elem){
+        console.log('event : Calendar.daynote-mouseenter');
+        console.log(instance);
+        console.log(elem);
+      });
+      $('#calendar').on('Calendar.daynote-mouseleave', function(event, instance, elem){
+        console.log('event : Calendar.daynote-mouseleave');
+        console.log(instance);
+        console.log(elem);
+      });
+      $('#calendar').on('Calendar.event-mouseenter', function(event, instance, elem){
+        console.log('event : Calendar.event-mouseenter');
+        console.log(instance);
+        console.log(elem);
+      });
+      $('#calendar').on('Calendar.event-mouseleave', function(event, instance, elem){
+        console.log('event : Calendar.event-mouseleave');
+        console.log(instance);
+        console.log(elem);
+      });
+      $('#calendar').on('Calendar.daynote-click', function(event, instance, elem, evt){
+        console.log('event : Calendar.daynote-click');
+        console.log(instance);
+        console.log(elem);
+        console.log(evt);
+      });
+      $('#calendar').on('Calendar.event-click', function(event, instance, elem, evt){
+        console.log('event : Calendar.event-click');
+        console.log(instance);
+        console.log(elem);
+        console.log(evt);
+      });
+    });
+  </script>
+  <div class="container-fluid px-4">
+    <div class="row">
+      <div class="col-xs-12">
+        <h1>Wow ! That calendar works !</h1>
+        <div id="calendar"></div>
+      </div>
+    </div>
+  </div>
+
+
+
+
+
+
