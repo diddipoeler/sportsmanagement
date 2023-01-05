fbc0ba73ff5db0bf6cee4895442e34a5cf738207
diddipoeler <diddipoeler@gmx.de>
2023-01-05 13:27:58 +0100

    Update default.php
    code

--- modules/mod_sportsmanagement_ajax_top_navigation_menu/tmpl/default.php
+++ modules/mod_sportsmanagement_ajax_top_navigation_menu/tmpl/default.php
@@ -69,9 +69,10 @@ HTMLHelper::_('behavior.tooltip');
 					<?PHP
 					if ($country_id)
 					{
+					$flag =  JSMCountries::getCountryFlag($country_id,'',true);	
 						?>
                         <img style="float: right;"
-                             src="images/com_sportsmanagement/database/laender_karten/<?php echo strtolower($country_id) ?>.gif"
+                             src="<?php echo $flag; ?>"
                              alt="<?php echo $country_id ?>" width="144" height=""/>
 						<?PHP
 					}
