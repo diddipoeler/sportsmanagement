a8496484d7a36ac1b3321748d088ddad9520b045
diddipoeler <diddipoeler@gmx.de>
2023-01-23 09:41:07 +0100

    Update division.php
    joomla 4

--- admin/models/division.php
+++ admin/models/division.php
@@ -165,14 +165,17 @@ class sportsmanagementModeldivision extends JSMModelAdmin
                 $tblDivision->modified         = $this->jsmdate->toSql();
 		        $tblDivision->modified_by      = $this->jsmuser->get('id');
 
-				if ($tblDivision->store())
-				{
+				try{
+					$tblDivision->store();
 					$msg = Text::sprintf('COM_SPORTSMANAGEMENT_ADMIN_DIVISIONS_CTRL_ROUNDS_ADDED', $i);
-				}
-				else
-				{
-					$msg = Text::_('COM_SPORTSMANAGEMENT_ADMIN_DIVISIONS_CTRL_ERROR_ADD') . $this->_db->getErrorMsg();
-				}
+					}
+		catch (Exception $e)
+		{
+$this->jsmapp->enqueueMessage(Text::sprintf('COM_SPORTSMANAGEMENT_DATABASE_ERROR_FUNCTION_FAILED', $e->getCode(), $e->getMessage()), 'notice');
+$this->jsmapp->enqueueMessage(Text::sprintf('COM_SPORTSMANAGEMENT_FILE_ERROR_FUNCTION_FAILED', __FILE__, __LINE__), 'notice');
+		$msg = Text::_('COM_SPORTSMANAGEMENT_ADMIN_DIVISIONS_CTRL_ERROR_ADD') . $e->getMessage();	
+		}
+				
 
 				$max++;
 			}
