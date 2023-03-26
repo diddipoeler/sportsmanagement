115601f4a92c5c28e521ca3753945dae7a620561
diddipoeler <diddipoeler@gmx.de>
2023-03-26 16:01:07 +0200

    joomla 4

--- admin/views/playgrounds/tmpl/default.php
+++ admin/views/playgrounds/tmpl/default.php
@@ -12,14 +12,10 @@
 defined('_JEXEC') or die('Restricted access');
 use Joomla\CMS\HTML\HTMLHelper;
 
-// Ordering allowed ?
-$ordering = ($this->sortColumn == 'v.ordering');
-
-
 $templatesToLoad = array('footer', 'listheader');
 sportsmanagementHelper::addTemplatePaths($templatesToLoad, $this);
 ?>
-    <script>
+<script>
 
         function searchPlayground(val, key) {
             var f = $('adminForm');
@@ -30,20 +26,18 @@ sportsmanagementHelper::addTemplatePaths($templatesToLoad, $this);
             }
         }
 
-    </script>
-    <form action="<?php echo $this->request_url; ?>" method="post" id="adminForm" name="adminForm">
-
-		<?PHP
-		echo $this->loadTemplate('joomla_version');
-		?>
-        <input type="hidden" name="search_mode" value="<?php echo $this->lists['search_mode']; ?>"/>
-        <input type="hidden" name="task" value=""/>
-        <input type="hidden" name="boxchecked" value="0"/>
-        <input type="hidden" name="filter_order" value="<?php echo $this->sortColumn; ?>"/>
-        <input type="hidden" name="filter_order_Dir" value="<?php echo $this->sortDirection; ?>"/>
-		<?php echo HTMLHelper::_('form.token') . "\n"; ?>
-    </form>
-
+</script>
+<form action="<?php echo $this->request_url; ?>" method="post" id="adminForm" name="adminForm">
+<?PHP
+echo $this->loadTemplate('joomla_version');
+?>
+<input type="hidden" name="search_mode" value="<?php echo $this->lists['search_mode']; ?>"/>
+<input type="hidden" name="task" value=""/>
+<input type="hidden" name="boxchecked" value="0"/>
+<input type="hidden" name="filter_order" value="<?php echo $this->sortColumn; ?>"/>
+<input type="hidden" name="filter_order_Dir" value="<?php echo $this->sortDirection; ?>"/>
+<?php echo HTMLHelper::_('form.token') . "\n"; ?>
+</form>
 <?PHP
 echo $this->loadTemplate('footer');
 ?>
