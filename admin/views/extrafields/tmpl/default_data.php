a3a5aac66dd2fcc39f915a2f45e4a422a439c869
diddipoeler <diddipoeler@gmx.de>
2023-03-26 18:18:15 +0200

    joomla 4

--- admin/views/extrafields/tmpl/default_data.php
+++ admin/views/extrafields/tmpl/default_data.php
@@ -13,7 +13,7 @@ defined('_JEXEC') or die('Restricted access');
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\Router\Route;
-
+use Joomla\CMS\Session\Session;
 
 $this->saveOrder = $this->sortColumn == 'objcountry.ordering';
 if ($this->saveOrder && !empty($this->items))
