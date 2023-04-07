d4e09163eb8c13cb51808caf752d16b8f82a340d
diddipoeler <diddipoeler@gmx.de>
2023-04-07 09:29:00 +0200

    joomla 4

--- modules/mod_sportsmanagement_club_birthday/mod_sportsmanagement_club_birthday.php
+++ modules/mod_sportsmanagement_club_birthday/mod_sportsmanagement_club_birthday.php
@@ -154,110 +154,16 @@ if (count($clubs) > 0)
 
 	switch ($mode)
 	{
-		case 'B':
-			$layout = isset($attribs['layout']) ? $attribs['layout'] : 'default';
-            
-            case 'BC':
-			$layout = 'default_carousel';
-
-			// $document->addStyleSheet(Uri::base().'modules/mod_sportsmanagement_club_birthday/css/mod_sportsmanagement_club_birthday.css');
-			break;
+        case 'BC':
+		$layout = 'default_carousel';
+		// $document->addStyleSheet(Uri::base().'modules/mod_sportsmanagement_club_birthday/css/mod_sportsmanagement_club_birthday.css');
+		break;
 		case 'T':
 			$layout = isset($attribs['layout']) ? $attribs['layout'] : 'default';
 			include dirname(__FILE__) . DIRECTORY_SEPARATOR . 'js' . DIRECTORY_SEPARATOR . 'ticker.js';
 			$document->addStyleSheet(Uri::base() . 'modules/mod_sportsmanagement_club_birthday/css/mod_sportsmanagement_club_birthday.css');
 			break;
-		case 'V':
 
-			$layout = isset($attribs['layout']) ? $attribs['layout'] : 'wowslider';
-
-			// $layout = isset($attribs['layout'])?$attribs['layout']:'default';
-
-			$html_li    = '';
-			$html_ahref = '';
-			$id         = 0;
-
-			foreach ($clubs AS $club)
-			{
-				$club->default_picture = sportsmanagementHelper::getDefaultPlaceholder('clublogobig');
-				$thispic               = "";
-				$whenmessage           = "";
-				$birthdaytext2         = "";
-				$flag                  = $params->get('show_club_flag') ? JSMCountries::getCountryFlag($club->country) . "&nbsp;" : "";
-				$text                  = $club->name;
-				$usedname              = $flag . $text;
-				$club_link             = "";
-				$club_link             = sportsmanagementHelperRoute::getClubInfoRoute($club->project_id, $club->id);
-				$showname              = HTMLHelper::link($club_link, $usedname);
-
-				if ($params->get('show_picture') == 1)
-				{
-					if (file_exists(JPATH_BASE . '/' . $club->picture) && $club->picture != '')
-					{
-						$thispic = $club->picture;
-					}
-                    elseif (file_exists(JPATH_BASE . '/' . $club->default_picture) && $club->default_picture != '')
-					{
-						$thispic = $club->default_picture;
-					}
-				}
-
-				switch ($club->days_to_birthday)
-				{
-					case 0:
-						$whenmessage = $params->get('todaymessage');
-						break;
-					case 1:
-						$whenmessage = $params->get('tomorrowmessage');
-						break;
-					default:
-						$whenmessage = str_replace('%DAYS_TO%', $club->days_to_birthday, trim($futuremessage));
-						break;
-				}
-
-				if ($club->founded != '0000-00-00')
-				{
-					$birthdaytext2  = htmlentities(trim(Text::_($params->get('birthdaytext'))), ENT_COMPAT, 'UTF-8');
-					$dayformat      = htmlentities(trim($params->get('dayformat')));
-					$birthdayformat = htmlentities(trim($params->get('birthdayformat')));
-					$birthdaytext2  = str_replace('%WHEN%', $whenmessage, $birthdaytext2);
-					$birthdaytext2  = str_replace('%AGE%', $club->age, $birthdaytext2);
-					$birthdaytext2  = str_replace('%DATE%', strftime($dayformat, strtotime($club->year . '-' . $club->daymonth)), $birthdaytext2);
-					$birthdaytext2  = str_replace('%DATE_OF_BIRTH%', strftime($birthdayformat, strtotime($club->date_of_birth)), $birthdaytext2);
-				}
-				else
-				{
-					$birthdaytext2 = htmlentities(trim(Text::_($params->get('birthdaytextyear'))), ENT_COMPAT, 'UTF-8');
-					$birthdaytext2 = str_replace('%AGE%', $club->age_year, $birthdaytext2);
-				}
-
-				$birthdaytext2 = str_replace('%BR%', '<br />', $birthdaytext2);
-				$birthdaytext2 = str_replace('%BOLD%', '<b>', $birthdaytext2);
-				$birthdaytext2 = str_replace('%BOLDEND%', '</b>', $birthdaytext2);
-
-				$text    .= '<br> ' . $birthdaytext2;
-				$html_li .= '<li><a href="' . $club_link . '"><img src="' . $thispic . '" alt="' . $text . '" title="' . $text . '" id="wows1_' . $id . '" /></a></li>';
-
-				// $html_li .= '<li><img src="'.$thispic.'" alt="" title="" id="wows1_'.$id.'" /></li>';
-				$id++;
-				$html_ahref .= '<a href="#" title=""><img src="' . $thispic . '" alt=""    />' . $id . '</a>';
-			}
-
-			// $wowslider_style = "basic_linear";
-			// $wowslider_style = "squares";
-			// $wowslider_style = "fade";
-			$wowslider_style = $params->def("wowsliderstyle");
-
-			// Include(dirname(__FILE__).DIRECTORY_SEPARATOR.'js'.DIRECTORY_SEPARATOR.'qscrollerv.js');
-			// $document->addScript(Uri::base().'modules/mod_sportsmanagement_club_birthday/js/qscrollerv.js');
-			// $document->addScript(Uri::base().'modules/mod_sportsmanagement_club_birthday/js/qscroller.js');
-			// $document->addScript(Uri::base().'modules/mod_sportsmanagement_club_birthday/js/jquery.simplyscroll.js');
-			$document->addStyleSheet(Uri::base() . 'modules/mod_sportsmanagement_club_birthday/wowslider/' . $wowslider_style . '/style.css');
-
-			// $document->addScript(Uri::base().'modules/mod_sportsmanagement_club_birthday/js/wowslider.js');
-			// $document->addScript(Uri::base().'modules/mod_sportsmanagement_club_birthday/js/'.$wowslider_style.'.js');
-
-			break;
 		case 'L':
 			$layout = isset($attribs['layout']) ? $attribs['layout'] : 'default';
 
@@ -268,89 +174,6 @@ if (count($clubs) > 0)
 			$document->addStyleSheet(Uri::base() . 'modules/mod_sportsmanagement_club_birthday/css/mod_sportsmanagement_club_birthday.css');
 			break;
 
-		case 'J':
-			$container = 'slider' . $module->id . '_container';
-			$layout    = isset($attribs['layout']) ? $attribs['layout'] : 'jssor';
-			$document->addScript(Uri::base() . 'modules/mod_sportsmanagement_club_birthday/js/jssor.slider.mini.js');
-
-			$html_li    = '';
-			$html_ahref = '';
-			$id         = 0;
-
-			foreach ($clubs AS $club)
-			{
-				$club->default_picture = sportsmanagementHelper::getDefaultPlaceholder('clublogobig');
-				$thispic               = "";
-				$whenmessage           = "";
-				$birthdaytext2         = "";
-				$flag                  = $params->get('show_club_flag') ? JSMCountries::getCountryFlag($club->country) . "&nbsp;" : "";
-				$text                  = '';
-				$usedname              = $flag . $text;
-				$club_link             = "";
-				$club_link             = sportsmanagementHelperRoute::getClubInfoRoute($club->project_id, $club->id);
-				$showname              = HTMLHelper::link($club_link, $usedname);
-
-				if ($params->get('show_picture') == 1)
-				{
-					if (file_exists(JPATH_BASE . '/' . $club->picture) && $club->picture != '')
-					{
-						$thispic = $club->picture;
-					}
-                    elseif (file_exists(JPATH_BASE . '/' . $club->default_picture) && $club->default_picture != '')
-					{
-						$thispic = $club->default_picture;
-					}
-				}
-
-				switch ($club->days_to_birthday)
-				{
-					case 0:
-						$whenmessage = $params->get('todaymessage');
-						break;
-					case 1:
-						$whenmessage = $params->get('tomorrowmessage');
-						break;
-					default:
-						$whenmessage = str_replace('%DAYS_TO%', $club->days_to_birthday, trim($futuremessage));
-						break;
-				}
-
-				if ($club->founded != '0000-00-00')
-				{
-					$birthdaytext2  = htmlentities(trim(Text::_($params->get('birthdaytext'))), ENT_COMPAT, 'UTF-8');
-					$dayformat      = htmlentities(trim($params->get('dayformat')));
-					$birthdayformat = htmlentities(trim($params->get('birthdayformat')));
-					$birthdaytext2  = str_replace('%WHEN%', $whenmessage, $birthdaytext2);
-					$birthdaytext2  = str_replace('%AGE%', $club->age, $birthdaytext2);
-					$birthdaytext2  = str_replace('%DATE%', strftime($dayformat, strtotime($club->year . '-' . $club->daymonth)), $birthdaytext2);
-					$birthdaytext2  = str_replace('%DATE_OF_BIRTH%', strftime($birthdayformat, strtotime($club->date_of_birth)), $birthdaytext2);
-				}
-				else
-				{
-					$birthdaytext2 = htmlentities(trim(Text::_($params->get('birthdaytextyear'))), ENT_COMPAT, 'UTF-8');
-					$birthdaytext2 = str_replace('%AGE%', $club->age_year, $birthdaytext2);
-				}
-
-				$birthdaytext2 = str_replace('%BR%', '<br />', $birthdaytext2);
-				$birthdaytext2 = str_replace('%BOLD%', '<b>', $birthdaytext2);
-				$birthdaytext2 = str_replace('%BOLDEND%', '</b>', $birthdaytext2);
-
-				$text .= '<br> ' . $birthdaytext2;
-
-				$html_li .= '<div><a href="' . $club_link . '"><img u="image" src="' . $thispic . '" /></a>';
-
-				$html_li .= '<div u="caption" t="' . $params->get('jssor_captiontransitions') . '" style="position:absolute;left:10px;top:80px;width:600px;height:40px;font-size:36px;color:#000;line-height:40px;">' . $club->name . '</div>';
-				$html_li .= '<div u="caption" t="' . $params->get('jssor_captiontransitions') . '" style="position:absolute;left:10px;top:130px;width:600px;height:40px;font-size:36px;color:#000;line-height:40px;">' . $text . '</div>';
-
-				$html_li .= '</div>';
-
-				$id++;
-				$html_ahref .= '<a href="#" title=""><img src="' . $thispic . '" alt=""    />' . $id . '</a>';
-			}
-
-
-			$document->addStyleSheet(Uri::base() . 'modules/' . $module->module . '/css/' . $module->module . '.css');
-			break;
 	}
 }
 ?>
