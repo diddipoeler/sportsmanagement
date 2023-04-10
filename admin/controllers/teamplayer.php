19a9e1b937e10381bff4f77855ab7f3034b54dfa
diddipoeler <diddipoeler@gmx.de>
2023-04-10 13:13:10 +0200

    tabletennis

--- admin/controllers/teamplayer.php
+++ admin/controllers/teamplayer.php
@@ -10,7 +10,8 @@
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 defined('_JEXEC') or die('Restricted access');
-//use Joomla\CMS\MVC\Controller\FormController;
+use Joomla\CMS\Factory;
+use Joomla\CMS\Language\Text;
 
 /**
  * sportsmanagementControllerteamplayer
@@ -33,6 +34,19 @@ class sportsmanagementControllerteamplayer extends JSMControllerForm
 	 */
 	function __construct($config = array())
 	{
+	   //Factory::getApplication()->enqueueMessage(Text::_(__METHOD__ . ' ' . ' ' . __LINE__ . ' ' . 'person_art<pre>'.print_r(Factory::getApplication()->input,true).'</pre>'), 'notice');
+       
+Factory::getApplication()->enqueueMessage(Text::_(__METHOD__ . ' ' . ' ' . __LINE__ . ' ' . 'project_team_id<pre>'.print_r(Factory::getApplication()->input->getInt('project_team_id'),true).'</pre>'), 'notice');
+Factory::getApplication()->enqueueMessage(Text::_(__METHOD__ . ' ' . ' ' . __LINE__ . ' ' . 'person_id<pre>'.print_r(Factory::getApplication()->input->getInt('person_id'),true).'</pre>'), 'notice');
+Factory::getApplication()->enqueueMessage(Text::_(__METHOD__ . ' ' . ' ' . __LINE__ . ' ' . 'id<pre>'.print_r(Factory::getApplication()->input->getInt('id'),true).'</pre>'), 'notice');
+Factory::getApplication()->enqueueMessage(Text::_(__METHOD__ . ' ' . ' ' . __LINE__ . ' ' . 'pid<pre>'.print_r(Factory::getApplication()->input->getInt('pid'),true).'</pre>'), 'notice');
+Factory::getApplication()->enqueueMessage(Text::_(__METHOD__ . ' ' . ' ' . __LINE__ . ' ' . 'team_id<pre>'.print_r(Factory::getApplication()->input->getInt('team_id'),true).'</pre>'), 'notice');
+Factory::getApplication()->enqueueMessage(Text::_(__METHOD__ . ' ' . ' ' . __LINE__ . ' ' . 'persontype<pre>'.print_r(Factory::getApplication()->input->getInt('persontype'),true).'</pre>'), 'notice');
+
+
+
+//Factory::getApplication()->setUserState("Factory::getApplication()->input->getCmd('option').project_team_id", Factory::getApplication()->input->getInt('project_team_id'));
+       
 		parent::__construct($config);
 	}
 
--- admin/views/fieldsets/view.html.php
+++ admin/views/fieldsets/view.html.php
@@ -10,6 +10,7 @@
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 defined('_JEXEC') or die('Restricted access');
+use Joomla\CMS\Factory;
 
 /**
  * sportsmanagementViewFieldsets
