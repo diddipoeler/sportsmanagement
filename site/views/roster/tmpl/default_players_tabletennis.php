7abd751ee5e0bdf50dc72d7b66a73dec3bca2a58
diddipoeler <diddipoeler@gmx.de>
2023-04-14 14:06:03 +0200

    tabletennis

--- site/views/roster/tmpl/default_players_tabletennis.php
+++ site/views/roster/tmpl/default_players_tabletennis.php
@@ -21,8 +21,66 @@ if ( Factory::getConfig()->get('debug') )
 }
 
 ?>
-<div class="container">
+<div class="container" id="roster_tabletennis">
+<div class="<?php echo $this->divclassrow; ?> table-responsive" id="defaultplayers_tabletennis" itemscope itemtype="http://schema.org/SportsTeam">
 
+ <?php
+        foreach ( $this->projectpositions as $positions => $position ) if( $position->persontype == 1 )
+{
+        ?>
+        <table class="<?php echo $this->config['table_class']; ?> table-sm nowrap " id="tableplayer_tabletennis<?php echo $position->id;?>" width="100%">
+			<?php
+			/**
+			 *
+			 * jetzt kommt die schleife über die positionen
+			 */
+			foreach ($this->rows as $position_id => $players) if ( $position_id == $position->id )
+			{
+			$row      = current($players); 
+foreach ($players as $row)
+				{
+					?>
+                    <tr class="" width="" onMouseOver="this.bgColor='#CCCCFF'" onMouseOut="this.bgColor='#ffffff'" itemprop="member" itemscope="" itemtype="http://schema.org/Person">
+						<?php
+                 $playerName = sportsmanagementHelper::formatName(
+							null, $row->firstname,
+							$row->nickname,
+							$row->lastname,
+							$this->config["name_format"]
+						);       
+?>
+                            <td class="" width="" nowrap="nowrap">
+                              <span itemprop="name" content="<?php echo $playerName;?>"></span> 
+                              <span itemprop="birthDate" content="<?php echo $row->birthday;?>"></span>
+				   <span itemprop="deathDate" content="<?php echo $row->deathday;?>"></span>
+				    <span itemprop="nationality" content="<?php echo JSMCountries::getCountryName($row->country);?>"></span>
+                              
+								<?PHP
+								echo sportsmanagementHelperHtml::getBootstrapModalImage(
+									'player' . $row->playerid,
+									$picture,
+									$playerName,
+									$this->config['player_picture_height'],
+									'',
+									$this->modalwidth,
+									$this->modalheight,
+									$this->overallconfig['use_jquery_modal'],
+                                  'itemprop',
+                                  'image'
+								);
+								?>
 
-
+                            </td>
+							<?php
+                                                    
+                        
+      }             
+             
+            }
+            ?> 
+</table>
+<?php
+}
+?>
+</div>
 </div>
