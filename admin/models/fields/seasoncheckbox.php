de8ac631c2930c0fe0bdffff1e7e5b0c18580658
diddipoeler <diddipoeler@gmx.de>
2023-05-12 15:52:32 +0200

    #1554

--- admin/models/fields/seasoncheckbox.php
+++ admin/models/fields/seasoncheckbox.php
@@ -178,7 +178,7 @@ switch ( $targettable )
 {
 case 'season_person_id':
 
-if ( ComponentHelper::getParams('com_sportsmanagement')->get('assign_clup_position_to_player', 0) )
+if ( ComponentHelper::getParams('com_sportsmanagement')->get('assign_club_position_to_player', 0) )
 {
 $html[]  = '<td>';
 $html[] = HTMLHelper::_('select.genericlist', $mitemsclub, 'season_person_club_id' . '[]', 'class="inputbox" ', 'value', 'text', $this->teamvalue[$option->value]['club_id'], 'id');		
