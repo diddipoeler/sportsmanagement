86d72f7acdbc957a3ba53764d997fc6d43baecdc
diddipoeler <diddipoeler@gmx.de>
2023-07-10 11:19:41 +0200

    Update season.php

--- admin/models/season.php
+++ admin/models/season.php
@@ -164,6 +164,7 @@ $this->jsmapp->enqueueMessage(Text::sprintf('COM_SPORTSMANAGEMENT_FILE_ERROR_FUN
 					$profile->project_id  = $project_id;
 					$profile->person_id   = $new_id;
 					$profile->published   = 1;
+			    
 					$profile->modified    = $this->jsmdate->toSql();
 					$profile->modified_by = $this->jsmuser->get('id');
 
@@ -194,8 +195,8 @@ $this->jsmapp->enqueueMessage(Text::sprintf('COM_SPORTSMANAGEMENT_FILE_ERROR_FUN
              }
              
 				$this->jsmquery->clear();
-				$columns = array('person_id', 'season_id', 'team_id', 'published', 'persontype', 'modified', 'modified_by');
-				$values  = array($value, $season_id, $teams, '1', $persontype, $this->jsmdb->Quote('' . $this->jsmdate->toSql() . ''), $this->jsmuser->get('id'));
+				$columns = array('person_id', 'season_id', 'team_id', 'published', 'persontype', 'modified', 'modified_by', '$profile->'  );
+				$values  = array($value, $season_id, $teams, '1', $persontype, $this->jsmdb->Quote('' . $this->jsmdate->toSql() . ''), $this->jsmuser->get('id'), '' );
 				$this->jsmquery
 					->insert($this->jsmdb->quoteName('#__sportsmanagement_season_team_person_id'))
 					->columns($this->jsmdb->quoteName($columns))
