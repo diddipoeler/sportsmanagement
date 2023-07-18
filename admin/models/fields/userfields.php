f8bdaa26a80feb4339c1a83d8dbc31788ca50b97
diddipoeler <diddipoeler@gmx.de>
2023-07-18 13:57:43 +0200

    Update userfields.php

--- admin/models/fields/userfields.php
+++ admin/models/fields/userfields.php
@@ -1,1 +1,56 @@
+<?php
+/**
+ * SportsManagement ein Programm zur Verwaltung für Sportarten
+ * @version    1.0.05
+ * @package    Sportsmanagement
+ * @subpackage fields
+ * @file       userfields.php
+ * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
+defined('_JEXEC') or die('Restricted access');
+use Joomla\CMS\Factory;
+use Joomla\CMS\Form\FormField;
+use Joomla\CMS\Form\FormHelper;
 
+jimport('joomla.filesystem.folder');
+FormHelper::loadFieldClass('list');
+
+/**
+ * FormFieldactseason
+ *
+ * @package
+ * @author
+ * @copyright diddi
+ * @version   2014
+ * @access    public
+ */
+class JFormFielduserfields extends \JFormFieldList
+{
+	protected $type = 'userfields';
+
+	/**
+	 * FormFieldactseason::getOptions()
+	 *
+	 * @return
+	 */
+	protected function getOptions()
+	{
+		$options = array();
+		$db    = Factory::getDbo();
+		$query = $db->getQuery(true);
+		$query->select('s.id AS value, s.name AS text');
+		$query->from('#__sportsmanagement_user_extra_fields as s');
+		$query->order('s.name');
+		$db->setQuery($query);
+		$options = $db->loadObjectList();
+
+		/** Merge any additional options in the XML definition. */
+		$options = array_merge(parent::getOptions(), $options);
+
+		return $options;
+	}
+
+
+}
