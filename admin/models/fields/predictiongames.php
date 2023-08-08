906bca4155d30018aa8b8355cebd9939d7bb55e9
diddipoeler <diddipoeler@gmx.de>
2023-08-08 12:25:24 +0200

    Create predictiongames.php

---
+++ admin/models/fields/predictiongames.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * SportsManagement ein Programm zur Verwaltung für Sportarten
+ * @version    1.0.05
+ * @package    Sportsmanagement
+ * @subpackage fields
+ * @file       predictiongames.php
+ * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
+defined('_JEXEC') or die('Restricted access');
+use Joomla\CMS\Form\FormField;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Component\ComponentHelper;
+use Joomla\CMS\Factory;
+use Joomla\CMS\MVC\Model\BaseDatabaseModel;
+
+if (!defined('JSM_PATH'))
+{
+	DEFINE('JSM_PATH', 'components/com_sportsmanagement');
+}
+
+// Prüft vor Benutzung ob die gewünschte Klasse definiert ist
+if (!class_exists('sportsmanagementHelper'))
+{
+	// Add the classes for handling
+	$classpath = JPATH_ADMINISTRATOR . DIRECTORY_SEPARATOR . JSM_PATH . DIRECTORY_SEPARATOR . 'helpers' . DIRECTORY_SEPARATOR . 'sportsmanagement.php';
+	JLoader::register('sportsmanagementHelper', $classpath);
+	BaseDatabaseModel::getInstance("sportsmanagementHelper", "sportsmanagementModel");
+}
+
+/**
+ * FormFieldPredictiongame
+ *
+ * @package
+ * @author
+ * @copyright diddi
+ * @version   2014
+ * @access    public
+ */
+class JFormFieldPredictiongame extends FormField
+{
+	protected $type = 'predictiongames';
+
+	/**
+	 * FormFieldPredictiongame::getInput()
+	 *
+	 * @return
+	 */
+	function getInput()
+	{
+		$db     = sportsmanagementHelper::getDBConnection();
+		$lang   = Factory::getLanguage();
+		$mitems = array();
+
+		// Welche tabelle soll genutzt werden
+		$params = ComponentHelper::getParams('com_sportsmanagement');
+		$query  = $db->getQuery(true);
+
+		$query->select('pg.id');
+
+		// $query->select('pg.id');
+		$query->select('pg.name');
+
+		$query->from('#__sportsmanagement_prediction_game pg');
+
+		$query->where('pg.published = 1');
+		$query->order('pg.name');
+
+		$db->setQuery($query);
+		$options = $db->loadObjectList();
+
+		// $mitems = array(HTMLHelper::_('select.option', '', Text::_('COM_SPORTSMANAGEMENT_GLOBAL_SELECT')));
+
+		foreach ($options as $option)
+		{
+			$mitems[] = HTMLHelper::_('select.option', $option->id, '&nbsp;' . $option->name . ' (' . $option->id . ')');
+		}
+
+		$output = HTMLHelper::_('select.genericlist', $mitems, $this->name, 'class="inputbox" multiple="multiple" size="10"', 'value', 'text', $this->value, $this->id);
+
+		return $output;
+	}
+}
