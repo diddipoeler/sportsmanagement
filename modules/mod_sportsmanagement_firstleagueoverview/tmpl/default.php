e1292f5f6e06eaefeba882132688787aa87778b7
diddipoeler <diddipoeler@gmx.de>
2024-03-10 15:09:39 +0100

    code

--- modules/mod_sportsmanagement_firstleagueoverview/tmpl/default.php
+++ modules/mod_sportsmanagement_firstleagueoverview/tmpl/default.php
@@ -28,29 +28,14 @@ echo Text::_('MOD_SPORTSMANAGEMENT_FIRSTLEAGUEOVERVIEW_DESCRIPTION');
 
 
 
-
-<div class="accordion" id="accordionExample">
-  <div class="accordion-item">
-<?php	
+<?php
+$zaehler = 0;  
+echo HTMLHelper::_('bootstrap.startTabSet', 'defaulttabs', array('active' => 'show_league_0')); // Start tab set 
 foreach ($federations as $key => $value)
-{
-$show = $start == 0 ? 'show' : '';	
-?>
-
-
-    <h2 class="accordion-header" id="heading<?php echo $value->name;?>">
-      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse<?php echo $value->name;?>" aria-expanded="true" aria-controls="collapse<?php echo $value->name;?>">
-      <?php
-        echo HTMLHelper::_('image',Uri::root() . $value->picture, $value->name, array(' title' => $value->name, ' width' => 50));
- echo $value->name; 
-        ?>
-      </button>
-    </h2>
-    
-<div id="collapse<?php echo $value->name;?>" class="accordion-collapse collapse <?php echo $show;?>" aria-labelledby="heading<?php echo $value->name;?>" data-bs-parent="#accordionExample">
-      <div class="accordion-body">
- <?php
-	foreach( $firstleagueoverview as $key2 => $value2 ) if ( $value->id == $value2->federation )
+{  
+echo HTMLHelper::_('bootstrap.addTab', 'defaulttabs', 'show_league_'.$zaehler, Text::_($value->name).HTMLHelper::_('image',Uri::root() . $value->picture, $value->name, array(' title' => $value->name, ' width' => 50))    );  
+  
+foreach( $firstleagueoverview as $key2 => $value2 ) if ( $value->id == $value2->federation )
 {
 $routeparameter                       = array();
 $routeparameter['cfg_which_database'] = Factory::getApplication()->input->getInt('cfg_which_database', 0);
@@ -67,35 +52,18 @@ $link                                 = sportsmanagementHelperRoute::getSportsma
 echo JSMCountries::getCountryFlag($value2->country).' '.HTMLHelper::link($link, $value2->name);    
     
     
-}      
-    ?>   
-
-      </div>
-    </div>
-
-
-
- <?php
-$start++; 
-}	
-?>   
+}  
   
-
-
-  </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-</div>
+  
+echo HTMLHelper::_('bootstrap.endTab');
+$zaehler++;  
+}  
+  
+  
+  
+  
+  
+  
+?>  
+  
+  
