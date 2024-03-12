314188c0ae1a84ece48c9b241f5703a7e84e94a5
diddipoeler <diddipoeler@gmx.de>
2024-03-12 10:39:05 +0100

    #1698

--- admin/views/fieldsets/tmpl/default_playgroundnotiz.php
+++ admin/views/fieldsets/tmpl/default_playgroundnotiz.php
@@ -18,11 +18,88 @@ use Joomla\CMS\Log\Log;
 
 //echo '<pre>'.print_r($this->playgroundnotic,true).'</pre';
 
-echo 'Hier können Sie die Notizen zum Stadion hinterlegen. Wie Name oder unterschiedliche Kapazitäten.';
+echo 'Hier können Sie die Notizen zum Stadion hinterlegen. Wie Name oder unterschiedliche Kapazitäten.<br>';
 
 ?>
+<script type="text/javascript">
+        function addRows()
+        {
+            // Reference to the table body
+var body = jQuery("#playgroundnotic").find('tbody');
+// Create a new row element
+var row = jQuery('<tr>');
+// Create a new column element
+var column = jQuery('<td>');
+// Create a new image element
+var image = jQuery('<img>');
+image.attr('src', 'img.png');
+image.text('Image cell');
 
-<table class="table table-striped">
+// Append the image to the column element
+column.append(image);
+// Append the column to the row element
+row.append(column);
+
+var column1 = jQuery('<td>');          
+var input = jQuery('<input>');          
+input.attr('type', 'text');          
+input.attr('id', 'playground_id');
+input.attr('name', 'playground_id');
+input.attr('value', '<?php echo $this->item->id; ?>');          
+column1.append(input);          
+row.append(column1); 
+          
+var column2 = jQuery('<td>');          
+var input = jQuery('<input>');          
+input.attr('type', 'text');          
+input.attr('id', 'date_von');
+input.attr('name', 'date_von');
+column2.append(input);          
+row.append(column2);          
+
+var column3 = jQuery('<td>');          
+var input = jQuery('<input>');          
+input.attr('type', 'text');          
+input.attr('id', 'date_bis');
+input.attr('name', 'date_bis');
+column3.append(input);          
+row.append(column3);                    
+          
+var column4 = jQuery('<td>');          
+var input = jQuery('<input>');          
+input.attr('type', 'text');          
+input.attr('id', 'name_visitors');
+input.attr('name', 'name_visitors');
+column4.append(input);          
+row.append(column4);            
+          
+          
+var column5 = jQuery('<td>');          
+var input = jQuery('<input>');          
+input.attr('type', 'text');          
+input.attr('id', 'notes');
+input.attr('name', 'notes');
+column5.append(input);          
+row.append(column5); 
+          
+          
+var column6 = jQuery('<td>');          
+var input = jQuery('<input>');          
+input.attr('type', 'text');          
+input.attr('id', 'max_visitors');
+input.attr('name', 'max_visitors');
+column6.append(input);          
+row.append(column6); 
+          
+          
+// Append the row to the table body
+body.append(row);
+        }
+  
+        </script>
+
+<input type="button" value="Add Rows" onclick="addRows()" />
+<table class="table table-striped" id="playgroundnotic">
   <thead>
     <tr>
     
@@ -83,6 +160,11 @@ echo 'Hier können Sie die Notizen zum Stadion hinterlegen. Wie Name oder unters
      <td>
     <?php
     echo $value->name_visitors;
+    ?>
+    </td>
+     <td>
+    <?php
+    echo $value->notes;
     ?>
     </td>
      <td>
