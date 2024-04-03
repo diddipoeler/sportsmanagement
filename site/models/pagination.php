0cf579d50d5ef2538514dc0dac30d5325fe62de1
diddipoeler <diddipoeler@gmx.de>
2024-04-03 08:31:42 +0200

    Update pagination.php

--- site/models/pagination.php
+++ site/models/pagination.php
@@ -1,4 +1,15 @@
 <?php
+/**
+I needed to override the default pagination in a component view template. Normally the pagination is shown with the line 
+<?php echo $this->pagination->getListFooter(); ?>
+But in this instance I wanted to show the total number of filtered items.
+After trawling through the files: libraries/src/Pagination/Pagination.php and layouts/joomla/pagination/links.php the answer was simple
+Create a folder layouts/pagination in your component administrator folder. Copy the links.php file into that folder.
+Instead of the usual getListFooter() line shown above use:
+<?php echo $this->pagination->getPaginationLinks('pagination.links'); ?>
+The getPaginationLinks function has the signature public function getPaginationLinks($layoutId = 'joomla.pagination.links', $options = []) so it will take an alternative layout to use.
+Now you can modify the links.php file for your component to get exactly the output you require.
+*/
 
 namespace sportsmanagement\Site\Model;
 
@@ -26,10 +37,10 @@ public function getPaginationLinks($layoutId = 'joomla.pagination.links', $optio
         'pagesTotal'   => $this->pagesTotal,
     ];
 
-//echo 'getLimitBox<pre>.print_r($this->getLimitBox(),true).'</pre>;
-//echo 'getPagesCounter<pre>.print_r($this->getPagesCounter(),true).'</pre>;
-//echo 'getPaginationPages<pre>.print_r($this->getPaginationPages(),true).'</pre>;
-//echo 'pagesTotal<pre>.print_r($this->pagesTotal(),true).'</pre>;
+//echo 'getLimitBox<pre>'.print_r($this->getLimitBox(),true).'</pre>';
+//echo 'getPagesCounter<pre>'.print_r($this->getPagesCounter(),true).'</pre>';
+//echo 'getPaginationPages<pre>'.print_r($this->getPaginationPages(),true).'</pre>';
+//echo 'pagesTotal<pre>'.print_r($this->pagesTotal(),true).'</pre>';
     
     return LayoutHelper::render($layoutId, ['list' => $list, 'options' => $options]);
 }
