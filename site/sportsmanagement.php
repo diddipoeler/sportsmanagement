2d1997f1e432acc75e2a404ecb63d042cd008cc9
diddipoeler <diddipoeler@gmx.de>
2024-04-07 09:23:54 +0200

    #1731

--- site/sportsmanagement.php
+++ site/sportsmanagement.php
@@ -129,20 +129,22 @@ switch ($view)
 		JLoader::import('components.com_sportsmanagement.models.rounds', JPATH_ADMINISTRATOR);
 		JLoader::import('components.com_sportsmanagement.models.round', JPATH_ADMINISTRATOR);
 		JLoader::import('components.com_sportsmanagement.models.projectteams', JPATH_ADMINISTRATOR);
-		JLoader::import('components.com_sportsmanagement.helpers.pagination', JPATH_SITE);
-        	JLoader::import('components.com_sportsmanagement.helpers.jsmpagination', JPATH_SITE);
+		//JLoader::import('components.com_sportsmanagement.helpers.pagination', JPATH_SITE);
+        	//JLoader::import('components.com_sportsmanagement.helpers.jsmpagination', JPATH_SITE);
 		JLoader::import('components.com_sportsmanagement.helpers.comments', JPATH_SITE);
 	JLoader::import('components.com_sportsmanagement.models.club', JPATH_ADMINISTRATOR);
 	JLoader::import('components.com_sportsmanagement.models.league', JPATH_ADMINISTRATOR);
+	JLoader::import('components.com_sportsmanagement.models.pagination', JPATH_SITE);
 		break;
 	case 'resultsmatrix':
 		JLoader::import('components.com_sportsmanagement.models.projectteams', JPATH_ADMINISTRATOR);
-		JLoader::import('components.com_sportsmanagement.helpers.pagination', JPATH_SITE);
-        	JLoader::import('components.com_sportsmanagement.helpers.jsmpagination', JPATH_SITE);
+		//JLoader::import('components.com_sportsmanagement.helpers.pagination', JPATH_SITE);
+        	//JLoader::import('components.com_sportsmanagement.helpers.jsmpagination', JPATH_SITE);
 		JLoader::import('components.com_sportsmanagement.models.matrix', JPATH_SITE);
 		JLoader::import('components.com_sportsmanagement.models.results', JPATH_SITE);
 		JLoader::import('components.com_sportsmanagement.helpers.comments', JPATH_SITE);
 		JLoader::import('components.com_sportsmanagement.models.round', JPATH_ADMINISTRATOR);
+	JLoader::import('components.com_sportsmanagement.models.pagination', JPATH_SITE);
 		break;
 	case 'roster':
     case 'rosteralltime':
