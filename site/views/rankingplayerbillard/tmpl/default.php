dd5fe61775df37f822875d573acaaba0c75ee911
diddipoeler <diddipoeler@gmx.de>
2024-09-05 13:56:10 +0200

    Update default.php

--- site/views/rankingplayerbillard/tmpl/default.php
+++ site/views/rankingplayerbillard/tmpl/default.php
@@ -12,3 +12,83 @@
 defined('_JEXEC') or die('Restricted access');
 use Joomla\CMS\HTML\HTMLHelper;
 
+?>
+<table>
+  
+<?php
+  echo '<tr>';
+
+echo '<td>';
+echo '</td>';    
+echo '<td>';
+echo '</td>';    
+  echo '<td>';
+echo '</td>';    
+  echo '<td>';
+echo '</td>';    
+
+  
+foreach ( $this->rounds as $key => $value )
+  {
+echo '<td>';
+echo $value->roundcode;
+
+echo '</td>';    
+  }
+echo '</tr>';  
+
+
+
+foreach ( $this->ranking as $rankkey => $rankvalue )
+{  
+echo '<tr>';
+echo '<td>';
+echo $rankkey;
+echo '</td>';
+echo '<td>';
+echo $rankvalue['teamplayerid'];
+echo '</td>';  
+
+echo '<td>';
+echo '</td>'; 
+echo '<td>';
+echo '</td>'; 
+  
+foreach ( $this->rounds as $key => $value )
+  {
+echo '<td>';
+echo $rankvalue[$value->roundcode];
+echo '</td>';
+  }
+echo '<td>';
+echo $rankvalue['total'];
+echo '</td>';   
+echo '</tr>'; 
+ } 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+?>
+  </table>
