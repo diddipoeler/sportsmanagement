25a668c7fbd27e2e02962a6c1c3781af0c918ed0
diddipoeler <diddipoeler@gmx.de>
2024-09-05 09:02:24 +0200

    Create view.html.php

---
+++ site/views/rankingplayerbillard/view.html.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * SportsManagement ein Programm zur Verwaltung für alle Sportarten
+ * @version    1.0.05
+ * @package    Sportsmanagement
+ * @subpackage rankingplayerbillard
+ * @file       view.html.php
+ * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
+defined('_JEXEC') or die('Restricted access');
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Uri\Uri;
+use Joomla\CMS\Factory;
+use Joomla\Registry\Registry;
+use Joomla\CMS\Filesystem\File;
+use Joomla\CMS\Form\Form;
+
+/**
+ * sportsmanagementViewMatchReport
+ *
+ * @package
+ * @author
+ * @copyright diddi
+ * @version   2014
+ * @access    public
+ */
+class sportsmanagementViewrankingplayerbillard extends sportsmanagementView
+{
+
+	
+	public function init()
+	{
+
+  }
+
+}
