38ce368b444f000db548fdeac7022c3a6d9253e5
diddipoeler <diddipoeler@gmx.de>
2024-09-09 16:07:45 +0200

    billard

--- site/views/editmatch/tmpl/editlineup_golfbillardplayer.php
+++ site/views/editmatch/tmpl/editlineup_golfbillardplayer.php
@@ -1,6 +1,68 @@
 <?php
 
+defined('_JEXEC') or die('Restricted access');
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Router\Route;
+use Joomla\CMS\HTML\HTMLHelper;
 
-echo '<pre>'.print_r($this->lists['team_players_billard'],true).'</pre>';
+//echo '<pre>'.print_r($this->lists['team_players_billard'],true).'</pre>';
+
+for ($a=1; $a < 6;$a++)
+{
+?>
+<div class="row">
+ <div class="col-sm">
+   <?php echo Text::_('COM_SPORTSMANAGEMENT_GOLF_BILLARD_P_PLAYER').' '.$a; ?>
+      
+    </div>
+    <div class="col-sm">
+      One of three columns
+    </div>
+  </div>
+
+
+<?php
+}
+?>
+<div class="row">
+  <div class="col-sm">
+      
+    </div>
+</div>
+  <?php
+for ($a=1; $a < 2;$a++)
+{
+?>
+<div class="row">
+ <div class="col-sm">
+   <?php echo Text::_('COM_SPORTSMANAGEMENT_GOLF_BILLARD_P_CAPTAIN'); ?>
+      
+    </div>
+    <div class="col-sm">
+      One of three columns
+    </div>
+  </div>
+
+
+<?php
+}
+
+for ($a=1; $a < 2;$a++)
+{
+?>
+<div class="row">
+ <div class="col-sm">
+   <?php echo Text::_('COM_SPORTSMANAGEMENT_GOLF_BILLARD_P_RESERVE'); ?>
+      
+    </div>
+    <div class="col-sm">
+      One of three columns
+    </div>
+  </div>
+
+
+<?php
+}
+
 
 ?>
