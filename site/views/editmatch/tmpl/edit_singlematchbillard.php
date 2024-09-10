49c47a5792ab2b0059105005f19dee7f5bf4582a
diddipoeler <diddipoeler@gmx.de>
2024-09-10 17:28:16 +0200

    update

--- site/views/editmatch/tmpl/edit_singlematchbillard.php
+++ site/views/editmatch/tmpl/edit_singlematchbillard.php
@@ -1,5 +1,11 @@
 <?php
 
+defined('_JEXEC') or die('Restricted access');
+use Joomla\CMS\Factory;
+use Joomla\CMS\Language\Text;
+
+
+
 echo 'match <pre>'.print_r($this->match,true).'</pre>';
 echo 'singlematch <pre>'.print_r($this->singlematches,true).'</pre>';
 
@@ -11,15 +17,22 @@ echo 'starters_away <pre>'.print_r($starters_away,true).'</pre>';
 /** erst einmal 5 spiele */
 for ($a=1; $a < 6;$a++)
 {
-
+?>
+<div class="row">
+<?php
 $checksinglematch = $this->model->getSingleMatchData($this->match->id,$a);
 if ( $checksinglematch )
 {
-    
+?>
+<div class="text-bg-primary p-3">Spiel vorhanden</div>
+<?php    
 }
 else
 {
-    echo 'nicht vorhanden<br>';
+    ?>
+    <div class="text-bg-danger p-3">Spiel nicht vorhanden</div>
+    <?php
+    //echo 'nicht vorhanden<br>';
     foreach ( $starters_home as $keyhome => $valuehome ) if ( $valuehome->trikot_number == $a)
     {
     foreach ( $starters_away as $keyaway => $valueaway ) if ( $valueaway->trikot_number == $a)
@@ -27,13 +40,19 @@ else
     $insertsinglematch = $this->model->insertSingleMatchData($this->match->id,$a,$valuehome->teamplayer_id, $valueaway->teamplayer_id,$valuehome->projectteam_id, $valueaway->projectteam_id);    
         if ( $insertsinglematch )
         {
-            echo 'spiel angelegt <br>';
+            ?>
+            <div class="text-bg-success p-3">Spiel angelegt</div>
+            <?php
+            //echo 'spiel angelegt <br>';
         }
         else
         {
-            echo 'spiel nicht angelegt <br>';
+            ?>
+            <div class="text-bg-danger p-3">Fehler beim Anlegen des Spiels</div>
+            <?php
+            //echo 'spiel nicht angelegt <br>';
         }
-        
+       
         
     }    
         
@@ -42,12 +61,10 @@ else
     }
     
     
-    
-    
-    
-    
 }
-
+?>
+</div>
+<?php
 }
 
 
