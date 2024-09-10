0d75fa77c2d2833f8128699e30ea8beb257eec00
diddipoeler <diddipoeler@gmx.de>
2024-09-10 09:13:45 +0200

    billard

--- site/models/editmatch.php
+++ site/models/editmatch.php
@@ -19,6 +19,7 @@ use Joomla\CMS\MVC\Model\BaseDatabaseModel;
 use Joomla\CMS\Form\Form;
 use Joomla\CMS\Form\FormField;
 use Joomla\CMS\Form\FormHelper;
+use Joomla\CMS\Language\Text;
 
 JLoader::import('components.com_sportsmanagement.helpers.imageselect', JPATH_SITE);
 JLoader::import('components.com_sportsmanagement.models.project', JPATH_SITE);
