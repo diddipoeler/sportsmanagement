9cfe75fdf8846d3c55a8189e825b2f88ce615f39
diddipoeler <diddipoeler@gmx.de>
2024-09-10 14:52:42 +0200

    update

--- site/views/editmatch/tmpl/edit_singlematchbillard.php
+++ site/views/editmatch/tmpl/edit_singlematchbillard.php
@@ -1,6 +1,12 @@
 <?php
 
+echo 'match <pre>'.print_r($this->match,true).'</pre>';
+echo 'singlematch <pre>'.print_r($this->singlematches,true).'</pre>';
 
-echo '<pre>'.print_r($this->singlematches,true).'</pre>';
+$starters_home    = sportsmanagementModelMatch::getMatchPersons($this->projectteam1_id, 0, $this->match->id, 'player');
+$starters_away    = sportsmanagementModelMatch::getMatchPersons($this->projectteam2_id, 0, $this->match->id, 'player');
+echo 'starters_home <pre>'.print_r($starters_home,true).'</pre>';
+echo 'starters_away <pre>'.print_r($starters_away,true).'</pre>';
+
 
 ?>
