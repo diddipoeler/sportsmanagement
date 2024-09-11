a2f9cddec76c13f3ad059dd958cf80566072f093
diddipoeler <diddipoeler@gmx.de>
2024-09-11 09:54:46 +0200

    Update edit_singlematchbillard.php

--- site/views/editmatch/tmpl/edit_singlematchbillard.php
+++ site/views/editmatch/tmpl/edit_singlematchbillard.php
@@ -6,13 +6,13 @@ use Joomla\CMS\Language\Text;
 
 
 
-echo 'match <pre>'.print_r($this->match,true).'</pre>';
-echo 'singlematch <pre>'.print_r($this->singlematches,true).'</pre>';
+//echo 'match <pre>'.print_r($this->match,true).'</pre>';
+//echo 'singlematch <pre>'.print_r($this->singlematches,true).'</pre>';
 
 $starters_home    = sportsmanagementModelMatch::getMatchPersons($this->match->projectteam1_id, 0, $this->match->id, 'player');
 $starters_away    = sportsmanagementModelMatch::getMatchPersons($this->match->projectteam2_id, 0, $this->match->id, 'player');
-echo 'starters_home <pre>'.print_r($starters_home,true).'</pre>';
-echo 'starters_away <pre>'.print_r($starters_away,true).'</pre>';
+//echo 'starters_home <pre>'.print_r($starters_home,true).'</pre>';
+//echo 'starters_away <pre>'.print_r($starters_away,true).'</pre>';
 
 /** erst einmal 5 spiele */
 for ($a=1; $a < 6;$a++)
@@ -68,7 +68,7 @@ else
 }
 
 $this->singlematches = $this->model->getSingleMatchDatas($this->match->id);
-echo 'singlematch <pre>'.print_r($this->singlematches,true).'</pre>';
+//echo 'singlematch <pre>'.print_r($this->singlematches,true).'</pre>';
 
 
 
