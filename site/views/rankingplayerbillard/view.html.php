6df9aa898d7549a5a30f6e507d4496711b3d68c6
diddipoeler <diddipoeler@gmx.de>
2024-09-30 12:55:17 +0200

    Update view.html.php

--- site/views/rankingplayerbillard/view.html.php
+++ site/views/rankingplayerbillard/view.html.php
@@ -51,52 +51,72 @@ $this->ranking[$value->teamplayer2_id]['teamplayerid'] = $value->teamplayer2_id;
 $this->ranking[$value->teamplayer2_id]['projectteamid'] = $value->projectteam2_id;
   
 $gesamtresult = $value->team1_result + $value->team2_result;
-$playerresult1 = 0;
-$playerresult2 = 0;  
+//$playerresult1 = 0;
+//$playerresult2 = 0;  
 
+  /**
   if ( $gesamtresult > 2 )
     {
       $playerresult1++;
       $playerresult2++;
     }
+*/
 
+
+
+  /**
   if ( $value->team1_result > $value->team2_result )
     {
       $playerresult1 = $playerresult1 + 2;
       $playerresult2 = $playerresult2 + 0;
-      $this->ranking[$value->teamplayer1_id]['W'] += 1;
-      $this->ranking[$value->teamplayer1_id]['V'] += 0;
+      $this->ranking[$value->teamplayer1_id]['G'] += $value->team1_result;
+      $this->ranking[$value->teamplayer1_id]['V'] += $value->team2_result;
       
-      $this->ranking[$value->teamplayer2_id]['V'] += 1;
-      $this->ranking[$value->teamplayer2_id]['W'] += 0;
+      $this->ranking[$value->teamplayer2_id]['V'] += $value->team2_result;
+      $this->ranking[$value->teamplayer2_id]['G'] += $value->team1_result;
     }
   elseif ( $value->team1_result < $value->team2_result )
     {
       $playerresult1 = $playerresult1 + 0;
       $playerresult2 = $playerresult2 + 2;
       
-      $this->ranking[$value->teamplayer2_id]['W'] += 1;
-      $this->ranking[$value->teamplayer2_id]['V'] += 0;
+      $this->ranking[$value->teamplayer2_id]['G'] += $value->team2_result;
+      $this->ranking[$value->teamplayer2_id]['V'] += $value->team1_result;
       
-      $this->ranking[$value->teamplayer1_id]['V'] += 1;
-      $this->ranking[$value->teamplayer1_id]['W'] += 0;
+      $this->ranking[$value->teamplayer1_id]['G'] += $value->team1_result;
+      $this->ranking[$value->teamplayer1_id]['V'] += $value->team2_result;
       
     }
-
+*/
   
-  
+if ( $value->team1_result > $value->team2_result && $gesamtresult > 2 )
+    {
+$this->ranking[$value->teamplayer1_id][$value->roundcode]['G'] += 1;
+$this->ranking[$value->teamplayer1_id]['totalG'] += 1;      
+      }
 
-$this->ranking[$value->teamplayer1_id][$value->roundcode] = $playerresult1;
+if ( $value->team1_result < $value->team2_result && $gesamtresult > 2 )
+    {
+$this->ranking[$value->teamplayer2_id][$value->roundcode]['G'] += 1;
+$this->ranking[$value->teamplayer2_id]['totalG'] += 1; 
+  
+}
+  
+$this->ranking[$value->teamplayer1_id][$value->roundcode]['G'] += $value->team1_result;
+$this->ranking[$value->teamplayer1_id][$value->roundcode]['V'] = $value->team2_result;  
 $this->ranking[$value->teamplayer1_id]['total'] += $playerresult1;
-    
+$this->ranking[$value->teamplayer1_id]['totalG'] += $value->team1_result;  
+$this->ranking[$value->teamplayer1_id]['totalV'] += $value->team2_result;     
 
-$this->ranking[$value->teamplayer2_id][$value->roundcode] = $playerresult2;
+$this->ranking[$value->teamplayer2_id][$value->roundcode]['G'] += $value->team2_result;
+$this->ranking[$value->teamplayer2_id][$value->roundcode]['V'] = $value->team1_result;  
 $this->ranking[$value->teamplayer2_id]['total'] += $playerresult2;    
-    
-    
+$this->ranking[$value->teamplayer2_id]['totalG'] += $value->team2_result;    
+$this->ranking[$value->teamplayer2_id]['totalV'] += $value->team1_result;
+  
 }
 
-$volume  = array_column($this->ranking, 'total');
+$volume  = array_column($this->ranking, 'totalG');
 // Sort the data with volume descending, edition ascending
 // Add $data as the last parameter, to sort by the common key
 array_multisort($volume, SORT_DESC,  $this->ranking);
