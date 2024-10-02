2146324a089ef920fe239ce74b511e8bf6c09105
diddipoeler <diddipoeler@gmx.de>
2024-10-02 10:57:52 +0200

    Update githubinstall.php

--- admin/controllers/githubinstall.php
+++ admin/controllers/githubinstall.php
@@ -18,6 +18,11 @@ defined('_JEXEC') or die('Restricted access');
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\Session\Session;
 use Joomla\CMS\MVC\Controller\FormController;
+use Joomla\CMS\Router\Route;
+use Joomla\CMS\Factory;
+use Joomla\CMS\Component\ComponentHelper;
+
+
 
 /**
  * SportsManagement Controller
@@ -35,6 +40,27 @@ use Joomla\CMS\MVC\Controller\FormController;
 class sportsmanagementControllergithubinstall extends FormController
 {
 
+	function CopyGithubLink()
+	{
+$model = $this->getModel();
+$github_link         = ComponentHelper::getParams('com_sportsmanagement')->get('cfg_update_server_file', '');
+$msg   = $model->CopyGithubLink();
+$url = Route::_('index.php?option=com_sportsmanagement&view=update&task=update.save&file_name=jsm_update_github.php"');
+$this->setRedirect->redirect($url, $msg);
+		
+	}
+	
+	/**
+	 * Proxy for getModel.
+	 *
+	 * @since 1.6
+	 */
+	public function getModel($name = 'githubinstall', $prefix = 'sportsmanagementModel', $config = Array())
+	{
+		$model = parent::getModel($name, $prefix, array('ignore_request' => true));
+		return $model;
+	}
+	
 	/**
 	 * Method to store
 	 *
