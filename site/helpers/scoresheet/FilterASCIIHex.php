2e26e48686a3ec0cc7b9027cc34474ea2cf34924
diddipoeler <diddipoeler@gmx.de>
2024-10-02 13:21:02 +0200

    Update FilterASCIIHex.php

--- site/helpers/scoresheet/FilterASCIIHex.php
+++ site/helpers/scoresheet/FilterASCIIHex.php
@@ -10,63 +10,51 @@ if (isset($FPDM_FILTERS)) array_push($FPDM_FILTERS, "ASCIIHexDecode");
 
 class FilterASCIIHex
 {
+    /**
+     * Get a binary string from its hexadecimal representation
+     * @internal same as _hex2bin ($hexString)
+     * @note Function was written because PHP has a bin2hex, but not a hex2bin!
+     * @internal note pack(�C�,hexdec(substr($data,$i,2))) DOES NOT WORK
+     * @param $hexString
+     * @return bool|string
+     */
+    public function decode($hexString)
+    {
+        $BinStr = '';
+
+        $hexLength = strlen($hexString);
+        // only hex numbers is allowed
+        if ($hexLength % 2 != 0 || preg_match("/[^\da-fA-F]/", $hexString)) return FALSE;
 
 
-	/**
-	 *Get a binary string from its hexadecimal representation
-	 *
-	 * @internal same as _hex2bin ($hexString)
-	 * @access   public
-	 * @note     Function was written because PHP has a bin2hex, but not a hex2bin!
-	 * @internal note pack(�C�,hexdec(substr($data,$i,2))) DOES NOT WORK
-	 *
-	 **/
-	function decode($data)
-	{
-
-		$hexString = $data;
-		$BinStr    = '';
-
-		$hexLength = strlen($hexString);
-		// only hex numbers is allowed
-		if ($hexLength % 2 != 0 || preg_match("/[^\da-fA-F]/", $hexString)) return false;
+        //Loop through the input and convert it
+        for ($i = 0; $i < $hexLength; $i += 2)
+            $BinStr .= '%' . substr($hexString, $i, 2);
 
 
-		//Loop through the input and convert it
-		for ($i = 0; $i < $hexLength; $i += 2)
-			$BinStr .= '%' . substr($hexString, $i, 2);
+        // Raw url-decode and return the result
+        return rawurldecode($BinStr);//chr(hexdec())
+    }
 
 
-		// Raw url-decode and return the result
-		return rawurldecode($BinStr);//chr(hexdec())
-	}
-
-
-	/**
-	 *Encodes a binary string to its hexadecimal representation
-	 *
-	 * @param   string  $str  a binary string
-	 *
-	 * @return string hex the hexified string
-	 **@internal same as bin2hex
-	 * @access    public
-	 * @internal  dechex(ord($str{$i})); is buggy because for hex value of 0-15 heading 0 is missing! Using sprintf() to get it right.
-	 */
-	function encode($data)
-	{
-		//----------------------
-		$str = $data;
-		$hex = "";
-		$i   = 0;
-		do
-		{
-			$hex .= sprintf("%02x", ord($str{$i}));
-			$i++;
-		} while ($i < strlen($str));
-
-		return $hex;
-	}
+    /**
+     *Encodes a binary string to its hexadecimal representation
+     *
+     * @internal same as bin2hex
+     * @internal  dechex(ord($str{$i})); is buggy because for hex value of 0-15 heading 0 is missing! Using sprintf() to get it right.
+     * @param string $str a binary string
+     * @return string hex the hexified string
+     **/
+    public function encode($str)
+    {
+        //----------------------
+        $hex = "";
+        $i = 0;
+        do {
+            $hex .= sprintf("%02x", ord($str{$i}));
+            $i++;
+        } while ($i < strlen($str));
+        return $hex;
+    }
 
 }
-
-?>
