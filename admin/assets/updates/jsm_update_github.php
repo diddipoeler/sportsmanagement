33d40d9c08b42db7807297687f3fc154b3c2edee
diddipoeler <diddipoeler@gmx.de>
2024-10-02 08:44:02 +0200

    Update jsm_update_github.php

--- admin/assets/updates/jsm_update_github.php
+++ admin/assets/updates/jsm_update_github.php
@@ -1,1 +1,23 @@
-
+<?php
+/**
+ * SportsManagement ein Programm zur Verwaltung für Sportarten
+ * @version    1.0.05
+ * @package    Sportsmanagement
+ * @subpackage updates
+ * @file       jsm_update_github.php
+ * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
+defined('_JEXEC') or die('Restricted access');
+use Joomla\CMS\Factory;
+use Joomla\CMS\Component\ComponentHelper;
+use Joomla\CMS\Filter\OutputFilter;
+use Joomla\CMS\Uri\Uri;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Installer\Installer;
+use Joomla\CMS\Updater\Update;
+use Joomla\CMS\Updater\Updater;
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Router\Route;
+use Joomla\CMS\Plugin\PluginHelper;
