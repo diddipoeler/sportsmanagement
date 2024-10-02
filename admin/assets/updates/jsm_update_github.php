06115e25a658545919c3275e3d7421da3555a283
diddipoeler <diddipoeler@gmx.de>
2024-10-02 08:51:58 +0200

    Update jsm_update_github.php

--- admin/assets/updates/jsm_update_github.php
+++ admin/assets/updates/jsm_update_github.php
@@ -21,3 +21,37 @@ use Joomla\CMS\Updater\Updater;
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Router\Route;
 use Joomla\CMS\Plugin\PluginHelper;
+
+
+?>
+
+<form enctype="multipart/form-data" action="<?php echo Route::_('index.php?option=com_installer&view=install&tmpl=component'); ?>" method="post" name="adminForm" id="adminForm">
+
+<fieldset class="uploadform option-fieldset options-form">
+<legend>Update vom Verzeichnis</legend>
+
+<div class="control-group">
+    <label for="install_directory" class="control-label">
+        Update vom Verzeichnis    </label>
+    <div class="controls">
+        <input type="text" id="install_directory" name="install_directory" class="form-control" value="">
+    </div>
+</div>
+<div class="control-group">
+    <div class="controls">
+        <button type="button" class="btn btn-primary" id="installbutton_directory" onclick="this.form.submit()">
+            Überprüfen und installieren        </button>
+    </div>
+</div>
+</fieldset>
+
+
+<input type="hidden" name="installtype" value="directory">
+<input type="hidden" name="task" value="install.install">
+<?php echo HTMLHelper::_('form.token'); ?>
+
+
+</form>
+
+<div id="loading"></div>
+
