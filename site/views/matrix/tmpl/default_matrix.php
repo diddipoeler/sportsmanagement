60141ef1b8d278ad45f2b10c611e5d58e7e6d211
diddipoeler <diddipoeler@gmx.de>
2024-10-05 15:15:54 +0200

    neu

--- site/views/matrix/tmpl/default_matrix.php
+++ site/views/matrix/tmpl/default_matrix.php
@@ -114,6 +114,24 @@ use Joomla\CMS\Factory;
 </style>
 <!--<![endif]-->
 
+<?php
+/** pdf download */
+if ( $this->config['show_button_download_pdf'] )
+{
+?>
+<button onclick="javascript:downpdf('rankingall')"><?php echo HTMLHelper::_('image', 'media/com_sportsmanagement/jl_images/pdf.png', Text::_('COM_SPORTSMANAGEMENT_FES_OVERALL_PARAM_LABEL_SHOW_BUTTON_DOWNLOAD_PDF'), array(' width' => 40));?>  PDF</button>
+<?php
+}
+
+/** excel download */
+if ( $this->config['show_button_download_excel'] )
+{
+?>
+<button onclick="javascript:downexcel('rankingall')"><?php echo HTMLHelper::_('image', 'media/com_sportsmanagement/jl_images/excel.png', Text::_('COM_SPORTSMANAGEMENT_FES_OVERALL_PARAM_LABEL_SHOW_BUTTON_DOWNLOAD_EXCEL'), array(' width' => 40));?> EXCEL</button>
+<?php
+}
+?>
+
 <div class="<?php echo $this->divclassrow; ?> table-responsive" id="defaultmatrix">
 	<?php
 
