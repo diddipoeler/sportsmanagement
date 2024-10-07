fc1039bb3a911f1453bca7895b04dfda3353a16b
diddipoeler <diddipoeler@gmx.de>
2024-10-07 10:48:52 +0200

    Update default_results.php

--- site/views/results/tmpl/default_results.php
+++ site/views/results/tmpl/default_results.php
@@ -22,7 +22,24 @@ else
 	?>
     <div class="<?php echo $this->divclassrow; ?> table-responsive" id="defaultresults" >
         <br/>
-        
+<?php
+/** pdf download */
+if ( $this->config['show_button_download_pdf'] )
+{
+?>
+<button onclick="javascript:downpdf('results')"><?php echo HTMLHelper::_('image', 'media/com_sportsmanagement/jl_images/pdf.png', Text::_('COM_SPORTSMANAGEMENT_FES_OVERALL_PARAM_LABEL_SHOW_BUTTON_DOWNLOAD_PDF'), array(' width' => 40));?>  PDF</button>
+<?php
+}
+
+/** excel download */
+if ( $this->config['show_button_download_excel'] )
+{
+?>
+<button onclick="javascript:downexcel('results')"><?php echo HTMLHelper::_('image', 'media/com_sportsmanagement/jl_images/excel.png', Text::_('COM_SPORTSMANAGEMENT_FES_OVERALL_PARAM_LABEL_SHOW_BUTTON_DOWNLOAD_EXCEL'), array(' width' => 40));?> EXCEL</button>
+<?php
+}
+
+	?>    
         <form name="adminForm" id="adminForm" action="<?php echo htmlspecialchars($this->uri->toString()); ?>"
           method="post">
         <input type="hidden" name="limitstart" value=""/>
