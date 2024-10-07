d20bf2f420e151cfe3440306e9369e6e126b3505
diddipoeler <diddipoeler@gmx.de>
2024-10-07 11:46:54 +0200

    Update default_results_all.php

--- site/views/allprojectrounds/tmpl/default_results_all.php
+++ site/views/allprojectrounds/tmpl/default_results_all.php
@@ -16,6 +16,23 @@ defined('_JEXEC') or die('Restricted access');
 <div class="row-fluid" id="">
 <!-- content -->
 <?php
+
+/** pdf download */
+if ( $this->config['show_button_download_pdf'] )
+{
+?>
+<button onclick="javascript:downpdf('allprojectrounds')"><?php echo HTMLHelper::_('image', 'media/com_sportsmanagement/jl_images/pdf.png', Text::_('COM_SPORTSMANAGEMENT_FES_OVERALL_PARAM_LABEL_SHOW_BUTTON_DOWNLOAD_PDF'), array(' width' => 40));?>  PDF</button>
+<?php
+}
+
+/** excel download */
+if ( $this->config['show_button_download_excel'] )
+{
+?>
+<button onclick="javascript:downexcel('allprojectrounds')"><?php echo HTMLHelper::_('image', 'media/com_sportsmanagement/jl_images/excel.png', Text::_('COM_SPORTSMANAGEMENT_FES_OVERALL_PARAM_LABEL_SHOW_BUTTON_DOWNLOAD_EXCEL'), array(' width' => 40));?> EXCEL</button>
+<?php
+}
+  
 ?>
 <table class="<?php echo $this->tableclass; ?>" id="allprojectrounds">
 <tr>
