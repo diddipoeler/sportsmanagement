09bdb92f77c4c235828bc385526b107dc129ad51
diddipoeler <diddipoeler@gmx.de>
2024-10-16 13:56:53 +0200

    Update default.php

--- site/views/billardlivegames/tmpl/default.php
+++ site/views/billardlivegames/tmpl/default.php
@@ -5,3 +5,47 @@
 
 
 ?>
+<table>
+<thead>
+<tr>
+<td></td>
+  <td colspan="2">Thuisploeg </td>
+  <td colspan="5"></td>
+  <td colspan="2">Bezoekers </td>
+<td colspan="8"></td>
+  
+</tr>
+  
+</thead>
+
+<tbody>
+<tr>
+<td></td>
+  <td>Kaart Nr</td>
+<td>Namen en voormaam</td>
+  <td>Letterw.</td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+  
+<td>Kaart Nr</td>
+<td>Namen en voormaam</td>
+  <td>Letterw.</td>
+<td></td>
+  <td></td>
+  <td></td>
+<td colspan="3">uitslag</td>
+
+  
+
+  
+  
+</tr>
+
+  
+</tbody>
+
+
+  
+</table>
