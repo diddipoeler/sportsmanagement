92484c0bf46f5dbf607eb1960c746f0dc0783228
diddipoeler <diddipoeler@gmx.de>
2025-02-21 12:30:04 +0100

    Update tournamentbracket.php

--- site/models/tournamentbracket.php
+++ site/models/tournamentbracket.php
@@ -25,8 +25,36 @@ class sportsmanagementModeltournamentbracket extends BaseDatabaseModel
 
 function gettournamentbracket($project_id = 0)
   {
+  $db    = $this->getDbo();
+		$query = $db->getQuery(true);
+echo $project_id;
 
+  /**
+		 *
+		 * alle runden
+		 */
+		$query->clear();
+		$query->select('*');
+		$query->from('#__sportsmanagement_round');
+		$query->where('project_id = ' . $project_id);
+		$query->where('tournement = 1');
+  $query->order('roundcode DESC');
+		$db->setQuery($query);
+		$roundresult = $db->loadObjectList('id');
 
+  /**
+		$roundresult2 = usort(
+			$roundresult, function ($a, $b) {
+			$c = $a->roundcode - $b->roundcode;
+
+			return $c;
+		}
+		);
+*/
+		 Factory::getApplication()->enqueueMessage(__METHOD__ . ' ' . __LINE__ . '<pre>'.print_r($roundresult,true).'</pre>'  , '');
+  //Factory::getApplication()->enqueueMessage(__METHOD__ . ' ' . __LINE__ . '<pre>'.print_r($roundresult2,true).'</pre>'  , '');
+  
+  
   }
 
   
