1aa0dde68a4f80e7bd5da6318c34080fb95eceb7
diddipoeler <diddipoeler@gmx.de>
2025-02-21 11:55:07 +0100

    Update tournamentbracket.php

--- site/models/tournamentbracket.php
+++ site/models/tournamentbracket.php
@@ -3,8 +3,8 @@
  * SportsManagement ein Programm zur Verwaltung für alle Sportarten
  * @version    1.0.05
  * @package    Sportsmanagement
- * @subpackage clubinfo
- * @file       clubinfo.php
+ * @subpackage tournamentbracket
+ * @file       tournamentbracket.php
  * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
  * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
@@ -19,19 +19,17 @@ use Joomla\CMS\MVC\Model\BaseDatabaseModel;
 use Joomla\CMS\Log\Log;
 use Joomla\CMS\Component\ComponentHelper;
 
-/**
- * sportsmanagementModelClubInfo
- *
- * @package
- * @author
- * @copyright diddi
- * @version   2014
- * @access    public
- */
+
 class sportsmanagementModeltournamentbracket extends BaseDatabaseModel
 {
 
+function gettournamentbracket()
+  {
 
+
+  }
+
+  
 }
 
 ?>
