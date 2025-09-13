6be0aab4034de7aaedf84df7d51fda2643ef74c7
diddipoeler <diddipoeler@gmx.de>
2025-09-13 14:19:23 +0200

    erläuterung

--- admin/helpers/browser.php
+++ admin/helpers/browser.php
@@ -40,6 +40,65 @@ defined('_JEXEC') or die('Restricted access');
  * This implementation is based on the original work from Gary White
  * http://apptools.com/phptools/browser/
  *
+
+ Browser Detection
+This solution identifies the following Browsers and does a best-guess on the version:
+
+Opera (Browser::BROWSER_OPERA)
+WebTV (Browser::BROWSER_WEBTV)
+NetPositive (Browser::BROWSER_NETPOSITIVE)
+Edge (Browser::BROWSER_EDGE)
+Internet Explorer (Browser::BROWSER_IE)
+Pocket Internet Explorer (Browser::BROWSER_POCKET_IE)
+Galeon (Browser::BROWSER_GALEON)
+Konqueror (Browser::BROWSER_KONQUEROR)
+iCab (Browser::BROWSER_ICAB)
+OmniWeb (Browser::BROWSER_OMNIWEB)
+Phoenix (Browser::BROWSER_PHOENIX)
+Firebird (Browser::BROWSER_FIREBIRD)
+UCBrowser (Browser::BROWSER_UCBROWSER)
+Firefox (Browser::BROWSER_FIREFOX)
+Mozilla (Browser::BROWSER_MOZILLA)
+Palemoon (Browser::BROWSER_PALEMOON)
+curl (Browser::BROWSER_CURL)
+wget (Browser::BROWSER_WGET)
+Amaya (Browser::BROWSER_AMAYA)
+Lynx (Browser::BROWSER_LYNX)
+Safari (Browser::BROWSER_SAFARI)
+Playstation (Browser::BROWSER_PLAYSTATION)
+iPhone (Browser::BROWSER_IPHONE)
+iPod (Browser::BROWSER_IPOD)
+Google's Android(Browser::BROWSER_ANDROID)
+Google's Chrome(Browser::BROWSER_CHROME)
+GoogleBot(Browser::BROWSER_GOOGLEBOT)
+Yahoo!'s Slurp(Browser::BROWSER_SLURP)
+W3C's Validator(Browser::BROWSER_W3CVALIDATOR)
+BlackBerry(Browser::BROWSER_BLACKBERRY)
+
+
+Operating System Detection
+This solution identifies the following Operating Systems:
+
+Windows (Browser::PLATFORM_WINDOWS)
+Windows CE (Browser::PLATFORM_WINDOWS_CE)
+Apple (Browser::PLATFORM_APPLE)
+Linux (Browser::PLATFORM_LINUX)
+Android (Browser::PLATFORM_ANDROID)
+OS/2 (Browser::PLATFORM_OS2)
+BeOS (Browser::PLATFORM_BEOS)
+iPhone (Browser::PLATFORM_IPHONE)
+iPod (Browser::PLATFORM_IPOD)
+BlackBerry (Browser::PLATFORM_BLACKBERRY)
+FreeBSD (Browser::PLATFORM_FREEBSD)
+OpenBSD (Browser::PLATFORM_OPENBSD)
+NetBSD (Browser::PLATFORM_NETBSD)
+SunOS (Browser::PLATFORM_SUNOS)
+OpenSolaris (Browser::PLATFORM_OPENSOLARIS)
+iPad (Browser::PLATFORM_IPAD)
+
+
+
+
  */
 class Browser
 {
