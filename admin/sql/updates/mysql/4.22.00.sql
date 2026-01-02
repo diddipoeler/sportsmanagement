a9b581f8bdc95c09d01bf9a3d6c9afcbfacb1b98
diddipoeler <diddipoeler@gmx.de>
2026-01-02 17:07:02 +0100

    joomla6

--- admin/sql/updates/mysql/4.22.00.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE  `#__sportsmanagement_team_player` ADD `market_text` VARCHAR(50) NULL ;
-ALTER TABLE  `#__sportsmanagement_season_team_person_id` ADD `market_text` VARCHAR(50) NULL ;
-
-
-
