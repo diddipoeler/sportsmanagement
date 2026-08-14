c2a18c51dcc340b4169770824da81038497eff19
diddipoeler <diddipoeler@gmx.de>
2026-08-14 15:58:59 +0200

    joomla6

--- site/views/results/tmpl/default_results.php
+++ site/views/results/tmpl/default_results.php
@@ -32,7 +32,7 @@ case 'results':
 if ( $this->config['show_button_download_pdf'] )
 {
 ?>
-<button onclick="javascript:downpdf('results')"><?php echo HTMLHelper::_('image', 'media/com_sportsmanagement/jl_images/pdf.png', Text::_('COM_SPORTSMANAGEMENT_FES_OVERALL_PARAM_LABEL_SHOW_BUTTON_DOWNLOAD_PDF'), array(' width' => 40));?>  PDF</button>
+<button onclick="javascript:downpdf('results')"><?php echo HTMLHelper::_('image', 'media/com_sportsmanagement/jl_images/pdf.png', Text::_('COM_SPORTSMANAGEMENT_FES_OVERALL_PARAM_LABEL_SHOW_BUTTON_DOWNLOAD_PDF'), array('width' => 40));?>  PDF</button>
 <?php
 }
 
@@ -40,7 +40,7 @@ if ( $this->config['show_button_download_pdf'] )
 if ( $this->config['show_button_download_excel'] )
 {
 ?>
-<button onclick="javascript:downexcel('results')"><?php echo HTMLHelper::_('image', 'media/com_sportsmanagement/jl_images/excel.png', Text::_('COM_SPORTSMANAGEMENT_FES_OVERALL_PARAM_LABEL_SHOW_BUTTON_DOWNLOAD_EXCEL'), array(' width' => 40));?> EXCEL</button>
+<button onclick="javascript:downexcel('results')"><?php echo HTMLHelper::_('image', 'media/com_sportsmanagement/jl_images/excel.png', Text::_('COM_SPORTSMANAGEMENT_FES_OVERALL_PARAM_LABEL_SHOW_BUTTON_DOWNLOAD_EXCEL'), array('width' => 40));?> EXCEL</button>
 <?php
 }
 break;
