71081a7e33cb0e868deafeda6ca9b773fd5af2d2
diddipoeler <diddipoeler@gmx.de>
2026-08-17 15:47:56 +0200

    Inject MVC factory into component service

--- admin/services/provider.php
+++ admin/services/provider.php
@@ -3,16 +3,18 @@
  * @package     SportsManagement
  * @subpackage  com_sportsmanagement
  *
- * Joomla 5/6 service provider scaffold.
+ * Joomla 5/6 service provider.
  */
 
-defined('_JEXEC') or die;
+\defined('_JEXEC') or die;
 
 use Diddipoeler\Component\SportsManagement\Administrator\Extension\SportsManagementComponent;
 use Joomla\CMS\Dispatcher\ComponentDispatcherFactoryInterface;
 use Joomla\CMS\Extension\ComponentInterface;
 use Joomla\CMS\Extension\Service\Provider\ComponentDispatcherFactory as ComponentDispatcherFactoryServiceProvider;
 use Joomla\CMS\Extension\Service\Provider\MVCFactory as MVCFactoryServiceProvider;
+use Joomla\CMS\HTML\Registry;
+use Joomla\CMS\MVC\Factory\MVCFactoryInterface;
 use Joomla\DI\Container;
 use Joomla\DI\ServiceProviderInterface;
 
@@ -31,9 +33,14 @@ return new class implements ServiceProviderInterface
         $container->set(
             ComponentInterface::class,
             static function (Container $container): ComponentInterface {
-                return new SportsManagementComponent(
+                $component = new SportsManagementComponent(
                     $container->get(ComponentDispatcherFactoryInterface::class)
                 );
+
+                $component->setRegistry($container->get(Registry::class));
+                $component->setMVCFactory($container->get(MVCFactoryInterface::class));
+
+                return $component;
             }
         );
     }
