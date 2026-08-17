702554216a89c13604eb36efffc0dd1aa59dedc8
diddipoeler <diddipoeler@gmx.de>
2026-08-17 15:22:17 +0200

    Migrate currentseason controller namespace

---
+++ admin/src/Controller/CurrentseasonController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @package     SportsManagement
+ * @subpackage  com_sportsmanagement
+ *
+ * Joomla 5/6 migration.
+ */
+
+namespace Diddipoeler\Component\SportsManagement\Administrator\Controller;
+
+\defined('_JEXEC') or die;
+
+/**
+ * Controller for the current-season view.
+ */
+class CurrentseasonController extends \JSMControllerAdmin
+{
+    protected $view_list = 'currentseasons';
+}
