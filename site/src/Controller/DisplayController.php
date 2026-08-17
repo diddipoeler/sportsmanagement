01ccb5fb4b9feb9417ec110f3588486f95ae07b8
diddipoeler <diddipoeler@gmx.de>
2026-08-17 15:15:32 +0200

    Add namespaced site display controller

---
+++ site/src/Controller/DisplayController.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @package     SportsManagement
+ * @subpackage  com_sportsmanagement
+ *
+ * Joomla 5/6 migration.
+ */
+
+namespace Diddipoeler\Component\SportsManagement\Site\Controller;
+
+\defined('_JEXEC') or die;
+
+use Joomla\CMS\MVC\Controller\BaseController;
+
+/**
+ * Default site controller for the modern Joomla dispatcher.
+ *
+ * The current legacy site controller contains no component-specific dispatch
+ * logic, so it can be represented directly by a namespaced BaseController.
+ * Task-specific legacy controllers are migrated separately.
+ */
+final class DisplayController extends BaseController
+{
+}
