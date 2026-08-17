2b6623373da7e9c666dd6fbfcc0fb5ca80740a44
diddipoeler <diddipoeler@gmx.de>
2026-08-17 15:40:41 +0200

    Add close smoke model

---
+++ admin/src/Model/CloseModel.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @package     SportsManagement
+ * @subpackage  com_sportsmanagement
+ */
+
+namespace Diddipoeler\Component\SportsManagement\Administrator\Model;
+
+\defined('_JEXEC') or die;
+
+use Joomla\CMS\MVC\Model\BaseDatabaseModel;
+
+/**
+ * Empty model for the side-effect-free close view smoke path.
+ */
+class CloseModel extends BaseDatabaseModel
+{
+}
