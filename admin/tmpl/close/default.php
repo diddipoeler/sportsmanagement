2794b8f85b89feb7abc15ea49645518cdf342795
diddipoeler <diddipoeler@gmx.de>
2026-08-17 15:40:15 +0200

    Add modern close template

---
+++ admin/tmpl/close/default.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @package     SportsManagement
+ * @subpackage  com_sportsmanagement
+ */
+
+\defined('_JEXEC') or die;
+
+// Intentionally empty. The Close view performs its work through an inline script.
