1491409cddba0271981ac2adabc95590656d7d61
diddipoeler <diddipoeler@gmx.de>
2026-08-17 15:19:55 +0200

    Migrate division controller namespace

---
+++ admin/src/Controller/DivisionController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @package     SportsManagement
+ * @subpackage  com_sportsmanagement
+ *
+ * Joomla 5/6 migration.
+ */
+
+namespace Diddipoeler\Component\SportsManagement\Administrator\Controller;
+
+\defined('_JEXEC') or die;
+
+/**
+ * Controller for editing a division.
+ */
+class DivisionController extends \JSMControllerForm
+{
+}
