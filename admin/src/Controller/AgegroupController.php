43017258d996e55175fc3d5e21828356ce06592f
diddipoeler <diddipoeler@gmx.de>
2026-08-17 15:19:35 +0200

    Migrate agegroup controller namespace

---
+++ admin/src/Controller/AgegroupController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @package     SportsManagement
+ * @subpackage  com_sportsmanagement
+ *
+ * Joomla 5/6 migration.
+ */
+
+namespace Diddipoeler\Component\SportsManagement\Administrator\Controller;
+
+\defined('_JEXEC') or die;
+
+/**
+ * Controller for editing an age group.
+ */
+class AgegroupController extends \JSMControllerForm
+{
+}
