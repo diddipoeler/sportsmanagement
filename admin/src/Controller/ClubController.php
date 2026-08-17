a41d51ac1a1febee98b2311b114f7dad8aae87e7
diddipoeler <diddipoeler@gmx.de>
2026-08-17 15:19:20 +0200

    Migrate club controller namespace

---
+++ admin/src/Controller/ClubController.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @package     SportsManagement
+ * @subpackage  com_sportsmanagement
+ *
+ * Joomla 5/6 migration.
+ */
+
+namespace Diddipoeler\Component\SportsManagement\Administrator\Controller;
+
+\defined('_JEXEC') or die;
+
+/**
+ * Controller for editing a club.
+ *
+ * Transitional controller: the legacy SportsManagement form base is retained
+ * until its shared save/redirect behaviour has been migrated to a namespaced
+ * base controller.
+ */
+class ClubController extends \JSMControllerForm
+{
+}
