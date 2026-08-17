03f58a3fdce2e6148f1318c3962724ec80933702
diddipoeler <diddipoeler@gmx.de>
2026-08-17 15:32:21 +0200

    Add namespaced agegroup model adapter

---
+++ admin/src/Model/AgegroupModel.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @package     SportsManagement
+ * @subpackage  com_sportsmanagement
+ *
+ * Joomla 5/6 migration adapter.
+ */
+
+namespace Diddipoeler\Component\SportsManagement\Administrator\Model;
+
+\defined('_JEXEC') or die;
+
+if (!class_exists('JSMModelAdmin')) {
+    \JLoader::import('components.com_sportsmanagement.libraries.sportsmanagement.model', JPATH_ADMINISTRATOR);
+}
+
+if (!class_exists('sportsmanagementModelagegroup')) {
+    \JLoader::import('components.com_sportsmanagement.models.agegroup', JPATH_ADMINISTRATOR);
+}
+
+/**
+ * Namespaced adapter for the existing Agegroup model.
+ */
+class AgegroupModel extends \sportsmanagementModelagegroup
+{
+}
