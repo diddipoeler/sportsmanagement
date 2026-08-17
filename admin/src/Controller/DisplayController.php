8f02f4249bdb19ff0dbf8dfb350f87a4ee0d2ee8
diddipoeler <diddipoeler@gmx.de>
2026-08-17 15:15:39 +0200

    Add namespaced administrator display controller

---
+++ admin/src/Controller/DisplayController.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @package     SportsManagement
+ * @subpackage  com_sportsmanagement
+ *
+ * Joomla 5/6 migration.
+ */
+
+namespace Diddipoeler\Component\SportsManagement\Administrator\Controller;
+
+\defined('_JEXEC') or die;
+
+use Joomla\CMS\Factory;
+use Joomla\CMS\MVC\Controller\BaseController;
+
+/**
+ * Default administrator controller for the modern Joomla dispatcher.
+ */
+final class DisplayController extends BaseController
+{
+    protected $default_view = 'cpanel';
+
+    public function display($cachable = false, $urlparams = []): BaseController
+    {
+        $input = Factory::getApplication()->getInput();
+        $input->set('view', $input->getCmd('view', $this->default_view));
+
+        return parent::display($cachable, $urlparams);
+    }
+}
