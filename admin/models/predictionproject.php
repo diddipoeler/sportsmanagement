649c68e497c4d36f125fdbebce15ee3038c9f755
diddipoeler <diddipoeler@gmx.de>
2026-08-18 19:11:02 +0200

    Modernize prediction project model

--- admin/models/predictionproject.php
+++ admin/models/predictionproject.php
@@ -14,11 +14,9 @@
 
 defined('_JEXEC') or die('Restricted access');
 
-use Joomla\CMS\Language\Text;
 use Joomla\CMS\Factory;
-use Joomla\CMS\Component\ComponentHelper;
-use Joomla\CMS\Table\Table;
 use Joomla\CMS\MVC\Model\AdminModel;
+use Joomla\CMS\Table\Table;
 use Joomla\Registry\Registry;
 
 /**
@@ -33,22 +31,20 @@ use Joomla\Registry\Registry;
 class sportsmanagementModelpredictionproject extends AdminModel
 {
 	/**
-	 * Method to get the record form.
+	 * Get the record form.
 	 *
-	 * @param   array    $data      Data for the form.
-	 * @param   boolean  $loadData  True if the form is to load its own data (default case), false if not.
+	 * @param array   $data     Form data.
+	 * @param boolean $loadData Whether to load stored data.
 	 *
-	 * @return mixed    A JForm object on success, false on failure
-	 * @since  1.6
+	 * @return mixed
 	 */
 	public function getForm($data = array(), $loadData = true)
 	{
-		$app                  = Factory::getApplication();
-		$option               = Factory::getApplication()->input->getCmd('option');
-		$cfg_which_media_tool = ComponentHelper::getParams($option)->get('cfg_which_media_tool', 0);
-
-		// Get the form.
-		$form = $this->loadForm('com_sportsmanagement.predictionproject', 'predictionproject', array('control' => 'jform', 'load_data' => $loadData));
+		$form = $this->loadForm(
+			'com_sportsmanagement.predictionproject',
+			'predictionproject',
+			array('control' => 'jform', 'load_data' => $loadData)
+		);
 
 		if (empty($form))
 		{
@@ -74,9 +70,9 @@ class sportsmanagementModelpredictionproject extends AdminModel
 	}
 
 	/**
-	 * Method to get the script that have to be included on the form
+	 * Return the form script path.
 	 *
-	 * @return string    Script files
+	 * @return string
 	 */
 	public function getScript()
 	{
@@ -84,24 +80,27 @@ class sportsmanagementModelpredictionproject extends AdminModel
 	}
 
 	/**
-	 * Method to save item order
+	 * Save item order.
 	 *
-	 * @access public
-	 * @return boolean    True on success
-	 * @since  1.5
+	 * @param array|null $pks   Record IDs.
+	 * @param array|null $order Ordering values.
+	 *
+	 * @return boolean True on success.
 	 */
-	function saveorder($pks = null, $order = null)
+	public function saveorder($pks = null, $order = null)
 	{
-		$row =& $this->getTable();
+		$pks   = (array) $pks;
+		$order = (array) $order;
+		$row   = $this->getTable();
+		$count = min(count($pks), count($order));
 
-		// Update ordering values
-		for ($i = 0; $i < count($pks); $i++)
+		for ($i = 0; $i < $count; $i++)
 		{
 			$row->load((int) $pks[$i]);
 
 			if ($row->ordering != $order[$i])
 			{
-				$row->ordering = $order[$i];
+				$row->ordering = (int) $order[$i];
 
 				if (!$row->store())
 				{
@@ -114,14 +113,13 @@ class sportsmanagementModelpredictionproject extends AdminModel
 	}
 
 	/**
-	 * Returns a reference to the a Table object, always creating it.
+	 * Return the prediction project table.
 	 *
-	 * @param   type    The table type to instantiate
-	 * @param   string    A prefix for the table class name. Optional.
-	 * @param   array    Configuration array for model. Optional.
+	 * @param string $type   Table type.
+	 * @param string $prefix Table class prefix.
+	 * @param array  $config Table configuration.
 	 *
-	 * @return JTable    A database object
-	 * @since  1.6
+	 * @return Table
 	 */
 	public function getTable($type = 'predictionproject', $prefix = 'sportsmanagementTable', $config = array())
 	{
@@ -131,130 +129,132 @@ class sportsmanagementModelpredictionproject extends AdminModel
 	}
 
 	/**
-	 * Method to save the form data.
+	 * Save the form data.
 	 *
-	 * @param   array    The form data.
+	 * @param array $data Form data.
 	 *
-	 * @return boolean    True on success.
-	 * @since  1.6
+	 * @return boolean True on success.
 	 */
 	public function save($data)
 	{
-		$app    = Factory::getApplication();
-		$option = Factory::getApplication()->input->getCmd('option');
-		$date   = Factory::getDate();
-		$user   = Factory::getUser();
-		$post   = Factory::getApplication()->input->post->getArray(array());
+		$app   = Factory::getApplication();
+		$input = $app->getInput();
+		$date  = Factory::getDate();
+		$user  = $app->getIdentity();
+		$post  = $input->post->getArray(array());
 
-		// Set the values
 		$data['modified']    = $date->toSql();
-		$data['modified_by'] = $user->get('id');
+		$data['modified_by'] = $user->id;
 
 		if (isset($post['extended']) && is_array($post['extended']))
 		{
-			// Convert the extended field to a string.
 			$parameter = new Registry;
 			$parameter->loadArray($post['extended']);
 			$data['extended'] = (string) $parameter;
 		}
-		// special case final4 Club IDs, save as CSV list
-		if (!isset($data['league_final4'])) {
-			$data['league_final4'] = "0";
-		} else {
-			if (is_array($data['league_final4'])) {
-				$data['league_final4'] = implode(",", $data['league_final4']);
-			}
+
+		if (!isset($data['league_final4']))
+		{
+			$data['league_final4'] = '0';
 		}
-		// Proceed with the save
+		elseif (is_array($data['league_final4']))
+		{
+			$data['league_final4'] = implode(',', array_map('intval', $data['league_final4']));
+		}
+
 		return parent::save($data);
 	}
 
 	/**
-	 * sportsmanagementModelpredictionproject::delete()
+	 * Delete prediction projects.
 	 *
-	 * @param   mixed  $pks
+	 * @param array $pks Record IDs.
 	 *
-	 * @return
+	 * @return boolean
 	 */
 	public function delete(&$pks)
 	{
-		$app = Factory::getApplication();
-
 		return parent::delete($pks);
 	}
 
 	/**
-	 * Method override to check if you can edit an existing record.
+	 * Check whether an existing record can be edited.
 	 *
-	 * @param   array   $data  An array of input data.
-	 * @param   string  $key   The name of the key for the primary key.
+	 * @param array  $data Input data.
+	 * @param string $key  Primary key field.
 	 *
 	 * @return boolean
-	 * @since  1.6
 	 */
 	protected function allowEdit($data = array(), $key = 'id')
 	{
-		// Check specific edit permission then general edit permission.
-		return Factory::getUser()->authorise('core.edit', 'com_sportsmanagement.message.' . ((int) isset($data[$key]) ? $data[$key] : 0)) || parent::allowEdit($data, $key);
+		$id = (int) ($data[$key] ?? 0);
+
+		return Factory::getApplication()->getIdentity()->authorise(
+			'core.edit',
+			'com_sportsmanagement.message.' . $id
+		) || parent::allowEdit($data, $key);
 	}
 
 	/**
-	 * Method to get the data that should be injected in the form.
+	 * Get form data from the session or current item.
 	 *
-	 * @return mixed    The data for the form.
-	 * @since  1.6
+	 * @return mixed
 	 */
 	protected function loadFormData()
 	{
-		// Check the session for previously entered form data.
 		$data = Factory::getApplication()->getUserState('com_sportsmanagement.edit.predictionproject.data', array());
 
 		if (empty($data))
 		{
 			$data = $this->getItem();
-			// Prepare Multiple selection list for final 4 which was stored as CSV string
-			$data->league_final4 = explode(",", $data->league_final4);	
+
+			if ($data && isset($data->league_final4))
+			{
+				$value = trim((string) $data->league_final4);
+				$data->league_final4 = $value === '' ? array() : explode(',', $value);
+			}
 		}
 
 		return $data;
 	}
 
 	/**
-	 * Method to return a prediction project object
+	 * Return a prediction project object.
 	 *
-	 * @access public
-	 * @return array
-	 * @since  0.1
+	 * @param int $prediction_id Prediction project ID.
+	 *
+	 * @return object|false
 	 */
-	function getPredictionProject($prediction_id = 0)
+	public function getPredictionProject($prediction_id = 0)
 	{
-		$db     = Factory::getDbo();
-		$query  = $db->getQuery(true);
-		if ($prediction_id)
-		{
-			// Select some fields
-			$query->clear();
-			$query->select('*');
-			$query->from('#__sportsmanagement_prediction_project');
-			$query->where('prediction_id = ' . $prediction_id);
+		$predictionId = (int) $prediction_id;
 
-			$db->setQuery($query);
-
-			if (!$result = $db->loadObject())
-			{
-				sportsmanagementModeldatabasetool::writeErrorLog(get_class($this), __FUNCTION__, __FILE__, $db->getErrorMsg(), __LINE__);
-
-				return false;
-			}
-			else
-			{
-				return $result;
-			}
-		}
-		else
+		if (!$predictionId)
 		{
 			return false;
 		}
 
+		$db    = $this->getDatabase();
+		$query = $db->getQuery(true)
+			->select('*')
+			->from($db->quoteName('#__sportsmanagement_prediction_project'))
+			->where($db->quoteName('prediction_id') . ' = ' . $predictionId);
+
+		try
+		{
+			$db->setQuery($query);
+			return $db->loadObject() ?: false;
+		}
+		catch (RuntimeException $e)
+		{
+			sportsmanagementModeldatabasetool::writeErrorLog(
+				get_class($this),
+				__FUNCTION__,
+				__FILE__,
+				$e->getMessage(),
+				__LINE__
+			);
+			return false;
+		}
 	}
 }
