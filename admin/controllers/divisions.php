275e4c2133af562a6ce867aec37047722f87068b
diddipoeler <diddipoeler@gmx.de>
2026-08-18 21:04:00 +0200

    Bridge legacy divisions controller to native implementation

--- admin/controllers/divisions.php
+++ admin/controllers/divisions.php
@@ -1,119 +1,19 @@
 <?php
 /**
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage controllers
- * @file       divisions.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\Factory;
-use Joomla\CMS\Session\Session;
-
-/**
- * sportsmanagementControllerdivisions
+ * SportsManagement legacy compatibility bridge.
  *
- * @package
- * @author
- * @copyright diddi
- * @version   2014
- * @access    public
+ * The active Joomla 5/6 implementation lives in admin/src/Controller/DivisionsController.php.
  */
-class sportsmanagementControllerdivisions extends JSMControllerAdmin
-{
 
-	/**
-	 * Constructor.
-	 *
-	 * @param   array An optional associative array of configuration settings.
-	 *
-	 * @see   JController
-	 * @since 1.6
-	 */
-	public function __construct($config = array())
-	{
-		parent::__construct($config);
-		$this->app    = Factory::getApplication();
-		$this->jinput = $this->app->input;
-		$this->option = $this->jinput->getCmd('option');
-	}
-    
-    /**
-     * sportsmanagementControllerdivisions::cancel()
-     * 
-     * @return void
-     */
-    function cancel()
-	{
-		$msg = '';
-		$this->setRedirect('index.php?option=com_sportsmanagement&view=close&tmpl=component', $msg);
-	}
-    
-    /**
-     * sportsmanagementControllerdivisions::massadd()
-     * 
-     * @return void
-     */
-    function massadd()
-	{
-		Session::checkToken() or jexit(\Text::_('JINVALID_TOKEN'));
-		$model = $this->getModel();
-		$msg   = $model->massadd();
-		$this->setRedirect('index.php?option=com_sportsmanagement&view=close&tmpl=component', $msg);
-	}
+defined('_JEXEC') or die('Restricted access');
 
-	/**
-	 * sportsmanagementControllerdivisions::divisiontoproject()
-	 * 
-	 * @return void
-	 */
-	function divisiontoproject()
-	{
-		$model = $this->getModel();
-		$msg   = $model->divisiontoproject();
-		$this->setRedirect('index.php?option=com_sportsmanagement&view=divisions&pid=' . $this->project_id, $msg);
-	}
+use Diddipoeler\Component\SportsManagement\Administrator\Controller\DivisionsController;
 
-	/**
-	 * Proxy for getModel.
-	 *
-	 * @since 1.6
-	 */
-	public function getModel($name = 'Division', $prefix = 'sportsmanagementModel', $config = Array())
-	{
-		$model = parent::getModel($name, $prefix, array('ignore_request' => true));
+if (!class_exists(DivisionsController::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Controller/SportsManagementAdminController.php';
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Controller/DivisionsController.php';
+}
 
-		return $model;
-	}
-
-	/**
-	 * sportsmanagementControllerdivisions::saveOrder()
-	 *
-	 * @return void
-	 */
-	public function saveOrder()
-	{
-		$this->project_id = $this->app->getUserState("$this->option.pid", '0');
-		$model = $this->getModel();
-		$pks   = $this->jinput->get('cid', array(), 'array');
-		$order = $this->jinput->get('order', array(), 'array');
-		$msg = $model->saveorder($pks, $order);
-		$this->setRedirect('index.php?option=com_sportsmanagement&view=divisions&pid=' . $this->project_id, $msg);
-	}
-
-	/**
-	 * sportsmanagementControllerdivisions::saveshort()
-	 *
-	 * @return void
-	 */
-	function saveshort()
-	{
-		$this->project_id = $this->app->getUserState("$this->option.pid", '0');
-		$model            = $this->getModel();
-		$msg              = $model->saveshort();
-		$this->setRedirect('index.php?option=com_sportsmanagement&view=divisions&pid=' . $this->project_id, $msg);
-	}
+if (!class_exists('sportsmanagementControllerdivisions', false)) {
+    class_alias(DivisionsController::class, 'sportsmanagementControllerdivisions');
 }
