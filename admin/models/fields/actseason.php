02c7da23c42871ec76cebfef9d0a084c786c2370
diddipoeler <diddipoeler@gmx.de>
2026-08-18 18:16:09 +0200

    Modernize active season form field for Joomla 5 and 6

--- admin/models/fields/actseason.php
+++ admin/models/fields/actseason.php
@@ -10,12 +10,10 @@
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\Factory;
-use Joomla\CMS\Form\FormField;
-use Joomla\CMS\Form\FormHelper;
 
-jimport('joomla.filesystem.folder');
-FormHelper::loadFieldClass('list');
+use Joomla\CMS\Factory;
+use Joomla\CMS\Form\Field\ListField;
+use Joomla\Database\DatabaseInterface;
 
 /**
  * FormFieldactseason
@@ -26,31 +24,25 @@ FormHelper::loadFieldClass('list');
  * @version   2014
  * @access    public
  */
-class JFormFieldactseason extends \JFormFieldList
+class JFormFieldactseason extends ListField
 {
 	protected $type = 'actseason';
 
 	/**
 	 * FormFieldactseason::getOptions()
 	 *
-	 * @return
+	 * @return array
 	 */
 	protected function getOptions()
 	{
-		$options = array();
-		$db    = Factory::getDbo();
-		$query = $db->getQuery(true);
+		$db    = Factory::getContainer()->get(DatabaseInterface::class);
+		$query = $db->createQuery();
 		$query->select('s.id AS value, s.name AS text');
-		$query->from('#__sportsmanagement_season as s');
+		$query->from('#__sportsmanagement_season AS s');
 		$query->order('s.name');
 		$db->setQuery($query);
 		$options = $db->loadObjectList();
 
-		/** Merge any additional options in the XML definition. */
-		$options = array_merge(parent::getOptions(), $options);
-
-		return $options;
+		return array_merge(parent::getOptions(), $options);
 	}
-
-
 }
