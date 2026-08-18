6882576d93ba59da9053f4c88dd0a7e29cb3efb8
diddipoeler <diddipoeler@gmx.de>
2026-08-18 21:23:36 +0200

    Bridge legacy sportstype controller to native implementation

--- admin/controllers/sportstype.php
+++ admin/controllers/sportstype.php
@@ -1,39 +1,19 @@
 <?php
 /**
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage controllers
- * @file       sportstype.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * SportsManagement legacy compatibility bridge.
+ *
+ * The active Joomla 5/6 implementation lives in admin/src/Controller/SportstypeController.php.
  */
+
 defined('_JEXEC') or die('Restricted access');
 
-/**
- * sportsmanagementControllersportstype
- *
- * @package
- * @author
- * @copyright diddi
- * @version   2014
- * @access    public
- */
-class sportsmanagementControllersportstype extends JSMControllerForm
-{
+use Diddipoeler\Component\SportsManagement\Administrator\Controller\SportstypeController;
 
-	/**
-	 * Class Constructor
-	 *
-	 * @param   array  $config  An optional associative array of configuration settings.
-	 *
-	 * @return void
-	 * @since  1.5
-	 */
-	function __construct($config = array())
-	{
-		parent::__construct($config);
-	}
+if (!class_exists(SportstypeController::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Controller/SportsManagementFormController.php';
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Controller/SportstypeController.php';
+}
 
+if (!class_exists('sportsmanagementControllersportstype', false)) {
+    class_alias(SportstypeController::class, 'sportsmanagementControllersportstype');
 }
