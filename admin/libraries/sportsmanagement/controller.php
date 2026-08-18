1dd4174bccc26aa3285a59d14266eac5c64d1eae
diddipoeler <diddipoeler@gmx.de>
2026-08-18 18:51:13 +0200

    Modernize legacy administrator controller bridge

--- admin/libraries/sportsmanagement/controller.php
+++ admin/libraries/sportsmanagement/controller.php
@@ -10,15 +10,16 @@
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 defined('_JEXEC') or die('Restricted access');
+
+use Joomla\CMS\Component\ComponentHelper;
 use Joomla\CMS\Factory;
 use Joomla\CMS\Language\Text;
+use Joomla\CMS\Log\Log;
 use Joomla\CMS\MVC\Controller\AdminController;
 use Joomla\CMS\MVC\Controller\FormController;
 use Joomla\CMS\MVC\Model\BaseDatabaseModel;
-use Joomla\CMS\Component\ComponentHelper;
-use Joomla\CMS\Session\Session;
 use Joomla\CMS\Router\Route;
-use Joomla\CMS\Log\Log;
+use Joomla\CMS\Session\Session;
 
 /**
  * JSMControllerAdmin
@@ -31,44 +32,51 @@ use Joomla\CMS\Log\Log;
  */
 class JSMControllerAdmin extends AdminController
 {
-	var $team_club_id = 0;
+	public $team_club_id = 0;
+	public $jsmapp;
+	public $jsmjinput;
+	public $jsmoption;
 
 	/**
 	 * Constructor.
 	 *
-	 * @param   array An optional associative array of configuration settings.
+	 * @param   array  $config  An optional associative array of configuration settings.
 	 *
 	 * @throws Exception
 	 * @since  1.6
-	 * @see    JController
 	 */
 	public function __construct($config = array())
 	{
 		parent::__construct($config);
+
 		$this->jsmapp    = Factory::getApplication();
-		$this->jsmjinput = $this->jsmapp->input;
+		$this->jsmjinput = $this->jsmapp->getInput();
 		$this->jsmoption = $this->jsmjinput->getCmd('option');
-if ( Factory::getConfig()->get('debug') )
-{  
-Log::add(Text::_(__METHOD__ . ' ' . __LINE__ . ' layout' . $this->jsmjinput->getVar('layout')), Log::NOTICE, 'jsmerror');
-}
+
+		if (Factory::getConfig()->get('debug'))
+		{
+			Log::add(
+				Text::_(__METHOD__ . ' ' . __LINE__ . ' layout' . $this->jsmjinput->getVar('layout')),
+				Log::NOTICE,
+				'jsmerror'
+			);
+		}
 	}
-    
-    /**
+
+	/**
 	 * Method to save the submitted ordering values for records.
 	 *
-	 * Overrides JControllerAdmin::saveorder to check the core.admin permission.
+	 * Overrides AdminController::saveorder to check the core.admin permission.
 	 *
-	 * @return  boolean  True on success
+	 * @return boolean True on success
 	 *
-	 * @since   1.6
+	 * @since 1.6
 	 */
 	public function saveorder()
 	{
-		if (!Factory::getUser()->authorise('core.admin', $this->option))
+		if (!$this->jsmapp->getIdentity()->authorise('core.admin', $this->option))
 		{
-			JError::raiseError(500, Text::_('JERROR_ALERTNOAUTHOR'));
-			jexit();
+			throw new RuntimeException(Text::_('JERROR_ALERTNOAUTHOR'), 403);
 		}
 
 		return parent::saveorder();
@@ -79,14 +87,11 @@ Log::add(Text::_(__METHOD__ . ' ' . __LINE__ . ' layout' . $this->jsmjinput->get
 	 *
 	 * @return void
 	 */
-	function cancel()
+	public function cancel()
 	{
-		$msg = '';
-		$this->setRedirect('index.php?option=com_sportsmanagement&view=close&tmpl=component', $msg);
+		$this->setRedirect('index.php?option=com_sportsmanagement&view=close&tmpl=component');
 	}
-
 }
-
 
 /**
  * JSMControllerForm
@@ -99,53 +104,47 @@ Log::add(Text::_(__METHOD__ . ' ' . __LINE__ . ' layout' . $this->jsmjinput->get
  */
 class JSMControllerForm extends FormController
 {
-	var $team_club_id = 0;
+	public $team_club_id = 0;
+	public $jsmdb;
+	public $jsmapp;
+	public $jsmjinput;
+	public $jsmoption;
+	public $jsmdocument;
+	public $jsmuser;
+	public $jsmdate;
+	public $club_id = 0;
+	public $person_id = 0;
+	public $team_id = 0;
+	public $insert_id = 0;
 
 	/**
 	 * Class Constructor
 	 *
 	 * @param   array  $config  An optional associative array of configuration settings.
 	 *
-	 * @return void
 	 * @throws Exception
 	 * @since  1.5
 	 */
-	function __construct($config = array())
+	public function __construct($config = array())
 	{
 		parent::__construct($config);
+
 		$this->jsmdb = sportsmanagementHelper::getDBConnection();
-
-		// Reference global application object
 		$this->jsmapp = Factory::getApplication();
-
-		// JInput object
-		$this->jsmjinput    = $this->jsmapp->input;
-		$this->jsmoption    = $this->jsmjinput->getCmd('option');
-		$this->team_club_id = $this->jsmapp->getUserState("$this->jsmoption.club_id", '0');
-		$this->jsmdocument  = Factory::getDocument();
-		$this->jsmuser      = Factory::getUser();
-		$this->jsmdate      = Factory::getDate();
-
-		//      if ( $this->view_list == 'people' )
-		//      {
-		//      $this->view_list == 'persons' ;
-		//      }
-		//      $this->jsmapp->enqueueMessage(Text::_(__METHOD__.' '.__LINE__.' '.$this->view_list), '');
-		//      $this->jsmapp->enqueueMessage(Text::_(__METHOD__.' '.__LINE__.' '.$this->view_item), '');
-		/**
-		 *
-		 * Map the apply task to the save method.
-		 */
-		// $this->registerTask('apply', 'save');
+		$this->jsmjinput = $this->jsmapp->getInput();
+		$this->jsmoption = $this->jsmjinput->getCmd('option');
+		$this->team_club_id = $this->jsmapp->getUserState($this->jsmoption . '.club_id', '0');
+		$this->jsmdocument = $this->jsmapp->getDocument();
+		$this->jsmuser = $this->jsmapp->getIdentity();
+		$this->jsmdate = Factory::getDate();
 	}
-
 
 	/**
 	 * JSMControllerForm::import()
 	 *
 	 * @return void
 	 */
-	function import()
+	public function import()
 	{
 		$message = Text::_('COM_SPORTSMANAGEMENT_GLOBAL_NO_IMPORT');
 		$this->setRedirect(Route::_('index.php?option=' . $this->option . '&view=' . $this->view_list, false), $message);
@@ -156,7 +155,7 @@ class JSMControllerForm extends FormController
 	 *
 	 * @return void
 	 */
-	function export()
+	public function export()
 	{
 		$message = Text::_('COM_SPORTSMANAGEMENT_GLOBAL_NO_EXPORT');
 		$this->setRedirect(Route::_('index.php?option=' . $this->option . '&view=' . $this->view_list, false), $message);
@@ -165,126 +164,113 @@ class JSMControllerForm extends FormController
 	/**
 	 * JSMControllerForm::cancelmodal()
 	 *
-	 * @param   mixed  $key
+	 * @param mixed $key
 	 *
 	 * @return void
 	 */
-	function cancelmodal($key = null)
+	public function cancelmodal($key = null)
 	{
-		$msg = '';
-		$this->setRedirect('index.php?option=com_sportsmanagement&view=close&tmpl=component', $msg);
+		$this->setRedirect('index.php?option=com_sportsmanagement&view=close&tmpl=component');
 	}
 
 	/**
 	 * JSMControllerForm::save()
 	 *
-	 * @param   mixed  $key
-	 * @param   mixed  $urlVar
+	 * @param mixed $key
+	 * @param mixed $urlVar
 	 *
 	 * @return boolean
 	 */
-	function save($key = null, $urlVar = null)
+	public function save($key = null, $urlVar = null)
 	{
-		// Check for request forgeries.
-		Session::checkToken() or jexit(Text::_('JINVALID_TOKEN'));
+		if (!Session::checkToken())
+		{
+			throw new RuntimeException(Text::_('JINVALID_TOKEN'), 403);
+		}
 
-				
-		// Initialise variables.
-		$post            = $this->jsmjinput->post->getArray();
-		$tmpl            = $this->jsmjinput->getVar('tmpl');
-		$model           = $this->getModel($this->view_item);
-		$data            = $this->jsmjinput->getVar('jform', array(), 'post', 'array');
-		$setRedirect     = '';
-		$createTeam      = $this->jsmjinput->getVar('createTeam');
-		
-		//$this->jsmapp->enqueueMessage(Text::_(__METHOD__.' '.__LINE__.' task '.$this->jsmjinput->get('task')), '');
-		//$this->jsmapp->enqueueMessage(Text::_(__METHOD__.' '.__LINE__.' view_list '.$this->view_list), '');
-		//$this->jsmapp->enqueueMessage(Text::_(__METHOD__.' '.__LINE__.' view_item '.$this->view_item), '');
-		//$this->jsmapp->enqueueMessage(Text::_(__METHOD__.' '.__LINE__.'data<pre>'.print_r($data,true).'</pre>'), '');
-		
+		$post        = $this->jsmjinput->post->getArray();
+		$tmpl        = $this->jsmjinput->getVar('tmpl');
+		$model       = $this->getModel($this->view_item);
+		$data        = $this->jsmjinput->getVar('jform', array(), 'post', 'array');
+		$setRedirect = '';
+		$createTeam  = $this->jsmjinput->getVar('createTeam');
+
 		if (version_compare(substr(JVERSION, 0, 3), '4.0', 'ge'))
-{
-		
+		{
 			switch ($this->view_item)
 			{
-			case 'round':
-			switch ($this->jsmjinput->get('task'))
-			{
-			case 'save':
-			if ( !$data )
-			{
-				$data['round_date_first'] = '0000-00-00';
-				$data['round_date_last'] = '0000-00-00';
-			}	
-			break;
-			}
-				
-				
-				
-			break;
+				case 'round':
+					switch ($this->jsmjinput->get('task'))
+					{
+						case 'save':
+							if (!$data)
+							{
+								$data['round_date_first'] = '0000-00-00';
+								$data['round_date_last'] = '0000-00-00';
+							}
+							break;
+					}
+					break;
 			}
 		}
-		
-		
-		$return          = $model->save($data);
-		
-		$this->jsmapp->enqueueMessage($model->getError(), 'error');
-		
-		$this->club_id   = $this->jsmapp->getUserState("$this->jsmoption.club_id", '0');
-		$this->person_id = $this->jsmapp->getUserState("$this->jsmoption.person_id", '0');
-		$this->team_id   = $this->jsmapp->getUserState("$this->jsmoption.team_id", '0');
-$this->insert_id   =      $this->jsmjinput->getInt('insert_id');
-//Log::add(Text::_(__METHOD__ . ' ' . __LINE__ . ' ' . 'post<pre>'.print_r($post,true).'</pre>'), Log::NOTICE, 'jsmerror');
-//Log::add(Text::_(__METHOD__ . ' ' . __LINE__ . ' ' . 'data<pre>'.print_r($data,true).'</pre>'), Log::NOTICE, 'jsmerror');
-		
-		$id = $this->insert_id ? $this->insert_id : $data['id'];
 
-		if ( empty($data['id']) )
+		$return = $model->save($data);
+		$modelError = $model->getError();
+
+		if ($modelError)
+		{
+			$this->jsmapp->enqueueMessage($modelError, 'error');
+		}
+
+		$this->club_id = $this->jsmapp->getUserState($this->jsmoption . '.club_id', '0');
+		$this->person_id = $this->jsmapp->getUserState($this->jsmoption . '.person_id', '0');
+		$this->team_id = $this->jsmapp->getUserState($this->jsmoption . '.team_id', '0');
+		$this->insert_id = $this->jsmjinput->getInt('insert_id');
+
+		$id = $this->insert_id ? $this->insert_id : ($data['id'] ?? 0);
+
+		if (empty($data['id']))
 		{
 			$id = $this->jsmjinput->getInt('insert_id');
 		}
 
-		if (ComponentHelper::getParams($this->jsmoption)->get('show_debug_info_backend'))
-		{
-		}
-
-		// $this->jsmapp->enqueueMessage(Text::_(__METHOD__.' '.__LINE__.' return '.$return), '');
-		// $this->jsmapp->enqueueMessage(Text::_(__METHOD__.' '.__LINE__.' getRedirectToItemAppend '.$this->getRedirectToItemAppend($id)), '');
 		if ($return)
 		{
 			switch ($this->view_item)
 			{
 				case 'club':
-
 					if ($createTeam)
 					{
-						$mdlTeam         = BaseDatabaseModel::getInstance("team", "sportsmanagementModel");
-						$team_name       = $data['name'];
+						$mdlTeam = BaseDatabaseModel::getInstance('team', 'sportsmanagementModel');
+						$team_name = $data['name'];
 						$team_short_name = strtoupper(substr(preg_replace('/[^a-zA-Z]/', '', $team_name), 0, 3));
 
-						$tpost['id']         = 0;
-						$tpost['name']       = $team_name;
+						$tpost['id'] = 0;
+						$tpost['name'] = $team_name;
 						$tpost['short_name'] = $team_short_name;
-						$tpost['club_id']    = $this->club_id;
+						$tpost['club_id'] = $this->club_id;
 						$mdlTeam->save($tpost);
 					}
 					break;
+
 				case 'rounds':
 					$setRedirect = '&pid=' . $post['pid'];
 					break;
+
 				case 'projects':
 					$setRedirect = '&pid=' . $id;
 					break;
+
 				case 'project':
 					$id = $this->jsmjinput->getInt('insert_project_id');
 					$setRedirect = '&pid=' . $id;
 					break;
+
 				case 'projectteam':
 					$setRedirect = '&pid=' . $data['project_id'];
 					break;
 			}
 
-			// Set the redirect based on the task.
 			switch ($this->getTask())
 			{
 				case 'apply':
@@ -297,6 +283,7 @@ $this->insert_id   =      $this->jsmjinput->getInt('insert_id');
 							case 'club':
 								$this->setRedirect('index.php?option=com_sportsmanagement&view=' . $this->view_item . '&layout=edit&tmpl=component&id=' . $this->club_id, $message);
 								break;
+
 							default:
 								$this->setRedirect('index.php?option=com_sportsmanagement&view=' . $this->view_item . '&layout=edit&tmpl=component&id=' . $id, $message);
 								break;
@@ -309,18 +296,22 @@ $this->insert_id   =      $this->jsmjinput->getInt('insert_id');
 							case 'club':
 								$this->setRedirect(Route::_('index.php?option=' . $this->option . '&view=' . $this->view_item . $this->getRedirectToItemAppend($this->club_id) . $setRedirect, false), $message);
 								break;
+
 							case 'player':
 								$this->setRedirect(Route::_('index.php?option=' . $this->option . '&view=' . $this->view_item . $this->getRedirectToItemAppend($this->person_id) . $setRedirect, false), $message);
 								break;
+
 							case 'team':
 								$this->setRedirect(Route::_('index.php?option=' . $this->option . '&view=' . $this->view_item . '&club_id=' . $this->team_club_id . $this->getRedirectToItemAppend($this->team_id) . $setRedirect, false), $message);
 								break;
+
 							default:
 								$this->setRedirect(Route::_('index.php?option=' . $this->option . '&view=' . $this->view_item . $this->getRedirectToItemAppend($id) . $setRedirect, false), $message);
 								break;
 						}
 					}
 					break;
+
 				case 'save2copy':
 					$this->setRedirect(Route::_('index.php?option=' . $this->option . '&view=' . $this->view_item . $this->getRedirectToItemAppend($id) . $setRedirect, false));
 					break;
@@ -328,8 +319,8 @@ $this->insert_id   =      $this->jsmjinput->getInt('insert_id');
 				case 'save2new':
 					$message = Text::_('JLIB_APPLICATION_SAVE_SUCCESS');
 					$this->setRedirect(Route::_('index.php?option=' . $this->option . '&view=' . $this->view_item . $this->getRedirectToItemAppend(null, $urlVar) . $setRedirect, false), $message);
-
 					break;
+
 				default:
 					$message = Text::_('JLIB_APPLICATION_SAVE_SUCCESS');
 
@@ -344,9 +335,11 @@ $this->insert_id   =      $this->jsmjinput->getInt('insert_id');
 							case 'club':
 								$this->setRedirect(Route::_('index.php?option=' . $this->option . '&view=' . $this->view_list . '&club_id=' . $this->club_id . $this->getRedirectToListAppend(), false), $message);
 								break;
+
 							case 'team':
 								$this->setRedirect(Route::_('index.php?option=' . $this->option . '&view=' . $this->view_list . '&club_id=' . $this->team_club_id . '&team_id=' . $this->team_id . $this->getRedirectToListAppend(), false), $message);
 								break;
+
 							default:
 								$this->setRedirect(Route::_('index.php?option=' . $this->option . '&view=' . $this->view_list . $this->getRedirectToListAppend() . $setRedirect, false), $message);
 								break;
@@ -357,20 +350,19 @@ $this->insert_id   =      $this->jsmjinput->getInt('insert_id');
 
 			return true;
 		}
-		else
-		{
-			$this->setRedirect(Route::_('index.php?option=' . $this->option . '&view=' . $this->view_item . $this->getRedirectToItemAppend($id) . $setRedirect, false), $message);
 
-			return false;
-		}
+		$message = $modelError ?: Text::_('JLIB_APPLICATION_ERROR_SAVE_FAILED');
+		$this->setRedirect(Route::_('index.php?option=' . $this->option . '&view=' . $this->view_item . $this->getRedirectToItemAppend($id) . $setRedirect, false), $message, 'error');
+
+		return false;
 	}
 
 	/**
 	 * Function that allows child controller access to model data after the data
 	 * has been saved.
 	 *
-	 * @param   BaseDatabaseModel  $model      The data model object.
-	 * @param   array              $validData  The validated data.
+	 * @param BaseDatabaseModel $model The data model object.
+	 * @param array $validData The validated data.
 	 *
 	 * @return void
 	 */
@@ -378,5 +370,4 @@ $this->insert_id   =      $this->jsmjinput->getInt('insert_id');
 	{
 		return;
 	}
-
 }
