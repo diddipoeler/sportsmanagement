ffcd221182be3e6f4b16877fce0032266d7dcccc
diddipoeler <diddipoeler@gmx.de>
2026-08-18 22:34:20 +0200

    Modernize legacy prediction rules context

--- site/models/predictionrules.php
+++ site/models/predictionrules.php
@@ -1,71 +1,41 @@
 <?php
 /**
- *
  * SportsManagement ein Programm zur Verwaltung für alle Sportarten
  *
  * @version    1.0.05
  * @package    Sportsmanagement
  * @subpackage predictionrules
- * @file       predictionrules.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
-
 
 defined('_JEXEC') or die('Restricted access');
 
 use Joomla\CMS\Factory;
 use Joomla\CMS\MVC\Model\BaseDatabaseModel;
 
-
-/**
- * sportsmanagementModelPredictionRules
- *
- * @package
- * @author
- * @copyright diddi
- * @version   2014
- * @access    public
- */
 class sportsmanagementModelPredictionRules extends BaseDatabaseModel
 {
-	/**
-	 * sportsmanagementModelPredictionRules::__construct()
-	 *
-	 * @return
-	 */
-	function __construct()
-	{
-		// Reference global application object
-		$app = Factory::getApplication();
+    public function __construct()
+    {
+        parent::__construct();
 
-		// JInput object
-		$jinput = $app->input;
-		$option = $jinput->getCmd('option');
+        $input = Factory::getApplication()->getInput();
+        $roundId = $input->getInt('r', 0);
 
-		$prediction = new sportsmanagementModelPrediction;
+        new sportsmanagementModelPrediction();
 
-		sportsmanagementModelPrediction::$roundID = $jinput->getVar('r', '0');
-		sportsmanagementModelPrediction::$pjID    = $jinput->getVar('pj', '0');
-		sportsmanagementModelPrediction::$from    = $jinput->getVar('from', $jinput->getVar('r', '0'));
-		sportsmanagementModelPrediction::$to      = $jinput->getVar('to', $jinput->getVar('r', '0'));
-
-		sportsmanagementModelPrediction::$predictionGameID = $jinput->getVar('prediction_id', '0');
-
-		sportsmanagementModelPrediction::$predictionMemberID = $jinput->getInt('uid', 0);
-		sportsmanagementModelPrediction::$joomlaUserID       = $jinput->getInt('juid', 0);
-
-		sportsmanagementModelPrediction::$pggroup     = $jinput->getInt('pggroup', 0);
-		sportsmanagementModelPrediction::$pggrouprank = $jinput->getInt('pggrouprank', 0);
-
-		sportsmanagementModelPrediction::$isNewMember   = $jinput->getInt('s', 0);
-		sportsmanagementModelPrediction::$tippEntryDone = $jinput->getInt('eok', 0);
-
-		sportsmanagementModelPrediction::$type = $jinput->getInt('type', 0);
-		sportsmanagementModelPrediction::$page = $jinput->getInt('page', 1);
-
-		parent::__construct();
-	}
-
+        sportsmanagementModelPrediction::$roundID = $roundId;
+        sportsmanagementModelPrediction::$pjID = $input->getInt('pj', 0);
+        sportsmanagementModelPrediction::$from = $input->getInt('from', $roundId);
+        sportsmanagementModelPrediction::$to = $input->getInt('to', $roundId);
+        sportsmanagementModelPrediction::$predictionGameID = $input->getInt('prediction_id', 0);
+        sportsmanagementModelPrediction::$predictionMemberID = $input->getInt('uid', 0);
+        sportsmanagementModelPrediction::$joomlaUserID = $input->getInt('juid', 0);
+        sportsmanagementModelPrediction::$pggroup = $input->getInt('pggroup', 0);
+        sportsmanagementModelPrediction::$pggrouprank = $input->getInt('pggrouprank', 0);
+        sportsmanagementModelPrediction::$isNewMember = $input->getInt('s', 0);
+        sportsmanagementModelPrediction::$tippEntryDone = $input->getInt('eok', 0);
+        sportsmanagementModelPrediction::$type = $input->getInt('type', 0);
+        sportsmanagementModelPrediction::$page = max(1, $input->getInt('page', 1));
+    }
 }
