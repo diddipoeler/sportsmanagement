5be25e25af94604eb9081d54cc4924ef39dd8ace
diddipoeler <diddipoeler@gmx.de>
2026-08-18 17:02:12 +0200

    Stage native ranking dispatcher

---
+++ modules/mod_sportsmanagement_ranking/src/Dispatcher/Dispatcher.php
@@ -0,0 +1,26 @@
+<?php
+namespace Diddipoeler\Module\SportsManagementRanking\Site\Dispatcher;
+
+\defined('_JEXEC') or die;
+
+use Joomla\CMS\Dispatcher\AbstractModuleDispatcher;
+use Joomla\CMS\Helper\HelperFactoryAwareInterface;
+use Joomla\CMS\Helper\HelperFactoryAwareTrait;
+
+final class Dispatcher extends AbstractModuleDispatcher implements HelperFactoryAwareInterface
+{
+    use HelperFactoryAwareTrait;
+
+    protected function getLayoutData(): array
+    {
+        $data = parent::getLayoutData();
+        $this->getApplication()->getLanguage()->load('com_sportsmanagement', JPATH_SITE, null, true);
+        $data['list'] = $this->getHelperFactory()->getHelper('RankingHelper')->getData(
+            $data['params'],
+            $data['module'],
+            $this->getApplication()
+        );
+
+        return $data;
+    }
+}
