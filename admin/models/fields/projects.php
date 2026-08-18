9a4544a62a5a82ed7061eef9fa7400a0abd53eb8
diddipoeler <diddipoeler@gmx.de>
2026-08-18 18:21:52 +0200

    Modernize projects form field for Joomla 5 and 6

--- admin/models/fields/projects.php
+++ admin/models/fields/projects.php
@@ -14,15 +14,11 @@
 
 defined('_JEXEC') or die('Restricted access');
 
-use Joomla\CMS\Factory;
-use Joomla\CMS\Form\FormField;
-use Joomla\CMS\Form\FormHelper;
-use Joomla\CMS\Language\Text;
-use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Component\ComponentHelper;
-
-jimport('joomla.form.helper');
-FormHelper::loadFieldClass('list');
+use Joomla\CMS\Factory;
+use Joomla\CMS\Form\Field\ListField;
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Language\Text;
 
 /**
  * FormFieldProjects
@@ -33,27 +29,22 @@ FormHelper::loadFieldClass('list');
  * @version   2014
  * @access    public
  */
-class JFormFieldProjects extends \JFormFieldList
+class JFormFieldProjects extends ListField
 {
 	protected $type = 'projects';
-
 
 	/**
 	 * FormFieldProjects::getOptions()
 	 *
-	 * @return
+	 * @return array
 	 */
 	protected function getOptions()
 	{
 		$options = array();
-		$app     = Factory::getApplication();
+		$lang = Factory::getApplication()->getLanguage();
 
-		// $db = sportsmanagementHelper::getDBConnection();
-		$lang = Factory::getLanguage();
-
-		// Welche tabelle soll genutzt werden
 		$params         = ComponentHelper::getParams('com_sportsmanagement');
-		$database_table = $params->get('cfg_which_database_table');
+		$databaseTable  = $params->get('cfg_which_database_table');
 
 		$val   = ($this->element['value_field'] ? $this->element['value_field'] : $this->name);
 		$value = $this->form->getValue($val, 'request');
@@ -68,29 +59,23 @@ class JFormFieldProjects extends \JFormFieldList
 			$div = 'request';
 		}
 
-		$cfg_which_database = $this->form->getValue('cfg_which_database', $div);
+		$cfgWhichDatabase = $this->form->getValue('cfg_which_database', $div);
+		$db = !$cfgWhichDatabase
+			? sportsmanagementHelper::getDBConnection()
+			: sportsmanagementHelper::getDBConnection(true, $cfgWhichDatabase);
 
-		if (!$cfg_which_database)
-		{
-			$db = sportsmanagementHelper::getDBConnection();
-		}
-		else
-		{
-			$db = sportsmanagementHelper::getDBConnection(true, $cfg_which_database);
-		}
-
-		$extension = "com_sportsmanagement";
+		$extension = 'com_sportsmanagement';
 		$source    = JPATH_ADMINISTRATOR . '/components/' . $extension;
-		$lang->load("$extension", JPATH_ADMINISTRATOR, null, false, false)
+		$lang->load($extension, JPATH_ADMINISTRATOR, null, false, false)
 		|| $lang->load($extension, $source, null, false, false)
 		|| $lang->load($extension, JPATH_ADMINISTRATOR, $lang->getDefault(), false, false)
 		|| $lang->load($extension, $source, $lang->getDefault(), false, false);
 
 		$query = 'SELECT p.id, concat(p.name, \' (' . Text::_('COM_SPORTSMANAGEMENT_GLOBAL_LEAGUE') . ': \', l.name, \')\', \' (' . Text::_('COM_SPORTSMANAGEMENT_GLOBAL_SEASON') . ': \', s.name, \' )\' ) as name
-					FROM #__' . $database_table . '_project AS p
-					LEFT JOIN #__' . $database_table . '_season AS s ON s.id = p.season_id
-					LEFT JOIN #__' . $database_table . '_league AS l ON l.id = p.league_id
-					WHERE p.published=1 ORDER BY p.id DESC';
+			FROM #__' . $databaseTable . '_project AS p
+			LEFT JOIN #__' . $databaseTable . '_season AS s ON s.id = p.season_id
+			LEFT JOIN #__' . $databaseTable . '_league AS l ON l.id = p.league_id
+			WHERE p.published=1 ORDER BY p.id DESC';
 		$db->setQuery($query);
 		$projects = $db->loadObjectList();
 
@@ -101,9 +86,6 @@ class JFormFieldProjects extends \JFormFieldList
 			$options[] = HTMLHelper::_('select.option', $project->id, '&nbsp;&nbsp;&nbsp;' . $project->name);
 		}
 
-		$options = array_merge(parent::getOptions(), $options);
-
-		return $options;
-
+		return array_merge(parent::getOptions(), $options);
 	}
 }
