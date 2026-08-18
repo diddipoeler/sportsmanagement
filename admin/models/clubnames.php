92d3bfcde3f0d3f77f25e397c435c75db510198a
diddipoeler <diddipoeler@gmx.de>
2026-08-18 20:06:38 +0200

    Bridge club names list model to Joomla 5/6 namespace

--- admin/models/clubnames.php
+++ admin/models/clubnames.php
@@ -1,165 +1,19 @@
 <?php
 /**
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage models
- * @file       clubnames.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\Language\Text;
-use Joomla\CMS\Component\ComponentHelper;
-
-/**
- * sportsmanagementModelclubnames
+ * SportsManagement legacy compatibility bridge.
  *
- * @package
- * @author    Dieter Plöger
- * @copyright 2016
- * @version   $Id$
- * @access    public
+ * The active Joomla 5/6 implementation lives in admin/src/Model/ClubnamesModel.php.
  */
-class sportsmanagementModelclubnames extends JSMModelList
-{
-	var $_identifier = "clubnames";
 
+defined('_JEXEC') or die('Restricted access');
 
-	/**
-	 * sportsmanagementModelclubnames::__construct()
-	 *
-	 * @param   mixed  $config
-	 *
-	 * @return void
-	 */
-	public function __construct($config = array())
-	{
-		$config['filter_fields'] = array(
-			'obj.name',
-			'obj.name_long',
-			'obj.country',
-			'obj.id',
-			'obj.published',
-			'obj.modified',
-			'obj.modified_by',
-			'obj.ordering',
-			'obj.checked_out',
-			'obj.checked_out_time','state','search_nation'
-		);
-		parent::__construct($config);
-		parent::setDbo($this->jsmdb);
+use Diddipoeler\Component\SportsManagement\Administrator\Model\ClubnamesModel;
 
-	}
+if (!class_exists(ClubnamesModel::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Model/SportsManagementListModel.php';
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Model/ClubnamesModel.php';
+}
 
-	/**
-	 * sportsmanagementModelagegroups::getListQuery()
-	 *
-	 * @return
-	 */
-	function getListQuery()
-	{
-		$this->jsmquery->clear();
-		$this->jsmquery->select('obj.*');
-		$this->jsmquery->from('#__sportsmanagement_club_names as obj');
-		$this->jsmquery->join('LEFT', '#__users AS uc ON uc.id = obj.checked_out');
-
-		if ($this->getState('filter.search'))
-		{
-			$this->jsmquery->where('LOWER(obj.name) LIKE ' . $this->jsmdb->Quote('%' . $this->getState('filter.search') . '%'));
-		}
-
-		if ($this->getState('filter.search_nation'))
-		{
-			$this->jsmquery->where('obj.country LIKE ' . $this->jsmdb->Quote('%' . $this->getState('filter.search_nation') . '%'));
-		}
-
-		if (is_numeric($this->getState('filter.state')))
-		{
-			$this->jsmquery->where('obj.published = ' . $this->getState('filter.state'));
-		}
-
-		$this->jsmquery->order(
-			$this->jsmdb->escape($this->getState('list.ordering', 'obj.name')) . ' ' .
-			$this->jsmdb->escape($this->getState('list.direction', 'ASC'))
-		);
-
-		return $this->jsmquery;
-
-	}
-
-	/**
-	 * sportsmanagementModelclubnames::getClubNames()
-	 *
-	 * @param   string  $country
-	 *
-	 * @return void
-	 */
-	function getClubNames($country = '')
-	{
-		$this->jsmquery->clear();
-		$this->jsmquery->select('name,name_long');
-		$this->jsmquery->from('#__sportsmanagement_club_names');
-
-		/** wenn das land mitegegeben wurde */
-		if ($country)
-		{
-			$this->jsmquery->where('country LIKE ' . $this->jsmdb->Quote('%' . $country . '%'));
-		}
-
-		try
-		{
-			$this->jsmdb->setQuery($this->jsmquery);
-			return $this->jsmdb->loadObjectList();
-		}
-		catch (Exception $e)
-		{
-$this->jsmapp->enqueueMessage(Text::sprintf('COM_SPORTSMANAGEMENT_DATABASE_ERROR_FUNCTION_FAILED', $e->getCode(), $e->getMessage()), 'notice');
-$this->jsmapp->enqueueMessage(Text::sprintf('COM_SPORTSMANAGEMENT_FILE_ERROR_FUNCTION_FAILED', __FILE__, __LINE__), 'notice');
-			return false;
-		}
-
-	}
-
-	/**
-	 * Method to auto-populate the model state.
-	 *
-	 * Note. Calling getState in this method will result in recursion.
-	 *
-	 * @since 1.6
-	 */
-	protected function populateState($ordering = null, $direction = null)
-	{
-		if (ComponentHelper::getParams($this->jsmoption)->get('show_debug_info'))
-		{
-			$this->jsmapp->enqueueMessage(Text::_(__METHOD__ . ' ' . __LINE__ . ' context -> ' . $this->context . ''), '');
-			$this->jsmapp->enqueueMessage(Text::_(__METHOD__ . ' ' . __LINE__ . ' identifier -> ' . $this->_identifier . ''), '');
-		}
-        $list = $this->getUserStateFromRequest($this->context . '.list', 'list', array(), 'array');
-        
-        $this->setState('filter.search', $this->getUserStateFromRequest($this->context . '.filter.search', 'filter_search', '', 'string'));
-		$this->setState('filter.state', $this->getUserStateFromRequest($this->context . '.filter.state', 'filter_state', '', 'string'));
-		$this->setState('filter.search_nation', $this->getUserStateFromRequest($this->context . '.filter.search_nation', 'filter_search_nation', ''));
-        $this->setState('list.limit', $this->getUserStateFromRequest($this->context . '.list.limit', 'list_limit', $this->jsmapp->get('list_limit'), 'int'));
-		$this->setState('list.start', $this->getUserStateFromRequest($this->context . '.limitstart', 'limitstart', 0, 'int'));
-		$orderCol = $this->getUserStateFromRequest($this->context . '.filter_order', 'filter_order', '', 'string');
-
-		if (!in_array($orderCol, $this->filter_fields))
-		{
-			$orderCol = 'obj.name';
-		}
-
-		$this->setState('list.ordering', $orderCol);
-		$listOrder = $this->getUserStateFromRequest($this->context . '.filter_order_Dir', 'filter_order_Dir', '', 'cmd');
-
-		if (!in_array(strtoupper($listOrder), array('ASC', 'DESC', '')))
-		{
-			$listOrder = 'ASC';
-		}
-
-		$this->setState('list.direction', $listOrder);
-	}
-
-
+if (!class_exists('sportsmanagementModelclubnames', false)) {
+    class_alias(ClubnamesModel::class, 'sportsmanagementModelclubnames');
 }
