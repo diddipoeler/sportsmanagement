84618547cd4e49851cf17e1af9318f3126dabf07
diddipoeler <diddipoeler@gmx.de>
2026-08-18 22:52:36 +0200

    Add native site results ranking model

---
+++ site/src/Model/ResultsrankingModel.php
@@ -0,0 +1,33 @@
+<?php
+namespace Diddipoeler\Component\SportsManagement\Site\Model;
+
+\defined('_JEXEC') or die;
+
+use Joomla\CMS\Factory;
+use Joomla\CMS\MVC\Factory\MVCFactoryInterface;
+
+final class ResultsrankingModel extends SportsManagementProjectModel
+{
+    public static int $divisionid = 0;
+    public static int $roundid = 0;
+    public static int $projectid = 0;
+    public static int $cfg_which_database = 0;
+    public static int $show_ranking_reiter = 0;
+
+    public function __construct($config = [], ?MVCFactoryInterface $factory = null)
+    {
+        parent::__construct($config, $factory);
+
+        $input = Factory::getApplication()->getInput();
+        self::$divisionid = $this->divisionId;
+        self::$roundid = $input->getInt('r', 0);
+        self::$projectid = $this->projectId;
+        self::$cfg_which_database = $input->getInt('cfg_which_database', 0);
+        self::$show_ranking_reiter = $input->getInt('show_ranking_reiter', 0);
+
+        if (class_exists('sportsmanagementModelProject')) {
+            \sportsmanagementModelProject::$projectid = self::$projectid;
+            \sportsmanagementModelProject::$cfg_which_database = self::$cfg_which_database;
+        }
+    }
+}
