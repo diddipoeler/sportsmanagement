00f4b46acd7b6d1edec8d0ecc87146db1cfc4d3a
diddipoeler <diddipoeler@gmx.de>
2026-08-18 21:31:35 +0200

    Add native site list model base

---
+++ site/src/Model/SportsManagementListModel.php
@@ -0,0 +1,35 @@
+<?php
+namespace Diddipoeler\Component\SportsManagement\Site\Model;
+
+\defined('_JEXEC') or die;
+
+use Joomla\CMS\Factory;
+use Joomla\CMS\MVC\Model\ListModel;
+use Joomla\Database\DatabaseInterface;
+
+abstract class SportsManagementListModel extends ListModel
+{
+    public function setDatabase(DatabaseInterface $db): void
+    {
+        if (!class_exists('sportsmanagementHelper')) {
+            \JLoader::register(
+                'sportsmanagementHelper',
+                JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/helpers/sportsmanagement.php'
+            );
+        }
+
+        try {
+            $databaseSelector = Factory::getApplication()->getInput()->getInt('cfg_which_database', 0);
+            $sportsManagementDb = \sportsmanagementHelper::getDBConnection(true, $databaseSelector);
+
+            if ($sportsManagementDb instanceof DatabaseInterface) {
+                parent::setDatabase($sportsManagementDb);
+                return;
+            }
+        } catch (\Throwable) {
+            // Keep Joomla's injected connection as a safe fallback.
+        }
+
+        parent::setDatabase($db);
+    }
+}
