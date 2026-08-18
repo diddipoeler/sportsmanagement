b1ffc98f88bfed35de11749fa881d563445e8272
diddipoeler <diddipoeler@gmx.de>
2026-08-18 19:06:46 +0200

    Modernize teamstaff legacy model

--- admin/models/teamstaff.php
+++ admin/models/teamstaff.php
@@ -14,12 +14,11 @@
 
 defined('_JEXEC') or die('Restricted access');
 
-use Joomla\CMS\Language\Text;
-use Joomla\CMS\Factory;
 use Joomla\CMS\Component\ComponentHelper;
+use Joomla\CMS\Factory;
+use Joomla\CMS\MVC\Model\AdminModel;
 use Joomla\CMS\Table\Table;
 use Joomla\Registry\Registry;
-use Joomla\CMS\MVC\Model\AdminModel;
 
 /**
  * SportsManagement Model
@@ -32,24 +31,28 @@ class sportsmanagementModelteamstaff extends AdminModel
 	 * @param   array    $data      Data for the form.
 	 * @param   boolean  $loadData  True if the form is to load its own data (default case), false if not.
 	 *
-	 * @return mixed    A JForm object on success, false on failure
+	 * @return mixed A form object on success, false on failure.
 	 * @since  1.6
 	 */
 	public function getForm($data = array(), $loadData = true)
 	{
 		$app                  = Factory::getApplication();
-		$option               = Factory::getApplication()->input->getCmd('option');
-		$cfg_which_media_tool = ComponentHelper::getParams($option)->get('cfg_which_media_tool', 0);
+		$option               = $app->getInput()->getCmd('option', 'com_sportsmanagement');
+		$params               = ComponentHelper::getParams($option);
+		$cfg_which_media_tool = $params->get('cfg_which_media_tool', 0);
 
-		// Get the form.
-		$form = $this->loadForm('com_sportsmanagement.teamstaff', 'teamstaff', array('control' => 'jform', 'load_data' => $loadData));
+		$form = $this->loadForm(
+			'com_sportsmanagement.teamstaff',
+			'teamstaff',
+			array('control' => 'jform', 'load_data' => $loadData)
+		);
 
 		if (empty($form))
 		{
 			return false;
 		}
 
-		$form->setFieldAttribute('picture', 'default', ComponentHelper::getParams($option)->get('ph_player', ''));
+		$form->setFieldAttribute('picture', 'default', $params->get('ph_player', ''));
 		$form->setFieldAttribute('picture', 'directory', 'com_sportsmanagement/database/teamstaffs');
 		$form->setFieldAttribute('picture', 'type', $cfg_which_media_tool);
 
@@ -57,9 +60,9 @@ class sportsmanagementModelteamstaff extends AdminModel
 	}
 
 	/**
-	 * Method to get the script that have to be included on the form
+	 * Method to get the script that has to be included on the form.
 	 *
-	 * @return string    Script files
+	 * @return string Script file.
 	 */
 	public function getScript()
 	{
@@ -67,45 +70,47 @@ class sportsmanagementModelteamstaff extends AdminModel
 	}
 
 	/**
-	 * Method to update checked teamstaff
+	 * Method to update checked team staff records.
 	 *
-	 * @access public
-	 * @return boolean    True on success
+	 * @return boolean True on success.
 	 */
-	function saveshort()
+	public function saveshort()
 	{
-		$app =& Factory::getApplication();
+		$input = Factory::getApplication()->getInput();
+		$pks   = $input->post->get('cid', array(), 'array');
+		$post  = $input->post->getArray(array());
+		$pks   = array_values(array_filter(array_map('intval', (array) $pks), static fn($id) => $id > 0));
 
-		// Get the input
-		$pks  = Factory::getApplication()->input->getVar('cid', null, 'post', 'array');
-		$post = Factory::getApplication()->input->post->getArray(array());
-
-		$result = true;
-
-		for ($x = 0; $x < count($pks); $x++)
+		foreach ($pks as $pk)
 		{
-			$tblPerson                      = &$this->getTable();
-			$tblPerson->id                  = $pks[$x];
-			$tblPerson->project_position_id = $post['project_position_id' . $pks[$x]];
+			$tblPerson                      = $this->getTable();
+			$tblPerson->id                  = $pk;
+			$tblPerson->project_position_id = (int) ($post['project_position_id' . $pk] ?? 0);
 
 			if (!$tblPerson->store())
 			{
-				sportsmanagementModeldatabasetool::writeErrorLog(get_class($this), __FUNCTION__, __FILE__, $this->_db->getErrorMsg(), __LINE__);
-				$result = false;
+				sportsmanagementModeldatabasetool::writeErrorLog(
+					get_class($this),
+					__FUNCTION__,
+					__FILE__,
+					$tblPerson->getError(),
+					__LINE__
+				);
+				return false;
 			}
 		}
 
-		return $result;
+		return true;
 	}
 
 	/**
-	 * Returns a reference to the a Table object, always creating it.
+	 * Returns a Table object, always creating it.
 	 *
-	 * @param   type    The table type to instantiate
-	 * @param   string    A prefix for the table class name. Optional.
-	 * @param   array    Configuration array for model. Optional.
+	 * @param   string  $type    The table type to instantiate.
+	 * @param   string  $prefix  A prefix for the table class name.
+	 * @param   array   $config  Configuration array for the table.
 	 *
-	 * @return Table    A database object
+	 * @return Table A database table object.
 	 * @since  1.6
 	 */
 	public function getTable($type = 'teamstaff', $prefix = 'sportsmanagementTable', $config = array())
@@ -116,29 +121,38 @@ class sportsmanagementModelteamstaff extends AdminModel
 	}
 
 	/**
-	 * Method to save item order
+	 * Method to save item order.
 	 *
-	 * @access public
-	 * @return boolean    True on success
+	 * @param array|null $pks   Record IDs.
+	 * @param array|null $order Ordering values.
+	 *
+	 * @return boolean True on success.
 	 * @since  1.5
 	 */
-	function saveorder($pks = null, $order = null)
+	public function saveorder($pks = null, $order = null)
 	{
-		$row =& $this->getTable();
+		$pks   = (array) $pks;
+		$order = (array) $order;
+		$row   = $this->getTable();
+		$count = min(count($pks), count($order));
 
-		// Update ordering values
-		for ($i = 0; $i < count($pks); $i++)
+		for ($i = 0; $i < $count; $i++)
 		{
 			$row->load((int) $pks[$i]);
 
 			if ($row->ordering != $order[$i])
 			{
-				$row->ordering = $order[$i];
+				$row->ordering = (int) $order[$i];
 
 				if (!$row->store())
 				{
-					sportsmanagementModeldatabasetool::writeErrorLog(get_class($this), __FUNCTION__, __FILE__, $this->_db->getErrorMsg(), __LINE__);
-
+					sportsmanagementModeldatabasetool::writeErrorLog(
+						get_class($this),
+						__FUNCTION__,
+						__FILE__,
+						$row->getError(),
+						__LINE__
+					);
 					return false;
 				}
 			}
@@ -148,102 +162,97 @@ class sportsmanagementModelteamstaff extends AdminModel
 	}
 
 	/**
-	 * Method to remove teamstaff
+	 * Method to remove team staff and dependent match staff data.
 	 *
-	 * @access public
-	 * @return boolean    True on success
+	 * @param array $pks Record IDs.
+	 *
+	 * @return boolean True on success.
 	 * @since  0.1
 	 */
 	public function delete(&$pks)
 	{
-		$app = Factory::getApplication();
+		$pks = array_values(array_filter(array_map('intval', (array) $pks), static fn($id) => $id > 0));
 
-		// Ein Datenbankobjekt beziehen
-
-		$db = Factory::getDbo();
-
-		// Ein JDatabaseQuery Objekt beziehen
-
-		$query = $db->getQuery(true);
-
-		// $pks    = (array) $pks;
-
-		$result = false;
-
-		if (count($pks))
+		if ($pks)
 		{
+			$db   = $this->getDatabase();
 			$cids = implode(',', $pks);
+			$query = 'DELETE mp, ms
+				FROM #__sportsmanagement_team_staff AS m
+				LEFT JOIN #__sportsmanagement_match_staff AS mp ON mp.team_staff_id = m.id
+				LEFT JOIN #__sportsmanagement_match_staff_statistic AS ms ON ms.team_staff_id = m.id
+				WHERE m.id IN (' . $cids . ')';
 
-			// Wir löschen mit join
-			$query = 'DELETE mp,ms
-            FROM #__sportsmanagement_team_staff as m  
-            LEFT JOIN #__sportsmanagement_match_staff as mp
-            ON mp.team_staff_id = m.id
-            LEFT JOIN #__sportsmanagement_match_staff_statistic as ms
-            ON ms.team_staff_id = m.id
-            WHERE m.id IN (' . $cids . ')';
-			$db->setQuery($query);
-			$db->execute();
-
-			if (!$db->execute())
+			try
 			{
+				$db->setQuery($query);
+				$db->execute();
+			}
+			catch (RuntimeException $e)
+			{
+				sportsmanagementModeldatabasetool::writeErrorLog(
+					get_class($this),
+					__FUNCTION__,
+					__FILE__,
+					$e->getMessage(),
+					__LINE__
+				);
 				return false;
 			}
 		}
 
 		return parent::delete($pks);
-
 	}
 
 	/**
 	 * Method to save the form data.
 	 *
-	 * @param   array    The form data.
+	 * @param array $data The form data.
 	 *
-	 * @return boolean    True on success.
+	 * @return boolean True on success.
 	 * @since  1.6
 	 */
 	public function save($data)
 	{
-		$app  = Factory::getApplication();
-		$post = Factory::getApplication()->input->post->getArray(array());
+		$post = Factory::getApplication()->getInput()->post->getArray(array());
 
 		if (isset($post['extended']) && is_array($post['extended']))
 		{
-			// Convert the extended field to a string.
 			$parameter = new Registry;
 			$parameter->loadArray($post['extended']);
 			$data['extended'] = (string) $parameter;
 		}
 
-		// Proceed with the save
 		return parent::save($data);
 	}
 
 	/**
 	 * Method override to check if you can edit an existing record.
 	 *
-	 * @param   array   $data  An array of input data.
-	 * @param   string  $key   The name of the key for the primary key.
+	 * @param array  $data An array of input data.
+	 * @param string $key  The name of the key for the primary key.
 	 *
 	 * @return boolean
 	 * @since  1.6
 	 */
 	protected function allowEdit($data = array(), $key = 'id')
 	{
-		// Check specific edit permission then general edit permission.
-		return Factory::getUser()->authorise('core.edit', 'com_sportsmanagement.message.' . ((int) isset($data[$key]) ? $data[$key] : 0)) || parent::allowEdit($data, $key);
+		$id = (int) ($data[$key] ?? 0);
+
+		return Factory::getApplication()->getIdentity()->authorise(
+			'core.edit',
+			'com_sportsmanagement.message.' . $id
+		) || parent::allowEdit($data, $key);
 	}
 
 	/**
 	 * Method to get the data that should be injected in the form.
 	 *
-	 * @return mixed    The data for the form.
+	 * @return mixed The data for the form.
 	 * @since  1.6
 	 */
 	protected function loadFormData()
 	{
-		// Check the session for previously entered form data.
 		$data = Factory::getApplication()->getUserState('com_sportsmanagement.edit.teamstaff.data', array());
 
 		if (empty($data))
@@ -253,6 +262,4 @@ class sportsmanagementModelteamstaff extends AdminModel
 
 		return $data;
 	}
-
-
 }
