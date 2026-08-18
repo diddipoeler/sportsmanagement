06f2f09ff316f532214c7c18bfd533f84284a40f
diddipoeler <diddipoeler@gmx.de>
2026-08-18 09:35:32 +0200

    Add native prediction results controller

---
+++ site/src/Controller/PredictionresultsController.php
@@ -0,0 +1,94 @@
+<?php
+namespace Diddipoeler\Component\SportsManagement\Site\Controller;
+
+\defined('_JEXEC') or die;
+
+use Diddipoeler\Component\SportsManagement\Site\Model\PredictionpointsModel;
+use Diddipoeler\Component\SportsManagement\Site\Model\PredictionresultsModel;
+use Joomla\CMS\Factory;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\MVC\Controller\BaseController;
+use Joomla\CMS\Session\Session;
+
+final class PredictionresultsController extends BaseController
+{
+    public function selectprojectround(): void
+    {
+        if (!Session::checkToken('post')) {
+            throw new \RuntimeException(Text::_('JINVALID_TOKEN'), 403);
+        }
+
+        $model = $this->getPredictionResultsModel();
+        $this->setRedirect($this->buildResultsRoute($model));
+    }
+
+    public function recalculatepoints(): void
+    {
+        if (!Session::checkToken('post')) {
+            throw new \RuntimeException(Text::_('JINVALID_TOKEN'), 403);
+        }
+
+        $model = $this->getPredictionPointsModel();
+        if (!$model->isAllowedAdmin()) {
+            throw new \RuntimeException(Text::_('JERROR_ALERTNOAUTHOR'), 403);
+        }
+
+        $updated = $model->recalculatePoints($model->getResultsConfig());
+        Factory::getApplication()->enqueueMessage(
+            Text::_('JTOOLBAR_REBUILD') . ': ' . $updated,
+            'message'
+        );
+        $this->setRedirect($this->buildResultsRoute($model));
+    }
+
+    private function getPredictionResultsModel(): PredictionresultsModel
+    {
+        $model = $this->getModel('Predictionresults');
+        if (!$model instanceof PredictionresultsModel) {
+            throw new \RuntimeException('PredictionresultsModel is unavailable.', 500);
+        }
+        return $model;
+    }
+
+    private function getPredictionPointsModel(): PredictionpointsModel
+    {
+        $model = $this->getModel('Predictionpoints');
+        if (!$model instanceof PredictionpointsModel) {
+            throw new \RuntimeException('PredictionpointsModel is unavailable.', 500);
+        }
+        return $model;
+    }
+
+    private function buildResultsRoute(PredictionresultsModel $model): string
+    {
+        $this->loadRouteHelpers();
+        $input = Factory::getApplication()->input;
+        $config = $model->getResultsConfig();
+
+        return \JSMPredictionHelperRoute::getPredictionResultsRoute(
+            $model->getPredictionGameId(),
+            $model->getSelectedRoundId($config),
+            $model->getProjectId(),
+            $model->getPredictionMemberId(),
+            '',
+            $model->getGroupId(),
+            $input->getInt('cfg_which_database', 0)
+        );
+    }
+
+    private function loadRouteHelpers(): void
+    {
+        if (!class_exists('sportsmanagementHelperRoute', false)) {
+            \JLoader::register(
+                'sportsmanagementHelperRoute',
+                JPATH_SITE . '/components/com_sportsmanagement/helpers/route.php'
+            );
+        }
+        if (!class_exists('JSMPredictionHelperRoute', false)) {
+            \JLoader::register(
+                'JSMPredictionHelperRoute',
+                JPATH_SITE . '/components/com_sportsmanagement/helpers/predictionroute.php'
+            );
+        }
+    }
+}
