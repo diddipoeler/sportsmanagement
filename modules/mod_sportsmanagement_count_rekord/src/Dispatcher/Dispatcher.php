9d6355598d8d6059f4199ca80f402934b8881a95
diddipoeler <diddipoeler@gmx.de>
2026-08-18 15:45:19 +0200

    Stage native count record module dispatcher

---
+++ modules/mod_sportsmanagement_count_rekord/src/Dispatcher/Dispatcher.php
@@ -0,0 +1,20 @@
+<?php
+namespace Diddipoeler\Module\SportsManagementCountRekord\Site\Dispatcher;
+
+\defined('_JEXEC') or die;
+
+use Joomla\CMS\Dispatcher\AbstractModuleDispatcher;
+use Joomla\CMS\Helper\HelperFactoryAwareInterface;
+use Joomla\CMS\Helper\HelperFactoryAwareTrait;
+
+final class Dispatcher extends AbstractModuleDispatcher implements HelperFactoryAwareInterface
+{
+    use HelperFactoryAwareTrait;
+
+    protected function getLayoutData(): array
+    {
+        $data = parent::getLayoutData();
+        $data['list'] = $this->getHelperFactory()->getHelper('CountRekordHelper')->getData($data['params'], $data['module'], $this->getApplication());
+        return $data;
+    }
+}
