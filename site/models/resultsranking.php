f532076be9e1f7c24d0e832d38c5b0a95c791535
diddipoeler <diddipoeler@gmx.de>
2026-08-18 22:52:48 +0200

    Bridge legacy results ranking model to native model

--- site/models/resultsranking.php
+++ site/models/resultsranking.php
@@ -1,62 +1,20 @@
 <?php
 /**
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage resultsranking
- * @file       resultsranking.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\Factory;
-use Joomla\CMS\MVC\Model\BaseDatabaseModel;
-use sportsmanagement\Site\Model\JSMSportsmanagementPagination;
-
-
-/**
- * sportsmanagementModelResultsranking
+ * SportsManagement legacy compatibility bridge.
  *
- * @package
- * @author
- * @copyright diddi
- * @version   2014
- * @access    public
+ * The active Joomla 5/6 implementation lives in site/src/Model/ResultsrankingModel.php.
  */
-class sportsmanagementModelResultsranking extends BaseDatabaseModel
-{
-	static $divisionid = 0;
 
-	static $roundid = 0;
+defined('_JEXEC') or die('Restricted access');
 
-	static $projectid = 0;
+use Diddipoeler\Component\SportsManagement\Site\Model\ResultsrankingModel;
 
-	static $cfg_which_database = 0;
-
-	static $show_ranking_reiter = 0;
-
-	/**
-	 * sportsmanagementModelResultsranking::__construct()
-	 *
-	 * @return
-	 */
-	function __construct()
-	{
-		$app = Factory::getApplication();
-
-		// JInput object
-		$jinput = $app->input;
-
-		parent::__construct();
-		self::$divisionid                                 = (int) $jinput->get('division', 0, '');
-		self::$roundid                                    = (int) $jinput->get('r', 0, '');
-		self::$projectid                                  = (int) $jinput->get('p', 0, '');
-		self::$cfg_which_database                         = $jinput->get('cfg_which_database', 0, '');
-		self::$show_ranking_reiter                        = $jinput->get('show_ranking_reiter', 0, '');
-		sportsmanagementModelProject::$projectid          = self::$projectid;
-		sportsmanagementModelProject::$cfg_which_database = self::$cfg_which_database;
-	}
-
+if (!class_exists(ResultsrankingModel::class)) {
+    require_once JPATH_SITE . '/components/com_sportsmanagement/src/Model/SportsManagementModel.php';
+    require_once JPATH_SITE . '/components/com_sportsmanagement/src/Model/SportsManagementProjectModel.php';
+    require_once JPATH_SITE . '/components/com_sportsmanagement/src/Model/ResultsrankingModel.php';
 }
 
+if (!class_exists('sportsmanagementModelResultsranking', false)) {
+    class_alias(ResultsrankingModel::class, 'sportsmanagementModelResultsranking');
+}
