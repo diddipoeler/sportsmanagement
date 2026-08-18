b5b3df83f8d656bf57cf4534393601f67c809584
diddipoeler <diddipoeler@gmx.de>
2026-08-18 19:57:57 +0200

    Bridge legacy Eventtype model to native Joomla model

--- admin/models/eventtype.php
+++ admin/models/eventtype.php
@@ -1,31 +1,19 @@
 <?php
 /**
+ * SportsManagement legacy compatibility bridge.
  *
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage models
- * @file       eventtype.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * The active Joomla 5/6 implementation lives in admin/src/Model/EventtypeModel.php.
  */
-
 
 defined('_JEXEC') or die('Restricted access');
 
-/**
- * sportsmanagementModeleventtype
- *
- * @package
- * @author
- * @copyright diddi
- * @version   2014
- * @access    public
- */
-class sportsmanagementModeleventtype extends JSMModelAdmin
-{
+use Diddipoeler\Component\SportsManagement\Administrator\Model\EventtypeModel;
 
+if (!class_exists(EventtypeModel::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Model/SportsManagementAdminModel.php';
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Model/EventtypeModel.php';
+}
 
+if (!class_exists('sportsmanagementModeleventtype', false)) {
+    class_alias(EventtypeModel::class, 'sportsmanagementModeleventtype');
 }
