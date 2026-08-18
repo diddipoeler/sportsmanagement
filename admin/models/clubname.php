4888900b4dc0e8957ad39225b750ed7a55e035ee
diddipoeler <diddipoeler@gmx.de>
2026-08-18 19:59:38 +0200

    Bridge legacy Clubname model to native Joomla model

--- admin/models/clubname.php
+++ admin/models/clubname.php
@@ -1,82 +1,19 @@
 <?php
 /**
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage models
- * @file       clubname.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\Factory;
-
-/**
- * sportsmanagementModelclubname
+ * SportsManagement legacy compatibility bridge.
  *
- * @package
- * @author    Dieter Plöger
- * @copyright 2016
- * @version   $Id$
- * @access    public
+ * The active Joomla 5/6 implementation lives in admin/src/Model/ClubnameModel.php.
  */
-class sportsmanagementModelclubname extends JSMModelAdmin
-{
 
-	/**
-	 * sportsmanagementModelclubname::import()
-	 *
-	 * @return void
-	 */
-	public function import()
-	{
+defined('_JEXEC') or die('Restricted access');
 
-if (version_compare(JVERSION, '3.0.0', 'ge'))
-{
-$xml = simplexml_load_file(JPATH_ADMINISTRATOR . '/components/' . $this->jsmoption . '/helpers/xml_files/clubnames.xml');
-}
-else
-{
-$xml = Factory::getXML(JPATH_ADMINISTRATOR . '/components/' . $this->jsmoption . '/helpers/xml_files/clubnames.xml', true);
+use Diddipoeler\Component\SportsManagement\Administrator\Model\ClubnameModel;
+
+if (!class_exists(ClubnameModel::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Model/SportsManagementAdminModel.php';
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Model/ClubnameModel.php';
 }
 
-		foreach ($xml->children() as $quote)
-		{
-			$country  = (string) $quote->clubname->attributes()->country;
-			$name     = (string) $quote->clubname->attributes()->name;
-			$clubname = (string) $quote->clubname;
-
-			$this->jsmquery->clear();
-			$this->jsmquery->select('id');
-			$this->jsmquery->from('#__sportsmanagement_club_names');
-			$this->jsmquery->where('country LIKE ' . $db->Quote('' . $country . ''));
-			$this->jsmquery->where('name LIKE ' . $db->Quote('' . $name . ''));
-			$this->jsmdb->setQuery($this->jsmquery);
-			$result = $this->jsmdb->loadResult();
-
-			if (!$result)
-			{
-				$insertquery = $this->jsmdb->getQuery(true);
-				$columns = array('country', 'name', 'name_long');
-				$values = array('\'' . $country . '\'', '\'' . $name . '\'', '\'' . $clubname . '\'');
-				$insertquery
-					->insert($this->jsmdb->quoteName('#__sportsmanagement_club_names'))
-					->columns($this->jsmdb->quoteName($columns))
-					->values(implode(',', $values));
-				$this->jsmdb->setQuery($insertquery);
-         try{       
-            $this->jsmdb->execute();
-                }
-		catch (Exception $e)
-		{
-        $this->jsmapp->enqueueMessage(Text::sprintf('COM_SPORTSMANAGEMENT_DATABASE_ERROR_FUNCTION_FAILED', $e->getCode(), $e->getMessage()), 'notice');
-        $this->jsmapp->enqueueMessage(Text::sprintf('COM_SPORTSMANAGEMENT_FILE_ERROR_FUNCTION_FAILED', __FILE__, __LINE__), 'notice');
-		}
-
-			}
-		}
-
-	}
-
+if (!class_exists('sportsmanagementModelclubname', false)) {
+    class_alias(ClubnameModel::class, 'sportsmanagementModelclubname');
 }
