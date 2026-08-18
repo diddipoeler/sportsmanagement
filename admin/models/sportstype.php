468c76c7f87eadf6b14d79e9232af4694b30d78d
diddipoeler <diddipoeler@gmx.de>
2026-08-18 21:23:26 +0200

    Bridge legacy sportstype model to native implementation

--- admin/models/sportstype.php
+++ admin/models/sportstype.php
@@ -1,58 +1,19 @@
 <?php
 /**
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage models
- * @file       sportstype.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * SportsManagement legacy compatibility bridge.
+ *
+ * The active Joomla 5/6 implementation lives in admin/src/Model/SportstypeModel.php.
  */
+
 defined('_JEXEC') or die('Restricted access');
 
+use Diddipoeler\Component\SportsManagement\Administrator\Model\SportstypeModel;
 
-/**
- * sportsmanagementModelsportstype
- * 
- * @package 
- * @author Dieter Plöger
- * @copyright 2020
- * @version $Id$
- * @access public
- */
-class sportsmanagementModelsportstype extends JSMModelAdmin
-{
+if (!class_exists(SportstypeModel::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Model/SportsManagementAdminModel.php';
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Model/SportstypeModel.php';
+}
 
-	/**
-	 * Override parent constructor.
-	 *
-	 * @param   array  $config  An optional associative array of configuration settings.
-	 *
-	 * @see   BaseDatabaseModel
-	 * @since 3.2
-	 */
-	public function __construct($config = array())
-	{
-		parent::__construct($config);
-
-	}
-
-	/**
-	 * return
-	 *
-	 * @param   int sportstype_id
-	 *
-	 * @return integer
-	 */
-	function getSportstype($sportstype_id)
-	{
-		$this->jsmquery->clear();
-		$this->jsmquery->select('*');
-		$this->jsmquery->from('#__sportsmanagement_sports_type');
-		$this->jsmquery->where('id = ' . $sportstype_id);
-		$this->jsmdb->setQuery($this->jsmquery);
-		return $this->jsmdb->loadObject();
-	}
-
+if (!class_exists('sportsmanagementModelsportstype', false)) {
+    class_alias(SportstypeModel::class, 'sportsmanagementModelsportstype');
 }
