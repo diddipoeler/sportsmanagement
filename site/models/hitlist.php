40b85376d25a2e114bbbda5616f8e6172d5f2948
diddipoeler <diddipoeler@gmx.de>
2026-08-18 21:53:04 +0200

    Bridge legacy site hitlist model to native model

--- site/models/hitlist.php
+++ site/models/hitlist.php
@@ -1,108 +1,19 @@
 <?php
 /**
+ * SportsManagement legacy compatibility bridge.
  *
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage models
- * @file       hitlist.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * The active Joomla 5/6 implementation lives in site/src/Model/HitlistModel.php.
  */
 
-defined('_JEXEC') or die;
+defined('_JEXEC') or die('Restricted access');
 
-use Joomla\CMS\Factory;
-use Joomla\CMS\Language\Text;
-use Joomla\CMS\MVC\Model\ListModel;
+use Diddipoeler\Component\SportsManagement\Site\Model\HitlistModel;
 
-/**
- * sportsmanagementModelhitlist
- *
- * @package
- * @author    diddi
- * @copyright 2015
- * @version   $Id$
- * @access    public
- */
-class sportsmanagementModelhitlist extends ListModel
-{
-	static $cfg_which_database = 0;
-	static $projectid = 0;
-	static $_success_text = null;
-	var $_identifier = "hitlist";
-	var $limitstart = 0;
-	var $limit = 0;
-
-	/**
-	 * sportsmanagementModelhitlist::__construct()
-	 *
-	 * @param   mixed  $config
-	 *
-	 * @return void
-	 */
-	public function __construct($config = array())
-	{
-		// Reference global application object
-		$app = Factory::getApplication();
-
-		// JInput object
-		$jinput          = $app->input;
-		self::$projectid = $jinput->getInt("p", 0);
-
-		sportsmanagementModelProject::$projectid = self::$projectid;
-		self::$cfg_which_database                = $jinput->getInt('cfg_which_database', 0);
-
-		parent::__construct($config);
-	}
-
-
-	/**
-	 * sportsmanagementModelhitlist::getSportsmanagementHits()
-	 *
-	 * @param   mixed    $config
-	 * @param   integer  $max_hits
-	 * @param   string   $table
-	 *
-	 * @return void
-	 */
-	public function getSportsmanagementHits($config = array(), $max_hits = 0, $table = 'project')
-	{
-		// Reference global application object
-		$app = Factory::getApplication();
-
-		// JInput object
-		$jinput = $app->input;
-		$option = $jinput->getCmd('option');
-
-		// Get a db connection.
-		$db    = sportsmanagementHelper::getDBConnection(true, self::$cfg_which_database);
-		$query = $db->getQuery(true);
-
-		switch ($table)
-		{
-			case 'person':
-				$query->select('CONCAT_WS(\' - \',firstname,lastname) AS name,hits');
-				break;
-			default:
-				$query->select('name,hits');
-				break;
-		}
-
-		$query->from('#__sportsmanagement_' . $table);
-		$query->where('hits != 0 ');
-		$query->order('hits DESC');
-
-		$db->setQuery($query, 0, $max_hits);
-		$db->query();
-		$rows = $db->loadObjectList();
-
-		self::$_success_text[Text::_('COM_SPORTSMANAGEMENT_HITLIST_' . strtoupper($table))] = $rows;
-
-	}
-
-
+if (!class_exists(HitlistModel::class)) {
+    require_once JPATH_SITE . '/components/com_sportsmanagement/src/Model/SportsManagementModel.php';
+    require_once JPATH_SITE . '/components/com_sportsmanagement/src/Model/HitlistModel.php';
 }
 
+if (!class_exists('sportsmanagementModelhitlist', false)) {
+    class_alias(HitlistModel::class, 'sportsmanagementModelhitlist');
+}
