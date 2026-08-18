7859fd012cf23ec989bd3d2f069d99986985b3f6
diddipoeler <diddipoeler@gmx.de>
2026-08-18 09:35:43 +0200

    Modernize prediction results layout shell

--- site/views/predictionresults/tmpl/default.php
+++ site/views/predictionresults/tmpl/default.php
@@ -1,32 +1,14 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage predictionresults
- * @file       default.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-defined('_JEXEC') or die('Restricted access');
-
-$templatesToLoad = array('globalviews', 'predictionheading');
-sportsmanagementHelper::addTemplatePaths($templatesToLoad, $this);
-
+\defined('_JEXEC') or die;
 ?>
-<div class="row-fluid">
-	<?php
-	echo $this->loadTemplate('predictionheading');
-	echo $this->loadTemplate('sectionheader');
-	echo $this->loadTemplate('results');
+<div class="<?php echo htmlspecialchars($this->divclasscontainer, ENT_QUOTES, 'UTF-8'); ?>" id="defaultpredictionresults">
+    <?php echo $this->loadTemplate('predictionheading'); ?>
+    <?php echo $this->loadTemplate('sectionheader'); ?>
+    <?php echo $this->loadTemplate('results'); ?>
 
-	if ($this->config['show_help'])
-	{
-		echo $this->loadTemplate('show_help');
-	}
+    <?php if (!empty($this->config['show_help'])) : ?>
+        <?php echo $this->loadTemplate('show_help'); ?>
+    <?php endif; ?>
 
-	echo $this->loadTemplate('jsminfo');
-	?>
-
+    <?php echo $this->loadTemplate('jsminfo'); ?>
 </div>
