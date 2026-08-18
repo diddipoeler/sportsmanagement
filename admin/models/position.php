2f28a48525f061d5e96107a26bfc922ba42e3f00
diddipoeler <diddipoeler@gmx.de>
2026-08-18 20:37:37 +0200

    Bridge legacy position form model

--- admin/models/position.php
+++ admin/models/position.php
@@ -1,64 +1,19 @@
 <?php
 /**
+ * SportsManagement legacy compatibility bridge.
  *
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage models
- * @file       position.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * The active Joomla 5/6 implementation lives in admin/src/Model/PositionModel.php.
  */
 
 defined('_JEXEC') or die('Restricted access');
 
-use Joomla\CMS\Factory;
+use Diddipoeler\Component\SportsManagement\Administrator\Model\PositionModel;
 
-/**
- * sportsmanagementModelposition
- *
- * @package
- * @author
- * @copyright diddi
- * @version   2014
- * @access    public
- */
-class sportsmanagementModelposition extends JSMModelAdmin
-{
+if (!class_exists(PositionModel::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Model/SportsManagementAdminModel.php';
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Model/PositionModel.php';
+}
 
-	/**
-	 * Method to update checked positions
-	 *
-	 * @access public
-	 * @return boolean    True on success
-	 */
-	function saveshort()
-	{
-		$app = Factory::getApplication();
-
-		// Get the input
-		$pks  = Factory::getApplication()->input->getVar('cid', null, 'post', 'array');
-		$post = Factory::getApplication()->input->post->getArray(array());
-
-		$result = true;
-
-		for ($x = 0; $x < count($pks); $x++)
-		{
-			$tblPosition            = &$this->getTable();
-			$tblPosition->id        = $pks[$x];
-			$tblPosition->parent_id = $post['parent_id' . $pks[$x]];
-
-			if (!$tblPosition->store())
-			{
-				sportsmanagementModeldatabasetool::writeErrorLog(get_class($this), __FUNCTION__, __FILE__, $this->_db->getErrorMsg(), __LINE__);
-				$result = false;
-			}
-		}
-
-		return $result;
-	}
-
-
+if (!class_exists('sportsmanagementModelposition', false)) {
+    class_alias(PositionModel::class, 'sportsmanagementModelposition');
 }
