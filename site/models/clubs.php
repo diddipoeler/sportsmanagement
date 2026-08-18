cd392a3813bc649fd4ef73f4c8d8bf0faf20db33
diddipoeler <diddipoeler@gmx.de>
2026-08-18 21:28:37 +0200

    Bridge legacy site clubs model to native implementation

--- site/models/clubs.php
+++ site/models/clubs.php
@@ -1,145 +1,20 @@
 <?php
 /**
+ * SportsManagement legacy compatibility bridge.
  *
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage clubs
- * @file       clubs.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * The active Joomla 5/6 implementation lives in site/src/Model/ClubsModel.php.
  */
 
 defined('_JEXEC') or die('Restricted access');
 
-use Joomla\CMS\Factory;
+use Diddipoeler\Component\SportsManagement\Site\Model\ClubsModel;
 
-use Joomla\CMS\MVC\Model\BaseDatabaseModel;
+if (!class_exists(ClubsModel::class)) {
+    require_once JPATH_SITE . '/components/com_sportsmanagement/src/Model/SportsManagementModel.php';
+    require_once JPATH_SITE . '/components/com_sportsmanagement/src/Model/SportsManagementProjectModel.php';
+    require_once JPATH_SITE . '/components/com_sportsmanagement/src/Model/ClubsModel.php';
+}
 
-/**
- * sportsmanagementModelClubs
- *
- * @package
- * @author    abcde
- * @copyright 2015
- * @version   $Id$
- * @access    public
- */
-class sportsmanagementModelClubs extends BaseDatabaseModel
-{
-	static $projectid = 0;
-
-	static $divisionid = 0;
-
-	static $cfg_which_database = 0;
-
-	/**
-	 * sportsmanagementModelClubs::__construct()
-	 *
-	 * @return void
-	 */
-	function __construct()
-	{
-		// Reference global application object
-		$app    = Factory::getApplication();
-		$jinput = $app->input;
-		parent::__construct();
-
-		self::$projectid          = $jinput->get('p', 0, 'INT');
-		self::$divisionid         = $jinput->get('division', 0, 'INT');
-		self::$cfg_which_database = $jinput->get('cfg_which_database', 0, 'INT');
-
-		sportsmanagementModelProject::$projectid = self::$projectid;
-
-	}
-
-	/**
-	 * sportsmanagementModelClubs::getClubs()
-	 *
-	 * @param   mixed  $ordering
-	 *
-	 * @return
-	 */
-	function getClubs($ordering = null)
-	{
-		$option = Factory::getApplication()->input->getCmd('option');
-		$app    = Factory::getApplication();
-
-		// Get a db connection.
-		$db    = sportsmanagementHelper::getDBConnection(true, self::$cfg_which_database);
-		$query = $db->getQuery(true);
-
-		$teams = array();
-
-		$query->select('c.*, \'\' as teams');
-		$query->select('CONCAT_WS( \':\', c.id, c.alias ) AS club_slug');
-		$query->from('#__sportsmanagement_club AS c ');
-		$query->join('INNER', '#__sportsmanagement_team AS t ON t.club_id = c.id ');
-		$query->join('INNER', '#__sportsmanagement_season_team_id as st ON st.team_id = t.id ');
-		$query->join('INNER', '#__sportsmanagement_project_team AS pt ON st.id = pt.team_id ');
-
-		if (self::$projectid > 0)
-		{
-			$query->where('pt.project_id = ' . (int) self::$projectid);
-
-			if (self::$divisionid > 0)
-			{
-				$query->where('pt.division_id = ' . (int) self::$divisionid);
-			}
-		}
-
-		$query->group('c.id');
-
-		if ($ordering)
-		{
-			$query->order($ordering);
-		}
-		else
-		{
-			$query->order('c.name');
-		}
-
-		$db->setQuery($query);
-
-		if (!$clubs = $db->loadObjectList())
-		{
-			echo $db->getErrorMsg();
-		}
-
-		$query->clear();
-
-		for ($index = 0; $index < count($clubs); $index++)
-		{
-			$teams = array();
-			$query->clear();
-			$query->select('t.*, t.picture AS team_picture, pt.picture AS projectteam_picture');
-			$query->select('CONCAT_WS( \':\', t.id, t.alias ) AS team_slug');
-			$query->from('#__sportsmanagement_team AS t ');
-			$query->join('INNER', '#__sportsmanagement_season_team_id as st ON st.team_id = t.id ');
-			$query->join('INNER', '#__sportsmanagement_project_team AS pt ON st.id = pt.team_id ');
-
-			$query->where('pt.project_id = ' . (int) self::$projectid);
-
-			if (self::$divisionid != 0)
-			{
-				$query->where('pt.division_id = ' . (int) self::$divisionid);
-			}
-
-			$query->where('t.club_id = ' . $clubs[$index]->id);
-
-			$db->setQuery($query);
-
-			if (!$teams = $db->loadObjectList())
-			{
-				echo $db->getErrorMsg();
-			}
-
-			$clubs[$index]->teams = $teams;
-		}
-
-		return $clubs;
-	}
-
+if (!class_exists('sportsmanagementModelClubs', false)) {
+    class_alias(ClubsModel::class, 'sportsmanagementModelClubs');
 }
