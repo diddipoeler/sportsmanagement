e61eb4621e9fe4fd9d88d1358494f333a66f3e30
diddipoeler <diddipoeler@gmx.de>
2026-08-18 20:36:56 +0200

    Bridge legacy league form model

--- admin/models/league.php
+++ admin/models/league.php
@@ -1,145 +1,19 @@
 <?php
 /**
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage models
- * @file       league.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\Factory;
-use Joomla\CMS\MVC\Model\BaseDatabaseModel;
-
-/**
- * sportsmanagementModelleague
+ * SportsManagement legacy compatibility bridge.
  *
- * @package
- * @author    Dieter Plöger
- * @copyright 2016
- * @version   $Id$
- * @access    public
+ * The active Joomla 5/6 implementation lives in admin/src/Model/LeagueModel.php.
  */
-class sportsmanagementModelleague extends JSMModelAdmin
-{
 
-	/**
-	 * Override parent constructor.
-	 *
-	 * @param   array  $config  An optional associative array of configuration settings.
-	 *
-	 * @see   BaseDatabaseModel
-	 * @since 3.2
-	 */
-	public function __construct($config = array())
-	{
-		parent::__construct($config);
-	}
+defined('_JEXEC') or die('Restricted access');
 
+use Diddipoeler\Component\SportsManagement\Administrator\Model\LeagueModel;
 
-/**
- * sportsmanagementModelleague::getlogohistoryLeague()
- * 
- * @param integer $league_id
- * @param integer $season_id
- * @param bool $logoonly
- * @return
- */
-function getlogohistoryLeague($league_id = 0, $season_id = 0, $logoonly = false )
-	{
-$app    = Factory::getApplication();
-$db    = Factory::getDbo();
-$query = $db->getQuery(true);
-$result    = array();
-
-$query->select('cl.*,se.name as seasonname');
-$query->from('#__sportsmanagement_league_logos as cl');
-$query->join('INNER', '#__sportsmanagement_season AS se ON se.id = cl.season_id');
-
-if ( $league_id )
-{		
-$query->where('cl.league_id = ' . $league_id);
+if (!class_exists(LeagueModel::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Model/SportsManagementAdminModel.php';
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Model/LeagueModel.php';
 }
 
-if ( $season_id )
-{
-$query->where('se.id = ' . $season_id);
-}
-
-$query->order('seasonname DESC');		
-$db->setQuery($query);
-try
-			{
-				$result = $db->loadObjectList();
-				$db->disconnect(); // See: http://api.joomla.org/cms-3/classes/JDatabaseDriver.html#method_disconnect
-			}
-			catch (Exception $e)
-			{
-				$db->disconnect(); // See: http://api.joomla.org/cms-3/classes/JDatabaseDriver.html#method_disconnect
-				$msg  = $e->getMessage(); // Returns "Normally you would have other code...
-				$code = $e->getCode(); // Returns '500';
-				$app->enqueueMessage(__METHOD__ . ' ' . __LINE__ . ' ' . $msg, 'error'); // commonly to still display that error
-				//$result = false;
-			}		
-
-		
-return $result;
-
-
-	}
-
-
-
-
-
-
-
-
-
-
-
-
-
-	/**
-	 * Method to update checked leagues
-	 *
-	 * @access public
-	 * @return boolean    True on success
-	 */
-	function saveshort()
-	{
-		// Reference global application object
-		$app = Factory::getApplication();
-
-		// JInput object
-		$jinput = $app->input;
-		$option = $jinput->getCmd('option');
-
-		// Get the input
-		$pks    = $jinput->getVar('cid', null, 'post', 'array');
-		$post   = Factory::getApplication()->input->post->getArray(array());
-		$result = true;
-
-		for ($x = 0; $x < count($pks); $x++)
-		{
-			$tblLeague                       = &$this->getTable();
-			$tblLeague->id                   = $pks[$x];
-			$tblLeague->associations         = $post['association' . $pks[$x]];
-			$tblLeague->country              = $post['country' . $pks[$x]];
-			$tblLeague->agegroup_id          = $post['agegroup' . $pks[$x]];
-			$tblLeague->published_act_season = $post['published_act_season' . $pks[$x]];
-            $tblLeague->champions_complete = $post['champions_complete' . $pks[$x]];
-
-			if (!$tblLeague->store())
-			{
-				$result = false;
-			}
-		}
-
-		return $result;
-	}
-
-
+if (!class_exists('sportsmanagementModelleague', false)) {
+    class_alias(LeagueModel::class, 'sportsmanagementModelleague');
 }
