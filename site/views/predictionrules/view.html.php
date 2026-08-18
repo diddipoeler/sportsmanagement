a54ed55cdbbded983b664709cf920d7e83f5f48f
diddipoeler <diddipoeler@gmx.de>
2026-08-18 22:46:43 +0200

    Bridge legacy prediction rules view to native view

--- site/views/predictionrules/view.html.php
+++ site/views/predictionrules/view.html.php
@@ -1,72 +1,20 @@
 <?php
 /**
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage predictionrules
- * @file       view.html.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\Language\Text;
-use Joomla\CMS\Factory;
-use Joomla\CMS\Log\Log;
-
-/**
- * sportsmanagementViewPredictionRules
+ * SportsManagement legacy compatibility bridge.
  *
- * @package
- * @author
- * @copyright diddi
- * @version   2014
- * @access    public
+ * The active Joomla 5/6 implementation lives in site/src/View/Predictionrules/HtmlView.php.
  */
-class sportsmanagementViewPredictionRules extends sportsmanagementView
-{
 
-	/**
-	 * sportsmanagementViewPredictionRules::init()
-	 * 
-	 * @return void
-	 */
-	function init()
-	{
+defined('_JEXEC') or die('Restricted access');
 
-//		$model    = $this->getModel();
-		$option   = Factory::getApplication()->input->getCmd('option');
+use Diddipoeler\Component\SportsManagement\Site\View\Predictionrules\HtmlView;
 
-		$app = Factory::getApplication();
+if (!class_exists(HtmlView::class)) {
+    require_once JPATH_SITE . '/components/com_sportsmanagement/src/View/SportsManagementHtmlView.php';
+    require_once JPATH_SITE . '/components/com_sportsmanagement/src/View/SportsManagementPredictionHtmlView.php';
+    require_once JPATH_SITE . '/components/com_sportsmanagement/src/View/Predictionrules/HtmlView.php';
+}
 
-		$this->predictionGame = sportsmanagementModelPrediction::getPredictionGame();
-		$this->allowedAdmin   = sportsmanagementModelPrediction::getAllowed();
-		$this->headertitle    = Text::_('COM_SPORTSMANAGEMENT_PRED_RULES_SECTION_TITLE');
-
-		if (isset($this->predictionGame))
-		{
-//			$config        = sportsmanagementModelPrediction::getPredictionTemplateConfig($this->getName());
-//			$overallConfig = sportsmanagementModelPrediction::getPredictionOverallConfig();
-
-//			$this->model              = $model;
-//			$this->config             = array_merge($overallConfig, $config);
-			$configavatar             = sportsmanagementModelPrediction::getPredictionTemplateConfig('predictionusers');
-			$this->configavatar       = $configavatar;
-			$this->predictionMember   = sportsmanagementModelPrediction::getPredictionMember($configavatar);
-			$this->predictionProjectS = sportsmanagementModelPrediction::getPredictionProjectS();
-			$this->actJoomlaUser      = Factory::getUser();
-
-			// Set page title
-			$pageTitle = Text::_('COM_SPORTSMANAGEMENT_PRED_USERS_TITLE'); // 'Tippspiel Regeln'
-
-			$this->document->setTitle($pageTitle);
-
-		}
-		else
-		{
-			Log::add(Text::_('COM_SPORTSMANAGEMENT_PRED_PREDICTION_NOT_EXISTING'), Log::INFO, 'jsmerror');
-		}
-
-	}
-
+if (!class_exists('sportsmanagementViewPredictionRules', false)) {
+    class_alias(HtmlView::class, 'sportsmanagementViewPredictionRules');
 }
