bc728e9855ff2cb33eace0beb4cbc7565ca36c70
diddipoeler <diddipoeler@gmx.de>
2026-08-18 15:48:39 +0200

    Migrate first SportsManagement module wave to Joomla 5/6

--- modules/mod_sportsmanagement_act_season/tmpl/default.php
+++ modules/mod_sportsmanagement_act_season/tmpl/default.php
@@ -1,151 +1,54 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage mod_sportsmanagement_act_season
- * @file       default.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- * https://www.tutorialrepublic.com/codelab.php?topic=bootstrap&file=accordion
- */
-defined('_JEXEC') or die('Restricted access');
+\defined('_JEXEC') or die;
+
+use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\Router\Route;
-use Joomla\CMS\HTML\HTMLHelper;
 
-if ($params->get("show_slider"))
-{
-	$ausland = array();
-	$auslandfed = array();
-	$zaehler = 0;
-	foreach ($list as $row)
-	{
-		$ausland[$row->country] = JSMCountries::getCountryName($row->country);
-		$auslandfed[$row->country] = $row->federation;
-		$zaehler++;
-	}
-
-	$zaehler = 0;
-	$zaehlerfed = 0;
-	asort($ausland);	
-
-
-//echo '<pre>'.print_r($ausland,true).'</pre>';	
-//echo '<pre>'.print_r($list,true).'</pre>';	
-//echo '<pre>'.print_r($auslandfed,true).'</pre>';
-//echo '<pre>'.print_r($countryfederation,true).'</pre>';
-	
-echo HTMLHelper::_('bootstrap.startTabSet', 'defaulttabsfederation', array('active' => 'show_table_0')); // Start tab set
-foreach ($federation as $keyfed => $valuefed) if ( $keyfed != 0 )
-{
-echo HTMLHelper::_('bootstrap.addTab', 'defaulttabsfederation', 'show_table_'.$zaehler, Text::_($valuefed->name));
-foreach ($countryfederation as $key => $value) if ( $value->federation == $keyfed  )
-{
+$routeFor = static function (object $row): string {
+    $query = http_build_query([
+        'option' => 'com_sportsmanagement',
+        'view' => 'ranking',
+        'cfg_which_database' => 0,
+        's' => 0,
+        'p' => $row->project_slug,
+        'type' => 0,
+        'r' => $row->roundcode,
+        'from' => 0,
+        'to' => 0,
+        'division' => 0,
+    ]);
+    return Route::_('index.php?' . $query, false);
+};
 ?>
-<div class="row">
-<?php	
-//echo $value->alpha3;
-foreach ($list as $row)
-						{
-							if ($row->country == $value->alpha3)
-							{
-								$createroute = array("option"             => "com_sportsmanagement",
-								                     "view"               => "ranking",
-								                     "cfg_which_database" => 0,
-								                     "s"                  => 0,
-								                     "p"                  => $row->project_slug,
-								                     "type"               => 0,
-								                     "r"                  => $row->roundcode,
-								                     "from"               => 0,
-								                     "to"                 => 0,
-								                     "division"           => 0,
-										    "Itemid"           => -1,);
-
-								$query = sportsmanagementHelperRoute::buildQuery($createroute);
-								$link  = Route::_('index.php?' . $query, false);
-								?>
-                                <div class="col-xl-4 col-lg-4 col-md-4 col-sm-4">
-                                    <a href="<?PHP echo $link; ?>"
-                                       class="<?PHP echo $params->get('button_class'); ?>  btn-block" role="button">
-<span>
-<?PHP
-echo JSMCountries::getCountryFlag($row->country);
-?>
-</span>
-										<?PHP
-										echo Text::_($row->name);
-										?>
-                                    </a>
-                                    <!-- </button> -->
-                                </div>
-								<?php
-							}
-						}
-?>
-</div>
-<?php
-}							
-echo HTMLHelper::_('bootstrap.endTab');
-$zaehler++;
-}
-?>
-
-
-
-
-<?php
-}
-else
-{
-	$start = 1;
-
-	foreach ($list as $row)
-	{
-		if ($start == 1)
-		{
-			?>
-            <div class="row-fluid">
-			<?PHP
-		}
-
-		$createroute = array("option"             => "com_sportsmanagement",
-		                     "view"               => "ranking",
-		                     "cfg_which_database" => 0,
-		                     "s"                  => 0,
-		                     "p"                  => $row->project_slug,
-		                     "type"               => 0,
-		                     "r"                  => $row->roundcode,
-		                     "from"               => 0,
-		                     "to"                 => 0,
-		                     "division"           => 0,);
-
-		$query = sportsmanagementHelperRoute::buildQuery($createroute);
-		$link  = Route::_('index.php?' . $query, false);
-		?>
-        <div class="col-sm-2">
-            <a href="<?PHP echo $link; ?>" class="<?PHP echo $params->get('button_class'); ?>  btn-block" role="button">
-		<span>
-		<?PHP
-		echo JSMCountries::getCountryFlag($row->country);
-		?>
-		</span>
-				<?PHP
-				echo Text::_($row->name);
-				?>
-            </a>
-            <!-- </button> -->
-        </div>
-		<?PHP
-		$start++;
-
-		if ($start == 7)
-		{
-			$start = 1;
-			?>
+<div class="<?php echo htmlspecialchars((string) $params->get('moduleclass_sfx', ''), ENT_QUOTES, 'UTF-8'); ?>" id="<?php echo htmlspecialchars($module->module . '-' . $module->id, ENT_QUOTES, 'UTF-8'); ?>">
+<?php if ($params->get('show_slider')) : ?>
+    <?php echo HTMLHelper::_('bootstrap.startTabSet', 'jsm-act-season-' . (int) $module->id, ['active' => 'fed-0']); ?>
+    <?php $tab = 0; foreach ($federations as $fedId => $federation) : ?>
+        <?php echo HTMLHelper::_('bootstrap.addTab', 'jsm-act-season-' . (int) $module->id, 'fed-' . $tab, Text::_($federation->name)); ?>
+        <?php foreach (($countriesByFederation[$fedId] ?? []) as $country) : ?>
+            <div class="row g-2 mb-2">
+                <?php foreach ($list as $row) : if ($row->country !== $country->alpha3) { continue; } ?>
+                    <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12">
+                        <a href="<?php echo htmlspecialchars($routeFor($row), ENT_QUOTES, 'UTF-8'); ?>" class="<?php echo htmlspecialchars((string) $params->get('button_class', 'btn btn-primary'), ENT_QUOTES, 'UTF-8'); ?> w-100" role="button">
+                            <?php echo $row->flag_html; ?> <?php echo Text::_($row->name); ?>
+                        </a>
+                    </div>
+                <?php endforeach; ?>
             </div>
-			<?PHP
-		}
-	}
-}
+        <?php endforeach; ?>
+        <?php echo HTMLHelper::_('bootstrap.endTab'); ?>
+    <?php $tab++; endforeach; ?>
+    <?php echo HTMLHelper::_('bootstrap.endTabSet'); ?>
+<?php else : ?>
+    <div class="row g-2">
+        <?php foreach ($list as $row) : ?>
+            <div class="col-xl-2 col-lg-3 col-md-4 col-sm-6">
+                <a href="<?php echo htmlspecialchars($routeFor($row), ENT_QUOTES, 'UTF-8'); ?>" class="<?php echo htmlspecialchars((string) $params->get('button_class', 'btn btn-primary'), ENT_QUOTES, 'UTF-8'); ?> w-100" role="button">
+                    <?php echo $row->flag_html; ?> <?php echo Text::_($row->name); ?>
+                </a>
+            </div>
+        <?php endforeach; ?>
+    </div>
+<?php endif; ?>
+</div>
--- modules/mod_sportsmanagement_count_rekord/tmpl/default.php
+++ modules/mod_sportsmanagement_count_rekord/tmpl/default.php
@@ -1,41 +1,14 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage mod_sportsmanagement_count_rekord
- * @file       default.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-defined('_JEXEC') or die('Restricted access');
-
+\defined('_JEXEC') or die;
 ?>
-<div class="<?php echo $module->module; ?>-<?php echo $module->id; ?>">
-	<?PHP
-	$list = modJSMStatistikRekordHelper::getData($params, $module);
-
-	?>
-
-
-    <table class="table">
-
-		<?PHP
-
-		if ($list)
-		{
-			foreach ($list as $row)
-			{
-				?>
-                <tr>
-                    <td width="50%" align="left"><?PHP echo $row->text; ?></td>
-                </tr>
-				<?PHP
-			}
-		}
-
-		?>
-
-    </table>
+<div class="<?php echo htmlspecialchars($module->module . '-' . $module->id, ENT_QUOTES, 'UTF-8'); ?>">
+    <?php if ($list) : ?>
+        <table class="table">
+            <tbody>
+            <?php foreach ($list as $row) : ?>
+                <tr><td><?php echo $row->text; ?></td></tr>
+            <?php endforeach; ?>
+            </tbody>
+        </table>
+    <?php endif; ?>
 </div>
