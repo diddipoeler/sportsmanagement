371d9d054582786fc8344e9a66bc30729a78397b
diddipoeler <diddipoeler@gmx.de>
2026-08-18 16:32:18 +0200

    Stage native sports type statistics dispatcher

---
+++ modules/mod_sportsmanagement_sports_type_statistics/src/Dispatcher/Dispatcher.php
@@ -0,0 +1,32 @@
+<?php
+namespace Diddipoeler\Module\SportsManagementSportsTypeStatistics\Site\Dispatcher;
+
+\defined('_JEXEC') or die;
+
+use Joomla\CMS\Dispatcher\AbstractModuleDispatcher;
+use Joomla\CMS\Helper\HelperFactoryAwareInterface;
+use Joomla\CMS\Helper\HelperFactoryAwareTrait;
+
+final class Dispatcher extends AbstractModuleDispatcher implements HelperFactoryAwareInterface
+{
+    use HelperFactoryAwareTrait;
+
+    protected function getLayoutData(): array
+    {
+        $data = parent::getLayoutData();
+        $this->getApplication()->getLanguage()->load('com_sportsmanagement', JPATH_ADMINISTRATOR, null, true);
+        $data['statistics'] = $this->getHelperFactory()
+            ->getHelper('SportsTypeStatisticsHelper')
+            ->getData($data['params'], $this->getApplication());
+
+        $document = $this->getApplication()->getDocument();
+        if (method_exists($document, 'getWebAssetManager')) {
+            $document->getWebAssetManager()->registerAndUseStyle(
+                'mod_sportsmanagement_sports_type_statistics',
+                'modules/mod_sportsmanagement_sports_type_statistics/css/mod_sportsmanagement_sports_type_statistics.css'
+            );
+        }
+
+        return $data;
+    }
+}
