b521e406d1c2cc1efcc698943948b826ccce6c07
diddipoeler <diddipoeler@gmx.de>
2026-08-18 21:52:55 +0200

    Bridge legacy site json feed model to native model

--- site/models/jsonfeed.php
+++ site/models/jsonfeed.php
@@ -1,84 +1,19 @@
 <?php
 /**
- * GCalendar is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * SportsManagement legacy compatibility bridge.
  *
- * GCalendar is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with GCalendar.  If not, see <http://www.gnu.org/licenses/>.
- *
- * @package   GCalendar
- * @author    Digital Peak http://www.digital-peak.com
- * @copyright Copyright (C) 2007 - 2013 Digital Peak. All rights reserved.
- * @license   http://www.gnu.org/licenses/gpl.html GNU/GPL
+ * The active Joomla 5/6 implementation lives in site/src/Model/JsonfeedModel.php.
  */
 
-defined('_JEXEC') or die();
+defined('_JEXEC') or die('Restricted access');
 
-use Joomla\CMS\Factory;
-use Joomla\CMS\Language\Text;
-use Joomla\CMS\MVC\Model\BaseDatabaseModel;
+use Diddipoeler\Component\SportsManagement\Site\Model\JsonfeedModel;
 
-class sportsmanagementModelJSONFeed extends BaseDatabaseModel
-{
+if (!class_exists(JsonfeedModel::class)) {
+    require_once JPATH_SITE . '/components/com_sportsmanagement/src/Model/SportsManagementModel.php';
+    require_once JPATH_SITE . '/components/com_sportsmanagement/src/Model/JsonfeedModel.php';
+}
 
-	public function getGoogleCalendarFeeds()
-	{
-		$app = Factory::getApplication();
-
-		$startDate = Factory::getApplication()->input->getVar('start', null, 'GET');
-		$endDate   = Factory::getApplication()->input->getVar('end', null, 'GET');
-
-		$calendarids = '';
-
-		if (Factory::getApplication()->input->getVar('gcids', null) != null)
-		{
-			if (is_array(Factory::getApplication()->input->getVar('gcids', null)))
-			{
-				$calendarids = Factory::getApplication()->input->getVar('gcids', null);
-			}
-			else
-			{
-				$calendarids = explode(',', Factory::getApplication()->input->getVar('gcids', null));
-			}
-		}
-		else
-		{
-			$calendarids = Factory::getApplication()->input->getVar('gcid', null);
-		}
-
-		$results = jsmGCalendarDBUtil::getCalendars($calendarids);
-
-		if (empty($results))
-		{
-			return null;
-		}
-
-		$calendars = array();
-
-		foreach ($results as $result)
-		{
-			if (empty($result->calendar_id))
-			{
-				continue;
-			}
-
-			$events = jsmGCalendarZendHelper::getEvents($result, $startDate, $endDate, 1000);
-
-			if ($events == null)
-			{
-				continue;
-			}
-
-			$calendars[] = $events;
-		}
-
-		return $calendars;
-	}
+if (!class_exists('sportsmanagementModelJSONFeed', false)) {
+    class_alias(JsonfeedModel::class, 'sportsmanagementModelJSONFeed');
 }
