a6819c84658ca212bd89dcda60fc5fd1ae5c5677
diddipoeler <diddipoeler@gmx.de>
2026-08-18 15:45:47 +0200

    Stage native quickicon module provider

---
+++ admin/modules/mod_sportsmanagement_quickicon/services/provider.php
@@ -0,0 +1,16 @@
+<?php
+\defined('_JEXEC') or die;
+
+use Joomla\CMS\Extension\Service\Provider\Module;
+use Joomla\CMS\Extension\Service\Provider\ModuleDispatcherFactory;
+use Joomla\DI\Container;
+use Joomla\DI\ServiceProviderInterface;
+
+return new class implements ServiceProviderInterface
+{
+    public function register(Container $container): void
+    {
+        $container->registerServiceProvider(new ModuleDispatcherFactory('\\Diddipoeler\\Module\\SportsManagementQuickIcon'));
+        $container->registerServiceProvider(new Module());
+    }
+};
