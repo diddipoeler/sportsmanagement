2e983bae8a4ae70a3f22bcc1d5adb9f9b96f5bc1
diddipoeler <diddipoeler@gmx.de>
2026-08-18 20:34:12 +0200

    Bridge legacy roster position form model

--- admin/models/rosterposition.php
+++ admin/models/rosterposition.php
@@ -1,73 +1,19 @@
 <?php
 /**
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage models
- * @file       rosterposition.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * SportsManagement legacy compatibility bridge.
+ *
+ * The active Joomla 5/6 implementation lives in admin/src/Model/RosterpositionModel.php.
  */
+
 defined('_JEXEC') or die('Restricted access');
 
-use Joomla\CMS\Factory;
-use Joomla\CMS\Language\Text;
-use Joomla\Registry\Registry;
+use Diddipoeler\Component\SportsManagement\Administrator\Model\RosterpositionModel;
 
-/**
- * sportsmanagementModelrosterposition
- *
- * @package
- * @author
- * @copyright diddi
- * @version   2014
- * @access    public
- */
-class sportsmanagementModelrosterposition extends JSMModelAdmin
-{
-	/**
-	 * Method to save the form data.
-	 *
-	 * @param array $data The form data.
-	 *
-	 * @return boolean True on success.
-	 * @since 1.6
-	 */
-	public function save($data)
-	{
-		$app    = Factory::getApplication();
-		$input  = $app->getInput();
-		$option = $input->getCmd('option', 'com_sportsmanagement');
-		$date   = Factory::getDate();
-		$user   = $app->getIdentity();
-		$post   = $input->post->getArray();
+if (!class_exists(RosterpositionModel::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Model/SportsManagementAdminModel.php';
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Model/RosterpositionModel.php';
+}
 
-		if (isset($post['extended']) && is_array($post['extended']))
-		{
-			$parameter = new Registry;
-			$parameter->loadArray($post['extended']);
-			$data['extended'] = (string) $parameter;
-		}
-
-		$data['modified']    = $date->toSql();
-		$data['modified_by'] = (int) $user->id;
-		$data['alias']       = $data['short_name'];
-
-		if (!parent::save($data))
-		{
-			return false;
-		}
-
-		$id         = (int) $this->getState($this->getName() . '.id');
-		$isNew      = $this->getState($this->getName() . '.new');
-		$data['id'] = $id;
-
-		if ($isNew)
-		{
-			$app->enqueueMessage(Text::plural(strtoupper($option) . '_N_ITEMS_CREATED', $id), '');
-		}
-
-		return true;
-	}
+if (!class_exists('sportsmanagementModelrosterposition', false)) {
+    class_alias(RosterpositionModel::class, 'sportsmanagementModelrosterposition');
 }
