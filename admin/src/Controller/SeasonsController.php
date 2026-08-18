a7bd36218ac747315e9c191a38b5dac1a2012e8e
diddipoeler <diddipoeler@gmx.de>
2026-08-18 20:57:34 +0200

    Migrate season assignment controller actions

--- admin/src/Controller/SeasonsController.php
+++ admin/src/Controller/SeasonsController.php
@@ -1,11 +1,62 @@
 <?php
 namespace Diddipoeler\Component\SportsManagement\Administrator\Controller;
+
 \defined('_JEXEC') or die;
-/** Native Joomla 5/6 list controller for seasons. */
+
+use Joomla\CMS\Router\Route;
+
 final class SeasonsController extends SportsManagementAdminController
 {
     public function getModel($name = 'Season', $prefix = 'Administrator', $config = [])
     {
         return parent::getModel($name, $prefix, ['ignore_request' => true]);
+    }
+
+    public function applypersons(): void
+    {
+        $post = $this->input->post->getArray();
+        $model = $this->getModel();
+        $message = (string) $model->saveshortpersons();
+
+        $url = 'index.php?option=com_sportsmanagement'
+            . '&tmpl=component'
+            . '&view=players'
+            . '&layout=assignpersons'
+            . '&season_id=' . (int) ($post['season_id'] ?? 0)
+            . '&team_id=' . (int) $this->scalarInput($post['team_id'] ?? 0)
+            . '&persontype=' . (int) ($post['persontype'] ?? 0)
+            . '&whichview=' . rawurlencode((string) ($post['whichview'] ?? ''));
+
+        $this->setRedirect(Route::_($url, false), $message);
+    }
+
+    public function applyteams(): void
+    {
+        $post = $this->input->post->getArray();
+        $model = $this->getModel();
+        $model->saveshortteams();
+
+        $url = 'index.php?option=com_sportsmanagement'
+            . '&tmpl=component'
+            . '&view=teams'
+            . '&layout=assignteams'
+            . '&season_id=' . (int) ($post['season_id'] ?? 0);
+
+        $this->setRedirect(Route::_($url, false));
+    }
+
+    private function scalarInput($value)
+    {
+        if (!is_array($value)) {
+            return $value;
+        }
+
+        foreach ($value as $candidate) {
+            if (is_scalar($candidate)) {
+                return $candidate;
+            }
+        }
+
+        return 0;
     }
 }
