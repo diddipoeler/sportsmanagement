f8a8f925b15367ac2498b3bd945b3663d83fb315
diddipoeler <diddipoeler@gmx.de>
2026-08-19 17:30:17 +0200

    Modernize country edit view input access

--- admin/views/jlextcountry/view.html.php
+++ admin/views/jlextcountry/view.html.php
@@ -1,47 +1,16 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage jlextcountry
- * @file       view.html.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
+/** SportsManagement administrator country edit view. */
 defined('_JEXEC') or die('Restricted access');
 
-/**
- * sportsmanagementViewJlextcountry
- *
- * @package
- * @author    diddi
- * @copyright 2014
- * @version   $Id$
- * @access    public
- */
 class sportsmanagementViewJlextcountry extends sportsmanagementView
 {
+    public function init()
+    {
+    }
 
-	/**
-	 * sportsmanagementViewJlextcountry::init()
-	 *
-	 * @return
-	 */
-	public function init()
-	{
-
-	}
-
-	/**
-	 * sportsmanagementViewJlextcountry::addToolBar()
-	 *
-	 * @return void
-	 */
-	protected function addToolBar()
-	{
-		$this->jinput->set('hidemainmenu', true);
-		parent::addToolbar();
-	}
-
+    protected function addToolBar()
+    {
+        $this->app->getInput()->set('hidemainmenu', true);
+        parent::addToolbar();
+    }
 }
