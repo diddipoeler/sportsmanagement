a903333e61dd9ff9388c0a323741ea49d32a8e9d
diddipoeler <diddipoeler@gmx.de>
2026-08-19 16:11:27 +0200

    Add native rounds admin controller

---
+++ admin/src/Controller/RoundsController.php
@@ -0,0 +1,72 @@
+<?php
+namespace Diddipoeler\Component\SportsManagement\Administrator\Controller;
+
+\defined('_JEXEC') or die;
+
+use Joomla\CMS\Router\Route;
+
+/**
+ * Native Joomla 5/6 administrator controller for project rounds.
+ */
+final class RoundsController extends SportsManagementAdminController
+{
+    protected $view_list = 'rounds';
+
+    public function getModel($name = 'Round', $prefix = 'Administrator', $config = [])
+    {
+        return parent::getModel($name, $prefix, ['ignore_request' => true]);
+    }
+
+    public function massadd(): void
+    {
+        $this->checkToken();
+
+        $message = $this->getModel()->massadd($this->app->getInput()->post->getArray());
+        $this->setRedirect(
+            'index.php?option=com_sportsmanagement&view=close&tmpl=component',
+            $message
+        );
+    }
+
+    public function deleteRoundMatches(): void
+    {
+        $this->checkToken();
+
+        $pks = $this->app->getInput()->post->get('cid', [], 'array');
+        $model = $this->getModel();
+        $success = $model->deleteRoundMatches($pks);
+        $message = $success ? '' : $model->getError();
+
+        $this->setRedirect(
+            Route::_('index.php?option=com_sportsmanagement&view=rounds', false),
+            $message,
+            $success ? 'message' : 'error'
+        );
+    }
+
+    public function saveshort(): void
+    {
+        $this->checkToken();
+
+        $input = $this->app->getInput();
+        $projectId = (int) $this->app->getUserState('com_sportsmanagement.pid', 0);
+        $message = $this->getModel()->saveshort(
+            $input->get('cid', [], 'array'),
+            $input->post->getArray()
+        );
+
+        $this->setRedirect(
+            'index.php?option=com_sportsmanagement&view=rounds&pid=' . $projectId,
+            $message,
+            $message === false ? 'error' : 'message'
+        );
+    }
+
+    public function populate(): void
+    {
+        $divisionId = $this->app->getInput()->getInt('division_id', 0);
+        $this->setRedirect(
+            'index.php?option=com_sportsmanagement&view=rounds&layout=populate&division_id=' . $divisionId
+        );
+    }
+}
