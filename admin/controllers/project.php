ae887b142afc342a514f9db5d589991167efbd4a
diddipoeler <diddipoeler@gmx.de>
2026-08-19 23:37:01 +0200

    Bridge legacy project form controller

--- admin/controllers/project.php
+++ admin/controllers/project.php
@@ -1,27 +1,14 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage controllers
- * @file       project.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
+/** Legacy compatibility bridge for the native administrator project controller. */
 defined('_JEXEC') or die('Restricted access');
 
-/**
- * sportsmanagementControllerproject
- *
- * @package
- * @author
- * @copyright diddi
- * @version   2014
- * @access    public
- */
-class sportsmanagementControllerproject extends JSMControllerForm
-{
+use Diddipoeler\Component\SportsManagement\Administrator\Controller\ProjectController;
 
+if (!class_exists(ProjectController::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Controller/SportsManagementFormController.php';
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Controller/ProjectController.php';
+}
 
+if (!class_exists('sportsmanagementControllerproject', false)) {
+    class_alias(ProjectController::class, 'sportsmanagementControllerproject');
 }
