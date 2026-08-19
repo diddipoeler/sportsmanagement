5e57f821042faf092e475aac0ddbd6bbc61ebbc7
diddipoeler <diddipoeler@gmx.de>
2026-08-19 17:41:37 +0200

    Add native Google calendar form model

---
+++ admin/src/Model/JsmgcalendarModel.php
@@ -0,0 +1,106 @@
+<?php
+namespace Diddipoeler\Component\SportsManagement\Administrator\Model;
+
+\defined('_JEXEC') or die;
+
+use Joomla\CMS\Factory;
+use Joomla\CMS\Form\Form;
+use Joomla\CMS\Table\Table;
+use Joomla\Registry\Registry;
+
+/** Native Joomla 5/6 administrator form model for Google calendars. */
+final class JsmgcalendarModel extends SportsManagementAdminModel
+{
+    public function getForm($data = [], $loadData = true)
+    {
+        Form::addFormPath(JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/forms');
+        Form::addFormPath(JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/models/forms');
+
+        return $this->loadForm(
+            'com_sportsmanagement.jsmgcalendar',
+            'jsmgcalendar',
+            ['control' => 'jform', 'load_data' => $loadData]
+        );
+    }
+
+    public function getTable($type = 'jsmGCalendar', $prefix = 'sportsmanagementTable', $config = [])
+    {
+        $config['dbo'] = $this->getDatabase();
+
+        return Table::getInstance($type, $prefix, $config);
+    }
+
+    public function save($data)
+    {
+        if (empty($data['id']) && !$this->writeCreateCalendarTemplate($data)) {
+            return false;
+        }
+
+        return parent::save($data);
+    }
+
+    protected function prepareSportsManagementData(array $data): array
+    {
+        $extended = Factory::getApplication()->getInput()->post->get('extended', [], 'array');
+
+        if ($extended) {
+            $params = new Registry();
+            $params->loadArray($extended);
+            $data['params'] = $params->toString();
+        }
+
+        return $data;
+    }
+
+    protected function allowEdit($data = [], $key = 'id')
+    {
+        $id = (int) ($data[$key] ?? 0);
+
+        return Factory::getApplication()->getIdentity()->authorise(
+            'core.edit',
+            'com_sportsmanagement.calendar.' . $id
+        ) || parent::allowEdit($data, $key);
+    }
+
+    protected function loadFormData()
+    {
+        $data = Factory::getApplication()->getUserState(
+            'com_sportsmanagement.edit.jsmGCalendar.data',
+            []
+        );
+
+        if (empty($data)) {
+            $data = $this->getItem();
+        }
+
+        return $data;
+    }
+
+    private function writeCreateCalendarTemplate(array $data): bool
+    {
+        $tmpDir = JPATH_SITE . DIRECTORY_SEPARATOR . 'tmp';
+
+        if (!is_dir($tmpDir) && !mkdir($tmpDir, 0755, true) && !is_dir($tmpDir)) {
+            return false;
+        }
+
+        $offset = (string) Factory::getApplication()->get('offset', 'UTC');
+        $color = ltrim((string) ($data['color'] ?? ''), '#');
+        $xml = "<entry xmlns='http://www.w3.org/2005/Atom'\n"
+            . "xmlns:gd='http://schemas.google.com/g/2005'\n"
+            . "xmlns:gCal='http://schemas.google.com/gCal/2005'>\n"
+            . "<title type='text'>[TITLE]</title>\n"
+            . "<summary type='text'>[SUMMARY]</summary>\n"
+            . "<gCal:timezone value='" . htmlspecialchars($offset, ENT_QUOTES, 'UTF-8') . "'></gCal:timezone>\n"
+            . "<gCal:hidden value='false'></gCal:hidden>\n"
+            . "<gCal:color value='#" . htmlspecialchars($color, ENT_QUOTES, 'UTF-8') . "'></gCal:color>\n"
+            . "<gd:where rel='' label='' valueString='Oakland'></gd:where>\n"
+            . "</entry>\n";
+
+        return file_put_contents(
+            $tmpDir . DIRECTORY_SEPARATOR . 'createcal.xml',
+            $xml,
+            LOCK_EX
+        ) !== false;
+    }
+}
