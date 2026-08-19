835cbf3958ea66a127a580c704f4195a73ea6dc3
diddipoeler <diddipoeler@gmx.de>
2026-08-19 13:52:27 +0200

    Bridge legacy sportsmanagement form model

--- admin/models/sportsmanagement.php
+++ admin/models/sportsmanagement.php
@@ -1,114 +1,14 @@
 <?php
-/**
- *
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage models
- * @file       sportsmanagement.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-
+/** Legacy compatibility bridge for the native administrator Sportsmanagement form model. */
 defined('_JEXEC') or die('Restricted access');
 
-use Joomla\CMS\Factory;
-use Joomla\CMS\MVC\Model\AdminModel;
-use Joomla\CMS\Table\Table;
+use Diddipoeler\Component\SportsManagement\Administrator\Model\SportsmanagementModel;
 
-/**
- * SportsManagement Model
- */
-class sportsmanagementModelsportsmanagement extends AdminModel
-{
-	/**
-	 * Returns a Table object, always creating it.
-	 *
-	 * @param string $type   The table type to instantiate.
-	 * @param string $prefix A prefix for the table class name.
-	 * @param array  $config Configuration array for the table.
-	 *
-	 * @return Table
-	 * @since  1.6
-	 */
-	public function getTable($type = 'sportsmanagement', $prefix = 'sportsmanagementTable', $config = array())
-	{
-		$config['dbo'] = sportsmanagementHelper::getDBConnection();
+if (!class_exists(SportsmanagementModel::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Model/SportsManagementAdminModel.php';
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Model/SportsmanagementModel.php';
+}
 
-		return Table::getInstance($type, $prefix, $config);
-	}
-
-	/**
-	 * Method to get the record form.
-	 *
-	 * @param array   $data     Data for the form.
-	 * @param boolean $loadData True if the form is to load its own data.
-	 *
-	 * @return mixed A form object on success, false on failure.
-	 * @since  1.6
-	 */
-	public function getForm($data = array(), $loadData = true)
-	{
-		$form = $this->loadForm(
-			'com_sportsmanagement.sportsmanagement',
-			'sportsmanagement',
-			array('control' => 'jform', 'load_data' => $loadData)
-		);
-
-		if (empty($form))
-		{
-			return false;
-		}
-
-		return $form;
-	}
-
-	/**
-	 * Method to get the script that has to be included on the form.
-	 *
-	 * @return string Script file.
-	 */
-	public function getScript()
-	{
-		return 'administrator/components/com_sportsmanagement/models/forms/sportsmanagement.js';
-	}
-
-	/**
-	 * Method override to check if an existing record can be edited.
-	 *
-	 * @param array  $data Input data.
-	 * @param string $key  Primary key field.
-	 *
-	 * @return boolean
-	 * @since  1.6
-	 */
-	protected function allowEdit($data = array(), $key = 'id')
-	{
-		$id = (int) ($data[$key] ?? 0);
-
-		return Factory::getApplication()->getIdentity()->authorise(
-			'core.edit',
-			'com_sportsmanagement.message.' . $id
-		) || parent::allowEdit($data, $key);
-	}
-
-	/**
-	 * Method to get the data that should be injected in the form.
-	 *
-	 * @return mixed The data for the form.
-	 * @since  1.6
-	 */
-	protected function loadFormData()
-	{
-		$data = Factory::getApplication()->getUserState('com_sportsmanagement.edit.sportsmanagement.data', array());
-
-		if (empty($data))
-		{
-			$data = $this->getItem();
-		}
-
-		return $data;
-	}
+if (!class_exists('sportsmanagementModelsportsmanagement', false)) {
+    class_alias(SportsmanagementModel::class, 'sportsmanagementModelsportsmanagement');
 }
