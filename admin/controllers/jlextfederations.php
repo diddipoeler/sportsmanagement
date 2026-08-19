b637c05f48702c9093ac263c1b73a07cad6b02f9
diddipoeler <diddipoeler@gmx.de>
2026-08-19 17:04:06 +0200

    Bridge legacy federations admin controller

--- admin/controllers/jlextfederations.php
+++ admin/controllers/jlextfederations.php
@@ -1,42 +1,14 @@
 <?php
-/**
- *
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage controllers
- * @file       jlextfederations.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-
-
+/** Legacy compatibility bridge for the native administrator federations controller. */
 defined('_JEXEC') or die('Restricted access');
 
-/**
- * sportsmanagementControllerjlextfederations
- *
- * @package
- * @author    diddi
- * @copyright 2014
- * @version   $Id$
- * @access    public
- */
-class sportsmanagementControllerjlextfederations extends JSMControllerAdmin
-{
+use Diddipoeler\Component\SportsManagement\Administrator\Controller\JlextfederationsController;
 
-	/**
-	 * Proxy for getModel.
-	 *
-	 * @since 1.6
-	 */
-	public function getModel($name = 'jlextfederation', $prefix = 'sportsmanagementModel', $config = Array())
-	{
-		$model = parent::getModel($name, $prefix, array('ignore_request' => true));
+if (!class_exists(JlextfederationsController::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Controller/SportsManagementAdminController.php';
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Controller/JlextfederationsController.php';
+}
 
-		return $model;
-	}
-
+if (!class_exists('sportsmanagementControllerjlextfederations', false)) {
+    class_alias(JlextfederationsController::class, 'sportsmanagementControllerjlextfederations');
 }
