4c873a16d8b8de504129d95e152a2518b4e93fbc
diddipoeler <diddipoeler@gmx.de>
2026-08-19 08:44:35 +0200

    Use native input in stats ranking template

--- site/views/statsranking/tmpl/default_stats.php
+++ site/views/statsranking/tmpl/default_stats.php
@@ -13,226 +13,219 @@
  */
 
 defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\Pagination\Pagination;
 
+use Joomla\CMS\Pagination\Pagination;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\HTML\HTMLHelper;
-use Joomla\CMS\Factory;
 
-?>
-
-<?php
-$colspan    = 4;
+$input = $this->input;
+$colspan = 4;
 $show_icons = 0;
-if ($this->config['show_picture_thumb'] == 1)
+if (($this->config['show_picture_thumb'] ?? 0) == 1)
 {
-	$colspan++;
+    $colspan++;
 }
-if ($this->config['show_nation'] == 1)
+if (($this->config['show_nation'] ?? 0) == 1)
 {
-	$colspan++;
+    $colspan++;
 }
-if ($this->config['show_icons'] == 1)
+if (($this->config['show_icons'] ?? 0) == 1)
 {
-	$show_icons = 1;
+    $show_icons = 1;
 }
 ?>
 
-<?php foreach ($this->stats AS $rows): ?>
-	<?php if ($this->multiple_stats == 1) : ?>
-        <h2><?php echo $rows->name; ?></h2>
-	<?php endif; ?>
-    <table class="<?php echo $this->config['table_class']; ?>">
+<?php foreach ($this->stats as $rows): ?>
+    <?php if ($this->multiple_stats == 1) : ?>
+        <h2><?php echo Text::_($rows->name); ?></h2>
+    <?php endif; ?>
+    <table class="<?php echo $this->config['table_class'] ?? 'table'; ?>">
         <thead>
         <tr class="sectiontableheader">
             <th class="td_r rank"><?php echo Text::_('COM_SPORTSMANAGEMENT_STATSRANKING_RANK'); ?></th>
 
-			<?php if ($this->config['show_picture_thumb'] == 1) : ?>
+            <?php if (($this->config['show_picture_thumb'] ?? 0) == 1) : ?>
                 <th class="td_c">&nbsp;</th>
-			<?php endif; ?>
+            <?php endif; ?>
 
-            <th class="td_l"><?php echo Text::_('COM_SPORTSMANAGEMENT_STATSRANKING_PLAYER_NAME'); ?>
-            </th>
+            <th class="td_l"><?php echo Text::_('COM_SPORTSMANAGEMENT_STATSRANKING_PLAYER_NAME'); ?></th>
 
-			<?php if ($this->config['show_nation'] == 1) : ?>
+            <?php if (($this->config['show_nation'] ?? 0) == 1) : ?>
                 <th class="td_c">&nbsp;</th>
-			<?php endif; ?>
+            <?php endif; ?>
 
-			<?php if ($this->config['show_team'] == 1) : ?>
+            <?php if (($this->config['show_team'] ?? 0) == 1) : ?>
                 <th class="td_l"><?php echo Text::_('COM_SPORTSMANAGEMENT_STATSRANKING_TEAM'); ?></th>
-			<?php endif; ?>
-			<?php if ($show_icons == 1) : ?>
-                <th class="td_r" class="nowrap"><?php echo $rows->getImage(); ?></th>
-			<?php else: ?>
-                <th class="td_r" class="nowrap"><?php echo Text::_($rows->name); ?></th>
-			<?php endif; ?>
+            <?php endif; ?>
+            <?php if ($show_icons == 1) : ?>
+                <th class="td_r nowrap"><?php echo $rows->getImage(); ?></th>
+            <?php else: ?>
+                <th class="td_r nowrap"><?php echo Text::_($rows->name); ?></th>
+            <?php endif; ?>
         </tr>
         </thead>
         <tbody>
-		<?php
-		if (count($this->playersstats[$rows->id]->ranking) > 0)
-		{
-			$k        = 0;
-			$lastrank = 0;
-			foreach ((array) $this->playersstats[$rows->id]->ranking as $row)
-			{
+        <?php
+        $ranking = $this->playersstats[$rows->id]->ranking ?? [];
+        if (count((array) $ranking) > 0)
+        {
+            $lastrank = 0;
+            foreach ((array) $ranking as $row)
+            {
+                $rank = $lastrank == $row->rank ? '-' : $row->rank;
+                $lastrank = $row->rank;
 
-				if ($lastrank == $row->rank)
-				{
-					$rank = '-';
-				}
-				else
-				{
-					$rank = $row->rank;
-				}
-				$lastrank = $row->rank;
+                $favStyle = '';
+                $isFavTeam = in_array((int) $row->team_id, $this->favteams, true);
+                if (($this->config['highlight_fav'] ?? 0) == 1 && $isFavTeam && ($this->project->fav_team_highlight_type ?? 0) == 1)
+                {
+                    $favStyle = ' style="';
+                    $favStyle .= !empty($this->project->fav_team_text_bold) ? 'font-weight:bold;' : '';
+                    $favStyle .= trim((string) ($this->project->fav_team_text_color ?? '')) !== ''
+                        ? 'color:' . trim((string) $this->project->fav_team_text_color) . ';'
+                        : '';
+                    $favStyle .= trim((string) ($this->project->fav_team_color ?? '')) !== ''
+                        ? 'background-color:' . trim((string) $this->project->fav_team_color) . ';'
+                        : '';
+                    $favStyle = $favStyle !== ' style="' ? $favStyle . '"' : '';
+                }
 
-
-				$favStyle  = '';
-				$isFavTeam = in_array($row->team_id, $this->favteams);
-				if ($this->config['highlight_fav'] == 1 && $isFavTeam && $this->project->fav_team_highlight_type == 1)
-				{
-					$format   = "%s";
-					$favStyle = ' style="';
-					$favStyle .= ($this->project->fav_team_text_bold != '') ? 'font-weight:bold;' : '';
-					$favStyle .= (trim($this->project->fav_team_text_color) != '') ? 'color:' . trim($this->project->fav_team_text_color) . ';' : '';
-					$favStyle .= (trim($this->project->fav_team_color) != '') ? 'background-color:' . trim($this->project->fav_team_color) . ';' : '';
-					if ($favStyle != ' style="')
-					{
-						$favStyle .= '"';
-					}
-					else
-					{
-						$favStyle = '';
-					}
-				}
-
-				?>
-
-                <tr class=""<?php echo $favStyle; ?>>
+                $playerName = sportsmanagementHelper::formatName(
+                    null,
+                    $row->firstname,
+                    $row->nickname,
+                    $row->lastname,
+                    $this->config['name_format'] ?? ''
+                );
+                ?>
+                <tr<?php echo $favStyle; ?>>
                     <td class="td_r rank"><?php echo $rank; ?></td>
-					<?php $playerName = sportsmanagementHelper::formatName(null, $row->firstname, $row->nickname, $row->lastname, $this->config["name_format"]); ?>
-					<?php if ($this->config['show_picture_thumb'] == 1) : ?>
+
+                    <?php if (($this->config['show_picture_thumb'] ?? 0) == 1) : ?>
                         <td class="td_c playerpic">
-							<?php
-							$picture = isset($row->teamplayerpic) ? $row->teamplayerpic : null;
-							if ((empty($picture)) || ($picture == sportsmanagementHelper::getDefaultPlaceholder("player")))
-							{
-								$picture = $row->picture;
-							}
-							if (!file_exists($picture))
-							{
-								$picture = sportsmanagementHelper::getDefaultPlaceholder("player");
-							}
+                            <?php
+                            $picture = $row->teamplayerpic ?? null;
+                            if (empty($picture) || $picture == sportsmanagementHelper::getDefaultPlaceholder('player'))
+                            {
+                                $picture = $row->picture ?? '';
+                            }
+                            if (!$picture || !file_exists($picture))
+                            {
+                                $picture = sportsmanagementHelper::getDefaultPlaceholder('player');
+                            }
 
-							echo sportsmanagementHelperHtml::getBootstrapModalImage(
-								'person' . $row->person_id,
-								$picture,
-								$playerName,
-								$this->config['player_picture_width'],
-								'',
-								$this->modalwidth,
-								$this->modalheight,
-								$this->overallconfig['use_jquery_modal']
-							);
-							?>
-
+                            echo sportsmanagementHelperHtml::getBootstrapModalImage(
+                                'person' . $row->person_id,
+                                $picture,
+                                $playerName,
+                                $this->config['player_picture_width'] ?? 40,
+                                '',
+                                $this->modalwidth,
+                                $this->modalheight,
+                                $this->overallconfig['use_jquery_modal'] ?? 0
+                            );
+                            ?>
                         </td>
-					<?php endif; ?>
+                    <?php endif; ?>
 
                     <td class="td_l playername">
-						<?php
-						if ($this->config['link_to_player'] == 1)
-						{
-							$routeparameter                       = array();
-							$routeparameter['cfg_which_database'] = Factory::getApplication()->input->get('cfg_which_database', 0);
-							$routeparameter['s']                  = Factory::getApplication()->input->get('s', '');
-							$routeparameter['p']                  = $this->project->id;
-							$routeparameter['tid']                = $row->team_id;
-							$routeparameter['pid']                = $row->person_id;
-							$link                                 = sportsmanagementHelperRoute::getSportsmanagementRoute('player', $routeparameter);
-							//$link = sportsmanagementHelperRoute::getPlayerRoute( $this->project->id, $row->team_id, $row->person_id );
-							echo HTMLHelper::link($link, $playerName);
-						}
-						else
-						{
-							echo $playerName;
-						}
-						?>
+                        <?php
+                        if (($this->config['link_to_player'] ?? 0) == 1)
+                        {
+                            $routeparameter = [];
+                            $routeparameter['cfg_which_database'] = $input->getInt('cfg_which_database', 0);
+                            $routeparameter['s'] = $input->get('s', '');
+                            $routeparameter['p'] = $this->project->id;
+                            $routeparameter['tid'] = $row->team_id;
+                            $routeparameter['pid'] = $row->person_id;
+                            $link = sportsmanagementHelperRoute::getSportsmanagementRoute('player', $routeparameter);
+                            echo HTMLHelper::link($link, $playerName);
+                        }
+                        else
+                        {
+                            echo $playerName;
+                        }
+                        ?>
                     </td>
 
-					<?php if ($this->config['show_nation'] == 1) : ?>
+                    <?php if (($this->config['show_nation'] ?? 0) == 1) : ?>
                         <td class="td_c playercountry"><?php echo JSMCountries::getCountryFlag($row->country); ?></td>
-					<?php endif; ?>
+                    <?php endif; ?>
 
-					<?php if ($this->config['show_team'] == 1) : ?>
+                    <?php if (($this->config['show_team'] ?? 0) == 1) : ?>
                         <td class="td_l playerteam">
-							<?php
-							$team = $this->teams[$row->team_id];
-							if (($this->config['link_to_team'] == 1) && ($this->project->id > 0) && ($row->team_id > 0))
-							{
-								$routeparameter                       = array();
-								$routeparameter['cfg_which_database'] = Factory::getApplication()->input->get('cfg_which_database', 0);
-								$routeparameter['s']                  = Factory::getApplication()->input->get('s', '');
-								$routeparameter['p']                  = $this->project->id;
-								$routeparameter['tid']                = $row->team_id;
-								$routeparameter['ptid']               = 0;
-								$routeparameter['division']           = 0;
-								$link                                 = sportsmanagementHelperRoute::getSportsmanagementRoute('teaminfo', $routeparameter);
-								//$link = sportsmanagementHelperRoute::getTeamInfoRoute( $this->project->id, $row->team_id  );
-							}
-							else
-							{
-								$link = null;
-							}
-							$teamName = sportsmanagementHelper::formatTeamName($team, 't' . $row->team_id . 'st' . $rows->id . 'p' . $row->person_id, $this->config, $isFavTeam, $link);
-							echo $teamName;
-							?>
+                            <?php
+                            $teamId = (int) $row->team_id;
+                            $team = $this->teams[$teamId] ?? null;
+                            if ($team)
+                            {
+                                if (($this->config['link_to_team'] ?? 0) == 1 && ($this->project->id ?? 0) > 0 && $teamId > 0)
+                                {
+                                    $routeparameter = [];
+                                    $routeparameter['cfg_which_database'] = $input->getInt('cfg_which_database', 0);
+                                    $routeparameter['s'] = $input->get('s', '');
+                                    $routeparameter['p'] = $this->project->id;
+                                    $routeparameter['tid'] = $teamId;
+                                    $routeparameter['ptid'] = 0;
+                                    $routeparameter['division'] = 0;
+                                    $link = sportsmanagementHelperRoute::getSportsmanagementRoute('teaminfo', $routeparameter);
+                                }
+                                else
+                                {
+                                    $link = null;
+                                }
+
+                                echo sportsmanagementHelper::formatTeamName(
+                                    $team,
+                                    't' . $teamId . 'st' . $rows->id . 'p' . $row->person_id,
+                                    $this->config,
+                                    $isFavTeam,
+                                    $link
+                                );
+                            }
+                            ?>
                         </td>
-					<?php endif; ?>
+                    <?php endif; ?>
 
                     <td class="td_r playertotal"><?php echo $row->total; ?></td>
                 </tr>
-				<?php
-				$k = (1 - $k);
-			}
-		}
-		?>
+                <?php
+            }
+        }
+        ?>
         </tbody>
     </table>
 
-	<?php
-	if ($this->multiple_stats == 1)
-	{
-		?>
+    <?php
+    if ($this->multiple_stats == 1)
+    {
+        $routeparameter = [];
+        $routeparameter['cfg_which_database'] = $input->getInt('cfg_which_database', 0);
+        $routeparameter['s'] = $input->get('s', '');
+        $routeparameter['p'] = $this->project->id;
+        $routeparameter['division'] = (int) ($this->division->id ?? 0);
+        $routeparameter['tid'] = $this->teamid;
+        $link = sportsmanagementHelperRoute::getSportsmanagementRoute('statsranking', $routeparameter);
+        ?>
         <div class="fulltablelink">
-			<?php
-			$routeparameter                       = array();
-			$routeparameter['cfg_which_database'] = Factory::getApplication()->input->get('cfg_which_database', 0);
-			$routeparameter['s']                  = Factory::getApplication()->input->get('s', '');
-			$routeparameter['p']                  = $this->project->id;
-			$routeparameter['division']           = $this->division->id;
-			$routeparameter['tid']                = $this->teamid;
-			$link                                 = sportsmanagementHelperRoute::getSportsmanagementRoute('statsranking', $routeparameter);
-			echo HTMLHelper::link($link, Text::_('COM_SPORTSMANAGEMENT_STATSRANKING_VIEW_FULL_TABLE'));
-			?>
+            <?php echo HTMLHelper::link($link, Text::_('COM_SPORTSMANAGEMENT_STATSRANKING_VIEW_FULL_TABLE')); ?>
         </div>
-		<?php
-	}
-	else
-	{
-		jimport('joomla.html.pagination');
-		$pagination = new Pagination($this->playersstats[$rows->id]->pagination_total, $this->limitstart, $this->limit);
-		?>
+        <?php
+    }
+    else
+    {
+        $statResult = $this->playersstats[$rows->id] ?? null;
+        $paginationTotal = (int) ($statResult->pagination_total ?? 0);
+        $pagination = new Pagination($paginationTotal, $this->limitstart, $this->limit);
+        ?>
         <div class="pageslinks">
-			<?php echo $pagination->getPagesLinks(); ?>
+            <?php echo $pagination->getPagesLinks(); ?>
         </div>
-
         <p class="pagescounter">
-			<?php echo $pagination->getPagesCounter(); ?>
+            <?php echo $pagination->getPagesCounter(); ?>
         </p>
-		<?php
-	}
-	?>
+        <?php
+    }
+    ?>
 
 <?php endforeach; ?>
