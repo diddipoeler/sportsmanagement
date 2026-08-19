427108290c94b51aa702723fb2a387b72e5d4b3e
diddipoeler <diddipoeler@gmx.de>
2026-08-19 14:01:03 +0200

    Add native installhelper controller

---
+++ admin/src/Controller/InstallhelperController.php
@@ -0,0 +1,35 @@
+<?php
+namespace Diddipoeler\Component\SportsManagement\Administrator\Controller;
+
+\defined('_JEXEC') or die;
+
+/**
+ * Native Joomla 5/6 controller for the installation helper wizard.
+ */
+final class InstallhelperController extends SportsManagementFormController
+{
+    public function savesportstype(): void
+    {
+        $this->checkToken();
+
+        $post = $this->input->post->getArray();
+        $model = $this->getModel('Installhelper', 'Administrator', ['ignore_request' => true]);
+        $warnings = $model->saveSportstype($post);
+
+        if ($warnings) {
+            foreach ($warnings as $warning) {
+                $this->app->enqueueMessage($warning, 'warning');
+            }
+
+            $this->setRedirect(
+                'index.php?option=com_sportsmanagement&view=installhelper&step=1'
+            );
+
+            return;
+        }
+
+        $this->setRedirect(
+            'index.php?option=com_sportsmanagement&view=installhelper&step=2'
+        );
+    }
+}
