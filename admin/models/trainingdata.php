008c6d97964487794ec6aaf1a2778766008217ed
diddipoeler <diddipoeler@gmx.de>
2026-08-19 14:03:14 +0200

    Bridge legacy trainingdata model

--- admin/models/trainingdata.php
+++ admin/models/trainingdata.php
@@ -1,82 +1,14 @@
 <?php
-/**
- *
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage models
- * @file       trainingdata.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-
+/** Legacy compatibility bridge for the native administrator Trainingdata model. */
 defined('_JEXEC') or die('Restricted access');
 
-use Joomla\CMS\Factory;
-use Joomla\CMS\Language\Text;
-use Joomla\CMS\Table\Table;
+use Diddipoeler\Component\SportsManagement\Administrator\Model\TrainingdataModel;
 
-/**
- * sportsmanagementModeltrainingdata
- *
- * @package
- * @author    diddi
- * @copyright 2014
- * @version   $Id$
- * @access    public
- */
-class sportsmanagementModeltrainingdata extends JSMModelAdmin
-{
-	/**
-	 * Returns a reference to a Table object, always creating it.
-	 *
-	 * @param string $type   The table type to instantiate.
-	 * @param string $prefix A prefix for the table class name.
-	 * @param array  $config Configuration array for model.
-	 *
-	 * @return Table
-	 * @since 1.6
-	 */
-	public function getTable($type = 'TeamTrainingData', $prefix = 'sportsmanagementTable', $config = array())
-	{
-		$config['dbo'] = sportsmanagementHelper::getDBConnection();
+if (!class_exists(TrainingdataModel::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Model/SportsManagementAdminModel.php';
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Model/TrainingdataModel.php';
+}
 
-		return Table::getInstance($type, $prefix, $config);
-	}
-
-	/**
-	 * Method to save the form data.
-	 *
-	 * @param array $data The form data.
-	 *
-	 * @return boolean True on success.
-	 * @since 1.6
-	 */
-	public function save($data)
-	{
-		$app  = Factory::getApplication();
-		$date = Factory::getDate();
-		$user = $app->getIdentity();
-
-		$data['modified']    = $date->toSql();
-		$data['modified_by'] = (int) $user->id;
-
-		if (!parent::save($data))
-		{
-			return false;
-		}
-
-		$id         = (int) $this->getState($this->getName() . '.id');
-		$isNew      = $this->getState($this->getName() . '.new');
-		$data['id'] = $id;
-
-		if ($isNew)
-		{
-			$this->jsmapp->enqueueMessage(Text::plural(strtoupper($this->jsmoption) . '_N_ITEMS_CREATED', $id), '');
-		}
-
-		return true;
-	}
+if (!class_exists('sportsmanagementModeltrainingdata', false)) {
+    class_alias(TrainingdataModel::class, 'sportsmanagementModeltrainingdata');
 }
