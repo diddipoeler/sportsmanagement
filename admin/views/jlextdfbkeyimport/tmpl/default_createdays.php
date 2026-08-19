8b0211f92e2956c43177de52f35dd3a1a9296016
diddipoeler <diddipoeler@gmx.de>
2026-08-19 16:54:14 +0200

    Add CSRF token to DFB round creation form

--- admin/views/jlextdfbkeyimport/tmpl/default_createdays.php
+++ admin/views/jlextdfbkeyimport/tmpl/default_createdays.php
@@ -1,122 +1,67 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage jlextdfbkeyimport
- * @file       default_createdays.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
+/** SportsManagement DFB-key round creation template. */
 defined('_JEXEC') or die('Restricted access');
+
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
-use Joomla\CMS\Factory;
 
-$templatesToLoad = array('footer', 'listheader');
+$templatesToLoad = ['footer', 'listheader'];
 sportsmanagementHelper::addTemplatePaths($templatesToLoad, $this);
 $this->tips[] = Text::_('COM_SPORTSMANAGEMENT_ADMIN_DFBKEYS_ERROR_3');
 $this->tips[] = Text::_('COM_SPORTSMANAGEMENT_ADMIN_DFBKEYS_ERROR_4');
 echo $this->loadTemplate('jsm_notes');
 echo $this->loadTemplate('jsm_tips');
-    
 ?>
 <form action="<?php echo $this->request_url; ?>" method="post" name="adminForm" id="adminForm">
     <div id="editcell">
         <fieldset class="adminform">
-            <legend>
-				<?php
-				echo Text::sprintf('COM_SPORTSMANAGEMENT_ADMIN_DFBKEYS_MATCHDAY_INFO_2', $this->project_id);
-				?>
-            </legend>
-
+            <legend><?php echo Text::sprintf('COM_SPORTSMANAGEMENT_ADMIN_DFBKEYS_MATCHDAY_INFO_2', $this->project_id); ?></legend>
             <table class="<?php echo $this->table_data_class; ?>" id="<?php echo $this->view; ?>list">
                 <thead>
                 <tr>
-                    <th class="title" nowrap="nowrap" style="vertical-align:top; ">
-						<?PHP echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_DFBKEYS_MATCHDAY_INFO_3'); ?>
-                    </th>
-                    <th class="title" nowrap="nowrap" style="vertical-align:top; ">
-						<?PHP echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_DFBKEYS_MATCHDAY_INFO_4'); ?>
-                    </th>
-                    <th class="title" nowrap="nowrap" style="vertical-align:top; ">
-						<?PHP echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_DFBKEYS_MATCHDAY_INFO_5'); ?>
-                    </th>
-                    <th class="title" nowrap="nowrap" style="vertical-align:top; ">
-						<?PHP echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_DFBKEYS_MATCHDAY_INFO_6'); ?>
-                    </th>
-
+                    <th class="title" nowrap="nowrap"><?php echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_DFBKEYS_MATCHDAY_INFO_3'); ?></th>
+                    <th class="title" nowrap="nowrap"><?php echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_DFBKEYS_MATCHDAY_INFO_4'); ?></th>
+                    <th class="title" nowrap="nowrap"><?php echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_DFBKEYS_MATCHDAY_INFO_5'); ?></th>
+                    <th class="title" nowrap="nowrap"><?php echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_DFBKEYS_MATCHDAY_INFO_6'); ?></th>
                 </tr>
                 </thead>
-
-				<?PHP
-				$i = 0;
-
-				foreach ($this->newmatchdays as $rowdays)
-				{
-					?>
+                <tbody>
+                <?php foreach ((array) $this->newmatchdays as $i => $rowdays) : ?>
                     <tr>
-                        <input type="hidden" name="roundcode[]" value="<?php echo $rowdays->spieltag; ?> "/>
-                        <td><?php echo $rowdays->spieltag; ?></td>
-                        <td><input type="text" name="name[]"
-                                   value="<?php echo $rowdays->spieltag . Text::_('COM_SPORTSMANAGEMENT_ADMIN_DFBKEYS_MATCHDAY_INFO_7'); ?> "/>
-                        </td>
-
                         <td>
-							<?php
-							$append = ' style="background-color:#bbffff;" ';
-							$date1  = '';
-							echo HTMLHelper::calendar(
-								$date1,
-								'round_date_first[' . $i . ']',
-								'round_date_first[' . $i . ']',
-								'%d-%m-%Y',
-								'size="10" ' . $append .
-								'onchange="document.getElementById(\'cb' . $i . '\').checked=true"'
-							);
-
-							?>
+                            <input type="hidden" name="roundcode[]" value="<?php echo (int) $rowdays->spieltag; ?>" />
+                            <?php echo (int) $rowdays->spieltag; ?>
                         </td>
                         <td>
-
-							<?php
-							$append = ' style="background-color:#bbffff;" ';
-
-							echo HTMLHelper::calendar(
-								$date1,
-								'round_date_last[' . $i . ']',
-								'round_date_last[' . $i . ']',
-								'%d-%m-%Y',
-								'size="10" ' . $append .
-								'onchange="document.getElementById(\'cb' . $i . '\').checked=true"'
-							);
-
-							?>
-
-
+                            <input type="text" name="name[]" value="<?php echo (int) $rowdays->spieltag . Text::_('COM_SPORTSMANAGEMENT_ADMIN_DFBKEYS_MATCHDAY_INFO_7'); ?>" />
                         </td>
-
+                        <td>
+                            <?php echo HTMLHelper::calendar(
+                                '',
+                                'round_date_first[' . $i . ']',
+                                'round_date_first[' . $i . ']',
+                                '%d-%m-%Y',
+                                'size="10" style="background-color:#bbffff;"'
+                            ); ?>
+                        </td>
+                        <td>
+                            <?php echo HTMLHelper::calendar(
+                                '',
+                                'round_date_last[' . $i . ']',
+                                'round_date_last[' . $i . ']',
+                                '%d-%m-%Y',
+                                'size="10" style="background-color:#bbffff;"'
+                            ); ?>
+                        </td>
                     </tr>
-
-					<?PHP
-
-					$i++;
-				}
-				?>
+                <?php endforeach; ?>
+                </tbody>
             </table>
         </fieldset>
     </div>
-
-    <fieldset class="actions">
-
-
-    </fieldset>
-    <input type="hidden" name="sent" value="1"/>
-    <input type="hidden" name="projectid" value="<?php echo $this->project_id; ?> "/>
-    <input type="hidden" name="task" value=""/>
-    <input type="hidden" name="divisionid" value="<?php echo $this->division_id; ?> "/>
-
-
+    <input type="hidden" name="sent" value="1" />
+    <input type="hidden" name="projectid" value="<?php echo (int) $this->project_id; ?>" />
+    <input type="hidden" name="task" value="" />
+    <input type="hidden" name="divisionid" value="<?php echo (int) $this->division_id; ?>" />
+    <?php echo HTMLHelper::_('form.token'); ?>
 </form>
-
