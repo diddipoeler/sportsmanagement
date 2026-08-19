db686bbd56918a7a2c0dd57513ca9277370dd775
diddipoeler <diddipoeler@gmx.de>
2026-08-19 13:39:27 +0200

    Bridge legacy sportsmanagements list model

--- admin/models/sportsmanagements.php
+++ admin/models/sportsmanagements.php
@@ -1,44 +1,14 @@
 <?php
-/**
- *
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage models
- * @file       sportsmanagements.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-
-
+/** Legacy compatibility bridge for the native administrator Sportsmanagements list model. */
 defined('_JEXEC') or die('Restricted access');
 
-use Joomla\CMS\MVC\Model\ListModel;
+use Diddipoeler\Component\SportsManagement\Administrator\Model\SportsmanagementsModel;
 
-/**
- * SportsManagementList Model
- */
-class sportsmanagementModelsportsmanagements extends ListModel
-{
-	/**
-	 * Method to build an SQL query to load the list data.
-	 *
-	 * @return string    An SQL query
-	 */
-	protected function getListQuery()
-	{
-		// Create a new query object.
-		$db    = sportsmanagementHelper::getDBConnection();
-		$query = $db->getQuery(true);
+if (!class_exists(SportsmanagementsModel::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Model/SportsManagementListModel.php';
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Model/SportsmanagementsModel.php';
+}
 
-		// Select some fields
-		$query->select('id,greeting');
-
-		// From the hello table
-		$query->from('#__sportsmanagement');
-
-		return $query;
-	}
+if (!class_exists('sportsmanagementModelsportsmanagements', false)) {
+    class_alias(SportsmanagementsModel::class, 'sportsmanagementModelsportsmanagements');
 }
