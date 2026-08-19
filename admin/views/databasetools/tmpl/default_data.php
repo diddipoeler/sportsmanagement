c280311fddbd336daad666be2ccf5ec257611896
diddipoeler <diddipoeler@gmx.de>
2026-08-19 22:09:20 +0200

    Post database tool actions with CSRF token

--- admin/views/databasetools/tmpl/default_data.php
+++ admin/views/databasetools/tmpl/default_data.php
@@ -1,186 +1,82 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage databasetools
- * @file       default_data.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
 defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\HTML\HTMLHelper;
-use Joomla\CMS\Language\Text;
-use Joomla\CMS\Router\Route;
 
-$templatesToLoad = array('footer', 'listheader');
-sportsmanagementHelper::addTemplatePaths($templatesToLoad, $this);
+use Joomla\CMS\Language\Text;
+
+$tools = [
+    [
+        'task' => 'databasetool.truncate',
+        'label' => 'COM_SPORTSMANAGEMENT_ADMIN_DBTOOLS_TRUNCATE',
+        'description' => 'COM_SPORTSMANAGEMENT_ADMIN_DBTOOLS_TRUNCATE_DESCR',
+        'title' => 'COM_SPORTSMANAGEMENT_ADMIN_DBTOOLS_TRUNCATE2',
+        'danger' => true,
+    ],
+    [
+        'task' => 'databasetool.truncatejl',
+        'label' => 'COM_SPORTSMANAGEMENT_ADMIN_DBTOOLS_TRUNCATEJL',
+        'description' => 'COM_SPORTSMANAGEMENT_ADMIN_DBTOOLS_TRUNCATEJL_DESCR',
+        'title' => 'COM_SPORTSMANAGEMENT_ADMIN_DBTOOLS_TRUNCATE2JL',
+        'danger' => true,
+    ],
+    [
+        'task' => 'databasetool.optimize',
+        'label' => 'COM_SPORTSMANAGEMENT_ADMIN_DBTOOLS_OPTIMIZE',
+        'description' => 'COM_SPORTSMANAGEMENT_ADMIN_DBTOOLS_OPTIMIZE_DESCR',
+        'title' => 'COM_SPORTSMANAGEMENT_ADMIN_DBTOOLS_OPTIMIZE2',
+        'danger' => false,
+    ],
+    [
+        'task' => 'databasetool.repair',
+        'label' => 'COM_SPORTSMANAGEMENT_ADMIN_DBTOOLS_REPAIR',
+        'description' => 'COM_SPORTSMANAGEMENT_ADMIN_DBTOOLS_REPAIR_DESCR',
+        'title' => 'COM_SPORTSMANAGEMENT_ADMIN_DBTOOLS_REPAIR2',
+        'danger' => false,
+    ],
+    [
+        'task' => 'databasetool.picturepath',
+        'label' => 'COM_SPORTSMANAGEMENT_ADMIN_DBTOOLS_PICTURE_PATH_MIGRATION',
+        'description' => 'COM_SPORTSMANAGEMENT_ADMIN_DBTOOLS_PICTURE_PATH_MIGRATION_DESCR',
+        'title' => 'COM_SPORTSMANAGEMENT_ADMIN_DBTOOLS_PICTURE_PATH_MIGRATION2',
+        'danger' => false,
+    ],
+    [
+        'task' => 'databasetool.updatetemplatemasters',
+        'label' => 'COM_SPORTSMANAGEMENT_ADMIN_DBTOOLS_UPDATE_TEMPLATE_MASTERS',
+        'description' => 'COM_SPORTSMANAGEMENT_ADMIN_DBTOOLS_UPDATE_TEMPLATE_MASTERS_DESCR',
+        'title' => 'COM_SPORTSMANAGEMENT_ADMIN_DBTOOLS_UPDATE_TEMPLATE_MASTERS2',
+        'danger' => false,
+    ],
+];
 ?>
 <div id="editcell">
-        <table class="table">
-            <thead>
+    <table class="table">
+        <thead>
             <tr>
-                <th class="title" class="nowrap">
-					<?php
-					echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_DBTOOLS_TOOL');
-					?>
-                </th>
-                <th class="title" class="nowrap">
-					<?php
-					echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_DBTOOLS_DESCR');
-					?>
-                </th>
+                <th><?php echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_DBTOOLS_TOOL'); ?></th>
+                <th><?php echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_DBTOOLS_DESCR'); ?></th>
             </tr>
-            </thead>
-            <tfoot>
+        </thead>
+        <tbody>
+        <?php foreach ($tools as $tool) : ?>
             <tr>
-                <td colspan="2">
-					<?php
-					echo "&nbsp;";
-					?>
-                </td>
-            </tr>
-            </tfoot>
-            <tbody>
-
-            <tr>
-                <td class="nowrap" valign="top">
-					<?php
-					$link = Route::_('index.php?option=com_sportsmanagement&view=databasetool&task=databasetool.truncate');
-					?>
-
-                    <a href="<?php echo $link; ?>" 
-		       class="btn btn-outline-primary"
-                       title="<?php echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_DBTOOLS_TRUNCATE2'); ?>">
-						<?php
-						echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_DBTOOLS_TRUNCATE');
-						?>
-                    </a>
+                <td class="text-nowrap align-top">
+                    <button
+                        type="submit"
+                        class="btn <?php echo $tool['danger'] ? 'btn-danger' : 'btn-outline-primary'; ?>"
+                        name="task"
+                        value="<?php echo htmlspecialchars($tool['task'], ENT_QUOTES, 'UTF-8'); ?>"
+                        title="<?php echo htmlspecialchars(Text::_($tool['title']), ENT_QUOTES, 'UTF-8'); ?>"
+                    >
+                        <?php echo Text::_($tool['label']); ?>
+                    </button>
                 </td>
                 <td>
-			<div class="alert alert-warning">
-					<?php
-					echo Text::_("COM_SPORTSMANAGEMENT_ADMIN_DBTOOLS_TRUNCATE_DESCR");
-					?>
-			</div>
+                    <div class="alert <?php echo $tool['danger'] ? 'alert-warning' : 'alert-info'; ?> mb-0">
+                        <?php echo Text::_($tool['description']); ?>
+                    </div>
                 </td>
             </tr>
-
-            <tr>
-                <td class="nowrap" valign="top">
-					<?php
-					$link = Route::_('index.php?option=com_sportsmanagement&view=databasetool&task=databasetool.truncatejl');
-					?>
-
-                    <a href="<?php echo $link; ?>"
-		       class="btn btn-outline-primary"
-                       title="<?php echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_DBTOOLS_TRUNCATE2JL'); ?>">
-						<?php
-						echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_DBTOOLS_TRUNCATEJL');
-						?>
-                    </a>
-                </td>
-                <td>
-			<div class="alert alert-warning">
-					<?php
-					echo Text::_("COM_SPORTSMANAGEMENT_ADMIN_DBTOOLS_TRUNCATEJL_DESCR");
-					?>
-			</div>
-                </td>
-            </tr>
-
-            <tr>
-                <td class="nowrap" valign="top">
-					<?php
-					$link = Route::_('index.php?option=com_sportsmanagement&view=databasetool&task=databasetool.optimize');
-					?>
-                    <a href="<?php echo $link; ?>"
-		       class="btn btn-outline-primary"
-                       title="<?php echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_DBTOOLS_OPTIMIZE2'); ?>">
-						<?php
-						echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_DBTOOLS_OPTIMIZE');
-						?>
-                    </a>
-                </td>
-                <td>
-			<div class="alert alert-warning">
-					<?php
-					echo Text::_("COM_SPORTSMANAGEMENT_ADMIN_DBTOOLS_OPTIMIZE_DESCR");
-					?>
-			</div>
-                </td>
-            </tr>
-
-            <tr>
-                <td class="nowrap" valign="top">
-					<?php
-					$link = Route::_('index.php?option=com_sportsmanagement&view=databasetool&task=databasetool.repair');
-					?>
-
-                    <a href="<?php echo $link; ?>"
-		       class="btn btn-outline-primary"
-                       title="<?php echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_DBTOOLS_REPAIR2'); ?>">
-						<?php
-						echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_DBTOOLS_REPAIR');
-						?>
-                    </a>
-                </td>
-                <td>
-			<div class="alert alert-warning">
-					<?php
-					echo Text::_("COM_SPORTSMANAGEMENT_ADMIN_DBTOOLS_REPAIR_DESCR");
-					?>
-			</div>
-                </td>
-            </tr>
-
-            <tr>
-                <td class="nowrap" valign="top">
-					<?php
-					$link = Route::_('index.php?option=com_sportsmanagement&view=databasetool&task=databasetool.picturepath');
-					?>
-
-                    <a href="<?php echo $link; ?>"
-		       class="btn btn-outline-primary"
-                       title="<?php echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_DBTOOLS_PICTURE_PATH_MIGRATION2'); ?>">
-						<?php
-						echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_DBTOOLS_PICTURE_PATH_MIGRATION');
-						?>
-                    </a>
-                </td>
-                <td>
-			<div class="alert alert-warning">
-					<?php
-					echo Text::_("COM_SPORTSMANAGEMENT_ADMIN_DBTOOLS_PICTURE_PATH_MIGRATION_DESCR");
-					?>
-			</div>
-                </td>
-            </tr>
-
-            <tr>
-                <td class="nowrap" valign="top">
-					<?php
-					$link = Route::_('index.php?option=com_sportsmanagement&view=databasetool&task=databasetool.updatetemplatemasters');
-					?>
-
-                    <a href="<?php echo $link; ?>"
-		       class="btn btn-outline-primary"
-                       title="<?php echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_DBTOOLS_UPDATE_TEMPLATE_MASTERS2'); ?>">
-						<?php
-						echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_DBTOOLS_UPDATE_TEMPLATE_MASTERS');
-						?>
-                    </a>
-                </td>
-                <td>
-			<div class="alert alert-warning">
-					<?php
-					echo Text::_("COM_SPORTSMANAGEMENT_ADMIN_DBTOOLS_UPDATE_TEMPLATE_MASTERS_DESCR");
-					?>
-			</div>
-                </td>
-            </tr>
-
-            </tbody>
-        </table>
+        <?php endforeach; ?>
+        </tbody>
+    </table>
 </div>
