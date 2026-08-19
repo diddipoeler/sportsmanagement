07153700fd3806c66dfaef4d2cb20603d6e933b7
diddipoeler <diddipoeler@gmx.de>
2026-08-19 12:29:16 +0200

    Add native rosterposition admin controller

---
+++ admin/src/Controller/RosterpositionController.php
@@ -0,0 +1,9 @@
+<?php
+namespace Diddipoeler\Component\SportsManagement\Administrator\Controller;
+
+\defined('_JEXEC') or die;
+
+/** Native Joomla 5/6 form controller for a roster position. */
+final class RosterpositionController extends SportsManagementFormController
+{
+}
