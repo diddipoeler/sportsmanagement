7253904dc93a5db6f51c8a37c85d74352882c685
diddipoeler <diddipoeler@gmx.de>
2026-08-19 17:20:06 +0200

    Bridge legacy association list controller

--- admin/controllers/jlextassociations.php
+++ admin/controllers/jlextassociations.php
@@ -1,57 +1,14 @@
 <?php
-/**
- *
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage controllers
- * @file       jlextassociations.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-
+/** Legacy compatibility bridge for the native administrator associations controller. */
 defined('_JEXEC') or die('Restricted access');
 
-use Joomla\CMS\MVC\Model\BaseDatabaseModel;
+use Diddipoeler\Component\SportsManagement\Administrator\Controller\JlextassociationsController;
 
-/**
- * sportsmanagementControllerjlextassociations
- *
- * @package
- * @author    diddi
- * @copyright 2014
- * @version   $Id$
- * @access    public
- */
-class sportsmanagementControllerjlextassociations extends JSMControllerAdmin
-{
+if (!class_exists(JlextassociationsController::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Controller/SportsManagementAdminController.php';
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Controller/JlextassociationsController.php';
+}
 
-	/**
-	 * sportsmanagementControllerjlextassociations::import()
-	 *
-	 * @return void
-	 */
-	function import()
-	{
-		$databasetool = BaseDatabaseModel::getInstance("databasetool", "sportsmanagementModel");
-		$model        = $this->getModel();
-		$msg          = $databasetool->checkAssociations();
-		$this->setRedirect('index.php?option=com_sportsmanagement&view=jlextassociations', $msg);
-
-	}
-
-	/**
-	 * Proxy for getModel.
-	 *
-	 * @since 1.6
-	 */
-	public function getModel($name = 'jlextassociation', $prefix = 'sportsmanagementModel', $config = Array())
-	{
-		$model = parent::getModel($name, $prefix, array('ignore_request' => true));
-
-		return $model;
-	}
-
+if (!class_exists('sportsmanagementControllerjlextassociations', false)) {
+    class_alias(JlextassociationsController::class, 'sportsmanagementControllerjlextassociations');
 }
