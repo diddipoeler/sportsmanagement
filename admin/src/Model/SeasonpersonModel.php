235d12d81c15300a4f581e4c146426842db8dc7c
diddipoeler <diddipoeler@gmx.de>
2026-08-19 13:54:29 +0200

    Add native season person form model

---
+++ admin/src/Model/SeasonpersonModel.php
@@ -0,0 +1,22 @@
+<?php
+namespace Diddipoeler\Component\SportsManagement\Administrator\Model;
+
+\defined('_JEXEC') or die;
+
+use Joomla\CMS\Factory;
+
+/**
+ * Native Joomla 5/6 administrator form model for a season person assignment.
+ */
+final class SeasonpersonModel extends SportsManagementAdminModel
+{
+    protected function allowEdit($data = [], $key = 'id')
+    {
+        $id = (int) ($data[$key] ?? 0);
+
+        return Factory::getApplication()->getIdentity()->authorise(
+            'core.edit',
+            'com_sportsmanagement.message.' . $id
+        ) || parent::allowEdit($data, $key);
+    }
+}
