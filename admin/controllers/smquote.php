9e498a172ece2a48c7d5753a9c170e244b591c89
diddipoeler <diddipoeler@gmx.de>
2026-08-19 16:23:03 +0200

    Bridge legacy quote form controller

--- admin/controllers/smquote.php
+++ admin/controllers/smquote.php
@@ -1,32 +1,14 @@
 <?php
-/**
- *
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage controllers
- * @file       smquote.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-
-
+/** Legacy compatibility bridge for the native administrator quote form controller. */
 defined('_JEXEC') or die('Restricted access');
 
+use Diddipoeler\Component\SportsManagement\Administrator\Controller\SmquoteController;
 
-/**
- * sportsmanagementControllersmquote
- *
- * @package
- * @author
- * @copyright diddi
- * @version   2014
- * @access    public
- */
-class sportsmanagementControllersmquote extends JSMControllerForm
-{
+if (!class_exists(SmquoteController::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Controller/SportsManagementFormController.php';
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Controller/SmquoteController.php';
+}
 
-
+if (!class_exists('sportsmanagementControllersmquote', false)) {
+    class_alias(SmquoteController::class, 'sportsmanagementControllersmquote');
 }
