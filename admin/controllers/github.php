a2f9f753faab5a6d3d8ee54ef4b6fd52e9e4b645
diddipoeler <diddipoeler@gmx.de>
2026-08-19 22:19:11 +0200

    Bridge legacy GitHub integration controller

--- admin/controllers/github.php
+++ admin/controllers/github.php
@@ -1,86 +1,13 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage controllers
- * @file       github.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
+/** Legacy compatibility bridge for the native administrator Github controller. */
 defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\Language\Text;
-use Joomla\CMS\Factory;
-use Joomla\CMS\Session\Session;
-use Joomla\CMS\MVC\Controller\FormController;
 
-/**
- * sportsmanagementControllergithub
- *
- * @package
- * @author    Dieter Plöger
- * @copyright 2016
- * @version   $Id$
- * @access    public
- */
-class sportsmanagementControllergithub extends FormController
-{
+use Diddipoeler\Component\SportsManagement\Administrator\Controller\GithubController;
 
-	/**
-	 * Constructor.
-	 *
-	 * @param   array An optional associative array of configuration settings.
-	 *
-	 * @see   JController
-	 * @since 1.6
-	 */
-	public function __construct($config = array())
-	{
-		parent::__construct($config);
-		$this->app    = Factory::getApplication();
-		$this->jinput = $this->app->input;
-		$this->option = $this->jinput->getCmd('option');
-		$this->model  = $this->getModel();
-		$this->post   = $this->jinput->post->getArray(array());
-	}
-
-	/**
-	 * Proxy for getModel.
-	 *
-	 * @since 1.6
-	 */
-	public function getModel($name = 'github', $prefix = 'sportsmanagementModel', $config = Array())
-	{
-		$model = parent::getModel($name, $prefix, array('ignore_request' => true));
-		return $model;
-	}
-
-	/**
-	 * sportsmanagementControllergithub::cancel()
-	 *
-	 * @param   mixed  $key
-	 *
-	 * @return void
-	 */
-	function cancel($key = null)
-	{
-		$msg = Text::_('JLIB_HTML_BEHAVIOR_CLOSE');
-		$link = 'index.php?option=' . $this->option . '&view=close&tmpl=component';
-		$this->setRedirect($link, $msg);
-	}
-
-	/**
-	 * sportsmanagementControllergithub::addissue()
-	 *
-	 * @return void
-	 */
-	function addissue()
-	{
-		Session::checkToken() or jexit(\Text::_('JINVALID_TOKEN'));
-		$msg = $this->model->addissue();
-		$this->setRedirect('index.php?option=com_sportsmanagement&view=github&tmpl=component&layout=github_result', $msg);
-	}
-
+if (!class_exists(GithubController::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Controller/GithubController.php';
 }
 
+if (!class_exists('sportsmanagementControllergithub', false)) {
+    class_alias(GithubController::class, 'sportsmanagementControllergithub');
+}
