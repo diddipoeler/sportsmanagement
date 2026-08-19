26ae60e33074f4abf610a818a294db1ced01bb55
diddipoeler <diddipoeler@gmx.de>
2026-08-19 18:37:02 +0200

    Modernize treetos list wrapper

--- admin/views/treetos/tmpl/default.php
+++ admin/views/treetos/tmpl/default.php
@@ -1,32 +1,22 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage treetos
- * @file       default.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
+/** SportsManagement tournament trees list wrapper. */
 defined('_JEXEC') or die('Restricted access');
+
 use Joomla\CMS\HTML\HTMLHelper;
 
-$templatesToLoad = array('footer', 'listheader');
+$templatesToLoad = ['footer', 'listheader'];
 sportsmanagementHelper::addTemplatePaths($templatesToLoad, $this);
 ?>
-    <form action="<?php echo $this->request_url; ?>" method="post" id="adminForm" name="adminForm">
-		<?PHP
-        echo $this->loadTemplate('joomla_version');
-		echo $this->loadTemplate('data');
-		?>
-        <input type="hidden" name="project_id" value="<?php echo $this->projectws->id; ?>"/>
-        <input type="hidden" name="boxchecked" value="0"/>
-        <input type="hidden" name="view" value="treetos"/>
-        <input type="hidden" name="task" value="treeto.display"/>
-		<?php echo HTMLHelper::_('form.token'); ?>
-    </form>
-
-<?PHP
-echo $this->loadTemplate('footer');
-?>
+<form action="<?php echo $this->escape($this->request_url); ?>" method="post" id="adminForm" name="adminForm">
+    <?php
+    echo $this->loadTemplate('joomla_version');
+    echo $this->loadTemplate('data');
+    ?>
+    <input type="hidden" name="project_id" value="<?php echo (int) $this->projectws->id; ?>">
+    <input type="hidden" name="pid" value="<?php echo (int) $this->project_id; ?>">
+    <input type="hidden" name="boxchecked" value="0">
+    <input type="hidden" name="view" value="treetos">
+    <input type="hidden" name="task" value="treeto.display">
+    <?php echo HTMLHelper::_('form.token'); ?>
+</form>
+<?php echo $this->loadTemplate('footer'); ?>
