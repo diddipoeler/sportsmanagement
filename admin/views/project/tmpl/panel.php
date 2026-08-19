c2476d852071da555de2ee0217205db865b1db43
diddipoeler <diddipoeler@gmx.de>
2026-08-19 23:38:02 +0200

    Modernize project control panel layout

--- admin/views/project/tmpl/panel.php
+++ admin/views/project/tmpl/panel.php
@@ -1,184 +1,104 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- * @version   1.0.05
- * @file      panel.php
- * @author    diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license   GNU General Public License version 2 or later; see LICENSE.txt
- */
+/** Native Joomla 5/6 project control panel. */
 defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\Language\Text;
+
 use Joomla\CMS\HTML\HTMLHelper;
-use Joomla\CMS\Factory;
-use Joomla\CMS\Toolbar\ToolbarHelper;
-use Joomla\CMS\Uri\Uri;
+use Joomla\CMS\Language\Text;
 use Joomla\CMS\Router\Route;
 
-$templatesToLoad = array('footer', 'listheader');
+$templatesToLoad = ['footer', 'listheader'];
 sportsmanagementHelper::addTemplatePaths($templatesToLoad, $this);
 
-// Get a refrence of the page instance in joomla
-$document  = Factory::getDocument();
-$stylelink = '<link rel="stylesheet" href="' . Uri::root() . 'administrator/components/com_sportsmanagement/assets/css/jlextusericons.css' . '" type="text/css" />' . "\n";
-$document->addCustomTag($stylelink);
-$path = '/administrator/components/com_sportsmanagement/assets/icons/';
-$user = Factory::getUser();
-ToolbarHelper::title(Text::_('COM_SPORTSMANAGEMENT_ADMIN_PROJECTS_CONTROL_PANEL_TITLE'), 'projects');
-
+$items = [
+    [
+        'show' => true,
+        'url' => 'index.php?option=com_sportsmanagement&task=project.edit&id=' . (int) $this->project->id,
+        'title' => Text::_('COM_SPORTSMANAGEMENT_P_PANEL_PSETTINGS'),
+        'icon' => 'icon-cog',
+    ],
+    [
+        'show' => true,
+        'url' => 'index.php?option=com_sportsmanagement&view=templates&pid=' . (int) $this->project->id,
+        'title' => Text::_('COM_SPORTSMANAGEMENT_P_PANEL_FES'),
+        'icon' => 'icon-list',
+    ],
+    [
+        'show' => in_array((string) $this->project->project_type, ['PROJECT_DIVISIONS', 'DIVISIONS_LEAGUE'], true),
+        'url' => 'index.php?option=com_sportsmanagement&view=divisions&pid=' . (int) $this->project->id,
+        'title' => Text::plural('COM_SPORTSMANAGEMENT_P_PANEL_DIVISIONS', (int) $this->count_projectdivisions),
+        'icon' => 'icon-grid',
+    ],
+    [
+        'show' => in_array((string) $this->project->project_type, ['TOURNAMENT_MODE', 'DIVISIONS_LEAGUE'], true),
+        'url' => 'index.php?option=com_sportsmanagement&view=treetos&pid=' . (int) $this->project->id,
+        'title' => Text::_('COM_SPORTSMANAGEMENT_P_PANEL_TREE'),
+        'icon' => 'icon-tree-2',
+    ],
+    [
+        'show' => (int) $this->project->project_art_id !== 3,
+        'url' => 'index.php?option=com_sportsmanagement&view=projectpositions&pid=' . (int) $this->project->id,
+        'title' => Text::plural('COM_SPORTSMANAGEMENT_P_PANEL_POSITIONS', (int) $this->count_projectpositions),
+        'icon' => 'icon-users',
+    ],
+    [
+        'show' => true,
+        'url' => 'index.php?option=com_sportsmanagement&view=projectreferees&persontype=3&pid=' . (int) $this->project->id,
+        'title' => Text::plural('COM_SPORTSMANAGEMENT_P_PANEL_REFEREES', (int) $this->count_projectreferees),
+        'icon' => 'icon-user',
+    ],
+    [
+        'show' => true,
+        'url' => 'index.php?option=com_sportsmanagement&view=projectteams&pid=' . (int) $this->project->id,
+        'title' => Text::plural('COM_SPORTSMANAGEMENT_P_PANEL_TEAMS', (int) $this->count_projectteams),
+        'icon' => 'icon-users',
+    ],
+    [
+        'show' => true,
+        'url' => 'index.php?option=com_sportsmanagement&view=rounds&pid=' . (int) $this->project->id,
+        'title' => Text::plural('COM_SPORTSMANAGEMENT_P_PANEL_MATCHDAYS', (int) $this->count_matchdays),
+        'icon' => 'icon-calendar',
+    ],
+    [
+        'show' => true,
+        'url' => 'index.php?option=com_sportsmanagement&view=jlxmlexports&pid=' . (int) $this->project->id,
+        'title' => Text::_('COM_SPORTSMANAGEMENT_P_PANEL_XML_EXPORT'),
+        'icon' => 'icon-download',
+    ],
+];
 ?>
 <div id="j-main-container">
-    <div id="jsm" class="admin override">
+    <?php echo $this->loadTemplate('jsm_warnings'); ?>
+    <?php echo $this->loadTemplate('jsm_notes'); ?>
+    <?php echo $this->loadTemplate('jsm_tips'); ?>
 
-        <div id="j-main-container" class="span10">
-            <section class="content-block" role="main">
-
-                <div class="row-fluid">
-                    <div class="span7">
-<?php
-echo $this->loadTemplate('jsm_warnings');
-echo $this->loadTemplate('jsm_notes');			    
-echo $this->loadTemplate('jsm_tips');			    
-?>	
-                        <div class="well well-small">
-                            <div id="dashboard-icons" class="btn-group">
-                                <div id="element-box">
-                                    <div class="m">
-                                        <div class="adminform">
-                                            <legend><?php echo Text::sprintf('COM_SPORTSMANAGEMENT_ADMIN_PROJECTS_CONTROL_PANEL_LEGEND', '<i>' . $this->project->name . '</i>'); ?></legend>
-                                            <div class="cpanel">
-												<?php
-												$link       = Route::_('index.php?option=com_sportsmanagement&task=project.edit&id=' . $this->project->id);
-												$text       = Text::_('COM_SPORTSMANAGEMENT_P_PANEL_PSETTINGS');
-												$imageFile  = 'projekte.png';
-												$linkParams = "<span>$text</span>&nbsp;";
-												$image      = HTMLHelper::_('image.administrator', $imageFile, $path, null, null, $text) . $linkParams;
-												?>
-                                                <div class="icon-wrapper">
-                                                    <div class="icon"><?php echo HTMLHelper::link($link, $image); ?></div>
-                                                </div>
-												<?php
-												$link       = Route::_('index.php?option=com_sportsmanagement&view=templates&pid=' . $this->project->id);
-												$text       = Text::_('COM_SPORTSMANAGEMENT_P_PANEL_FES');
-												$imageFile  = 'templates.png';
-												$linkParams = "<span>$text</span>&nbsp;";
-												$image      = HTMLHelper::_('image.administrator', $imageFile, $path, null, null, $text) . $linkParams;
-												?>
-                                                <div class="icon-wrapper">
-                                                    <div class="icon"><?php echo HTMLHelper::link($link, $image); ?></div>
-                                                </div>
-												<?php
-												if ((isset($this->project->project_type))
-													&& (($this->project->project_type == 'PROJECT_DIVISIONS')
-														|| ($this->project->project_type == 'DIVISIONS_LEAGUE'))
-												)
-												{
-													$link       = Route::_('index.php?option=com_sportsmanagement&view=divisions&pid=' . $this->project->id);
-													$text       = Text::plural('COM_SPORTSMANAGEMENT_P_PANEL_DIVISIONS', $this->count_projectdivisions);
-													$imageFile  = 'divisionen.png';
-													$linkParams = "<span>$text</span>&nbsp;";
-													$image      = HTMLHelper::_('image.administrator', $imageFile, $path, null, null, $text) . $linkParams;
-													?>
-                                                    <div class="icon-wrapper">
-                                                        <div class="icon"><?php echo HTMLHelper::link($link, $image); ?></div>
-                                                    </div>
-													<?php
-												}
-												if ((isset($this->project->project_type))
-													&& (($this->project->project_type == 'TOURNAMENT_MODE')
-														|| ($this->project->project_type == 'DIVISIONS_LEAGUE'))
-												)
-												{
-													$link       = Route::_('index.php?option=com_sportsmanagement&view=treetos&pid=' . $this->project->id);
-													$text       = Text::_('COM_SPORTSMANAGEMENT_P_PANEL_TREE');
-													$imageFile  = 'turnierbaum.png';
-													$linkParams = "<span>$text</span>&nbsp;";
-													$image      = HTMLHelper::_('image.administrator', $imageFile, $path, null, null, $text) . $linkParams;
-													?>
-                                                    <div class="icon-wrapper">
-                                                        <div class="icon"><?php echo HTMLHelper::link($link, $image); ?></div>
-                                                    </div>
-													<?php
-												}
-
-												if ($this->project->project_art_id != 3)
-												{
-													$link       = Route::_('index.php?option=com_sportsmanagement&view=projectpositions&pid=' . $this->project->id);
-													$text       = Text::plural('COM_SPORTSMANAGEMENT_P_PANEL_POSITIONS', $this->count_projectpositions);
-													$imageFile  = 'positionen.png';
-													$linkParams = "<span>$text</span>&nbsp;";
-													$image      = HTMLHelper::_('image.administrator', $imageFile, $path, null, null, $text) . $linkParams;
-													?>
-                                                    <div class="icon-wrapper">
-                                                        <div class="icon"><?php echo HTMLHelper::link($link, $image); ?></div>
-                                                    </div>
-													<?php
-												}
-												$link       = Route::_('index.php?option=com_sportsmanagement&view=projectreferees&persontype=3&pid=' . $this->project->id);
-												$text       = Text::plural('COM_SPORTSMANAGEMENT_P_PANEL_REFEREES', $this->count_projectreferees);
-												$imageFile  = 'projektschiedsrichter.png';
-												$linkParams = "<span>$text</span>&nbsp;";
-												$image      = HTMLHelper::_('image.administrator', $imageFile, $path, null, null, $text) . $linkParams;
-												?>
-                                                <div class="icon-wrapper">
-                                                    <div class="icon"><?php echo HTMLHelper::link($link, $image); ?></div>
-                                                </div>
-												<?php
-												$link       = Route::_('index.php?option=com_sportsmanagement&view=projectteams&pid=' . $this->project->id);
-												$text       = Text::plural('COM_SPORTSMANAGEMENT_P_PANEL_TEAMS', $this->count_projectteams);
-												$imageFile  = 'mannschaften.png';
-												$linkParams = "<span>$text</span>&nbsp;";
-												$image      = HTMLHelper::_('image.administrator', $imageFile, $path, null, null, $text) . $linkParams;
-												?>
-                                                <div class="icon-wrapper">
-                                                    <div class="icon"><?php echo HTMLHelper::link($link, $image); ?></div>
-                                                </div>
-												<?php
-												$link       = Route::_('index.php?option=com_sportsmanagement&view=rounds&pid=' . $this->project->id);
-												$text       = Text::plural('COM_SPORTSMANAGEMENT_P_PANEL_MATCHDAYS', $this->count_matchdays);
-												$imageFile  = 'spieltage.png';
-												$linkParams = "<span>$text</span>&nbsp;";
-												$image      = HTMLHelper::_('image.administrator', $imageFile, $path, null, null, $text) . $linkParams;
-												?>
-                                                <div class="icon-wrapper">
-                                                    <div class="icon"><?php echo HTMLHelper::link($link, $image); ?></div>
-                                                </div>
-												<?php
-												$link       = Route::_('index.php?option=com_sportsmanagement&view=jlxmlexports&pid=' . $this->project->id);
-												$text       = Text::_('COM_SPORTSMANAGEMENT_P_PANEL_XML_EXPORT');
-												$imageFile  = 'xmlexport.png';
-												$linkParams = "<span>$text</span>&nbsp;";
-												$image      = HTMLHelper::_('image.administrator', $imageFile, $path, null, null, $text) . $linkParams;
-												?>
-                                                <div class="icon-wrapper">
-                                                    <div class="icon"><?php echo HTMLHelper::link($link, $image); ?></div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div id="element-box">
-                                    <div class="m">
-                                        <div class="adminform">
-                                            <div class="cpanel"><?php echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_PROJECTS_CONTROL_PANEL_HINT'); ?></div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <!-- bottom close main table opened in default_admin -->
-                                </td>
-                                </tr>
-                                </table>
-
+    <div class="card mb-3">
+        <div class="card-header">
+            <h2 class="h4 mb-0">
+                <?php echo Text::sprintf(
+                    'COM_SPORTSMANAGEMENT_ADMIN_PROJECTS_CONTROL_PANEL_LEGEND',
+                    '<i>' . htmlspecialchars((string) $this->project->name, ENT_QUOTES, 'UTF-8') . '</i>'
+                ); ?>
+            </h2>
+        </div>
+        <div class="card-body">
+            <div class="row g-3">
+                <?php foreach ($items as $item) : ?>
+                    <?php if (!$item['show']) { continue; } ?>
+                    <div class="col-sm-6 col-lg-4 col-xl-3">
+                        <a class="card h-100 text-decoration-none" href="<?php echo Route::_($item['url']); ?>">
+                            <div class="card-body d-flex align-items-center gap-3">
+                                <span class="<?php echo htmlspecialchars($item['icon'], ENT_QUOTES, 'UTF-8'); ?> fs-2" aria-hidden="true"></span>
+                                <span><?php echo $item['title']; ?></span>
                             </div>
-                        </div>
+                        </a>
                     </div>
-                </div>
-            </section>
+                <?php endforeach; ?>
+            </div>
         </div>
     </div>
+
+    <div class="alert alert-info">
+        <?php echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_PROJECTS_CONTROL_PANEL_HINT'); ?>
+    </div>
 </div>
-<?PHP
-
-echo $this->loadTemplate('footer');
-
-?> 
+<?php echo $this->loadTemplate('footer'); ?>
