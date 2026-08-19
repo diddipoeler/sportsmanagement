7c29666e167ec40691c0dbfcac4a0a3ea170abda
diddipoeler <diddipoeler@gmx.de>
2026-08-19 15:21:35 +0200

    Bridge legacy prediction group controller

--- admin/controllers/predictiongroup.php
+++ admin/controllers/predictiongroup.php
@@ -1,42 +1,14 @@
 <?php
-/**
- *
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage controllers
- * @file       predictiongroup.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-
+/** Legacy compatibility bridge for the native administrator Predictiongroup controller. */
 defined('_JEXEC') or die('Restricted access');
 
-/**
- * sportsmanagementControllerpredictiongroup
- *
- * @package
- * @author
- * @copyright diddi
- * @version   2013
- * @access    public
- */
-class sportsmanagementControllerpredictiongroup extends JSMControllerForm
-{
+use Diddipoeler\Component\SportsManagement\Administrator\Controller\PredictiongroupController;
 
-	/**
-	 * Class Constructor
-	 *
-	 * @param   array  $config  An optional associative array of configuration settings.
-	 *
-	 * @return void
-	 * @since  1.5
-	 */
-	function __construct($config = array())
-	{
-		parent::__construct($config);
-	}
+if (!class_exists(PredictiongroupController::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Controller/SportsManagementFormController.php';
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Controller/PredictiongroupController.php';
+}
 
+if (!class_exists('sportsmanagementControllerpredictiongroup', false)) {
+    class_alias(PredictiongroupController::class, 'sportsmanagementControllerpredictiongroup');
 }
