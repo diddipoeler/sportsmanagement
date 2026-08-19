7ce5559c5791d9152a92e5dc846a856d78aa58d2
diddipoeler <diddipoeler@gmx.de>
2026-08-19 12:27:37 +0200

    Bridge legacy eventtype admin controller

--- admin/controllers/eventtype.php
+++ admin/controllers/eventtype.php
@@ -1,25 +1,19 @@
 <?php
 /**
+ * SportsManagement legacy compatibility bridge.
  *
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage controllers
- * @file       eventtype.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * The active Joomla 5/6 implementation lives in admin/src/Controller/EventtypeController.php.
  */
-
 
 defined('_JEXEC') or die('Restricted access');
 
-/**
- * SportsManagement Controller
- */
-class sportsmanagementControllereventtype extends JSMControllerForm
-{
+use Diddipoeler\Component\SportsManagement\Administrator\Controller\EventtypeController;
 
+if (!class_exists(EventtypeController::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Controller/SportsManagementFormController.php';
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Controller/EventtypeController.php';
+}
 
+if (!class_exists('sportsmanagementControllereventtype', false)) {
+    class_alias(EventtypeController::class, 'sportsmanagementControllereventtype');
 }
