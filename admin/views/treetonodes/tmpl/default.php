a7a1cb9d7ef8efca5df6bfe203279570daca0959
diddipoeler <diddipoeler@gmx.de>
2026-08-19 19:59:44 +0200

    Remove tournament tree legacy list wrapper

--- admin/views/treetonodes/tmpl/default.php
+++ admin/views/treetonodes/tmpl/default.php
@@ -1,59 +1,28 @@
 <?php
-/**
- *
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage treetonodes
- * @file       default.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-
+/** Tournament-tree bracket wrapper for Joomla 5/6. */
 defined('_JEXEC') or die('Restricted access');
 
 use Joomla\CMS\HTML\HTMLHelper;
 
-$templatesToLoad = array('footer', 'listheader');
-sportsmanagementHelper::addTemplatePaths($templatesToLoad, $this);
+sportsmanagementHelper::addTemplatePaths(['footer'], $this);
 ?>
+<form action="<?php echo $this->request_url; ?>" method="post" id="adminForm" name="adminForm">
+    <?php echo $this->loadTemplate('data'); ?>
 
+    <input type="hidden" name="project_id" value="<?php echo (int) $this->project_id; ?>">
+    <input type="hidden" name="pid" value="<?php echo (int) $this->project_id; ?>">
+    <input type="hidden" name="tid" value="<?php echo (int) $this->tree_id; ?>">
+    <input type="hidden" name="tree_i" value="<?php echo (int) $this->treetows->tree_i; ?>">
+    <input type="hidden" name="treeto_id" value="<?php echo (int) $this->treetows->id; ?>">
+    <input type="hidden" name="global_fake" value="<?php echo (int) $this->treetows->global_fake; ?>">
+    <input type="hidden" name="global_known" value="<?php echo (int) $this->treetows->global_known; ?>">
+    <input type="hidden" name="global_matchday" value="<?php echo (int) $this->treetows->global_matchday; ?>">
+    <input type="hidden" name="global_bestof" value="<?php echo (int) $this->treetows->global_bestof; ?>">
+    <input type="hidden" name="task" value="">
+    <input type="hidden" name="boxchecked" value="0">
+    <?php echo HTMLHelper::_('form.token'); ?>
 
-    <form action="<?php echo $this->request_url; ?>" method="post" id="adminForm" name="adminForm">
-		<?PHP
-/**
-		if (version_compare(JVERSION, '3.0.0', 'ge'))
-		{
-			echo $this->loadTemplate('joomla3');
-		}
-		else
-		{
-			echo $this->loadTemplate('joomla2');
-		}
-*/
-echo $this->loadTemplate('joomla_version');
-		//echo $this->loadTemplate('data');
+    <?php echo $this->table_data_div; ?>
+</form>
 
-
-		?>
-        <input type="hidden" name="project_id" value="<?php echo $this->projectws->id; ?>"/>
-        <!--	<input type="hidden" name="roundcode" value="<?php echo $this->roundcode; ?>" /> -->
-        <input type="hidden" name="tree_i" value="<?php echo $this->treetows->tree_i; ?>"/>
-        <input type="hidden" name="treeto_id" value="<?php echo $this->treetows->id; ?>"/>
-        <input type="hidden" name="global_fake" value="<?php echo $this->treetows->global_fake; ?>"/>
-        <input type="hidden" name="global_known" value="<?php echo $this->treetows->global_known; ?>"/>
-        <input type="hidden" name="global_matchday" value="<?php echo $this->treetows->global_matchday; ?>"/>
-        <input type="hidden" name="global_bestof" value="<?php echo $this->treetows->global_bestof; ?>"/>
-        <input type="hidden" name="task" value="treetonode.display"/>
-        <input type="hidden" name="boxchecked" value="0"/>
-		<?php echo HTMLHelper::_('form.token') . "\n"; ?>
-
-		<?php echo $this->table_data_div; ?>
-
-    </form>
-<?PHP
-
-echo $this->loadTemplate('footer');
-
+<?php echo $this->loadTemplate('footer'); ?>
