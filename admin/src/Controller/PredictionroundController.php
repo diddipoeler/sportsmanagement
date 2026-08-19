61d048bde8bc7eb6fc35607a3e3dfe290b2e020b
diddipoeler <diddipoeler@gmx.de>
2026-08-19 15:36:34 +0200

    Add native prediction round controller

---
+++ admin/src/Controller/PredictionroundController.php
@@ -0,0 +1,11 @@
+<?php
+namespace Diddipoeler\Component\SportsManagement\Administrator\Controller;
+
+\defined('_JEXEC') or die;
+
+/**
+ * Native Joomla 5/6 form controller for prediction rounds.
+ */
+final class PredictionroundController extends SportsManagementFormController
+{
+}
