d5e3eb9dc7b73a8a2cab1f5a049571c536911bd4
diddipoeler <diddipoeler@gmx.de>
2026-08-19 15:32:44 +0200

    Bridge legacy prediction templates controller

--- admin/controllers/predictiontemplates.php
+++ admin/controllers/predictiontemplates.php
@@ -1,42 +1,14 @@
 <?php
-/**
- *
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage controllers
- * @file       predictiontemplates.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-
-
+/** Legacy compatibility bridge for the native administrator Predictiontemplates controller. */
 defined('_JEXEC') or die('Restricted access');
 
-/**
- * sportsmanagementControllerpredictiontemplates
- *
- * @package
- * @author
- * @copyright diddi
- * @version   2014
- * @access    public
- */
-class sportsmanagementControllerpredictiontemplates extends JSMControllerAdmin
-{
+use Diddipoeler\Component\SportsManagement\Administrator\Controller\PredictiontemplatesController;
 
+if (!class_exists(PredictiontemplatesController::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Controller/SportsManagementAdminController.php';
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Controller/PredictiontemplatesController.php';
+}
 
-	/**
-	 * Proxy for getModel.
-	 *
-	 * @since 1.6
-	 */
-	public function getModel($name = 'predictiontemplate', $prefix = 'sportsmanagementModel', $config = Array())
-	{
-		$model = parent::getModel($name, $prefix, array('ignore_request' => true));
-
-		return $model;
-	}
+if (!class_exists('sportsmanagementControllerpredictiontemplates', false)) {
+    class_alias(PredictiontemplatesController::class, 'sportsmanagementControllerpredictiontemplates');
 }
