96335a790cd224c3b97e434ce6c2e66b9dcdb7e9
diddipoeler <diddipoeler@gmx.de>
2026-08-19 12:58:25 +0200

    Bridge legacy administrator cpanel controller

--- admin/controllers/cpanel.php
+++ admin/controllers/cpanel.php
@@ -1,43 +1,13 @@
 <?php
-/**
- *
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage controllers
- * @file       cpanel.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-
-
+/** Legacy compatibility bridge for the native administrator Cpanel controller. */
 defined('_JEXEC') or die('Restricted access');
 
-use Joomla\CMS\MVC\Controller\FormController;
+use Diddipoeler\Component\SportsManagement\Administrator\Controller\CpanelController;
 
+if (!class_exists(CpanelController::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Controller/CpanelController.php';
+}
 
-/**
- * sportsmanagementControllercpanel
- *
- * @package
- * @author
- * @copyright diddi
- * @version   2014
- * @access    public
- */
-class sportsmanagementControllercpanel extends FormController
-{
-
-	/**
-	 * sportsmanagementControllercpanel::jqueryinstall()
-	 *
-	 * @return void
-	 */
-	function jqueryinstall()
-	{
-
-	}
-
+if (!class_exists('sportsmanagementControllercpanel', false)) {
+    class_alias(CpanelController::class, 'sportsmanagementControllercpanel');
 }
