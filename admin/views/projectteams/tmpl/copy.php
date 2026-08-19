8042ea85bbabbde2f77b10afdb0ca1daeaed2f79
diddipoeler <diddipoeler@gmx.de>
2026-08-19 23:10:44 +0200

    Wire native project team copy dialog

--- admin/views/projectteams/tmpl/copy.php
+++ admin/views/projectteams/tmpl/copy.php
@@ -1,43 +1,33 @@
 <?php
-/**
- *
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage projectteams
- * @file       copy.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-
+/** Project-team copy modal. */
 defined('_JEXEC') or die('Restricted access');
 
+use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
-
 ?>
-
-<form action="<?php echo $this->request_url; ?>" method="post" id="adminForm">
-
+<form action="<?php echo $this->request_url; ?>" method="post" id="adminForm" name="adminForm">
     <fieldset>
-        <legend><?php echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_PROJECTTEAMS_COPY_DEST') ?></legend>
-        <table class="admintable">
-            <tr>
-                <td class="key">
-                    <label for="dest"><?php echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_PROJECTTEAMS_SELECT_PROJECT') . ':'; ?></label>
-                </td>
-                <td>
-					<?php echo $this->lists['projects']; ?>
-                </td>
-            </tr>
-        </table>
+        <legend><?php echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_PROJECTTEAMS_COPY_DEST'); ?></legend>
+        <div class="control-group">
+            <div class="control-label">
+                <label for="dest"><?php echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_PROJECTTEAMS_SELECT_PROJECT'); ?></label>
+            </div>
+            <div class="controls">
+                <?php echo $this->lists['projects']; ?>
+            </div>
+        </div>
     </fieldset>
 
-	<?php foreach ($this->ptids as $ptid)
-		:
-		?>
-        <input type="hidden" name="ptids[]" value="<?php echo $ptid; ?>"/>
-	<?php endforeach; ?>
-    <input type="hidden" name="task" value=""/>
+    <?php foreach ($this->ptids as $ptid) : ?>
+        <input type="hidden" name="ptids[]" value="<?php echo (int) $ptid; ?>" />
+    <?php endforeach; ?>
+    <input type="hidden" name="pid" value="<?php echo (int) $this->project_id; ?>" />
+    <input type="hidden" name="task" value="projectteams.storecopy" />
+    <?php echo HTMLHelper::_('form.token'); ?>
+
+    <div class="mt-3">
+        <button type="submit" class="btn btn-primary">
+            <?php echo Text::_('JTOOLBAR_COPY'); ?>
+        </button>
+    </div>
 </form>
