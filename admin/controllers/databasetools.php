1152c24bc7aac79c886fd315dcda55998f66c32a
diddipoeler <diddipoeler@gmx.de>
2026-08-19 13:32:44 +0200

    Bridge legacy database tools controller

--- admin/controllers/databasetools.php
+++ admin/controllers/databasetools.php
@@ -1,43 +1,13 @@
 <?php
-/**
- *
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage controllers
- * @file       databasetools.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-
+/** Legacy compatibility bridge for the native administrator Databasetools controller. */
 defined('_JEXEC') or die('Restricted access');
 
-use Joomla\CMS\MVC\Controller\BaseController;
+use Diddipoeler\Component\SportsManagement\Administrator\Controller\DatabasetoolsController;
 
-/**
- * sportsmanagementControllerDatabaseTools
- *
- * @package
- * @author
- * @copyright diddi
- * @version   2014
- * @access    public
- */
-class sportsmanagementControllerDatabaseTools extends BaseController
-{
+if (!class_exists(DatabasetoolsController::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Controller/DatabasetoolsController.php';
+}
 
-	/**
-	 * Proxy for getModel.
-	 *
-	 * @since 1.6
-	 */
-	public function getModel($name = 'databasetool', $prefix = 'sportsmanagementModel')
-	{
-		$model = parent::getModel($name, $prefix, array('ignore_request' => true));
-
-		return $model;
-	}
-
+if (!class_exists('sportsmanagementControllerDatabaseTools', false)) {
+    class_alias(DatabasetoolsController::class, 'sportsmanagementControllerDatabaseTools');
 }
