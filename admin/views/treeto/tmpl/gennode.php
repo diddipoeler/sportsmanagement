b6a6a6c6c70f6655ce37dfed961b9f896a943dc0
diddipoeler <diddipoeler@gmx.de>
2026-08-19 18:36:51 +0200

    Modernize treeto node generation layout

--- admin/views/treeto/tmpl/gennode.php
+++ admin/views/treeto/tmpl/gennode.php
@@ -1,49 +1,17 @@
 <?php
-/**
- *
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage treeto
- * @file       gennode.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-
+/** SportsManagement tournament-tree node generation form. */
 defined('_JEXEC') or die('Restricted access');
 
 use Joomla\CMS\HTML\HTMLHelper;
 
-
-$templatesToLoad = array('footer', 'listheader');
+$templatesToLoad = ['footer', 'listheader'];
 sportsmanagementHelper::addTemplatePaths($templatesToLoad, $this);
 ?>
-    <form method="post" name="adminForm" id="adminForm" class="">
-		<?PHP
-		//
-		// if(version_compare(JVERSION,'3.0.0','ge'))
-		// {
-		// echo $this->loadTemplate('joomla3');
-		// }
-		// else
-		// {
-		// echo $this->loadTemplate('joomla2');
-		// }
-
-
-		echo $this->loadTemplate('data');
-
-
-		?>
-
-        <input type="hidden" name="pid" value='<?php echo $this->projectws->id; ?>'/>
-        <input type="hidden" name="id" value='<?php echo $this->treeto->id; ?>'/>
-        <input type="hidden" name="task" value="treeto.generatenode"/>
-		<?php echo HTMLHelper::_('form.token'); ?>
-    </form>
-<?PHP
-
-echo $this->loadTemplate('footer');
-
+<form method="post" name="adminForm" id="adminForm">
+    <?php echo $this->loadTemplate('data'); ?>
+    <input type="hidden" name="pid" value="<?php echo (int) $this->projectws->id; ?>">
+    <input type="hidden" name="id" value="<?php echo (int) $this->treeto->id; ?>">
+    <input type="hidden" name="task" value="treeto.generatenode">
+    <?php echo HTMLHelper::_('form.token'); ?>
+</form>
+<?php echo $this->loadTemplate('footer'); ?>
