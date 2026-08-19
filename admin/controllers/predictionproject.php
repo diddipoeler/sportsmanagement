992f5ef8f0dfe71348c866a5cd6cb1aa784a7d2e
diddipoeler <diddipoeler@gmx.de>
2026-08-19 15:44:12 +0200

    Bridge legacy prediction project controller

--- admin/controllers/predictionproject.php
+++ admin/controllers/predictionproject.php
@@ -1,52 +1,14 @@
 <?php
-/**
- *
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage controllers
- * @file       predictionproject.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-
+/** Legacy compatibility bridge for the native administrator Predictionproject controller. */
 defined('_JEXEC') or die('Restricted access');
 
-use Joomla\CMS\Language\Text;
-use Joomla\CMS\Factory;
-use Joomla\CMS\Session\Session;
-use Joomla\CMS\MVC\Controller\FormController;
+use Diddipoeler\Component\SportsManagement\Administrator\Controller\PredictionprojectController;
 
-/**
- * sportsmanagementControllerpredictionproject
- *
- * @package
- * @author
- * @copyright diddi
- * @version   2014
- * @access    public
- */
-class sportsmanagementControllerpredictionproject extends FormController
-{
+if (!class_exists(PredictionprojectController::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Controller/SportsManagementFormController.php';
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Controller/PredictionprojectController.php';
+}
 
-	/**
-	 * Method to store prediction project
-	 *
-	 * @access public
-	 * @return boolean    True on success
-	 */
-	function store()
-	{
-		$post = Factory::getApplication()->input->post->getArray(array());
-
-		// Check for request forgeries
-		Session::checkToken() or jexit(\Text::_('JINVALID_TOKEN'));
-
-		$model = $this->getModel();
-		$msg   = $model->save($post['jform']);
-		$this->setRedirect('index.php?option=com_sportsmanagement&view=close&tmpl=component', $msg);
-	}
-
+if (!class_exists('sportsmanagementControllerpredictionproject', false)) {
+    class_alias(PredictionprojectController::class, 'sportsmanagementControllerpredictionproject');
 }
