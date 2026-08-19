2c364b088070565162a2cf3cef25ef4a79e80d33
diddipoeler <diddipoeler@gmx.de>
2026-08-19 16:25:55 +0200

    Use native quote text editor input flow

--- admin/views/smquotetxt/view.html.php
+++ admin/views/smquotetxt/view.html.php
@@ -1,71 +1,28 @@
 <?php
-/**
- *
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage smquotetxt
- * @file       view.html.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-
-
+/** SportsManagement administrator quote text editor view. */
 defined('_JEXEC') or die('Restricted access');
 
 use Joomla\CMS\Language\Text;
-use Joomla\CMS\Factory;
 
-
-/**
- * sportsmanagementViewsmquotetxt
- *
- * @package
- * @author    diddi
- * @copyright 2014
- * @version   $Id$
- * @access    public
- */
 class sportsmanagementViewsmquotetxt extends sportsmanagementView
 {
-	/**
-	 * sportsmanagementViewsmquotetxt::init()
-	 *
-	 * @return void
-	 */
-	public function init()
-	{
-		$app             = Factory::getApplication();
-		$jinput          = $app->input;
-		$option          = $jinput->getCmd('option');
-		$model           = $this->getModel();
-		$this->file_name = $jinput->getString('file_name');
+    public function init()
+    {
+        $input = $this->app->getInput();
+        $this->file_name = $input->getString('file_name');
+        $this->form = $this->get('Form');
+        $this->source = $this->get('Source');
+        $this->option = 'com_sportsmanagement';
+    }
 
-		// Initialise variables.
-		$this->form   = $this->get('Form');
-		$this->source = $this->get('Source');
+    protected function addToolbar()
+    {
+        $this->app->getInput()->set('hidemainmenu', true);
+        $this->title = $this->file_name !== ''
+            ? Text::_('COM_SPORTSMANAGEMENT_ADMIN_SMQUOTE_EDIT')
+            : Text::_('COM_SPORTSMANAGEMENT_ADMIN_SMQUOTE_ADD_NEW');
+        $this->icon = 'quote';
 
-		$this->option = $option;
-
-	}
-
-
-	/**
-	 * sportsmanagementViewsmquotetxt::addToolbar()
-	 *
-	 * @return void
-	 */
-	protected function addToolbar()
-	{
-		$jinput = Factory::getApplication()->input;
-		$jinput->set('hidemainmenu', true);
-		$isNew      = $this->item->id ? $this->title = Text::_('COM_SPORTSMANAGEMENT_ADMIN_SMQUOTE_EDIT') : $this->title = Text::_('COM_SPORTSMANAGEMENT_ADMIN_SMQUOTE_ADD_NEW');
-		$this->icon = 'quote';
-
-		parent::addToolbar();
-	}
-
-
+        parent::addToolbar();
+    }
 }
