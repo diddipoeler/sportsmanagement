0c1aca53d82b1b2eb54bf3385c84a4e7e2b77df2
diddipoeler <diddipoeler@gmx.de>
2026-08-19 17:30:02 +0200

    Use native countries model in view

--- admin/views/jlextcountries/view.html.php
+++ admin/views/jlextcountries/view.html.php
@@ -1,81 +1,52 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage jlextcountries
- * @file       view.html.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
+/** SportsManagement administrator countries list view. */
 defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\HTML\HTMLHelper;
-use Joomla\CMS\Table\Table;
-use Joomla\CMS\Language\Text;
-use Joomla\CMS\Toolbar\ToolbarHelper;
-use Joomla\CMS\Form\Form;
-use Joomla\CMS\Log\Log;
 
-/**
- * sportsmanagementViewjlextcountries
- *
- * @package
- * @author
- * @copyright diddi
- * @version   2014
- * @access    public
- */
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Table\Table;
+use Joomla\CMS\Toolbar\ToolbarHelper;
+
 class sportsmanagementViewjlextcountries extends sportsmanagementView
 {
+    public function init()
+    {
+        $this->table = Table::getInstance('jlextcountry', 'sportsmanagementTable');
+        $federations = [
+            HTMLHelper::_('select.option', '0', Text::_('COM_SPORTSMANAGEMENT_GLOBAL_SELECT_FEDERATION')),
+        ];
 
-	/**
-	 * sportsmanagementViewjlextcountries::init()
-	 *
-	 * @return void
-	 */
-	public function init()
-	{
+        if ($res = $this->model->getFederation()) {
+            $federations = array_merge($federations, $res);
+            $this->federation = $res;
+        }
 
-		$inputappend = '';
+        $this->lists = [
+            'federation' => HTMLHelper::_(
+                'select.genericlist',
+                $federations,
+                'filter_federation',
+                'class="inputbox" style="width:140px;" onchange="this.form.submit();"',
+                'value',
+                'text',
+                $this->state->get('filter.federation')
+            ),
+        ];
+    }
 
-		$this->table = Table::getInstance('jlextcountry', 'sportsmanagementTable');
-
-		/** Build the html options for nation */
-		$nation[] = HTMLHelper::_('select.option', '0', Text::_('COM_SPORTSMANAGEMENT_GLOBAL_SELECT_FEDERATION'));
-
-		if ($res = $this->get('Federation'))
-		{
-			$nation           = array_merge($nation, $res);
-			$this->federation = $res;
-		}
-
-		$lists['federation'] = JHtmlSelect::genericlist(
-			$nation,
-			'filter_federation',
-			$inputappend . 'class="inputbox" style="width:140px; " onchange="this.form.submit();"',
-			'value',
-			'text',
-			$this->state->get('filter.federation')
-		);
-
-		$this->lists = $lists;
-      
-
-	}
-
-	/**
-	 * Add the page title and toolbar.
-	 *
-	 * @since 1.7
-	 */
-	protected function addToolbar()
-	{
-		ToolbarHelper::addNew('jlextcountry.add');
-		ToolbarHelper::editList('jlextcountry.edit');
-		ToolbarHelper::custom('jlextcountry.import', 'upload', 'upload', Text::_('JTOOLBAR_UPLOAD'), false);
-		ToolbarHelper::custom('jlextcountries.importplz', 'upload', 'upload', Text::_('COM_SPORTSMANAGEMENT_ADMIN_COUNTRY_IMPORT_PLZ'), true);
-		ToolbarHelper::archiveList('jlextcountry.export', Text::_('JTOOLBAR_EXPORT'));
-		parent::addToolbar();
-	}
+    protected function addToolbar()
+    {
+        ToolbarHelper::addNew('jlextcountry.add');
+        ToolbarHelper::editList('jlextcountry.edit');
+        ToolbarHelper::custom('jlextcountry.import', 'upload', 'upload', Text::_('JTOOLBAR_UPLOAD'), false);
+        ToolbarHelper::custom(
+            'jlextcountries.importplz',
+            'upload',
+            'upload',
+            Text::_('COM_SPORTSMANAGEMENT_ADMIN_COUNTRY_IMPORT_PLZ'),
+            true
+        );
+        ToolbarHelper::archiveList('jlextcountry.export', Text::_('JTOOLBAR_EXPORT'));
+        parent::addToolbar();
+    }
 }
