453a96a11f0be99c1f30718fae3947feb94b5bac
diddipoeler <diddipoeler@gmx.de>
2026-08-19 12:42:38 +0200

    Bridge legacy teams admin controller

--- admin/controllers/teams.php
+++ admin/controllers/teams.php
@@ -1,94 +1,14 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage controllers
- * @file       teams.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
+/** Legacy compatibility bridge for the native administrator Teams controller. */
 defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\Router\Route;
-use Joomla\CMS\Language\Text;
-use Joomla\CMS\Factory;
-use Joomla\CMS\Session\Session;
 
-/**
- * sportsmanagementControllerteams
- *
- * @package
- * @author
- * @copyright diddi
- * @version   2014
- * @access    public
- */
-class sportsmanagementControllerteams extends JSMControllerAdmin
-{
+use Diddipoeler\Component\SportsManagement\Administrator\Controller\TeamsController;
 
-/**
- * sportsmanagementControllerteams::copysave()
- * 
- * @return void
- */
-function copysave()
-	{
-		$model = $this->getModel();
-		$msg   = $model->copysave();
-		$this->setRedirect('index.php?option=com_sportsmanagement&view=teams', $msg);
-	}
-	/**
-	 * Save the manual order inputs from the categories list page.
-	 *
-	 * @return void
-	 * @since  1.6
-	 */
-	public function saveorder()
-	{
-		Session::checkToken() or jexit(Text::_('JINVALID_TOKEN'));
+if (!class_exists(TeamsController::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Controller/SportsManagementAdminController.php';
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Controller/TeamsController.php';
+}
 
-		// Get the arrays from the Request
-		$order         = Factory::getApplication()->input->getVar('order', null, 'post', 'array');
-		$originalOrder = explode(',', Factory::getApplication()->input->getString('original_order_values'));
-
-		// Make sure something has changed
-		if (!($order === $originalOrder))
-		{
-			parent::saveorder();
-		}
-		else
-		{
-			// Nothing to reorder
-			$this->setRedirect(Route::_('index.php?option=' . $this->option . '&view=' . $this->view_list, false));
-
-			return true;
-		}
-	}
-
-	/**
-	 * sportsmanagementControllerteams::saveshort()
-	 *
-	 * @return void
-	 */
-	function saveshort()
-	{
-		$model   = $this->getModel();
-		$msg     = $model->saveshort();
-		$club_id = $this->jsmjinput->getInt('club_id');
-		$this->setRedirect('index.php?option=com_sportsmanagement&view=teams&club_id=' . $club_id, $msg);
-	}
-
-	/**
-	 * Proxy for getModel.
-	 *
-	 * @since 1.6
-	 */
-	public function getModel($name = 'Team', $prefix = 'sportsmanagementModel', $config = Array())
-	{
-		$model = parent::getModel($name, $prefix, array('ignore_request' => true));
-
-		return $model;
-	}
-
+if (!class_exists('sportsmanagementControllerteams', false)) {
+    class_alias(TeamsController::class, 'sportsmanagementControllerteams');
 }
