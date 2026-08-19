db91dfdf99f809b0fe4b142fa1fa1d0959354571
diddipoeler <diddipoeler@gmx.de>
2026-08-19 13:38:21 +0200

    Bridge legacy special extensions model

--- admin/models/specialextensions.php
+++ admin/models/specialextensions.php
@@ -1,64 +1,14 @@
 <?php
-/**
- *
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage models
- * @file       specialextensions.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-
-
+/** Legacy compatibility bridge for the native administrator Specialextensions model. */
 defined('_JEXEC') or die('Restricted access');
 
-use Joomla\CMS\MVC\Model\BaseDatabaseModel;
-use Joomla\CMS\Filesystem\Folder;
+use Diddipoeler\Component\SportsManagement\Administrator\Model\SpecialextensionsModel;
 
-/**
- * sportsmanagementModelspecialextensions
- *
- * @package
- * @author    diddi
- * @copyright 2014
- * @version   $Id$
- * @access    public
- */
-class sportsmanagementModelspecialextensions extends BaseDatabaseModel
-{
-
-	/**
-	 * sportsmanagementModelspecialextensions::getSpecialExtensions()
-	 *
-	 * @return
-	 */
-	function getSpecialExtensions()
-	{
-		$option           = 'com_sportsmanagement';
-		$arrExtensions    = array();
-		$excludeExtension = array();
-
-		if (Folder::exists(JPATH_SITE . DIRECTORY_SEPARATOR . 'components' . DIRECTORY_SEPARATOR . 'com_sportsmanagement' . DIRECTORY_SEPARATOR . 'extensions'))
-		{
-			$folderExtensions = Folder::folders(
-				JPATH_SITE . DIRECTORY_SEPARATOR . 'components' . DIRECTORY_SEPARATOR . 'com_sportsmanagement' . DIRECTORY_SEPARATOR . 'extensions',
-				'.', false, false, $excludeExtension
-			);
-
-			if ($folderExtensions !== false)
-			{
-				foreach ($folderExtensions as $ext)
-				{
-					$arrExtensions[] = $ext;
-				}
-			}
-		}
-
-		return $arrExtensions;
-	}
-
+if (!class_exists(SpecialextensionsModel::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Model/SportsManagementListModel.php';
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Model/SpecialextensionsModel.php';
 }
 
+if (!class_exists('sportsmanagementModelspecialextensions', false)) {
+    class_alias(SpecialextensionsModel::class, 'sportsmanagementModelspecialextensions');
+}
