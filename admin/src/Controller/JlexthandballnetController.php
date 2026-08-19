8e6a6fe88b179b7c9061745408ccb3d08b611def
diddipoeler <diddipoeler@gmx.de>
2026-08-19 16:48:02 +0200

    Add native handball.net import controller

---
+++ admin/src/Controller/JlexthandballnetController.php
@@ -0,0 +1,11 @@
+<?php
+namespace Diddipoeler\Component\SportsManagement\Administrator\Controller;
+
+\defined('_JEXEC') or die;
+
+use Joomla\CMS\MVC\Controller\BaseController;
+
+/** Native Joomla 5/6 controller for the handball.net import screen. */
+final class JlexthandballnetController extends BaseController
+{
+}
