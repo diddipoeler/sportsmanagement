5b6c4a10e5d2e6a2d4dec5a8d1f8bc603d20fcf9
diddipoeler <diddipoeler@gmx.de>
2026-08-19 14:00:46 +0200

    Bridge legacy installhelper model

--- admin/models/installhelper.php
+++ admin/models/installhelper.php
@@ -1,85 +1,14 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage models
- * @file       installhelper.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
+/** Legacy compatibility bridge for the native administrator Installhelper model. */
 defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\Language\Text;
-use Joomla\CMS\Factory;
-use Joomla\CMS\Component\ComponentHelper;
-use Joomla\CMS\MVC\Model\BaseDatabaseModel;
-use Joomla\CMS\Filter\OutputFilter;
-use Joomla\Utilities\ArrayHelper;
-use Joomla\CMS\Log\Log;
 
-/**
- * sportsmanagementModelinstallhelper
- * 
- * @package 
- * @author Dieter Plöger
- * @copyright 2021
- * @version $Id$
- * @access public
- */
-class sportsmanagementModelinstallhelper extends JSMModelAdmin
-{
+use Diddipoeler\Component\SportsManagement\Administrator\Model\InstallhelperModel;
 
-	/**
-	 * Override parent constructor.
-	 *
-	 * @param   array  $config  An optional associative array of configuration settings.
-	 *
-	 * @see   BaseDatabaseModel
-	 * @since 3.2
-	 */
-	public function __construct($config = array())
-	{
-		parent::__construct($config);
-	}
-    
-    
-    /**
-     * sportsmanagementModelinstallhelper::savesportstype()
-     * 
-     * @param mixed $post
-     * @return void
-     */
-    function savesportstype($post = array() )
-    {
-    
-    if ( !$post['filter_sports_type'] )
-    {
-    JSMModelAdmin::setWarning(Text::_('COM_SPORTSMANAGEMENT_ADMIN_INSTALLHELPER_ERROR_1'));
-    //Factory::getApplication()->enqueueMessage(Text::_(__METHOD__.' '.__LINE__.' warning <pre>'.print_r(JSMModelAdmin::$_warnings,true).'</pre>' ), '');
-    //  $this->setMessage($model->getError(), 'error');
-    return JSMModelAdmin::$_warnings;    
-    }   
-    else
-    {
-        $this->jsmquery->clear();
-        $profile = new stdClass;
-		$profile->name = 'COM_SPORTSMANAGEMENT_ST_'.strtoupper($post['filter_sports_type']);
-        
-        $profile->modified         = $this->jsmdate->toSql();
-		$profile->modified_by      = $this->jsmuser->get('id');
-		$profile->checked_out      = 0;
-		$profile->checked_out_time = $this->jsmdb->getNullDate();
-        
-		$insertresult = $this->jsmdb->insertObject('#__sportsmanagement_sports_type', $profile);
-        
-        
-    return true;     
-    } 
-        
-       
-    }
+if (!class_exists(InstallhelperModel::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Model/SportsManagementAdminModel.php';
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Model/InstallhelperModel.php';
+}
 
-
-
+if (!class_exists('sportsmanagementModelinstallhelper', false)) {
+    class_alias(InstallhelperModel::class, 'sportsmanagementModelinstallhelper');
 }
