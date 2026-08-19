801f1254a2305eed48b1f2624bf8d97e27346415
diddipoeler <diddipoeler@gmx.de>
2026-08-19 12:30:22 +0200

    Bridge legacy season admin controller

--- admin/controllers/season.php
+++ admin/controllers/season.php
@@ -1,45 +1,19 @@
 <?php
 /**
+ * SportsManagement legacy compatibility bridge.
  *
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage controllers
- * @file       season.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * The active Joomla 5/6 implementation lives in admin/src/Controller/SeasonController.php.
  */
-
 
 defined('_JEXEC') or die('Restricted access');
 
+use Diddipoeler\Component\SportsManagement\Administrator\Controller\SeasonController;
 
-/**
- * sportsmanagementControllerseason
- *
- * @package
- * @author
- * @copyright diddi
- * @version   2014
- * @access    public
- */
-class sportsmanagementControllerseason extends JSMControllerForm
-{
+if (!class_exists(SeasonController::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Controller/SportsManagementFormController.php';
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Controller/SeasonController.php';
+}
 
-	/**
-	 * Class Constructor
-	 *
-	 * @param   array  $config  An optional associative array of configuration settings.
-	 *
-	 * @return void
-	 * @since  1.5
-	 */
-	function __construct($config = array())
-	{
-		parent::__construct($config);
-	}
-
-
+if (!class_exists('sportsmanagementControllerseason', false)) {
+    class_alias(SeasonController::class, 'sportsmanagementControllerseason');
 }
