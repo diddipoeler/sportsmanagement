2f1779e35d7382e3c49863c31b19811b0c6c1fcb
diddipoeler <diddipoeler@gmx.de>
2026-08-19 16:30:58 +0200

    Use native input in extended XML editor view

--- admin/views/smextxmleditor/view.html.php
+++ admin/views/smextxmleditor/view.html.php
@@ -1,59 +1,29 @@
 <?php
 /**
- *
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage smextxmleditor
- * @file       view.html.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * SportsManagement administrator extended XML/PHP editor view.
  */
-
 defined('_JEXEC') or die('Restricted access');
 
-use Joomla\CMS\Factory;
 use Joomla\CMS\Toolbar\ToolbarHelper;
 
-/**
- * sportsmanagementViewsmextxmleditor
- *
- * @package
- * @author
- * @copyright diddi
- * @version   2013
- * @access    public
- */
 class sportsmanagementViewsmextxmleditor extends sportsmanagementView
 {
-	/**
-	 * sportsmanagementViewsmextxmleditor::init()
-	 *
-	 * @return void
-	 */
-	public function init()
-	{
-		$this->file_name = $this->jinput->getString('file_name', "");
-		$this->form      = $this->get('Form');
-		$this->source    = $this->get('Source');
-	}
+    public function init()
+    {
+        $input = $this->app->getInput();
+        $this->file_name = $input->getString('file_name', '');
+        $this->form = $this->get('Form');
+        $this->source = $this->get('Source');
+    }
 
-	/**
-	 * Add the page title and toolbar.
-	 *
-	 * @since 1.7
-	 */
-	protected function addToolbar()
-	{
-		$this->jinput->set('hidemainmenu', true);
-		parent::addToolbar();
-		ToolbarHelper::apply('smextxmleditor.apply');
-		ToolbarHelper::save('smextxmleditor.save');
-		ToolbarHelper::cancel('smextxmleditor.cancel', 'JTOOLBAR_CANCEL');
-		$this->title = $this->file_name;
-		$this->icon  = 'xml-edit';
-	}
-
+    protected function addToolbar()
+    {
+        $this->app->getInput()->set('hidemainmenu', true);
+        parent::addToolbar();
+        ToolbarHelper::apply('smextxmleditor.apply');
+        ToolbarHelper::save('smextxmleditor.save');
+        ToolbarHelper::cancel('smextxmleditor.cancel', 'JTOOLBAR_CANCEL');
+        $this->title = $this->file_name;
+        $this->icon = 'xml-edit';
+    }
 }
