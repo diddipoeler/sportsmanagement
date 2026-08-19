1e17463bdf162f29a588c838a7500d63db2aacb3
diddipoeler <diddipoeler@gmx.de>
2026-08-19 22:20:35 +0200

    Simplify GitHub issue result view

--- admin/views/github/tmpl/github_result.php
+++ admin/views/github/tmpl/github_result.php
@@ -1,76 +1,17 @@
 <?php
-/**
- *
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage github
- * @file       github_result.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-
 defined('_JEXEC') or die('Restricted access');
 
-use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
-use Joomla\CMS\Factory;
 use Joomla\CMS\Router\Route;
-
-// Include the component HTML helpers.
-HTMLHelper::addIncludePath(JPATH_COMPONENT . '/helpers/html');
-
-
-// Welche joomla version ?
-if (version_compare(JVERSION, '3.0.0', 'ge'))
-{
-	HTMLHelper::_('jquery.framework');
-}
-
-
-HTMLHelper::_('behavior.keepalive');
-
-
-Factory::getDocument()->addScriptDeclaration(
-	'
-	Joomla.submitbutton = function(task)
-	{
-		if (task == "github.cancel" )
-		{
-			' . '
-
-			if (window.opener && task == "github.cancel" )
-			{
-				window.opener.document.closeEditWindow = self;
-				window.opener.setTimeout("window.document.closeEditWindow.close()", 1000);
-			}
-
-			Joomla.submitform(task, document.getElementById("addissue-formresult"));
-		}
-	};
-'
-);
-
 ?>
-
 <div class="container-popup">
-
-    <div class="pull-right">
-        <button class="btn" type="button"
-                onclick="Joomla.submitbutton('github.cancel', this.form);"><?php echo Text::_('JCANCEL') ?></button>
+    <div class="alert alert-info">
+        <?php echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_GITHUB_TITLE'); ?>
     </div>
-    <div class="clearfix"></div>
-
-    <form action="<?php echo Route::_('index.php?option=com_sportsmanagement'); ?>" id='addissue-formresult'
-          method='post' style='display:inline' name='adminform'>
-        <input type="hidden" name="component" value="<?PHP echo $this->option; ?>"/>
-        <input type='hidden' name='task' value=''/>
-        <input type="hidden" name="close" id="close" value="0"/>
-        <input type="hidden" name="gh.token" value="<?PHP echo $this->gh_token; ?>"/>
-        <input type="hidden" name="api.username" value="<?PHP echo $this->api_username; ?>"/>
-        <input type="hidden" name="api.password" value="<?PHP echo $this->api_password; ?>"/>
-    </form>
-
+    <a class="btn btn-primary" href="<?php echo Route::_('index.php?option=com_sportsmanagement&view=github&tmpl=component'); ?>">
+        <?php echo Text::_('JBACK'); ?>
+    </a>
+    <a class="btn btn-secondary" href="<?php echo Route::_('index.php?option=com_sportsmanagement&view=close&tmpl=component'); ?>">
+        <?php echo Text::_('JCLOSE'); ?>
+    </a>
 </div>
