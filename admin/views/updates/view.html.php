8aa187e4bda9e8c403663e849487aaf20fefe977
diddipoeler <diddipoeler@gmx.de>
2026-08-19 20:29:50 +0200

    Modernize updates administrator view

--- admin/views/updates/view.html.php
+++ admin/views/updates/view.html.php
@@ -1,81 +1,42 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage updates
- * @file       view.html.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
+/** Joomla 5/6 administrator updates view. */
 defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\Language\Text;
-use Joomla\CMS\Factory;
-use Joomla\CMS\Uri\Uri;
-jimport('joomla.html.html.bootstrap');
 
-/**
- * sportsmanagementViewUpdates
- *
- * @package
- * @author
- * @copyright diddi
- * @version   2014
- * @access    public
- */
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Uri\Uri;
+
 class sportsmanagementViewUpdates extends sportsmanagementView
 {
+    public function init()
+    {
+        $this->app->setUserState($this->option . 'update_part', 0);
+        $filterOrder = $this->app->getUserStateFromRequest(
+            $this->option . 'updates_filter_order',
+            'filter_order',
+            'dates',
+            'cmd'
+        );
+        $filterOrderDirection = $this->app->getUserStateFromRequest(
+            $this->option . 'updates_filter_order_Dir',
+            'filter_order_Dir',
+            '',
+            'word'
+        );
 
-	/**
-	 * sportsmanagementViewUpdates::init()
-	 *
-	 * @return void
-	 */
-	public function init()
-	{
-		$this->app->setUserState($this->option . 'update_part', 0); // 0
-		$filter_order     = $this->app->getUserStateFromRequest($this->option . 'updates_filter_order', 'filter_order', 'dates', 'cmd');
-		$filter_order_Dir = $this->app->getUserStateFromRequest($this->option . 'updates_filter_order_Dir', 'filter_order_Dir', '', 'word');
+        $this->versions = $this->model->getVersions();
+        $this->versionhistory = $this->model->getVersionHistory();
+        $this->updateFiles = $this->model->loadUpdateFiles();
+        $this->request_url = Uri::getInstance()->toString();
+        $this->lists = [
+            'order_Dir' => $filterOrderDirection,
+            'order' => $filterOrder,
+        ];
+    }
 
-		$db = sportsmanagementHelper::getDBConnection();
-
-		if (version_compare(JVERSION, '4.0.0', 'ge'))
-		{
-			$uri = Uri::getInstance();
-		}
-		else
-		{
-			$uri = Factory::getURI();
-		}
-
-		$model       = $this->getModel();
-		$this->versions    = $model->getVersions();
-        $this->versionhistory    = $model->getVersionHistory();
-		$updateFiles = array();
-		$lists       = array();
-		$updateFiles = $model->loadUpdateFiles();
-
-		// Table ordering
-		$lists['order_Dir'] = $filter_order_Dir;
-		$lists['order']     = $filter_order;
-		$this->updateFiles  = $updateFiles;
-		$this->request_url  = $uri->toString();
-		$this->lists        = $lists;
-
-	}
-
-	/**
-	 * Add the page title and toolbar.
-	 *
-	 * @since 1.7
-	 */
-	protected function addToolbar()
-	{
-		$this->title = Text::_('COM_SPORTSMANAGEMENT_ADMIN_UPDATES_TITLE');
-		$this->icon  = 'updates';
-		parent::addToolbar();
-	}
-
+    protected function addToolbar()
+    {
+        $this->title = Text::_('COM_SPORTSMANAGEMENT_ADMIN_UPDATES_TITLE');
+        $this->icon = 'updates';
+        parent::addToolbar();
+    }
 }
-
