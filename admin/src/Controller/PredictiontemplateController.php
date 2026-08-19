8bdec95f4c84583d03cad70050bf119ba4c76db8
diddipoeler <diddipoeler@gmx.de>
2026-08-19 15:31:52 +0200

    Add native prediction template controller

---
+++ admin/src/Controller/PredictiontemplateController.php
@@ -0,0 +1,11 @@
+<?php
+namespace Diddipoeler\Component\SportsManagement\Administrator\Controller;
+
+\defined('_JEXEC') or die;
+
+/**
+ * Native Joomla 5/6 form controller for prediction templates.
+ */
+final class PredictiontemplateController extends SportsManagementFormController
+{
+}
