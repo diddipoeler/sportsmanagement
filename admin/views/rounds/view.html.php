ff0e676f9a1f9ff1932fd3287fe0a57a5842d571
diddipoeler <diddipoeler@gmx.de>
2026-08-19 16:19:32 +0200

    Use native rounds model in administrator view

--- admin/views/rounds/view.html.php
+++ admin/views/rounds/view.html.php
@@ -1,203 +1,177 @@
 <?php
 /**
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage rounds
- * @file       view.html.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * SportsManagement administrator rounds view.
  */
 defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\HTML\HTMLHelper;
-use Joomla\CMS\Uri\Uri;
-use Joomla\CMS\Table\Table;
-use Joomla\CMS\Language\Text;
-use Joomla\CMS\MVC\Model\BaseDatabaseModel;
-use Joomla\CMS\Factory;
-use Joomla\CMS\Toolbar\ToolbarHelper;
-use Joomla\CMS\Form\Form;
-use Joomla\CMS\Log\Log;
 
-/**
- * sportsmanagementViewRounds
- *
- * @package
- * @author
- * @copyright diddi
- * @version   2014
- * @access    public
- */
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Table\Table;
+use Joomla\CMS\Toolbar\ToolbarHelper;
+use Joomla\CMS\Uri\Uri;
+
 class sportsmanagementViewRounds extends sportsmanagementView
 {
+    public function init()
+    {
+        $this->massadd = 0;
+        $this->populate = 0;
+        $tpl = null;
 
-	/**
-	 * sportsmanagementViewRounds::init()
-	 *
-	 * @return
-	 */
-	public function init()
-	{
-		$app            = Factory::getApplication();
-		$this->massadd  = 0;
-		$this->populate = 0;
-		$tpl            = null;
-		/** dadurch werden die spaltenbreiten optimiert */
-		$this->document->addStyleSheet(Uri::root() . 'administrator/components/com_sportsmanagement/assets/css/form_control.css');
-        
-        switch ($this->getLayout())
-		{
-			case 'default':
-			case 'default_3':
-			case 'default_4':
-			$this->_displayDefault($tpl);
-			return;
+        $this->document->addStyleSheet(
+            Uri::root() . 'administrator/components/com_sportsmanagement/assets/css/form_control.css'
+        );
+
+        switch ($this->getLayout()) {
+            case 'default':
+            case 'default_3':
+            case 'default_4':
+                $this->_displayDefault($tpl);
+                return;
+
             case 'populate':
-			case 'populate_3':
-			case 'populate_4':
-			$this->_displayPopulate($tpl);
-			return;
+            case 'populate_3':
+            case 'populate_4':
+                $this->_displayPopulate($tpl);
+                return;
+
             case 'massadd':
-			case 'massadd_3':
-			case 'massadd_4':
-			$this->_displayMassadd($tpl);
-			return;
-		}
-		
-	
+            case 'massadd_3':
+            case 'massadd_4':
+                $this->_displayMassadd($tpl);
+                return;
+        }
+    }
 
-	}
+    public function _displayDefault($tpl)
+    {
+        $this->get('Items');
+        $this->table = Table::getInstance('round', 'sportsmanagementTable');
+        $this->project_id = (int) $this->app->getUserState('com_sportsmanagement.pid', 0);
+        $this->project = $this->model->getProject($this->project_id);
 
-	/**
-	 * sportsmanagementViewRounds::_displayDefault()
-	 *
-	 * @param   mixed  $tpl
-	 *
-	 * @return void
-	 */
-	function _displayDefault($tpl)
-	{
-		$matchday = $this->get('Items');
-		$this->table = Table::getInstance('round', 'sportsmanagementTable');
-		$this->project_id = $this->app->getUserState("$this->option.pid", '0');
+        $lists = [];
+        $lists['tournementround'] = [
+            HTMLHelper::_('select.option', '0', Text::_('JNO')),
+            HTMLHelper::_('select.option', '1', Text::_('JYES')),
+        ];
 
-		$mdlProject               = BaseDatabaseModel::getInstance('Project', 'sportsmanagementModel');
-		$this->project                  = $mdlProject->getProject($this->project_id);
-		$myoptions                = array();
-		$myoptions[]              = HTMLHelper::_('select.option', '0', Text::_('JNO'));
-		$myoptions[]              = HTMLHelper::_('select.option', '1', Text::_('JYES'));
-		$lists['tournementround'] = $myoptions;
+        $this->lists = $lists;
+        $this->matchday = $this->items;
+    }
 
-		$this->lists    = $lists;
-		$this->matchday = $this->items;
-		
-		
-	}
+    public function _displayPopulate($tpl)
+    {
+        $this->document->setTitle(Text::_('COM_SPORTSMANAGEMENT_ADMIN_ROUNDS_POPULATE_TITLE'));
 
-	/**
-	 * sportsmanagementViewRounds::_displayPopulate()
-	 *
-	 * @param   mixed  $tpl
-	 *
-	 * @return void
-	 */
-	function _displayPopulate($tpl)
-	{
+        $options = [
+            HTMLHelper::_(
+                'select.option',
+                0,
+                Text::_('COM_SPORTSMANAGEMENT_ADMIN_ROUNDS_POPULATE_TYPE_SINGLE_ROUND_ROBIN')
+            ),
+            HTMLHelper::_(
+                'select.option',
+                1,
+                Text::_('COM_SPORTSMANAGEMENT_ADMIN_ROUNDS_POPULATE_TYPE_DOUBLE_ROUND_ROBIN')
+            ),
+            HTMLHelper::_(
+                'select.option',
+                2,
+                Text::_('COM_SPORTSMANAGEMENT_ADMIN_ROUNDS_POPULATE_TYPE_TOURNAMENT_ROUND_ROBIN')
+            ),
+        ];
+        $lists = [];
+        $lists['scheduling'] = HTMLHelper::_(
+            'select.genericlist',
+            $options,
+            'scheduling',
+            '',
+            'value',
+            'text'
+        );
 
-		$this->document->setTitle(Text::_('COM_SPORTSMANAGEMENT_ADMIN_ROUNDS_POPULATE_TITLE'));
-		$lists = array();
+        $this->project_id = (int) $this->app->getUserState('com_sportsmanagement.pid', 0);
+        $teams = $this->model->getProjectTeamsOptions($this->project_id);
+        $project = $this->model->getProject($this->project_id);
+        $teamOptions = [];
 
-		$options             = array(HTMLHelper::_('select.option', 0, Text::_('COM_SPORTSMANAGEMENT_ADMIN_ROUNDS_POPULATE_TYPE_SINGLE_ROUND_ROBIN')),
-			HTMLHelper::_('select.option', 1, Text::_('COM_SPORTSMANAGEMENT_ADMIN_ROUNDS_POPULATE_TYPE_DOUBLE_ROUND_ROBIN')),
-			HTMLHelper::_('select.option', 2, Text::_('COM_SPORTSMANAGEMENT_ADMIN_ROUNDS_POPULATE_TYPE_TOURNAMENT_ROUND_ROBIN'))
-		);
-		$lists['scheduling'] = HTMLHelper::_('select.genericlist', $options, 'scheduling', '', 'value', 'text');
+        foreach ($teams as $team) {
+            $teamOptions[] = HTMLHelper::_('select.option', $team->value, $team->text);
+        }
 
-		$this->project_id = $this->app->getUserState("$this->option.pid", '0');
-		$mdlProject       = BaseDatabaseModel::getInstance("Project", "sportsmanagementModel");
-		$teams            = $mdlProject->getProjectTeamsOptions($this->project_id);
-		$projectws        = $mdlProject->getProject($this->project_id);
+        $lists['teamsorder'] = HTMLHelper::_(
+            'select.genericlist',
+            $teamOptions,
+            'teamsorder[]',
+            'multiple="multiple" size="20"'
+        );
 
-		$options = array();
+        $this->projectws = $project;
+        $this->lists = $lists;
+        $this->populate = 1;
+        $this->setLayout('populate');
+    }
 
-		foreach ($teams as $t)
-		{
-			$options[] = HTMLHelper::_('select.option', $t->value, $t->text);
-		}
+    public function _displayMassadd($tpl)
+    {
+        $this->project_id = (int) $this->app->getUserState('com_sportsmanagement.pid', 0);
+        $this->project = $this->model->getProject($this->project_id);
+        $this->setLayout('massadd');
+    }
 
-		$lists['teamsorder'] = HTMLHelper::_('select.genericlist', $options, 'teamsorder[]', 'multiple="multiple" size="20"');
+    protected function addToolbar()
+    {
+        $this->title = Text::_('COM_SPORTSMANAGEMENT_ADMIN_ROUNDS_TITLE');
+        ToolbarHelper::back(
+            'JPREV',
+            'index.php?option=com_sportsmanagement&view=project&layout=panel&id=' . $this->project_id
+        );
 
-		$this->projectws = $projectws;
-		$this->lists     = $lists;
-		$this->populate  = 1;
-		$this->setLayout('populate');
-	}
+        if (!$this->massadd) {
+            if ($this->populate) {
+                $this->title = Text::_('COM_SPORTSMANAGEMENT_ADMIN_ROUNDS_POPULATE_TITLE');
+                ToolbarHelper::apply('round.startpopulate');
+                ToolbarHelper::back();
+                parent::addToolbar();
+                return;
+            }
 
-	/**
-	 * sportsmanagementViewRounds::_displayMassadd()
-	 *
-	 * @param   mixed  $tpl
-	 *
-	 * @return void
-	 */
-	function _displayMassadd($tpl)
-	{
+            ToolbarHelper::publishList('rounds.publish');
+            ToolbarHelper::unpublishList('rounds.unpublish');
+            ToolbarHelper::divider();
+            ToolbarHelper::custom(
+                'rounds.populate',
+                'purge.png',
+                'purge_f2.png',
+                Text::_('COM_SPORTSMANAGEMENT_ADMIN_ROUNDS_POPULATE_BUTTON'),
+                false
+            );
+            ToolbarHelper::divider();
+            ToolbarHelper::apply('rounds.saveshort');
+            ToolbarHelper::divider();
+            sportsmanagementHelper::ToolbarButton(
+                'massadd',
+                'new',
+                Text::_('COM_SPORTSMANAGEMENT_ADMIN_ROUNDS_MASSADD_BUTTON')
+            );
+            ToolbarHelper::addNew('round.save');
+            ToolbarHelper::divider();
+            ToolbarHelper::deleteList(
+                '',
+                'rounds.deleteroundmatches',
+                Text::_('COM_SPORTSMANAGEMENT_ADMIN_ROUNDS_MASSDEL_BUTTON')
+            );
+            parent::addToolbar();
+            return;
+        }
 
-		$this->project_id = $this->app->getUserState("$this->option.pid", '0');
-
-		$mdlProject    = BaseDatabaseModel::getInstance("Project", "sportsmanagementModel");
-		$project       = $mdlProject->getProject($this->project_id);
-		$this->project = $project;
-		$this->setLayout('massadd');
-	}
-
-	/**
-	 * Add the page title and toolbar.
-	 *
-	 * @since 1.6
-	 */
-	protected function addToolbar()
-	{
-		$this->title = Text::_('COM_SPORTSMANAGEMENT_ADMIN_ROUNDS_TITLE');
-        ToolbarHelper::back('JPREV', 'index.php?option=com_sportsmanagement&view=project&layout=panel&id='.$this->project_id);
-
-		if (!$this->massadd)
-		{
-			if ($this->populate)
-			{
-				$this->title = Text::_('COM_SPORTSMANAGEMENT_ADMIN_ROUNDS_POPULATE_TITLE');
-				ToolbarHelper::apply('round.startpopulate');
-				ToolbarHelper::back();
-				parent::addToolbar();
-			}
-			else
-			{
-				// JLToolBarHelper::custom('round.roundrobin','purge.png','purge_f2.png',Text::_('COM_SPORTSMANAGEMENT_ADMIN_ROUND_ROBIN_MASSADD_BUTTON'),false);
-				ToolbarHelper::publishList('rounds.publish');
-				ToolbarHelper::unpublishList('rounds.unpublish');
-				ToolbarHelper::divider();
-				ToolbarHelper::custom('rounds.populate', 'purge.png', 'purge_f2.png', Text::_('COM_SPORTSMANAGEMENT_ADMIN_ROUNDS_POPULATE_BUTTON'), false);
-				ToolbarHelper::divider();
-				ToolbarHelper::apply('rounds.saveshort');
-				ToolbarHelper::divider();
-
-				sportsmanagementHelper::ToolbarButton('massadd', 'new', Text::_('COM_SPORTSMANAGEMENT_ADMIN_ROUNDS_MASSADD_BUTTON'));
-
-				ToolbarHelper::addNew('round.save');
-				ToolbarHelper::divider();
-
-				ToolbarHelper::deleteList('', 'rounds.deleteroundmatches', Text::_('COM_SPORTSMANAGEMENT_ADMIN_ROUNDS_MASSDEL_BUTTON'));
-				parent::addToolbar();
-			}
-		}
-		else
-		{
-			ToolbarHelper::custom('round.cancelmassadd', 'cancel.png', 'cancel_f2.png', Text::_('COM_SPORTSMANAGEMENT_ADMIN_ROUNDS_MASSADD_CANCEL'), false);
-		}
-
-	}
-
-
+        ToolbarHelper::custom(
+            'round.cancelmassadd',
+            'cancel.png',
+            'cancel_f2.png',
+            Text::_('COM_SPORTSMANAGEMENT_ADMIN_ROUNDS_MASSADD_CANCEL'),
+            false
+        );
+    }
 }
-
