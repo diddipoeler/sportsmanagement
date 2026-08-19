efbc0b18b8ee3bc9388f80d9e90f30de473ee899
diddipoeler <diddipoeler@gmx.de>
2026-08-19 22:18:46 +0200

    Add native GitHub integration controller

---
+++ admin/src/Controller/GithubController.php
@@ -0,0 +1,44 @@
+<?php
+namespace Diddipoeler\Component\SportsManagement\Administrator\Controller;
+
+\defined('_JEXEC') or die;
+
+use Joomla\CMS\Factory;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\MVC\Controller\BaseController;
+
+/** Native administrator controller for the GitHub helper view. */
+final class GithubController extends BaseController
+{
+    public function addissue(): void
+    {
+        $this->checkToken();
+        $identity = Factory::getApplication()->getIdentity();
+
+        if (!$identity->authorise('core.manage', 'com_sportsmanagement')) {
+            throw new \RuntimeException(Text::_('JERROR_ALERTNOAUTHOR'), 403);
+        }
+
+        $model = $this->getModel('Github', 'Administrator', ['ignore_request' => true]);
+        $success = $model !== false && $model->addissue();
+
+        $this->setRedirect(
+            'index.php?option=com_sportsmanagement&view=github&tmpl=component&layout=github_result',
+            $success ? 'GitHub-Issue wurde erstellt.' : 'GitHub-Issue konnte nicht erstellt werden.',
+            $success ? 'message' : 'error'
+        );
+    }
+
+    public function cancel(): void
+    {
+        $this->setRedirect(
+            'index.php?option=com_sportsmanagement&view=close&tmpl=component',
+            Text::_('JLIB_HTML_BEHAVIOR_CLOSE')
+        );
+    }
+
+    public function getModel($name = 'Github', $prefix = 'Administrator', $config = [])
+    {
+        return parent::getModel($name, $prefix, $config + ['ignore_request' => true]);
+    }
+}
