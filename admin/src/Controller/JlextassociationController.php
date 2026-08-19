2679560222568145046f004221ea39e5cd38cfeb
diddipoeler <diddipoeler@gmx.de>
2026-08-19 17:08:08 +0200

    Add native association form controller

---
+++ admin/src/Controller/JlextassociationController.php
@@ -0,0 +1,9 @@
+<?php
+namespace Diddipoeler\Component\SportsManagement\Administrator\Controller;
+
+\defined('_JEXEC') or die;
+
+/** Native Joomla 5/6 form controller for associations. */
+final class JlextassociationController extends SportsManagementFormController
+{
+}
