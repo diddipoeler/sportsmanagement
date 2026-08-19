f4dfd562fb77ac92afbdbf447c4cb3411d3f2097
diddipoeler <diddipoeler@gmx.de>
2026-08-19 19:43:01 +0200

    Remove prediction members legacy list wrapper

--- admin/views/predictionmembers/tmpl/default.php
+++ admin/views/predictionmembers/tmpl/default.php
@@ -1,30 +1,25 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage predictionmembers
- * @file       default.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
+/** Prediction members list wrapper for Joomla 5/6. */
 defined('_JEXEC') or die('Restricted access');
+
 use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Layout\LayoutHelper;
 
-$templatesToLoad = array('footer', 'listheader');
-sportsmanagementHelper::addTemplatePaths($templatesToLoad, $this);
-
+sportsmanagementHelper::addTemplatePaths(['footer'], $this);
 ?>
 <form action="<?php echo $this->request_url; ?>" method="post" id="adminForm" name="adminForm">
+    <?php if ($this->filterForm) : ?>
+        <?php echo LayoutHelper::render('joomla.searchtools.default', ['view' => $this]); ?>
+    <?php endif; ?>
 
-	<?PHP
-	echo $this->loadTemplate('joomla_version');
-	?>
-    <input type="hidden" name="task" value=""/>
-    <input type="hidden" name="boxchecked" value="0"/>
-    <input type="hidden" name="prediction_id" value="<?php echo $this->prediction_id; ?>"/>
-    <input type="hidden" name="filter_order" value="<?php echo $this->sortColumn; ?>"/>
-    <input type="hidden" name="filter_order_Dir" value="<?php echo $this->sortDirection; ?>"/>
-	<?php echo HTMLHelper::_('form.token'); ?>
+    <?php echo $this->loadTemplate('data'); ?>
+
+    <input type="hidden" name="task" value="">
+    <input type="hidden" name="boxchecked" value="0">
+    <input type="hidden" name="prediction_id" value="<?php echo (int) $this->prediction_id; ?>">
+    <input type="hidden" name="filter_order" value="<?php echo $this->escape($this->sortColumn); ?>">
+    <input type="hidden" name="filter_order_Dir" value="<?php echo $this->escape($this->sortDirection); ?>">
+    <?php echo HTMLHelper::_('form.token'); ?>
 </form>
+
+<?php echo $this->loadTemplate('footer'); ?>
