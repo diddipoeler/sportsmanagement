fb10e3c71c110843b7d4a03269daa7caf4f9125b
diddipoeler <diddipoeler@gmx.de>
2026-08-19 15:23:02 +0200

    Add native project position controller

---
+++ admin/src/Controller/ProjectpositionController.php
@@ -0,0 +1,11 @@
+<?php
+namespace Diddipoeler\Component\SportsManagement\Administrator\Controller;
+
+\defined('_JEXEC') or die;
+
+/**
+ * Native Joomla 5/6 form controller for project-position assignments.
+ */
+final class ProjectpositionController extends SportsManagementFormController
+{
+}
