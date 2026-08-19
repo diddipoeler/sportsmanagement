3a3a496220aae256994c33adde62612b14abe2ae
diddipoeler <diddipoeler@gmx.de>
2026-08-19 12:30:01 +0200

    Bridge legacy rosterposition admin controller

--- admin/controllers/rosterposition.php
+++ admin/controllers/rosterposition.php
@@ -1,31 +1,19 @@
 <?php
 /**
+ * SportsManagement legacy compatibility bridge.
  *
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage controllers
- * @file       rosterposition.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * The active Joomla 5/6 implementation lives in admin/src/Controller/RosterpositionController.php.
  */
-
 
 defined('_JEXEC') or die('Restricted access');
 
-/**
- * sportsmanagementControllerrosterposition
- *
- * @package
- * @author
- * @copyright diddi
- * @version   2014
- * @access    public
- */
-class sportsmanagementControllerrosterposition extends JSMControllerForm
-{
+use Diddipoeler\Component\SportsManagement\Administrator\Controller\RosterpositionController;
 
+if (!class_exists(RosterpositionController::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Controller/SportsManagementFormController.php';
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Controller/RosterpositionController.php';
+}
 
+if (!class_exists('sportsmanagementControllerrosterposition', false)) {
+    class_alias(RosterpositionController::class, 'sportsmanagementControllerrosterposition');
 }
