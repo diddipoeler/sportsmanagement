9c15fa19222f0e2db7abb5245df792fb052296c5
diddipoeler <diddipoeler@gmx.de>
2026-08-19 17:49:25 +0200

    Modernize Google calendars list view

--- admin/views/jsmgcalendars/view.html.php
+++ admin/views/jsmgcalendars/view.html.php
@@ -1,55 +1,27 @@
 <?php
-/**
- *
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage jsmgcalendars
- * @file       view.html.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
+/** SportsManagement administrator Google calendars list view. */
+defined('_JEXEC') or die('Restricted access');
 
-defined('_JEXEC') or die();
-
-use Joomla\CMS\Factory;
 use Joomla\CMS\Toolbar\ToolbarHelper;
 
-/**
- * sportsmanagementViewjsmgcalendars
- *
- * @package
- * @author    Dieter Plöger
- * @copyright 2015
- * @version   $Id$
- * @access    public
- */
 class sportsmanagementViewjsmgcalendars extends sportsmanagementView
 {
+    public function init()
+    {
+        $this->model->check_google_api();
+    }
 
-	/**
-	 * sportsmanagementViewjsmgcalendars::init()
-	 *
-	 * @return void
-	 */
-	public function init()
-	{
-		$this->model->check_google_api();
-	}
-
-	/**
-	 * sportsmanagementViewjsmgcalendars::addToolbar()
-	 *
-	 * @return void
-	 */
-	protected function addToolbar()
-	{
-		ToolbarHelper::addNew('jsmgcalendar.add', 'JTOOLBAR_NEW');
-		ToolbarHelper::custom('jsmgcalendarimport.import', 'upload.png', 'upload.png', 'COM_SPORTSMANAGEMENT_JSMGCALENDAR_VIEW_GCALENDARS_BUTTON_IMPORT', false);
-		$this->icon = 'google-calendar-48-icon.png';
-		parent::addToolbar();
-	}
-
+    protected function addToolbar()
+    {
+        ToolbarHelper::addNew('jsmgcalendar.add', 'JTOOLBAR_NEW');
+        ToolbarHelper::custom(
+            'jsmgcalendarimport.import',
+            'upload.png',
+            'upload.png',
+            'COM_SPORTSMANAGEMENT_JSMGCALENDAR_VIEW_GCALENDARS_BUTTON_IMPORT',
+            false
+        );
+        $this->icon = 'google-calendar-48-icon.png';
+        parent::addToolbar();
+    }
 }
