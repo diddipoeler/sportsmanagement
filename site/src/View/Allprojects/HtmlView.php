c7799ef4555dc25289f8ad4b5fd4c52c8bafd860
diddipoeler <diddipoeler@gmx.de>
2026-08-19 09:42:57 +0200

    Add native allprojects view

---
+++ site/src/View/Allprojects/HtmlView.php
@@ -0,0 +1,134 @@
+<?php
+namespace Diddipoeler\Component\SportsManagement\Site\View\Allprojects;
+
+\defined('_JEXEC') or die;
+
+use Diddipoeler\Component\SportsManagement\Site\Model\AllprojectsModel;
+use Diddipoeler\Component\SportsManagement\Site\View\SportsManagementHtmlView;
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Language\Text;
+
+final class HtmlView extends SportsManagementHtmlView
+{
+    public $state = null;
+    public array $items = [];
+    public $pagination = null;
+    public string $tableclass = 'table';
+    public string $template = 'ranking';
+    public int $use_jquery_modal = 2;
+    public string $filter = '';
+    public object $form;
+    public $user = null;
+    public string $sortDirection = 'ASC';
+    public string $sortColumn = 'v.name';
+    public array $lists = [];
+    public string $divclasscontainer = 'container-fluid';
+    public string $divclassrow = 'row-fluid';
+    public int $modalheight = 600;
+    public int $modalwidth = 900;
+
+    public function __construct($config = [])
+    {
+        $config['template_path'] = JPATH_SITE . '/components/com_sportsmanagement/views/allprojects/tmpl';
+        parent::__construct($config);
+        $this->loadPresentationDependencies();
+    }
+
+    public function display($tpl = null)
+    {
+        /** @var AllprojectsModel $model */
+        $model = $this->getModel();
+        if (!$model instanceof AllprojectsModel) {
+            throw new \RuntimeException('Allprojects view requires AllprojectsModel.', 500);
+        }
+
+        $tableClass = trim($this->input->getString('table_class', 'table'));
+        $this->tableclass = preg_replace('/[^A-Za-z0-9_\- ]/', '', $tableClass) ?: 'table';
+        $this->template = $this->input->getCmd('template', 'ranking') ?: 'ranking';
+        $this->use_jquery_modal = $this->input->getInt('use_jquery_modal', 2);
+        $this->modalheight = (int) $this->params->get('modal_popup_height', 600);
+        $this->modalwidth = (int) $this->params->get('modal_popup_width', 900);
+
+        $this->state = $model->getState();
+        $this->items = $model->getItems() ?: [];
+        $this->pagination = $model->getPagination();
+        $this->filter = (string) $this->state->get('filter.search', '');
+        $this->sortDirection = (string) $this->state->get('filter_order_Dir', 'ASC');
+        $this->sortColumn = (string) $this->state->get('filter_order', 'v.name');
+        $this->user = $this->app->getIdentity();
+        $this->lists = $this->buildFilterLists($model);
+
+        $this->form = (object) [
+            'limitField' => $this->pagination->getLimitBox(),
+        ];
+
+        $this->getDocument()->setTitle(Text::_('COM_SPORTSMANAGEMENT_ALLPROJECTS_PAGE_TITLE'));
+        parent::display($tpl);
+    }
+
+    private function buildFilterLists(AllprojectsModel $model): array
+    {
+        $lists = [];
+        $countryOptions = [
+            HTMLHelper::_('select.option', '0', Text::_('COM_SPORTSMANAGEMENT_GLOBAL_SELECT_COUNTRY')),
+        ];
+        if (class_exists('JSMCountries') && ($countries = \JSMCountries::getCountryOptions())) {
+            $countryOptions = array_merge($countryOptions, $countries);
+        }
+        $lists['nation2'] = HTMLHelper::_(
+            'select.genericlist',
+            $countryOptions,
+            'filter_search_nation',
+            'class="inputbox" style="width:140px;" onchange="this.form.submit();"',
+            'value',
+            'text',
+            $this->state->get('filter.search_nation')
+        );
+
+        $leagueOptions = [
+            HTMLHelper::_('select.option', '', Text::_('COM_SPORTSMANAGEMENT_GLOBAL_SELECT_LEAGUES'), 'id', 'name'),
+        ];
+        $leagueOptions = array_merge($leagueOptions, $model->getLeagueOptions());
+        $lists['leagues'] = HTMLHelper::_(
+            'select.genericlist',
+            $leagueOptions,
+            'filter_search_leagues',
+            'class="inputbox" style="width:140px;" onchange="this.form.submit();"',
+            'id',
+            'name',
+            $this->state->get('filter.search_leagues')
+        );
+
+        $seasonOptions = [
+            HTMLHelper::_('select.option', '', Text::_('COM_SPORTSMANAGEMENT_GLOBAL_SELECT_SEASONS'), 'id', 'name'),
+        ];
+        $seasonOptions = array_merge($seasonOptions, $model->getSeasonOptions());
+        $lists['seasons'] = HTMLHelper::_(
+            'select.genericlist',
+            $seasonOptions,
+            'filter_search_seasons',
+            'class="inputbox" style="width:140px;" onchange="this.form.submit();"',
+            'id',
+            'name',
+            $this->state->get('filter.search_seasons')
+        );
+
+        return $lists;
+    }
+
+    private function loadPresentationDependencies(): void
+    {
+        $classes = [
+            'sportsmanagementHelper' => JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/helpers/sportsmanagement.php',
+            'sportsmanagementHelperHtml' => JPATH_SITE . '/components/com_sportsmanagement/helpers/html.php',
+            'sportsmanagementHelperRoute' => JPATH_SITE . '/components/com_sportsmanagement/helpers/route.php',
+            'JSMCountries' => JPATH_SITE . '/components/com_sportsmanagement/helpers/countries.php',
+        ];
+
+        foreach ($classes as $class => $path) {
+            if (!class_exists($class, false)) {
+                \JLoader::register($class, $path);
+            }
+        }
+    }
+}
