00a28d603fbe3165029056399d564700e3961340
diddipoeler <diddipoeler@gmx.de>
2026-08-19 13:52:18 +0200

    Add native sportsmanagement form model

---
+++ admin/src/Model/SportsmanagementModel.php
@@ -0,0 +1,35 @@
+<?php
+namespace Diddipoeler\Component\SportsManagement\Administrator\Model;
+
+\defined('_JEXEC') or die;
+
+use Joomla\CMS\Factory;
+use Joomla\CMS\Table\Table;
+
+/**
+ * Native Joomla 5/6 administrator form model for the legacy SportsManagement sample record.
+ */
+final class SportsmanagementModel extends SportsManagementAdminModel
+{
+    public function getTable($type = 'sportsmanagement', $prefix = 'sportsmanagementTable', $config = [])
+    {
+        $config['dbo'] = $this->getDatabase();
+
+        return Table::getInstance($type, $prefix, $config);
+    }
+
+    public function getScript(): string
+    {
+        return 'administrator/components/com_sportsmanagement/models/forms/sportsmanagement.js';
+    }
+
+    protected function allowEdit($data = [], $key = 'id')
+    {
+        $id = (int) ($data[$key] ?? 0);
+
+        return Factory::getApplication()->getIdentity()->authorise(
+            'core.edit',
+            'com_sportsmanagement.message.' . $id
+        ) || parent::allowEdit($data, $key);
+    }
+}
