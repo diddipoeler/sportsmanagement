0d16871b3b7cc1e64d7b522487533f5378b474b2
diddipoeler <diddipoeler@gmx.de>
2026-08-19 12:29:07 +0200

    Add native position admin controller

---
+++ admin/src/Controller/PositionController.php
@@ -0,0 +1,9 @@
+<?php
+namespace Diddipoeler\Component\SportsManagement\Administrator\Controller;
+
+\defined('_JEXEC') or die;
+
+/** Native Joomla 5/6 form controller for a position. */
+final class PositionController extends SportsManagementFormController
+{
+}
