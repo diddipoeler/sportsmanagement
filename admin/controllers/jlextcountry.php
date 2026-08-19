6f1330ef09f8e94b7fd0e03b9524b3ec0f903a68
diddipoeler <diddipoeler@gmx.de>
2026-08-19 17:29:26 +0200

    Bridge legacy country controller

--- admin/controllers/jlextcountry.php
+++ admin/controllers/jlextcountry.php
@@ -1,31 +1,14 @@
 <?php
-/**
- *
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage controllers
- * @file       jlextcountry.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-
-
+/** Legacy compatibility bridge for the native administrator country controller. */
 defined('_JEXEC') or die('Restricted access');
 
-/**
- * sportsmanagementControllerjlextcountry
- *
- * @package
- * @author    diddi
- * @copyright 2014
- * @version   $Id$
- * @access    public
- */
-class sportsmanagementControllerjlextcountry extends JSMControllerForm
-{
+use Diddipoeler\Component\SportsManagement\Administrator\Controller\JlextcountryController;
 
+if (!class_exists(JlextcountryController::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Controller/SportsManagementFormController.php';
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Controller/JlextcountryController.php';
+}
 
+if (!class_exists('sportsmanagementControllerjlextcountry', false)) {
+    class_alias(JlextcountryController::class, 'sportsmanagementControllerjlextcountry');
 }
