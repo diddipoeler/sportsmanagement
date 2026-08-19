518f60d9d79c90545b3c33af92c005ac11b2b26f
diddipoeler <diddipoeler@gmx.de>
2026-08-19 17:46:23 +0200

    Add native Google calendar form controller

---
+++ admin/src/Controller/JsmgcalendarController.php
@@ -0,0 +1,13 @@
+<?php
+namespace Diddipoeler\Component\SportsManagement\Administrator\Controller;
+
+\defined('_JEXEC') or die;
+
+/** Native Joomla 5/6 form controller for Google calendars. */
+final class JsmgcalendarController extends SportsManagementFormController
+{
+    /** Retained for compatibility with the legacy task. */
+    public function insertgooglecalendar(): void
+    {
+    }
+}
