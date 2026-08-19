35a66b30b57e5233b9dfe41727b4935b780c1310
diddipoeler <diddipoeler@gmx.de>
2026-08-19 17:04:36 +0200

    Modernize federations administrator view

--- admin/views/jlextfederations/view.html.php
+++ admin/views/jlextfederations/view.html.php
@@ -1,82 +1,53 @@
 <?php
 /**
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage jlextfederations
- * @file       view.html.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * SportsManagement administrator federations list view.
  */
 defined('_JEXEC') or die('Restricted access');
+
 use Joomla\CMS\HTML\HTMLHelper;
-use Joomla\CMS\Table\Table;
 use Joomla\CMS\Language\Text;
+use Joomla\CMS\Table\Table;
 use Joomla\CMS\Toolbar\ToolbarHelper;
 
-/**
- * sportsmanagementViewjlextfederations
- *
- * @package
- * @author
- * @copyright diddi
- * @version   2014
- * @access    public
- */
 class sportsmanagementViewjlextfederations extends sportsmanagementView
 {
+    public function init()
+    {
+        $this->table = Table::getInstance('jlextfederation', 'sportsmanagementTable');
 
-	/**
-	 * sportsmanagementViewjlextfederations::init()
-	 *
-	 * @return void
-	 */
-	public function init()
-	{
+        $nation = [
+            HTMLHelper::_('select.option', '0', Text::_('COM_SPORTSMANAGEMENT_GLOBAL_SELECT_COUNTRY')),
+        ];
 
-		$this->table = Table::getInstance('jlextfederation', 'sportsmanagementTable');
+        if ($res = JSMCountries::getCountryOptions()) {
+            $nation = array_merge($nation, $res);
+            $this->search_nation = $res;
+        }
 
-		/** Build the html options for nation */
-		$nation[] = HTMLHelper::_('select.option', '0', Text::_('COM_SPORTSMANAGEMENT_GLOBAL_SELECT_COUNTRY'));
+        $this->lists = [
+            'nation' => $nation,
+            'nation2' => HTMLHelper::_(
+                'select.genericlist',
+                $nation,
+                'filter_search_nation',
+                'class="inputbox" style="width:140px;" onchange="this.form.submit();"',
+                'value',
+                'text',
+                $this->state->get('filter.search_nation')
+            ),
+        ];
+    }
 
-		if ($res = JSMCountries::getCountryOptions())
-		{
-			$nation              = array_merge($nation, $res);
-			$this->search_nation = $res;
-		}
+    protected function addToolbar()
+    {
+        $this->title = Text::_('COM_SPORTSMANAGEMENT_ADMIN_FEDERATIONS_TITLE');
+        $this->icon = 'federations';
 
-		$lists['nation']  = $nation;
-		$lists['nation2'] = JHtmlSelect::genericlist(
-			$nation,
-			'filter_search_nation',
-			'class="inputbox" style="width:140px; " onchange="this.form.submit();"',
-			'value',
-			'text',
-			$this->state->get('filter.search_nation')
-		);
+        ToolbarHelper::addNew('jlextfederation.add');
+        ToolbarHelper::editList('jlextfederation.edit');
+        ToolbarHelper::custom('jlextfederation.import', 'upload', 'upload', Text::_('JTOOLBAR_UPLOAD'), false);
+        ToolbarHelper::archiveList('jlextfederation.export', Text::_('JTOOLBAR_EXPORT'));
 
-		$this->lists = $lists;
-
-	}
-
-	/**
-	 * Add the page title and toolbar.
-	 *
-	 * @since 1.7
-	 */
-	protected function addToolbar()
-	{
-		$this->title = Text::_('COM_SPORTSMANAGEMENT_ADMIN_FEDERATIONS_TITLE');
-		$this->icon  = 'federations';
-
-		ToolbarHelper::addNew('jlextfederation.add');
-		ToolbarHelper::editList('jlextfederation.edit');
-		ToolbarHelper::custom('jlextfederation.import', 'upload', 'upload', Text::_('JTOOLBAR_UPLOAD'), false);
-		ToolbarHelper::archiveList('jlextfederation.export', Text::_('JTOOLBAR_EXPORT'));
-
-		parent::addToolbar();
-	}
-
-
+        parent::addToolbar();
+    }
 }
