d0026edacbba2330b95f96fa7a8fcf9a28989bf3
diddipoeler <diddipoeler@gmx.de>
2026-08-19 08:43:40 +0200

    Bridge legacy team stats ranking view to native MVC

--- site/views/statsrankingteams/view.html.php
+++ site/views/statsrankingteams/view.html.php
@@ -1,45 +1,14 @@
 <?php
-/**
- *
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage statsrankingteams
- * @file       view.html.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@arcor.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-defined('_JEXEC') or die('Restricted access');
+\defined('_JEXEC') or die;
 
-use Joomla\CMS\Uri\Uri;
+use Diddipoeler\Component\SportsManagement\Site\View\Statsrankingteams\HtmlView;
 
-/**
- * sportsmanagementViewStatsRankingTeams
- *
- * @package
- * @author    diddi
- * @copyright 2014
- * @version   $Id$
- * @access    public
- */
-class sportsmanagementViewStatsRankingTeams extends sportsmanagementView
-{
+if (!class_exists(HtmlView::class)) {
+    require_once JPATH_SITE . '/components/com_sportsmanagement/src/View/SportsManagementHtmlView.php';
+    require_once JPATH_SITE . '/components/com_sportsmanagement/src/View/SportsManagementProjectHtmlView.php';
+    require_once JPATH_SITE . '/components/com_sportsmanagement/src/View/Statsrankingteams/HtmlView.php';
+}
 
-	function init()
-	{
-		
-		$this->document->addScript(Uri::root(true) . '/components/' . $this->option . '/assets/js/smsportsmanagement.js');
-		sportsmanagementModelProject::setProjectID($this->jinput->getInt('p', 0), $this->cfg_which_database);
-
-		$teams              = sportsmanagementModelProject::getTeamsIndexedById(0, 'name', $this->cfg_which_database);
-		$this->teams        = $teams;
-		$this->stats        = $this->model->getProjectUniqueStats();
-		$this->playersstats = $this->model->getPlayersStats();
-		$this->teamsstats   = $this->model->getTeamsStats();
-		$this->teamstotal   = $this->model->getTeamsTotal($this->teamsstats);
-
-	}
-
+if (!class_exists('sportsmanagementViewStatsRankingTeams', false)) {
+    class_alias(HtmlView::class, 'sportsmanagementViewStatsRankingTeams');
 }
