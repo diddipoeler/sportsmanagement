b4ba9ea47d10935112fc3c23f9a00a8ae60dc8a9
diddipoeler <diddipoeler@gmx.de>
2026-08-19 22:20:22 +0200

    Harden GitHub commit list rendering

--- admin/views/github/tmpl/default.php
+++ admin/views/github/tmpl/default.php
@@ -1,56 +1,64 @@
-<?PHP
-/**
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage github
- * @file       default.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
+<?php
 defined('_JEXEC') or die('Restricted access');
+
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
+use Joomla\CMS\Router\Route;
 
-$templatesToLoad = array('footer', 'listheader');
+$templatesToLoad = ['footer', 'listheader'];
 sportsmanagementHelper::addTemplatePaths($templatesToLoad, $this);
-
-$attribs['width']  = '20px';
-$attribs['height'] = 'auto';
 ?>
+<div class="mb-3">
+    <a class="btn btn-primary" href="<?php echo Route::_('index.php?option=com_sportsmanagement&view=github&tmpl=component&layout=addissue'); ?>">
+        <?php echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_GITHUB_ADD_ISSUE'); ?>
+    </a>
+</div>
+
 <table class="table">
-	<?PHP
-	foreach ($this->commitlist as $key => $value)
-	{
-	   //$value->author->avatar_url = 'administrator/components/com_sportsmanagement/assets/images/user_edit.png';
-		?>
+    <tbody>
+    <?php foreach ((array) $this->commitlist as $commit) : ?>
+        <?php
+        $details = isset($commit->commit) && is_object($commit->commit) ? $commit->commit : null;
+        $author = $details && isset($details->author) && is_object($details->author) ? $details->author : null;
+        $account = isset($commit->author) && is_object($commit->author) ? $commit->author : null;
+        $date = $author ? (string) ($author->date ?? '') : '';
+        $name = $author ? (string) ($author->name ?? '') : '';
+        $message = $details ? (string) ($details->message ?? '') : '';
+        $htmlUrl = (string) ($commit->html_url ?? '');
+        $avatarUrl = $account ? (string) ($account->avatar_url ?? '') : '';
+        ?>
         <tr>
+            <td class="text-nowrap">
+                <?php echo $date !== '' ? HTMLHelper::_('date', $date, 'd.m.Y H:i:s') : ''; ?>
+            </td>
+            <td><?php echo htmlspecialchars($name, ENT_QUOTES, 'UTF-8'); ?></td>
             <td>
-				<?PHP
-				$new_date = substr($value->commit->author->date, 0, 10) . ' ' . substr($value->commit->author->date, 11, 8);
-				$timestamp = sportsmanagementHelper::getTimestamp($new_date);
-				echo date("d.m.Y H:i:s", $timestamp);
-				?>
+                <?php if (str_starts_with($htmlUrl, 'https://github.com/')) : ?>
+                    <a
+                        class="btn btn-sm btn-info"
+                        href="<?php echo htmlspecialchars($htmlUrl, ENT_QUOTES, 'UTF-8'); ?>"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <span class="octicon octicon-mark-github"></span>
+                        <?php echo nl2br(htmlspecialchars($message, ENT_QUOTES, 'UTF-8')); ?>
+                    </a>
+                <?php else : ?>
+                    <?php echo nl2br(htmlspecialchars($message, ENT_QUOTES, 'UTF-8')); ?>
+                <?php endif; ?>
             </td>
             <td>
-				<?PHP
-				echo $value->commit->author->name;
-				?>
-            </td>
-            <td>
-                <a class="btn btn-small btn-info" href="<?php echo $value->html_url; ?>" target="_blank">
-                    <span class="octicon octicon-mark-github"></span> <?php echo $value->commit->message; ?>
-                </a>
-            </td>
-            <td>
-			<?PHP
-			echo HTMLHelper::_('image',$value->author->avatar_url, $value->commit->author->name, $attribs, true, false);
-			?>
+                <?php if (str_starts_with($avatarUrl, 'https://')) : ?>
+                    <img
+                        src="<?php echo htmlspecialchars($avatarUrl, ENT_QUOTES, 'UTF-8'); ?>"
+                        alt="<?php echo htmlspecialchars($name, ENT_QUOTES, 'UTF-8'); ?>"
+                        width="20"
+                        height="20"
+                        loading="lazy"
+                    >
+                <?php endif; ?>
             </td>
         </tr>
-		<?PHP
-	}
-	?>
+    <?php endforeach; ?>
+    </tbody>
 </table>
-
