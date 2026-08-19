d7caa0439b5fd221ca09243c1656c7d6ed5e4c25
diddipoeler <diddipoeler@gmx.de>
2026-08-19 12:29:35 +0200

    Bridge legacy league admin controller

--- admin/controllers/league.php
+++ admin/controllers/league.php
@@ -1,26 +1,19 @@
 <?php
 /**
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage controllers
- * @file       league.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * SportsManagement legacy compatibility bridge.
+ *
+ * The active Joomla 5/6 implementation lives in admin/src/Controller/LeagueController.php.
  */
+
 defined('_JEXEC') or die('Restricted access');
 
-/**
- * sportsmanagementControllerleague
- *
- * @package
- * @author
- * @copyright diddi
- * @version   2014
- * @access    public
- */
-class sportsmanagementControllerleague extends JSMControllerForm
-{
+use Diddipoeler\Component\SportsManagement\Administrator\Controller\LeagueController;
 
+if (!class_exists(LeagueController::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Controller/SportsManagementFormController.php';
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Controller/LeagueController.php';
+}
+
+if (!class_exists('sportsmanagementControllerleague', false)) {
+    class_alias(LeagueController::class, 'sportsmanagementControllerleague');
 }
