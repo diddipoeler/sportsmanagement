12e60cfe8cfe101f21ec42e4b9d303b10db8350b
diddipoeler <diddipoeler@gmx.de>
2026-08-19 08:29:32 +0200

    Add native staff site view

---
+++ site/src/View/Staff/HtmlView.php
@@ -0,0 +1,65 @@
+<?php
+namespace Diddipoeler\Component\SportsManagement\Site\View\Staff;
+
+\defined('_JEXEC') or die;
+
+use Diddipoeler\Component\SportsManagement\Site\Model\PersonModel;
+use Diddipoeler\Component\SportsManagement\Site\Model\StaffModel;
+use Diddipoeler\Component\SportsManagement\Site\View\SportsManagementProjectHtmlView;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Uri\Uri;
+
+final class HtmlView extends SportsManagementProjectHtmlView
+{
+    public ?object $person = null;
+    public bool $showediticon = false;
+    public $inprojectinfo = null;
+    public array $history = [];
+    public array $stats = [];
+    public array $staffstats = [];
+    public array $historystats = [];
+    public string $title = '';
+    public $extended = null;
+
+    public function __construct($config = [])
+    {
+        $config['template_path'] = JPATH_SITE . '/components/com_sportsmanagement/views/staff/tmpl';
+        parent::__construct($config);
+    }
+
+    protected function prepareView(): void
+    {
+        /** @var StaffModel $model */
+        $model = $this->getModel();
+        if (!$model instanceof StaffModel) {
+            throw new \RuntimeException('Staff view requires StaffModel.', 500);
+        }
+
+        $this->person = PersonModel::getPerson(0, $model::$cfg_which_database);
+        $this->showediticon = PersonModel::getAllowed($this->config['edit_own_player'] ?? 0);
+        $this->inprojectinfo = $model->getTeamStaff();
+        $this->history = $model->getStaffHistory('DESC');
+        $this->stats = $model->getStats();
+        $this->staffstats = $model->getStaffStats();
+        $this->historystats = $model->getHistoryStaffStats();
+
+        if ($this->person) {
+            $name = \sportsmanagementHelper::formatName(
+                null,
+                (string) ($this->person->firstname ?? ''),
+                (string) ($this->person->nickname ?? ''),
+                (string) ($this->person->lastname ?? ''),
+                (string) ($this->config['name_format'] ?? '')
+            );
+            $this->title = Text::sprintf('COM_SPORTSMANAGEMENT_STAFF_ABOUT_AS_A_STAFF', $name);
+            $this->extended = \sportsmanagementHelper::getExtended($this->person->extended ?? '', 'staff');
+        } else {
+            $this->title = Text::_('COM_SPORTSMANAGEMENT_STAFF_ABOUT_AS_A_STAFF');
+        }
+
+        $this->headertitle = $this->title;
+        $document = $this->getDocument();
+        $document->setTitle($this->title);
+        $document->addStyleSheet(Uri::root(true) . '/components/com_sportsmanagement/assets/css/staff.css');
+    }
+}
