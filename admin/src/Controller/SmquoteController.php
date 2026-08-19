6944e5ec75d7df27d7e0c599e567f0ae23ad6011
diddipoeler <diddipoeler@gmx.de>
2026-08-19 16:22:25 +0200

    Add native quote form controller

---
+++ admin/src/Controller/SmquoteController.php
@@ -0,0 +1,11 @@
+<?php
+namespace Diddipoeler\Component\SportsManagement\Administrator\Controller;
+
+\defined('_JEXEC') or die;
+
+/**
+ * Native Joomla 5/6 form controller for SportsManagement quotes.
+ */
+final class SmquoteController extends SportsManagementFormController
+{
+}
