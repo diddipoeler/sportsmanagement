e72a44c8674259b8763a51597bacf42224c14210
diddipoeler <diddipoeler@gmx.de>
2026-08-19 16:39:44 +0200

    Bridge legacy project referee controller

--- admin/controllers/projectreferee.php
+++ admin/controllers/projectreferee.php
@@ -1,50 +1,14 @@
 <?php
-/**
- *
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage controllers
- * @file       projectreferee.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-
-
+/** Legacy compatibility bridge for the native project referee controller. */
 defined('_JEXEC') or die('Restricted access');
 
-use Joomla\CMS\Factory;
+use Diddipoeler\Component\SportsManagement\Administrator\Controller\ProjectrefereeController;
 
-/**
- * sportsmanagementControllerprojectreferee
- *
- * @package
- * @author
- * @copyright diddi
- * @version   2014
- * @access    public
- */
-class sportsmanagementControllerprojectreferee extends JSMControllerForm
-{
+if (!class_exists(ProjectrefereeController::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Controller/SportsManagementFormController.php';
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Controller/ProjectrefereeController.php';
+}
 
-	/**
-	 * Method to remove a projectreferee
-	 *
-	 * @access public
-	 * @return boolean    True on success
-	 * @since  0.1
-	 */
-	function remove()
-	{
-		$app   =& Factory::getApplication();
-		$pks   = Factory::getApplication()->input->getVar('cid', array(), 'post', 'array');
-		$model = $this->getModel('projectreferee');
-		$model->delete($pks);
-
-		$this->setRedirect('index.php?option=com_sportsmanagement&view=projectreferees');
-
-	}
-
+if (!class_exists('sportsmanagementControllerprojectreferee', false)) {
+    class_alias(ProjectrefereeController::class, 'sportsmanagementControllerprojectreferee');
 }
