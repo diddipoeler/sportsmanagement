8617503888c736941d7408de23eae2405ee5afae
diddipoeler <diddipoeler@gmx.de>
2026-08-19 19:32:21 +0200

    Use native prediction members view model

--- admin/views/predictionmembers/view.html.php
+++ admin/views/predictionmembers/view.html.php
@@ -1,183 +1,120 @@
 <?php
-/**
- *
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage predictionmembers
- * @file       view.html.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-
-
+/** SportsManagement administrator prediction members view. */
 defined('_JEXEC') or die('Restricted access');
 
 use Joomla\CMS\HTML\HTMLHelper;
-use Joomla\CMS\Table\Table;
 use Joomla\CMS\Language\Text;
-use Joomla\CMS\MVC\Model\BaseDatabaseModel;
 use Joomla\CMS\Toolbar\ToolbarHelper;
 
-/**
- * sportsmanagementViewPredictionMembers
- *
- * @package
- * @author
- * @copyright diddi
- * @version   2014
- * @access    public
- */
 class sportsmanagementViewPredictionMembers extends sportsmanagementView
 {
+    public function init()
+    {
+        $this->prediction_id = (int) $this->state->get('filter.prediction_id');
+        $layout = $this->getLayout();
 
-	/**
-	 * sportsmanagementViewPredictionMembers::init()
-	 *
-	 * @return
-	 */
-	public function init()
-	{
+        if (in_array($layout, ['default', 'default_3', 'default_4'], true)) {
+            $this->app->setUserState($this->option . '.prediction_id', $this->prediction_id);
+            $this->displayList();
 
-		$tpl = '';
+            return;
+        }
 
-		$this->prediction_id = $this->state->get('filter.prediction_id');
+        if (in_array($layout, ['editlist', 'editlist_3', 'editlist_4'], true)) {
+            $this->displayEditList();
+        }
+    }
 
-		switch ($this->getLayout())
-		{
-			case 'default':
-			case 'default_3':
-			case 'default_4':
-				$this->app->setUserState("$this->option.prediction_id", $this->state->get('filter.prediction_id'));
-				$this->_display($tpl);
+    private function displayList(): void
+    {
+        $predictions = [
+            HTMLHelper::_('select.option', '0', Text::_('COM_SPORTSMANAGEMENT_GLOBAL_SELECT_PRED_GAME'), 'value', 'text'),
+        ];
+        $games = $this->model->getPredictionGames();
 
-				return;
-				break;
-			case 'editlist':
-			case 'editlist_3':
-			case 'editlist_4':
-				$this->_editlist($tpl);
+        if ($games) {
+            $predictions = array_merge($predictions, $games);
+            $this->prediction_ids = $games;
+        } else {
+            $this->prediction_ids = [];
+        }
 
-				return;
-				break;
-		}
+        $this->lists = [
+            'predictions' => HTMLHelper::_(
+                'select.genericlist',
+                $predictions,
+                'filter_prediction_id',
+                'class="form-select" onchange="this.form.submit();"',
+                'value',
+                'text',
+                $this->prediction_id
+            ),
+        ];
+    }
 
-	}
+    private function displayEditList(): void
+    {
+        $this->prediction_id = (int) $this->app->getUserState($this->option . '.prediction_id', 0);
+        $this->prediction_name = $this->model->getPredictionProjectName($this->prediction_id);
+        $predictionMembers = $this->model->getPredictionMembers($this->prediction_id);
+        $joomlaMembers = $this->model->getJLUsers($this->prediction_id);
 
-	/**
-	 * sportsmanagementViewPredictionMembers::_display()
-	 *
-	 * @param   mixed  $tpl
-	 *
-	 * @return void
-	 */
-	function _display($tpl = null)
-	{
-		$this->table = Table::getInstance('predictionmember', 'sportsmanagementTable');
+        $this->lists = [
+            'prediction_members' => $predictionMembers
+                ? HTMLHelper::_(
+                    'select.genericlist',
+                    $predictionMembers,
+                    'prediction_members[]',
+                    'class="form-select" multiple size="15"',
+                    'value',
+                    'text'
+                )
+                : '<select name="prediction_members[]" id="prediction_members" class="form-select" multiple size="15"></select>',
+            'members' => $joomlaMembers
+                ? HTMLHelper::_(
+                    'select.genericlist',
+                    $joomlaMembers,
+                    'members[]',
+                    'class="form-select" multiple size="15"',
+                    'value',
+                    'text'
+                )
+                : '<select name="members[]" id="members" class="form-select" multiple size="15"></select>',
+        ];
 
-		// Build the html select list for prediction games
-		$mdlPredGames  = BaseDatabaseModel::getInstance('PredictionGames', 'sportsmanagementModel');
-		$predictions[] = HTMLHelper::_('select.option', '0', Text::_('COM_SPORTSMANAGEMENT_GLOBAL_SELECT_PRED_GAME'), 'value', 'text');
+        $this->setLayout('editlist');
+    }
 
-		if ($res = $mdlPredGames->getPredictionGames())
-		{
-			$predictions          = array_merge($predictions, $res);
-			$this->prediction_ids = $res;
-		}
+    protected function addToolbar()
+    {
+        $this->title = Text::_('COM_SPORTSMANAGEMENT_ADMIN_PMEMBERS_TITLE');
+        ToolbarHelper::custom(
+            'predictionmembers.reminder',
+            'mail',
+            'mail-2',
+            Text::_('COM_SPORTSMANAGEMENT_ADMIN_PMEMBERS_SEND_REMINDER'),
+            true
+        );
+        ToolbarHelper::divider();
 
-		$lists['predictions'] = HTMLHelper::_(
-			'select.genericlist',
-			$predictions,
-			'filter_prediction_id',
-			'class="inputbox" onChange="this.form.submit();" ',
-			'value',
-			'text',
-			$this->state->get('filter.prediction_id')
-		);
-		unset($res);
+        if ($this->prediction_id) {
+            sportsmanagementHelper::ToolbarButton(
+                'editlist',
+                'new',
+                Text::_('COM_SPORTSMANAGEMENT_ADMIN_PMEMBERS_BUTTON_ASSIGN')
+            );
+            ToolbarHelper::publishList(
+                'predictionmembers.publish',
+                Text::_('COM_SPORTSMANAGEMENT_ADMIN_PMEMBERS_APPROVE')
+            );
+            ToolbarHelper::unpublishList(
+                'predictionmembers.unpublish',
+                Text::_('COM_SPORTSMANAGEMENT_ADMIN_PMEMBERS_REJECT')
+            );
+            ToolbarHelper::deleteList('', 'predictionmembers.remove');
+        }
 
-		$this->lists = $lists;
-
-	}
-
-	/**
-	 * sportsmanagementViewPredictionMembers::_editlist()
-	 *
-	 * @param   mixed  $tpl
-	 *
-	 * @return void
-	 */
-	function _editlist($tpl = null)
-	{
-		$this->prediction_id = $this->app->getUserState($this->option . '.prediction_id');
-
-		$prediction_name       = $this->getModel()->getPredictionProjectName($this->prediction_id);
-		$this->prediction_name = $prediction_name;
-
-		$res_prediction_members = $this->getModel()->getPredictionMembers($this->prediction_id);
-
-		if ($res_prediction_members)
-		{
-			$lists['prediction_members'] = HTMLHelper::_(
-				'select.genericlist',
-				$res_prediction_members,
-				'prediction_members[]',
-				'class="inputbox" multiple="true" onchange="" size="15"',
-				'value',
-				'text'
-			);
-		}
-		else
-		{
-			$lists['prediction_members'] = '<select name="prediction_members[]" id="prediction_members" style="" class="inputbox" multiple="true" size="15"></select>';
-		}
-
-		$res_joomla_members = $this->getModel()->getJLUsers($this->prediction_id);
-
-		if ($res_joomla_members)
-		{
-			$lists['members'] = HTMLHelper::_(
-				'select.genericlist',
-				$res_joomla_members,
-				'members[]',
-				'class="inputbox" multiple="true" onchange="" size="15"',
-				'value',
-				'text'
-			);
-		}
-
-		$this->lists = $lists;
-		$this->setlayout('editlist');
-
-	}
-
-	/**
-	 * Add the page title and toolbar.
-	 *
-	 * @since 1.7
-	 */
-	protected function addToolbar()
-	{
-
-		$this->title = Text::_('COM_SPORTSMANAGEMENT_ADMIN_PMEMBERS_TITLE');
-
-		ToolbarHelper::custom('predictionmembers.reminder', 'mail', 'mail-2', Text::_('COM_SPORTSMANAGEMENT_ADMIN_PMEMBERS_SEND_REMINDER'), true);
-		ToolbarHelper::divider();
-
-		if ($this->prediction_id)
-		{
-			sportsmanagementHelper::ToolbarButton('editlist', 'new', Text::_('COM_SPORTSMANAGEMENT_ADMIN_PMEMBERS_BUTTON_ASSIGN'));
-			ToolbarHelper::publishList('predictionmembers.publish', Text::_('COM_SPORTSMANAGEMENT_ADMIN_PMEMBERS_APPROVE'));
-			ToolbarHelper::unpublishList('predictionmembers.unpublish', Text::_('COM_SPORTSMANAGEMENT_ADMIN_PMEMBERS_REJECT'));
-			ToolbarHelper::deleteList('', 'predictionmembers.remove');
-		}
-
-		ToolbarHelper::checkin('predictionmembers.checkin');
-
-		parent::addToolbar();
-
-	}
-
+        ToolbarHelper::checkin('predictionmembers.checkin');
+        parent::addToolbar();
+    }
 }
