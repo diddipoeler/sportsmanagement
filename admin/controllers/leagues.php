4f6802d3b65f740bc22f9a21b383fedfe9f33e6b
diddipoeler <diddipoeler@gmx.de>
2026-08-19 12:52:13 +0200

    Bridge legacy leagues admin controller

--- admin/controllers/leagues.php
+++ admin/controllers/leagues.php
@@ -1,56 +1,14 @@
 <?php
-/**
- *
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage controllers
- * @file       leagues.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-
+/** Legacy compatibility bridge for the native administrator Leagues controller. */
 defined('_JEXEC') or die('Restricted access');
 
-use Joomla\CMS\Router\Route;
+use Diddipoeler\Component\SportsManagement\Administrator\Controller\LeaguesController;
 
-/**
- * sportsmanagementControllerleagues
- *
- * @package
- * @author
- * @copyright diddi
- * @version   2014
- * @access    public
- */
-class sportsmanagementControllerleagues extends JSMControllerAdmin
-{
+if (!class_exists(LeaguesController::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Controller/SportsManagementAdminController.php';
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Controller/LeaguesController.php';
+}
 
-	/**
-	 * Method to update checked positions
-	 *
-	 * @access public
-	 * @return boolean    True on success
-	 */
-	function saveshort()
-	{
-		$model = $this->getModel();
-		$model->saveshort();
-		$this->setRedirect(Route::_('index.php?option=' . $this->option . '&view=' . $this->view_list, false));
-	}
-
-	/**
-	 * Proxy for getModel.
-	 *
-	 * @since 1.6
-	 */
-	public function getModel($name = 'League', $prefix = 'sportsmanagementModel', $config = Array())
-	{
-		$model = parent::getModel($name, $prefix, array('ignore_request' => true));
-
-		return $model;
-	}
-
+if (!class_exists('sportsmanagementControllerleagues', false)) {
+    class_alias(LeaguesController::class, 'sportsmanagementControllerleagues');
 }
