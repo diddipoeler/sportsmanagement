be3e17c0685291d0d6839fdfbd817a5fa255279a
diddipoeler <diddipoeler@gmx.de>
2026-08-19 08:55:12 +0200

    Bridge legacy events ranking view to native MVC

--- site/views/eventsranking/view.html.php
+++ site/views/eventsranking/view.html.php
@@ -1,113 +1,14 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage eventsranking
- * @file       view.html.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\Language\Text;
-use Joomla\CMS\Uri\Uri;
+\defined('_JEXEC') or die;
 
-/**
- * sportsmanagementViewEventsRanking
- *
- * @package
- * @author    diddi
- * @copyright 2015
- * @version   $Id$
- * @access    public
- */
-class sportsmanagementViewEventsRanking extends sportsmanagementView
-{
+use Diddipoeler\Component\SportsManagement\Site\View\Eventsranking\HtmlView;
 
-	/**
-	 * sportsmanagementViewEventsRanking::init()
-	 *
-	 * @return void
-	 */
-	function init()
-	{
+if (!class_exists(HtmlView::class)) {
+    require_once JPATH_SITE . '/components/com_sportsmanagement/src/View/SportsManagementHtmlView.php';
+    require_once JPATH_SITE . '/components/com_sportsmanagement/src/View/SportsManagementProjectHtmlView.php';
+    require_once JPATH_SITE . '/components/com_sportsmanagement/src/View/Eventsranking/HtmlView.php';
+}
 
-		$this->document->addScript(Uri::root(true) . '/components/' . $this->option . '/assets/js/smsportsmanagement.js');
-
-		sportsmanagementModelProject::setProjectID($this->jinput->getInt('p', 0), $this->jinput->getInt('cfg_which_database', 0));
-
-		$this->division   = sportsmanagementModelProject::getDivision(0, $this->jinput->getInt('cfg_which_database', 0));
-		$this->matchid    = sportsmanagementModelEventsRanking::$matchid;
-		$this->teamid     = $this->model->getTeamId();
-		$this->teams      = sportsmanagementModelProject::getTeamsIndexedById(0, 'name', $this->jinput->getInt('cfg_which_database', 0));
-		$this->favteams   = sportsmanagementModelProject::getFavTeams($this->jinput->getInt('cfg_which_database', 0));
-		$this->eventtypes = sportsmanagementModelProject::getEventTypes(sportsmanagementModelEventsRanking::$eventid, $this->jinput->getInt('cfg_which_database', 0), $this->project->sports_type_id,
-									       $this->jinput->getInt('p', 0),
-									       $this->jinput->getInt('tid', 0),
-									       $this->jinput->getInt('s', 0),
-									       $this->jinput->getInt('mid', 0));
-		$this->limit      = $this->model->getLimit();
-		$this->limitstart = $this->model->getLimitStart();
-		$this->pagination = $this->get('Pagination');
-        
-        switch ( $this->project->sport_type_name )
-        {
-        case 'COM_SPORTSMANAGEMENT_ST_DART':
-        $this->eventranking = $this->model->getEventRankings($this->limit, $this->limitstart, null, true,$this->project->sports_type_id);
-        break;
-        default:
-        $this->eventranking = $this->model->getEventRankings($this->limit, $this->limitstart, null, false,$this->project->sports_type_id);
-        break;    
-        }
-
-		$this->multiple_events = count($this->eventtypes) > 1;
-
-		$prefix = Text::_('COM_SPORTSMANAGEMENT_EVENTSRANKING_PAGE_TITLE');
-
-		if ($this->multiple_events)
-		{
-			$prefix .= " - " . Text::_('COM_SPORTSMANAGEMENT_EVENTSRANKING_TITLE');
-		}
-		else
-		{
-			/** Next query will result in an array with exactly 1 statistic id */
-			$evid = array_keys($this->eventtypes);
-
-			/** Selected one valid eventtype, so show its name */
-			$prefix .= " - " . Text::_($this->eventtypes[$evid[0]]->name);
-		}
-
-		/** Set page title */
-		$titleInfo = sportsmanagementHelper::createTitleInfo($prefix);
-
-		if (!empty($this->teamid) && array_key_exists($this->teamid, $this->teams))
-		{
-			$titleInfo->team1Name = $this->teams[$this->teamid]->name;
-		}
-
-		if (!empty($this->project))
-		{
-			$titleInfo->projectName = $this->project->name;
-			$titleInfo->leagueName  = $this->project->league_name;
-			$titleInfo->seasonName  = $this->project->season_name;
-		}
-
-		if (!empty($this->division) && $this->division->id != 0)
-		{
-			$titleInfo->divisionName = $this->division->name;
-		}
-
-		$this->pagetitle = sportsmanagementHelper::formatTitle($titleInfo, $this->config["page_title_format"]);
-		$this->document->setTitle($this->pagetitle);
-
-		$this->headertitle = $this->pagetitle;
-
-		if (!isset($this->config['table_class']))
-		{
-			$this->config['table_class'] = 'table';
-		}
-
-	}
-
+if (!class_exists('sportsmanagementViewEventsRanking', false)) {
+    class_alias(HtmlView::class, 'sportsmanagementViewEventsRanking');
 }
