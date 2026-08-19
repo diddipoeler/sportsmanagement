036f368ee7daa2e5dd29dd24a6271b1254fb2a1e
diddipoeler <diddipoeler@gmx.de>
2026-08-19 12:42:17 +0200

    Bridge legacy positions admin controller

--- admin/controllers/positions.php
+++ admin/controllers/positions.php
@@ -1,55 +1,14 @@
 <?php
-/**
- *
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage controllers
- * @file       positions.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-
+/** Legacy compatibility bridge for the native administrator Positions controller. */
 defined('_JEXEC') or die('Restricted access');
 
-use Joomla\CMS\Router\Route;
+use Diddipoeler\Component\SportsManagement\Administrator\Controller\PositionsController;
 
-/**
- * sportsmanagementControllerPositions
- *
- * @package
- * @author
- * @copyright diddi
- * @version   2014
- * @access    public
- */
-class sportsmanagementControllerPositions extends JSMControllerAdmin
-{
+if (!class_exists(PositionsController::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Controller/SportsManagementAdminController.php';
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Controller/PositionsController.php';
+}
 
-	/**
-	 * Method to update checked positions
-	 *
-	 * @access public
-	 * @return boolean    True on success
-	 */
-	function saveshort()
-	{
-		$model = $this->getModel();
-		$model->saveshort();
-		$this->setRedirect(Route::_('index.php?option=' . $this->option . '&view=' . $this->view_list, false));
-	}
-
-	/**
-	 * Proxy for getModel.
-	 *
-	 * @since 1.6
-	 */
-	public function getModel($name = 'Position', $prefix = 'sportsmanagementModel', $config = Array())
-	{
-		$model = parent::getModel($name, $prefix, array('ignore_request' => true));
-
-		return $model;
-	}
+if (!class_exists('sportsmanagementControllerPositions', false)) {
+    class_alias(PositionsController::class, 'sportsmanagementControllerPositions');
 }
