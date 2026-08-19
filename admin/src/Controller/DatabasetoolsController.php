26f7488d4582f574972e28206af08c6de094c865
diddipoeler <diddipoeler@gmx.de>
2026-08-19 13:32:35 +0200

    Add native database tools controller

---
+++ admin/src/Controller/DatabasetoolsController.php
@@ -0,0 +1,27 @@
+<?php
+namespace Diddipoeler\Component\SportsManagement\Administrator\Controller;
+
+\defined('_JEXEC') or die;
+
+use Joomla\CMS\MVC\Controller\BaseController;
+
+/**
+ * Native Joomla 5/6 controller for the database-tools screen.
+ *
+ * The list screen itself uses DatabasetoolsModel; explicit maintenance tasks
+ * continue to resolve the large Databasetool action model until that model is
+ * migrated separately.
+ */
+final class DatabasetoolsController extends BaseController
+{
+    /**
+     * Preserve the historical default action-model routing.
+     */
+    public function getModel($name = 'databasetool', $prefix = 'sportsmanagementModel', $config = [])
+    {
+        $config = is_array($config) ? $config : [];
+        $config['ignore_request'] = true;
+
+        return parent::getModel($name, $prefix, $config);
+    }
+}
