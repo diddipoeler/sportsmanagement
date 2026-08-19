81f290d70fce4f7cbb6d89b0926e9cf4ae905d12
diddipoeler <diddipoeler@gmx.de>
2026-08-19 19:52:22 +0200

    Modernize tree match assignment layout

--- admin/views/treetomatchs/tmpl/editlist_data.php
+++ admin/views/treetomatchs/tmpl/editlist_data.php
@@ -1,57 +1,39 @@
 <?php
-/**
- *
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage treetomatchs
- * @file       editlist_data.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-
+/** Tournament-tree match assignment controls for Joomla 5/6. */
 defined('_JEXEC') or die('Restricted access');
 
 use Joomla\CMS\Language\Text;
-
 ?>
 <legend>
-	<?php
-	echo Text::sprintf('COM_SPORTSMANAGEMENT_ADMIN_TREETOMATCH_ASSIGN_TITLE', '<i>' . $this->projectws->name . '</i>');
-	?>
+    <?php echo Text::sprintf(
+        'COM_SPORTSMANAGEMENT_ADMIN_TREETOMATCH_ASSIGN_TITLE',
+        '<i>' . htmlspecialchars((string) $this->projectws->name) . '</i>'
+    ); ?>
 </legend>
-<table class="table" border="0">
-    <tr>
-        <td>
-            <b>
-				<?php
-				echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_TREETOMATCH_ASSIGN_AVAIL_MATCHES');
-				?>
-            </b><br/>
-			<?php
-			echo $this->lists['matches'];
-			?>
-        </td>
-        <td style="">
-            <input id="moveright" type="button" value="<?php echo Text::_('&gt;&gt;'); ?>"
-                   onclick="move_list_items('matcheslist','node_matcheslist');"/>
-            <input id="moveleft" type="button" value="<?php echo Text::_('&lt;&lt;'); ?>"
-                   onclick="move_list_items('node_matcheslist','matcheslist');"/>
-        </td>
-        <td>
-            <b>
-				<?php
-				echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_TREETOMATCH_ASSIGN_NODE_MATCHES');
-				?>
-            </b><br/>
-			<?php
-			echo $this->lists['node_matches'];
-			?>
-        </td>
-    </tr>
-</table>
 
+<div class="row g-3 align-items-center">
+    <div class="col-md-5">
+        <label class="form-label fw-bold" for="matcheslist">
+            <?php echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_TREETOMATCH_ASSIGN_AVAIL_MATCHES'); ?>
+        </label>
+        <?php echo $this->lists['matches']; ?>
+    </div>
 
-	  
+    <div class="col-md-2 d-flex flex-column justify-content-center align-items-center gap-2">
+        <button type="button" class="btn btn-outline-secondary"
+                onclick="move_list_items('matcheslist','node_matcheslist');">
+            &gt;&gt;
+        </button>
+        <button type="button" class="btn btn-outline-secondary"
+                onclick="move_list_items('node_matcheslist','matcheslist');">
+            &lt;&lt;
+        </button>
+    </div>
+
+    <div class="col-md-5">
+        <label class="form-label fw-bold" for="node_matcheslist">
+            <?php echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_TREETOMATCH_ASSIGN_NODE_MATCHES'); ?>
+        </label>
+        <?php echo $this->lists['node_matches']; ?>
+    </div>
+</div>
