9515da212a4f1d1cee7fb09ad8f1c326ed98a311
diddipoeler <diddipoeler@gmx.de>
2026-08-19 17:04:19 +0200

    Modernize federation administrator view

--- admin/views/jlextfederation/view.html.php
+++ admin/views/jlextfederation/view.html.php
@@ -1,75 +1,37 @@
 <?php
 /**
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage jlextfederation
- * @file       view.html.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * SportsManagement administrator federation edit view.
  */
 defined('_JEXEC') or die('Restricted access');
 
-/**
- * sportsmanagementViewJlextfederation
- *
- * @package
- * @author
- * @copyright diddi
- * @version   2014
- * @access    public
- */
 class sportsmanagementViewJlextfederation extends sportsmanagementView
 {
+    public function init()
+    {
+        if ($this->item->id) {
+            if ($this->item->founded === '0000-00-00') {
+                $this->item->founded = '';
+                $this->form->setValue('founded', null, '');
+            }
 
-
-	/**
-	 * sportsmanagementViewJlextfederation::init()
-	 *
-	 * @return
-	 */
-	public function init()
-	{
-	   
-       if ($this->item->id)
-		{
-			/** Alles ok */
-			if ($this->item->founded == '0000-00-00')
-			{
-				$this->item->founded = '';
-				$this->form->setValue('founded',null, '');
-			}
-
-			if ($this->item->dissolved == '0000-00-00')
-			{
-				$this->item->dissolved = '';
-				$this->form->setValue('dissolved',null, '');
-			}
-		}
-		else
-		{
-			$this->form->setValue('founded',null, '');
-			$this->form->setValue('dissolved',null, '');
-		}
-        if ( !$this->item->founded_year )
-        {
-            $this->item->founded_year = 'kein';
-            $this->form->setValue('founded_year','', 'kein');
+            if ($this->item->dissolved === '0000-00-00') {
+                $this->item->dissolved = '';
+                $this->form->setValue('dissolved', null, '');
+            }
+        } else {
+            $this->form->setValue('founded', null, '');
+            $this->form->setValue('dissolved', null, '');
         }
 
-	}
+        if (!$this->item->founded_year) {
+            $this->item->founded_year = 'kein';
+            $this->form->setValue('founded_year', '', 'kein');
+        }
+    }
 
-
-	/**
-	 * sportsmanagementViewJlextfederation::addToolBar()
-	 *
-	 * @return void
-	 */
-	protected function addToolBar()
-	{
-		$this->jinput->set('hidemainmenu', true);
-		parent::addToolbar();
-	}
-
+    protected function addToolbar()
+    {
+        $this->app->getInput()->set('hidemainmenu', true);
+        parent::addToolbar();
+    }
 }
