b254abc696deb1d8c51053e512b16895a7f72425
diddipoeler <diddipoeler@gmx.de>
2026-08-19 12:54:16 +0200

    Add native administrator databasetools state model

---
+++ admin/src/Model/DatabasetoolsModel.php
@@ -0,0 +1,24 @@
+<?php
+namespace Diddipoeler\Component\SportsManagement\Administrator\Model;
+
+\defined('_JEXEC') or die;
+
+use Joomla\CMS\Factory;
+
+/**
+ * Native Joomla 5/6 state model for the database-tools list screen.
+ *
+ * The large Databasetool action model is intentionally migrated separately.
+ */
+final class DatabasetoolsModel extends SportsManagementListModel
+{
+    protected function populateState($ordering = null, $direction = null)
+    {
+        parent::populateState($ordering, $direction);
+
+        $this->setState(
+            'list.start',
+            max(0, Factory::getApplication()->getInput()->getUInt('limitstart', 0))
+        );
+    }
+}
