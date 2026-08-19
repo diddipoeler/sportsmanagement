abac614e7d064b0bd131d0fc57e8d054b70a917a
diddipoeler <diddipoeler@gmx.de>
2026-08-19 15:21:15 +0200

    Bridge legacy prediction group form model

--- admin/models/predictiongroup.php
+++ admin/models/predictiongroup.php
@@ -1,64 +1,14 @@
 <?php
-/**
- *
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage models
- * @file       predictiongroup.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-
+/** Legacy compatibility bridge for the native administrator Predictiongroup form model. */
 defined('_JEXEC') or die('Restricted access');
 
-use Joomla\CMS\Factory;
-use Joomla\CMS\Language\Text;
+use Diddipoeler\Component\SportsManagement\Administrator\Model\PredictiongroupModel;
 
-/**
- * sportsmanagementModelpredictiongroup
- *
- * @package
- * @author    diddi
- * @copyright 2014
- * @version   $Id$
- * @access    public
- */
-class sportsmanagementModelpredictiongroup extends JSMModelAdmin
-{
-	/**
-	 * Method to save the form data.
-	 *
-	 * @param array $data The form data.
-	 *
-	 * @return boolean True on success.
-	 * @since 1.6
-	 */
-	public function save($data)
-	{
-		$app  = Factory::getApplication();
-		$date = Factory::getDate();
-		$user = $app->getIdentity();
+if (!class_exists(PredictiongroupModel::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Model/SportsManagementAdminModel.php';
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Model/PredictiongroupModel.php';
+}
 
-		$data['modified']    = $date->toSql();
-		$data['modified_by'] = (int) $user->id;
-
-		if (!parent::save($data))
-		{
-			return false;
-		}
-
-		$id         = (int) $this->getState($this->getName() . '.id');
-		$isNew      = $this->getState($this->getName() . '.new');
-		$data['id'] = $id;
-
-		if ($isNew)
-		{
-			$app->enqueueMessage(Text::plural(strtoupper($this->jsmoption) . '_N_ITEMS_CREATED', $id), '');
-		}
-
-		return true;
-	}
+if (!class_exists('sportsmanagementModelpredictiongroup', false)) {
+    class_alias(PredictiongroupModel::class, 'sportsmanagementModelpredictiongroup');
 }
