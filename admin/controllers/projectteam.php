5c0b86915b5464ad4a812dc886101bab5e77a7d7
diddipoeler <diddipoeler@gmx.de>
2026-08-19 22:50:54 +0200

    Bridge legacy project-team form controller

--- admin/controllers/projectteam.php
+++ admin/controllers/projectteam.php
@@ -1,40 +1,14 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage controllers
- * @file       projectteam.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
+/** Legacy compatibility bridge for the native administrator Projectteam controller. */
 defined('_JEXEC') or die('Restricted access');
 
-/**
- * sportsmanagementControllerprojectteam
- *
- * @package
- * @author
- * @copyright diddi
- * @version   2014
- * @access    public
- */
-class sportsmanagementControllerprojectteam extends JSMControllerForm
-{
+use Diddipoeler\Component\SportsManagement\Administrator\Controller\ProjectteamController;
 
-	/**
-	 * sportsmanagementControllerprojectteam::storechangeteams()
-	 *
-	 * @return void
-	 */
-	function storechangeteams()
-	{
-		$model = $this->getModel('projectteams');
-		$model->setNewTeamID();
-		$msg = '';
-		$this->setRedirect('index.php?option=com_sportsmanagement&view=close&tmpl=component', $msg);
-	}
+if (!class_exists(ProjectteamController::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Controller/SportsManagementFormController.php';
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Controller/ProjectteamController.php';
+}
 
+if (!class_exists('sportsmanagementControllerprojectteam', false)) {
+    class_alias(ProjectteamController::class, 'sportsmanagementControllerprojectteam');
 }
