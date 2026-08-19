facaad159efd2d4acf336f156bea5411d73782bc
diddipoeler <diddipoeler@gmx.de>
2026-08-19 22:19:52 +0200

    Secure GitHub issue form

--- admin/views/github/tmpl/add_issue.php
+++ admin/views/github/tmpl/add_issue.php
@@ -1,129 +1,66 @@
 <?php
-/**
- *
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage github
- * @file       add_issue.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-
 defined('_JEXEC') or die('Restricted access');
 
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
-use Joomla\CMS\Factory;
 use Joomla\CMS\Router\Route;
-
-// Welche joomla version ?
-if (version_compare(JVERSION, '3.0.0', 'ge'))
-{
-	HTMLHelper::_('jquery.framework');
-}
-
-
-HTMLHelper::_('behavior.keepalive');
-
-Factory::getDocument()->addScriptDeclaration(
-	'
-	Joomla.submitbutton = function(task)
-	{
-		if (task == "github.cancel" )
-		{
-			' . '
-
-			if (window.opener && task == "github.cancel" )
-			{
-				window.opener.document.closeEditWindow = self;
-				window.opener.setTimeout("window.document.closeEditWindow.close()", 1000);
-			}
-
-			Joomla.submitform(task, document.getElementById("addissue-form"));
-		}
-	};
-'
-);
-
 ?>
+<div class="container-popup">
+    <form action="<?php echo Route::_('index.php?option=com_sportsmanagement'); ?>" method="post" id="addissue-form" name="adminForm">
+        <fieldset class="options-form">
+            <legend><?php echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_GITHUB_ADD_ISSUE'); ?></legend>
 
-    <div class="container-popup">
-
-        <div class="pull-right">
-            <button class="btn" type="button"
-                    onclick="Joomla.submitbutton('github.cancel', this.form);"><?php echo Text::_('JCANCEL') ?></button>
-        </div>
-
-        <div class="clearfix"></div>
-
-        <fieldset class='adminform'>
-            <legend><?php echo Text::sprintf('COM_SPORTSMANAGEMENT_ADMIN_GITHUB_ADD_ISSUE', '<i>' . '</i>'); ?></legend>
-            <form action="<?php echo Route::_('index.php?option=com_sportsmanagement'); ?>" id='addissue-form'
-                  method='post' style='display:inline' name='adminform'>
-
-                <fieldset>
-                    <div class="fltrt">
-                        <button type="button" onclick="Joomla.submitform('github.addissue', this.form)">
-							<?php echo Text::_('JSAVE'); ?></button>
-
+            <?php if (!$this->hasConfiguredToken) : ?>
+                <div class="control-group">
+                    <label class="control-label" for="gh_token"><?php echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_GITHUB_TOKEN'); ?></label>
+                    <div class="controls">
+                        <input type="password" name="gh_token" id="gh_token" value="" class="form-control" autocomplete="off" required>
                     </div>
+                </div>
+            <?php endif; ?>
 
+            <div class="control-group">
+                <label class="control-label" for="labels"><?php echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_GITHUB_LABELS'); ?></label>
+                <div class="controls"><?php echo $this->lists['labels']; ?></div>
+            </div>
 
-                </fieldset>
+            <div class="control-group">
+                <label class="control-label" for="milestones"><?php echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_GITHUB_MILESTONE'); ?></label>
+                <div class="controls"><?php echo $this->lists['milestones']; ?></div>
+            </div>
 
+            <div class="control-group">
+                <label class="control-label" for="title"><?php echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_GITHUB_NI_TITLE'); ?></label>
+                <div class="controls">
+                    <input
+                        type="text"
+                        name="title"
+                        id="title"
+                        value="<?php echo htmlspecialchars((string) $this->issuetitle, ENT_QUOTES, 'UTF-8'); ?>"
+                        class="form-control"
+                        maxlength="256"
+                        required
+                    >
+                </div>
+            </div>
 
-				<?php echo HTMLHelper::_('form.token') . "\n"; ?>
-                <table class='table'>
-                    <tbody>
-                    <tr>
+            <div class="control-group">
+                <label class="control-label" for="message"><?php echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_GITHUB_NI_MESSAGE'); ?></label>
+                <div class="controls">
+                    <textarea id="message" name="message" rows="10" class="form-control" required></textarea>
+                </div>
+            </div>
 
-                        <td>
-							<?PHP
-							echo $this->lists['labels'];
-							?>
-                        </td>
-                        <td>
-							<?PHP
-							echo $this->lists['milestones'];
-							?>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td colspan="2">
-							<?PHP
-							echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_GITHUB_NI_TITLE');
-							?>
-                            <input type="text" name="title" id="title" value="<?php echo $this->issuetitle; ?>"
-                                   class="form-control form-control-inline" size="100" maxlength="100" required="">
-                        </td>
-                    </tr>
-                    <tr>
-                        <td colspan="2">
-							<?PHP
-							echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_GITHUB_NI_MESSAGE');
-							?>
-                            <div role="application" aria-labelledby="jform_projectinfo_voice"
-                                 id="jform_projectinfo_parent" class="mceEditor defaultSkin">
-                                <textarea id="message" name="message" cols="" rows="" style="width:80%;height:200px;"
-                                          class="wfEditor mce_editable source" wrap="off"></textarea>
-                            </div>
-                        </td>
-                    </tr>
-                    </tbody>
-                </table>
-                <input type="hidden" name="component" value="<?PHP echo $this->option; ?>"/>
-                <input type='hidden' name='task' value=''/>
-                <input type="hidden" name="close" id="close" value="0"/>
-                <input type="hidden" name="gh.token" value="<?PHP echo $this->gh_token; ?>"/>
-                <input type="hidden" name="api.username" value="<?PHP echo $this->api_username; ?>"/>
-                <input type="hidden" name="api.password" value="<?PHP echo $this->api_password; ?>"/>
-
-            </form>
+            <div class="btn-toolbar mt-3">
+                <button type="submit" class="btn btn-primary" name="task" value="github.addissue">
+                    <?php echo Text::_('JSAVE'); ?>
+                </button>
+                <button type="submit" class="btn btn-secondary" name="task" value="github.cancel" formnovalidate>
+                    <?php echo Text::_('JCANCEL'); ?>
+                </button>
+            </div>
         </fieldset>
-    </div>
 
-<?PHP
-
+        <?php echo HTMLHelper::_('form.token'); ?>
+    </form>
+</div>
