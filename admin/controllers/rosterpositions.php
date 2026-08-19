71ded09d216ab2b24d3965387b0e9d165d9360b5
diddipoeler <diddipoeler@gmx.de>
2026-08-19 12:42:29 +0200

    Bridge legacy rosterpositions admin controller

--- admin/controllers/rosterpositions.php
+++ admin/controllers/rosterpositions.php
@@ -1,63 +1,14 @@
 <?php
-/**
- *
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage controllers
- * @file       rosterpositions.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-
+/** Legacy compatibility bridge for the native administrator Rosterpositions controller. */
 defined('_JEXEC') or die('Restricted access');
 
-use Joomla\CMS\Router\Route;
+use Diddipoeler\Component\SportsManagement\Administrator\Controller\RosterpositionsController;
 
-/**
- * sportsmanagementControllerrosterpositions
- *
- * @package
- * @author
- * @copyright diddi
- * @version   2014
- * @access    public
- */
-class sportsmanagementControllerrosterpositions extends JSMControllerAdmin
-{
+if (!class_exists(RosterpositionsController::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Controller/SportsManagementAdminController.php';
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Controller/RosterpositionsController.php';
+}
 
-	/**
-	 * sportsmanagementControllerrosterpositions::addhome()
-	 *
-	 * @return void
-	 */
-	function addhome()
-	{
-		$this->setRedirect(Route::_('index.php?option=' . $this->option . '&view=rosterposition&addposition=HOME_POS&layout=edit', false));
-	}
-
-	/**
-	 * sportsmanagementControllerrosterpositions::addaway()
-	 *
-	 * @return void
-	 */
-	function addaway()
-	{
-		$this->setRedirect(Route::_('index.php?option=' . $this->option . '&view=rosterposition&addposition=AWAY_POS&layout=edit', false));
-	}
-
-
-	/**
-	 * Proxy for getModel.
-	 *
-	 * @since 1.6
-	 */
-	public function getModel($name = 'rosterposition', $prefix = 'sportsmanagementModel', $config = Array())
-	{
-		$model = parent::getModel($name, $prefix, array('ignore_request' => true));
-
-		return $model;
-	}
+if (!class_exists('sportsmanagementControllerrosterpositions', false)) {
+    class_alias(RosterpositionsController::class, 'sportsmanagementControllerrosterpositions');
 }
