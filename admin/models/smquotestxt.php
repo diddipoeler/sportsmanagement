5502ad76d290a7289f387c3809252c9078a7d0fd
diddipoeler <diddipoeler@gmx.de>
2026-08-19 16:25:24 +0200

    Bridge legacy quote text files model

--- admin/models/smquotestxt.php
+++ admin/models/smquotestxt.php
@@ -1,38 +1,13 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage models
- * @file       smquotestxt.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
+/** Legacy compatibility bridge for the native administrator quote text-files model. */
 defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\Factory;
-use Joomla\CMS\Filesystem\Folder;
 
-/**
- * sportsmanagementModelsmquotestxt
- *
- * @package
- * @author
- * @copyright diddi
- * @version   2014
- * @access    public
- */
-class sportsmanagementModelsmquotestxt extends JSMModelAdmin
-{
+use Diddipoeler\Component\SportsManagement\Administrator\Model\SmquotestxtModel;
 
-	function getTXTFiles()
-	{
-		$app    = Factory::getApplication();
-		$option = Factory::getApplication()->input->getCmd('option');
-		$path   = JPATH_SITE . DIRECTORY_SEPARATOR . 'modules' . DIRECTORY_SEPARATOR . 'mod_sportsmanagement_rquotes' . DIRECTORY_SEPARATOR . 'mod_sportsmanagement_rquotes';
-		/** Get a list of files in the search path with the given filter. */
-		$files = Folder::files($path, '.txt$|.php$');
-		return $files;
-	}
+if (!class_exists(SmquotestxtModel::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Model/SmquotestxtModel.php';
+}
 
+if (!class_exists('sportsmanagementModelsmquotestxt', false)) {
+    class_alias(SmquotestxtModel::class, 'sportsmanagementModelsmquotestxt');
 }
