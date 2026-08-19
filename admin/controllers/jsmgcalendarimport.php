224cb746efa45faf7c6adf4b6c968b89bafdfe9b
diddipoeler <diddipoeler@gmx.de>
2026-08-19 17:48:44 +0200

    Bridge legacy Google calendar import controller

--- admin/controllers/jsmgcalendarimport.php
+++ admin/controllers/jsmgcalendarimport.php
@@ -1,80 +1,13 @@
 <?php
-/**
- *
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage jsmgcalendar
- * @file       jsmgcalendarimport.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
+/** Legacy compatibility bridge for the native administrator Google calendar import controller. */
+defined('_JEXEC') or die('Restricted access');
 
-defined('_JEXEC') or die();
+use Diddipoeler\Component\SportsManagement\Administrator\Controller\JsmgcalendarimportController;
 
-use Joomla\CMS\Language\Text;
-use Joomla\CMS\Factory;
-use Joomla\CMS\MVC\Controller\BaseController;
+if (!class_exists(JsmgcalendarimportController::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Controller/JsmgcalendarimportController.php';
+}
 
-/**
- * sportsmanagementControllerjsmgcalendarImport
- *
- * @package
- * @author    Dieter Plöger
- * @copyright 2017
- * @version   $Id$
- * @access    public
- */
-class sportsmanagementControllerjsmgcalendarImport extends BaseController
-{
-
-	/**
-	 * Class Constructor
-	 *
-	 * @param   array  $config  An optional associative array of configuration settings.
-	 *
-	 * @return void
-	 * @since  1.5
-	 */
-	function __construct($config = array())
-	{
-		/**
-		 *
-		 * Initialise variables.
-		 */
-		$app = Factory::getApplication();
-		parent::__construct($config);
-
-	}
-
-	/**
-	 * sportsmanagementControllerjsmgcalendarImport::import()
-	 *
-	 * @return void
-	 */
-	public function import()
-	{
-
-		$option = Factory::getApplication()->input->getCmd('option');
-		$app    = Factory::getApplication();
-		$model  = $this->getModel('jsmgcalendarImport');
-		$result = $model->import();
-
-		if ($result)
-		{
-			$msg = Text::_('COM_SPORTSMANAGEMENT_JSMGCALENDAR_VIEW_GCALENDARS_IMPORT_YES');
-		}
-		else
-		{
-			$msg = Text::_('COM_SPORTSMANAGEMENT_JSMGCALENDAR_VIEW_GCALENDARS_IMPORT_NO');
-		}
-
-		$link = 'index.php?option=com_sportsmanagement&view=jsmgcalendars';
-
-		$this->setRedirect($link, $msg);
-
-	}
-
+if (!class_exists('sportsmanagementControllerjsmgcalendarImport', false)) {
+    class_alias(JsmgcalendarimportController::class, 'sportsmanagementControllerjsmgcalendarImport');
 }
