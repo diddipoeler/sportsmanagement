5e33d631cb83641a194d339b0973ed3025048b6e
diddipoeler <diddipoeler@gmx.de>
2026-08-19 16:55:15 +0200

    Add CSRF token to DFB division selection form

--- admin/views/jlextdfbkeyimport/tmpl/default_getdivision.php
+++ admin/views/jlextdfbkeyimport/tmpl/default_getdivision.php
@@ -1,32 +1,21 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage jlextdfbkeyimport
- * @file       default_getdivision.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
+/** SportsManagement DFB-key division selection template. */
 defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\HTML\HTMLHelper;
-use Joomla\CMS\Factory;
 
+use Joomla\CMS\HTML\HTMLHelper;
 ?>
 <form action="<?php echo $this->request_url; ?>" method="post" name="adminForm" id="adminForm">
-	<?php
-	echo '<br>' . HTMLHelper::_(
-			'select.genericlist',
-			$this->lists['divisions'],
-			'divisionid',
-			'class="inputbox" size="1" onchange=""',
-			'value', 'text', $this->division
-		);
-
-	?>
-    <input type="hidden" name="sent" value="1"/>
-    <input type="hidden" name="task" value=""/>
-    <input type="hidden" name="projectid" value="<?php echo $this->project_id; ?> "/>
-
+    <?php echo '<br>' . HTMLHelper::_(
+        'select.genericlist',
+        $this->lists['divisions'],
+        'divisionid',
+        'class="inputbox" size="1"',
+        'value',
+        'text',
+        $this->division
+    ); ?>
+    <input type="hidden" name="sent" value="1" />
+    <input type="hidden" name="task" value="" />
+    <input type="hidden" name="projectid" value="<?php echo (int) $this->project_id; ?>" />
+    <?php echo HTMLHelper::_('form.token'); ?>
 </form>
