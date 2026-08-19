c3df44f7c230bf92d1876ee363481e7f529a2fa1
diddipoeler <diddipoeler@gmx.de>
2026-08-19 18:22:21 +0200

    Add native templates controller

---
+++ admin/src/Controller/TemplatesController.php
@@ -0,0 +1,31 @@
+<?php
+namespace Diddipoeler\Component\SportsManagement\Administrator\Controller;
+
+\defined('_JEXEC') or die;
+
+use Joomla\CMS\Router\Route;
+
+/** Native Joomla 5/6 administrator list controller for project templates. */
+final class TemplatesController extends SportsManagementAdminController
+{
+    public function changetemplate(): void
+    {
+        $this->checkToken();
+
+        $id = $this->input->post->getInt('new_id');
+        $projectId = $this->input->post->getInt('pid');
+
+        $this->setRedirect(
+            Route::_(
+                'index.php?option=com_sportsmanagement&view=template&layout=edit&id=' . $id
+                . ($projectId > 0 ? '&pid=' . $projectId : ''),
+                false
+            )
+        );
+    }
+
+    public function getModel($name = 'Template', $prefix = 'Administrator', $config = ['ignore_request' => true])
+    {
+        return parent::getModel($name, $prefix, $config);
+    }
+}
