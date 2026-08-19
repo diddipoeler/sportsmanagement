5ed6190acc3ed7c147dd30e10a315d3ce4884318
diddipoeler <diddipoeler@gmx.de>
2026-08-19 17:29:36 +0200

    Bridge legacy countries controller

--- admin/controllers/jlextcountries.php
+++ admin/controllers/jlextcountries.php
@@ -1,55 +1,14 @@
 <?php
-/**
- *
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage controllers
- * @file       jlextcountries.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-
+/** Legacy compatibility bridge for the native administrator countries controller. */
 defined('_JEXEC') or die('Restricted access');
 
-use Joomla\CMS\Router\Route;
+use Diddipoeler\Component\SportsManagement\Administrator\Controller\JlextcountriesController;
 
-/**
- * sportsmanagementControllerjlextcountries
- *
- * @package
- * @author    diddi
- * @copyright 2014
- * @version   $Id$
- * @access    public
- */
-class sportsmanagementControllerjlextcountries extends JSMControllerAdmin
-{
+if (!class_exists(JlextcountriesController::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Controller/SportsManagementAdminController.php';
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Controller/JlextcountriesController.php';
+}
 
-
-	/**
-	 * sportsmanagementControllerjlextcountries::importplz()
-	 *
-	 * @return void
-	 */
-	function importplz()
-	{
-		$model = $this->getModel();
-		$model->importplz();
-		$this->setRedirect(Route::_('index.php?option=' . $this->option . '&view=' . $this->view_list, false));
-	}
-
-	/**
-	 * Proxy for getModel.
-	 *
-	 * @since 1.6
-	 */
-	public function getModel($name = 'Jlextcountry', $prefix = 'sportsmanagementModel', $config = array())
-	{
-		$model = parent::getModel($name, $prefix, array('ignore_request' => true));
-
-		return $model;
-	}
+if (!class_exists('sportsmanagementControllerjlextcountries', false)) {
+    class_alias(JlextcountriesController::class, 'sportsmanagementControllerjlextcountries');
 }
