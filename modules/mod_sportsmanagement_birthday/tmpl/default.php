f1d49d215fc1e3ceb7c128b5c9d733352f7e8706
diddipoeler <diddipoeler@gmx.de>
2026-08-20 16:32:19 +0200

    Modernize birthday default layout

--- modules/mod_sportsmanagement_birthday/tmpl/default.php
+++ modules/mod_sportsmanagement_birthday/tmpl/default.php
@@ -1,275 +1,84 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- * @version    1.1.1
- * @package    Sportsmanagement
- * @subpackage mod_sportsmanagement_birthday
- * @file       default.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de), llambion
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- * 
- * https://getbootstrap.com/docs/4.0/components/carousel/
- * 
- */
-defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\HTML\HTMLHelper;
-use Joomla\CMS\Uri\Uri;
+\defined('_JEXEC') or die;
+
 use Joomla\CMS\Language\Text;
-use Joomla\CMS\Factory;
 
-switch ($mode)
-{
-	/** bootstrap mode template */
-	case 'B':
-    if ( $params->def("load_bootstrap") ) // Could be removed, joomla has built in a bootstrap version
-    {
-?>
-<!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">	
-<!-- <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script> -->
-<!-- <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>	 -->    
-<?php
-    }
-		?>
-	
-		<div class="row">
-		<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
-		<div class="carousel-indicators">
-		<?php
-		$text_color = $params->get('text_color');
-		$text_size = $params->get('text_size');
-		$a = 0;
-		foreach ($persons AS $person)
-			{
-				$active = ($a == 0) ? 'active' : '';
-		?>
-				<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="<?php echo $a; ?>" class="<?php echo $active; ?>" aria-current="true" aria-label="Slide <?php echo $a; ?>"></button>
-		
-		<?php
-				$a++;
-			}    
-		?>
-		</div>
-				<div class="carousel-inner">
-		<?php
-				$a = 0;
-				foreach ($persons AS $person)
-					{
-						$text = htmlspecialchars(sportsmanagementHelper::formatName(null, $person['firstname'], $person['nickname'], $person['lastname'], $params->get("name_format")), ENT_QUOTES, 'UTF-8');
-
-						$active = ($a == 0) ? 'active' : '';
-
-						if ($params->get('show_picture'))
-						{
-							if (curl_init($module->picture_server . DIRECTORY_SEPARATOR . $person['picture']) && $person['picture'] != '')
-							{
-								$thispic = $module->picture_server . DIRECTORY_SEPARATOR . $person['picture'];
-							}
-                            elseif (curl_init($module->picture_server . DIRECTORY_SEPARATOR . $person['default_picture']) && $person['default_picture'] != '')
-							{
-								$thispic = $module->picture_server . DIRECTORY_SEPARATOR . $person['default_picture'];
-							}
-						}
-
-						switch ($person['days_to_birthday'])
-						{
-							case 0:
-								$whenmessage = $params->get('todaymessage');
-								break;
-							case 1:
-								$whenmessage = $params->get('tomorrowmessage');
-								break;
-							default:
-								$whenmessage = str_replace('%DAYS_TO%', $person['days_to_birthday'], trim($params->get('futuremessage')));
-								break;
-						}
-
-						$birthdaytext   = htmlentities(trim(Text::_($params->get('birthdaytext'))), ENT_COMPAT, 'UTF-8');
-						$dayformat      = htmlentities(trim($params->get('dayformat')));
-						$birthdayformat = htmlentities(trim($params->get('birthdayformat')));
-						$birthdaytext   = str_replace('%WHEN%', $whenmessage, $birthdaytext);
-						$birthdaytext   = str_replace('%AGE%', $person['age'], $birthdaytext);
-						$birthdaytext   = str_replace('%DATE%', date($dayformat, strtotime($person['year'] . '-' . $person['daymonth'])), $birthdaytext);
-						$birthdaytext   = str_replace('%DATE_OF_BIRTH%', date($birthdayformat, strtotime($person['date_of_birth'])), $birthdaytext);
-						$birthdaytext   = str_replace('%BR%', '<br />', $birthdaytext);
-						$birthdaytext   = str_replace('%BOLD%', '<b>', $birthdaytext);
-						$birthdaytext   = str_replace('%BOLDEND%', '</b>', $birthdaytext);
-    
-				?>
-						<style>
-						.w-100 {
-						width: <?php echo $params->get('picture_width'); ?>px !important;
-						height: auto;
-							}	    
-						</style>
-						<div class="carousel-item <?php echo $active; ?>">
-						<img class="d-block w-100" src="<?php echo $thispic; ?>" style="width:<?php echo $params->get('picture_width'); ?>px;height:auto" alt="<?php echo $text; ?>" />
-						<div class="carousel-caption d-none d-md-block">
-						<p style=" 	color: <? echo $text_color;?>;
-									font-size: <? echo $text_size;?>px;
-									font-family: sans-serif; 
-									"> <?php echo $text; ?></p>
-						<p style=" 	color: <? echo $text_color;?>;
-									font-size: <? echo $text_size;?>px;
-									font-family: sans-serif; 
-									"><?php echo $birthdaytext; ?></p>
-						</div>
-    
-    
-						</div>
-    
-						<?php
-						$a++;
-					}
-				?>
-		</div>
-  
-		<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-		<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-		<span class="sr-only">Previous</span>
-		</a>
-		<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-		<span class="carousel-control-next-icon" aria-hidden="true"></span>
-		<span class="sr-only">Next</span>
-		</a>
-  
-		</div>
-
-		</div>        
-        
-
-		<?PHP
-		break;
-
-	default:
-		?>
-
-        <table class="table">
-			<?php
-			if (count($persons) > 0)
-			{
-				foreach ($persons AS $person)
-				{
-					if (($params->get('limit') > 0) && ($counter == intval($params->get('limit'))))
-					{
-						break;
-					}
-
-					$class = ($k == 0) ? $params->get('sectiontableentry1') : $params->get('sectiontableentry2');
-
-					$thispic  = "";
-					$flag     = $params->get('show_player_flag') ? JSMCountries::getCountryFlag($person['country']) . "&nbsp;" : "";
-					$text     = htmlspecialchars(sportsmanagementHelper::formatName(null, $person['firstname'], $person['nickname'], $person['lastname'], $params->get("name_format")), ENT_QUOTES, 'UTF-8');
-					$usedname = $flag . $text;
-
-					$person_link = "";
-					$person_type = $person['type'];
-
-					if ($person_type == 1)
-					{
-						$routeparameter                       = array();
-						$routeparameter['cfg_which_database'] = $params->get("cfg_which_database");
-						$routeparameter['s']                  = $params->get("cfg_which_database");
-						$routeparameter['p']                  = $person['project_slug'];
-						$routeparameter['tid']                = $person['team_slug'];
-						$routeparameter['pid']                = $person['person_slug'];
-						$person_link                          = sportsmanagementHelperRoute::getSportsmanagementRoute('player', $routeparameter);
-					}
-                    elseif ($person_type == 2)
-					{
-						$routeparameter                       = array();
-						$routeparameter['cfg_which_database'] = $params->get("cfg_which_database");
-						$routeparameter['s']                  = $params->get("cfg_which_database");
-						$routeparameter['p']                  = $person['project_slug'];
-						$routeparameter['tid']                = $person['team_slug'];
-						$routeparameter['pid']                = $person['person_slug'];
-						$person_link                          = sportsmanagementHelperRoute::getSportsmanagementRoute('staff', $routeparameter);
-					}
-                    elseif ($person_type == 3)
-					{
-						$routeparameter                       = array();
-						$routeparameter['cfg_which_database'] = $params->get("cfg_which_database");
-						$routeparameter['s']                  = $params->get("cfg_which_database");
-						$routeparameter['p']                  = $person['project_slug'];
-						$routeparameter['pid']                = $person['person_slug'];
-						$person_link                          = sportsmanagementHelperRoute::getSportsmanagementRoute('referee', $routeparameter);
-					}
-
-					$showname = HTMLHelper::link($person_link, $usedname);
-					?>
-                    <tr class="<?php echo $params->get('heading_style'); ?>">
-                        <td class="birthday"><?php echo $showname; ?></td>
-                    </tr>
-                    <tr class="<?php echo $class; ?>">
-                        <td class="birthday">
-							<?php
-							if ($params->get('show_picture') == 1)
-							{
-								if (file_exists(JPATH_BASE . '/' . $person['picture']) && $person['picture'] != '')
-								{
-									$thispic = $person['picture'];
-								}
-                                elseif (file_exists(JPATH_BASE . '/' . $person['default_picture']) && $person['default_picture'] != '')
-								{
-									$thispic = $person['default_picture'];
-								}
-
-								echo '<img src="' . Uri::base() . '/' . $thispic . '" alt="' . $text . '" title="' . $text . '"';
-
-								if ($params->get('picture_height') != '')
-								{
-									echo ' width="auto" height="' . $params->get('picture_height') . '"';
-								}
-
-								echo ' /><br />';
-							}
-
-
-							switch ($person['days_to_birthday'])
-							{
-								case 0:
-									$whenmessage = $params->get('todaymessage');
-									break;
-								case 1:
-									$whenmessage = $params->get('tomorrowmessage');
-									break;
-								default:
-									$whenmessage = str_replace('%DAYS_TO%', $person['days_to_birthday'], trim($params->get('futuremessage')));
-									break;
-							}
-
-							$birthdaytext   = htmlentities(trim(Text::_($params->get('birthdaytext'))), ENT_COMPAT, 'UTF-8');
-							$dayformat      = htmlentities(trim($params->get('dayformat')));
-							$birthdayformat = htmlentities(trim($params->get('birthdayformat')));
-							$birthdaytext   = str_replace('%WHEN%', $whenmessage, $birthdaytext);
-							$birthdaytext   = str_replace('%AGE%', $person['age'], $birthdaytext);
-							$birthdaytext   = str_replace('%DATE%', date($dayformat, strtotime($person['year'] . '-' . $person['daymonth'])), $birthdaytext);
-							$birthdaytext   = str_replace('%DATE_OF_BIRTH%', date($birthdayformat, strtotime($person['date_of_birth'])), $birthdaytext);
-							$birthdaytext   = str_replace('%BR%', '<br />', $birthdaytext);
-							$birthdaytext   = str_replace('%BOLD%', '<b>', $birthdaytext);
-							$birthdaytext   = str_replace('%BOLDEND%', '</b>', $birthdaytext);
-
-							echo $birthdaytext;
-							?></td>
-                    </tr>
-					<?php
-					$k = 1 - $k;
-					$counter++;
-				}
-			}
-			else
-			{
-				?>
-                <tr>
-                    <td class="birthday">
-                        <div class="bg-warning alert alert-warning">
-							<?php echo '' . str_replace('%DAYS%', $params->get('maxdays'), htmlentities(trim(Text::_($params->get('not_found_text'))))) . ''; ?>
-                        </div>
-                    </td>
-                </tr>
-			<?php } ?>
-        </table>
-		<?PHP
-		break;
+if (!$persons) {
+    echo '<p>' . htmlspecialchars(Text::_((string) $params->get('not_found_text', '')), ENT_QUOTES, 'UTF-8') . '</p>';
+    return;
 }
+
+if ($mode === 'S') {
+    require __DIR__ . '/default_player_sticker.php';
+    return;
+}
+
+if ($mode === 'J') {
+    require __DIR__ . '/jssor.php';
+    return;
+}
+
+if ($mode === 'L' && (bool) $params->get('show_player_card', 1)) {
+    require __DIR__ . '/default_player_card.php';
+    return;
+}
+
+$moduleId = 'jsm-birthday-' . (int) $module->id;
+?>
+<div class="<?php echo htmlspecialchars((string) $params->get('moduleclass_sfx', ''), ENT_QUOTES, 'UTF-8'); ?>" id="<?php echo $moduleId; ?>">
+<?php if ($mode === 'B') : ?>
+    <div id="<?php echo $moduleId; ?>-carousel" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+            <?php foreach ($persons as $index => $person) : ?>
+                <button type="button" data-bs-target="#<?php echo $moduleId; ?>-carousel" data-bs-slide-to="<?php echo (int) $index; ?>"
+                        class="<?php echo $index === 0 ? 'active' : ''; ?>"
+                        <?php echo $index === 0 ? 'aria-current="true"' : ''; ?>
+                        aria-label="<?php echo Text::sprintf('JLIB_HTML_PAGE_CURRENT_OF_TOTAL', $index + 1, count($persons)); ?>"></button>
+            <?php endforeach; ?>
+        </div>
+        <div class="carousel-inner">
+            <?php foreach ($persons as $index => $person) : ?>
+                <div class="carousel-item<?php echo $index === 0 ? ' active' : ''; ?>">
+                    <?php if (!empty($person['picture_url'])) : ?>
+                        <img class="d-block mx-auto" src="<?php echo htmlspecialchars($person['picture_url'], ENT_QUOTES, 'UTF-8'); ?>"
+                             alt="<?php echo htmlspecialchars($person['display_name'], ENT_QUOTES, 'UTF-8'); ?>"
+                             style="max-width:<?php echo max(1, (int) $params->get('picture_width', 250)); ?>px;height:auto">
+                    <?php endif; ?>
+                    <div class="carousel-caption position-static text-body">
+                        <h5><a href="<?php echo htmlspecialchars($person['person_link'], ENT_QUOTES, 'UTF-8'); ?>"><?php echo $person['flag_html']; ?> <?php echo htmlspecialchars($person['display_name'], ENT_QUOTES, 'UTF-8'); ?></a></h5>
+                        <p><?php echo $person['birthday_text']; ?></p>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#<?php echo $moduleId; ?>-carousel" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span><span class="visually-hidden"><?php echo Text::_('JPREV'); ?></span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#<?php echo $moduleId; ?>-carousel" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span><span class="visually-hidden"><?php echo Text::_('JNEXT'); ?></span>
+        </button>
+    </div>
+<?php else : ?>
+    <div class="table-responsive">
+        <table class="table table-striped align-middle">
+            <tbody>
+            <?php foreach ($persons as $person) : ?>
+                <tr>
+                    <td>
+                        <a href="<?php echo htmlspecialchars($person['person_link'], ENT_QUOTES, 'UTF-8'); ?>">
+                            <?php echo $person['flag_html']; ?> <?php echo htmlspecialchars($person['display_name'], ENT_QUOTES, 'UTF-8'); ?>
+                        </a>
+                        <?php if (!empty($person['picture_url'])) : ?>
+                            <div class="mt-2"><img src="<?php echo htmlspecialchars($person['picture_url'], ENT_QUOTES, 'UTF-8'); ?>"
+                                 alt="<?php echo htmlspecialchars($person['display_name'], ENT_QUOTES, 'UTF-8'); ?>"
+                                 style="max-height:<?php echo max(1, (int) $params->get('picture_height', 50)); ?>px;width:auto"></div>
+                        <?php endif; ?>
+                    </td>
+                    <td><?php echo $person['birthday_text']; ?></td>
+                </tr>
+            <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
+<?php endif; ?>
+</div>
