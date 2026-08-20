ea4bfec9a5f2aac5677d5f82ac121443bc1ba329
diddipoeler <diddipoeler@gmx.de>
2026-08-20 16:35:33 +0200

    Make birthday entry point package-safe

--- modules/mod_sportsmanagement_birthday/mod_sportsmanagement_birthday.php
+++ modules/mod_sportsmanagement_birthday/mod_sportsmanagement_birthday.php
@@ -2,19 +2,19 @@
 /** Joomla 5/6 compatibility entry point for the birthday module. */
 defined('_JEXEC') or die;
 
-use Diddipoeler\Module\SportsManagementBirthday\Site\Helper\BirthdayHelper;
 use Joomla\CMS\Component\ComponentHelper;
 use Joomla\CMS\Factory;
 use Joomla\CMS\Helper\ModuleHelper;
 
-if (!class_exists(BirthdayHelper::class)) {
-    require_once __DIR__ . '/src/Helper/BirthdayHelper.php';
-}
+require_once __DIR__ . '/helper.php';
 
 $app = Factory::getApplication();
 $app->getLanguage()->load('com_sportsmanagement', JPATH_ADMINISTRATOR, null, true);
-$componentParams = ComponentHelper::getParams('com_sportsmanagement');
-$result = (new BirthdayHelper())->getData($params, $componentParams, $app);
+$result = (new modSportsmanagementBirthdayDataHelper())->getData(
+    $params,
+    ComponentHelper::getParams('com_sportsmanagement'),
+    $app
+);
 
 $persons = $result['persons'];
 $mode = $result['mode'];
