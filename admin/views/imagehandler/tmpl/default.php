6d86beca3b30e979c16c330dc313a55c24bbdf2a
diddipoeler <diddipoeler@gmx.de>
2026-08-20 09:12:41 +0200

    Modernize image handler list template

--- admin/views/imagehandler/tmpl/default.php
+++ admin/views/imagehandler/tmpl/default.php
@@ -1,66 +1,40 @@
 <?php
-/**
- *
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage imagehandler
- * @file       default.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-
-
+/** SportsManagement administrator image selector. */
 defined('_JEXEC') or die('Restricted access');
 
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
-
-// Welche joomla version
-if (version_compare(JVERSION, '3.0.0', 'ge'))
-{
-	HTMLHelper::_('behavior.framework', true);
-}
-else
-{
-	HTMLHelper::_('behavior.mootools');
-}
-
 ?>
 <div class="container-fluid">
-    <form action="<?php echo $this->request_url; ?>" method="post" id="adminForm" name="adminForm">
-        <div class="imghead">
-
-			<?php echo Text::_('JSEARCH_FILTER_LABEL') . ' '; ?>
-            <input type="text" name="search" id="search" value="<?php echo $this->search; ?>" class="text_area"
-                   onChange="document.getElementById('adminForm').submit();"/>
-            <button onclick="this.form.submit();"><?php echo Text::_('JSEARCH_FILTER_SUBMIT'); ?></button>
-            <button onclick="this.form.getElementById('search').value='';this.form.submit();"><?php echo Text::_('JSEARCH_FILTER_CLEAR'); ?></button>
-
+    <form action="<?php echo $this->escape($this->request_url); ?>" method="post" id="adminForm" name="adminForm">
+        <div class="d-flex gap-2 align-items-center mb-3">
+            <label for="search" class="visually-hidden"><?php echo Text::_('JSEARCH_FILTER_LABEL'); ?></label>
+            <input type="search" name="search" id="search" value="<?php echo $this->escape($this->search); ?>"
+                   class="form-control" placeholder="<?php echo Text::_('JSEARCH_FILTER'); ?>">
+            <button type="submit" class="btn btn-primary"><?php echo Text::_('JSEARCH_FILTER_SUBMIT'); ?></button>
+            <button type="button" class="btn btn-secondary"
+                    onclick="document.getElementById('search').value='';this.form.submit();">
+                <?php echo Text::_('JSEARCH_FILTER_CLEAR'); ?>
+            </button>
         </div>
 
-        <div class="imglist">
-
-			<?php
-			for ($i = 0, $n = count($this->images); $i < $n; $i++)
-				:
-				$this->setImage($i);
-				echo $this->loadTemplate('image');
-			endfor;
-			?>
+        <div class="imglist d-flex flex-wrap gap-3">
+            <?php for ($i = 0, $n = count($this->images); $i < $n; $i++) : ?>
+                <?php $this->setImage($i); ?>
+                <?php echo $this->loadTemplate('image'); ?>
+            <?php endfor; ?>
         </div>
 
-        <div class="clr"></div>
+        <div class="mt-3"><?php echo $this->pageNav->getListFooter(); ?></div>
 
-        <div class="pnav"><?php echo $this->pageNav->getListFooter(); ?></div>
-
-        <input type="hidden" name="option" value="com_sportsmanagement"/>
-        <input type="hidden" name="view" value="imagehandler"/>
-        <input type="hidden" name="tmpl" value="component"/>
-        <input type="hidden" name="task" value="imagehandler.select"/>
-        <input type="hidden" name="folder" value="<?php echo $this->folder; ?>"/>
-		<?php echo HTMLHelper::_('form.token'); ?>
+        <input type="hidden" name="option" value="com_sportsmanagement">
+        <input type="hidden" name="view" value="imagehandler">
+        <input type="hidden" name="tmpl" value="component">
+        <input type="hidden" name="task" value="">
+        <input type="hidden" name="folder" value="<?php echo $this->escape($this->folder); ?>">
+        <input type="hidden" name="type" value="<?php echo $this->escape($this->type); ?>">
+        <input type="hidden" name="field" value="<?php echo $this->escape($this->field); ?>">
+        <input type="hidden" name="fieldid" value="<?php echo $this->escape($this->fieldid); ?>">
+        <?php echo HTMLHelper::_('form.token'); ?>
     </form>
 </div>
