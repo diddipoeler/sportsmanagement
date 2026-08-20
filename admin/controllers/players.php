6e23d161a3c2a9d0382b468730cc4d4db6650a9a
diddipoeler <diddipoeler@gmx.de>
2026-08-20 09:30:42 +0200

    Bridge legacy players controller

--- admin/controllers/players.php
+++ admin/controllers/players.php
@@ -1,99 +1,14 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage controllers
- * @file       players.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
+/** Legacy compatibility bridge for the native administrator players controller. */
 defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\Language\Text;
-use Joomla\CMS\Factory;
-use Joomla\CMS\Session\Session;
-use Joomla\CMS\Router\Route;
 
+use Diddipoeler\Component\SportsManagement\Administrator\Controller\PlayersController;
 
-/**
- * sportsmanagementControllerplayers
- *
- * @package
- * @author    Dieter Plöger
- * @copyright 2019
- * @version   $Id$
- * @access    public
- */
-class sportsmanagementControllerplayers extends JSMControllerAdmin
-{
-
-
-/**
- * sportsmanagementControllerplayers::importupload()
- * 
- * @return void
- */
-function importupload()
-{
-$model = $this->getModel();    
-$post = Factory::getApplication()->input->post->getArray(array());
-Session::checkToken() or jexit(Text::_('JINVALID_TOKEN'));
-$model = $this->getModel();
-$msg   = $model->importupload($post);
-            
+if (!class_exists(PlayersController::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Controller/SportsManagementAdminController.php';
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Controller/PlayersController.php';
 }
 
-	/**
-	 * sportsmanagementControllerplayers::assign()
-	 *
-	 * @return
-	 */
-	function assign()
-	{
-		$post = Factory::getApplication()->input->post->getArray(array());
-		Session::checkToken() or jexit(Text::_('JINVALID_TOKEN'));
-		$model = $this->getModel();
-		$msg   = $model->storeAssign($post);
-		$this->setRedirect('index.php?option=com_sportsmanagement&view=close&tmpl=component', $msg);
-	}
-
-	/**
-	 * sportsmanagementControllerplayers::getModel()
-	 *
-	 * @param   string  $name
-	 * @param   string  $prefix
-	 * @param   mixed   $config
-	 *
-	 * @return
-	 */
-	public function getModel($name = 'player', $prefix = 'sportsmanagementModel', $config = Array())
-	{
-		$model = parent::getModel($name, $prefix, array('ignore_request' => true));
-		return $model;
-	}
-
-	/**
-	 * sportsmanagementControllerplayers::close()
-	 *
-	 * @return
-	 */
-	function close()
-	{
-		$post = Factory::getApplication()->input->post->getArray(array());
-		Session::checkToken() or jexit(\Text::_('JINVALID_TOKEN'));
-		$this->setRedirect('index.php?option=com_sportsmanagement&view=close&tmpl=component', $msg);
-	}
-
-	/**
-	 * sportsmanagementControllerplayers::saveshort()
-	 *
-	 * @return
-	 */
-	function saveshort()
-	{
-		$model = $this->getModel();
-		$model->saveshort();
-		$this->setRedirect(Route::_('index.php?option=' . $this->option . '&view=' . $this->view_list, false));
-	}
+if (!class_exists('sportsmanagementControllerplayers', false)) {
+    class_alias(PlayersController::class, 'sportsmanagementControllerplayers');
 }
