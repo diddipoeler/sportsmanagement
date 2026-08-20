d2171048fa7cb3559e73d3d7fb5a3afe91257a3f
diddipoeler <diddipoeler@gmx.de>
2026-08-20 16:38:34 +0200

    Modernize club birthday helper

--- modules/mod_sportsmanagement_club_birthday/helper.php
+++ modules/mod_sportsmanagement_club_birthday/helper.php
@@ -1,143 +1,181 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage mod_sportsmanagement_club_birthday
- * @file       helper.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-defined('_JEXEC') or die('Restricted access');
-use Joomla\Utilities\ArrayHelper;
+/** Joomla 5/6 runtime helper for club anniversaries. */
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Component\ComponentHelper;
 use Joomla\CMS\Factory;
 use Joomla\CMS\Language\Text;
+use Joomla\CMS\Router\Route;
+use Joomla\CMS\Uri\Uri;
+use Joomla\Database\DatabaseInterface;
+use Joomla\Registry\Registry;
 
-/**
- * welche joomla version ?
- */
-if (version_compare(substr(JVERSION, 0, 1), '4', 'eq'))
-{
-	JLoader::import('components.com_sportsmanagement.libraries.sportsmanagement.arrayhelper', JPATH_SITE);
-}
-else
-{
-	jimport('joomla.utilities.arrayhelper');
-}
-
-$clubs = array();
-$crew  = array();
-
-/**
- * modSportsmanagementClubBirthdayHelper
- *
- * @package
- * @author
- * @copyright diddi
- * @version   2014
- * @access    public
- */
 class modSportsmanagementClubBirthdayHelper
 {
+    public static function getData(Registry $params): array
+    {
+        $clubs = self::getClubs((int) $params->get('limit', 0), $params->get('s', []));
+        if (count($clubs) > 1) {
+            $clubs = self::jsm_birthday_sort($clubs, (int) $params->get('sort_order', 1));
+        }
+        foreach ($clubs as $club) {
+            $club->birthday_text = self::birthdayText($club, $params);
+        }
+        return ['clubs' => $clubs, 'mode' => strtoupper((string) $params->get('mode', 'L'))];
+    }
 
-	/**
-	 * modSportsmanagementClubBirthdayHelper::jsm_birthday_sort()
-	 *
-	 * @param   mixed  $array
-	 * @param   mixed  $sort
-	 *
-	 * @return
-	 */
-	public static function jsm_birthday_sort($array, $sort)
-	{
+    public static function jsm_birthday_sort($array, $sort)
+    {
+        $direction = (int) $sort < 0 ? -1 : 1;
+        usort($array, static fn(object $a, object $b): int => (((int) $a->age <=> (int) $b->age) * $direction));
+        return $array;
+    }
 
-		/**
-		 * Utility function to sort an array of objects on a given field
-		 *
-		 * @param   array  &$a              An array of objects
-		 * @param   mixed   $k              The key (string) or a array of key to sort on
-		 * @param   mixed   $direction      Direction (integer) or an array of direction to sort in [1 = Ascending] [-1 = Descending]
-		 * @param   mixed   $caseSensitive  Boolean or array of booleans to let sort occur case sensitive or insensitive
-		 * @param   mixed   $locale         Boolean or array of booleans to let sort occur using the locale language or not
-		 *
-		 * @return array  The sorted array of objects
-		 *
-		 * @since 11.1
-		 */
+    public static function getClubs($limit, $seasonIds)
+    {
+        $db = self::database();
+        $seasonIds = self::normaliseIds($seasonIds);
+        $query = $db->getQuery(true)
+            ->select([
+                'c.id', 'c.country', 'c.founded', 'c.name', 'c.alias', 'c.founded_year',
+                'c.logo_big AS picture', 'c.founded_timestamp', 'pt.project_id',
+                'co.alpha2 AS country_alpha2', 'co.picture AS country_picture',
+            ])
+            ->from($db->quoteName('#__sportsmanagement_club', 'c'))
+            ->join('INNER', $db->quoteName('#__sportsmanagement_team', 't') . ' ON t.club_id = c.id')
+            ->join('INNER', $db->quoteName('#__sportsmanagement_season_team_id', 'st') . ' ON st.team_id = t.id')
+            ->join('INNER', $db->quoteName('#__sportsmanagement_project_team', 'pt') . ' ON pt.team_id = st.id')
+            ->join('LEFT', $db->quoteName('#__sportsmanagement_countries', 'co') . ' ON co.alpha3 = c.country')
+            ->where("c.founded <> '0000-00-00'")
+            ->where("c.founded_year <> '0000'")
+            ->where("c.founded_year <> ''")
+            ->order('c.name ASC');
+        if ($seasonIds) {
+            $query->where('st.season_id IN (' . implode(',', $seasonIds) . ')');
+        }
+        $db->setQuery($query);
+        $rows = $db->loadObjectList() ?: [];
 
-		$res = ArrayHelper::sortObjects($array, 'age', $sort);
+        $timezone = self::timezone();
+        $today = new DateTimeImmutable('today', $timezone);
+        $componentParams = ComponentHelper::getParams('com_sportsmanagement');
+        $clubs = [];
+        foreach ($rows as $row) {
+            $id = (int) $row->id;
+            if (isset($clubs[$id])) {
+                continue;
+            }
+            $birth = DateTimeImmutable::createFromFormat('!Y-m-d', (string) $row->founded, $timezone);
+            if (!$birth instanceof DateTimeImmutable) {
+                continue;
+            }
+            $birthday = $today->setDate((int) $today->format('Y'), (int) $birth->format('m'), (int) $birth->format('d'));
+            if ($birthday < $today) {
+                $birthday = $birthday->modify('+1 year');
+            }
+            $row->date_of_birth = $birth->format('Y-m-d');
+            $row->daymonth = $birth->format('m-d');
+            $row->year = $birthday->format('Y');
+            $row->days_to_birthday = (int) $today->diff($birthday)->format('%a');
+            $row->age = (int) $birthday->format('Y') - (int) $birth->format('Y');
+            $row->age_year = (int) $today->format('Y') - (int) $row->founded_year;
+            $row->club_link = self::clubLink((int) $row->project_id, $id);
+            $row->picture_url = self::pictureUrl((string) $row->picture, $componentParams);
+            $row->flag_html = self::flagHtml($row, $componentParams);
+            $clubs[$id] = $row;
+        }
 
-		return $res;
-	}
+        $clubs = array_values($clubs);
+        usort($clubs, static fn(object $a, object $b): int => ((int) $a->days_to_birthday <=> (int) $b->days_to_birthday));
+        $limit = max(0, (int) $limit);
+        return $limit > 0 ? array_slice($clubs, 0, $limit) : $clubs;
+    }
 
+    private static function birthdayText(object $club, Registry $params): string
+    {
+        $when = match ((int) $club->days_to_birthday) {
+            0 => (string) $params->get('todaymessage', ''),
+            1 => (string) $params->get('tomorrowmessage', ''),
+            default => str_replace('%DAYS_TO%', (string) $club->days_to_birthday, trim((string) $params->get('futuremessage', ''))),
+        };
+        $text = htmlspecialchars(trim(Text::_((string) $params->get('birthdaytext', ''))), ENT_QUOTES, 'UTF-8');
+        $birth = new DateTimeImmutable((string) $club->date_of_birth, self::timezone());
+        $next = new DateTimeImmutable((string) $club->year . '-' . (string) $club->daymonth, self::timezone());
+        return strtr($text, [
+            '%WHEN%' => htmlspecialchars($when, ENT_QUOTES, 'UTF-8'),
+            '%AGE%' => (string) $club->age,
+            '%DATE%' => $next->format((string) $params->get('dayformat', 'd.m.Y')),
+            '%DATE_OF_BIRTH%' => $birth->format((string) $params->get('birthdayformat', 'd.m.Y')),
+            '%BR%' => '<br />', '%BOLD%' => '<strong>', '%BOLDEND%' => '</strong>',
+        ]);
+    }
 
-	/**
-	 * modSportsmanagementClubBirthdayHelper::getClubs()
-	 *
-	 * @param   mixed  $limit
-	 * @param   mixed  $season_ids
-	 *
-	 * @return
-	 */
-	public static function getClubs($limit, $season_ids)
-	{
-		$app          = Factory::getApplication();
-		$birthdaytext = '';
-		$database     = sportsmanagementHelper::getDBConnection();
-        $result = array();
-		/**
-		 *
-		 * get club info, we have to make a function for this
-		 */
-		$dateformat = "DATE_FORMAT(c.founded,'%Y-%m-%d') AS date_of_birth";
+    private static function clubLink(int $projectId, int $clubId): string
+    {
+        if (!class_exists('sportsmanagementHelperRoute')) {
+            JLoader::register('sportsmanagementHelperRoute', JPATH_SITE . '/components/com_sportsmanagement/helpers/route.php');
+        }
+        if (class_exists('sportsmanagementHelperRoute') && method_exists('sportsmanagementHelperRoute', 'getClubInfoRoute')) {
+            return (string) sportsmanagementHelperRoute::getClubInfoRoute($projectId, $clubId);
+        }
+        return Route::_('index.php?' . http_build_query(['option' => 'com_sportsmanagement', 'view' => 'clubinfo', 'p' => $projectId, 'cid' => $clubId]), false);
+    }
 
-		if ($season_ids)
-		{
-			foreach ($season_ids as $key => $val)
-			{
-				$season_ids[$key] = (int) $val;
-			}
+    private static function pictureUrl(string $picture, Registry $params): string
+    {
+        $picture = ltrim(trim($picture), '/');
+        if ($picture !== '' && is_file(JPATH_ROOT . '/' . $picture)) {
+            return Uri::root() . $picture;
+        }
+        $placeholder = ltrim((string) $params->get('ph_clublogo', ''), '/');
+        return $placeholder !== '' && is_file(JPATH_ROOT . '/' . $placeholder) ? Uri::root() . $placeholder : '';
+    }
 
-			$seasons = implode(",", $season_ids);
-		}
+    private static function flagHtml(object $row, Registry $params): string
+    {
+        $alpha3 = strtoupper((string) ($row->country ?? ''));
+        $alpha2 = strtolower((string) ($row->country_alpha2 ?? ''));
+        $label = htmlspecialchars($alpha3, ENT_QUOTES, 'UTF-8');
+        if ((int) $params->get('cfg_flags_css', 0) === 1) {
+            $css = match ($alpha3) { 'WAL' => 'gb-wls', 'SCO' => 'gb-sct', 'GBR' => 'gb-eng', default => $alpha2 };
+            return $css !== '' ? '<span class="fi fi-' . htmlspecialchars($css, ENT_QUOTES, 'UTF-8') . '" title="' . $label . '"></span>' : '';
+        }
+        $path = $alpha2 !== '' ? 'images/com_sportsmanagement/database/flags/' . $alpha2 . '.png' : ltrim((string) ($row->country_picture ?? $params->get('ph_flags', '')), '/');
+        return $path === '' ? '' : '<img src="' . htmlspecialchars(Uri::root() . $path, ENT_QUOTES, 'UTF-8') . '" alt="' . $label . '" />';
+    }
 
-		$query = $database->getQuery(true);
-		$query->select('c.id,c.country,c.founded,c.name,c.alias,c.founded_year,c.logo_big AS picture,c.founded_timestamp, DATE_FORMAT(c.founded, \'%m-%d\')AS daymonth,YEAR( CURRENT_DATE( ) ) as year');
-		$query->select('(YEAR( CURRENT_DATE( ) ) - YEAR( c.founded ) + IF(DATE_FORMAT(CURDATE(), \'%m.%d\') > DATE_FORMAT(c.founded, \'%m.%d\'), 1, 0)) AS age, YEAR( CURRENT_DATE( ) ) - c.founded_year as age_year');
-		$query->select($dateformat);
-		$query->select('(TO_DAYS(DATE_ADD(c.founded, INTERVAL(YEAR(CURDATE()) - YEAR(c.founded) + IF(DATE_FORMAT(CURDATE(), \'%m.%d\') > DATE_FORMAT(c.founded, \'%m.%d\'), 1, 0))YEAR)) - TO_DAYS( CURDATE())+0) AS days_to_birthday');
-		$query->from('#__sportsmanagement_club AS c ');
-		$query->join('INNER', ' #__sportsmanagement_team as t ON t.club_id = c.id ');
-		$query->join('INNER', ' #__sportsmanagement_season_team_id as st ON st.team_id = t.id ');
-		$query->join('INNER', ' #__sportsmanagement_project_team as pt ON st.id = pt.team_id ');
-		$query->where('( c.founded != \'0000-00-00\' AND c.founded_year != \'0000\'  AND c.founded_year != \'\' ) ');
+    private static function normaliseIds(mixed $values): array
+    {
+        $values = is_array($values) ? $values : [$values];
+        $ids = [];
+        foreach ($values as $value) {
+            foreach (preg_split('/[\s,;]+/', (string) $value, -1, PREG_SPLIT_NO_EMPTY) ?: [] as $part) {
+                $id = (int) $part;
+                if ($id > 0) $ids[$id] = $id;
+            }
+        }
+        return array_values($ids);
+    }
 
-		if ($seasons)
-		{
-			$query->where('st.season_id IN (' . $seasons . ')');
-		}
+    private static function timezone(): DateTimeZone
+    {
+        try {
+            return new DateTimeZone((string) Factory::getApplication()->get('offset', 'UTC'));
+        } catch (Throwable) {
+            return new DateTimeZone('UTC');
+        }
+    }
 
-		$query->group('c.id,c.country,c.founded,c.name,c.alias,c.founded_year,c.logo_big');
-		$query->order('days_to_birthday ASC');
-
-		try
-		{
-			$database->setQuery($query, 0, $limit);
-			$result = $database->loadObjectList();
-			$database->disconnect(); // See: http://api.joomla.org/cms-3/classes/JDatabaseDriver.html#method_disconnect
-			return $result;
-		}
-		catch (Exception $e)
-		{
-            $app->enqueueMessage(Text::sprintf('COM_SPORTSMANAGEMENT_DATABASE_ERROR_FUNCTION_FAILED', $e->getCode(), $e->getMessage()), 'error');
-            $app->enqueueMessage(Text::sprintf('COM_SPORTSMANAGEMENT_FILE_ERROR_FUNCTION_FAILED', __FILE__, __LINE__), 'error');
-			$database->disconnect(); // See: http://api.joomla.org/cms-3/classes/JDatabaseDriver.html#method_disconnect
-			return $result;
-		}
-
-	}
-
+    private static function database(): DatabaseInterface
+    {
+        if (!class_exists('sportsmanagementHelper')) {
+            JLoader::register('sportsmanagementHelper', JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/helpers/sportsmanagement.php');
+        }
+        try {
+            $db = sportsmanagementHelper::getDBConnection();
+            if ($db instanceof DatabaseInterface) return $db;
+        } catch (Throwable) {
+        }
+        return Factory::getContainer()->get(DatabaseInterface::class);
+    }
 }
-
-
