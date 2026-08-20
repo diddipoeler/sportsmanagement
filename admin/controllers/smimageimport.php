734fc1da50b535bb13d1d0cafc7df451cfda7422
diddipoeler <diddipoeler@gmx.de>
2026-08-20 08:31:00 +0200

    Bridge legacy image package form controller

--- admin/controllers/smimageimport.php
+++ admin/controllers/smimageimport.php
@@ -1,34 +1,14 @@
 <?php
-/**
- *
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage controllers
- * @file       smimageimport.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-
-
+/** Legacy compatibility bridge for the native administrator Smimageimport controller. */
 defined('_JEXEC') or die('Restricted access');
 
-use Joomla\CMS\MVC\Controller\FormController;
+use Diddipoeler\Component\SportsManagement\Administrator\Controller\SmimageimportController;
 
+if (!class_exists(SmimageimportController::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Controller/SportsManagementFormController.php';
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Controller/SmimageimportController.php';
+}
 
-/**
- * sportsmanagementControllersmimageimport
- *
- * @package
- * @author
- * @copyright diddi
- * @version   2014
- * @access    public
- */
-class sportsmanagementControllersmimageimport extends FormController
-{
-
-
+if (!class_exists('sportsmanagementControllersmimageimport', false)) {
+    class_alias(SmimageimportController::class, 'sportsmanagementControllersmimageimport');
 }
