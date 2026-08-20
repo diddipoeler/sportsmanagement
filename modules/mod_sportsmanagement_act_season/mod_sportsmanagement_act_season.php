e0ced02b5524ffd0ba841fc88863e6a314cd8280
diddipoeler <diddipoeler@gmx.de>
2026-08-20 16:23:46 +0200

    Modernize active season module entry point

--- modules/mod_sportsmanagement_act_season/mod_sportsmanagement_act_season.php
+++ modules/mod_sportsmanagement_act_season/mod_sportsmanagement_act_season.php
@@ -1,80 +1,31 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage mod_sportsmanagement_act_season
- * @file       mod_sportsmanagement_act_season.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\MVC\Model\BaseDatabaseModel;
-use Joomla\CMS\Helper\ModuleHelper;
-use Joomla\CMS\Uri\Uri;
-use Joomla\CMS\Factory;
+/** Joomla 5/6 compatibility entry point for the current-season module. */
+defined('_JEXEC') or die;
+
+use Diddipoeler\Module\SportsManagementActSeason\Site\Helper\ActSeasonHelper;
 use Joomla\CMS\Component\ComponentHelper;
+use Joomla\CMS\Factory;
+use Joomla\CMS\Helper\ModuleHelper;
 
-if (!defined('DS'))
-{
-	define('DS', DIRECTORY_SEPARATOR);
+if (!class_exists(ActSeasonHelper::class)) {
+    require_once __DIR__ . '/src/Helper/ActSeasonHelper.php';
 }
 
-if (!defined('JSM_PATH'))
-{
-	DEFINE('JSM_PATH', 'components/com_sportsmanagement');
-}
+$app = Factory::getApplication();
+$app->getLanguage()->load('com_sportsmanagement', JPATH_ADMINISTRATOR, null, true);
 
-/** prüft vor Benutzung ob die gewünschte Klasse definiert ist */
-if (!class_exists('JSMModelLegacy'))
-{
-	JLoader::import('components.com_sportsmanagement.libraries.sportsmanagement.model', JPATH_SITE);
-}
+$componentParams = ComponentHelper::getParams('com_sportsmanagement');
+$result = (new ActSeasonHelper())->getData(
+    $componentParams->get('current_season', []),
+    $componentParams,
+    $app
+);
 
+$list = $result['list'];
+$federations = $result['federations'];
+$countriesByFederation = $result['countriesByFederation'];
 
-if (!class_exists('JSMCountries'))
-{
-	JLoader::import('components.com_sportsmanagement.helpers.countries', JPATH_SITE);
-}
-
-
-if (!class_exists('sportsmanagementHelper'))
-{
-	/** add the classes for handling */
-	$classpath = JPATH_ADMINISTRATOR . DIRECTORY_SEPARATOR . JSM_PATH . DIRECTORY_SEPARATOR . 'helpers' . DIRECTORY_SEPARATOR . 'sportsmanagement.php';
-	JLoader::register('sportsmanagementHelper', $classpath);
-	BaseDatabaseModel::getInstance("sportsmanagementHelper", "sportsmanagementModel");
-}
-
-if (!defined('COM_SPORTSMANAGEMENT_CFG_WHICH_DATABASE'))
-{
-	DEFINE('COM_SPORTSMANAGEMENT_CFG_WHICH_DATABASE', ComponentHelper::getParams('com_sportsmanagement')->get('cfg_which_database'));
-}
-
-/** Include the functions only once */
-JLoader::register('modJSMActSeasonHelper', __DIR__ . '/helper.php');
-
-$document = Factory::getDocument();
-
-/** die übersetzungen der länder laden */
-$language = Factory::getLanguage();
-$language->load('com_sportsmanagement', JPATH_ADMINISTRATOR, null, true);
-
-$season_ids = ComponentHelper::getParams('com_sportsmanagement')->get('current_season');
-
-$list = modJSMActSeasonHelper::getData($season_ids);
-$federation = modJSMActSeasonHelper::getDataFederation($list);
-$countryfederation = modJSMActSeasonHelper::getDataCcountryFederation();
-
-/** add css file */
-// $document->addStyleSheet(Uri::base().'modules/mod_sportsmanagement_new_project/css/mod_sportsmanagement_new_project.css');
-
-/** Layout */
-?>
-<div class="<?php echo $params->get('moduleclass_sfx'); ?>"
-     id="<?php echo $module->module; ?>-<?php echo $module->id; ?>">
-	<?PHP
-	require ModuleHelper::getLayoutPath($module->module);
-	?>
-</div>
+require ModuleHelper::getLayoutPath(
+    $module->module,
+    (string) $params->get('layout', 'default')
+);
