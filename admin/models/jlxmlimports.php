6e8cc49e6d9315a74e755146c9e78bfa2a8ed147
diddipoeler <diddipoeler@gmx.de>
2026-08-20 08:25:30 +0200

    Bridge legacy XML imports landing model

--- admin/models/jlxmlimports.php
+++ admin/models/jlxmlimports.php
@@ -1,47 +1,13 @@
 <?php
-/**
- *
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage models
- * @file       jlxmlimports.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-
-
+/** Legacy compatibility bridge for the native administrator JLXMLImports model. */
 defined('_JEXEC') or die('Restricted access');
 
-use Joomla\CMS\MVC\Model\BaseDatabaseModel;
+use Diddipoeler\Component\SportsManagement\Administrator\Model\JlxmlimportsModel;
 
-/**
- * sportsmanagementModelJLXMLImports
- *
- * @package
- * @author    diddi
- * @copyright 2014
- * @version   $Id$
- * @access    public
- */
-class sportsmanagementModelJLXMLImports extends BaseDatabaseModel
-{
-
-	/**
-	 * sportsmanagementModelJLXMLImports::__construct()
-	 *
-	 * @param   mixed  $config
-	 *
-	 * @return void
-	 */
-	public function __construct($config = array())
-	{
-		parent::__construct($config);
-		$getDBConnection = sportsmanagementHelper::getDBConnection();
-		parent::setDbo($getDBConnection);
-	}
-
+if (!class_exists(JlxmlimportsModel::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Model/JlxmlimportsModel.php';
 }
 
+if (!class_exists('sportsmanagementModelJLXMLImports', false)) {
+    class_alias(JlxmlimportsModel::class, 'sportsmanagementModelJLXMLImports');
+}
