427019cd26154c1743d7b8495a22f863e4d4806c
diddipoeler <diddipoeler@gmx.de>
2026-08-20 08:32:20 +0200

    Modernize image package import wrapper

--- admin/views/smimageimports/tmpl/default.php
+++ admin/views/smimageimports/tmpl/default.php
@@ -1,52 +1,18 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage smimageimports
- * @file       default.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
+/** Native Joomla 5/6 wrapper for the image-package import list. */
 defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\Language\Text;
+
 use Joomla\CMS\HTML\HTMLHelper;
 
-$templatesToLoad = array('footer', 'listheader');
+$templatesToLoad = ['footer'];
 sportsmanagementHelper::addTemplatePaths($templatesToLoad, $this);
-
 ?>
-<form action="<?php echo $this->request_url; ?>" method="post" id="adminForm" name="adminForm">
-
-	<?PHP
-echo $this->loadTemplate('joomla_version');
-	/*
-	if (version_compare(JVERSION, '3.0.0', 'ge'))
-	{
-		echo $this->loadTemplate('joomla3');
-	}
-	else
-	{
-		echo $this->loadTemplate('joomla2');
-	}
-*/
-	?>
-
-
-
-
-        <!-- </fieldset> -->
-
-        <input type="hidden" name="task" value=""/>
-        <input type="hidden" name="boxchecked" value="0"/>
-        <input type="hidden" name="filter_order" value="<?php echo $this->sortColumn; ?>"/>
-        <input type="hidden" name="filter_order_Dir" value="<?php echo $this->sortDirection; ?>"/>
-
-		<?php echo HTMLHelper::_('form.token') . "\n"; ?>
+<form action="<?php echo htmlspecialchars((string) $this->request_url, ENT_QUOTES, 'UTF-8'); ?>" method="post" id="adminForm" name="adminForm">
+    <?php echo $this->loadTemplate('data'); ?>
+    <input type="hidden" name="task" value="" />
+    <input type="hidden" name="boxchecked" value="0" />
+    <input type="hidden" name="filter_order" value="<?php echo htmlspecialchars((string) $this->sortColumn, ENT_QUOTES, 'UTF-8'); ?>" />
+    <input type="hidden" name="filter_order_Dir" value="<?php echo htmlspecialchars((string) $this->sortDirection, ENT_QUOTES, 'UTF-8'); ?>" />
+    <?php echo HTMLHelper::_('form.token'); ?>
 </form>
-<?PHP
-
-echo $this->loadTemplate('footer');
-
-?>
+<?php echo $this->loadTemplate('footer'); ?>
