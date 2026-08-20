292a2a3d28c91977af92803e913ea16c93c9c8bb
diddipoeler <diddipoeler@gmx.de>
2026-08-20 09:12:58 +0200

    Make image deletion token-protected POST

--- admin/views/imagehandler/tmpl/default_image.php
+++ admin/views/imagehandler/tmpl/default_image.php
@@ -1,46 +1,37 @@
 <?php
-/**
- *
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage imagehandler
- * @file       default_image.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-
+/** One image in the SportsManagement administrator selector. */
 defined('_JEXEC') or die('Restricted access');
 
-use Joomla\CMS\Uri\Uri;
 use Joomla\CMS\Language\Text;
+use Joomla\CMS\Uri\Uri;
 
+$imageName = (string) ($this->_tmp_img->name ?? '');
+$imageUrl  = Uri::root() . 'images/com_sportsmanagement/database/'
+    . trim((string) $this->folder, '/') . '/' . rawurlencode($imageName);
+$selectFunction = 'selectImage_' . (string) $this->type;
+$selectJs = 'if (window.parent && typeof window.parent['
+    . json_encode($selectFunction, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES)
+    . '] === "function") { window.parent['
+    . json_encode($selectFunction, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES)
+    . ']('
+    . json_encode($imageName, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES) . ','
+    . json_encode($imageName, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES) . ','
+    . json_encode((string) $this->field, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES) . ','
+    . json_encode((string) $this->fieldid, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES)
+    . '); } return false;';
 ?>
-<div class="container-fluid">
-    <div align="center" class="imgBorder">
-        <a onclick="window.parent.selectImage_<?php echo $this->type; ?>('<?php echo $this->_tmp_img->name; ?>', '<?php echo $this->_tmp_img->name; ?>', '<?php echo $this->field; ?>', '<?php echo $this->fieldid; ?>');">
-            <div class="image">
-                <img src="<?php echo Uri::root(); ?>/images/com_sportsmanagement/database/<?php echo $this->folder; ?>/<?php echo $this->_tmp_img->name; ?>"
-                     width="<?php echo $this->_tmp_img->width_60; ?>" height="<?php echo $this->_tmp_img->height_60; ?>"
-                     alt="<?php echo $this->_tmp_img->name; ?> - <?php echo $this->_tmp_img->size; ?>"/>
-            </div>
-        </a>
-    </div>
-    <div class="controls">
-		<?php echo $this->_tmp_img->size; ?> -
-        <a class="delete-item"
-           href="index.php?option=com_sportsmanagement&amp;task=imagehandler.delete&amp;&amp;tmpl=component&amp;type=<?php echo $this->type; ?>&amp;rm[]=<?php echo $this->_tmp_img->name; ?>">
-            <img src="<?php echo Uri::root(); ?>/media/com_sportsmanagement/jl_images/publish_x.png" width="16"
-                 height="16" border="0"
-                 alt="<?php echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_IMAGEHANDLER_DEL_IMG'); ?>"/>
-        </a>
-    </div>
-    <div class="imageinfo">
-		<?php
-		echo $this->_tmp_img->name;
-
-		?>
-    </div>
+<div class="card p-2 text-center" style="width: 9rem;">
+    <a href="#" onclick="<?php echo $this->escape($selectJs); ?>">
+        <img src="<?php echo $this->escape($imageUrl); ?>"
+             width="<?php echo (int) ($this->_tmp_img->width_60 ?? 60); ?>"
+             height="<?php echo (int) ($this->_tmp_img->height_60 ?? 60); ?>"
+             alt="<?php echo $this->escape($imageName); ?>">
+    </a>
+    <div class="small mt-2"><?php echo $this->escape((string) ($this->_tmp_img->size ?? '')); ?></div>
+    <div class="small text-break"><?php echo $this->escape($imageName); ?></div>
+    <button type="submit" class="btn btn-sm btn-danger mt-2" name="rm[]"
+            value="<?php echo $this->escape($imageName); ?>"
+            onclick="this.form.task.value='imagehandler.delete';">
+        <?php echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_IMAGEHANDLER_DEL_IMG'); ?>
+    </button>
 </div>
