40579350f8fbc1d342d7c6b31ce42e98f7f57a1a
diddipoeler <diddipoeler@gmx.de>
2026-08-20 08:32:09 +0200

    Modernize image package import view

--- admin/views/smimageimports/view.html.php
+++ admin/views/smimageimports/view.html.php
@@ -1,71 +1,45 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage smimageimports
- * @file       view.html.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
+/** Native Joomla 5/6 administrator view for image-package imports. */
 defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\Language\Text;
-use Joomla\CMS\Factory;
+
 use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Language\Text;
 use Joomla\CMS\Toolbar\ToolbarHelper;
 
-/**
- * sportsmanagementViewsmimageimports
- *
- * @package
- * @author
- * @copyright diddi
- * @version   2014
- * @access    public
- */
 class sportsmanagementViewsmimageimports extends sportsmanagementView
 {
-	
-	/**
-	 * sportsmanagementViewsmimageimports::init()
-	 * 
-	 * @return void
-	 */
-	public function init()
-	{
-		$checkimages = $this->model->getimagesxml();
-		$this->files = $this->model->getXMLFiles();
+    public function init()
+    {
+        // Refresh and synchronize the package manifest before rendering the list.
+        $this->model->getimagesxml();
+        $this->model->getXMLFiles();
+        $this->items = $this->model->getItems();
+        $this->total = $this->model->getTotal();
+        $this->pagination = $this->model->getPagination();
 
-		$folders[]        = HTMLHelper::_('select.option', '', Text::_('COM_SPORTSMANAGEMENT_ADMIN_IMAGE_FOLDER'), 'id', 'name');
-		$allfolders       = $this->model->getXMLFolder();
-		$folders          = array_merge($folders, $allfolders);
-		$lists['folders'] = HTMLHelper::_(
-			'select.genericList',
-			$folders,
-			'filter_image_folder',
-			'class="inputbox" onChange="this.form.submit();" style="width:220px"',
-			'id',
-			'name',
-			$this->state->get('filter.image_folder')
-		);
+        $folders = [
+            HTMLHelper::_('select.option', '', Text::_('COM_SPORTSMANAGEMENT_ADMIN_IMAGE_FOLDER'), 'id', 'name'),
+        ];
+        $folders = array_merge($folders, $this->model->getXMLFolder());
+        $this->lists = [
+            'folders' => HTMLHelper::_(
+                'select.genericList',
+                $folders,
+                'filter_image_folder',
+                'class="form-select" onchange="this.form.submit();"',
+                'id',
+                'name',
+                (string) $this->state->get('filter.image_folder')
+            ),
+        ];
+    }
 
-		$this->lists = $lists;
-
-	}
-
-	/**
-	 * Add the page title and toolbar.
-	 *
-	 * @since 1.7
-	 */
-	protected function addToolbar()
-	{
-		$this->title = Text::_('COM_SPORTSMANAGEMENT_ADMIN_IMAGES_IMPORT');
-		$this->icon  = 'images-import';
-		ToolbarHelper::custom('smimageimports.import', 'upload', 'upload', Text::_('JTOOLBAR_UPLOAD'), false);
-		ToolbarHelper::divider();
+    protected function addToolbar()
+    {
+        $this->title = Text::_('COM_SPORTSMANAGEMENT_ADMIN_IMAGES_IMPORT');
+        $this->icon = 'images-import';
+        ToolbarHelper::custom('smimageimports.import', 'upload', 'upload', Text::_('JTOOLBAR_UPLOAD'), false);
+        ToolbarHelper::divider();
         parent::addToolbar();
-	}
-
+    }
 }
