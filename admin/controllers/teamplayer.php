65893a357a28f1cac7dd414653a9f847c8f5acc0
diddipoeler <diddipoeler@gmx.de>
2026-08-20 09:52:00 +0200

    Bridge legacy team player controller

--- admin/controllers/teamplayer.php
+++ admin/controllers/teamplayer.php
@@ -1,64 +1,14 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage controllers
- * @file       teamplayer.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: � 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
+/** Legacy compatibility bridge for the native administrator team player controller. */
 defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\Factory;
-use Joomla\CMS\Language\Text;
 
-/**
- * sportsmanagementControllerteamplayer
- *
- * @package
- * @author
- * @copyright diddi
- * @version   2014
- * @access    public
- */
-class sportsmanagementControllerteamplayer extends JSMControllerForm
-{
+use Diddipoeler\Component\SportsManagement\Administrator\Controller\TeamplayerController;
 
-   /**
-	 * sportsmanagementControllerteamplayer::__construct()
-	 *
-	 * @param   mixed  $config
-	 *
-	 * @return void
-	 */
-	function __construct($config = array())
-	{
-
-if ( Factory::getConfig()->get('debug') )
-{        
-Factory::getApplication()->enqueueMessage(Text::_(__METHOD__ . ' ' . ' ' . __LINE__ . ' ' . 'project_team_id<pre>'.print_r(Factory::getApplication()->input->getInt('project_team_id'),true).'</pre>'), 'notice');
-Factory::getApplication()->enqueueMessage(Text::_(__METHOD__ . ' ' . ' ' . __LINE__ . ' ' . 'season_team_id<pre>'.print_r(Factory::getApplication()->input->getInt('season_team_id'),true).'</pre>'), 'notice');
-Factory::getApplication()->enqueueMessage(Text::_(__METHOD__ . ' ' . ' ' . __LINE__ . ' ' . 'season_id<pre>'.print_r(Factory::getApplication()->input->getInt('season_id'),true).'</pre>'), 'notice');
-Factory::getApplication()->enqueueMessage(Text::_(__METHOD__ . ' ' . ' ' . __LINE__ . ' ' . 'person_id<pre>'.print_r(Factory::getApplication()->input->getInt('person_id'),true).'</pre>'), 'notice');
-Factory::getApplication()->enqueueMessage(Text::_(__METHOD__ . ' ' . ' ' . __LINE__ . ' ' . 'id<pre>'.print_r(Factory::getApplication()->input->getInt('id'),true).'</pre>'), 'notice');
-Factory::getApplication()->enqueueMessage(Text::_(__METHOD__ . ' ' . ' ' . __LINE__ . ' ' . 'pid<pre>'.print_r(Factory::getApplication()->input->getInt('pid'),true).'</pre>'), 'notice');
-Factory::getApplication()->enqueueMessage(Text::_(__METHOD__ . ' ' . ' ' . __LINE__ . ' ' . 'team_id<pre>'.print_r(Factory::getApplication()->input->getInt('team_id'),true).'</pre>'), 'notice');
-Factory::getApplication()->enqueueMessage(Text::_(__METHOD__ . ' ' . ' ' . __LINE__ . ' ' . 'persontype<pre>'.print_r(Factory::getApplication()->input->getInt('persontype'),true).'</pre>'), 'notice');
+if (!class_exists(TeamplayerController::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Controller/SportsManagementFormController.php';
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Controller/TeamplayerController.php';
 }
 
-Factory::getApplication()->setUserState("teamplayer.project_team_id", Factory::getApplication()->input->getInt('project_team_id'));
-Factory::getApplication()->setUserState("teamplayer.season_team_id", Factory::getApplication()->input->getInt('season_team_id'));
-Factory::getApplication()->setUserState("teamplayer.season_id", Factory::getApplication()->input->getInt('season_id'));
-Factory::getApplication()->setUserState("teamplayer.person_id", Factory::getApplication()->input->getInt('person_id'));
-Factory::getApplication()->setUserState("teamplayer.id", Factory::getApplication()->input->getInt('id'));
-Factory::getApplication()->setUserState("teamplayer.pid", Factory::getApplication()->input->getInt('pid'));
-Factory::getApplication()->setUserState("teamplayer.team_id", Factory::getApplication()->input->getInt('team_id'));
-Factory::getApplication()->setUserState("teamplayer.persontype", Factory::getApplication()->input->getInt('persontype'));
-
-
-       
-		parent::__construct($config);
-	}
-
+if (!class_exists('sportsmanagementControllerteamplayer', false)) {
+    class_alias(TeamplayerController::class, 'sportsmanagementControllerteamplayer');
 }
