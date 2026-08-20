3a0a560cd3ec34cad4c1e06a1eb3987aab5e4db1
diddipoeler <diddipoeler@gmx.de>
2026-08-20 09:52:49 +0200

    Modernize team players administrator view

--- admin/views/teamplayers/view.html.php
+++ admin/views/teamplayers/view.html.php
@@ -1,244 +1,131 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage teamplayers
- * @file       view.html.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
 defined('_JEXEC') or die('Restricted access');
+
+use Joomla\CMS\Component\ComponentHelper;
+use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\Layout\FileLayout;
-use Joomla\CMS\HTML\HTMLHelper;
-use Joomla\CMS\MVC\Model\BaseDatabaseModel;
-use Joomla\CMS\Table\Table;
-use Joomla\CMS\Factory;
-use Joomla\CMS\Toolbar\ToolbarHelper;
 use Joomla\CMS\Toolbar\Toolbar;
-use Joomla\CMS\Log\Log;
-use Joomla\CMS\Component\ComponentHelper;
+use Joomla\CMS\Toolbar\ToolbarHelper;
 
-/**
- * sportsmanagementViewteamplayers
- *
- * @package
- * @author
- * @copyright diddi
- * @version   2014
- * @access    public
- */
+/** Joomla 5/6 administrator roster view for team players and staff. */
 class sportsmanagementViewteamplayers extends sportsmanagementView
 {
+    public function init()
+    {
+        $this->state = $this->get('State');
+        $this->items = $this->get('Items') ?: [];
+        $this->pagination = $this->get('Pagination');
+        $this->total = $this->get('Total');
+        $this->sortDirection = (string) $this->state->get('list.direction', 'ASC');
+        $this->sortColumn = (string) $this->state->get('list.ordering', 'ppl.lastname');
 
-	/**
-	 * sportsmanagementViewteamplayers::init()
-	 *
-	 * @return void
-	 */
-	public function init()
-	{
+        $this->project_id = (int) $this->state->get('filter.pid', 0);
+        $this->project_team_id = (int) $this->state->get('filter.project_team_id', 0);
+        $this->team_id = (int) $this->state->get('filter.team_id', 0);
+        $this->season_team_id = (int) $this->state->get('filter.season_team_id', 0);
+        $this->season_id = (int) $this->state->get('filter.season_id', 0);
+        $this->_persontype = (int) $this->state->get('filter.persontype', 1);
+        $this->restartpage = false;
 
-		$this->restartpage = false;
+        $this->project = $this->model->getProjectContext();
+        $this->project_team = $this->model->getTeamContext();
 
-		$this->project_id  = $this->app->getUserState("$this->option.pid", '0');
-		$this->_persontype = $this->jinput->getVar('persontype');
+        if (!$this->project || !$this->project_team) {
+            $this->app->enqueueMessage(Text::_('JGLOBAL_NO_MATCHING_RESULTS'), 'warning');
+            $this->lists = ['project_position_id' => [], 'nation' => [], 'search_mode' => ''];
 
-		if (empty($this->_persontype))
-		{
-			$this->_persontype = $this->app->getUserState("$this->option.persontype", '0');
-		}
-		
-		$this->items = $this->model->getprojectpublished($this->items);
-		$this->items = $this->model->getprojectposition($this->items);
+            return;
+        }
 
-		$this->project_team_id = $this->jinput->getVar('project_team_id');
-		$this->team_id = $this->jinput->getInt('team_id');
-		$this->season_team_id = $this->jinput->getInt('season_team_id');
+        $positionOptions = [
+            HTMLHelper::_('select.option', 0, Text::_('COM_SPORTSMANAGEMENT_GLOBAL_SELECT_PLAYER_FUNCTION')),
+        ];
+        $positionOptions = array_merge($positionOptions, $this->model->getProjectPositionOptions());
 
-		if (!$this->team_id)
-		{
-			$this->team_id = $this->app->getUserState("$this->option.team_id", '0');
-		}
+        $countries = [
+            HTMLHelper::_('select.option', '', Text::_('COM_SPORTSMANAGEMENT_GLOBAL_SELECT_COUNTRY')),
+        ];
+        if (!class_exists('JSMCountries')) {
+            \JLoader::register(
+                'JSMCountries',
+                JPATH_SITE . '/components/com_sportsmanagement/helpers/countries.php'
+            );
+        }
+        if (class_exists('JSMCountries')) {
+            $options = \JSMCountries::getCountryOptions();
+            if ($options) {
+                $countries = array_merge($countries, $options);
+            }
+        }
 
-		if (!$this->project_team_id)
-		{
-			$this->project_team_id = $this->app->getUserState("$this->option.project_team_id", '0');
-		}
+        $this->lists = [
+            'project_position_id' => $positionOptions,
+            'nation' => $countries,
+            'search_mode' => '',
+        ];
+    }
 
-		$mdlProject = BaseDatabaseModel::getInstance('Project', 'sportsmanagementModel');
-		$project    = $mdlProject->getProject($this->project_id);
+    protected function addToolbar()
+    {
+        if (!$this->project || !$this->project_team) {
+            return;
+        }
 
-		$this->season_id = $project->season_id;
-		$items           = $this->model->PersonProjectPosition($this->project_id, $this->_persontype);
+        $teamName = (string) ($this->project_team->team_name ?? '');
+        $this->title = ($this->_persontype === 2
+            ? Text::_('COM_SPORTSMANAGEMENT_ADMIN_TSTAFFS_TITLE')
+            : Text::_('COM_SPORTSMANAGEMENT_ADMIN_TPLAYERS_TITLE')) . ' ' . $teamName;
 
-		if (!$items)
-		{
-			/**
-			 * fehlen im projekt die positionen ?
-			 * wenn ja, dann fehlende positionen hinzufügen
-			 */
-		}
-		else
-		{
-			$this->restartpage = false;
-		}
+        ToolbarHelper::back(
+            'COM_SPORTSMANAGEMENT_ADMIN_TPLAYERS_BACK',
+            'index.php?option=' . $this->option . '&view=projectteams&pid=' . $this->project_id
+        );
+        ToolbarHelper::apply('teamplayers.saveshort', Text::_('COM_SPORTSMANAGEMENT_ADMIN_TPLAYERS_APPLY'));
+        ToolbarHelper::publish('teamplayers.publish', 'JTOOLBAR_PUBLISH', true);
+        ToolbarHelper::unpublish('teamplayers.unpublish', 'JTOOLBAR_UNPUBLISH', true);
+        ToolbarHelper::deleteList('', 'teamplayers.delete');
+        ToolbarHelper::divider();
 
-		$this->table = Table::getInstance('teamplayer', 'sportsmanagementTable');
-$this->app->setUserState("$this->option.pid", $project->id);
-$this->app->setUserState("$this->option.season_id", $project->season_id);
-$this->app->setUserState("$this->option.project_art_id", $project->project_art_id);
-$this->app->setUserState("$this->option.sports_type_id", $project->sports_type_id);
-$this->app->setUserState("$this->option.project_id", $this->project_id);
-$this->app->setUserState("$this->option.persontype", $this->_persontype);
-$this->app->setUserState("$this->option.project_team_id", $this->project_team_id);
-$this->app->setUserState("$this->option.team_id", $this->team_id);
-$this->app->setUserState("$this->option.season_team_id", $this->season_team_id);
-		
-Factory::getApplication()->input->set("pid", $project->id);
-Factory::getApplication()->input->set("season_id", $project->season_id);
-Factory::getApplication()->input->set("project_art_id", $project->project_art_id);
-Factory::getApplication()->input->set("sports_type_id", $project->sports_type_id);
-Factory::getApplication()->input->set("project_id", $this->project_id);
-Factory::getApplication()->input->set("persontype", $this->_persontype);
-Factory::getApplication()->input->set("project_team_id", $this->project_team_id);
-Factory::getApplication()->input->set("team_id", $this->team_id);
-Factory::getApplication()->input->set("season_team_id", $this->season_team_id);		
-		
-		
-$values = array();
-$values[] = $project->id;
-$values[] = $project->season_id;
-$values[] = $project->project_art_id;
-$values[] = $project->sports_type_id;
-$values[] = $this->project_id;
-$values[] = $this->_persontype;
-$values[] = $this->project_team_id;
-$values[] = $this->team_id;
-$values[] = $this->season_team_id;
-Factory::getApplication()->input->set("teamplayers", $values);	
-		
-		
-// Get the cookie
-$value = Factory::getApplication()->input->cookie->get('teamplayers', null);
-// Set the cookie
-$time = time() + 604800; // 1 week
-Factory::getApplication()->input->cookie->set('teamplayers', implode(";", $values), $time, Factory::getApplication()->get('cookie_path', '/'), Factory::getApplication()->get('cookie_domain'), Factory::getApplication()->isSSLConnection());		
-		
-		
-		
-		$mdlProjectTeam = BaseDatabaseModel::getInstance('ProjectTeam', 'sportsmanagementModel');
-		$project_team   = $mdlProjectTeam->getProjectTeam($this->team_id);
+        $layout = new FileLayout('assignpersons', JPATH_ROOT . '/components/com_sportsmanagement/layouts');
+        Toolbar::getInstance('toolbar')->appendButton('Custom', $layout->render(), 'upload');
+        $this->renderAssignModal('collapseModalassignPersons', 'assignpersons', false);
 
-		/** build the html options for position */
-		$position_id   = array();
-		$position_id[] = HTMLHelper::_('select.option', '0', Text::_('COM_SPORTSMANAGEMENT_GLOBAL_SELECT_PLAYER_FUNCTION'));
-		$mdlPositions  = BaseDatabaseModel::getInstance('Positions', 'sportsmanagementModel');
+        if (ComponentHelper::getParams($this->option)->get('assign_club_position_to_player', 0)) {
+            $layout = new FileLayout('assignpersonsclub', JPATH_ROOT . '/components/com_sportsmanagement/layouts');
+            Toolbar::getInstance('toolbar')->appendButton('Custom', $layout->render(), 'upload');
+            $this->renderAssignModal('collapseModalassignPersonsClub', 'assignpersonsclub', true);
+        }
 
-		if ($this->_persontype == 1)
-		{
-			$project_ref_positions = $mdlPositions->getProjectPositions($this->project_id, $this->_persontype);
-		}
-		elseif ($this->_persontype == 2)
-		{
-			$project_ref_positions = $mdlPositions->getProjectPositions($this->project_id, $this->_persontype);
-		}
+        ToolbarHelper::apply(
+            'teamplayers.assignplayerscountry',
+            Text::_('COM_SPORTSMANAGEMENT_ADMIN_TPLAYERS_ASSIGN_COUNTRY')
+        );
+    }
 
-		if ($project_ref_positions)
-		{
-			$position_id = array_merge($position_id, $project_ref_positions);
-		}
+    private function renderAssignModal(string $id, string $layout, bool $assignClub): void
+    {
+        $query = [
+            'option' => 'com_sportsmanagement',
+            'view' => 'players',
+            'tmpl' => 'component',
+            'layout' => $layout,
+            'type' => $this->_persontype === 2 ? 1 : 0,
+            'pid' => $this->project_id,
+            'team_id' => $this->team_id,
+            'persontype' => $this->_persontype,
+            'season_id' => $this->season_id,
+            'whichview' => 'teamplayers',
+        ];
+        if ($assignClub) {
+            $query['assignclub'] = 1;
+        }
 
-		$lists                        = array();
-		$lists['project_position_id'] = $position_id;
-		unset($position_id);
-
-		/** build the html options for nation */
-		$nation[] = HTMLHelper::_('select.option', '0', Text::_('COM_SPORTSMANAGEMENT_GLOBAL_SELECT_COUNTRY'));
-
-		if ($res = JSMCountries::getCountryOptions())
-		{
-			$nation              = array_merge($nation, $res);
-			$this->search_nation = $res;
-		}
-
-		$lists['nation']    = $nation;
-		$this->lists        = $lists;
-		$this->project      = $project;
-		$this->project_team = $project_team;
-
-		if (!array_key_exists('search_mode', $this->lists))
-		{
-			$this->lists['search_mode'] = '';
-		}
-	}
-
-	/**
-	 * Add the page title and toolbar.
-	 *
-	 * @since 1.7
-	 */
-	protected function addToolbar()
-	{
-		$this->app->setUserState("$this->option.pid", $this->project_id);
-		$this->app->setUserState("$this->option.project_team_id", $this->project_team_id);
-		$this->app->setUserState("$this->option.team_id", $this->team_id);
-		$this->app->setUserState("$this->option.persontype", $this->_persontype);
-		$this->app->setUserState("$this->option.season_id", $this->season_id);
-
-		if ($this->_persontype == 1)
-		{
-			$this->title = Text::_('COM_SPORTSMANAGEMENT_ADMIN_TPLAYERS_TITLE') . ' ' . $this->project_team->name;
-		}
-		elseif ($this->_persontype == 2)
-		{
-			$this->title = Text::_('COM_SPORTSMANAGEMENT_ADMIN_TSTAFFS_TITLE') . ' ' . $this->project_team->name;
-		}
-
-		ToolbarHelper::back('COM_SPORTSMANAGEMENT_ADMIN_TPLAYERS_BACK', 'index.php?option=' . $this->option . '&view=projectteams&pid=' . $this->project_id . '&id=' . $this->project_id);
-
-		ToolbarHelper::apply('teamplayers.saveshort', Text::_('COM_SPORTSMANAGEMENT_ADMIN_TPLAYERS_APPLY'));
-		ToolbarHelper::divider();
-		//sportsmanagementHelper::ToolbarButton('assignpersons', 'upload', Text::_('COM_SPORTSMANAGEMENT_ADMIN_TPLAYERS_ASSIGN'), 'players', 0);
-		//sportsmanagementHelper::ToolbarButton('assignpersonsclub', 'upload', Text::_('COM_SPORTSMANAGEMENT_ADMIN_TPLAYERS_ASSIGN_CLUB'), 'players', 0);
-		$layout = new FileLayout('assignpersons', JPATH_ROOT . '/components/com_sportsmanagement/layouts');
-		$html   = $layout->render();
-		Toolbar::getInstance('toolbar')->appendButton('Custom', $html, 'upload');
-		$modal_params           = array();
-
-		$zusatz = '&team_id=' . $this->team_id;
-		$zusatz .= '&persontype=' . $this->_persontype;
-		$zusatz .= '&season_id=' . $this->season_id;
-		$zusatz .= '&whichview=teamplayers';
-
-		$modal_params['url']    = 'index.php?option=com_sportsmanagement&view=players&tmpl=component&layout=assignpersons&type=0&issueview=&issuelayout=' . $zusatz;
-		$modal_params['height'] = $this->modalheight;
-		$modal_params['width']  = $this->modalwidth;
-		$modal_params['modalWidth']  = '60';
-		echo HTMLHelper::_('bootstrap.renderModal', 'collapseModalassignPersons', $modal_params);
-
-if ( ComponentHelper::getParams($this->option)->get('assign_club_position_to_player', 0) )
-{
-		$layout = new FileLayout('assignpersonsclub', JPATH_ROOT . '/components/com_sportsmanagement/layouts');
-		$html   = $layout->render();
-		Toolbar::getInstance('toolbar')->appendButton('Custom', $html, 'upload');
-		$modal_params           = array();
-		$zusatz .= '&whichview=teamplayers&assignclub=1';
-		$modal_params['url']    = 'index.php?option=com_sportsmanagement&view=players&tmpl=component&layout=assignpersons&type=0&issueview=&issuelayout=' . $zusatz;
-		$modal_params['height'] = $this->modalheight;
-		$modal_params['width']  = $this->modalwidth;
-		$modal_params['modalWidth']  = '60';
-		echo HTMLHelper::_('bootstrap.renderModal', 'collapseModalassignPersonsClub', $modal_params);
-}		
-        
-        
-		ToolbarHelper::apply('teamplayers.assignplayerscountry', Text::_('COM_SPORTSMANAGEMENT_ADMIN_TPLAYERS_ASSIGN_COUNTRY'));
-		ToolbarHelper::divider();
-		
-		parent::addToolbar();
-	}
-
+        echo HTMLHelper::_('bootstrap.renderModal', $id, [
+            'url' => 'index.php?' . http_build_query($query),
+            'height' => $this->modalheight,
+            'width' => $this->modalwidth,
+            'modalWidth' => '60',
+        ]);
+    }
 }
-
