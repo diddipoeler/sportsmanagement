c89602a6da4034865581cf3168bceb3df7e00fa5
diddipoeler <diddipoeler@gmx.de>
2026-08-20 09:31:03 +0200

    Modernize player administrator view

--- admin/views/player/view.html.php
+++ admin/views/player/view.html.php
@@ -1,120 +1,69 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage player
- * @file       view.html.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
+/** Joomla 5/6 administrator player view. */
 defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\Uri\Uri;
-use Joomla\CMS\Language\Text;
-use Joomla\CMS\Environment\Browser;
-use Joomla\CMS\Factory;
 
-/**
- * sportsmanagementViewplayer
- *
- * @package
- * @author    Dieter Plöger
- * @copyright 2019
- * @version   $Id$
- * @access    public
- */
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Uri\Uri;
+
 class sportsmanagementViewplayer extends sportsmanagementView
 {
+    public function init()
+    {
+        $this->item->name = $this->item->lastname . ' - ' . $this->item->firstname;
 
-	/**
-	 * sportsmanagementViewplayer::init()
-	 *
-	 * @return void
-	 */
-	public function init()
-	{
+        foreach (['sports_type_id', 'position_id', 'agegroup_id', 'person_art', 'person_id1', 'person_id2'] as $field) {
+            $this->form->setValue($field, 'request', $this->item->{$field} ?? null);
+        }
 
-		/**
-		 * name für den titel setzen
-		 */
-		$this->item->name = $this->item->lastname . ' - ' . $this->item->firstname;
+        if ((float) ($this->item->latitude ?? 0) === 255.0) {
+            $this->app->enqueueMessage(Text::_('COM_SPORTSMANAGEMENT_NO_GEOCODE'), 'error');
+            $this->map = false;
+        } else {
+            $this->map = true;
+        }
 
-		$this->form->setValue('sports_type_id', 'request', $this->item->sports_type_id);
-		$this->form->setValue('position_id', 'request', $this->item->position_id);
-		$this->form->setValue('agegroup_id', 'request', $this->item->agegroup_id);
+        foreach (['birthday', 'deathday', 'injury_date_start', 'injury_date_end', 'susp_date_start', 'susp_date_end', 'away_date_start', 'away_date_end'] as $field) {
+            if (($this->item->{$field} ?? '') === '0000-00-00') {
+                $this->item->{$field} = '';
+                $this->form->setValue($field, null, '');
+            }
+        }
 
-		$this->form->setValue('person_art', 'request', $this->item->person_art);
-		$this->form->setValue('person_id1', 'request', $this->item->person_id1);
-		$this->form->setValue('person_id2', 'request', $this->item->person_id2);
+        $this->extended = sportsmanagementHelper::getExtended($this->item->extended ?? '', 'player');
+        $this->extendeduser = sportsmanagementHelper::getExtendedUser($this->item->extendeduser ?? '', 'player');
+        $this->checkextrafields = sportsmanagementHelper::checkUserExtraFields('backend', 0, 'player');
+        $this->lists = [];
 
-		if ($this->item->latitude == 255) {
-			$this->app->enqueueMessage(Text::_('COM_SPORTSMANAGEMENT_NO_GEOCODE'), 'Error');
-			$this->map = false;
-		} else {
-			$this->map = true;
-		}
+        if ($this->checkextrafields) {
+            $this->lists['ext_fields'] = sportsmanagementHelper::getUserExtraFields((int) $this->item->id, 'backend', 0, 'player');
+        }
 
-		$isNew = $this->item->id == 0;
+        $personAge = sportsmanagementHelper::getAge(
+            $this->form->getValue('birthday'),
+            $this->form->getValue('deathday')
+        );
+        $personRange = $this->model->getAgeGroupID($personAge);
 
-		$fields = array('birthday', 'deathday', 'injury_date_start', 'injury_date_end', 'susp_date_start', 'susp_date_end', 'away_date_start', 'away_date_end');
-		foreach ($fields as $field) {
-			if ($this->item->$field == '0000-00-00') {
-				$this->item->$field = '';
-				$this->form->setValue($field, null, '');
-			}
-		}
+        if ($personRange) {
+            $this->form->setValue('agegroup_id', null, $personRange);
+        }
 
-		$extended = sportsmanagementHelper::getExtended($this->item->extended, 'player');
-		$this->extended = $extended;
-		$extendeduser = sportsmanagementHelper::getExtendedUser($this->item->extendeduser, 'player');
-		$this->extendeduser = $extendeduser;
-		$this->checkextrafields = sportsmanagementHelper::checkUserExtraFields('backend', 0, Factory::getApplication()->input->get('view'));
-		$lists = array();
+        $this->document->addScript(Uri::base() . 'components/' . $this->option . '/assets/js/sm_functions.js');
+        $this->document->addScript(Uri::base() . 'components/' . $this->option . '/assets/js/editgeocode.js');
 
-		if ($this->checkextrafields) {
-			$lists['ext_fields'] = sportsmanagementHelper::getUserExtraFields($this->item->id, 'backend', 0, Factory::getApplication()->input->get('view'));
-		}
+        $language = $this->app->getLanguage();
+        $language->load('com_contact', JPATH_ADMINISTRATOR, 'en-GB', true);
+        $language->load('com_contact', JPATH_ADMINISTRATOR, $language->getDefault(), true);
+        $language->load('com_contact', JPATH_ADMINISTRATOR, null, true);
+    }
 
-		$this->lists = $lists;
-		unset($lists);
-
-		$person_age = sportsmanagementHelper::getAge($this->form->getValue('birthday'), $this->form->getValue('deathday'));
-		$person_range = $this->model->getAgeGroupID($person_age);
-
-		if ($person_range) {
-			$this->form->setValue('agegroup_id', null, $person_range);
-		}
-
-		$this->document->addScript(Uri::base() . 'components/' . $this->option . '/assets/js/sm_functions.js');
-
-		/**
-		 * Load the language files for the contact integration
-		 */
-		$jlang = Factory::getLanguage();
-		$jlang->load('com_contact', JPATH_ADMINISTRATOR, 'en-GB', true);
-		$jlang->load('com_contact', JPATH_ADMINISTRATOR, $jlang->getDefault(), true);
-		$jlang->load('com_contact', JPATH_ADMINISTRATOR, null, true);
-
-		//$this->document->addScript('https://maps.googleapis.com/maps/api/js?libraries=places&language=de');
-		//$this->document->addScript(Uri::base() . 'components/' . $this->option . '/assets/js/geocomplete.js');
-		//$this->document->addScript(Uri::base() . 'components/' . $this->option . '/views/person/tmpl/edit.js');
-		$this->document->addScript(Uri::base() . 'components/' . $this->option . '/assets/js/editgeocode.js');
-
-	}
-
-	/**
-	 * sportsmanagementViewplayer::addToolBar()
-	 *
-	 * @return void
-	 */
-	protected function addToolBar()
-	{
-		$this->jinput->set('hidemainmenu', true);
-		$isNew = $this->item->id ? $this->title = Text::_('COM_SPORTSMANAGEMENT_ADMIN_PERSON_EDIT') : $this->title = Text::_('COM_SPORTSMANAGEMENT_ADMIN_PERSON_NEW');
-		$this->icon = 'person';
-		parent::addToolbar();
-
-	}
-
+    protected function addToolBar()
+    {
+        $this->app->getInput()->set('hidemainmenu', true);
+        $this->title = $this->item->id
+            ? Text::_('COM_SPORTSMANAGEMENT_ADMIN_PERSON_EDIT')
+            : Text::_('COM_SPORTSMANAGEMENT_ADMIN_PERSON_NEW');
+        $this->icon = 'person';
+        parent::addToolbar();
+    }
 }
