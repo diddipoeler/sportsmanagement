39800714f8470129456670450a8940a4004a9369
diddipoeler <diddipoeler@gmx.de>
2026-08-20 09:14:00 +0200

    Remove Joomla 3 branches from image browser item

--- admin/views/imagelist/tmpl/default_image.php
+++ admin/views/imagelist/tmpl/default_image.php
@@ -1,101 +1,33 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage imagelist
- * @file       default_imagelist.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
+/** One image in the SportsManagement administrator image browser. */
 defined('_JEXEC') or die;
-use Joomla\Registry\Registry;
+
+use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\Uri\Uri;
-use Joomla\CMS\Factory;
-use Joomla\CMS\HTML\HTMLHelper;
 
-$params     = new Registry;
-
-// Get the base version
-$baseVersion = substr(JVERSION, 0, 3);
-if (version_compare($baseVersion, '4.0', 'ge'))
-{
-//$dispatcher = Factory::getApplication()->triggerEvent();
-}
-elseif (version_compare($baseVersion, '3.0', 'ge'))
-{
-$dispatcher = JEventDispatcher::getInstance();
-$dispatcher->trigger('onContentBeforeDisplay', array('com_media.file', &$this->_tmp_img, &$params));
-}
-//$dispatcher->trigger('onContentBeforeDisplay', array('com_media.file', &$this->_tmp_img, &$params));
-
-//echo '<pre>'.print_r($this->_tmp_img,true).'</pre>';
-
+$file = (string) ($this->_tmp_img->file ?? '');
+$name = (string) ($this->_tmp_img->name ?? $file);
+$imageUrl = Uri::root() . 'images/com_sportsmanagement/database/'
+    . trim((string) ($this->_tmp_img->path_relative ?? ''), '/') . '/' . rawurlencode($file);
+$onclick = 'exportToForm(' . json_encode($file, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES) . '); return false;';
 ?>
+<div class="media-browser-item card p-2 text-center">
+    <a href="<?php echo $this->escape($imageUrl); ?>" target="_blank" rel="noopener">
+        <img src="<?php echo $this->escape($imageUrl); ?>" alt="<?php echo $this->escape($name); ?>"
+             width="60" height="60" loading="lazy">
+    </a>
+    <div class="small text-break mt-1">
+        <?php echo Text::sprintf(
+            'COM_MEDIA_IMAGE_TITLE',
+            HTMLHelper::_('string.truncate', $name, 24, false),
+            HTMLHelper::_('number.bytes', (int) ($this->_tmp_img->size ?? 0))
+        ); ?>
+    </div>
 
-<script>
-
-</script>
-
-<li class="imgOutline thumbnail height-80 width-80 center">
-<div class="height-50">
-<?php
-
-echo sportsmanagementHelper::getBootstrapModalImage(
-$this->_tmp_img->name,
-Uri::root() . 'images/com_sportsmanagement/database/'.$this->_tmp_img->path_relative.'/'.$this->_tmp_img->file,
-Text::_($this->_tmp_img->name),
-$this->_tmp_img->width_60,
-'',
-$this->modalwidth,
-$this->modalheight
-);
-
-//switch ( $this->folder )
-//{
-//    case 'rosterground':
-//    break;
-//    default:
-//    $image_attributes = array();
-//    $image_attributes['title'] = Text::_('COM_SPORTSMANAGEMENT_GLOBAL_ADD');
-//    $image_attributes['id'] = $this->_tmp_img->file;		  
-//    $image_attributes['onclick'] = "javascript:exportToForm('".$this->_tmp_img->file."')";
-//    echo HTMLHelper::_('image', 'administrator/components/com_sportsmanagement/assets/images/ok.png', '', $image_attributes);
-//    break;
-//}
-	  
-?>
+    <?php if ($this->folder !== 'rosterground') : ?>
+        <button type="button" class="btn btn-sm btn-primary mt-2" onclick="<?php echo $this->escape($onclick); ?>">
+            <?php echo Text::_('COM_SPORTSMANAGEMENT_GLOBAL_ADD'); ?>
+        </button>
+    <?php endif; ?>
 </div>
-<div class="small">
-<?php
-echo Text::sprintf('COM_MEDIA_IMAGE_TITLE', HTMLHelper::_('string.truncate', $this->_tmp_img->name, 10, false), HTMLHelper::_('number.bytes', $this->_tmp_img->size));
-?>
-</div> 
-
-<?php
-switch ( $this->folder )
-{
-    case 'rosterground':
-    break;
-    default:
-?>
-<div class="small">  
-<button onclick="exportToForm('<?php echo $this->_tmp_img->file;?> ')"><?php echo Text::_('COM_SPORTSMANAGEMENT_GLOBAL_ADD'); ?></button>
-</div> 
-<?php
- break;
-} 
-?> 
-</li>
-          
-<?php
-if (version_compare($baseVersion, '4.0', 'ge'))
-{
-//$dispatcher = Factory::getApplication()->triggerEvent();
-}
-elseif (version_compare($baseVersion, '3.0', 'ge'))
-{
-$dispatcher->trigger('onContentAfterDisplay', array('com_media.file', &$this->_tmp_img, &$params));
-}
