eb577566ea5592c3c40677b572fb7b9834abeda1
diddipoeler <diddipoeler@gmx.de>
2026-08-20 22:44:11 +0200

    Move individual match generation UI to delegated Joomla 5/6 runtime

--- admin/tmpl/jlextindividualsportes/default_generate.php
+++ admin/tmpl/jlextindividualsportes/default_generate.php
@@ -1,119 +1,82 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage jlextindividualsportes
- * @file       default_generate.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
+/** Joomla 5/6 individual-sport pairing generator. */
 defined('_JEXEC') or die('Restricted access');
+
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
 
-if ($this->debug)
-{
-echo 'homeplayers<pre>'.print_r($this->homeplayers,true).'</pre>';
-echo 'awayplayers<pre>'.print_r($this->awayplayers,true).'</pre>';
-echo 'show_matches<pre>'.print_r($this->show_matches,true).'</pre>';
+$homeById = [];
+foreach ($this->homeplayers as $player) {
+    $homeById[(int) $player->season_team_person_id] = $player;
 }
 
+$awayById = [];
+foreach ($this->awayplayers as $player) {
+    $awayById[(int) $player->season_team_person_id] = $player;
+}
 ?>
+<?php if ($this->debug) : ?>
+    <details>
+        <summary>Individual match generator debug</summary>
+        <pre><?php echo htmlspecialchars(print_r([
+            'homeplayers' => $this->homeplayers,
+            'awayplayers' => $this->awayplayers,
+            'show_matches' => $this->show_matches,
+        ], true), ENT_QUOTES, 'UTF-8'); ?></pre>
+    </details>
+<?php endif; ?>
+
 <div class="table-responsive" id="editcell">
-<!-- Start games list -->
-<form action="<?php echo $this->request_url; ?>" method="post" name="adminForm" id='adminForm'>
-<fieldset>
-<div class="fltlft">
-<button type="button" onclick="Joomla.submitform('jlextindividualsportes.generatematchsingles', this.form);">
-<?php echo Text::_('JAPPLY'); ?></button>
-</div>
-</fieldset>
-            
-<table class="table">
-<?php
-foreach ( $this->show_matches as $count_i => $item )
-{
-    
-$match_type = ( $item->teamplayer1_position == 'Double' || $item->teamplayer2_position  == 'Double' ) ? 'DOUBLE' : 'SINGLE';     
-    
-?>       
-<tr>     
-<td>
+    <form action="<?php echo $this->request_url; ?>" method="post" name="adminForm" id="adminForm">
+        <fieldset>
+            <div class="fltlft">
+                <button type="button" data-jsm-action="submit-task" data-task="jlextindividualsportes.generatematchsingles">
+                    <?php echo Text::_('JAPPLY'); ?>
+                </button>
+            </div>
+        </fieldset>
 
-<input type="hidden" name="match_type[]" value="<?php echo $match_type; ?>" />
+        <table class="table">
+            <tbody>
+                <?php foreach ($this->show_matches as $item) : ?>
+                    <?php
+                    $homeId = (int) $item->teamplayer1_id;
+                    $awayId = (int) $item->teamplayer2_id;
+                    $home = $homeById[$homeId] ?? null;
+                    $away = $awayById[$awayId] ?? null;
+                    $matchType = ($item->teamplayer1_position === 'Double' || $item->teamplayer2_position === 'Double') ? 'DOUBLE' : 'SINGLE';
+                    ?>
+                    <tr>
+                        <td>
+                            <input type="hidden" name="match_type[]" value="<?php echo $matchType; ?>">
+                            <?php echo $item->teamplayer1_position; ?>
+                        </td>
+                        <td><?php echo $item->teamplayer2_position; ?></td>
+                        <?php if ($home) : ?>
+                            <td>
+                                <input type="hidden" name="teamplayer1_id[]" value="<?php echo $homeId; ?>">
+                                <?php echo $home->lastname; ?>
+                            </td>
+                            <td><?php echo $home->firstname; ?></td>
+                        <?php endif; ?>
+                        <?php if ($away) : ?>
+                            <td>
+                                <input type="hidden" name="teamplayer2_id[]" value="<?php echo $awayId; ?>">
+                                <?php echo $away->lastname; ?>
+                            </td>
+                            <td><?php echo $away->firstname; ?></td>
+                        <?php endif; ?>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
 
-
-<?php
-echo $item->teamplayer1_position;
-?>
-</td>
-<td>
-<?php
-echo $item->teamplayer2_position;
-?>
-</td>
-
-
-<?php
-foreach ( $this->homeplayers as $count_home => $home )
-{
-   
-if ( $home->season_team_person_id == $item->teamplayer1_id )
-{
-?>
-<td>
-<input type="hidden" name="teamplayer1_id[]" value="<?php echo $home->season_team_person_id; ?>" />
-
-<?php
-echo $home->lastname;
-?>
-</td>
-<td>
-<?php
-echo $home->firstname;
-?>
-</td>
-<?php    
-}       
-       
-}
-
-foreach ( $this->awayplayers as $count_away => $away )
-{
-if ( $away->season_team_person_id == $item->teamplayer2_id )
-{
-?>
-<td>
-<input type="hidden" name="teamplayer2_id[]" value="<?php echo $away->season_team_person_id; ?>" />
-<?php
-echo $away->lastname;
-?>
-</td>
-<td>
-<?php
-echo $away->firstname;
-?>
-</td>
-<?php    
-}       
-       
-       
-}	   
-?>       
-</tr>  
-<?php       
-}
-?>
-</table>
-<input type='hidden' name='project_id' value='<?php echo $this->pid; ?>'/>
-<input type='hidden' name='match_id' value='<?php echo $this->id; ?>'/>
-<input type='hidden' name='projectteam1_id' value='<?php echo $this->projectteam1_id; ?>'/>
-<input type='hidden' name='projectteam2_id' value='<?php echo $this->projectteam2_id; ?>'/>
-<input type='hidden' name='round_id' value='<?php echo $this->rid; ?>'/>
-
-<input type='hidden' name='task' value='' id='task'/>
-<?php echo HTMLHelper::_('form.token') . "\n"; ?>
-</form>
+        <input type="hidden" name="project_id" value="<?php echo $this->pid; ?>">
+        <input type="hidden" name="match_id" value="<?php echo $this->id; ?>">
+        <input type="hidden" name="projectteam1_id" value="<?php echo $this->projectteam1_id; ?>">
+        <input type="hidden" name="projectteam2_id" value="<?php echo $this->projectteam2_id; ?>">
+        <input type="hidden" name="round_id" value="<?php echo $this->rid; ?>">
+        <input type="hidden" name="task" value="" id="task">
+        <?php echo HTMLHelper::_('form.token') . "\n"; ?>
+    </form>
 </div>
