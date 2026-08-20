e6d3e5a648b4141d2e8a4bdcf6d9bab203e88a11
diddipoeler <diddipoeler@gmx.de>
2026-08-20 09:30:31 +0200

    Bridge legacy player controller

--- admin/controllers/player.php
+++ admin/controllers/player.php
@@ -1,57 +1,14 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage controllers
- * @file       player.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
+/** Legacy compatibility bridge for the native administrator player controller. */
 defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\Router\Route;
-use Joomla\CMS\Language\Text;
 
+use Diddipoeler\Component\SportsManagement\Administrator\Controller\PlayerController;
 
-/**
- * sportsmanagementControllerplayer
- *
- * @package
- * @author    Dieter Plöger
- * @copyright 2019
- * @version   $Id$
- * @access    public
- */
-class sportsmanagementControllerplayer extends JSMControllerForm
-{
+if (!class_exists(PlayerController::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Controller/SportsManagementFormController.php';
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Controller/PlayerController.php';
+}
 
-	/**
-	 * sportsmanagementControllerplayer::__construct()
-	 *
-	 * @param   mixed  $config
-	 *
-	 * @return void
-	 */
-	function __construct($config = array())
-	{
-		parent::__construct($config);
-	}
-    
-    
-    
-    /**
-     * sportsmanagementControllerplayer::import()
-     * 
-     * @return void
-     */
-    function import()
-    {
-    //      $this->jsmapp->enqueueMessage(Text::_(__METHOD__.' '.__LINE__.' '.$this->view_list), '');
-    $this->setRedirect(Route::_('index.php?option=' . $this->option . '&view=' . $this->view_list.'&layout=players_upload', false));
-    
-        
-        
-    }
-
+if (!class_exists('sportsmanagementControllerplayer', false)) {
+    class_alias(PlayerController::class, 'sportsmanagementControllerplayer');
 }
