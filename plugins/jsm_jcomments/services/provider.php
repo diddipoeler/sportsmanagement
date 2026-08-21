0eedba297acd521644f86ec89fb22684cf44f1c2
diddipoeler <diddipoeler@gmx.de>
2026-08-21 17:57:44 +0200

    Inject event dispatcher into JComments plugin

--- plugins/jsm_jcomments/services/provider.php
+++ plugins/jsm_jcomments/services/provider.php
@@ -7,14 +7,17 @@ use Joomla\CMS\Factory;
 use Joomla\CMS\Plugin\PluginHelper;
 use Joomla\DI\Container;
 use Joomla\DI\ServiceProviderInterface;
+use Joomla\Event\DispatcherInterface;
 
-return new class () implements ServiceProviderInterface {
+return new class () implements ServiceProviderInterface
+{
     public function register(Container $container): void
     {
         $container->set(
             PluginInterface::class,
             static function (Container $container): PluginInterface {
                 $plugin = new SportsmanagementComments(
+                    $container->get(DispatcherInterface::class),
                     (array) PluginHelper::getPlugin('content', 'sportsmanagement_comments')
                 );
                 $plugin->setApplication(Factory::getApplication());
