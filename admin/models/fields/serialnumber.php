8a96cff6d3bb33313c2f5bf445a9e4216b8f102e
diddipoeler <diddipoeler@gmx.de>
2026-08-21 21:13:52 +0200

    Bridge serial number field to native class

--- admin/models/fields/serialnumber.php
+++ admin/models/fields/serialnumber.php
@@ -1,63 +1,12 @@
 <?php
-/**
- *
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage fields
- * @file       serialnumber.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
+\defined('_JEXEC') or die;
 
-defined('_JEXEC') or die;
+use Diddipoeler\Component\SportsManagement\Administrator\Field\SerialnumberField;
 
-use Joomla\CMS\Factory;
-use Joomla\CMS\MVC\Model\BaseDatabaseModel;
-use Joomla\CMS\Form\FormField;
-
-if (!class_exists('sportsmanagementHelper'))
-{
-	/**
-	 * add the classes for handling
-	 */
-	$classpath = JPATH_ADMINISTRATOR . DIRECTORY_SEPARATOR . 'components/com_sportsmanagement' . DIRECTORY_SEPARATOR . 'helpers' . DIRECTORY_SEPARATOR . 'sportsmanagement.php';
-	JLoader::register('sportsmanagementHelper', $classpath);
+if (!class_exists(SerialnumberField::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Field/SerialnumberField.php';
 }
 
-/**
- * JFormFieldserialnumber
- *
- * @package
- * @author    Dieter Plöger
- * @copyright 2019
- * @version   $Id$
- * @access    public
- */
-class JFormFieldserialnumber extends FormField
-{
-	public $type = 'serialnumber';
-
-	/**
-	 * JFormFieldserialnumber::getInput()
-	 *
-	 * @return
-	 */
-	protected function getInput()
-	{
-		$app = Factory::getApplication();
-
-		if (!$this->value)
-		{
-			$this->value = sportsmanagementHelper::jsmsernum();
-		}
-
-		$html = '<input type="text" id="' . $this->id . '" name="' . $this->name . '" value="' . $this->value . '" />';
-
-		return $html;
-
-	}
-
+if (!class_exists('JFormFieldserialnumber', false)) {
+    class_alias(SerialnumberField::class, 'JFormFieldserialnumber');
 }
