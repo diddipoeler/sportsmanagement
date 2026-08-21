e51f92957faf64476a19252844ccf0701cb5da3e
diddipoeler <diddipoeler@gmx.de>
2026-08-21 19:05:43 +0200

    Use namespaced club birthday helper in legacy entry

--- modules/mod_sportsmanagement_club_birthday/mod_sportsmanagement_club_birthday.php
+++ modules/mod_sportsmanagement_club_birthday/mod_sportsmanagement_club_birthday.php
@@ -1,15 +1,25 @@
 <?php
-/** Joomla 5/6 runtime entry point for club anniversaries. */
-defined('_JEXEC') or die;
+/**
+ * Legacy entry bridge for the Joomla 5/6 SportsManagement club birthday module.
+ *
+ * The active implementation is loaded through services/provider.php.
+ */
+\defined('_JEXEC') or die;
 
+use Diddipoeler\Module\SportsManagementClubBirthday\Site\Helper\ClubBirthdayHelper;
 use Joomla\CMS\Factory;
 use Joomla\CMS\Helper\ModuleHelper;
+use Joomla\Database\DatabaseInterface;
 
-require_once __DIR__ . '/helper.php';
+if (!class_exists(ClubBirthdayHelper::class)) {
+    require_once __DIR__ . '/src/Helper/ClubBirthdayHelper.php';
+}
 
 $app = Factory::getApplication();
 $app->getLanguage()->load('com_sportsmanagement', JPATH_ADMINISTRATOR, null, true);
-$result = modSportsmanagementClubBirthdayHelper::getData($params);
+/** @var DatabaseInterface $database */
+$database = Factory::getContainer()->get(DatabaseInterface::class);
+$result = (new ClubBirthdayHelper())->getData($params, $app, $database);
 $clubs = $result['clubs'];
 $mode = $result['mode'];
 
