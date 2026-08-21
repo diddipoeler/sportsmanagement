5a6c694ccc210056a76f0d37352e15edf8b9d1e0
diddipoeler <diddipoeler@gmx.de>
2026-08-21 17:42:58 +0200

    Add Joomla 5/6 service provider for Google Calendar module

---
+++ modules/mod_sportsmanagement_google_calendar/services/provider.php
@@ -0,0 +1,21 @@
+<?php
+\defined('_JEXEC') or die;
+
+use Joomla\CMS\Extension\Service\Provider\HelperFactory;
+use Joomla\CMS\Extension\Service\Provider\Module;
+use Joomla\CMS\Extension\Service\Provider\ModuleDispatcherFactory;
+use Joomla\DI\Container;
+use Joomla\DI\ServiceProviderInterface;
+
+return new class () implements ServiceProviderInterface {
+    public function register(Container $container): void
+    {
+        $container->registerServiceProvider(
+            new ModuleDispatcherFactory('\\Diddipoeler\\Module\\SportsManagementGoogleCalendar')
+        );
+        $container->registerServiceProvider(
+            new HelperFactory('\\Diddipoeler\\Module\\SportsManagementGoogleCalendar\\Site\\Helper')
+        );
+        $container->registerServiceProvider(new Module());
+    }
+};
