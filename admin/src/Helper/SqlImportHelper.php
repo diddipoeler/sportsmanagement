36608a9cfd93e42b9366aeeb764bbb830bac4d7e
diddipoeler <diddipoeler@gmx.de>
2026-08-21 20:04:57 +0200

    Add native SQL import helper

---
+++ admin/src/Helper/SqlImportHelper.php
@@ -0,0 +1,38 @@
+<?php
+namespace Diddipoeler\Component\SportsManagement\Administrator\Helper;
+
+\defined('_JEXEC') or die;
+
+use Joomla\CMS\Installer\Installer;
+use Joomla\Database\DatabaseInterface;
+
+/** Execute bundled SportsManagement SQL files with Joomla's SQL splitter. */
+final class SqlImportHelper
+{
+    public static function importFile(DatabaseInterface $db, string $sqlFile): void
+    {
+        if (!is_file($sqlFile) || !is_readable($sqlFile)) {
+            throw new \RuntimeException('SQL import file is not readable: ' . basename($sqlFile));
+        }
+
+        $sql = file_get_contents($sqlFile);
+
+        if ($sql === false) {
+            throw new \RuntimeException('Unable to read SQL import file: ' . basename($sqlFile));
+        }
+
+        foreach (Installer::splitSql($sql) as $statement) {
+            if (is_array($statement)) {
+                $statement = $statement['query'] ?? '';
+            }
+
+            $statement = trim((string) $statement);
+
+            if ($statement === '') {
+                continue;
+            }
+
+            $db->setQuery($statement)->execute();
+        }
+    }
+}
