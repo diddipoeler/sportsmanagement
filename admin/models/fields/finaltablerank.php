36d62f889f4937f4a125e47b60206a2d30a83802
diddipoeler <diddipoeler@gmx.de>
2026-08-21 21:12:19 +0200

    Bridge final table rank field to native class

--- admin/models/fields/finaltablerank.php
+++ admin/models/fields/finaltablerank.php
@@ -1,48 +1,12 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage fields
- * @file       finaltablerank.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
+\defined('_JEXEC') or die;
 
-defined('_JEXEC') or die('Restricted access');
+use Diddipoeler\Component\SportsManagement\Administrator\Field\FinaltablerankField;
 
-use Joomla\CMS\Form\Field\ListField;
-use Joomla\CMS\HTML\HTMLHelper;
-use Joomla\CMS\Language\Text;
+if (!class_exists(FinaltablerankField::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Field/FinaltablerankField.php';
+}
 
-/**
- * FormFieldfinaltablerank
- *
- * @package
- * @author    Dieter Plöger
- * @copyright 2017
- * @version   $Id$
- * @access    public
- */
-class JFormFieldfinaltablerank extends ListField
-{
-	public $type = 'finaltablerank';
-
-	/**
-	 * Method to get the field options.
-	 *
-	 * @return array
-	 */
-	protected function getOptions()
-	{
-		$options = array();
-
-		for ($rank = 1; $rank < 41; $rank++)
-		{
-			$options[] = HTMLHelper::_('select.option', $rank, Text::_('COM_SPORTSMANAGEMENT_ADMIN_PROJECTTEAM_FINALTABLERANK') . ' - ' . $rank);
-		}
-
-		return array_merge(parent::getOptions(), $options);
-	}
+if (!class_exists('JFormFieldfinaltablerank', false)) {
+    class_alias(FinaltablerankField::class, 'JFormFieldfinaltablerank');
 }
