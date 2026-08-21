3c5ae1be8d80a5d9293f0852e372f4338898d5db
diddipoeler <diddipoeler@gmx.de>
2026-08-21 21:05:41 +0200

    Add native matchday list field

---
+++ admin/src/Field/MatchdaylistField.php
@@ -0,0 +1,53 @@
+<?php
+namespace Diddipoeler\Component\SportsManagement\Administrator\Field;
+
+\defined('_JEXEC') or die;
+
+use Joomla\CMS\Factory;
+use Joomla\CMS\HTML\HTMLHelper;
+
+final class MatchdaylistField extends SportsManagementListField
+{
+    protected $type = 'Matchdaylist';
+
+    protected function getOptions(): array
+    {
+        $varname = trim((string) ($this->element['varname'] ?? ''));
+
+        if ($varname === '') {
+            return parent::getOptions();
+        }
+
+        $projectId = Factory::getApplication()->getInput()->get($varname, null, 'raw');
+
+        if (is_array($projectId)) {
+            $projectId = reset($projectId) ?: 0;
+        }
+
+        $projectId = (int) $projectId;
+
+        if ($projectId <= 0) {
+            return parent::getOptions();
+        }
+
+        $db = $this->getSportsManagementDatabase();
+        $query = $db->getQuery(true)
+            ->select([
+                $db->quoteName('id', 'value'),
+                $db->quoteName('name', 'text'),
+            ])
+            ->from($db->quoteName('#__sportsmanagement_round'))
+            ->where($db->quoteName('project_id') . ' = ' . $projectId)
+            ->where($db->quoteName('published') . ' = 1')
+            ->order($db->quoteName('roundcode') . ' ASC');
+        $db->setQuery($query);
+
+        $options = [];
+
+        foreach ($db->loadObjectList() ?: [] as $item) {
+            $options[] = HTMLHelper::_('select.option', $item->value, $item->text);
+        }
+
+        return array_merge(parent::getOptions(), $options);
+    }
+}
