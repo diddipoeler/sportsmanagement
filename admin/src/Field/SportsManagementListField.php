39e28404356e826dd667b2223db749fe6642f665
diddipoeler <diddipoeler@gmx.de>
2026-08-21 21:08:35 +0200

    Use shared SportsManagement field database trait

--- admin/src/Field/SportsManagementListField.php
+++ admin/src/Field/SportsManagementListField.php
@@ -4,30 +4,8 @@ namespace Diddipoeler\Component\SportsManagement\Administrator\Field;
 \defined('_JEXEC') or die;
 
 use Joomla\CMS\Form\Field\ListField;
-use Joomla\Database\DatabaseInterface;
 
 abstract class SportsManagementListField extends ListField
 {
-    protected function getSportsManagementDatabase(): DatabaseInterface
-    {
-        if (!class_exists('sportsmanagementHelper', false)) {
-            $helperFile = JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/helpers/sportsmanagement.php';
-
-            if (is_file($helperFile)) {
-                require_once $helperFile;
-            }
-        }
-
-        if (!class_exists('sportsmanagementHelper', false)) {
-            throw new \RuntimeException('SportsManagement database helper is unavailable.');
-        }
-
-        $database = \sportsmanagementHelper::getDBConnection();
-
-        if (!$database instanceof DatabaseInterface) {
-            throw new \RuntimeException('SportsManagement database connection is unavailable.');
-        }
-
-        return $database;
-    }
+    use SportsManagementDatabaseTrait;
 }
