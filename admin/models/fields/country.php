74175aab2ca2f0f90ba79754b7e864a012206c41
diddipoeler <diddipoeler@gmx.de>
2026-08-21 20:40:50 +0200

    Bridge legacy country field to native class

--- admin/models/fields/country.php
+++ admin/models/fields/country.php
@@ -1,68 +1,19 @@
 <?php
 /**
- *
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage fields
- * @file       country.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * Legacy compatibility bridge for the SportsManagement country field.
  */
-
 defined('_JEXEC') or die('Restricted access');
 
-use Joomla\CMS\Factory;
-use Joomla\CMS\Form\FormField;
-use Joomla\CMS\Form\FormHelper;
+use Diddipoeler\Component\SportsManagement\Administrator\Field\CountryField;
 
-JLoader::import('components.com_sportsmanagement.helpers.countries', JPATH_SITE);
-jimport('joomla.filesystem.folder');
-FormHelper::loadFieldClass('list');
+if (!class_exists(CountryField::class)) {
+    $fieldFile = JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Field/CountryField.php';
 
+    if (is_file($fieldFile)) {
+        require_once $fieldFile;
+    }
+}
 
-/**
- * FormFieldCountry
- *
- * @package
- * @author
- * @copyright diddi
- * @version   2013
- * @access    public
- */
-class JFormFieldCountry extends \JFormFieldList
-{
-	/**
-	 * field type
-	 *
-	 * @var string
-	 */
-	public $type = 'Country';
-
-	/**
-	 * Method to get the field options.
-	 *
-	 * @return array  The field option objects.
-	 *
-	 * @since 11.1
-	 */
-	protected function getOptions()
-	{
-		$app    = Factory::getApplication();
-		$option = $app->input->getCmd('option');
-		/**
-		 * Initialize variables.
-		 */
-		$options = JSMCountries::getCountryOptions();
-
-		/**
-		 * Merge any additional options in the XML definition.
-		 */
-		$options = array_merge(parent::getOptions(), $options);
-
-		return $options;
-	}
-
+if (class_exists(CountryField::class) && !class_exists('JFormFieldCountry', false)) {
+    class_alias(CountryField::class, 'JFormFieldCountry');
 }
