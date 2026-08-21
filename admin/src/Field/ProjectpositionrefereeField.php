18fe01e5333a825bb6b6ead0d48c1d8d900c3beb
diddipoeler <diddipoeler@gmx.de>
2026-08-21 21:15:39 +0200

    Add native project referee position field

---
+++ admin/src/Field/ProjectpositionrefereeField.php
@@ -0,0 +1,47 @@
+<?php
+namespace Diddipoeler\Component\SportsManagement\Administrator\Field;
+
+\defined('_JEXEC') or die;
+
+use Joomla\CMS\Factory;
+use Joomla\CMS\HTML\HTMLHelper;
+
+final class ProjectpositionrefereeField extends SportsManagementListField
+{
+    protected $type = 'projectpositionreferee';
+
+    protected function getOptions(): array
+    {
+        $app = Factory::getApplication();
+        $option = $app->getInput()->getCmd('option', 'com_sportsmanagement');
+        $projectId = (int) $app->getUserState($option . '.pid', 0);
+
+        if ($projectId <= 0) {
+            return parent::getOptions();
+        }
+
+        $db = $this->getSportsManagementDatabase();
+        $query = $db->getQuery(true)
+            ->select([
+                $db->quoteName('ppos.id', 'value'),
+                $db->quoteName('pos.name', 'text'),
+            ])
+            ->from($db->quoteName('#__sportsmanagement_position', 'pos'))
+            ->join(
+                'INNER',
+                $db->quoteName('#__sportsmanagement_project_position', 'ppos')
+                . ' ON ' . $db->quoteName('pos.id') . ' = ' . $db->quoteName('ppos.position_id')
+            )
+            ->where($db->quoteName('ppos.project_id') . ' = ' . $projectId)
+            ->where($db->quoteName('pos.persontype') . ' = 3');
+        $db->setQuery($query);
+
+        $options = [];
+
+        foreach ($db->loadObjectList() ?: [] as $item) {
+            $options[] = HTMLHelper::_('select.option', $item->value, $item->text);
+        }
+
+        return array_merge(parent::getOptions(), $options);
+    }
+}
