0da316a2be398d837ed158f8bb5b804ee2e879cd
diddipoeler <diddipoeler@gmx.de>
2026-08-21 19:29:05 +0200

    Use native project team table bridge

--- admin/tables/projectteam.php
+++ admin/tables/projectteam.php
@@ -1,38 +1,13 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage tables
- * @file       projectteam.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
 defined('_JEXEC') or die('Restricted access');
 
-/**
- * sportsmanagementTableProjectteam
- *
- * @package
- * @author
- * @copyright diddi
- * @version   2014
- * @access    public
- */
-class sportsmanagementTableProjectteam extends JSMTable
-{
-	/**
-	 * Constructor
-	 *
-	 * @param   object Database connector object
-	 *
-	 * @since 1.0
-	 */
-	function __construct(&$db)
-	{
-		$db = sportsmanagementHelper::getDBConnection();
-		parent::__construct('#__sportsmanagement_project_team', 'id', $db);
-	}
+use Diddipoeler\Component\SportsManagement\Administrator\Table\ProjectteamTable;
 
+if (!class_exists(ProjectteamTable::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Table/SportsManagementTable.php';
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Table/ProjectteamTable.php';
+}
+
+if (!class_exists('sportsmanagementTableProjectteam', false)) {
+    class_alias(ProjectteamTable::class, 'sportsmanagementTableProjectteam');
 }
