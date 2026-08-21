87482b415d07f32d7453d8fabbd7ecb4a2c5c434
diddipoeler <diddipoeler@gmx.de>
2026-08-21 19:12:04 +0200

    Bridge legacy match staff table to native class

--- admin/tables/matchstaff.php
+++ admin/tables/matchstaff.php
@@ -1,57 +1,18 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage tables
- * @file       matchstaff.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\Language\Text;
+/** Legacy compatibility bridge for the native Matchstaff table. */
+\defined('_JEXEC') or die;
 
-/**
- * sportsmanagementTableMatchStaff
- *
- * @package
- * @author
- * @copyright diddi
- * @version   2014
- * @access    public
- */
-class sportsmanagementTableMatchStaff extends JSMTable
-{
-	/**
-	 * Constructor
-	 *
-	 * @param   object Database connector object
-	 *
-	 * @since 1.0
-	 */
-	function __construct(&$db)
-	{
-		$db = sportsmanagementHelper::getDBConnection();
-		parent::__construct('#__sportsmanagement_match_staff', 'id', $db);
-	}
+use Diddipoeler\Component\SportsManagement\Administrator\Table\MatchstaffTable;
+use Diddipoeler\Component\SportsManagement\Administrator\Table\SportsManagementTable;
 
-	/**
-	 * Overloaded check method to ensure data integrity
-	 *
-	 * @access public
-	 * @return boolean True on success
-	 * @since  1.0
-	 */
-	function check()
-	{
-		if (!($this->match_id && $this->team_staff_id))
-		{
-			$this->setError(Text::_('CHECK FAILED'));
+if (!class_exists(SportsManagementTable::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Table/SportsManagementTable.php';
+}
 
-			return false;
-		}
+if (!class_exists(MatchstaffTable::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Table/MatchstaffTable.php';
+}
 
-		return true;
-	}
+if (!class_exists('sportsmanagementTableMatchStaff', false)) {
+    class_alias(MatchstaffTable::class, 'sportsmanagementTableMatchStaff');
 }
