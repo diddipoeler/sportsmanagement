968e15caf99ef4d73fdf9d058eaef2a55dc0dd4b
diddipoeler <diddipoeler@gmx.de>
2026-08-21 17:23:04 +0200

    Remove JLoader from native site base model

--- site/src/Model/SportsManagementModel.php
+++ site/src/Model/SportsManagementModel.php
@@ -12,19 +12,23 @@ abstract class SportsManagementModel extends BaseDatabaseModel
     public function setDatabase(DatabaseInterface $db): void
     {
         if (!class_exists('sportsmanagementHelper')) {
-            \JLoader::register(
-                'sportsmanagementHelper',
-                JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/helpers/sportsmanagement.php'
-            );
+            $helperFile = JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/helpers/sportsmanagement.php';
+
+            if (is_file($helperFile)) {
+                require_once $helperFile;
+            }
         }
 
         try {
-            $databaseSelector = Factory::getApplication()->getInput()->getInt('cfg_which_database', 0);
-            $sportsManagementDb = \sportsmanagementHelper::getDBConnection(true, $databaseSelector);
+            if (class_exists('sportsmanagementHelper')) {
+                $databaseSelector = Factory::getApplication()->getInput()->getInt('cfg_which_database', 0);
+                $sportsManagementDb = \sportsmanagementHelper::getDBConnection(true, $databaseSelector);
 
-            if ($sportsManagementDb instanceof DatabaseInterface) {
-                parent::setDatabase($sportsManagementDb);
-                return;
+                if ($sportsManagementDb instanceof DatabaseInterface) {
+                    parent::setDatabase($sportsManagementDb);
+
+                    return;
+                }
             }
         } catch (\Throwable) {
             // Keep Joomla's injected database connection as a safe fallback.
