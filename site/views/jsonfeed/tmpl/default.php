4538bad3f85042758f29b2b92f2e4c1e9fac029f
diddipoeler <diddipoeler@gmx.de>
2026-08-21 17:04:01 +0200

    Modernize individual Google Calendar JSON output

--- site/views/jsonfeed/tmpl/default.php
+++ site/views/jsonfeed/tmpl/default.php
@@ -1,108 +1,81 @@
 <?php
 /**
- * GCalendar is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * GCalendar is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with GCalendar.  If not, see <http://www.gnu.org/licenses/>.
- *
- * @package   GCalendar
- * @author    Digital Peak http://www.digital-peak.com
- * @copyright Copyright (C) 2007 - 2013 Digital Peak. All rights reserved.
- * @license   http://www.gnu.org/licenses/gpl.html GNU/GPL
+ * Individual-event JSON output for SportsManagement Google Calendar feeds.
  */
 
-defined('_JEXEC') or die();
+defined('_JEXEC') or die;
 
-use Joomla\CMS\Router\Route;
 use Joomla\CMS\Factory;
-use Joomla\CMS\Plugin\PluginHelper;
-use Joomla\CMS\Component\ComponentHelper;
+use Joomla\CMS\Router\Route;
 
-$document = Factory::getDocument();
-$document->setMimeEncoding('application/json');
+$app = Factory::getApplication();
+$app->getDocument()->setMimeEncoding('application/json');
+$itemId = $app->getInput()->getInt('Itemid', 0);
 
-$dispatcher = JDispatcher::getInstance();
-PluginHelper::importPlugin('gcalendar');
+$fadeColor = static function (string $color): string {
+    $hex = strtoupper(ltrim(trim($color), '#'));
 
-$data      = array();
-$SECSINDAY = 86400;
+    if (!preg_match('/^[0-9A-F]{6}$/', $hex)) {
+        $hex = '135CAE';
+    }
 
-if (!empty($this->calendars))
-{
-	$itemID = Factory::getApplication()->input->getVar('Itemid', null);
+    $red = (int) round((hexdec(substr($hex, 0, 2)) + 4 * 255) / 5);
+    $green = (int) round((hexdec(substr($hex, 2, 2)) + 4 * 255) / 5);
+    $blue = (int) round((hexdec(substr($hex, 4, 2)) + 4 * 255) / 5);
 
-	foreach ($this->calendars as $calendar)
-	{
-		if ($itemID == null)
-		{
-			$itemID = jsmGCalendarUtil::getItemId($calendar->id);
-		}
+    return sprintf('#%02X%02X%02X', $red, $green, $blue);
+};
 
-		$params = Factory::getApplication()->getMenu()->getParams($itemID);
-		$tmp    = clone ComponentHelper::getParams('com_sportsmanagement');
+$data = [];
 
-		if (empty($params))
-		{
-			$params = $tmp;
-		}
-		else
-		{
-			$tmp->merge($params);
-			$params = $tmp;
-		}
+foreach ($this->events as $event) {
+    $eventId = trim((string) ($event['id'] ?? ''));
+    $calendarId = (int) ($event['gcid'] ?? 0);
+    $start = trim((string) ($event['start'] ?? ''));
 
+    if ($eventId === '' || $calendarId <= 0 || $start === '') {
+        continue;
+    }
 
-		foreach ($calendar as $event)
-		{
-			$dateformat = $params->get('description_date_format', 'm.d.Y');
-			$timeformat = $params->get('description_time_format', 'g:i a');
+    $title = trim((string) ($event['title'] ?? ''));
+    $descriptionText = trim((string) ($event['description'] ?? ''));
+    $location = trim((string) ($event['location'] ?? ''));
+    $descriptionParts = [];
 
-			$params->set('event_date_format', $dateformat);
-			$params->set('event_time_format', $timeformat);
+    if ($descriptionText !== '') {
+        $descriptionParts[] = nl2br(htmlspecialchars($descriptionText, ENT_QUOTES, 'UTF-8'));
+    }
 
-			if (!empty($itemID))
-			{
-				$itemID = '&Itemid=' . $itemID;
-			}
-			else
-			{
-				$menu       = Factory::getApplication()->getMenu();
-				$activemenu = $menu->getActive();
+    if ($location !== '') {
+        $descriptionParts[] = htmlspecialchars($location, ENT_QUOTES, 'UTF-8');
+    }
 
-				if ($activemenu != null)
-				{
-					$itemID = '&Itemid=' . $activemenu->id;
-				}
-			}
+    $description = implode('<br>', $descriptionParts);
+    $url = trim((string) ($event['htmlLink'] ?? ''));
 
-			$params->set('description_length', 100);
-			$description = jsmGCalendarUtil::renderEvents(array($event), $params->get('description_format', '{{#events}}<p>{{date}}<br/>{{{description}}}</p>{{/events}}'), $params);
+    if ($url === '' || !preg_match('#^https?://#i', $url)) {
+        $url = Route::_(
+            'index.php?option=com_sportsmanagement&view=event&eventID=' . rawurlencode($eventId)
+                . '&gcid=' . $calendarId
+                . ($itemId > 0 ? '&Itemid=' . $itemId : ''),
+            false
+        );
+    }
 
-			$eventData = array(
-				'id'          => $event->getGCalId(),
-				'gcid'        => $event->getParam('gcid'),
-				'title'       => $this->compactMode == 0 ? htmlspecialchars_decode($event->getTitle()) : utf8_encode(chr(160)),
-				'start'       => $event->getStartDate()->format('c', true),
-				'end'         => $event->getEndDate()->format('c', true),
-				'url'         => Route::_('index.php?option=com_sportsmanagement&view=event&eventID=' . $event->getGCalId() . '&gcid=' . $event->getParam('gcid') . (empty($itemID) ? '' : $itemID)),
-				'color'       => jsmGCalendarUtil::getFadedColor($event->getParam('gccolor')),
-				'allDay'      => $this->compactMode == 0 ? $event->isAllDay() : true,
-				'description' => $description
-			);
-
-			$dispatcher->trigger('onGCEventBeforeLoad', array($event, &$eventData));
-			$data[] = $eventData;
-		}
-	}
+    $data[] = [
+        'id' => $eventId,
+        'gcid' => $calendarId,
+        'title' => $this->compactMode === 0 ? $title : "\u{00A0}",
+        'start' => $start,
+        'end' => (string) ($event['end'] ?? $start),
+        'url' => $url,
+        'color' => $fadeColor((string) ($event['color'] ?? '#135CAE')),
+        'allDay' => $this->compactMode === 0 ? !empty($event['allDay']) : true,
+        'description' => $description,
+    ];
 }
 
-ob_clean();
-echo json_encode($data);
+echo json_encode(
+    $data,
+    JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE | JSON_HEX_TAG | JSON_HEX_AMP | JSON_HEX_APOS | JSON_HEX_QUOT
+);
