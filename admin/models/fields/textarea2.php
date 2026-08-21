fab7ec956f53a934ee6cd2c49d6f1f2a992fcead
diddipoeler <diddipoeler@gmx.de>
2026-08-21 21:52:47 +0200

    Bridge extended textarea field to native class

--- admin/models/fields/textarea2.php
+++ admin/models/fields/textarea2.php
@@ -1,73 +1,12 @@
 <?php
-/**
- *
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage fields
- * @file       textarea2.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
+\defined('_JEXEC') or die;
 
-defined('_JEXEC') or die();
+use Diddipoeler\Component\SportsManagement\Administrator\Field\Textarea2Field;
 
-use Joomla\CMS\Factory;
-use Joomla\CMS\Form\FormField;
-use Joomla\CMS\Form\FormHelper;
-use Joomla\CMS\Language\Text;
+if (!class_exists(Textarea2Field::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Field/Textarea2Field.php';
+}
 
-FormHelper::loadFieldClass('textarea');
-
-/**
- * FormFieldTextarea2
- *
- * @package
- * @author    diddi
- * @copyright 2014
- * @version   $Id$
- * @access    public
- */
-class JFormFieldTextarea2 extends JFormFieldTextarea
-{
-	protected $type = 'Textarea2';
-
-	/**
-	 * FormFieldTextarea2::getInput()
-	 *
-	 * @return
-	 */
-	public function getInput()
-	{
-		$buffer = parent::getInput();
-
-		if (isset($this->element->description))
-		{
-			$buffer .= '<label></label>';
-			$buffer .= '<div style="float:left;">' . Text::_($this->element->description) . '</div>';
-		}
-
-		return $buffer;
-	}
-
-	/**
-	 * FormFieldTextarea2::setup()
-	 *
-	 * @param   mixed  $element
-	 * @param   mixed  $value
-	 * @param   mixed  $group
-	 *
-	 * @return
-	 */
-	public function setup(SimpleXMLElement $element, $value, $group = null)
-	{
-		if (isset($element->content) && empty($value))
-		{
-			$value = $element->content;
-		}
-
-		return parent::setup($element, $value, $group);
-	}
+if (!class_exists('JFormFieldTextarea2', false)) {
+    class_alias(Textarea2Field::class, 'JFormFieldTextarea2');
 }
