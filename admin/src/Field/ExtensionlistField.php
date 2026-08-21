30fbed78afd2a55f3b647eb7c79ec6a16c35c95e
diddipoeler <diddipoeler@gmx.de>
2026-08-21 21:46:14 +0200

    Add native extension list field

---
+++ admin/src/Field/ExtensionlistField.php
@@ -0,0 +1,37 @@
+<?php
+namespace Diddipoeler\Component\SportsManagement\Administrator\Field;
+
+\defined('_JEXEC') or die;
+
+use Joomla\CMS\Form\Field\ListField;
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\Filesystem\Folder;
+
+final class ExtensionlistField extends ListField
+{
+    protected $type = 'Extensionlist';
+
+    protected function getOptions(): array
+    {
+        $path = JPATH_ROOT . '/components/com_sportsmanagement/extensions';
+
+        if (!is_dir($path)) {
+            return parent::getOptions();
+        }
+
+        $filter = (string) ($this->element['filter'] ?? '');
+        $exclude = (string) ($this->element['exclude'] ?? '');
+        $folders = Folder::folders($path, $filter) ?: [];
+        $options = [];
+
+        foreach ($folders as $folder) {
+            if ($exclude !== '' && @preg_match(chr(1) . $exclude . chr(1), $folder) === 1) {
+                continue;
+            }
+
+            $options[] = HTMLHelper::_('select.option', $folder, $folder);
+        }
+
+        return array_merge(parent::getOptions(), $options);
+    }
+}
