6d86a82b4764dd3519f5ee892ab97d8f6da8af67
diddipoeler <diddipoeler@gmx.de>
2026-08-21 17:43:05 +0200

    Add native dispatcher for Google Calendar module

---
+++ modules/mod_sportsmanagement_google_calendar/src/Dispatcher/Dispatcher.php
@@ -0,0 +1,35 @@
+<?php
+namespace Diddipoeler\Module\SportsManagementGoogleCalendar\Site\Dispatcher;
+
+\defined('_JEXEC') or die;
+
+use Joomla\CMS\Dispatcher\AbstractModuleDispatcher;
+use Joomla\CMS\Helper\HelperFactoryAwareInterface;
+use Joomla\CMS\Helper\HelperFactoryAwareTrait;
+
+final class Dispatcher extends AbstractModuleDispatcher implements HelperFactoryAwareInterface
+{
+    use HelperFactoryAwareTrait;
+
+    protected function getLayoutData(): array
+    {
+        $data = parent::getLayoutData();
+
+        try {
+            $data = array_merge(
+                $data,
+                $this->getHelperFactory()
+                    ->getHelper('GoogleCalendarHelper')
+                    ->getData($data['params'], $data['module'], $this->getApplication())
+            );
+        } catch (\Throwable $exception) {
+            $this->getApplication()->enqueueMessage(
+                'JSM Google Calendar error: ' . $exception->getMessage(),
+                'error'
+            );
+            $data['events'] = [];
+        }
+
+        return $data;
+    }
+}
