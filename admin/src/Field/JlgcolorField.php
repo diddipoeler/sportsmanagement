3b03238aa10e09e7ecd371b7e83c347cb9eca084
diddipoeler <diddipoeler@gmx.de>
2026-08-21 21:58:55 +0200

    Use Joomla color field for JLG color selector

---
+++ admin/src/Field/JlgcolorField.php
@@ -0,0 +1,11 @@
+<?php
+namespace Diddipoeler\Component\SportsManagement\Administrator\Field;
+
+\defined('_JEXEC') or die;
+
+use Joomla\CMS\Form\Field\ColorField;
+
+final class JlgcolorField extends ColorField
+{
+    protected $type = 'JLGColor';
+}
