1263e0ed6b78f3165527704e4cccfce4f6601bbb
diddipoeler <diddipoeler@gmx.de>
2026-08-21 19:43:56 +0200

    Modernize legacy image select element

--- admin/elements/imageselect.php
+++ admin/elements/imageselect.php
@@ -1,56 +1,55 @@
 <?php
 /**
- *
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage elements
- * @file       imageselect.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * SportsManagement legacy image-select element bridge.
  */
-
 defined('_JEXEC') or die('Restricted access');
 
-use Joomla\CMS\Factory;
+use Diddipoeler\Component\SportsManagement\Site\Helper\ImageSelectHelper;
+use Joomla\CMS\Form\FormField;
+use Joomla\CMS\Uri\Uri;
 
-/**
- * JFormFieldImageSelect
- *
- * @package
- * @author
- * @copyright diddi
- * @version   2014
- * @access    public
- */
-class JFormFieldImageSelect extends JFormField
-{
-	protected $type = 'imageselect';
+if (!class_exists('JFormFieldImageSelect', false)) {
+    class JFormFieldImageSelect extends FormField
+    {
+        protected $type = 'imageselect';
 
-	/**
-	 * JFormFieldImageSelect::getInput()
-	 *
-	 * @return
-	 */
-	function getInput()
-	{
-		$app    = Factory::getApplication();
-		$option = Factory::getApplication()->input->getCmd('option');
+        protected function getInput()
+        {
+            if (!class_exists(ImageSelectHelper::class)) {
+                $helperFile = JPATH_SITE . '/components/com_sportsmanagement/src/Helper/ImageSelectHelper.php';
 
-		$default   = $this->value;
-		$arrPathes = explode('/', $default);
-		$filename  = array_pop($arrPathes);
+                if (is_file($helperFile)) {
+                    require_once $helperFile;
+                }
+            }
 
-		// $targetfolder = array_pop($arrPathes);
-		$targetfolder = $this->element['targetfolder'];
+            if (!class_exists(ImageSelectHelper::class)) {
+                return '';
+            }
 
-		$output = ImageSelectSM::getSelector($this->name, $this->name . '_preview', $targetfolder, $this->value, $default, $this->name, $this->id);
-		$output .= '<img class="imagepreview" src="' . JURI::root(true) . '/media/com_sportsmanagement/jl_images/spinner.gif" ';
-		$output .= ' name="' . $this->name . '_preview" id="' . $this->id . '_preview" border="3" alt="Preview" title="Preview" />';
-		$output .= '<input type="hidden" id="' . $this->id . '" name="' . $this->name . '" value="' . $this->value . '" />';
+            $default = (string) $this->value;
+            $targetFolder = (string) ($this->element['targetfolder'] ?? '');
+            $output = ImageSelectHelper::getSelector(
+                $this->name,
+                $this->name . '_preview',
+                $targetFolder,
+                $this->value,
+                $default,
+                $this->name,
+                $this->id
+            );
 
-		return $output;
-	}
+            $output .= '<img class="imagepreview" src="'
+                . htmlspecialchars(Uri::root(true) . '/media/com_sportsmanagement/jl_images/spinner.gif', ENT_QUOTES, 'UTF-8')
+                . '" name="' . htmlspecialchars($this->name . '_preview', ENT_QUOTES, 'UTF-8')
+                . '" id="' . htmlspecialchars($this->id . '_preview', ENT_QUOTES, 'UTF-8')
+                . '" alt="Preview" title="Preview" />';
+            $output .= '<input type="hidden" id="original_' . htmlspecialchars($this->id, ENT_QUOTES, 'UTF-8')
+                . '" value="' . htmlspecialchars((string) $this->value, ENT_QUOTES, 'UTF-8') . '" />';
+            $output .= '<input type="hidden" id="copy_' . htmlspecialchars($this->id, ENT_QUOTES, 'UTF-8')
+                . '" value="' . htmlspecialchars((string) $this->value, ENT_QUOTES, 'UTF-8') . '" />';
+
+            return $output;
+        }
+    }
 }
