f5f375761229ce3895b1a2c41b10d119967014cb
diddipoeler <diddipoeler@gmx.de>
2026-08-21 21:25:30 +0200

    Bridge K2 category list field to native class

--- admin/models/fields/categorylistk2.php
+++ admin/models/fields/categorylistk2.php
@@ -1,70 +1,12 @@
 <?php
-/**
- *
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage fields
- * @file       categorylistk2.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
+\defined('_JEXEC') or die;
 
-defined('_JEXEC') or die('Restricted access');
+use Diddipoeler\Component\SportsManagement\Administrator\Field\Categorylistk2Field;
 
-use Joomla\CMS\Factory;
-use Joomla\CMS\Form\FormField;
-use Joomla\CMS\Form\FormHelper;
+if (!class_exists(Categorylistk2Field::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Field/Categorylistk2Field.php';
+}
 
-jimport('joomla.filesystem.folder');
-FormHelper::loadFieldClass('list');
-
-
-/**
- * FormFieldcategorylistk2
- *
- * @package
- * @author
- * @copyright diddi
- * @version   2014
- * @access    public
- */
-class JFormFieldcategorylistk2 extends \JFormFieldList
-{
-	/**
-	 * field type
-	 *
-	 * @var string
-	 */
-	public $type = 'categorylistk2';
-
-	/**
-	 * Method to get the field options.
-	 *
-	 * @return array  The field option objects.
-	 *
-	 * @since 11.1
-	 */
-	protected function getOptions()
-	{
-		// Initialize variables.
-		$options = array();
-
-		$db    = Factory::getDbo();
-		$query = $db->getQuery(true);
-
-		$query->select('id AS value, name AS text');
-		$query->from('#__k2_categories');
-		$query->where('trash = 0');
-		$query->order('name');
-		$db->setQuery($query);
-		$options = $db->loadObjectList();
-
-		// Merge any additional options in the XML definition.
-		$options = array_merge(parent::getOptions(), $options);
-
-		return $options;
-	}
+if (!class_exists('JFormFieldcategorylistk2', false)) {
+    class_alias(Categorylistk2Field::class, 'JFormFieldcategorylistk2');
 }
