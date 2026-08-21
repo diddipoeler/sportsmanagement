d842f8a569f5f1f064c1bf5b6372755827abcb9b
diddipoeler <diddipoeler@gmx.de>
2026-08-21 21:23:16 +0200

    Bridge legacy dependent SQL variant to native field

--- admin/models/fields/dependsql_jl.php
+++ admin/models/fields/dependsql_jl.php
@@ -1,118 +1,12 @@
 <?php
-/**
- *
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage fields
- * @file       dependsql_jl.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
+\defined('_JEXEC') or die;
 
-defined('_JEXEC') or die;
+use Diddipoeler\Component\SportsManagement\Administrator\Field\DependsqlField;
 
-use Joomla\CMS\Factory;
-use Joomla\CMS\Form\FormField;
-use Joomla\CMS\Form\FormHelper;
-use Joomla\CMS\HTML\HTMLHelper;
-use Joomla\CMS\Uri\Uri;
+if (!class_exists(DependsqlField::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Field/DependsqlField.php';
+}
 
-FormHelper::loadFieldClass('list');
-
-HTMLHelper::_('behavior.framework');
-
-
-/**
- * FormFieldDependSQL
- *
- * @package
- * @author    Dieter Plöger
- * @copyright 2018
- * @version   $Id$
- * @access    public
- */
-class JFormFieldDependSQL extends FormField
-{
-	/**
-	 * field name
-	 *
-	 * @access protected
-	 * @var    string
-	 */
-	protected $type = 'dependsql';
-
-	/**
-	 * FormFieldDependSQL::getInput()
-	 *
-	 * @return
-	 */
-	protected function getInput()
-	{
-		// Elements
-		// $required   = $this->element['required'] ? ' required aria-required="true"' : '';
-		$required = $this->required ? ' required aria-required="true"' : '';
-		$key      = ($this->element['key_field'] ? $this->element['key_field'] : 'value');
-		$val      = ($this->element['value_field'] ? $this->element['value_field'] : $this->name);
-		$task     = $this->element['task'];
-		$depends  = $this->element['depends'];
-		$ctrl     = $this->name;
-
-		// Attribs
-		$attribs = ' task="' . $task . '"';
-		$attribs .= $required;
-
-		if ($v = $this->element['size'])
-		{
-			$attribs .= ' size="' . $v . '"';
-		}
-
-		if ($depends)
-		{
-			$attribs .= ' depends="' . $depends . '"';
-		}
-
-		$attribs .= ' class="inputbox';
-
-		// Optionally add "depend" to the class attribute
-		if ($depends)
-		{
-			$attribs .= ' depend"';
-		}
-		else
-		{
-			$attribs .= '"';
-		}
-
-		$attribs .= ' current="' . $this->value . '"';
-
-		if ($required == 'true')
-		{
-			$options = array();
-		}
-		else
-		{
-			$options = array();
-		}
-
-		$query = $this->element['query'];
-
-		if ($query != '')
-		{
-			$db = Factory::getDbo();
-			$db->setQuery($query);
-			$options = array_merge($options, $db->loadObjectList());
-		}
-
-		if ($depends)
-		{
-			$doc = Factory::getDocument();
-			$doc->addScript(Uri::base() . 'components/com_sportsmanagement/assets/js/depend.js');
-		}
-
-		return HTMLHelper::_('select.genericlist', $options, $this->name, trim($attribs), $key, $val, $this->value, $this->id);
-
-	}
+if (!class_exists('JFormFieldDependSQL', false)) {
+    class_alias(DependsqlField::class, 'JFormFieldDependSQL');
 }
