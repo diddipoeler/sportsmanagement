51c98da8cb1713490c8154459a8a4dee2daa813d
diddipoeler <diddipoeler@gmx.de>
2026-08-21 19:54:12 +0200

    Bridge legacy prediction round table

--- admin/tables/predictionround.php
+++ admin/tables/predictionround.php
@@ -1,40 +1,17 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- * @version    3.8.20
- * @package    Sportsmanagement
- * @subpackage tables
- * @file       predictionround.php
- * @author     jst, diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2020 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
+/** Legacy compatibility bridge for the native PredictionroundTable. */
 defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\Language\Text;
-use Joomla\CMS\Filter\OutputFilter;
 
-/**
- * sportsmanagementTablePredictionRound
- *
- * @package
- * @author
- * @copyright jst
- * @version   2020
- * @access    public
- */
-class sportsmanagementTablePredictionRound extends JSMTable
-{
-	/**
-	 * Constructor
-	 *
-	 * @param   object Database connector object
-	 *
-	 * @since 1.0
-	 */
-	function __construct(&$db)
-	{
-		$db = sportsmanagementHelper::getDBConnection();
-		parent::__construct('#__sportsmanagement_prediction_tippround', 'id', $db);
-	}
+use Diddipoeler\Component\SportsManagement\Administrator\Table\PredictionroundTable;
 
+if (!class_exists(PredictionroundTable::class)) {
+    $tableFile = JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Table/PredictionroundTable.php';
+
+    if (is_file($tableFile)) {
+        require_once $tableFile;
+    }
+}
+
+if (class_exists(PredictionroundTable::class) && !class_exists('sportsmanagementTablePredictionRound', false)) {
+    class_alias(PredictionroundTable::class, 'sportsmanagementTablePredictionRound');
 }
