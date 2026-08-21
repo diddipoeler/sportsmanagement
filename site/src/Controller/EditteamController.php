300697c4695a3235c76072881f03045e8ef6cf26
diddipoeler <diddipoeler@gmx.de>
2026-08-21 08:07:08 +0200

    Move frontend team editor to Joomla 5/6 MVC

---
+++ site/src/Controller/EditteamController.php
@@ -0,0 +1,80 @@
+<?php
+namespace Diddipoeler\Component\SportsManagement\Site\Controller;
+
+\defined('_JEXEC') or die;
+
+use Diddipoeler\Component\SportsManagement\Site\Model\EditteamModel;
+use Joomla\CMS\Factory;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\MVC\Controller\FormController;
+
+/** Joomla 5/6 frontend controller for team editing. */
+final class EditteamController extends FormController
+{
+    public function apply($key = null, $urlVar = null)
+    {
+        return $this->save($key, $urlVar);
+    }
+
+    public function submit()
+    {
+        return true;
+    }
+
+    public function save($key = null, $urlVar = null)
+    {
+        $this->checkToken();
+
+        $input = Factory::getApplication()->getInput();
+        $data = $input->post->getArray();
+        $id = $input->getInt('id', 0);
+        $saved = $this->editTeamModel()->updItem($data);
+
+        if (!$saved) {
+            $this->setRedirect(
+                'index.php?option=com_sportsmanagement&view=editteam&tmpl=component&id=' . $id
+                . '&ptid=' . (int) ($data['ptid'] ?? 0)
+                . '&p=' . (int) ($data['p'] ?? 0)
+                . '&tid=' . (int) ($data['tid'] ?? 0),
+                Text::_('JERROR_AN_ERROR_HAS_OCCURRED'),
+                'error'
+            );
+
+            return false;
+        }
+
+        if ($this->getTask() === 'apply') {
+            $this->setRedirect(
+                'index.php?option=com_sportsmanagement&view=editteam&tmpl=component&id=' . $id
+                . '&ptid=' . (int) ($data['ptid'] ?? 0)
+                . '&p=' . (int) ($data['p'] ?? 0)
+                . '&tid=' . (int) ($data['tid'] ?? 0),
+                Text::_('COM_SPORTSMANAGEMENT_EDIT_TEAM_SAVED')
+            );
+
+            return true;
+        }
+
+        $this->setRedirect('index.php?option=com_sportsmanagement&view=close&tmpl=component');
+
+        return true;
+    }
+
+    public function cancel($key = null)
+    {
+        $this->setRedirect('index.php?option=com_sportsmanagement&view=close&tmpl=component', 'cancel');
+
+        return true;
+    }
+
+    private function editTeamModel(): EditteamModel
+    {
+        $model = $this->getModel('Editteam');
+
+        if (!$model instanceof EditteamModel) {
+            throw new \RuntimeException('EditteamModel is unavailable.', 500);
+        }
+
+        return $model;
+    }
+}
---
+++ site/src/Model/EditteamModel.php
@@ -0,0 +1,98 @@
+<?php
+namespace Diddipoeler\Component\SportsManagement\Site\Model;
+
+\defined('_JEXEC') or die;
+
+use Diddipoeler\Component\SportsManagement\Administrator\Table\TeamTable;
+use Joomla\CMS\Factory;
+use Joomla\CMS\Form\Form;
+use Joomla\CMS\Log\Log;
+use Joomla\CMS\MVC\Model\AdminModel;
+
+/** Joomla 5/6 frontend model for editing teams. */
+final class EditteamModel extends AdminModel
+{
+    public $latitude = null;
+    public $longitude = null;
+    public $name = 'editteam';
+
+    private ?TeamTable $team = null;
+
+    public function updItem($data): bool
+    {
+        foreach ((array) ($data['request'] ?? []) as $key => $value) {
+            $data[$key] = $value;
+        }
+
+        // Preserve the historical frontend editor behaviour: empty/zero values were filtered before binding.
+        $data = array_filter((array) $data);
+
+        try {
+            $table = $this->getTable('team');
+
+            if (!$table->bind($data)) {
+                return false;
+            }
+
+            if (!$table->check()) {
+                return false;
+            }
+
+            return (bool) $table->store();
+        } catch (\Throwable $e) {
+            Log::add($e->getMessage(), Log::ERROR, 'jsmerror');
+            $this->setError($e->getMessage());
+
+            return false;
+        }
+    }
+
+    public function getTable($type = 'team', $prefix = 'sportsmanagementTable', $config = [])
+    {
+        if (strcasecmp((string) $type, 'team') === 0) {
+            return new TeamTable($this->getDatabase());
+        }
+
+        return parent::getTable($type, $prefix, $config);
+    }
+
+    public function getForm($data = [], $loadData = true)
+    {
+        Form::addFormPath(JPATH_SITE . '/components/com_sportsmanagement/models/forms');
+        Form::addFieldPath(JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/models/fields');
+
+        $form = $this->loadForm(
+            'com_sportsmanagement.' . $this->name,
+            $this->name,
+            ['load_data' => $loadData]
+        );
+
+        return $form ?: false;
+    }
+
+    protected function loadFormData()
+    {
+        $app = Factory::getApplication();
+        $data = $app->getUserState('com_sportsmanagement.edit.' . $this->name . '.data', []);
+
+        if (empty($data)) {
+            $data = $this->getData();
+        }
+
+        return $data;
+    }
+
+    public function getData(): TeamTable
+    {
+        if ($this->team === null) {
+            $teamId = Factory::getApplication()->getInput()->getInt('tid', 0);
+            $this->team = $this->getTable('team');
+
+            if ($teamId > 0) {
+                $this->team->load($teamId);
+            }
+        }
+
+        return $this->team;
+    }
+}
