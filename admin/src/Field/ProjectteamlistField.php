ffc637d524683493437348a1a6b1fa3d422153fa
diddipoeler <diddipoeler@gmx.de>
2026-08-21 21:17:27 +0200

    Add native project team list field

---
+++ admin/src/Field/ProjectteamlistField.php
@@ -0,0 +1,52 @@
+<?php
+namespace Diddipoeler\Component\SportsManagement\Administrator\Field;
+
+\defined('_JEXEC') or die;
+
+use Joomla\CMS\Factory;
+use Joomla\CMS\HTML\HTMLHelper;
+
+final class ProjectteamlistField extends SportsManagementListField
+{
+    protected $type = 'projectteamlist';
+
+    protected function getOptions(): array
+    {
+        $app = Factory::getApplication();
+        $option = $app->getInput()->getCmd('option', 'com_sportsmanagement');
+        $projectId = (int) $app->getUserState($option . '.pid', 0);
+
+        if ($projectId <= 0) {
+            return parent::getOptions();
+        }
+
+        $db = $this->getSportsManagementDatabase();
+        $query = $db->getQuery(true)
+            ->select([
+                $db->quoteName('pt.id', 'value'),
+                $db->quoteName('t.name', 'text'),
+            ])
+            ->from($db->quoteName('#__sportsmanagement_team', 't'))
+            ->join(
+                'INNER',
+                $db->quoteName('#__sportsmanagement_season_team_id', 'st')
+                . ' ON ' . $db->quoteName('st.team_id') . ' = ' . $db->quoteName('t.id')
+            )
+            ->join(
+                'INNER',
+                $db->quoteName('#__sportsmanagement_project_team', 'pt')
+                . ' ON ' . $db->quoteName('pt.team_id') . ' = ' . $db->quoteName('st.id')
+            )
+            ->where($db->quoteName('pt.project_id') . ' = ' . $projectId)
+            ->order($db->quoteName('t.name'));
+        $db->setQuery($query);
+
+        $options = [];
+
+        foreach ($db->loadObjectList() ?: [] as $item) {
+            $options[] = HTMLHelper::_('select.option', $item->value, $item->text);
+        }
+
+        return array_merge(parent::getOptions(), $options);
+    }
+}
