62627a0778197b56ea7d3d12b563536913759d2f
diddipoeler <diddipoeler@gmx.de>
2026-08-21 21:06:08 +0200

    Add native active event type field

---
+++ admin/src/Field/ActeventtypeField.php
@@ -0,0 +1,48 @@
+<?php
+namespace Diddipoeler\Component\SportsManagement\Administrator\Field;
+
+\defined('_JEXEC') or die;
+
+use Joomla\CMS\Factory;
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Language\Text;
+
+final class ActeventtypeField extends SportsManagementListField
+{
+    protected $type = 'acteventtype';
+
+    protected function getOptions(): array
+    {
+        $targetTable = preg_replace('/[^A-Za-z0-9_]/', '', (string) ($this->element['targettable'] ?? ''));
+        $selectedId = Factory::getApplication()->getInput()->getInt('id', 0);
+
+        if ($targetTable === '' || $selectedId <= 0) {
+            return parent::getOptions();
+        }
+
+        $db = $this->getSportsManagementDatabase();
+        $target = '#__sportsmanagement_' . $targetTable;
+        $query = $db->getQuery(true)
+            ->select([
+                $db->quoteName('s.id', 'value'),
+                $db->quoteName('s.name', 'text'),
+            ])
+            ->from($db->quoteName('#__sportsmanagement_eventtype', 's'))
+            ->join(
+                'INNER',
+                $db->quoteName($target, 't')
+                . ' ON ' . $db->quoteName('t.sports_type_id') . ' = ' . $db->quoteName('s.sports_type_id')
+            )
+            ->where($db->quoteName('t.id') . ' = ' . $selectedId)
+            ->order($db->quoteName('s.name'));
+        $db->setQuery($query);
+
+        $options = [];
+
+        foreach ($db->loadObjectList() ?: [] as $item) {
+            $options[] = HTMLHelper::_('select.option', $item->value, Text::_($item->text));
+        }
+
+        return array_merge(parent::getOptions(), $options);
+    }
+}
