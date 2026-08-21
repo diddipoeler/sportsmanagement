b50a4bed30d2ae78f7062ea6e334f88b3a3410f4
diddipoeler <diddipoeler@gmx.de>
2026-08-21 20:56:02 +0200

    Add native active season field

---
+++ admin/src/Field/ActseasonField.php
@@ -0,0 +1,32 @@
+<?php
+namespace Diddipoeler\Component\SportsManagement\Administrator\Field;
+
+\defined('_JEXEC') or die;
+
+use Joomla\CMS\HTML\HTMLHelper;
+
+final class ActseasonField extends SportsManagementListField
+{
+    protected $type = 'actseason';
+
+    protected function getOptions(): array
+    {
+        $db = $this->getSportsManagementDatabase();
+        $query = $db->getQuery(true)
+            ->select([
+                $db->quoteName('id', 'value'),
+                $db->quoteName('name', 'text'),
+            ])
+            ->from($db->quoteName('#__sportsmanagement_season'))
+            ->order($db->quoteName('name'));
+        $db->setQuery($query);
+
+        $options = [];
+
+        foreach ($db->loadObjectList() ?: [] as $item) {
+            $options[] = HTMLHelper::_('select.option', $item->value, $item->text);
+        }
+
+        return array_merge(parent::getOptions(), $options);
+    }
+}
