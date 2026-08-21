f707f817dc1a460c2cbf45b7718479d4e7bd99da
diddipoeler <diddipoeler@gmx.de>
2026-08-21 18:57:24 +0200

    Modernize default calendar layout

--- modules/mod_sportsmanagement_calendar/tmpl/default_jsm.php
+++ modules/mod_sportsmanagement_calendar/tmpl/default_jsm.php
@@ -1,243 +1,183 @@
 <?php
 /**
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage mod_sportsmanagement_calendar
- * @file       default.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * Joomla 5/6 default SportsManagement calendar layout.
  */
-defined('_JEXEC') or die('Restricted access');
+\defined('_JEXEC') or die;
+
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Uri\Uri;
-use Joomla\CMS\Factory;
 
-$display = ($params->get('update_module') == 1) ? 'block' : 'none';
-$show_teamlist = ($params->get('show_teamslist') == 1) ? 'show' : 'hidden';
+$moduleId = (int) ($module->id ?? 0);
+$display = (int) $params->get('update_module', 1) === 1 ? 'block' : 'none';
+$showTeamList = (int) $params->get('show_teamslist', 1) === 1 ? 'visible' : 'hidden';
+$teamList = is_array($calendar['teamslist'] ?? null) ? $calendar['teamslist'] : [];
+$list = is_array($calendar['list'] ?? null) ? $calendar['list'] : [];
+$selectedTeam = (int) ($selected_team ?? 0);
 ?>
-
-<script type="text/javascript">
-	jlcinjectcontainer['<?php echo $module->id ?>'] = '<?php echo $inject_container ?>';
-	jlcmodal['<?php echo $module->id ?>'] = '<?php echo $lightbox ?>';
-	var calendar_baseurl = '<?php echo Uri::base() ?>';
-	// <?PHP
-	// if ($lightbox == 1 && (Factory::getApplication()->input->getVar('format') != 'pdf'))
-	// {
-	// ?>		
-	// 	window.addEvent('domready', function() {
-	// 		$$('a.jlcmodal<?php echo $module->id ?>').each(function(el) {
-	// 			el.addEvent('click', function(e) {
-	// 				new Event(e).stop();
-	// 				SqueezeBox.fromElement(el);
-	// 			});
-	// 		});
-	// 	});
-	// <?PHP
-	// }
-	// ?>
+<script>
+window.jlcinjectcontainer[<?php echo $moduleId; ?>] = <?php echo json_encode((string) $inject_container, JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_AMP | JSON_HEX_QUOT); ?>;
+window.jlcmodal[<?php echo $moduleId; ?>] = <?php echo (int) $lightbox; ?>;
+window.calendar_baseurl = <?php echo json_encode((string) Uri::base(), JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_AMP | JSON_HEX_QUOT); ?>;
 </script>
 
-<!-- Trigger the modal with a button -->
-<!--
-<button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal<?php echo $module->id; ?>">Open Modal</button>
--->
-
-<!-- Modal -->
-<div id="myModal<?php echo $module->id; ?>" class="modal fade" role="dialog">
-	<div id="myModaldialog<?php echo $module->id; ?>" class="modal-dialog modal-dialog-scrollable">
-
-		<!-- Modal content-->
-		<div id="myModalcontent<?php echo $module->id; ?>" class="modal-content">
-			<div id="myModalheader<?php echo $module->id; ?>" class="modal-header">
-				<button type="button" class="close" data-bs-dismiss="modal">&times;</button>
-				<h4 class="modal-title">Modal Header</h4>
-			</div>
-			<div id="myModalbody<?php echo $module->id; ?>" class="modal-body">
-				<p>Some text in the modal.</p>
-			</div>
-			<div id="myModalfooter<?php echo $module->id; ?>" class="modal-footer">
-				<button type="button" class="btn btn-default" data-bs-dismiss="modal">Close</button>
-			</div>
-		</div>
-
-	</div>
+<div id="myModal<?php echo $moduleId; ?>" class="modal fade" tabindex="-1" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-scrollable">
+        <div class="modal-content">
+            <div id="myModalheader<?php echo $moduleId; ?>" class="modal-header">
+                <h2 class="modal-title fs-5"></h2>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div id="myModalbody<?php echo $moduleId; ?>" class="modal-body"></div>
+        </div>
+    </div>
 </div>
 
-<div id="<?php echo $inject_container ?>">
+<?php if ((string) $inject_container !== '') : ?>
+    <div id="<?php echo htmlspecialchars((string) $inject_container, ENT_QUOTES, 'UTF-8'); ?>"></div>
+<?php endif; ?>
 
+<div id="jlccalendar-<?php echo $moduleId; ?>">
+    <!--jlccalendar-<?php echo $moduleId; ?> start-->
+
+    <?php if (isset($calendar['calendar'])) : ?>
+        <?php echo $calendar['calendar']; ?>
+    <?php endif; ?>
+
+    <?php if ($teamList !== []) : ?>
+        <div class="my-2">
+            <?php
+            echo HTMLHelper::_(
+                'select.genericlist',
+                $teamList,
+                'jlcteam' . $moduleId,
+                'class="form-select" style="width:100%;visibility:' . $showTeamList . ';" onchange="jlcnewDate(' . (int) $month . ',' . (int) $year . ',' . $moduleId . ');"',
+                'value',
+                'text',
+                $selectedTeam
+            );
+            ?>
+        </div>
+    <?php endif; ?>
+
+    <?php if ($list !== []) : ?>
+        <div>
+            <div class="d-none">
+                <div id="jlCalList-<?php echo $moduleId; ?>_temp" class="m-2 overflow-auto"></div>
+            </div>
+
+            <?php $counter = 0; ?>
+            <?php foreach ($list as $row) : ?>
+                <?php if (isset($row['tag'])) : ?>
+                    <?php switch ((string) $row['tag']) :
+                        case 'span': ?>
+                            <span id="<?php echo htmlspecialchars((string) ($row['divid'] ?? ''), ENT_QUOTES, 'UTF-8'); ?>"
+                                  class="<?php echo htmlspecialchars((string) ($row['class'] ?? ''), ENT_QUOTES, 'UTF-8'); ?>">
+                                <?php echo htmlspecialchars((string) ($row['text'] ?? ''), ENT_QUOTES, 'UTF-8'); ?>
+                            </span>
+                            <?php break; ?>
+
+                        <?php case 'div': ?>
+                            <div id="<?php echo htmlspecialchars((string) ($row['divid'] ?? ''), ENT_QUOTES, 'UTF-8'); ?>"
+                                 class="<?php echo htmlspecialchars((string) ($row['class'] ?? ''), ENT_QUOTES, 'UTF-8'); ?>">
+                            <?php break; ?>
+
+                        <?php case 'divend': ?>
+                            </div>
+                            <?php break; ?>
+
+                        <?php case 'table': ?>
+                            <div class="table-responsive">
+                                <table class="table table-striped" style="margin:0 auto;min-width:60%">
+                            <?php break; ?>
+
+                        <?php case 'tableend': ?>
+                                </table>
+                            </div>
+                            <?php break; ?>
+
+                        <?php case 'headingrow': ?>
+                            <tr>
+                                <th class="sectiontableheader jlcal_heading" colspan="5" scope="colgroup">
+                                    <?php echo htmlspecialchars((string) ($row['text'] ?? ''), ENT_QUOTES, 'UTF-8'); ?>
+                                </th>
+                            </tr>
+                            <?php break; ?>
+                    <?php endswitch; ?>
+                    <?php continue; ?>
+                <?php endif; ?>
+
+                <?php $rowClass = ($counter++ % 2) ? 'sectiontableentry1' : 'sectiontableentry2'; ?>
+
+                <?php if (($row['type'] ?? '') === 'jevents') : ?>
+                    <?php
+                    $eventColor = (string) ($row['color'] ?? '');
+                    $eventStyle = preg_match('/^#[0-9a-f]{3,8}$/i', $eventColor)
+                        ? 'border-left:4px solid ' . $eventColor
+                        : '';
+                    ?>
+                    <tr class="<?php echo $rowClass; ?> jlcal_matchrow">
+                        <td class="jlcal_jevents" colspan="5"<?php echo $eventStyle !== '' ? ' style="' . $eventStyle . '"' : ''; ?>>
+                            <?php if (!empty($row['time'])) : ?>
+                                <span class="jlcal_jevents_time"><?php echo htmlspecialchars((string) $row['time'], ENT_QUOTES, 'UTF-8'); ?>: </span>
+                            <?php endif; ?>
+                            <span class="jlcal_jevents_title">
+                                <a href="<?php echo htmlspecialchars((string) ($row['link'] ?? ''), ENT_QUOTES, 'UTF-8'); ?>">
+                                    <?php echo htmlspecialchars((string) ($row['title'] ?? ''), ENT_QUOTES, 'UTF-8'); ?>
+                                </a>
+                            </span>
+                            <?php if (!empty($row['location'])) : ?>
+                                – <span class="jlcal_jevents_location"><?php echo htmlspecialchars((string) $row['location'], ENT_QUOTES, 'UTF-8'); ?></span>
+                            <?php endif; ?>
+                        </td>
+                    </tr>
+                <?php elseif (($row['type'] ?? '') === 'jlb') : ?>
+                    <tr class="<?php echo $rowClass; ?> jlcal_matchrow">
+                        <td class="jlcal_birthday" colspan="5">
+                            <?php echo (string) ($row['image'] ?? ''); ?>
+                            <span class="jlc_player_name">
+                                <?php if (!empty($row['link'])) : ?>
+                                    <a href="<?php echo htmlspecialchars((string) $row['link'], ENT_QUOTES, 'UTF-8'); ?>">
+                                <?php endif; ?>
+                                <?php echo htmlspecialchars((string) ($row['name'] ?? ''), ENT_QUOTES, 'UTF-8'); ?>
+                                <?php if (!empty($row['link'])) : ?>
+                                    </a>
+                                <?php endif; ?>
+                            </span>
+                            <span class="jlc_player_age"><?php echo htmlspecialchars((string) ($row['age'] ?? ''), ENT_QUOTES, 'UTF-8'); ?></span>
+                        </td>
+                    </tr>
+                <?php else : ?>
+                    <?php
+                    $timestamp = (int) ($row['timestamp'] ?? 0);
+                    $time = $timestamp > 0 ? date('H:i', $timestamp) : '';
+                    $link = (string) ($row['link'] ?? '');
+                    ?>
+                    <tr class="<?php echo $rowClass; ?> jlcal_matchrow">
+                        <td class="jlcal_matchdate">
+                            <?php if ($link !== '') : ?>
+                                <a href="<?php echo htmlspecialchars($link, ENT_QUOTES, 'UTF-8'); ?>"><?php echo htmlspecialchars($time, ENT_QUOTES, 'UTF-8'); ?></a>
+                            <?php else : ?>
+                                <?php echo htmlspecialchars($time, ENT_QUOTES, 'UTF-8'); ?>
+                            <?php endif; ?>
+                        </td>
+                        <td class="jlcal_hometeam">
+                            <?php echo (string) ($row['homepic'] ?? ''); ?><?php echo htmlspecialchars((string) ($row['homename'] ?? ''), ENT_QUOTES, 'UTF-8'); ?>
+                        </td>
+                        <td class="jlcal_teamseperator">–</td>
+                        <td class="jlcal_awayteam">
+                            <?php echo (string) ($row['awaypic'] ?? ''); ?><?php echo htmlspecialchars((string) ($row['awayname'] ?? ''), ENT_QUOTES, 'UTF-8'); ?>
+                        </td>
+                        <td class="jlcal_result"><?php echo htmlspecialchars((string) ($row['result'] ?? ''), ENT_QUOTES, 'UTF-8'); ?></td>
+                    </tr>
+                <?php endif; ?>
+            <?php endforeach; ?>
+        </div>
+
+        <div style="display:<?php echo $display; ?>">
+            <div id="jlCalList-<?php echo $moduleId; ?>" class="overflow-auto"></div>
+        </div>
+    <?php endif; ?>
+
+    <!--jlccalendar-<?php echo $moduleId; ?> end-->
 </div>
-<?php if (isset($calendar['calendar']))
-{
-?>
-	<div id="jlccalendar-<?php echo $module->id ?>">
-		<!--jlccalendar-<?php echo $module->id ?> start-->
-
-		<?php echo $calendar['calendar'] ?> <?php
-										} ?>
-                                         
-<?php 
-if (count($calendar['teamslist']) > 0)
-{
-?>
-<div style="margin: 0 auto;">
-<?php
-echo HTMLHelper::_('select.genericlist', $calendar['teamslist'], 'jlcteam' . $module->id, 'class="inputbox" style="width:100%;visibility:'.$show_teamlist.';" size="1" onchange="jlcnewDate(' . $month . ',' . $year . ',' . $module->id . ');"', 'value', 'text', Factory::getApplication()->input->getVar('jlcteam', 0, 'default', 'POST'));
-?>
-</div>
-<?php
-}
-
-	?> <?php
-			if (isset($calendar['list']))
-			{
-			?>
-		<div>
-
-			<div style="display: none;">
-				<div id="jlCalList-<?php echo $module->id; ?>_temp" style="overflow: auto; margin: 10px;"></div>
-			</div>
-
-			<?php
-				// Echo $calendar['list'];
-				$cnt = 0;
-
-				for ($x = 0; $x < count($calendar['list']); $x++)
-				{
-					$row = $calendar['list'][$x];
-
-					if (isset($row['tag']))
-					{
-						switch ($row['tag'])
-						{
-							case 'span':
-			?> <span id="<?php echo $row['divid']; ?>" class="<?php echo $row['class']; ?>"><?php echo $row['text']; ?></span><?php
-																																					break;
-																																				case 'div':
-																																					?>
-							<div id="<?php echo $row['divid']; ?>" class="<?php echo $row['class']; ?>"><?php
-																																					break;
-																																				case 'table':
-																										?>
-								<div class="table-responsive">
-									<table style="margin: 0 auto; min-width: 60%;" cellspacing="0" cellpadding="0" class="table table-striped">
-									<?php
-																																					break;
-																																				case 'divend':
-									?>
-								</div>
-							<?php
-																																					break;
-																																				case 'tableend':
-							?>
-								</table>
-							</div>
-						<?php
-																																					break;
-																																				case 'headingrow':
-						?>
-							<tr>
-								<td class="sectiontableheader jlcal_heading" colspan="5"><?php echo $row['text']; ?></td>
-							</tr>
-						<?php
-																																					break;
-																																			}
-																																		}
-																																		else
-																																		{
-																																			$sclass  = ($cnt % 2) ? 'sectiontableentry1' : 'sectiontableentry2';
-																																			$date    = HTMLHelper::date($row['timestamp'], $params->get('dateformat'));
-																																			$uhrzeit = date("H:i", $row['timestamp']);
-																																			$time    = date("H:i", $row['timestamp']);
-
-																																			switch ($row['type'])
-																																			{
-																																				case 'jevents':
-																																					$style = ($row['color'] != '') ? ' style="border-left:4px ' . $row['color'] . ' solid;"' : '';
-						?>
-							<tr class="<?php echo $sclass; ?> jlcal_matchrow">
-								<td class="jlcal_jevents" colspan="5" <?php echo $style; ?>><?php
-
-																																					if ($row['time'] != '')
-																																					{
-																							?> <span class="jlcal_jevents_time"><?php echo $row['time'] . ': '; ?></span>
-									<?php } ?> <span class="jlcal_jevents_title"><a href="<?php echo $row['link']; ?>"><?php echo $row['title']; ?></a></span>
-									<?php
-																																					if ($row['location'] != '')
-																																					{
-									?> - <span class="jlcal_jevents_location"><?php echo $row['location']; ?></span> <?php
-																																					} ?>
-								</td>
-							</tr>
-						<?php
-																																					break;
-
-																																				case 'jlb':
-						?>
-							<tr class="<?php echo $sclass; ?> jlcal_matchrow">
-								<td class="jlcal_birthday" colspan="5"><?php
-																																					if (!empty($row['image']))
-																																					{
-																																						echo $row['image'];
-																																					} ?> <span class="jlc_player_name"><?php
-																																					if (!empty($row['link']))
-																																					{
-																			?> <a href="<?php echo $row['link']; ?>" title="<?php echo $row['link']; ?>"> <?php
-																																					}
-
-																																					echo $row['name'];
-
-																																					if (!empty($row['link']))
-																																					{
-																															?> </a> <?php
-																																					} ?></span> <span class="jlc_player_age"><?php echo $row['age']; ?></span>
-								</td>
-							</tr>
-						<?php
-																																					break;
-																																				default:
-						?>
-							<tr class="<?php echo $sclass; ?> jlcal_matchrow">
-								<td class="jlcal_matchdate"><?php
-																																					// Link to matchdetails
-																																					if (!empty($row['link']))
-																																					{
-															?> <a href="<?php echo $row['link']; ?>" title="<?php echo $row['link']; ?>"> <?php
-																																						echo $time;
-																															?> </a> <?php
-																																					}
-																																					else
-																																					{
-																																						echo $time;
-																																					}
-													?></td>
-								<td class="jlcal_hometeam"><?php echo $row['homepic'] . $row['homename']; ?></td>
-								<td class="jlcal_teamseperator">-</td>
-								<td class="jlcal_awayteam"><?php echo $row['awaypic'] . $row['awayname'];; ?></td>
-								<td class="jlcal_result"><?php echo $row['result']; ?></td>
-							</tr>
-			<?php
-																																					break;
-																																			}
-																																		}
-																																	}
-			?>
-		</div>
-		<div style="display:<?php echo $display; ?>;">
-			<div id="jlCalList-<?php echo $module->id; ?>" style="overflow: auto;"></div>
-		</div>
-	<?php
-			}
-	?>
-	<!--jlccalendar-<?php echo $module->id ?> end-->
-	</div>
-	<div id="jlcTestlist-<?php echo $module->id; ?>">
-	</div>
-
-
-	<?php
-	if ($ajax && $ajaxmod == $module->id)
-	{
-		exit();
-	}
+<div id="jlcTestlist-<?php echo $moduleId; ?>"></div>
