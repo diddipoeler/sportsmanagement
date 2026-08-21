c041f86a34a2b786ae440badb051a0be4d776789
diddipoeler <diddipoeler@gmx.de>
2026-08-21 19:48:44 +0200

    Bridge legacy prediction project table

--- admin/tables/predictionproject.php
+++ admin/tables/predictionproject.php
@@ -1,59 +1,17 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage tables
- * @file       predictionproject.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
+/** Legacy compatibility bridge for the native PredictionprojectTable. */
 defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\Language\Text;
 
-/**
- * sportsmanagementTablePredictionProject
- *
- * @package
- * @author
- * @copyright diddi
- * @version   2013
- * @access    public
- */
-class sportsmanagementTablePredictionProject extends JSMTable
-{
+use Diddipoeler\Component\SportsManagement\Administrator\Table\PredictionprojectTable;
 
-	/**
-	 * Constructor
-	 *
-	 * @param   object Database connector object
-	 *
-	 * @since 1.0
-	 */
-	function __construct(&$db)
-	{
-		$db = sportsmanagementHelper::getDBConnection();
-		parent::__construct('#__sportsmanagement_prediction_project', 'id', $db);
-	}
+if (!class_exists(PredictionprojectTable::class)) {
+    $tableFile = JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Table/PredictionprojectTable.php';
 
-	/**
-	 * Overloaded check method to ensure data integrity
-	 *
-	 * @access public
-	 * @return boolean True on success
-	 * @since  1.0
-	 */
-	function check()
-	{
-		if (!($this->prediction_id && $this->project_id))
-		{
-			$this->setError(Text::_('CHECK FAILED'));
+    if (is_file($tableFile)) {
+        require_once $tableFile;
+    }
+}
 
-			return false;
-		}
-
-		return true;
-	}
-
+if (class_exists(PredictionprojectTable::class) && !class_exists('sportsmanagementTablePredictionProject', false)) {
+    class_alias(PredictionprojectTable::class, 'sportsmanagementTablePredictionProject');
 }
