3d5a953f903f2469b9c5790c9ccde404936e5826
diddipoeler <diddipoeler@gmx.de>
2026-08-21 18:01:36 +0200

    Remove JLoader from scoresheet raw view

--- site/views/scoresheet/view.raw.php
+++ site/views/scoresheet/view.raw.php
@@ -1,29 +1,15 @@
 <?php
 /**
- *
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage scoresheet
- * @file       view.raw.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * SportsManagement Joomla 5/6 raw scoresheet view.
  */
+defined('_JEXEC') or die;
 
-defined('_JEXEC') or die();
 use Joomla\CMS\MVC\View\HtmlView;
-
-use Joomla\CMS\MVC\Model\BaseDatabaseModel;
-
-JLoader::import('joomla.application.component.view');
 
 class sportsmanagementViewScoresheet extends HtmlView
 {
-
-	public function display($tpl = null)
-	{
-		parent::display($tpl);
-	}
+    public function display($tpl = null)
+    {
+        parent::display($tpl);
+    }
 }
