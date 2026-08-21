e372e9ac10731cf708ddba6e421ea14fca136af5
diddipoeler <diddipoeler@gmx.de>
2026-08-21 19:10:59 +0200

    Add native match player table

---
+++ admin/src/Table/MatchplayerTable.php
@@ -0,0 +1,27 @@
+<?php
+namespace Diddipoeler\Component\SportsManagement\Administrator\Table;
+
+\defined('_JEXEC') or die;
+
+use Joomla\CMS\Language\Text;
+use Joomla\Database\DatabaseInterface;
+
+/** Native Joomla 5/6 table for match players. */
+final class MatchplayerTable extends SportsManagementTable
+{
+    public function __construct(DatabaseInterface $db)
+    {
+        parent::__construct('#__sportsmanagement_match_player', 'id', $db);
+    }
+
+    public function check()
+    {
+        if (!($this->match_id && $this->teamplayer_id)) {
+            $this->setError(Text::_('CHECK FAILED'));
+
+            return false;
+        }
+
+        return true;
+    }
+}
