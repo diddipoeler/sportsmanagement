7c57f6b7bf165e9794579f7015d7d7d166f5f051
diddipoeler <diddipoeler@gmx.de>
2026-08-21 22:18:08 +0200

    Add native age group list field

---
+++ admin/src/Field/AgegrouplistField.php
@@ -0,0 +1,49 @@
+<?php
+namespace Diddipoeler\Component\SportsManagement\Administrator\Field;
+
+\defined('_JEXEC') or die;
+
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Language\Text;
+
+final class AgegrouplistField extends SportsManagementListField
+{
+    protected $type = 'agegrouplist';
+
+    protected function getInput(): string
+    {
+        if (trim((string) ($this->element['onchange'] ?? '')) === '') {
+            $this->element['onchange'] = 'this.form.submit();';
+        }
+
+        return parent::getInput();
+    }
+
+    protected function getOptions(): array
+    {
+        $db = $this->getSportsManagementDatabase();
+        $query = $db->getQuery(true)
+            ->select([
+                $db->quoteName('id', 'value'),
+                $db->quoteName('name', 'text'),
+                $db->quoteName('country'),
+            ])
+            ->from($db->quoteName('#__sportsmanagement_agegroup'))
+            ->order($db->quoteName('name'));
+        $db->setQuery($query);
+
+        $options = [];
+
+        foreach ($db->loadObjectList() ?: [] as $item) {
+            $label = Text::_((string) $item->text);
+
+            if (!empty($item->country)) {
+                $label .= ' (' . (string) $item->country . ')';
+            }
+
+            $options[] = HTMLHelper::_('select.option', $item->value, $label);
+        }
+
+        return array_merge(parent::getOptions(), $options);
+    }
+}
