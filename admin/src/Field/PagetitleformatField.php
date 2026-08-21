7f4901e8c7874af16ef9cfd72365f16514745404
diddipoeler <diddipoeler@gmx.de>
2026-08-21 21:51:52 +0200

    Add native page title format field

---
+++ admin/src/Field/PagetitleformatField.php
@@ -0,0 +1,38 @@
+<?php
+namespace Diddipoeler\Component\SportsManagement\Administrator\Field;
+
+\defined('_JEXEC') or die;
+
+use Joomla\CMS\Factory;
+use Joomla\CMS\Form\Field\ListField;
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Language\Text;
+
+final class PagetitleformatField extends ListField
+{
+    protected $type = 'pagetitleformat';
+
+    protected function getOptions(): array
+    {
+        $language = Factory::getApplication()->getLanguage();
+        $language->load('com_sportsmanagement', JPATH_ADMINISTRATOR, $language->getTag(), true);
+
+        $keys = [
+            'COM_SPORTSMANAGEMENT_FES_PARAM_PAGE_TITLE_PROJECT',
+            'COM_SPORTSMANAGEMENT_FES_PARAM_PAGE_TITLE_PROJECT_LEAGUE',
+            'COM_SPORTSMANAGEMENT_FES_PARAM_PAGE_TITLE_PROJECT_LEAGUE_SEASON',
+            'COM_SPORTSMANAGEMENT_FES_PARAM_PAGE_TITLE_PROJECT_SEASON',
+            'COM_SPORTSMANAGEMENT_FES_PARAM_PAGE_TITLE_LEAGUE',
+            'COM_SPORTSMANAGEMENT_FES_PARAM_PAGE_TITLE_LEAGUE_SEASON',
+            'COM_SPORTSMANAGEMENT_FES_PARAM_PAGE_TITLE_SEASON',
+            'COM_SPORTSMANAGEMENT_FES_PARAM_PAGE_TITLE_NONE',
+        ];
+        $options = [];
+
+        foreach ($keys as $value => $key) {
+            $options[] = HTMLHelper::_('select.option', $value, Text::_($key));
+        }
+
+        return array_merge(parent::getOptions(), $options);
+    }
+}
