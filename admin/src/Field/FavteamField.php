ba8a256cfbf602b3a102e98be2c4c4698714aac2
diddipoeler <diddipoeler@gmx.de>
2026-08-21 21:46:38 +0200

    Add native favorite team field

---
+++ admin/src/Field/FavteamField.php
@@ -0,0 +1,46 @@
+<?php
+namespace Diddipoeler\Component\SportsManagement\Administrator\Field;
+
+\defined('_JEXEC') or die;
+
+final class FavteamField extends SportsManagementListField
+{
+    protected $type = 'Favteam';
+
+    protected function getOptions(): array
+    {
+        $app = \Joomla\CMS\Factory::getApplication();
+        $input = $app->getInput();
+        $option = $input->getCmd('option', 'com_sportsmanagement');
+        $projectId = $input->getCmd('layout') === 'edit'
+            ? $input->getInt('id', 0)
+            : (int) $app->getUserState($option . '.pid', 0);
+
+        if ($projectId <= 0) {
+            return parent::getOptions();
+        }
+
+        $db = $this->getSportsManagementDatabase();
+        $query = $db->getQuery(true)
+            ->select([
+                $db->quoteName('t.id', 'value'),
+                $db->quoteName('t.name', 'text'),
+            ])
+            ->from($db->quoteName('#__sportsmanagement_team', 't'))
+            ->join(
+                'INNER',
+                $db->quoteName('#__sportsmanagement_season_team_id', 'st')
+                . ' ON ' . $db->quoteName('st.team_id') . ' = ' . $db->quoteName('t.id')
+            )
+            ->join(
+                'INNER',
+                $db->quoteName('#__sportsmanagement_project_team', 'pt')
+                . ' ON ' . $db->quoteName('pt.team_id') . ' = ' . $db->quoteName('st.id')
+            )
+            ->where($db->quoteName('pt.project_id') . ' = ' . $projectId)
+            ->order($db->quoteName('t.name'));
+        $db->setQuery($query);
+
+        return array_merge(parent::getOptions(), $db->loadObjectList() ?: []);
+    }
+}
