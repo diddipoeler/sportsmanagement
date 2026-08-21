4e3f4e13fcfb637800b5f4a860434487edc2146c
diddipoeler <diddipoeler@gmx.de>
2026-08-21 18:20:20 +0200

    Bridge legacy Pictures table to Joomla 5/6 table

--- admin/tables/pictures.php
+++ admin/tables/pictures.php
@@ -1,39 +1,13 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage tables
- * @file       pictures.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-defined('_JEXEC') or die('Restricted access');
+/** Legacy compatibility bridge for the native administrator Pictures table. */
+\defined('_JEXEC') or die;
 
-/**
- * sportsmanagementTablepictures
- *
- * @package
- * @author
- * @copyright diddi
- * @version   2014
- * @access    public
- */
-class sportsmanagementTablepictures extends JSMTable
-{
-	/**
-	 * Constructor
-	 *
-	 * @param   object Database connector object
-	 *
-	 * @since 1.0
-	 */
-	function __construct(&$db)
-	{
-		$db = sportsmanagementHelper::getDBConnection();
-		parent::__construct('#__sportsmanagement_pictures', 'id', $db);
-	}
+use Diddipoeler\Component\SportsManagement\Administrator\Table\PicturesTable;
 
+if (!class_exists(PicturesTable::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Table/PicturesTable.php';
+}
 
+if (!class_exists('sportsmanagementTablepictures', false)) {
+    class_alias(PicturesTable::class, 'sportsmanagementTablepictures');
 }
