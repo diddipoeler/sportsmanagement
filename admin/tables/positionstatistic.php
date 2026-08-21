9693d7244e56e66d420e0321520b2f1e67c0eba9
diddipoeler <diddipoeler@gmx.de>
2026-08-21 19:35:08 +0200

    Use native position statistic table bridge

--- admin/tables/positionstatistic.php
+++ admin/tables/positionstatistic.php
@@ -1,58 +1,13 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage tables
- * @file       positionstatistic.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
 defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\Language\Text;
 
-/**
- * sportsmanagementTablePositionStatistic
- *
- * @package
- * @author
- * @copyright diddi
- * @version   2014
- * @access    public
- */
-class sportsmanagementTablePositionStatistic extends JSMTable
-{
-	/**
-	 * Constructor
-	 *
-	 * @param   object Database connector object
-	 *
-	 * @since 1.0
-	 */
-	function __construct(&$db)
-	{
-		$db = sportsmanagementHelper::getDBConnection();
-		parent::__construct('#__sportsmanagement_position_statistic', 'id', $db);
-	}
+use Diddipoeler\Component\SportsManagement\Administrator\Table\PositionstatisticTable;
 
-	/**
-	 * Overloaded check method to ensure data integrity
-	 *
-	 * @access public
-	 * @return boolean True on success
-	 * @since  1.0
-	 */
-	function check()
-	{
-		if (!($this->statistic_id && $this->projectteam_id && $this->match_id))
-		{
-			$this->setError(Text::_('CHECK FAILED'));
+if (!class_exists(PositionstatisticTable::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Table/SportsManagementTable.php';
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Table/PositionstatisticTable.php';
+}
 
-			return false;
-		}
-
-		return true;
-	}
-
+if (!class_exists('sportsmanagementTablePositionStatistic', false)) {
+    class_alias(PositionstatisticTable::class, 'sportsmanagementTablePositionStatistic');
 }
