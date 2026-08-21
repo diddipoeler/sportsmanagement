f0dd8b2d347e6d55ddfcde24bac776513b4c7662
diddipoeler <diddipoeler@gmx.de>
2026-08-21 17:53:46 +0200

    Remove JLoader from shared Joomla 5/6 MVC factory

--- admin/src/Service/SportsManagementMVCFactory.php
+++ admin/src/Service/SportsManagementMVCFactory.php
@@ -15,16 +15,20 @@ final class SportsManagementMVCFactory extends MVCFactory
     public function createModel($name, $prefix = '', array $config = [])
     {
         $model = parent::createModel($name, $prefix, $config);
+
         if ($model === null && $this->loadLegacyModel((string) $name, (string) $prefix)) {
             $model = parent::createModel($name, $prefix, $config);
         }
+
         $this->restoreSportsManagementDatabase($model);
+
         return $model;
     }
 
     public function createView($name, $prefix = '', $type = '', array $config = [])
     {
         $view = parent::createView($name, $prefix, $type, $config);
+
         if ($view !== null) {
             return $view;
         }
@@ -32,11 +36,13 @@ final class SportsManagementMVCFactory extends MVCFactory
         $prefix = $this->normalisePrefix((string) $prefix);
         $type = $type !== '' ? (string) $type : 'html';
         $basePath = $this->legacyBasePath($prefix);
+
         if ($basePath === null || !$this->loadLegacyView((string) $name, $prefix, $type)) {
             return null;
         }
 
         $config['template_path'] = $basePath . '/views/' . strtolower((string) $name) . '/tmpl';
+
         return parent::createView($name, $prefix, $type, $config);
     }
 
@@ -44,20 +50,26 @@ final class SportsManagementMVCFactory extends MVCFactory
     {
         $prefix = $this->normalisePrefix($prefix);
         $basePath = $this->legacyBasePath($prefix);
+
         if ($basePath === null) {
             return false;
         }
+
         $this->bootLegacy($prefix, $name);
         $file = $basePath . '/models/' . strtolower($name) . '.php';
+
         if (!is_file($file)) {
             return false;
         }
+
         require_once $file;
         $legacyClass = 'sportsmanagementModel' . ucfirst($name);
         $targetClass = 'Diddipoeler\\Component\\SportsManagement\\' . $prefix . '\\Model\\' . ucfirst($name) . 'Model';
+
         if (!class_exists($legacyClass, false) || class_exists($targetClass, false)) {
             return class_exists($targetClass, false);
         }
+
         return class_alias($legacyClass, $targetClass);
     }
 
@@ -65,20 +77,26 @@ final class SportsManagementMVCFactory extends MVCFactory
     {
         $prefix = $this->normalisePrefix($prefix);
         $basePath = $this->legacyBasePath($prefix);
+
         if ($basePath === null) {
             return false;
         }
+
         $this->bootLegacy($prefix, $name);
         $file = $basePath . '/views/' . strtolower($name) . '/view.' . strtolower($type) . '.php';
+
         if (!is_file($file)) {
             return false;
         }
+
         require_once $file;
         $legacyClass = 'sportsmanagementView' . ucfirst($name);
         $targetClass = 'Diddipoeler\\Component\\SportsManagement\\' . $prefix . '\\View\\' . ucfirst($name) . '\\' . ucfirst($type) . 'View';
+
         if (!class_exists($legacyClass, false) || class_exists($targetClass, false)) {
             return class_exists($targetClass, false);
         }
+
         return class_alias($legacyClass, $targetClass);
     }
 
@@ -86,8 +104,10 @@ final class SportsManagementMVCFactory extends MVCFactory
     {
         if ($prefix === 'Administrator') {
             AdministratorLegacyBootstrap::bootForView($view);
+
             return;
         }
+
         SiteLegacyBootstrap::bootForView($view);
     }
 
@@ -96,6 +116,7 @@ final class SportsManagementMVCFactory extends MVCFactory
         if ($prefix === '') {
             $prefix = Factory::getApplication()->getName();
         }
+
         return ucfirst(strtolower($prefix));
     }
 
@@ -113,15 +134,27 @@ final class SportsManagementMVCFactory extends MVCFactory
         if (!$model instanceof DatabaseAwareInterface) {
             return;
         }
+
         if (!class_exists('sportsmanagementHelper')) {
-            \JLoader::register('sportsmanagementHelper', JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/helpers/sportsmanagement.php');
+            $helperFile = JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/helpers/sportsmanagement.php';
+
+            if (is_file($helperFile)) {
+                require_once $helperFile;
+            }
         }
+
         try {
+            if (!class_exists('sportsmanagementHelper')) {
+                return;
+            }
+
             $database = \sportsmanagementHelper::getDBConnection();
+
             if ($database instanceof DatabaseInterface) {
                 $model->setDatabase($database);
             }
         } catch (\Throwable) {
+            // Keep Joomla's injected database connection as fallback.
         }
     }
 }
