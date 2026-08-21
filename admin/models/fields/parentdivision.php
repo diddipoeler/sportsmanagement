d5df4ed874c9b8b6746208491a89cf35076d61b1
diddipoeler <diddipoeler@gmx.de>
2026-08-21 21:12:45 +0200

    Bridge parent division field to native class

--- admin/models/fields/parentdivision.php
+++ admin/models/fields/parentdivision.php
@@ -1,58 +1,12 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage fields
- * @file       parentdivision.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-defined('_JEXEC') or die('Restricted access');
+\defined('_JEXEC') or die;
 
-use Joomla\CMS\Factory;
-use Joomla\CMS\Form\Field\ListField;
-use Joomla\Database\DatabaseInterface;
+use Diddipoeler\Component\SportsManagement\Administrator\Field\ParentdivisionField;
 
-/**
- * FormFieldparentdivision
- *
- * @package
- * @author
- * @copyright diddi
- * @version   2014
- * @access    public
- */
-class JFormFieldparentdivision extends ListField
-{
-	public $type = 'parentdivision';
+if (!class_exists(ParentdivisionField::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Field/ParentdivisionField.php';
+}
 
-	/**
-	 * Method to get the field options.
-	 *
-	 * @return array
-	 */
-	protected function getOptions()
-	{
-		$app       = Factory::getApplication();
-		$option    = $app->getInput()->getCmd('option');
-		$projectId = (int) $app->getUserState($option . '.pid', 0);
-		$options   = array();
-
-		if ($projectId)
-		{
-			$db    = Factory::getContainer()->get(DatabaseInterface::class);
-			$query = $db->createQuery();
-			$query->select('dv.id AS value, dv.name AS text');
-			$query->from('#__sportsmanagement_division AS dv');
-			$query->where('dv.project_id = ' . $projectId);
-			$query->where('dv.parent_id = 0');
-			$query->order('dv.ordering ASC');
-			$db->setQuery($query);
-			$options = $db->loadObjectList();
-		}
-
-		return array_merge(parent::getOptions(), $options);
-	}
+if (!class_exists('JFormFieldparentdivision', false)) {
+    class_alias(ParentdivisionField::class, 'JFormFieldparentdivision');
 }
