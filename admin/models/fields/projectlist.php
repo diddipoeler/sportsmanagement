1616bbfb0f7b1a2a30916e24f5860c288a03d605
diddipoeler <diddipoeler@gmx.de>
2026-08-21 20:58:17 +0200

    Bridge legacy project list field

--- admin/models/fields/projectlist.php
+++ admin/models/fields/projectlist.php
@@ -1,51 +1,17 @@
 <?php
-/**
- *
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage fields
- * @file       projectlist.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-
+/** Compatibility bridge for the Joomla 5/6 project list field. */
 defined('_JEXEC') or die('Restricted access');
 
-use Joomla\CMS\Factory;
-use Joomla\CMS\Form\Field\ListField;
-use Joomla\Database\DatabaseInterface;
+use Diddipoeler\Component\SportsManagement\Administrator\Field\ProjectlistField;
 
-/**
- * FormFieldprojectlist
- *
- * @package
- * @author
- * @copyright diddi
- * @version   2014
- * @access    public
- */
-class JFormFieldprojectlist extends ListField
-{
-	public $type = 'projectlist';
+if (!class_exists(ProjectlistField::class)) {
+    $fieldFile = JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Field/ProjectlistField.php';
 
-	/**
-	 * Method to get the field options.
-	 *
-	 * @return array
-	 */
-	protected function getOptions()
-	{
-		$db    = Factory::getContainer()->get(DatabaseInterface::class);
-		$query = $db->createQuery();
-		$query->select('l.id AS value, l.name AS text');
-		$query->from('#__sportsmanagement_project AS l');
-		$query->order('l.name');
-		$db->setQuery($query);
-		$options = $db->loadObjectList();
+    if (is_file($fieldFile)) {
+        require_once $fieldFile;
+    }
+}
 
-		return array_merge(parent::getOptions(), $options);
-	}
+if (class_exists(ProjectlistField::class) && !class_exists('JFormFieldprojectlist', false)) {
+    class_alias(ProjectlistField::class, 'JFormFieldprojectlist');
 }
