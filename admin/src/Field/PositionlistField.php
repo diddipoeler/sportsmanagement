8ab3bcb7c9d69c9b9952335393379938d1ece099
diddipoeler <diddipoeler@gmx.de>
2026-08-21 21:06:43 +0200

    Add native position list field

---
+++ admin/src/Field/PositionlistField.php
@@ -0,0 +1,48 @@
+<?php
+namespace Diddipoeler\Component\SportsManagement\Administrator\Field;
+
+\defined('_JEXEC') or die;
+
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Language\Text;
+
+final class PositionlistField extends SportsManagementListField
+{
+    protected $type = 'positionlist';
+
+    protected function getOptions(): array
+    {
+        $db = $this->getSportsManagementDatabase();
+        $query = $db->getQuery(true)
+            ->select([
+                $db->quoteName('pos.id', 'value'),
+                $db->quoteName('pos.name', 'text'),
+            ])
+            ->from($db->quoteName('#__sportsmanagement_position', 'pos'))
+            ->join(
+                'INNER',
+                $db->quoteName('#__sportsmanagement_sports_type', 's')
+                . ' ON ' . $db->quoteName('s.id') . ' = ' . $db->quoteName('pos.sports_type_id')
+            )
+            ->where($db->quoteName('pos.published') . ' = 1')
+            ->order([
+                $db->quoteName('pos.ordering'),
+                $db->quoteName('pos.name'),
+            ]);
+        $db->setQuery($query);
+
+        try {
+            $items = $db->loadObjectList() ?: [];
+        } catch (\Throwable $e) {
+            $items = [];
+        }
+
+        $options = [];
+
+        foreach ($items as $item) {
+            $options[] = HTMLHelper::_('select.option', $item->value, Text::_($item->text));
+        }
+
+        return array_merge(parent::getOptions(), $options);
+    }
+}
