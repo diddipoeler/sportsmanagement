2dadb7f55a4e09e36a1b4f3c1f9143a959d14a77
diddipoeler <diddipoeler@gmx.de>
2026-08-21 19:54:19 +0200

    Bridge legacy prediction group table

--- admin/tables/predictiongroup.php
+++ admin/tables/predictiongroup.php
@@ -1,67 +1,17 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage tables
- * @file       division.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
+/** Legacy compatibility bridge for the native PredictiongroupTable. */
 defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\Language\Text;
-use Joomla\CMS\Filter\OutputFilter;
 
-/**
- * sportsmanagementTablePredictionGroup
- *
- * @package
- * @author
- * @copyright diddi
- * @version   2013
- * @access    public
- */
-class sportsmanagementTablePredictionGroup extends JSMTable
-{
+use Diddipoeler\Component\SportsManagement\Administrator\Table\PredictiongroupTable;
 
-	/**
-	 * Constructor
-	 *
-	 * @param   object Database connector object
-	 *
-	 * @since 1.0
-	 */
-	function __construct(&$db)
-	{
-		$db = sportsmanagementHelper::getDBConnection();
-		parent::__construct('#__sportsmanagement_prediction_groups', 'id', $db);
-	}
+if (!class_exists(PredictiongroupTable::class)) {
+    $tableFile = JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Table/PredictiongroupTable.php';
 
-	/**
-	 * Overloaded check method to ensure data integrity
-	 *
-	 * @access public
-	 * @return boolean True on success
-	 * @since  1.0
-	 */
-	function check()
-	{
-		if (trim($this->name) == '')
-		{
-			$this->setError(Text::_('CHECK FAILED - Empty name of prediction game'));
+    if (is_file($tableFile)) {
+        require_once $tableFile;
+    }
+}
 
-			return false;
-		}
-
-		$alias = OutputFilter::stringURLSafe($this->name);
-
-		if (empty($this->alias) || $this->alias === $alias)
-		{
-			$this->alias = $alias;
-		}
-
-		return true;
-	}
-
+if (class_exists(PredictiongroupTable::class) && !class_exists('sportsmanagementTablePredictionGroup', false)) {
+    class_alias(PredictiongroupTable::class, 'sportsmanagementTablePredictionGroup');
 }
