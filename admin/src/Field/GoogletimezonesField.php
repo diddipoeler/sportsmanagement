e2ca9ddb0418b0dbc6cc8a5cec1da27b408c69c9
diddipoeler <diddipoeler@gmx.de>
2026-08-21 22:01:00 +0200

    Replace static Google timezone list with native IANA data

---
+++ admin/src/Field/GoogletimezonesField.php
@@ -0,0 +1,58 @@
+<?php
+namespace Diddipoeler\Component\SportsManagement\Administrator\Field;
+
+\defined('_JEXEC') or die;
+
+use DateTimeImmutable;
+use DateTimeZone;
+use Joomla\CMS\Form\Field\ListField;
+use Joomla\CMS\HTML\HTMLHelper;
+
+final class GoogletimezonesField extends ListField
+{
+    protected $type = 'Googletimezones';
+
+    protected function getOptions(): array
+    {
+        $now = new DateTimeImmutable('now', new DateTimeZone('UTC'));
+        $zones = [];
+
+        foreach (DateTimeZone::listIdentifiers(DateTimeZone::ALL_WITH_BC) as $identifier) {
+            try {
+                $timezone = new DateTimeZone($identifier);
+                $offset = $timezone->getOffset($now);
+            } catch (\Throwable) {
+                continue;
+            }
+
+            $zones[] = [
+                'identifier' => $identifier,
+                'offset' => $offset,
+                'label' => $this->formatOffset($offset) . ' ' . str_replace('_', ' ', $identifier),
+            ];
+        }
+
+        usort(
+            $zones,
+            static fn(array $left, array $right): int => [$left['offset'], $left['identifier']] <=> [$right['offset'], $right['identifier']]
+        );
+
+        $options = [HTMLHelper::_('select.option', '', '')];
+
+        foreach ($zones as $zone) {
+            $options[] = HTMLHelper::_('select.option', $zone['identifier'], $zone['label']);
+        }
+
+        return array_merge(parent::getOptions(), $options);
+    }
+
+    private function formatOffset(int $seconds): string
+    {
+        $sign = $seconds < 0 ? '-' : '+';
+        $seconds = abs($seconds);
+        $hours = intdiv($seconds, 3600);
+        $minutes = intdiv($seconds % 3600, 60);
+
+        return sprintf('(UTC%s%02d:%02d)', $sign, $hours, $minutes);
+    }
+}
