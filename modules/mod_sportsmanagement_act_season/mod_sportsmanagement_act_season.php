c63236a462801d71e4a21ced6f802fdcb384dc27
diddipoeler <diddipoeler@gmx.de>
2026-08-21 20:30:47 +0200

    Inject database fallback in current-season entry

--- modules/mod_sportsmanagement_act_season/mod_sportsmanagement_act_season.php
+++ modules/mod_sportsmanagement_act_season/mod_sportsmanagement_act_season.php
@@ -1,11 +1,12 @@
 <?php
 /** Joomla 5/6 compatibility entry point for the current-season module. */
-defined('_JEXEC') or die;
+\defined('_JEXEC') or die;
 
 use Diddipoeler\Module\SportsManagementActSeason\Site\Helper\ActSeasonHelper;
 use Joomla\CMS\Component\ComponentHelper;
 use Joomla\CMS\Factory;
 use Joomla\CMS\Helper\ModuleHelper;
+use Joomla\Database\DatabaseInterface;
 
 if (!class_exists(ActSeasonHelper::class)) {
     require_once __DIR__ . '/src/Helper/ActSeasonHelper.php';
@@ -13,12 +14,14 @@ if (!class_exists(ActSeasonHelper::class)) {
 
 $app = Factory::getApplication();
 $app->getLanguage()->load('com_sportsmanagement', JPATH_ADMINISTRATOR, null, true);
-
 $componentParams = ComponentHelper::getParams('com_sportsmanagement');
+/** @var DatabaseInterface $database */
+$database = Factory::getContainer()->get(DatabaseInterface::class);
 $result = (new ActSeasonHelper())->getData(
     $componentParams->get('current_season', []),
     $componentParams,
-    $app
+    $app,
+    $database
 );
 
 $list = $result['list'];
