bb5e60564b25b91a82151b2c5b3cd743b5ab331d
diddipoeler <diddipoeler@gmx.de>
2026-08-21 21:12:28 +0200

    Bridge sort order field to native class

--- admin/models/fields/sortorder.php
+++ admin/models/fields/sortorder.php
@@ -1,58 +1,12 @@
 <?php
-/**
- *
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage fields
- * @file       sortorder.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
+\defined('_JEXEC') or die;
 
-defined('_JEXEC') or die('Restricted access');
+use Diddipoeler\Component\SportsManagement\Administrator\Field\SortorderField;
 
-use Joomla\CMS\Component\ComponentHelper;
-use Joomla\CMS\Form\Field\ListField;
-use Joomla\CMS\HTML\HTMLHelper;
+if (!class_exists(SortorderField::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Field/SortorderField.php';
+}
 
-/**
- * FormFieldsortorder
- *
- * @package
- * @author    diddi
- * @copyright 2014
- * @version   $Id$
- * @access    public
- */
-class JFormFieldsortorder extends ListField
-{
-	/**
-	 * field type
-	 *
-	 * @var string
-	 */
-	public $type = 'sortorder';
-
-	/**
-	 * Method to get the field options.
-	 *
-	 * @return array  The field option objects.
-	 *
-	 * @since 11.1
-	 */
-	protected function getOptions()
-	{
-		$options = array();
-		$templateSortOrders = (int) ComponentHelper::getParams('com_sportsmanagement')->get('template_sort_orders', 0);
-
-		for ($i = 1; $i <= $templateSortOrders; $i++)
-		{
-			$options[] = HTMLHelper::_('select.option', $i, $i, 'value', 'text');
-		}
-
-		return array_merge(parent::getOptions(), $options);
-	}
+if (!class_exists('JFormFieldsortorder', false)) {
+    class_alias(SortorderField::class, 'JFormFieldsortorder');
 }
