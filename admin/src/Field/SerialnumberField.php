248d847afa9366a20d63381364702a0b400b0efa
diddipoeler <diddipoeler@gmx.de>
2026-08-21 21:13:46 +0200

    Add native serial number field

---
+++ admin/src/Field/SerialnumberField.php
@@ -0,0 +1,45 @@
+<?php
+namespace Diddipoeler\Component\SportsManagement\Administrator\Field;
+
+\defined('_JEXEC') or die;
+
+use Joomla\CMS\Form\FormField;
+
+final class SerialnumberField extends FormField
+{
+    protected $type = 'serialnumber';
+
+    protected function getInput(): string
+    {
+        $value = trim((string) $this->value);
+
+        if ($value === '') {
+            $value = $this->generateSerialNumber();
+            $this->value = $value;
+        }
+
+        return '<input type="text" id="'
+            . htmlspecialchars($this->id, ENT_QUOTES, 'UTF-8')
+            . '" name="'
+            . htmlspecialchars($this->name, ENT_QUOTES, 'UTF-8')
+            . '" value="'
+            . htmlspecialchars($value, ENT_QUOTES, 'UTF-8')
+            . '" />';
+    }
+
+    private function generateSerialNumber(): string
+    {
+        $template = 'XX99-XX99-99XX-99XX-XXXX-99XX';
+        $serial = '';
+
+        foreach (str_split($template) as $token) {
+            $serial .= match ($token) {
+                'X' => chr(random_int(65, 90)),
+                '9' => (string) random_int(0, 9),
+                default => $token,
+            };
+        }
+
+        return $serial;
+    }
+}
