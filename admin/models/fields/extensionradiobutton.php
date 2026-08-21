cdb0d95a08ad25b27741c74edd6d045ef1f244f7
diddipoeler <diddipoeler@gmx.de>
2026-08-21 21:47:11 +0200

    Bridge extension radio field to native class

--- admin/models/fields/extensionradiobutton.php
+++ admin/models/fields/extensionradiobutton.php
@@ -1,82 +1,12 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- * @version   1.0.58
- * @file
- * @author    diddipoeler, stony, svdoldie (diddipoeler@gmx.de)
- * @copyright Copyright: 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license   GNU General Public License version 2 or later; see LICENSE.txt
- *
- * https://docs.joomla.org/Creating_a_custom_form_field_type
- * https://hotexamples.com/examples/-/FormFieldRadio/-/php-FormFieldradio-class-examples.html
- */
-defined('_JEXEC') or die;
-use Joomla\CMS\Form\Field\RadioField;
+\defined('_JEXEC') or die;
 
-/** welche joomla version ? */
-if (version_compare(JVERSION, '4.0.0', 'ge'))
-{
-	class JSMFormField extends RadioField
-	{
+use Diddipoeler\Component\SportsManagement\Administrator\Field\ExtensionradiobuttonField;
 
-	}
-}
-else
-{
-	include_once JPATH_LIBRARIES . '/joomla/form/fields/radio.php';
-	class JSMFormField extends JFormFieldRadio
-	{
-
-	}
+if (!class_exists(ExtensionradiobuttonField::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Field/ExtensionradiobuttonField.php';
 }
 
-/**
- * FormFieldExtensionRadioButton
- *
- * @package
- * @author    Dieter Plöger
- * @copyright 2017
- * @version   $Id$
- * @access    public
- */
-class JFormFieldExtensionRadioButton extends JSMFormField
-{
-	public $type = 'ExtensionRadioButton';
-
-	/**
-	 * FormFieldExtensionRadioButton::getLabel()
-	 *
-	 * @return void
-	 */
-	protected function getLabel()
-	{
-
-		return parent::getLabel();
-	}
-
-
-	/**
-	 * FormFieldExtensionRadioButton::getInput()
-	 *
-	 * @return void
-	 */
-	protected function getInput()
-	{
-
-		/** welche joomla version ? */
-		if (version_compare(JVERSION, '4.0.0', 'ge'))
-		{
-			// $this->class = "switcher btn-group btn-group-yesno";
-            $this->layout = "joomla.form.field.radio.switcher";
-			$this->type = "radio";
-		}
-		else
-		{
-			$this->class = "radio btn-group btn-group-yesno";
-		}
-
-		return parent::getInput();
-	}
-
-
+if (!class_exists('JFormFieldExtensionRadioButton', false)) {
+    class_alias(ExtensionradiobuttonField::class, 'JFormFieldExtensionRadioButton');
 }
