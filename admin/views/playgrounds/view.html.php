e17476cdbf9f549ad7fe7794cf81dd577aaf75e9
diddipoeler <diddipoeler@gmx.de>
2026-08-21 15:39:27 +0200

    Bridge playgrounds to native Joomla view

--- admin/views/playgrounds/view.html.php
+++ admin/views/playgrounds/view.html.php
@@ -1,84 +1,16 @@
 <?php
 /**
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage playgrounds
- * @file       view.html.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * SportsManagement legacy administrator view bridge.
+ * The active Joomla 5/6 implementation lives in admin/src/View/Playgrounds/HtmlView.php.
  */
-defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\HTML\HTMLHelper;
-use Joomla\CMS\Table\Table;
-use Joomla\CMS\Language\Text;
-use Joomla\CMS\Toolbar\ToolbarHelper;
-use Joomla\CMS\Toolbar\Toolbar;
-use Joomla\CMS\Log\Log;
-use Joomla\CMS\Form\Form;
+\defined('_JEXEC') or die;
 
-/**
- * sportsmanagementViewPlaygrounds
- *
- * @package
- * @author    diddi
- * @copyright 2014
- * @version   $Id$
- * @access    public
- */
-class sportsmanagementViewPlaygrounds extends sportsmanagementView
-{
+use Diddipoeler\Component\SportsManagement\Administrator\View\Playgrounds\HtmlView;
 
-	/**
-	 * sportsmanagementViewPlaygrounds::init()
-	 *
-	 * @return void
-	 */
-	public function init()
-	{
-		$this->table = Table::getInstance('playground', 'sportsmanagementTable');
+if (!class_exists(HtmlView::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/View/Playgrounds/HtmlView.php';
+}
 
-		/** Build the html options for nation */
-		$nation[] = HTMLHelper::_('select.option', '0', Text::_('COM_SPORTSMANAGEMENT_GLOBAL_SELECT_COUNTRY'));
-
-		if ($res = JSMCountries::getCountryOptions())
-		{
-			$nation              = array_merge($nation, $res);
-			$this->search_nation = $res;
-		}
-
-		$lists['nation']  = $nation;
-		$lists['nation2'] = JHtmlSelect::genericlist(
-			$nation,
-			'filter_search_nation',
-			'class="inputbox" style="width:140px; " onchange="this.form.submit();"',
-			'value',
-			'text',
-			$this->state->get('filter.search_nation')
-		);
-
-		$this->lists = $lists;
-
-		if (!array_key_exists('search_mode', $this->lists))
-		{
-			$this->lists['search_mode'] = '';
-		}
-	}
-
-
-	/**
-	 * sportsmanagementViewPlaygrounds::addToolbar()
-	 *
-	 * @return void
-	 */
-	protected function addToolbar()
-	{
-		$this->title = Text::_('COM_SPORTSMANAGEMENT_ADMIN_PLAYGROUNDS_TITLE');
-		ToolbarHelper::editList('playground.edit');
-		ToolbarHelper::addNew('playground.add');
-		ToolbarHelper::custom('playground.import', 'upload', 'upload', Text::_('JTOOLBAR_UPLOAD'), false);
-		ToolbarHelper::archiveList('playground.export', Text::_('JTOOLBAR_EXPORT'));
-		parent::addToolbar();
-	}
+if (!class_exists('sportsmanagementViewPlaygrounds', false)) {
+    class_alias(HtmlView::class, 'sportsmanagementViewPlaygrounds');
 }
