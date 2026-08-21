84e0428ba0ce5b51e8e6d0adee1c4fc3eca6c3a9
diddipoeler <diddipoeler@gmx.de>
2026-08-21 12:54:47 +0200

    Add Joomla 5/6 service provider for comments plugin

---
+++ plugins/jsm_jcomments/services/provider.php
@@ -0,0 +1,26 @@
+<?php
+\defined('_JEXEC') or die;
+
+use Diddipoeler\Plugin\Content\SportsmanagementComments\Extension\SportsmanagementComments;
+use Joomla\CMS\Extension\PluginInterface;
+use Joomla\CMS\Factory;
+use Joomla\CMS\Plugin\PluginHelper;
+use Joomla\DI\Container;
+use Joomla\DI\ServiceProviderInterface;
+
+return new class () implements ServiceProviderInterface {
+    public function register(Container $container): void
+    {
+        $container->set(
+            PluginInterface::class,
+            static function (Container $container): PluginInterface {
+                $plugin = new SportsmanagementComments(
+                    (array) PluginHelper::getPlugin('content', 'sportsmanagement_comments')
+                );
+                $plugin->setApplication(Factory::getApplication());
+
+                return $plugin;
+            }
+        );
+    }
+};
