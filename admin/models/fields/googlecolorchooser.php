f0e7e9c0dec5ea308ad3f010b55f83359cdd8cd0
diddipoeler <diddipoeler@gmx.de>
2026-08-21 21:59:22 +0200

    Bridge Google color chooser field to native class

--- admin/models/fields/googlecolorchooser.php
+++ admin/models/fields/googlecolorchooser.php
@@ -1,92 +1,12 @@
 <?php
-/**
- *
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage fields
- * @file       googlecolorchooser.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
+\defined('_JEXEC') or die;
 
-defined('_JEXEC') or die();
+use Diddipoeler\Component\SportsManagement\Administrator\Field\GooglecolorchooserField;
 
-use Joomla\CMS\Factory;
-use Joomla\CMS\Uri\Uri;
-use Joomla\CMS\Form\FormField;
+if (!class_exists(GooglecolorchooserField::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Field/GooglecolorchooserField.php';
+}
 
-/**
- * JFormFieldGoogleColorChooser
- *
- * @package
- * @author    Dieter Plöger
- * @copyright 2019
- * @version   $Id$
- * @access    public
- */
-class JFormFieldGoogleColorChooser extends \JFormFieldText
-{
-	protected $type = 'GoogleColorChooser';
-
-	private $googleColors = array(
-		'A32929'
-	, 'B1365F'
-	, '7A367A'
-	, '5229A3'
-	, '29527A'
-	, '2952A3'
-	, '1B887A'
-	, '28754E'
-	, '0D7813'
-	, '528800'
-	, '88880E'
-	, 'AB8B00'
-	, 'BE6D00'
-	, 'B1440E'
-	, '865A5A'
-	, '705770'
-	, '4E5D6C'
-	, '5A6986'
-	, '4A716C'
-	, '6E6E41'
-	, '8D6F47');
-
-	/**
-	 * JFormFieldGoogleColorChooser::getInput()
-	 *
-	 * @return
-	 */
-	public function getInput()
-	{
-		$document = Factory::getDocument();
-		$document->addScript(Uri::base() . 'components/com_sportsmanagement/libraries/jscolor/jscolor.js');
-
-		$buffer = "<input type=\"text\" name=\"" . $this->name . "\" id=\"" . $this->id . "\" readonly=\"readonly\" class=\"inputbox\" \n";
-		$buffer .= "size=\"100%\" value=\"" . $this->value . "\" style=\"background-color: " . $this->value . "" . "\" />\n";
-		$buffer .= "<br CLEAR=\"both\"/><label id=\"jform_colors-lbl\" title=\"\" for=\"jform_color\"></label><table><tbody>\n";
-
-		for ($i = 0; $i < count($this->googleColors); $i++)
-		{
-			if ($i % 7 == 0)
-			{
-				$buffer .= "<tr>\n";
-			}
-
-			$c      = $this->googleColors[$i];
-			$cFaded = $c;
-			$buffer .= "<td onmouseover=\"this.style.cursor='pointer'\" onclick=\"document.getElementById('" . $this->id . "').style.backgroundColor = '#" . $cFaded . "';document.getElementById('" . $this->id . "').value = '#" . $c . "';\" style=\"background-color: #" . $cFaded . ";width: 20px;\">" . $c . "</td>\n";
-
-			if ($i % 7 == 6)
-			{
-				$buffer .= "</tr>\n";
-			}
-		}
-
-		$buffer .= "</tbody></table>\n";
-
-		return $buffer;
-	}
+if (!class_exists('JFormFieldGoogleColorChooser', false)) {
+    class_alias(GooglecolorchooserField::class, 'JFormFieldGoogleColorChooser');
 }
