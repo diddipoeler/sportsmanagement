9b7dd9acd3c473fd1db980331a1111212cec0747
diddipoeler <diddipoeler@gmx.de>
2026-08-21 17:15:07 +0200

    Bridge event view to native Joomla 5/6 view

--- site/views/event/view.html.php
+++ site/views/event/view.html.php
@@ -1,44 +1,14 @@
 <?php
-/**
- * GCalendar is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * GCalendar is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with GCalendar.  If not, see <http://www.gnu.org/licenses/>.
- *
- * @package   GCalendar
- * @author    Digital Peak http://www.digital-peak.com
- * @copyright Copyright (C) 2007 - 2013 Digital Peak. All rights reserved.
- * @license   http://www.gnu.org/licenses/gpl.html GNU/GPL
- */
+/** Legacy compatibility bridge for the native Joomla 5/6 event view. */
+defined('_JEXEC') or die('Restricted access');
 
-defined('_JEXEC') or die();
-use Joomla\CMS\MVC\View\HtmlView;
+use Diddipoeler\Component\SportsManagement\Site\View\Event\HtmlView;
 
-// JLoader::import('components.com_gcalendar.libraries.gcalendar.view', JPATH_SITE);
-JLoader::import('joomla.application.component.view');
-JLoader::import('components.com_sportsmanagement.libraries.GCalendar.GCalendarZendHelper', JPATH_ADMINISTRATOR);
-JLoader::import('components.com_sportsmanagement.libraries.dbutil', JPATH_ADMINISTRATOR);
-JLoader::import('components.com_sportsmanagement.libraries.util', JPATH_ADMINISTRATOR);
+if (!class_exists(HtmlView::class)) {
+    require_once JPATH_SITE . '/components/com_sportsmanagement/src/View/SportsManagementHtmlView.php';
+    require_once JPATH_SITE . '/components/com_sportsmanagement/src/View/Event/HtmlView.php';
+}
 
-class sportsmanagementViewEvent extends HtmlView
-{
-	public function display($tpl = null)
-	{
-		$this->event = $this->get('GCalendar');
-		parent::display($tpl);
-	}
-
-	//	public function init()
-	//    {
-	//		$this->event = $this->get('GCalendar');
-	//	}
-
+if (!class_exists('sportsmanagementViewEvent', false)) {
+    class_alias(HtmlView::class, 'sportsmanagementViewEvent');
 }
