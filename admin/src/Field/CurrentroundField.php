53178202167e6b7747371d55805d010afd917231
diddipoeler <diddipoeler@gmx.de>
2026-08-21 20:56:22 +0200

    Add native current round field

---
+++ admin/src/Field/CurrentroundField.php
@@ -0,0 +1,49 @@
+<?php
+namespace Diddipoeler\Component\SportsManagement\Administrator\Field;
+
+\defined('_JEXEC') or die;
+
+use Joomla\CMS\Factory;
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Language\Text;
+
+final class CurrentroundField extends SportsManagementListField
+{
+    protected $type = 'Currentround';
+
+    protected function getOptions(): array
+    {
+        $projectId = Factory::getApplication()->getInput()->getInt('id', 0);
+
+        if ($projectId <= 0) {
+            return parent::getOptions();
+        }
+
+        $db = $this->getSportsManagementDatabase();
+        $query = $db->getQuery(true)
+            ->select([
+                $db->quoteName('id'),
+                $db->quoteName('name'),
+                $db->quoteName('round_date_first'),
+            ])
+            ->from($db->quoteName('#__sportsmanagement_round'))
+            ->where($db->quoteName('project_id') . ' = ' . $projectId)
+            ->order([
+                $db->quoteName('roundcode'),
+                $db->quoteName('round_date_first'),
+            ]);
+        $db->setQuery($query);
+
+        $options = [];
+
+        foreach ($db->loadObjectList() ?: [] as $item) {
+            $name = trim((string) $item->name);
+            $label = $name !== ''
+                ? $name . ' (' . (string) $item->round_date_first . ')'
+                : Text::_('COM_SPORTSMANAGEMENT_GLOBAL_MATCHDAY_NAME') . ' ' . (int) $item->id;
+            $options[] = HTMLHelper::_('select.option', (int) $item->id, $label);
+        }
+
+        return array_merge(parent::getOptions(), $options);
+    }
+}
