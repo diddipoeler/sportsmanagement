f62386adce2c2599dbda94b1ae3ca31f3ef65cef
diddipoeler <diddipoeler@gmx.de>
2026-08-21 17:03:35 +0200

    Modernize compact Google Calendar JSON output

--- site/views/jsonfeed/tmpl/module.php
+++ site/views/jsonfeed/tmpl/module.php
@@ -1,103 +1,97 @@
 <?php
 /**
- * GCalendar is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * GCalendar is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with GCalendar.  If not, see <http://www.gnu.org/licenses/>.
- *
- * @package   GCalendar
- * @author    Digital Peak http://www.digital-peak.com
- * @copyright Copyright (C) 2007 - 2013 Digital Peak. All rights reserved.
- * @license   http://www.gnu.org/licenses/gpl.html GNU/GPL
+ * Compact-by-day JSON output for the SportsManagement Google Calendar module.
  */
 
-defined('_JEXEC') or die();
+defined('_JEXEC') or die;
 
-use Joomla\CMS\Language\Text;
-use Joomla\CMS\Router\Route;
 use Joomla\CMS\Factory;
-use Joomla\CMS\Component\ComponentHelper;
+use Joomla\CMS\Router\Route;
 
-$document = Factory::getDocument();
-$document->setMimeEncoding('application/json');
+Factory::getApplication()->getDocument()->setMimeEncoding('application/json');
 
-$tmp = array();
+$grouped = [];
 
-if (!empty($this->calendars))
-{
-	foreach ($this->calendars as $calendar)
-	{
-		if (empty($calendar))
-		{
-			continue;
-		}
+foreach ($this->events as $event) {
+    $startValue = (string) ($event['start'] ?? '');
 
+    if ($startValue === '') {
+        continue;
+    }
 
-		foreach ($calendar as $item)
-		{
-			$start = clone $item->getStartDate();
-			$end   = clone $item->getEndDate();
+    try {
+        $start = new DateTimeImmutable($startValue);
+    } catch (Throwable) {
+        continue;
+    }
 
-			do
-			{
-				$date = $start->format('Y-m-d', true);
+    $dates = [$start->format('Y-m-d')];
 
-				if (!key_exists($date, $tmp))
-				{
-					$tmp[$date] = array();
-				}
+    if (!empty($event['allDay']) && !empty($event['end'])) {
+        try {
+            $cursor = $start->setTime(0, 0)->modify('+1 day');
+            $end = (new DateTimeImmutable((string) $event['end']))->setTime(0, 0);
 
-				$tmp[$date][] = $item;
-				$start->modify("+1 day");
-			} while ($start < $end);
-		}
-	}
+            while ($cursor < $end) {
+                $dates[] = $cursor->format('Y-m-d');
+                $cursor = $cursor->modify('+1 day');
+            }
+        } catch (Throwable) {
+            // A malformed end value must not break the complete feed.
+        }
+    }
+
+    foreach ($dates as $date) {
+        $grouped[$date] ??= [];
+        $grouped[$date][] = $event;
+    }
 }
 
-$params = clone ComponentHelper::getParams('com_sportsmanagement');
-$params->set('show_event_title', 1);
-$data = array();
+ksort($grouped);
+$data = [];
 
-foreach ($tmp as $date => $events)
-{
-	$linkIDs = array();
-	$itemId  = '';
+foreach ($grouped as $date => $events) {
+    $calendarIds = array_values(array_unique(array_filter(array_map(
+        static fn (array $event): int => (int) ($event['gcid'] ?? 0),
+        $events
+    ))));
+    $titles = array_values(array_filter(array_map(
+        static fn (array $event): string => trim((string) ($event['title'] ?? '')),
+        $events
+    ), 'strlen'));
+    $description = '<strong>' . count($events) . '</strong><ul>';
 
-	foreach ($events as $event)
-	{
-		$linkIDs[$event->getParam('gcid')] = $event->getParam('gcid');
+    foreach ($titles as $title) {
+        $description .= '<li>' . htmlspecialchars($title, ENT_QUOTES, 'UTF-8') . '</li>';
+    }
 
-		$id = jsmGCalendarUtil::getItemId($event->getParam('gcid'), true);
+    $description .= '</ul>';
+    $firstEvent = $events[0] ?? [];
+    $url = trim((string) ($firstEvent['htmlLink'] ?? ''));
 
-		if (!empty($id))
-		{
-			$itemId = '&Itemid=' . $id;
-		}
-	}
+    if ($url === '' || !preg_match('#^https?://#i', $url)) {
+        [$year, $month, $day] = array_pad(explode('-', $date), 3, '');
+        $url = Route::_(
+            'index.php?option=com_sportsmanagement&view=gcalendar&gcids=' . implode(',', $calendarIds),
+            false
+        ) . '#year=' . rawurlencode($year)
+            . '&month=' . rawurlencode($month)
+            . '&day=' . rawurlencode($day)
+            . '&view=agendaDay';
+    }
 
-	$parts = explode('-', $date);
-	$day   = $parts[2];
-	$month = $parts[1];
-	$year  = $parts[0];
-	$url   = Route::_('index.php?option=com_sportsmanagement&view=gcalendar&gcids=' . implode(',', $linkIDs) . $itemId . '#year=' . $year . '&month=' . $month . '&day=' . $day . '&view=agendaDay');
-
-	$data[] = array(
-		'id'          => $date,
-		'title'       => utf8_encode(chr(160)), // Space only works in IE, empty only in Chrome... sighh
-		'start'       => $date,
-		'url'         => $url,
-		'allDay'      => true,
-		'description' => jsmGCalendarUtil::renderEvents($events, sprintf(Text::_('COM_GCALENDAR_JSON_VIEW_EVENT_TITLE'), count($events)) . '<ul>{{#events}}<li>{{title}}</li>{{/events}}</ul>', $params)
-	);
+    $data[] = [
+        'id' => $date,
+        'title' => "\u{00A0}",
+        'start' => $date,
+        'url' => $url,
+        'color' => (string) ($firstEvent['color'] ?? '#135CAE'),
+        'allDay' => true,
+        'description' => $description,
+    ];
 }
 
-ob_clean();
-echo json_encode($data);
+echo json_encode(
+    $data,
+    JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE | JSON_HEX_TAG | JSON_HEX_AMP | JSON_HEX_APOS | JSON_HEX_QUOT
+);
