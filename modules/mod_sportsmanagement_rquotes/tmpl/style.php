d71ab02ebd70f387daa3453cfc28c780ef9efe19
diddipoeler <diddipoeler@gmx.de>
2026-08-21 20:25:08 +0200

    Modernize styled random quotes layout

--- modules/mod_sportsmanagement_rquotes/tmpl/style.php
+++ modules/mod_sportsmanagement_rquotes/tmpl/style.php
@@ -1,30 +1,22 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage rquotes
- * @file       default.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\Uri\Uri;
+\defined('_JEXEC') or die;
+
 use Joomla\CMS\Language\Text;
 
-if ($list)
-{
-	foreach ($list as $rquote)
-	{
-		?>
+if (($source ?? 'db') === 'text') {
+    require __DIR__ . '/textfile.php';
+    return;
+}
+
+if (!$list) {
+    echo Text::_('MOD_SPORTSMANAGEMENT_RQUOTES_NUMBER_RANDOM_QUOTES_ERROR');
+    return;
+}
+?>
+<div class="<?php echo htmlspecialchars((string) $params->get('moduleclass_sfx', ''), ENT_QUOTES, 'UTF-8'); ?>">
+    <?php foreach ($list as $rquote) : ?>
         <div class="mod_rquote_style">
-			<?php modRquotesHelper::renderRquote($rquote, $params); ?>
+            <?php require __DIR__ . '/_rquote.php'; ?>
         </div>
-		<?PHP
-	}
-}
-else
-{
-	echo Text::_('MOD_SPORTSMANAGEMENT_RQUOTES_NUMBER_RANDOM_QUOTES_ERROR');
-}
+    <?php endforeach; ?>
+</div>
