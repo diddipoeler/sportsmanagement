98db746e4eae950c122bf846d961d9d874238e7c
diddipoeler <diddipoeler@gmx.de>
2026-08-21 21:59:35 +0200

    Bridge JLG color field to Joomla native color field

--- admin/models/fields/jlgcolor.php
+++ admin/models/fields/jlgcolor.php
@@ -1,83 +1,12 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage fields
- * @file       jlgcolor.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\Factory;
-use Joomla\CMS\Form\FormField;
-use Joomla\CMS\Uri\Uri;
+\defined('_JEXEC') or die;
 
-/**
- * FormFieldJLGColor
- *
- * @package
- * @author    diddi
- * @copyright 2014
- * @version   $Id$
- * @access    public
- */
-class JFormFieldJLGColor extends FormField
-{
-	/**
-	 * The form field type.
-	 *
-	 * @var   string
-	 * @since 11.3
-	 */
-	protected $type = 'JLGColor';
+use Diddipoeler\Component\SportsManagement\Administrator\Field\JlgcolorField;
 
+if (!class_exists(JlgcolorField::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Field/JlgcolorField.php';
+}
 
-	/**
-	 * FormFieldJLGColor::getInput()
-	 *
-	 * @return
-	 */
-	protected function getInput()
-	{
-		$app      = Factory::getApplication();
-		$document = Factory::getDocument();
-		$document->addScript(Uri::base() . 'components/com_sportsmanagement/assets/js/301a.js');
-
-		/** Initialize some field attributes. */
-		$size     = $this->element['size'] ? ' size="' . (int) $this->element['size'] . '"' : '';
-		$classes  = (string) $this->element['class'];
-		$disabled = ((string) $this->element['disabled'] == 'true') ? ' disabled="disabled"' : '';
-
-		/** Initialize JavaScript field attributes. */
-		$onchange = $this->element['onchange'] ? ' onchange="' . (string) $this->element['onchange'] . '"' : '';
-
-		$class = $classes ? ' class="' . trim($classes) . '"' : '';
-
-		$document->addScriptDeclaration(
-			"
-				jQuery(document).ready(function($){
-					$('.pickerbutton').on('click', function(event){
-						var field = this.id.substr(12);
-						showColorGrid3(field,'sample_'+field);
-						return overlib(document.id('colorpicker301').innerHTML,
-						               CAPTION,'Farbe:', BELOW, RIGHT,FOLLOWMOUSE=false,
-						               CLOSEBTN=true, CLOSEBTNCOLORS=['white', 'white', 'white', 'white'],
-				                   STICKY=false);
-					});
-					document.id('sample_" . $this->id . "').style.backgroundColor = document.id('" . $this->id . "').value;
-				});
-		"
-		);
-
-		$html   = array();
-		$html[] = '<input class="inputbox" type="text" id="sample_' . $this->id . '" size="1" value="">&nbsp;';
-		$html[] = '<input type="text" name="' . $this->name . '" id="' . $this->id . '"' . ' value="'
-			. htmlspecialchars($this->value, ENT_COMPAT, 'UTF-8') . '"' . $class . $size . $disabled . '/>';
-		$html[] = '<input	type="button" id="buttonpicker' . $this->id . '" class="inputbox pickerbutton" value="..."/>';
-		$html[] = '<div	id="colorpicker301" class="colorpicker301" style="position:absolute;top:0px;left:0px;z-index:1000;display:none;"></div>';
-
-		return implode("\n", $html);
-	}
+if (!class_exists('JFormFieldJLGColor', false)) {
+    class_alias(JlgcolorField::class, 'JFormFieldJLGColor');
 }
