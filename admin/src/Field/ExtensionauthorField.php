dc369c415d42e86e3f93f29b674ed4a22f7fae18
diddipoeler <diddipoeler@gmx.de>
2026-08-21 21:28:09 +0200

    Add native extension author field

---
+++ admin/src/Field/ExtensionauthorField.php
@@ -0,0 +1,28 @@
+<?php
+namespace Diddipoeler\Component\SportsManagement\Administrator\Field;
+
+\defined('_JEXEC') or die;
+
+use Joomla\CMS\Factory;
+use Joomla\CMS\Form\FormField;
+use Joomla\CMS\Language\Text;
+
+final class ExtensionauthorField extends FormField
+{
+    protected $type = 'ExtensionAuthor';
+
+    protected function getLabel(): string
+    {
+        $language = Factory::getApplication()->getLanguage();
+        $language->load('com_sportsmanagement', JPATH_ADMINISTRATOR, $language->getTag(), true);
+
+        return '<div style="clear: both;">' . Text::_('COM_SPORTSMANAGEMENT_AUTHOR_LABEL') . '</div>';
+    }
+
+    protected function getInput(): string
+    {
+        return '<div style="padding-top: 5px; overflow: inherit">'
+            . 'Dieter Pl&ouml;ger @ <a href="https://www.fussballineuropa.de" target="_blank" rel="noopener noreferrer">Fussball in Europa</a>'
+            . '</div>';
+    }
+}
