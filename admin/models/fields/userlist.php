4af3a1e0c208250f52973cc5cb719bc9418d3134
diddipoeler <diddipoeler@gmx.de>
2026-08-21 21:00:26 +0200

    Bridge legacy user list field

--- admin/models/fields/userlist.php
+++ admin/models/fields/userlist.php
@@ -1,59 +1,17 @@
 <?php
-/**
- *
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage fields
- * @file       userlist.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-
+/** Compatibility bridge for the Joomla 5/6 user list field. */
 defined('_JEXEC') or die('Restricted access');
 
-use Joomla\CMS\Factory;
-use Joomla\CMS\Form\Field\ListField;
-use Joomla\Database\DatabaseInterface;
+use Diddipoeler\Component\SportsManagement\Administrator\Field\UserlistField;
 
-/**
- * FormFielduserlist
- *
- * @package
- * @author
- * @copyright diddi
- * @version   2014
- * @access    public
- */
-class JFormFielduserlist extends ListField
-{
-	/**
-	 * field type
-	 *
-	 * @var string
-	 */
-	public $type = 'userlist';
+if (!class_exists(UserlistField::class)) {
+    $fieldFile = JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Field/UserlistField.php';
 
-	/**
-	 * Method to get the field options.
-	 *
-	 * @return array  The field option objects.
-	 *
-	 * @since 11.1
-	 */
-	protected function getOptions()
-	{
-		$db    = Factory::getContainer()->get(DatabaseInterface::class);
-		$query = $db->createQuery();
+    if (is_file($fieldFile)) {
+        require_once $fieldFile;
+    }
+}
 
-		$query->select('id AS value, name AS text');
-		$query->from('#__users');
-		$query->order('name');
-		$db->setQuery($query);
-		$options = $db->loadObjectList();
-
-		return array_merge(parent::getOptions(), $options);
-	}
+if (class_exists(UserlistField::class) && !class_exists('JFormFielduserlist', false)) {
+    class_alias(UserlistField::class, 'JFormFielduserlist');
 }
