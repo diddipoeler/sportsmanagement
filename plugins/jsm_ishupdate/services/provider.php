5739683f7ebee2ea6a8346370a8610719f7bc2b2
diddipoeler <diddipoeler@gmx.de>
2026-08-21 13:09:14 +0200

    Add Joomla 5/6 service provider for ISH update plugin

---
+++ plugins/jsm_ishupdate/services/provider.php
@@ -0,0 +1,29 @@
+<?php
+\defined('_JEXEC') or die;
+
+use Diddipoeler\Plugin\System\SportsmanagementIshupdate\Extension\SportsmanagementIshupdate;
+use Joomla\CMS\Extension\PluginInterface;
+use Joomla\CMS\Factory;
+use Joomla\CMS\Plugin\PluginHelper;
+use Joomla\Database\DatabaseInterface;
+use Joomla\DI\Container;
+use Joomla\DI\ServiceProviderInterface;
+
+return new class () implements ServiceProviderInterface
+{
+    public function register(Container $container): void
+    {
+        $container->set(
+            PluginInterface::class,
+            static function (Container $container): PluginInterface {
+                $plugin = new SportsmanagementIshupdate(
+                    (array) PluginHelper::getPlugin('system', 'jsm_ishupdate')
+                );
+                $plugin->setApplication(Factory::getApplication());
+                $plugin->setDatabase($container->get(DatabaseInterface::class));
+
+                return $plugin;
+            }
+        );
+    }
+};
