a124cd8042b8101553345883e8b107f8acfb23c3
diddipoeler <diddipoeler@gmx.de>
2026-08-21 20:59:49 +0200

    Bridge legacy person list field

--- admin/models/fields/personlist.php
+++ admin/models/fields/personlist.php
@@ -1,52 +1,17 @@
 <?php
-/**
- *
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage fields
- * @file       personlist.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-
+/** Compatibility bridge for the Joomla 5/6 person list field. */
 defined('_JEXEC') or die('Restricted access');
 
-use Joomla\CMS\Factory;
-use Joomla\CMS\Form\Field\ListField;
-use Joomla\Database\DatabaseInterface;
+use Diddipoeler\Component\SportsManagement\Administrator\Field\PersonlistField;
 
-/**
- * FormFieldpersonlist
- *
- * @package
- * @author
- * @copyright diddi
- * @version   2014
- * @access    public
- */
-class JFormFieldpersonlist extends ListField
-{
-	public $type = 'personlist';
+if (!class_exists(PersonlistField::class)) {
+    $fieldFile = JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Field/PersonlistField.php';
 
-	/**
-	 * Method to get the field options.
-	 *
-	 * @return array
-	 */
-	protected function getOptions()
-	{
-		$db    = Factory::getContainer()->get(DatabaseInterface::class);
-		$query = $db->createQuery();
+    if (is_file($fieldFile)) {
+        require_once $fieldFile;
+    }
+}
 
-		$query->select("id AS value, concat(lastname,' - ',firstname,'' ) AS text");
-		$query->from('#__sportsmanagement_person ');
-		$query->order('lastname');
-		$db->setQuery($query);
-		$options = $db->loadObjectList();
-
-		return array_merge(parent::getOptions(), $options);
-	}
+if (class_exists(PersonlistField::class) && !class_exists('JFormFieldpersonlist', false)) {
+    class_alias(PersonlistField::class, 'JFormFieldpersonlist');
 }
