be0bd2269ea6d13db79f93b3f96f84398126c3e2
diddipoeler <diddipoeler@gmx.de>
2026-08-21 19:22:20 +0200

    Use native tournament tree node table bridge

--- admin/tables/treetonode.php
+++ admin/tables/treetonode.php
@@ -1,37 +1,13 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung f�r alle Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage tables
- * @file       treetonode.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: � 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
 defined('_JEXEC') or die('Restricted access');
 
-/**
- * sportsmanagementTableTreetoNode
- *
- * @package
- * @author    diddi
- * @copyright 2014
- * @version   $Id$
- * @access    public
- */
-class sportsmanagementTableTreetoNode extends JSMTable
-{
-	/**
-	 * Constructor
-	 *
-	 * @param   object Database connector object
-	 *
-	 * @since 1.0
-	 */
-	public function __construct(&$db)
-	{
-		$db = sportsmanagementHelper::getDBConnection();
-		parent::__construct('#__sportsmanagement_treeto_node', 'id', $db);
-	}
+use Diddipoeler\Component\SportsManagement\Administrator\Table\TreetonodeTable;
+
+if (!class_exists(TreetonodeTable::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Table/SportsManagementTable.php';
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Table/TreetonodeTable.php';
+}
+
+if (!class_exists('sportsmanagementTableTreetoNode', false)) {
+    class_alias(TreetonodeTable::class, 'sportsmanagementTableTreetoNode');
 }
