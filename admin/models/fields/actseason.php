dec86f1a211e877a319e1d4b24d2755444c0cf2d
diddipoeler <diddipoeler@gmx.de>
2026-08-21 20:56:29 +0200

    Bridge legacy active season field

--- admin/models/fields/actseason.php
+++ admin/models/fields/actseason.php
@@ -1,48 +1,17 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage fields
- * @file       actseason.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
+/** Compatibility bridge for the Joomla 5/6 active season field. */
 defined('_JEXEC') or die('Restricted access');
 
-use Joomla\CMS\Factory;
-use Joomla\CMS\Form\Field\ListField;
-use Joomla\Database\DatabaseInterface;
+use Diddipoeler\Component\SportsManagement\Administrator\Field\ActseasonField;
 
-/**
- * FormFieldactseason
- *
- * @package
- * @author
- * @copyright diddi
- * @version   2014
- * @access    public
- */
-class JFormFieldactseason extends ListField
-{
-	protected $type = 'actseason';
+if (!class_exists(ActseasonField::class)) {
+    $fieldFile = JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Field/ActseasonField.php';
 
-	/**
-	 * FormFieldactseason::getOptions()
-	 *
-	 * @return array
-	 */
-	protected function getOptions()
-	{
-		$db    = Factory::getContainer()->get(DatabaseInterface::class);
-		$query = $db->createQuery();
-		$query->select('s.id AS value, s.name AS text');
-		$query->from('#__sportsmanagement_season AS s');
-		$query->order('s.name');
-		$db->setQuery($query);
-		$options = $db->loadObjectList();
+    if (is_file($fieldFile)) {
+        require_once $fieldFile;
+    }
+}
 
-		return array_merge(parent::getOptions(), $options);
-	}
+if (class_exists(ActseasonField::class) && !class_exists('JFormFieldactseason', false)) {
+    class_alias(ActseasonField::class, 'JFormFieldactseason');
 }
