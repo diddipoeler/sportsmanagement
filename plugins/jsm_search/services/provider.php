649baa04e12c3aab14bec0a29ba9411d772ba651
diddipoeler <diddipoeler@gmx.de>
2026-08-21 17:58:10 +0200

    Inject event dispatcher into finder plugin

--- plugins/jsm_search/services/provider.php
+++ plugins/jsm_search/services/provider.php
@@ -9,14 +9,17 @@ use Joomla\CMS\Plugin\PluginHelper;
 use Joomla\Database\DatabaseInterface;
 use Joomla\DI\Container;
 use Joomla\DI\ServiceProviderInterface;
+use Joomla\Event\DispatcherInterface;
 
-return new class () implements ServiceProviderInterface {
+return new class () implements ServiceProviderInterface
+{
     public function register(Container $container): void
     {
         $container->set(
             PluginInterface::class,
             static function (Container $container): Sportsmanagement {
                 $plugin = new Sportsmanagement(
+                    $container->get(DispatcherInterface::class),
                     (array) PluginHelper::getPlugin('finder', 'jsm_search')
                 );
                 $plugin->setApplication(Factory::getApplication());
