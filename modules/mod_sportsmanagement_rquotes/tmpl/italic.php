50e787524618e54856d5390ebe04846821e963da
diddipoeler <diddipoeler@gmx.de>
2026-08-21 20:25:00 +0200

    Modernize italic random quotes layout

--- modules/mod_sportsmanagement_rquotes/tmpl/italic.php
+++ modules/mod_sportsmanagement_rquotes/tmpl/italic.php
@@ -1,29 +1,20 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage rquotes
- * @file       default.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-defined('_JEXEC') or die('Restricted access');
+\defined('_JEXEC') or die;
+
 use Joomla\CMS\Language\Text;
 
-$quotemarks = $params->get('quotemarks');
+if (($source ?? 'db') === 'text') {
+    require __DIR__ . '/textfile.php';
+    return;
+}
 
-if ($list)
-{
-	foreach ($list as $rquote)
-	{
-		echo '<i>';
-		modRquotesHelper::renderRquote($rquote, $params);
-		echo '</i>';
-	}
+if (!$list) {
+    echo Text::_('MOD_SPORTSMANAGEMENT_RQUOTES_NUMBER_RANDOM_QUOTES_ERROR');
+    return;
 }
-else
-{
-	echo Text::_('MOD_SPORTSMANAGEMENT_RQUOTES_NUMBER_RANDOM_QUOTES_ERROR');
-}
+?>
+<div class="fst-italic <?php echo htmlspecialchars((string) $params->get('moduleclass_sfx', ''), ENT_QUOTES, 'UTF-8'); ?>">
+    <?php foreach ($list as $rquote) : ?>
+        <?php require __DIR__ . '/_rquote.php'; ?>
+    <?php endforeach; ?>
+</div>
