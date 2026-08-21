257b6b629abc90eb81790d6a9662f83f0b1268ae
diddipoeler <diddipoeler@gmx.de>
2026-08-21 21:15:20 +0200

    Add native country association field

---
+++ admin/src/Field/CountryassociationField.php
@@ -0,0 +1,70 @@
+<?php
+namespace Diddipoeler\Component\SportsManagement\Administrator\Field;
+
+\defined('_JEXEC') or die;
+
+use Joomla\CMS\Factory;
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Language\Text;
+
+final class CountryassociationField extends SportsManagementListField
+{
+    protected $type = 'countryassociation';
+
+    protected function getOptions(): array
+    {
+        $varname = trim((string) ($this->element['varname'] ?? ''));
+        $targetTable = preg_replace('/[^A-Za-z0-9_]/', '', (string) ($this->element['targettable'] ?? ''));
+
+        if ($varname === '' || $targetTable === '') {
+            return parent::getOptions();
+        }
+
+        $selectedId = Factory::getApplication()->getInput()->get($varname, null, 'raw');
+
+        if (is_array($selectedId)) {
+            $selectedId = reset($selectedId) ?: 0;
+        }
+
+        $selectedId = (int) $selectedId;
+
+        if ($selectedId <= 0) {
+            return parent::getOptions();
+        }
+
+        $db = $this->getSportsManagementDatabase();
+        $target = '#__sportsmanagement_' . $targetTable;
+        $query = $db->getQuery(true)
+            ->select([
+                $db->quoteName('t.id', 'value'),
+                $db->quoteName('t.name', 'text'),
+            ])
+            ->from($db->quoteName('#__sportsmanagement_associations', 't'))
+            ->join(
+                'INNER',
+                $db->quoteName($target, 'wt')
+                . ' ON ' . $db->quoteName('wt.country') . ' = ' . $db->quoteName('t.country')
+            )
+            ->where($db->quoteName('wt.id') . ' = ' . $selectedId)
+            ->order($db->quoteName('t.name'));
+
+        try {
+            $db->setQuery($query);
+            $items = $db->loadObjectList() ?: [];
+        } catch (\Throwable $e) {
+            Factory::getApplication()->enqueueMessage(
+                Text::sprintf('COM_SPORTSMANAGEMENT_DATABASE_ERROR_FUNCTION_FAILED', $e->getCode(), $e->getMessage()),
+                'notice'
+            );
+            $items = [];
+        }
+
+        $options = [];
+
+        foreach ($items as $item) {
+            $options[] = HTMLHelper::_('select.option', $item->value, $item->text);
+        }
+
+        return array_merge(parent::getOptions(), $options);
+    }
+}
