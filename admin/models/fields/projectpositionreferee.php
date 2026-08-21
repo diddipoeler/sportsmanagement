41579521b02bc8ca0217de7d0ffa3c7cffbb5470
diddipoeler <diddipoeler@gmx.de>
2026-08-21 21:16:14 +0200

    Bridge project referee position field to native class

--- admin/models/fields/projectpositionreferee.php
+++ admin/models/fields/projectpositionreferee.php
@@ -1,86 +1,12 @@
 <?php
-/**
- *
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage fields
- * @file       projectpositionreferee.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
+\defined('_JEXEC') or die;
 
-defined('_JEXEC') or die('Restricted access');
+use Diddipoeler\Component\SportsManagement\Administrator\Field\ProjectpositionrefereeField;
 
-use Joomla\CMS\Factory;
-use Joomla\CMS\Form\FormField;
-use Joomla\CMS\Form\FormHelper;
+if (!class_exists(ProjectpositionrefereeField::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Field/ProjectpositionrefereeField.php';
+}
 
-jimport('joomla.filesystem.folder');
-FormHelper::loadFieldClass('list');
-
-/**
- * FormFieldprojectpositionreferee
- *
- * @package
- * @author
- * @copyright diddi
- * @version   2014
- * @access    public
- */
-class JFormFieldprojectpositionreferee extends \JFormFieldList
-{
-	/**
-	 * field type
-	 *
-	 * @var string
-	 */
-	public $type = 'projectpositionreferee';
-
-	/**
-	 * Method to get the field options.
-	 *
-	 * @return array  The field option objects.
-	 *
-	 * @since 11.1
-	 */
-	protected function getOptions()
-	{
-		$option = Factory::getApplication()->input->getCmd('option');
-		$app    = Factory::getApplication();
-
-		// Initialize variables.
-		$options = array();
-
-		$varname    = (string) $this->element['varname'];
-		$project_id = $app->getUserState("$option.pid", '0');;
-
-		/*
-        $project_id = Factory::getApplication()->input->getVar($varname);
-        if (is_array($project_id)) {
-         $project_id = $project_id[0];
-        }
-        */
-		if ($project_id)
-		{
-			$db    = Factory::getDbo();
-			$query = $db->getQuery(true);
-
-			$query->select('ppos.id AS value,pos.name AS text');
-			$query->from('#__sportsmanagement_position AS pos');
-			$query->join('inner', '#__sportsmanagement_project_position AS ppos ON pos.id=ppos.position_id');
-			$query->where('ppos.project_id=' . $project_id . ' AND pos.persontype=3');
-
-			// $query->order('t.name');
-			$db->setQuery($query);
-			$options = $db->loadObjectList();
-		}
-
-		// Merge any additional options in the XML definition.
-		$options = array_merge(parent::getOptions(), $options);
-
-		return $options;
-	}
+if (!class_exists('JFormFieldprojectpositionreferee', false)) {
+    class_alias(ProjectpositionrefereeField::class, 'JFormFieldprojectpositionreferee');
 }
