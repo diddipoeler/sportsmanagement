38b5296f4390c44a68a0fa56100d066b3d9d8dcc
diddipoeler <diddipoeler@gmx.de>
2026-08-21 18:59:42 +0200

    Use native fallback for Zabuto calendar layout

--- modules/mod_sportsmanagement_calendar/tmpl/default_zabuto.php
+++ modules/mod_sportsmanagement_calendar/tmpl/default_zabuto.php
@@ -1,30 +1,10 @@
 <?php
 /**
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage mod_sportsmanagement_calendar
- * @file       default_bootstrap.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * Zabuto layout compatibility alias.
+ *
+ * The historical Zabuto template only loaded third-party assets and did not render
+ * SportsManagement events. Use the native Joomla 5/6 calendar layout instead.
  */
-defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\HTML\HTMLHelper;
-use Joomla\CMS\Uri\Uri;
-use Joomla\CMS\Factory;
+\defined('_JEXEC') or die;
 
-//$display = ($params->get('update_module') == 1) ? 'block' : 'none';
-//$show_teamlist = ($params->get('show_teamslist') == 1) ? 'show' : 'hidden';
-
-$cal       = new JSMCalendar; // This object creates the html for the calendar
-$cal::getMatches($month, $year);
-
-//echo '<pre>'.print_r($cal::$matches,true).'</pre>';
-
-?>
-  
-<script src="https://cdnjs.cloudflare.com/ajax/libs/zabuto_calendar/1.6.4/zabuto_calendar.js"></script>  
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/zabuto_calendar/1.6.4/zabuto_calendar.css">
-
-
+require __DIR__ . '/default_jsm.php';
