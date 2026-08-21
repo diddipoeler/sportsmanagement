bd7897f87d88da91ee834e39b7c9a2a36d2089c3
diddipoeler <diddipoeler@gmx.de>
2026-08-21 19:35:01 +0200

    Use native position event type table bridge

--- admin/tables/positioneventtype.php
+++ admin/tables/positioneventtype.php
@@ -1,38 +1,13 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage tables
- * @file       positioneventtype.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
 defined('_JEXEC') or die('Restricted access');
 
-/**
- * sportsmanagementTablePositioneventtype
- *
- * @package
- * @author
- * @copyright diddi
- * @version   2014
- * @access    public
- */
-class sportsmanagementTablePositioneventtype extends JSMTable
-{
-	/**
-	 * Constructor
-	 *
-	 * @param   object Database connector object
-	 *
-	 * @since 1.0
-	 */
-	function __construct(&$db)
-	{
-		$db = sportsmanagementHelper::getDBConnection();
-		parent::__construct('#__sportsmanagement_position_eventtype', 'id', $db);
-	}
+use Diddipoeler\Component\SportsManagement\Administrator\Table\PositioneventtypeTable;
 
+if (!class_exists(PositioneventtypeTable::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Table/SportsManagementTable.php';
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Table/PositioneventtypeTable.php';
+}
+
+if (!class_exists('sportsmanagementTablePositioneventtype', false)) {
+    class_alias(PositioneventtypeTable::class, 'sportsmanagementTablePositioneventtype');
 }
