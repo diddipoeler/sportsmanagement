9c9d758b873343e89a941cc18629a41c3752ee03
diddipoeler <diddipoeler@gmx.de>
2026-08-21 19:32:46 +0200

    Use native team staff table bridge

--- admin/tables/teamstaff.php
+++ admin/tables/teamstaff.php
@@ -1,52 +1,13 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage tables
- * @file       teamstaff.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
 defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\Language\Text;
 
-/**
- * sportsmanagementTableTeamStaff
- *
- * @package
- * @author
- * @copyright diddi
- * @version   2014
- * @access    public
- */
-class sportsmanagementTableTeamStaff extends JSMTable
-{
-	/**
-	 * Constructor
-	 *
-	 * @param   object Database connector object
-	 *
-	 * @since 1.0
-	 */
-	function __construct(&$db)
-	{
-		$db = sportsmanagementHelper::getDBConnection();
-		parent::__construct('#__sportsmanagement_team_staff', 'id', $db);
-	}
+use Diddipoeler\Component\SportsManagement\Administrator\Table\TeamstaffTable;
 
-	/**
-	 * sportsmanagementTableTeamStaff::canDelete()
-	 *
-	 * @param   mixed  $id
-	 * @param   mixed  $joins
-	 *
-	 * @return
-	 */
-	function canDelete($id, $joins = null)
-	{
+if (!class_exists(TeamstaffTable::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Table/SportsManagementTable.php';
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Table/TeamstaffTable.php';
+}
 
-		return true;
-	}
+if (!class_exists('sportsmanagementTableTeamStaff', false)) {
+    class_alias(TeamstaffTable::class, 'sportsmanagementTableTeamStaff');
 }
