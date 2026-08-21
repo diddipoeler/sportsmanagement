27e34c94a08021d14684c3bd08ad8e2089f0cb99
diddipoeler <diddipoeler@gmx.de>
2026-08-21 20:17:59 +0200

    Use native birthday helper in legacy entry

--- modules/mod_sportsmanagement_birthday/mod_sportsmanagement_birthday.php
+++ modules/mod_sportsmanagement_birthday/mod_sportsmanagement_birthday.php
@@ -1,23 +1,34 @@
 <?php
 /** Joomla 5/6 compatibility entry point for the birthday module. */
-defined('_JEXEC') or die;
+\defined('_JEXEC') or die;
 
+use Diddipoeler\Module\SportsManagementBirthday\Site\Helper\BirthdayHelper;
 use Joomla\CMS\Component\ComponentHelper;
 use Joomla\CMS\Factory;
 use Joomla\CMS\Helper\ModuleHelper;
+use Joomla\Database\DatabaseInterface;
 
-require_once __DIR__ . '/helper.php';
+if (!class_exists(BirthdayHelper::class)) {
+    require_once __DIR__ . '/src/Helper/BirthdayHelper.php';
+}
 
 $app = Factory::getApplication();
 $app->getLanguage()->load('com_sportsmanagement', JPATH_ADMINISTRATOR, null, true);
-$result = (new modSportsmanagementBirthdayDataHelper())->getData(
+/** @var DatabaseInterface $database */
+$database = Factory::getContainer()->get(DatabaseInterface::class);
+$result = (new BirthdayHelper())->getData(
     $params,
     ComponentHelper::getParams('com_sportsmanagement'),
-    $app
+    $app,
+    $database
 );
 
 $persons = $result['persons'];
 $mode = $result['mode'];
 $pictureServer = $result['pictureServer'];
+
+if ($mode === 'B') {
+    $app->getDocument()->getWebAssetManager()->useScript('bootstrap.carousel');
+}
 
 require ModuleHelper::getLayoutPath($module->module, (string) $params->get('layout', 'default'));
