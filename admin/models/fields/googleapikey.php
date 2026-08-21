d6b2fecd031d5f16a68d7a827c5584e9a6dc4042
diddipoeler <diddipoeler@gmx.de>
2026-08-21 21:47:29 +0200

    Bridge Google API key field to native class

--- admin/models/fields/googleapikey.php
+++ admin/models/fields/googleapikey.php
@@ -1,52 +1,12 @@
 <?php
-/**
- *
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage fields
- * @file       googleapikey.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
+\defined('_JEXEC') or die;
 
-defined('_JEXEC') or die();
+use Diddipoeler\Component\SportsManagement\Administrator\Field\GoogleapikeyField;
 
-use Joomla\CMS\Component\ComponentHelper;
-use Joomla\CMS\Form\Field\TextField;
+if (!class_exists(GoogleapikeyField::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Field/GoogleapikeyField.php';
+}
 
-/**
- * FormFieldGoogleApiKey
- *
- * @package
- * @author    Dieter Plöger
- * @copyright 2017
- * @version   $Id$
- * @access    public
- */
-class JFormFieldGoogleApiKey extends TextField
-{
-	protected $type = 'GoogleApiKey';
-
-	/**
-	 * Prepare the text field and use the component API key as a default only
-	 * when the form does not already provide a value.
-	 *
-	 * @param   SimpleXMLElement  $element  The SimpleXMLElement object representing the field.
-	 * @param   mixed             $value    The form field value to validate.
-	 * @param   string|null       $group    The field name group control value.
-	 *
-	 * @return  bool
-	 */
-	public function setup(SimpleXMLElement $element, $value, $group = null)
-	{
-		if (($value === null || $value === '') && !isset($element['default']))
-		{
-			$value = (string) ComponentHelper::getParams('com_sportsmanagement')->get('google_api_developerkey', '');
-		}
-
-		return parent::setup($element, $value, $group);
-	}
+if (!class_exists('JFormFieldGoogleApiKey', false)) {
+    class_alias(GoogleapikeyField::class, 'JFormFieldGoogleApiKey');
 }
