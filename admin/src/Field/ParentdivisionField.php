6a3739d5be3b3c599c20450c165eb91fbe62edb1
diddipoeler <diddipoeler@gmx.de>
2026-08-21 21:12:10 +0200

    Add native parent division field

---
+++ admin/src/Field/ParentdivisionField.php
@@ -0,0 +1,43 @@
+<?php
+namespace Diddipoeler\Component\SportsManagement\Administrator\Field;
+
+\defined('_JEXEC') or die;
+
+use Joomla\CMS\Factory;
+use Joomla\CMS\HTML\HTMLHelper;
+
+final class ParentdivisionField extends SportsManagementListField
+{
+    protected $type = 'parentdivision';
+
+    protected function getOptions(): array
+    {
+        $app = Factory::getApplication();
+        $option = $app->getInput()->getCmd('option', 'com_sportsmanagement');
+        $projectId = (int) $app->getUserState($option . '.pid', 0);
+
+        if ($projectId <= 0) {
+            return parent::getOptions();
+        }
+
+        $db = $this->getSportsManagementDatabase();
+        $query = $db->getQuery(true)
+            ->select([
+                $db->quoteName('id', 'value'),
+                $db->quoteName('name', 'text'),
+            ])
+            ->from($db->quoteName('#__sportsmanagement_division'))
+            ->where($db->quoteName('project_id') . ' = ' . $projectId)
+            ->where($db->quoteName('parent_id') . ' = 0')
+            ->order($db->quoteName('ordering') . ' ASC');
+        $db->setQuery($query);
+
+        $options = [];
+
+        foreach ($db->loadObjectList() ?: [] as $item) {
+            $options[] = HTMLHelper::_('select.option', $item->value, $item->text);
+        }
+
+        return array_merge(parent::getOptions(), $options);
+    }
+}
