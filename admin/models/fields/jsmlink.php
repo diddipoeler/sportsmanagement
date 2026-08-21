e384ce60b04ae86264803de78d8a3c1a4e27277d
diddipoeler <diddipoeler@gmx.de>
2026-08-21 21:55:47 +0200

    Bridge JSM link field to native class

--- admin/models/fields/jsmlink.php
+++ admin/models/fields/jsmlink.php
@@ -1,96 +1,12 @@
 <?php
-/**
- * @copyright      Copyright (C) 2011 Simplify Your Web, Inc. All rights reserved.
- * @license        GNU General Public License version 3 or later; see LICENSE.txt
- */
+\defined('_JEXEC') or die;
 
+use Diddipoeler\Component\SportsManagement\Administrator\Field\JsmlinkField;
 
-defined('_JEXEC') or die;
+if (!class_exists(JsmlinkField::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Field/JsmlinkField.php';
+}
 
-use Joomla\CMS\Language\Text;
-
-jimport('joomla.form.formfield');
-
-class JFormFieldJSMLink extends FormField
-{
-	public $type = 'JSMLink';
-
-	/**
-	 * Method to get the field options.
-	 */
-	protected function getLabel()
-	{
-
-		$html = '';
-
-		$title     = trim($this->element['title']);
-		$image_src = $this->element['imagesrc']; // Path ex: ../modules/mod_latestnews/images/icon.png
-		$link      = $this->element['link'];
-
-		$html .= '<div style="overflow: hidden; margin: 5px 0">';
-		$html .= '<label style="margin: 0">';
-
-		$html .= '<a href="' . $link . '" target="_blank" title="' . Text::_($title) . '">';
-
-		if ($image_src)
-		{
-			$html .= '<img src="' . $image_src . '" alt="' . Text::_($title) . '">';
-		}
-		else
-		{
-			$html .= Text::_($title);
-		}
-
-		$html .= '</a>';
-
-		$html .= '</label>';
-
-		return $html;
-	}
-
-	/**
-	 * Method to get the field input markup.
-	 */
-	protected function getInput()
-	{
-
-		$title     = trim($this->element['title']);
-		$image_src = $this->element['imagesrc'];
-		$text      = trim($this->element['text']);
-		$link      = $this->element['link'];
-
-		$titleintext = false;
-
-		if ($this->element['titleintext'])
-		{
-			$titleintext = ($this->element['titleintext'] === 'true');
-		}
-
-		$html = '';
-
-		if ($image_src)
-		{
-			$html .= '<div style="padding: 5px 0 0 0; overflow: inherit">';
-		}
-		else
-		{
-			$html .= '<div style="padding: 0; overflow: inherit">';
-		}
-
-		if ($titleintext)
-		{
-			$html .= '<strong>' . Text::_($title) . '</strong>: ';
-		}
-
-		if ($text)
-		{
-			$html .= Text::sprintf($text, $link);
-		}
-
-		$html .= '</div>';
-		$html .= '</div>';
-
-		return $html;
-	}
-
+if (!class_exists('JFormFieldJSMLink', false)) {
+    class_alias(JsmlinkField::class, 'JFormFieldJSMLink');
 }
