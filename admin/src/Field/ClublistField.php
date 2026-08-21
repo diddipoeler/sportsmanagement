65e1ec2e0b4d2e96106cf2375b9fe1b057a3d3b3
diddipoeler <diddipoeler@gmx.de>
2026-08-21 21:08:54 +0200

    Add native club list field

---
+++ admin/src/Field/ClublistField.php
@@ -0,0 +1,61 @@
+<?php
+namespace Diddipoeler\Component\SportsManagement\Administrator\Field;
+
+\defined('_JEXEC') or die;
+
+use Joomla\CMS\Factory;
+use Joomla\CMS\HTML\HTMLHelper;
+
+final class ClublistField extends SportsManagementListField
+{
+    protected $type = 'clublist';
+
+    protected function getOptions(): array
+    {
+        $app = Factory::getApplication();
+        $input = $app->getInput();
+        $sportType = trim((string) ($this->element['target'] ?? ''));
+        $clubId = $input->getInt('club_id', 0)
+            ?: (int) $app->getUserState('com_sportsmanagement.club_id', 0);
+
+        $db = $this->getSportsManagementDatabase();
+        $query = $db->getQuery(true)
+            ->select([
+                $db->quoteName('c.id', 'value'),
+                $db->quoteName('c.name', 'text'),
+            ])
+            ->from($db->quoteName('#__sportsmanagement_club', 'c'))
+            ->join(
+                'LEFT',
+                $db->quoteName('#__sportsmanagement_team', 't')
+                . ' ON ' . $db->quoteName('t.club_id') . ' = ' . $db->quoteName('c.id')
+            );
+
+        if ($clubId > 0) {
+            $query->where($db->quoteName('c.id') . ' = ' . $clubId);
+        }
+
+        if ($sportType !== '') {
+            $query->join(
+                'INNER',
+                $db->quoteName('#__sportsmanagement_sports_type', 'st')
+                . ' ON ' . $db->quoteName('st.id') . ' = ' . $db->quoteName('t.sports_type_id')
+            );
+            $query->where($db->quoteName('st.name') . ' = ' . $db->quote($sportType));
+        }
+
+        $query->group([
+            $db->quoteName('c.id'),
+            $db->quoteName('c.name'),
+        ])->order($db->quoteName('c.name'));
+        $db->setQuery($query);
+
+        $options = [];
+
+        foreach ($db->loadObjectList() ?: [] as $item) {
+            $options[] = HTMLHelper::_('select.option', $item->value, $item->text);
+        }
+
+        return array_merge(parent::getOptions(), $options);
+    }
+}
