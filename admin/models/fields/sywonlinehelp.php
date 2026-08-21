29d2fbe9912280aabb4bc14119700ee1079ea85e
diddipoeler <diddipoeler@gmx.de>
2026-08-21 21:59:46 +0200

    Bridge SYW online help field to native class

--- admin/models/fields/sywonlinehelp.php
+++ admin/models/fields/sywonlinehelp.php
@@ -1,74 +1,12 @@
 <?php
-/**
- *
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage fields
- * @file       sywonlinehelp.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
+\defined('_JEXEC') or die;
 
+use Diddipoeler\Component\SportsManagement\Administrator\Field\SywonlinehelpField;
 
-defined('_JEXEC') or die;
+if (!class_exists(SywonlinehelpField::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Field/SywonlinehelpField.php';
+}
 
-use Joomla\CMS\Language\Text;
-use Joomla\CMS\HTML\HTMLHelper;
-use Joomla\CMS\Form\FormField;
-
-/**
- * FormFieldSYWOnlineHelp
- *
- * @package
- * @author    Dieter Plöger
- * @copyright 2017
- * @version   $Id$
- * @access    public
- */
-class JFormFieldSYWOnlineHelp extends FormField
-{
-	protected $type = 'SYWOnlineHelp';
-
-	/**
-	 * FormFieldSYWOnlineHelp::getLabel()
-	 *
-	 * @return
-	 */
-	protected function getLabel()
-	{
-
-		HTMLHelper::_('stylesheet', 'syw/fonts-min.css', false, true);
-
-		$title       = $this->element['label'] ? (string) $this->element['label'] : ($this->element['title'] ? (string) $this->element['title'] : '');
-		$heading     = $this->element['heading'] ? (string) $this->element['heading'] : 'h4';
-		$description = (string) $this->element['description'];
-		$class       = !empty($this->class) ? ' class="' . $this->class . '"' : '';
-
-		$url = (string) $this->element['url'];
-
-		$html = array();
-
-		$html[] = !empty($title) ? '<' . $heading . '>' . Text::_($title) . '</' . $heading . '>' : '';
-
-		$html[] = '<table style="width: 100%"><tr>';
-		$html[] = !empty($description) ? '<td>' . Text::_($description) . '</td>' : '';
-		$html[] = '<td style="text-align: right"><a href="' . $url . '" target="_blank" class="btn btn-info btn-mini btn-xs"><i class="SYWicon-local-library"></i></a></td>';
-		$html[] = '</tr></table>';
-
-		return '</div><div ' . $class . '>' . implode('', $html);
-	}
-
-	/**
-	 * FormFieldSYWOnlineHelp::getInput()
-	 *
-	 * @return
-	 */
-	protected function getInput()
-	{
-		return '';
-	}
-
+if (!class_exists('JFormFieldSYWOnlineHelp', false)) {
+    class_alias(SywonlinehelpField::class, 'JFormFieldSYWOnlineHelp');
 }
