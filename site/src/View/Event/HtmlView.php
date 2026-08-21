caffd57c95e08c819dcfec9f23d57c9664be6282
diddipoeler <diddipoeler@gmx.de>
2026-08-21 17:05:58 +0200

    Add native Google Calendar event view

---
+++ site/src/View/Event/HtmlView.php
@@ -0,0 +1,32 @@
+<?php
+namespace Diddipoeler\Component\SportsManagement\Site\View\Event;
+
+\defined('_JEXEC') or die;
+
+use Diddipoeler\Component\SportsManagement\Site\Model\EventModel;
+use Diddipoeler\Component\SportsManagement\Site\View\SportsManagementHtmlView;
+
+final class HtmlView extends SportsManagementHtmlView
+{
+    /** @var array<string, mixed>|null */
+    public ?array $event = null;
+
+    public function __construct($config = [])
+    {
+        $config['template_path'] = JPATH_SITE . '/components/com_sportsmanagement/views/event/tmpl';
+        parent::__construct($config);
+    }
+
+    public function display($tpl = null)
+    {
+        /** @var EventModel $model */
+        $model = $this->getModel();
+        $this->event = $model->getGCalendar();
+
+        if ($this->event && trim((string) ($this->event['title'] ?? '')) !== '') {
+            $this->getDocument()->setTitle((string) $this->event['title']);
+        }
+
+        parent::display($tpl);
+    }
+}
