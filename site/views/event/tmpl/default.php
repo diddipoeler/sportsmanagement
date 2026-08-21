ccae944e137f05f076ed4b1bf7f89d1b4080b8fb
diddipoeler <diddipoeler@gmx.de>
2026-08-21 17:15:51 +0200

    Modernize Google Calendar event layout

--- site/views/event/tmpl/default.php
+++ site/views/event/tmpl/default.php
@@ -1,93 +1,144 @@
 <?php
 /**
- * GCalendar is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * GCalendar is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with GCalendar.  If not, see <http://www.gnu.org/licenses/>.
- *
- * @package   GCalendar
- * @author    Digital Peak http://www.digital-peak.com
- * @copyright Copyright (C) 2007 - 2013 Digital Peak. All rights reserved.
- * @license   http://www.gnu.org/licenses/gpl.html GNU/GPL
+ * Native Joomla 5/6 Google Calendar event layout.
  */
+defined('_JEXEC') or die;
 
-defined('_JEXEC') or die();
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Router\Route;
 
-use Joomla\CMS\Uri\Uri;
-use Joomla\CMS\Factory;
+$event = is_array($this->event ?? null) ? $this->event : null;
+$wa = $this->getDocument()->getWebAssetManager();
+$wa->registerAndUseStyle(
+    'com_sportsmanagement.event',
+    'components/com_sportsmanagement/views/event/tmpl/default.css'
+);
 
-jsmGCalendarUtil::loadLibrary(array('jquery' => true, 'maps' => true, 'bootstrap' => true, 'gcalendar' => true));
+if (!$event) {
+    echo '<div class="alert alert-info">' . htmlspecialchars(Text::_('JGLOBAL_NO_MATCHING_RESULTS'), ENT_QUOTES, 'UTF-8') . '</div>';
 
-$document = Factory::getDocument();
-$document->addStyleSheet(Uri::base() . 'components/com_sportsmanagement/views/event/tmpl/default.css');
-$document->addScript(Uri::base() . 'components/com_sportsmanagement/views/event/tmpl/default.js');
-
-if (Factory::getApplication()->input->getCmd('tmpl', '') == 'component')
-{
-	$document->addStyleSheet(Uri::base() . 'components/com_sportsmanagement/views/event/tmpl/none-responsive.css');
+    return;
 }
 
-$dispatcher = JDispatcher::getInstance();
+$escape = static fn (mixed $value): string => htmlspecialchars((string) $value, ENT_QUOTES, 'UTF-8');
+$title = trim((string) ($event['title'] ?? ''));
+$calendarName = trim((string) ($event['calendarName'] ?? ''));
+$description = trim((string) ($event['description'] ?? ''));
+$location = trim((string) ($event['location'] ?? ''));
+$start = trim((string) ($event['start'] ?? ''));
+$end = trim((string) ($event['end'] ?? ''));
+$allDay = !empty($event['allDay']);
+$calendarId = (int) ($event['gcid'] ?? 0);
+$color = (string) ($event['color'] ?? '#135CAE');
 
-$content = '{{#events}}
-<div id="gcal-event-container" class="dp-container">
-{{#pluginsBefore}} {{{.}}} {{/pluginsBefore}}
-<div class="clearfix"></div>
-<h2>{{eventLabel}}</h2>
-<div class="row-fluid">
-	<div class="span7">
-		<div class="row-fluid">
-			<div class="span3 event-label">{{titleLabel}}: </div>
-			<div class="span9 event-content">{{title}}</div>
-		</div>
-		<div class="row-fluid">
-			<div class="span3 event-label">{{calendarNameLabel}}: </div>
-			<div class="span9 event-content">{{#calendarLink}}<a href="{{calendarLink}}">{{calendarName}}</a>{{/calendarLink}}{{^calendarLink}}{{calendarName}}{{/calendarLink}}</div>
-		</div>
-		<div class="row-fluid">
-			<div class="span3 event-label">{{dateLabel}}: </div>
-			<div class="span9 event-content">{{date}}</div>
-		</div>
-		<div class="row-fluid">
-			<div class="span3 event-label">{{locationLabel}}: </div>
-			<div class="span9 event-content">{{#location}}<a href="http://maps.google.com/?q={{location}}" target="_blank" id="gc-event-details-location">{{location}}</a>{{/location}}</div>
-		</div>
-		<div class="row-fluid">
-			<div class="span3 event-label">{{copyLabel}}: </div>
-			<div class="span9"><a target="_blank" href="{{copyGoogleUrl}}">{{copyGoogleLabel}}</a></div>
-		</div>
-		<div class="row-fluid">
-			<div class="span3 event-label"></div>
-			<div class="span9 event-content"><a target="_blank" href="{{copyOutlookUrl}}">{{copyOutlookLabel}}</a></div>
-		</div>
-	</div>
-	<div class="span5">{{#maplink}}<div id="gc-event-details-map" class="pull-right gcalendar-fixed-map" data-zoom="4"></div>{{/maplink}}</div>
+if (!preg_match('/^#[0-9A-Fa-f]{6}$/', $color)) {
+    $color = '#135CAE';
+}
+
+$googleUrl = trim((string) ($event['htmlLink'] ?? ''));
+$googleScheme = strtolower((string) parse_url($googleUrl, PHP_URL_SCHEME));
+
+if (!filter_var($googleUrl, FILTER_VALIDATE_URL) || !in_array($googleScheme, ['http', 'https'], true)) {
+    $googleUrl = '';
+}
+
+$calendarUrl = $calendarId > 0
+    ? Route::_('index.php?option=com_sportsmanagement&view=gcalendar&gcids=' . $calendarId)
+    : '';
+$mapsUrl = $location !== ''
+    ? 'https://www.google.com/maps/search/?api=1&query=' . rawurlencode($location)
+    : '';
+
+$params = $this->app->getParams();
+$dateFormat = (string) $params->get('description_date_format', 'd.m.Y');
+$timeFormat = (string) $params->get('description_time_format', 'H:i');
+$timezoneName = (string) $this->app->get('offset', 'UTC');
+
+try {
+    $timezone = new DateTimeZone($timezoneName ?: 'UTC');
+} catch (Throwable) {
+    $timezone = new DateTimeZone('UTC');
+}
+
+$displayDate = '';
+
+try {
+    $startDate = $start !== '' ? new DateTimeImmutable($start) : null;
+    $endDate = $end !== '' ? new DateTimeImmutable($end) : null;
+
+    if ($startDate) {
+        $startDate = $startDate->setTimezone($timezone);
+    }
+
+    if ($endDate) {
+        $endDate = $endDate->setTimezone($timezone);
+    }
+
+    if ($allDay && $startDate) {
+        if ($endDate && $endDate > $startDate) {
+            // Google Calendar stores all-day end dates exclusively.
+            $endDate = $endDate->modify('-1 day');
+        }
+
+        $displayDate = $startDate->format($dateFormat);
+
+        if ($endDate && $endDate->format('Y-m-d') !== $startDate->format('Y-m-d')) {
+            $displayDate .= ' – ' . $endDate->format($dateFormat);
+        }
+    } elseif ($startDate) {
+        $displayDate = $startDate->format($dateFormat . ' ' . $timeFormat);
+
+        if ($endDate) {
+            $sameDay = $endDate->format('Y-m-d') === $startDate->format('Y-m-d');
+            $displayDate .= ' – ' . $endDate->format($sameDay ? $timeFormat : $dateFormat . ' ' . $timeFormat);
+        }
+    }
+} catch (Throwable) {
+    $displayDate = $start;
+}
+?>
+<div id="gcal-event-container" class="card shadow-sm" style="border-left: .35rem solid <?= $escape($color) ?>;">
+    <div class="card-body">
+        <h2 class="card-title mb-3"><?= $escape($title !== '' ? $title : Text::_('JGLOBAL_UNTITLED')) ?></h2>
+
+        <dl class="row mb-0">
+            <?php if ($calendarName !== '') : ?>
+                <dt class="col-sm-3"><?= $escape(Text::_('COM_SPORTSMANAGEMENT_JSMGCALENDAR_GCALENDAR_VIEW_CALENDAR_LIST')) ?></dt>
+                <dd class="col-sm-9">
+                    <?php if ($calendarUrl !== '') : ?>
+                        <a href="<?= $escape($calendarUrl) ?>"><?= $escape($calendarName) ?></a>
+                    <?php else : ?>
+                        <?= $escape($calendarName) ?>
+                    <?php endif; ?>
+                </dd>
+            <?php endif; ?>
+
+            <?php if ($displayDate !== '') : ?>
+                <dt class="col-sm-3"><?= $escape(Text::_('JDATE')) ?></dt>
+                <dd class="col-sm-9">
+                    <time datetime="<?= $escape($start) ?>"><?= $escape($displayDate) ?></time>
+                </dd>
+            <?php endif; ?>
+
+            <?php if ($location !== '') : ?>
+                <dt class="col-sm-3"><?= $escape(Text::_('COM_SPORTSMANAGEMENT_TEAMINFO_TRAINING_LOCATION')) ?></dt>
+                <dd class="col-sm-9">
+                    <a href="<?= $escape($mapsUrl) ?>" target="_blank" rel="noopener noreferrer"><?= $escape($location) ?></a>
+                </dd>
+            <?php endif; ?>
+        </dl>
+
+        <?php if ($description !== '') : ?>
+            <h3 class="h5 mt-4"><?= $escape(Text::_('JGLOBAL_DESCRIPTION')) ?></h3>
+            <div class="gcal-event-description"><?= nl2br($escape($description)) ?></div>
+        <?php endif; ?>
+
+        <?php if ($googleUrl !== '') : ?>
+            <p class="mt-4 mb-0">
+                <a class="btn btn-outline-primary" href="<?= $escape($googleUrl) ?>" target="_blank" rel="noopener noreferrer">
+                    Google Calendar
+                </a>
+            </p>
+        <?php endif; ?>
+    </div>
 </div>
-{{#description}}
-<h2>{{descriptionLabel}}</h2>
-{{{description}}}
-{{/description}}
-{{#pluginsAfter}} {{{.}}} {{/pluginsAfter}}
-</div>
-{{/events}}
-{{^events}}
-{{emptyText}}
-{{/events}}';
-
-$plugins                  = array();
-$plugins['pluginsBefore'] = array();
-$plugins['pluginsAfter']  = array();
-$dispatcher->trigger('onBeforeDisplayEvent', array($this->event, &$content, &$plugins['pluginsBefore']));
-$dispatcher->trigger('onAfterDisplayEvent', array($this->event, &$content, &$plugins['pluginsAfter']));
-
-echo jsmGCalendarUtil::renderEvents(array($this->event), $content, Factory::getApplication()->getParams(), $plugins);
-
-// Echo "<div style=\"text-align:center;margin-top:10px\" ><a href=\"http://g4j.digital-peak.com\">GCalendar</a></div>\n";
