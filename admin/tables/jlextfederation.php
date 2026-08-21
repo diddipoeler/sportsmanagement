68b2adde7112061b4cdb507f7e5a3fba5f25857f
diddipoeler <diddipoeler@gmx.de>
2026-08-21 19:58:48 +0200

    Bridge legacy federation table

--- admin/tables/jlextfederation.php
+++ admin/tables/jlextfederation.php
@@ -1,60 +1,17 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage tables
- * @file       jlextfederation.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
+/** Legacy compatibility bridge for the native JlextfederationTable. */
 defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\Filter\OutputFilter;
 
-/**
- * sportsmanagementTablejlextfederation
- *
- * @package
- * @author
- * @copyright diddi
- * @version   2014
- * @access    public
- */
-class sportsmanagementTablejlextfederation extends JSMTable
-{
+use Diddipoeler\Component\SportsManagement\Administrator\Table\JlextfederationTable;
 
-	/**
-	 * Constructor
-	 *
-	 * @param   object Database connector object
-	 *
-	 * @since 1.0
-	 */
-	function __construct(&$db)
-	{
-		$db = sportsmanagementHelper::getDBConnection();
-		parent::__construct('#__sportsmanagement_federations', 'id', $db);
-	}
+if (!class_exists(JlextfederationTable::class)) {
+    $tableFile = JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Table/JlextfederationTable.php';
 
-	/**
-	 * Overloaded check method to ensure data integrity
-	 *
-	 * @access public
-	 * @return boolean True on success
-	 * @since  1.0
-	 */
-	function check()
-	{
-		$alias = OutputFilter::stringURLSafe($this->name);
+    if (is_file($tableFile)) {
+        require_once $tableFile;
+    }
+}
 
-		if (empty($this->alias) || $this->alias === $alias)
-		{
-			$this->alias = $alias;
-		}
-
-		// Should check name unicity
-		return true;
-	}
-
+if (class_exists(JlextfederationTable::class) && !class_exists('sportsmanagementTablejlextfederation', false)) {
+    class_alias(JlextfederationTable::class, 'sportsmanagementTablejlextfederation');
 }
