8dca7e90ad59a84b2c4c2ca195063551901a1fbb
diddipoeler <diddipoeler@gmx.de>
2026-08-21 21:12:36 +0200

    Bridge coefficient years field to native class

--- admin/models/fields/coefficientyears.php
+++ admin/models/fields/coefficientyears.php
@@ -1,60 +1,12 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage fields
- * @file       coefficientyears.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-defined('_JEXEC') or die('Restricted access');
+\defined('_JEXEC') or die;
 
-use Joomla\CMS\Factory;
-use Joomla\CMS\Form\Field\ListField;
-use Joomla\CMS\Language\Text;
-use Joomla\Database\DatabaseInterface;
+use Diddipoeler\Component\SportsManagement\Administrator\Field\CoefficientyearsField;
 
-/**
- * JFormFieldcoefficientyears
- *
- * @package
- * @author Dieter Plöger
- * @copyright 2022
- * @version $Id$
- * @access public
- */
-class JFormFieldcoefficientyears extends ListField
-{
-	protected $type = 'coefficientyears';
+if (!class_exists(CoefficientyearsField::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Field/CoefficientyearsField.php';
+}
 
-	/**
-	 * FormFieldcoefficientyears::getOptions()
-	 *
-	 * @return array
-	 */
-	protected function getOptions()
-	{
-		$db    = Factory::getContainer()->get(DatabaseInterface::class);
-		$query = $db->createQuery();
-		$query->select('season AS value, season AS text');
-		$query->from('#__sportsmanagement_uefawertung');
-		$query->group('season');
-		$query->order('season DESC');
-
-		try
-		{
-			$db->setQuery($query);
-			$options = $db->loadObjectList();
-		}
-		catch (Exception $e)
-		{
-			Factory::getApplication()->enqueueMessage(Text::sprintf('COM_SPORTSMANAGEMENT_DATABASE_ERROR_FUNCTION_FAILED', $e->getCode(), $e->getMessage()), 'error');
-			Factory::getApplication()->enqueueMessage(Text::sprintf('COM_SPORTSMANAGEMENT_FILE_ERROR_FUNCTION_FAILED', __FILE__, __LINE__), 'error');
-			$options = array();
-		}
-
-		return array_merge(parent::getOptions(), $options);
-	}
+if (!class_exists('JFormFieldcoefficientyears', false)) {
+    class_alias(CoefficientyearsField::class, 'JFormFieldcoefficientyears');
 }
