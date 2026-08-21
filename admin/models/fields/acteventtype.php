d4e678ca30b2bb74e82e11b038af6b860330ef83
diddipoeler <diddipoeler@gmx.de>
2026-08-21 21:06:18 +0200

    Bridge active event type field to native class

--- admin/models/fields/acteventtype.php
+++ admin/models/fields/acteventtype.php
@@ -1,68 +1,12 @@
 <?php
-/**
- *
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage fields
- * @file       acteventtype.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
+\defined('_JEXEC') or die;
 
-defined('_JEXEC') or die('Restricted access');
+use Diddipoeler\Component\SportsManagement\Administrator\Field\ActeventtypeField;
 
-use Joomla\CMS\Factory;
-use Joomla\CMS\Form\Field\ListField;
-use Joomla\CMS\Language\Text;
-use Joomla\Database\DatabaseInterface;
+if (!class_exists(ActeventtypeField::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Field/ActeventtypeField.php';
+}
 
-/**
- * FormFieldacteventtype
- *
- * @package
- * @author
- * @copyright diddi
- * @version   2014
- * @access    public
- */
-class JFormFieldacteventtype extends ListField
-{
-	protected $type = 'acteventtype';
-
-	/**
-	 * FormFieldacteventtype::getOptions()
-	 *
-	 * @return array
-	 */
-	protected function getOptions()
-	{
-		$options   = array();
-		$targetTable = preg_replace('/[^A-Za-z0-9_]/', '', (string) $this->element['targettable']);
-		$selectId = Factory::getApplication()->getInput()->getInt('id', 0);
-
-		if ($targetTable === '' || !$selectId)
-		{
-			return parent::getOptions();
-		}
-
-		$db    = Factory::getContainer()->get(DatabaseInterface::class);
-		$query = $db->createQuery();
-		$query->select('s.id AS value, s.name AS text');
-		$query->from('#__sportsmanagement_eventtype AS s');
-		$query->join('INNER', '#__sportsmanagement_' . $targetTable . ' AS t ON t.sports_type_id = s.sports_type_id');
-		$query->where('t.id = ' . (int) $selectId);
-		$query->order('s.name');
-		$db->setQuery($query);
-		$options = $db->loadObjectList();
-
-		foreach ($options as $row)
-		{
-			$row->text = Text::_($row->text);
-		}
-
-		return array_merge(parent::getOptions(), $options);
-	}
+if (!class_exists('JFormFieldacteventtype', false)) {
+    class_alias(ActeventtypeField::class, 'JFormFieldacteventtype');
 }
