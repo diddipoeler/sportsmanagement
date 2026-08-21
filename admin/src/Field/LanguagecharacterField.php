a7108a6173fbb180986d8690a52a84d3a3188436
diddipoeler <diddipoeler@gmx.de>
2026-08-21 21:52:01 +0200

    Add native language character field

---
+++ admin/src/Field/LanguagecharacterField.php
@@ -0,0 +1,31 @@
+<?php
+namespace Diddipoeler\Component\SportsManagement\Administrator\Field;
+
+\defined('_JEXEC') or die;
+
+use Joomla\CMS\Factory;
+use Joomla\CMS\Form\Field\ListField;
+use Joomla\CMS\HTML\HTMLHelper;
+
+final class LanguagecharacterField extends ListField
+{
+    protected $type = 'languagecharacter';
+
+    protected function getOptions(): array
+    {
+        $tag = Factory::getApplication()->getLanguage()->getTag();
+        [$start, $end] = match ($tag) {
+            'ru-RU' => [hexdec('0410'), hexdec('042F')],
+            'el-GR' => [hexdec('0391'), hexdec('03A9')],
+            default => [hexdec('0041'), hexdec('005A')],
+        };
+        $options = [];
+
+        for ($codepoint = $start; $codepoint <= $end; $codepoint++) {
+            $character = html_entity_decode('&#' . $codepoint . ';', ENT_QUOTES | ENT_HTML5, 'UTF-8');
+            $options[] = HTMLHelper::_('select.option', $codepoint, $character);
+        }
+
+        return array_merge(parent::getOptions(), $options);
+    }
+}
