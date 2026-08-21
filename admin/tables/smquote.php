b905841118db741f6761798ebbd1376af5a23839
diddipoeler <diddipoeler@gmx.de>
2026-08-21 20:01:59 +0200

    Bridge legacy quote table

--- admin/tables/smquote.php
+++ admin/tables/smquote.php
@@ -1,38 +1,17 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage tables
- * @file       smquote.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
+/** Legacy compatibility bridge for the native SmquoteTable. */
 defined('_JEXEC') or die('Restricted access');
 
-/**
- * sportsmanagementTablesmquote
- *
- * @package
- * @author
- * @copyright diddi
- * @version   2014
- * @access    public
- */
-class sportsmanagementTablesmquote extends JSMTable
-{
-	/**
-	 * Constructor
-	 *
-	 * @param   object Database connector object
-	 *
-	 * @since 1.0
-	 */
-	function __construct(&$db)
-	{
-		$db = sportsmanagementHelper::getDBConnection();
-		parent::__construct('#__sportsmanagement_rquote', 'id', $db);
-	}
+use Diddipoeler\Component\SportsManagement\Administrator\Table\SmquoteTable;
 
+if (!class_exists(SmquoteTable::class)) {
+    $tableFile = JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Table/SmquoteTable.php';
+
+    if (is_file($tableFile)) {
+        require_once $tableFile;
+    }
+}
+
+if (class_exists(SmquoteTable::class) && !class_exists('sportsmanagementTablesmquote', false)) {
+    class_alias(SmquoteTable::class, 'sportsmanagementTablesmquote');
 }
