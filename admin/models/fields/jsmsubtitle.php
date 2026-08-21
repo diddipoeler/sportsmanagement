b934ff34218396b42c4c3135159ee768f1666138
diddipoeler <diddipoeler@gmx.de>
2026-08-21 21:55:53 +0200

    Bridge JSM subtitle field to native class

--- admin/models/fields/jsmsubtitle.php
+++ admin/models/fields/jsmsubtitle.php
@@ -1,71 +1,12 @@
 <?php
-/**
- *
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage fields
- * @file       jsmsubtitle.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
+\defined('_JEXEC') or die;
 
+use Diddipoeler\Component\SportsManagement\Administrator\Field\JsmsubtitleField;
 
-defined('_JEXEC') or die;
+if (!class_exists(JsmsubtitleField::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Field/JsmsubtitleField.php';
+}
 
-use Joomla\CMS\Language\Text;
-use Joomla\CMS\Form\FormField;
-
-
-/**
- * FormFieldJSMSubtitle
- *
- * @package
- * @author    Dieter Plöger
- * @copyright 2018
- * @version   $Id$
- * @access    public
- */
-class JFormFieldJSMSubtitle extends FormField
-{
-	public $type = 'JSMSubtitle';
-
-	/**
-	 * Method to get the field options.
-	 *
-	 * @return array    The field option objects.
-	 * @since  1.6
-	 */
-	protected function getLabel()
-	{
-
-		$html  = '';
-		$value = trim($this->element['title']);
-
-		$html .= '<div style="clear: both;"></div>';
-		$html .= '<div style="margin: 20px 0 20px 20px; font-weight: bold; padding: 5px; color: #444444; border-bottom: 1px solid #444444;">';
-
-		if ($value)
-		{
-			$html .= Text::_($value);
-		}
-
-		$html .= '</div>';
-
-		return $html;
-	}
-
-	/**
-	 * Method to get the field input markup.
-	 *
-	 * @return string    The field input markup.
-	 * @since  1.6
-	 */
-	protected function getInput()
-	{
-		return '';
-	}
-
+if (!class_exists('JFormFieldJSMSubtitle', false)) {
+    class_alias(JsmsubtitleField::class, 'JFormFieldJSMSubtitle');
 }
