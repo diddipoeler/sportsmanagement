6c1fed1abbb9897c3d3c100be3823d2de88152f7
diddipoeler <diddipoeler@gmx.de>
2026-08-21 20:57:55 +0200

    Add native project list field

---
+++ admin/src/Field/ProjectlistField.php
@@ -0,0 +1,32 @@
+<?php
+namespace Diddipoeler\Component\SportsManagement\Administrator\Field;
+
+\defined('_JEXEC') or die;
+
+use Joomla\CMS\HTML\HTMLHelper;
+
+final class ProjectlistField extends SportsManagementListField
+{
+    protected $type = 'projectlist';
+
+    protected function getOptions(): array
+    {
+        $db = $this->getSportsManagementDatabase();
+        $query = $db->getQuery(true)
+            ->select([
+                $db->quoteName('id', 'value'),
+                $db->quoteName('name', 'text'),
+            ])
+            ->from($db->quoteName('#__sportsmanagement_project'))
+            ->order($db->quoteName('name'));
+        $db->setQuery($query);
+
+        $options = [];
+
+        foreach ($db->loadObjectList() ?: [] as $item) {
+            $options[] = HTMLHelper::_('select.option', $item->value, $item->text);
+        }
+
+        return array_merge(parent::getOptions(), $options);
+    }
+}
