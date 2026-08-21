b93c3bcc44d2897f878a26c11f7b1e93882e4cc4
diddipoeler <diddipoeler@gmx.de>
2026-08-21 17:53:21 +0200

    Modernize count record module compatibility entry

--- modules/mod_sportsmanagement_count_rekord/mod_sportsmanagement_count_rekord.php
+++ modules/mod_sportsmanagement_count_rekord/mod_sportsmanagement_count_rekord.php
@@ -1,59 +1,20 @@
 <?php
 /**
- *
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage mod_sportsmanagement_count_rekord
- * @file       mod_sportsmanagement_count_rekord.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * Legacy entry bridge for the Joomla 5/6 SportsManagement count record module.
  */
+defined('_JEXEC') or die;
 
-defined('_JEXEC') or die('Restricted access');
-
-use Joomla\CMS\Helper\ModuleHelper;
+use Diddipoeler\Module\SportsManagementCountRekord\Site\Helper\CountRekordHelper;
 use Joomla\CMS\Factory;
-use Joomla\CMS\MVC\Model\BaseDatabaseModel;
+use Joomla\CMS\Helper\ModuleHelper;
 
-if (!defined('DS'))
-{
-	define('DS', DIRECTORY_SEPARATOR);
+if (!class_exists(CountRekordHelper::class)) {
+    require_once __DIR__ . '/src/Helper/CountRekordHelper.php';
 }
 
-if (!defined('JSM_PATH'))
-{
-	DEFINE('JSM_PATH', 'components/com_sportsmanagement');
-}
+$list = (new CountRekordHelper())->getData($params, $module, Factory::getApplication());
 
-/**
- *
- * prüft vor Benutzung ob die gewünschte Klasse definiert ist
- */
-if (!class_exists('sportsmanagementHelper'))
-{
-	/**
-	 * add the classes for handling
-	 */
-	$classpath = JPATH_ADMINISTRATOR . DIRECTORY_SEPARATOR . JSM_PATH . DIRECTORY_SEPARATOR . 'helpers' . DIRECTORY_SEPARATOR . 'sportsmanagement.php';
-	JLoader::register('sportsmanagementHelper', $classpath);
-	BaseDatabaseModel::getInstance("sportsmanagementHelper", "sportsmanagementModel");
-}
-
-/**
- *
- * Include the functions only once
- */
-JLoader::register('modJSMStatistikRekordHelper', __DIR__ . '/helper.php');
-
-$document = Factory::getDocument();
-
-?>
-<div class="<?php echo $params->get('moduleclass_sfx'); ?>"
-     id="<?php echo $module->module; ?>-<?php echo $module->id; ?>">
-	<?PHP
-	require ModuleHelper::getLayoutPath($module->module);
-	?>
-</div>
+require ModuleHelper::getLayoutPath(
+    $module->module,
+    (string) $params->get('layout', 'default')
+);
