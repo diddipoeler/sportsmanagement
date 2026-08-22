7c814a2593f1d83aaeeef60419ada6747d32695c
diddipoeler <diddipoeler@gmx.de>
2026-08-22 13:46:24 +0200

    Modernize current season Bootstrap button rendering

--- modules/mod_sportsmanagement_act_season/tmpl/default.php
+++ modules/mod_sportsmanagement_act_season/tmpl/default.php
@@ -18,10 +18,19 @@ $routeFor = static function (object $row): string {
         'to' => 0,
         'division' => 0,
     ]);
+
     return Route::_('index.php?' . $query, false);
 };
+
+$buttonClass = trim((string) $params->get('button_class', 'btn btn-secondary'));
+if ($buttonClass === 'btn btn-default') {
+    $buttonClass = 'btn btn-secondary';
+}
+$buttonClass = htmlspecialchars($buttonClass, ENT_QUOTES, 'UTF-8');
+$moduleClass = htmlspecialchars((string) $params->get('moduleclass_sfx', ''), ENT_QUOTES, 'UTF-8');
+$moduleId = htmlspecialchars($module->module . '-' . $module->id, ENT_QUOTES, 'UTF-8');
 ?>
-<div class="<?php echo htmlspecialchars((string) $params->get('moduleclass_sfx', ''), ENT_QUOTES, 'UTF-8'); ?>" id="<?php echo htmlspecialchars($module->module . '-' . $module->id, ENT_QUOTES, 'UTF-8'); ?>">
+<div class="<?php echo $moduleClass; ?>" id="<?php echo $moduleId; ?>">
 <?php if ($params->get('show_slider')) : ?>
     <?php echo HTMLHelper::_('bootstrap.startTabSet', 'jsm-act-season-' . (int) $module->id, ['active' => 'fed-0']); ?>
     <?php $tab = 0; foreach ($federations as $fedId => $federation) : ?>
@@ -30,8 +39,8 @@ $routeFor = static function (object $row): string {
             <div class="row g-2 mb-2">
                 <?php foreach ($list as $row) : if ($row->country !== $country->alpha3) { continue; } ?>
                     <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12">
-                        <a href="<?php echo htmlspecialchars($routeFor($row), ENT_QUOTES, 'UTF-8'); ?>" class="<?php echo htmlspecialchars((string) $params->get('button_class', 'btn btn-primary'), ENT_QUOTES, 'UTF-8'); ?> w-100" role="button">
-                            <?php echo $row->flag_html; ?> <?php echo Text::_($row->name); ?>
+                        <a href="<?php echo htmlspecialchars($routeFor($row), ENT_QUOTES, 'UTF-8'); ?>" class="<?php echo $buttonClass; ?> w-100" role="button">
+                            <?php echo $row->flag_html; ?> <?php echo htmlspecialchars(Text::_((string) $row->name), ENT_QUOTES, 'UTF-8'); ?>
                         </a>
                     </div>
                 <?php endforeach; ?>
@@ -44,8 +53,8 @@ $routeFor = static function (object $row): string {
     <div class="row g-2">
         <?php foreach ($list as $row) : ?>
             <div class="col-xl-2 col-lg-3 col-md-4 col-sm-6">
-                <a href="<?php echo htmlspecialchars($routeFor($row), ENT_QUOTES, 'UTF-8'); ?>" class="<?php echo htmlspecialchars((string) $params->get('button_class', 'btn btn-primary'), ENT_QUOTES, 'UTF-8'); ?> w-100" role="button">
-                    <?php echo $row->flag_html; ?> <?php echo Text::_($row->name); ?>
+                <a href="<?php echo htmlspecialchars($routeFor($row), ENT_QUOTES, 'UTF-8'); ?>" class="<?php echo $buttonClass; ?> w-100" role="button">
+                    <?php echo $row->flag_html; ?> <?php echo htmlspecialchars(Text::_((string) $row->name), ENT_QUOTES, 'UTF-8'); ?>
                 </a>
             </div>
         <?php endforeach; ?>
