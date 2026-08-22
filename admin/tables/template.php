3b8a16c9d557dbad771b7db4b5a3f864389f3d71
diddipoeler <diddipoeler@gmx.de>
2026-08-22 18:55:17 +0200

    Bridge template table to Joomla 5/6 class

--- admin/tables/template.php
+++ admin/tables/template.php
@@ -1,39 +1,13 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage tables
- * @file       division.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-defined('_JEXEC') or die('Restricted access');
+/** SportsManagement legacy compatibility bridge for the template table. */
+\defined('_JEXEC') or die;
 
-/**
- * sportsmanagementTableTemplate
- *
- * @package
- * @author
- * @copyright diddi
- * @version   2014
- * @access    public
- */
-class sportsmanagementTableTemplate extends JSMTable
-{
-	/**
-	 * Constructor
-	 *
-	 * @param   object Database connector object
-	 *
-	 * @since 1.0
-	 */
-	function __construct(&$db)
-	{
-		$db = sportsmanagementHelper::getDBConnection();
-		parent::__construct('#__sportsmanagement_template_config', 'id', $db);
-	}
+use Diddipoeler\Component\SportsManagement\Administrator\Table\TemplateTable;
 
+if (!class_exists(TemplateTable::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Table/TemplateTable.php';
+}
 
+if (!class_exists('sportsmanagementTableTemplate', false)) {
+    class_alias(TemplateTable::class, 'sportsmanagementTableTemplate');
 }
