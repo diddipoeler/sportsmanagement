349c042944f91714fcebb6061a69edf7485686c1
diddipoeler <diddipoeler@gmx.de>
2026-08-22 12:57:12 +0200

    Add Joomla 5/6 AJAX top navigation module provider

---
+++ modules/mod_sportsmanagement_ajax_top_navigation_menu/services/provider.php
@@ -0,0 +1,22 @@
+<?php
+\defined('_JEXEC') or die;
+
+use Joomla\CMS\Extension\Service\Provider\HelperFactory;
+use Joomla\CMS\Extension\Service\Provider\Module;
+use Joomla\CMS\Extension\Service\Provider\ModuleDispatcherFactory;
+use Joomla\DI\Container;
+use Joomla\DI\ServiceProviderInterface;
+
+return new class implements ServiceProviderInterface
+{
+    public function register(Container $container): void
+    {
+        $container->registerServiceProvider(
+            new ModuleDispatcherFactory('\\Diddipoeler\\Module\\SportsManagementAjaxTopNavigationMenu')
+        );
+        $container->registerServiceProvider(
+            new HelperFactory('\\Diddipoeler\\Module\\SportsManagementAjaxTopNavigationMenu\\Site\\Helper')
+        );
+        $container->registerServiceProvider(new Module());
+    }
+};
