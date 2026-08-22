3ae809981a52a9162b2b9e716f9f4a8a58f69179
diddipoeler <diddipoeler@gmx.de>
2026-08-22 22:20:28 +0200

    Bridge legacy teams view to Joomla 5/6

--- site/views/teams/view.html.php
+++ site/views/teams/view.html.php
@@ -1,54 +1,15 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage teams
- * @file       view.html.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@arcor.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\Language\Text;
+/** Legacy compatibility bridge for the native Joomla 5/6 Teams view. */
+\defined('_JEXEC') or die;
 
-/**
- * sportsmanagementViewTeams
- *
- * @package
- * @author    diddi
- * @copyright 2014
- * @version   $Id$
- * @access    public
- */
-class sportsmanagementViewTeams extends sportsmanagementView
-{
+use Diddipoeler\Component\SportsManagement\Site\View\Teams\HtmlView;
 
-	/**
-	 * sportsmanagementViewTeams::init()
-	 *
-	 * @return void
-	 */
-	function init()
-	{
-		
-		$this->division = sportsmanagementModelProject::getDivision($this->jinput->getInt("division", 0), $this->jinput->getInt('cfg_which_database', 0));
-		$this->teams    = sportsmanagementModelProject::getTeams($this->jinput->getInt("division", 0), 'name', $this->jinput->getInt('cfg_which_database', 0), '', $this->config['show_club_playground']);
+if (!class_exists(HtmlView::class)) {
+    require_once JPATH_SITE . '/components/com_sportsmanagement/src/View/SportsManagementHtmlView.php';
+    require_once JPATH_SITE . '/components/com_sportsmanagement/src/View/SportsManagementProjectHtmlView.php';
+    require_once JPATH_SITE . '/components/com_sportsmanagement/src/View/Teams/HtmlView.php';
+}
 
-		$pageTitle = Text::_('COM_SPORTSMANAGEMENT_TEAMS_TITLE');
-
-		if (isset($this->project))
-		{
-			$pageTitle .= " " . $this->project->name;
-
-			if (isset($this->division))
-			{
-				$pageTitle .= " : " . $this->division->name;
-			}
-		}
-
-		$this->document->setTitle($pageTitle);
-		$this->headertitle = Text::_('COM_SPORTSMANAGEMENT_TEAMS_TITLE');
-
-	}
+if (!class_exists('sportsmanagementViewTeams', false)) {
+    class_alias(HtmlView::class, 'sportsmanagementViewTeams');
 }
