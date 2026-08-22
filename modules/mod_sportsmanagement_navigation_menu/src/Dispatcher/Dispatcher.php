6b5747ec81b220071009a90075cac6db78e6b2b9
diddipoeler <diddipoeler@gmx.de>
2026-08-22 13:08:36 +0200

    Add native Joomla 5/6 navigation menu dispatcher

---
+++ modules/mod_sportsmanagement_navigation_menu/src/Dispatcher/Dispatcher.php
@@ -0,0 +1,43 @@
+<?php
+namespace Diddipoeler\Module\SportsManagementNavigationMenu\Site\Dispatcher;
+
+\defined('_JEXEC') or die;
+
+use Joomla\CMS\Dispatcher\AbstractModuleDispatcher;
+use Joomla\CMS\Helper\HelperFactoryAwareInterface;
+use Joomla\CMS\Helper\HelperFactoryAwareTrait;
+
+final class Dispatcher extends AbstractModuleDispatcher implements HelperFactoryAwareInterface
+{
+    use HelperFactoryAwareTrait;
+
+    protected function getLayoutData(): array
+    {
+        $data = parent::getLayoutData();
+        $app = $this->getApplication();
+        $app->getLanguage()->load('com_sportsmanagement', JPATH_ADMINISTRATOR, null, true);
+
+        $payload = $this->getHelperFactory()
+            ->getHelper('NavigationMenuHelper')
+            ->getData($data['params'], $app);
+
+        foreach ($payload as $key => $value) {
+            $data[$key] = $value;
+        }
+
+        $assets = $app->getDocument()->getWebAssetManager();
+        $assets->registerAndUseStyle(
+            'mod_sportsmanagement_navigation_menu',
+            'modules/mod_sportsmanagement_navigation_menu/css/mod_sportsmanagement_navigation_menu.css',
+            ['version' => 'auto']
+        );
+        $assets->registerAndUseScript(
+            'mod_sportsmanagement_navigation_menu',
+            'modules/mod_sportsmanagement_navigation_menu/js/mod_sportsmanagement_navigation_menu.js',
+            ['version' => 'auto'],
+            ['defer' => true]
+        );
+
+        return $data;
+    }
+}
