10cbe79268d23cdc49e7b8fd3292f243c0aa3cf9
diddipoeler <diddipoeler@gmx.de>
2026-08-22 18:56:21 +0200

    Bridge event type table to Joomla 5/6 class

--- admin/tables/eventtype.php
+++ admin/tables/eventtype.php
@@ -1,62 +1,13 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage tables
- * @file       eventtype.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\Filter\OutputFilter;
+/** SportsManagement legacy compatibility bridge for the event type table. */
+\defined('_JEXEC') or die;
 
-/**
- * sportsmanagementTableEventtype
- *
- * @package
- * @author
- * @copyright diddi
- * @version   2014
- * @access    public
- */
-class sportsmanagementTableEventtype extends JSMTable
-{
-	/**
-	 * Constructor
-	 *
-	 * @param   object Database connector object
-	 *
-	 * @since 1.0
-	 */
-	function __construct(&$db)
-	{
-		$db = sportsmanagementHelper::getDBConnection();
-		parent::__construct('#__sportsmanagement_eventtype', 'id', $db);
-	}
+use Diddipoeler\Component\SportsManagement\Administrator\Table\EventtypeTable;
 
-	/**
-	 * Overloaded check method to ensure data integrity
-	 *
-	 * @access public
-	 * @return boolean True on success
-	 * @since  1.0
-	 */
-	function check()
-	{
-		// Setting alias
-		if (empty($this->alias))
-		{
-			$this->alias = OutputFilter::stringURLSafe($this->name);
-		}
-		else
-		{
-			$this->alias = OutputFilter::stringURLSafe($this->alias); // Make sure the user didn't modify it to something illegal...
-		}
+if (!class_exists(EventtypeTable::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Table/EventtypeTable.php';
+}
 
-		return true;
-	}
-
-
+if (!class_exists('sportsmanagementTableEventtype', false)) {
+    class_alias(EventtypeTable::class, 'sportsmanagementTableEventtype');
 }
