3b624d77e0e41f509a6ec3bb78707182e4824394
diddipoeler <diddipoeler@gmx.de>
2026-08-22 15:30:14 +0200

    Load first league Bootstrap tabs via WAM

--- modules/mod_sportsmanagement_firstleagueoverview/src/Dispatcher/Dispatcher.php
+++ modules/mod_sportsmanagement_firstleagueoverview/src/Dispatcher/Dispatcher.php
@@ -14,8 +14,8 @@ final class Dispatcher extends AbstractModuleDispatcher implements HelperFactory
     protected function getLayoutData(): array
     {
         $data = parent::getLayoutData();
-
-        $this->getApplication()->getLanguage()->load('com_sportsmanagement', JPATH_SITE, null, true);
+        $app = $this->getApplication();
+        $app->getLanguage()->load('com_sportsmanagement', JPATH_SITE, null, true);
 
         $overview = $this->getHelperFactory()
             ->getHelper('FirstLeagueOverviewHelper')
@@ -24,13 +24,12 @@ final class Dispatcher extends AbstractModuleDispatcher implements HelperFactory
         $data['firstleagueoverview'] = $overview['projects'];
         $data['federations'] = $overview['federations'];
 
-        $this->getApplication()
-            ->getDocument()
-            ->getWebAssetManager()
-            ->registerAndUseStyle(
-                'mod_sportsmanagement_firstleagueoverview',
-                'modules/mod_sportsmanagement_firstleagueoverview/css/mod_sportsmanagement_firstleagueoverview.css'
-            );
+        $wam = $app->getDocument()->getWebAssetManager();
+        $wam->useScript('bootstrap.tab');
+        $wam->registerAndUseStyle(
+            'mod_sportsmanagement_firstleagueoverview',
+            'modules/mod_sportsmanagement_firstleagueoverview/css/mod_sportsmanagement_firstleagueoverview.css'
+        );
 
         return $data;
     }
