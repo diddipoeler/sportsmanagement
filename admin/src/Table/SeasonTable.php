da421eb2cb35cd655efb9022400f7898089e62dd
diddipoeler <diddipoeler@gmx.de>
2026-08-22 08:50:58 +0200

    Preserve season alias validation in native table

--- admin/src/Table/SeasonTable.php
+++ admin/src/Table/SeasonTable.php
@@ -1,11 +1,31 @@
 <?php
 namespace Diddipoeler\Component\SportsManagement\Administrator\Table;
+
 \defined('_JEXEC') or die;
+
+use Joomla\CMS\Factory;
+use Joomla\CMS\Filter\OutputFilter;
 use Joomla\Database\DatabaseInterface;
+
 final class SeasonTable extends SportsManagementTable
 {
     public function __construct(DatabaseInterface $db)
     {
         parent::__construct('#__sportsmanagement_season', 'id', $db);
+    }
+
+    public function check()
+    {
+        if (!$this->alias) {
+            $this->alias = $this->name;
+        }
+
+        $this->alias = OutputFilter::stringURLSafe((string) $this->alias);
+
+        if (trim(str_replace('-', '', (string) $this->alias)) === '') {
+            $this->alias = Factory::getDate()->format('Y-m-d-H-i-s');
+        }
+
+        return parent::check();
     }
 }
