a9b2ecba497b54c8990cd9eb5478c758c4e154c7
diddipoeler <diddipoeler@gmx.de>
2026-08-22 18:58:21 +0200

    Bridge division table to Joomla 5/6 class

--- admin/tables/division.php
+++ admin/tables/division.php
@@ -1,51 +1,13 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage tables
- * @file       division.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\Filter\OutputFilter;
+/** SportsManagement legacy compatibility bridge for the division table. */
+\defined('_JEXEC') or die;
 
-/**
- * sportsmanagementTableDivision
- *
- * @package
- * @author
- * @copyright diddi
- * @version   2014
- * @access    public
- */
-class sportsmanagementTableDivision extends JSMTable
-{
-	/**
-	 * Constructor
-	 *
-	 * @param   object Database connector object
-	 *
-	 * @since 1.0
-	 */
-	function __construct(&$db)
-	{
-		$db = sportsmanagementHelper::getDBConnection();
-		parent::__construct('#__sportsmanagement_division', 'id', $db);
-	}
+use Diddipoeler\Component\SportsManagement\Administrator\Table\DivisionTable;
 
-	/**
-	 * Overloaded check method to ensure data integrity
-	 *
-	 * @access public
-	 * @return boolean True on success
-	 * @since  1.0
-	 */
-	function check()
-	{
-		$this->alias = OutputFilter::stringURLSafe($this->name);
-		return true;
-	}
+if (!class_exists(DivisionTable::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Table/DivisionTable.php';
+}
+
+if (!class_exists('sportsmanagementTableDivision', false)) {
+    class_alias(DivisionTable::class, 'sportsmanagementTableDivision');
 }
