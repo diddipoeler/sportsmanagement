5f9c641e3ed7ad76955dea896b5429d7b2e828c8
diddipoeler <diddipoeler@gmx.de>
2026-08-22 17:11:45 +0200

    Replace legacy Google Calendar layout

--- modules/mod_sportsmanagement_google_calendar/tmpl/calendar.php
+++ modules/mod_sportsmanagement_google_calendar/tmpl/calendar.php
@@ -1,51 +1,12 @@
 <?php
 /**
+ * Joomla 5/6 compatibility layout for the historical calendar view.
  *
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage mod_sportsmanagement_google_calendar
- * @file       calendar.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * The previous layout embedded an obsolete jQuery FullCalendar runtime and
+ * exposed the Google API key in inline JavaScript. Keep existing module
+ * assignments using the "calendar" layout working by rendering the same
+ * server-side, escaped event data as the native default layout.
  */
+\defined('_JEXEC') or die;
 
-defined('_JEXEC') or die;
-
-use Joomla\CMS\Uri\Uri;
-use Joomla\CMS\Factory;
-
-// Load the FullCalendar assets
-$document = Factory::getDocument();
-$document->addStyleSheet(Uri::root() . '/modules/mod_google_calendar/media/fullcalendar/fullcalendar.min.css');
-$document->addScript(Uri::root() . '/modules/mod_google_calendar/media/fullcalendar/lib/moment.min.js');
-$document->addScript(Uri::root() . '/modules/mod_google_calendar/media/fullcalendar/fullcalendar.min.js');
-$document->addScript(Uri::root() . '/modules/mod_google_calendar/media/fullcalendar/lang/de.js');
-$document->addScript(Uri::root() . '/modules/mod_google_calendar/media/fullcalendar/gcal.js');
-?>
-<script>
-    jQuery(document).ready(function ($) {
-        jQuery('#calendar-<?php echo $module->id; ?>').fullCalendar({
-            googleCalendarApiKey: '<?php echo $params->get('api_key', null); ?>',
-            events: {
-                googleCalendarId: '<?php echo $params->get('calendar_id', null); ?>'
-            },
-            header: {
-                left: 'prev,next today',
-                center: 'title',
-                right: 'month,agendaWeek,agendaDay'
-            },
-            timeFormat: 'H:mm',
-            eventClick: function (event) {
-                window.open(event.url, 'gcalevent', 'width=700,height=600');
-                return false;
-            },
-            loading: function (bool) {
-                $('#loading').toggle(bool);
-            }
-        });
-    });
-</script>
-<div id="calendar-<?php echo $module->id; ?>"></div>
+require __DIR__ . '/default.php';
