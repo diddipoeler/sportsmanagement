46b13cf50c8073cc2227b4843c94ad386f983ace
diddipoeler <diddipoeler@gmx.de>
2026-08-22 10:28:01 +0200

    Modernize playground ticker module entry point

--- modules/mod_sportsmanagement_playground_ticker/mod_sportsmanagement_playground_ticker.php
+++ modules/mod_sportsmanagement_playground_ticker/mod_sportsmanagement_playground_ticker.php
@@ -1,83 +1,30 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage mod_sportsmanagement_playground_ticker
- * @file       mod_sportsmanagement_playground_ticker.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\MVC\Model\BaseDatabaseModel;
+/** Joomla 5/6 compatibility entry point for the SportsManagement playground ticker module. */
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Component\ComponentHelper;
+use Joomla\CMS\Factory;
 use Joomla\CMS\Helper\ModuleHelper;
 use Joomla\CMS\Uri\Uri;
-use Joomla\CMS\Factory;
-use Joomla\CMS\Component\ComponentHelper;
 
-if (!defined('DS'))
-{
-	define('DS', DIRECTORY_SEPARATOR);
-}
+require_once __DIR__ . '/helper.php';
 
-if (!defined('JSM_PATH'))
-{
-	DEFINE('JSM_PATH', 'components/com_sportsmanagement');
-}
+$app = Factory::getApplication();
+$componentParams = ComponentHelper::getParams('com_sportsmanagement');
+$module->picture_server = $componentParams->get('cfg_which_database')
+    ? (string) $componentParams->get('cfg_which_database_server')
+    : Uri::root();
 
-/**
- * prüft vor Benutzung ob die gewünschte Klasse definiert ist
- */
-if (!class_exists('JSMModelLegacy'))
-{
-	JLoader::import('components.com_sportsmanagement.libraries.sportsmanagement.model', JPATH_SITE);
-}
+$playgrounds = modJSMPlaygroundTicker::getData($params);
 
+$app->getDocument()
+    ->getWebAssetManager()
+    ->registerAndUseStyle(
+        'mod_sportsmanagement_playground_ticker',
+        'modules/' . $module->module . '/css/' . $module->module . '.css'
+    );
 
-if (!class_exists('JSMCountries'))
-{
-	JLoader::import('components.com_sportsmanagement.helpers.countries', JPATH_SITE);
-}
-
-
-if (!class_exists('sportsmanagementHelper'))
-{
-	// Add the classes for handling
-	$classpath = JPATH_ADMINISTRATOR . DIRECTORY_SEPARATOR . JSM_PATH . DIRECTORY_SEPARATOR . 'helpers' . DIRECTORY_SEPARATOR . 'sportsmanagement.php';
-	JLoader::register('sportsmanagementHelper', $classpath);
-	BaseDatabaseModel::getInstance("sportsmanagementHelper", "sportsmanagementModel");
-}
-
-/**
- *
- * Include the functions only once
- */
-JLoader::register('modJSMPlaygroundTicker', __DIR__ . '/helper.php');
-
-/**
- * soll die externe datenbank genutzt werden ?
- */
-if (ComponentHelper::getParams('com_sportsmanagement')->get('cfg_which_database'))
-{
-	$module->picture_server = ComponentHelper::getParams('com_sportsmanagement')->get('cfg_which_database_server');
-}
-else
-{
-	$module->picture_server = Uri::root();
-}
-
-$playgrounds = modJSMPlaygroundTicker::getData($params);	
-
-$document = Factory::getDocument();
-
-// Add css file
-$document->addStyleSheet(Uri::base() . 'modules' . DIRECTORY_SEPARATOR . $module->module . DIRECTORY_SEPARATOR . 'css' . DIRECTORY_SEPARATOR . $module->module . '.css');
-
-?>
-<div class="<?php echo $params->get('moduleclass_sfx'); ?>"
-     id="<?php echo $module->module; ?>-<?php echo $module->id; ?>">
-	<?PHP
-	require ModuleHelper::getLayoutPath($module->module);
-	?>
-</div>
+require ModuleHelper::getLayoutPath(
+    $module->module,
+    (string) $params->get('layout', 'default')
+);
