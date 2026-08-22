c92c66688fdd0abc905e5fb3e913398ffc9da5bc
diddipoeler <diddipoeler@gmx.de>
2026-08-22 08:52:21 +0200

    Bridge legacy league table to native implementation

--- admin/tables/league.php
+++ admin/tables/league.php
@@ -1,55 +1,18 @@
 <?php
 /**
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage tables
- * @file       league.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\Filter\OutputFilter;
-
-/**
- * sportsmanagementTableLeague
+ * SportsManagement legacy compatibility bridge.
  *
- * @package
- * @author
- * @copyright diddi
- * @version   2014
- * @access    public
+ * The active Joomla 5/6 implementation lives in admin/src/Table/LeagueTable.php.
  */
-class sportsmanagementTableLeague extends JSMTable
-{
-	/**
-	 * Constructor
-	 *
-	 * @param   object Database connector object
-	 *
-	 * @since 1.0
-	 */
-	function __construct(&$db)
-	{
-		$db = sportsmanagementHelper::getDBConnection();
-		parent::__construct('#__sportsmanagement_league', 'id', $db);
-	}
 
-	/**
-	 * Overloaded check method to ensure data integrity
-	 *
-	 * @access public
-	 * @return boolean True on success
-	 * @since  1.0
-	 */
-	function check()
-	{
-		// Setting alias
-		$this->alias = OutputFilter::stringURLSafe($this->name);
+defined('_JEXEC') or die('Restricted access');
 
-		// Should check name unicity
-		return true;
-	}
+use Diddipoeler\Component\SportsManagement\Administrator\Table\LeagueTable;
 
+if (!class_exists(LeagueTable::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Table/LeagueTable.php';
+}
+
+if (!class_exists('sportsmanagementTableLeague', false)) {
+    class_alias(LeagueTable::class, 'sportsmanagementTableLeague');
 }
