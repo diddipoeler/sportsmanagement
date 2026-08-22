26128768b6eabd14077401e89278700f47427644
diddipoeler <diddipoeler@gmx.de>
2026-08-22 14:41:51 +0200

    Reduce new project entry to compatibility stub

--- modules/mod_sportsmanagement_new_project/mod_sportsmanagement_new_project.php
+++ modules/mod_sportsmanagement_new_project/mod_sportsmanagement_new_project.php
@@ -1,22 +1,7 @@
 <?php
 /**
- * Legacy entry bridge for the Joomla 5/6 SportsManagement New Project module.
+ * Joomla 5/6 compatibility entry point for the SportsManagement New Project module.
  *
- * The active implementation is loaded through services/provider.php.
+ * The active implementation is booted by services/provider.php.
  */
 \defined('_JEXEC') or die;
-
-use Diddipoeler\Module\SportsManagementNewProject\Site\Helper\NewProjectHelper;
-use Joomla\CMS\Factory;
-use Joomla\CMS\Helper\ModuleHelper;
-
-if (!class_exists(NewProjectHelper::class)) {
-    require_once __DIR__ . '/src/Helper/NewProjectHelper.php';
-}
-
-$app = Factory::getApplication();
-$helper = new NewProjectHelper();
-$list = $helper->getData($params, $app);
-$canCreateArticles = $helper->canCreateArticles($params, $app);
-
-require ModuleHelper::getLayoutPath('mod_sportsmanagement_new_project', 'native');
