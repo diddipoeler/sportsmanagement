376dbcc7d178763b69e4114436a2be8728958b55
diddipoeler <diddipoeler@gmx.de>
2026-08-22 13:09:11 +0200

    Route navigation menu through Joomla 5/6 native helper

--- modules/mod_sportsmanagement_navigation_menu/mod_sportsmanagement_navigation_menu.php
+++ modules/mod_sportsmanagement_navigation_menu/mod_sportsmanagement_navigation_menu.php
@@ -1,64 +1,31 @@
 <?php
-/**
- *
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage mod_sportsmanagement_navigation_menu
- * @file       mod_sportsmanagement_navigation_menu.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
+/** Joomla 5/6 compatibility entry point for the SportsManagement navigation menu. */
+\defined('_JEXEC') or die;
 
-defined('_JEXEC') or die('Restricted access');
-
-use Joomla\CMS\HTML\HTMLHelper;
-use Joomla\CMS\Helper\ModuleHelper;
-use Joomla\CMS\Uri\Uri;
+use Diddipoeler\Module\SportsManagementNavigationMenu\Site\Helper\NavigationMenuHelper;
 use Joomla\CMS\Factory;
-use Joomla\CMS\Component\ComponentHelper;
+use Joomla\CMS\Helper\ModuleHelper;
 
-if (!defined('JSM_PATH'))
-{
-	DEFINE('JSM_PATH', 'components/com_sportsmanagement');
+if (!class_exists(NavigationMenuHelper::class)) {
+    require_once __DIR__ . '/src/Helper/NavigationMenuHelper.php';
 }
 
-// Get helper
-require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'helper.php';
+$app = Factory::getApplication();
+$app->getLanguage()->load('com_sportsmanagement', JPATH_ADMINISTRATOR, null, true);
+$payload = (new NavigationMenuHelper())->getData($params, $app);
+extract($payload, EXTR_SKIP);
 
-// Welche joomla version ?
-if (version_compare(JVERSION, '4.0.0', 'ge'))
-{
-	//HTMLHelper::_('behavior.framework', true);
-}
-else if (version_compare(JVERSION, '3.0.0', 'ge'))
-{
-	HTMLHelper::_('behavior.framework', true);
-}
+$assets = $app->getDocument()->getWebAssetManager();
+$assets->registerAndUseStyle(
+    'mod_sportsmanagement_navigation_menu',
+    'modules/mod_sportsmanagement_navigation_menu/css/mod_sportsmanagement_navigation_menu.css',
+    ['version' => 'auto']
+);
+$assets->registerAndUseScript(
+    'mod_sportsmanagement_navigation_menu',
+    'modules/mod_sportsmanagement_navigation_menu/js/mod_sportsmanagement_navigation_menu.js',
+    ['version' => 'auto'],
+    ['defer' => true]
+);
 
-$document = Factory::getDocument();
-
-// Add css file
-$document->addStyleSheet(Uri::base() . 'modules' . DIRECTORY_SEPARATOR . $module->module . DIRECTORY_SEPARATOR . 'css' . DIRECTORY_SEPARATOR . $module->module . '.css');
-$document->addScript(Uri::base() . 'modules' . DIRECTORY_SEPARATOR . $module->module . DIRECTORY_SEPARATOR . 'js' . DIRECTORY_SEPARATOR . $module->module . '.js');
-
-$helper = new modsportsmanagementNavigationMenuHelper($params);
-
-$seasonselect   = $helper->getSeasonSelect();
-$leagueselect   = $helper->getLeagueSelect();
-$projectselect  = $helper->getProjectSelect();
-$divisionselect = $helper->getDivisionSelect();
-$teamselect     = $helper->getTeamSelect();
-
-$defaultview   = $params->get('project_start');
-$defaultitemid = $params->get('custom_item_id');
-
-?>
-<div class="<?php echo $params->get('moduleclass_sfx'); ?>"
-     id="<?php echo $module->module; ?>-<?php echo $module->id; ?>">
-	<?PHP
-	require ModuleHelper::getLayoutPath($module->module);
-	?>
-</div>
+require ModuleHelper::getLayoutPath($module->module, (string) $params->get('layout', 'default'));
