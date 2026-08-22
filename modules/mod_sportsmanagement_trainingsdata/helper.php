abb6b1c1ab2c6edd8194bacd358b5a10cbd4b801
diddipoeler <diddipoeler@gmx.de>
2026-08-22 13:36:56 +0200

    Bridge legacy trainings data helper to native helper

--- modules/mod_sportsmanagement_trainingsdata/helper.php
+++ modules/mod_sportsmanagement_trainingsdata/helper.php
@@ -1,67 +1,24 @@
 <?php
 /**
- *
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage mod_sportsmanagement_trainingsdata
- * @file       helper.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * SportsManagement legacy helper bridge for third-party template overrides.
  */
+\defined('_JEXEC') or die;
 
-defined('_JEXEC') or die('Restricted access');
+use Diddipoeler\Module\SportsManagementTrainingsData\Site\Helper\TrainingsDataHelper;
+use Joomla\Registry\Registry;
 
-use Joomla\CMS\Factory;
+if (!class_exists(TrainingsDataHelper::class)) {
+    require_once __DIR__ . '/src/Helper/TrainingsDataHelper.php';
+}
 
-/**
- * modJSMTrainingsData
- *
- * @package
- * @author    abcde
- * @copyright 2015
- * @version   $Id$
- * @access    public
- */
-class modJSMTrainingsData
-{
+if (!class_exists('modJSMTrainingsData', false)) {
+    final class modJSMTrainingsData
+    {
+        public static function getData($params): array
+        {
+            $registry = $params instanceof Registry ? $params : new Registry((array) $params);
 
-	/**
-	 * modJSMTrainingsData::getData()
-	 *
-	 * @param   mixed  $params
-	 *
-	 * @return
-	 */
-	public static function getData($params)
-	{
-		$app = Factory::getApplication();
-
-		// JInput object
-		$jinput = $app->input;
-
-		// Get a db connection.
-		$db = sportsmanagementHelper::getDBConnection();
-
-		// Create a new query object.
-		$query = $db->getQuery(true);
-
-		$result = array();
-
-		$query->select('*');
-		$query->from('#__sportsmanagement_team_trainingdata');
-		$query->where('team_id = ' . (int) $params->get('teams'));
-		$query->order('dayofweek ASC');
-
-		$db->setQuery($query);
-		$result = $db->loadObjectList();
-
-		$db->disconnect(); // See: http://api.joomla.org/cms-3/classes/JDatabaseDriver.html#method_disconnect
-
-		return $result;
-
-	}
-
+            return (new TrainingsDataHelper())->getData($registry);
+        }
+    }
 }
