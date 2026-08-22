883f0137d3769cf6a3837728ba88a890f25ae36a
diddipoeler <diddipoeler@gmx.de>
2026-08-22 15:45:31 +0200

    Use native calendar module entry point

--- modules/mod_sportsmanagement_calendar/mod_sportsmanagement_calendar.php
+++ modules/mod_sportsmanagement_calendar/mod_sportsmanagement_calendar.php
@@ -1,23 +1,8 @@
 <?php
 /**
- * Legacy entry bridge for the Joomla 5/6 SportsManagement calendar module.
+ * Legacy entry point kept for package compatibility.
  *
- * The active implementation is loaded through services/provider.php.
+ * Joomla 5/6 boots this module through services/provider.php and the
+ * namespaced module dispatcher.
  */
 \defined('_JEXEC') or die;
-
-use Diddipoeler\Module\SportsManagementCalendar\Site\Helper\CalendarHelper;
-use Joomla\CMS\Factory;
-use Joomla\CMS\Helper\ModuleHelper;
-
-if (!class_exists(CalendarHelper::class)) {
-    require_once __DIR__ . '/src/Helper/CalendarHelper.php';
-}
-
-$data = (new CalendarHelper())->getData($params, $module, Factory::getApplication());
-extract($data, EXTR_SKIP);
-
-require ModuleHelper::getLayoutPath(
-    'mod_sportsmanagement_calendar',
-    (string) $params->get('which_layout', $params->get('layout', 'default'))
-);
