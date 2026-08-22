85548fbb36901e57e3d6ac178fb6171045367d98
diddipoeler <diddipoeler@gmx.de>
2026-08-22 09:07:28 +0200

    Bridge legacy project table to native table

--- admin/tables/project.php
+++ admin/tables/project.php
@@ -1,57 +1,18 @@
 <?php
 /**
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage tables
- * @file       project.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\Filter\OutputFilter;
-
-/**
- * sportsmanagementTableProject
+ * SportsManagement legacy compatibility bridge.
  *
- * @package
- * @author
- * @copyright diddi
- * @version   2014
- * @access    public
+ * The active Joomla 5/6 implementation lives in admin/src/Table/ProjectTable.php.
  */
-class sportsmanagementTableProject extends JSMTable
-{
 
+defined('_JEXEC') or die('Restricted access');
 
-	/**
-	 * Constructor
-	 *
-	 * @param   object Database connector object
-	 *
-	 * @since 1.0
-	 */
-	function __construct(&$db)
-	{
-		$db = sportsmanagementHelper::getDBConnection();
-		parent::__construct('#__sportsmanagement_project', 'id', $db);
-	}
+use Diddipoeler\Component\SportsManagement\Administrator\Table\ProjectTable;
 
+if (!class_exists(ProjectTable::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Table/ProjectTable.php';
+}
 
-	/**
-	 * Overloaded check method to ensure data integrity
-	 *
-	 * @access public
-	 * @return boolean True on success
-	 * @since  1.0
-	 */
-	function check()
-	{
-		// Setting alias
-		$this->alias = OutputFilter::stringURLSafe($this->name);
-		return true;
-	}
-
-
+if (!class_exists('sportsmanagementTableProject', false)) {
+    class_alias(ProjectTable::class, 'sportsmanagementTableProject');
 }
