de542f2cb943f19d347c4fc6f118b0702d1a7d98
diddipoeler <diddipoeler@gmx.de>
2026-08-22 14:56:10 +0200

    Add native UEFA ranking dispatcher

---
+++ modules/mod_sportsmanagement_uefawertung/src/Dispatcher/Dispatcher.php
@@ -0,0 +1,38 @@
+<?php
+namespace Diddipoeler\Module\SportsManagementUefaWertung\Site\Dispatcher;
+
+\defined('_JEXEC') or die;
+
+use Joomla\CMS\Dispatcher\AbstractModuleDispatcher;
+use Joomla\CMS\Factory;
+use Joomla\CMS\Helper\HelperFactoryAwareInterface;
+use Joomla\CMS\Helper\HelperFactoryAwareTrait;
+use Joomla\Database\DatabaseInterface;
+
+final class Dispatcher extends AbstractModuleDispatcher implements HelperFactoryAwareInterface
+{
+    use HelperFactoryAwareTrait;
+
+    protected function getLayoutData(): array
+    {
+        $data = parent::getLayoutData();
+        $app = $this->getApplication();
+        $app->getLanguage()->load('com_sportsmanagement', JPATH_SITE, null, true);
+
+        /** @var DatabaseInterface $database */
+        $database = Factory::getContainer()->get(DatabaseInterface::class);
+        $result = $this->getHelperFactory()
+            ->getHelper('UefaWertungHelper')
+            ->getData($data['params'], $app, $database);
+
+        $data['seasons'] = $result['seasons'];
+        $data['rankings'] = $result['rankings'];
+
+        $app->getDocument()->getWebAssetManager()->registerAndUseStyle(
+            'mod_sportsmanagement_uefawertung',
+            'modules/mod_sportsmanagement_uefawertung/css/mod_sportsmanagement_uefawertung.css'
+        );
+
+        return $data;
+    }
+}
