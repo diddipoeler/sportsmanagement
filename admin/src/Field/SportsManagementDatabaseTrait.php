d350a72e8e407d4465d204d3b64acdfe726e7f91
diddipoeler <diddipoeler@gmx.de>
2026-08-22 08:35:15 +0200

    Use native SportsManagement database resolver in fields

--- admin/src/Field/SportsManagementDatabaseTrait.php
+++ admin/src/Field/SportsManagementDatabaseTrait.php
@@ -3,27 +3,14 @@ namespace Diddipoeler\Component\SportsManagement\Administrator\Field;
 
 \defined('_JEXEC') or die;
 
+use Diddipoeler\Component\SportsManagement\Administrator\Helper\SportsManagementDatabaseResolver;
 use Joomla\Database\DatabaseInterface;
 
 trait SportsManagementDatabaseTrait
 {
     protected function getSportsManagementDatabase(mixed $whichDatabase = null): DatabaseInterface
     {
-        if (!class_exists('sportsmanagementHelper', false)) {
-            $helperFile = JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/helpers/sportsmanagement.php';
-
-            if (is_file($helperFile)) {
-                require_once $helperFile;
-            }
-        }
-
-        if (!class_exists('sportsmanagementHelper', false)) {
-            throw new \RuntimeException('SportsManagement database helper is unavailable.');
-        }
-
-        $database = $whichDatabase === null || $whichDatabase === '' || $whichDatabase === false || (int) $whichDatabase === 0
-            ? \sportsmanagementHelper::getDBConnection()
-            : \sportsmanagementHelper::getDBConnection(true, $whichDatabase);
+        $database = (new SportsManagementDatabaseResolver())->resolve($whichDatabase);
 
         if (!$database instanceof DatabaseInterface) {
             throw new \RuntimeException('SportsManagement database connection is unavailable.');
