73ac801a62db6ec53f2e24f2726f5c4ede782b54
diddipoeler <diddipoeler@gmx.de>
2026-08-22 17:47:45 +0200

    Make playground ticker entry a Joomla 5/6 stub

--- modules/mod_sportsmanagement_playground_ticker/mod_sportsmanagement_playground_ticker.php
+++ modules/mod_sportsmanagement_playground_ticker/mod_sportsmanagement_playground_ticker.php
@@ -1,31 +1,7 @@
 <?php
 /**
- * Legacy entry bridge for the Joomla 5/6 SportsManagement playground ticker module.
+ * SportsManagement Joomla 5/6 module entry compatibility file.
+ *
+ * The active module is booted by services/provider.php and the namespaced dispatcher.
  */
-defined('_JEXEC') or die;
-
-use Diddipoeler\Module\SportsManagementPlaygroundTicker\Site\Helper\PlaygroundTickerHelper;
-use Joomla\CMS\Factory;
-use Joomla\CMS\Helper\ModuleHelper;
-
-if (!class_exists(PlaygroundTickerHelper::class)) {
-    require_once __DIR__ . '/src/Helper/PlaygroundTickerHelper.php';
-}
-
-$app = Factory::getApplication();
-$helper = new PlaygroundTickerHelper();
-$playgrounds = $helper->getData($params, $app);
-$module->picture_server = $helper->getPictureServer();
-
-$app->getDocument()
-    ->getWebAssetManager()
-    ->registerAndUseStyle(
-        'mod_sportsmanagement_playground_ticker',
-        'modules/mod_sportsmanagement_playground_ticker/css/mod_sportsmanagement_playground_ticker.css',
-        ['version' => 'auto']
-    );
-
-require ModuleHelper::getLayoutPath(
-    $module->module,
-    (string) $params->get('layout', 'default')
-);
+\defined('_JEXEC') or die;
