8a88e64d2e28455372604442e374732179c82101
diddipoeler <diddipoeler@gmx.de>
2026-08-22 13:45:55 +0200

    Switch current season to native module bootstrap

--- modules/mod_sportsmanagement_act_season/mod_sportsmanagement_act_season.php
+++ modules/mod_sportsmanagement_act_season/mod_sportsmanagement_act_season.php
@@ -1,34 +1,7 @@
 <?php
-/** Joomla 5/6 compatibility entry point for the current-season module. */
+/**
+ * SportsManagement Joomla 5/6 module entry compatibility file.
+ *
+ * The active module is booted by services/provider.php and the namespaced dispatcher.
+ */
 \defined('_JEXEC') or die;
-
-use Diddipoeler\Module\SportsManagementActSeason\Site\Helper\ActSeasonHelper;
-use Joomla\CMS\Component\ComponentHelper;
-use Joomla\CMS\Factory;
-use Joomla\CMS\Helper\ModuleHelper;
-use Joomla\Database\DatabaseInterface;
-
-if (!class_exists(ActSeasonHelper::class)) {
-    require_once __DIR__ . '/src/Helper/ActSeasonHelper.php';
-}
-
-$app = Factory::getApplication();
-$app->getLanguage()->load('com_sportsmanagement', JPATH_ADMINISTRATOR, null, true);
-$componentParams = ComponentHelper::getParams('com_sportsmanagement');
-/** @var DatabaseInterface $database */
-$database = Factory::getContainer()->get(DatabaseInterface::class);
-$result = (new ActSeasonHelper())->getData(
-    $componentParams->get('current_season', []),
-    $componentParams,
-    $app,
-    $database
-);
-
-$list = $result['list'];
-$federations = $result['federations'];
-$countriesByFederation = $result['countriesByFederation'];
-
-require ModuleHelper::getLayoutPath(
-    $module->module,
-    (string) $params->get('layout', 'default')
-);
