2f429ff8ff02e19641128064c54c77b382ed2db9
diddipoeler <diddipoeler@gmx.de>
2026-08-22 20:41:32 +0200

    Migrate basic match controller tasks to Joomla 5/6

--- admin/src/Controller/MatchController.php
+++ admin/src/Controller/MatchController.php
@@ -22,8 +22,93 @@ if (!class_exists('sportsmanagementControllermatch', false)) {
  */
 final class MatchController extends \sportsmanagementControllermatch
 {
+    public function cancelmodal($key = null)
+    {
+        $this->setRedirect('index.php?option=com_sportsmanagement&view=close&tmpl=component');
+    }
+
+    public function cancelmassadd()
+    {
+        $this->setRedirect('index.php?option=com_sportsmanagement&view=matches&massadd=0');
+    }
+
+    public function massadd()
+    {
+        $this->setRedirect('index.php?option=com_sportsmanagement&view=matches&layout=massadd&massadd=1');
+    }
+
+    public function addmatch()
+    {
+        $this->checkToken();
+
+        $app = Factory::getApplication();
+        $input = $app->getInput();
+        $option = $input->getCmd('option', 'com_sportsmanagement');
+        $data = $input->post->getArray();
+        $data['project_id'] = (int) $app->getUserState($option . '.pid', 0);
+        $data['round_id'] = (int) $app->getUserState($option . '.rid', 0);
+        $data['count_result'] = 1;
+        $data['published'] = 1;
+        $data['summary'] = '-';
+        $data['preview'] = '-';
+
+        $model = $this->getModel('match');
+        $success = $model !== false && $model->save($data);
+        $message = $success
+            ? Text::_('COM_SPORTSMANAGEMENT_ADMIN_MATCH_CTRL_ADD_MATCH')
+            : Text::_('COM_SPORTSMANAGEMENT_ADMIN_MATCH_CTRL_ERROR_ADD_MATCH')
+                . ($model && method_exists($model, 'getError') ? (string) $model->getError() : '');
+
+        $this->setRedirect(
+            'index.php?option=com_sportsmanagement&view=matches',
+            $message,
+            $success ? 'message' : 'error'
+        );
+
+        return $success;
+    }
+
+    public function save($key = null, $urlVar = null)
+    {
+        $this->checkToken();
+
+        $app = Factory::getApplication();
+        $input = $app->getInput();
+        $id = $input->getInt('id');
+        $data = $input->post->get('jform', [], 'array');
+        $model = $this->getModel('match');
+        $success = $model !== false && $model->save($data);
+
+        if (!$success) {
+            $message = $model && method_exists($model, 'getError') && $model->getError()
+                ? (string) $model->getError()
+                : Text::_('JLIB_APPLICATION_ERROR_SAVE_FAILED');
+            $this->setRedirect(
+                'index.php?option=com_sportsmanagement&view=match&layout=edit&tmpl=component&id=' . $id,
+                $message,
+                'error'
+            );
+
+            return false;
+        }
+
+        if ($this->getTask() === 'apply') {
+            $this->setRedirect(
+                'index.php?option=com_sportsmanagement&view=match&layout=edit&tmpl=component&id=' . $id,
+                Text::_('JLIB_APPLICATION_SAVE_SUCCESS'),
+                'message'
+            );
+        } else {
+            $this->setRedirect('index.php?option=com_sportsmanagement&view=close&tmpl=component');
+        }
+
+        return true;
+    }
+
     public function insertgooglecalendar()
     {
+        $this->checkToken();
+
         $app = Factory::getApplication();
         $input = $app->getInput();
         $matchIds = (array) $input->post->get('cid', [], 'array');
