70911db28c8daec612f1cc9f1bf20af830015508
diddipoeler <diddipoeler@gmx.de>
2026-08-22 16:48:25 +0200

    Inject database into first league overview helper

--- modules/mod_sportsmanagement_firstleagueoverview/src/Dispatcher/Dispatcher.php
+++ modules/mod_sportsmanagement_firstleagueoverview/src/Dispatcher/Dispatcher.php
@@ -4,8 +4,10 @@ namespace Diddipoeler\Module\SportsManagementFirstLeagueOverview\Site\Dispatcher
 \defined('_JEXEC') or die;
 
 use Joomla\CMS\Dispatcher\AbstractModuleDispatcher;
+use Joomla\CMS\Factory;
 use Joomla\CMS\Helper\HelperFactoryAwareInterface;
 use Joomla\CMS\Helper\HelperFactoryAwareTrait;
+use Joomla\Database\DatabaseInterface;
 
 final class Dispatcher extends AbstractModuleDispatcher implements HelperFactoryAwareInterface
 {
@@ -17,9 +19,11 @@ final class Dispatcher extends AbstractModuleDispatcher implements HelperFactory
         $app = $this->getApplication();
         $app->getLanguage()->load('com_sportsmanagement', JPATH_SITE, null, true);
 
+        /** @var DatabaseInterface $database */
+        $database = Factory::getContainer()->get(DatabaseInterface::class);
         $overview = $this->getHelperFactory()
             ->getHelper('FirstLeagueOverviewHelper')
-            ->getData($data['params']);
+            ->getData($data['params'], $database);
 
         $data['firstleagueoverview'] = $overview['projects'];
         $data['federations'] = $overview['federations'];
