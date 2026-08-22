ef5e4dbdbc3b00969521a9ff8bc45bea9d4fec0c
diddipoeler <diddipoeler@gmx.de>
2026-08-22 08:52:32 +0200

    Bridge legacy season table to native implementation

--- admin/tables/season.php
+++ admin/tables/season.php
@@ -1,62 +1,18 @@
 <?php
 /**
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage tables
- * @file       season.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\Filter\OutputFilter;
-
-/**
- * sportsmanagementTableSeason
+ * SportsManagement legacy compatibility bridge.
  *
- * @package
- * @author
- * @copyright diddi
- * @version   2014
- * @access    public
+ * The active Joomla 5/6 implementation lives in admin/src/Table/SeasonTable.php.
  */
-class sportsmanagementTableSeason extends JSMTable
-{
-	/**
-	 * Constructor
-	 *
-	 * @param   object Database connector object
-	 *
-	 * @since 1.0
-	 */
-	function __construct(&$db)
-	{
-		$db = sportsmanagementHelper::getDBConnection();
-		parent::__construct('#__sportsmanagement_season', 'id', $db);
-	}
 
-	/**
-	 * Overloaded check method to ensure data integrity
-	 *
-	 * @access public
-	 * @return boolean True on success
-	 * @since  1.0
-	 */
-	function check()
-	{
-		// Setting alias
-		if (empty($this->alias))
-		{
-			$this->alias = OutputFilter::stringURLSafe($this->name);
-		}
-		else
-		{
-			$this->alias = OutputFilter::stringURLSafe($this->alias); // Make sure the user didn't modify it to something illegal...
-		}
+defined('_JEXEC') or die('Restricted access');
 
-		// Should check name unicity
-		return true;
-	}
+use Diddipoeler\Component\SportsManagement\Administrator\Table\SeasonTable;
 
+if (!class_exists(SeasonTable::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Table/SeasonTable.php';
+}
+
+if (!class_exists('sportsmanagementTableSeason', false)) {
+    class_alias(SeasonTable::class, 'sportsmanagementTableSeason');
 }
