e86630242065aa8acb92a64759144172ec1a922a
diddipoeler <diddipoeler@gmx.de>
2026-08-22 15:34:14 +0200

    Use native Google Calendar module dispatcher

--- modules/mod_sportsmanagement_google_calendar/mod_sportsmanagement_google_calendar.php
+++ modules/mod_sportsmanagement_google_calendar/mod_sportsmanagement_google_calendar.php
@@ -1,21 +1,7 @@
 <?php
 /**
- * Joomla 5/6 compatibility bridge for the SportsManagement Google Calendar module.
+ * SportsManagement Joomla 5/6 module entry compatibility file.
  *
- * The active implementation is loaded through services/provider.php.
+ * The active module is booted by services/provider.php and the namespaced dispatcher.
  */
-defined('_JEXEC') or die;
-
-use Diddipoeler\Module\SportsManagementGoogleCalendar\Site\Helper\GoogleCalendarHelper;
-
-if (!class_exists(GoogleCalendarHelper::class)) {
-    require_once __DIR__ . '/src/Helper/GoogleCalendarHelper.php';
-}
-
-if (!class_exists('ModJSMGoogleCalendarHelper', false)) {
-    class_alias(GoogleCalendarHelper::class, 'ModJSMGoogleCalendarHelper');
-}
-
-if (!class_exists('ModGoogleCalendarHelper', false)) {
-    class_alias(GoogleCalendarHelper::class, 'ModGoogleCalendarHelper');
-}
+\defined('_JEXEC') or die;
