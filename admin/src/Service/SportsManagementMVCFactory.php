59b3554624f251b9721e58972d3da4ce8711af71
diddipoeler <diddipoeler@gmx.de>
2026-08-22 12:11:11 +0200

    Route legacy admin MVC through Joomla 5/6 MVCFactory

--- admin/src/Service/SportsManagementMVCFactory.php
+++ admin/src/Service/SportsManagementMVCFactory.php
@@ -5,13 +5,35 @@ namespace Diddipoeler\Component\SportsManagement\Administrator\Service;
 
 use Diddipoeler\Component\SportsManagement\Administrator\Legacy\LegacyBootstrap as AdministratorLegacyBootstrap;
 use Diddipoeler\Component\SportsManagement\Site\Legacy\LegacyBootstrap as SiteLegacyBootstrap;
+use Joomla\CMS\Application\CMSApplicationInterface;
 use Joomla\CMS\Factory;
 use Joomla\CMS\MVC\Factory\MVCFactory;
 use Joomla\Database\DatabaseAwareInterface;
 use Joomla\Database\DatabaseInterface;
+use Joomla\Input\Input;
 
 final class SportsManagementMVCFactory extends MVCFactory
 {
+    public function createController(
+        $name,
+        $prefix,
+        array $config,
+        CMSApplicationInterface $app,
+        Input $input
+    ) {
+        $controller = parent::createController($name, $prefix, $config, $app, $input);
+
+        if ($controller !== null) {
+            return $controller;
+        }
+
+        if (!$this->loadLegacyController((string) $name, (string) $prefix)) {
+            return null;
+        }
+
+        return parent::createController($name, $prefix, $config, $app, $input);
+    }
+
     public function createModel($name, $prefix = '', array $config = [])
     {
         $model = parent::createModel($name, $prefix, $config);
@@ -35,69 +57,179 @@ final class SportsManagementMVCFactory extends MVCFactory
 
         $prefix = $this->normalisePrefix((string) $prefix);
         $type = $type !== '' ? (string) $type : 'html';
-        $basePath = $this->legacyBasePath($prefix);
+        $legacyBasePath = $this->loadLegacyView((string) $name, $prefix, $type);
 
-        if ($basePath === null || !$this->loadLegacyView((string) $name, $prefix, $type)) {
+        if ($legacyBasePath === null) {
             return null;
         }
 
-        $config['template_path'] = $basePath . '/views/' . strtolower((string) $name) . '/tmpl';
+        $config['template_path'] = $legacyBasePath . '/views/' . strtolower((string) $name) . '/tmpl';
 
         return parent::createView($name, $prefix, $type, $config);
+    }
+
+    private function loadLegacyController(string $name, string $prefix): bool
+    {
+        $prefix = $this->normalisePrefix($prefix);
+        $basePath = $this->legacyBasePath($prefix);
+
+        if ($basePath === null) {
+            return false;
+        }
+
+        $this->bootLegacy($prefix, $name);
+        $targetClass = 'Diddipoeler\\Component\\SportsManagement\\' . $prefix . '\\Controller\\' . ucfirst($name) . 'Controller';
+
+        if (class_exists($targetClass, false)) {
+            return true;
+        }
+
+        foreach ($this->legacySearchPaths($prefix) as $searchPath) {
+            $file = $searchPath . '/controllers/' . strtolower($name) . '.php';
+
+            if (!is_file($file)) {
+                continue;
+            }
+
+            require_once $file;
+
+            if (class_exists($targetClass, false)) {
+                return true;
+            }
+
+            foreach ([
+                'sportsmanagementController' . ucfirst($name),
+                'SportsManagementController' . ucfirst($name),
+            ] as $legacyClass) {
+                if (class_exists($legacyClass, false)) {
+                    return class_alias($legacyClass, $targetClass);
+                }
+            }
+        }
+
+        return false;
     }
 
     private function loadLegacyModel(string $name, string $prefix): bool
     {
         $prefix = $this->normalisePrefix($prefix);
-        $basePath = $this->legacyBasePath($prefix);
 
-        if ($basePath === null) {
+        if ($this->legacyBasePath($prefix) === null) {
             return false;
         }
 
         $this->bootLegacy($prefix, $name);
-        $file = $basePath . '/models/' . strtolower($name) . '.php';
-
-        if (!is_file($file)) {
-            return false;
-        }
-
-        require_once $file;
-        $legacyClass = 'sportsmanagementModel' . ucfirst($name);
         $targetClass = 'Diddipoeler\\Component\\SportsManagement\\' . $prefix . '\\Model\\' . ucfirst($name) . 'Model';
 
-        if (!class_exists($legacyClass, false) || class_exists($targetClass, false)) {
-            return class_exists($targetClass, false);
+        if (class_exists($targetClass, false)) {
+            return true;
         }
 
-        return class_alias($legacyClass, $targetClass);
+        foreach ($this->legacySearchPaths($prefix) as $searchPath) {
+            $file = $searchPath . '/models/' . strtolower($name) . '.php';
+
+            if (!is_file($file)) {
+                continue;
+            }
+
+            require_once $file;
+            $legacyClass = 'sportsmanagementModel' . ucfirst($name);
+
+            if (class_exists($targetClass, false)) {
+                return true;
+            }
+
+            if (class_exists($legacyClass, false)) {
+                return class_alias($legacyClass, $targetClass);
+            }
+        }
+
+        return false;
     }
 
-    private function loadLegacyView(string $name, string $prefix, string $type): bool
+    private function loadLegacyView(string $name, string $prefix, string $type): ?string
     {
         $prefix = $this->normalisePrefix($prefix);
-        $basePath = $this->legacyBasePath($prefix);
 
-        if ($basePath === null) {
-            return false;
+        if ($this->legacyBasePath($prefix) === null) {
+            return null;
         }
 
         $this->bootLegacy($prefix, $name);
-        $file = $basePath . '/views/' . strtolower($name) . '/view.' . strtolower($type) . '.php';
-
-        if (!is_file($file)) {
-            return false;
-        }
-
-        require_once $file;
-        $legacyClass = 'sportsmanagementView' . ucfirst($name);
         $targetClass = 'Diddipoeler\\Component\\SportsManagement\\' . $prefix . '\\View\\' . ucfirst($name) . '\\' . ucfirst($type) . 'View';
 
-        if (!class_exists($legacyClass, false) || class_exists($targetClass, false)) {
-            return class_exists($targetClass, false);
+        foreach ($this->legacySearchPaths($prefix) as $searchPath) {
+            $file = $searchPath . '/views/' . strtolower($name) . '/view.' . strtolower($type) . '.php';
+
+            if (!is_file($file)) {
+                continue;
+            }
+
+            require_once $file;
+            $legacyClass = 'sportsmanagementView' . ucfirst($name);
+
+            if (!class_exists($targetClass, false) && class_exists($legacyClass, false)) {
+                class_alias($legacyClass, $targetClass);
+            }
+
+            if (class_exists($targetClass, false)) {
+                return $searchPath;
+            }
         }
 
-        return class_alias($legacyClass, $targetClass);
+        return null;
+    }
+
+    private function legacySearchPaths(string $prefix): array
+    {
+        $basePath = $this->legacyBasePath($prefix);
+
+        if ($basePath === null) {
+            return [];
+        }
+
+        $paths = [$basePath];
+
+        if ($prefix !== 'Administrator') {
+            return $paths;
+        }
+
+        if (!class_exists('sportsmanagementHelper')) {
+            $helperFile = JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/helpers/sportsmanagement.php';
+
+            if (is_file($helperFile)) {
+                require_once $helperFile;
+            }
+        }
+
+        if (!class_exists('sportsmanagementHelper')) {
+            return $paths;
+        }
+
+        try {
+            $extensions = \sportsmanagementHelper::getExtensions();
+        } catch (\Throwable) {
+            return $paths;
+        }
+
+        foreach ((array) $extensions as $extensionName) {
+            $extensionName = preg_replace('/[^A-Z0-9_-]/i', '', (string) $extensionName);
+
+            if ($extensionName === '') {
+                continue;
+            }
+
+            $extensionPath = JPATH_SITE
+                . '/components/com_sportsmanagement/extensions/'
+                . $extensionName
+                . '/admin';
+
+            if (is_dir($extensionPath)) {
+                $paths[] = $extensionPath;
+            }
+        }
+
+        return array_values(array_unique($paths));
     }
 
     private function bootLegacy(string $prefix, string $view): void
