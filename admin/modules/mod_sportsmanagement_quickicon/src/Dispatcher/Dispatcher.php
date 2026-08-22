7fd1b3d4bf16c99457676fde4d4fb8a7b7e84d28
diddipoeler <diddipoeler@gmx.de>
2026-08-22 14:49:38 +0200

    Add Joomla 5/6 ACL to quickicon dispatcher

--- admin/modules/mod_sportsmanagement_quickicon/src/Dispatcher/Dispatcher.php
+++ admin/modules/mod_sportsmanagement_quickicon/src/Dispatcher/Dispatcher.php
@@ -14,15 +14,46 @@ final class Dispatcher extends AbstractModuleDispatcher
     protected function getLayoutData(): array
     {
         $data = parent::getLayoutData();
+        $app = $this->getApplication();
+        $identity = $app->getIdentity();
+
         $data['componentEnabled'] = ComponentHelper::isEnabled('com_sportsmanagement', true);
-        $base = Uri::base() . 'components/com_sportsmanagement/assets/icons/';
+        $data['canManage'] = $identity->authorise('core.manage', 'com_sportsmanagement');
+
+        $base = rtrim((string) Uri::base(), '/') . '/components/com_sportsmanagement/assets/icons/';
         $data['links'] = [
-            ['url' => Route::_('index.php?option=com_sportsmanagement'), 'icon' => $base . 'transparent_schrift_48.png', 'title' => Text::_('MOD_SPORTSMANAGEMENT_QUICKICON_PANEL_LINK'), 'label' => Text::_('MOD_SPORTSMANAGEMENT_QUICKICON_PANEL_LABEL')],
-            ['url' => Route::_('index.php?option=com_sportsmanagement&view=extensions'), 'icon' => $base . 'extensions.png', 'title' => Text::_('MOD_SPORTSMANAGEMENT_QUICKICON_EXTENSIONS_LINK'), 'label' => Text::_('MOD_SPORTSMANAGEMENT_QUICKICON_EXTENSIONS_LABEL')],
-            ['url' => Route::_('index.php?option=com_sportsmanagement&view=projects'), 'icon' => $base . 'projekte.png', 'title' => Text::_('MOD_SPORTSMANAGEMENT_QUICKICON_PROJECTS_LINK'), 'label' => Text::_('MOD_SPORTSMANAGEMENT_QUICKICON_PROJECTS_LABEL')],
-            ['url' => Route::_('index.php?option=com_sportsmanagement&view=predictiongames'), 'icon' => $base . 'tippspiele.png', 'title' => Text::_('MOD_SPORTSMANAGEMENT_QUICKICON_PREDICTIONS_LINK'), 'label' => Text::_('MOD_SPORTSMANAGEMENT_QUICKICON_PREDICTIONS_LABEL')],
-            ['url' => Route::_('index.php?option=com_sportsmanagement&view=currentseasons'), 'icon' => $base . 'aktuellesaison.png', 'title' => Text::_('MOD_SPORTSMANAGEMENT_QUICKICON_CURRENT_SAISON_LINK'), 'label' => Text::_('MOD_SPORTSMANAGEMENT_QUICKICON_CURRENT_SAISON_LABEL')],
+            [
+                'url' => Route::_('index.php?option=com_sportsmanagement'),
+                'icon' => $base . 'transparent_schrift_48.png',
+                'title' => Text::_('MOD_SPORTSMANAGEMENT_QUICKICON_PANEL_LINK'),
+                'label' => Text::_('MOD_SPORTSMANAGEMENT_QUICKICON_PANEL_LABEL'),
+            ],
+            [
+                'url' => Route::_('index.php?option=com_sportsmanagement&view=extensions'),
+                'icon' => $base . 'extensions.png',
+                'title' => Text::_('MOD_SPORTSMANAGEMENT_QUICKICON_EXTENSIONS_LINK'),
+                'label' => Text::_('MOD_SPORTSMANAGEMENT_QUICKICON_EXTENSIONS_LABEL'),
+            ],
+            [
+                'url' => Route::_('index.php?option=com_sportsmanagement&view=projects'),
+                'icon' => $base . 'projekte.png',
+                'title' => Text::_('MOD_SPORTSMANAGEMENT_QUICKICON_PROJECTS_LINK'),
+                'label' => Text::_('MOD_SPORTSMANAGEMENT_QUICKICON_PROJECTS_LABEL'),
+            ],
+            [
+                'url' => Route::_('index.php?option=com_sportsmanagement&view=predictiongames'),
+                'icon' => $base . 'tippspiele.png',
+                'title' => Text::_('MOD_SPORTSMANAGEMENT_QUICKICON_PREDICTIONS_LINK'),
+                'label' => Text::_('MOD_SPORTSMANAGEMENT_QUICKICON_PREDICTIONS_LABEL'),
+            ],
+            [
+                'url' => Route::_('index.php?option=com_sportsmanagement&view=currentseasons'),
+                'icon' => $base . 'aktuellesaison.png',
+                'title' => Text::_('MOD_SPORTSMANAGEMENT_QUICKICON_CURRENT_SAISON_LINK'),
+                'label' => Text::_('MOD_SPORTSMANAGEMENT_QUICKICON_CURRENT_SAISON_LABEL'),
+            ],
         ];
+
         return $data;
     }
 }
