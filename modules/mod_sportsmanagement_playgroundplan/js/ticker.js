22ac1fff31d4d7770c0bc3dfec932159fce97b6d
diddipoeler <diddipoeler@gmx.de>
2026-08-22 12:37:49 +0200

    Replace playground plan MooTools ticker

--- modules/mod_sportsmanagement_playgroundplan/js/ticker.js
+++ modules/mod_sportsmanagement_playgroundplan/js/ticker.js
@@ -1,18 +1,49 @@
-<script type="text/javascript">
-<!--
-window.addEvent('domready', function() {
-var opt = {
-slides: 'qslidejl',
-  duration: 500,
-  delay: 4000,
-  auto:true,
-  direction: 'v',
- transition:Fx.Transitions.Quart.easeIn,
-  onMouseEnter: function(){this.stop();},
-  onMouseLeave: function(){this.play();}
-}
-var scroller = new QScroller('modjlplaygroundplan0',opt);
-scroller.load();
-});
-//-->
-</script>
+(() => {
+    const initialise = (container) => {
+        const items = Array.from(container.querySelectorAll('.jsm-playgroundplan-item'));
+
+        if (items.length <= 1) {
+            return;
+        }
+
+        let index = 0;
+        let timer = null;
+
+        const show = (nextIndex) => {
+            items[index].hidden = true;
+            index = nextIndex % items.length;
+            items[index].hidden = false;
+        };
+
+        const play = () => {
+            if (timer !== null) {
+                return;
+            }
+
+            timer = window.setInterval(() => show(index + 1), 4000);
+        };
+
+        const stop = () => {
+            if (timer === null) {
+                return;
+            }
+
+            window.clearInterval(timer);
+            timer = null;
+        };
+
+        container.addEventListener('mouseenter', stop);
+        container.addEventListener('mouseleave', play);
+        play();
+    };
+
+    const boot = () => {
+        document.querySelectorAll('[data-jsm-playgroundplan-ticker]').forEach(initialise);
+    };
+
+    if (document.readyState === 'loading') {
+        document.addEventListener('DOMContentLoaded', boot, {once: true});
+    } else {
+        boot();
+    }
+})();
