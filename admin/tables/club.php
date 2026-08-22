1ba3361ee2168cc28e3fb7454829ad22b08f59da
diddipoeler <diddipoeler@gmx.de>
2026-08-22 08:52:13 +0200

    Bridge legacy club table to native implementation

--- admin/tables/club.php
+++ admin/tables/club.php
@@ -1,55 +1,18 @@
 <?php
 /**
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage tables
- * @file       club.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\Filter\OutputFilter;
-
-/**
- * sportsmanagementTableClub
+ * SportsManagement legacy compatibility bridge.
  *
- * @package
- * @author
- * @copyright diddi
- * @version   2014
- * @access    public
+ * The active Joomla 5/6 implementation lives in admin/src/Table/ClubTable.php.
  */
-class sportsmanagementTableClub extends JSMTable
-{
 
-	/**
-	 * Constructor
-	 *
-	 * @param   object Database connector object
-	 *
-	 * @since 1.0
-	 */
-	function __construct(&$db)
-	{
-		$db = sportsmanagementHelper::getDBConnection();
-		parent::__construct('#__sportsmanagement_club', 'id', $db);
-	}
+defined('_JEXEC') or die('Restricted access');
 
-	/**
-	 * Overloaded check method to ensure data integrity
-	 *
-	 * @access public
-	 * @return boolean True on success
-	 * @since  1.0
-	 */
-	function check()
-	{
-		// Setting alias
-		$this->alias = OutputFilter::stringURLSafe($this->name);
+use Diddipoeler\Component\SportsManagement\Administrator\Table\ClubTable;
 
-		return true;
-	}
+if (!class_exists(ClubTable::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Table/ClubTable.php';
+}
 
+if (!class_exists('sportsmanagementTableClub', false)) {
+    class_alias(ClubTable::class, 'sportsmanagementTableClub');
 }
