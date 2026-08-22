04d08f5e3ac4bb480d52d7a163d565c0a2d8de2a
diddipoeler <diddipoeler@gmx.de>
2026-08-22 16:22:29 +0200

    Add native team stats ranking dispatcher

---
+++ modules/mod_sportsmanagement_teamstats_ranking/src/Dispatcher/Dispatcher.php
@@ -0,0 +1,36 @@
+<?php
+namespace Diddipoeler\Module\SportsManagementTeamStatsRanking\Site\Dispatcher;
+
+\defined('_JEXEC') or die;
+
+use Joomla\CMS\Dispatcher\AbstractModuleDispatcher;
+use Joomla\CMS\Helper\HelperFactoryAwareInterface;
+use Joomla\CMS\Helper\HelperFactoryAwareTrait;
+
+final class Dispatcher extends AbstractModuleDispatcher implements HelperFactoryAwareInterface
+{
+    use HelperFactoryAwareTrait;
+
+    protected function getLayoutData(): array
+    {
+        $data = parent::getLayoutData();
+        $app = $this->getApplication();
+        $app->getLanguage()->load('com_sportsmanagement', JPATH_SITE);
+        $app->getLanguage()->load(
+            'mod_sportsmanagement_teamstats_ranking',
+            JPATH_SITE . '/modules/mod_sportsmanagement_teamstats_ranking'
+        );
+
+        $app->getDocument()->getWebAssetManager()->registerAndUseStyle(
+            'mod_sportsmanagement_teamstats_ranking',
+            'modules/mod_sportsmanagement_teamstats_ranking/css/mod_sportsmanagement_teamstats_ranking.css'
+        );
+
+        $helper = $this->getHelperFactory()->getHelper('TeamStatsRankingHelper');
+
+        return array_merge(
+            $data,
+            $helper->getData($data['params'])
+        );
+    }
+}
