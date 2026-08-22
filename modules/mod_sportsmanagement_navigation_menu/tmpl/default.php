cc4931c7af512b5f565e5667b4674a48a33e8173
diddipoeler <diddipoeler@gmx.de>
2026-08-22 13:09:22 +0200

    Make navigation menu layout module-instance safe

--- modules/mod_sportsmanagement_navigation_menu/tmpl/default.php
+++ modules/mod_sportsmanagement_navigation_menu/tmpl/default.php
@@ -1,114 +1,64 @@
 <?php
-/**
- *
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage mod_sportsmanagement_navigation_menu
- * @file       default.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
+/** Joomla 5/6 default layout for mod_sportsmanagement_navigation_menu. */
+\defined('_JEXEC') or die;
 
-defined('_JEXEC') or die('Restricted access');
-
-use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Uri\Uri;
-use Joomla\CMS\Router\Route;
 
+$moduleId = (int) $module->id;
 ?>
-<div id="jl-nav-module">
-    <div class="jl-nav-module">
+<div
+    id="jl-nav-module-<?= $moduleId ?>"
+    class="jl-nav-module"
+    data-jsm-navigation-menu
+>
+    <form method="post" action="<?= htmlspecialchars((string) Uri::root(), ENT_QUOTES, 'UTF-8') ?>">
+        <ul class="nav menu<?= htmlspecialchars((string) $params->get('moduleclass_sfx', ''), ENT_QUOTES, 'UTF-8') ?>">
+            <?php if ((string) $params->get('show_project_dropdown') === 'season') : ?>
+                <?php if ($seasonselect) : ?>
+                    <li class="season-select"><?= $seasonselect ?></li>
+                <?php endif; ?>
+                <?php if ($leagueselect) : ?>
+                    <li class="league-select"><?= $leagueselect ?></li>
+                <?php endif; ?>
+            <?php endif; ?>
 
-        <form method="post" action="<?php echo Uri::root(); ?>">
+            <?php if ($params->get('show_project_dropdown') && $projectselect) : ?>
+                <li class="project-select"><?= $projectselect ?></li>
+            <?php endif; ?>
 
-            <ul class="nav menu<?php echo $params->get('moduleclass_sfx'); ?>">
+            <?php if ($params->get('show_division_dropdown') && $divisionselect) : ?>
+                <li class="division-select"><?= $divisionselect ?></li>
+            <?php endif; ?>
 
-				<?php if ($params->get('show_project_dropdown') == 'season')
-					:
-					?>
-					<?php if ($seasonselect)
-					:
-					?>
-                    <li class="season-select"><?php echo $seasonselect; ?></li>
-				<?php endif; ?>
-					<?php
-					if ($leagueselect)
-						:
-						?>
-                        <li class="league-select"><?php echo $leagueselect; ?></li>
-					<?php endif; ?>
-				<?php endif; ?>
+            <?php if ($params->get('show_teams_dropdown') && $teamselect) : ?>
+                <li class="team-select">
+                    <?php if ($heading = trim((string) $params->get('heading_teams_dropdown', ''))) : ?>
+                        <span class="label"><?= htmlspecialchars($heading, ENT_QUOTES, 'UTF-8') ?></span>
+                    <?php endif; ?>
+                    <?= $teamselect ?>
+                </li>
+            <?php endif; ?>
 
-				<?php if ($params->get('show_project_dropdown'))
-					:
-					?>
-					<?php if ($projectselect)
-					:
-					?>
-                    <li class="project-select"><?php echo $projectselect; ?></li>
-				<?php endif; ?>
-				<?php endif; ?>
+            <?php if ($params->get('show_nav_links')) : ?>
+                <?php for ($i = 1; $i <= 16; $i++) : ?>
+                    <?php $view = (string) $params->get('navpoint' . $i, ''); ?>
+                    <?php $label = (string) $params->get('navpoint_label' . $i, ''); ?>
+                    <?php if ($view === 'separator') : ?>
+                        <li class="nav-item separator"><?= htmlspecialchars($label, ENT_QUOTES, 'UTF-8') ?></li>
+                    <?php elseif ($view !== '' && ($link = $helper->getLink($view))) : ?>
+                        <li class="nav-item">
+                            <a href="<?= htmlspecialchars((string) $link, ENT_QUOTES, 'UTF-8') ?>"><?= htmlspecialchars($label, ENT_QUOTES, 'UTF-8') ?></a>
+                        </li>
+                    <?php endif; ?>
+                <?php endfor; ?>
+            <?php endif; ?>
+        </ul>
 
-				<?php if ($params->get('show_division_dropdown'))
-					:
-					?>
-					<?php if ($divisionselect)
-					:
-					?>
-                    <li class="division-select"><?php echo $divisionselect; ?></li>
-				<?php endif; ?>
-				<?php endif; ?>
-
-				<?php if ($params->get('show_teams_dropdown') && $teamselect)
-					:
-					?>
-                    <li class="team-select">
-						<?php if ($params->get('heading_teams_dropdown'))
-							:
-							?>
-                            <span class="label"><?php echo $params->get('heading_teams_dropdown'); ?></span><?php echo $teamselect; ?>
-						<?php else
-
-							:
-							?>
-							<?php echo $teamselect; ?>
-						<?php endif; ?>
-                    </li>
-				<?php endif; ?>
-
-				<?php if ($params->get('show_nav_links'))
-					:
-					?>
-
-					<?php for ($i = 1; $i < 17; $i++)
-					:
-					?>
-					<?php
-					if ($params->get('navpoint' . $i) && $link = $helper->getLink($params->get('navpoint' . $i)))
-						:
-						?>
-                        <li class="nav-item"><?php echo HTMLHelper::link(Route::_($link), $params->get('navpoint_label' . $i)); ?></li>
-					<?php elseif ($params->get('navpoint' . $i) == "separator")
-						:
-						?>
-                        <li class="nav-item separator"><?php echo $params->get('navpoint_label' . $i); ?></li>
-					<?php endif; ?>
-				<?php endfor; ?>
-
-				<?php endif; ?>
-            </ul>
-
-            <input type="hidden" name="option" value="com_sportsmanagement"/>
-            <input type="hidden" name="view" value="<?php echo $defaultview; ?>"/>
-            <input type="hidden" name="teamview" value="<?php echo $params->get('link_teams_dropdown', 'roster'); ?>"/>
-            <input type="hidden" name="o" value="<?php echo $params->get('project_ordering', 0); ?>"/>
-            <input type="hidden" name="include_season"
-                   value="<?php echo $params->get('project_include_season_name', 0); ?>"/>
-            <input type="hidden" name="itemid" value="<?php echo $defaultitemid; ?>"/>
-        </form>
-
-    </div>
+        <input type="hidden" name="option" value="com_sportsmanagement">
+        <input type="hidden" name="view" value="<?= htmlspecialchars((string) $defaultview, ENT_QUOTES, 'UTF-8') ?>">
+        <input type="hidden" name="teamview" value="<?= htmlspecialchars((string) $params->get('link_teams_dropdown', 'roster'), ENT_QUOTES, 'UTF-8') ?>">
+        <input type="hidden" name="o" value="<?= (int) $params->get('project_ordering', 0) ?>">
+        <input type="hidden" name="include_season" value="<?= (int) $params->get('project_include_season_name', 0) ?>">
+        <input type="hidden" name="Itemid" value="<?= (int) $defaultitemid ?>">
+    </form>
 </div>
