a791e1b47dcbd0f773ee65a2916bfe5e56d5ad66
diddipoeler <diddipoeler@gmx.de>
2026-08-22 15:08:02 +0200

    Use count record display parameters in native template

--- modules/mod_sportsmanagement_count_rekord/tmpl/default.php
+++ modules/mod_sportsmanagement_count_rekord/tmpl/default.php
@@ -1,12 +1,18 @@
 <?php
 \defined('_JEXEC') or die;
+
+$moduleClass = htmlspecialchars((string) $params->get('moduleclass_sfx', ''), ENT_QUOTES, 'UTF-8');
+$tableClass = htmlspecialchars((string) $params->get('table_class', 'table'), ENT_QUOTES, 'UTF-8');
+$moduleId = htmlspecialchars($module->module . '-' . $module->id, ENT_QUOTES, 'UTF-8');
 ?>
-<div class="<?php echo htmlspecialchars($module->module . '-' . $module->id, ENT_QUOTES, 'UTF-8'); ?>">
+<div id="<?php echo $moduleId; ?>" class="<?php echo $moduleClass; ?>">
     <?php if ($list) : ?>
-        <table class="table">
+        <table class="<?php echo $tableClass; ?>">
             <tbody>
             <?php foreach ($list as $row) : ?>
-                <tr><td><?php echo $row->text; ?></td></tr>
+                <tr>
+                    <td><?php echo $row->text; ?></td>
+                </tr>
             <?php endforeach; ?>
             </tbody>
         </table>
