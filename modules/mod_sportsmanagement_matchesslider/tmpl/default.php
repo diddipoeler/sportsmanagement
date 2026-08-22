8cdb47e0307aca020bc06c5e42cb77a1a5a20742
diddipoeler <diddipoeler@gmx.de>
2026-08-22 18:19:54 +0200

    Use native matches slider markup

--- modules/mod_sportsmanagement_matchesslider/tmpl/default.php
+++ modules/mod_sportsmanagement_matchesslider/tmpl/default.php
@@ -8,82 +8,71 @@ $scrollerId = 'jsm-matchesslider-' . (int) ($module->id ?? 0);
 $direction = (string) $params->get('slide_direction', 'backwards');
 $direction = in_array($direction, ['backwards', 'forwards'], true) ? $direction : 'backwards';
 $showPictures = (int) $params->get('show_picture', 1) === 1;
+$pictureWidth = max(1, (int) $params->get('xsize', 50));
+$moduleClass = trim((string) $params->get('moduleclass_sfx', ''));
 $escape = static fn (mixed $value): string => htmlspecialchars((string) $value, ENT_QUOTES, 'UTF-8');
 ?>
 
-<script>
-(() => {
-    const initialise = () => {
-        const $ = window.jQuery;
-        const element = document.getElementById(<?php echo json_encode($scrollerId); ?>);
+<div
+    id="<?php echo $escape($scrollerId); ?>"
+    class="jsm-matchesslider<?php echo $moduleClass !== '' ? ' ' . $escape($moduleClass) : ''; ?>"
+    data-jsm-matchesslider
+    data-scroll-direction="<?php echo $escape($direction); ?>"
+    data-scroll-speed="40"
+>
+    <div class="jsm-matchesslider__track" data-jsm-matchesslider-track>
+        <?php foreach ($slidermatches as $match) : ?>
+            <article class="jsm-matchesslider__item section">
+                <div class="hp-highlight">
+                    <div class="feature-headline">
+                        <h3 class="jsm-matchesslider__date">
+                            <a href="<?php echo $escape($match->link ?? '#'); ?>">
+                                <?php
+                                echo HTMLHelper::_('date', $match->match_date, (string) $params->get('dateformat', 'D, d. M. Y'), null);
+                                echo ' ';
+                                echo HTMLHelper::_('date', $match->match_date, (string) $params->get('timeformat', 'H.i'), null);
+                                ?>
+                            </a>
+                        </h3>
 
-        if (!$ || !element || typeof $.fn.simplyScroll !== 'function') {
-            return;
-        }
+                        <div class="jsm-matchesslider__score">
+                            <?php if ($showPictures && !empty($match->home_logo_url)) : ?>
+                                <img
+                                    class="jsm-matchesslider__logo jsm-matchesslider__logo--home"
+                                    src="<?php echo $escape($match->home_logo_url); ?>"
+                                    alt="<?php echo $escape($match->teamhome ?? ''); ?>"
+                                    title="<?php echo $escape($match->teamhome ?? ''); ?>"
+                                    width="<?php echo $pictureWidth; ?>"
+                                    loading="lazy"
+                                >
+                            <?php endif; ?>
 
-        $(element).simplyScroll({
-            customClass: 'custom',
-            direction: <?php echo json_encode($direction); ?>,
-            pauseOnHover: false,
-            frameRate: 20,
-            speed: 2
-        });
-    };
+                            <span class="jsm-matchesslider__result">
+                                <?php echo $escape($match->team1_result ?? ''); ?>
+                                -
+                                <?php echo $escape($match->team2_result ?? ''); ?>
+                            </span>
 
-    if (document.readyState === 'loading') {
-        document.addEventListener('DOMContentLoaded', initialise, {once: true});
-    } else {
-        initialise();
-    }
-})();
-</script>
+                            <?php if ($showPictures && !empty($match->away_logo_url)) : ?>
+                                <img
+                                    class="jsm-matchesslider__logo jsm-matchesslider__logo--away"
+                                    src="<?php echo $escape($match->away_logo_url); ?>"
+                                    alt="<?php echo $escape($match->teamaway ?? ''); ?>"
+                                    title="<?php echo $escape($match->teamaway ?? ''); ?>"
+                                    width="<?php echo $pictureWidth; ?>"
+                                    loading="lazy"
+                                >
+                            <?php endif; ?>
+                        </div>
 
-<div id="<?php echo $escape($scrollerId); ?>" class="row">
-    <?php foreach ($slidermatches as $match) : ?>
-        <div class="section">
-            <div class="hp-highlight">
-                <div class="feature-headline">
-                    <h1>
-                        <a href="<?php echo $escape($match->link ?? '#'); ?>" title="">
-                            <?php
-                            echo HTMLHelper::_('date', $match->match_date, (string) $params->get('dateformat', 'D, d. M. Y'), null);
-                            echo ' ';
-                            echo HTMLHelper::_('date', $match->match_date, (string) $params->get('timeformat', 'H.i'), null);
-                            ?>
-                        </a>
-                    </h1>
-
-                    <p style="text-align: center;">
-                        <?php if ($showPictures && !empty($match->home_logo_url)) : ?>
-                            <img
-                                style="float: left; width: <?php echo (int) $params->get('xsize', 50); ?>px"
-                                src="<?php echo $escape($match->home_logo_url); ?>"
-                                alt="<?php echo $escape($match->teamhome ?? ''); ?>"
-                                title="<?php echo $escape($match->teamhome ?? ''); ?>"
-                            >
-                        <?php endif; ?>
-
-                        <?php echo $escape($match->team1_result ?? ''); ?>
-                        -
-                        <?php echo $escape($match->team2_result ?? ''); ?>
-
-                        <?php if ($showPictures && !empty($match->away_logo_url)) : ?>
-                            <img
-                                style="float: right; width: <?php echo (int) $params->get('xsize', 50); ?>px"
-                                src="<?php echo $escape($match->away_logo_url); ?>"
-                                alt="<?php echo $escape($match->teamaway ?? ''); ?>"
-                                title="<?php echo $escape($match->teamaway ?? ''); ?>"
-                            >
-                        <?php endif; ?>
-                    </p>
-
-                    <p style="text-align: center;">
-                        <?php echo $escape($match->teamhome ?? ''); ?>
-                        -
-                        <?php echo $escape($match->teamaway ?? ''); ?>
-                    </p>
+                        <p class="jsm-matchesslider__teams">
+                            <?php echo $escape($match->teamhome ?? ''); ?>
+                            -
+                            <?php echo $escape($match->teamaway ?? ''); ?>
+                        </p>
+                    </div>
                 </div>
-            </div>
-        </div>
-    <?php endforeach; ?>
+            </article>
+        <?php endforeach; ?>
+    </div>
 </div>
