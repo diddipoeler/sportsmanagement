03a1ad8b6b1abcfbc5b49b6061bf2c32618704d0
diddipoeler <diddipoeler@gmx.de>
2026-08-22 15:18:01 +0200

    Align birthday layouts with Joomla 5/6 parameters

--- modules/mod_sportsmanagement_birthday/tmpl/default.php
+++ modules/mod_sportsmanagement_birthday/tmpl/default.php
@@ -3,8 +3,16 @@
 
 use Joomla\CMS\Language\Text;
 
+$moduleClass = htmlspecialchars((string) $params->get('moduleclass_sfx', ''), ENT_QUOTES, 'UTF-8');
+$tableClass = htmlspecialchars((string) $params->get('table_class', 'table'), ENT_QUOTES, 'UTF-8');
+$maxDays = trim((string) $params->get('maxdays', ''));
+
 if (!$persons) {
-    echo '<p>' . htmlspecialchars(Text::_((string) $params->get('not_found_text', '')), ENT_QUOTES, 'UTF-8') . '</p>';
+    $notFound = Text::_((string) $params->get('not_found_text', ''));
+    if ($maxDays !== '') {
+        $notFound = str_replace('%DAYS%', $maxDays, $notFound);
+    }
+    echo '<p>' . htmlspecialchars($notFound, ENT_QUOTES, 'UTF-8') . '</p>';
     return;
 }
 
@@ -25,23 +33,26 @@ if ($mode === 'L' && (bool) $params->get('show_player_card', 1)) {
 
 $moduleId = 'jsm-birthday-' . (int) $module->id;
 ?>
-<div class="<?php echo htmlspecialchars((string) $params->get('moduleclass_sfx', ''), ENT_QUOTES, 'UTF-8'); ?>" id="<?php echo $moduleId; ?>">
+<div class="<?php echo $moduleClass; ?>" id="<?php echo $moduleId; ?>">
 <?php if ($mode === 'B') : ?>
     <div id="<?php echo $moduleId; ?>-carousel" class="carousel slide" data-bs-ride="carousel">
-        <div class="carousel-indicators">
-            <?php foreach ($persons as $index => $person) : ?>
-                <button type="button" data-bs-target="#<?php echo $moduleId; ?>-carousel" data-bs-slide-to="<?php echo (int) $index; ?>"
-                        class="<?php echo $index === 0 ? 'active' : ''; ?>"
-                        <?php echo $index === 0 ? 'aria-current="true"' : ''; ?>
-                        aria-label="<?php echo Text::sprintf('JLIB_HTML_PAGE_CURRENT_OF_TOTAL', $index + 1, count($persons)); ?>"></button>
-            <?php endforeach; ?>
-        </div>
+        <?php if (count($persons) > 1) : ?>
+            <div class="carousel-indicators">
+                <?php foreach ($persons as $index => $person) : ?>
+                    <button type="button" data-bs-target="#<?php echo $moduleId; ?>-carousel" data-bs-slide-to="<?php echo (int) $index; ?>"
+                            class="<?php echo $index === 0 ? 'active' : ''; ?>"
+                            <?php echo $index === 0 ? 'aria-current="true"' : ''; ?>
+                            aria-label="<?php echo htmlspecialchars(Text::sprintf('JLIB_HTML_PAGE_CURRENT_OF_TOTAL', $index + 1, count($persons)), ENT_QUOTES, 'UTF-8'); ?>"></button>
+                <?php endforeach; ?>
+            </div>
+        <?php endif; ?>
         <div class="carousel-inner">
             <?php foreach ($persons as $index => $person) : ?>
                 <div class="carousel-item<?php echo $index === 0 ? ' active' : ''; ?>">
                     <?php if (!empty($person['picture_url'])) : ?>
                         <img class="d-block mx-auto" src="<?php echo htmlspecialchars($person['picture_url'], ENT_QUOTES, 'UTF-8'); ?>"
                              alt="<?php echo htmlspecialchars($person['display_name'], ENT_QUOTES, 'UTF-8'); ?>"
+                             loading="lazy"
                              style="max-width:<?php echo max(1, (int) $params->get('picture_width', 250)); ?>px;height:auto">
                     <?php endif; ?>
                     <div class="carousel-caption position-static text-body">
@@ -51,16 +62,18 @@ $moduleId = 'jsm-birthday-' . (int) $module->id;
                 </div>
             <?php endforeach; ?>
         </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#<?php echo $moduleId; ?>-carousel" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span><span class="visually-hidden"><?php echo Text::_('JPREV'); ?></span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#<?php echo $moduleId; ?>-carousel" data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span><span class="visually-hidden"><?php echo Text::_('JNEXT'); ?></span>
-        </button>
+        <?php if (count($persons) > 1) : ?>
+            <button class="carousel-control-prev" type="button" data-bs-target="#<?php echo $moduleId; ?>-carousel" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span><span class="visually-hidden"><?php echo Text::_('JPREV'); ?></span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#<?php echo $moduleId; ?>-carousel" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span><span class="visually-hidden"><?php echo Text::_('JNEXT'); ?></span>
+            </button>
+        <?php endif; ?>
     </div>
 <?php else : ?>
     <div class="table-responsive">
-        <table class="table table-striped align-middle">
+        <table class="<?php echo $tableClass; ?> align-middle">
             <tbody>
             <?php foreach ($persons as $person) : ?>
                 <tr>
@@ -71,6 +84,7 @@ $moduleId = 'jsm-birthday-' . (int) $module->id;
                         <?php if (!empty($person['picture_url'])) : ?>
                             <div class="mt-2"><img src="<?php echo htmlspecialchars($person['picture_url'], ENT_QUOTES, 'UTF-8'); ?>"
                                  alt="<?php echo htmlspecialchars($person['display_name'], ENT_QUOTES, 'UTF-8'); ?>"
+                                 loading="lazy"
                                  style="max-height:<?php echo max(1, (int) $params->get('picture_height', 50)); ?>px;width:auto"></div>
                         <?php endif; ?>
                     </td>
