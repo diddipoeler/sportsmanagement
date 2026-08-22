ad8f07dd33baf94267b565ad2eacc2b91243c20e
diddipoeler <diddipoeler@gmx.de>
2026-08-22 13:36:32 +0200

    Add Joomla 5/6 trainings data dispatcher

---
+++ modules/mod_sportsmanagement_trainingsdata/src/Dispatcher/Dispatcher.php
@@ -0,0 +1,31 @@
+<?php
+namespace Diddipoeler\Module\SportsManagementTrainingsData\Site\Dispatcher;
+
+\defined('_JEXEC') or die;
+
+use Joomla\CMS\Dispatcher\AbstractModuleDispatcher;
+use Joomla\CMS\Helper\HelperFactoryAwareInterface;
+use Joomla\CMS\Helper\HelperFactoryAwareTrait;
+
+final class Dispatcher extends AbstractModuleDispatcher implements HelperFactoryAwareInterface
+{
+    use HelperFactoryAwareTrait;
+
+    protected function getLayoutData(): array
+    {
+        $data = parent::getLayoutData();
+        $app = $this->getApplication();
+
+        $app->getLanguage()->load('com_sportsmanagement', JPATH_SITE, null, true);
+        $data['trainingsdata'] = $this->getHelperFactory()
+            ->getHelper('TrainingsDataHelper')
+            ->getData($data['params']);
+
+        $app->getDocument()->getWebAssetManager()->registerAndUseStyle(
+            'mod_sportsmanagement_trainingsdata',
+            'modules/mod_sportsmanagement_trainingsdata/css/mod_sportsmanagement_trainingsdata.css'
+        );
+
+        return $data;
+    }
+}
