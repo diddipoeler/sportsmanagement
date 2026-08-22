79afe303d77db3837cd760027d2d05043c6d30ee
diddipoeler <diddipoeler@gmx.de>
2026-08-22 10:27:50 +0200

    Modernize playground ticker data helper

--- modules/mod_sportsmanagement_playground_ticker/helper.php
+++ modules/mod_sportsmanagement_playground_ticker/helper.php
@@ -1,223 +1,93 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- * @version    1.0.06
- * @package    Sportsmanagement
- * @subpackage mod_sportsmanagement_playground_ticker
- * @file       helper.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @modded 	   llambion (2020)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\Language\Text;
+/** Joomla 5/6 compatibility helper for the SportsManagement playground ticker module. */
+defined('_JEXEC') or die;
+
 use Joomla\CMS\Factory;
 
-/**
- * modJSMPlaygroundTicker
- *
- * @package
- * @author    abcde
- * @copyright 2015
- * @version   $Id$
- * @access    public
- */
 class modJSMPlaygroundTicker
 {
+    public static function getData($params): array
+    {
+        self::ensureSportsManagementHelper();
 
-	/**
-	 * modJSMPlaygroundTicker::getData()
-	 *
-	 * @param   mixed  $params
-	 *
-	 * @return void
-	 */
-	public static function getData($params)
-	{
-		$app = Factory::getApplication();
-        $result = array();
-        $rands = array();
-        $queryparts = array();
-		
-		// llambion
-		$project = (int) $params->get('p');
-		$x = $params->get('limit', 1);
+        $app = Factory::getApplication();
+        $input = $app->getInput();
+        $projectId = (int) $params->get('p', 0);
+        $limit = max(1, (int) $params->get('limit', 1));
+        $whichDatabase = $input->getInt('cfg_which_database', 0);
+        $db = sportsmanagementHelper::getDBConnection(true, $whichDatabase);
+        $query = $db->getQuery(true)
+            ->select([
+                $db->quoteName('pg.id', 'id_playground'),
+                $db->quoteName('pg.name', 'playground_name'),
+                $db->quoteName('pg.name'),
+                $db->quoteName('pg.address'),
+                $db->quoteName('pg.zipcode'),
+                $db->quoteName('pg.city'),
+                $db->quoteName('pg.country'),
+                $db->quoteName('pg.club_id'),
+                $db->quoteName('pg.extended'),
+                $db->quoteName('pg.latitude'),
+                $db->quoteName('pg.longitude'),
+                $db->quoteName('pg.state'),
+                $db->quoteName('pg.picture'),
+                $db->quoteName('pg.website'),
+                $db->quoteName('pg.max_visitors'),
+                $db->quoteName('cl.name', 'club_name'),
+            ])
+            ->from($db->quoteName('#__sportsmanagement_playground', 'pg'))
+            ->join(
+                'LEFT',
+                $db->quoteName('#__sportsmanagement_club', 'cl')
+                . ' ON ' . $db->quoteName('cl.id') . ' = ' . $db->quoteName('pg.club_id')
+            );
 
-		$jinput             = $app->input;
-		$cfg_which_database = $jinput->getInt('cfg_which_database', 0);
-		$db = sportsmanagementHelper::getDBConnection(true, $cfg_which_database);
-		$query = $db->getQuery(true);
+        if ($projectId > 0) {
+            $query->join(
+                'INNER',
+                $db->quoteName('#__sportsmanagement_project_team', 'pt')
+                . ' ON ' . $db->quoteName('pt.standard_playground') . ' = ' . $db->quoteName('pg.id')
+            )
+                ->where($db->quoteName('pt.project_id') . ' = ' . $projectId);
+        }
 
-		/**
-		 * Nun möchte man aber manchmal mehrere Datensätze zufällig selektieren und nicht nur einen.
-		 * Zuerst wird die Gesamtanzahl an Datensätzen bestimmt, die die Bedingungen erfüllt.
-		 * Anschließend müssen x Zufallszahlen gebildet werden. Und mit diesen wird dann eine SQL-Abfrage mit UNIONs gebaut.
-		 */
-	
-		/** 2020.07.22 All estadiums or only certain */ 
-		
-		
-		if ($project < 1)
-	    {
-			$query->select('count( * )');
+        $db->setQuery($query);
+        $rows = $db->loadObjectList() ?: [];
+        $byId = [];
 
-			// From table
-			$query->from('#__sportsmanagement_playground');
-			$db->setQuery($query);
-			$anz_cnt = $db->loadResult();
+        foreach ($rows as $row) {
+            $byId[(int) $row->id_playground] = $row;
+        }
 
-			/**
-			* Die Schleife beim Erhalten der Zufallszahlen ist deshalb eine while- und keine for-Schleife, weil es sonst passieren kann,
-			* dass es zwar mehr als x Datensätze gibt, die die Bedingungen erfüllen, aber dummerweise 2 mal die gleiche Zufallszahl ermittelt wird.
-			* Die Bedingung $anz_cnt>count($rands) dient dazu, dass keine Endlosschleife entsteht, wenn weniger als x Datensätze die Bedingung erfüllen.
-			* Bei der abschließenden Abfrage wird UNION ALL benutzt statt UNION, damit MySQL die Einzelergebnisse nicht noch versucht zu gruppieren
-			* wir wissen ja durch die while-Schleife bereits, dass keine Duplikate selektiert werden können). UNION bedeutet nämlich in Wirklichkeit UNION DISTINCT.
-			*/
-			
+        $playgrounds = array_values($byId);
 
-			while (count($rands) < $x && $anz_cnt > count($rands))
-			{
-				$rand = mt_rand(0, $anz_cnt - 1);
+        if (count($playgrounds) <= $limit) {
+            return $playgrounds;
+        }
 
-				if (!isset($rands[$rand]))
-				{
-					$rands[$rand] = $rand;
-				}
-			}
+        $keys = array_rand($playgrounds, $limit);
+        $keys = is_array($keys) ? $keys : [$keys];
 
-		}
-		
-		else // added 20.07.24 Llambion
-			
-		{		
-			/** Select playgrounds that belong to certain proyect  */
-			$query->select('estadio.id');
-			$query->from('#__sportsmanagement_playground AS estadio');
-			$query->join('INNER','#__sportsmanagement_project_team t ON estadio.id = t.standard_playground');
-			$query->where('t.project_id = '. $project);
-			$db->setQuery($query); 
-			$result = $db->loadObjectList(); 
-			$estadios = count($result);
-			
-			if ($estadios > 0) 
-			{
-			/** Search id's for random values */
-			
-			$x     = $params->get('limit', 1);
+        return array_values(array_map(static fn (int $key) => $playgrounds[$key], $keys));
+    }
 
-			while (count($rands) < $x && $estadios > count($rands))
-			{
-				$rand = mt_rand(0, $estadios - 1);
-				if (!isset($rands[$rand]))
-				{
-				$rands[$rand] = $result[$rand]->id;
-				}
-			}
+    public static function getEstadios_Proyecto($params): array
+    {
+        return self::getData($params);
+    }
 
+    private static function ensureSportsManagementHelper(): void
+    {
+        if (!class_exists('sportsmanagementHelper')) {
+            $file = JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/helpers/sportsmanagement.php';
+
+            if (is_file($file)) {
+                require_once $file;
             }
-			
-		}
+        }
 
-		
-
-        /** Changed to consider all or only project stadiums */
-		
-		if (count($rands) > 0)
-	    {
-		foreach ($rands as $rand)
-		{
-			if ($project = 0)
-			{			
-				$queryparts[] = "SELECT * FROM #__sportsmanagement_playground LIMIT " . $rand .
-					",1";
-			}
-			else
-			{
-				$queryparts[] = "SELECT pg.id AS id_playground, pg.name as playground_name, pg.address, pg.zipcode, pg.city, pg.country, 
-										pg.club_id, pg.extended, pg.latitude, pg.longitude, pg.state, pg.picture, pg.website, pg.max_visitors,
-										cl.name AS club_name
-										FROM #__sportsmanagement_playground AS pg 
-										INNER JOIN #__sportsmanagement_club AS cl 
-										ON pg.club_id = cl.id WHERE pg.id=" . $rand . ''  ;
-			}
-		}
-
-		$query = "(" . implode(") UNION ALL (", $queryparts) . ")";
-		$db->setQuery($query);
-		$result = $db->loadObjectList();
-		}
-		else
-		{
-			return $result;
-		}
-
-		return $result;
-
-	}
-	
-	// 
-	/**
-	 * modJSMPlaygroundTicker::getEstadios_Proyecto()
-	 *
-	 * @param   mixed  $params
-	 *
-	 * @return estadios
-	 */
-	public static function getEstadios_Proyecto($params)
-	{
-		$app = Factory::getApplication();
-        $result = array();
-        $queryparts = array();
-		$p = (int) $params->get('p');
-		$x = $params->get('limit', 1);
-		$jinput             = $app->input;
-		$cfg_which_database = $jinput->getInt('cfg_which_database', 0);
-		$db = sportsmanagementHelper::getDBConnection(true, $cfg_which_database);
-		$query = $db->getQuery(true);
- 
-        /** Select playgrounds that belong to certain teams proyect  */
-		$query->select('estadio.id');
-		$query->from('#__sportsmanagement_playground AS estadio');
-		$query->join('INNER','#__sportsmanagement_project_team t ON estadio.id = t.standard_playground');
-		$query->where('t.project_id = '. $p);
-		$db->setQuery($query); 
-		$result = $db->loadObjectList(); 
-		$estadios = count($result);
-		
-		if ($estadios > 0 )
-			{
-			/** Busco los numeros aleatorios y extraigo los campos que me interesan */
-			$rands = array();
-			$x     = $params->get('limit', 1);
-
-			while (count($rands) < $x && $estadios > count($rands))
-			{
-				$rand = mt_rand(0, $estadios - 1);
-				if (!isset($rands[$rand]))
-				{
-				
-					$rands[$rand] = $result[$rand]->id;
-				}
-			}
-
-			
-
-			foreach ($rands as $rand)
-			{
-				$queryparts[] = "SELECT * FROM #__sportsmanagement_playground WHERE id=" . $rand ;
-			}
-
-			$query = "(" . implode(") UNION ALL (", $queryparts) . ")";
-			$db->setQuery($query);
-			$result = $db->loadObjectList();							
-			}
-		else
-			{
-			 return $result;	
-			}	
-						
-		return $result;
-	}
-
+        if (!class_exists('sportsmanagementHelper')) {
+            throw new RuntimeException('SportsManagement database helper is not available.');
+        }
+    }
 }
