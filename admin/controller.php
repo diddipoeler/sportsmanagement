27364f5293ab33b9c71a7cabcbf33f2ade719015
diddipoeler <diddipoeler@gmx.de>
2026-08-22 19:57:15 +0200

    Bridge main controllers and remove legacy installer

--- admin/controller.php
+++ admin/controller.php
@@ -1,75 +1,15 @@
 <?php
 /**
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage admin
- * @file       controller.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * Legacy administrator controller bridge for Joomla 5/6.
  */
-defined('_JEXEC') or die('Restricted access');
+\defined('_JEXEC') or die;
 
-use Joomla\CMS\Factory;
-use Joomla\CMS\MVC\Controller\BaseController;
+use Diddipoeler\Component\SportsManagement\Administrator\Controller\DisplayController;
 
-if (!defined('JSM_PATH'))
-{
-	define('JSM_PATH', 'components/com_sportsmanagement');
+if (!class_exists(DisplayController::class)) {
+    require_once __DIR__ . '/src/Controller/DisplayController.php';
 }
 
-if (!class_exists('sportsmanagementHelper'))
-{
-	$helperFile = JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/helpers/sportsmanagement.php';
-
-	if (is_file($helperFile))
-	{
-		require_once $helperFile;
-	}
-}
-
-/**
- * SportsManagementController
- *
- * @package
- * @author    diddi
- * @copyright 2014
- * @version   $Id$
- * @access    public
- */
-class SportsManagementController extends BaseController
-{
-
-	/**
-	 * SportsManagementController::display()
-	 *
-	 * @param   bool  $cachable
-	 * @param   bool  $urlparams
-	 *
-	 * @return void
-	 */
-	public function display($cachable = false, $urlparams = false)
-	{
-		$input = Factory::getApplication()->getInput();
-
-		/**
-		 * set default view if not set
-		 */
-		$input->set('view', $input->getCmd('view', 'cpanel'));
-		$layout = $input->getCmd('layout', 'default');
-
-		/**
-		 * call parent behavior
-		 */
-		parent::display($cachable, $urlparams);
-
-		if ($layout !== 'edit' && class_exists('sportsmanagementHelper'))
-		{
-			/**
-			 * Set the submenu
-			 */
-			sportsmanagementHelper::addSubmenu('messages');
-		}
-	}
+if (!class_exists('SportsManagementController', false)) {
+    class_alias(DisplayController::class, 'SportsManagementController');
 }
--- site/controller.php
+++ site/controller.php
@@ -1,56 +1,15 @@
 <?php
 /**
- *
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @file       controller.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * Legacy site controller bridge for Joomla 5/6.
  */
+\defined('_JEXEC') or die;
 
-defined('_JEXEC') or die('Restricted access');
+use Diddipoeler\Component\SportsManagement\Site\Controller\DisplayController;
 
-use Joomla\CMS\MVC\Controller\BaseController;
-
-/**
- * sportsmanagementController
- *
- * @package
- * @author
- * @copyright diddi
- * @version   2014
- * @access    public
- */
-class sportsmanagementController extends BaseController
-{
-
-	/**
-	 * sportsmanagementController::__construct()
-	 *
-	 * @param   mixed  $config
-	 *
-	 * @return void
-	 */
-	function __construct($config = array())
-	{
-		parent::__construct($config);
-	}
-
-	/**
-	 * sportsmanagementController::display()
-	 *
-	 * @param   bool  $cachable
-	 * @param   bool  $urlparams
-	 *
-	 * @return void
-	 */
-	public function display($cachable = false, $urlparams = false)
-	{
-		parent::display($cachable, $urlparams);
-	}
-
+if (!class_exists(DisplayController::class)) {
+    require_once __DIR__ . '/src/Controller/DisplayController.php';
 }
 
+if (!class_exists('sportsmanagementController', false)) {
+    class_alias(DisplayController::class, 'sportsmanagementController');
+}
