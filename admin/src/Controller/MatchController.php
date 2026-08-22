48b1711ab765b02bef1619489cca2f9fbd9afc17
diddipoeler <diddipoeler@gmx.de>
2026-08-22 21:05:16 +0200

    Migrate match copy workflow to Joomla 5/6

--- admin/src/Controller/MatchController.php
+++ admin/src/Controller/MatchController.php
@@ -3,6 +3,7 @@ namespace Diddipoeler\Component\SportsManagement\Administrator\Controller;
 
 \defined('_JEXEC') or die;
 
+use Diddipoeler\Component\SportsManagement\Administrator\Helper\SportsManagementDateHelper;
 use Diddipoeler\Component\SportsManagement\Administrator\Service\GoogleCalendarMatchSynchronizer;
 use Diddipoeler\Component\SportsManagement\Site\Service\SportsManagementDatabaseResolver;
 use Joomla\CMS\Factory;
@@ -90,6 +91,172 @@ final class MatchController extends \sportsmanagementControllermatch
         );
 
         return true;
+    }
+
+    public function copyfrom()
+    {
+        $this->checkToken();
+
+        $app = Factory::getApplication();
+        $input = $app->getInput();
+        $option = $input->getCmd('option', 'com_sportsmanagement');
+        $post = $input->post->getArray();
+        $projectId = (int) $app->getUserState($option . '.pid', 0);
+        $roundId = $input->getInt('rid');
+        $model = $this->getModel('match');
+        $redirect = 'index.php?option=com_sportsmanagement&view=matches';
+
+        if ($model === false) {
+            $this->setRedirect($redirect, Text::_('JLIB_APPLICATION_ERROR_SAVE_FAILED'), 'error');
+
+            return false;
+        }
+
+        $post['project_id'] = $projectId;
+        $post['round_id'] = $roundId;
+        $addType = (int) ($post['addtype'] ?? 0);
+        $success = true;
+        $message = '';
+
+        try {
+            /** @var DatabaseInterface $joomlaDatabase */
+            $joomlaDatabase = Factory::getContainer()->get(DatabaseInterface::class);
+            $databaseSelector = $input->getInt(
+                'cfg_which_database',
+                (int) $app->getUserState('com_sportsmanagement.cfg_which_database', 0)
+            );
+            $db = SportsManagementDatabaseResolver::resolve($joomlaDatabase, $databaseSelector);
+
+            if ($addType === 1) {
+                $addMatchCount = max(0, (int) ($post['add_match_count'] ?? 0));
+                $projectRounds = $model->getProjectRoundCodes($projectId);
+
+                if ($addMatchCount <= 0 || !$projectRounds) {
+                    $success = false;
+                    $message = Text::_('COM_SPORTSMANAGEMENT_ADMIN_MATCH_CTRL_ERROR_ADD_MATCH');
+                } else {
+                    if (!empty($post['autoPublish'])) {
+                        $post['published'] = 1;
+                    }
+
+                    $convertedDate = SportsManagementDateHelper::convertDate(
+                        (string) ($post['match_date'] ?? ''),
+                        0
+                    );
+                    $post['match_date'] = trim($convertedDate . ' ' . (string) ($post['startTime'] ?? ''));
+                    $matchNumber = max(1, $input->getInt('firstMatchNumber', 1));
+                    $roundFound = false;
+                    $saved = 0;
+
+                    foreach ($projectRounds as $projectRound) {
+                        if ((int) $projectRound->id === $roundId) {
+                            $roundFound = true;
+                        }
+
+                        if (!$roundFound) {
+                            continue;
+                        }
+
+                        for ($index = 0; $index < $addMatchCount; $index++) {
+                            $matchData = $post;
+                            $matchData['id'] = 0;
+                            $matchData['round_id'] = (int) $projectRound->id;
+                            $matchData['roundcode'] = (int) ($projectRound->roundcode ?? 0);
+
+                            if (!empty($post['firstMatchNumber'])) {
+                                $matchData['match_number'] = $matchNumber;
+                            }
+
+                            if (!$model->save($matchData)) {
+                                $success = false;
+                                $message = Text::_('COM_SPORTSMANAGEMENT_ADMIN_MATCH_CTRL_ERROR_ADD_MATCH');
+                                break 2;
+                            }
+
+                            $saved++;
+                            $matchNumber++;
+                        }
+
+                        if (empty($post['addToRound'])) {
+                            break;
+                        }
+                    }
+
+                    if ($saved === 0) {
+                        $success = false;
+                        $message = Text::_('COM_SPORTSMANAGEMENT_ADMIN_MATCH_CTRL_ERROR_ADD_MATCH');
+                    } elseif ($success) {
+                        $message = Text::_('COM_SPORTSMANAGEMENT_ADMIN_MATCH_CTRL_ADD_MATCH');
+                    }
+                }
+            } elseif ($addType === 2) {
+                $matches = $model->getRoundMatches($roundId);
+
+                if (!$matches) {
+                    $success = false;
+                    $message = Text::_('COM_SPORTSMANAGEMENT_ADMIN_MATCH_CTRL_ERROR_COPY_MATCH2');
+                } else {
+                    $convertedDate = SportsManagementDateHelper::convertDate(
+                        (string) ($post['date'] ?? ''),
+                        0
+                    );
+                    $matchDate = trim($convertedDate . ' ' . (string) ($post['startTime'] ?? ''));
+                    $targetRoundId = $roundId;
+
+                    if (!empty($post['create_new'])) {
+                        $round = new \stdClass();
+                        $round->project_id = $projectId;
+                        $round->roundcode = '';
+                        $round->name = (string) ($post['start_round_name'] ?? '');
+                        $round->modified = Factory::getDate()->toSql();
+                        $round->modified_by = (int) $app->getIdentity()->id;
+                        $db->insertObject('#__sportsmanagement_round', $round);
+                        $targetRoundId = (int) $db->insertid();
+                    }
+
+                    $startMatchNumber = $post['start_match_number'] ?? '';
+
+                    foreach ($matches as $match) {
+                        $matchData = [
+                            'id' => 0,
+                            'match_date' => $matchDate,
+                            'projectteam1_id' => !empty($post['mirror'])
+                                ? (int) $match->projectteam2_id
+                                : (int) $match->projectteam1_id,
+                            'projectteam2_id' => !empty($post['mirror'])
+                                ? (int) $match->projectteam1_id
+                                : (int) $match->projectteam2_id,
+                            'project_id' => $projectId,
+                            'round_id' => $targetRoundId,
+                        ];
+
+                        if ($startMatchNumber !== '') {
+                            $matchData['match_number'] = (int) $startMatchNumber;
+                            $startMatchNumber = (int) $startMatchNumber + 1;
+                        }
+
+                        if (!$model->save($matchData)) {
+                            $success = false;
+                        }
+                    }
+
+                    $message = Text::_($success
+                        ? 'COM_SPORTSMANAGEMENT_ADMIN_MATCH_CTRL_COPY_MATCH'
+                        : 'COM_SPORTSMANAGEMENT_ADMIN_MATCH_CTRL_ERROR_COPY_MATCH');
+                }
+            } else {
+                $success = false;
+                $message = Text::_('JLIB_APPLICATION_ERROR_SAVE_FAILED');
+            }
+        } catch (\Throwable $exception) {
+            Log::add(__METHOD__ . ': ' . $exception->getMessage(), Log::ERROR, 'jsmerror');
+            $success = false;
+            $message = $exception->getMessage();
+        }
+
+        $this->setRedirect($redirect, $message, $success ? 'message' : 'error');
+
+        return $success;
     }
 
     public function addmatch()
