adfa2edb1f9b9281758de50d5d86f47a1f07d3cd
diddipoeler <diddipoeler@gmx.de>
2026-08-22 15:32:32 +0200

    Use native gcalendar module dispatcher

--- modules/mod_sportsmanagement_gcalendar/mod_sportsmanagement_gcalendar.php
+++ modules/mod_sportsmanagement_gcalendar/mod_sportsmanagement_gcalendar.php
@@ -1,19 +1,7 @@
 <?php
 /**
- * Legacy entry bridge for the Joomla 5/6 SportsManagement Google calendar module.
+ * SportsManagement Joomla 5/6 module entry compatibility file.
+ *
+ * The active module is booted by services/provider.php and the namespaced dispatcher.
  */
-
-defined('_JEXEC') or die;
-
-use Diddipoeler\Module\SportsManagementGcalendar\Site\Helper\GcalendarHelper;
-use Joomla\CMS\Factory;
-use Joomla\CMS\Helper\ModuleHelper;
-
-if (!class_exists(GcalendarHelper::class)) {
-    require_once __DIR__ . '/src/Helper/GcalendarHelper.php';
-}
-
-$data = (new GcalendarHelper())->getData($params, $module, Factory::getApplication());
-extract($data, EXTR_SKIP);
-
-require ModuleHelper::getLayoutPath('mod_sportsmanagement_gcalendar', $params->get('layout', 'default'));
+\defined('_JEXEC') or die;
