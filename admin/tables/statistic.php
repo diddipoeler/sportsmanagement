b424b362c16cbfcec9b3ce8c28ea6e8e700d8bbf
diddipoeler <diddipoeler@gmx.de>
2026-08-22 18:56:54 +0200

    Bridge statistic table to Joomla 5/6 class

--- admin/tables/statistic.php
+++ admin/tables/statistic.php
@@ -1,75 +1,13 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage tables
- * @file       statistic.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\Language\Text;
-use Joomla\CMS\Filter\OutputFilter;
+/** SportsManagement legacy compatibility bridge for the statistic table. */
+\defined('_JEXEC') or die;
 
-/**
- * sportsmanagementTableStatistic
- *
- * @package
- * @author    diddi
- * @copyright 2014
- * @version   $Id$
- * @access    public
- */
-class sportsmanagementTableStatistic extends JSMTable
-{
-	/**
-	 * Constructor
-	 *
-	 * @param   object Database connector object
-	 *
-	 * @since 1.0
-	 */
-	function __construct(&$db)
-	{
-		$db = sportsmanagementHelper::getDBConnection();
-		parent::__construct('#__sportsmanagement_statistic', 'id', $db);
-	}
+use Diddipoeler\Component\SportsManagement\Administrator\Table\StatisticTable;
 
-	/**
-	 * Overloaded check method to ensure data integrity
-	 *
-	 * @access public
-	 * @return boolean True on success
-	 * @since  1.0
-	 */
-	function check()
-	{
-		if (empty($this->name))
-		{
-			$this->setError(Text::_('NAME REQUIRED'));
+if (!class_exists(StatisticTable::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Table/StatisticTable.php';
+}
 
-			return false;
-		}
-
-		if (empty($this->short))
-		{
-			$this->short = strtoupper(substr($this->name, 0, 4));
-		}
-
-		// Setting alias
-		if (empty($this->alias))
-		{
-			$this->alias = OutputFilter::stringURLSafe($this->name);
-		}
-		else
-		{
-			$this->alias = OutputFilter::stringURLSafe($this->alias); // Make sure the user didn't modify it to something illegal...
-		}
-
-		return true;
-	}
-
-
+if (!class_exists('sportsmanagementTableStatistic', false)) {
+    class_alias(StatisticTable::class, 'sportsmanagementTableStatistic');
 }
