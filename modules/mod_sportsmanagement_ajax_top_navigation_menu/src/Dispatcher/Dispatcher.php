ddbd16d3d4acde805865d7028df488dad7398153
diddipoeler <diddipoeler@gmx.de>
2026-08-22 12:57:55 +0200

    Add native AJAX top navigation dispatcher

---
+++ modules/mod_sportsmanagement_ajax_top_navigation_menu/src/Dispatcher/Dispatcher.php
@@ -0,0 +1,63 @@
+<?php
+namespace Diddipoeler\Module\SportsManagementAjaxTopNavigationMenu\Site\Dispatcher;
+
+\defined('_JEXEC') or die;
+
+use Joomla\CMS\Dispatcher\AbstractModuleDispatcher;
+use Joomla\CMS\Helper\HelperFactoryAwareInterface;
+use Joomla\CMS\Helper\HelperFactoryAwareTrait;
+
+final class Dispatcher extends AbstractModuleDispatcher implements HelperFactoryAwareInterface
+{
+    use HelperFactoryAwareTrait;
+
+    protected function getLayoutData(): array
+    {
+        $data = parent::getLayoutData();
+        $app = $this->getApplication();
+        $app->getLanguage()->load('com_sportsmanagement', JPATH_ADMINISTRATOR, null, true);
+
+        $requestedLayout = (string) $data['params']->get('layout', 'default');
+        if ($requestedLayout === '' || $requestedLayout === '_:native' || $requestedLayout === 'native') {
+            $requestedLayout = 'default';
+        }
+
+        $payload = $this->getHelperFactory()
+            ->getHelper('AjaxTopNavigationHelper')
+            ->getData($data['params'], $data['module'], $app);
+
+        foreach ($payload as $key => $value) {
+            $data[$key] = $value;
+        }
+
+        $data['legacyLayout'] = $requestedLayout;
+        $data['params']->set('layout', 'native');
+
+        $document = $app->getDocument();
+        $assets = $document->getWebAssetManager();
+        $assets->useScript('bootstrap.tab');
+        $assets->registerAndUseStyle(
+            'mod_sportsmanagement_ajax_top_navigation_menu',
+            'modules/mod_sportsmanagement_ajax_top_navigation_menu/css/mod_sportsmanagement_ajax_top_navigation_menu.css',
+            ['version' => 'auto']
+        );
+        $assets->registerAndUseStyle(
+            'mod_sportsmanagement_ajax_top_navigation_menu.tabs',
+            'modules/mod_sportsmanagement_ajax_top_navigation_menu/css/mod_sportsmanagement_ajax_top_navigation_tabs_sliders.css',
+            ['version' => 'auto']
+        );
+        $assets->registerAndUseScript(
+            'mod_sportsmanagement_ajax_top_navigation_menu.native',
+            'modules/mod_sportsmanagement_ajax_top_navigation_menu/js/native.js',
+            ['version' => 'auto'],
+            ['defer' => true]
+        );
+
+        $document->addScriptOptions(
+            'mod_sportsmanagement_ajax_top_navigation_menu.' . (int) $data['module']->id,
+            $data['clientConfig']
+        );
+
+        return $data;
+    }
+}
