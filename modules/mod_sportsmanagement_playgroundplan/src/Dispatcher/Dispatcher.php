705a159e8682ed2881409ac9ba6b8ef49e365062
diddipoeler <diddipoeler@gmx.de>
2026-08-22 12:36:41 +0200

    Add Joomla 5/6 playground plan dispatcher

---
+++ modules/mod_sportsmanagement_playgroundplan/src/Dispatcher/Dispatcher.php
@@ -0,0 +1,39 @@
+<?php
+namespace Diddipoeler\Module\SportsManagementPlaygroundPlan\Site\Dispatcher;
+
+\defined('_JEXEC') or die;
+
+use Joomla\CMS\Dispatcher\AbstractModuleDispatcher;
+use Joomla\CMS\Helper\HelperFactoryAwareInterface;
+use Joomla\CMS\Helper\HelperFactoryAwareTrait;
+
+final class Dispatcher extends AbstractModuleDispatcher implements HelperFactoryAwareInterface
+{
+    use HelperFactoryAwareTrait;
+
+    protected function getLayoutData(): array
+    {
+        $data = parent::getLayoutData();
+        $app = $this->getApplication();
+
+        $app->getLanguage()->load('com_sportsmanagement', JPATH_SITE, null, true);
+        $data['list'] = $this->getHelperFactory()
+            ->getHelper('PlaygroundPlanHelper')
+            ->getData($data['params'], $app, $data['module']);
+
+        $wam = $app->getDocument()->getWebAssetManager();
+        $wam->registerAndUseStyle(
+            'mod_sportsmanagement_playgroundplan',
+            'modules/mod_sportsmanagement_playgroundplan/css/mod_sportsmanagement_playgroundplan.css'
+        );
+
+        if ((int) $data['params']->get('mode', 0) === 0) {
+            $wam->registerAndUseScript(
+                'mod_sportsmanagement_playgroundplan.ticker',
+                'modules/mod_sportsmanagement_playgroundplan/js/ticker.js'
+            );
+        }
+
+        return $data;
+    }
+}
