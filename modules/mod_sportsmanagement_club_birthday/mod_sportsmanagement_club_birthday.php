eddce64bd2bf0ad28ac83c2b46838b4ac043f932
diddipoeler <diddipoeler@gmx.de>
2026-08-22 14:46:56 +0200

    Reduce club birthday entry to compatibility stub

--- modules/mod_sportsmanagement_club_birthday/mod_sportsmanagement_club_birthday.php
+++ modules/mod_sportsmanagement_club_birthday/mod_sportsmanagement_club_birthday.php
@@ -1,26 +1,7 @@
 <?php
 /**
- * Legacy entry bridge for the Joomla 5/6 SportsManagement club birthday module.
+ * Joomla 5/6 compatibility entry point for the SportsManagement Club Birthday module.
  *
- * The active implementation is loaded through services/provider.php.
+ * The active implementation is booted by services/provider.php.
  */
 \defined('_JEXEC') or die;
-
-use Diddipoeler\Module\SportsManagementClubBirthday\Site\Helper\ClubBirthdayHelper;
-use Joomla\CMS\Factory;
-use Joomla\CMS\Helper\ModuleHelper;
-use Joomla\Database\DatabaseInterface;
-
-if (!class_exists(ClubBirthdayHelper::class)) {
-    require_once __DIR__ . '/src/Helper/ClubBirthdayHelper.php';
-}
-
-$app = Factory::getApplication();
-$app->getLanguage()->load('com_sportsmanagement', JPATH_ADMINISTRATOR, null, true);
-/** @var DatabaseInterface $database */
-$database = Factory::getContainer()->get(DatabaseInterface::class);
-$result = (new ClubBirthdayHelper())->getData($params, $app, $database);
-$clubs = $result['clubs'];
-$mode = $result['mode'];
-
-require ModuleHelper::getLayoutPath($module->module, (string) $params->get('layout', 'default'));
