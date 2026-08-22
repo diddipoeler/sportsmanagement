33678e104289c243c2aa9a28f3d7dd56151fffd2
diddipoeler <diddipoeler@gmx.de>
2026-08-22 14:49:46 +0200

    Harden native quickicon template

--- admin/modules/mod_sportsmanagement_quickicon/tmpl/default.php
+++ admin/modules/mod_sportsmanagement_quickicon/tmpl/default.php
@@ -3,17 +3,21 @@
 
 use Joomla\CMS\Language\Text;
 
-if (!$componentEnabled) {
+if (!$componentEnabled || !$canManage) {
     return;
 }
+
+$escape = static fn(mixed $value): string => htmlspecialchars((string) $value, ENT_QUOTES, 'UTF-8');
 ?>
-<nav class="quick-icons px-3 pb-3" aria-label="<?php echo htmlspecialchars(Text::_('MOD_SPORTSMANAGEMENT_QUICKICON_LABEL'), ENT_QUOTES, 'UTF-8'); ?>">
+<nav class="quick-icons px-3 pb-3" aria-label="<?php echo $escape(Text::_('MOD_SPORTSMANAGEMENT_QUICKICON_LABEL')); ?>">
     <ul class="nav flex-wrap">
         <?php foreach ($links as $link) : ?>
             <li class="quickicon quickicon-single">
-                <a href="<?php echo htmlspecialchars($link['url'], ENT_QUOTES, 'UTF-8'); ?>" title="<?php echo htmlspecialchars($link['title'], ENT_QUOTES, 'UTF-8'); ?>">
-                    <div class="quickicon-icon"><img src="<?php echo htmlspecialchars($link['icon'], ENT_QUOTES, 'UTF-8'); ?>" alt="" /></div>
-                    <div class="quickicon-name d-flex align-items-end"><?php echo htmlspecialchars($link['label'], ENT_QUOTES, 'UTF-8'); ?></div>
+                <a href="<?php echo $escape($link['url']); ?>" title="<?php echo $escape($link['title']); ?>">
+                    <div class="quickicon-icon">
+                        <img src="<?php echo $escape($link['icon']); ?>" alt="" loading="lazy">
+                    </div>
+                    <div class="quickicon-name d-flex align-items-end"><?php echo $escape($link['label']); ?></div>
                 </a>
             </li>
         <?php endforeach; ?>
