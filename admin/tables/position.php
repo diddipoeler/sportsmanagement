5ea9f8522312a38b0077dd7fc48bd08e9fa4a761
diddipoeler <diddipoeler@gmx.de>
2026-08-22 18:56:30 +0200

    Bridge position table to Joomla 5/6 class

--- admin/tables/position.php
+++ admin/tables/position.php
@@ -1,70 +1,13 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage tables
- * @file       position.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\Language\Text;
-use Joomla\CMS\Filter\OutputFilter;
+/** SportsManagement legacy compatibility bridge for the position table. */
+\defined('_JEXEC') or die;
 
-/**
- * sportsmanagementTablePosition
- *
- * @package
- * @author
- * @copyright diddi
- * @version   2014
- * @access    public
- */
-class sportsmanagementTablePosition extends JSMTable
-{
-	/**
-	 * Constructor
-	 *
-	 * @param   object Database connector object
-	 *
-	 * @since 1.0
-	 */
-	function __construct(&$db)
-	{
-		$db = sportsmanagementHelper::getDBConnection();
-		parent::__construct('#__sportsmanagement_position', 'id', $db);
-	}
+use Diddipoeler\Component\SportsManagement\Administrator\Table\PositionTable;
 
-	/**
-	 * Overloaded check method to ensure data integrity
-	 *
-	 * @access public
-	 * @return boolean True on success
-	 * @since  1.0
-	 */
-	function check()
-	{
-		if (empty($this->name))
-		{
-			$this->setError(Text::_('ERROR NAME REQUIRED'));
+if (!class_exists(PositionTable::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Table/PositionTable.php';
+}
 
-			return false;
-		}
-
-		// Setting alias
-		if (empty($this->alias))
-		{
-			$this->alias = OutputFilter::stringURLSafe($this->name);
-		}
-		else
-		{
-			$this->alias = OutputFilter::stringURLSafe($this->alias); // Make sure the user didn't modify it to something illegal...
-		}
-
-		return true;
-	}
-
-
+if (!class_exists('sportsmanagementTablePosition', false)) {
+    class_alias(PositionTable::class, 'sportsmanagementTablePosition');
 }
