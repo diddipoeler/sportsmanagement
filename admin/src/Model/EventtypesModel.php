17f9b482043f6707d97d122225e108903f432b44
diddipoeler <diddipoeler@gmx.de>
2026-08-22 22:03:07 +0200

    Remove legacy helper from event type options

--- admin/src/Model/EventtypesModel.php
+++ admin/src/Model/EventtypesModel.php
@@ -3,6 +3,7 @@ namespace Diddipoeler\Component\SportsManagement\Administrator\Model;
 
 \defined('_JEXEC') or die;
 
+use Diddipoeler\Component\SportsManagement\Administrator\Helper\SportsManagementDatabaseResolver;
 use Joomla\CMS\Factory;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\MVC\Factory\MVCFactoryInterface;
@@ -92,19 +93,7 @@ final class EventtypesModel extends SportsManagementListModel
 
     public static function getEvents(int $sportsTypeId = 0): array
     {
-        if (!class_exists('sportsmanagementHelper', false)) {
-            $helperFile = JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/helpers/sportsmanagement.php';
-
-            if (is_file($helperFile)) {
-                require_once $helperFile;
-            }
-        }
-
-        if (!class_exists('sportsmanagementHelper')) {
-            return [];
-        }
-
-        $db = \sportsmanagementHelper::getDBConnection();
+        $db = (new SportsManagementDatabaseResolver())->resolve();
         $query = $db->getQuery(true)
             ->select(['evt.id AS value', 'evt.name AS posname', 'st.name AS stname'])
             ->from($db->quoteName('#__sportsmanagement_eventtype', 'evt'))
