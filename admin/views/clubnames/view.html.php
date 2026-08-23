622cabcd620f5f66ab1b9c487b7a3479e6f224a7
diddipoeler <diddipoeler@gmx.de>
2026-08-23 14:58:22 +0200

    Use native club-name table in admin view

--- admin/views/clubnames/view.html.php
+++ admin/views/clubnames/view.html.php
@@ -1,69 +1,43 @@
 <?php
 /**
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage clubnames
- * @file       view.html.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * SportsManagement administrator club names list view.
  */
 defined('_JEXEC') or die('Restricted access');
+
+use Diddipoeler\Component\SportsManagement\Administrator\Table\ClubnameTable;
 use Joomla\CMS\HTML\HTMLHelper;
-use Joomla\CMS\Table\Table;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\Toolbar\ToolbarHelper;
 
-/**
- * sportsmanagementViewClubnames
- *
- * @package
- * @author    Dieter Plöger
- * @copyright 2016
- * @version   $Id$
- * @access    public
- */
 class sportsmanagementViewClubnames extends sportsmanagementView
 {
+    public function init()
+    {
+        $lists = [];
+        $nation = [
+            HTMLHelper::_('select.option', '0', Text::_('COM_SPORTSMANAGEMENT_GLOBAL_SELECT_COUNTRY')),
+        ];
 
-	/**
-	 * sportsmanagementViewClubnames::init()
-	 *
-	 * @return void
-	 */
-	public function init()
-	{
-		$lists = array();
-		$nation[] = HTMLHelper::_('select.option', '0', Text::_('COM_SPORTSMANAGEMENT_GLOBAL_SELECT_COUNTRY'));
+        if ($res = JSMCountries::getCountryOptions()) {
+            $nation = array_merge($nation, $res);
+            $this->search_nation = $res;
+        }
 
-		if ($res = JSMCountries::getCountryOptions())
-		{
-			$nation              = array_merge($nation, $res);
-			$this->search_nation = $res;
-		}
+        $lists['nation'] = $nation;
+        $this->table = new ClubnameTable($this->model->getDatabase());
+        $this->lists = $lists;
+    }
 
-		$lists['nation'] = $nation;
-		$this->table     = Table::getInstance('clubname', 'sportsmanagementTable');
-		$this->lists     = $lists;
-
-	}
-
-	/**
-	 * Add the page title and toolbar.
-	 *
-	 * @since 1.7
-	 */
-	protected function addToolbar()
-	{
-		$this->title = Text::_('COM_SPORTSMANAGEMENT_ADMIN_CLUBNAMES_TITLE');
-		ToolbarHelper::publish('clubnames.publish', 'JTOOLBAR_PUBLISH', true);
-		ToolbarHelper::unpublish('clubnames.unpublish', 'JTOOLBAR_UNPUBLISH', true);
-		ToolbarHelper::checkin('clubnames.checkin');
-		ToolbarHelper::custom('clubnames.import', 'upload', 'upload', Text::_('JTOOLBAR_INSTALL'), false);
-		ToolbarHelper::divider();
-		ToolbarHelper::addNew('clubname.add');
-		ToolbarHelper::editList('clubname.edit');
-		parent::addToolbar();
-	}
+    protected function addToolbar()
+    {
+        $this->title = Text::_('COM_SPORTSMANAGEMENT_ADMIN_CLUBNAMES_TITLE');
+        ToolbarHelper::publish('clubnames.publish', 'JTOOLBAR_PUBLISH', true);
+        ToolbarHelper::unpublish('clubnames.unpublish', 'JTOOLBAR_UNPUBLISH', true);
+        ToolbarHelper::checkin('clubnames.checkin');
+        ToolbarHelper::custom('clubnames.import', 'upload', 'upload', Text::_('JTOOLBAR_INSTALL'), false);
+        ToolbarHelper::divider();
+        ToolbarHelper::addNew('clubname.add');
+        ToolbarHelper::editList('clubname.edit');
+        parent::addToolbar();
+    }
 }
