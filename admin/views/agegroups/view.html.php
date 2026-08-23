61ef895e8e639172da90252cbd0bab8ad1975021
diddipoeler <diddipoeler@gmx.de>
2026-08-23 14:44:54 +0200

    Use native MVC factory in age groups view

--- admin/views/agegroups/view.html.php
+++ admin/views/agegroups/view.html.php
@@ -10,111 +10,100 @@
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 defined('_JEXEC') or die('Restricted access');
+
+use Diddipoeler\Component\SportsManagement\Administrator\Table\AgegroupTable;
 use Joomla\CMS\HTML\HTMLHelper;
-use Joomla\CMS\MVC\Model\BaseDatabaseModel;
 use Joomla\CMS\Language\Text;
-use Joomla\CMS\Table\Table;
 use Joomla\CMS\Toolbar\ToolbarHelper;
-use Joomla\CMS\Form\Form;
-use Joomla\CMS\Log\Log;
 
 /**
  * sportsmanagementViewagegroups
- *
- * @package
- * @author
- * @copyright diddi
- * @version   2014
- * @access    public
  */
 class sportsmanagementViewagegroups extends sportsmanagementView
 {
+    public function init()
+    {
+        $factory = $this->app->bootComponent('com_sportsmanagement')->getMVCFactory();
+        $this->table = new AgegroupTable($this->model->getDatabase());
 
-	/**
-	 * sportsmanagementViewagegroups::init()
-	 *
-	 * @return void
-	 */
-	public function init()
-	{
-		$mdlSportsType = BaseDatabaseModel::getInstance('SportsType', 'sportsmanagementModel');
-		$this->table = Table::getInstance('agegroup', 'sportsmanagementTable');
+        /** Build the html select list for sportstypes */
+        $sportstypes = [
+            HTMLHelper::_(
+                'select.option',
+                '0',
+                Text::_('COM_SPORTSMANAGEMENT_ADMIN_PROJECTS_SPORTSTYPE_FILTER'),
+                'id',
+                'name'
+            ),
+        ];
+        $sportsTypesModel = $factory->createModel('Sportstypes', 'Administrator');
+        $allSportstypes = $sportsTypesModel ? $sportsTypesModel->getSportsTypes() : [];
+        $sportstypes = array_merge($sportstypes, $allSportstypes);
+        $this->sports_type = $allSportstypes;
 
-		/** Build the html select list for sportstypes */
-		$sportstypes[]     = HTMLHelper::_('select.option', '0', Text::_('COM_SPORTSMANAGEMENT_ADMIN_PROJECTS_SPORTSTYPE_FILTER'), 'id', 'name');
-		$mdlSportsTypes    = BaseDatabaseModel::getInstance('SportsTypes', 'sportsmanagementModel');
-		$allSportstypes    = $mdlSportsTypes->getSportsTypes();
-		$sportstypes       = array_merge($sportstypes, $allSportstypes);
-		$this->sports_type = $allSportstypes;
+        $lists['sportstypes'] = HTMLHelper::_(
+            'select.genericList',
+            $sportstypes,
+            'filter_sports_type',
+            'class="inputbox" onChange="this.form.submit();" style="width:120px"',
+            'id',
+            'name',
+            $this->state->get('filter.sports_type')
+        );
 
-		$lists['sportstypes'] = HTMLHelper::_(
-			'select.genericList',
-			$sportstypes,
-			'filter_sports_type',
-			'class="inputbox" onChange="this.form.submit();" style="width:120px"',
-			'id',
-			'name',
-			$this->state->get('filter.sports_type')
-		);
-		unset($sportstypes);
+        /** Build the html options for nation */
+        $nation = [
+            HTMLHelper::_('select.option', '0', Text::_('COM_SPORTSMANAGEMENT_GLOBAL_SELECT_COUNTRY')),
+        ];
 
-		/** Build the html options for nation */
-		$nation[] = HTMLHelper::_('select.option', '0', Text::_('COM_SPORTSMANAGEMENT_GLOBAL_SELECT_COUNTRY'));
+        if ($res = JSMCountries::getCountryOptions()) {
+            $nation = array_merge($nation, $res);
+            $this->search_nation = $res;
+        }
 
-		if ($res = JSMCountries::getCountryOptions())
-		{
-			$nation              = array_merge($nation, $res);
-			$this->search_nation = $res;
-		}
+        $lists['nation'] = $nation;
+        $lists['nation2'] = JHtmlSelect::genericlist(
+            $nation,
+            'filter_search_nation',
+            'class="inputbox" style="width:140px; " onchange="this.form.submit();"',
+            'value',
+            'text',
+            $this->state->get('filter.search_nation')
+        );
 
-		$lists['nation']  = $nation;
-		$lists['nation2'] = JHtmlSelect::genericlist(
-			$nation,
-			'filter_search_nation',
-			'class="inputbox" style="width:140px; " onchange="this.form.submit();"',
-			'value',
-			'text',
-			$this->state->get('filter.search_nation')
-		);
+        $sportstypeNames = [];
+        foreach ($allSportstypes as $sportstype) {
+            $sportstypeNames[(int) $sportstype->id] = (string) $sportstype->name;
+        }
 
-		foreach ($this->items as $item)
-		{
-			$sportstype = $mdlSportsType->getSportstype($item->sportstype_id);
+        foreach ($this->items as $item) {
+            $item->sportstype = $sportstypeNames[(int) $item->sportstype_id] ?? null;
+        }
 
-			if ($sportstype)
-			{
-				$item->sportstype = $sportstype->name;
-			}
-			else
-			{
-				$item->sportstype = null;
-			}
-		}
+        if (!$this->items) {
+            $databaseTool = $factory->createModel('Databasetool', 'Administrator');
 
-		if (count($this->items) == 0)
-		{
-			$databasetool    = BaseDatabaseModel::getInstance("databasetool", "sportsmanagementModel");
-			$insert_agegroup = $databasetool->insertAgegroup($this->state->get('filter.search_nation'), $this->state->get('filter.sports_type'));
-			$this->app->enqueueMessage(Text::_('COM_SPORTSMANAGEMENT_ADMIN_AGEGROUPS_NO_RESULT'), 'Error');
-		}
+            if ($databaseTool) {
+                $databaseTool->insertAgegroup(
+                    $this->state->get('filter.search_nation'),
+                    $this->state->get('filter.sports_type')
+                );
+            }
 
-		$this->lists = $lists;
+            $this->app->enqueueMessage(Text::_('COM_SPORTSMANAGEMENT_ADMIN_AGEGROUPS_NO_RESULT'), 'error');
+        }
 
-	}
+        $this->lists = $lists;
+    }
 
-	/**
-	 * Add the page title and toolbar.
-	 *
-	 * @since 1.7
-	 */
-	protected function addToolbar()
-	{
-		$this->title = Text::_('COM_SPORTSMANAGEMENT_ADMIN_AGEGROUPS_TITLE');
-		ToolbarHelper::addNew('agegroup.add');
-		ToolbarHelper::editList('agegroup.edit');
-		ToolbarHelper::apply('agegroups.saveshort');
-		ToolbarHelper::custom('agegroups.import', 'upload', 'upload', Text::_('JTOOLBAR_UPLOAD'), false);
-		ToolbarHelper::archiveList('agegroup.export', Text::_('JTOOLBAR_EXPORT'));
-		parent::addToolbar();
-	}
+    protected function addToolbar()
+    {
+        $this->title = Text::_('COM_SPORTSMANAGEMENT_ADMIN_AGEGROUPS_TITLE');
+        ToolbarHelper::addNew('agegroup.add');
+        ToolbarHelper::editList('agegroup.edit');
+        ToolbarHelper::apply('agegroups.saveshort');
+        ToolbarHelper::custom('agegroups.import', 'upload', 'upload', Text::_('JTOOLBAR_UPLOAD'), false);
+        ToolbarHelper::archiveList('agegroup.export', Text::_('JTOOLBAR_EXPORT'));
+        parent::addToolbar();
+    }
 }
