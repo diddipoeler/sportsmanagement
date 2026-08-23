40d9aba29fe3fae0c63df26f52e28d84dbcf96e2
diddipoeler <diddipoeler@gmx.de>
2026-08-23 19:11:55 +0200

    Bridge legacy projects layout to native tmpl

--- admin/views/projects/tmpl/default.php
+++ admin/views/projects/tmpl/default.php
@@ -1,23 +1,5 @@
 <?php
-/** Main administrator projects list layout. */
-defined('_JEXEC') or die('Restricted access');
+/** Legacy compatibility bridge for the native administrator projects layout. */
+\defined('_JEXEC') or die;
 
-use Joomla\CMS\HTML\HTMLHelper;
-
-$templatesToLoad = ['footer', 'listheader'];
-sportsmanagementHelper::addTemplatePaths($templatesToLoad, $this);
-?>
-<form action="<?php echo $this->request_url; ?>" method="post" id="adminForm" name="adminForm">
-    <?php if ($this->filterForm) : ?>
-        <?php echo HTMLHelper::_('searchtools.default', ['view' => $this]); ?>
-    <?php endif; ?>
-
-    <?php echo $this->loadTemplate('data'); ?>
-
-    <input type="hidden" name="task" value="" />
-    <input type="hidden" name="boxchecked" value="0" />
-    <input type="hidden" name="filter_order" value="<?php echo htmlspecialchars($this->sortColumn, ENT_QUOTES, 'UTF-8'); ?>" />
-    <input type="hidden" name="filter_order_Dir" value="<?php echo htmlspecialchars($this->sortDirection, ENT_QUOTES, 'UTF-8'); ?>" />
-    <?php echo HTMLHelper::_('form.token'); ?>
-</form>
-<?php echo $this->loadTemplate('footer'); ?>
+require JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/tmpl/projects/default.php';
