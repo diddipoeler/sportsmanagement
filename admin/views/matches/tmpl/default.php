dad2030eb800262f548b68ec41f827ab258847cf
diddipoeler <diddipoeler@gmx.de>
2026-08-23 20:00:23 +0200

    Bridge legacy matches layout to native tmpl

--- admin/views/matches/tmpl/default.php
+++ admin/views/matches/tmpl/default.php
@@ -1,31 +1,3 @@
 <?php
-defined('_JEXEC') or die('Restricted access');
-
-use Joomla\CMS\Component\ComponentHelper;
-
-$massadd = $this->app->getInput()->getInt('massadd', 0);
-$templatesToLoad = ['footer', 'listheader'];
-sportsmanagementHelper::addTemplatePaths($templatesToLoad, $this);
-
-if (COM_SPORTSMANAGEMENT_SHOW_DEBUG_INFO) {
-    echo $this->loadTemplate('debug');
-}
-?>
-<div id="alt_decision_enter" style="display:<?php echo $massadd === 0 ? 'none' : 'block'; ?>">
-    <?php echo $this->loadTemplate('massadd'); ?>
-</div>
-<?php
-switch ((string) ($this->projectws->sports_type_name ?? '')) {
-    case 'COM_SPORTSMANAGEMENT_ST_SMALL_BORE_RIFLE_ASSOCIATION':
-        echo $this->loadTemplate('matches_small_bore_rifle');
-        break;
-
-    default:
-        echo $this->loadTemplate('matches');
-        if (ComponentHelper::getParams($this->option)->get('show_edit_matches_matrix')) {
-            echo $this->loadTemplate('matrix');
-        }
-        break;
-}
-
-echo $this->loadTemplate('footer');
+\defined('_JEXEC') or die;
+require JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/tmpl/matches/default.php';
