b6d389c04922e027df2ee7de2b09198b2cd42ac0
diddipoeler <diddipoeler@gmx.de>
2026-08-23 16:54:08 +0200

    Modernize site image selector template

--- site/views/imagehandler/tmpl/default.php
+++ site/views/imagehandler/tmpl/default.php
@@ -1,66 +1,42 @@
 <?php
-/**
- *
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage imagehandler
- * @file       default.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-
+/** Joomla 5/6 SportsManagement frontend image selector. */
 defined('_JEXEC') or die('Restricted access');
 
-use Joomla\CMS\Language\Text;
 use Joomla\CMS\HTML\HTMLHelper;
-
-/**
- *
- * welche joomla version
- */
-if (version_compare(JVERSION, '3.0.0', 'ge'))
-{
-	HTMLHelper::_('behavior.framework', true);
-}
-else
-{
-	HTMLHelper::_('behavior.mootools');
-}
-
+use Joomla\CMS\Language\Text;
 ?>
-<form action="<?php echo $this->request_url; ?>" method="post" id="adminForm" name="adminForm">
+<form action="<?php echo $this->escape($this->request_url); ?>" method="post" id="adminForm" name="adminForm">
     <div class="imghead">
-
-		<?php echo Text::_('JSEARCH_FILTER_LABEL') . ' '; ?>
-        <input type="text" name="search" id="search" value="<?php echo $this->search; ?>" class="text_area"
-               onChange="document.getElementById('adminForm').submit();"/>
-        <button onclick="this.form.submit();"><?php echo Text::_('JSEARCH_FILTER_SUBMIT'); ?></button>
-        <button onclick="this.form.getElementById('search').value='';this.form.submit();"><?php echo Text::_('JSEARCH_FILTER_CLEAR'); ?></button>
-
+        <?php echo Text::_('JSEARCH_FILTER_LABEL') . ' '; ?>
+        <input type="text"
+               name="search"
+               id="search"
+               value="<?php echo $this->escape($this->search); ?>"
+               class="text_area"
+               onchange="document.getElementById('adminForm').submit();">
+        <button type="submit"><?php echo Text::_('JSEARCH_FILTER_SUBMIT'); ?></button>
+        <button type="submit" onclick="document.getElementById('search').value='';">
+            <?php echo Text::_('JSEARCH_FILTER_CLEAR'); ?>
+        </button>
     </div>
 
     <div class="imglist">
-
-		<?php
-		for ($i = 0, $n = count($this->images); $i < $n; $i++)
-			:
-			$this->setImage($i);
-			echo $this->loadTemplate('image');
-		endfor;
-		?>
+        <?php foreach (array_keys($this->images) as $index) : ?>
+            <?php $this->setImage($index); ?>
+            <?php echo $this->loadTemplate('image'); ?>
+        <?php endforeach; ?>
     </div>
 
     <div class="clr"></div>
 
-    <div class="pnav"><?php echo $this->pageNav->getListFooter(); ?></div>
+    <?php if ($this->pageNav !== null) : ?>
+        <div class="pnav"><?php echo $this->pageNav->getListFooter(); ?></div>
+    <?php endif; ?>
 
-    <input type="hidden" name="option" value="com_sportsmanagement"/>
-    <input type="hidden" name="view" value="imagehandler"/>
-    <input type="hidden" name="tmpl" value="component"/>
-    <input type="hidden" name="task" value="imagehandler.select"/>
-    <input type="hidden" name="folder" value="<?php echo $this->folder; ?>"/>
-	<?php echo HTMLHelper::_('form.token'); ?>
+    <input type="hidden" name="option" value="com_sportsmanagement">
+    <input type="hidden" name="view" value="imagehandler">
+    <input type="hidden" name="tmpl" value="component">
+    <input type="hidden" name="task" value="display">
+    <input type="hidden" name="folder" value="<?php echo $this->escape($this->folder); ?>">
+    <?php echo HTMLHelper::_('form.token'); ?>
 </form>
