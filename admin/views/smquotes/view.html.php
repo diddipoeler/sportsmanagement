9af3680ab71ecc805f203abcb4f1ad7c785b6189
diddipoeler <diddipoeler@gmx.de>
2026-08-23 15:03:39 +0200

    Use native quote table in admin view

--- admin/views/smquotes/view.html.php
+++ admin/views/smquotes/view.html.php
@@ -1,86 +1,43 @@
 <?php
 /**
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage smquotes
- * @file       view.html.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * SportsManagement administrator quotes list view.
  */
 defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\Language\Text;
-use Joomla\CMS\Table\Table;
-use Joomla\CMS\Toolbar\ToolbarHelper;
-use Joomla\CMS\Toolbar\Toolbar;
-use Joomla\CMS\Form\Form;
-use Joomla\CMS\Log\Log;
 
-/**
- * sportsmanagementViewsmquotes
- *
- * @package
- * @author
- * @copyright diddi
- * @version   2014
- * @access    public
- */
+use Diddipoeler\Component\SportsManagement\Administrator\Table\SmquoteTable;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Log\Log;
+use Joomla\CMS\Toolbar\Toolbar;
+use Joomla\CMS\Toolbar\ToolbarHelper;
+
 class sportsmanagementViewsmquotes extends sportsmanagementView
 {
-	/**
-	 * A \JForm instance with filter fields.
-	 *
-	 * @var    \JForm
-	 * @since  3.6.3
-	 */
-	public $filterForm;
+    public $filterForm;
+    public $activeFilters;
 
-	/**
-	 * An array with active filters.
-	 *
-	 * @var    array
-	 * @since  3.6.3
-	 */
-	public $activeFilters;
+    public function init()
+    {
+        $this->table = new SmquoteTable($this->model->getDatabase());
 
-	/**
-	 * sportsmanagementViewsmquotes::init()
-	 *
-	 * @return void
-	 */
-	public function init()
-	{
-		$this->table = Table::getInstance('smquote', 'sportsmanagementTable');
-		
-try
-{		
-$this->filterForm    = $this->model->getFilterForm();
-$this->activeFilters = $this->model->getActiveFilters();
-}
-catch (Exception $e)
-{
-Log::add(Text::_(__METHOD__ . ' ' . __LINE__ . ' ' . $e->getCode()), Log::ERROR, 'jsmerror');
-Log::add(Text::_(__METHOD__ . ' ' . __LINE__ . ' ' . $e->getMessage()), Log::ERROR, 'jsmerror');	
-}	
-		
-	}
+        try {
+            $this->filterForm = $this->model->getFilterForm();
+            $this->activeFilters = $this->model->getActiveFilters();
+        } catch (\Throwable $e) {
+            Log::add(__METHOD__ . ' ' . $e->getCode(), Log::ERROR, 'jsmerror');
+            Log::add(__METHOD__ . ' ' . $e->getMessage(), Log::ERROR, 'jsmerror');
+        }
+    }
 
-	/**
-	 * Add the page title and toolbar.
-	 *
-	 * @since 1.7
-	 */
-	protected function addToolbar()
-	{
-		$this->title = Text::_('COM_SPORTSMANAGEMENT_ADMIN_QUOTES_TITLE');
-		ToolbarHelper::addNew('smquote.add');
-		ToolbarHelper::editList('smquote.edit');
-		ToolbarHelper::custom('smquote.import', 'upload', 'upload', Text::_('JTOOLBAR_UPLOAD'), false);
-		ToolbarHelper::custom('smquotes.edittxt', 'featured.png', 'featured_f2.png', Text::_('JTOOLBAR_EDIT'), false);
-		$bar = Toolbar::getInstance('toolbar');
-		$bar->appendButton('Link', 'info', 'Kategorie', 'index.php?option=com_categories&extension=com_sportsmanagement');
-		ToolbarHelper::archiveList('smquote.export', Text::_('JTOOLBAR_EXPORT'));
-		parent::addToolbar();
-	}
+    protected function addToolbar()
+    {
+        $this->title = Text::_('COM_SPORTSMANAGEMENT_ADMIN_QUOTES_TITLE');
+        ToolbarHelper::addNew('smquote.add');
+        ToolbarHelper::editList('smquote.edit');
+        ToolbarHelper::custom('smquote.import', 'upload', 'upload', Text::_('JTOOLBAR_UPLOAD'), false);
+        ToolbarHelper::custom('smquotes.edittxt', 'featured.png', 'featured_f2.png', Text::_('JTOOLBAR_EDIT'), false);
+        $bar = Toolbar::getInstance('toolbar');
+        $bar->appendButton('Link', 'info', 'Kategorie', 'index.php?option=com_categories&extension=com_sportsmanagement');
+        ToolbarHelper::archiveList('smquote.export', Text::_('JTOOLBAR_EXPORT'));
+        parent::addToolbar();
+    }
 }
