f6039e8eb4263ba50b191a0131727e9bf73a6a8c
diddipoeler <diddipoeler@gmx.de>
2026-08-23 09:10:03 +0200

    Persist club extended form data

--- admin/src/Controller/ClubController.php
+++ admin/src/Controller/ClubController.php
@@ -10,9 +10,25 @@ namespace Diddipoeler\Component\SportsManagement\Administrator\Controller;
 
 \defined('_JEXEC') or die;
 
+use Joomla\Registry\Registry;
+
 /**
  * Controller for editing a club.
  */
 class ClubController extends SportsManagementFormController
 {
+    public function save($key = null, $urlVar = null)
+    {
+        $data = $this->input->post->get('jform', [], 'array');
+        $extended = $this->input->post->get('extended', null, 'array');
+
+        if (is_array($extended)) {
+            $registry = new Registry();
+            $registry->loadArray($extended);
+            $data['extended'] = $registry->toString();
+            $this->input->post->set('jform', $data);
+        }
+
+        return parent::save($key, $urlVar);
+    }
 }
