40305c27e7b022fabe223f51f6c52b0ba39651c9
diddipoeler <diddipoeler@gmx.de>
2026-08-23 14:59:15 +0200

    Use native sports-type table in admin view

--- admin/views/sportstypes/view.html.php
+++ admin/views/sportstypes/view.html.php
@@ -1,63 +1,34 @@
 <?php
 /**
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage sportstypes
- * @file       view.html.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * SportsManagement administrator sports-types list view.
  */
 defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\Language\Text;
+
+use Diddipoeler\Component\SportsManagement\Administrator\Table\SportstypeTable;
 use Joomla\CMS\HTML\HTMLHelper;
-use Joomla\CMS\Table\Table;
+use Joomla\CMS\Language\Text;
 use Joomla\CMS\Toolbar\ToolbarHelper;
 
-/**
- * sportsmanagementViewSportsTypes
- *
- * @package
- * @author
- * @copyright diddi
- * @version   2014
- * @access    public
- */
 class sportsmanagementViewSportsTypes extends sportsmanagementView
 {
+    public function init()
+    {
+        $options = [
+            HTMLHelper::_('select.option', '0', Text::_('COM_SPORTSMANAGEMENT_ADMIN_SPORTSART_TEAM')),
+            HTMLHelper::_('select.option', '1', Text::_('COM_SPORTSMANAGEMENT_ADMIN_SPORTSART_SINGLE')),
+        ];
 
-	/**
-	 * sportsmanagementViewSportsTypes::init()
-	 *
-	 * @return void
-	 */
-	public function init()
-	{
+        $this->table = new SportstypeTable($this->model->getDatabase());
+        $this->lists = ['sportart' => $options];
+    }
 
-		$myoptions   = array();
-		$myoptions[] = HTMLHelper::_('select.option', '0', Text::_('COM_SPORTSMANAGEMENT_ADMIN_SPORTSART_TEAM'));
-		$myoptions[] = HTMLHelper::_('select.option', '1', Text::_('COM_SPORTSMANAGEMENT_ADMIN_SPORTSART_SINGLE'));
-
-		$this->table = Table::getInstance('sportstype', 'sportsmanagementTable');
-
-		$lists['sportart'] = $myoptions;
-		$this->lists       = $lists;
-
-	}
-
-	/**
-	 * Add the page title and toolbar.
-	 *
-	 * @since 1.7
-	 */
-	protected function addToolbar()
-	{
-		$this->title = Text::_('COM_SPORTSMANAGEMENT_ADMIN_SPORTSTYPES_TITLE');
-		ToolbarHelper::addNew('sportstype.add');
-		ToolbarHelper::editList('sportstype.edit');
-		ToolbarHelper::custom('sportstype.import', 'upload', 'upload', Text::_('JTOOLBAR_UPLOAD'), false);
-		ToolbarHelper::archiveList('sportstype.export', Text::_('JTOOLBAR_EXPORT'));
-		parent::addToolbar();
-	}
+    protected function addToolbar()
+    {
+        $this->title = Text::_('COM_SPORTSMANAGEMENT_ADMIN_SPORTSTYPES_TITLE');
+        ToolbarHelper::addNew('sportstype.add');
+        ToolbarHelper::editList('sportstype.edit');
+        ToolbarHelper::custom('sportstype.import', 'upload', 'upload', Text::_('JTOOLBAR_UPLOAD'), false);
+        ToolbarHelper::archiveList('sportstype.export', Text::_('JTOOLBAR_EXPORT'));
+        parent::addToolbar();
+    }
 }
