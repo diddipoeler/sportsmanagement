720cd9a9a7586b7bf5e55e7191043f30f752886e
diddipoeler <diddipoeler@gmx.de>
2026-08-23 16:46:17 +0200

    Harden UEFA coefficient template output

--- site/views/uefawertung/tmpl/default.php
+++ site/views/uefawertung/tmpl/default.php
@@ -1,146 +1,90 @@
 <?php
 /**
  * SportsManagement ein Programm zur Verwaltung für Sportarten
+ *
  * @version    1.0.05
  * @package    Sportsmanagement
  * @subpackage uefawertung
- * @file       default.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 defined('_JEXEC') or die;
-$templatesToLoad = array('globalviews');
+
+$templatesToLoad = ['globalviews'];
 sportsmanagementHelper::addTemplatePaths($templatesToLoad, $this);
-
 ?>
-<div class="<?php echo $this->divclasscontainer; ?>" id="uefawertung">
-<form name="adminForm" id="adminForm" action="<?php echo htmlspecialchars($this->uri->toString()); ?>" method="post">
-    <div class="<?php echo $this->divclassrow; ?> table-responsive" id="uefawertunganzeige">
-		<?php
-		if ($this->config['show_sectionheader'] == 1)
-		{
-			echo $this->loadTemplate('sectionheader');
-		}
-        
-        
-        
-$picture = '/images/com_sportsmanagement/database/placeholders/uefa5jahreswertung.jpg';
-			if (empty($picture))
-			{
-				$picture = sportsmanagementHelper::getDefaultPlaceholder("team");
-			}
-			echo sportsmanagementHelperHtml::getBootstrapModalImage(
-				'uefa' . '5',
-				$picture,
-				'UEFA 5-Jahreswertung',
-				'400',
-				'',
-				$this->modalwidth,
-				$this->modalheight,
-				$this->overallconfig['use_jquery_modal']
-			);
-?>
-<?php
+<div class="<?php echo htmlspecialchars((string) $this->divclasscontainer, ENT_QUOTES, 'UTF-8'); ?>" id="uefawertung">
+    <form name="adminForm" id="adminForm" action="<?php echo htmlspecialchars($this->uri->toString(), ENT_QUOTES, 'UTF-8'); ?>" method="post">
+        <div class="<?php echo htmlspecialchars((string) $this->divclassrow, ENT_QUOTES, 'UTF-8'); ?> table-responsive" id="uefawertunganzeige">
+            <?php
+            if ((int) ($this->config['show_sectionheader'] ?? 0) === 1) {
+                echo $this->loadTemplate('sectionheader');
+            }
 
-echo $this->lists['coefficientyears'];
-?>
+            $picture = '/images/com_sportsmanagement/database/placeholders/uefa5jahreswertung.jpg';
+            echo sportsmanagementHelperHtml::getBootstrapModalImage(
+                'uefa5',
+                $picture,
+                'UEFA 5-Jahreswertung',
+                '400',
+                '',
+                $this->modalwidth,
+                $this->modalheight,
+                $this->overallconfig['use_jquery_modal'] ?? 0
+            );
 
- <table class="table">
-        <thead>
-  <tr>
-  <td>
-  </td>
-<?php
-  foreach( $this->seasonnames as $key => $value )
-{
-    ?>
-    <td>
-<?php
-echo $value;
-?>
-</td>  
-      
-      <?php
-  }
-  
-  ?>
-     <td>
-  </td>
-    </tr>  
-        </thead>
-		<?php
-foreach( $this->uefapoints as $key => $value )
-{
-?>    
-<tr>
-<td>
-<?php
-$country = substr($value->team, 1, 3);	
-	
-switch ( $country )
-{
-	case 'GER':
-    case 'HOL':
-    case 'POR':
-    case 'SUI':
-    case 'GRE':
-    case 'CRO':
-    case 'DEN':
-    case 'BUL':
-    case 'SLO':
-    case 'KOS':
-    case 'LAT':
-    case 'FAR':
-		$country = JSMCountries::getCountryalpha3fifa($country);;
-		break;
-	default:
-		break;
-}
-echo JSMCountries::getCountryFlag($country);
-echo $value->team;
-?>
-</td>
+            echo $this->lists['coefficientyears'];
+            ?>
 
-  
- <?php
-  foreach( $this->seasonnames as $key1 => $value1 )
-{
-    ?>
-    <td>
-<?php
-echo $value->$value1;
-?>
-</td>  
-      
-      <?php
-  }
-  
-  ?> 
-  
-  
-  
-  
-  
-<td>
-<?php
-echo $value->total;
-?>
-</td>
-</tr>  
-  
-<?php    
-}
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col"></th>
+                        <?php foreach ($this->seasonnames as $season) : ?>
+                            <th scope="col"><?php echo htmlspecialchars((string) $season, ENT_QUOTES, 'UTF-8'); ?></th>
+                        <?php endforeach; ?>
+                        <th scope="col"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($this->uefapoints as $row) : ?>
+                        <tr>
+                            <td>
+                                <?php
+                                $country = substr((string) ($row->team ?? ''), 1, 3);
 
-		?>
-    </table>
+                                switch ($country) {
+                                    case 'GER':
+                                    case 'HOL':
+                                    case 'POR':
+                                    case 'SUI':
+                                    case 'GRE':
+                                    case 'CRO':
+                                    case 'DEN':
+                                    case 'BUL':
+                                    case 'SLO':
+                                    case 'KOS':
+                                    case 'LAT':
+                                    case 'FAR':
+                                        $country = JSMCountries::getCountryalpha3fifa($country);
+                                        break;
+                                }
 
+                                echo JSMCountries::getCountryFlag($country);
+                                echo htmlspecialchars((string) ($row->team ?? ''), ENT_QUOTES, 'UTF-8');
+                                ?>
+                            </td>
 
+                            <?php foreach ($this->seasonnames as $season) : ?>
+                                <td><?php echo htmlspecialchars((string) ($row->{$season} ?? 0), ENT_QUOTES, 'UTF-8'); ?></td>
+                            <?php endforeach; ?>
 
+                            <td><?php echo htmlspecialchars((string) ($row->total ?? 0), ENT_QUOTES, 'UTF-8'); ?></td>
+                        </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
 
-<?php
-		echo $this->loadTemplate('jsminfo');
-		?>
-    </div>
+            <?php echo $this->loadTemplate('jsminfo'); ?>
+        </div>
     </form>
 </div>
