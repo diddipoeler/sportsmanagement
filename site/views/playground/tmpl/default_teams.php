2756a8441c9dd0894c2c605e6ddc4e2fb5c2f792
diddipoeler <diddipoeler@gmx.de>
2026-08-23 09:43:09 +0200

    Use native routes in playground teams

--- site/views/playground/tmpl/default_teams.php
+++ site/views/playground/tmpl/default_teams.php
@@ -10,42 +10,52 @@
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\Language\Text;
+
+use Diddipoeler\Component\SportsManagement\Site\Helper\SiteRouteHelper;
 use Joomla\CMS\HTML\HTMLHelper;
-use Joomla\CMS\Factory;
+use Joomla\CMS\Language\Text;
 
-?>
+$escape = static fn ($value): string => htmlspecialchars((string) $value, ENT_QUOTES, 'UTF-8');
 
-<?php  
-$this->notes = array();
-$this->notes[] = Text::_('COM_SPORTSMANAGEMENT_PLAYGROUND_CLUB_TEAMS');
+$this->notes = [Text::_('COM_SPORTSMANAGEMENT_PLAYGROUND_CLUB_TEAMS')];
 echo $this->loadTemplate('jsm_notes');
-?>   
-<!-- Now show teams of this club -->
+?>
 <div class="<?php echo $this->divclassrow; ?> table-responsive" id="playground_teams">
-	<?php
-	foreach ((array) $this->teams AS $key => $value)
-		:
-		?>
-		<?php $projectname                        = $value->project; ?>
-		<?php
-		foreach ($value->project_team AS $team)
-			:
-			$teaminfo = $value->teaminfo[0][0];
-			$routeparameter                       = array();
-			$routeparameter['cfg_which_database'] = Factory::getApplication()->input->getInt('cfg_which_database', 0);
-			$routeparameter['s']                  = Factory::getApplication()->input->getInt('s', 0);
-			$routeparameter['p']                  = $team->project_slug;
-			$routeparameter['tid']                = $teaminfo->team_slug;
-			$routeparameter['ptid']               = 0;
-			$link                                 = sportsmanagementHelperRoute::getSportsmanagementRoute('teaminfo', $routeparameter);
-			?>
-            <h4><?php echo $projectname . " - " . HTMLHelper::link($link, $teaminfo->name) . ($teaminfo->short_name ? " (" . $teaminfo->short_name . ")" : ''); ?></h4>
+    <?php foreach ($this->teams as $value) : ?>
+        <?php
+        $projectName = (string) ($value->project ?? '');
+        $teamInfo = $value->teaminfo[0][0] ?? null;
+
+        if (!$teamInfo) {
+            continue;
+        }
+        ?>
+
+        <?php foreach ((array) ($value->project_team ?? []) as $team) : ?>
+            <?php
+            $link = SiteRouteHelper::view('teaminfo', [
+                'cfg_which_database' => $this->input->getInt('cfg_which_database', 0),
+                's' => $this->input->getInt('s', 0),
+                'p' => (string) ($team->project_slug ?? ''),
+                'tid' => (string) ($teamInfo->team_slug ?? ''),
+                'ptid' => 0,
+            ]);
+            $teamName = $escape($teamInfo->name ?? '');
+            $shortName = trim((string) ($teamInfo->short_name ?? ''));
+            ?>
+            <h4>
+                <?php echo $escape($projectName); ?> - <?php echo HTMLHelper::link($link, $teamName); ?><?php echo $shortName !== '' ? ' (' . $escape($shortName) . ')' : ''; ?>
+            </h4>
             <div class="clubteaminfo">
-				<?php
-				$description = $teaminfo->notes;
-				echo(!empty($description) ? Text:: _('COM_SPORTSMANAGEMENT_PLAYGROUND_TEAMINFO') . " " . HTMLHelper::_('content.prepare', $description) : ''); ?>
+                <?php
+                $description = (string) ($teamInfo->notes ?? '');
+
+                if ($description !== '') {
+                    echo Text::_('COM_SPORTSMANAGEMENT_PLAYGROUND_TEAMINFO')
+                        . ' ' . HTMLHelper::_('content.prepare', $description);
+                }
+                ?>
             </div>
-		<?php endforeach; ?>
-	<?php endforeach; ?>
+        <?php endforeach; ?>
+    <?php endforeach; ?>
 </div>
