e2398a1766f309070f3073eb192a12ebecfe0962
diddipoeler <diddipoeler@gmx.de>
2026-08-23 20:02:53 +0200

    Bridge legacy matches team selector to native tmpl

--- admin/views/matches/tmpl/default_teamselect.php
+++ admin/views/matches/tmpl/default_teamselect.php
@@ -1,22 +1,3 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage matches
- * @file       default_teamselect.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\HTML\HTMLHelper;
-
-?>
-<form name="projectteamForm" id="projectteamForm" method="post">
-<?php
-if (array_key_exists('projectteams', $this->lists)) {
-echo "" . HTMLHelper::_('select.genericlist', $this->lists['projectteams'], 'projectteam', 'class="inputbox" size="1" onchange="this.form.submit();" ', 'value', 'text', $this->projectteamsel) . "";
-}
-?>
-</form>
+\defined('_JEXEC') or die;
+require JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/tmpl/matches/default_teamselect.php';
