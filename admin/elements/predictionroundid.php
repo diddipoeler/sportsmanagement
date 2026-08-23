11e5d697bd4c3f70af5bd423fd64a65832a0ccdb
diddipoeler <diddipoeler@gmx.de>
2026-08-23 12:46:23 +0200

    Bridge legacy prediction round field to Joomla 5/6 implementation

--- admin/elements/predictionroundid.php
+++ admin/elements/predictionroundid.php
@@ -1,81 +1,17 @@
 <?php
-/**
- *
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage elements
- * @file       predictionroundid.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
+/** Legacy compatibility bridge for the Joomla 5/6 prediction round field. */
+\defined('_JEXEC') or die;
 
-defined('_JEXEC') or die('Restricted access');
+use Diddipoeler\Component\SportsManagement\Administrator\Field\PredictionroundidField;
 
-use Joomla\CMS\Language\Text;
-use Joomla\CMS\Factory;
-use Joomla\CMS\HTML\HTMLHelper;
-use Joomla\CMS\Component\ComponentHelper;
-use Joomla\CMS\Form\FormField;
+if (!class_exists(PredictionroundidField::class)) {
+    $fieldFile = JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Field/PredictionroundidField.php';
 
-/**
- * JFormFieldpredictionroundid
- *
- * @package
- * @author
- * @copyright diddi
- * @version   2014
- * @access    public
- */
-class JFormFieldpredictionroundid extends FormField
-{
-	var $_name = 'predictionroundid';
-
-	/**
-	 * JFormFieldpredictionroundid::getInput()
-	 *
-	 * @return
-	 */
-	protected function getInput()
-	{
-		$db     = sportsmanagementHelper::getDBConnection();
-		$app    = Factory::getApplication();
-		$option = 'com_sportsmanagement';
-
-		$prediction_id = $app->getUserState("$option.prediction_id", '0');
-
-		// Welche tabelle soll genutzt werden
-		$params         = ComponentHelper::getParams('com_sportsmanagement');
-		$database_table = $params->get('cfg_which_database_table');
-
-		$query = $db->getQuery(true);
-		$query->select('r.id AS id,r.name as roundname');
-		$query->from('#__sportsmanagement_match AS m');
-		$query->join('INNER', '#__sportsmanagement_round AS r ON r.id = m.round_id');
-		$query->join('INNER', '#__sportsmanagement_prediction_project as prepro on prepro.project_id = r.project_id');
-		$query->where('prepro.prediction_id = ' . $prediction_id);
-		$query->group('r.id');
-
-		$db->setQuery($query);
-
-		$teams = $db->loadObjectList();
-
-		if (!$teams)
-		{
-		}
-
-		$mitems = array();
-
-		foreach ($teams as $team)
-		{
-			$mitems[] = HTMLHelper::_('select.option', $team->id, '&nbsp;' . ' ( ' . $team->roundname . ' ) ');
-		}
-
-		$output = HTMLHelper::_('select.genericlist', $mitems, $this->name . '[]', 'class="inputbox" multiple="multiple" size="' . count($mitems) . '"', 'value', 'text', $this->value, $this->id);
-
-		return $output;
-	}
+    if (is_file($fieldFile)) {
+        require_once $fieldFile;
+    }
 }
 
+if (class_exists(PredictionroundidField::class) && !class_exists('JFormFieldpredictionroundid', false)) {
+    class_alias(PredictionroundidField::class, 'JFormFieldpredictionroundid');
+}
