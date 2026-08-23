7fc2992504d894e2603f836038e2579e749d785b
diddipoeler <diddipoeler@gmx.de>
2026-08-23 09:46:03 +0200

    Use native logos in played playground matches

--- site/views/playground/tmpl/default_played_matches.php
+++ site/views/playground/tmpl/default_played_matches.php
@@ -10,112 +10,91 @@
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\Language\Text;
+
+use Diddipoeler\Component\SportsManagement\Site\Helper\TeamLogoHelper;
 use Joomla\CMS\HTML\HTMLHelper;
-use Joomla\CMS\Factory;
+use Joomla\CMS\Language\Text;
 
-?>
-
-<?php
-
-if ($this->playedgames)
-{
-	?>
-    <!-- Playground played games -->
-<?php  
-$this->notes = array();
-$this->notes[] = Text::_('COM_SPORTSMANAGEMENT_PLAYGROUND_PREV_GAMES');
-echo $this->loadTemplate('jsm_notes');
-?>       
-    <div class="<?php echo $this->divclassrow; ?> table-responsive" id="playground_played_matches">
-        <table class="<?php echo $this->config['matches_table_class']; ?>">
-			<?php
-			// Sort games by dates
-			$gamesByDate = Array();
-
-			foreach ($this->playedgames as $game)
-			{
-				$gamesByDate[substr($game->match_date, 0, 10)][] = $game;
-			}
-
-			$colspan = 5;
-
-			if ($this->config['show_logo'])
-			{
-				$colspan = 7;
-			}
-
-
-			foreach ($gamesByDate as $date => $games)
-			{
-				?>
-                <tr>
-                    <td align="left" colspan="<?php echo $colspan; ?>" class="">
-						<?php
-						echo HTMLHelper::date($date, Text::_('COM_SPORTSMANAGEMENT_GLOBAL_MATCHDAYDATE'));
-						?>
-                    </td>
-                </tr>
-				<?php
-				foreach ($games as $game)
-				{
-					$home = $this->playedgamesteams[$game->team1];
-					$away = $this->playedgamesteams[$game->team2];
-					?>
-                    <tr class="">
-                        <td>
-							<?php
-							echo substr($game->match_date, 11, 5);
-							?>
-                        </td>
-                        <td class="nowrap">
-							<?php
-							echo $game->project_name;
-							?>
-                        </td>
-						<?php
-						if ($this->config['show_logo'])
-						{
-							$home_logo = sportsmanagementModelteam::getTeamLogo($home->id, $this->config['show_logo_small']);
-							$away_logo = sportsmanagementModelteam::getTeamLogo($away->id, $this->config['show_logo_small']);
-							$teamA     = '<td align="right" valign="top" class="nowrap">';
-							$teamA     .= " " . sportsmanagementModelProject::getClubIconHtml($home_logo[0], 1, 0, 'logo_small', Factory::getApplication()->input->getInt('cfg_which_database', 0), 0, $this->modalwidth, $this->modalheight, $this->overallconfig['use_jquery_modal']);
-							$teamA     .= '</td>';
-							echo $teamA;
-						}
-						?>
-                        <td class="nowrap">
-							<?php
-							echo $home->name;
-							?>
-                        </td>
-                        <td class="nowrap">
-							<?php echo $game->team1_result; ?>
-                            -
-							<?php echo $game->team2_result; ?>
-                        </td>
-						<?php
-						if ($this->config['show_logo'])
-						{
-							$teamB = '<td align="right" valign="top" class="nowrap">';
-							$teamB .= " " . sportsmanagementModelProject::getClubIconHtml($away_logo[0], 1, 0, 'logo_small', Factory::getApplication()->input->getInt('cfg_which_database', 0), 0, $this->modalwidth, $this->modalheight, $this->overallconfig['use_jquery_modal']);
-							$teamB .= '</td>';
-							echo $teamB;
-						}
-						?>
-                        <td class="nowrap">
-							<?php
-							echo $away->name;
-							?>
-                        </td>
-                    </tr>
-					<?php
-				}
-			}
-			?>
-        </table>
-    </div>
-
-    <!-- End of playground played games -->
-	<?php
+if (!$this->playedgames) {
+    return;
 }
+
+$escape = static fn ($value): string => htmlspecialchars((string) $value, ENT_QUOTES, 'UTF-8');
+$showLogo = !empty($this->config['show_logo']);
+$preferSmall = !empty($this->config['show_logo_small']);
+$logoHeight = $preferSmall ? 20 : 50;
+$modalMode = (int) ($this->overallconfig['use_jquery_modal'] ?? 0);
+$gamesByDate = [];
+
+foreach ($this->playedgames as $game) {
+    $gamesByDate[substr((string) $game->match_date, 0, 10)][] = $game;
+}
+
+$this->notes = [Text::_('COM_SPORTSMANAGEMENT_PLAYGROUND_PREV_GAMES')];
+echo $this->loadTemplate('jsm_notes');
+?>
+<div class="<?php echo $this->divclassrow; ?> table-responsive" id="playground_played_matches">
+    <table class="<?php echo $escape($this->config['matches_table_class'] ?? 'table'); ?>">
+        <?php foreach ($gamesByDate as $date => $games) : ?>
+            <tr>
+                <td colspan="<?php echo $showLogo ? 7 : 5; ?>">
+                    <?php echo HTMLHelper::date($date, Text::_('COM_SPORTSMANAGEMENT_GLOBAL_MATCHDAYDATE')); ?>
+                </td>
+            </tr>
+
+            <?php foreach ($games as $game) : ?>
+                <?php
+                $home = $this->playedgamesteams[(int) ($game->team1 ?? 0)] ?? null;
+                $away = $this->playedgamesteams[(int) ($game->team2 ?? 0)] ?? null;
+
+                if (!$home || !$away) {
+                    continue;
+                }
+                ?>
+                <tr>
+                    <td><?php echo $escape(substr((string) $game->match_date, 11, 5)); ?></td>
+                    <td class="nowrap"><?php echo $escape($game->project_name ?? ''); ?></td>
+
+                    <?php if ($showLogo) : ?>
+                        <td class="nowrap text-end">
+                            <?php
+                            echo TeamLogoHelper::render(
+                                $home,
+                                'playground-played-' . (int) $game->id . '-home',
+                                $preferSmall,
+                                $logoHeight,
+                                $this->modalwidth,
+                                $this->modalheight,
+                                $modalMode
+                            );
+                            ?>
+                        </td>
+                    <?php endif; ?>
+
+                    <td class="nowrap"><?php echo $escape($home->name ?? ''); ?></td>
+                    <td class="nowrap">
+                        <?php echo $escape($game->team1_result ?? ''); ?> - <?php echo $escape($game->team2_result ?? ''); ?>
+                    </td>
+
+                    <?php if ($showLogo) : ?>
+                        <td class="nowrap text-end">
+                            <?php
+                            echo TeamLogoHelper::render(
+                                $away,
+                                'playground-played-' . (int) $game->id . '-away',
+                                $preferSmall,
+                                $logoHeight,
+                                $this->modalwidth,
+                                $this->modalheight,
+                                $modalMode
+                            );
+                            ?>
+                        </td>
+                    <?php endif; ?>
+
+                    <td class="nowrap"><?php echo $escape($away->name ?? ''); ?></td>
+                </tr>
+            <?php endforeach; ?>
+        <?php endforeach; ?>
+    </table>
+</div>
