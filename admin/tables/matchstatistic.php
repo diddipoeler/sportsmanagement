8c9f89083460baca6c58943dea9f06ae8869cc81
diddipoeler <diddipoeler@gmx.de>
2026-08-23 14:21:00 +0200

    Bridge match statistic table to native Joomla table

--- admin/tables/matchstatistic.php
+++ admin/tables/matchstatistic.php
@@ -1,57 +1,13 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage tables
- * @file       matchstatistic.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
 defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\Language\Text;
 
-/**
- * sportsmanagementTableMatchStatistic
- *
- * @package
- * @author
- * @copyright diddi
- * @version   2014
- * @access    public
- */
-class sportsmanagementTableMatchStatistic extends JSMTable
-{
-	/**
-	 * Constructor
-	 *
-	 * @param   object Database connector object
-	 *
-	 * @since 1.0
-	 */
-	function __construct(&$db)
-	{
-		$db = sportsmanagementHelper::getDBConnection();
-		parent::__construct('#__sportsmanagement_match_statistic', 'id', $db);
-	}
+use Diddipoeler\Component\SportsManagement\Administrator\Table\MatchstatisticTable;
 
-	/**
-	 * Overloaded check method to ensure data integrity
-	 *
-	 * @access public
-	 * @return boolean True on success
-	 * @since  1.0
-	 */
-	function check()
-	{
-		/*
-        if ( ! ( $this->statistic_id && $this->projectteam_id && $this->match_id) )
-        {
-         $this->setError( Text::_( 'CHECK FAILED' ) );
-         return false;
-        }
-        */
-		return true;
-	}
+if (!class_exists(MatchstatisticTable::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Table/SportsManagementTable.php';
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Table/MatchstatisticTable.php';
+}
+
+if (!class_exists('sportsmanagementTableMatchStatistic', false)) {
+    class_alias(MatchstatisticTable::class, 'sportsmanagementTableMatchStatistic');
 }
