e20f18f41b763ec5f883af500b6a7358707a224b
diddipoeler <diddipoeler@gmx.de>
2026-08-23 15:05:19 +0200

    Modernize divisions admin view factories

--- admin/views/divisions/view.html.php
+++ admin/views/divisions/view.html.php
@@ -1,87 +1,61 @@
 <?php
 /**
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage divisions
- * @file       view.html.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * SportsManagement administrator divisions list view.
  */
 defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\Table\Table;
-use Joomla\CMS\Language\Text;
-use Joomla\CMS\MVC\Model\BaseDatabaseModel;
-use Joomla\CMS\Toolbar\ToolbarHelper;
-use Joomla\CMS\Form\Form;
-use Joomla\CMS\Log\Log;
-use Joomla\CMS\Factory;
 
-/**
- * sportsmanagementViewDivisions
- *
- * @package
- * @author
- * @copyright diddi
- * @version   2014
- * @access    public
- */
+use Diddipoeler\Component\SportsManagement\Administrator\Table\DivisionTable;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Toolbar\ToolbarHelper;
+
 class sportsmanagementViewDivisions extends sportsmanagementView
 {
+    public function init()
+    {
+        $input = $this->app->getInput();
+        $projectId = (int) $this->app->getUserState("$this->option.pid", 0);
 
-	/**
-	 * sportsmanagementViewDivisions::init()
-	 *
-	 * @return void
-	 */
-	public function init()
-	{
-		$lists            = array();
-		$this->project_id = $this->app->getUserState("$this->option.pid", '0') ? $this->app->getUserState("$this->option.pid", '0') : Factory::getApplication()->input->get( "pid", '0' );
-		$mdlProject       = BaseDatabaseModel::getInstance("Project", "sportsmanagementModel");
-		$this->projectws  = $mdlProject->getProject($this->project_id);
-		$this->table      = Table::getInstance('division', 'sportsmanagementTable');
-		$this->lists      = $lists;
+        if ($projectId <= 0) {
+            $projectId = $input->getInt('pid');
+        }
 
-switch ($this->getLayout())
-		{
-            case 'massadd':
-			case 'massadd_3':
-			case 'massadd_4':
+        $this->project_id = $projectId;
+        $factory = $this->app->bootComponent('com_sportsmanagement')->getMVCFactory();
+        $projectModel = $factory->createModel('Project', 'Administrator');
+        $this->projectws = $projectModel ? $projectModel->getProject($projectId) : null;
+        $this->table = new DivisionTable($this->model->getDatabase());
+        $this->lists = [];
+
+        if (in_array($this->getLayout(), ['massadd', 'massadd_3', 'massadd_4'], true)) {
             $this->project = $this->projectws;
-		$this->setLayout('massadd');
-//			$this->_displayMassadd($tpl);
-			return;
-		}
+            $this->setLayout('massadd');
+        }
+    }
 
- 
-	}
+    protected function addToolbar()
+    {
+        $this->title = Text::_('COM_SPORTSMANAGEMENT_ADMIN_DIVS_TITLE');
+        ToolbarHelper::back(
+            'JPREV',
+            'index.php?option=com_sportsmanagement&view=project&layout=panel&id=' . $this->project_id
+        );
 
-	/**
-	 * Add the page title and toolbar.
-	 *
-	 * @since 1.7
-	 */
-	protected function addToolbar()
-	{
-		$this->title = Text::_('COM_SPORTSMANAGEMENT_ADMIN_DIVS_TITLE');
-        ToolbarHelper::back('JPREV', 'index.php?option=com_sportsmanagement&view=project&layout=panel&id='.$this->project_id);
+        if (($this->user->username ?? '') === 'admin') {
+            ToolbarHelper::publish('divisions.divisiontoproject', 'Division to Projekt', true);
+        }
 
-		if ($this->user->username == 'admin')
-		{
-			ToolbarHelper::publish('divisions.divisiontoproject', 'Division to Projekt', true);
-		}
-
-		ToolbarHelper::publish('divisions.publish', 'JTOOLBAR_PUBLISH', true);
-		ToolbarHelper::unpublish('divisions.unpublish', 'JTOOLBAR_UNPUBLISH', true);
-		ToolbarHelper::checkin('divisions.checkin');
-		ToolbarHelper::apply('divisions.saveshort');
-		ToolbarHelper::divider();
-        sportsmanagementHelper::ToolbarButton('massadd', 'new', Text::_('COM_SPORTSMANAGEMENT_ADMIN_DIVISIONS_MASSADD_BUTTON'));
-		ToolbarHelper::addNew('division.add');
-		ToolbarHelper::editList('division.edit');
-
-		parent::addToolbar();
-	}
+        ToolbarHelper::publish('divisions.publish', 'JTOOLBAR_PUBLISH', true);
+        ToolbarHelper::unpublish('divisions.unpublish', 'JTOOLBAR_UNPUBLISH', true);
+        ToolbarHelper::checkin('divisions.checkin');
+        ToolbarHelper::apply('divisions.saveshort');
+        ToolbarHelper::divider();
+        sportsmanagementHelper::ToolbarButton(
+            'massadd',
+            'new',
+            Text::_('COM_SPORTSMANAGEMENT_ADMIN_DIVISIONS_MASSADD_BUTTON')
+        );
+        ToolbarHelper::addNew('division.add');
+        ToolbarHelper::editList('division.edit');
+        parent::addToolbar();
+    }
 }
