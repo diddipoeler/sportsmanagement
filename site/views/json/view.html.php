cbae700408e4d26c024952414032497a5decd8a8
diddipoeler <diddipoeler@gmx.de>
2026-08-23 12:29:20 +0200

    Remove Joomla 3 bootstrap calls from legacy JSON view

--- site/views/json/view.html.php
+++ site/views/json/view.html.php
@@ -1,68 +1,44 @@
 <?php
 /**
- *
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage json
- * @file       view.html.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- * @package    mod_sportsmanagement_calendar
+ * Legacy JSON view kept for compatibility with older SportsManagement links.
  */
 
 defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\MVC\View\HtmlView;
 
 use Joomla\CMS\Factory;
 use Joomla\CMS\Log\Log;
-
-// Import Joomla view library
-jimport('joomla.application.component.view');
-
-// Import Joomla html for use with stylesheets
-jimport('joomla.html.html');
-
+use Joomla\CMS\MVC\View\HtmlView;
 
 class sportsmanagementViewjson extends HtmlView
 {
-	// Overwriting JViewLegacy display method
-	function display($tpl = null)
-	{
-		$app        = Factory::getApplication();
-		$params     = $app->getParams();
-		$dispatcher = JDispatcher::getInstance();
+    public function display($tpl = null)
+    {
+        // Keep the historical model access for this compatibility-only view,
+        // but do not bootstrap removed Joomla 3 view/HTML libraries.
+        $state = $this->get('State');
+        $this->form = $this->get('Form');
+        $this->state = $state;
 
-		// Get some data from the models
-		$state       = $this->get('State');
-		$item        = $this->get('Item');
-		$this->form  = $this->get('Form');
-		$this->state = $state;
+        if (count($errors = $this->get('Errors'))) {
+            Log::add(implode('<br />', $errors));
 
-		// Check for errors.
-		if (count($errors = $this->get('Errors')))
-		{
-			Log::add(implode('<br />', $errors));
+            return false;
+        }
 
-			return false;
-		}
+        $this->addDocStyle();
 
-		// Get the stylesheet and/or other document values
-		$this->addDocStyle();
+        return parent::display($tpl);
+    }
 
-		// Display the view
-		parent::display($tpl);
-
-	}
-
-	/**
-	 * Add the stylesheet to the document.
-	 */
-	protected function addDocStyle()
-	{
-		$doc = Factory::getDocument();
-		$doc->addStyleSheet('media/com_sportsmanagement/css/site.stylesheet.css');
-	}
+    protected function addDocStyle(): void
+    {
+        Factory::getApplication()
+            ->getDocument()
+            ->getWebAssetManager()
+            ->registerAndUseStyle(
+                'com_sportsmanagement.site',
+                'media/com_sportsmanagement/css/site.stylesheet.css',
+                ['version' => 'auto']
+            );
+    }
 }
