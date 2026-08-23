0abba21a9d932fb101fe5e1ccbfae0fcbbdb25c0
diddipoeler <diddipoeler@gmx.de>
2026-08-23 12:39:36 +0200

    Bridge legacy color picker to Joomla 5/6 field

--- admin/elements/colorpicker.php
+++ admin/elements/colorpicker.php
@@ -1,52 +1,17 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage elements
- * @file       colorpicker.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\Factory;
+/** Legacy compatibility bridge for the Joomla 5/6 color picker field. */
+\defined('_JEXEC') or die;
 
-/**
- * display a colorpicker element
- * example usage:
- *    <pre>
- *        <param type="colorpicker" name="color1" label="LABEL COLORPICKER 1" description="DESCR COLORPICKER" />
- *        <param type="colorpicker" name="color2" label="LABEL COLORPICKER 2" description="DESCR COLORPICKER" />
- *    </pre>
- *
- * @return string html code for the colorpicker
- * @since  1.5.1-daniela
- * @author Wolfgang Pinitsch <and_one@aon.at>
- */
-class JFormFieldColorpicker extends JFormField
-{
-	protected $type = 'colorpicker';
+use Diddipoeler\Component\SportsManagement\Administrator\Field\ColorpickerField;
 
-	/**
-	 * JFormFieldColorpicker::getInput()
-	 *
-	 * @return
-	 */
-	function getInput()
-	{
-		// Add javascript
-		// $method = (stripos($node->_attributes['name'] , 'JL_EXT') !== false)? 'extended' : 'params';
-		$document = Factory::getDocument();
-		$document->addStylesheet(JURI::base() . 'components/com_sportsmanagement/assets/js/js_color_picker_v2/js_color_picker_v2.css');
-		$document->addScript(JURI::base() . 'components/com_sportsmanagement/assets/js/js_color_picker_v2/color_functions.js');
-		$document->addScript(JURI::base() . 'components/com_sportsmanagement/assets/js/js_color_picker_v2/js_color_picker_v2.js');
-		$output = "<input type=\"text\" style=\"background-color: " . $this->value . "\"
-					name=\"" . $this->name . "\" value=\"" . $this->value . "\">";
-		$output .= "<input type=\"button\" value=\"#\"
-					onclick=\"showColorPicker(this, document.getElementsByName('" . $this->name . "')[0])\">";
+if (!class_exists(ColorpickerField::class)) {
+    $fieldFile = JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Field/ColorpickerField.php';
 
-		return $output;
-	}
+    if (is_file($fieldFile)) {
+        require_once $fieldFile;
+    }
 }
 
+if (class_exists(ColorpickerField::class) && !class_exists('JFormFieldColorpicker', false)) {
+    class_alias(ColorpickerField::class, 'JFormFieldColorpicker');
+}
