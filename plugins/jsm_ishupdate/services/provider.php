765f7595e886ca2177fa536e11132a1cf4d29b8f
diddipoeler <diddipoeler@gmx.de>
2026-08-23 18:59:59 +0200

    Fix ishupdate plugin provider application bootstrap

--- plugins/jsm_ishupdate/services/provider.php
+++ plugins/jsm_ishupdate/services/provider.php
@@ -2,8 +2,8 @@
 \defined('_JEXEC') or die;
 
 use Diddipoeler\Plugin\System\SportsmanagementIshupdate\Extension\SportsmanagementIshupdate;
-use Joomla\CMS\Application\CMSApplicationInterface;
 use Joomla\CMS\Extension\PluginInterface;
+use Joomla\CMS\Factory;
 use Joomla\CMS\Plugin\PluginHelper;
 use Joomla\Database\DatabaseInterface;
 use Joomla\DI\Container;
@@ -21,7 +21,7 @@ return new class () implements ServiceProviderInterface
                     $container->get(DispatcherInterface::class),
                     (array) PluginHelper::getPlugin('system', 'jsm_ishupdate')
                 );
-                $plugin->setApplication($container->get(CMSApplicationInterface::class));
+                $plugin->setApplication(Factory::getApplication());
                 $plugin->setDatabase($container->get(DatabaseInterface::class));
 
                 return $plugin;
