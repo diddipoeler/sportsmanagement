95301260d175bb4672d85b9952048c3577e7d322
diddipoeler <diddipoeler@gmx.de>
2026-08-23 09:51:28 +0200

    Use native club info template context

--- site/views/clubinfo/tmpl/default.php
+++ site/views/clubinfo/tmpl/default.php
@@ -8,129 +8,61 @@
  * @license   GNU General Public License version 2 or later; see LICENSE.txt
  */
 defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\Uri\Uri;
-use Joomla\CMS\Component\ComponentHelper;
 
-$templatesToLoad = array('globalviews');
-sportsmanagementHelper::addTemplatePaths($templatesToLoad, $this);
-/**
- * kml file laden
- */
-if ($this->mapconfig['map_kmlfile'])
-{
-	$this->kmlpath = Uri::root() . 'tmp' . DIRECTORY_SEPARATOR . $this->club->id . '-club.kml';
-	$this->kmlfile = $this->club->id . '-club.kml';
+if (!$this->club) {
+    return;
 }
 
-$params = ComponentHelper::getParams('com_sportsmanagement');
-
-if (version_compare(JVERSION, '4.0.0', 'ge') || $params->get('use_jsmgrid'))
-{
-	$container = 'container';
-}
-else
-{
-	$container = "container-fluid";
-}
-//<div vocab="http://schema.org/" typeof="SportsTeam">
+$container = 'container';
 ?>
-<div class="<?php echo $container ?>" id="clubinfo" >
-	<?php
-	if (COM_SPORTSMANAGEMENT_SHOW_DEBUG_INFO)
-	{
-		echo $this->loadTemplate('debug');
-	}
-	?>
+<div class="<?php echo $container; ?>" id="clubinfo">
+    <?php
+    if (COM_SPORTSMANAGEMENT_SHOW_DEBUG_INFO) {
+        echo $this->loadTemplate('debug');
+    }
 
-	<?PHP
-	echo $this->loadTemplate('projectheading');
-	?>
+    echo $this->loadTemplate('projectheading');
 
-	<?PHP
-	if ($this->config['show_sectionheader'])
-	{
-		?>
+    if (!empty($this->config['show_sectionheader'])) {
+        echo $this->loadTemplate('sectionheader');
+    }
 
-		<?PHP
-		echo $this->loadTemplate('sectionheader');
-		?>
+    echo $this->loadTemplate('clubinfo');
 
-		<?PHP
-	}
-	?>
-    <!-- <div class="row"> -->
-	<?PHP
-	echo $this->loadTemplate('clubinfo');
-	
-	?>
-    <!-- </div> -->
-	<?PHP
-	/**
-	 * diddipoeler
-	 * aufbau der templates als array
-	 */
-	$this->output = array();
+    $this->output = [];
 
-	if ($this->config['show_fusion'])
-	{
-	$this->output['COM_SPORTSMANAGEMENT_TABS_EXTRA_FUSION'] = 'fusion';
-	}
-	
-	if ($this->config['show_extra_fields'])
-	{
-		$this->output['COM_SPORTSMANAGEMENT_TABS_EXTRA_FIELDS'] = 'extrafields';
-	}
+    if (!empty($this->config['show_fusion'])) {
+        $this->output['COM_SPORTSMANAGEMENT_TABS_EXTRA_FUSION'] = 'fusion';
+    }
 
-	if ($this->config['show_extended'])
-	{
-		$this->output['COM_SPORTSMANAGEMENT_TABS_EXTENDED'] = 'extended';
-	}
+    if (!empty($this->config['show_extra_fields'])) {
+        $this->output['COM_SPORTSMANAGEMENT_TABS_EXTRA_FIELDS'] = 'extrafields';
+    }
 
-	if ($this->club->latitude && $this->club->longitude)
-	{
-		if ($this->config['show_maps'])
-		{
-			if ($this->club->latitude != '0.00000000' && $this->club->longitude != '0.00000000')
-			{
-				$this->output['COM_SPORTSMANAGEMENT_GMAP_DIRECTIONS'] = 'googlemap';
-			}
-		}
-	}
+    if (!empty($this->config['show_extended'])) {
+        $this->output['COM_SPORTSMANAGEMENT_TABS_EXTENDED'] = 'extended';
+    }
 
-	if ($this->config['show_teams_of_club'])
-	{
-		$this->output['COM_SPORTSMANAGEMENT_CLUBINFO_TEAMS'] = 'teams';
-	}
+    if (
+        !empty($this->config['show_maps'])
+        && !empty($this->club->latitude)
+        && !empty($this->club->longitude)
+        && (string) $this->club->latitude !== '0.00000000'
+        && (string) $this->club->longitude !== '0.00000000'
+    ) {
+        $this->output['COM_SPORTSMANAGEMENT_GMAP_DIRECTIONS'] = 'googlemap';
+    }
 
-	if ($this->config['show_club_rssfeed'])
-	{
-		if ($this->rssfeeditems)
-		{
-			if (version_compare(JVERSION, '4.0.0', 'ge'))
-			{
-				$this->output['COM_SPORTSMANAGEMENT_CLUBINFO_RSSFEED'] = 'rssfeed_4';
-			}
-			else
-			{
-				$this->output['COM_SPORTSMANAGEMENT_CLUBINFO_RSSFEED'] = 'rssfeed';
-			}
-		}
-	}
+    if (!empty($this->config['show_teams_of_club'])) {
+        $this->output['COM_SPORTSMANAGEMENT_CLUBINFO_TEAMS'] = 'teams';
+    }
 
-	/**
-	 * je nach einstellung der templates im backend, wird das template
-	 * aus dem verzeichnis globalviews geladen.
-	 * hat den vorteil, das weniger programmcode erzeugt wird.
-	 * no_tabs
-	 * show_tabs
-	 * show_slider
-	 */
-	$template = isset($this->config['show_clubinfo_tabs']) ? $this->config['show_clubinfo_tabs'] : 'no_tabs';
-	echo $this->loadTemplate($template);
+    if (!empty($this->config['show_club_rssfeed']) && $this->rssfeeditems) {
+        $this->output['COM_SPORTSMANAGEMENT_CLUBINFO_RSSFEED'] = 'rssfeed_4';
+    }
 
-	echo $this->loadTemplate('jsminfo');
-	?>
-
-    <!-- ende clubinfo -->
+    $template = (string) ($this->config['show_clubinfo_tabs'] ?? 'no_tabs');
+    echo $this->loadTemplate($template);
+    echo $this->loadTemplate('jsminfo');
+    ?>
 </div>
-
