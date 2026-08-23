477ae88a06adf58df0ebaa1f9069de5eeb5aeed6
diddipoeler <diddipoeler@gmx.de>
2026-08-23 15:12:12 +0200

    Bridge legacy GCalendar table to native class

--- admin/tables/jsmgcalendarap.php
+++ admin/tables/jsmgcalendarap.php
@@ -1,47 +1,14 @@
 <?php
-/**
- * @package        GCalendar Action Pack
- * @author         Digital Peak http://www.digital-peak.com
- * @copyright      Copyright (C) 2012 - 2013 Digital Peak. All rights reserved.
- * @license        http://www.gnu.org/licenses/gpl.html GNU/GPL
- */
+/** Legacy compatibility bridge for the native JsmgcalendarapTable. */
+defined('_JEXEC') or die('Restricted access');
 
-defined('_JEXEC') or die();
+use Diddipoeler\Component\SportsManagement\Administrator\Table\JsmgcalendarapTable;
 
-use Joomla\CMS\Table\Table;
-use Joomla\CMS\Access\Rules;
+if (!class_exists(JsmgcalendarapTable::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Table/SportsManagementTable.php';
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Table/JsmgcalendarapTable.php';
+}
 
-class  sportsmanagementTablejsmGCalendarAP extends JSMTable
-{
-
-	public function __construct(&$db)
-	{
-		parent::__construct('#__sportsmanagement_gcalendarap', 'id', $db);
-	}
-
-	public function bind($array, $ignore = '')
-	{
-		if (isset($array['rules']) && is_array($array['rules']))
-		{
-			$rules = new Rules($array['rules']);
-			$this->setRules($rules);
-		}
-
-		return parent::bind($array, $ignore);
-	}
-
-	protected function _getAssetName()
-	{
-		$k = $this->_tbl_key;
-
-		return 'com_gcalendarap.event.' . (int) $this->$k;
-	}
-
-	protected function _getAssetParentId($table = null, $id = null)
-	{
-		$asset = Table::getInstance('Asset');
-		$asset->loadByName('com_gcalendarap');
-
-		return $asset->id;
-	}
+if (class_exists(JsmgcalendarapTable::class) && !class_exists('sportsmanagementTablejsmGCalendarAP', false)) {
+    class_alias(JsmgcalendarapTable::class, 'sportsmanagementTablejsmGCalendarAP');
 }
