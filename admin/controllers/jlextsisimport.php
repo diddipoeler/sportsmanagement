c29db84eac5dbf11a8a58a249f1ba57abbbffbad
diddipoeler <diddipoeler@gmx.de>
2026-08-23 12:58:37 +0200

    Bridge legacy SIS importer controller to Joomla 5/6 implementation

--- admin/controllers/jlextsisimport.php
+++ admin/controllers/jlextsisimport.php
@@ -1,60 +1,13 @@
 <?php
-/**
- *
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage controllers
- * @file       jlextsisimport.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-
+/** Legacy compatibility bridge for the native SIS import controller. */
 defined('_JEXEC') or die('Restricted access');
 
-use Joomla\CMS\Language\Text;
-use Joomla\CMS\Factory;
-use Joomla\CMS\MVC\Controller\BaseController;
+use Diddipoeler\Component\SportsManagement\Administrator\Controller\JlextsisimportController;
 
-/**
- * sportsmanagementControllerjlextsisimport
- *
- * @package
- * @author
- * @copyright diddi
- * @version   2014
- * @access    public
- */
-class sportsmanagementControllerjlextsisimport extends BaseController
-{
-
-	/**
-	 * sportsmanagementControllerjlextsisimport::save()
-	 *
-	 * @return void
-	 */
-	function save()
-	{
-		$option   = Factory::getApplication()->input->getCmd('option');
-		$app      = Factory::getApplication();
-		$document = Factory::getDocument();
-
-		// Check for request forgeries
-		Factory::getApplication()->input->checkToken() or die('COM_SPORTSMANAGEMENT_GLOBAL_INVALID_TOKEN');
-		$msg = '';
-
-		// $app = Factory::getApplication();
-		$model = $this->getModel('jlextsisimport');
-		$post  = Factory::getApplication()->input->get('post');
-
-		$xml_file = $model->getData();
-		$link     = 'index.php?option=' . $option . '&view=jlxmlimports&task=jlxmlimport.edit';
-
-		$this->setRedirect($link, $msg);
-	}
-
-
+if (!class_exists(JlextsisimportController::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Controller/JlextsisimportController.php';
 }
 
+if (!class_exists('sportsmanagementControllerjlextsisimport', false)) {
+    class_alias(JlextsisimportController::class, 'sportsmanagementControllerjlextsisimport');
+}
