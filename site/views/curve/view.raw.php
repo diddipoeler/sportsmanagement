6a9492dd88025bd8922f7eba2bdee25da25b033c
diddipoeler <diddipoeler@gmx.de>
2026-08-23 09:37:51 +0200

    Bridge curve raw view to Joomla 5/6

--- site/views/curve/view.raw.php
+++ site/views/curve/view.raw.php
@@ -1,96 +1,13 @@
 <?php
-/**
- *
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage curve
- * @file       view.raw.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@arcor.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
+/** Legacy compatibility bridge for the native Joomla 5/6 curve raw view. */
+\defined('_JEXEC') or die;
 
-defined('_JEXEC') or die('Restricted access');
+use Diddipoeler\Component\SportsManagement\Site\View\Curve\RawView;
 
-use Joomla\CMS\Language\Text;
-use Joomla\CMS\Factory;
-
-/**
- * sportsmanagementViewCurve
- *
- * @package
- * @author    abcde
- * @copyright 2015
- * @version   $Id$
- * @access    public
- */
-class sportsmanagementViewCurve extends sportsmanagementView
-{
-
-	/**
-	 * sportsmanagementViewCurve::init()
-	 *
-	 * @return void
-	 */
-	function init()
-	{
-		$option   = Factory::getApplication()->input->getCmd('option');
-		$division = Factory::getApplication()->input->getInt('division', 0);
-
-		$model         = $this->getModel();
-		$rankingconfig = sportsmanagementModelProject::getTemplateConfig("ranking", $model::$cfg_which_database);
-		$flashconfig   = sportsmanagementModelProject::getTemplateConfig("flash", $model::$cfg_which_database);
-		$config        = sportsmanagementModelProject::getTemplateConfig($this->getName(), $model::$cfg_which_database);
-
-		$this->project = sportsmanagementModelProject::getProject($model::$cfg_which_database);
-
-		if (isset($this->project))
-		{
-			$this->overallconfig = sportsmanagementModelProject::getOverallConfig($model::$cfg_which_database);
-
-			if (!isset($this->overallconfig['seperator']))
-			{
-				$this->overallconfig['seperator'] = ":";
-			}
-
-			$this->config   = $config;
-			$this->model    = $model;
-			$this->colors   = sportsmanagementModelProject::getColors($rankingconfig['colors'], $model::$cfg_which_database);
-			$this->division = $model->getDivision($division);
-			$this->team1    = $model->getTeam1($division);
-			$this->team2    = $model->getTeam2($division);
-
-			$this->_setChartdata(array_merge($flashconfig, $rankingconfig));
-		}
-
-		// Parent::display( $tpl );
-	}
-
-	/**
-	 * assign the chartdata object for open flash chart library
-	 *
-	 * @param  $config
-	 *
-	 * @return unknown_type
-	 */
-	function _setChartdata($config)
-	{
-		$model        = $this->getModel();
-		$rounds       = sportsmanagementModelProject::getRounds('ASC', $model::$cfg_which_database);
-		$round_labels = array();
-
-		foreach ($rounds as $r)
-		{
-			$round_labels[] = $r->name;
-		}
-
-		$division = $this->get('division');
-		$data     = $model->getDataByDivision($division->id);
-
-	}
+if (!class_exists(RawView::class)) {
+    require_once JPATH_SITE . '/components/com_sportsmanagement/src/View/Curve/RawView.php';
 }
 
-
-
+if (!class_exists('sportsmanagementViewCurve', false)) {
+    class_alias(RawView::class, 'sportsmanagementViewCurve');
+}
