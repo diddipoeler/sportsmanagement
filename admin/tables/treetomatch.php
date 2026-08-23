c7dd3958bc9d449742b7ff99185283078c680c8d
diddipoeler <diddipoeler@gmx.de>
2026-08-23 14:21:12 +0200

    Bridge tournament tree match table to native Joomla table

--- admin/tables/treetomatch.php
+++ admin/tables/treetomatch.php
@@ -1,37 +1,13 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung f�r alle Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage tables
- * @file       treetomatch.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: � 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
 defined('_JEXEC') or die('Restricted access');
 
-/**
- * sportsmanagementTableTreetoMatch
- *
- * @package
- * @author    diddi
- * @copyright 2014
- * @version   $Id$
- * @access    public
- */
-class sportsmanagementTableTreetoMatch extends JSMTable
-{
-	/**
-	 * Constructor
-	 *
-	 * @param   object Database connector object
-	 *
-	 * @since 1.0
-	 */
-	public function __construct(&$db)
-	{
-		$db = sportsmanagementHelper::getDBConnection();
-		parent::__construct('#__sportsmanagement_treeto_match', 'id', $db);
-	}
+use Diddipoeler\Component\SportsManagement\Administrator\Table\TreetomatchTable;
+
+if (!class_exists(TreetomatchTable::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Table/SportsManagementTable.php';
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Table/TreetomatchTable.php';
+}
+
+if (!class_exists('sportsmanagementTableTreetoMatch', false)) {
+    class_alias(TreetomatchTable::class, 'sportsmanagementTableTreetoMatch');
 }
