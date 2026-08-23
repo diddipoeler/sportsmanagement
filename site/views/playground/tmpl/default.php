ed50fef7be08988f504258af146e6f10168d8dd2
diddipoeler <diddipoeler@gmx.de>
2026-08-23 09:40:44 +0200

    Use native playground template context

--- site/views/playground/tmpl/default.php
+++ site/views/playground/tmpl/default.php
@@ -10,17 +10,9 @@
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\Uri\Uri;
 
-$templatesToLoad = array('globalviews');
-sportsmanagementHelper::addTemplatePaths($templatesToLoad, $this);
-/**
- * kml file laden
- */
-if ($this->mapconfig['map_kmlfile'])
-{
-	$this->kmlpath = Uri::root() . 'tmp' . DIRECTORY_SEPARATOR . $this->playground->id . '-playground.kml';
-	$this->kmlfile = $this->playground->id . '-playground.kml';
+if (!$this->playground) {
+    return;
 }
 ?>
 
@@ -33,50 +25,50 @@ if ($this->mapconfig['map_kmlfile'])
 
 	echo $this->loadTemplate('projectheading');
 
-	if ($this->config['show_sectionheader'])
+	if (!empty($this->config['show_sectionheader']))
 	{
 		echo $this->loadTemplate('sectionheader');
 	}
 
-	if ($this->config['show_playground'])
+	if (!empty($this->config['show_playground']))
 	{
 		echo $this->loadTemplate('playground');
 	}
 
-	if ($this->config['show_extended'])
+	if (!empty($this->config['show_extended']))
 	{
 		echo $this->loadTemplate('extended');
 	}
 
-	if ($this->config['show_picture'])
+	if (!empty($this->config['show_picture']))
 	{
 		echo $this->loadTemplate('picture');
 	}
 
-	if ($this->playground->latitude && $this->playground->longitude)
+	if (!empty($this->playground->latitude) && !empty($this->playground->longitude))
 	{
-		if ($this->config['show_maps'])
+		if (!empty($this->config['show_maps']))
 		{
 			echo $this->loadTemplate('googlemap');
 		}
 	}
 
-	if ($this->config['show_description'])
+	if (!empty($this->config['show_description']))
 	{
 		echo $this->loadTemplate('description');
 	}
 
-	if ($this->config['show_teams'])
+	if (!empty($this->config['show_teams']))
 	{
 		echo $this->loadTemplate('teams');
 	}
 
-	if ($this->config['show_matches'])
+	if (!empty($this->config['show_matches']))
 	{
 		echo $this->loadTemplate('matches');
 	}
 
-	if ($this->config['show_played_matches'])
+	if (!empty($this->config['show_played_matches']))
 	{
 		echo $this->loadTemplate('played_matches');
 	}
