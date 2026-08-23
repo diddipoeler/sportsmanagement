21b5ae5a771de46ccb2e55e6263ae7811d05fdea
diddipoeler <diddipoeler@gmx.de>
2026-08-23 15:02:21 +0200

    Use native club table in extra-fields view

--- admin/views/extrafields/view.html.php
+++ admin/views/extrafields/view.html.php
@@ -1,54 +1,28 @@
 <?php
 /**
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage extrafields
- * @file       view.html.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * SportsManagement administrator extra-fields list view.
  */
 defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\Table\Table;
+
+use Diddipoeler\Component\SportsManagement\Administrator\Table\ClubTable;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\Toolbar\ToolbarHelper;
 
-/**
- * sportsmanagementViewextrafields
- *
- * @package
- * @author
- * @copyright diddi
- * @version   2014
- * @access    public
- */
 class sportsmanagementViewextrafields extends sportsmanagementView
 {
+    public function init()
+    {
+        // Preserve the historic view contract: templates expect a club table here.
+        $this->table = new ClubTable($this->model->getDatabase());
+    }
 
-	/**
-	 * sportsmanagementViewextrafields::init()
-	 *
-	 * @return void
-	 */
-	public function init()
-	{
-		$this->table = Table::getInstance('club', 'sportsmanagementTable');
-
-	}
-
-	/**
-	 * Add the page title and toolbar.
-	 *
-	 * @since 1.7
-	 */
-	protected function addToolbar()
-	{
-		$this->title = Text::_('COM_SPORTSMANAGEMENT_ADMIN_EXTRAFIELDS_TITLE');
-		ToolbarHelper::addNew('extrafield.add');
-		ToolbarHelper::editList('extrafield.edit');
-		ToolbarHelper::custom('extrafield.import', 'upload', 'upload', Text::_('JTOOLBAR_UPLOAD'), false);
-		ToolbarHelper::archiveList('extrafield.export', Text::_('JTOOLBAR_EXPORT'));
-		parent::addToolbar();
-	}
+    protected function addToolbar()
+    {
+        $this->title = Text::_('COM_SPORTSMANAGEMENT_ADMIN_EXTRAFIELDS_TITLE');
+        ToolbarHelper::addNew('extrafield.add');
+        ToolbarHelper::editList('extrafield.edit');
+        ToolbarHelper::custom('extrafield.import', 'upload', 'upload', Text::_('JTOOLBAR_UPLOAD'), false);
+        ToolbarHelper::archiveList('extrafield.export', Text::_('JTOOLBAR_EXPORT'));
+        parent::addToolbar();
+    }
 }
