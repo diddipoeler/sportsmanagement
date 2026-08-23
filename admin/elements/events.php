aa16b479d2b6c83026d2e91172314b3542bddc9d
diddipoeler <diddipoeler@gmx.de>
2026-08-23 12:36:13 +0200

    Bridge legacy events element to native field

--- admin/elements/events.php
+++ admin/elements/events.php
@@ -1,67 +1,15 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage elements
- * @file       events.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\HTML\HTMLHelper;
-use Joomla\CMS\Language\Text;
-use Joomla\CMS\Factory;
-use Joomla\CMS\Component\ComponentHelper;
+/** Legacy compatibility bridge for the native Joomla 5/6 events field. */
+\defined('_JEXEC') or die;
 
-/**
- * JFormFieldEvents
- *
- * @package
- * @author
- * @copyright diddi
- * @version   2014
- * @access    public
- */
-class JFormFieldEvents extends JFormField
-{
-	protected $type = 'events';
+use Diddipoeler\Component\SportsManagement\Administrator\Field\EventsField;
 
-	/**
-	 * JFormFieldEvents::getInput()
-	 *
-	 * @return
-	 */
-	function getInput()
-	{
-		$db   = sportsmanagementHelper::getDBConnection();
-		$lang = Factory::getLanguage();
-
-		// Welche tabelle soll genutzt werden
-		$params         =& ComponentHelper::getParams('com_sportsmanagement');
-		$database_table = $params->get('cfg_which_database_table');
-
-		$extension = "com_sportsmanagement";
-		$source    = JPATH_ADMINISTRATOR . '/components/' . $extension;
-		$lang->load("$extension", JPATH_ADMINISTRATOR, null, false, false)
-		|| $lang->load($extension, $source, null, false, false)
-		|| $lang->load($extension, JPATH_ADMINISTRATOR, $lang->getDefault(), false, false)
-		|| $lang->load($extension, $source, $lang->getDefault(), false, false);
-
-		$query = 'SELECT e.id, e.name FROM #__' . $database_table . '_eventtype e WHERE published=1 ORDER BY name';
-		$db->setQuery($query);
-		$events = $db->loadObjectList();
-		$mitems = array(HTMLHelper::_('select.option', '', Text::_('COM_SPORTSMANAGEMENT_GLOBAL_SELECT')));
-
-		foreach ($events as $event)
-		{
-			$mitems[] = HTMLHelper::_('select.option', $event->id, '&nbsp;' . Text::_($event->name) . ' (' . $event->id . ')');
-		}
-
-		$output = HTMLHelper::_('select.genericlist', $mitems, $this->name, 'class="inputbox" multiple="multiple" size="10"', 'value', 'text', $this->value, $this->id);
-
-		return $output;
-	}
+if (!class_exists(EventsField::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Field/SportsManagementDatabaseTrait.php';
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Field/SportsManagementListField.php';
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Field/EventsField.php';
 }
 
+if (class_exists(EventsField::class) && !class_exists('JFormFieldEvents', false)) {
+    class_alias(EventsField::class, 'JFormFieldEvents');
+}
