7e60be593e3fdaaaf5cff0f1dbfa4fb29018846e
diddipoeler <diddipoeler@gmx.de>
2026-08-23 12:40:45 +0200

    Add Joomla 5/6 team age groups field

---
+++ admin/src/Field/AgegroupsField.php
@@ -0,0 +1,56 @@
+<?php
+namespace Diddipoeler\Component\SportsManagement\Administrator\Field;
+
+\defined('_JEXEC') or die;
+
+use Joomla\CMS\Factory;
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Language\Text;
+
+/** Age groups filtered by the sport type of the currently edited team. */
+final class AgegroupsField extends SportsManagementListField
+{
+    protected $type = 'agegroups';
+
+    protected function getOptions(): array
+    {
+        $options = parent::getOptions();
+        $teamId = Factory::getApplication()->getInput()->getInt('id', 0);
+
+        if ($teamId <= 0) {
+            return $options;
+        }
+
+        $db = $this->getSportsManagementDatabase();
+        $teamQuery = $db->getQuery(true)
+            ->select($db->quoteName('sports_type_id'))
+            ->from($db->quoteName('#__sportsmanagement_team'))
+            ->where($db->quoteName('id') . ' = ' . $teamId);
+        $db->setQuery($teamQuery, 0, 1);
+        $sportsTypeId = (int) $db->loadResult();
+
+        if ($sportsTypeId <= 0) {
+            return $options;
+        }
+
+        $query = $db->getQuery(true)
+            ->select([
+                $db->quoteName('id', 'value'),
+                $db->quoteName('name', 'text'),
+            ])
+            ->from($db->quoteName('#__sportsmanagement_agegroup'))
+            ->where($db->quoteName('sportstype_id') . ' = ' . $sportsTypeId)
+            ->order($db->quoteName('name'));
+        $db->setQuery($query);
+
+        foreach ($db->loadObjectList() ?: [] as $item) {
+            $options[] = HTMLHelper::_(
+                'select.option',
+                $item->value,
+                "\u{00A0}" . Text::_((string) $item->text) . ' (' . (int) $item->value . ')'
+            );
+        }
+
+        return $options;
+    }
+}
