aeed3bfcb091cb9647d1d7ed99a3bfe73fea00bb
diddipoeler <diddipoeler@gmx.de>
2026-08-23 16:08:59 +0200

    Modernize legacy site base model table access

--- site/models/sportsmanagement.php
+++ site/models/sportsmanagement.php
@@ -10,114 +10,57 @@
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 defined('_JEXEC') or die('Restricted access');
+
+use Diddipoeler\Component\SportsManagement\Administrator\Table\LegacySportsmanagementTable;
 use Joomla\CMS\Factory;
-use Joomla\CMS\Table\Table;
 use Joomla\CMS\MVC\Model\ItemModel;
-use Joomla\Registry\Registry;
+use Joomla\Database\DatabaseInterface;
 
 /**
  * sportsmanagementModelsportsmanagement
  *
- * @package
- * @author    diddi
- * @copyright 2014
- * @version   $Id$
- * @access    public
+ * Historic site model retained for compatibility with legacy callers.
  */
 class sportsmanagementModelsportsmanagement extends ItemModel
 {
-	/**
-	 * @var object item
-	 */
-	protected $item;
+    /** @var object|null */
+    protected $item;
 
-	/**
-	 * Returns a reference to the a Table object, always creating it.
-	 *
-	 * @param   type    The table type to instantiate
-	 * @param   string    A prefix for the table class name. Optional.
-	 * @param   array    Configuration array for model. Optional.
-	 *
-	 * @return Table    A database object
-	 * @since  1.6
-	 */
-	public function getTable($type = 'sportsmanagement', $prefix = 'sportsmanagementTable', $config = array())
-	{
-		return Table::getInstance($type, $prefix, $config);
-	}
+    /**
+     * Return the historic SportsManagement compatibility table without using
+     * Joomla's removed static Table::getInstance() factory.
+     */
+    public function getTable($type = 'sportsmanagement', $prefix = 'sportsmanagementTable', $config = array())
+    {
+        if (!class_exists(LegacySportsmanagementTable::class)) {
+            require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Table/LegacySportsmanagementTable.php';
+        }
 
-	/**
-	 * Get the message
-	 *
-	 * @return object The message to be displayed to the user
-	 */
-	public function getItem($pk = null)
-	{
-	    /**
-		if (!isset($this->item))
-		{
-			$id = $this->getState('message.id');
-			$this->_db->setQuery(
-				$this->_db->getQuery(true)
-					->from('#__sportsmanagement as h')
-					->leftJoin('#__categories as c ON h.catid=c.id')
-					->select('h.greeting, h.params, c.title as category')
-					->where('h.id=' . (int) $id)
-			);
+        /** @var DatabaseInterface $db */
+        $db = Factory::getContainer()->get(DatabaseInterface::class);
 
-			if (!$this->item = $this->_db->loadObject())
-			{
-				$this->setError($this->_db->getError());
-			}
-			else
-			{
-				// Load the JSON string
-				$params = new Registry;
+        return new LegacySportsmanagementTable($db);
+    }
 
-				if (version_compare(JVERSION, '3.0.0', 'ge'))
-				{
-					$params->loadString($this->item->params);
-				}
-				else
-				{
-					$params->loadJSON($this->item->params);
-				}
+    /**
+     * Get the historic item payload.
+     */
+    public function getItem($pk = null)
+    {
+        return $this->item;
+    }
 
-				$this->item->params = $params;
+    /**
+     * Method to auto-populate the model state.
+     */
+    protected function populateState()
+    {
+        $app = Factory::getApplication();
+        $input = $app->getInput();
 
-				// Merge global params with item params
-				$params = clone $this->getState('params');
-				$params->merge($this->item->params);
-				$this->item->params = $params;
-			}
-		}
-                    */
-		return $this->item;
-	}
+        $this->setState('message.id', $input->getInt('id'));
+        $this->setState('params', $app->getParams());
 
-	/**
-	 * Method to auto-populate the model state.
-	 *
-	 * This method should only be called once per instantiation and is designed
-	 * to be called on the first call to the getState() method unless the model
-	 * configuration flag to ignore the request is set.
-	 *
-	 * Note. Calling getState in this method will result in recursion.
-	 *
-	 * @return void
-	 * @since  1.6
-	 */
-	protected function populateState()
-	{
-		$app = Factory::getApplication();
-
-		// Get the message id
-		$id = Factory::getApplication()->input->getInt('id');
-		$this->setState('message.id', $id);
-
-		// Load the parameters.
-		$params = $app->getParams();
-		$this->setState('params', $params);
-		parent::populateState();
-	}
+        parent::populateState();
+    }
 }
