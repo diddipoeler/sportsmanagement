6215217e25ef47d364d91713e53cf0371a56cb4e
diddipoeler <diddipoeler@gmx.de>
2026-08-23 12:41:35 +0200

    Bridge legacy active season field to Joomla 5/6 implementation

--- admin/elements/actseason.php
+++ admin/elements/actseason.php
@@ -1,67 +1,17 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage elements
- * @file       actseason.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\Factory;
-use Joomla\CMS\HTML\HTMLHelper;
-use Joomla\CMS\Component\ComponentHelper;
+/** Legacy compatibility bridge for the Joomla 5/6 active season field. */
+\defined('_JEXEC') or die;
 
-/**
- * JFormFieldactseason
- *
- * @package
- * @author
- * @copyright diddi
- * @version   2014
- * @access    public
- */
-class JFormFieldactseason extends JFormField
-{
-	protected $type = 'actseason';
+use Diddipoeler\Component\SportsManagement\Administrator\Field\ActseasonField;
 
-	/**
-	 * JFormFieldactseason::getInput()
-	 *
-	 * @return
-	 */
-	protected function getInput()
-	{
-		$db     = sportsmanagementHelper::getDBConnection();
-		$lang   = Factory::getLanguage();
-		$option = Factory::getApplication()->input->getCmd('option');
+if (!class_exists(ActseasonField::class)) {
+    $fieldFile = JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Field/ActseasonField.php';
 
-		// Welche tabelle soll genutzt werden
-		$params         = ComponentHelper::getParams('COM_SPORTSMANAGEMENT');
-		$database_table = $params->get('cfg_which_database_table');
+    if (is_file($fieldFile)) {
+        require_once $fieldFile;
+    }
+}
 
-		$extension = "com_sportsmanagement";
-		$source    = JPATH_ADMINISTRATOR . '/components/' . $extension;
-		$lang->load("$extension", JPATH_ADMINISTRATOR, null, false, false)
-		|| $lang->load($extension, $source, null, false, false)
-		|| $lang->load($extension, JPATH_ADMINISTRATOR, $lang->getDefault(), false, false)
-		|| $lang->load($extension, $source, $lang->getDefault(), false, false);
-
-		$query = 'SELECT s.id, s.name as name
-					FROM #__' . $database_table . '_season AS s
-					ORDER BY s.name DESC';
-		$db->setQuery($query);
-		$projects = $db->loadObjectList();
-
-		foreach ($projects as $project)
-		{
-			$mitems[] = HTMLHelper::_('select.option', $project->id, '&nbsp;&nbsp;&nbsp;' . $project->name);
-		}
-
-		$output = HTMLHelper::_('select.genericlist', $mitems, $this->name . '[]', 'class="inputbox" style="width:90%;" ', 'value', 'text', $this->value, $this->id);
-
-		return $output;
-	}
+if (class_exists(ActseasonField::class) && !class_exists('JFormFieldactseason', false)) {
+    class_alias(ActseasonField::class, 'JFormFieldactseason');
 }
