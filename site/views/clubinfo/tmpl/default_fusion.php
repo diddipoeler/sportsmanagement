84ce4ad422c8e41572fd3e9bd9ea7f9460336498
diddipoeler <diddipoeler@gmx.de>
2026-08-23 09:52:45 +0200

    Clean club info fusion template

--- site/views/clubinfo/tmpl/default_fusion.php
+++ site/views/clubinfo/tmpl/default_fusion.php
@@ -8,60 +8,49 @@
  * @license   GNU General Public License version 2 or later; see LICENSE.txt
  */
 defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\Uri\Uri;
-use Joomla\CMS\Language\Text;
-use Joomla\CMS\HTML\HTMLHelper;
-use Joomla\CMS\Factory;
-use Joomla\CMS\Component\ComponentHelper;
-use Joomla\CMS\Log\Log;
 
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Language\Text;
+
+$escape = static fn ($value): string => htmlspecialchars((string) $value, ENT_QUOTES, 'UTF-8');
+$this->notes = [Text::_('Fusionen')];
+echo $this->loadTemplate('jsm_notes');
 ?>
 <div class="<?php echo $this->divclassrow; ?>" id="default_fusion">
-	<?php
-	$this->notes = array();
-	$this->notes[] = Text::_('Fusionen');
-	echo $this->loadTemplate('jsm_notes');
-	?>
-	<div class="panel-group" id="<?php echo $this->club->name; ?>">
-		<div class="panel panel-default">
-			<div id="<?php echo $this->club->id; ?>" class="">
-				<div class="panel-body">
+    <div class="panel-group" id="club-fusion-<?php echo (int) $this->club->id; ?>">
+        <div class="panel panel-default">
+            <div id="club-fusion-panel-<?php echo (int) $this->club->id; ?>">
+                <div class="panel-body">
+                    <div class="tree" style="display: flow-root;">
+                        <ul>
+                            <li>
+                                <?php if (empty($this->config['show_bootstrap_tree'])) : ?>
+                                    <span><i class="icon-folder-open"></i> aktueller Verein</span>
+                                <?php endif; ?>
 
-					<div class="tree" style="display: flow-root;">
-						<ul>
-							<li>
-								<?php
-								if (!$this->config['show_bootstrap_tree'])
-								{
-								?>
-								<span><i class="icon-folder-open"></i> aktueller Verein</span>
-								<?php
-								}
-								?>
-								<a href="#"><?PHP echo HTMLHelper::image($this->club->logo_big, $this->club->name, 'width="30"') . ' ' . $this->club->name; ?></a>
-								<?php
-								echo $this->familytree;
-								?>
-							</li>
-						</ul>
-                        
-                        <?php
-                        if ( $this->club->new_club_id )
-                        {
-                            echo $this->clubhistoryhtml;
-                        }
-                        
-                        ?>
-					</div>
+                                <a href="#">
+                                    <?php
+                                    if (!empty($this->club->logo_big)) {
+                                        echo HTMLHelper::image(
+                                            (string) $this->club->logo_big,
+                                            (string) $this->club->name,
+                                            ['width' => '30']
+                                        ) . ' ';
+                                    }
+                                    echo $escape($this->club->name ?? '');
+                                    ?>
+                                </a>
 
-				</div>
+                                <?php echo $this->familytree; ?>
+                            </li>
+                        </ul>
 
-			</div>
-
-		</div>
-
-
-
-	</div>
-
+                        <?php if (!empty($this->club->new_club_id)) : ?>
+                            <?php echo $this->clubhistoryhtml; ?>
+                        <?php endif; ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
