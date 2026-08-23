5cde2b076e8d97f386dde85a36ad426581e0fcc7
diddipoeler <diddipoeler@gmx.de>
2026-08-23 10:24:02 +0200

    Remove legacy prediction project script setup

--- admin/views/predictionproject/view.html.php
+++ admin/views/predictionproject/view.html.php
@@ -10,9 +10,8 @@
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 defined('_JEXEC') or die('Restricted access');
+
 use Joomla\CMS\Language\Text;
-use Joomla\CMS\Factory;
-use Joomla\CMS\Uri\Uri;
 use Joomla\CMS\Log\Log;
 
 /**
@@ -26,76 +25,31 @@ use Joomla\CMS\Log\Log;
  */
 class sportsmanagementViewpredictionproject extends sportsmanagementView
 {
-	/**
-	 * sportsmanagementViewpredictionproject::init()
-	 *
-	 * @return
-	 */
-	public function init()
-	{
+    /**
+     * sportsmanagementViewpredictionproject::init()
+     *
+     * @return
+     */
+    public function init()
+    {
+        $this->form = $this->get('Form');
+        $this->item = $this->get('Item');
 
-		// Get the Data
-		$this->form   = $this->get('Form');
-		$this->item   = $this->get('Item');
-		$this->script = $this->get('Script');
+        if (count($errors = $this->get('Errors'))) {
+            Log::add(implode('<br />', $errors));
 
-		if (version_compare(substr(JVERSION, 0, 3), '4.0', 'ge'))
-		{
-			$this->form->setFieldAttribute('champ', 'type', 'radio');
-			$this->form->setFieldAttribute('champ', 'class', 'switcher');
-			$this->form->setFieldAttribute('joker', 'type', 'radio');
-			$this->form->setFieldAttribute('joker', 'class', 'switcher');
-			$this->form->setFieldAttribute('published', 'type', 'radio');
-			$this->form->setFieldAttribute('published', 'class', 'switcher');
-			$this->form->setFieldAttribute('mode', 'type', 'radio');
-			$this->form->setFieldAttribute('mode', 'class', 'switcher');
-			$this->form->setFieldAttribute('overview', 'type', 'radio');
-			$this->form->setFieldAttribute('overview', 'class', 'switcher');
-		}
+            return false;
+        }
 
-		// Check for errors.
-		if (count($errors = $this->get('Errors')))
-		{
-			Log::add(implode('<br />', $errors));
+        $this->item->name = '';
+        $this->app->setUserState("$this->option.pid", $this->item->project_id);
 
-			return false;
-		}
-
-		$this->item->name = '';
-
-		$this->app->setUserState("$this->option.pid", $this->item->project_id);
-
-		// Set the document
-		//$this->setDocument();
-
-		switch ($this->getLayout())
-		{
-			case 'edit';
-			case 'edit_3';
-			case 'edit_4';
-				$this->setLayout('edit_3');
-				break;
-		}
-
-	}
-
-	/**
-	 * Method to set up the document properties
-	 *
-	 * @return void
-	 */
-	 /**
-	public function setDocument($document)
-	{
-		$isNew = $this->item->id == 0;
-
-		// $this->name = $this->item->name;
-		$document = Factory::getDocument();
-		$document->setTitle($isNew ? Text::_('COM_HELLOWORLD_HELLOWORLD_CREATING') : Text::_('COM_HELLOWORLD_HELLOWORLD_EDITING'));
-		$document->addScript(Uri::root() . $this->script);
-		$document->addScript(Uri::root() . "/administrator/components/com_sportsmanagement/views/sportsmanagement/submitbutton.js");
-		Text::script('COM_HELLOWORLD_HELLOWORLD_ERROR_UNACCEPTABLE');
-	}
-	*/
-
+        switch ($this->getLayout()) {
+            case 'edit';
+            case 'edit_3';
+            case 'edit_4';
+                $this->setLayout('edit_3');
+                break;
+        }
+    }
 }
