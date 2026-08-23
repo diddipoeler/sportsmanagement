39049af6233f26c75a583dc2b36c8dbc7842aca9
diddipoeler <diddipoeler@gmx.de>
2026-08-23 12:35:06 +0200

    Bridge legacy teams element to native field

--- admin/elements/teams.php
+++ admin/elements/teams.php
@@ -1,73 +1,14 @@
 <?php
-/**
- *
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage elements
- * @file       teams.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
+/** Legacy compatibility bridge for the native Joomla 5/6 teams field. */
+\defined('_JEXEC') or die;
 
-defined('_JEXEC') or die('Restricted access');
+use Diddipoeler\Component\SportsManagement\Administrator\Field\TeamsField;
 
-use Joomla\CMS\Language\Text;
-use Joomla\CMS\Factory;
-use Joomla\CMS\HTML\HTMLHelper;
-use Joomla\CMS\Component\ComponentHelper;
-use Joomla\CMS\Form\FormField;
+if (!class_exists(TeamsField::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Field/SportsManagementDatabaseTrait.php';
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Field/TeamsField.php';
+}
 
-/**
- * JFormFieldTeams
- *
- * @package
- * @author
- * @copyright diddi
- * @version   2014
- * @access    public
- */
-class JFormFieldTeams extends FormField
-{
-	protected $type = 'teams';
-
-	/**
-	 * JFormFieldTeams::getInput()
-	 *
-	 * @return
-	 */
-	protected function getInput()
-	{
-		$db   = sportsmanagementHelper::getDBConnection();
-		$lang = Factory::getLanguage();
-
-		// Welche tabelle soll genutzt werden
-		$params = ComponentHelper::getParams('com_sportsmanagement');
-
-		$extension = "com_sportsmanagement";
-		$source    = JPATH_ADMINISTRATOR . '/components/' . $extension;
-		$lang->load("$extension", JPATH_ADMINISTRATOR, null, false, false)
-		|| $lang->load($extension, $source, null, false, false)
-		|| $lang->load($extension, JPATH_ADMINISTRATOR, $lang->getDefault(), false, false)
-		|| $lang->load($extension, $source, $lang->getDefault(), false, false);
-
-		$query = $db->getQuery(true);
-		$query->select('t.id, t.name');
-		$query->from('#__sportsmanagement_team AS t');
-		$query->order('name');
-		$db->setQuery($query);
-		$teams  = $db->loadObjectList();
-		$mitems = array(HTMLHelper::_('select.option', '', Text::_('COM_SPORTSMANAGEMENT_GLOBAL_SELECT')));
-
-		foreach ($teams as $team)
-		{
-			$mitems[] = HTMLHelper::_('select.option', $team->id, '&nbsp;' . $team->name . ' (' . $team->id . ')');
-		}
-
-		$output = HTMLHelper::_('select.genericlist', $mitems, $this->name . '[]', 'class="inputbox" multiple="multiple" size="10"', 'value', 'text', $this->value, $this->id);
-
-		return $output;
-	}
+if (class_exists(TeamsField::class) && !class_exists('JFormFieldTeams', false)) {
+    class_alias(TeamsField::class, 'JFormFieldTeams');
 }
