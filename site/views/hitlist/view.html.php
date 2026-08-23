4b30fcf63ada2c163a7173709536b9b4fd1328e2
diddipoeler <diddipoeler@gmx.de>
2026-08-23 12:31:44 +0200

    Bridge legacy hitlist view to native implementation

--- site/views/hitlist/view.html.php
+++ site/views/hitlist/view.html.php
@@ -1,36 +1,14 @@
 <?php
-/**
- * SportsManagement hit list compatibility view for Joomla 5/6.
- */
+/** Legacy compatibility bridge for the native Joomla 5/6 hitlist view. */
+\defined('_JEXEC') or die;
 
-defined('_JEXEC') or die('Restricted access');
+use Diddipoeler\Component\SportsManagement\Site\View\Hitlist\HtmlView;
 
-class sportsmanagementViewhitlist extends sportsmanagementView
-{
-    public function init(): void
-    {
-        $model = $this->getModel();
+if (!class_exists(HtmlView::class)) {
+    require_once JPATH_SITE . '/components/com_sportsmanagement/src/View/SportsManagementHtmlView.php';
+    require_once JPATH_SITE . '/components/com_sportsmanagement/src/View/Hitlist/HtmlView.php';
+}
 
-        $this->tableclass = $this->jinput->getString('table_class', 'table');
-        $this->show_project = $this->jinput->getString('show_project', 'table');
-        $this->show_club = $this->jinput->getString('show_club', 'table');
-        $this->show_team = $this->jinput->getString('show_team', 'table');
-        $this->show_person = $this->jinput->getString('show_person', 'table');
-        $this->show_playground = $this->jinput->getString('show_playground', 'table');
-        $this->max_hits = $this->jinput->getInt('max_hits', 0);
-
-        foreach ([
-            'project' => $this->show_project,
-            'club' => $this->show_club,
-            'team' => $this->show_team,
-            'person' => $this->show_person,
-            'playground' => $this->show_playground,
-        ] as $table => $enabled) {
-            if ($enabled) {
-                $model->getSportsmanagementHits(null, $this->max_hits, $table);
-            }
-        }
-
-        $this->model_hits = $model::$_success_text;
-    }
+if (!class_exists('sportsmanagementViewhitlist', false)) {
+    class_alias(HtmlView::class, 'sportsmanagementViewhitlist');
 }
