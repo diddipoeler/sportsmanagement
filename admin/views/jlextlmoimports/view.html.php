a4d6e3d233e42dde17338efaa535969a3b11fa4a
diddipoeler <diddipoeler@gmx.de>
2026-08-23 12:57:43 +0200

    Remove Joomla 6 legacy model factory from LMO view

--- admin/views/jlextlmoimports/view.html.php
+++ admin/views/jlextlmoimports/view.html.php
@@ -1,88 +1,80 @@
 <?php
 /**
- *
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage jlextlmoimports
- * @file       view.html.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * SportsManagement LMO import view compatibility implementation.
  */
-
 defined('_JEXEC') or die('Restricted access');
 
+use Joomla\CMS\Component\ComponentHelper;
 use Joomla\CMS\Factory;
 use Joomla\CMS\HTML\HTMLHelper;
-use Joomla\CMS\Toolbar\ToolbarHelper;
-use Joomla\CMS\Component\ComponentHelper;
-use Joomla\CMS\MVC\Model\BaseDatabaseModel;
 use Joomla\CMS\Language\Text;
+use Joomla\CMS\Toolbar\ToolbarHelper;
 
-/**
- * sportsmanagementViewjlextlmoimports
- *
- * @package
- * @author
- * @copyright diddi
- * @version   2013
- * @access    public
- */
 class sportsmanagementViewjlextlmoimports extends sportsmanagementView
 {
+    public function init()
+    {
+        $app = Factory::getApplication();
+        $language = $app->getLanguage();
+        $this->config = ComponentHelper::getParams('com_media');
 
-	/**
-	 * sportsmanagementViewjlextlmoimports::init()
-	 *
-	 * @return void
-	 */
-	public function init()
-	{
-		$lang = Factory::getLanguage();
+        $languageParts = explode('-', $language->getTag());
+        $iso2 = (string) ($languageParts[1] ?? 'DE');
+        $country = JSMCountries::convertIso2to3($iso2);
+        $this->country = $country;
 
-		$config = ComponentHelper::getParams('com_media');
-		$post   = $this->jinput->post->getArray(array());
-		$files  = $this->jinput->getArray(array('files'));
+        $countries = JSMCountries::getCountryOptions();
+        $this->countries = HTMLHelper::_(
+            'select.genericlist',
+            $countries,
+            'country',
+            'class="form-select" size="1"',
+            'value',
+            'text',
+            $country
+        );
 
-		$this->config       = $config;
-		$teile              = explode("-", $lang->getTag());
-		$country            = JSMCountries::convertIso2to3($teile[1]);
-		$this->country      = $country;
-		$countries          = JSMCountries::getCountryOptions();
-		$lists['countries'] = HTMLHelper::_('select.genericlist', $countries, 'country', 'class="inputbox" size="1"', 'value', 'text', $country);
-		$this->countries    = $lists['countries'];
+        $agegroupOptions = [
+            HTMLHelper::_('select.option', '0', Text::_('COM_SPORTSMANAGEMENT_ADMIN_PROJECTS_AGEGROUP')),
+        ];
 
-		$myoptions[] = HTMLHelper::_('select.option', '0', Text::_('COM_SPORTSMANAGEMENT_ADMIN_PROJECTS_AGEGROUP'));
-		$mdlagegroup = BaseDatabaseModel::getInstance('agegroups', 'sportsmanagementModel');
+        $mvcFactory = $app->bootComponent('com_sportsmanagement')->getMVCFactory();
+        $agegroupsModel = $mvcFactory->createModel(
+            'Agegroups',
+            'Administrator',
+            ['ignore_request' => true]
+        );
 
-		if ($res = $mdlagegroup->getAgeGroups('', 0))
-		{
-			$myoptions = array_merge($myoptions, $res);
-		}
+        if ($agegroupsModel && method_exists($agegroupsModel, 'getAgeGroups')) {
+            $agegroups = $agegroupsModel->getAgeGroups('', 0);
 
-		$lists['agegroup'] = $myoptions;
-		$this->agegroup    = HTMLHelper::_('select.genericlist', $lists['agegroup'], 'agegroup', 'class="inputbox" size="1"', 'value', 'text', 0);
+            if ($agegroups) {
+                $agegroupOptions = array_merge($agegroupOptions, $agegroups);
+            }
+        }
 
-		$model           = BaseDatabaseModel::getInstance('jlxmlimport', 'sportsmanagementmodel');
-		$this->templates = $model->getTemplateList();
+        $this->agegroup = HTMLHelper::_(
+            'select.genericlist',
+            $agegroupOptions,
+            'agegroup',
+            'class="form-select" size="1"',
+            'value',
+            'text',
+            0
+        );
 
-	}
+        if (!class_exists('sportsmanagementModelJLXMLImport', false)) {
+            require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/models/jlxmlimport.php';
+        }
 
-	/**
-	 * sportsmanagementViewjlextlmoimports::addToolbar()
-	 *
-	 * @return void
-	 */
-	protected function addToolbar()
-	{
-		ToolbarHelper::back('JPREV', 'index.php?option=com_sportsmanagement&view=extensions');
-		ToolbarHelper::divider();
+        $xmlImportModel = new sportsmanagementModelJLXMLImport();
+        $this->templates = $xmlImportModel->getTemplateList();
+    }
 
-		// SportsmanagementHelper::ToolbarButtonOnlineHelp();
-		parent::addToolbar();
-	}
-
-
+    protected function addToolbar()
+    {
+        ToolbarHelper::back('JPREV', 'index.php?option=com_sportsmanagement&view=extensions');
+        ToolbarHelper::divider();
+        parent::addToolbar();
+    }
 }
