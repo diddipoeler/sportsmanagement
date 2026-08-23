48aaf7037c609af6103d07e74e0d452813d17beb
diddipoeler <diddipoeler@gmx.de>
2026-08-23 12:43:44 +0200

    Bridge legacy playgrounds field to Joomla 5/6 implementation

--- admin/elements/playgrounds.php
+++ admin/elements/playgrounds.php
@@ -1,67 +1,17 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage elements
- * @file       playgrounds.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\Language\Text;
-use Joomla\CMS\Factory;
-use Joomla\CMS\HTML\HTMLHelper;
-use Joomla\CMS\Component\ComponentHelper;
+/** Legacy compatibility bridge for the Joomla 5/6 playgrounds field. */
+\defined('_JEXEC') or die;
 
-/**
- * JFormFieldPlaygrounds
- *
- * @package
- * @author
- * @copyright diddi
- * @version   2014
- * @access    public
- */
-class JFormFieldPlaygrounds extends JFormField
-{
-	protected $type = 'playgrounds';
+use Diddipoeler\Component\SportsManagement\Administrator\Field\PlaygroundsField;
 
-	/**
-	 * JFormFieldPlaygrounds::getInput()
-	 *
-	 * @return
-	 */
-	function getInput()
-	{
-		$db   = sportsmanagementHelper::getDBConnection();
-		$lang = Factory::getLanguage();
+if (!class_exists(PlaygroundsField::class)) {
+    $fieldFile = JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Field/PlaygroundsField.php';
 
-		// Welche tabelle soll genutzt werden
-		$params         = ComponentHelper::getParams('com_sportsmanagement');
-		$database_table = $params->get('cfg_which_database_table');
-
-		$extension = "com_sportsmanagement";
-		$source    = JPATH_ADMINISTRATOR . '/components/' . $extension;
-		$lang->load("$extension", JPATH_ADMINISTRATOR, null, false, false)
-		|| $lang->load($extension, $source, null, false, false)
-		|| $lang->load($extension, JPATH_ADMINISTRATOR, $lang->getDefault(), false, false)
-		|| $lang->load($extension, $source, $lang->getDefault(), false, false);
-
-		$query = 'SELECT pl.id, pl.name FROM #__' . $database_table . '_playground pl ORDER BY name';
-		$db->setQuery($query);
-		$playgrounds = $db->loadObjectList();
-		$mitems      = array(HTMLHelper::_('select.option', '', Text::_('COM_SPORTSMANAGEMENT_GLOBAL_SELECT')));
-
-		foreach ($playgrounds as $playground)
-		{
-			$mitems[] = HTMLHelper::_('select.option', $playground->id, '&nbsp;' . $playground->name . ' (' . $playground->id . ')');
-		}
-
-		$output = HTMLHelper::_('select.genericlist', $mitems, $this->name, 'class="inputbox" multiple="multiple" size="10"', 'value', 'text', $this->value, $this->id);
-
-		return $output;
-	}
+    if (is_file($fieldFile)) {
+        require_once $fieldFile;
+    }
 }
 
+if (class_exists(PlaygroundsField::class) && !class_exists('JFormFieldPlaygrounds', false)) {
+    class_alias(PlaygroundsField::class, 'JFormFieldPlaygrounds');
+}
