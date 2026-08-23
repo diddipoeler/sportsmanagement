1aa06814b88e45f190d43105d552f212dbfcf294
diddipoeler <diddipoeler@gmx.de>
2026-08-23 12:46:14 +0200

    Bridge legacy prediction project team field to Joomla 5/6 implementation

--- admin/elements/predictionproteamid.php
+++ admin/elements/predictionproteamid.php
@@ -1,83 +1,17 @@
 <?php
-/**
- *
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage elements
- * @file       predictionproteamid.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
+/** Legacy compatibility bridge for the Joomla 5/6 prediction project-team field. */
+\defined('_JEXEC') or die;
 
-defined('_JEXEC') or die('Restricted access');
+use Diddipoeler\Component\SportsManagement\Administrator\Field\PredictionproteamidField;
 
-use Joomla\CMS\Language\Text;
-use Joomla\CMS\Factory;
-use Joomla\CMS\HTML\HTMLHelper;
-use Joomla\CMS\Component\ComponentHelper;
-use Joomla\CMS\Form\FormField;
+if (!class_exists(PredictionproteamidField::class)) {
+    $fieldFile = JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Field/PredictionproteamidField.php';
 
-/**
- * JFormFieldpredictionproteamid
- *
- * @package
- * @author    diddi
- * @copyright 2014
- * @version   $Id$
- * @access    public
- */
-class JFormFieldpredictionproteamid extends FormField
-{
-	var $_name = 'predictionproteamid';
-
-
-	/**
-	 * JFormFieldpredictionproteamid::getInput()
-	 *
-	 * @return
-	 */
-	protected function getInput()
-	{
-		$db     = sportsmanagementHelper::getDBConnection();
-		$app    = Factory::getApplication();
-		$option = 'com_sportsmanagement';
-
-		$prediction_id = $app->getUserState("$option.prediction_id", '0');
-
-		// Welche tabelle soll genutzt werden
-		$params         = ComponentHelper::getParams('com_sportsmanagement');
-		$database_table = $params->get('cfg_which_database_table');
-
-		$query = $db->getQuery(true);
-		$query->select('tl.id AS id,t.name as teamname');
-		$query->from('#__sportsmanagement_project_team AS tl');
-		$query->join('LEFT', '#__sportsmanagement_season_team_id AS st on tl.team_id = st.id');
-		$query->join('LEFT', '#__sportsmanagement_team AS t on st.team_id = t.id');
-		$query->join('INNER', '#__sportsmanagement_prediction_project as prepro on prepro.project_id = tl.project_id');
-		$query->where('prepro.prediction_id = ' . $prediction_id);
-		$query->group('tl.id');
-
-		$db->setQuery($query);
-
-		$teams = $db->loadObjectList();
-
-		if (!$teams)
-		{
-		}
-
-		$mitems = array();
-
-		foreach ($teams as $team)
-		{
-			$mitems[] = HTMLHelper::_('select.option', $team->id, '&nbsp;' . ' ( ' . $team->teamname . ' ) ');
-		}
-
-		$output = HTMLHelper::_('select.genericlist', $mitems, $this->name . '[]', 'class="inputbox" multiple="multiple" size="' . count($mitems) . '"', 'value', 'text', $this->value, $this->id);
-
-		return $output;
-	}
+    if (is_file($fieldFile)) {
+        require_once $fieldFile;
+    }
 }
 
+if (class_exists(PredictionproteamidField::class) && !class_exists('JFormFieldpredictionproteamid', false)) {
+    class_alias(PredictionproteamidField::class, 'JFormFieldpredictionproteamid');
+}
