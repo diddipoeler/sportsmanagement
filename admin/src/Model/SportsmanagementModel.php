a83847e8f1547d917daed364d10b3ce928962652
diddipoeler <diddipoeler@gmx.de>
2026-08-23 15:11:00 +0200

    Use Joomla database for legacy core table model

--- admin/src/Model/SportsmanagementModel.php
+++ admin/src/Model/SportsmanagementModel.php
@@ -5,6 +5,7 @@ namespace Diddipoeler\Component\SportsManagement\Administrator\Model;
 
 use Diddipoeler\Component\SportsManagement\Administrator\Table\LegacySportsmanagementTable;
 use Joomla\CMS\Factory;
+use Joomla\Database\DatabaseInterface;
 
 /**
  * Native Joomla 5/6 administrator form model for the legacy SportsManagement sample record.
@@ -14,7 +15,9 @@ final class SportsmanagementModel extends SportsManagementAdminModel
     public function getTable($type = 'sportsmanagement', $prefix = 'sportsmanagementTable', $config = [])
     {
         if (strcasecmp((string) $type, 'sportsmanagement') === 0) {
-            return new LegacySportsmanagementTable($this->getDatabase());
+            return new LegacySportsmanagementTable(
+                Factory::getContainer()->get(DatabaseInterface::class)
+            );
         }
 
         return parent::getTable($type, $prefix, $config);
