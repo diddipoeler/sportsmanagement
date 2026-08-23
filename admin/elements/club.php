105634c09ccb5a687ad4f1da3aa9d210bba86b46
diddipoeler <diddipoeler@gmx.de>
2026-08-23 12:34:00 +0200

    Bridge legacy club element to native field

--- admin/elements/club.php
+++ admin/elements/club.php
@@ -1,69 +1,15 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage elements
- * @file       club.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\HTML\HTMLHelper;
-use Joomla\CMS\Language\Text;
-use Joomla\CMS\Factory;
-use Joomla\CMS\Component\ComponentHelper;
-use Joomla\CMS\Form\FormField;
+/** Legacy compatibility bridge for the native Joomla 5/6 club field. */
+\defined('_JEXEC') or die;
 
-/**
- * JFormFieldClub
- *
- * @package
- * @author
- * @copyright diddi
- * @version   2014
- * @access    public
- */
-class JFormFieldClub extends FormField
-{
-	protected $type = 'club';
+use Diddipoeler\Component\SportsManagement\Administrator\Field\ClubField;
 
-	/**
-	 * JFormFieldClub::getInput()
-	 *
-	 * @return
-	 */
-	function getInput()
-	{
-		$db   = sportsmanagementHelper::getDBConnection();
-		$lang = Factory::getLanguage();
-
-		// Welche tabelle soll genutzt werden
-		$params = ComponentHelper::getParams('com_sportsmanagement');
-
-		$extension = "com_sportsmanagement";
-		$source    = JPATH_ADMINISTRATOR . '/components/' . $extension;
-		$lang->load("$extension", JPATH_ADMINISTRATOR, null, false, false)
-		|| $lang->load($extension, $source, null, false, false)
-		|| $lang->load($extension, JPATH_ADMINISTRATOR, $lang->getDefault(), false, false)
-		|| $lang->load($extension, $source, $lang->getDefault(), false, false);
-
-		$query->select('c.id, c.name');
-		$query->from('#__sportsmanagement_club as c');
-		$query->order('name');
-		$db->setQuery($query);
-		$clubs  = $db->loadObjectList();
-		$mitems = array(HTMLHelper::_('select.option', '', Text::_('COM_SPORTSMANAGEMENT_GLOBAL_SELECT')));
-
-		foreach ($clubs as $club)
-		{
-			$mitems[] = HTMLHelper::_('select.option', $club->id, '&nbsp;' . $club->name . ' (' . $club->id . ')');
-		}
-
-		$output = HTMLHelper::_('select.genericlist', $mitems, $this->name, 'class="inputbox" size="1"', 'value', 'text', $this->value, $this->id);
-
-		return $output;
-	}
+if (!class_exists(ClubField::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Field/SportsManagementDatabaseTrait.php';
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Field/SportsManagementListField.php';
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Field/ClubField.php';
 }
 
+if (class_exists(ClubField::class) && !class_exists('JFormFieldClub', false)) {
+    class_alias(ClubField::class, 'JFormFieldClub');
+}
