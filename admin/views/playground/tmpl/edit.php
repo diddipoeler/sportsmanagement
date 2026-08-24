8ade9e2797213de36a2da24eea99f6c25f7ce419
diddipoeler <diddipoeler@gmx.de>
2026-08-24 07:27:41 +0200

    Bridge legacy playground edit layout to native template

---
+++ admin/views/playground/tmpl/edit.php
@@ -0,0 +1,5 @@
+<?php
+/** Legacy compatibility bridge for the native Joomla 5/6 playground edit layout. */
+\defined('_JEXEC') or die;
+
+require JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/tmpl/playground/edit.php';
