4be470e796fa91dc758979b60261dac5e6cd2a2b
diddipoeler <diddipoeler@gmx.de>
2026-08-24 10:34:44 +0200

    Reduce Handball controller to Joomla 6 compatibility bridge

--- site/extensions/jsmhandball/admin/controllers/jsmhandball.php
+++ site/extensions/jsmhandball/admin/controllers/jsmhandball.php
@@ -1,27 +1,15 @@
-<?PHP
+<?php
 /**
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage extension jsmhandball controllers
- * @file       jsmhandball.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * SportsManagement Handball legacy controller compatibility bridge.
  */
+
 defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\Language\Text;
-use Joomla\CMS\Factory;
+
 use Joomla\CMS\MVC\Controller\AdminController;
-use Joomla\CMS\Filesystem\Folder;
-use Joomla\CMS\Filesystem\File;
-use Joomla\CMS\Log\Log;
 
-
+/**
+ * Backward-compatible controller name for the currently inactive Handball extension.
+ */
 class sportsmanagementControllerjsmhandball extends AdminController
 {
-    
 }
-    
-    
-?>    
