553f6ea29c98049f2ec7a7e68feeaaf2d33e3f38
diddipoeler <diddipoeler@gmx.de>
2026-08-24 23:24:38 +0200

    Use native navigation menu helper in fallback entry

--- modules/mod_sportsmanagement_navigation_menu/mod_sportsmanagement_navigation_menu.php
+++ modules/mod_sportsmanagement_navigation_menu/mod_sportsmanagement_navigation_menu.php
@@ -2,17 +2,18 @@
 /** Joomla 5/6 compatibility entry point for the SportsManagement navigation menu. */
 \defined('_JEXEC') or die;
 
-use Diddipoeler\Module\SportsManagementNavigationMenu\Site\Helper\NavigationMenuHelper;
+use Diddipoeler\Module\SportsManagementNavigationMenu\Site\Helper\NativeNavigationMenuHelper;
 use Joomla\CMS\Factory;
 use Joomla\CMS\Helper\ModuleHelper;
 
-if (!class_exists(NavigationMenuHelper::class)) {
+if (!class_exists(NativeNavigationMenuHelper::class)) {
     require_once __DIR__ . '/src/Helper/NavigationMenuHelper.php';
+    require_once __DIR__ . '/src/Helper/NativeNavigationMenuHelper.php';
 }
 
 $app = Factory::getApplication();
 $app->getLanguage()->load('com_sportsmanagement', JPATH_ADMINISTRATOR, null, true);
-$payload = (new NavigationMenuHelper())->getData($params, $app);
+$payload = (new NativeNavigationMenuHelper())->getData($params, $app);
 extract($payload, EXTR_SKIP);
 
 $assets = $app->getDocument()->getWebAssetManager();
