b5bfb4866d0aac98038b65c479795ed1fa8d1b58
diddipoeler <diddipoeler@gmx.de>
2026-08-24 07:33:33 +0200

    Bridge legacy position edit layout to native tmpl

---
+++ admin/views/position/tmpl/edit.php
@@ -0,0 +1,5 @@
+<?php
+/** Legacy compatibility bridge for the native administrator position edit layout. */
+\defined('_JEXEC') or die;
+
+require JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/tmpl/position/edit.php';
