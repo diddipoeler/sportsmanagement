46724f133dc235bf2d5652b6b580d8c450bf0060
diddipoeler <diddipoeler@gmx.de>
2026-08-24 07:49:25 +0200

    Harden native roster position save data

--- admin/src/Model/RosterpositionModel.php
+++ admin/src/Model/RosterpositionModel.php
@@ -3,39 +3,21 @@ namespace Diddipoeler\Component\SportsManagement\Administrator\Model;
 
 \defined('_JEXEC') or die;
 
-use Joomla\CMS\Factory;
-use Joomla\CMS\Language\Text;
-use Joomla\Registry\Registry;
-
 /** Native Joomla 5/6 roster-position form model. */
 final class RosterpositionModel extends SportsManagementAdminModel
 {
     protected function prepareSportsManagementData(array $data): array
     {
-        $post = Factory::getApplication()->getInput()->post->getArray();
-
-        if (array_key_exists('extended', $post) && is_array($post['extended'])) {
-            $parameter = new Registry();
-            $parameter->loadArray($post['extended']);
-            $data['extended'] = (string) $parameter;
-        }
+        $data['players'] = min(11, max(1, (int) ($data['players'] ?? 11)));
 
         if (array_key_exists('short_name', $data)) {
-            $data['alias'] = (string) $data['short_name'];
+            $shortName = strtoupper(trim((string) $data['short_name']));
+            $data['short_name'] = in_array($shortName, ['HOME_POS', 'AWAY_POS'], true)
+                ? $shortName
+                : 'HOME_POS';
+            $data['alias'] = $data['short_name'];
         }
 
         return parent::prepareSportsManagementData($data);
-    }
-
-    protected function afterSportsManagementSave(array $data, int $id, bool $isNew): void
-    {
-        if ($isNew) {
-            Factory::getApplication()->enqueueMessage(
-                Text::plural('COM_SPORTSMANAGEMENT_N_ITEMS_CREATED', $id),
-                'message'
-            );
-        }
-
-        parent::afterSportsManagementSave($data, $id, $isNew);
     }
 }
