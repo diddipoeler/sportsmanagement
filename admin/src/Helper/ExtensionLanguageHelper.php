32418e69d40b8432fcf3e94c3a60053646d5fd4f
diddipoeler <diddipoeler@gmx.de>
2026-08-24 08:56:02 +0200

    Add native admin extension language resolver

---
+++ admin/src/Helper/ExtensionLanguageHelper.php
@@ -0,0 +1,25 @@
+<?php
+namespace Diddipoeler\Component\SportsManagement\Administrator\Helper;
+
+\defined('_JEXEC') or die;
+
+/**
+ * Resolve the optional SportsManagement extension language directory for the
+ * active administrator view without bootstrapping the legacy global helper.
+ */
+final class ExtensionLanguageHelper
+{
+    /** @return list<string> */
+    public static function forView(string $view): array
+    {
+        $view = preg_replace('/[^A-Z0-9_-]/i', '', $view) ?? '';
+
+        if ($view === '') {
+            return [];
+        }
+
+        $path = JPATH_SITE . '/components/com_sportsmanagement/extensions/' . $view;
+
+        return is_dir($path) ? [$view] : [];
+    }
+}
