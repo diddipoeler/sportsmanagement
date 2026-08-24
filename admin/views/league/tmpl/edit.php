e9b26ad0e04b47d6a34cacdd6007d0a1c2e77071
diddipoeler <diddipoeler@gmx.de>
2026-08-24 07:22:00 +0200

    Bridge legacy league edit layout to Joomla 5/6 template

---
+++ admin/views/league/tmpl/edit.php
@@ -0,0 +1,5 @@
+<?php
+/** Legacy compatibility bridge for the native administrator league edit layout. */
+\defined('_JEXEC') or die;
+
+require JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/tmpl/league/edit.php';
