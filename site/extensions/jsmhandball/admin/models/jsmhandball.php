1454bd0b759405878422e09c6a88f1f80b600fa2
diddipoeler <diddipoeler@gmx.de>
2026-08-24 10:34:35 +0200

    Reduce Handball model to Joomla 6 compatibility bridge

--- site/extensions/jsmhandball/admin/models/jsmhandball.php
+++ site/extensions/jsmhandball/admin/models/jsmhandball.php
@@ -1,49 +1,25 @@
 <?php
 /**
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage jsmhandball
- * @file       jsmhandball.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@arcor.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * SportsManagement Handball legacy model compatibility bridge.
  */
+
 defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\Http\HttpFactory;
-use Joomla\CMS\Factory;
-use Joomla\CMS\Language\Text;
+
 use Joomla\CMS\MVC\Model\BaseDatabaseModel;
-use Joomla\CMS\Filesystem\File;
-use Joomla\CMS\Component\ComponentHelper;
-use Joomla\CMS\Log\Log;
 
-$maxImportTime = 480;
-
-if ((int) ini_get('max_execution_time') < $maxImportTime)
-{
-	@set_time_limit($maxImportTime);
-}
-
-$maxImportMemory = '350M';
-
-if ((int) ini_get('memory_limit') < (int) $maxImportMemory)
-{
-	@ini_set('memory_limit', $maxImportMemory);
-}
-
-
+/**
+ * Backward-compatible model name for the currently inactive Handball extension.
+ *
+ * The extension exposes no importer implementation at present. Keeping this
+ * class allows legacy extension loaders to resolve the historic model name
+ * without pulling Joomla 6 deprecated HTTP, filesystem or Factory APIs.
+ */
 class sportsmanagementModeljsmhandball extends BaseDatabaseModel
 {
-	static public $success_text = '';
-	var $storeFailedColor = 'red';
-	var $storeSuccessColor = 'green';
-	var $existingInDbColor = 'orange';
-	var $success_text_teams = '';
-	var $success_text_results = '';
-    
-        
-    
+    public static $success_text = '';
+    public $storeFailedColor = 'red';
+    public $storeSuccessColor = 'green';
+    public $existingInDbColor = 'orange';
+    public $success_text_teams = '';
+    public $success_text_results = '';
 }
-    
-?>
