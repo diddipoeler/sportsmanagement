292abff1ca57f724ad63fa2b229c59c0228e416e
diddipoeler <diddipoeler@gmx.de>
2026-08-24 23:25:55 +0200

    Bridge legacy navigation helper to native routes

--- modules/mod_sportsmanagement_navigation_menu/helper.php
+++ modules/mod_sportsmanagement_navigation_menu/helper.php
@@ -2,12 +2,13 @@
 /** Joomla 5/6 compatibility bridge for mod_sportsmanagement_navigation_menu. */
 \defined('_JEXEC') or die;
 
-use Diddipoeler\Module\SportsManagementNavigationMenu\Site\Helper\NavigationMenuHelper;
+use Diddipoeler\Module\SportsManagementNavigationMenu\Site\Helper\NativeNavigationMenuHelper;
 
-if (!class_exists(NavigationMenuHelper::class)) {
+if (!class_exists(NativeNavigationMenuHelper::class)) {
     require_once __DIR__ . '/src/Helper/NavigationMenuHelper.php';
+    require_once __DIR__ . '/src/Helper/NativeNavigationMenuHelper.php';
 }
 
 if (!class_exists('modsportsmanagementNavigationMenuHelper', false)) {
-    class_alias(NavigationMenuHelper::class, 'modsportsmanagementNavigationMenuHelper');
+    class_alias(NativeNavigationMenuHelper::class, 'modsportsmanagementNavigationMenuHelper');
 }
