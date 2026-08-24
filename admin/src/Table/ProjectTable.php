e804b9efe9c04479c148e36f296c9b6310b8460d
diddipoeler <diddipoeler@gmx.de>
2026-08-24 13:57:25 +0200

    Fix empty project dates for Joomla 5/6

--- admin/src/Table/ProjectTable.php
+++ admin/src/Table/ProjectTable.php
@@ -13,6 +13,27 @@ final class ProjectTable extends SportsManagementTable
         parent::__construct('#__sportsmanagement_project', 'id', $db);
     }
 
+    public function bind($src, $ignore = '')
+    {
+        foreach (['start_date', 'end_date'] as $field) {
+            if (is_array($src) && array_key_exists($field, $src)) {
+                $value = trim((string) $src[$field]);
+
+                if ($value === '' || $value === '0000-00-00') {
+                    $src[$field] = null;
+                }
+            } elseif (is_object($src) && property_exists($src, $field)) {
+                $value = trim((string) $src->{$field});
+
+                if ($value === '' || $value === '0000-00-00') {
+                    $src->{$field} = null;
+                }
+            }
+        }
+
+        return parent::bind($src, $ignore);
+    }
+
     public function check()
     {
         $this->alias = OutputFilter::stringURLSafe((string) $this->name);
