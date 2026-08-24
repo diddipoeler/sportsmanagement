1ce7d24db40f16bd8ec102c2799bdb020016f4fd
diddipoeler <diddipoeler@gmx.de>
2026-08-24 10:36:50 +0200

    Reduce OpenLigaDB controller to Joomla 6 bridge

--- site/extensions/jsmopenligadb/admin/controllers/jsmopenligadb.php
+++ site/extensions/jsmopenligadb/admin/controllers/jsmopenligadb.php
@@ -1,35 +1,12 @@
-<?PHP
+<?php
 /**
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage extension jsmopenligadb controllers
- * @file       jsmopenligadb.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * SportsManagement OpenLigaDB legacy controller compatibility bridge.
  */
-defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\Language\Text;
-use Joomla\CMS\Factory;
-use Joomla\CMS\MVC\Controller\AdminController;
-use Joomla\CMS\Filesystem\Folder;
-use Joomla\CMS\Filesystem\File;
-use Joomla\CMS\Log\Log;
 
-/**
- * sportsmanagementControllerjsmopenligadb
- *
- * @package
- * @author    Dieter Plöger
- * @copyright 2019
- * @version   $Id$
- * @access    public
- */
+defined('_JEXEC') or die('Restricted access');
+
+use Joomla\CMS\MVC\Controller\AdminController;
+
 class sportsmanagementControllerjsmopenligadb extends AdminController
 {
-    
 }
-    
-    
-?>    
