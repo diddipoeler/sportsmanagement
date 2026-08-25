efcacc270fd9d42a19142a411d6a573f4e3b95e2
diddipoeler <diddipoeler@gmx.de>
2026-08-25 19:54:47 +0200

    Use Joomla site tmpl path for roster

--- site/src/View/Roster/HtmlView.php
+++ site/src/View/Roster/HtmlView.php
@@ -26,12 +26,6 @@ final class HtmlView extends SportsManagementProjectHtmlView
     public array $playerstats = [];
     public array $lists = [];
 
-    public function __construct($config = [])
-    {
-        $config['template_path'] = JPATH_SITE . '/components/com_sportsmanagement/views/roster/tmpl';
-        parent::__construct($config);
-    }
-
     protected function prepareView(): void
     {
         /** @var RosterModel $model */
