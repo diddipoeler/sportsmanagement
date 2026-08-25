8f5b2ccf5a2a8668ddc5ce9dc7b0411f0508e8b5
diddipoeler <diddipoeler@gmx.de>
2026-08-25 16:46:50 +0200

    Use Joomla 5 router compatibility wrapper

--- admin/src/Extension/SportsManagementComponent.php
+++ admin/src/Extension/SportsManagementComponent.php
@@ -10,10 +10,13 @@ namespace Diddipoeler\Component\SportsManagement\Administrator\Extension;
 
 defined('_JEXEC') or die;
 
+use Joomla\CMS\Application\CMSApplicationInterface;
+use Joomla\CMS\Component\Router\RouterInterface;
 use Joomla\CMS\Component\Router\RouterServiceInterface;
 use Joomla\CMS\Component\Router\RouterServiceTrait;
 use Joomla\CMS\Extension\MVCComponent;
 use Joomla\CMS\HTML\HTMLRegistryAwareTrait;
+use Joomla\CMS\Menu\AbstractMenu;
 
 /**
  * Component extension class for the Joomla 5/6 architecture.
@@ -25,5 +28,29 @@ use Joomla\CMS\HTML\HTMLRegistryAwareTrait;
 final class SportsManagementComponent extends MVCComponent implements RouterServiceInterface
 {
     use HTMLRegistryAwareTrait;
-    use RouterServiceTrait;
+    use RouterServiceTrait {
+        createRouter as private createFactoryRouter;
+    }
+
+    /**
+     * Joomla 5 keeps a few legacy routing semantics around menu parsing which
+     * are no longer relevant on Joomla 6. Use the compatibility wrapper there,
+     * while Joomla 6 continues to use the normal RouterFactory service.
+     */
+    public function createRouter(CMSApplicationInterface $application, AbstractMenu $menu): RouterInterface
+    {
+        if (version_compare(JVERSION, '6.0.0', 'lt')) {
+            $routerFile = JPATH_SITE . '/components/com_sportsmanagement/router.php';
+
+            if (is_file($routerFile)) {
+                require_once $routerFile;
+            }
+
+            if (class_exists('SportsmanagementRouter', false)) {
+                return new \SportsmanagementRouter($application, $menu);
+            }
+        }
+
+        return $this->createFactoryRouter($application, $menu);
+    }
 }
