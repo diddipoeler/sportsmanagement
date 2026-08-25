809defa52a779238154717c37b9df6e4d4c60f58
diddipoeler <diddipoeler@gmx.de>
2026-08-25 20:00:33 +0200

    Use Joomla site tmpl path for about

--- site/src/View/About/HtmlView.php
+++ site/src/View/About/HtmlView.php
@@ -10,12 +10,6 @@ final class HtmlView extends SportsManagementHtmlView
 {
     public object $about;
 
-    public function __construct($config = [])
-    {
-        $config['template_path'] = JPATH_SITE . '/components/com_sportsmanagement/views/about/tmpl';
-        parent::__construct($config);
-    }
-
     public function display($tpl = null)
     {
         $this->about = $this->getModel()->getAbout();
