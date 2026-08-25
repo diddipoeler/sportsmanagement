a9113e5a6e8fe0fe6158cb8197cc1ba8263b067e
diddipoeler <diddipoeler@gmx.de>
2026-08-25 20:00:54 +0200

    Use Joomla site tmpl path for allclubs

--- site/src/View/Allclubs/HtmlView.php
+++ site/src/View/Allclubs/HtmlView.php
@@ -27,8 +27,8 @@ final class HtmlView extends SportsManagementHtmlView
 
     public function __construct($config = [])
     {
-        $config['template_path'] = JPATH_SITE . '/components/com_sportsmanagement/views/allclubs/tmpl';
         parent::__construct($config);
+        $this->addTemplatePath(JPATH_SITE . '/components/com_sportsmanagement/tmpl/globalviews');
         $this->loadPresentationDependencies();
     }
 
