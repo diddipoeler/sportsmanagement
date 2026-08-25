0e789a152660a3749765a6b493f4ed206867b5bd
diddipoeler <diddipoeler@gmx.de>
2026-08-25 20:01:11 +0200

    Use Joomla site tmpl path for allpersons

--- site/src/View/Allpersons/HtmlView.php
+++ site/src/View/Allpersons/HtmlView.php
@@ -28,8 +28,8 @@ final class HtmlView extends SportsManagementHtmlView
 
     public function __construct($config = [])
     {
-        $config['template_path'] = JPATH_SITE . '/components/com_sportsmanagement/views/allpersons/tmpl';
         parent::__construct($config);
+        $this->addTemplatePath(JPATH_SITE . '/components/com_sportsmanagement/tmpl/globalviews');
         $this->loadPresentationDependencies();
     }
 
