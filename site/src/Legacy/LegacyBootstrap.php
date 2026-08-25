798da890f4359d6ef59ede6af7bd9a8c8e32102c
diddipoeler <diddipoeler@gmx.de>
2026-08-25 12:19:30 +0200

    Remove obsolete teamplan presentation bootstrap

--- site/src/Legacy/LegacyBootstrap.php
+++ site/src/Legacy/LegacyBootstrap.php
@@ -11,7 +11,6 @@ use Joomla\CMS\Uri\Uri;
 final class LegacyBootstrap
 {
     private static bool $booted = false;
-    private static bool $presentationBooted = false;
 
     public static function boot(): void
     {
@@ -46,55 +45,6 @@ final class LegacyBootstrap
 
         self::registerLegacyIncludePaths();
         self::initialiseConstants();
-    }
-
-    /**
-     * Load only the historical presentation helpers needed by a native view.
-     *
-     * Teamplan owns its project/model data natively, so loading models.project
-     * here would reintroduce a large Joomla 3-era data layer solely because
-     * the historical tmpl files still use a few global helper class names.
-     */
-    public static function bootPresentationForView(string $view): void
-    {
-        $view = strtolower($view);
-
-        if ($view !== 'teamplan') {
-            self::bootForView($view);
-            return;
-        }
-
-        if (!self::$presentationBooted) {
-            self::$presentationBooted = true;
-
-            if (!\defined('JSM_PATH')) {
-                \define('JSM_PATH', 'components/com_sportsmanagement');
-            }
-
-            if (!class_exists('sportsmanagementHelper')) {
-                self::import('helpers.sportsmanagement', JPATH_ADMINISTRATOR);
-            }
-
-            foreach ([
-                ['libraries.sportsmanagement.view', JPATH_SITE],
-                ['libraries.sportsmanagement.model', JPATH_SITE],
-                ['libraries.sportsmanagement.controller', JPATH_SITE],
-                ['libraries.sportsmanagement.table', JPATH_ADMINISTRATOR],
-                ['libraries.sportsmanagement.formbehavior2', JPATH_ADMINISTRATOR],
-                ['helpers.route', JPATH_SITE],
-                ['helpers.html', JPATH_SITE],
-                ['helpers.countries', JPATH_SITE],
-                ['helpers.simpleGMapGeocoder', JPATH_SITE],
-            ] as [$path, $base]) {
-                self::import($path, $base);
-            }
-
-            self::registerLegacyIncludePaths();
-            self::initialiseConstants();
-        }
-
-        self::import('helpers.comments', JPATH_SITE);
-        self::setConstant('COM_SPORTSMANAGEMENT_SHOW_VIEW', ucfirst($view));
     }
 
     public static function bootForView(string $view): void
