cdf2bc3a643bfa0ebd8a0fd65f827803adb34f8e
diddipoeler <diddipoeler@gmx.de>
2026-08-25 09:33:30 +0200

    Migrate sorted teamplan routes to Joomla 5/6 helper

--- site/views/teamplan/tmpl/default_plan_sorted_by_date.php
+++ site/views/teamplan/tmpl/default_plan_sorted_by_date.php
@@ -9,6 +9,7 @@
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 defined('_JEXEC') or die('Restricted access');
+use Diddipoeler\Component\SportsManagement\Site\Helper\SiteRouteHelper;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Uri\Uri;
@@ -263,7 +264,7 @@ if (!empty($this->matches))
 					$routeparameter['s']                  = Factory::getApplication()->input->getInt('s', 0);
 					$routeparameter['p']                  = $this->project->slug;
 					$routeparameter['mid']                = $match->id;
-					$history_link                         = sportsmanagementHelperRoute::getSportsmanagementRoute('nextmatch', $routeparameter);
+					$history_link                         = SiteRouteHelper::view('nextmatch', $routeparameter);
 				}
 
 				if ($this->config['show_teamplan_link'])
@@ -276,12 +277,12 @@ if (!empty($this->matches))
 					$routeparameter['division']           = $match->division_slug;
 					$routeparameter['mode']               = 0;
 					$routeparameter['ptid']               = 0;
-					$homelink                             = sportsmanagementHelperRoute::getSportsmanagementRoute('teamplan', $routeparameter);
+					$homelink                             = SiteRouteHelper::view('teamplan', $routeparameter);
 					$routeparameter['tid']                = $guestteam->team_slug;
 					$routeparameter['division']           = $match->division_slug;
 					$routeparameter['mode']               = 0;
 					$routeparameter['ptid']               = 0;
-					$awaylink                             = sportsmanagementHelperRoute::getSportsmanagementRoute('teamplan', $routeparameter);
+					$awaylink                             = SiteRouteHelper::view('teamplan', $routeparameter);
 				}
 				else
 				{
@@ -464,11 +465,11 @@ if (!empty($this->matches))
 					$routeparameter['mid']                = $match->match_slug;
 					if (isset($match->team1_result))
 					{
-						$link = sportsmanagementHelperRoute::getSportsmanagementRoute('matchreport', $routeparameter);
+						$link = SiteRouteHelper::view('matchreport', $routeparameter);
 					}
 					else
 					{
-						$link = sportsmanagementHelperRoute::getSportsmanagementRoute('nextmatch', $routeparameter);
+						$link = SiteRouteHelper::view('nextmatch', $routeparameter);
 					}
 
 					$ResultsTooltipTitle = $result;
@@ -646,8 +647,7 @@ usort($match->referees, function ($a, $b) {
 											$routeparameter['s']                  = Factory::getApplication()->input->getInt('s', 0);
 											$routeparameter['p']                  = $this->project->slug;
 											$routeparameter['pid']                = $match->referees[$i]->referee_id;
-											$link                                 = sportsmanagementHelperRoute::getSportsmanagementRoute('referee', $routeparameter);
-//$link=sportsmanagementHelperRoute::getRefereeRoute($this->project->slug,$match->referees[$i]->referee_id,3);
+											$link                                 = SiteRouteHelper::view('referee', $routeparameter);
 											$ref = HTMLHelper::link($link, $ref);
 										}
 										$output .= $ref;
@@ -714,7 +714,7 @@ usort($match->referees, function ($a, $b) {
 									$href_text = Text::_('COM_SPORTSMANAGEMENT_TEAMPLAN_VIEW_MATCHREPORT');
 								}
 
-								$link       = sportsmanagementHelperRoute::getSportsmanagementRoute('matchreport', $routeparameter);
+								$link       = SiteRouteHelper::view('matchreport', $routeparameter);
 								$viewReport = HTMLHelper::link($link, $href_text);
 								echo $viewReport;
 							}
@@ -728,7 +728,7 @@ usort($match->referees, function ($a, $b) {
 								{
 									$href_text = Text::_('COM_SPORTSMANAGEMENT_TEAMPLAN_VIEW_MATCHPREVIEW');
 								}
-								$link        = sportsmanagementHelperRoute::getSportsmanagementRoute('nextmatch', $routeparameter);
+								$link        = SiteRouteHelper::view('nextmatch', $routeparameter);
 								$viewPreview = HTMLHelper::link($link, $href_text);
 								echo $viewPreview;
 							}
