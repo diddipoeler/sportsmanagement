5174c63fda817b313df067eaf69ba41dfdff54d6
diddipoeler <diddipoeler@gmx.de>
2026-08-26 12:38:38 +0200

    Add native editmatch list controls

---
+++ site/assets/js/editmatch-lists.js
@@ -0,0 +1,91 @@
+(function () {
+    'use strict';
+
+    function getSelect(id) {
+        const element = document.getElementById(id);
+        return element instanceof HTMLSelectElement ? element : null;
+    }
+
+    function markChanged() {
+        const field = document.getElementById('changes_check');
+
+        if (field) {
+            field.value = '1';
+        }
+    }
+
+    function moveListItems(sourceId, destinationId) {
+        const source = getSelect(sourceId);
+        const destination = getSelect(destinationId);
+
+        if (!source || !destination) {
+            return false;
+        }
+
+        const selected = Array.from(source.options).filter((option) => option.selected);
+
+        selected.forEach((option) => destination.appendChild(option));
+
+        if (selected.length > 0) {
+            markChanged();
+        }
+
+        return false;
+    }
+
+    function moveUp(selectId) {
+        const select = getSelect(selectId);
+
+        if (!select) {
+            return false;
+        }
+
+        let changed = false;
+
+        for (let index = 1; index < select.options.length; index += 1) {
+            const option = select.options[index];
+            const previous = select.options[index - 1];
+
+            if (option.selected && !previous.selected) {
+                select.insertBefore(option, previous);
+                changed = true;
+            }
+        }
+
+        if (changed) {
+            markChanged();
+        }
+
+        return false;
+    }
+
+    function moveDown(selectId) {
+        const select = getSelect(selectId);
+
+        if (!select) {
+            return false;
+        }
+
+        let changed = false;
+
+        for (let index = select.options.length - 2; index >= 0; index -= 1) {
+            const option = select.options[index];
+            const next = select.options[index + 1];
+
+            if (option.selected && !next.selected) {
+                select.insertBefore(next, option);
+                changed = true;
+            }
+        }
+
+        if (changed) {
+            markChanged();
+        }
+
+        return false;
+    }
+
+    window.move_list_items = moveListItems;
+    window.move_up = moveUp;
+    window.move_down = moveDown;
+}());
