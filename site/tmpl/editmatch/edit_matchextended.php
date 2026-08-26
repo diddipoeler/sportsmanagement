49336cc4bd22865bdf20bef2249da97a1a9b231a
diddipoeler <diddipoeler@gmx.de>
2026-08-26 07:54:46 +0200

    Guard extended match form template for Joomla 5/6

--- site/tmpl/editmatch/edit_matchextended.php
+++ site/tmpl/editmatch/edit_matchextended.php
@@ -1,8 +1,6 @@
 <?php
 /**
- *
  * SportsManagement ein Programm zur Verwaltung für Sportarten
- *
  * @version    1.0.05
  * @package    Sportsmanagement
  * @subpackage editmatch
@@ -11,30 +9,26 @@
  * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
-
 defined('_JEXEC') or die('Restricted access');
 
 use Joomla\CMS\Language\Text;
 
-foreach ($this->extended->getFieldsets() as $fieldset)
-{
-	?>
+if (!$this->extended) {
+    echo Text::_('COM_SPORTSMANAGEMENT_GLOBAL_NO_PARAMS');
+    return;
+}
+?>
+<?php foreach ($this->extended->getFieldsets() as $fieldset) : ?>
     <fieldset class="adminform">
         <legend><?php echo Text::_($fieldset->name); ?></legend>
-		<?php
-		$fields = $this->extended->getFieldset($fieldset->name);
-
-		if (!count($fields))
-		{
-			echo Text::_('COM_SPORTSMANAGEMENT_GLOBAL_NO_PARAMS');
-		}
-
-		foreach ($fields as $field)
-		{
-			echo $field->label;
-			echo $field->input;
-		}
-		?>
+        <?php $fields = $this->extended->getFieldset($fieldset->name); ?>
+        <?php if ($fields === []) : ?>
+            <?php echo Text::_('COM_SPORTSMANAGEMENT_GLOBAL_NO_PARAMS'); ?>
+        <?php else : ?>
+            <?php foreach ($fields as $field) : ?>
+                <?php echo $field->label; ?>
+                <?php echo $field->input; ?>
+            <?php endforeach; ?>
+        <?php endif; ?>
     </fieldset>
-	<?php
-}
+<?php endforeach; ?>
