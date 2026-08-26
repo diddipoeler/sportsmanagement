0443182b9321fb39d833d941bd7a56a13e25cabb
diddipoeler <diddipoeler@gmx.de>
2026-08-26 07:54:37 +0200

    Clean up match details template for Joomla 5/6

--- site/tmpl/editmatch/edit_matchdetails.php
+++ site/tmpl/editmatch/edit_matchdetails.php
@@ -1,8 +1,6 @@
 <?php
 /**
- *
  * SportsManagement ein Programm zur Verwaltung für Sportarten
- *
  * @version    1.0.05
  * @package    Sportsmanagement
  * @subpackage editmatch
@@ -11,29 +9,20 @@
  * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
-
 defined('_JEXEC') or die('Restricted access');
 
 use Joomla\CMS\Language\Text;
-
 ?>
 <fieldset class="adminform">
-    <legend><?php echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_MATCH_F_MD'); ?>
-    </legend>
+    <legend><?php echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_MATCH_F_MD'); ?></legend>
     <table class="admintable">
-		<?php
-		foreach ($this->form->getFieldset('matchdetails') as $field)
-			:
-			if ($field->type == 'Radio')
-			{
-			}
-
-			?>
+        <tbody>
+        <?php foreach ($this->form->getFieldset('matchdetails') as $field) : ?>
             <tr>
                 <td class="key"><?php echo $field->label; ?></td>
                 <td><?php echo $field->input; ?></td>
             </tr>
-		<?php endforeach; ?>
+        <?php endforeach; ?>
+        </tbody>
     </table>
-</fieldset>	
-
+</fieldset>
