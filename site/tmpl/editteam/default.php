6da40634df27292c670a4403105d8e400463e415
diddipoeler <diddipoeler@gmx.de>
2026-08-26 22:20:00 +0200

    Harden frontend team editor form

--- site/tmpl/editteam/default.php
+++ site/tmpl/editteam/default.php
@@ -5,9 +5,17 @@ defined('_JEXEC') or die('Restricted access');
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
 
+$escape = static fn ($value): string => htmlspecialchars((string) $value, ENT_QUOTES, 'UTF-8');
 $fieldsets = $this->form->getFieldsets();
 ?>
-<form name="adminForm" id="adminForm" class="form-validate" method="post" action="<?php echo $this->uri->toString(); ?>" data-jsm-editteam-form>
+<form
+    name="adminForm"
+    id="adminForm"
+    class="form-validate"
+    method="post"
+    action="<?php echo $escape($this->uri->toString()); ?>"
+    data-jsm-editteam-form
+>
     <fieldset class="adminform">
         <div class="btn-toolbar justify-content-end gap-2">
             <button type="button" class="btn btn-success" data-jsm-task="editteam.apply">
@@ -20,7 +28,7 @@ $fieldsets = $this->form->getFieldsets();
                 <?php echo Text::_('JCANCEL'); ?>
             </button>
         </div>
-        <legend><?php echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_TEAM_EDIT') . ' ' . $this->item->name; ?></legend>
+        <legend><?php echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_TEAM_EDIT') . ' ' . $escape($this->item->name ?? ''); ?></legend>
     </fieldset>
 
     <?php echo HTMLHelper::_('bootstrap.startTabSet', 'myTab', ['active' => 'details']); ?>
@@ -34,10 +42,10 @@ $fieldsets = $this->form->getFieldsets();
     <?php echo HTMLHelper::_('bootstrap.endTabSet'); ?>
 
     <input type="hidden" name="option" value="com_sportsmanagement">
-    <input type="hidden" name="id" value="<?php echo (int) $this->item->id; ?>">
-    <input type="hidden" name="p" value="<?php echo $this->projectId; ?>">
-    <input type="hidden" name="tid" value="<?php echo $this->teamId; ?>">
-    <input type="hidden" name="ptid" value="<?php echo $this->projectTeamId; ?>">
+    <input type="hidden" name="id" value="<?php echo (int) ($this->item->id ?? 0); ?>">
+    <input type="hidden" name="p" value="<?php echo (int) $this->projectId; ?>">
+    <input type="hidden" name="tid" value="<?php echo (int) $this->teamId; ?>">
+    <input type="hidden" name="ptid" value="<?php echo (int) $this->projectTeamId; ?>">
     <input type="hidden" name="task" value="">
     <?php echo HTMLHelper::_('form.token') . "\n"; ?>
 </form>
