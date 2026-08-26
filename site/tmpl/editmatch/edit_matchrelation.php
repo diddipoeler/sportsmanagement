0d6ef8054863d77f91ca81a37d061179a766c1e2
diddipoeler <diddipoeler@gmx.de>
2026-08-26 07:55:31 +0200

    Use Joomla routing in match relation template

--- site/tmpl/editmatch/edit_matchrelation.php
+++ site/tmpl/editmatch/edit_matchrelation.php
@@ -1,8 +1,6 @@
 <?php
 /**
- *
  * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- *
  * @version    1.0.05
  * @package    Sportsmanagement
  * @subpackage editmatch
@@ -11,52 +9,44 @@
  * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
-
 defined('_JEXEC') or die('Restricted access');
 
 use Joomla\CMS\Language\Text;
+use Joomla\CMS\Router\Route;
 
+$oldMatchId = (int) ($this->match->old_match_id ?? 0);
+$newMatchId = (int) ($this->match->new_match_id ?? 0);
 ?>
 <fieldset class="adminform">
-    <legend>
-		<?php
-		echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_MATCH_F_MREL_DETAILS');
-		?>
-    </legend>
-    <br/>
-    <table class='admintable'>
+    <legend><?php echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_MATCH_F_MREL_DETAILS'); ?></legend>
+    <table class="admintable">
+        <tbody>
         <tr>
-            <td align="right" class="key">
-                <label>
-					<?php
-					echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_MATCH_F_MREL_OLD_ID');
-					?>
-                </label>
+            <td class="key" style="text-align:right;">
+                <label><?php echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_MATCH_F_MREL_OLD_ID'); ?></label>
             </td>
-            <td align="left">
-				<?php echo $this->lists['old_match']; ?>
-				<?php if ($this->match->old_match_id > 0) : ?>
-                    <a href="index.php?option=com_sportsmanagement&tmpl=component&controller=match&task=edit&cid[]=<?php echo $this->match->old_match_id ?>">Match
-                        Link</a>
-				<?php endif ?>
+            <td style="text-align:left;">
+                <?php echo $this->lists['old_match']; ?>
+                <?php if ($oldMatchId > 0) : ?>
+                    <a href="<?php echo Route::_('index.php?option=com_sportsmanagement&tmpl=component&controller=match&task=edit&cid[]=' . $oldMatchId); ?>">
+                        <?php echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_MATCHES_MATCH'); ?>
+                    </a>
+                <?php endif; ?>
             </td>
         </tr>
         <tr>
-            <td align="right" class="key">
-                <label>
-					<?php
-					echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_MATCH_F_MREL_NEW_ID');
-					?>
-                </label>
+            <td class="key" style="text-align:right;">
+                <label><?php echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_MATCH_F_MREL_NEW_ID'); ?></label>
             </td>
-            <td align="left">
-				<?php echo $this->lists['new_match']; ?>
-				<?php if ($this->match->new_match_id > 0) : ?>
-                    <a href="index.php?option=com_sportsmanagement&tmpl=component&controller=match&task=edit&cid[]=<?php echo $this->match->new_match_id ?>">Match
-                        Link</a>
-				<?php endif ?>
+            <td style="text-align:left;">
+                <?php echo $this->lists['new_match']; ?>
+                <?php if ($newMatchId > 0) : ?>
+                    <a href="<?php echo Route::_('index.php?option=com_sportsmanagement&tmpl=component&controller=match&task=edit&cid[]=' . $newMatchId); ?>">
+                        <?php echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_MATCHES_MATCH'); ?>
+                    </a>
+                <?php endif; ?>
             </td>
         </tr>
-
+        </tbody>
     </table>
 </fieldset>
