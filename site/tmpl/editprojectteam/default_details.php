230b9ff291b1b97524d7bf68c7ca4a71eaf493e2
diddipoeler <diddipoeler@gmx.de>
2026-08-26 12:49:39 +0200

    Clean frontend project-team details template

--- site/tmpl/editprojectteam/default_details.php
+++ site/tmpl/editprojectteam/default_details.php
@@ -1,36 +1,17 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage editperson
- * @file       deafult_details.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
+/** Joomla 5/6 frontend project-team details template. */
 defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\Language\Text;
 
-/** Get the form fieldsets. */
-$fieldsets = $this->form->getFieldsets();
+use Joomla\CMS\Language\Text;
 ?>
 <fieldset class="adminform">
-    <legend><?php echo Text::_('COM_SPORTSMANAGEMENT_TABS_DETAILS'); ?>
-    </legend>
+    <legend><?php echo Text::_('COM_SPORTSMANAGEMENT_TABS_DETAILS'); ?></legend>
     <table class="admintable">
-		<?php
-
-		foreach ($this->form->getFieldset('details') as $field)
-			:
-			if ($field->type == 'Radio')
-			{
-			}
-			?>
+        <?php foreach ($this->form->getFieldset('details') as $field) : ?>
             <tr>
                 <td class="key"><?php echo $field->label; ?></td>
                 <td><?php echo $field->input; ?></td>
             </tr>
-		<?php endforeach; ?>
+        <?php endforeach; ?>
     </table>
 </fieldset>
