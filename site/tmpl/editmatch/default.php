51f6ebdadb30f16a7aae6c32c660b66133850905
diddipoeler <diddipoeler@gmx.de>
2026-08-26 22:10:44 +0200

    Modernize default frontend match editor

--- site/tmpl/editmatch/default.php
+++ site/tmpl/editmatch/default.php
@@ -13,36 +13,32 @@ defined('_JEXEC') or die('Restricted access');
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
 
-$this->getDocument()->getWebAssetManager()->useScript('form.validate');
-$fieldsets = $this->form->getFieldsets();
+$escape = static fn ($value): string => htmlspecialchars((string) $value, ENT_QUOTES, 'UTF-8');
+$altDecision = (int) ($this->match->alt_decision ?? 0);
+$assets = $this->getDocument()->getWebAssetManager();
+$assets->useScript('form.validate');
+$assets->registerAndUseScript(
+    'com_sportsmanagement.editmatch-form',
+    'components/com_sportsmanagement/assets/js/editmatch-form.js'
+);
 ?>
-<script>
-Joomla.submitbutton = function (task) {
-    const form = document.getElementById('editperson');
-
-    if (!form) {
-        return;
-    }
-
-    if (
-        task === 'editmatch.cancel'
-        || !document.formvalidator
-        || document.formvalidator.isValid(form)
-    ) {
-        Joomla.submitform(task, form);
-    }
-};
-</script>
-<form name="editperson" id="editperson" class="form-validate" method="post" action="<?php echo $this->uri->toString(); ?>">
+<form
+    name="editperson"
+    id="editperson"
+    class="form-validate"
+    method="post"
+    action="<?php echo $escape($this->uri->toString()); ?>"
+    data-editmatch-form
+>
     <fieldset class="adminform">
         <div class="fltrt">
-            <button type="button" onclick="Joomla.submitbutton('editmatch.apply');">
+            <button type="button" data-editmatch-submit-task="editmatch.apply">
                 <?php echo Text::_('COM_SPORTSMANAGEMENT_GLOBAL_SAVE'); ?>
             </button>
-            <button type="button" onclick="Joomla.submitbutton('editmatch.save');">
+            <button type="button" data-editmatch-submit-task="editmatch.save">
                 <?php echo Text::_('COM_SPORTSMANAGEMENT_GLOBAL_SAVECLOSE'); ?>
             </button>
-            <button type="button" onclick="Joomla.submitbutton('editmatch.cancel');">
+            <button type="button" data-editmatch-submit-task="editmatch.cancel">
                 <?php echo Text::_('JCANCEL'); ?>
             </button>
         </div>
@@ -51,18 +47,21 @@ Joomla.submitbutton = function (task) {
     <fieldset class="adminform">
         <legend><?php echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_MATCH_F_MD'); ?></legend>
         <table class="admintable">
+            <tbody>
             <?php foreach ($this->form->getFieldset('matchdetails') as $field) : ?>
                 <tr>
                     <td class="key"><?php echo $field->label; ?></td>
                     <td><?php echo $field->input; ?></td>
                 </tr>
             <?php endforeach; ?>
+            </tbody>
         </table>
     </fieldset>
 
     <fieldset class="adminform">
         <legend><?php echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_MATCH_F_AD'); ?></legend>
         <table class="admintable">
+            <tbody>
             <tr>
                 <td class="key"><?php echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_MATCH_F_AD_INCL'); ?></td>
                 <td colspan="3"><?php echo $this->lists['count_result']; ?></td>
@@ -71,10 +70,10 @@ Joomla.submitbutton = function (task) {
                 <td class="key"><?php echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_MATCH_F_AD_SUB_DEC'); ?></td>
                 <td colspan="3">
                     <select name="alt_decision" id="alt_decision">
-                        <option value="0"<?php echo ((int) $this->match->alt_decision === 0) ? ' selected="selected"' : ''; ?>>
+                        <option value="0"<?php echo $altDecision === 0 ? ' selected="selected"' : ''; ?>>
                             <?php echo Text::_('JNO'); ?>
                         </option>
-                        <option value="1"<?php echo ((int) $this->match->alt_decision === 1) ? ' selected="selected"' : ''; ?>>
+                        <option value="1"<?php echo $altDecision === 1 ? ' selected="selected"' : ''; ?>>
                             <?php echo Text::_('JYES'); ?>
                         </option>
                     </select>
@@ -82,10 +81,17 @@ Joomla.submitbutton = function (task) {
             </tr>
             <tr>
                 <td colspan="4">
-                    <div id="alt_decision_enter" style="display:<?php echo ((int) $this->match->alt_decision === 0) ? 'none' : 'block'; ?>">
-                        <table class="adminForm" cellpadding="0" cellspacing="7" border="0">
+                    <div id="alt_decision_enter"<?php echo $altDecision === 0 ? ' hidden' : ''; ?>>
+                        <table class="adminForm" cellspacing="7">
+                            <tbody>
                             <tr>
-                                <td class="key"><?php echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_MATCH_F_AD_NEW_SCORE') . ' ' . $this->match->hometeam; ?></td>
+                                <td class="key">
+                                    <?php echo $escape(
+                                        Text::_('COM_SPORTSMANAGEMENT_ADMIN_MATCH_F_AD_NEW_SCORE')
+                                        . ' '
+                                        . ($this->match->hometeam ?? '')
+                                    ); ?>
+                                </td>
                                 <td>
                                     <input
                                         type="text"
@@ -93,13 +99,19 @@ Joomla.submitbutton = function (task) {
                                         id="team1_result_decision"
                                         name="team1_result_decision"
                                         size="4"
-                                        value="<?php echo ((int) $this->match->alt_decision === 1) ? ($this->match->team1_result_decision ?? 'X') : ''; ?>"
-                                        <?php echo ((int) $this->match->alt_decision === 0) ? 'disabled' : ''; ?>
+                                        value="<?php echo $escape($altDecision === 1 ? ($this->match->team1_result_decision ?? 'X') : ''); ?>"
+                                        <?php echo $altDecision === 0 ? 'disabled' : ''; ?>
                                     >
                                 </td>
                             </tr>
                             <tr>
-                                <td class="key"><?php echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_MATCH_F_AD_NEW_SCORE') . ' ' . $this->match->awayteam; ?></td>
+                                <td class="key">
+                                    <?php echo $escape(
+                                        Text::_('COM_SPORTSMANAGEMENT_ADMIN_MATCH_F_AD_NEW_SCORE')
+                                        . ' '
+                                        . ($this->match->awayteam ?? '')
+                                    ); ?>
+                                </td>
                                 <td>
                                     <input
                                         type="text"
@@ -107,8 +119,8 @@ Joomla.submitbutton = function (task) {
                                         id="team2_result_decision"
                                         name="team2_result_decision"
                                         size="4"
-                                        value="<?php echo ((int) $this->match->alt_decision === 1) ? ($this->match->team2_result_decision ?? 'X') : ''; ?>"
-                                        <?php echo ((int) $this->match->alt_decision === 0) ? 'disabled' : ''; ?>
+                                        value="<?php echo $escape($altDecision === 1 ? ($this->match->team2_result_decision ?? 'X') : ''); ?>"
+                                        <?php echo $altDecision === 0 ? 'disabled' : ''; ?>
                                     >
                                 </td>
                             </tr>
@@ -121,8 +133,8 @@ Joomla.submitbutton = function (task) {
                                         id="decision_info"
                                         name="decision_info"
                                         size="30"
-                                        value="<?php echo ((int) $this->match->alt_decision === 1) ? htmlspecialchars((string) ($this->match->decision_info ?? ''), ENT_QUOTES, 'UTF-8') : ''; ?>"
-                                        <?php echo ((int) $this->match->alt_decision === 0) ? 'disabled' : ''; ?>
+                                        value="<?php echo $escape($altDecision === 1 ? ($this->match->decision_info ?? '') : ''); ?>"
+                                        <?php echo $altDecision === 0 ? 'disabled' : ''; ?>
                                     >
                                 </td>
                             </tr>
@@ -130,10 +142,12 @@ Joomla.submitbutton = function (task) {
                                 <td class="key"><?php echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_MATCH_F_AD_TEAM_WON'); ?></td>
                                 <td><?php echo $this->lists['team_won']; ?></td>
                             </tr>
+                            </tbody>
                         </table>
                     </div>
                 </td>
             </tr>
+            </tbody>
         </table>
     </fieldset>
 
