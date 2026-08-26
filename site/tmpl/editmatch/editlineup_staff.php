a439c6322332639d9a1e06743069d8d2f0b6b84a
diddipoeler <diddipoeler@gmx.de>
2026-08-26 07:53:48 +0200

    Clean up site lineup staff template for Joomla 5/6

--- site/tmpl/editmatch/editlineup_staff.php
+++ site/tmpl/editmatch/editlineup_staff.php
@@ -10,85 +10,74 @@
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\Language\Text;
 
+use Joomla\CMS\Language\Text;
 ?>
 <fieldset class="adminform">
     <legend><?php echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_MATCH_ELUS'); ?></legend>
-    <table class='adminlist'>
+    <table class="adminlist">
         <thead>
         <tr>
-            <th>
-				<?php echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_MATCH_ELUS_STAFF'); ?>
-            </th>
-            <th>
-				<?php echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_MATCH_ELUS_ASSIGNED'); ?>
-            </th>
+            <th><?php echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_MATCH_ELUS_STAFF'); ?></th>
+            <th><?php echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_MATCH_ELUS_ASSIGNED'); ?></th>
         </tr>
         </thead>
+        <tbody>
         <tr>
-            <td colspan="2">
-        </tr>
-        <tr>
-            <td style="text-align:center; vertical-align:middle; ">
-				<?php
-				// Echo select list of non assigned players from team roster
-				if (isset($this->lists['team_staffs']))
-				{
-					echo $this->lists['team_staffs'];
-				}
-				else
-				{
-					echo Text::_('JGLOBAL_NO_MATCHING_RESULTS');
-				}
-				?>
+            <td style="text-align:center; vertical-align:middle;">
+                <?php
+                if (isset($this->lists['team_staffs'])) {
+                    echo $this->lists['team_staffs'];
+                } else {
+                    echo Text::_('JGLOBAL_NO_MATCHING_RESULTS');
+                }
+                ?>
             </td>
-            <td style="text-align:center; vertical-align:top; ">
-                <table border='0'>
-					<?php
-					if (isset($this->staffpositions))
-					{
-						foreach ($this->staffpositions AS $position_id => $pos)
-						{
-							?>
+            <td style="text-align:center; vertical-align:top;">
+                <table>
+                    <tbody>
+                    <?php if (isset($this->staffpositions)) : ?>
+                        <?php foreach ($this->staffpositions as $positionId => $pos) : ?>
                             <tr>
-                                <td style='text-align:center; vertical-align:middle; '>
-                                    <!-- left / right buttons -->
-                                    <br/>
-
-
-                                    <input id="moveright" type="button"
-                                           value="<?php echo Text::_('COM_SPORTSMANAGEMENT_GLOBAL_RIGHT'); ?>"
-                                           onclick="move_list_items('staff','staffposition<?php echo $position_id; ?>');"/>
-                                    <input id="moveleft" type="button"
-                                           value="<?php echo Text::_('COM_SPORTSMANAGEMENT_GLOBAL_LEFT'); ?>"
-                                           onclick="move_list_items('staffposition<?php echo $position_id; ?>','staff');"/>
-
-
+                                <td style="text-align:center; vertical-align:middle;">
+                                    <br>
+                                    <input
+                                        type="button"
+                                        value="<?php echo Text::_('COM_SPORTSMANAGEMENT_GLOBAL_RIGHT'); ?>"
+                                        onclick="move_list_items('staff', 'staffposition<?php echo (int) $positionId; ?>');"
+                                    >
+                                    <input
+                                        type="button"
+                                        value="<?php echo Text::_('COM_SPORTSMANAGEMENT_GLOBAL_LEFT'); ?>"
+                                        onclick="move_list_items('staffposition<?php echo (int) $positionId; ?>', 'staff');"
+                                    >
                                 </td>
                                 <td>
-                                    <!-- player affected to this position -->
-                                    <b><?php echo Text::_($pos->text); ?></b><br/>
-									<?php echo $this->lists['team_staffs' . $position_id]; ?>
+                                    <b><?php echo Text::_($pos->text); ?></b><br>
+                                    <?php echo $this->lists['team_staffs' . $positionId]; ?>
                                 </td>
-                                <td style='text-align:center; vertical-align:middle; '>
-                                    <!-- up/down buttons -->
-                                    <br/>
-                                    <input type="button" onclick="move_up('staffposition<?php echo $position_id; ?>');"
-                                           class="inputbox smove-up"
-                                           value="<?php echo Text::_('COM_SPORTSMANAGEMENT_GLOBAL_UP'); ?>"/><br/>
-                                    <input type="button"
-                                           onclick="move_down('staffposition<?php echo $position_id; ?>');"
-                                           class="inputbox smove-down"
-                                           value="<?php echo Text::_('COM_SPORTSMANAGEMENT_GLOBAL_DOWN'); ?>"/>
+                                <td style="text-align:center; vertical-align:middle;">
+                                    <br>
+                                    <input
+                                        type="button"
+                                        class="inputbox smove-up"
+                                        value="<?php echo Text::_('COM_SPORTSMANAGEMENT_GLOBAL_UP'); ?>"
+                                        onclick="move_up('staffposition<?php echo (int) $positionId; ?>');"
+                                    ><br>
+                                    <input
+                                        type="button"
+                                        class="inputbox smove-down"
+                                        value="<?php echo Text::_('COM_SPORTSMANAGEMENT_GLOBAL_DOWN'); ?>"
+                                        onclick="move_down('staffposition<?php echo (int) $positionId; ?>');"
+                                    >
                                 </td>
                             </tr>
-							<?php
-						}
-					}
-					?>
+                        <?php endforeach; ?>
+                    <?php endif; ?>
+                    </tbody>
                 </table>
             </td>
         </tr>
+        </tbody>
     </table>
 </fieldset>
