662daeef393308333fac1505ed7df10f48b9d18d
diddipoeler <diddipoeler@gmx.de>
2026-08-26 22:11:46 +0200

    Remove inline JavaScript from match stats form

--- site/tmpl/editmatch/editstats.php
+++ site/tmpl/editmatch/editstats.php
@@ -17,15 +17,16 @@ use Diddipoeler\Component\SportsManagement\Site\Model\EditmatchModel;
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
 
+$escape = static fn ($value): string => htmlspecialchars((string) $value, ENT_QUOTES, 'UTF-8');
 ?>
-<form name="adminForm" id="adminForm" method="post" action="<?php echo $this->uri->toString(); ?>">
+<form name="adminForm" id="adminForm" method="post" action="<?php echo $escape($this->uri->toString()); ?>">
     <div id="jlstatsform">
         <fieldset>
             <div class="fltrt">
-                <button type="button" onclick="Joomla.submitform('editmatch.savestats', this.form);">
+                <button type="button" data-stats-submit-task="editmatch.savestats">
                     <?php echo Text::_('JSAVE'); ?>
                 </button>
-                <button type="button" onclick="Joomla.submitform('editmatch.cancel', this.form);">
+                <button type="button" data-stats-submit-task="editmatch.cancel">
                     <?php echo Text::_('JCANCEL'); ?>
                 </button>
             </div>
@@ -46,11 +47,11 @@ use Joomla\CMS\Language\Text;
         <input type="hidden" name="view" value="">
         <input type="hidden" name="close" id="close" value="0">
         <input type="hidden" name="task" value="">
-        <input type="hidden" name="p" value="<?php echo EditmatchModel::$projectid; ?>">
-        <input type="hidden" name="r" value="<?php echo EditmatchModel::$roundid; ?>">
-        <input type="hidden" name="s" value="<?php echo EditmatchModel::$seasonid; ?>">
-        <input type="hidden" name="id" value="<?php echo $this->match->id; ?>">
-        <input type="hidden" name="match_id" value="<?php echo $this->match->id; ?>">
+        <input type="hidden" name="p" value="<?php echo (int) EditmatchModel::$projectid; ?>">
+        <input type="hidden" name="r" value="<?php echo (int) EditmatchModel::$roundid; ?>">
+        <input type="hidden" name="s" value="<?php echo (int) EditmatchModel::$seasonid; ?>">
+        <input type="hidden" name="id" value="<?php echo (int) $this->match->id; ?>">
+        <input type="hidden" name="match_id" value="<?php echo (int) $this->match->id; ?>">
         <input type="hidden" name="boxchecked" value="0">
         <?php echo HTMLHelper::_('form.token'); ?>
     </div>
