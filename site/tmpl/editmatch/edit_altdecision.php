4ddc99cbee3a0121afe18064c79787caf66e4940
diddipoeler <diddipoeler@gmx.de>
2026-08-26 07:56:14 +0200

    Clean up alternative decision template for Joomla 5/6

--- site/tmpl/editmatch/edit_altdecision.php
+++ site/tmpl/editmatch/edit_altdecision.php
@@ -8,127 +8,91 @@
  * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
-
 defined('_JEXEC') or die('Restricted access');
 
 use Joomla\CMS\Language\Text;
 
+$altDecision = (int) ($this->match->alt_decision ?? 0);
 ?>
-
-
-<!-- Alt decision table START -->
 <fieldset class="adminform">
-    <legend><?php echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_MATCH_F_AD'); ?>
-    </legend>
-    <table class='admintable'>
-		<?php
-
-		foreach ($this->form->getFieldset('altdecision') as $field)
-			:
-			?>
+    <legend><?php echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_MATCH_F_AD'); ?></legend>
+    <table class="admintable">
+        <tbody>
+        <?php foreach ($this->form->getFieldset('altdecision') as $field) : ?>
             <tr>
                 <td class="key"><?php echo $field->label; ?></td>
                 <td><?php echo $field->input; ?></td>
             </tr>
-		<?php endforeach; ?>
+        <?php endforeach; ?>
         <tr>
             <td class="key"><?php echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_MATCH_F_AD_SUB_DEC'); ?></td>
             <td colspan="2">
                 <select name="alt_decision" id="alt_decision">
-                    <option value="0"<?php if ($this->match->alt_decision == 0)
-					{
-						echo ' selected="selected"';
-					} ?>>
-						<?php echo Text::_('JNO'); ?>
+                    <option value="0"<?php echo $altDecision === 0 ? ' selected="selected"' : ''; ?>>
+                        <?php echo Text::_('JNO'); ?>
                     </option>
-                    <option value="1"<?php if ($this->match->alt_decision == 1)
-					{
-						echo ' selected="selected"';
-					} ?>>
-						<?php echo Text::_('JYES'); ?>
+                    <option value="1"<?php echo $altDecision === 1 ? ' selected="selected"' : ''; ?>>
+                        <?php echo Text::_('JYES'); ?>
                     </option>
                 </select>
             </td>
         </tr>
         <tr>
             <td colspan="3">
-                <div id="alt_decision_enter"
-                     style="display:<?php echo ($this->match->alt_decision == 0) ? 'none' : 'block'; ?>">
-                    <table class='adminForm' cellpadding='0' cellspacing='7' border='0'>
+                <div id="alt_decision_enter" style="display:<?php echo $altDecision === 0 ? 'none' : 'block'; ?>">
+                    <table class="adminForm" cellspacing="7">
+                        <tbody>
                         <tr>
                             <td class="key"><?php echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_MATCH_F_AD_NEW_SCORE') . ' ' . $this->match->hometeam; ?></td>
                             <td>
-                                <input type="text" class="inputbox" id="team1_result_decision"
-                                       name="team1_result_decision"
-                                       size="3"
-                                       value="<?php if ($this->match->alt_decision == 1)
-								       {
-									       if (isset($this->match->team1_result_decision))
-									       {
-										       echo $this->match->team1_result_decision;
-									       }
-									       else
-									       {
-										       echo 'X';
-									       }
-								       } ?>" <?php if ($this->match->alt_decision == 0)
-								{
-									echo 'DISABLED ';
-								} ?>/>
+                                <input
+                                    type="text"
+                                    class="inputbox"
+                                    id="team1_result_decision"
+                                    name="team1_result_decision"
+                                    size="3"
+                                    value="<?php echo $altDecision === 1 ? ($this->match->team1_result_decision ?? 'X') : ''; ?>"
+                                    <?php echo $altDecision === 0 ? 'disabled' : ''; ?>
+                                >
                             </td>
                         </tr>
                         <tr>
                             <td class="key"><?php echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_MATCH_F_AD_NEW_SCORE') . ' ' . $this->match->awayteam; ?></td>
                             <td>
-                                <input type="text" class="inputbox" id="team2_result_decision"
-                                       name="team2_result_decision"
-                                       size="3" value="<?php
-								if ($this->match->alt_decision == 1)
-								{
-									if (isset($this->match->team2_result_decision))
-									{
-										echo $this->match->team2_result_decision;
-									}
-									else
-									{
-										echo 'X';
-									}
-								} ?>" <?php
-
-								if ($this->match->alt_decision == 0)
-								{
-									echo 'DISABLED ';
-								} ?>/>
+                                <input
+                                    type="text"
+                                    class="inputbox"
+                                    id="team2_result_decision"
+                                    name="team2_result_decision"
+                                    size="3"
+                                    value="<?php echo $altDecision === 1 ? ($this->match->team2_result_decision ?? 'X') : ''; ?>"
+                                    <?php echo $altDecision === 0 ? 'disabled' : ''; ?>
+                                >
                             </td>
                         </tr>
                         <tr>
                             <td class="key"><?php echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_MATCH_F_AD_REASON_NEW_SCORE'); ?></td>
-							<?php
-							if (is_null($this->match->team1_result) || ($this->match->alt_decision == 0))
-							{
-								$disinfo = 'DISABLED ';
-							}
-							?>
                             <td>
-                                <input type="text" class="inputbox" id="decision_info" name="decision_info" size="30"
-                                       value="<?php if ($this->match->alt_decision == 1)
-								       {
-									       echo $this->match->decision_info;
-								       } ?>" <?php
-
-								if ($this->match->alt_decision == 0)
-								{
-									echo 'DISABLED ';
-								} ?>/>
+                                <input
+                                    type="text"
+                                    class="inputbox"
+                                    id="decision_info"
+                                    name="decision_info"
+                                    size="30"
+                                    value="<?php echo $altDecision === 1 ? ($this->match->decision_info ?? '') : ''; ?>"
+                                    <?php echo $altDecision === 0 ? 'disabled' : ''; ?>
+                                >
                             </td>
                         </tr>
                         <tr>
                             <td class="key"><?php echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_MATCH_F_AD_TEAM_WON'); ?></td>
                             <td><?php echo $this->lists['team_won']; ?></td>
                         </tr>
+                        </tbody>
                     </table>
                 </div>
             </td>
         </tr>
+        </tbody>
     </table>
 </fieldset>
