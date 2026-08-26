6bde7a25af51ee9b9b5f511beaac3e72cd48922a
diddipoeler <diddipoeler@gmx.de>
2026-08-26 18:37:20 +0200

    Remove inline JavaScript from lineup form actions

--- site/tmpl/editmatch/editlineup.php
+++ site/tmpl/editmatch/editlineup.php
@@ -14,31 +14,38 @@ defined('_JEXEC') or die('Restricted access');
 use Diddipoeler\Component\SportsManagement\Site\Model\EditmatchModel;
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
+
+$escape = static fn ($value): string => htmlspecialchars((string) $value, ENT_QUOTES, 'UTF-8');
+$isGolfBillard = (string) ($this->project->sport_type_name ?? '') === 'COM_SPORTSMANAGEMENT_ST_GOLF_BILLARD';
 ?>
-<form name="editmatch" id="editmatch" method="post" action="<?php echo $this->uri->toString(); ?>">
+<form name="editmatch" id="editmatch" method="post" action="<?php echo $escape($this->uri->toString()); ?>">
     <fieldset>
         <div class="fltrt">
-            <?php if ($this->project->sport_type_name === 'COM_SPORTSMANAGEMENT_ST_GOLF_BILLARD') : ?>
-                <button type="button" onclick="Joomla.submitform('editmatch.saverosterbillard', this.form);">
+            <?php if ($isGolfBillard) : ?>
+                <button type="button" data-submit-task="editmatch.saverosterbillard">
                     <?php echo Text::_('JSAVE'); ?>
                 </button>
             <?php else : ?>
-                <button type="button" onclick="this.form.querySelectorAll('select.position-starters option, select.position-staff option').forEach(function (option) { option.selected = true; }); Joomla.submitform('editmatch.saveroster', this.form);">
+                <button
+                    type="button"
+                    data-submit-task="editmatch.saveroster"
+                    data-select-all-before-submit="select.position-starters option, select.position-staff option"
+                >
                     <?php echo Text::_('JSAVE'); ?>
                 </button>
             <?php endif; ?>
-            <button type="button" onclick="Joomla.submitform('editmatch.cancel', this.form);">
+            <button type="button" data-submit-task="editmatch.cancel">
                 <?php echo Text::_('JCANCEL'); ?>
             </button>
         </div>
         <div class="configuration">
-            <?php echo Text::sprintf('COM_SPORTSMANAGEMENT_ADMIN_MATCH_ELU_TITLE', $this->teamname); ?>
+            <?php echo $escape(Text::sprintf('COM_SPORTSMANAGEMENT_ADMIN_MATCH_ELU_TITLE', (string) $this->teamname)); ?>
         </div>
     </fieldset>
     <div class="clear"></div>
     <div id="lineup">
         <?php echo HTMLHelper::_('bootstrap.startTabSet', 'myTab', ['active' => 'player']); ?>
-        <?php if ($this->project->sport_type_name === 'COM_SPORTSMANAGEMENT_ST_GOLF_BILLARD') : ?>
+        <?php if ($isGolfBillard) : ?>
             <?php echo HTMLHelper::_('bootstrap.addTab', 'myTab', 'player', Text::_('COM_SPORTSMANAGEMENT_TABS_PLAYERS', true)); ?>
             <?php echo $this->loadTemplate('golfbillardplayer'); ?>
             <?php echo HTMLHelper::_('bootstrap.endTab'); ?>
@@ -60,16 +67,16 @@ use Joomla\CMS\Language\Text;
 
         <input type="hidden" name="task" value="">
         <input type="hidden" name="view" value="">
-        <input type="hidden" name="project_id" value="<?php echo $this->project_id; ?>">
-        <input type="hidden" name="p" value="<?php echo EditmatchModel::$projectid; ?>">
-        <input type="hidden" name="r" value="<?php echo EditmatchModel::$roundid; ?>">
-        <input type="hidden" name="s" value="<?php echo EditmatchModel::$seasonid; ?>">
-        <input type="hidden" name="division" value="<?php echo EditmatchModel::$divisionid; ?>">
-        <input type="hidden" name="cfg_which_database" value="<?php echo EditmatchModel::$cfg_which_database; ?>">
+        <input type="hidden" name="project_id" value="<?php echo (int) $this->project_id; ?>">
+        <input type="hidden" name="p" value="<?php echo (int) EditmatchModel::$projectid; ?>">
+        <input type="hidden" name="r" value="<?php echo (int) EditmatchModel::$roundid; ?>">
+        <input type="hidden" name="s" value="<?php echo (int) EditmatchModel::$seasonid; ?>">
+        <input type="hidden" name="division" value="<?php echo (int) EditmatchModel::$divisionid; ?>">
+        <input type="hidden" name="cfg_which_database" value="<?php echo (int) EditmatchModel::$cfg_which_database; ?>">
         <input type="hidden" name="close" id="close" value="0">
-        <input type="hidden" name="id" value="<?php echo $this->match->id; ?>">
+        <input type="hidden" name="id" value="<?php echo (int) ($this->match->id ?? 0); ?>">
         <input type="hidden" name="changes_check" value="0" id="changes_check">
-        <input type="hidden" name="team" value="<?php echo $this->tid; ?>" id="team">
+        <input type="hidden" name="team" value="<?php echo (int) $this->tid; ?>" id="team">
         <input type="hidden" name="positionscount" value="<?php echo count($this->positions); ?>" id="positioncount">
         <?php echo HTMLHelper::_('form.token'); ?>
     </div>
