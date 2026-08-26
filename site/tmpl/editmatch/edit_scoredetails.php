5ec5a54e88251b0c4e6c9340b07b8f1eab847e3c
diddipoeler <diddipoeler@gmx.de>
2026-08-26 07:56:28 +0200

    Clean up score details template for Joomla 5/6

--- site/tmpl/editmatch/edit_scoredetails.php
+++ site/tmpl/editmatch/edit_scoredetails.php
@@ -1,8 +1,6 @@
 <?php
 /**
- *
  * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- *
  * @version    1.0.05
  * @package    Sportsmanagement
  * @subpackage editmatch
@@ -11,109 +9,68 @@
  * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
-
 defined('_JEXEC') or die('Restricted access');
 
 use Joomla\CMS\Language\Text;
 
+$useLegs = (int) ($this->projectws->use_legs ?? 0) === 1;
+$alternativeLegs = (string) ($this->table_config['alternative_legs'] ?? '');
 ?>
 <fieldset class="adminform">
-    <legend>
-		<?php
-		echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_MATCH_F_SD');
-		?>
-    </legend>
-    <br/>
-    <table class='admintable'>
+    <legend><?php echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_MATCH_F_SD'); ?></legend>
+    <table class="admintable">
+        <thead>
         <tr>
-
-            <th>
-				<?php
-				echo $this->match->hometeam;
-				?>
-            </th>
-            <th>
-                &nbsp;
-            </th>
-            <th align="left">
-				<?php
-				echo $this->match->awayteam;
-				?>
-            </th>
+            <th></th>
+            <th><?php echo $this->match->hometeam; ?></th>
+            <th></th>
+            <th><?php echo $this->match->awayteam; ?></th>
         </tr>
-        <!-- Header team names END -->
-        <!-- match legs -->
-		<?php
-		if ($this->projectws->use_legs == 1)
-		{
-			?>
+        </thead>
+        <tbody>
+        <?php if ($useLegs) : ?>
             <tr>
                 <td>
-					<?php
-					if ($this->table_config['alternative_legs'] == '')
-					{
-						echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_MATCH_F_SD_SETS');
-					}
-					else
-					{
-						echo $this->table_config['alternative_legs'];
-					}
-					?>:
+                    <?php echo $alternativeLegs === ''
+                        ? Text::_('COM_SPORTSMANAGEMENT_ADMIN_MATCH_F_SD_SETS')
+                        : $alternativeLegs; ?>:
                 </td>
                 <td>
-                    <input type="text" name="team1_legs" value="<?php echo $this->match->team1_legs; ?>" size="3"
-                           tabindex="100" class="inputbox"/>
+                    <input type="text" name="team1_legs" value="<?php echo $this->match->team1_legs; ?>" size="3" tabindex="100" class="inputbox">
                 </td>
-                <td align="center">:</td>
+                <td style="text-align:center;">:</td>
                 <td>
-                    <input type="text" name="team2_legs" value="<?php echo $this->match->team2_legs; ?>" size="3"
-                           tabindex="101" class="inputbox"/>
+                    <input type="text" name="team2_legs" value="<?php echo $this->match->team2_legs; ?>" size="3" tabindex="101" class="inputbox">
                 </td>
             </tr>
-			<?php
-		}
-		?>
-        <!-- END match legs -->
-
-        <!-- Bonus points -->
+        <?php endif; ?>
         <tr>
             <td class="key">
-                <label>
-					<?php
-					echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_MATCH_F_SD_BONUS');
-					?>
-                </label>
+                <label><?php echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_MATCH_F_SD_BONUS'); ?></label>
             </td>
             <td>
-                <input type="text" name="team1_bonus" value="<?php echo $this->match->team1_bonus; ?>" size="3"
-                       class="inputbox"/>
+                <input type="text" name="team1_bonus" value="<?php echo $this->match->team1_bonus; ?>" size="3" class="inputbox">
             </td>
-            <td align="center">:</td>
+            <td style="text-align:center;">:</td>
             <td>
-                <input type="text" name="team2_bonus" value="<?php echo $this->match->team2_bonus; ?>" size="3"
-                       class="inputbox"/>
+                <input type="text" name="team2_bonus" value="<?php echo $this->match->team2_bonus; ?>" size="3" class="inputbox">
             </td>
         </tr>
-        <!-- Bonus points END -->
-
-        <!-- Score Table END -->
-        <!-- Additional Details Table START -->
-        <!-- Result notice -->
         <tr>
             <td class="key">
-                <label for="match_result_detail">
-					<?php
-					echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_MATCH_F_SD_SCORE_NOTICE');
-					?>
-                </label>
+                <label for="match_result_detail"><?php echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_MATCH_F_SD_SCORE_NOTICE'); ?></label>
             </td>
-            <td colspan='3'>
-                <input type="text" name="match_result_detail" value="<?php echo $this->match->match_result_detail; ?>"
-                       size="40"
-                       class="inputbox"/>
+            <td colspan="3">
+                <input
+                    type="text"
+                    id="match_result_detail"
+                    name="match_result_detail"
+                    value="<?php echo $this->match->match_result_detail; ?>"
+                    size="40"
+                    class="inputbox"
+                >
             </td>
         </tr>
-        <!-- Result notice END -->
-
+        </tbody>
     </table>
 </fieldset>
