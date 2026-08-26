2c05ea424c7dbdcda016b56013a0f07d4eaa7c59
diddipoeler <diddipoeler@gmx.de>
2026-08-26 07:19:31 +0200

    Modernize frontend edit form JavaScript for Joomla 5/6

--- site/tmpl/editmatch/edit.php
+++ site/tmpl/editmatch/edit.php
@@ -10,144 +10,99 @@
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\Language\Text;
-use Joomla\CMS\HTML\HTMLHelper;
-use Joomla\CMS\Uri\Uri;
+
 use Joomla\CMS\Factory;
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Language\Text;
 
-jimport('joomla.html.pane');
-
+$this->getDocument()->getWebAssetManager()->useScript('form.validate');
 $fieldsets = $this->form->getFieldsets();
 ?>
-<script type="text/javascript">
-    //	Joomla.submitbutton = function(task)
-    //	{
-    //		if (document.formvalidator.isValid(document.id('editperson'))) {
-    //			Joomla.submitform(task, document.getElementById('editperson'));
-    //		}
-    //	}
-</script>
+<script>
+document.addEventListener('DOMContentLoaded', function () {
+    const altDecision = document.getElementById('alt_decision');
 
-<script type="text/javascript">
-    <!--
-    //window.addEvent('domready', function() {
-    jQuery(document).ready(function () {
-        // altered decision fields management
-        toggle_altdecision();
-        //jQuery('alt_decision').addEvent('change', toggle_altdecision);
-
-        jQuery('#alt_decision').change(function () {
-            toggle_altdecision();
-        });
-
-    });
-
-    function toggle_altdecision() {
-        if (jQuery('#alt_decision').val() == 0) {
-            //jQuery('alt_decision_enter').style.display='none';
-            jQuery("#alt_decision_enter").css("display", "none");
-            jQuery('#team1_result_decision').disabled = true;
-            jQuery('#team2_result_decision').disabled = true;
-            jQuery('#decision_info').disabled = true;
-        } else {
-            //jQuery('alt_decision_enter').style.display='block';
-            jQuery("#alt_decision_enter").css("display", "block");
-            jQuery('#team1_result_decision').disabled = false;
-            jQuery('#team2_result_decision').disabled = false;
-            jQuery('#decision_info').disabled = false;
-        }
+    if (!altDecision) {
+        return;
     }
 
-    //-->
+    const toggleAltDecision = function () {
+        const enabled = altDecision.value !== '0';
+        const container = document.getElementById('alt_decision_enter');
+
+        if (container) {
+            container.style.display = enabled ? 'block' : 'none';
+        }
+
+        ['team1_result_decision', 'team2_result_decision', 'decision_info'].forEach(function (id) {
+            const field = document.getElementById(id);
+
+            if (field) {
+                field.disabled = !enabled;
+            }
+        });
+    };
+
+    altDecision.addEventListener('change', toggleAltDecision);
+    toggleAltDecision();
+});
 </script>
 
-<form name="editmatch" id="editmatch" method="post" action="<?php echo $this->uri->toString(); ?>">
-	<?php
-
-	?>
+<form name="editmatch" id="editmatch" class="form-validate" method="post" action="<?php echo $this->uri->toString(); ?>">
     <fieldset class="adminform">
-        <div class="fltrt">
-	<!--	
-            <button type="button" onclick="Joomla.submitform('editmatch.saveshort', this.form);">
-				<?php echo Text::_('COM_SPORTSMANAGEMENT_GLOBAL_SAVE'); ?></button>
--->		
-            <!--
-<button type="button" onclick="Joomla.submitform('editmatch.save', this.form);">
-<?php echo Text::_('COM_SPORTSMANAGEMENT_GLOBAL_SAVECLOSE'); ?></button>
--->
-	<!--	
-            <button type="button" onclick="Joomla.submitform('editmatch.cancel', this.form);">
-				<?php echo Text::_('JCANCEL'); ?></button>
--->		
-        </div>
-        <legend>
-			<?php
-
-			?>
-        </legend>
+        <div class="fltrt"></div>
+        <legend></legend>
     </fieldset>
-	<?php
 
-	echo HTMLHelper::_('bootstrap.startTabSet', 'myTab', array('active' => 'home'));
-switch ( $this->project->sport_type_name )
-	{
-		case 'COM_SPORTSMANAGEMENT_ST_GOLF_BILLARD':
-echo HTMLHelper::_('bootstrap.addTab', 'myTab', 'home', Text::_('COM_SPORTSMANAGEMENT_TABS_MATCHDETAILS', true));
-		echo $this->loadTemplate('singlematchbillard');
-		echo HTMLHelper::_('bootstrap.endTab');
-		break;
+    <?php echo HTMLHelper::_('bootstrap.startTabSet', 'myTab', ['active' => 'home']); ?>
+    <?php if (($this->project->sport_type_name ?? '') === 'COM_SPORTSMANAGEMENT_ST_GOLF_BILLARD') : ?>
+        <?php echo HTMLHelper::_('bootstrap.addTab', 'myTab', 'home', Text::_('COM_SPORTSMANAGEMENT_TABS_MATCHDETAILS', true)); ?>
+        <?php echo $this->loadTemplate('singlematchbillard'); ?>
+        <?php echo HTMLHelper::_('bootstrap.endTab'); ?>
+    <?php else : ?>
+        <?php echo HTMLHelper::_('bootstrap.addTab', 'myTab', 'home', Text::_('COM_SPORTSMANAGEMENT_TABS_MATCHDETAILS', true)); ?>
+        <?php echo $this->loadTemplate('matchdetails'); ?>
+        <?php echo HTMLHelper::_('bootstrap.endTab'); ?>
+        <?php echo HTMLHelper::_('bootstrap.addTab', 'myTab', 'menu1', Text::_('COM_SPORTSMANAGEMENT_TABS_ALTDECISION', true)); ?>
+        <?php echo $this->loadTemplate('altdecision'); ?>
+        <?php echo HTMLHelper::_('bootstrap.endTab'); ?>
+        <?php echo HTMLHelper::_('bootstrap.addTab', 'myTab', 'menu2', Text::_('COM_SPORTSMANAGEMENT_TABS_MATCHPREVIEW', true)); ?>
+        <?php echo $this->loadTemplate('matchpreview'); ?>
+        <?php echo HTMLHelper::_('bootstrap.endTab'); ?>
+        <?php echo HTMLHelper::_('bootstrap.addTab', 'myTab', 'menu3', Text::_('COM_SPORTSMANAGEMENT_TABS_SCOREDETAILS', true)); ?>
+        <?php echo $this->loadTemplate('scoredetails'); ?>
+        <?php echo HTMLHelper::_('bootstrap.endTab'); ?>
+        <?php echo HTMLHelper::_('bootstrap.addTab', 'myTab', 'menu4', Text::_('COM_SPORTSMANAGEMENT_TABS_MATCHREPORT', true)); ?>
+        <?php echo $this->loadTemplate('matchreport'); ?>
+        <?php echo HTMLHelper::_('bootstrap.endTab'); ?>
+        <?php echo HTMLHelper::_('bootstrap.addTab', 'myTab', 'menu5', Text::_('COM_SPORTSMANAGEMENT_TABS_MATCHRELATION', true)); ?>
+        <?php echo $this->loadTemplate('matchrelation'); ?>
+        <?php echo HTMLHelper::_('bootstrap.endTab'); ?>
+        <?php echo HTMLHelper::_('bootstrap.addTab', 'myTab', 'menu6', Text::_('COM_SPORTSMANAGEMENT_TABS_EXTENDED', true)); ?>
+        <?php echo $this->loadTemplate('matchextended'); ?>
+        <?php echo HTMLHelper::_('bootstrap.endTab'); ?>
+    <?php endif; ?>
+    <?php echo HTMLHelper::_('bootstrap.endTabSet'); ?>
 
-		default:
-
-	echo HTMLHelper::_('bootstrap.addTab', 'myTab', 'home', Text::_('COM_SPORTSMANAGEMENT_TABS_MATCHDETAILS', true));
-	echo $this->loadTemplate('matchdetails');
-	echo HTMLHelper::_('bootstrap.endTab');
-	echo HTMLHelper::_('bootstrap.addTab', 'myTab', 'menu1', Text::_('COM_SPORTSMANAGEMENT_TABS_ALTDECISION', true));
-	echo $this->loadTemplate('altdecision');
-	echo HTMLHelper::_('bootstrap.endTab');
-	echo HTMLHelper::_('bootstrap.addTab', 'myTab', 'menu2', Text::_('COM_SPORTSMANAGEMENT_TABS_MATCHPREVIEW', true));
-	echo $this->loadTemplate('matchpreview');
-	echo HTMLHelper::_('bootstrap.endTab');
-	echo HTMLHelper::_('bootstrap.addTab', 'myTab', 'menu3', Text::_('COM_SPORTSMANAGEMENT_TABS_SCOREDETAILS', true));
-	echo $this->loadTemplate('scoredetails');
-	echo HTMLHelper::_('bootstrap.endTab');
-	echo HTMLHelper::_('bootstrap.addTab', 'myTab', 'menu4', Text::_('COM_SPORTSMANAGEMENT_TABS_MATCHREPORT', true));
-	echo $this->loadTemplate('matchreport');
-	echo HTMLHelper::_('bootstrap.endTab');
-	echo HTMLHelper::_('bootstrap.addTab', 'myTab', 'menu5', Text::_('COM_SPORTSMANAGEMENT_TABS_MATCHRELATION', true));
-	echo $this->loadTemplate('matchrelation');
-	echo HTMLHelper::_('bootstrap.endTab');
-	echo HTMLHelper::_('bootstrap.addTab', 'myTab', 'menu6', Text::_('COM_SPORTSMANAGEMENT_TABS_EXTENDED', true));
-	echo $this->loadTemplate('matchextended');
-	echo HTMLHelper::_('bootstrap.endTab');
-break;
-	}
-
-	echo HTMLHelper::_('bootstrap.endTabSet');
-
-	?>
     <div class="clr"></div>
-
-    <input type='hidden' name='option' value='com_sportsmanagement'/>
-    <input type='hidden' name='view' value='editmatch'/>
-    <input type='hidden' name='layout' value='edit'/>
-    <input type='hidden' name='oldlayout' value='<?php echo sportsmanagementModelEditMatch::$oldlayout; ?>'/>
-    <input type='hidden' name='tmpl' value='component'/>
-    <input type='hidden' name='cfg_which_database'
-           value='<?php echo sportsmanagementModelEditMatch::$cfg_which_database; ?>'/>
-    <input type='hidden' name='s' value='<?php echo sportsmanagementModelEditMatch::$seasonid; ?>'/>
-    <input type='hidden' name='p' value='<?php echo sportsmanagementModelEditMatch::$projectid; ?>'/>
-    <input type='hidden' name='r' value='<?php echo sportsmanagementModelEditMatch::$roundid; ?>'/>
-    <input type='hidden' name='divisionid' value='<?php echo sportsmanagementModelEditMatch::$divisionid; ?>'/>
-    <input type='hidden' name='mode' value='<?php echo sportsmanagementModelEditMatch::$mode; ?>'/>
-    <input type='hidden' name='order' value='<?php echo sportsmanagementModelEditMatch::$order; ?>'/>
-    <input type='hidden' name='task' value='editmatch.saveshort'/>
-    <input type='hidden' name='matchid' value='<?php echo $this->match->id; ?> '/>
-    <input type='hidden' name='sel_r' value='<?php echo sportsmanagementModelEditMatch::$roundid; ?>'/>
-    <input type='hidden' name='Itemid'
-           value='<?php echo Factory::getApplication()->input->getInt('Itemid', 1, 'get'); ?>'/>
-    <input type='hidden' name='boxchecked' value='0' id='boxchecked'/>
-    <input type='hidden' name='checkmycontainers' value='0' id='checkmycontainers'/>
-    <input type='hidden' name='save_data' value='1' class='button'/>
-	<?php echo HTMLHelper::_('form.token'); ?>
+    <input type="hidden" name="option" value="com_sportsmanagement">
+    <input type="hidden" name="view" value="editmatch">
+    <input type="hidden" name="layout" value="edit">
+    <input type="hidden" name="oldlayout" value="<?php echo sportsmanagementModelEditMatch::$oldlayout; ?>">
+    <input type="hidden" name="tmpl" value="component">
+    <input type="hidden" name="cfg_which_database" value="<?php echo sportsmanagementModelEditMatch::$cfg_which_database; ?>">
+    <input type="hidden" name="s" value="<?php echo sportsmanagementModelEditMatch::$seasonid; ?>">
+    <input type="hidden" name="p" value="<?php echo sportsmanagementModelEditMatch::$projectid; ?>">
+    <input type="hidden" name="r" value="<?php echo sportsmanagementModelEditMatch::$roundid; ?>">
+    <input type="hidden" name="divisionid" value="<?php echo sportsmanagementModelEditMatch::$divisionid; ?>">
+    <input type="hidden" name="mode" value="<?php echo sportsmanagementModelEditMatch::$mode; ?>">
+    <input type="hidden" name="order" value="<?php echo sportsmanagementModelEditMatch::$order; ?>">
+    <input type="hidden" name="task" value="editmatch.saveshort">
+    <input type="hidden" name="matchid" value="<?php echo (int) $this->match->id; ?>">
+    <input type="hidden" name="sel_r" value="<?php echo sportsmanagementModelEditMatch::$roundid; ?>">
+    <input type="hidden" name="Itemid" value="<?php echo Factory::getApplication()->input->getInt('Itemid', 1, 'get'); ?>">
+    <input type="hidden" name="boxchecked" value="0" id="boxchecked">
+    <input type="hidden" name="checkmycontainers" value="0" id="checkmycontainers">
+    <input type="hidden" name="save_data" value="1" class="button">
+    <?php echo HTMLHelper::_('form.token'); ?>
 </form>
--- site/tmpl/editprojectteam/default.php
+++ site/tmpl/editprojectteam/default.php
@@ -10,79 +10,71 @@
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\Language\Text;
-use Joomla\CMS\Router\Route;
-use Joomla\CMS\HTML\HTMLHelper;
+
 use Joomla\CMS\Factory;
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Language\Text;
 
-/** Get the form fieldsets. */
 $fieldsets = $this->form->getFieldsets();
-
 ?>
-<script type="text/javascript">
-    Joomla.submitbutton = function (task) {
-        if (document.formvalidator.isValid(document.id('editprojectteam'))) {
-            Joomla.submitform(task, document.getElementById('editprojectteam'));
-        }
+<script>
+Joomla.submitbutton = function (task) {
+    const form = document.getElementById('adminForm');
+
+    if (!form) {
+        return;
     }
+
+    if (
+        task === 'editprojectteam.cancel'
+        || !document.formvalidator
+        || document.formvalidator.isValid(form)
+    ) {
+        Joomla.submitform(task, form);
+    }
+};
 </script>
-<form name="adminForm" id="adminForm" method="post" action="<?php echo $this->uri->toString(); ?>">
-	<?php
-	?>
+<form name="adminForm" id="adminForm" class="form-validate" method="post" action="<?php echo $this->uri->toString(); ?>">
     <fieldset class="adminform">
         <div class="fltrt">
-            <button type="button" onclick="Joomla.submitform('editprojectteam.apply', this.form);">
-				<?php echo Text::_('COM_SPORTSMANAGEMENT_GLOBAL_SAVE'); ?></button>
-            <button type="button" onclick="Joomla.submitform('editprojectteam.save', this.form);">
-				<?php echo Text::_('COM_SPORTSMANAGEMENT_GLOBAL_SAVECLOSE'); ?></button>
-            <button type="button" onclick="Joomla.submitform('editprojectteam.cancel', this.form);">
-				<?php echo Text::_('JCANCEL'); ?></button>
+            <button type="button" onclick="Joomla.submitbutton('editprojectteam.apply');">
+                <?php echo Text::_('COM_SPORTSMANAGEMENT_GLOBAL_SAVE'); ?>
+            </button>
+            <button type="button" onclick="Joomla.submitbutton('editprojectteam.save');">
+                <?php echo Text::_('COM_SPORTSMANAGEMENT_GLOBAL_SAVECLOSE'); ?>
+            </button>
+            <button type="button" onclick="Joomla.submitbutton('editprojectteam.cancel');">
+                <?php echo Text::_('JCANCEL'); ?>
+            </button>
         </div>
         <legend>
-			<?php
-			echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_PROJECTTEAM_EDIT') . ' ' . $this->item->name;
-			?>
+            <?php echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_PROJECTTEAM_EDIT') . ' ' . $this->item->name; ?>
         </legend>
     </fieldset>
 
-	<?php
-	echo HTMLHelper::_('bootstrap.startTabSet', 'myTab', array('active' => 'details'));
-
-	foreach ($fieldsets as $fieldset)
-		:
-		switch ($fieldset->name)
-		{
-			case 'details':
-				echo HTMLHelper::_('bootstrap.addTab', 'myTab', $fieldset->name, Text::_($fieldset->label, true));
-				echo $this->loadTemplate($fieldset->name);
-				echo HTMLHelper::_('bootstrap.endTab');
-				break;
-                case 'picture':
-			    echo HTMLHelper::_('bootstrap.addTab', 'myTab', $fieldset->name, Text::_($fieldset->label, true));
-				echo $this->loadTemplate($fieldset->name);
-				echo HTMLHelper::_('bootstrap.endTab');
-                break;
-                case 'description':
-                
-                break;
-                case 'training':
-                
-                break;
-		}
-	endforeach;
-
-	echo HTMLHelper::_('bootstrap.endTabSet');
-	?>
-
+    <?php echo HTMLHelper::_('bootstrap.startTabSet', 'myTab', ['active' => 'details']); ?>
+    <?php foreach ($fieldsets as $fieldset) : ?>
+        <?php switch ($fieldset->name) :
+            case 'details': ?>
+                <?php echo HTMLHelper::_('bootstrap.addTab', 'myTab', $fieldset->name, Text::_($fieldset->label, true)); ?>
+                <?php echo $this->loadTemplate($fieldset->name); ?>
+                <?php echo HTMLHelper::_('bootstrap.endTab'); ?>
+                <?php break; ?>
+            <?php case 'picture': ?>
+                <?php echo HTMLHelper::_('bootstrap.addTab', 'myTab', $fieldset->name, Text::_($fieldset->label, true)); ?>
+                <?php echo $this->loadTemplate($fieldset->name); ?>
+                <?php echo HTMLHelper::_('bootstrap.endTab'); ?>
+                <?php break; ?>
+        <?php endswitch; ?>
+    <?php endforeach; ?>
+    <?php echo HTMLHelper::_('bootstrap.endTabSet'); ?>
 
     <div class="clr"></div>
-    <input type="hidden" name="option" value="com_sportsmanagement"/>
-    <input type="hidden" name="id" value="<?php echo $this->item->id; ?>"/>
-    <input type="hidden" name="ptid" value="<?php echo $this->item->id; ?>"/>
-
-    <input type="hidden" name="p" value="<?php echo Factory::getApplication()->input->getInt('p', 0); ?>"/>
-    <input type="hidden" name="tid" value="<?php echo Factory::getApplication()->input->getInt('tid', 0); ?>"/>
-
-    <input type="hidden" name="task" value=""/>
-	<?php echo HTMLHelper::_('form.token') . "\n"; ?>
+    <input type="hidden" name="option" value="com_sportsmanagement">
+    <input type="hidden" name="id" value="<?php echo (int) $this->item->id; ?>">
+    <input type="hidden" name="ptid" value="<?php echo (int) $this->item->id; ?>">
+    <input type="hidden" name="p" value="<?php echo Factory::getApplication()->input->getInt('p', 0); ?>">
+    <input type="hidden" name="tid" value="<?php echo Factory::getApplication()->input->getInt('tid', 0); ?>">
+    <input type="hidden" name="task" value="">
+    <?php echo HTMLHelper::_('form.token') . "\n"; ?>
 </form>
