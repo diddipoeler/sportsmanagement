f7edcb17db0e21139e4de6d181ccc8dfdd28ec36
diddipoeler <diddipoeler@gmx.de>
2026-08-26 18:38:37 +0200

    Clean Golf Billiards lineup player template

--- site/tmpl/editmatch/editlineup_golfbillardplayer.php
+++ site/tmpl/editmatch/editlineup_golfbillardplayer.php
@@ -1,7 +1,6 @@
 <?php
 /**
  * https://getbootstrap.com/docs/5.2/helpers/color-background/
- *
  */
 
 defined('_JEXEC') or die('Restricted access');
@@ -10,134 +9,106 @@ use Diddipoeler\Component\SportsManagement\Site\Helper\PersonNameFormatter;
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
 
-$not_assigned_options = [];
-$not_assigned_options[] = HTMLHelper::_('select.option', '0', Text::_('COM_SPORTSMANAGEMENT_GLOBAL_SELECT_PLAYER'));
+$escape = static fn ($value): string => htmlspecialchars((string) $value, ENT_QUOTES, 'UTF-8');
+$formatPlayer = function (object $player): string {
+    $name = PersonNameFormatter::format(
+        null,
+        (string) ($player->firstname ?? ''),
+        (string) ($player->nickname ?? ''),
+        (string) ($player->lastname ?? ''),
+        $this->default_name_format
+    );
+    $associationNumber = trim((string) ($player->knvbnr ?? ''));
 
-foreach ((array) $this->lists['team_players_billard'] as $p)
-{
-    $not_assigned_options[] = HTMLHelper::_(
+    return $associationNumber !== '' ? $name . ' (' . $associationNumber . ')' : $name;
+};
+$assignedPlayers = (array) ($this->lists['team_players_billard_assign'] ?? []);
+$findAssignedPlayer = static function (array $players, int $slot) use ($formatPlayer): array {
+    foreach ($players as $player) {
+        if ((int) ($player->trikot_number ?? 0) !== $slot) {
+            continue;
+        }
+
+        return [
+            'id' => (int) ($player->tpid ?? 0),
+            'name' => $formatPlayer($player),
+        ];
+    }
+
+    return ['id' => 0, 'name' => ''];
+};
+
+$notAssignedOptions = [
+    HTMLHelper::_('select.option', 0, Text::_('COM_SPORTSMANAGEMENT_GLOBAL_SELECT_PLAYER')),
+];
+
+foreach ((array) ($this->lists['team_players_billard'] ?? []) as $player) {
+    $notAssignedOptions[] = HTMLHelper::_(
         'select.option',
-        $p->value,
-        PersonNameFormatter::format(null, $p->firstname, $p->nickname, $p->lastname, $this->default_name_format) . ' (' . $p->knvbnr . ')'
+        (int) ($player->value ?? 0),
+        $formatPlayer($player)
     );
 }
 
-for ($a = 1; $a < 6; $a++)
-{
-?>
-<div class="row">
-    <div class="col-md-1 box">
-        <?php echo Text::_('COM_SPORTSMANAGEMENT_GOLF_BILLARD_P_PLAYER') . ' ' . $a; ?>
+for ($slot = 1; $slot <= 5; $slot++) :
+    $assigned = $findAssignedPlayer($assignedPlayers, $slot);
+    ?>
+    <div class="row">
+        <div class="col-md-1 box">
+            <?php echo Text::_('COM_SPORTSMANAGEMENT_GOLF_BILLARD_P_PLAYER') . ' ' . $slot; ?>
+        </div>
+        <div class="col-sm">
+            <?php if ($assigned['id'] > 0) : ?>
+                <?php echo $escape($assigned['name']); ?>
+            <?php else : ?>
+                <?php
+                echo HTMLHelper::_(
+                    'select.genericlist',
+                    $notAssignedOptions,
+                    'roster[' . $slot . ']',
+                    '',
+                    'value',
+                    'text',
+                    0
+                );
+                ?>
+            <?php endif; ?>
+        </div>
     </div>
-    <div class="col-sm">
-        <?php
-        $player_id = 0;
-        $player_name = '';
+<?php endfor; ?>
 
-        foreach ($this->lists['team_players_billard_assign'] as $value)
-        {
-            if ($value->trikot_number == $a)
-            {
-                $player_id = $value->tpid;
-                $player_name = PersonNameFormatter::format(
-                    null,
-                    $value->firstname,
-                    $value->nickname,
-                    $value->lastname,
-                    $this->default_name_format
-                ) . ' (' . $value->knvbnr . ')';
-            }
-        }
-
-        if ($player_id)
-        {
-            echo $player_name;
-        }
-        else
-        {
-            echo HTMLHelper::_('select.genericlist', $not_assigned_options, 'roster[' . $a . ']', '', 'value', 'text', $player_id);
-        }
-        ?>
-    </div>
-</div>
-<?php
-}
-?>
 <div class="row">
     <div class="text-bg-secondary p-3"></div>
 </div>
-<?php
-$player_id = 0;
-$player_name = '';
 
-foreach ($this->lists['team_players_billard_assign'] as $value)
-{
-    if ($value->trikot_number == 100)
-    {
-        $player_id = $value->tpid;
-        $player_name = PersonNameFormatter::format(
-            null,
-            $value->firstname,
-            $value->nickname,
-            $value->lastname,
-            $this->default_name_format
-        ) . ' (' . $value->knvbnr . ')';
-    }
-}
-?>
+<?php $captain = $findAssignedPlayer($assignedPlayers, 100); ?>
 <div class="row">
     <div class="col-md-1 box">
         <?php echo Text::_('COM_SPORTSMANAGEMENT_GOLF_BILLARD_P_CAPTAIN'); ?>
     </div>
     <div class="col-sm">
-        <?php
-        if ($player_id)
-        {
-            echo $player_name;
-        }
-        else
-        {
-            echo HTMLHelper::_('select.genericlist', $not_assigned_options, 'rosterc[]', '', 'value', 'text', $player_id);
-        }
-        ?>
+        <?php if ($captain['id'] > 0) : ?>
+            <?php echo $escape($captain['name']); ?>
+        <?php else : ?>
+            <?php echo HTMLHelper::_('select.genericlist', $notAssignedOptions, 'rosterc[]', '', 'value', 'text', 0); ?>
+        <?php endif; ?>
     </div>
 </div>
+
 <div class="row">
     <div class="text-bg-secondary p-3"></div>
 </div>
-<?php
-$player_id = 0;
-$player_name = '';
 
-foreach ($this->lists['team_players_billard_assign'] as $value)
-{
-    if ($value->trikot_number == 50)
-    {
-        $player_id = $value->tpid;
-        $player_name = PersonNameFormatter::format(
-            null,
-            $value->firstname,
-            $value->nickname,
-            $value->lastname,
-            $this->default_name_format
-        ) . ' (' . $value->knvbnr . ')';
-    }
-}
-?>
+<?php $reserve = $findAssignedPlayer($assignedPlayers, 50); ?>
 <div class="row">
     <div class="col-md-1 box">
         <?php echo Text::_('COM_SPORTSMANAGEMENT_GOLF_BILLARD_P_RESERVE'); ?>
     </div>
     <div class="col-sm">
-        <?php
-        if ($player_id)
-        {
-            echo $player_name;
-        }
-        else
-        {
-            echo HTMLHelper::_('select.genericlist', $not_assigned_options, 'rosterr[]', '', 'value', 'text', $player_id);
-        }
-        ?>
+        <?php if ($reserve['id'] > 0) : ?>
+            <?php echo $escape($reserve['name']); ?>
+        <?php else : ?>
+            <?php echo HTMLHelper::_('select.genericlist', $notAssignedOptions, 'rosterr[]', '', 'value', 'text', 0); ?>
+        <?php endif; ?>
     </div>
 </div>
