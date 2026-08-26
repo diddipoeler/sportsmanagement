16355ac972fd4a90dea1484c106a44456762f6d7
diddipoeler <diddipoeler@gmx.de>
2026-08-26 18:36:33 +0200

    Remove inline JavaScript from referee editor

--- site/tmpl/editmatch/editreferees.php
+++ site/tmpl/editmatch/editreferees.php
@@ -1,6 +1,5 @@
 <?php
 /**
- *
  * SportsManagement ein Programm zur Verwaltung für Sportarten
  *
  * @version    1.0.05
@@ -18,16 +17,20 @@ use Diddipoeler\Component\SportsManagement\Site\Model\EditmatchModel;
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
 
+$escape = static fn ($value): string => htmlspecialchars((string) $value, ENT_QUOTES, 'UTF-8');
 ?>
 <div id="lineup">
-    <form action="<?php echo $this->uri->toString(); ?>" id="editreferees" method="post" name="editreferees">
+    <form action="<?php echo $escape($this->uri->toString()); ?>" id="editreferees" method="post" name="editreferees">
         <fieldset>
             <div class="fltrt">
-                <button type="button"
-                        onclick="this.form.querySelectorAll('select.position-starters option').forEach(function (option) { option.selected = true; }); Joomla.submitform('editmatch.saveReferees', this.form);">
+                <button
+                    type="button"
+                    data-submit-task="editmatch.saveReferees"
+                    data-select-all-before-submit="select.position-starters option"
+                >
                     <?php echo Text::_('JSAVE'); ?>
                 </button>
-                <button type="button" onclick="Joomla.submitform('editmatch.cancel', this.form);">
+                <button type="button" data-submit-task="editmatch.cancel">
                     <?php echo Text::_('JCANCEL'); ?>
                 </button>
             </div>
@@ -50,40 +53,55 @@ use Joomla\CMS\Language\Text;
                     <td style="text-align:center;">
                         <?php echo $this->lists['team_referees']; ?>
                     </td>
-                    <td style="text-align:center; vertical-align:top;">
+                    <td style="text-align:center;vertical-align:top;">
                         <table>
-                            <?php if (isset($this->positions)) : ?>
-                                <?php foreach ($this->positions as $key => $pos) : ?>
-                                    <tr>
-                                        <td style="text-align:center; vertical-align:middle;">
-                                            <br>
-                                            <input type="button"
-                                                   value="<?php echo Text::_('COM_SPORTSMANAGEMENT_GLOBAL_RIGHT'); ?>"
-                                                   onclick="move_list_items('roster', 'position<?php echo (int) $key; ?>');">
-                                            <input type="button"
-                                                   value="<?php echo Text::_('COM_SPORTSMANAGEMENT_GLOBAL_LEFT'); ?>"
-                                                   onclick="move_list_items('position<?php echo (int) $key; ?>', 'roster');">
-                                        </td>
-                                        <td>
-                                            <b><?php echo Text::_($pos->text); ?></b><br>
-                                            <?php echo $this->lists['team_referees' . $key]; ?>
-                                        </td>
-                                        <td style="text-align:center; vertical-align:middle;">
-                                            <br>
-                                            <input type="button"
-                                                   id="moveup-<?php echo (int) $key; ?>"
-                                                   class="inputbox move-up"
-                                                   value="<?php echo Text::_('COM_SPORTSMANAGEMENT_GLOBAL_UP'); ?>"
-                                                   onclick="move_up('position<?php echo (int) $key; ?>');"><br>
-                                            <input type="button"
-                                                   id="movedown-<?php echo (int) $key; ?>"
-                                                   class="inputbox move-down"
-                                                   value="<?php echo Text::_('COM_SPORTSMANAGEMENT_GLOBAL_DOWN'); ?>"
-                                                   onclick="move_down('position<?php echo (int) $key; ?>');">
-                                        </td>
-                                    </tr>
-                                <?php endforeach; ?>
-                            <?php endif; ?>
+                            <tbody>
+                            <?php foreach ($this->positions as $key => $position) : ?>
+                                <?php $positionKey = (int) $key; ?>
+                                <tr>
+                                    <td style="text-align:center;vertical-align:middle;">
+                                        <br>
+                                        <button
+                                            type="button"
+                                            data-list-source="roster"
+                                            data-list-destination="position<?php echo $positionKey; ?>"
+                                        >
+                                            <?php echo Text::_('COM_SPORTSMANAGEMENT_GLOBAL_RIGHT'); ?>
+                                        </button>
+                                        <button
+                                            type="button"
+                                            data-list-source="position<?php echo $positionKey; ?>"
+                                            data-list-destination="roster"
+                                        >
+                                            <?php echo Text::_('COM_SPORTSMANAGEMENT_GLOBAL_LEFT'); ?>
+                                        </button>
+                                    </td>
+                                    <td>
+                                        <b><?php echo $escape(Text::_((string) ($position->text ?? ''))); ?></b><br>
+                                        <?php echo $this->lists['team_referees' . $key]; ?>
+                                    </td>
+                                    <td style="text-align:center;vertical-align:middle;">
+                                        <br>
+                                        <button
+                                            type="button"
+                                            id="moveup-<?php echo $positionKey; ?>"
+                                            class="inputbox move-up"
+                                            data-list-move-up="position<?php echo $positionKey; ?>"
+                                        >
+                                            <?php echo Text::_('COM_SPORTSMANAGEMENT_GLOBAL_UP'); ?>
+                                        </button><br>
+                                        <button
+                                            type="button"
+                                            id="movedown-<?php echo $positionKey; ?>"
+                                            class="inputbox move-down"
+                                            data-list-move-down="position<?php echo $positionKey; ?>"
+                                        >
+                                            <?php echo Text::_('COM_SPORTSMANAGEMENT_GLOBAL_DOWN'); ?>
+                                        </button>
+                                    </td>
+                                </tr>
+                            <?php endforeach; ?>
+                            </tbody>
                         </table>
                     </td>
                 </tr>
@@ -95,14 +113,14 @@ use Joomla\CMS\Language\Text;
         <input type="hidden" name="task" value="">
         <input type="hidden" name="view" value="">
         <input type="hidden" name="close" id="close" value="0">
-        <input type="hidden" name="id" value="<?php echo $this->match->id; ?>">
-        <input type="hidden" name="project_id" value="<?php echo $this->project_id; ?>">
+        <input type="hidden" name="id" value="<?php echo (int) ($this->match->id ?? 0); ?>">
+        <input type="hidden" name="project_id" value="<?php echo (int) $this->project_id; ?>">
         <input type="hidden" name="changes_check" value="0" id="changes_check">
-        <input type="hidden" name="p" value="<?php echo EditmatchModel::$projectid; ?>">
-        <input type="hidden" name="r" value="<?php echo EditmatchModel::$roundid; ?>">
-        <input type="hidden" name="s" value="<?php echo EditmatchModel::$seasonid; ?>">
-        <input type="hidden" name="division" value="<?php echo EditmatchModel::$divisionid; ?>">
-        <input type="hidden" name="cfg_which_database" value="<?php echo EditmatchModel::$cfg_which_database; ?>">
+        <input type="hidden" name="p" value="<?php echo (int) EditmatchModel::$projectid; ?>">
+        <input type="hidden" name="r" value="<?php echo (int) EditmatchModel::$roundid; ?>">
+        <input type="hidden" name="s" value="<?php echo (int) EditmatchModel::$seasonid; ?>">
+        <input type="hidden" name="division" value="<?php echo (int) EditmatchModel::$divisionid; ?>">
+        <input type="hidden" name="cfg_which_database" value="<?php echo (int) EditmatchModel::$cfg_which_database; ?>">
         <input type="hidden" name="positionscount" value="<?php echo count($this->positions); ?>" id="positioncount">
         <?php echo HTMLHelper::_('form.token'); ?>
     </form>
