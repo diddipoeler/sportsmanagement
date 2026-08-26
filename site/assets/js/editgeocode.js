2bb6c7fbc3f545aec1a58abe87cb14cdcde6822a
diddipoeler <diddipoeler@gmx.de>
2026-08-26 12:44:25 +0200

    Modernize frontend geocoding for Joomla 5/6

--- site/assets/js/editgeocode.js
+++ site/assets/js/editgeocode.js
@@ -1,343 +1,284 @@
-var dpjQuery = jQuery.noConflict();
-var cities;
-var country;			
-var countryleafletsearch;
-var countryleaflet;
+(function () {
+    'use strict';
 
-var address;
-var street;
-var state;
-var city;
-var zip;
-var province;
-var baseurl;
-var yourQuery;	
+    let activeRequest = null;
 
+    function field(name) {
+        return document.getElementById(`jform_${name}`) || document.getElementById(name);
+    }
 
-dpjQuery(document).ready(function(){
-console.log("editgeocode document.location.origin : "+document.location.origin);  
-baseurl = document.location.origin;  
-dpjQuery("#geocomplete").val(getAddresString());
-getlatlonopenstreet(1);  
-  
-  
+    function fieldValue(name) {
+        const element = field(name);
+        return element ? String(element.value || '').trim() : '';
+    }
 
-//dpjQuery(this).attr("href");
-//console.log('href ' + dpjQuery(this).attr("href") );
-  
-}); 
+    function setFieldValue(name, value) {
+        const element = field(name);
 
+        if (element) {
+            element.value = value == null ? '' : String(value);
+        }
+    }
 
+    function setElementValue(id, value) {
+        const element = document.getElementById(id);
 
-	/*
-dpjQuery(document).ready(function(){
-dpjQuery("#geocomplete").val(getAddresString());
+        if (element) {
+            element.value = value == null ? '' : String(value);
+        }
+    }
 
-console.log('latitude ' + dpjQuery("#latitude").val() );  
-console.log('longitude ' + dpjQuery("#longitude").val() );  
-getlatlonopenstreet(1);
+    function selectedCountryName() {
+        const country = field('country');
 
-//geocoder = new L.Control.Geocoder.Nominatim();
-if (dpjQuery('#address_country').length == 0) {
-console.log('Das Element mit der ID address_country ist nicht vorhanden.');	
-countryleaflet = dpjQuery("#country").val();	
-}
-	else
-	{
-console.log('Das Element mit der ID address_country ist vorhanden.');		
-countryleaflet = dpjQuery("#address_country").val();
-	}
-	
-console.log('ready countryleaflet ' + countryleaflet);
-var url = 'administrator/index.php?option=com_sportsmanagement&format=json&tmpl=component&task=ajax.getCcountryAlpha2&country=' + countryleaflet;
-dpjQuery.ajax({
-url: url,
-dataType: 'json',
-async: false,
-type : 'POST'
-}).done(function(data1) {
-console.log(data1);
-dpjQuery.each(data1, function (i, val) {
-console.log('ready i ' + i);
-console.log('ready text ' + val.text);
-countryleafletsearch = val.text;
-});
-});
-console.log('ready url ' + url );
-console.log('ready countryleafletsearch ' + countryleafletsearch );
+        if (!country) {
+            return '';
+        }
 
-street = dpjQuery("#address").val();
-zip = dpjQuery("#zipcode").val();
-	
-if (dpjQuery('#city').length == 0) {
-console.log('Das Element mit der ID city ist nicht vorhanden.');
-city = dpjQuery("#location").val();
-}
-else	
-{	
-city = dpjQuery("#city").val();
-}	
-yourQuery = ( street + ',' + zip + ' ' + city + ',' + countryleafletsearch );
+        if (country instanceof HTMLSelectElement && country.selectedIndex >= 0) {
+            const option = country.options[country.selectedIndex];
 
-console.log('ready yourQuery ' + yourQuery );
+            if (option && option.value !== '') {
+                return String(option.textContent || '').trim();
+            }
+        }
 
-  
-});
-*/
+        return fieldValue('country');
+    }
 
-function getlatlonopenstreet(result)
-{
-dpjQuery("#geocomplete").val(getAddresString());
-dpjQuery("#geocomplete").trigger("geocode");	
-	
-	
-var inp = dpjQuery("#geocomplete").val();
-console.log('geocomplete ' + inp );
-//var xmlhttp = new XMLHttpRequest();
-var url = "https://nominatim.openstreetmap.org/search?format=json&addressdetails=1&limit=1&q=" + inp ;
-console.log('openstreetmap url ' + url );
-dpjQuery("#extended_COM_SPORTSMANAGEMENT_ADMINISTRATIVE_AREA_LEVEL_1_LONG_NAME").val('');
-dpjQuery("#extended_COM_SPORTSMANAGEMENT_ADMINISTRATIVE_AREA_LEVEL_1_SHORT_NAME").val('');
-dpjQuery("#extended_COM_SPORTSMANAGEMENT_ADMINISTRATIVE_AREA_LEVEL_2_LONG_NAME").val('');
-dpjQuery("#extended_COM_SPORTSMANAGEMENT_ADMINISTRATIVE_AREA_LEVEL_2_SHORT_NAME").val('');
-dpjQuery("#extended_COM_SPORTSMANAGEMENT_ADMINISTRATIVE_AREA_LEVEL_3_LONG_NAME").val('');
-dpjQuery("#extended_COM_SPORTSMANAGEMENT_ADMINISTRATIVE_AREA_LEVEL_3_SHORT_NAME").val('');
-	
-dpjQuery.ajax({
-url:url,
-dataType: 'json',
-async: false,
-type: "POST",
-success:function(res){
-console.log('openstreetmap ' + res );
+    function getAddressString() {
+        const parts = [];
+        const street = fieldValue('address');
+        const city = fieldValue('city') || fieldValue('location');
+        const zipcode = fieldValue('zipcode');
+        const state = fieldValue('state');
+        const country = selectedCountryName();
 
-dpjQuery.each(res , function (i, val) {
-console.log(i);
-console.log(val);
+        if (street) {
+            parts.push(street);
+        }
 
-console.log('latitude ' + val.lat);
-console.log('longitude ' + val.lon);
+        const locality = [zipcode, city].filter(Boolean).join(' ');
 
-console.log('121 county ' + val.address.county);
-console.log('state_district ' + val.address.state_district);
-console.log('state ' + val.address.state);
-console.log('city_district ' + val.address.city_district);
+        if (locality) {
+            parts.push(locality);
+        }
 
-console.log('postcode ' + val.address.postcode);
-console.log('road ' + val.address.road);
-console.log('suburb ' + val.address.suburb);
-console.log('neighbourhood ' + val.address.neighbourhood);
+        if (state) {
+            parts.push(state);
+        }
 
+        if (country) {
+            parts.push(country);
+        }
 
-state = val.address.state;
+        return parts.join(', ');
+    }
 
-dpjQuery("#extended_COM_SPORTSMANAGEMENT_ADMINISTRATIVE_AREA_LEVEL_1_LONG_NAME").val(state);
-dpjQuery("#state").val(state);	
+    function clearAdministrativeFields() {
+        [
+            'extended_COM_SPORTSMANAGEMENT_ADMINISTRATIVE_AREA_LEVEL_1_LONG_NAME',
+            'extended_COM_SPORTSMANAGEMENT_ADMINISTRATIVE_AREA_LEVEL_1_SHORT_NAME',
+            'extended_COM_SPORTSMANAGEMENT_ADMINISTRATIVE_AREA_LEVEL_2_LONG_NAME',
+            'extended_COM_SPORTSMANAGEMENT_ADMINISTRATIVE_AREA_LEVEL_2_SHORT_NAME',
+            'extended_COM_SPORTSMANAGEMENT_ADMINISTRATIVE_AREA_LEVEL_3_LONG_NAME',
+            'extended_COM_SPORTSMANAGEMENT_ADMINISTRATIVE_AREA_LEVEL_3_SHORT_NAME',
+        ].forEach((id) => setElementValue(id, ''));
+    }
 
-if ( val.address.county )
-{
-dpjQuery("#extended_COM_SPORTSMANAGEMENT_ADMINISTRATIVE_AREA_LEVEL_2_LONG_NAME").val(val.address.county);	
-dpjQuery("#state").val(val.address.county);	
-}	
-if ( val.address.state_district )
-{
-dpjQuery("#extended_COM_SPORTSMANAGEMENT_ADMINISTRATIVE_AREA_LEVEL_3_LONG_NAME").val(val.address.state_district);	
-}	
-	
-dpjQuery("#latitude").val(val.lat);
-dpjQuery("#longitude").val(val.lon);
-/*
-if ( result )
-{
-addLayer(val.lat,val.lon);
-}
-*/
-}); 
-}
-});
+    function applyNominatimResult(result) {
+        const address = result && typeof result.address === 'object' ? result.address : {};
+        const state = address.state || address.region || '';
+        const county = address.county || '';
+        const district = address.state_district || address.city_district || '';
 
-}
+        setElementValue(
+            'extended_COM_SPORTSMANAGEMENT_ADMINISTRATIVE_AREA_LEVEL_1_LONG_NAME',
+            state
+        );
+        setElementValue(
+            'extended_COM_SPORTSMANAGEMENT_ADMINISTRATIVE_AREA_LEVEL_2_LONG_NAME',
+            county
+        );
+        setElementValue(
+            'extended_COM_SPORTSMANAGEMENT_ADMINISTRATIVE_AREA_LEVEL_3_LONG_NAME',
+            district
+        );
 
-function getAddresString()
-{
+        if (state) {
+            setFieldValue('state', state);
+        }
 
-	street = '';
-	city = '';
-	country = '';
-	if(dpjQuery("#address").val()){
-		street = dpjQuery("#address").val();
-		street += ', ';
-	}
-	
-if (dpjQuery('#city').length == 0) {
-console.log('Das Element mit der ID city ist nicht vorhanden.');
-	if(dpjQuery("#location").val()){
-		city = dpjQuery("#location").val();
-		if(dpjQuery("#zipcode").val()){
-			city += ' ' + dpjQuery("#zipcode").val();
-		}
-		city += ', ';
-	}
-}
-	else
-	{
-console.log('Das Element mit der ID city ist vorhanden.');		
-	if(dpjQuery("#city").val()){
-		city = dpjQuery("#city").val();
-		if(dpjQuery("#zipcode").val()){
-			city += ' ' + dpjQuery("#zipcode").val();
-		}
-		city += ', ';
-	}
-	}
-	
-	if (dpjQuery("#state").val()) {
-		province = dpjQuery("#state").val() + ', ';
-	}
-	/*
-if (dpjQuery('#address_country').length == 0) {
-console.log('Das Element mit der ID address_country ist nicht vorhanden.');	
-countryleaflet = dpjQuery("#country").val();	
-}
-	else
-	{
-console.log('Das Element mit der ID address_country ist vorhanden.');		
-countryleaflet = dpjQuery("#address_country").val();
-	}
-*/
-countryleaflet = dpjQuery("#country").val();	
-if ( countryleaflet === 'DDR')
-{
-countryleaflet = 'DEU';
-}		
-	
-console.log('getAddresString countryleaflet ' + countryleaflet);
+        if (result.lat != null) {
+            setFieldValue('latitude', result.lat);
+        }
 
-var url = baseurl + '/administrator/index.php?option=com_sportsmanagement&format=json&tmpl=component&task=ajax.getCcountryAlpha2&country=' + countryleaflet;
-dpjQuery.ajax({
-url: url,
-dataType: 'json',
-async: false,
-type : 'POST'
-}).done(function(data1) {
-console.log(data1);
+        if (result.lon != null) {
+            setFieldValue('longitude', result.lon);
+        }
 
-dpjQuery.each(data1, function (i, val) {
-console.log(i);
-console.log(val.text);
+        if (result.display_name) {
+            setFieldValue('geocomplete', result.display_name);
+        }
+    }
 
-countryleafletsearch = val.text;
+    async function getLatLonOpenStreet() {
+        const query = getAddressString();
+        setFieldValue('geocomplete', query);
 
-});
+        if (!query) {
+            return false;
+        }
 
-});	
+        clearAdministrativeFields();
 
-var url2 = baseurl +'/administrator/index.php?option=com_sportsmanagement&format=json&tmpl=component&task=ajax.getCcountryName&country=' + countryleaflet;
-dpjQuery.ajax({
-url: url2,
-dataType: 'json',
-async: false,
-type : 'POST'
-}).done(function(data2) {
-console.log(data2);
+        if (activeRequest) {
+            activeRequest.abort();
+        }
 
-dpjQuery.each(data2, function (i, val) {
-console.log(i);
-console.log(val.text);
+        activeRequest = new AbortController();
 
-country = val.text;
+        const url = new URL('https://nominatim.openstreetmap.org/search');
+        url.search = new URLSearchParams({
+            format: 'jsonv2',
+            addressdetails: '1',
+            limit: '1',
+            q: query,
+        }).toString();
 
-});
+        try {
+            const response = await fetch(url.toString(), {
+                method: 'GET',
+                headers: {
+                    Accept: 'application/json',
+                },
+                signal: activeRequest.signal,
+            });
 
-});	
-	
-	
-console.log('getAddresString country alpha2 leaflet ' + countryleafletsearch );
-console.log('getAddresString country  ' + country );	
-console.log('getAddresString city  ' + city );	
-console.log('getAddresString street ' + street);
-  
-	return street + city + country;
-}
+            if (!response.ok) {
+                throw new Error(`Nominatim request failed with status ${response.status}`);
+            }
 
-function setGeoResult(result)
-{
-var street_number = '';
-var route = '';
-	
-	dpjQuery('#location-form #details input:not("#title")').removeAttr('value');
-	
-	for(var i=0;i<result.address_components.length;i++){
-		switch(result.address_components[i].types[0]){
+            const results = await response.json();
+            const result = Array.isArray(results) ? results[0] : null;
 
-			case 'route':
-				dpjQuery("#address").val(result.address_components[i].long_name);
-				route = result.address_components[i].long_name;
-			break;
-			case 'locality':
-				dpjQuery("#city").val(result.address_components[i].long_name);
-			break;
-			case 'street_number':
-			street_number = result.address_components[i].long_name;
-			break;
+            if (result) {
+                applyNominatimResult(result);
+            }
+        } catch (error) {
+            if (error instanceof DOMException && error.name === 'AbortError') {
+                return false;
+            }
 
-		  case 'administrative_area_level_1':
-			dpjQuery("#state").val(result.address_components[i].long_name);
-      dpjQuery("#extended_COM_SPORTSMANAGEMENT_ADMINISTRATIVE_AREA_LEVEL_1_LONG_NAME").val(result.address_components[i].long_name);
-      dpjQuery("#extended_COM_SPORTSMANAGEMENT_ADMINISTRATIVE_AREA_LEVEL_1_SHORT_NAME").val(result.address_components[i].short_name);
-			break;
-      case 'administrative_area_level_2':
-      dpjQuery("#extended_COM_SPORTSMANAGEMENT_ADMINISTRATIVE_AREA_LEVEL_2_LONG_NAME").val(result.address_components[i].long_name);
-      dpjQuery("#extended_COM_SPORTSMANAGEMENT_ADMINISTRATIVE_AREA_LEVEL_2_SHORT_NAME").val(result.address_components[i].short_name);
-			break;
-      
-			case 'postal_code':
-				dpjQuery("#zipcode").val(result.address_components[i].long_name);
-			break;
-		}
-	}
+            console.error('SportsManagement geocoding request failed.', error);
+        } finally {
+            activeRequest = null;
+        }
 
-route += ' ';
-route += street_number;
-dpjQuery("#address").val(route);
-	
-	if (typeof result.geometry.location.lat === 'function')
-	{
-		dpjQuery("#latitude").val(result.geometry.location.lat());
-		dpjQuery("#longitude").val(result.geometry.location.lng());
-	} else
-	{		
-		dpjQuery("#latitude").val(result.geometry.location.lat);
-		dpjQuery("#longitude").val(result.geometry.location.lng);
-	}
+        return false;
+    }
 
-var lat = dpjQuery("#latitude").val();
-var lng = dpjQuery("#longitude").val();	
-console.log('lat ' + lat );
-console.log('lng ' + lng );
-//addLayer(lat,lng);
-	
-	dpjQuery("#geocomplete").val(result.formatted_address);
-}
+    function setGeoResult(result) {
+        if (!result || !Array.isArray(result.address_components)) {
+            return;
+        }
 
-/*
-function addLayer(lat,lng) {
-	
-var markerLocation = new L.LatLng(lat,lng);
-var marker = new L.Marker(markerLocation);
-//console.log(marker);
-console.log("Adding layer");
+        let route = '';
+        let streetNumber = '';
 
+        result.address_components.forEach((component) => {
+            const type = Array.isArray(component.types) ? component.types[0] : '';
 
-    
-//L.marker([lat, lng]).addTo(layerGroup);
-//console.log(layerGroup);
-//map.removeLayer(layerGroup);
-//Add a marker to show where you clicked.
-theMarker = L.marker([lat,lng]).addTo(map);  
-map.setView(new L.LatLng(lat, lng), 15);	
-//L.marker([lat, lng]).addTo(layerGroup);
-//layerGroup.addLayer(marker);
+            switch (type) {
+                case 'route':
+                    route = component.long_name || '';
+                    break;
+                case 'locality':
+                case 'postal_town':
+                    setFieldValue('city', component.long_name || '');
+                    setFieldValue('location', component.long_name || '');
+                    break;
+                case 'street_number':
+                    streetNumber = component.long_name || '';
+                    break;
+                case 'administrative_area_level_1':
+                    setFieldValue('state', component.long_name || '');
+                    setElementValue(
+                        'extended_COM_SPORTSMANAGEMENT_ADMINISTRATIVE_AREA_LEVEL_1_LONG_NAME',
+                        component.long_name || ''
+                    );
+                    setElementValue(
+                        'extended_COM_SPORTSMANAGEMENT_ADMINISTRATIVE_AREA_LEVEL_1_SHORT_NAME',
+                        component.short_name || ''
+                    );
+                    break;
+                case 'administrative_area_level_2':
+                    setElementValue(
+                        'extended_COM_SPORTSMANAGEMENT_ADMINISTRATIVE_AREA_LEVEL_2_LONG_NAME',
+                        component.long_name || ''
+                    );
+                    setElementValue(
+                        'extended_COM_SPORTSMANAGEMENT_ADMINISTRATIVE_AREA_LEVEL_2_SHORT_NAME',
+                        component.short_name || ''
+                    );
+                    break;
+                case 'administrative_area_level_3':
+                    setElementValue(
+                        'extended_COM_SPORTSMANAGEMENT_ADMINISTRATIVE_AREA_LEVEL_3_LONG_NAME',
+                        component.long_name || ''
+                    );
+                    setElementValue(
+                        'extended_COM_SPORTSMANAGEMENT_ADMINISTRATIVE_AREA_LEVEL_3_SHORT_NAME',
+                        component.short_name || ''
+                    );
+                    break;
+                case 'postal_code':
+                    setFieldValue('zipcode', component.long_name || '');
+                    break;
+                default:
+                    break;
+            }
+        });
 
-}
-	*/		
+        setFieldValue('address', [route, streetNumber].filter(Boolean).join(' '));
+
+        const location = result.geometry && result.geometry.location;
+
+        if (location) {
+            const latitude = typeof location.lat === 'function' ? location.lat() : location.lat;
+            const longitude = typeof location.lng === 'function' ? location.lng() : location.lng;
+
+            if (latitude != null) {
+                setFieldValue('latitude', latitude);
+            }
+
+            if (longitude != null) {
+                setFieldValue('longitude', longitude);
+            }
+        }
+
+        if (result.formatted_address) {
+            setFieldValue('geocomplete', result.formatted_address);
+        }
+    }
+
+    window.getlatlonopenstreet = getLatLonOpenStreet;
+    window.getAddresString = getAddressString;
+    window.getAddressString = getAddressString;
+    window.setGeoResult = setGeoResult;
+
+    document.addEventListener('DOMContentLoaded', () => {
+        const query = getAddressString();
+        setFieldValue('geocomplete', query);
+
+        const latitude = Number.parseFloat(fieldValue('latitude'));
+        const longitude = Number.parseFloat(fieldValue('longitude'));
+        const hasCoordinates = Number.isFinite(latitude)
+            && Number.isFinite(longitude)
+            && (latitude !== 0 || longitude !== 0);
+
+        if (query && !hasCoordinates) {
+            getLatLonOpenStreet();
+        }
+    });
+}());
