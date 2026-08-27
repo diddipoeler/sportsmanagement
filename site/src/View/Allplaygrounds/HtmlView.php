a69d92f545fc8c7c2856a962e6c654110022c949
diddipoeler <diddipoeler@gmx.de>
2026-08-28 00:03:51 +0200

    Remove legacy loader from all playgrounds view

--- site/src/View/Allplaygrounds/HtmlView.php
+++ site/src/View/Allplaygrounds/HtmlView.php
@@ -5,7 +5,6 @@ namespace Diddipoeler\Component\SportsManagement\Site\View\Allplaygrounds;
 
 use Diddipoeler\Component\SportsManagement\Site\Helper\CountryPresentationHelper;
 use Diddipoeler\Component\SportsManagement\Site\Model\AllplaygroundsModel;
-use Diddipoeler\Component\SportsManagement\Site\Service\LegacyPresentationLoader;
 use Diddipoeler\Component\SportsManagement\Site\View\SportsManagementHtmlView;
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
@@ -31,7 +30,6 @@ final class HtmlView extends SportsManagementHtmlView
     {
         parent::__construct($config);
         $this->addTemplatePath(JPATH_SITE . '/components/com_sportsmanagement/tmpl/globalviews');
-        LegacyPresentationLoader::register();
     }
 
     public function display($tpl = null)
