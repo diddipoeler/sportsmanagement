4720ed2a6f674c8754927363988f1044dc6e3170
diddipoeler <diddipoeler@gmx.de>
2026-08-28 00:11:38 +0200

    Remove legacy helpers from teams main template

--- site/tmpl/teams/default.php
+++ site/tmpl/teams/default.php
@@ -1,33 +1,19 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage teams
- * @file       default.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
+/** SportsManagement teams template for Joomla 5/6. */
 defined('_JEXEC') or die('Restricted access');
-
-$templatesToLoad = array('globalviews');
-sportsmanagementHelper::addTemplatePaths($templatesToLoad, $this);
 ?>
-<div class="<?php echo $this->divclasscontainer; ?>" id="defaultteams">
-	<?php
-	echo $this->loadTemplate('projectheading');
+<div class="<?php echo $this->escape($this->divclasscontainer); ?>" id="defaultteams">
+    <?php echo $this->loadTemplate('projectheading'); ?>
 
-	if ($this->config['show_sectionheader'])
-	{
-		echo $this->loadTemplate('sectionheader');
-	}
+    <?php if (!empty($this->config['show_sectionheader'])) : ?>
+        <div class="<?php echo $this->escape($this->divclassrow); ?>" id="sectionheader">
+            <p><strong><?php echo $this->escape($this->headertitle); ?></strong></p>
+        </div>
+    <?php endif; ?>
 
-	if ($this->config['show_teams'])
-	{
-		echo $this->loadTemplate('teams');
-	}
+    <?php if (!empty($this->config['show_teams'])) : ?>
+        <?php echo $this->loadTemplate('teams'); ?>
+    <?php endif; ?>
 
-	echo $this->loadTemplate('jsminfo');
-	?>
+    <?php echo $this->loadTemplate('jsminfo'); ?>
 </div>
