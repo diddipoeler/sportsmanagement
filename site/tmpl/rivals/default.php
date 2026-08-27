f08f1ea0126a7ecc1a202db923b1dc73f01564fb
diddipoeler <diddipoeler@gmx.de>
2026-08-27 23:23:54 +0200

    Detach rivals from legacy section header template

--- site/tmpl/rivals/default.php
+++ site/tmpl/rivals/default.php
@@ -4,12 +4,22 @@
  */
 \defined('_JEXEC') or die;
 ?>
-<div class="<?php echo $this->divclasscontainer; ?>" id="rivals">
-    <?php
-    if (!empty($this->config['show_sectionheader'])) {
-        echo $this->loadTemplate('sectionheader');
-    }
+<div class="<?php echo htmlspecialchars((string) $this->divclasscontainer, ENT_QUOTES, 'UTF-8'); ?>" id="rivals">
+    <?php if (!empty($this->config['show_sectionheader'])) : ?>
+        <div class="<?php echo htmlspecialchars((string) $this->divclassrow, ENT_QUOTES, 'UTF-8'); ?>" id="sectionheader">
+            <div class="color-box">
+                <div class="shadow">
+                    <div class="info-tab note-icon" title="sectionheader"><i></i></div>
+                    <div class="note-box">
+                        <p><strong><?php echo htmlspecialchars((string) $this->headertitle, ENT_QUOTES, 'UTF-8'); ?></strong></p>
+                    </div>
+                </div>
+            </div>
+            <br>
+        </div>
+    <?php endif; ?>
 
+    <?php
     echo $this->loadTemplate('projectheading');
     echo $this->loadTemplate('rivals');
     echo $this->loadTemplate('jsminfo');
