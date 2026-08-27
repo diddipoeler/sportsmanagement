2bd93d46f51374d526fc03f3b3b7aec63eeca267
diddipoeler <diddipoeler@gmx.de>
2026-08-28 00:02:29 +0200

    Remove legacy helper from all projects template

--- site/tmpl/allprojects/default.php
+++ site/tmpl/allprojects/default.php
@@ -1,13 +1,6 @@
 <?php
 /**
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage allprojects
- * @file       default.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * SportsManagement all projects template for Joomla 5/6.
  */
 defined('_JEXEC') or die('Restricted access');
 
@@ -16,44 +9,47 @@ use Joomla\CMS\Language\Text;
 
 HTMLHelper::_('behavior.keepalive');
 $this->getDocument()->getWebAssetManager()->useScript('jquery');
-
-$templatesToLoad = ['globalviews'];
-sportsmanagementHelper::addTemplatePaths($templatesToLoad, $this);
 ?>
 <script>
-    function tableOrdering(order, dir, task) {
-        const form = document.adminForm;
-        form.filter_order.value = order;
-        form.filter_order_Dir.value = dir;
-        form.submit();
-    }
+function tableOrdering(order, dir) {
+    const form = document.getElementById('adminForm');
+    form.filter_order.value = order;
+    form.filter_order_Dir.value = dir;
+    form.submit();
+}
 </script>
 <?php if ($this->params->get('show_page_heading', 1)) : ?>
     <h1 class="componentheading">
         <?php echo $this->escape($this->params->get('page_title')); ?>
     </h1>
 <?php endif; ?>
-<div class="<?php echo $this->divclasscontainer; ?>" id="allprojects">
-    <form name="adminForm" id="adminForm" action="<?php echo htmlspecialchars($this->uri->toString()); ?>" method="post">
+<div class="<?php echo $this->escape($this->divclasscontainer); ?>" id="allprojects">
+    <form name="adminForm" id="adminForm"
+          action="<?php echo htmlspecialchars($this->uri->toString(), ENT_QUOTES, 'UTF-8'); ?>"
+          method="post">
         <fieldset class="filters">
             <legend class="hidelabeltxt"><?php echo Text::_('JGLOBAL_FILTER_LABEL'); ?></legend>
             <div class="filter-search">
                 <input type="text" name="filter_search" id="filter_search"
                        value="<?php echo $this->escape($this->filter); ?>" class="inputbox"
-                       onchange="document.getElementById('adminForm').submit();"/>
+                       onchange="this.form.submit();">
 
-                <button type="submit" class="btn" title=""><i class="icon-search"></i><?php echo Text::_('JGLOBAL_FILTER_BUTTON'); ?></button>
-                <button type="button" class="btn" title=""
-                        onclick="document.getElementById('filter_search').value='';this.form.submit();"><i class="icon-remove"></i><?php echo Text::_('JSEARCH_FILTER_CLEAR'); ?></button>
+                <button type="submit" class="btn">
+                    <span class="icon-search" aria-hidden="true"></span><?php echo Text::_('JGLOBAL_FILTER_BUTTON'); ?>
+                </button>
+                <button type="button" class="btn"
+                        onclick="document.getElementById('filter_search').value='';this.form.submit();">
+                    <span class="icon-remove" aria-hidden="true"></span><?php echo Text::_('JSEARCH_FILTER_CLEAR'); ?>
+                </button>
 
                 <?php echo $this->lists['nation2'] . '&nbsp;&nbsp;'; ?>
                 <?php echo $this->lists['leagues'] . '&nbsp;&nbsp;'; ?>
                 <?php echo $this->lists['seasons'] . '&nbsp;&nbsp;'; ?>
             </div>
 
-            <input type="hidden" name="filter_order" value="<?php echo $this->sortColumn; ?>"/>
-            <input type="hidden" name="filter_order_Dir" value="<?php echo $this->sortDirection; ?>"/>
-            <input type="hidden" name="limitstart" value=""/>
+            <input type="hidden" name="filter_order" value="<?php echo $this->escape($this->sortColumn); ?>">
+            <input type="hidden" name="filter_order_Dir" value="<?php echo $this->escape($this->sortDirection); ?>">
+            <input type="hidden" name="limitstart" value="">
 
             <div class="display-limit">
                 <?php echo Text::_('JGLOBAL_DISPLAY_NUM'); ?>&#160;
@@ -61,9 +57,7 @@ sportsmanagementHelper::addTemplatePaths($templatesToLoad, $this);
             </div>
         </fieldset>
 
-        <?php
-        echo $this->loadTemplate('items');
-        echo $this->loadTemplate('jsminfo');
-        ?>
+        <?php echo $this->loadTemplate('items'); ?>
+        <?php echo $this->loadTemplate('jsminfo'); ?>
     </form>
 </div>
