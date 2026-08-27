cec5fab3866891438ba2755aafcd2f0aaa67ee4b
diddipoeler <diddipoeler@gmx.de>
2026-08-27 23:58:20 +0200

    Use native helpers in all persons items template

--- site/tmpl/allpersons/default_items.php
+++ site/tmpl/allpersons/default_items.php
@@ -6,7 +6,7 @@
  * @version    1.0.05
  * @package    Sportsmanagement
  * @subpackage allpersons
- * @file       default_irems.php
+ * @file       default_items.php
  * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
  * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
@@ -14,107 +14,130 @@
 
 defined('_JEXEC') or die('Restricted access');
 
-use Joomla\CMS\Language\Text;
+use Diddipoeler\Component\SportsManagement\Site\Helper\CountryPresentationHelper;
+use Diddipoeler\Component\SportsManagement\Site\Helper\DatePresentationHelper;
+use Diddipoeler\Component\SportsManagement\Site\Helper\ModalImageHelper;
+use Diddipoeler\Component\SportsManagement\Site\Helper\SiteRouteHelper;
 use Joomla\CMS\HTML\HTMLHelper;
-use Joomla\CMS\Factory;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Uri\Uri;
 
+$useExternalPictureServer = (bool) $this->params->get('cfg_dbprefix')
+    || (bool) $this->params->get('cfg_which_database')
+    || $this->databaseSelector === 1;
+$pictureServer = $useExternalPictureServer
+    ? trim((string) $this->params->get('cfg_which_database_server', ''))
+    : Uri::root();
+if ($pictureServer === '') {
+    $pictureServer = Uri::root();
+}
+$pictureUrl = static function (string $picture) use ($pictureServer): string {
+    $picture = trim($picture);
+
+    if ($picture === '') {
+        return '';
+    }
+
+    return preg_match('#^https?://#i', $picture)
+        ? $picture
+        : rtrim($pictureServer, '/') . '/' . ltrim($picture, '/');
+};
 ?>
 <div class="<?php echo $this->divclassrow; ?> table-responsive" id="allpersons">
     <table class="<?php echo $this->tableclass; ?>">
-
         <thead>
         <tr>
-
-			<?PHP
-			if ($this->columns)
-			{
-				foreach ($this->columns as $key => $value)
-				{
-					?>
+            <?php if ($this->columns) : ?>
+                <?php foreach ($this->columns as $value) : ?>
                     <th class="" id="">
-						<?php echo HTMLHelper::_('grid.sort', 'COM_SPORTSMANAGEMENT_FES_ALLPERSONS_' . strtoupper($value), 'v.' . $value, $this->sortDirection, $this->sortColumn); ?>
+                        <?php echo HTMLHelper::_('grid.sort', 'COM_SPORTSMANAGEMENT_FES_ALLPERSONS_' . strtoupper($value), 'v.' . $value, $this->sortDirection, $this->sortColumn); ?>
                     </th>
-					<?PHP
-				}
-			}
-			?>
+                <?php endforeach; ?>
+            <?php endif; ?>
         </tr>
         </thead>
 
-
-		<?php foreach ($this->items as $field => $item)
-			:
-			?>
+        <?php foreach ($this->items as $i => $item) : ?>
             <tr class="row<?php echo $i % 2; ?>">
-
-				<?php
-
-				foreach ($this->columns as $key => $value)
-				{
-					?>
+                <?php foreach ($this->columns as $value) : ?>
                     <td>
-						<?PHP
-						switch ($value)
-						{
-							case 'lastname':
-								if ($item->projectslug)
-								{
-									$routeparameter                       = array();
-									$routeparameter['cfg_which_database'] = Factory::getApplication()->input->getInt('cfg_which_database', 0);
-									$routeparameter['s']                  = Factory::getApplication()->input->getInt('s', 0);
-									$routeparameter['p']                  = $item->projectslug;
-									$routeparameter['tid']                = $item->teamslug;
-									$routeparameter['pid']                = $item->slug;
-									$link                                 = sportsmanagementHelperRoute::getSportsmanagementRoute('player', $routeparameter);
-									echo HTMLHelper::link($link, $item->$value);
-								}
-								else
-								{
-									echo $item->$value;
-								}
-								break;
-							case 'country':
-								echo JSMCountries::getCountryFlag($item->$value);
-								break;
-							case 'picture':
-								echo sportsmanagementHelperHtml::getBootstrapModalImage(
-									'allperson' . $item->id, $item->$value, $item->lastname, '20', '', $this->modalwidth,
-									$this->modalheight,
-									$this->use_jquery_modal
-								);
-								break;
-							case 'website':
-								echo HTMLHelper::link($item->$value, $item->$value, array('target' => '_blank'));
-								break;
-							case 'birthday':
-							case 'deathday':
-								echo sportsmanagementHelper::convertDate($item->$value, 1);
-								break;
-							case 'position_id':
-								echo Text::_($item->position_name);
-								break;
-							default:
-								echo $item->$value;
-								break;
-						}
-						?>
+                        <?php
+                        $fieldValue = $item->{$value} ?? '';
+
+                        switch ($value)
+                        {
+                            case 'lastname':
+                                if ($item->projectslug)
+                                {
+                                    $link = SiteRouteHelper::view('player', [
+                                        'cfg_which_database' => $this->databaseSelector,
+                                        's' => $this->input->getInt('s', 0),
+                                        'p' => $item->projectslug,
+                                        'tid' => $item->teamslug,
+                                        'pid' => $item->slug,
+                                    ]);
+                                    echo HTMLHelper::link($link, $fieldValue);
+                                }
+                                else
+                                {
+                                    echo $fieldValue;
+                                }
+                                break;
+
+                            case 'country':
+                                echo CountryPresentationHelper::flag((string) $fieldValue);
+                                break;
+
+                            case 'picture':
+                                echo ModalImageHelper::render(
+                                    'allperson' . (int) $item->id,
+                                    $pictureUrl((string) $fieldValue),
+                                    (string) ($item->lastname ?? ''),
+                                    20,
+                                    '',
+                                    $this->modalwidth,
+                                    $this->modalheight,
+                                    $this->use_jquery_modal
+                                );
+                                break;
+
+                            case 'website':
+                                if ($fieldValue !== '') {
+                                    echo HTMLHelper::link(
+                                        (string) $fieldValue,
+                                        (string) $fieldValue,
+                                        ['target' => '_blank', 'rel' => 'noopener']
+                                    );
+                                }
+                                break;
+
+                            case 'birthday':
+                            case 'deathday':
+                                echo DatePresentationHelper::convert((string) $fieldValue, 1);
+                                break;
+
+                            case 'position_id':
+                                echo Text::_((string) ($item->position_name ?? ''));
+                                break;
+
+                            default:
+                                echo $fieldValue;
+                                break;
+                        }
+                        ?>
                     </td>
-					<?PHP
-				}
-
-				?>
-
+                <?php endforeach; ?>
             </tr>
-		<?php endforeach; ?>
+        <?php endforeach; ?>
     </table>
 </div>
 
 <div class="pagination">
     <p class="counter">
-		<?php echo $this->pagination->getPagesCounter(); ?>
+        <?php echo $this->pagination->getPagesCounter(); ?>
     </p>
     <p class="counter">
-		<?php echo $this->pagination->getResultsCounter(); ?>
+        <?php echo $this->pagination->getResultsCounter(); ?>
     </p>
-	<?php echo $this->pagination->getPagesLinks(); ?>
+    <?php echo $this->pagination->getPagesLinks(); ?>
 </div>
