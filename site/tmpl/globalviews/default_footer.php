6d0bed787e44e0f145160501beaf695ff8dfa307
diddipoeler <diddipoeler@gmx.de>
2026-08-27 23:23:22 +0200

    Modernize shared frontend footer for Joomla 6

--- site/tmpl/globalviews/default_footer.php
+++ site/tmpl/globalviews/default_footer.php
@@ -1,156 +1,87 @@
 <?php
 /**
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage globalviews
- * @file       deafault_footer.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * Shared Joomla 5/6 frontend footer.
  */
-defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\Language\Text;
-use Joomla\CMS\HTML\HTMLHelper;
-use Joomla\CMS\Uri\Uri;
-use Joomla\CMS\Factory;
+\defined('_JEXEC') or die;
+
 use Joomla\CMS\Component\ComponentHelper;
+use Joomla\CMS\Extension\ExtensionHelper;
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Uri\Uri;
+use Joomla\Registry\Registry;
 
-$option                = Factory::getApplication()->input->getCmd('option');
-$view                  = Factory::getApplication()->input->getVar("view");
-$view                  = ucfirst(strtolower($view));
-$cfg_help_server       = ComponentHelper::getParams($option)->get('cfg_help_server', '');
-$modal_popup_width     = ComponentHelper::getParams($option)->get('modal_popup_width', 0);
-$modal_popup_height    = ComponentHelper::getParams($option)->get('modal_popup_height', 0);
-$show_facebook_link    = ComponentHelper::getParams($option)->get('show_facebook_link', 0);
-$cfg_bugtracker_server = ComponentHelper::getParams($option)->get('cfg_bugtracker_server', '');
-$logo_width            = ComponentHelper::getParams($option)->get('logo_picture_width', 100);
+$params = ComponentHelper::getParams('com_sportsmanagement');
+$view = ucfirst(strtolower($this->input->getCmd('view', (string) ($this->view ?? ''))));
+$helpServer = trim((string) $params->get('cfg_help_server', ''));
+$showFacebookLink = (int) $params->get('show_facebook_link', 0);
+$logoWidth = max(1, (int) $params->get('logo_picture_width', 100));
+$version = '';
+
+try {
+    $extension = ExtensionHelper::getExtensionRecord('com_sportsmanagement', 'component');
+    if ($extension && !empty($extension->manifest_cache)) {
+        $manifest = new Registry();
+        $manifest->loadString((string) $extension->manifest_cache, 'JSON');
+        $version = (string) $manifest->get('version', '');
+    }
+} catch (\Throwable) {
+    $version = '';
+}
+
+$helpLink = $helpServer !== '' ? $helpServer . 'SM-Frontend:' . $view : '';
 ?>
+<div class="row">
+    <div class="container text-center align-items-center justify-content-center">
+        <br>
+        <?php echo Text::_('COM_SPORTSMANAGEMENT_DESC'); ?>
+        <br>
+        <img
+            src="<?php echo htmlspecialchars(Uri::root(true) . '/components/com_sportsmanagement/assets/images/fussballineuropa.png', ENT_QUOTES, 'UTF-8'); ?>"
+            width="<?php echo $logoWidth; ?>"
+            alt="Fussball in Europa"
+        >
+        <?php echo Text::_('COM_SPORTSMANAGEMENT_COPYRIGHT'); ?>: &copy;
+        <a href="https://www.fussballineuropa.de" target="_blank" rel="noopener">Fussball in Europa</a>
+        <br>
 
-<style>
-    .modaljsm {
-        width: 80%;
-        height: 60%;
-    }
+        <?php if ($showFacebookLink === 3) : ?>
+            <img
+                src="<?php echo htmlspecialchars(Uri::root(true) . '/components/com_sportsmanagement/assets/images/facebook.png', ENT_QUOTES, 'UTF-8'); ?>"
+                width="<?php echo $logoWidth; ?>"
+                alt="Facebook"
+            >
+            <a href="https://www.facebook.com/joomlasportsmanagement/" target="_blank" rel="noopener">JSM auf Facebook</a>
+            <br>
+        <?php endif; ?>
 
-</style>
+        <?php echo Text::_('COM_SPORTSMANAGEMENT_VERSION'); ?>:
+        <?php echo htmlspecialchars($version !== '' ? $version . ' (diddipoeler)' : 'diddipoeler', ENT_QUOTES, 'UTF-8'); ?>
+        <br>
 
-<style>
-    .modal-dialog {
-        width: 80%;
-    }
+        <?php if (isset($this->jsmseitenaufbau)) : ?>
+            <div class="center">
+                <?php echo Text::sprintf('COM_SPORTSMANAGEMENT_FOOTER_TIME', $this->jsmseitenaufbau); ?>
+            </div>
+        <?php endif; ?>
 
-    .modal-dialog,
-    .modal-content {
-        /* 95% of window height */
-        height: 95%;
-    }
-</style>
-
-<script type="text/javascript">
-Joomla.popupWindow = function(mypage, myname, w, h, scroll) {
-		var winl = (screen.width - w) / 2,
-		    wint = (screen.height - h) / 2,
-		    winprops = 'height=' + h +
-			    ',width=' + w +
-			    ',top=' + wint +
-			    ',left=' + winl +
-			    ',scrollbars=' + scroll +
-			    ',resizable';
-
-		window.open(mypage, myname, winprops).window.focus();
-	};
-    function openLink(url) {
-        var width = get_windowPopUpWidth();
-        var heigth = get_windowPopUpHeight();
-
-        SqueezeBox.open(url, {
-            handler: 'iframe',
-            size: {x: width, y: heigth}
-        });
-
-    }
-
-</script>
-
-
-  
-  <div class="row">
-<div class="container text-center align-items-center justify-content-center">
- <br/>
-	<?php echo Text::_("COM_SPORTSMANAGEMENT_DESC"); ?>
-    <br/>
-    <img src="<?php echo Uri::root(true); ?>/components/com_sportsmanagement/assets/images/fussballineuropa.png"
-         width="<?PHP echo $logo_width; ?>" height="auto"></a>
-	<?php echo Text::_("COM_SPORTSMANAGEMENT_COPYRIGHT"); ?> : &copy;
-    <a href="https://www.fussballineuropa.de" target="_blank">Fussball in Europa</a>
-    <br/>
-	<?php
-	if ($show_facebook_link == 3)
-	{
-		?>
-        <img src="<?php echo Uri::root(true); ?>/components/com_sportsmanagement/assets/images/facebook.png"
-             width="<?PHP echo $logo_width; ?>" height="auto"></a>
-        <a href="https://www.facebook.com/joomlasportsmanagement/" target="_blank">JSM auf Facebook</a>
-        <br/>
-		<?php
-	}
-	?>
-	<?php echo Text::_("COM_SPORTSMANAGEMENT_VERSION"); ?> :
-	<?php
-	// Echo HTMLHelper::link('index.php?option='.$option.'&amp;view=about',sprintf('Version %1$s (diddipoeler)',sportsmanagementHelper::getVersion()));
-	echo sprintf('%1$s (diddipoeler)', sportsmanagementHelper::getVersion());
-	?>
-    <br/>
-
-	<?php
-	if(isset($this->jsmseitenaufbau))
-	{
-	?>
-		<div class="center">
-		<?php echo Text::sprintf('COM_SPORTSMANAGEMENT_FOOTER_TIME', $this->jsmseitenaufbau); ?>
-		</div>
-	<?php
-	}
-	?>
-  
-<?php
-$link_onlinehelp = $cfg_help_server . 'SM-Frontend:' . $view ;                                                
-$cmd = "Joomla.popupWindow('$link_onlinehelp', '" . Text::_('COM_SPORTSMANAGEMENT_HELP_LINK', true) . "',". $modal_popup_width." ,". $modal_popup_height.", 1)";
-?>
-<div class="center">
-<button onclick="<?php echo $cmd; ?>">
-<?php
-echo HTMLHelper::_(
-'image', 'media/com_sportsmanagement/jl_images/help.png',
-Text::_('COM_SPORTSMANAGEMENT_HELP_LINK'),
-);
-echo ' '.Text::_('COM_SPORTSMANAGEMENT_HELP_LINK');
-?>                      
-</button>
-</div>  
-  
-  
-</div>    
-  
-</div>    
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+        <?php if ($helpLink !== '') : ?>
+            <div class="center">
+                <a
+                    class="btn btn-secondary"
+                    href="<?php echo htmlspecialchars($helpLink, ENT_QUOTES, 'UTF-8'); ?>"
+                    target="_blank"
+                    rel="noopener"
+                >
+                    <?php
+                    echo HTMLHelper::image(
+                        'media/com_sportsmanagement/jl_images/help.png',
+                        Text::_('COM_SPORTSMANAGEMENT_HELP_LINK')
+                    );
+                    echo ' ' . Text::_('COM_SPORTSMANAGEMENT_HELP_LINK');
+                    ?>
+                </a>
+            </div>
+        <?php endif; ?>
+    </div>
+</div>
