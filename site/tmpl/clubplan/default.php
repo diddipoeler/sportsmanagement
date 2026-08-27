bfca6ef45f121fc74d69216c21eea2fbfd0964ba
diddipoeler <diddipoeler@gmx.de>
2026-08-28 00:07:05 +0200

    Remove legacy helpers from club plan main template

--- site/tmpl/clubplan/default.php
+++ site/tmpl/clubplan/default.php
@@ -1,134 +1,71 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage clubplan
- * @file       default.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@arcor.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
+/** SportsManagement club plan template for Joomla 5/6. */
 defined('_JEXEC') or die('Restricted access');
+
 use Joomla\CMS\Language\Text;
-$templatesToLoad = array('globalviews');
-sportsmanagementHelper::addTemplatePaths($templatesToLoad, $this);
 ?>
-<div class="<?php echo $this->divclasscontainer; ?>" id="clubplan">
-	<?php
-	echo $this->loadTemplate('projectheading');
+<div class="<?php echo $this->escape($this->divclasscontainer); ?>" id="clubplan">
+    <?php echo $this->loadTemplate('projectheading'); ?>
 
-	if (($this->config['show_sectionheader']) == 1 && $this->club)
-	{
-		echo $this->loadTemplate('sectionheader');
-	}
+    <?php if (!empty($this->config['show_sectionheader']) && $this->club) : ?>
+        <div class="<?php echo $this->escape($this->divclassrow); ?>" id="sectionheader">
+            <p><strong><?php echo $this->escape($this->headertitle); ?></strong></p>
+        </div>
+    <?php endif; ?>
 
-	echo $this->loadTemplate('datenav');
+    <?php echo $this->loadTemplate('datenav'); ?>
 
-	switch ($this->config['type_matches'])
-	{
-		case 0 : /** All matches */
-			if (!empty($this->allmatches))
-			{
-				$tm = count($this->allmatches);
-				echo "<h4>" . $tm . " " . Text::_('COM_SPORTSMANAGEMENT_CLUBPLAN_MATCHES') . "</h4>";
-				$this->matches = $this->allmatches;
-				echo $this->loadTemplate('matches');// Or use matches_sbd (sort by date)
-			}
-			else
-			{
-				echo "<h4>" . Text::_('COM_SPORTSMANAGEMENT_CLUBPLAN_NO_MATCHES') . "</h4><br/>";
-			}
-			break;
+    <?php
+    $type = (int) ($this->config['type_matches'] ?? 4);
 
-		case 1 : /** Home matches */
-			if (!empty($this->homematches))
-			{
-				$tm = count($this->homematches);
-				echo "<h4>" . $tm . " " . Text::_('COM_SPORTSMANAGEMENT_CLUBPLAN_MATCHES') . "</h4>";
-				$this->matches = $this->homematches;
-				echo $this->loadTemplate('matches');// Or use matches_sbd (sort by date)
-			}
-			else
-			{
-				echo "<h4>" . Text::_('COM_SPORTSMANAGEMENT_CLUBPLAN_NO_HOME_MATCHES') . "</h4><br/>";
-			}
-			break;
+    $renderMatches = function (array $matches, string $emptyText, string $titleText = ''): void {
+        if ($matches) {
+            $count = count($matches);
+            $label = $titleText !== '' ? $titleText : 'COM_SPORTSMANAGEMENT_CLUBPLAN_MATCHES';
+            echo '<h4>' . $count . ' ' . Text::_($label) . '</h4>';
+            $this->matches = $matches;
+            echo $this->loadTemplate('matches');
+            return;
+        }
 
-		case 2 : /** Away matches */
-			if (!empty($this->awaymatches))
-			{
-				$tm = count($this->awaymatches);
-				echo "<h4>" . $tm . " " . Text::_('COM_SPORTSMANAGEMENT_CLUBPLAN_MATCHES') . "</h4>";
-				$this->matches = $this->awaymatches;
-				echo $this->loadTemplate('matches');// Or use matches_sbd (sort by date)
-			}
-			else
-			{
-				echo "<h4>" . Text::_('COM_SPORTSMANAGEMENT_CLUBPLAN_NO_AWAY_MATCHES') . "</h4><br/>";
-			}
-			break;
+        echo '<h4>' . Text::_($emptyText) . '</h4><br>';
+    };
 
-		case 4 : /** Matches sorted by date */
-			if (!empty($this->allmatches))
-			{
-				$tm = count($this->allmatches);
-				echo "<h4>" . $tm . " " . Text::_('COM_SPORTSMANAGEMENT_CLUBPLAN_MATCHES') . "</h4>";
-				$this->matches = $this->allmatches;
-				echo $this->loadTemplate('matches_sorted_by_date');// Or use matches_sbd (sort by date)
-			}
-			else
-			{
-				echo "<h4>" . Text::_('COM_SPORTSMANAGEMENT_CLUBPLAN_NO_MATCHES') . "</h4><br/>";
-			}
-			break;
+    switch ($type) {
+        case 0:
+            $renderMatches($this->allmatches, 'COM_SPORTSMANAGEMENT_CLUBPLAN_NO_MATCHES');
+            break;
 
-		default : /** Home+Away matches */
-			if (!empty($this->homematches))
-			{
-				$tm = count($this->homematches);
+        case 1:
+            $renderMatches($this->homematches, 'COM_SPORTSMANAGEMENT_CLUBPLAN_NO_HOME_MATCHES');
+            break;
 
-				if (count($this->awaymatches) == 0)
-				{
-					echo "<h4>" . $tm . " " . Text::_('COM_SPORTSMANAGEMENT_CLUBPLAN_MATCHES') . "</h4>";
-				}
-				else
-				{
-					echo "<h4>" . $tm . " " . Text::_('COM_SPORTSMANAGEMENT_CLUBPLAN_HOME_MATCHES') . "</h4>";
-				}
+        case 2:
+            $renderMatches($this->awaymatches, 'COM_SPORTSMANAGEMENT_CLUBPLAN_NO_AWAY_MATCHES');
+            break;
 
-				$this->matches = $this->homematches;
-				echo $this->loadTemplate('matches');// Or use matches_sbd (sort by date)
-			}
-			else
-			{
-				echo "<h4>" . Text::_('COM_SPORTSMANAGEMENT_CLUBPLAN_NO_HOME_MATCHES') . "</h4><br/>";
-			}
+        case 4:
+            if ($this->allmatches) {
+                echo '<h4>' . count($this->allmatches) . ' ' . Text::_('COM_SPORTSMANAGEMENT_CLUBPLAN_MATCHES') . '</h4>';
+                $this->matches = $this->allmatches;
+                echo $this->loadTemplate('matches_sorted_by_date');
+            } else {
+                echo '<h4>' . Text::_('COM_SPORTSMANAGEMENT_CLUBPLAN_NO_MATCHES') . '</h4><br>';
+            }
+            break;
 
+        default:
+            $homeLabel = $this->awaymatches
+                ? 'COM_SPORTSMANAGEMENT_CLUBPLAN_HOME_MATCHES'
+                : 'COM_SPORTSMANAGEMENT_CLUBPLAN_MATCHES';
+            $awayLabel = $this->homematches
+                ? 'COM_SPORTSMANAGEMENT_CLUBPLAN_AWAY_MATCHES'
+                : 'COM_SPORTSMANAGEMENT_CLUBPLAN_MATCHES';
+            $renderMatches($this->homematches, 'COM_SPORTSMANAGEMENT_CLUBPLAN_NO_HOME_MATCHES', $homeLabel);
+            $renderMatches($this->awaymatches, 'COM_SPORTSMANAGEMENT_CLUBPLAN_NO_AWAY_MATCHES', $awayLabel);
+            break;
+    }
+    ?>
 
-			if (!empty($this->awaymatches))
-			{
-				$tm = count($this->awaymatches);
-
-				if (count($this->homematches) == 0)
-				{
-					echo "<h4>" . $tm . " " . Text::_('COM_SPORTSMANAGEMENT_CLUBPLAN_MATCHES') . "</h4>";
-				}
-				else
-				{
-					echo "<h4>" . $tm . " " . Text::_('COM_SPORTSMANAGEMENT_CLUBPLAN_AWAY_MATCHES') . "</h4>";
-				}
-
-				$this->matches = $this->awaymatches;
-				echo $this->loadTemplate('matches');// Or use matches_sbd (sort by date)
-			}
-			else
-			{
-				echo "<h4>" . Text::_('COM_SPORTSMANAGEMENT_CLUBPLAN_NO_AWAY_MATCHES') . "</h4><br/>";
-			}
-			break;
-	}
-
-	echo $this->loadTemplate('jsminfo');
-	?>
+    <?php echo $this->loadTemplate('jsminfo'); ?>
 </div>
