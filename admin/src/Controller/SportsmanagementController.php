540538f3b79010900728434bf50edee3128846f5
diddipoeler <diddipoeler@gmx.de>
2026-08-28 16:37:04 +0200

    Add native Sportsmanagement form controller

---
+++ admin/src/Controller/SportsmanagementController.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @package     SportsManagement
+ * @subpackage  com_sportsmanagement
+ *
+ * Joomla 5/6 migration.
+ */
+
+namespace Diddipoeler\Component\SportsManagement\Administrator\Controller;
+
+\defined('_JEXEC') or die;
+
+/** Controller for the SportsManagement sample record form. */
+final class SportsmanagementController extends SportsManagementFormController
+{
+}
