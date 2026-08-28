899130d9d095eaa99b80d32a0f3ce065f9c172cc
diddipoeler <diddipoeler@gmx.de>
2026-08-28 17:17:14 +0200

    Use native debug rendering in DFBnet import update

--- admin/tmpl/jlextdfbnetplayerimport/default_update.php
+++ admin/tmpl/jlextdfbnetplayerimport/default_update.php
@@ -1,85 +1,55 @@
 <?php
-/**
- *
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage jlextdfbnetplayerimport
- * @file       default_update.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-
+/** SportsManagement DFB.net import update result. */
 defined('_JEXEC') or die('Restricted access');
 
-use Joomla\CMS\Language\Text;
-use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Component\ComponentHelper;
+use Joomla\CMS\Language\Text;
 
+$showDebug = (bool) ComponentHelper::getParams('com_sportsmanagement')->get('show_debug_info_backend', 0);
 ?>
+<div id="editcell">
+    <a name="page_top"></a>
+    <table class="adminlist">
+        <thead>
+        <tr>
+            <th><?php echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_DFBNET_UPDATE_MATCH_DATA_TITLE'); ?></th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr><td>&nbsp;</td></tr>
+        </tbody>
+    </table>
 
-    <div id='editcell'>
-        <a name='page_top'></a>
-        <table class='adminlist'>
-            <thead>
-            <tr>
-                <th><?php echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_DFBNET_UPDATE_MATCH_DATA_TITLE'); ?></th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr>
-                <td><?php echo '&nbsp;'; ?></td>
-            </tr>
-            </tbody>
-        </table>
-		<?php
-		if (is_array($this->importData))
-		{
-			foreach ($this->importData as $key => $value)
-			{
-				?>
-                <fieldset>
-                    <legend><?php echo Text::_($key); ?></legend>
-                    <table class='adminlist'>
-                        <tr>
-                            <td><?php echo $value; ?></td>
-                        </tr>
-                    </table>
-                </fieldset>
-				<?php
-			}
-		}
-
-
-		if (ComponentHelper::getParams('com_sportsmanagement')->get('show_debug_info_backend', 0))
-		{
-			?>
+    <?php if (is_array($this->importData)) : ?>
+        <?php foreach ($this->importData as $key => $value) : ?>
             <fieldset>
+                <legend><?php echo Text::_((string) $key); ?></legend>
+                <table class="adminlist">
+                    <tr><td><?php echo $value; ?></td></tr>
+                </table>
+            </fieldset>
+        <?php endforeach; ?>
+    <?php endif; ?>
+
+    <?php if ($showDebug) : ?>
+        <fieldset>
             <legend><?php echo Text::_('Post data from importform was:'); ?></legend>
-            <table class='adminlist'>
+            <table class="adminlist">
                 <tr>
-                    <td><?php echo TVarDumper::dump($this->postData, 10, true); ?></td>
+                    <td><pre><?php echo htmlspecialchars(print_r($this->postData, true), ENT_QUOTES, 'UTF-8'); ?></pre></td>
                 </tr>
             </table>
-            </fieldset><?php
-		}
-		?>
+        </fieldset>
+    <?php endif; ?>
+</div>
+<p style="text-align:right;"><a href="#page_top"><?php echo Text::_('top'); ?></a></p>
+<?php if ($showDebug) : ?>
+    <div class="text-center">
+        <hr>
+        <?php echo Text::sprintf('Memory Limit is %1$s', ini_get('memory_limit')); ?><br />
+        <?php echo Text::sprintf('Memory Peak Usage was %1$s Bytes', number_format(memory_get_peak_usage(true), 0, '', '.')); ?><br />
+        <?php echo Text::sprintf('Time Limit is %1$s seconds', ini_get('max_execution_time')); ?><br />
+        <?php echo Text::sprintf('This page was created in %1$s seconds', microtime(true) - $this->starttime); ?>
+        <hr>
     </div>
-    <p style='text-align:right;'><a href='#page_top'><?php echo Text::_('top'); ?></a></p>
-<?php
-if (ComponentHelper::getParams('com_sportsmanagement')->get('show_debug_info_backend', 0))
-{
-	echo '<center><hr>';
-	echo Text::sprintf('Memory Limit is %1$s', ini_get('memory_limit')) . '<br />';
-	echo Text::sprintf('Memory Peak Usage was %1$s Bytes', number_format(memory_get_peak_usage(true), 0, '', '.')) . '<br />';
-	echo Text::sprintf('Time Limit is %1$s seconds', ini_get('max_execution_time')) . '<br />';
-	$mtime     = microtime();
-	$mtime     = explode(" ", $mtime);
-	$mtime     = $mtime[1] + $mtime[0];
-	$endtime   = $mtime;
-	$totaltime = ($endtime - $this->starttime);
-	echo Text::sprintf('This page was created in %1$s seconds', $totaltime);
-	echo '<hr></center>';
-}
+<?php endif; ?>
