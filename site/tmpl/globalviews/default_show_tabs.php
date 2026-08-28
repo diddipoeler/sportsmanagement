f086c733e5046b71c8b79ddb51dc8637f517e27b
diddipoeler <diddipoeler@gmx.de>
2026-08-28 09:28:42 +0200

    Use Bootstrap 5 markup for shared site tabs

--- site/tmpl/globalviews/default_show_tabs.php
+++ site/tmpl/globalviews/default_show_tabs.php
@@ -1,10 +1,9 @@
 <?php
 /**
- * Shared Joomla 5/6 tab layout.
+ * Shared Joomla 5/6 Bootstrap 5 tab layout.
  */
 \defined('_JEXEC') or die;
 
-use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
 
 $output = (array) ($this->output ?? []);
@@ -32,16 +31,37 @@ foreach ($output as $key => $templateData) {
 ?>
 <div class="<?php echo htmlspecialchars((string) $this->divclassrow, ENT_QUOTES, 'UTF-8'); ?>" id="show_tabs">
     <?php if ($tabs) : ?>
-        <?php echo HTMLHelper::_('bootstrap.startTabSet', 'jsm-view-tabs', ['active' => $tabs[0]['id']]); ?>
-        <?php foreach ($tabs as $tab) : ?>
-            <?php echo HTMLHelper::_('bootstrap.addTab', 'jsm-view-tabs', $tab['id'], Text::_($tab['text'])); ?>
-            <div class="<?php echo htmlspecialchars((string) $this->divclasscontainer, ENT_QUOTES, 'UTF-8'); ?>">
-                <div class="<?php echo htmlspecialchars((string) $this->divclassrow, ENT_QUOTES, 'UTF-8'); ?>">
-                    <?php echo $this->loadTemplate($tab['template']); ?>
+        <ul class="nav nav-tabs" id="jsm-view-tabs" role="tablist">
+            <?php foreach ($tabs as $index => $tab) : ?>
+                <li class="nav-item" role="presentation">
+                    <button class="nav-link<?php echo $index === 0 ? ' active' : ''; ?>"
+                            id="<?php echo htmlspecialchars($tab['id'], ENT_QUOTES, 'UTF-8'); ?>-tab"
+                            data-bs-toggle="tab"
+                            data-bs-target="#<?php echo htmlspecialchars($tab['id'], ENT_QUOTES, 'UTF-8'); ?>"
+                            type="button"
+                            role="tab"
+                            aria-controls="<?php echo htmlspecialchars($tab['id'], ENT_QUOTES, 'UTF-8'); ?>"
+                            aria-selected="<?php echo $index === 0 ? 'true' : 'false'; ?>">
+                        <?php echo htmlspecialchars(Text::_($tab['text']), ENT_QUOTES, 'UTF-8'); ?>
+                    </button>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+
+        <div class="tab-content" id="jsm-view-tab-content">
+            <?php foreach ($tabs as $index => $tab) : ?>
+                <div class="tab-pane fade<?php echo $index === 0 ? ' show active' : ''; ?>"
+                     id="<?php echo htmlspecialchars($tab['id'], ENT_QUOTES, 'UTF-8'); ?>"
+                     role="tabpanel"
+                     aria-labelledby="<?php echo htmlspecialchars($tab['id'], ENT_QUOTES, 'UTF-8'); ?>-tab"
+                     tabindex="0">
+                    <div class="<?php echo htmlspecialchars((string) $this->divclasscontainer, ENT_QUOTES, 'UTF-8'); ?>">
+                        <div class="<?php echo htmlspecialchars((string) $this->divclassrow, ENT_QUOTES, 'UTF-8'); ?>">
+                            <?php echo $this->loadTemplate($tab['template']); ?>
+                        </div>
+                    </div>
                 </div>
-            </div>
-            <?php echo HTMLHelper::_('bootstrap.endTab'); ?>
-        <?php endforeach; ?>
-        <?php echo HTMLHelper::_('bootstrap.endTabSet'); ?>
+            <?php endforeach; ?>
+        </div>
     <?php endif; ?>
 </div>
