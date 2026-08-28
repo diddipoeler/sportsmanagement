1232e33dbb8280e8c4fcafec2b66d05d080fffae
diddipoeler <diddipoeler@gmx.de>
2026-08-28 18:16:48 +0200

    Migrate XML export admin view to native Joomla MVC

--- admin/src/View/Jlxmlexports/HtmlView.php
+++ admin/src/View/Jlxmlexports/HtmlView.php
@@ -3,11 +3,47 @@ namespace Diddipoeler\Component\SportsManagement\Administrator\View\Jlxmlexports
 
 \defined('_JEXEC') or die;
 
-use Diddipoeler\Component\SportsManagement\Administrator\Legacy\LegacyBootstrap;
+use Joomla\CMS\Factory;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\MVC\View\HtmlView as BaseHtmlView;
+use Joomla\CMS\Toolbar\ToolbarHelper;
+use Joomla\CMS\Uri\Uri;
 
-LegacyBootstrap::bootForView('jlxmlexports');
-require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/views/jlxmlexports/view.html.php';
+/** Native Joomla 5/6 administrator XML export view. */
+final class HtmlView extends BaseHtmlView
+{
+    public string $exportSystem = '';
+    public string $request_url = '';
+    public string $table_data_div = '';
 
-if (!class_exists(__NAMESPACE__ . '\\HtmlView', false)) {
-    class_alias('sportsmanagementViewjlxmlexports', __NAMESPACE__ . '\\HtmlView');
+    public function display($tpl = null)
+    {
+        $layout = preg_replace('/_(?:3|4|5)$/', '', (string) $this->getLayout()) ?: 'default';
+        $this->setLayout($layout);
+
+        $app = Factory::getApplication();
+        $this->exportSystem = (string) $app->get('sitename', '');
+        $this->request_url = Uri::getInstance()->toString();
+
+        $this->addToolbar();
+        parent::display($tpl);
+    }
+
+    protected function addToolbar(): void
+    {
+        ToolbarHelper::title(Text::_('COM_SPORTSMANAGEMENT_ADMIN_XML_EXPORT_TITLE'), 'download');
+        ToolbarHelper::custom(
+            'jlxmlexports.export',
+            'upload',
+            'upload',
+            Text::_('JTOOLBAR_EXPORT'),
+            false
+        );
+        ToolbarHelper::divider();
+        ToolbarHelper::back('JPREV', 'index.php?option=com_sportsmanagement&view=projects');
+
+        if (Factory::getApplication()->getIdentity()->authorise('core.admin', 'com_sportsmanagement')) {
+            ToolbarHelper::preferences('com_sportsmanagement');
+        }
+    }
 }
