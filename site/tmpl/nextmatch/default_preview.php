e980edab2e19c3ec77b420e8fb06870a6844e332
diddipoeler <diddipoeler@gmx.de>
2026-08-28 08:56:27 +0200

    Modernize next match preview for Joomla 5/6

--- site/tmpl/nextmatch/default_preview.php
+++ site/tmpl/nextmatch/default_preview.php
@@ -1,60 +1,40 @@
 <?php
 /**
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage nextmatch
- * @file       default_preview.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * Native Joomla 5/6 next-match preview.
  */
-defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\Language\Text;
+\defined('_JEXEC') or die;
+
 use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Language\Text;
 
+$preview = trim((string) ($this->match->preview ?? ''));
 ?>
-<!-- START of match preview -->
-<div class="<?php echo $this->divclassrow; ?> table-responsive" id="nextmatch-preview">
-    <div class="panel-group" id="accordionnextmatch">
-        <div class="panel panel-default">
-            <div class="panel-heading">
-                <h4 class="panel-title">
-                    <a data-toggle="collapse" data-parent="#accordionnextmatch"
-                       href="#nextpreview"><?php echo Text::_('COM_SPORTSMANAGEMENT_NEXTMATCH_PREVIEW'); ?></a>
-                </h4>
-            </div>
-            <div id="nextpreview" class="panel-collapse collapse">
-                <div class="panel-body">
-					<?php
-
-					if (!empty($this->match->preview))
-					{
-						?>
-                        <h2><?php echo Text::_('COM_SPORTSMANAGEMENT_NEXTMATCH_PREVIEW'); ?></h2>
-                        <table class="table">
-                            <tr>
-                                <td><?php
-									$preview = $this->match->preview;
-									$preview = HTMLHelper::_('content.prepare', $preview);
-
-									if ($commentsDisabled)
-									{
-										$preview = preg_replace('#{jcomments\s+(off|lock)}#is', '', $preview);
-									}
-
-									echo $preview;
-									?>
-                                </td>
-                            </tr>
-                        </table>
-                        <!-- END of match preview -->
-
-						<?php
-					}
-					?>
+<?php if ($preview !== '') : ?>
+    <div class="<?php echo htmlspecialchars((string) $this->divclassrow, ENT_QUOTES, 'UTF-8'); ?> table-responsive" id="nextmatch-preview">
+        <div class="accordion" id="accordion-nextmatch-preview">
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="heading-nextmatch-preview">
+                    <button class="accordion-button collapsed" type="button"
+                            data-bs-toggle="collapse" data-bs-target="#nextmatch-preview-content"
+                            aria-expanded="false" aria-controls="nextmatch-preview-content">
+                        <?php echo Text::_('COM_SPORTSMANAGEMENT_NEXTMATCH_PREVIEW'); ?>
+                    </button>
+                </h2>
+                <div id="nextmatch-preview-content" class="accordion-collapse collapse"
+                     aria-labelledby="heading-nextmatch-preview" data-bs-parent="#accordion-nextmatch-preview">
+                    <div class="accordion-body">
+                        <?php
+                        $preview = HTMLHelper::_('content.prepare', $preview);
+                        // Do not leak JComments control tags when the comments
+                        // extension is not present on a Joomla 5/6 installation.
+                        if (!class_exists('JComments')) {
+                            $preview = preg_replace('#{jcomments\s+(off|lock)}#is', '', $preview) ?? $preview;
+                        }
+                        echo $preview;
+                        ?>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
+<?php endif; ?>
