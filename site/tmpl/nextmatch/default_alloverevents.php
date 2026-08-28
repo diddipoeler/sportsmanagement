1ad0ba50075171828830db577eb846704db63311
diddipoeler <diddipoeler@gmx.de>
2026-08-28 09:26:53 +0200

    Migrate next match event totals layout

--- site/tmpl/nextmatch/default_alloverevents.php
+++ site/tmpl/nextmatch/default_alloverevents.php
@@ -1,104 +1,91 @@
 <?php
 /**
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage nextmatch
- * @file       default_alloverevents.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * Native Joomla 5/6 project event totals for next-match.
  */
-defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\Language\Text;
+\defined('_JEXEC') or die;
+
+use Diddipoeler\Component\SportsManagement\Site\Helper\ModalImageHelper;
+use Diddipoeler\Component\SportsManagement\Site\Helper\PersonImageHelper;
+use Diddipoeler\Component\SportsManagement\Site\Helper\PersonNameFormatter;
 use Joomla\CMS\HTML\HTMLHelper;
-use Joomla\CMS\Uri\Uri;
-use Joomla\CMS\Factory;
+use Joomla\CMS\Language\Text;
 
-//echo 'alloverevents <pre>'.print_r($this->alloverevents,true).'</pre>';
-?>
-<div class="<?php echo $this->divclassrow; ?> table-responsive" id="nextmatchalloverevents">
-<?php
-$this->notes = array();
-$this->notes[] = Text::_('COM_SPORTSMANAGEMENT_NEXTMATCH_ALLOVEREVENTS') . " " . $this->club->name;
-echo $this->loadTemplate('jsm_notes'); 
-?>
+$escape = static fn (mixed $value): string => htmlspecialchars((string) $value, ENT_QUOTES, 'UTF-8');
+$events = is_array($this->overallevents ?? null) ? $this->overallevents : [];
+$players = is_array($this->alloverevents ?? null) ? $this->alloverevents : [];
+$tableClass = (string) ($this->config['hystory_table_class'] ?? 'table');
+$modalMode = (int) ($this->overallconfig['use_jquery_modal'] ?? 0);
 
-<table class="<?php echo $this->config['hystory_table_class']; ?>">
-<?php          
-foreach ( $this->alloverevents as $alloverevents )
-{
+$this->notes = [Text::_('COM_SPORTSMANAGEMENT_NEXTMATCH_ALLOVEREVENTS')];
 ?>
-<tr>  
-<td>
-<?php
-echo $alloverevents->team_name;
-?>
-</td>
- 
-<td>
-<?php
-echo sportsmanagementHelper::formatName(null, $alloverevents->firstname1, $alloverevents->nickname1, $alloverevents->lastname1, $this->config["name_format"]);
-?>
-</td>
-  
-<td>
-<?php
-echo sportsmanagementHelperHtml::getBootstrapModalImage(
-											'nextmatchalloverevents' . $alloverevents->playerid ,
-											$alloverevents->tppicture1,
-											$alloverevents->lastname1,
-											'20',
-											'',
-											$this->modalwidth,
-											$this->modalheight,
-											$this->overallconfig['use_jquery_modal']
-										);
-?>
-</td>
-</tr>
-<tr> 
-<td>
-<table class="<?php echo $this->config['hystory_table_class']; ?>">
-<thead>
-<tr>
-<?php
-foreach ( $this->overallevents as $overallevents )
-{
-$width    = 20;
-$height   = 20;
-$type     = 4;
-$imgTitle = Text::_($overallevents->name);
-$icon     = sportsmanagementHelper::getPictureThumb($overallevents->icon, $imgTitle, $width, $height, $type);
-?>
-<td>
-<?php
-echo $icon;
-?>
-</td>                
-<?php
-}
-?>
-</tr>
-</thead>
-<?php
-foreach ( $alloverevents->events as $key => $value )
-{
-?>
-<td>
-<?php
-echo $value->event_sum;
-?>
-</td>                
-<?php    
-}
+<div class="<?php echo $escape($this->divclassrow); ?> table-responsive" id="nextmatch-alloverevents">
+    <?php echo $this->loadTemplate('jsm_notes'); ?>
 
-?>
-</table>
-</td>
-</tr>  
-<?php  
-}          
-?>          
-</table>          
+    <?php if ($players) : ?>
+        <table class="<?php echo $escape($tableClass); ?>">
+            <thead>
+            <tr>
+                <th><?php echo Text::_('COM_SPORTSMANAGEMENT_EVENTSRANKING_TEAM'); ?></th>
+                <th><?php echo Text::_('COM_SPORTSMANAGEMENT_EVENTSRANKING_PLAYER_NAME'); ?></th>
+                <th></th>
+                <?php foreach ($events as $event) : ?>
+                    <?php
+                    $title = Text::_((string) ($event->name ?? ''));
+                    $icon = trim((string) ($event->icon ?? ''));
+                    if ($icon !== '' && !str_contains($icon, '/')) {
+                        $icon = 'media/com_sportsmanagement/events/' . $icon;
+                    }
+                    ?>
+                    <th title="<?php echo $escape($title); ?>">
+                        <?php if ($icon !== '') : ?>
+                            <?php echo HTMLHelper::image($icon, $title, ['title' => $title, 'height' => 20]); ?>
+                        <?php else : ?>
+                            <?php echo $escape($title); ?>
+                        <?php endif; ?>
+                    </th>
+                <?php endforeach; ?>
+            </tr>
+            </thead>
+            <tbody>
+            <?php foreach ($players as $player) : ?>
+                <?php
+                $name = PersonNameFormatter::format(
+                    null,
+                    (string) ($player->firstname1 ?? ''),
+                    (string) ($player->nickname1 ?? ''),
+                    (string) ($player->lastname1 ?? ''),
+                    $this->config['name_format'] ?? 0
+                );
+                $picture = PersonImageHelper::url(
+                    PersonImageHelper::resolve((string) ($player->tppicture1 ?? ''))
+                );
+                ?>
+                <tr>
+                    <td><?php echo $escape($player->team_name ?? ''); ?></td>
+                    <td><?php echo $name; ?></td>
+                    <td>
+                        <?php if ($picture !== '') : ?>
+                            <?php echo ModalImageHelper::render(
+                                'nextmatch-alloverevents-player-' . (int) ($player->playerid ?? 0),
+                                $picture,
+                                strip_tags($name),
+                                20,
+                                '',
+                                $this->modalwidth,
+                                $this->modalheight,
+                                $modalMode
+                            ); ?>
+                        <?php endif; ?>
+                    </td>
+                    <?php foreach ($events as $event) : ?>
+                        <?php $eventId = (int) ($event->id ?? 0); ?>
+                        <td><?php echo $escape($player->events[$eventId]->event_sum ?? 0); ?></td>
+                    <?php endforeach; ?>
+                </tr>
+            <?php endforeach; ?>
+            </tbody>
+        </table>
+    <?php else : ?>
+        <p class="text-muted mb-0"><?php echo Text::_('JGLOBAL_NO_MATCHING_RESULTS'); ?></p>
+    <?php endif; ?>
 </div>
