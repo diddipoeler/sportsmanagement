7859ae15def302d8257629b2196613061b5d8217
diddipoeler <diddipoeler@gmx.de>
2026-08-28 09:26:22 +0200

    Use native next match comments adapter

--- site/tmpl/nextmatch/default_comments.php
+++ site/tmpl/nextmatch/default_comments.php
@@ -1,39 +1,27 @@
 <?php
 /**
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage nextmatch
- * @file       default_preview.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * Joomla 5/6 match comments integration for the next-match view.
  */
-defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\Language\Text;
-use Joomla\CMS\HTML\HTMLHelper;
+defined('_JEXEC') or die;
 
-/**
- * workaround to support {jcomments (off|lock)} in match preview
- * no comments are shown if {jcomments (off|lock)} is found in the match preview
- */
-$commentsDisabled = 0;
+use Diddipoeler\Component\SportsManagement\Site\Helper\MatchCommentsHelper;
 
-if (!empty($this->match->preview) && preg_match('/{jcomments\s+(off|lock)}/is', $this->match->preview))
-{
-	$commentsDisabled = 1;
+if (!$this->match || !isset($this->teams[0], $this->teams[1])) {
+    return;
 }
 
-// Comments integration
-if (!$commentsDisabled)
-{
-	?>
-    <!-- START of match comments -->
-    <div class="<?php echo $this->divclassrow; ?> table-responsive" id="nextmatch-comments">
-		<?php
-		$commmentsInstance = sportsmanagementModelComments::CreateInstance($this->config);
-		echo $commmentsInstance->showMatchComments($this->match, $this->teams[0], $this->teams[1], $this->config, $this->project);
-		?>
-    </div>
-	<?php
+$comments = MatchCommentsHelper::render(
+    $this->match,
+    $this->teams[0],
+    $this->teams[1],
+    $this->config,
+    $this->project
+);
+
+if ($comments === '') {
+    return;
 }
+?>
+<div class="<?php echo $this->divclassrow; ?> table-responsive" id="nextmatch-comments">
+    <?php echo $comments; ?>
+</div>
