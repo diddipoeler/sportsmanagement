8d5094e9443539e2ba3ae6309c967a9b65b5f49f
diddipoeler <diddipoeler@gmx.de>
2026-08-28 16:06:22 +0200

    Decouple DBB import template from legacy helper

--- admin/tmpl/jlextdbbimport/default.php
+++ admin/tmpl/jlextdbbimport/default.php
@@ -14,9 +14,7 @@ use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\Factory;
 
-$option          = Factory::getApplication()->input->getCmd('option');
-$templatesToLoad = array('footer', 'listheader');
-sportsmanagementHelper::addTemplatePaths($templatesToLoad, $this);
+$option = Factory::getApplication()->getInput()->getCmd('option');
 
 $url = 'administrator' . DIRECTORY_SEPARATOR . 'components' . DIRECTORY_SEPARATOR . $option . DIRECTORY_SEPARATOR . 'assets' . DIRECTORY_SEPARATOR . 'icons' . DIRECTORY_SEPARATOR . 'dbb-gross.png';
 $alt = 'DFBNet';
@@ -31,52 +29,52 @@ $attribs['align']  = 'left';
             <thead>
             <tr>
                 <th>
-					<?php echo HTMLHelper::_('image', $url, $alt, $attribs);; ?>
-					<?php echo Text::sprintf('COM_SPORTSMANAGEMENT_ADMIN_DBB_IMPORT_TABLE_TITLE_1', $this->config->get('upload_maxsize')); ?>
+                    <?php echo HTMLHelper::_('image', $url, $alt, $attribs);; ?>
+                    <?php echo Text::sprintf('COM_SPORTSMANAGEMENT_ADMIN_DBB_IMPORT_TABLE_TITLE_1', $this->config->get('upload_maxsize')); ?>
                 </th>
             </tr>
             </thead>
             <tfoot>
             <tr>
                 <td>
-					<?php
-					echo '<br />';
-					echo '<b>' . Text::_('COM_SPORTSMANAGEMENT_ADMIN_DBB_IMPORT_EXTENTION_INFO') . '</b><br />';
-					echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_DBB_IMPORT_HINT1') . '<br />';
-					echo Text::sprintf('COM_SPORTSMANAGEMENT_ADMIN_DBB_IMPORT_HINT2', $this->revisionDate);
-					?>
+                    <?php
+                    echo '<br />';
+                    echo '<b>' . Text::_('COM_SPORTSMANAGEMENT_ADMIN_DBB_IMPORT_EXTENTION_INFO') . '</b><br />';
+                    echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_DBB_IMPORT_HINT1') . '<br />';
+                    echo Text::sprintf('COM_SPORTSMANAGEMENT_ADMIN_DBB_IMPORT_HINT2', $this->revisionDate);
+                    ?>
                 </td>
             </tr>
             </tfoot>
             <tbody>
-			<?php
-			// TODO: Check update functionality in later version of that extension. For now, disabled
-			if (0)
-			{
-				?>
+            <?php
+            // TODO: Check update functionality in later version of that extension. For now, disabled
+            if (0)
+            {
+                ?>
                 <tr>
                     <td>
                         <fieldset>
                             <legend>
-								<?php
-								echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_DBB_IMPORT_SELECT_USE_PROJECT');
-								?>
+                                <?php
+                                echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_DBB_IMPORT_SELECT_USE_PROJECT');
+                                ?>
                             </legend>
                             <input class='input_box' type='checkbox' id='dfbimportupdate'
                                    name='dfbimportupdate'/><?php echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_DBB_IMPORT_USE_PROJECT'); ?>
                         </fieldset>
                     </td>
                 </tr>
-				<?php
-			}
-			?>
+                <?php
+            }
+            ?>
             <tr>
                 <td>
                     <fieldset>
                         <legend>
-							<?php
-							echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_DBB_IMPORT_WHICH_FILE');
-							?>
+                            <?php
+                            echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_DBB_IMPORT_WHICH_FILE');
+                            ?>
                         </legend>
                         <input type="radio" name="whichfile"
                                value="playerfile"> <?PHP echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_DBB_IMPORT_PLAYERFILE'); ?>
@@ -90,18 +88,18 @@ $attribs['align']  = 'left';
                 </td>
             </tr>
 
-			<?php
-			// TODO: Disabled, set delimiter hardcoded to tab in the model (because DFBNet uses only that delimiter since 2013)
-			if (0)
-			{
-				?>
+            <?php
+            // TODO: Disabled, set delimiter hardcoded to tab in the model (because DFBNet uses only that delimiter since 2013)
+            if (0)
+            {
+                ?>
                 <tr>
                     <td>
                         <fieldset>
                             <legend>
-								<?php
-								echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_DBB_IMPORT_DELIMITER');
-								?>
+                                <?php
+                                echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_DBB_IMPORT_DELIMITER');
+                                ?>
                             </legend>
 
                             <input type="radio" name="delimiter" value=";"
@@ -116,18 +114,17 @@ $attribs['align']  = 'left';
                         </fieldset>
                     </td>
                 </tr>
-				<?php
-			}
-			?>
-
+                <?php
+            }
+            ?>
 
             <tr>
                 <td>
                     <fieldset>
                         <legend>
-							<?php
-							echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_DBB_IMPORT_FILE');
-							?>
+                            <?php
+                            echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_DBB_IMPORT_FILE');
+                            ?>
                         </legend>
 
                         <input class="input_box" id="import_package" name="import_package" type="file" size="57"/>
@@ -142,7 +139,7 @@ $attribs['align']  = 'left';
         <input type="hidden" name='MAX_FILE_SIZE' value='<?php echo $this->config->get('upload_maxsize'); ?>'/>
         <input type="hidden" name="option" value="com_sportsmanagement"/>
         <input type="hidden" name='task' value='jlextdbbimport.save'/>
-		<?php echo HTMLHelper::_('form.token') . "\n"; ?>
+        <?php echo HTMLHelper::_('form.token') . "\n"; ?>
     </form>
 </div>
 <?PHP
