9b52e5cf1987aeb88c1e7b5ddcb5d5812a4c3b18
diddipoeler <diddipoeler@gmx.de>
2026-08-28 16:06:53 +0200

    Use native database resolver in active season module

--- modules/mod_sportsmanagement_act_season/src/Helper/ActSeasonHelper.php
+++ modules/mod_sportsmanagement_act_season/src/Helper/ActSeasonHelper.php
@@ -3,6 +3,7 @@ namespace Diddipoeler\Module\SportsManagementActSeason\Site\Helper;
 
 \defined('_JEXEC') or die;
 
+use Diddipoeler\Component\SportsManagement\Site\Service\SportsManagementDatabaseResolver;
 use Joomla\CMS\Application\CMSApplicationInterface;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\Uri\Uri;
@@ -155,23 +156,6 @@ final class ActSeasonHelper
 
     private function database(int $selector, DatabaseInterface $fallbackDatabase): DatabaseInterface
     {
-        if (!class_exists('sportsmanagementHelper', false)) {
-            $path = JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/helpers/sportsmanagement.php';
-            if (is_file($path)) {
-                require_once $path;
-            }
-        }
-
-        if (class_exists('sportsmanagementHelper', false)) {
-            try {
-                $db = \sportsmanagementHelper::getDBConnection(true, $selector);
-                if ($db instanceof DatabaseInterface) {
-                    return $db;
-                }
-            } catch (\Throwable) {
-            }
-        }
-
-        return $fallbackDatabase;
+        return SportsManagementDatabaseResolver::resolve($fallbackDatabase, $selector);
     }
 }
