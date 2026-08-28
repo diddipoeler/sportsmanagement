9966d9be3494cc1b6ad4dcb1b5eff9ec7fd5bb8a
diddipoeler <diddipoeler@gmx.de>
2026-08-28 11:21:39 +0200

    Render curve color legend without legacy helper

--- site/tmpl/curve/default_colorlegend.php
+++ site/tmpl/curve/default_colorlegend.php
@@ -1,31 +1,65 @@
 <?php
-/**
- *
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage curve
- * @file       default_colorlegend.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
+/** Native Joomla 5/6 curve color legend. */
+defined('_JEXEC') or die;
 
-defined('_JEXEC') or die('Restricted access');
-?>
-    <!-- colors legend -->
-<?php
-if ($this->config['show_colorlegend'])
-{
-	?>
-    <table class="table">
-        <tr>
-			<?php
-			sportsmanagementHelper::showColorsLegend($this->colors, $this->divisions);
-			?>
-        </tr>
-    </table>
-    <br/>
-	<?php
+use Joomla\CMS\Language\Text;
+
+if (empty($this->config['show_colorlegend'])) {
+    return;
 }
+
+$colors = $this->colors;
+if ($this->input->getString('func', '') !== 'showCurve' && !empty($this->project->fav_team)) {
+    $colors[] = [
+        'color' => (string) ($this->project->fav_team_color ?? ''),
+        'description' => Text::_('COM_SPORTSMANAGEMENT_RANKING_FAVTEAM'),
+    ];
+}
+
+$entries = [];
+foreach ($colors as $color) {
+    $value = is_array($color) ? $color : (array) $color;
+    $background = trim((string) ($value['color'] ?? ''));
+    $description = trim((string) ($value['description'] ?? ''));
+    $from = trim((string) ($value['from'] ?? ''));
+    $to = trim((string) ($value['to'] ?? ''));
+
+    if ($background === '' && $description === '' && $from === '' && $to === '') {
+        continue;
+    }
+
+    if ($description === '' && ($from !== '' || $to !== '')) {
+        $description = trim($from . ($from !== '' && $to !== '' ? ' - ' : '') . $to);
+    }
+
+    $entries[] = [
+        'color' => preg_replace('/[^#A-Za-z0-9(),.%\s-]/', '', $background) ?? '',
+        'description' => $description,
+    ];
+}
+
+if ($entries === []) {
+    return;
+}
+?>
+<div class="table-responsive mb-3" id="curve-colorlegend">
+    <table class="table table-sm">
+        <tbody>
+        <tr>
+            <?php foreach ($entries as $entry) : ?>
+                <td class="text-center align-middle">
+                    <?php if ($entry['color'] !== '') : ?>
+                        <span
+                            aria-hidden="true"
+                            style="display:inline-block;width:1.25rem;height:1.25rem;vertical-align:middle;background-color:<?php echo $this->escape($entry['color']); ?>;border:1px solid currentColor;"
+                        ></span>
+                    <?php endif; ?>
+                    <?php if ($entry['description'] !== '') : ?>
+                        <span class="ms-1"><?php echo $this->escape(Text::_($entry['description'])); ?></span>
+                    <?php endif; ?>
+                </td>
+            <?php endforeach; ?>
+        </tr>
+        </tbody>
+    </table>
+</div>
