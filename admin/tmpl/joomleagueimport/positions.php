389cb88b2df78b73726ab53eade5b9c560e26d9c
diddipoeler <diddipoeler@gmx.de>
2026-08-28 17:35:43 +0200

    Remove legacy helpers from JoomlaLeague positions template

--- admin/tmpl/joomleagueimport/positions.php
+++ admin/tmpl/joomleagueimport/positions.php
@@ -1,95 +1,54 @@
 <?php
-/**
- *
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage joomleagueimport
- * @file       positions.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-
-
+/** SportsManagement JoomlaLeague position mapping. */
 defined('_JEXEC') or die('Restricted access');
 
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
-
-$templatesToLoad = array('footer', 'listheader');
-sportsmanagementHelper::addTemplatePaths($templatesToLoad, $this);
 ?>
-
-<form action="<?php echo $this->request_url; ?>" method="post" id="adminForm" name="adminForm">
-
-
-    <table class="<?php echo $this->table_data_class; ?>">
+<form action="<?php echo $this->escape($this->request_url); ?>" method="post" id="adminForm" name="adminForm">
+    <table class="<?php echo $this->escape($this->table_data_class); ?>">
         <thead>
         <tr>
-			<?php echo $this->lists['whichtable']; ?>
+            <?php echo $this->lists['whichtable']; ?>
         </tr>
         <tr>
             <th width="5"><?php echo Text::_('COM_SPORTSMANAGEMENT_GLOBAL_NUM'); ?></th>
-            <th width="20">
-			<?php echo HTMLHelper::_('grid.checkall'); ?>
-			</th>
-
-            <th width=""
-                style="vertical-align: top; "><?php echo Text::_('COM_SPORTSMANAGEMENT_JOOMLEAGUE_POSITIONS'); ?></th>
-            <th width=""
-                style="vertical-align: top; "><?php echo Text::_('COM_SPORTSMANAGEMENT_SPORTSMANAGEMENT_POSITIONS'); ?></th>
+            <th width="20"><?php echo HTMLHelper::_('grid.checkall'); ?></th>
+            <th style="vertical-align: top;"><?php echo Text::_('COM_SPORTSMANAGEMENT_JOOMLEAGUE_POSITIONS'); ?></th>
+            <th style="vertical-align: top;"><?php echo Text::_('COM_SPORTSMANAGEMENT_SPORTSMANAGEMENT_POSITIONS'); ?></th>
         </tr>
         </thead>
         <tbody>
-		<?php
-		$k = 0;
-		for ($i = 0, $n = count($this->joomleague); $i < $n; $i++)
-		{
-			$row     =& $this->joomleague[$i];
-			$checked = HTMLHelper::_('grid.checkedout', $row, $i);
-			?>
-            <tr class="<?php echo "row$k"; ?>">
-                <td class="center"><?php echo $i; ?></td>
+        <?php foreach ($this->joomleague as $i => $row) : ?>
+            <?php $checked = HTMLHelper::_('grid.checkedout', $row, $i); ?>
+            <tr class="row<?php echo $i % 2; ?>">
+                <td class="center"><?php echo (int) $i; ?></td>
                 <td class="center"><?php echo $checked; ?></td>
-
-                <td><?php echo $row->name; ?>
-
-                </td>
+                <td><?php echo $this->escape($row->name ?? ''); ?></td>
                 <td class="center">
-					<?php
-					$append = ' onchange="document.getElementById(\'cb' . $i . '\').checked=true" ';
-					echo HTMLHelper::_(
-						'select.genericlist', $this->lists['position'], 'position' . $row->id,
-						'class="inputbox" size="1"' . $append, 'value', 'text', 0
-					);
-					?>
+                    <?php
+                    $append = ' onchange="document.getElementById(\'cb' . (int) $i . '\').checked=true" ';
+                    echo HTMLHelper::_(
+                        'select.genericlist',
+                        $this->lists['position'],
+                        'position' . (int) ($row->id ?? 0),
+                        'class="inputbox" size="1"' . $append,
+                        'value',
+                        'text',
+                        0
+                    );
+                    ?>
                 </td>
-
-
             </tr>
-			<?php
-			$k = 1 - $k;
-		}
-		?>
+        <?php endforeach; ?>
         </tbody>
     </table>
 
-	<?PHP
-
-
-	?>
-    <input type="hidden" name="search_mode" value="<?php echo $this->lists['search_mode']; ?>"/>
-    <input type="hidden" name="boxchecked" value="0"/>
-    <input type="hidden" name="task" value=""/>
-    <input type="hidden" name="filter_order" value="<?php echo $this->sortColumn; ?>"/>
-    <input type="hidden" name="filter_order_Dir" value="<?php echo $this->sortDirection; ?>"/>
-	<?php echo HTMLHelper::_('form.token') . "\n"; ?>
-
+    <input type="hidden" name="search_mode" value="<?php echo $this->escape($this->lists['search_mode']); ?>" />
+    <input type="hidden" name="boxchecked" value="0" />
+    <input type="hidden" name="task" value="" />
+    <input type="hidden" name="filter_order" value="<?php echo $this->escape($this->sortColumn); ?>" />
+    <input type="hidden" name="filter_order_Dir" value="<?php echo $this->escape($this->sortDirection); ?>" />
+    <?php echo HTMLHelper::_('form.token') . "\n"; ?>
 </form>
-<?PHP
-
-echo $this->loadTemplate('footer');
-
-?>  
+<?php echo $this->loadTemplate('footer'); ?>
