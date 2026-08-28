37b58654c6c8c6c84213a858d7b1094197ca6bb5
diddipoeler <diddipoeler@gmx.de>
2026-08-28 08:07:35 +0200

    Use native helpers in referees list template

--- site/tmpl/referees/default_referees.php
+++ site/tmpl/referees/default_referees.php
@@ -1,175 +1,178 @@
 <?php
-/**
- *
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage referees
- * @file       default_referees.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-
+/** SportsManagement referees list for Joomla 5/6. */
 defined('_JEXEC') or die('Restricted access');
 
-use Joomla\CMS\Language\Text;
+use Diddipoeler\Component\SportsManagement\Site\Helper\CountryPresentationHelper;
+use Diddipoeler\Component\SportsManagement\Site\Helper\ModalImageHelper;
+use Diddipoeler\Component\SportsManagement\Site\Helper\PersonAgeHelper;
+use Diddipoeler\Component\SportsManagement\Site\Helper\PersonImageHelper;
+use Diddipoeler\Component\SportsManagement\Site\Helper\PersonNameFormatter;
+use Diddipoeler\Component\SportsManagement\Site\Helper\SiteRouteHelper;
 use Joomla\CMS\HTML\HTMLHelper;
-use Joomla\CMS\Factory;
+use Joomla\CMS\Language\Text;
 
-// Show referees as defined
-if (!empty($this->rows))
-{
-	?>
-    <div class="<?php echo $this->divclassrow; ?> table-responsive" id="referees">
-        <table class="<?php echo $this->config['table_class']; ?>">
-			<?php
-			$k           = 0;
-			$position    = '';
-			$totalEvents = array();
-
-			foreach ($this->rows as $row)
-			{
-				if ($position != $row->position)
-				{
-					$position = $row->position;
-					$k        = 0;
-					$colspan  = (($this->config['show_birthday'] > 0) ? '5' : '4');
-					?>
-                    <tr class="sectiontableheader">
-                        <td width="60%" colspan="<?php echo $colspan; ?>">
-							<?php
-							echo '&nbsp;' . Text::_($row->position);
-							?>
-                        </td>
-						<?php if ($this->config['show_games_count']) : ?>
-                            <td style="text-align:center; ">
-								<?php
-								$imageTitle = Text::_('COM_SPORTSMANAGEMENT_REFEREES_GAMES');
-								echo HTMLHelper::image(
-									'images/com_sportsmanagement/database/events/' . $this->project->fs_sport_type_name . '/refereed.png',
-									$imageTitle, array('title' => $imageTitle, 'height' => 20)
-								);
-								?>
-                            </td>
-						<?php endif; ?>
-                    </tr>
-					<?php
-				}
-				?>
-                <tr class="">
-                    <td width="30" style="text-align:center; ">
-						<?php
-						echo '&nbsp;';
-						?>
-                    </td>
-                    <td width="40" style="text-align:center; " class="nowrap">
-						<?php
-						$refereeName = sportsmanagementHelper::formatName(null, $row->firstname, $row->nickname, $row->lastname, $this->config["name_format"]);
-						if ($this->config['show_icon'] == 1)
-						{
-							echo sportsmanagementHelperHtml::getBootstrapModalImage(
-								'referee' . $row->id,
-								$row->picture,
-								$refereeName,
-								$this->config['referee_picture_width'],
-								'',
-								$this->modalwidth,
-								$this->modalheight,
-								$this->overallconfig['use_jquery_modal']
-							);
-
-						}
-						?>
-                    </td>
-                    <td style="width:20%;">
-						<?php
-						if ($this->config['link_name'] == 1)
-						{
-							$routeparameter                       = array();
-							$routeparameter['cfg_which_database'] = Factory::getApplication()->input->getInt('cfg_which_database', 0);
-							$routeparameter['s']                  = Factory::getApplication()->input->getInt('s', 0);
-							$routeparameter['p']                  = $this->project->slug;
-							$routeparameter['pid']                = $row->slug;
-							$link                                 = sportsmanagementHelperRoute::getSportsmanagementRoute('referee', $routeparameter);
-
-							echo HTMLHelper::link($link, '<i>' . $refereeName . '</i>');
-						}
-						else
-						{
-							echo '<i>' . $refereeName . '</i>';
-						}
-						?>
-                    </td>
-                    <td style="width:16px; text-align:center; " class="nowrap">
-						<?php
-						echo JSMCountries::getCountryFlag($row->country);
-						?>
-                    </td>
-					<?php
-					if ($this->config['show_birthday'] > 0)
-					{
-						?>
-                        <td width="10%" class="nowrap" style="text-align:left; ">
-							<?php
-
-							switch ($this->config['show_birthday'])
-							{
-								case 1:     // show Birthday and Age
-									$birthdateStr = $row->birthday != "0000-00-00" ? HTMLHelper::date(
-										$row->birthday . ' UTC',
-										Text::_('COM_SPORTSMANAGEMENT_GLOBAL_DAYDATE'),
-										sportsmanagementHelper::getTimezone($this->project, $this->overallconfig)
-									) : "-";
-									$birthdateStr .= "&nbsp;(" . sportsmanagementHelper::getAge($row->birthday, $row->deathday) . ")";
-									break;
-								case 2:     // show Only Birthday
-									$birthdateStr = $row->birthday != "0000-00-00" ? HTMLHelper::date(
-										$row->birthday . ' UTC',
-										Text::_('COM_SPORTSMANAGEMENT_GLOBAL_DAYDATE'),
-										sportsmanagementHelper::getTimezone($this->project, $this->overallconfig)
-									) : "-";
-									break;
-								case 3:     // show Only Age
-									$birthdateStr = "(" . sportsmanagementHelper::getAge($row->birthday, $row->deathday) . ")";
-									break;
-								case 4:     // show Only Year of birth
-									$birthdateStr = $row->birthday != "0000-00-00" ? HTMLHelper::date(
-										$row->birthday . ' UTC',
-										Text::_('%Y'),
-										sportsmanagementHelper::getTimezone($this->project, $this->overallconfig)
-									) : "-";
-									break;
-
-								default:
-									$birthdateStr = "";
-									break;
-							}
-							echo $birthdateStr;
-							?>
-                        </td>
-						<?php
-					}
-					?>
-					<?php if ($this->config['show_games_count']) : ?>
-                        <td>
-							<?php echo $row->countGames; ?>
-                        </td>
-					<?php endif; ?>
-                </tr>
-				<?php
-				$k = 1 - $k;
-			}
-			$colspan = 9;
-			if ($this->config['show_birthday'] > 0)
-			{
-				$colspan++;
-			}
-			?>
-        </table>
-    </div>
-	<?php
+if (!$this->rows) {
+    return;
 }
+
+$escape = static fn (mixed $value): string => htmlspecialchars((string) $value, ENT_QUOTES, 'UTF-8');
+$pictureServer = \defined('COM_SPORTSMANAGEMENT_PICTURE_SERVER')
+    ? (string) COM_SPORTSMANAGEMENT_PICTURE_SERVER
+    : '';
+$pictureUrl = static function (string $picture) use ($pictureServer): string {
+    $picture = trim($picture);
+
+    if ($picture === '') {
+        return '';
+    }
+
+    return preg_match('#^https?://#i', $picture)
+        ? $picture
+        : rtrim($pictureServer, '/') . '/' . ltrim($picture, '/');
+};
+
+$databaseSelector = $this->databaseSelector;
+$seasonId = $this->input->getInt('s', 0);
+$projectSlug = (string) ($this->project->slug ?? $this->project->id ?? '');
+$projectTimezone = trim((string) ($this->project->timezone ?? '')) ?: 'UTC';
+$showBirthday = (int) ($this->config['show_birthday'] ?? 0);
+$showGamesCount = !empty($this->config['show_games_count']);
+$showIcon = !empty($this->config['show_icon']);
+$modalMode = (int) ($this->overallconfig['use_jquery_modal'] ?? 0);
+$position = null;
 ?>
-<br/>
+<div class="<?php echo $this->escape($this->divclassrow); ?> table-responsive" id="referees">
+    <table class="<?php echo $this->escape((string) ($this->config['table_class'] ?? 'table')); ?>">
+        <tbody>
+        <?php foreach ($this->rows as $row) : ?>
+            <?php
+            if ($position !== (string) $row->position) {
+                $position = (string) $row->position;
+                $colspan = 4 + ($showBirthday > 0 ? 1 : 0);
+                ?>
+                <tr class="sectiontableheader">
+                    <th colspan="<?php echo $colspan; ?>">
+                        <?php echo Text::_($position); ?>
+                    </th>
+                    <?php if ($showGamesCount) : ?>
+                        <th class="text-center">
+                            <?php
+                            $imageTitle = Text::_('COM_SPORTSMANAGEMENT_REFEREES_GAMES');
+                            echo HTMLHelper::image(
+                                'images/com_sportsmanagement/database/events/'
+                                . (string) ($this->project->fs_sport_type_name ?? '')
+                                . '/refereed.png',
+                                $imageTitle,
+                                ['title' => $imageTitle, 'height' => 20]
+                            );
+                            ?>
+                        </th>
+                    <?php endif; ?>
+                </tr>
+                <?php
+            }
+
+            $refereeName = PersonNameFormatter::format(
+                null,
+                (string) ($row->firstname ?? ''),
+                (string) ($row->nickname ?? ''),
+                (string) ($row->lastname ?? ''),
+                $this->config['name_format'] ?? 0
+            );
+            $picture = trim((string) ($row->picture ?? ''));
+            if ($picture === '') {
+                $picture = PersonImageHelper::placeholder();
+            }
+            ?>
+            <tr>
+                <td class="text-center" style="width:30px">&nbsp;</td>
+                <td class="text-center nowrap" style="width:40px">
+                    <?php
+                    if ($showIcon && $picture !== '') {
+                        echo ModalImageHelper::render(
+                            'referee' . (int) ($row->id ?? 0),
+                            $pictureUrl($picture),
+                            $refereeName,
+                            (int) ($this->config['referee_picture_width'] ?? 20),
+                            '',
+                            $this->modalwidth,
+                            $this->modalheight,
+                            $modalMode,
+                            'itemprop',
+                            'image'
+                        );
+                    }
+                    ?>
+                </td>
+                <td style="width:20%">
+                    <?php
+                    if (!empty($this->config['link_name'])) {
+                        $link = SiteRouteHelper::view('referee', [
+                            'cfg_which_database' => $databaseSelector,
+                            's' => $seasonId,
+                            'p' => $projectSlug,
+                            'pid' => (string) ($row->slug ?? $row->id ?? ''),
+                        ]);
+                        echo HTMLHelper::link($link, '<i>' . $escape($refereeName) . '</i>');
+                    } else {
+                        echo '<i>' . $escape($refereeName) . '</i>';
+                    }
+                    ?>
+                </td>
+                <td class="text-center nowrap" style="width:16px">
+                    <?php echo CountryPresentationHelper::flag((string) ($row->country ?? '')); ?>
+                </td>
+
+                <?php if ($showBirthday > 0) : ?>
+                    <td class="nowrap text-start" style="width:10%">
+                        <?php
+                        $birthday = (string) ($row->birthday ?? '0000-00-00');
+                        $deathday = (string) ($row->deathday ?? '0000-00-00');
+                        $hasBirthday = $birthday !== '' && $birthday !== '0000-00-00';
+                        $age = PersonAgeHelper::calculate($birthday, $deathday);
+
+                        switch ($showBirthday) {
+                            case 1:
+                                $birthdate = $hasBirthday
+                                    ? HTMLHelper::date(
+                                        $birthday . ' UTC',
+                                        Text::_('COM_SPORTSMANAGEMENT_GLOBAL_DAYDATE'),
+                                        $projectTimezone
+                                    )
+                                    : '-';
+                                echo $escape($birthdate) . '&nbsp;(' . $escape($age) . ')';
+                                break;
+
+                            case 2:
+                                echo $hasBirthday
+                                    ? $escape(HTMLHelper::date(
+                                        $birthday . ' UTC',
+                                        Text::_('COM_SPORTSMANAGEMENT_GLOBAL_DAYDATE'),
+                                        $projectTimezone
+                                    ))
+                                    : '-';
+                                break;
+
+                            case 3:
+                                echo '(' . $escape($age) . ')';
+                                break;
+
+                            case 4:
+                                echo $hasBirthday
+                                    ? $escape(HTMLHelper::date($birthday . ' UTC', 'Y', $projectTimezone))
+                                    : '-';
+                                break;
+                        }
+                        ?>
+                    </td>
+                <?php endif; ?>
+
+                <?php if ($showGamesCount) : ?>
+                    <td><?php echo (int) ($row->countGames ?? 0); ?></td>
+                <?php endif; ?>
+            </tr>
+        <?php endforeach; ?>
+        </tbody>
+    </table>
+</div>
+<br>
