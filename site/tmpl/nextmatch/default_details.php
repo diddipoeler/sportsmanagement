7cbe268f1c3c6007994fc95ed50281dd3a80c437
diddipoeler <diddipoeler@gmx.de>
2026-08-28 08:56:07 +0200

    Migrate next match details to native helpers

--- site/tmpl/nextmatch/default_details.php
+++ site/tmpl/nextmatch/default_details.php
@@ -1,216 +1,170 @@
 <?php
 /**
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage nextmatch
- * @file       default_details.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * Native Joomla 5/6 next-match details.
  */
-defined('_JEXEC') or die('Restricted access');
+\defined('_JEXEC') or die;
+
+use Diddipoeler\Component\SportsManagement\Site\Helper\MatchTimeHelper;
+use Diddipoeler\Component\SportsManagement\Site\Helper\PersonNameFormatter;
 use Diddipoeler\Component\SportsManagement\Site\Helper\SiteRouteHelper;
-use Joomla\CMS\Language\Text;
 use Joomla\CMS\HTML\HTMLHelper;
-use Joomla\CMS\Factory;
+use Joomla\CMS\Language\Text;
 
+$escape = static fn ($value): string => htmlspecialchars((string) $value, ENT_QUOTES, 'UTF-8');
+$databaseSelector = $this->input->getInt('cfg_which_database', 0) === 1 ? 1 : 0;
+$seasonId = max(0, $this->input->getInt('s', 0));
+$projectRoute = $this->project->slug ?? $this->project->id ?? 0;
 ?>
-<!-- START of match details -->
-<div class="<?php echo $this->divclassrow; ?> table-responsive" id="nextmatch">
-<?php
-$this->notes = array();
-//$this->notes[] = Text::_('COM_SPORTSMANAGEMENT_NEXTMATCH_DETAILS') . " " . $this->club->name;
-$this->notes[] = Text::_('COM_SPORTSMANAGEMENT_NEXTMATCH_DETAILS');
-echo $this->loadTemplate('jsm_notes'); 
-?>
+<div class="<?php echo $escape($this->divclassrow); ?> table-responsive" id="nextmatch-details">
+    <?php
+    $this->notes = [Text::_('COM_SPORTSMANAGEMENT_NEXTMATCH_DETAILS')];
+    echo $this->loadTemplate('jsm_notes');
+    ?>
+
     <table class="table">
-        <!-- Prev Match-->
-		<?php
-		if ($this->match->old_match_id > 0)
-		{
-			?>
+        <?php if ((int) ($this->match->old_match_id ?? 0) > 0) : ?>
+            <?php
+            $oldLink = SiteRouteHelper::view('matchreport', [
+                'cfg_which_database' => $databaseSelector,
+                's' => $seasonId,
+                'p' => $projectRoute,
+                'mid' => (int) $this->match->old_match_id,
+            ]);
+            ?>
             <tr>
-                <td colspan="3"><span class=""><?php echo Text::_('COM_SPORTSMANAGEMENT_NEXTMATCH_OLD_MATCH'); ?></span>
-                    <span><?php
-						$routeparameter                       = array();
-						$routeparameter['cfg_which_database'] = Factory::getApplication()->input->getInt('cfg_which_database', 0);
-						$routeparameter['s']                  = Factory::getApplication()->input->getInt('s', 0);
-						$routeparameter['p']                  = $this->project->slug;
-						$routeparameter['mid']                = $this->match->old_match_id;
-						$link                                 = SiteRouteHelper::view('matchreport', $routeparameter);
-						echo HTMLHelper:: link($link, $this->oldmatchtext);
-						?></span></td>
+                <td colspan="3">
+                    <span><?php echo Text::_('COM_SPORTSMANAGEMENT_NEXTMATCH_OLD_MATCH'); ?></span>
+                    <span><?php echo HTMLHelper::link($oldLink, $escape($this->oldmatchtext)); ?></span>
+                </td>
             </tr>
-			<?php
-		}
-		?>
-        <!-- Next Match-->
-		<?php
-		if ($this->match->new_match_id > 0)
-		{
-			?>
+        <?php endif; ?>
+
+        <?php if ((int) ($this->match->new_match_id ?? 0) > 0) : ?>
+            <?php
+            $newLink = SiteRouteHelper::view('nextmatch', [
+                'cfg_which_database' => $databaseSelector,
+                's' => $seasonId,
+                'p' => $projectRoute,
+                'mid' => (int) $this->match->new_match_id,
+            ]);
+            ?>
             <tr>
-                <td colspan="3"><span class=""><?php echo Text::_('COM_SPORTSMANAGEMENT_NEXTMATCH_NEW_MATCH'); ?></span>
+                <td colspan="3">
+                    <span><?php echo Text::_('COM_SPORTSMANAGEMENT_NEXTMATCH_NEW_MATCH'); ?></span>
+                    <span><?php echo HTMLHelper::link($newLink, $escape($this->newmatchtext)); ?></span>
+                </td>
+            </tr>
+        <?php endif; ?>
+
+        <?php if (!empty($this->config['show_match_date']) && !empty($this->match->match_date)) : ?>
+            <tr>
+                <td colspan="3">
+                    <span><?php echo Text::_('COM_SPORTSMANAGEMENT_NEXTMATCH_DATE'); ?></span>
+                    <span><?php echo HTMLHelper::date(
+                        (string) $this->match->match_date,
+                        Text::_('COM_SPORTSMANAGEMENT_NEXTMATCH_GAMES_DATE')
+                    ); ?></span>
+                </td>
+            </tr>
+        <?php endif; ?>
+
+        <?php if (!empty($this->config['show_match_time']) && !empty($this->match->match_date)) : ?>
+            <tr>
+                <td colspan="3">
+                    <span><?php echo Text::_('COM_SPORTSMANAGEMENT_NEXTMATCH_TIME'); ?></span>
+                    <span><?php echo MatchTimeHelper::format(
+                        $this->match,
+                        $this->config,
+                        $this->overallconfig,
+                        $this->project
+                    ); ?></span>
+                </td>
+            </tr>
+        <?php endif; ?>
+
+        <?php if (!empty($this->config['show_time_present']) && !empty($this->match->time_present)) : ?>
+            <tr>
+                <td colspan="3">
+                    <span><?php echo Text::_('COM_SPORTSMANAGEMENT_NEXTMATCH_PRESENT'); ?></span>
+                    <span><?php echo $escape($this->match->time_present); ?></span>
+                </td>
+            </tr>
+        <?php endif; ?>
+
+        <?php if (!empty($this->config['show_match_number']) && !empty($this->match->match_number)) : ?>
+            <tr>
+                <td colspan="3">
+                    <span><?php echo Text::_('COM_SPORTSMANAGEMENT_NEXTMATCH_NUMBER'); ?></span>
+                    <span><?php echo $escape($this->match->match_number); ?></span>
+                </td>
+            </tr>
+        <?php endif; ?>
+
+        <?php if (!empty($this->match->cancel)) : ?>
+            <tr>
+                <td colspan="3">
+                    <span><?php echo Text::_('COM_SPORTSMANAGEMENT_NEXTMATCH_CANCEL_REASON'); ?></span>
+                    <span><?php echo $escape($this->match->cancel_reason ?? ''); ?></span>
+                </td>
+            </tr>
+        <?php endif; ?>
+
+        <tr><td colspan="3">&nbsp;</td></tr>
+
+        <?php if (!empty($this->config['show_match_playground']) && (int) ($this->match->playground_id ?? 0) > 0) : ?>
+            <?php
+            $playgroundLink = SiteRouteHelper::view('playground', [
+                'cfg_which_database' => $databaseSelector,
+                's' => $seasonId,
+                'p' => $projectRoute,
+                'pgid' => $this->match->playground_slug ?? (int) $this->match->playground_id,
+            ]);
+            ?>
+            <tr>
+                <td colspan="3">
+                    <span><?php echo Text::_('COM_SPORTSMANAGEMENT_NEXTMATCH_PLAYGROUND'); ?></span>
                     <span>
-		<?php
-		$routeparameter                       = array();
-		$routeparameter['cfg_which_database'] = Factory::getApplication()->input->getInt('cfg_which_database', 0);
-		$routeparameter['s']                  = Factory::getApplication()->input->getInt('s', 0);
-		$routeparameter['p']                  = $this->project->id;
-		$routeparameter['mid']                = $this->match->new_match_id;
-		echo HTMLHelper:: link(SiteRouteHelper::view('nextmatch', $routeparameter), $this->newmatchtext);
-		?>
-		</span></td>
+                        <?php echo isset($this->playground->name)
+                            ? HTMLHelper::link($playgroundLink, $escape($this->playground->name))
+                            : Text::_('COM_SPORTSMANAGEMENT_NEXTMATCH_PLAYGROUND_NO_ASSIGN'); ?>
+                    </span>
+                </td>
             </tr>
-			<?php
-		}
-		?>
+        <?php endif; ?>
 
-        <!-- Date -->
-		<?php
-		if ($this->config['show_match_date'])
-		{
-			if ($this->match->match_date > 0)
-				:
-				?>
-                <tr>
-                    <td colspan="3"><span class=""><?php echo Text::_('COM_SPORTSMANAGEMENT_NEXTMATCH_DATE'); ?></span>
-                        <span><?php echo HTMLHelper::date($this->match->match_date, Text::_('COM_SPORTSMANAGEMENT_NEXTMATCH_GAMES_DATE')); ?></span>
-                    </td>
-                </tr>
-			<?php endif;
-		} ?>
-
-        <!-- Time -->
-		<?php
-		if ($this->config['show_match_time'])
-		{
-			if ($this->match->match_date > 0)
-				:
-				?>
-                <tr>
-                    <td colspan="3"><span class=""><?php echo Text::_('COM_SPORTSMANAGEMENT_NEXTMATCH_TIME'); ?></span>
-                        <span><?php echo sportsmanagementHelperHtml::showMatchTime($this->match, $this->config, $this->overallconfig, $this->project); ?></span>
-                    </td>
-                </tr>
-			<?php endif;
-		} ?>
-
-        <!-- present -->
-		<?php
-		if ($this->config['show_time_present'])
-		{
-			if ($this->match->time_present > 0)
-				:
-				?>
-                <tr>
-                    <td colspan="3"><span
-                                class=""><?php echo Text::_('COM_SPORTSMANAGEMENT_NEXTMATCH_PRESENT'); ?></span>
-                        <span><?php echo $this->match->time_present; ?></span></td>
-                </tr>
-			<?php endif;
-		} ?>
-
-        <!-- match number -->
-		<?php
-		if ($this->config['show_match_number'])
-		{
-			if ($this->match->match_number > 0)
-				:
-				?>
-                <tr>
-                    <td colspan="3"><span
-                                class=""><?php echo Text::_('COM_SPORTSMANAGEMENT_NEXTMATCH_NUMBER'); ?></span>
-                        <span><?php echo $this->match->match_number; ?></span></td>
-                </tr>
-			<?php endif;
-		} ?>
-
-        <!-- match canceled -->
-		<?php if ($this->match->cancel > 0)
-			:
-			?>
+        <?php if (!empty($this->config['show_match_referees']) && $this->referees) : ?>
+            <?php
+            $refereeLinks = [];
+            foreach ($this->referees as $referee) {
+                $name = PersonNameFormatter::format(
+                    null,
+                    (string) ($referee->firstname ?? ''),
+                    (string) ($referee->nickname ?? ''),
+                    (string) ($referee->lastname ?? ''),
+                    $this->config['name_format'] ?? 0
+                );
+                $link = SiteRouteHelper::view('referee', [
+                    'cfg_which_database' => $databaseSelector,
+                    's' => $seasonId,
+                    'p' => $projectRoute,
+                    'pid' => (int) ($referee->person_id ?? 0),
+                ]);
+                $position = trim((string) ($referee->position_name ?? ''));
+                $label = HTMLHelper::link($link, $name);
+                if ($position !== '') {
+                    $label .= ' (' . $escape(Text::_($position)) . ')';
+                }
+                $refereeLinks[] = $label;
+            }
+            ?>
             <tr>
-                <td colspan="3"><span
-                            class=""><?php echo Text::_('COM_SPORTSMANAGEMENT_NEXTMATCH_CANCEL_REASON'); ?></span>
-                    <span><?php echo $this->match->cancel_reason; ?></span></td>
+                <td colspan="3">
+                    <span><?php echo Text::_('COM_SPORTSMANAGEMENT_NEXTMATCH_REFEREE'); ?></span>
+                    <span><?php echo implode(', ', $refereeLinks); ?></span>
+                </td>
             </tr>
-		<?php endif; ?>
-
-        <tr>
-            <td colspan="3">&nbsp;</td>
-        </tr>
-
-
-        <!-- playground -->
-		<?php
-		if ($this->config['show_match_playground'])
-		{
-			if ($this->match->playground_id > 0)
-				:
-				?>
-				<?php
-				$routeparameter                       = array();
-				$routeparameter['cfg_which_database'] = Factory::getApplication()->input->getInt('cfg_which_database', 0);
-				$routeparameter['s']                  = Factory::getApplication()->input->getInt('s', 0);
-				$routeparameter['p']                  = $this->project->slug;
-				$routeparameter['pgid']               = $this->match->playground_slug;
-				$playground_link                      = SiteRouteHelper::view('playground', $routeparameter);
-				?>
-                <tr>
-                    <td colspan="3"><span
-                                class=""><?php echo Text::_('COM_SPORTSMANAGEMENT_NEXTMATCH_PLAYGROUND'); ?></span>
-                        <span>
-					<?php
-					if (isset($this->playground->name))
-					{
-						echo HTMLHelper::link($playground_link, $this->playground->name);
-					}
-					else
-					{
-						echo Text::_('COM_SPORTSMANAGEMENT_NEXTMATCH_PLAYGROUND_NO_ASSIGN');
-					}
-					?>
-					</span>
-                    </td>
-                </tr>
-			<?php endif;
-		} ?>
-
-        <!-- referee -->
-		<?php
-		if ($this->config['show_match_referees'])
-		{
-			if (!empty($this->referees))
-				:
-				?>
-				<?php $html = array(); ?>
-                <tr>
-                    <td colspan="3"><span
-                                class=""><?php echo Text::_('COM_SPORTSMANAGEMENT_NEXTMATCH_REFEREE'); ?></span>
-						<?php
-						foreach ($this->referees AS $ref)
-							:
-							?>
-							<?php
-							$routeparameter                       = array();
-							$routeparameter['cfg_which_database'] = Factory::getApplication()->input->getInt('cfg_which_database', 0);
-							$routeparameter['s']                  = Factory::getApplication()->input->getInt('s', 0);
-							$routeparameter['p']                  = $this->project->id;
-							$routeparameter['pid']                = $ref->person_id;
-							$link                                 = SiteRouteHelper::view('referee', $routeparameter);
-							?>
-							<?php $html[] = HTMLHelper::link($link, sportsmanagementHelper::formatName(null, $ref->firstname, $ref->nickname, $ref->lastname, $this->config["name_format"])) . ' (' . $ref->position_name . ')'; ?>
-						<?php endforeach; ?> <span><?php echo implode('</span>, <span>', $html); ?></span>
-                    </td>
-                </tr>
-			<?php endif;
-		} ?>
-
+        <?php endif; ?>
     </table>
 
-    <br/>
+    <br>
 </div>
-
-
