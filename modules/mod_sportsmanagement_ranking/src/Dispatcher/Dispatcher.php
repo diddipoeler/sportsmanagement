98200fe2763308662c2b2c3cdec9e12beb4a18d6
diddipoeler <diddipoeler@gmx.de>
2026-08-28 19:19:09 +0200

    Move ranking assets into native dispatcher

--- modules/mod_sportsmanagement_ranking/src/Dispatcher/Dispatcher.php
+++ modules/mod_sportsmanagement_ranking/src/Dispatcher/Dispatcher.php
@@ -16,6 +16,17 @@ final class Dispatcher extends AbstractModuleDispatcher implements HelperFactory
         $data = parent::getLayoutData();
         $data['params']->set('layout', 'native');
         $this->getApplication()->getLanguage()->load('com_sportsmanagement', JPATH_SITE, null, true);
+
+        $moduleName = (string) ($data['module']->module ?? 'mod_sportsmanagement_ranking');
+        $style = 'modules/' . $moduleName . '/css/' . $moduleName . '.css';
+
+        if (is_file(JPATH_ROOT . '/' . $style)) {
+            $this->getApplication()
+                ->getDocument()
+                ->getWebAssetManager()
+                ->registerAndUseStyle($moduleName, $style);
+        }
+
         $data['list'] = $this->getHelperFactory()->getHelper('RankingHelper')->getData(
             $data['params'],
             $data['module'],
