fdf966531abcdaca5f3a8f8fd579653bc421ed9a
diddipoeler <diddipoeler@gmx.de>
2026-08-29 14:39:28 +0200

    Joomla 5/6: resolve club birthday application through DI

--- modules/mod_sportsmanagement_club_birthday/helper.php
+++ modules/mod_sportsmanagement_club_birthday/helper.php
@@ -5,6 +5,7 @@
 \defined('_JEXEC') or die;
 
 use Diddipoeler\Module\SportsManagementClubBirthday\Site\Helper\ClubBirthdayHelper;
+use Joomla\CMS\Application\SiteApplication;
 use Joomla\CMS\Factory;
 use Joomla\Database\DatabaseInterface;
 use Joomla\Registry\Registry;
@@ -18,9 +19,11 @@ if (!class_exists('modSportsmanagementClubBirthdayHelper', false)) {
     {
         public static function getData(Registry $params): array
         {
-            $app = Factory::getApplication();
+            $container = Factory::getContainer();
+            /** @var SiteApplication $app */
+            $app = $container->get(SiteApplication::class);
             /** @var DatabaseInterface $database */
-            $database = Factory::getContainer()->get(DatabaseInterface::class);
+            $database = $container->get(DatabaseInterface::class);
 
             return (new ClubBirthdayHelper())->getData($params, $app, $database);
         }
