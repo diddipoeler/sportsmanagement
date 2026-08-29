c5d4b9407060a2539ce18d1aaff2ec91f269eda5
diddipoeler <diddipoeler@gmx.de>
2026-08-29 18:21:48 +0200

    Joomla 5/6: use inherited site application in player model

--- site/src/Model/PlayerModel.php
+++ site/src/Model/PlayerModel.php
@@ -3,7 +3,6 @@ namespace Diddipoeler\Component\SportsManagement\Site\Model;
 
 \defined('_JEXEC') or die;
 
-use Joomla\CMS\Factory;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\MVC\Factory\MVCFactoryInterface;
 use Throwable;
@@ -24,7 +23,7 @@ final class PlayerModel extends SportsManagementProjectModel
     {
         parent::__construct($config, $factory);
 
-        $input = Factory::getApplication()->getInput();
+        $input = $this->siteApplication()->getInput();
         $this->personId = max(0, $input->getInt('pid', 0));
         $this->teamPlayerId = max(0, $input->getInt('pt', 0));
     }
@@ -325,7 +324,7 @@ final class PlayerModel extends SportsManagementProjectModel
 
     private function reportDatabaseError(Throwable $e): void
     {
-        Factory::getApplication()->enqueueMessage(
+        $this->siteApplication()->enqueueMessage(
             Text::sprintf('COM_SPORTSMANAGEMENT_DATABASE_ERROR_FUNCTION_FAILED', $e->getCode(), $e->getMessage()),
             'error'
         );
