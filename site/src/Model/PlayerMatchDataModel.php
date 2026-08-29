3a95687e2aa7d67b32de9b717dcdb258c96929b0
diddipoeler <diddipoeler@gmx.de>
2026-08-29 18:23:37 +0200

    Joomla 5/6: use inherited site application in player match data

--- site/src/Model/PlayerMatchDataModel.php
+++ site/src/Model/PlayerMatchDataModel.php
@@ -3,7 +3,6 @@ namespace Diddipoeler\Component\SportsManagement\Site\Model;
 
 \defined('_JEXEC') or die;
 
-use Joomla\CMS\Factory;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\MVC\Factory\MVCFactoryInterface;
 use stdClass;
@@ -216,7 +215,7 @@ final class PlayerMatchDataModel extends SportsManagementProjectModel
 
     private function reportDatabaseError(Throwable $e): void
     {
-        Factory::getApplication()->enqueueMessage(
+        $this->siteApplication()->enqueueMessage(
             Text::sprintf('COM_SPORTSMANAGEMENT_DATABASE_ERROR_FUNCTION_FAILED', $e->getCode(), $e->getMessage()),
             'error'
         );
