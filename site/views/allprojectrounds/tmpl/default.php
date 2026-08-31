34591a13444aed9bb0ee06ccda644ad08748ec54
diddipoeler <diddipoeler@gmx.de>
2026-08-31 18:51:59 +0200

    Bridge legacy allprojectrounds layout to Joomla 5/6 template

--- site/views/allprojectrounds/tmpl/default.php
+++ site/views/allprojectrounds/tmpl/default.php
@@ -1,30 +1,5 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage allprojectrounds
- * @file       default.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@arcor.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-defined('_JEXEC') or die('Restricted access');
+/** Legacy compatibility bridge for the native Joomla 5/6 all-project-rounds layout. */
+\defined('_JEXEC') or die;
 
-$templatesToLoad = array('globalviews');
-sportsmanagementHelper::addTemplatePaths($templatesToLoad, $this);
-?>
-<div class="container-fluid">
-<!-- projectheading -->
-<?php echo $this->loadTemplate('projectheading'); ?>
-<?php
-if ($this->config['show_sectionheader'])
-{
-	echo $this->loadTemplate('sectionheader');
-}
-?>
-<?php
-echo $this->loadTemplate('results_all');
-echo $this->loadTemplate('jsminfo');
-?>
-</div>
+require JPATH_SITE . '/components/com_sportsmanagement/tmpl/allprojectrounds/default.php';
