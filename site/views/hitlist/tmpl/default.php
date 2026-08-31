3ad4816a83a314207de6429cd0cc3eb6a9bb12d0
diddipoeler <diddipoeler@gmx.de>
2026-08-31 18:42:26 +0200

    Bridge legacy hitlist layout to Joomla 5/6 template

--- site/views/hitlist/tmpl/default.php
+++ site/views/hitlist/tmpl/default.php
@@ -1,39 +1,5 @@
 <?php
-/**
- * SportsManagement hit list layout for Joomla 5/6.
- */
-defined('_JEXEC') or die('Restricted access');
+/** Legacy compatibility bridge for the native Joomla 5/6 hit-list layout. */
+\defined('_JEXEC') or die;
 
-use Joomla\CMS\HTML\HTMLHelper;
-use Joomla\CMS\Uri\Uri;
-
-HTMLHelper::_('behavior.keepalive');
-
-$templatesToLoad = ['globalviews'];
-sportsmanagementHelper::addTemplatePaths($templatesToLoad, $this);
-$uri = Uri::getInstance();
-?>
-<script>
-    function tableOrdering(order, dir, task) {
-        const form = document.adminForm;
-        form.filter_order.value = order;
-        form.filter_order_Dir.value = dir;
-        Joomla.submitform(task || '', form);
-    }
-
-    function searchPerson(val) {
-        const search = document.getElementById('filter_search');
-        if (search) {
-            search.value = val;
-        }
-        Joomla.submitform('', document.adminForm);
-    }
-</script>
-<div class="row">
-    <form name="adminForm" id="adminForm" action="<?php echo htmlspecialchars($uri->toString(), ENT_QUOTES, 'UTF-8'); ?>" method="post">
-        <?php
-        echo $this->loadTemplate('items');
-        echo $this->loadTemplate('jsminfo');
-        ?>
-    </form>
-</div>
+require JPATH_SITE . '/components/com_sportsmanagement/tmpl/hitlist/default.php';
