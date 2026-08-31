4dce7e6e6ab83ad64b44bc860aa261d4ca5393bf
diddipoeler <diddipoeler@gmx.de>
2026-08-31 18:59:57 +0200

    Deduplicate AJAX navigation Joomla 5/6 adapter bootstrap

--- modules/mod_sportsmanagement_ajax_top_navigation_menu/src/Helper/NativeAjaxTopNavigationLegacyAdapter.php
+++ modules/mod_sportsmanagement_ajax_top_navigation_menu/src/Helper/NativeAjaxTopNavigationLegacyAdapter.php
@@ -3,15 +3,13 @@ namespace Diddipoeler\Module\SportsManagementAjaxTopNavigationMenu\Site\Helper;
 
 \defined('_JEXEC') or die;
 
-use Diddipoeler\Component\SportsManagement\Site\Service\SportsManagementDatabaseResolver;
 use Joomla\CMS\Application\CMSApplicationInterface;
-use Joomla\CMS\Component\ComponentHelper;
 use Joomla\Database\DatabaseInterface;
 use Joomla\Registry\Registry;
 
 /**
- * Bridge the native module dispatcher to the legacy query/link implementation
- * without resolving the Joomla application or database through global Factory calls.
+ * Bridge the native Joomla 5/6 module dispatcher to the remaining legacy
+ * query/link implementation while keeping application and database injection.
  */
 final class NativeAjaxTopNavigationLegacyAdapter extends \modSportsmanagementAjaxTopNavigationMenuHelper
 {
@@ -20,25 +18,6 @@ final class NativeAjaxTopNavigationLegacyAdapter extends \modSportsmanagementAja
         CMSApplicationInterface $app,
         DatabaseInterface $database
     ) {
-        $this->_params = $params;
-        $this->_app = $app;
-
-        $input = $app->getInput();
-        $selector = $input->getInt(
-            'cfg_which_database',
-            (int) $params->get(
-                'cfg_which_database',
-                (int) ComponentHelper::getParams('com_sportsmanagement')->get('cfg_which_database', 0)
-            )
-        );
-        $this->_db = SportsManagementDatabaseResolver::resolve($database, $selector);
-
-        if (self::$_project_id) {
-            $input->set('jlamtopseason', $this->getSeasonId());
-            $input->set('jlamtopleague', $this->getLeagueId());
-            $input->set('jlamtopproject', self::$_project_id);
-            $input->set('jlamtopteam', $this->_team_id);
-            $input->set('jlamtopdivisionid', $this->_division_id);
-        }
+        parent::__construct($params, $app, $database);
     }
 }
