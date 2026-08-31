b81d163a48af8fb6944d1f472a98d55adf006763
diddipoeler <diddipoeler@gmx.de>
2026-08-31 23:06:58 +0200

    Bridge legacy prediction template view to Joomla 5/6 MVC

--- admin/views/predictiontemplate/view.html.php
+++ admin/views/predictiontemplate/view.html.php
@@ -1,84 +1,13 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage predictiontemplate
- * @file       view.html.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\Language\Text;
-use Joomla\CMS\Factory;
-use Joomla\CMS\Form\FormFactoryInterface;
+/** Legacy compatibility bridge for the native Joomla 5/6 prediction template view. */
+\defined('_JEXEC') or die;
 
-/**
- * sportsmanagementViewPredictionTemplate
- *
- * @package
- * @author
- * @copyright diddi
- * @version   2013
- * @access    public
- */
-class sportsmanagementViewPredictionTemplate extends sportsmanagementView
-{
+use Diddipoeler\Component\SportsManagement\Administrator\View\Predictiontemplate\HtmlView;
 
-	/**
-	 * sportsmanagementViewPredictionTemplate::init()
-	 *
-	 * @return
-	 */
-	public function init()
-	{
+if (!class_exists(HtmlView::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/View/Predictiontemplate/HtmlView.php';
+}
 
-		$item       = $this->get('Item');
-		$this->item = $item;
-
-		$templatepath = JPATH_COMPONENT_SITE . DIRECTORY_SEPARATOR . 'settings';
-		$xmlfile      = $templatepath . DIRECTORY_SEPARATOR . 'default' . DIRECTORY_SEPARATOR . $item->template . '.xml';
-
-		$formFactory = Factory::getContainer()->get(FormFactoryInterface::class);
-		$form = $formFactory->createForm($item->template, array('control' => 'params'));
-
-		if (!$form->loadFile($xmlfile))
-		{
-			throw new RuntimeException('Prediction template form could not be loaded.');
-		}
-
-		$form->bind($item->params);
-
-		/** Assign the Data */
-		$this->form = $form;
-
-		$script       = $this->get('Script');
-		$this->script = $script;
-
-		$this->prediction_id  = $this->app->getUserState("$this->option.prediction_id", '0');
-		$this->predictionGame = $this->model->getPredictionGame($this->prediction_id);
-
-	}
-
-
-	/**
-	 * Add the page title and toolbar.
-	 *
-	 * @since 1.7
-	 */
-	protected function addToolbar()
-	{
-
-		$jinput = Factory::getApplication()->getInput();
-		$jinput->set('hidemainmenu', true);
-		$isNew      = $this->item->id ? $this->title = Text::_('COM_SPORTSMANAGEMENT_ADMIN_PTMPLS_EDIT') : $this->title = Text::_('COM_SPORTSMANAGEMENT_ADMIN_PTMPLS_NEW');
-		$this->icon = 'predtemplate';
-
-		$this->item->name = $this->item->template;
-
-		parent::addToolbar();
-
-	}
-
+if (!class_exists('sportsmanagementViewPredictionTemplate', false)) {
+    class_alias(HtmlView::class, 'sportsmanagementViewPredictionTemplate');
 }
