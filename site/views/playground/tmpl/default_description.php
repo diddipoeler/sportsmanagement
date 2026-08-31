544d27c84c382279a376e8677d63a6afdb1d35bf
diddipoeler <diddipoeler@gmx.de>
2026-08-31 20:55:10 +0200

    Bridge legacy playground description to Joomla 5/6 template

--- site/views/playground/tmpl/default_description.php
+++ site/views/playground/tmpl/default_description.php
@@ -1,40 +1,5 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage playground
- * @file       default_description.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\Language\Text;
-use Joomla\CMS\HTML\HTMLHelper;
+/** Legacy compatibility bridge for the native Joomla 5/6 playground description layout. */
+\defined('_JEXEC') or die;
 
-?>
-<?php
-if ($this->playground->notes)
-{
-	?>
-
-<?php  
-$this->notes = array();
-$this->notes[] = Text::_('COM_SPORTSMANAGEMENT_PLAYGROUND_NOTES');
-echo $this->loadTemplate('jsm_notes');
-?>       
-    <div class="<?php echo $this->divclassrow; ?> table-responsive" id="playground_description">
-		<?php
-		$description = $this->playground->notes;
-		$description = HTMLHelper::_('content.prepare', $description);
-		echo $description;
-		?>
-    </div>
-	<?php
-}
-
-
-//$this->notes = array();
-//$this->notes[] = Text::_('COM_SPORTSMANAGEMENT_PLAYGROUND_HISTORY_NOTIC');
-//echo $this->loadTemplate('jsm_notes');
+require JPATH_SITE . '/components/com_sportsmanagement/tmpl/playground/default_description.php';
