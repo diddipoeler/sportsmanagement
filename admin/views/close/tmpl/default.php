99ab8b6edb507362cd850bc9d21b1623157f1ce4
diddipoeler <diddipoeler@gmx.de>
2026-08-31 19:11:48 +0200

    Bridge legacy close layout to Joomla 5/6 template

--- admin/views/close/tmpl/default.php
+++ admin/views/close/tmpl/default.php
@@ -1,1 +1,5 @@
+<?php
+/** Legacy compatibility bridge for the native Joomla 5/6 close layout. */
+\defined('_JEXEC') or die;
 
+require JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/tmpl/close/default.php';
