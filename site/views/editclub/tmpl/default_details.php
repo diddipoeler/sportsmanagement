8da238b383e06f215f7c0d01f607bb767fa89470
diddipoeler <diddipoeler@gmx.de>
2026-08-31 22:05:08 +0200

    Bridge legacy editclub details to Joomla 5/6 template

--- site/views/editclub/tmpl/default_details.php
+++ site/views/editclub/tmpl/default_details.php
@@ -1,39 +1,5 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage editclub
- * @file       deafult_details.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\Language\Text;
+/** Legacy compatibility bridge for the native Joomla 5/6 edit-club details layout. */
+\defined('_JEXEC') or die;
 
-/** Get the form fieldsets. */
-$fieldsets = $this->form->getFieldsets();
-
-?>
-<fieldset class="adminform">
-    <legend><?php echo Text::_('COM_SPORTSMANAGEMENT_TABS_DETAILS'); ?>
-    </legend>
-    <table class="admintable">
-		<?php
-
-		foreach ($this->form->getFieldset('details') as $field)
-			:
-			if ($field->type == 'Radio')
-			{
-			}
-
-			?>
-            <tr>
-                <td class="key"><?php echo $field->label; ?></td>
-                <td><?php echo $field->input; ?></td>
-            </tr>
-		<?php endforeach; ?>
-    </table>
-</fieldset>	
-
+require JPATH_SITE . '/components/com_sportsmanagement/tmpl/editclub/default_details.php';
