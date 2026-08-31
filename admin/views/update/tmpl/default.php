69059a1f9fab9c3622f1c752ff52b07d203f6cef
diddipoeler <diddipoeler@gmx.de>
2026-08-31 18:28:09 +0200

    Bridge legacy update layout to Joomla 5/6 template

--- admin/views/update/tmpl/default.php
+++ admin/views/update/tmpl/default.php
@@ -1,16 +1,5 @@
 <?php
-/**
- *
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage update
- * @file       default.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
+/** Legacy compatibility bridge for the native Joomla 5/6 administrator update layout. */
+\defined('_JEXEC') or die;
 
-defined('_JEXEC') or die('Restricted access');
-
+require JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/tmpl/update/default.php';
