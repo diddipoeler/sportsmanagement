dab27523ab0137c341eca51f92dc401924c6a7ac
diddipoeler <diddipoeler@gmx.de>
2026-08-31 18:20:25 +0200

    Use Joomla 5/6 dispatcher for navigation menu module

--- modules/mod_sportsmanagement_navigation_menu/mod_sportsmanagement_navigation_menu.php
+++ modules/mod_sportsmanagement_navigation_menu/mod_sportsmanagement_navigation_menu.php
@@ -1,32 +1,7 @@
 <?php
-/** Joomla 5/6 compatibility entry point for the SportsManagement navigation menu. */
+/**
+ * SportsManagement Joomla 5/6 module entry compatibility file.
+ *
+ * The active module is booted by services/provider.php and the namespaced dispatcher.
+ */
 \defined('_JEXEC') or die;
-
-use Diddipoeler\Module\SportsManagementNavigationMenu\Site\Helper\NativeNavigationMenuHelper;
-use Joomla\CMS\Factory;
-use Joomla\CMS\Helper\ModuleHelper;
-
-if (!class_exists(NativeNavigationMenuHelper::class)) {
-    require_once __DIR__ . '/src/Helper/NavigationMenuHelper.php';
-    require_once __DIR__ . '/src/Helper/NativeNavigationMenuHelper.php';
-}
-
-$app = Factory::getApplication();
-$app->getLanguage()->load('com_sportsmanagement', JPATH_ADMINISTRATOR, null, true);
-$payload = (new NativeNavigationMenuHelper())->getData($params, $app);
-extract($payload, EXTR_SKIP);
-
-$assets = $app->getDocument()->getWebAssetManager();
-$assets->registerAndUseStyle(
-    'mod_sportsmanagement_navigation_menu',
-    'modules/mod_sportsmanagement_navigation_menu/css/mod_sportsmanagement_navigation_menu.css',
-    ['version' => 'auto']
-);
-$assets->registerAndUseScript(
-    'mod_sportsmanagement_navigation_menu',
-    'modules/mod_sportsmanagement_navigation_menu/js/mod_sportsmanagement_navigation_menu.js',
-    ['version' => 'auto'],
-    ['defer' => true]
-);
-
-require ModuleHelper::getLayoutPath($module->module, (string) $params->get('layout', 'default'));
