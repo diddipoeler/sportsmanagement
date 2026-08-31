91204681ee6c67db236bfdb12a9211e1d23abf8a
diddipoeler <diddipoeler@gmx.de>
2026-08-31 20:45:23 +0200

    Bridge legacy teamstats layout to Joomla 5/6 template

--- site/views/teamstats/tmpl/default.php
+++ site/views/teamstats/tmpl/default.php
@@ -1,43 +1,5 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage teamstats
- * @file       default.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-defined('_JEXEC') or die('Restricted access');
+/** Legacy compatibility bridge for the native Joomla 5/6 teamstats layout. */
+\defined('_JEXEC') or die;
 
-$templatesToLoad = array('globalviews');
-sportsmanagementHelper::addTemplatePaths($templatesToLoad, $this);
-?>
-<div class="<?php echo $this->divclasscontainer; ?>" id="defaultteamstats">
-	<?php
-	echo $this->loadTemplate('projectheading');
-
-	if ($this->config['show_sectionheader'])
-	{
-		echo $this->loadTemplate('sectionheader');
-	}
-
-	if ($this->config['show_general_stats'])
-	{
-		echo $this->loadTemplate('stats');
-	}
-
-	if ($this->config['show_attendance_stats'])
-	{
-		echo $this->loadTemplate('attendance_stats');
-	}
-
-	if ($this->config['show_goals_stats_flash'])
-	{
-		echo $this->loadTemplate('flashchart');
-	}
-
-	echo $this->loadTemplate('jsminfo');
-	?>
-</div>
+require JPATH_SITE . '/components/com_sportsmanagement/tmpl/teamstats/default.php';
