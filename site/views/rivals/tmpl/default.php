b7defc257c5ab5d5a2d9fb43a35ea49e193de47d
diddipoeler <diddipoeler@gmx.de>
2026-08-31 19:11:12 +0200

    Bridge legacy rivals layout to Joomla 5/6 template

--- site/views/rivals/tmpl/default.php
+++ site/views/rivals/tmpl/default.php
@@ -1,31 +1,5 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage rivals
- * @file       default.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-defined('_JEXEC') or die;
+/** Legacy compatibility bridge for the native Joomla 5/6 rivals layout. */
+\defined('_JEXEC') or die;
 
-$templatesToLoad = array('globalviews');
-sportsmanagementHelper::addTemplatePaths($templatesToLoad, $this);
-
-?>
-<div class="<?php echo $this->divclasscontainer; ?>" id="rivals">
-	<?php
-	if ($this->config['show_sectionheader'])
-	{
-		echo $this->loadTemplate('sectionheader');
-	}
-
-	echo $this->loadTemplate('projectheading');
-
-	echo $this->loadTemplate('rivals');
-
-	echo $this->loadTemplate('jsminfo');
-	?>
-</div>
+require JPATH_SITE . '/components/com_sportsmanagement/tmpl/rivals/default.php';
