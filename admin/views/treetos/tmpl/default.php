3c8859c56d3a8be1ad59a9c1f911900cdc84b5dd
diddipoeler <diddipoeler@gmx.de>
2026-08-31 18:57:19 +0200

    Bridge legacy treetos layout to Joomla 5/6 template

--- admin/views/treetos/tmpl/default.php
+++ admin/views/treetos/tmpl/default.php
@@ -1,22 +1,5 @@
 <?php
-/** SportsManagement tournament trees list wrapper. */
-defined('_JEXEC') or die('Restricted access');
+/** Legacy compatibility bridge for the native Joomla 5/6 tournament trees layout. */
+\defined('_JEXEC') or die;
 
-use Joomla\CMS\HTML\HTMLHelper;
-
-$templatesToLoad = ['footer', 'listheader'];
-sportsmanagementHelper::addTemplatePaths($templatesToLoad, $this);
-?>
-<form action="<?php echo $this->escape($this->request_url); ?>" method="post" id="adminForm" name="adminForm">
-    <?php
-    echo $this->loadTemplate('joomla_version');
-    echo $this->loadTemplate('data');
-    ?>
-    <input type="hidden" name="project_id" value="<?php echo (int) $this->projectws->id; ?>">
-    <input type="hidden" name="pid" value="<?php echo (int) $this->project_id; ?>">
-    <input type="hidden" name="boxchecked" value="0">
-    <input type="hidden" name="view" value="treetos">
-    <input type="hidden" name="task" value="treeto.display">
-    <?php echo HTMLHelper::_('form.token'); ?>
-</form>
-<?php echo $this->loadTemplate('footer'); ?>
+require JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/tmpl/treetos/default.php';
