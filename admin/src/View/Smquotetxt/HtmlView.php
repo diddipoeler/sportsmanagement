d1d0f781fd38f32185ac796a6befe06ebfe59e39
diddipoeler <diddipoeler@gmx.de>
2026-08-31 22:00:32 +0200

    Make smquotetxt a native Joomla 5/6 view

--- admin/src/View/Smquotetxt/HtmlView.php
+++ admin/src/View/Smquotetxt/HtmlView.php
@@ -3,11 +3,51 @@ namespace Diddipoeler\Component\SportsManagement\Administrator\View\Smquotetxt;
 
 \defined('_JEXEC') or die;
 
-use Diddipoeler\Component\SportsManagement\Administrator\Legacy\LegacyBootstrap;
+use Joomla\CMS\Factory;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\MVC\View\HtmlView as BaseHtmlView;
+use Joomla\CMS\Toolbar\ToolbarHelper;
 
-LegacyBootstrap::bootForView('smquotetxt');
-require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/views/smquotetxt/view.html.php';
+/** Native Joomla 5/6 administrator editor view for random-quote source files. */
+final class HtmlView extends BaseHtmlView
+{
+    public $form;
+    public $source;
+    public $state;
+    public string $file_name = '';
 
-if (!class_exists(__NAMESPACE__ . '\\HtmlView', false)) {
-    class_alias('sportsmanagementViewsmquotetxt', __NAMESPACE__ . '\\HtmlView');
+    public function display($tpl = null): void
+    {
+        $app = Factory::getApplication();
+        $app->getInput()->set('hidemainmenu', true);
+
+        $this->file_name = $app->getInput()->getString('file_name');
+        $this->form = $this->get('Form');
+        $this->source = $this->get('Source');
+        $this->state = $this->get('State');
+
+        if ($errors = $this->get('Errors')) {
+            throw new \RuntimeException(implode("\n", $errors), 500);
+        }
+
+        if (!$this->form) {
+            throw new \RuntimeException('Quote source form could not be loaded.', 500);
+        }
+
+        if ($this->file_name === '' && !empty($this->source->filename)) {
+            $this->file_name = (string) $this->source->filename;
+        }
+
+        ToolbarHelper::title(
+            Text::_($this->file_name !== ''
+                ? 'COM_SPORTSMANAGEMENT_ADMIN_SMQUOTE_EDIT'
+                : 'COM_SPORTSMANAGEMENT_ADMIN_SMQUOTE_ADD_NEW'),
+            'quote'
+        );
+        ToolbarHelper::apply('smquotetxt.apply');
+        ToolbarHelper::save('smquotetxt.save');
+        ToolbarHelper::cancel('smquotetxt.cancel', 'JTOOLBAR_CLOSE');
+
+        parent::display($tpl);
+    }
 }
