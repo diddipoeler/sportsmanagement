e81b7a913b22b47a5e1cc787d32a278d49e14366
diddipoeler <diddipoeler@gmx.de>
2026-08-31 20:58:13 +0200

    Bridge legacy jlextfederations view to Joomla 5/6 implementation

--- admin/views/jlextfederations/view.html.php
+++ admin/views/jlextfederations/view.html.php
@@ -1,50 +1,13 @@
 <?php
-/**
- * SportsManagement administrator federations list view.
- */
-defined('_JEXEC') or die('Restricted access');
+/** Legacy compatibility bridge for the native administrator Jlextfederations view. */
+\defined('_JEXEC') or die;
 
-use Joomla\CMS\HTML\HTMLHelper;
-use Joomla\CMS\Language\Text;
-use Joomla\CMS\Toolbar\ToolbarHelper;
+use Diddipoeler\Component\SportsManagement\Administrator\View\Jlextfederations\HtmlView;
 
-class sportsmanagementViewjlextfederations extends sportsmanagementView
-{
-    public function init()
-    {
-        $nation = [
-            HTMLHelper::_('select.option', '0', Text::_('COM_SPORTSMANAGEMENT_GLOBAL_SELECT_COUNTRY')),
-        ];
+if (!class_exists(HtmlView::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/View/Jlextfederations/HtmlView.php';
+}
 
-        if ($res = JSMCountries::getCountryOptions()) {
-            $nation = array_merge($nation, $res);
-            $this->search_nation = $res;
-        }
-
-        $this->lists = [
-            'nation' => $nation,
-            'nation2' => HTMLHelper::_(
-                'select.genericlist',
-                $nation,
-                'filter_search_nation',
-                'class="inputbox" style="width:140px;" onchange="this.form.submit();"',
-                'value',
-                'text',
-                $this->state->get('filter.search_nation')
-            ),
-        ];
-    }
-
-    protected function addToolbar()
-    {
-        $this->title = Text::_('COM_SPORTSMANAGEMENT_ADMIN_FEDERATIONS_TITLE');
-        $this->icon = 'federations';
-
-        ToolbarHelper::addNew('jlextfederation.add');
-        ToolbarHelper::editList('jlextfederation.edit');
-        ToolbarHelper::custom('jlextfederation.import', 'upload', 'upload', Text::_('JTOOLBAR_UPLOAD'), false);
-        ToolbarHelper::archiveList('jlextfederation.export', Text::_('JTOOLBAR_EXPORT'));
-
-        parent::addToolbar();
-    }
+if (!class_exists('sportsmanagementViewjlextfederations', false)) {
+    class_alias(HtmlView::class, 'sportsmanagementViewjlextfederations');
 }
