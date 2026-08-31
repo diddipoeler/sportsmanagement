08d7d55a7e5c9cc64b451380882f4154958e5ffe
diddipoeler <diddipoeler@gmx.de>
2026-08-31 22:22:59 +0200

    Make prediction groups administrator view native for Joomla 5/6

--- admin/src/View/Predictiongroups/HtmlView.php
+++ admin/src/View/Predictiongroups/HtmlView.php
@@ -3,11 +3,57 @@ namespace Diddipoeler\Component\SportsManagement\Administrator\View\Predictiongr
 
 \defined('_JEXEC') or die;
 
-use Diddipoeler\Component\SportsManagement\Administrator\Legacy\LegacyBootstrap;
+use Joomla\CMS\Factory;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\MVC\View\HtmlView as BaseHtmlView;
+use Joomla\CMS\Toolbar\ToolbarHelper;
 
-LegacyBootstrap::bootForView('predictiongroups');
-require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/views/predictiongroups/view.html.php';
+/** Native Joomla 5/6 administrator list view for prediction groups. */
+final class HtmlView extends BaseHtmlView
+{
+    public array $items = [];
+    public $pagination;
+    public $state;
+    public $filterForm;
+    public array $activeFilters = [];
+    public $user;
+    public string $sortDirection = 'ASC';
+    public string $sortColumn = 's.name';
 
-if (!class_exists(__NAMESPACE__ . '\\HtmlView', false)) {
-    class_alias('sportsmanagementViewpredictiongroups', __NAMESPACE__ . '\\HtmlView');
+    public function display($tpl = null)
+    {
+        $this->items = $this->get('Items') ?: [];
+        $this->pagination = $this->get('Pagination');
+        $this->state = $this->get('State');
+        $this->filterForm = $this->get('FilterForm');
+        $this->activeFilters = $this->get('ActiveFilters') ?: [];
+        $this->user = Factory::getApplication()->getIdentity();
+        $this->sortDirection = (string) $this->state->get('list.direction', 'ASC');
+        $this->sortColumn = (string) $this->state->get('list.ordering', 's.name');
+
+        if ($errors = $this->get('Errors')) {
+            throw new \RuntimeException(implode("\n", $errors), 500);
+        }
+
+        if (!$this->items) {
+            Factory::getApplication()->enqueueMessage(
+                Text::_('COM_SPORTSMANAGEMENT_ADMIN_PGAMES_NO_GROUPS'),
+                'warning'
+            );
+        }
+
+        $this->addToolbar();
+        parent::display($tpl);
+    }
+
+    private function addToolbar(): void
+    {
+        ToolbarHelper::title(Text::_('COM_SPORTSMANAGEMENT_ADMIN_PREDICTIONGROUPS_TITLE'), 'users');
+        ToolbarHelper::addNew('predictiongroup.add');
+        ToolbarHelper::editList('predictiongroup.edit');
+        ToolbarHelper::custom('predictiongroup.import', 'upload', 'upload', Text::_('JTOOLBAR_UPLOAD'), false);
+        ToolbarHelper::custom('predictiongroup.export', 'download', 'download', Text::_('JTOOLBAR_EXPORT'), false);
+        ToolbarHelper::deleteList('', 'predictiongroups.delete', 'JTOOLBAR_DELETE');
+        ToolbarHelper::checkin('predictiongroups.checkin');
+    }
 }
