153a008303b74820d944b810b2551e46a445f02e
diddipoeler <diddipoeler@gmx.de>
2026-08-31 20:58:21 +0200

    Bridge legacy jlextfederations layout to Joomla 5/6 template

--- admin/views/jlextfederations/tmpl/default.php
+++ admin/views/jlextfederations/tmpl/default.php
@@ -1,44 +1,5 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage jlextfederations
- * @file       default.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\HTML\HTMLHelper;
+/** Legacy compatibility bridge for the native Joomla 5/6 administrator Jlextfederations layout. */
+\defined('_JEXEC') or die;
 
-$templatesToLoad = array('footer', 'listheader');
-sportsmanagementHelper::addTemplatePaths($templatesToLoad, $this);
-?>
-    <script>
-
-        function searchPlayground(val, key) {
-            var f = $('adminForm');
-            if (f) {
-                f.elements['filter_search'].value = val;
-
-                f.submit();
-            }
-        }
-
-    </script>
-    <form action="<?php echo $this->request_url; ?>" method="post" name="adminForm" id="adminForm">
-
-		<?PHP
-		echo $this->loadTemplate('joomla_version');
-
-		?>
-        <input type="hidden" name="task" value=""/>
-        <input type="hidden" name="boxchecked" value="0"/>
-        <input type="hidden" name="filter_order" value="<?php echo $this->sortColumn; ?>"/>
-        <input type="hidden" name="filter_order_Dir" value="<?php echo $this->sortDirection; ?>"/>
-		<?php echo HTMLHelper::_('form.token') . "\n"; ?>
-		<?php echo $this->table_data_div; ?>
-    </form>
-<?PHP
-echo $this->loadTemplate('footer');
+require JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/tmpl/jlextfederations/default.php';
