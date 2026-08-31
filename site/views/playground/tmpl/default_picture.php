5e868f09b64e55fb750b3fb4140055fbe78d383d
diddipoeler <diddipoeler@gmx.de>
2026-08-31 20:55:41 +0200

    Bridge legacy playground picture to Joomla 5/6 template

--- site/views/playground/tmpl/default_picture.php
+++ site/views/playground/tmpl/default_picture.php
@@ -1,39 +1,5 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage playground
- * @file       default_picture.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-defined('_JEXEC') or die('Restricted access');
+/** Legacy compatibility bridge for the native Joomla 5/6 playground picture layout. */
+\defined('_JEXEC') or die;
 
-use Diddipoeler\Component\SportsManagement\Site\Helper\ModalImageHelper;
-use Joomla\CMS\Language\Text;
-
-if (empty($this->playground->picture)) {
-    return;
-}
-
-$this->notes = [Text::_('COM_SPORTSMANAGEMENT_PLAYGROUND_CLUB_PICTURE')];
-echo $this->loadTemplate('jsm_notes');
-
-$picture = COM_SPORTSMANAGEMENT_PICTURE_SERVER . ltrim((string) $this->playground->picture, '/');
-?>
-<div class="<?php echo $this->divclassrow; ?> table-responsive" id="playground_picture">
-    <?php
-    echo ModalImageHelper::render(
-        'playground' . (int) $this->playground->id,
-        $picture,
-        (string) ($this->playground->name ?? ''),
-        (int) ($this->config['playground_picture_width'] ?? 150),
-        '',
-        $this->modalwidth,
-        $this->modalheight,
-        (int) ($this->overallconfig['use_jquery_modal'] ?? 0)
-    );
-    ?>
-</div>
+require JPATH_SITE . '/components/com_sportsmanagement/tmpl/playground/default_picture.php';
