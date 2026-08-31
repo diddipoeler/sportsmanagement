1dd8b3fa534d13958912cfc18230be905d81a7c6
diddipoeler <diddipoeler@gmx.de>
2026-08-31 21:22:36 +0200

    Bridge legacy smquote view to Joomla 5/6

--- admin/views/smquote/view.html.php
+++ admin/views/smquote/view.html.php
@@ -1,64 +1,13 @@
 <?php
-/**
- *
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage smquote
- * @file       view.html.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
+/** Legacy compatibility bridge for the native administrator Smquote view. */
+\defined('_JEXEC') or die;
 
-defined('_JEXEC') or die('Restricted access');
+use Diddipoeler\Component\SportsManagement\Administrator\View\Smquote\HtmlView;
 
-use Joomla\CMS\Language\Text;
-use Joomla\CMS\Factory;
+if (!class_exists(HtmlView::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/View/Smquote/HtmlView.php';
+}
 
-/**
- * sportsmanagementViewsmquote
- *
- * @package
- * @author
- * @copyright diddi
- * @version   2014
- * @access    public
- */
-class sportsmanagementViewsmquote extends sportsmanagementView
-{
-
-
-	/**
-	 * sportsmanagementViewsmquote::init()
-	 *
-	 * @return
-	 */
-	public function init()
-	{
-
-		$this->item->name = $this->item->author;
-
-	}
-
-
-	/**
-	 * sportsmanagementViewsmquote::addToolBar()
-	 *
-	 * @return void
-	 */
-	protected function addToolBar()
-	{
-
-		$jinput = Factory::getApplication()->input;
-		$jinput->set('hidemainmenu', true);
-
-		$isNew      = $this->item->id ? $this->title = Text::_('COM_SPORTSMANAGEMENT_ADMIN_SMQUOTE_EDIT') : $this->title = Text::_('COM_SPORTSMANAGEMENT_ADMIN_SMQUOTE_ADD_NEW');
-		$this->icon = 'quote';
-
-		parent::addToolbar();
-
-	}
-
+if (!class_exists('sportsmanagementViewsmquote', false)) {
+    class_alias(HtmlView::class, 'sportsmanagementViewsmquote');
 }
