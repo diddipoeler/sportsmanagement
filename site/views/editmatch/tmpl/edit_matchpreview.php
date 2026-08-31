ec356716091e95ea0115fef9dbd5dd06aec3d880
diddipoeler <diddipoeler@gmx.de>
2026-08-31 22:12:54 +0200

    Bridge editmatch preview to Joomla 5/6 template

--- site/views/editmatch/tmpl/edit_matchpreview.php
+++ site/views/editmatch/tmpl/edit_matchpreview.php
@@ -1,33 +1,5 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage editmatch
- * @file       edit_matchpreview.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
+/** Legacy compatibility bridge for the native Joomla 5/6 edit-match preview layout. */
+\defined('_JEXEC') or die;
 
-defined('_JEXEC') or die('Restricted access');
-
-use Joomla\CMS\Language\Text;
-
-?>
-<fieldset class="adminform">
-    <legend><?php echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_MATCH_F_MP'); ?>
-    </legend>
-    <table class="admintable">
-		<?php
-
-		foreach ($this->form->getFieldset('matchpreview') as $field):
-			?>
-            <tr>
-                <td class="key"><?php echo $field->label; ?></td>
-                <td><?php echo $field->input; ?></td>
-            </tr>
-		<?php endforeach; ?>
-    </table>
-</fieldset>		
+require JPATH_SITE . '/components/com_sportsmanagement/tmpl/editmatch/edit_matchpreview.php';
