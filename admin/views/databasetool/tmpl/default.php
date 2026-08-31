1e80d7353cdf8fcd3d4e1f2d3a7948f194a4c7b2
diddipoeler <diddipoeler@gmx.de>
2026-08-31 19:08:33 +0200

    Bridge legacy database tool layout to Joomla 5/6 template

--- admin/views/databasetool/tmpl/default.php
+++ admin/views/databasetool/tmpl/default.php
@@ -1,16 +1,5 @@
 <?php
-defined('_JEXEC') or die('Restricted access');
+/** Legacy compatibility bridge for the native Joomla 5/6 database-tool layout. */
+\defined('_JEXEC') or die;
 
-use Joomla\CMS\HTML\HTMLHelper;
-use Joomla\CMS\Language\Text;
-use Joomla\CMS\Router\Route;
-?>
-<div class="alert alert-info">
-    <?php echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_DBTOOLS_TITLE'); ?>
-</div>
-<p>
-    <a class="btn btn-primary" href="<?php echo Route::_('index.php?option=com_sportsmanagement&view=databasetools'); ?>">
-        <?php echo Text::_('JTOOLBAR_BACK'); ?>
-    </a>
-</p>
-<?php echo HTMLHelper::_('form.token'); ?>
+require JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/tmpl/databasetool/default.php';
