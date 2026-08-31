08c4569e41f618495b1f267cc33fc1ac3192a26c
diddipoeler <diddipoeler@gmx.de>
2026-08-31 23:02:25 +0200

    Add native Joomla 5/6 prediction rounds list layout

---
+++ admin/tmpl/predictionrounds/default.php
@@ -0,0 +1,29 @@
+<?php
+/** Native Joomla 5/6 prediction rounds list layout. */
+\defined('_JEXEC') or die;
+
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Layout\LayoutHelper;
+use Joomla\CMS\Router\Route;
+
+HTMLHelper::_('behavior.multiselect');
+?>
+<form
+    action="<?php echo Route::_('index.php?option=com_sportsmanagement&view=predictionrounds'); ?>"
+    method="post"
+    id="adminForm"
+    name="adminForm"
+>
+    <?php if ($this->filterForm) : ?>
+        <?php echo LayoutHelper::render('joomla.searchtools.default', ['view' => $this]); ?>
+    <?php endif; ?>
+
+    <?php echo $this->loadTemplate('data'); ?>
+
+    <input type="hidden" name="task" value="">
+    <input type="hidden" name="boxchecked" value="0">
+    <input type="hidden" name="prediction_id" value="<?php echo (int) $this->prediction_id; ?>">
+    <input type="hidden" name="filter_order" value="<?php echo $this->escape($this->sortColumn); ?>">
+    <input type="hidden" name="filter_order_Dir" value="<?php echo $this->escape($this->sortDirection); ?>">
+    <?php echo HTMLHelper::_('form.token'); ?>
+</form>
