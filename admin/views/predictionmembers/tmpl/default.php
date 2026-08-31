61c4bb08ca79bf67ad3462b0bdcec4c96bbdc139
diddipoeler <diddipoeler@gmx.de>
2026-08-31 22:17:38 +0200

    Bridge legacy prediction members list to Joomla 5/6 template

--- admin/views/predictionmembers/tmpl/default.php
+++ admin/views/predictionmembers/tmpl/default.php
@@ -1,25 +1,5 @@
 <?php
-/** Prediction members list wrapper for Joomla 5/6. */
-defined('_JEXEC') or die('Restricted access');
+/** Legacy compatibility bridge for the native Joomla 5/6 prediction members list layout. */
+\defined('_JEXEC') or die;
 
-use Joomla\CMS\HTML\HTMLHelper;
-use Joomla\CMS\Layout\LayoutHelper;
-
-sportsmanagementHelper::addTemplatePaths(['footer'], $this);
-?>
-<form action="<?php echo $this->request_url; ?>" method="post" id="adminForm" name="adminForm">
-    <?php if ($this->filterForm) : ?>
-        <?php echo LayoutHelper::render('joomla.searchtools.default', ['view' => $this]); ?>
-    <?php endif; ?>
-
-    <?php echo $this->loadTemplate('data'); ?>
-
-    <input type="hidden" name="task" value="">
-    <input type="hidden" name="boxchecked" value="0">
-    <input type="hidden" name="prediction_id" value="<?php echo (int) $this->prediction_id; ?>">
-    <input type="hidden" name="filter_order" value="<?php echo $this->escape($this->sortColumn); ?>">
-    <input type="hidden" name="filter_order_Dir" value="<?php echo $this->escape($this->sortDirection); ?>">
-    <?php echo HTMLHelper::_('form.token'); ?>
-</form>
-
-<?php echo $this->loadTemplate('footer'); ?>
+require JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/tmpl/predictionmembers/default.php';
