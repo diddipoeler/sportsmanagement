b692539d4f1f27806bb5683022569da978b9a87f
diddipoeler <diddipoeler@gmx.de>
2026-08-31 17:46:21 +0200

    Use typed Joomla 5/6 seasons administrator view

--- admin/src/View/Seasons/HtmlView.php
+++ admin/src/View/Seasons/HtmlView.php
@@ -1,17 +1,52 @@
 <?php
 namespace Diddipoeler\Component\SportsManagement\Administrator\View\Seasons;
+
 \defined('_JEXEC') or die;
+
+use Diddipoeler\Component\SportsManagement\Administrator\Model\SeasonsModel;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\MVC\View\HtmlView as BaseHtmlView;
 use Joomla\CMS\Toolbar\ToolbarHelper;
+
+/** Native Joomla 5/6 administrator list view for seasons. */
 final class HtmlView extends BaseHtmlView
 {
-    public $items = []; public $pagination; public $state; public $filterForm; public $activeFilters = [];
+    public array $items = [];
+    public $pagination;
+    public $state;
+    public $filterForm;
+    public array $activeFilters = [];
+
     public function display($tpl = null)
     {
-        $this->items = $this->get('Items') ?: []; $this->pagination = $this->get('Pagination'); $this->state = $this->get('State'); $this->filterForm = $this->get('FilterForm'); $this->activeFilters = $this->get('ActiveFilters') ?: [];
-        if ($errors = $this->get('Errors')) { throw new \RuntimeException(implode("\n", $errors), 500); }
-        ToolbarHelper::title(Text::_('COM_SPORTSMANAGEMENT_ADMIN_SEASONS_TITLE'), 'list'); ToolbarHelper::addNew('season.add'); ToolbarHelper::editList('season.edit'); ToolbarHelper::publish('seasons.publish', 'JTOOLBAR_PUBLISH', true); ToolbarHelper::unpublish('seasons.unpublish', 'JTOOLBAR_UNPUBLISH', true); ToolbarHelper::checkin('seasons.checkin'); ToolbarHelper::trash('seasons.trash');
+        $model = $this->getModel();
+
+        if (!$model instanceof SeasonsModel) {
+            throw new \RuntimeException('Seasons model could not be loaded.', 500);
+        }
+
+        $this->items = $model->getItems() ?: [];
+        $this->pagination = $model->getPagination();
+        $this->state = $model->getState();
+        $this->filterForm = $model->getFilterForm();
+        $this->activeFilters = $model->getActiveFilters() ?: [];
+
+        if ($errors = $model->getErrors()) {
+            throw new \RuntimeException(implode("\n", $errors), 500);
+        }
+
+        $this->addToolbar();
         parent::display($tpl);
+    }
+
+    private function addToolbar(): void
+    {
+        ToolbarHelper::title(Text::_('COM_SPORTSMANAGEMENT_ADMIN_SEASONS_TITLE'), 'list');
+        ToolbarHelper::addNew('season.add');
+        ToolbarHelper::editList('season.edit');
+        ToolbarHelper::publish('seasons.publish', 'JTOOLBAR_PUBLISH', true);
+        ToolbarHelper::unpublish('seasons.unpublish', 'JTOOLBAR_UNPUBLISH', true);
+        ToolbarHelper::checkin('seasons.checkin');
+        ToolbarHelper::trash('seasons.trash');
     }
 }
