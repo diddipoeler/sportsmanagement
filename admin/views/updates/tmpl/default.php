ff692f05a1350006bb921c84855df4075f7eeb10
diddipoeler <diddipoeler@gmx.de>
2026-08-31 19:38:10 +0200

    Bridge legacy updates layout to Joomla 5/6 template

--- admin/views/updates/tmpl/default.php
+++ admin/views/updates/tmpl/default.php
@@ -1,19 +1,5 @@
 <?php
-/** Joomla 5/6 administrator updates layout. */
-defined('_JEXEC') or die('Restricted access');
+/** Legacy compatibility bridge for the native Joomla 5/6 administrator updates layout. */
+\defined('_JEXEC') or die;
 
-use Joomla\CMS\HTML\HTMLHelper;
-
-$templatesToLoad = ['footer'];
-sportsmanagementHelper::addTemplatePaths($templatesToLoad, $this);
-?>
-<form action="<?php echo htmlspecialchars($this->request_url, ENT_QUOTES, 'UTF-8'); ?>"
-      method="post" id="adminForm" name="adminForm">
-    <?php echo $this->loadTemplate('data'); ?>
-    <input type="hidden" name="view" value="updates">
-    <input type="hidden" name="task" value="">
-    <input type="hidden" name="filter_order" value="<?php echo htmlspecialchars((string) $this->sortColumn, ENT_QUOTES, 'UTF-8'); ?>">
-    <input type="hidden" name="filter_order_Dir" value="<?php echo htmlspecialchars((string) $this->sortDirection, ENT_QUOTES, 'UTF-8'); ?>">
-    <?php echo HTMLHelper::_('form.token'); ?>
-</form>
-<?php echo $this->loadTemplate('footer'); ?>
+require JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/tmpl/updates/default.php';
