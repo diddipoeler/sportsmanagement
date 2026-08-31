ce4bacbd125ee00738420a48140935196e964990
diddipoeler <diddipoeler@gmx.de>
2026-08-31 22:24:38 +0200

    Bridge legacy prediction group edit view to Joomla 5/6 MVC

--- admin/views/predictiongroup/view.html.php
+++ admin/views/predictiongroup/view.html.php
@@ -1,71 +1,13 @@
 <?php
-/**
- *
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage predictiongroup
- * @file       view.html.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
+/** Legacy compatibility bridge for the native Joomla 5/6 prediction group edit view. */
+\defined('_JEXEC') or die;
 
-defined('_JEXEC') or die('Restricted access');
+use Diddipoeler\Component\SportsManagement\Administrator\View\Predictiongroup\HtmlView;
 
-use Joomla\CMS\Language\Text;
-use Joomla\CMS\Factory;
-use Joomla\CMS\Log\Log;
+if (!class_exists(HtmlView::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/View/Predictiongroup/HtmlView.php';
+}
 
-/**
- * sportsmanagementViewpredictiongroup
- *
- * @package
- * @author    diddi
- * @copyright 2014
- * @version   $Id$
- * @access    public
- */
-class sportsmanagementViewpredictiongroup extends sportsmanagementView
-{
-
-
-	/**
-	 * sportsmanagementViewpredictiongroup::init()
-	 *
-	 * @return
-	 */
-	public function init()
-	{
-
-		// Check for errors.
-		if (count($errors = $this->get('Errors')))
-		{
-			Log::add(implode('<br />', $errors));
-
-			return false;
-		}
-
-	}
-
-
-	/**
-	 * Add the page title and toolbar.
-	 *
-	 * @since 1.7
-	 */
-	protected function addToolbar()
-	{
-
-		$jinput = Factory::getApplication()->input;
-		$jinput->set('hidemainmenu', true);
-
-		$isNew      = $this->item->id ? $this->title = Text::_('COM_SPORTSMANAGEMENT_PREDICTION_GROUP_EDIT') : $this->title = Text::_('COM_SPORTSMANAGEMENT_ADMIN_PREDICTION_GROUP_NEW');
-		$this->icon = 'pgame';
-
-		parent::addToolbar();
-	}
-
-
+if (!class_exists('sportsmanagementViewpredictiongroup', false)) {
+    class_alias(HtmlView::class, 'sportsmanagementViewpredictiongroup');
 }
