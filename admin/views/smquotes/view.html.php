292b66b5cfe6b92fdeb9c90dacb2b908ca1a0516
diddipoeler <diddipoeler@gmx.de>
2026-08-31 18:44:08 +0200

    Bridge legacy smquotes view to Joomla 5/6 MVC

--- admin/views/smquotes/view.html.php
+++ admin/views/smquotes/view.html.php
@@ -1,40 +1,13 @@
 <?php
-/**
- * SportsManagement administrator quotes list view.
- */
-defined('_JEXEC') or die('Restricted access');
+/** Legacy compatibility bridge for the native administrator Smquotes view. */
+\defined('_JEXEC') or die;
 
-use Joomla\CMS\Language\Text;
-use Joomla\CMS\Log\Log;
-use Joomla\CMS\Toolbar\Toolbar;
-use Joomla\CMS\Toolbar\ToolbarHelper;
+use Diddipoeler\Component\SportsManagement\Administrator\View\Smquotes\HtmlView;
 
-class sportsmanagementViewsmquotes extends sportsmanagementView
-{
-    public $filterForm;
-    public $activeFilters;
+if (!class_exists(HtmlView::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/View/Smquotes/HtmlView.php';
+}
 
-    public function init()
-    {
-        try {
-            $this->filterForm = $this->model->getFilterForm();
-            $this->activeFilters = $this->model->getActiveFilters();
-        } catch (\Throwable $e) {
-            Log::add(__METHOD__ . ' ' . $e->getCode(), Log::ERROR, 'jsmerror');
-            Log::add(__METHOD__ . ' ' . $e->getMessage(), Log::ERROR, 'jsmerror');
-        }
-    }
-
-    protected function addToolbar()
-    {
-        $this->title = Text::_('COM_SPORTSMANAGEMENT_ADMIN_QUOTES_TITLE');
-        ToolbarHelper::addNew('smquote.add');
-        ToolbarHelper::editList('smquote.edit');
-        ToolbarHelper::custom('smquote.import', 'upload', 'upload', Text::_('JTOOLBAR_UPLOAD'), false);
-        ToolbarHelper::custom('smquotes.edittxt', 'featured.png', 'featured_f2.png', Text::_('JTOOLBAR_EDIT'), false);
-        $bar = Toolbar::getInstance('toolbar');
-        $bar->appendButton('Link', 'info', 'Kategorie', 'index.php?option=com_categories&extension=com_sportsmanagement');
-        ToolbarHelper::archiveList('smquote.export', Text::_('JTOOLBAR_EXPORT'));
-        parent::addToolbar();
-    }
+if (!class_exists('sportsmanagementViewsmquotes', false)) {
+    class_alias(HtmlView::class, 'sportsmanagementViewsmquotes');
 }
