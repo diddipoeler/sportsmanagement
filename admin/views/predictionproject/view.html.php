b7264f832f0f170602bff347c24a166e4a568667
diddipoeler <diddipoeler@gmx.de>
2026-08-31 22:57:51 +0200

    Bridge legacy prediction project view to Joomla 5/6 MVC

--- admin/views/predictionproject/view.html.php
+++ admin/views/predictionproject/view.html.php
@@ -1,55 +1,13 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage predictionproject
- * @file       view.html.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-defined('_JEXEC') or die('Restricted access');
+/** Legacy compatibility bridge for the native Joomla 5/6 prediction project view. */
+\defined('_JEXEC') or die;
 
-use Joomla\CMS\Language\Text;
-use Joomla\CMS\Log\Log;
+use Diddipoeler\Component\SportsManagement\Administrator\View\Predictionproject\HtmlView;
 
-/**
- * sportsmanagementViewpredictionproject
- *
- * @package
- * @author    diddi
- * @copyright 2014
- * @version   $Id$
- * @access    public
- */
-class sportsmanagementViewpredictionproject extends sportsmanagementView
-{
-    /**
-     * sportsmanagementViewpredictionproject::init()
-     *
-     * @return
-     */
-    public function init()
-    {
-        $this->form = $this->get('Form');
-        $this->item = $this->get('Item');
+if (!class_exists(HtmlView::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/View/Predictionproject/HtmlView.php';
+}
 
-        if (count($errors = $this->get('Errors'))) {
-            Log::add(implode('<br />', $errors));
-
-            return false;
-        }
-
-        $this->item->name = '';
-        $this->app->setUserState("$this->option.pid", $this->item->project_id);
-
-        switch ($this->getLayout()) {
-            case 'edit';
-            case 'edit_3';
-            case 'edit_4';
-                $this->setLayout('edit_3');
-                break;
-        }
-    }
+if (!class_exists('sportsmanagementViewpredictionproject', false)) {
+    class_alias(HtmlView::class, 'sportsmanagementViewpredictionproject');
 }
