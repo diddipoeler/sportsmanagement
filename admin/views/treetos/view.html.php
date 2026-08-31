954a460a19ad75e7e4f8993daed567aebd77d8ee
diddipoeler <diddipoeler@gmx.de>
2026-08-31 18:57:12 +0200

    Bridge legacy treetos view to Joomla 5/6 MVC

--- admin/views/treetos/view.html.php
+++ admin/views/treetos/view.html.php
@@ -1,58 +1,13 @@
 <?php
-/** SportsManagement administrator tournament trees list view. */
-defined('_JEXEC') or die('Restricted access');
+/** Legacy compatibility bridge for the native Joomla 5/6 tournament trees view. */
+\defined('_JEXEC') or die;
 
-use Joomla\CMS\HTML\HTMLHelper;
-use Joomla\CMS\Language\Text;
-use Joomla\CMS\Toolbar\ToolbarHelper;
+use Diddipoeler\Component\SportsManagement\Administrator\View\Treetos\HtmlView;
 
-class sportsmanagementViewTreetos extends sportsmanagementView
-{
-    public function init()
-    {
-        $this->project_id = $this->model->getProjectId()
-            ?: (int) $this->app->getUserState("$this->option.pid", 0);
-        $this->projectws = $this->model->getProject();
-        $this->division = $this->app->getUserStateFromRequest(
-            $this->option . 'tt_division',
-            'division',
-            '',
-            'string'
-        );
+if (!class_exists(HtmlView::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/View/Treetos/HtmlView.php';
+}
 
-        $divisions = [
-            HTMLHelper::_('select.option', 0, Text::_('COM_SPORTSMANAGEMENT_GLOBAL_SELECT_DIVISION')),
-        ];
-        $divisions = array_merge($divisions, $this->model->getDivisions());
-        $this->lists = ['divisions' => $divisions];
-
-        if (!$this->projectws) {
-            $this->app->enqueueMessage(Text::_('JLIB_APPLICATION_ERROR_COMPONENT_NOT_FOUND'), 'error');
-            $this->projectws = (object) [
-                'id' => $this->project_id,
-                'name' => '',
-                'project_type' => '',
-            ];
-        }
-    }
-
-    protected function addToolbar()
-    {
-        ToolbarHelper::title(Text::_('COM_SPORTSMANAGEMENT_ADMIN_TREETOS_TITLE'), 'Tree');
-        ToolbarHelper::back(
-            'JPREV',
-            'index.php?option=com_sportsmanagement&view=project&layout=panel&id=' . (int) $this->project_id
-        );
-        ToolbarHelper::apply('treeto.saveshort');
-        ToolbarHelper::publishList('treetos.publish');
-        ToolbarHelper::unpublishList('treetos.unpublish');
-        ToolbarHelper::divider();
-        ToolbarHelper::addNew('treetos.save');
-        ToolbarHelper::deleteList(
-            Text::_('COM_SPORTSMANAGEMENT_ADMIN_TREETOS_WARNING'),
-            'treeto.remove'
-        );
-        ToolbarHelper::divider();
-        parent::addToolbar();
-    }
+if (!class_exists('sportsmanagementViewTreetos', false)) {
+    class_alias(HtmlView::class, 'sportsmanagementViewTreetos');
 }
