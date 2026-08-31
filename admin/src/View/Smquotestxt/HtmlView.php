2623a1afff7455668e2dbff8a980e1230edf72ec
diddipoeler <diddipoeler@gmx.de>
2026-08-31 22:00:12 +0200

    Make smquotestxt a native Joomla 5/6 view

--- admin/src/View/Smquotestxt/HtmlView.php
+++ admin/src/View/Smquotestxt/HtmlView.php
@@ -3,11 +3,31 @@ namespace Diddipoeler\Component\SportsManagement\Administrator\View\Smquotestxt;
 
 \defined('_JEXEC') or die;
 
-use Diddipoeler\Component\SportsManagement\Administrator\Legacy\LegacyBootstrap;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\MVC\View\HtmlView as BaseHtmlView;
+use Joomla\CMS\Router\Route;
+use Joomla\CMS\Toolbar\ToolbarHelper;
 
-LegacyBootstrap::bootForView('smquotestxt');
-require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/views/smquotestxt/view.html.php';
+/** Native Joomla 5/6 administrator list view for random-quote source files. */
+final class HtmlView extends BaseHtmlView
+{
+    public array $files = [];
 
-if (!class_exists(__NAMESPACE__ . '\\HtmlView', false)) {
-    class_alias('sportsmanagementViewsmquotestxt', __NAMESPACE__ . '\\HtmlView');
+    public function display($tpl = null): void
+    {
+        $model = $this->getModel();
+        $this->files = method_exists($model, 'getTXTFiles') ? $model->getTXTFiles() : [];
+
+        if ($errors = $model->getErrors()) {
+            throw new \RuntimeException(implode("\n", $errors), 500);
+        }
+
+        ToolbarHelper::title(Text::_('COM_SPORTSMANAGEMENT_ADMIN_TXT_EDITORS'), 'quote');
+        ToolbarHelper::back(
+            Text::_('JPREV'),
+            Route::_('index.php?option=com_sportsmanagement&view=smquotes')
+        );
+
+        parent::display($tpl);
+    }
 }
