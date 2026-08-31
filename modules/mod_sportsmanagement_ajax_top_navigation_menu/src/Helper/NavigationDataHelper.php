ba6d15de7888965108209a48245e690cad031bb5
diddipoeler <diddipoeler@gmx.de>
2026-08-31 22:53:58 +0200

    Add namespaced Joomla 5/6 AJAX navigation data helper

---
+++ modules/mod_sportsmanagement_ajax_top_navigation_menu/src/Helper/NavigationDataHelper.php
@@ -0,0 +1,16 @@
+<?php
+namespace Diddipoeler\Module\SportsManagementAjaxTopNavigationMenu\Site\Helper;
+
+\defined('_JEXEC') or die;
+
+require_once __DIR__ . '/LegacyNavigationDataHelper.php';
+
+/**
+ * Namespaced Joomla 5/6 entry point for the AJAX navigation query and routing data.
+ *
+ * The inherited implementation remains byte-compatible with legacy callers while
+ * native module code can depend exclusively on the module namespace.
+ */
+final class NavigationDataHelper extends \modSportsmanagementAjaxTopNavigationMenuHelper
+{
+}
