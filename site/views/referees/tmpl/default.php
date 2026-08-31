68799d16547c0c7063197ae30d42b708039e9240
diddipoeler <diddipoeler@gmx.de>
2026-08-31 20:57:34 +0200

    Bridge legacy referees layout to Joomla 5/6 template

--- site/views/referees/tmpl/default.php
+++ site/views/referees/tmpl/default.php
@@ -1,43 +1,5 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage referees
- * @file       default.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-defined('_JEXEC') or die('Restricted access');
+/** Legacy compatibility bridge for the native Joomla 5/6 referees layout. */
+\defined('_JEXEC') or die;
 
-$templatesToLoad = array('globalviews');
-sportsmanagementHelper::addTemplatePaths($templatesToLoad, $this);
-
-if (!isset($this->config['show_referees']))
-{
-	$this->config['show_referees'] = 1;
-}
-?>
-<div class="<?php echo $this->divclasscontainer; ?>" id="referees">
-	<?php
-	if (COM_SPORTSMANAGEMENT_SHOW_DEBUG_INFO)
-	{
-		echo $this->loadTemplate('debug');
-	}
-
-	echo $this->loadTemplate('projectheading');
-
-	if ($this->config['show_sectionheader'])
-	{
-		echo $this->loadTemplate('sectionheader');
-	}
-
-	if ($this->config['show_referees'])
-	{
-		echo $this->loadTemplate('referees');
-	}
-
-	echo $this->loadTemplate('jsminfo');
-	?>
-</div>
+require JPATH_SITE . '/components/com_sportsmanagement/tmpl/referees/default.php';
