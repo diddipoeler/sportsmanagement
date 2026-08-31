947bbb7d704d1d04e206b62d2037e80a56112e4c
diddipoeler <diddipoeler@gmx.de>
2026-08-31 18:21:03 +0200

    Use Joomla 5/6 dispatcher for playground plan module

--- modules/mod_sportsmanagement_playgroundplan/mod_sportsmanagement_playgroundplan.php
+++ modules/mod_sportsmanagement_playgroundplan/mod_sportsmanagement_playgroundplan.php
@@ -1,33 +1,7 @@
 <?php
-/** Joomla 5/6 compatibility entry point for mod_sportsmanagement_playgroundplan. */
-defined('_JEXEC') or die;
-
-use Diddipoeler\Module\SportsManagementPlaygroundPlan\Site\Helper\PlaygroundPlanHelper;
-use Joomla\CMS\Factory;
-use Joomla\CMS\Helper\ModuleHelper;
-
-$app = Factory::getApplication();
-$app->getLanguage()->load('com_sportsmanagement', JPATH_SITE, null, true);
-
-if (!class_exists(PlaygroundPlanHelper::class)) {
-    require_once __DIR__ . '/src/Helper/PlaygroundPlanHelper.php';
-}
-
-$list = (new PlaygroundPlanHelper())->getData($params, $app, $module);
-$wam = $app->getDocument()->getWebAssetManager();
-$wam->registerAndUseStyle(
-    'mod_sportsmanagement_playgroundplan',
-    'modules/mod_sportsmanagement_playgroundplan/css/mod_sportsmanagement_playgroundplan.css'
-);
-
-if ((int) $params->get('mode', 0) === 0) {
-    $wam->registerAndUseScript(
-        'mod_sportsmanagement_playgroundplan.ticker',
-        'modules/mod_sportsmanagement_playgroundplan/js/ticker.js'
-    );
-}
-
-require ModuleHelper::getLayoutPath(
-    $module->module,
-    (string) $params->get('layout', 'default')
-);
+/**
+ * SportsManagement Joomla 5/6 module entry compatibility file.
+ *
+ * The active module is booted by services/provider.php and the namespaced dispatcher.
+ */
+\defined('_JEXEC') or die;
