4bdb2b2109cbe10e2dbb7307c12e1ce68fe41532
diddipoeler <diddipoeler@gmx.de>
2026-08-31 19:38:02 +0200

    Bridge legacy updates view to Joomla 5/6 MVC

--- admin/views/updates/view.html.php
+++ admin/views/updates/view.html.php
@@ -1,42 +1,13 @@
 <?php
-/** Joomla 5/6 administrator updates view. */
-defined('_JEXEC') or die('Restricted access');
+/** Legacy compatibility bridge for the native Joomla 5/6 administrator Updates view. */
+\defined('_JEXEC') or die;
 
-use Joomla\CMS\Language\Text;
-use Joomla\CMS\Uri\Uri;
+use Diddipoeler\Component\SportsManagement\Administrator\View\Updates\HtmlView;
 
-class sportsmanagementViewUpdates extends sportsmanagementView
-{
-    public function init()
-    {
-        $this->app->setUserState($this->option . 'update_part', 0);
-        $filterOrder = $this->app->getUserStateFromRequest(
-            $this->option . 'updates_filter_order',
-            'filter_order',
-            'dates',
-            'cmd'
-        );
-        $filterOrderDirection = $this->app->getUserStateFromRequest(
-            $this->option . 'updates_filter_order_Dir',
-            'filter_order_Dir',
-            '',
-            'word'
-        );
+if (!class_exists(HtmlView::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/View/Updates/HtmlView.php';
+}
 
-        $this->versions = $this->model->getVersions();
-        $this->versionhistory = $this->model->getVersionHistory();
-        $this->updateFiles = $this->model->loadUpdateFiles();
-        $this->request_url = Uri::getInstance()->toString();
-        $this->lists = [
-            'order_Dir' => $filterOrderDirection,
-            'order' => $filterOrder,
-        ];
-    }
-
-    protected function addToolbar()
-    {
-        $this->title = Text::_('COM_SPORTSMANAGEMENT_ADMIN_UPDATES_TITLE');
-        $this->icon = 'updates';
-        parent::addToolbar();
-    }
+if (!class_exists('sportsmanagementViewUpdates', false)) {
+    class_alias(HtmlView::class, 'sportsmanagementViewUpdates');
 }
