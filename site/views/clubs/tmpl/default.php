22015f2a1625c34c956ec3f661b31a09c156f8e8
diddipoeler <diddipoeler@gmx.de>
2026-08-31 19:00:46 +0200

    Bridge legacy clubs view to Joomla 5/6 template

--- site/views/clubs/tmpl/default.php
+++ site/views/clubs/tmpl/default.php
@@ -1,33 +1,5 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage clubs
- * @file       default.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-defined('_JEXEC') or die('Restricted access');
+/** Legacy compatibility bridge for the native Joomla 5/6 clubs layout. */
+\defined('_JEXEC') or die;
 
-$templatesToLoad = array('globalviews');
-sportsmanagementHelper::addTemplatePaths($templatesToLoad, $this);
-?>
-<div class="<?php echo $this->divclasscontainer; ?>" id="clubs">
-	<?php
-	echo $this->loadTemplate('projectheading');
-
-	if ($this->config['show_sectionheader'])
-	{
-		echo $this->loadTemplate('sectionheader');
-	}
-
-	if ($this->config['show_clubs'])
-	{
-		echo $this->loadTemplate('clubs');
-	}
-
-	echo $this->loadTemplate('jsminfo');
-	?>
-</div>
+require JPATH_SITE . '/components/com_sportsmanagement/tmpl/clubs/default.php';
