d9b21b34023f1aa0b984a7f034b18287da854d0a
diddipoeler <diddipoeler@gmx.de>
2026-08-31 22:24:22 +0200

    Make prediction group edit view native for Joomla 5/6

--- admin/src/View/Predictiongroup/HtmlView.php
+++ admin/src/View/Predictiongroup/HtmlView.php
@@ -3,11 +3,52 @@ namespace Diddipoeler\Component\SportsManagement\Administrator\View\Predictiongr
 
 \defined('_JEXEC') or die;
 
-use Diddipoeler\Component\SportsManagement\Administrator\Legacy\LegacyBootstrap;
+use Joomla\CMS\Factory;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\MVC\View\HtmlView as BaseHtmlView;
+use Joomla\CMS\Toolbar\ToolbarHelper;
 
-LegacyBootstrap::bootForView('predictiongroup');
-require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/views/predictiongroup/view.html.php';
+/** Native Joomla 5/6 administrator edit view for a prediction group. */
+final class HtmlView extends BaseHtmlView
+{
+    public $form;
+    public $item;
+    public $state;
 
-if (!class_exists(__NAMESPACE__ . '\\HtmlView', false)) {
-    class_alias('sportsmanagementViewpredictiongroup', __NAMESPACE__ . '\\HtmlView');
+    public function display($tpl = null)
+    {
+        $app = Factory::getApplication();
+        $app->getInput()->set('hidemainmenu', true);
+
+        $this->form = $this->get('Form');
+        $this->item = $this->get('Item');
+        $this->state = $this->get('State');
+
+        if ($errors = $this->get('Errors')) {
+            throw new \RuntimeException(implode("\n", $errors), 500);
+        }
+
+        if (!$this->form || !$this->item) {
+            throw new \RuntimeException('Prediction group form could not be loaded.', 500);
+        }
+
+        $this->getDocument()->getWebAssetManager()->useScript('form.validate');
+        $this->addToolbar();
+        parent::display($tpl);
+    }
+
+    private function addToolbar(): void
+    {
+        $isNew = empty($this->item->id);
+        ToolbarHelper::title(
+            Text::_($isNew
+                ? 'COM_SPORTSMANAGEMENT_ADMIN_PREDICTION_GROUP_NEW'
+                : 'COM_SPORTSMANAGEMENT_PREDICTION_GROUP_EDIT'),
+            'users'
+        );
+        ToolbarHelper::apply('predictiongroup.apply');
+        ToolbarHelper::save('predictiongroup.save');
+        ToolbarHelper::save2new('predictiongroup.save2new');
+        ToolbarHelper::cancel('predictiongroup.cancel', $isNew ? 'JTOOLBAR_CANCEL' : 'JTOOLBAR_CLOSE');
+    }
 }
