fad4854a2b2f8d6ed18238956381e5335f061a73
diddipoeler <diddipoeler@gmx.de>
2026-09-01 09:20:19 +0200

    Bridge legacy tournament tree match assignment controls to Joomla 5/6 template

--- admin/views/treetomatchs/tmpl/editlist_data.php
+++ admin/views/treetomatchs/tmpl/editlist_data.php
@@ -1,39 +1,4 @@
 <?php
-/** Tournament-tree match assignment controls for Joomla 5/6. */
-defined('_JEXEC') or die('Restricted access');
-
-use Joomla\CMS\Language\Text;
-?>
-<legend>
-    <?php echo Text::sprintf(
-        'COM_SPORTSMANAGEMENT_ADMIN_TREETOMATCH_ASSIGN_TITLE',
-        '<i>' . htmlspecialchars((string) $this->projectws->name) . '</i>'
-    ); ?>
-</legend>
-
-<div class="row g-3 align-items-center">
-    <div class="col-md-5">
-        <label class="form-label fw-bold" for="matcheslist">
-            <?php echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_TREETOMATCH_ASSIGN_AVAIL_MATCHES'); ?>
-        </label>
-        <?php echo $this->lists['matches']; ?>
-    </div>
-
-    <div class="col-md-2 d-flex flex-column justify-content-center align-items-center gap-2">
-        <button type="button" class="btn btn-outline-secondary"
-                onclick="move_list_items('matcheslist','node_matcheslist');">
-            &gt;&gt;
-        </button>
-        <button type="button" class="btn btn-outline-secondary"
-                onclick="move_list_items('node_matcheslist','matcheslist');">
-            &lt;&lt;
-        </button>
-    </div>
-
-    <div class="col-md-5">
-        <label class="form-label fw-bold" for="node_matcheslist">
-            <?php echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_TREETOMATCH_ASSIGN_NODE_MATCHES'); ?>
-        </label>
-        <?php echo $this->lists['node_matches']; ?>
-    </div>
-</div>
+/** Legacy compatibility bridge for the native Joomla 5/6 tournament-tree match assignment controls. */
+\defined('_JEXEC') or die;
+require JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/tmpl/treetomatchs/editlist_data.php';
