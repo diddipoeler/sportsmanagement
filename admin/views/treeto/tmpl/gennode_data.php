7e8be5a349e9ce057c75e45f8519180c732eb720
diddipoeler <diddipoeler@gmx.de>
2026-09-01 08:47:22 +0200

    Bridge legacy tournament tree generation fields to Joomla 5/6 template

--- admin/views/treeto/tmpl/gennode_data.php
+++ admin/views/treeto/tmpl/gennode_data.php
@@ -1,41 +1,5 @@
 <?php
-/**
- *
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage treeto
- * @file       gennode_data.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
+/** Legacy compatibility bridge for the native Joomla 5/6 tournament tree generation fields. */
+\defined('_JEXEC') or die;
 
-defined('_JEXEC') or die('Restricted access');
-
-use Joomla\CMS\Language\Text;
-use Joomla\CMS\HTML\HTMLHelper;
-
-
-?>
-
-<div>
-    <fieldset class="adminform">
-        <legend><?php echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_TREETOS_TITLE_GENERATENODE'); ?></legend>
-        <ul class="adminformlist">
-			<?php foreach ($this->form->getFieldset('generate') as $field)
-				:
-				?>
-                <li><?php echo $field->label;
-					echo $field->input; ?></li>
-			<?php endforeach; ?>
-            <li>
-                <input type="submit" value="<?php echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_TREETO_GENERATE'); ?>"/>
-            </li>
-        </ul>
-    </fieldset>
-</div>
-
-
-
+require JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/tmpl/treeto/gennode_data.php';
