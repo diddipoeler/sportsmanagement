f590ccfdd68b26913ab6d6c9a0171b7d469f8a7a
diddipoeler <diddipoeler@gmx.de>
2026-09-01 08:42:13 +0200

    Bridge legacy project templates layout to Joomla 5/6 template

--- admin/views/templates/tmpl/default.php
+++ admin/views/templates/tmpl/default.php
@@ -1,22 +1,5 @@
 <?php
-/** SportsManagement project templates list wrapper. */
-defined('_JEXEC') or die('Restricted access');
+/** Legacy compatibility bridge for the native Joomla 5/6 project templates layout. */
+\defined('_JEXEC') or die;
 
-use Joomla\CMS\HTML\HTMLHelper;
-
-$templatesToLoad = ['footer', 'listheader'];
-sportsmanagementHelper::addTemplatePaths($templatesToLoad, $this);
-?>
-<form action="<?php echo $this->escape($this->request_url); ?>" method="post" id="adminForm" name="adminForm">
-    <?php
-    echo $this->loadTemplate('joomla_version');
-    echo $this->loadTemplate('data');
-    ?>
-    <input type="hidden" name="task" value="">
-    <input type="hidden" name="boxchecked" value="0">
-    <input type="hidden" name="filter_order_Dir" value="<?php echo $this->escape($this->sortDirection); ?>">
-    <input type="hidden" name="filter_order" value="<?php echo $this->escape($this->sortColumn); ?>">
-    <input type="hidden" name="pid" value="<?php echo (int) $this->projectws->id; ?>">
-    <?php echo HTMLHelper::_('form.token'); ?>
-</form>
-<?php echo $this->loadTemplate('footer'); ?>
+require JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/tmpl/templates/default.php';
