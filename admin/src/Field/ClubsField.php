e6f02ed79c669e073577522b57cae9883bbbee7a
diddipoeler <diddipoeler@gmx.de>
2026-09-01 14:21:51 +0200

    Modernize administrator club fields

--- admin/src/Field/ClubsField.php
+++ admin/src/Field/ClubsField.php
@@ -4,17 +4,22 @@ namespace Diddipoeler\Component\SportsManagement\Administrator\Field;
 \defined('_JEXEC') or die;
 
 use Joomla\CMS\Component\ComponentHelper;
-use Joomla\CMS\Form\FormField;
-use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
 
-final class ClubsField extends FormField
+final class ClubsField extends SportsManagementListField
 {
-    use SportsManagementDatabaseTrait;
-
     protected $type = 'clubs';
 
-    protected function getInput(): string
+    public function setup(\SimpleXMLElement $element, $value, $group = null)
+    {
+        $element['multiple'] = 'true';
+        $element['size'] = '10';
+        $element['class'] = 'inputbox form-select';
+
+        return parent::setup($element, $value, $group);
+    }
+
+    protected function getOptions(): array
     {
         $databaseTable = preg_replace(
             '/[^A-Za-z0-9_]/',
@@ -26,32 +31,27 @@ final class ClubsField extends FormField
         $db = $this->getSportsManagementDatabase();
         $query = $db->getQuery(true)
             ->select([
-                $db->quoteName('c.id'),
-                $db->quoteName('c.name'),
+                $db->quoteName('c.id', 'value'),
+                $db->quoteName('c.name', 'text'),
             ])
             ->from($db->quoteName('#__' . $databaseTable . '_club', 'c'))
             ->order($db->quoteName('c.name'));
         $db->setQuery($query);
 
-        $options = [HTMLHelper::_('select.option', '', Text::_('COM_SPORTSMANAGEMENT_GLOBAL_SELECT'))];
+        $options = [
+            (object) [
+                'value' => '',
+                'text' => Text::_('COM_SPORTSMANAGEMENT_GLOBAL_SELECT'),
+            ],
+        ];
 
-        foreach ($db->loadObjectList() ?: [] as $club) {
-            $options[] = HTMLHelper::_(
-                'select.option',
-                $club->id,
-                "\u{00A0}" . $club->name . ' (' . $club->id . ')'
-            );
+        foreach ($db->loadObjectList() ?: [] as $item) {
+            $options[] = (object) [
+                'value' => (string) $item->value,
+                'text' => "\u{00A0}" . (string) $item->text . ' (' . (int) $item->value . ')',
+            ];
         }
 
-        return HTMLHelper::_(
-            'select.genericlist',
-            $options,
-            $this->name,
-            'class="inputbox form-select" multiple="multiple" size="10"',
-            'value',
-            'text',
-            $this->value,
-            $this->id
-        );
+        return array_merge($options, parent::getOptions());
     }
 }
