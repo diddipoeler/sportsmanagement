f0660cb007d46c791df1a225fa74abe8d32e21c0
diddipoeler <diddipoeler@gmx.de>
2026-09-01 08:14:56 +0200

    Bridge legacy singular XML import layout to Joomla 5/6 template

--- admin/views/jlxmlimport/tmpl/default.php
+++ admin/views/jlxmlimport/tmpl/default.php
@@ -1,20 +1,5 @@
 <?php
-/**
- *
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage jlxmlimport
- * @file       default.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
+/** Legacy compatibility bridge for the native Joomla 5/6 singular XML import entry layout. */
+\defined('_JEXEC') or die;
 
-defined('_JEXEC') or die('Restricted access');
-
-use Joomla\CMS\HTML\HTMLHelper;
-
-$model = $this->getModel('jlxmlimport');
-echo $model->getXml;
+require JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/tmpl/jlxmlimport/default.php';
