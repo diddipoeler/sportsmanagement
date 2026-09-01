706d14dced25db57844444543f4ebafdfbb7a7b1
diddipoeler <diddipoeler@gmx.de>
2026-09-01 08:47:31 +0200

    Bridge legacy tournament tree edit layout to Joomla 5/6 template

---
+++ admin/views/treeto/tmpl/edit.php
@@ -0,0 +1,5 @@
+<?php
+/** Legacy compatibility bridge for the native Joomla 5/6 tournament tree edit layout. */
+\defined('_JEXEC') or die;
+
+require JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/tmpl/treeto/edit.php';
