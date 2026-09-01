e5a8460a1e1fa8654864b929fd2d7f2824a17c4c
diddipoeler <diddipoeler@gmx.de>
2026-09-01 09:19:47 +0200

    Bridge legacy tournament tree match list to Joomla 5/6 template

--- admin/views/treetomatchs/tmpl/default.php
+++ admin/views/treetomatchs/tmpl/default.php
@@ -1,20 +1,4 @@
 <?php
-/** Tournament-tree assigned matches list for Joomla 5/6. */
-defined('_JEXEC') or die('Restricted access');
-
-use Joomla\CMS\HTML\HTMLHelper;
-
-sportsmanagementHelper::addTemplatePaths(['footer'], $this);
-?>
-<form action="<?php echo $this->request_url; ?>" method="post" id="adminForm" name="adminForm">
-    <?php echo $this->loadTemplate('data'); ?>
-
-    <input type="hidden" name="boxchecked" value="0">
-    <input type="hidden" name="task" value="">
-    <input type="hidden" name="nid" value="<?php echo (int) $this->node_id; ?>">
-    <input type="hidden" name="tid" value="<?php echo (int) $this->tree_id; ?>">
-    <input type="hidden" name="pid" value="<?php echo (int) $this->project_id; ?>">
-    <?php echo HTMLHelper::_('form.token'); ?>
-</form>
-
-<?php echo $this->loadTemplate('footer'); ?>
+/** Legacy compatibility bridge for the native Joomla 5/6 tournament-tree match list. */
+\defined('_JEXEC') or die;
+require JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/tmpl/treetomatchs/default.php';
