ec16abe0e59f0aba8ba71d95c9dfddd38f323652
diddipoeler <diddipoeler@gmx.de>
2026-09-01 08:54:03 +0200

    Bridge legacy tournament tree node view to Joomla 5/6 MVC

--- admin/views/treetonode/view.html.php
+++ admin/views/treetonode/view.html.php
@@ -1,47 +1,13 @@
 <?php
-/** SportsManagement administrator tournament-tree node edit view. */
-defined('_JEXEC') or die('Restricted access');
+/** Legacy compatibility bridge for the native Joomla 5/6 tournament tree node view. */
+\defined('_JEXEC') or die;
 
-use Joomla\CMS\HTML\HTMLHelper;
-use Joomla\CMS\Language\Text;
+use Diddipoeler\Component\SportsManagement\Administrator\View\Treetonode\HtmlView;
 
-class sportsmanagementViewTreetonode extends sportsmanagementView
-{
-    public function init()
-    {
-        if (in_array($this->getLayout(), ['edit', 'edit_3', 'edit_4'], true)) {
-            $this->displayForm();
-        }
-    }
+if (!class_exists(HtmlView::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/View/Treetonode/HtmlView.php';
+}
 
-    private function displayForm(): void
-    {
-        $input = $this->app->getInput();
-        $projectId = $input->getInt('pid') ?: (int) $this->app->getUserState($this->option . '.pid', 0);
-        $treeId = $input->getInt('tid') ?: (int) $this->app->getUserState($this->option . '.tid', 0);
-        $nodeId = !empty($this->item->id) ? (int) $this->item->id : $input->getInt('id');
-        $project = $this->model->getProject($projectId);
-
-        if (!$project) {
-            $this->app->enqueueMessage(Text::_('JLIB_APPLICATION_ERROR_COMPONENT_NOT_FOUND'), 'error');
-
-            return;
-        }
-
-        $teamOptions = [
-            HTMLHelper::_('select.option', 0, Text::_('COM_SPORTSMANAGEMENT_GLOBAL_SELECT_TEAM')),
-        ];
-        $projectTeams = $this->model->getProjectTeamsOptions($projectId);
-
-        if ($projectTeams) {
-            $teamOptions = array_merge($teamOptions, $projectTeams);
-        }
-
-        $this->project_id = $projectId;
-        $this->tree_id = $treeId;
-        $this->projectws = $project;
-        $this->lists = ['team' => $teamOptions];
-        $this->node = $this->item;
-        $this->match = $this->model->getNodeMatch($nodeId);
-    }
+if (!class_exists('sportsmanagementViewTreetonode', false)) {
+    class_alias(HtmlView::class, 'sportsmanagementViewTreetonode');
 }
