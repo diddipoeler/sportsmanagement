043ebb19e225f480bb73b0ea1dfbdc122473cd74
diddipoeler <diddipoeler@gmx.de>
2026-09-01 07:52:29 +0200

    Bridge legacy GitHub install view to Joomla 5/6 MVC

--- admin/views/githubinstall/view.html.php
+++ admin/views/githubinstall/view.html.php
@@ -1,21 +1,13 @@
 <?php
-/** Joomla 5/6 GitHub update download view. */
-defined('_JEXEC') or die('Restricted access');
+/** Legacy compatibility bridge for the native Joomla 5/6 GitHub install view. */
+\defined('_JEXEC') or die;
 
-use Joomla\CMS\Component\ComponentHelper;
-use Joomla\CMS\Language\Text;
+use Diddipoeler\Component\SportsManagement\Administrator\View\Githubinstall\HtmlView;
 
-class sportsmanagementViewgithubinstall extends sportsmanagementView
-{
-    public function init()
-    {
-        $this->github_link = trim((string) ComponentHelper::getParams($this->option)->get('cfg_update_server_file', ''));
-        $this->_success_text = [];
-    }
+if (!class_exists(HtmlView::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/View/Githubinstall/HtmlView.php';
+}
 
-    protected function addToolbar()
-    {
-        $this->title = Text::_('COM_SPORTSMANAGEMENT_GITHUBINSTALL');
-        parent::addToolbar();
-    }
+if (!class_exists('sportsmanagementViewgithubinstall', false)) {
+    class_alias(HtmlView::class, 'sportsmanagementViewgithubinstall');
 }
