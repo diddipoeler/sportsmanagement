aa12523f6d82440c43f2fe80a9056d4ff5091be8
diddipoeler <diddipoeler@gmx.de>
2026-09-01 09:20:09 +0200

    Bridge legacy tournament tree match assignment form to Joomla 5/6 template

--- admin/views/treetomatchs/tmpl/editlist.php
+++ admin/views/treetomatchs/tmpl/editlist.php
@@ -1,30 +1,4 @@
 <?php
-/** Tournament-tree match assignment form for Joomla 5/6. */
-defined('_JEXEC') or die('Restricted access');
-
-use Joomla\CMS\HTML\HTMLHelper;
-
-sportsmanagementHelper::addTemplatePaths(['footer'], $this);
-?>
-<script>
-Joomla.submitbutton = function (task) {
-    document.querySelectorAll('#node_matcheslist option').forEach(function (option) {
-        option.selected = true;
-    });
-
-    Joomla.submitform(task, document.getElementById('adminForm'));
-};
-</script>
-
-<form action="<?php echo $this->request_url; ?>" method="post" id="adminForm" name="adminForm">
-    <?php echo $this->loadTemplate('data'); ?>
-
-    <input type="hidden" name="option" value="com_sportsmanagement">
-    <input type="hidden" name="task" value="">
-    <input type="hidden" name="nid" value="<?php echo (int) $this->node_id; ?>">
-    <input type="hidden" name="tid" value="<?php echo (int) $this->tree_id; ?>">
-    <input type="hidden" name="pid" value="<?php echo (int) $this->project_id; ?>">
-    <?php echo HTMLHelper::_('form.token'); ?>
-</form>
-
-<?php echo $this->loadTemplate('footer'); ?>
+/** Legacy compatibility bridge for the native Joomla 5/6 tournament-tree match assignment form. */
+\defined('_JEXEC') or die;
+require JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/tmpl/treetomatchs/editlist.php';
