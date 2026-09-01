09ee62fdabad870743f1121e4e80144a2ba6551b
diddipoeler <diddipoeler@gmx.de>
2026-09-01 07:47:16 +0200

    Bridge legacy prediction game view to Joomla 5/6 MVC

--- admin/views/predictiongame/view.html.php
+++ admin/views/predictiongame/view.html.php
@@ -1,36 +1,13 @@
 <?php
-/**
- * SportsManagement administrator prediction game view.
- */
-defined('_JEXEC') or die('Restricted access');
+/** Legacy compatibility bridge for the native Joomla 5/6 prediction game editor. */
+\defined('_JEXEC') or die;
 
-use Diddipoeler\Component\SportsManagement\Administrator\Model\PredictiongameModel;
-use Diddipoeler\Component\SportsManagement\Administrator\Model\PredictiongamesModel;
-use Joomla\CMS\Language\Text;
+use Diddipoeler\Component\SportsManagement\Administrator\View\Predictiongame\HtmlView;
 
-/**
- * sportsmanagementViewPredictionGame
- */
-class sportsmanagementViewPredictionGame extends sportsmanagementView
-{
-    public function init()
-    {
-        $this->pred_admins = PredictiongamesModel::getAdmins($this->item->id);
-        $this->pred_projects = $this->model->getPredictionProjectIDs($this->item->id);
+if (!class_exists(HtmlView::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/View/Predictiongame/HtmlView.php';
+}
 
-        $this->form->setValue('user_ids', null, $this->pred_admins);
-        $this->form->setValue('project_ids', null, $this->pred_projects);
-        $this->form->setValue('s', null, PredictiongameModel::$seasonid);
-    }
-
-    protected function addToolBar()
-    {
-        $this->app->getInput()->set('hidemainmenu', true);
-        $this->title = $this->item->id
-            ? Text::_('COM_SPORTSMANAGEMENT_ADMIN_PGAME_EDIT')
-            : Text::_('COM_SPORTSMANAGEMENT_ADMIN_PGAME_NEW');
-        $this->icon = 'pgame';
-
-        parent::addToolbar();
-    }
+if (!class_exists('sportsmanagementViewPredictionGame', false)) {
+    class_alias(HtmlView::class, 'sportsmanagementViewPredictionGame');
 }
