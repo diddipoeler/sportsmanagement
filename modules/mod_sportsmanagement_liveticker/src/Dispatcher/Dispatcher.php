30a226e061981ca26da94b3ec6374bdc2d8a2e05
diddipoeler <diddipoeler@gmx.de>
2026-09-01 22:41:27 +0200

    Move Liveticker assets into native dispatcher

--- modules/mod_sportsmanagement_liveticker/src/Dispatcher/Dispatcher.php
+++ modules/mod_sportsmanagement_liveticker/src/Dispatcher/Dispatcher.php
@@ -6,6 +6,7 @@ namespace Diddipoeler\Module\SportsManagementLiveticker\Site\Dispatcher;
 use Joomla\CMS\Dispatcher\AbstractModuleDispatcher;
 use Joomla\CMS\Helper\HelperFactoryAwareInterface;
 use Joomla\CMS\Helper\HelperFactoryAwareTrait;
+use Joomla\CMS\Uri\Uri;
 
 final class Dispatcher extends AbstractModuleDispatcher implements HelperFactoryAwareInterface
 {
@@ -22,6 +23,26 @@ final class Dispatcher extends AbstractModuleDispatcher implements HelperFactory
         $result = $this->getHelperFactory()
             ->getHelper('LivetickerHelper')
             ->getData($data['params'], $data['module'], $this->getApplication());
+
+        $assets = $this->getApplication()->getDocument()->getWebAssetManager();
+        $assets->registerAndUseScript(
+            'mod_sportsmanagement_liveticker',
+            'modules/mod_sportsmanagement_liveticker/js/turtushout.js',
+            ['version' => 'auto'],
+            ['defer' => true]
+        );
+
+        $cssFile = basename((string) ($result['cssFile'] ?? ''));
+        if ($cssFile !== '') {
+            $assets->registerAndUseStyle(
+                'mod_sportsmanagement_liveticker',
+                'modules/mod_sportsmanagement_liveticker/css/' . $cssFile,
+                ['version' => 'auto']
+            );
+        }
+
+        $result['refreshUrl'] = rtrim((string) Uri::base(), '/')
+            . '/index.php?option=com_ajax&module=sportsmanagement_liveticker&method=refresh&format=raw';
 
         return array_merge($data, $result);
     }
