c83c5042428653bbe57b8617bca616ab2fa831bb
diddipoeler <diddipoeler@gmx.de>
2026-09-01 07:47:25 +0200

    Add legacy bridge for prediction game edit layout

---
+++ admin/views/predictiongame/tmpl/edit.php
@@ -0,0 +1,5 @@
+<?php
+/** Legacy compatibility bridge for the native Joomla 5/6 prediction game edit layout. */
+\defined('_JEXEC') or die;
+
+require JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/tmpl/predictiongame/edit.php';
