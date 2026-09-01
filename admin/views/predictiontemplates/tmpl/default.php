c0b2d776521c85cbcb1d87b41e1c3db640378c6f
diddipoeler <diddipoeler@gmx.de>
2026-09-01 07:36:02 +0200

    Bridge legacy prediction templates layout to Joomla 5/6

--- admin/views/predictiontemplates/tmpl/default.php
+++ admin/views/predictiontemplates/tmpl/default.php
@@ -1,29 +1,5 @@
 <?php
-/**
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage predictiontemplates
- * @file       default.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\HTML\HTMLHelper;
+/** Legacy compatibility bridge for the native Joomla 5/6 prediction templates layout. */
+\defined('_JEXEC') or die;
 
-$templatesToLoad = array('footer', 'listheader');
-sportsmanagementHelper::addTemplatePaths($templatesToLoad, $this);
-
-?>
-<form action="<?php echo $this->request_url; ?>" method="post" id="adminForm" name="adminForm">
-
-	<?PHP
-	echo $this->loadTemplate('joomla_version');
-	?>
-    <input type='hidden' name='task' value=''/>
-    <input type='hidden' name='boxchecked' value='0'/>
-    <input type='hidden' name='filter_order_Dir' value='<?php echo $this->sortDirection; ?>'/>
-    <input type='hidden' name='filter_order' value='<?php echo $this->sortColumn; ?>'/>
-	<?php echo HTMLHelper::_('form.token'); ?>
-</form>
+require JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/tmpl/predictiontemplates/default.php';
