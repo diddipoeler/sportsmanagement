79d09e5048f624f8e3c976fc03f099c5ae59bd15
diddipoeler <diddipoeler@gmx.de>
2026-09-01 08:44:15 +0200

    Bridge legacy project template edit layout to Joomla 5/6 template

---
+++ admin/views/template/tmpl/edit.php
@@ -0,0 +1,5 @@
+<?php
+/** Legacy compatibility bridge for the native Joomla 5/6 project template edit layout. */
+\defined('_JEXEC') or die;
+
+require JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/tmpl/template/edit.php';
