84cd53fb703faecf6843b520a7a936a13548ed9a
diddipoeler <diddipoeler@gmx.de>
2026-09-01 07:48:17 +0200

    Bridge legacy predictions dashboard layout to Joomla 5/6

--- admin/views/predictions/tmpl/default.php
+++ admin/views/predictions/tmpl/default.php
@@ -1,44 +1,5 @@
 <?php
-/**
- *
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage predictions
- * @file       default.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
+/** Legacy compatibility bridge for the native Joomla 5/6 predictions dashboard layout. */
+\defined('_JEXEC') or die;
 
-defined('_JEXEC') or die('Restricted access');
-
-use Joomla\CMS\Language\Text;
-
-?>
-
-<?php if (!empty($this->sidebar)) : ?>
-<div id="j-sidebar-container" class="span2">
-	<?php echo $this->sidebar; ?>
-</div>
-<div id="j-main-container" class="span10">
-	<?php else : ?>
-    <div id="j-main-container">
-		<?php endif; ?>
-
-        <table width="100%" border="0">
-            <tr>
-                <td width="100%" valign="top">
-                    <div id="cpanel">
-						<?php echo $this->addIcon('tippspiel.png', 'index.php?option=com_sportsmanagement&view=predictiongames', Text::_('COM_SPORTSMANAGEMENT_EXT_PREDICTION_GAMES')); ?>
-						<?php echo $this->addIcon('tippspielgruppen.png', 'index.php?option=com_sportsmanagement&view=predictiongroups', Text::_('COM_SPORTSMANAGEMENT_EXT_PREDICTION_GROUPS')); ?>
-						<?php echo $this->addIcon('tippspielmitglieder.png', 'index.php?option=com_sportsmanagement&view=predictionmembers', Text::_('COM_SPORTSMANAGEMENT_EXT_PREDICTION_MEMBERS')); ?>
-						<?php echo $this->addIcon('tippspieltemplates.png', 'index.php?option=com_sportsmanagement&view=predictiontemplates', Text::_('COM_SPORTSMANAGEMENT_EXT_PREDICTION_TEMPLATES')); ?>
-
-                    </div>
-                </td>
-
-            </tr>
-
-        </table>
+require JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/tmpl/predictions/default.php';
