1ce37aa799104ae5e7c9f2d1f3b783a355b10456
diddipoeler <diddipoeler@gmx.de>
2026-09-01 08:21:43 +0200

    Bridge legacy JoomLeague import layout to Joomla 5/6 template

--- admin/views/joomleagueimports/tmpl/import.php
+++ admin/views/joomleagueimports/tmpl/import.php
@@ -1,17 +1,5 @@
 <?php
-/**
- *
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage joomleagueimports
- * @file       import.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
+/** Legacy compatibility bridge for the native Joomla 5/6 JoomLeague import layout. */
+\defined('_JEXEC') or die;
 
-defined('_JEXEC') or die('Restricted access');
-
-
+require JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/tmpl/joomleagueimports/import.php';
