ea671534ddac131979796581604649693df90abc
diddipoeler <diddipoeler@gmx.de>
2026-09-02 15:48:50 +0200

    Add required metadata to colorpicker bridge

--- admin/elements/colorpicker.php
+++ admin/elements/colorpicker.php
@@ -1,5 +1,12 @@
 <?php
-/** Legacy compatibility bridge for the Joomla 5/6 color picker field. */
+/**
+ * Legacy compatibility bridge for the Joomla 5/6 color picker field.
+ *
+ * @version    4.24.00
+ * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 \defined('_JEXEC') or die;
 
 use Diddipoeler\Component\SportsManagement\Administrator\Field\ColorpickerField;
