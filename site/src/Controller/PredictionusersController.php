33a68851d6316f77b306cc859bcdda7a35ded973
diddipoeler <diddipoeler@gmx.de>
2026-09-02 16:11:21 +0200

    Use native prediction routes in Joomla 5/6 controller

--- site/src/Controller/PredictionusersController.php
+++ site/src/Controller/PredictionusersController.php
@@ -1,8 +1,17 @@
 <?php
+/**
+ * Native Joomla 5/6 controller for SportsManagement prediction users.
+ *
+ * @version    4.24.00
+ * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 namespace Diddipoeler\Component\SportsManagement\Site\Controller;
 
 \defined('_JEXEC') or die;
 
+use Diddipoeler\Component\SportsManagement\Site\Helper\PredictionRouteHelper;
 use Diddipoeler\Component\SportsManagement\Site\Model\PredictionmemberModel;
 use Diddipoeler\Component\SportsManagement\Site\Model\PredictionusersModel;
 use Joomla\CMS\Language\Text;
@@ -86,10 +95,9 @@ final class PredictionusersController extends BaseController
 
     private function buildMemberRoute(PredictionusersModel $model): string
     {
-        $this->loadRouteHelper();
         $input = $this->getApplication()->getInput();
 
-        return \JSMPredictionHelperRoute::getPredictionMemberRoute(
+        return PredictionRouteHelper::member(
             $model->getPredictionGameId(),
             $input->post->getInt('uid', $model->getSelectedMemberNumericId()),
             null,
@@ -102,10 +110,9 @@ final class PredictionusersController extends BaseController
 
     private function buildEditorRoute(PredictionmemberModel $model): string
     {
-        $this->loadRouteHelper();
         $input = $this->getApplication()->getInput();
 
-        return \JSMPredictionHelperRoute::getPredictionMemberRoute(
+        return PredictionRouteHelper::member(
             $model->getPredictionGameId(),
             $model->getSelectedMemberNumericId(),
             null,
@@ -114,12 +121,5 @@ final class PredictionusersController extends BaseController
             $model->getRoundId(),
             $input->getInt('cfg_which_database', 0)
         );
-    }
-
-    private function loadRouteHelper(): void
-    {
-        if (!class_exists('JSMPredictionHelperRoute', false)) {
-            require_once JPATH_SITE . '/components/com_sportsmanagement/helpers/predictionroute.php';
-        }
     }
 }
