303ed3e101e2073dd2a4ea3b175dd240d9c88e20
diddipoeler <diddipoeler@gmx.de>
2026-09-02 21:02:41 +0200

    Add required metadata to admin close model

--- admin/src/Model/CloseModel.php
+++ admin/src/Model/CloseModel.php
@@ -1,5 +1,11 @@
 <?php
 /**
+ * Empty Joomla 5/6 administrator model for the close view.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
  * @package     SportsManagement
  * @subpackage  com_sportsmanagement
  */
