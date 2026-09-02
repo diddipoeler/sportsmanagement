2e59488519ac064fec40aa10bdb50ea0630c8e5f
diddipoeler <diddipoeler@gmx.de>
2026-09-02 18:38:01 +0200

    Complete Matches Slider entry metadata

--- modules/mod_sportsmanagement_matchesslider/mod_sportsmanagement_matchesslider.php
+++ modules/mod_sportsmanagement_matchesslider/mod_sportsmanagement_matchesslider.php
@@ -1,6 +1,13 @@
 <?php
-/** Joomla 5/6 compatibility entry point for mod_sportsmanagement_matchesslider. */
+/**
+ * Joomla 5/6 compatibility entry point for mod_sportsmanagement_matchesslider.
+ *
+ * Runtime execution is handled by services/provider.php and the namespaced
+ * module dispatcher. This file remains only for legacy package compatibility.
+ *
+ * @version    4.24.00
+ * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 defined('_JEXEC') or die;
-
-// Runtime execution is handled by services/provider.php and the namespaced
-// module dispatcher. This file remains only for legacy package compatibility.
