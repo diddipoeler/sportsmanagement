d2484528b7d30577a454a0fbed2519c36db0b62e
diddipoeler <diddipoeler@gmx.de>
2026-09-02 17:42:43 +0200

    Add GCalendar layout PHP metadata

--- modules/mod_sportsmanagement_gcalendar/tmpl/default.php
+++ modules/mod_sportsmanagement_gcalendar/tmpl/default.php
@@ -1,8 +1,12 @@
 <?php
 /**
  * Joomla 5/6 native calendar layout for SportsManagement Google calendars.
+ *
+ * @version    4.24.00
+ * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
-
 defined('_JEXEC') or die;
 
 $moduleId = (int) $module->id;
