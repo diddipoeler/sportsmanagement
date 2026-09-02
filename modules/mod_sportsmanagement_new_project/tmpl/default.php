0cee6c4b706b9a7eb18666ac8decf9d09b755e1a
diddipoeler <diddipoeler@gmx.de>
2026-09-02 16:23:47 +0200

    Complete New Project default layout metadata

--- modules/mod_sportsmanagement_new_project/tmpl/default.php
+++ modules/mod_sportsmanagement_new_project/tmpl/default.php
@@ -1,9 +1,14 @@
 <?php
 /**
- * Default layout compatibility wrapper.
+ * Default layout compatibility wrapper for mod_sportsmanagement_new_project.
  *
  * The Joomla 5/6 implementation lives in native.php so existing module
  * instances and template overrides selecting "default" remain functional.
+ *
+ * @version    4.24.00
+ * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 \defined('_JEXEC') or die;
 
