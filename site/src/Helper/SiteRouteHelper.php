8289a58e9a05489a8b108a3a6ff07326bf465c9f
diddipoeler <diddipoeler@gmx.de>
2026-09-02 16:51:14 +0200

    Complete site route helper PHPDoc metadata

--- site/src/Helper/SiteRouteHelper.php
+++ site/src/Helper/SiteRouteHelper.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Central Joomla 5/6 site routing helper for SportsManagement.
+ *
+ * @version   5.6.0
+ * @author    diddipoeler
+ * @copyright Copyright (C) diddipoeler
+ * @license   GNU General Public License version 2 or later; see LICENSE.txt
+ */
 namespace Diddipoeler\Component\SportsManagement\Site\Helper;
 
 \defined('_JEXEC') or die;
