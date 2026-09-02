92d1cf563e653faf64a4573a03b68aba9784a98b
diddipoeler <diddipoeler@gmx.de>
2026-09-02 17:30:49 +0200

    Add Google Calendar helper bridge metadata

--- modules/mod_sportsmanagement_google_calendar/helper.php
+++ modules/mod_sportsmanagement_google_calendar/helper.php
@@ -1,6 +1,11 @@
 <?php
 /**
  * Joomla 5/6 compatibility bridge for the SportsManagement Google Calendar module helper.
+ *
+ * @version    4.24.00
+ * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 defined('_JEXEC') or die;
 
