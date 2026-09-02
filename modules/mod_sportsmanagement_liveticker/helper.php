5581d40971ff3d7bb2f397f8457ad801fd5829c0
diddipoeler <diddipoeler@gmx.de>
2026-09-02 18:19:13 +0200

    Complete Liveticker helper bridge metadata

--- modules/mod_sportsmanagement_liveticker/helper.php
+++ modules/mod_sportsmanagement_liveticker/helper.php
@@ -1,5 +1,12 @@
 <?php
-/** Legacy helper bridge for the Joomla 5/6 SportsManagement liveticker module. */
+/**
+ * Legacy helper bridge for the Joomla 5/6 SportsManagement liveticker module.
+ *
+ * @version    4.24.00
+ * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 \defined('_JEXEC') or die;
 
 use Diddipoeler\Module\SportsManagementLiveticker\Site\Helper\LivetickerHelper;
