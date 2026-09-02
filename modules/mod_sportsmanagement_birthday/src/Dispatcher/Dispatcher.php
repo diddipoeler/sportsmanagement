45e7526c4cb3eb713b28940e97a3b4cebefaa848
diddipoeler <diddipoeler@gmx.de>
2026-09-02 14:28:18 +0200

    Harden Birthday dispatcher runtime

--- modules/mod_sportsmanagement_birthday/src/Dispatcher/Dispatcher.php
+++ modules/mod_sportsmanagement_birthday/src/Dispatcher/Dispatcher.php
@@ -13,9 +13,14 @@ final class Dispatcher extends AbstractModuleDispatcher implements HelperFactory
 {
     use HelperFactoryAwareTrait;
 
-    protected function getLayoutData(): array
+    protected function getLayoutData(): array|false
     {
         $data = parent::getLayoutData();
+
+        if ($data === false) {
+            return false;
+        }
+
         $app = $this->getApplication();
         $app->getLanguage()->load('com_sportsmanagement', JPATH_ADMINISTRATOR, null, true);
 
