d6bc8ba023e3f3a7bd750cb6c358f4eabcf95247
diddipoeler <diddipoeler@gmx.de>
2026-09-02 20:40:18 +0200

    Add required metadata to calendar service provider

--- modules/mod_sportsmanagement_calendar/services/provider.php
+++ modules/mod_sportsmanagement_calendar/services/provider.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Joomla 5/6 service provider for the SportsManagement calendar module.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 \defined('_JEXEC') or die;
 
 use Joomla\CMS\Extension\Service\Provider\HelperFactory;
