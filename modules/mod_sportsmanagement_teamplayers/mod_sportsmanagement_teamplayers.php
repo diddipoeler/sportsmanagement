f274c2e845df6906d0dd49f7e0422b1da3b3aea0
diddipoeler <diddipoeler@gmx.de>
2026-09-02 17:09:24 +0200

    Complete TeamPlayers module entry metadata

--- modules/mod_sportsmanagement_teamplayers/mod_sportsmanagement_teamplayers.php
+++ modules/mod_sportsmanagement_teamplayers/mod_sportsmanagement_teamplayers.php
@@ -1,7 +1,12 @@
 <?php
 /**
- * SportsManagement Joomla 5/6 module entry compatibility file.
+ * Joomla 5/6 compatibility entry point for the TeamPlayers module.
  *
  * The active module is booted by services/provider.php and the namespaced dispatcher.
+ *
+ * @version   5.6.0
+ * @author    diddipoeler
+ * @copyright Copyright (C) diddipoeler
+ * @license   GNU General Public License version 2 or later; see LICENSE.txt
  */
 \defined('_JEXEC') or die;
