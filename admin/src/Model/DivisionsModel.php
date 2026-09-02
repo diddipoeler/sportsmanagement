10142b8cfb564fa2a87229bd1acb5b472776e943
diddipoeler <diddipoeler@gmx.de>
2026-09-02 21:24:02 +0200

    Add required metadata to divisions list model

--- admin/src/Model/DivisionsModel.php
+++ admin/src/Model/DivisionsModel.php
@@ -2,6 +2,10 @@
 /**
  * @package     SportsManagement
  * @subpackage  com_sportsmanagement
+ * @version     4.24.00
+ * @author      diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright   Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
 namespace Diddipoeler\Component\SportsManagement\Administrator\Model;
