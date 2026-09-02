f3611997d1425f5478700781a3972c96c56424c0
diddipoeler <diddipoeler@gmx.de>
2026-09-02 16:56:46 +0200

    Complete admin database field trait PHPDoc metadata

--- admin/src/Field/SportsManagementDatabaseTrait.php
+++ admin/src/Field/SportsManagementDatabaseTrait.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Shared Joomla 5/6 database resolver trait for administrator form fields.
+ *
+ * @version   5.6.0
+ * @author    diddipoeler
+ * @copyright Copyright (C) diddipoeler
+ * @license   GNU General Public License version 2 or later; see LICENSE.txt
+ */
 namespace Diddipoeler\Component\SportsManagement\Administrator\Field;
 
 \defined('_JEXEC') or die;
