50e8def88566ec4091565ac7deac98e6b46972b3
diddipoeler <diddipoeler@gmx.de>
2026-09-02 20:34:33 +0200

    Add required metadata to administrator component extension

--- admin/src/Extension/SportsManagementComponent.php
+++ admin/src/Extension/SportsManagementComponent.php
@@ -4,6 +4,11 @@
  * @subpackage  com_sportsmanagement
  *
  * Joomla 5/6 migration scaffold.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 
 namespace Diddipoeler\Component\SportsManagement\Administrator\Extension;
