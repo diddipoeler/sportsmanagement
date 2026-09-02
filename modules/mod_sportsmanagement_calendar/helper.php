b852dccbf31395f3b83f3a273efac0ef4c326185
diddipoeler <diddipoeler@gmx.de>
2026-09-02 20:39:46 +0200

    Add required metadata to calendar helper bridge

--- modules/mod_sportsmanagement_calendar/helper.php
+++ modules/mod_sportsmanagement_calendar/helper.php
@@ -1,6 +1,11 @@
 <?php
 /**
  * Legacy compatibility bridge for the Joomla 5/6 SportsManagement calendar module.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 \defined('_JEXEC') or die;
 
