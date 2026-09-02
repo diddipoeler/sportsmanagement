5cc40eb2c271b5a7ed0ca1cf6a5ecb9e59344a31
diddipoeler <diddipoeler@gmx.de>
2026-09-02 16:51:51 +0200

    Complete Matches default layout PHPDoc metadata

--- modules/mod_sportsmanagement_matches/tmpl/default.php
+++ modules/mod_sportsmanagement_matches/tmpl/default.php
@@ -1,5 +1,12 @@
 <?php
+/**
+ * Default Joomla 5/6 layout compatibility wrapper for the matches module.
+ *
+ * @version   5.6.0
+ * @author    diddipoeler
+ * @copyright Copyright (C) diddipoeler
+ * @license   GNU General Public License version 2 or later; see LICENSE.txt
+ */
 \defined('_JEXEC') or die;
 
-/** Joomla 5/6 dispatcher default layout. */
 require __DIR__ . '/native.php';
