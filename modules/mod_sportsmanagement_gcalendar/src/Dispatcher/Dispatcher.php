ceba4590bc98732b8710515be1a243febdf0f94e
diddipoeler <diddipoeler@gmx.de>
2026-09-02 07:58:14 +0200

    Guard GCalendar module rendering in Joomla 5/6

--- modules/mod_sportsmanagement_gcalendar/src/Dispatcher/Dispatcher.php
+++ modules/mod_sportsmanagement_gcalendar/src/Dispatcher/Dispatcher.php
@@ -11,9 +11,14 @@ final class Dispatcher extends AbstractModuleDispatcher implements HelperFactory
 {
     use HelperFactoryAwareTrait;
 
-    protected function getLayoutData(): array
+    protected function getLayoutData(): array|false
     {
         $data = parent::getLayoutData();
+
+        if ($data === false) {
+            return false;
+        }
+
         $data = array_merge(
             $data,
             $this->getHelperFactory()
