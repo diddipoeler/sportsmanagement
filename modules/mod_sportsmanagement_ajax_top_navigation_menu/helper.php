bf0483cb6d4977e0c47b70f61c6ee7f8c0ed6eb8
diddipoeler <diddipoeler@gmx.de>
2026-09-02 21:41:20 +0200

    Add required metadata to AJAX navigation helper bridge

--- modules/mod_sportsmanagement_ajax_top_navigation_menu/helper.php
+++ modules/mod_sportsmanagement_ajax_top_navigation_menu/helper.php
@@ -1,5 +1,12 @@
 <?php
-/** Legacy compatibility facade for the namespaced Joomla 5/6 AJAX navigation data helper. */
+/**
+ * Legacy compatibility facade for the namespaced Joomla 5/6 AJAX navigation data helper.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 \defined('_JEXEC') or die;
 
 use Diddipoeler\Module\SportsManagementAjaxTopNavigationMenu\Site\Helper\NavigationDataHelper;
