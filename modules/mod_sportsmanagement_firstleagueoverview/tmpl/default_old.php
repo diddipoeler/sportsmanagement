888ede3697bede4fb3363f803882edba1be6de34
diddipoeler <diddipoeler@gmx.de>
2026-09-02 17:56:45 +0200

    Document First League Overview legacy layout alias

--- modules/mod_sportsmanagement_firstleagueoverview/tmpl/default_old.php
+++ modules/mod_sportsmanagement_firstleagueoverview/tmpl/default_old.php
@@ -4,6 +4,11 @@
  *
  * Keep old module layout selections working while using the Joomla 5/6
  * passive default template.
+ *
+ * @version    4.24.00
+ * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 \defined('_JEXEC') or die;
 
