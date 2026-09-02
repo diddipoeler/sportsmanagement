4942198eb30b20a79c298ff65dde0905547002a8
diddipoeler <diddipoeler@gmx.de>
2026-09-02 20:33:39 +0200

    Add required metadata to birthday dispatcher

--- modules/mod_sportsmanagement_birthday/src/Dispatcher/Dispatcher.php
+++ modules/mod_sportsmanagement_birthday/src/Dispatcher/Dispatcher.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Joomla 5/6 dispatcher for the SportsManagement birthday module.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 namespace Diddipoeler\Module\SportsManagementBirthday\Site\Dispatcher;
 
 \defined('_JEXEC') or die;
