69ff663ce61fda6276f3cea408e2bf435357bc5a
diddipoeler <diddipoeler@gmx.de>
2026-09-02 16:17:18 +0200

    Complete Count Rekord helper metadata

--- modules/mod_sportsmanagement_count_rekord/src/Helper/CountRekordHelper.php
+++ modules/mod_sportsmanagement_count_rekord/src/Helper/CountRekordHelper.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Joomla 5/6 data helper for mod_sportsmanagement_count_rekord.
+ *
+ * @version    4.24.00
+ * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 namespace Diddipoeler\Module\SportsManagementCountRekord\Site\Helper;
 
 \defined('_JEXEC') or die;
