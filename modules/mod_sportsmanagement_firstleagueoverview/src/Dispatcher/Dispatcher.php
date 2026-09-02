45bba5cac17112826866a4d083dbeb4a6f2c9bfd
diddipoeler <diddipoeler@gmx.de>
2026-09-02 17:55:36 +0200

    Align First League Overview dispatcher metadata and Factory usage

--- modules/mod_sportsmanagement_firstleagueoverview/src/Dispatcher/Dispatcher.php
+++ modules/mod_sportsmanagement_firstleagueoverview/src/Dispatcher/Dispatcher.php
@@ -1,9 +1,16 @@
 <?php
+/**
+ * @version    4.24.00
+ * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 namespace Diddipoeler\Module\SportsManagementFirstLeagueOverview\Site\Dispatcher;
 
 \defined('_JEXEC') or die;
 
 use Joomla\CMS\Dispatcher\AbstractModuleDispatcher;
+use Joomla\CMS\Factory;
 use Joomla\CMS\Helper\HelperFactoryAwareInterface;
 use Joomla\CMS\Helper\HelperFactoryAwareTrait;
 use Joomla\Database\DatabaseInterface;
@@ -24,7 +31,7 @@ final class Dispatcher extends AbstractModuleDispatcher implements HelperFactory
         $app->getLanguage()->load('com_sportsmanagement', JPATH_SITE, null, true);
 
         /** @var DatabaseInterface $database */
-        $database = \Joomla\CMS\Factory::getContainer()->get(DatabaseInterface::class);
+        $database = Factory::getContainer()->get(DatabaseInterface::class);
         $overview = $this->getHelperFactory()
             ->getHelper('FirstLeagueOverviewHelper')
             ->getData($data['params'], $database);
