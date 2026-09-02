e79d070378929e6061ae61f47bbf5baa88bc988c
diddipoeler <diddipoeler@gmx.de>
2026-09-02 16:58:32 +0200

    Complete EventsRanking helper bridge metadata

--- modules/mod_sportsmanagement_eventsranking/helper.php
+++ modules/mod_sportsmanagement_eventsranking/helper.php
@@ -1,5 +1,12 @@
 <?php
-/** Legacy compatibility bridge for mod_sportsmanagement_eventsranking. */
+/**
+ * Legacy compatibility bridge for mod_sportsmanagement_eventsranking.
+ *
+ * @version   5.6.0
+ * @author    diddipoeler
+ * @copyright Copyright (C) diddipoeler
+ * @license   GNU General Public License version 2 or later; see LICENSE.txt
+ */
 \defined('_JEXEC') or die;
 
 use Diddipoeler\Module\SportsManagementEventsRanking\Site\Helper\EventsRankingHelper;
