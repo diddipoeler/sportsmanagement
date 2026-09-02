e7b0cdbc5962d4a68c3bd3aa69c6a288da34ed44
diddipoeler <diddipoeler@gmx.de>
2026-09-02 21:22:49 +0200

    Add required metadata to prediction results controller

--- site/src/Controller/PredictionresultsController.php
+++ site/src/Controller/PredictionresultsController.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Native Joomla 5/6 controller for SportsManagement prediction results.
+ *
+ * @version    4.24.00
+ * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 namespace Diddipoeler\Component\SportsManagement\Site\Controller;
 
 \defined('_JEXEC') or die;
