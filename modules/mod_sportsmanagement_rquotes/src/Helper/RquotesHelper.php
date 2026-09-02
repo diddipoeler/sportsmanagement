42b588830ef498c5cae4db03ac454b011ba9f000
diddipoeler <diddipoeler@gmx.de>
2026-09-02 18:49:09 +0200

    Complete Rquotes native helper metadata

--- modules/mod_sportsmanagement_rquotes/src/Helper/RquotesHelper.php
+++ modules/mod_sportsmanagement_rquotes/src/Helper/RquotesHelper.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Joomla 5/6-native data helper for the random quotes module.
+ *
+ * @version   5.6.0
+ * @author    diddipoeler
+ * @copyright Copyright (C) diddipoeler
+ * @license   GNU General Public License version 2 or later; see LICENSE.txt
+ */
 namespace Diddipoeler\Module\SportsManagementRquotes\Site\Helper;
 
 \defined('_JEXEC') or die;
@@ -10,7 +18,6 @@ use Joomla\CMS\Uri\Uri;
 use Joomla\Database\DatabaseInterface;
 use Joomla\Registry\Registry;
 
-/** Joomla 5/6-native data helper for the random quotes module. */
 final class RquotesHelper
 {
     public function getData(
