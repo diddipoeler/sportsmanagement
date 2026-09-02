ffa4418b93554ec6be25f1cc629163399875ddbe
diddipoeler <diddipoeler@gmx.de>
2026-09-02 20:39:57 +0200

    Add required metadata to calendar renderer bridge

--- modules/mod_sportsmanagement_calendar/calendarClass.php
+++ modules/mod_sportsmanagement_calendar/calendarClass.php
@@ -1,5 +1,12 @@
 <?php
-/** Legacy compatibility bridge for the native Joomla 5/6 calendar renderer. */
+/**
+ * Legacy compatibility bridge for the native Joomla 5/6 calendar renderer.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 \defined('_JEXEC') or die;
 
 use Diddipoeler\Module\SportsManagementCalendar\Site\Runtime\CalendarRenderer;
