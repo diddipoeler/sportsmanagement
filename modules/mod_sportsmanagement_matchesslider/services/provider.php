e29ea2af7ddbeb639138bc6e0ddc58f8ef53242e
diddipoeler <diddipoeler@gmx.de>
2026-09-02 18:38:28 +0200

    Complete Matches Slider service provider metadata

--- modules/mod_sportsmanagement_matchesslider/services/provider.php
+++ modules/mod_sportsmanagement_matchesslider/services/provider.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Joomla 5/6 service provider for the SportsManagement matches slider module.
+ *
+ * @version    4.24.00
+ * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 \defined('_JEXEC') or die;
 
 use Joomla\CMS\Extension\Service\Provider\HelperFactory;
