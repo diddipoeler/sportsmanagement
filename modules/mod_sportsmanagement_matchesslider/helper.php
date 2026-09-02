ac00246a1ff6a77fc39500325bb8d5df98d7b478
diddipoeler <diddipoeler@gmx.de>
2026-09-02 18:38:14 +0200

    Complete Matches Slider helper bridge metadata

--- modules/mod_sportsmanagement_matchesslider/helper.php
+++ modules/mod_sportsmanagement_matchesslider/helper.php
@@ -1,5 +1,12 @@
 <?php
-/** Legacy helper alias for Joomla 5/6 match slider compatibility. */
+/**
+ * Legacy helper alias for Joomla 5/6 match slider compatibility.
+ *
+ * @version    4.24.00
+ * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 defined('_JEXEC') or die;
 
 use Diddipoeler\Module\SportsManagementMatchesSlider\Site\Helper\MatchesSliderHelper;
