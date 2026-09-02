ef2417a4665d449c0ce1289ede080cf941793463
diddipoeler <diddipoeler@gmx.de>
2026-09-02 17:41:42 +0200

    Add GCalendar helper bridge PHP metadata

--- modules/mod_sportsmanagement_gcalendar/helper.php
+++ modules/mod_sportsmanagement_gcalendar/helper.php
@@ -1,5 +1,12 @@
 <?php
-/** Legacy helper bridge kept for third-party overrides that still call the old module helper class. */
+/**
+ * Joomla 5/6 compatibility bridge for the SportsManagement GCalendar helper.
+ *
+ * @version    4.24.00
+ * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 \defined('_JEXEC') or die;
 
 use Diddipoeler\Module\SportsManagementGcalendar\Site\Helper\GcalendarHelper;
