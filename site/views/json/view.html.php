d8c1d5cf5c8c9baa35c5fcaf5f323c1a71fb67b2
diddipoeler <diddipoeler@gmx.de>
2026-09-02 16:07:16 +0200

    Document Joomla 5/6 legacy JSON view

--- site/views/json/view.html.php
+++ site/views/json/view.html.php
@@ -1,6 +1,11 @@
 <?php
 /**
  * Legacy JSON view kept for compatibility with older SportsManagement links.
+ *
+ * @version    4.24.00
+ * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 
 defined('_JEXEC') or die('Restricted access');
