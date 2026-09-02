51b3384dc3fbc365085601a6fb6b876aafc897ce
diddipoeler <diddipoeler@gmx.de>
2026-09-02 21:41:07 +0200

    Add required metadata to AJAX navigation entry point

--- modules/mod_sportsmanagement_ajax_top_navigation_menu/mod_sportsmanagement_ajax_top_navigation_menu.php
+++ modules/mod_sportsmanagement_ajax_top_navigation_menu/mod_sportsmanagement_ajax_top_navigation_menu.php
@@ -3,5 +3,10 @@
  * Joomla 5/6 compatibility entry point for the SportsManagement AJAX top navigation module.
  *
  * Normal module execution is handled by services/provider.php and the native dispatcher.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 \defined('_JEXEC') or die;
