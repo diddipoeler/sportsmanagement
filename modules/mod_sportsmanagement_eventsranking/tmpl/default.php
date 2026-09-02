5ee68a2a372e5c70df9cf97170c3d2ac4429d178
diddipoeler <diddipoeler@gmx.de>
2026-09-02 17:00:23 +0200

    Complete EventsRanking template metadata

--- modules/mod_sportsmanagement_eventsranking/tmpl/default.php
+++ modules/mod_sportsmanagement_eventsranking/tmpl/default.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Default Joomla 5/6 layout for the events ranking module.
+ *
+ * @version   5.6.0
+ * @author    diddipoeler
+ * @copyright Copyright (C) diddipoeler
+ * @license   GNU General Public License version 2 or later; see LICENSE.txt
+ */
 \defined('_JEXEC') or die;
 
 use Joomla\CMS\Language\Text;
