93246cc8af43b9978579e91432d343b1f3ff15eb
diddipoeler <diddipoeler@gmx.de>
2026-09-02 08:52:54 +0200

    Keep Google Calendar template passive

--- modules/mod_sportsmanagement_google_calendar/tmpl/default.php
+++ modules/mod_sportsmanagement_google_calendar/tmpl/default.php
@@ -4,23 +4,16 @@
  */
 defined('_JEXEC') or die;
 
-use Diddipoeler\Module\SportsManagementGoogleCalendar\Site\Helper\GoogleCalendarHelper;
-use Joomla\CMS\Date\Date;
-
 $escape = static fn ($value): string => htmlspecialchars((string) $value, ENT_QUOTES, 'UTF-8');
 ?>
 <ul class="next-events">
     <?php foreach ($events ?? [] as $event) : ?>
-        <?php
-        $startDate = isset($event->startDate) ? Date::getInstance($event->startDate) : null;
-        $endDate = isset($event->endDate) ? Date::getInstance($event->endDate) : null;
-        ?>
         <li class="event" itemscope itemtype="https://schema.org/Event">
-            <?php if ($startDate !== null) : ?>
-                <meta itemprop="startDate" content="<?php echo $escape($startDate->toISO8601(true)); ?>">
+            <?php if (!empty($event->jsmStartIso)) : ?>
+                <meta itemprop="startDate" content="<?php echo $escape($event->jsmStartIso); ?>">
             <?php endif; ?>
-            <?php if ($endDate !== null) : ?>
-                <meta itemprop="endDate" content="<?php echo $escape($endDate->toISO8601(true)); ?>">
+            <?php if (!empty($event->jsmEndIso)) : ?>
+                <meta itemprop="endDate" content="<?php echo $escape($event->jsmEndIso); ?>">
             <?php endif; ?>
             <div class="event-name">
                 <?php if ($params->get('show_link', true) && !empty($event->htmlLink)) : ?>
@@ -32,7 +25,7 @@ $escape = static fn ($value): string => htmlspecialchars((string) $value, ENT_QU
                 <?php endif; ?>
             </div>
             <div class="event-duration">
-                <?php echo $escape(GoogleCalendarHelper::duration($event)); ?>
+                <?php echo $escape($event->jsmDuration ?? ''); ?>
             </div>
             <?php if ($params->get('show_location', false) && !empty($event->location)) : ?>
                 <div class="event-location"><?php echo $escape($event->location); ?></div>
