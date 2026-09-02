6605772104caa54440e03cdd67d7d0fcf8983974
diddipoeler <diddipoeler@gmx.de>
2026-09-02 19:34:57 +0200

    Modernize Team Statistics Counter dispatcher

--- modules/mod_sportsmanagement_teamstatistics_counter/src/Dispatcher/Dispatcher.php
+++ modules/mod_sportsmanagement_teamstatistics_counter/src/Dispatcher/Dispatcher.php
@@ -1,10 +1,19 @@
 <?php
+/**
+ * Joomla 5/6 dispatcher for the SportsManagement Team Statistics Counter module.
+ *
+ * @version    4.24.00
+ * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 namespace Diddipoeler\Module\SportsManagementTeamStatisticsCounter\Site\Dispatcher;
 
 \defined('_JEXEC') or die;
 
 use Joomla\CMS\Dispatcher\AbstractModuleDispatcher;
 use Joomla\CMS\Document\HtmlDocument;
+use Joomla\CMS\Factory;
 use Joomla\CMS\Helper\HelperFactoryAwareInterface;
 use Joomla\CMS\Helper\HelperFactoryAwareTrait;
 use Joomla\Database\DatabaseInterface;
@@ -13,14 +22,19 @@ final class Dispatcher extends AbstractModuleDispatcher implements HelperFactory
 {
     use HelperFactoryAwareTrait;
 
-    protected function getLayoutData(): array
+    protected function getLayoutData(): array|false
     {
         $data = parent::getLayoutData();
+
+        if ($data === false) {
+            return false;
+        }
+
         $app = $this->getApplication();
         $app->getLanguage()->load('com_sportsmanagement', JPATH_ADMINISTRATOR, null, true);
 
         /** @var DatabaseInterface $joomlaDatabase */
-        $joomlaDatabase = \Joomla\CMS\Factory::getContainer()->get(DatabaseInterface::class);
+        $joomlaDatabase = Factory::getContainer()->get(DatabaseInterface::class);
         $data['data'] = $this->getHelperFactory()
             ->getHelper('TeamStatisticsCounterHelper')
             ->getData($data['params'], $joomlaDatabase);
