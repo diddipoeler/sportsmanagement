9d2e75ce392a5ff2569a1128e8ac0f3b2d1cabc6
diddipoeler <diddipoeler@gmx.de>
2026-09-02 17:18:50 +0200

    Fix Joomla document lifecycle in site views

--- site/src/View/SportsManagementHtmlView.php
+++ site/src/View/SportsManagementHtmlView.php
@@ -1,9 +1,18 @@
 <?php
+/**
+ * Base Joomla 5/6 HTML view for the SportsManagement site application.
+ *
+ * @version    4.24.00
+ * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 namespace Diddipoeler\Component\SportsManagement\Site\View;
 
 \defined('_JEXEC') or die;
 
 use Joomla\CMS\Component\ComponentHelper;
+use Joomla\CMS\Document\Document;
 use Joomla\CMS\Factory;
 use Joomla\CMS\MVC\View\HtmlView;
 use Joomla\CMS\Uri\Uri;
@@ -26,8 +35,17 @@ abstract class SportsManagementHtmlView extends HtmlView
         $this->params = ComponentHelper::getParams($this->option);
         $this->uri = Uri::getInstance();
         $this->databaseSelector = $this->input->getInt('cfg_which_database', 0);
+    }
 
-        $this->getDocument()->getWebAssetManager()->registerAndUseScript(
+    /**
+     * Joomla injects the active document after the MVC factory has created the view.
+     * Register shared assets only after that injection has happened.
+     */
+    public function setDocument(Document $document): void
+    {
+        parent::setDocument($document);
+
+        $document->getWebAssetManager()->registerAndUseScript(
             'com_sportsmanagement.site.modal-image-popup',
             Uri::root(true) . '/components/com_sportsmanagement/assets/js/modal-image-popup.js',
             ['version' => 'auto'],
