91bbf58a2ad1f4858b15feb835f63980375cc847
diddipoeler <diddipoeler@gmx.de>
2026-09-02 16:50:40 +0200

    Complete Matches module entry PHPDoc metadata

--- modules/mod_sportsmanagement_matches/mod_sportsmanagement_matches.php
+++ modules/mod_sportsmanagement_matches/mod_sportsmanagement_matches.php
@@ -1,6 +1,12 @@
 <?php
 /**
- * Legacy entry point kept for package compatibility.
- * Joomla 5/6 boots this module through services/provider.php.
+ * Legacy entry point kept for Joomla 5/6 package compatibility.
+ *
+ * The active module is booted through services/provider.php and the namespaced dispatcher.
+ *
+ * @version   5.6.0
+ * @author    diddipoeler
+ * @copyright Copyright (C) diddipoeler
+ * @license   GNU General Public License version 2 or later; see LICENSE.txt
  */
 \defined('_JEXEC') or die;
