75226501b8eb6803d3260905061dcf55388ce58c
diddipoeler <diddipoeler@gmx.de>
2026-09-02 16:17:03 +0200

    Complete Count Rekord dispatcher metadata

--- modules/mod_sportsmanagement_count_rekord/src/Dispatcher/Dispatcher.php
+++ modules/mod_sportsmanagement_count_rekord/src/Dispatcher/Dispatcher.php
@@ -1,9 +1,18 @@
 <?php
+/**
+ * Joomla 5/6 dispatcher for mod_sportsmanagement_count_rekord.
+ *
+ * @version    4.24.00
+ * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 namespace Diddipoeler\Module\SportsManagementCountRekord\Site\Dispatcher;
 
 \defined('_JEXEC') or die;
 
 use Joomla\CMS\Dispatcher\AbstractModuleDispatcher;
+use Joomla\CMS\Factory;
 use Joomla\CMS\Helper\HelperFactoryAwareInterface;
 use Joomla\CMS\Helper\HelperFactoryAwareTrait;
 use Joomla\Database\DatabaseInterface;
@@ -20,10 +29,8 @@ final class Dispatcher extends AbstractModuleDispatcher implements HelperFactory
             return false;
         }
 
-        $app = $this->getApplication();
-
         /** @var DatabaseInterface $database */
-        $database = \Joomla\CMS\Factory::getContainer()->get(DatabaseInterface::class);
+        $database = Factory::getContainer()->get(DatabaseInterface::class);
         $data['list'] = $this->getHelperFactory()
             ->getHelper('CountRekordHelper')
             ->getData($data['params'], $data['module'], $database);
