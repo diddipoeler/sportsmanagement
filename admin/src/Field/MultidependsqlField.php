efb64f8e340f0354cc76b95e4f59e5e51a4174e3
diddipoeler <diddipoeler@gmx.de>
2026-09-02 16:12:16 +0200

    Document Joomla 5/6 multi dependent field

--- admin/src/Field/MultidependsqlField.php
+++ admin/src/Field/MultidependsqlField.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Joomla 5/6 replacement for the historical dependent multi-select field.
+ *
+ * @version    4.24.00
+ * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 namespace Diddipoeler\Component\SportsManagement\Administrator\Field;
 
 \defined('_JEXEC') or die;
