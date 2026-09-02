b73f8cfee4344d77e7e57f61b8ce80a0a4e0b23a
diddipoeler <diddipoeler@gmx.de>
2026-09-02 19:49:17 +0200

    Complete Playground Plan layout metadata

--- modules/mod_sportsmanagement_playgroundplan/tmpl/default.php
+++ modules/mod_sportsmanagement_playgroundplan/tmpl/default.php
@@ -1,5 +1,12 @@
 <?php
-/** Joomla 5/6 layout for the SportsManagement playground plan module. */
+/**
+ * Joomla 5/6 layout for the SportsManagement Playground Plan module.
+ *
+ * @version    4.24.00
+ * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 defined('_JEXEC') or die;
 
 use Joomla\CMS\HTML\HTMLHelper;
