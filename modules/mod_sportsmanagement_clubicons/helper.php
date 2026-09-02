3b0ebcf5962094c846ee212077f60eec3d52442f
diddipoeler <diddipoeler@gmx.de>
2026-09-02 21:25:01 +0200

    Add required metadata to clubicons legacy helper

--- modules/mod_sportsmanagement_clubicons/helper.php
+++ modules/mod_sportsmanagement_clubicons/helper.php
@@ -2,6 +2,11 @@
 /**
  * Legacy helper adapter for callers that still instantiate modJSMClubiconsHelper.
  * The active Joomla 5/6 implementation lives in src/Helper/ClubiconsHelper.php.
+ *
+ * @version    4.24.00
+ * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 \defined('_JEXEC') or die;
 
