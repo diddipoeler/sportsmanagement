52384a9bd0d3c99154230065dc6f2371c8d0d98f
diddipoeler <diddipoeler@gmx.de>
2026-09-02 16:58:53 +0200

    Complete EventsRanking dispatcher metadata

--- modules/mod_sportsmanagement_eventsranking/src/Dispatcher/Dispatcher.php
+++ modules/mod_sportsmanagement_eventsranking/src/Dispatcher/Dispatcher.php
@@ -1,9 +1,18 @@
 <?php
+/**
+ * Native Joomla 5/6 dispatcher for the events ranking module.
+ *
+ * @version   5.6.0
+ * @author    diddipoeler
+ * @copyright Copyright (C) diddipoeler
+ * @license   GNU General Public License version 2 or later; see LICENSE.txt
+ */
 namespace Diddipoeler\Module\SportsManagementEventsRanking\Site\Dispatcher;
 
 \defined('_JEXEC') or die;
 
 use Joomla\CMS\Dispatcher\AbstractModuleDispatcher;
+use Joomla\CMS\Factory;
 use Joomla\CMS\Helper\HelperFactoryAwareInterface;
 use Joomla\CMS\Helper\HelperFactoryAwareTrait;
 use Joomla\Database\DatabaseInterface;
@@ -24,7 +33,7 @@ final class Dispatcher extends AbstractModuleDispatcher implements HelperFactory
         $app->getLanguage()->load('com_sportsmanagement', JPATH_ADMINISTRATOR, null, true);
 
         /** @var DatabaseInterface $database */
-        $database = \Joomla\CMS\Factory::getContainer()->get(DatabaseInterface::class);
+        $database = Factory::getContainer()->get(DatabaseInterface::class);
         $data['rankingData'] = $this->getHelperFactory()
             ->getHelper('EventsRankingHelper')
             ->getData($data['params'], $app, $database);
