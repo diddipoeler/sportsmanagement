0861259a6c9c7e64406138aafc49ed330195506d
diddipoeler <diddipoeler@gmx.de>
2026-09-02 16:51:59 +0200

    Complete Matches native connector PHPDoc metadata

--- modules/mod_sportsmanagement_matches/connectors/native.php
+++ modules/mod_sportsmanagement_matches/connectors/native.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Joomla 5/6 native connector bootstrap for the matches module.
+ *
+ * @version   5.6.0
+ * @author    diddipoeler
+ * @copyright Copyright (C) diddipoeler
+ * @license   GNU General Public License version 2 or later; see LICENSE.txt
+ */
 namespace Diddipoeler\Module\SportsManagementMatches\Site\Helper;
 
 \defined('_JEXEC') or die;
