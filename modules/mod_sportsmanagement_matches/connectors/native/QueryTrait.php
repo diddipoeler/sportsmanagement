e23ad5c1892ef4ac44f376d74523ca5fff3584d4
diddipoeler <diddipoeler@gmx.de>
2026-09-02 16:52:57 +0200

    Complete Matches query trait PHPDoc metadata

--- modules/mod_sportsmanagement_matches/connectors/native/QueryTrait.php
+++ modules/mod_sportsmanagement_matches/connectors/native/QueryTrait.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Native Joomla 5/6 database query trait for the matches module.
+ *
+ * @version   5.6.0
+ * @author    diddipoeler
+ * @copyright Copyright (C) diddipoeler
+ * @license   GNU General Public License version 2 or later; see LICENSE.txt
+ */
 namespace Diddipoeler\Module\SportsManagementMatches\Site\Helper;
 
 \defined('_JEXEC') or die;
