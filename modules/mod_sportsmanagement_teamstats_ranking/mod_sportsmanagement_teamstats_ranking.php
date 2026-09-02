7df06b089b8204e47d958f14513e7f09d87495ac
diddipoeler <diddipoeler@gmx.de>
2026-09-02 19:38:08 +0200

    Complete Team Stats Ranking entry metadata

--- modules/mod_sportsmanagement_teamstats_ranking/mod_sportsmanagement_teamstats_ranking.php
+++ modules/mod_sportsmanagement_teamstats_ranking/mod_sportsmanagement_teamstats_ranking.php
@@ -2,5 +2,10 @@
 /**
  * Legacy entry point kept for Joomla package compatibility.
  * Joomla 5/6 dispatches this module through services/provider.php.
+ *
+ * @version    4.24.00
+ * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 \defined('_JEXEC') or die;
