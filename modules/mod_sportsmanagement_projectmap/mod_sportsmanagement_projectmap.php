e311bc807afb4923d619dfbc821dc495eb539ece
diddipoeler <diddipoeler@gmx.de>
2026-09-02 17:36:41 +0200

    Add Project Map entry PHP metadata

--- modules/mod_sportsmanagement_projectmap/mod_sportsmanagement_projectmap.php
+++ modules/mod_sportsmanagement_projectmap/mod_sportsmanagement_projectmap.php
@@ -1,6 +1,13 @@
 <?php
 /**
- * Legacy entry point kept for package compatibility.
- * Joomla 5/6 boots this module through services/provider.php.
+ * SportsManagement Project Map Joomla 5/6 module entry compatibility file.
+ *
+ * Joomla 5/6 boots the active module through services/provider.php and the
+ * namespaced module dispatcher.
+ *
+ * @version    4.24.00
+ * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 \defined('_JEXEC') or die;
