06911f828ee8f5df8655f2c193fc411ea0c8087a
diddipoeler <diddipoeler@gmx.de>
2026-09-03 11:41:50 +0200

    Add Joomla 5/6 metadata to TrainingsData entrypoint

--- modules/mod_sportsmanagement_trainingsdata/mod_sportsmanagement_trainingsdata.php
+++ modules/mod_sportsmanagement_trainingsdata/mod_sportsmanagement_trainingsdata.php
@@ -1,7 +1,12 @@
 <?php
 /**
- * SportsManagement Joomla 5/6 module entry compatibility file.
+ * SportsManagement TrainingsData Joomla 5/6 module entry compatibility file.
  *
  * The active module is booted by services/provider.php and the namespaced dispatcher.
+ *
+ * @version    3.8.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) 2015 diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 \defined('_JEXEC') or die;
