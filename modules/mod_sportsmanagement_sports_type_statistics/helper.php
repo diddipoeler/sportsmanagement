c9b73cb37904e6b4343d140fa6f57d38c84a313e
diddipoeler <diddipoeler@gmx.de>
2026-09-03 15:05:35 +0200

    Align Sports Type Statistics legacy facade with database DI

--- modules/mod_sportsmanagement_sports_type_statistics/helper.php
+++ modules/mod_sportsmanagement_sports_type_statistics/helper.php
@@ -10,8 +10,8 @@
 \defined('_JEXEC') or die;
 
 use Diddipoeler\Module\SportsManagementSportsTypeStatistics\Site\Helper\SportsTypeStatisticsHelper;
-use Joomla\CMS\Application\SiteApplication;
 use Joomla\CMS\Factory;
+use Joomla\Database\DatabaseInterface;
 use Joomla\Registry\Registry;
 
 if (!class_exists(SportsTypeStatisticsHelper::class)) {
@@ -24,10 +24,9 @@ if (!class_exists('modJSMSportsHelper', false)) {
         public static function getData(&$params): array
         {
             $registry = $params instanceof Registry ? $params : new Registry((array) $params);
-            $container = Factory::getContainer();
-            /** @var SiteApplication $app */
-            $app = $container->get(SiteApplication::class);
-            $data = (new SportsTypeStatisticsHelper())->getData($registry, $app);
+            /** @var DatabaseInterface $database */
+            $database = Factory::getContainer()->get(DatabaseInterface::class);
+            $data = (new SportsTypeStatisticsHelper())->getData($registry, $database);
             $sportTypeId = (int) $registry->get('sportstypes', 0);
             $legacy = ['sportstype' => []];
 
