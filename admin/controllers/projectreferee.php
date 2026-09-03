3c1ecde0b74dd7563d53dbb3ea525ae01d1d62b4
diddipoeler <diddipoeler@gmx.de>
2026-09-03 19:55:46 +0200

    Continue Joomla 5/6 project and count record bridge metadata

--- admin/controllers/projectreferee.php
+++ admin/controllers/projectreferee.php
@@ -1,5 +1,12 @@
 <?php
-/** Legacy compatibility bridge for the native project referee controller. */
+/**
+ * Legacy compatibility bridge for the native Joomla 5/6 administrator Projectreferee controller.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 defined('_JEXEC') or die('Restricted access');
 
 use Diddipoeler\Component\SportsManagement\Administrator\Controller\ProjectrefereeController;
