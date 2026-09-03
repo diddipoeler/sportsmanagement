0e4e36dc8412aec3b08fce6f295522cfe050f6e3
diddipoeler <diddipoeler@gmx.de>
2026-09-03 11:50:57 +0200

    Add Joomla 5/6 metadata to clubname model bridge

--- admin/models/clubname.php
+++ admin/models/clubname.php
@@ -1,10 +1,12 @@
 <?php
 /**
- * SportsManagement legacy compatibility bridge.
+ * Legacy compatibility bridge for the native administrator Clubname model.
  *
- * The active Joomla 5/6 implementation lives in admin/src/Model/ClubnameModel.php.
+ * @version    4.24.00
+ * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
-
 defined('_JEXEC') or die('Restricted access');
 
 use Diddipoeler\Component\SportsManagement\Administrator\Model\ClubnameModel;
