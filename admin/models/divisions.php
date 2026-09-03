92873bd2192cd7670ee19653083d61faa624d083
diddipoeler <diddipoeler@gmx.de>
2026-09-03 12:03:13 +0200

    Add Joomla 5/6 divisions model metadata

--- admin/models/divisions.php
+++ admin/models/divisions.php
@@ -1,10 +1,12 @@
 <?php
 /**
- * SportsManagement legacy compatibility bridge.
+ * SportsManagement legacy compatibility bridge for the native administrator Divisions model.
  *
- * The active Joomla 5/6 implementation lives in admin/src/Model/DivisionsModel.php.
+ * @version    4.24.00
+ * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
-
 defined('_JEXEC') or die('Restricted access');
 
 use Diddipoeler\Component\SportsManagement\Administrator\Model\DivisionsModel;
