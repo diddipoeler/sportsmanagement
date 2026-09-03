78048412a5a5fe24f961aa72be6853a620b60030
diddipoeler <diddipoeler@gmx.de>
2026-09-03 11:03:27 +0200

    Add Joomla 5/6 metadata to image select bridge

--- site/helpers/imageselect.php
+++ site/helpers/imageselect.php
@@ -1,6 +1,11 @@
 <?php
 /**
  * SportsManagement legacy image-select compatibility bridge.
+ *
+ * @version    4.24.00
+ * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 \defined('_JEXEC') or die('Restricted access');
 
