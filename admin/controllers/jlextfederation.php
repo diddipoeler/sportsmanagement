3b5a7543b09a431bcb88e0a347da17e67cce5014
diddipoeler <diddipoeler@gmx.de>
2026-09-03 21:37:22 +0200

    Continue Joomla 5/6 jlext CRUD and ranking metadata

--- admin/controllers/jlextfederation.php
+++ admin/controllers/jlextfederation.php
@@ -1,5 +1,12 @@
 <?php
-/** Legacy compatibility bridge for the native administrator federation controller. */
+/**
+ * Legacy compatibility bridge for the native Joomla 5/6 administrator Jlextfederation controller.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 defined('_JEXEC') or die('Restricted access');
 
 use Diddipoeler\Component\SportsManagement\Administrator\Controller\JlextfederationController;
--- admin/controllers/jlextindividualsport.php
+++ admin/controllers/jlextindividualsport.php
@@ -1,5 +1,12 @@
 <?php
-/** Legacy compatibility bridge for the native administrator individual-sport controller. */
+/**
+ * Legacy compatibility bridge for the native Joomla 5/6 administrator Jlextindividualsport controller.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 defined('_JEXEC') or die('Restricted access');
 
 use Diddipoeler\Component\SportsManagement\Administrator\Controller\JlextindividualsportController;
