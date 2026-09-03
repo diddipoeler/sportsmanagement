5beae86dc57289ede5fc79512526db95b0698057
diddipoeler <diddipoeler@gmx.de>
2026-09-03 21:49:20 +0200

    Complete Joomla 5/6 jlext import controller metadata

--- admin/controllers/jlextprofleagimport.php
+++ admin/controllers/jlextprofleagimport.php
@@ -1,5 +1,12 @@
 <?php
-/** Legacy compatibility bridge for the native ProfiLeague import controller. */
+/**
+ * Legacy compatibility bridge for the native Joomla 5/6 ProfiLeague import controller.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 defined('_JEXEC') or die('Restricted access');
 
 use Diddipoeler\Component\SportsManagement\Administrator\Controller\JlextprofleagimportController;
