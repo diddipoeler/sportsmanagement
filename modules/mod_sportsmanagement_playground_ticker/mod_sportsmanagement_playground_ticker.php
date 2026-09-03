aa91485f1dd0aa8a8c1de7fce4b96cae6ce5c1cf
diddipoeler <diddipoeler@gmx.de>
2026-09-03 08:45:30 +0200

    Add required metadata to playground ticker entry

--- modules/mod_sportsmanagement_playground_ticker/mod_sportsmanagement_playground_ticker.php
+++ modules/mod_sportsmanagement_playground_ticker/mod_sportsmanagement_playground_ticker.php
@@ -3,5 +3,10 @@
  * SportsManagement Joomla 5/6 module entry compatibility file.
  *
  * The active module is booted by services/provider.php and the namespaced dispatcher.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 \defined('_JEXEC') or die;
