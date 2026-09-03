b240599ee80c276d8075b8bb7736d5dade3dadc7
diddipoeler <diddipoeler@gmx.de>
2026-09-03 18:09:19 +0200

    Add Joomla 5/6 metadata to JSON feed bridge

--- site/models/jsonfeed.php
+++ site/models/jsonfeed.php
@@ -1,8 +1,11 @@
 <?php
 /**
- * SportsManagement legacy compatibility bridge.
+ * SportsManagement legacy compatibility bridge for the native Jsonfeed model.
  *
- * The active Joomla 5/6 implementation lives in site/src/Model/JsonfeedModel.php.
+ * @version    4.24.00
+ * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 
 defined('_JEXEC') or die('Restricted access');
