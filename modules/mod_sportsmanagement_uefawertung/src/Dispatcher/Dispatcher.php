46b030b325d4ea97819da1d992f7ca5a19b011ba
diddipoeler <diddipoeler@gmx.de>
2026-09-03 11:52:00 +0200

    Modernize Joomla 5/6 UEFA dispatcher facade

--- modules/mod_sportsmanagement_uefawertung/src/Dispatcher/Dispatcher.php
+++ modules/mod_sportsmanagement_uefawertung/src/Dispatcher/Dispatcher.php
@@ -1,9 +1,18 @@
 <?php
+/**
+ * Joomla 5/6 dispatcher for the SportsManagement UEFA ranking module.
+ *
+ * @version    3.8.0
+ * @author     diddipoeler
+ * @copyright  (C) 2015-2026
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 namespace Diddipoeler\Module\SportsManagementUefaWertung\Site\Dispatcher;
 
 \defined('_JEXEC') or die;
 
 use Joomla\CMS\Dispatcher\AbstractModuleDispatcher;
+use Joomla\CMS\Factory;
 use Joomla\CMS\Helper\HelperFactoryAwareInterface;
 use Joomla\CMS\Helper\HelperFactoryAwareTrait;
 use Joomla\Database\DatabaseInterface;
@@ -19,7 +28,7 @@ final class Dispatcher extends AbstractModuleDispatcher implements HelperFactory
         $app->getLanguage()->load('com_sportsmanagement', JPATH_SITE, null, true);
 
         /** @var DatabaseInterface $database */
-        $database = \Joomla\CMS\Factory::getContainer()->get(DatabaseInterface::class);
+        $database = Factory::getContainer()->get(DatabaseInterface::class);
         $result = $this->getHelperFactory()
             ->getHelper('UefaWertungHelper')
             ->getData($data['params'], $app, $database);
