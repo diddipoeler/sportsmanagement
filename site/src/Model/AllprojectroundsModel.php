e798187be494f812cf4dc8a26358e67316d6d330
diddipoeler <diddipoeler@gmx.de>
2026-09-03 09:12:11 +0200

    Add required metadata to all project rounds model

--- site/src/Model/AllprojectroundsModel.php
+++ site/src/Model/AllprojectroundsModel.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Joomla 5/6 model for the public all-project-rounds view.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 namespace Diddipoeler\Component\SportsManagement\Site\Model;
 
 \defined('_JEXEC') or die;
