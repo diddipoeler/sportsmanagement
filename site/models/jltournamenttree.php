553d36f864c20e24e6b96458bde1e54dd4d8de23
diddipoeler <diddipoeler@gmx.de>
2026-09-03 18:25:54 +0200

    Add Joomla 5/6 metadata to tournament tree bridge

--- site/models/jltournamenttree.php
+++ site/models/jltournamenttree.php
@@ -1,5 +1,12 @@
 <?php
-/** Legacy model bridge for Joomla 5/6. */
+/**
+ * Legacy model bridge for the native Joomla 5/6 tournament tree model.
+ *
+ * @version    4.24.00
+ * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 defined('_JEXEC') or die('Restricted access');
 
 require_once JPATH_SITE . '/components/com_sportsmanagement/src/Model/SportsManagementModel.php';
