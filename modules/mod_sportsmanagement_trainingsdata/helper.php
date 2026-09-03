d5cb452f387c720a7336065273a009490770db25
diddipoeler <diddipoeler@gmx.de>
2026-09-03 11:41:57 +0200

    Fix Joomla 5/6 TrainingsData legacy helper bridge

--- modules/mod_sportsmanagement_trainingsdata/helper.php
+++ modules/mod_sportsmanagement_trainingsdata/helper.php
@@ -1,10 +1,17 @@
 <?php
 /**
- * SportsManagement legacy helper bridge for third-party template overrides.
+ * SportsManagement TrainingsData legacy helper bridge for Joomla 5/6.
+ *
+ * @version    3.8.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) 2015 diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 \defined('_JEXEC') or die;
 
 use Diddipoeler\Module\SportsManagementTrainingsData\Site\Helper\TrainingsDataHelper;
+use Joomla\CMS\Factory;
+use Joomla\Database\DatabaseInterface;
 use Joomla\Registry\Registry;
 
 if (!class_exists(TrainingsDataHelper::class)) {
@@ -17,8 +24,10 @@ if (!class_exists('modJSMTrainingsData', false)) {
         public static function getData($params): array
         {
             $registry = $params instanceof Registry ? $params : new Registry((array) $params);
+            /** @var DatabaseInterface $database */
+            $database = Factory::getContainer()->get(DatabaseInterface::class);
 
-            return (new TrainingsDataHelper())->getData($registry);
+            return (new TrainingsDataHelper())->getData($registry, $database);
         }
     }
 }
