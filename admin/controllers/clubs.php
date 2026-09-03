a85e5df9fd6922ae2fb6bc7d4fababf7c6611900
diddipoeler <diddipoeler@gmx.de>
2026-09-03 18:59:29 +0200

    Extend Joomla 5/6 controller bridge metadata coverage

--- admin/controllers/clubs.php
+++ admin/controllers/clubs.php
@@ -1,5 +1,12 @@
 <?php
-/** Legacy compatibility bridge for the native administrator Clubs controller. */
+/**
+ * Legacy compatibility bridge for the native Joomla 5/6 administrator Clubs controller.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 defined('_JEXEC') or die('Restricted access');
 
 use Diddipoeler\Component\SportsManagement\Administrator\Controller\ClubsController;
