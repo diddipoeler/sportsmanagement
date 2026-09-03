523e844795707a6076d0fbbc4dcf44867bd76267
diddipoeler <diddipoeler@gmx.de>
2026-09-03 09:25:13 +0200

    Add metadata to sports type statistics dispatcher

--- modules/mod_sportsmanagement_sports_type_statistics/src/Dispatcher/Dispatcher.php
+++ modules/mod_sportsmanagement_sports_type_statistics/src/Dispatcher/Dispatcher.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 namespace Diddipoeler\Module\SportsManagementSportsTypeStatistics\Site\Dispatcher;
 
 \defined('_JEXEC') or die;
