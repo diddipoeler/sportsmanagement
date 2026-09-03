77965b26ef6b5ec82d69e8ea193074da6a8c6e75
diddipoeler <diddipoeler@gmx.de>
2026-09-03 11:51:34 +0200

    Add Joomla 5/6 metadata to UEFA module entry

--- modules/mod_sportsmanagement_uefawertung/mod_sportsmanagement_uefawertung.php
+++ modules/mod_sportsmanagement_uefawertung/mod_sportsmanagement_uefawertung.php
@@ -3,5 +3,10 @@
  * Joomla 5/6 compatibility entry point for the SportsManagement UEFA ranking module.
  *
  * The active implementation is booted by services/provider.php.
+ *
+ * @version    3.8.0
+ * @author     diddipoeler
+ * @copyright  (C) 2015-2026
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 \defined('_JEXEC') or die;
