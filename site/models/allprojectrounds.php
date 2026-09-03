3b36515bb06c042b31396a3a9ce78da6a2cd7566
diddipoeler <diddipoeler@gmx.de>
2026-09-03 15:43:14 +0200

    Add Joomla 5/6 metadata to all project rounds bridge

--- site/models/allprojectrounds.php
+++ site/models/allprojectrounds.php
@@ -1,7 +1,11 @@
 <?php
 /**
- * SportsManagement legacy compatibility bridge.
- * The active Joomla 5/6 implementation lives in site/src/Model/AllprojectroundsModel.php.
+ * SportsManagement legacy compatibility bridge for the native Allprojectrounds model.
+ *
+ * @version    4.24.00
+ * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 \defined('_JEXEC') or die;
 
