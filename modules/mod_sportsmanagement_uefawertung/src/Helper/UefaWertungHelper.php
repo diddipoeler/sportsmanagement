e344b91f90a858462d5737c2b4141a7ed20ead62
diddipoeler <diddipoeler@gmx.de>
2026-09-03 11:52:16 +0200

    Add Joomla 5/6 metadata to UEFA native helper

--- modules/mod_sportsmanagement_uefawertung/src/Helper/UefaWertungHelper.php
+++ modules/mod_sportsmanagement_uefawertung/src/Helper/UefaWertungHelper.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Joomla 5/6 data helper for the SportsManagement UEFA ranking module.
+ *
+ * @version    3.8.0
+ * @author     diddipoeler
+ * @copyright  (C) 2015-2026
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 namespace Diddipoeler\Module\SportsManagementUefaWertung\Site\Helper;
 
 \defined('_JEXEC') or die;
