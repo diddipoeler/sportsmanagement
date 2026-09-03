f3c0bfc6df2a7f2c29d85f31ade4029a6d5a2e29
diddipoeler <diddipoeler@gmx.de>
2026-09-03 11:51:51 +0200

    Add Joomla 5/6 metadata to UEFA service provider

--- modules/mod_sportsmanagement_uefawertung/services/provider.php
+++ modules/mod_sportsmanagement_uefawertung/services/provider.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Joomla 5/6 service provider for the SportsManagement UEFA ranking module.
+ *
+ * @version    3.8.0
+ * @author     diddipoeler
+ * @copyright  (C) 2015-2026
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 \defined('_JEXEC') or die;
 
 use Joomla\CMS\Extension\Service\Provider\HelperFactory;
