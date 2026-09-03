5126062a2ddc6c077f00b5b57a78d079cd24d4a0
diddipoeler <diddipoeler@gmx.de>
2026-09-03 10:56:22 +0200

    Extend Joomla 5/6 legacy site bridge coverage

--- site/models/allteams.php
+++ site/models/allteams.php
@@ -3,6 +3,11 @@
  * SportsManagement legacy compatibility bridge.
  *
  * The active Joomla 5/6 implementation lives in site/src/Model/AllteamsModel.php.
+ *
+ * @version    4.24.00
+ * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 
 defined('_JEXEC') or die('Restricted access');
