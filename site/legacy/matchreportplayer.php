a2ba9858e5974909fc213ce7fccb6c66ba1906e9
diddipoeler <diddipoeler@gmx.de>
2026-09-03 11:40:42 +0200

    Add Joomla 5/6 metadata to match report player bridge

--- site/legacy/matchreportplayer.php
+++ site/legacy/matchreportplayer.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Legacy match-report player model bridge for Joomla 5/6.
+ *
+ * @version    4.24.00
+ * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 \defined('_JEXEC') or die;
 
 // Keep the historical match-report import path, but expose the same complete
