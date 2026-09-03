6fa4e4bfba98f3bf8c6609ecb757b7c858aef417
diddipoeler <diddipoeler@gmx.de>
2026-09-03 11:42:15 +0200

    Modernize Joomla 5/6 TrainingsData dispatcher metadata

--- modules/mod_sportsmanagement_trainingsdata/src/Dispatcher/Dispatcher.php
+++ modules/mod_sportsmanagement_trainingsdata/src/Dispatcher/Dispatcher.php
@@ -1,10 +1,19 @@
 <?php
+/**
+ * Joomla 5/6 dispatcher for the SportsManagement TrainingsData module.
+ *
+ * @version    3.8.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) 2015 diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 namespace Diddipoeler\Module\SportsManagementTrainingsData\Site\Dispatcher;
 
 \defined('_JEXEC') or die;
 
 use Joomla\CMS\Dispatcher\AbstractModuleDispatcher;
 use Joomla\CMS\Document\HtmlDocument;
+use Joomla\CMS\Factory;
 use Joomla\CMS\Helper\HelperFactoryAwareInterface;
 use Joomla\CMS\Helper\HelperFactoryAwareTrait;
 use Joomla\Database\DatabaseInterface;
@@ -20,7 +29,7 @@ final class Dispatcher extends AbstractModuleDispatcher implements HelperFactory
 
         $app->getLanguage()->load('com_sportsmanagement', JPATH_SITE, null, true);
         /** @var DatabaseInterface $database */
-        $database = \Joomla\CMS\Factory::getContainer()->get(DatabaseInterface::class);
+        $database = Factory::getContainer()->get(DatabaseInterface::class);
         $data['trainingsdata'] = $this->getHelperFactory()
             ->getHelper('TrainingsDataHelper')
             ->getData($data['params'], $database);
