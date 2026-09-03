b525a39e8099f23c5c7b7e3fe7476d31e5ae4f77
diddipoeler <diddipoeler@gmx.de>
2026-09-03 07:56:52 +0200

    Use native prediction ranking routing

--- site/src/Controller/PredictionrankingController.php
+++ site/src/Controller/PredictionrankingController.php
@@ -11,11 +11,10 @@ namespace Diddipoeler\Component\SportsManagement\Site\Controller;
 
 \defined('_JEXEC') or die;
 
+use Diddipoeler\Component\SportsManagement\Site\Helper\PredictionRouteHelper;
 use Diddipoeler\Component\SportsManagement\Site\Model\PredictionrankingModel;
 use Joomla\CMS\MVC\Controller\BaseController;
-use Joomla\CMS\Router\Route;
 use Joomla\CMS\Session\Session;
-use Joomla\CMS\Uri\Uri;
 
 final class PredictionrankingController extends BaseController
 {
@@ -30,20 +29,16 @@ final class PredictionrankingController extends BaseController
             throw new \RuntimeException('Prediction ranking model not available.', 500);
         }
 
-        $query = Uri::buildQuery([
-            'option' => 'com_sportsmanagement',
-            'view' => 'predictionranking',
-            'cfg_which_database' => $this->input->getInt('cfg_which_database', 0),
-            'prediction_id' => $model->getPredictionGameId(),
-            'pj' => $model->getProjectId(),
-            'r' => $model->getRoundId(),
-            'pggroup' => $model->getGroupId(),
-            'pggrouprank' => $model->getGroupRanking(),
-            'type' => $model->getRankingType(),
-            'from' => $model->getFromRoundId(),
-            'to' => $model->getToRoundId(),
-        ]);
-
-        $this->setRedirect(Route::_('index.php?' . $query, false));
+        $this->setRedirect(PredictionRouteHelper::ranking(
+            $model->getPredictionGameId(),
+            $model->getProjectId(),
+            $model->getRoundId(),
+            $model->getGroupId(),
+            $model->getGroupRanking(),
+            $model->getRankingType(),
+            $model->getFromRoundId(),
+            $model->getToRoundId(),
+            $this->input->getInt('cfg_which_database', 0)
+        ));
     }
 }
