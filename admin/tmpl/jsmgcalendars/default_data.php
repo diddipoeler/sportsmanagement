d7ae5539b2a0d67d892396617d68a6e1907c2cf9
diddipoeler <diddipoeler@gmx.de>
2026-09-04 20:29:47 +0200

    Normalize Joomla 5/6 cPanel template partial metadata

--- admin/tmpl/jsmgcalendars/default_data.php
+++ admin/tmpl/jsmgcalendars/default_data.php
@@ -1,5 +1,12 @@
 <?php
-/** Joomla 5/6 Google calendars table rows. */
+/**
+ * Native Joomla 5/6 Google calendars table rows.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 defined('_JEXEC') or die;
 
 use Joomla\CMS\HTML\HTMLHelper;
--- admin/tmpl/predictiongames/default_projects.php
+++ admin/tmpl/predictiongames/default_projects.php
@@ -1,5 +1,12 @@
 <?php
-/** Native Joomla 5/6 prediction-game project relations. */
+/**
+ * Native Joomla 5/6 prediction-game project relations.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 \defined('_JEXEC') or die;
 
 use Joomla\CMS\HTML\HTMLHelper;
