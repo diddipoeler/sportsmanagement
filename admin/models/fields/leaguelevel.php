46a7c0a17b7108a57ae16b8524f12e0e4fb4623b
diddipoeler <diddipoeler@gmx.de>
2026-09-04 21:03:16 +0200

    Harden Joomla 5/6 project and league level fields

--- admin/models/fields/leaguelevel.php
+++ admin/models/fields/leaguelevel.php
@@ -1,19 +1,28 @@
 <?php
 /**
- * Legacy compatibility bridge for the SportsManagement league-level field.
+ * Legacy compatibility bridge for the native Joomla 5/6 league-level field.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
-defined('_JEXEC') or die('Restricted access');
+\defined('_JEXEC') or die;
 
 use Diddipoeler\Component\SportsManagement\Administrator\Field\LeaguelevelField;
 
 if (!class_exists(LeaguelevelField::class)) {
-    $fieldFile = JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Field/LeaguelevelField.php';
+    $nativeField = JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Field/LeaguelevelField.php';
 
-    if (is_file($fieldFile)) {
-        require_once $fieldFile;
+    if (is_file($nativeField)) {
+        require_once $nativeField;
     }
 }
 
-if (class_exists(LeaguelevelField::class) && !class_exists('JFormFieldLeagueLevel', false)) {
+if (!class_exists(LeaguelevelField::class)) {
+    throw new \RuntimeException('SportsManagement native Leaguelevel field could not be loaded.', 500);
+}
+
+if (!class_exists('JFormFieldLeagueLevel', false)) {
     class_alias(LeaguelevelField::class, 'JFormFieldLeagueLevel');
 }
--- admin/models/fields/projectlist.php
+++ admin/models/fields/projectlist.php
@@ -1,17 +1,28 @@
 <?php
-/** Compatibility bridge for the Joomla 5/6 project list field. */
-defined('_JEXEC') or die('Restricted access');
+/**
+ * Legacy compatibility bridge for the native Joomla 5/6 project list field.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
+\defined('_JEXEC') or die;
 
 use Diddipoeler\Component\SportsManagement\Administrator\Field\ProjectlistField;
 
 if (!class_exists(ProjectlistField::class)) {
-    $fieldFile = JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Field/ProjectlistField.php';
+    $nativeField = JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Field/ProjectlistField.php';
 
-    if (is_file($fieldFile)) {
-        require_once $fieldFile;
+    if (is_file($nativeField)) {
+        require_once $nativeField;
     }
 }
 
-if (class_exists(ProjectlistField::class) && !class_exists('JFormFieldprojectlist', false)) {
+if (!class_exists(ProjectlistField::class)) {
+    throw new \RuntimeException('SportsManagement native Projectlist field could not be loaded.', 500);
+}
+
+if (!class_exists('JFormFieldprojectlist', false)) {
     class_alias(ProjectlistField::class, 'JFormFieldprojectlist');
 }
