5de8d41e8bcf79d70b3ce09f0dacdb759e5b8845
diddipoeler <diddipoeler@gmx.de>
2026-09-04 16:36:46 +0200

    Continue Joomla 5/6 prediction users age group and playground plan migration

--- modules/mod_sportsmanagement_playgroundplan/helper.php
+++ modules/mod_sportsmanagement_playgroundplan/helper.php
@@ -1,18 +1,26 @@
 <?php
 /**
- * Legacy helper bridge for the Joomla 5/6 SportsManagement Playground Plan module.
+ * Legacy compatibility bridge for the native Joomla 5/6 playground plan module helper.
  *
- * @version    4.24.00
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
-defined('_JEXEC') or die;
+\defined('_JEXEC') or die;
 
 use Diddipoeler\Module\SportsManagementPlaygroundPlan\Site\Helper\PlaygroundPlanHelper;
 
 if (!class_exists(PlaygroundPlanHelper::class)) {
-    require_once __DIR__ . '/src/Helper/PlaygroundPlanHelper.php';
+    $nativeHelper = __DIR__ . '/src/Helper/PlaygroundPlanHelper.php';
+
+    if (is_file($nativeHelper)) {
+        require_once $nativeHelper;
+    }
+}
+
+if (!class_exists(PlaygroundPlanHelper::class)) {
+    throw new \RuntimeException('SportsManagement native PlaygroundPlan module helper could not be loaded.', 500);
 }
 
 if (!class_exists('modSportsmanagementPlaygroundplanHelper', false)) {
