6747ac5673e53e19a1834fc239407ba7c52ec63a
diddipoeler <diddipoeler@gmx.de>
2026-09-04 22:16:08 +0200

    Harden Joomla 5/6 season project team and age group fields

--- admin/models/fields/seasonlist.php
+++ admin/models/fields/seasonlist.php
@@ -1,10 +1,26 @@
 <?php
+/**
+ * Legacy compatibility bridge for the native Joomla 5/6 season list field.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 \defined('_JEXEC') or die;
 
 use Diddipoeler\Component\SportsManagement\Administrator\Field\SeasonlistField;
 
 if (!class_exists(SeasonlistField::class)) {
-    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Field/SeasonlistField.php';
+    $nativeField = JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Field/SeasonlistField.php';
+
+    if (is_file($nativeField)) {
+        require_once $nativeField;
+    }
+}
+
+if (!class_exists(SeasonlistField::class)) {
+    throw new \RuntimeException('SportsManagement native Seasonlist field could not be loaded.', 500);
 }
 
 if (!class_exists('JFormFieldseasonlist', false)) {
