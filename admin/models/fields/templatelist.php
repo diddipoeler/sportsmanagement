31903c22cf834b1ef8eeb037ac5ce6234f8bce5b
diddipoeler <diddipoeler@gmx.de>
2026-09-04 22:06:47 +0200

    Harden Joomla 5/6 template and native list fields

--- admin/models/fields/templatelist.php
+++ admin/models/fields/templatelist.php
@@ -1,17 +1,28 @@
 <?php
-/** Compatibility bridge for the Joomla 5/6 template list field. */
-defined('_JEXEC') or die('Restricted access');
+/**
+ * Legacy compatibility bridge for the native Joomla 5/6 template list field.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
+\defined('_JEXEC') or die;
 
 use Diddipoeler\Component\SportsManagement\Administrator\Field\TemplatelistField;
 
 if (!class_exists(TemplatelistField::class)) {
-    $fieldFile = JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Field/TemplatelistField.php';
+    $nativeField = JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Field/TemplatelistField.php';
 
-    if (is_file($fieldFile)) {
-        require_once $fieldFile;
+    if (is_file($nativeField)) {
+        require_once $nativeField;
     }
 }
 
-if (class_exists(TemplatelistField::class) && !class_exists('JFormFieldtemplatelist', false)) {
+if (!class_exists(TemplatelistField::class)) {
+    throw new \RuntimeException('SportsManagement native Templatelist field could not be loaded.', 500);
+}
+
+if (!class_exists('JFormFieldtemplatelist', false)) {
     class_alias(TemplatelistField::class, 'JFormFieldtemplatelist');
 }
