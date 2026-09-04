0a90aecc8680a1fa755f9a2c593629b06bc5179e
diddipoeler <diddipoeler@gmx.de>
2026-09-04 21:14:34 +0200

    Harden Joomla 5/6 project division and statistics fields

--- admin/models/fields/projectdivisionlist.php
+++ admin/models/fields/projectdivisionlist.php
@@ -1,19 +1,28 @@
 <?php
 /**
- * Legacy compatibility bridge for the SportsManagement project-division field.
+ * Legacy compatibility bridge for the native Joomla 5/6 project-division field.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
-defined('_JEXEC') or die('Restricted access');
+\defined('_JEXEC') or die;
 
 use Diddipoeler\Component\SportsManagement\Administrator\Field\ProjectdivisionField;
 
 if (!class_exists(ProjectdivisionField::class)) {
-    $fieldFile = JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Field/ProjectdivisionField.php';
+    $nativeField = JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Field/ProjectdivisionField.php';
 
-    if (is_file($fieldFile)) {
-        require_once $fieldFile;
+    if (is_file($nativeField)) {
+        require_once $nativeField;
     }
 }
 
-if (class_exists(ProjectdivisionField::class) && !class_exists('JFormFieldprojectdivisionlist', false)) {
+if (!class_exists(ProjectdivisionField::class)) {
+    throw new \RuntimeException('SportsManagement native Projectdivision field could not be loaded.', 500);
+}
+
+if (!class_exists('JFormFieldprojectdivisionlist', false)) {
     class_alias(ProjectdivisionField::class, 'JFormFieldprojectdivisionlist');
 }
