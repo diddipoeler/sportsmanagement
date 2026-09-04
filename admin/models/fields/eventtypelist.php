0ca0ec7cb905b4009bedcc10c8b9e1cac8b65f8e
diddipoeler <diddipoeler@gmx.de>
2026-09-04 22:33:16 +0200

    Harden Joomla 5/6 event and sports type fields

--- admin/models/fields/eventtypelist.php
+++ admin/models/fields/eventtypelist.php
@@ -1,10 +1,26 @@
 <?php
+/**
+ * Legacy compatibility bridge for the native Joomla 5/6 event type list field.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 \defined('_JEXEC') or die;
 
 use Diddipoeler\Component\SportsManagement\Administrator\Field\EventtypelistField;
 
 if (!class_exists(EventtypelistField::class)) {
-    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Field/EventtypelistField.php';
+    $nativeField = JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Field/EventtypelistField.php';
+
+    if (is_file($nativeField)) {
+        require_once $nativeField;
+    }
+}
+
+if (!class_exists(EventtypelistField::class)) {
+    throw new \RuntimeException('SportsManagement native Eventtypelist field could not be loaded.', 500);
 }
 
 if (!class_exists('JFormFieldeventtypelist', false)) {
