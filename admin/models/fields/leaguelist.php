691838b8deb235a2c5dc69c030a9498090823aa6
diddipoeler <diddipoeler@gmx.de>
2026-09-04 22:47:07 +0200

    Harden Joomla 5/6 league matchday and associations fields

--- admin/models/fields/leaguelist.php
+++ admin/models/fields/leaguelist.php
@@ -1,10 +1,26 @@
 <?php
+/**
+ * Legacy compatibility bridge for the native Joomla 5/6 league list field.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 \defined('_JEXEC') or die;
 
 use Diddipoeler\Component\SportsManagement\Administrator\Field\LeaguelistField;
 
 if (!class_exists(LeaguelistField::class)) {
-    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Field/LeaguelistField.php';
+    $nativeField = JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Field/LeaguelistField.php';
+
+    if (is_file($nativeField)) {
+        require_once $nativeField;
+    }
+}
+
+if (!class_exists(LeaguelistField::class)) {
+    throw new \RuntimeException('SportsManagement native Leaguelist field could not be loaded.', 500);
 }
 
 if (!class_exists('JFormFieldleaguelist', false)) {
--- admin/models/fields/matchdaylist.php
+++ admin/models/fields/matchdaylist.php
@@ -1,10 +1,26 @@
 <?php
+/**
+ * Legacy compatibility bridge for the native Joomla 5/6 matchday list field.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 \defined('_JEXEC') or die;
 
 use Diddipoeler\Component\SportsManagement\Administrator\Field\MatchdaylistField;
 
 if (!class_exists(MatchdaylistField::class)) {
-    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Field/MatchdaylistField.php';
+    $nativeField = JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Field/MatchdaylistField.php';
+
+    if (is_file($nativeField)) {
+        require_once $nativeField;
+    }
+}
+
+if (!class_exists(MatchdaylistField::class)) {
+    throw new \RuntimeException('SportsManagement native Matchdaylist field could not be loaded.', 500);
 }
 
 if (!class_exists('JFormFieldMatchdaylist', false)) {
