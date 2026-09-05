3a389fdd3188a6e1c47d76200ca3e96e0144c46e
diddipoeler <diddipoeler@gmx.de>
2026-09-05 22:04:38 +0200

    Fix Joomla 5/6 AJAX navigation namespace registration

--- modules/mod_sportsmanagement_ajax_top_navigation_menu/services/provider.php
+++ modules/mod_sportsmanagement_ajax_top_navigation_menu/services/provider.php
@@ -20,10 +20,10 @@ return new class implements ServiceProviderInterface
     public function register(Container $container): void
     {
         $container->registerServiceProvider(
-            new ModuleDispatcherFactory('\\Diddipoeler\\Module\\SportsManagementAjaxTopNavigationMenu')
+            new ModuleDispatcherFactory('Diddipoeler\\Module\\SportsManagementAjaxTopNavigationMenu')
         );
         $container->registerServiceProvider(
-            new HelperFactory('\\Diddipoeler\\Module\\SportsManagementAjaxTopNavigationMenu\\Site\\Helper')
+            new HelperFactory('Diddipoeler\\Module\\SportsManagementAjaxTopNavigationMenu\\Site\\Helper')
         );
         $container->registerServiceProvider(new Module());
     }
