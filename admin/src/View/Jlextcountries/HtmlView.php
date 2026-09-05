be8ca1eaecdec79e7c6b8ec58707e6213a9935df
diddipoeler <diddipoeler@gmx.de>
2026-09-05 20:34:40 +0200

    Add Joomla 5/6 metadata to country list view

--- admin/src/View/Jlextcountries/HtmlView.php
+++ admin/src/View/Jlextcountries/HtmlView.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Native Joomla 5/6 administrator list view for countries.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 namespace Diddipoeler\Component\SportsManagement\Administrator\View\Jlextcountries;
 
 \defined('_JEXEC') or die;
