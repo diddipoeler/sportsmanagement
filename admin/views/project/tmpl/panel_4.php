b29939b95fe66b64784c7a3741e2737c9c329b56
diddipoeler <diddipoeler@gmx.de>
2026-09-05 15:12:35 +0200

    Add Joomla 5/6 metadata to project panel bridge

--- admin/views/project/tmpl/panel_4.php
+++ admin/views/project/tmpl/panel_4.php
@@ -1,3 +1,12 @@
 <?php
+/**
+ * Legacy Joomla 4 compatibility bridge for the native Joomla 5/6 project panel layout.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 \defined('_JEXEC') or die;
+
 require JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/tmpl/project/panel.php';
