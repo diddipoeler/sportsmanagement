7afa5276e50216fce961711513905484978a439f
diddipoeler <diddipoeler@gmx.de>
2026-09-05 19:45:45 +0200

    Align UEFA ranking layout metadata with Joomla 5/6

--- modules/mod_sportsmanagement_uefawertung/tmpl/default.php
+++ modules/mod_sportsmanagement_uefawertung/tmpl/default.php
@@ -2,7 +2,7 @@
 /**
  * Joomla 5/6 UEFA ranking layout.
  *
- * @version    3.8.0
+ * @version    5.6.0
  * @author     diddipoeler
  * @copyright  (C) 2015-2026
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
