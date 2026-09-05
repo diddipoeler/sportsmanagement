ddcb8c70c18d117f9d3cd849fee3ae85dba631d3
diddipoeler <diddipoeler@gmx.de>
2026-09-05 18:16:13 +0200

    Add Joomla 5/6 metadata to calendar Arrobe alias

--- modules/mod_sportsmanagement_calendar/tmpl/default_arrobefr.php
+++ modules/mod_sportsmanagement_calendar/tmpl/default_arrobefr.php
@@ -4,6 +4,11 @@
  *
  * The historical layout depended on a jQuery/Bootstrap 4 calendar plugin.
  * Joomla 5/6 renders the same module data through the native calendar layout.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 \defined('_JEXEC') or die;
 
