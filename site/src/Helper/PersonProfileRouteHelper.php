bdb59940b7dc840f552425f1617465a00ec55c66
diddipoeler <diddipoeler@gmx.de>
2026-09-05 21:35:30 +0200

    Add Joomla 5/6 metadata to person profile route helper

--- site/src/Helper/PersonProfileRouteHelper.php
+++ site/src/Helper/PersonProfileRouteHelper.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Joomla 5/6 helper for external person-profile routes.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 namespace Diddipoeler\Component\SportsManagement\Site\Helper;
 
 \defined('_JEXEC') or die;
