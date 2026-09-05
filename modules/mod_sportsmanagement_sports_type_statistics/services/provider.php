04926e060a91b0caf1d452e31b71625016c3af68
diddipoeler <diddipoeler@gmx.de>
2026-09-05 19:17:01 +0200

    Normalize sports type statistics Joomla 5/6 service provider namespace

--- modules/mod_sportsmanagement_sports_type_statistics/services/provider.php
+++ modules/mod_sportsmanagement_sports_type_statistics/services/provider.php
@@ -17,8 +17,8 @@ return new class implements ServiceProviderInterface
 {
     public function register(Container $container): void
     {
-        $container->registerServiceProvider(new ModuleDispatcherFactory('\\Diddipoeler\\Module\\SportsManagementSportsTypeStatistics'));
-        $container->registerServiceProvider(new HelperFactory('\\Diddipoeler\\Module\\SportsManagementSportsTypeStatistics\\Site\\Helper'));
+        $container->registerServiceProvider(new ModuleDispatcherFactory('Diddipoeler\\Module\\SportsManagementSportsTypeStatistics'));
+        $container->registerServiceProvider(new HelperFactory('Diddipoeler\\Module\\SportsManagementSportsTypeStatistics\\Site\\Helper'));
         $container->registerServiceProvider(new Module());
     }
 };
