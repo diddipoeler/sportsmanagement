4b9c377862f48b4de8afc1d4a84f4ab68dcb0f0d
diddipoeler <diddipoeler@gmx.de>
2026-09-05 10:36:34 +0200

    Harden Joomla 5/6 player model bridge

--- site/models/player.php
+++ site/models/player.php
@@ -2,7 +2,7 @@
 /**
  * SportsManagement legacy compatibility bridge for the native player model stack.
  *
- * @version    4.24.00
+ * @version    5.6.0
  * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
  * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
@@ -12,13 +12,23 @@
 use Diddipoeler\Component\SportsManagement\Site\Model\PlayerLegacyModel;
 
 if (!class_exists(PlayerLegacyModel::class)) {
-    require_once JPATH_SITE . '/components/com_sportsmanagement/src/Model/SportsManagementModel.php';
-    require_once JPATH_SITE . '/components/com_sportsmanagement/src/Model/SportsManagementProjectModel.php';
-    require_once JPATH_SITE . '/components/com_sportsmanagement/src/Model/PlayerModel.php';
-    require_once JPATH_SITE . '/components/com_sportsmanagement/src/Model/PlayerMatchDataModel.php';
-    require_once JPATH_SITE . '/components/com_sportsmanagement/src/Model/PlayerStatisticsModel.php';
-    require_once JPATH_SITE . '/components/com_sportsmanagement/src/Model/PlayerTimeModel.php';
-    require_once JPATH_SITE . '/components/com_sportsmanagement/src/Model/PlayerLegacyModel.php';
+    foreach ([
+        JPATH_SITE . '/components/com_sportsmanagement/src/Model/SportsManagementModel.php',
+        JPATH_SITE . '/components/com_sportsmanagement/src/Model/SportsManagementProjectModel.php',
+        JPATH_SITE . '/components/com_sportsmanagement/src/Model/PlayerModel.php',
+        JPATH_SITE . '/components/com_sportsmanagement/src/Model/PlayerMatchDataModel.php',
+        JPATH_SITE . '/components/com_sportsmanagement/src/Model/PlayerStatisticsModel.php',
+        JPATH_SITE . '/components/com_sportsmanagement/src/Model/PlayerTimeModel.php',
+        JPATH_SITE . '/components/com_sportsmanagement/src/Model/PlayerLegacyModel.php',
+    ] as $nativeFile) {
+        if (is_file($nativeFile)) {
+            require_once $nativeFile;
+        }
+    }
+}
+
+if (!class_exists(PlayerLegacyModel::class)) {
+    throw new \RuntimeException('SportsManagement native Player model stack could not be loaded.', 500);
 }
 
 if (!class_exists('sportsmanagementModelPlayer', false)) {
