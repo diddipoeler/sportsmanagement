25d988bec859e25f2f3b9ade0345909fb0b55f89
diddipoeler <diddipoeler@gmx.de>
2026-09-05 10:14:50 +0200

    Harden Joomla 5/6 stats legacy bridge

--- site/models/stats.php
+++ site/models/stats.php
@@ -2,19 +2,29 @@
 /**
  * Legacy compatibility bridge for the native Joomla 5/6 frontend Stats model.
  *
- * @version    4.24.00
+ * @version    5.6.0
  * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
  * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
-defined('_JEXEC') or die;
+\defined('_JEXEC') or die;
 
 use Diddipoeler\Component\SportsManagement\Site\Model\StatsModel;
 
 if (!class_exists(StatsModel::class)) {
-    require_once JPATH_SITE . '/components/com_sportsmanagement/src/Model/SportsManagementModel.php';
-    require_once JPATH_SITE . '/components/com_sportsmanagement/src/Model/SportsManagementProjectModel.php';
-    require_once JPATH_SITE . '/components/com_sportsmanagement/src/Model/StatsModel.php';
+    $baseModel = JPATH_SITE . '/components/com_sportsmanagement/src/Model/SportsManagementModel.php';
+    $projectModel = JPATH_SITE . '/components/com_sportsmanagement/src/Model/SportsManagementProjectModel.php';
+    $nativeModel = JPATH_SITE . '/components/com_sportsmanagement/src/Model/StatsModel.php';
+
+    foreach ([$baseModel, $projectModel, $nativeModel] as $modelFile) {
+        if (is_file($modelFile)) {
+            require_once $modelFile;
+        }
+    }
+}
+
+if (!class_exists(StatsModel::class)) {
+    throw new \RuntimeException('SportsManagement native Stats model could not be loaded.', 500);
 }
 
 if (!class_exists('sportsmanagementModelStats', false)) {
