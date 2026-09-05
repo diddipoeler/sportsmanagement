165b6b609929360a4d2a2acac219f67fa415d65b
diddipoeler <diddipoeler@gmx.de>
2026-09-05 12:44:06 +0200

    Modernize Joomla 5/6 team field query

--- admin/src/Field/TeamField.php
+++ admin/src/Field/TeamField.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @version    4.24.00
+ * @version    5.6.0
  * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
  * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
@@ -18,7 +18,7 @@ final class TeamField extends SportsManagementListField
     protected function getOptions(): array
     {
         $db = $this->getSportsManagementDatabase();
-        $query = $db->getQuery(true)
+        $query = $db->createQuery()
             ->select([
                 $db->quoteName('t.id', 'value'),
                 $db->quoteName('t.name', 'text'),
