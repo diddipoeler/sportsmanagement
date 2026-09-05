275dfd30d81aca4cdf1bf12fa4d1352e3866a645
diddipoeler <diddipoeler@gmx.de>
2026-09-05 09:31:25 +0200

    Normalize Joomla 5/6 referees legacy bridge

--- site/models/referees.php
+++ site/models/referees.php
@@ -1,11 +1,13 @@
 <?php
 /**
- * SportsManagement legacy compatibility bridge.
+ * Legacy compatibility bridge for the native Joomla 5/6 frontend Referees model.
  *
- * The active Joomla 5/6 implementation lives in site/src/Model/RefereesModel.php.
+ * @version    5.6.0
+ * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
-
-defined('_JEXEC') or die('Restricted access');
+\defined('_JEXEC') or die;
 
 use Diddipoeler\Component\SportsManagement\Site\Model\RefereesModel;
 
@@ -13,6 +15,10 @@ if (!class_exists(RefereesModel::class)) {
     require_once JPATH_SITE . '/components/com_sportsmanagement/src/Model/SportsManagementModel.php';
     require_once JPATH_SITE . '/components/com_sportsmanagement/src/Model/SportsManagementProjectModel.php';
     require_once JPATH_SITE . '/components/com_sportsmanagement/src/Model/RefereesModel.php';
+}
+
+if (!class_exists(RefereesModel::class)) {
+    throw new \RuntimeException('SportsManagement native Referees model could not be loaded.', 500);
 }
 
 if (!class_exists('sportsmanagementModelReferees', false)) {
