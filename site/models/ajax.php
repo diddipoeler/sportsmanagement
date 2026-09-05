e7c62a556ef3222d5e43e99e6710ed02ffa647ae
diddipoeler <diddipoeler@gmx.de>
2026-09-05 10:29:00 +0200

    Harden Joomla 5/6 ajax model bridge

--- site/models/ajax.php
+++ site/models/ajax.php
@@ -2,17 +2,25 @@
 /**
  * Legacy compatibility bridge for the native Joomla 5/6 frontend Ajax model.
  *
- * @version    4.24.00
+ * @version    5.6.0
  * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
  * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
-\defined('_JEXEC') or die('Restricted access');
+\defined('_JEXEC') or die;
 
 use Diddipoeler\Component\SportsManagement\Site\Model\AjaxModel;
 
 if (!class_exists(AjaxModel::class)) {
-    require_once JPATH_SITE . '/components/com_sportsmanagement/src/Model/AjaxModel.php';
+    $nativeModel = JPATH_SITE . '/components/com_sportsmanagement/src/Model/AjaxModel.php';
+
+    if (is_file($nativeModel)) {
+        require_once $nativeModel;
+    }
+}
+
+if (!class_exists(AjaxModel::class)) {
+    throw new \RuntimeException('SportsManagement native Ajax model could not be loaded.', 500);
 }
 
 if (!class_exists('sportsmanagementModelAjax', false)) {
