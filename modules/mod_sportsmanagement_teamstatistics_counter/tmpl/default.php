63dc47684943e27151cabfde1b9b2fff33ac8d5a
diddipoeler <diddipoeler@gmx.de>
2026-09-05 19:47:41 +0200

    Align team statistics counter layout metadata with Joomla 5/6

--- modules/mod_sportsmanagement_teamstatistics_counter/tmpl/default.php
+++ modules/mod_sportsmanagement_teamstatistics_counter/tmpl/default.php
@@ -1,14 +1,10 @@
 <?php
 /**
+ * Joomla 5/6 layout for the SportsManagement Team Statistics Counter module.
  *
- * SportsManagement ein Programm zur Verwaltung f�r alle Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage mod_sportsmanagement_trainingsdata
- * @file       default.php
+ * @version    5.6.0
  * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: � 2013 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 
