663266e9b0743e8b6282b5abe6231344340954a5
diddipoeler <diddipoeler@gmx.de>
2026-09-05 18:16:18 +0200

    Add Joomla 5/6 metadata to calendar Bootstrap alias

--- modules/mod_sportsmanagement_calendar/tmpl/default_bootstrap.php
+++ modules/mod_sportsmanagement_calendar/tmpl/default_bootstrap.php
@@ -3,6 +3,11 @@
  * Bootstrap layout compatibility alias.
  *
  * The Joomla 5/6 standard layout already renders Bootstrap 5 compatible markup.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 \defined('_JEXEC') or die;
 
