c62b26c3f2c55fef94710648a18d98536aedf8e2
diddipoeler <diddipoeler@gmx.de>
2026-09-05 19:57:53 +0200

    Add Joomla 5/6 metadata to tournament bracket data model

--- site/src/Model/TournamentbracketDataModel.php
+++ site/src/Model/TournamentbracketDataModel.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Joomla 5/6 data reader for the frontend tournament bracket.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 namespace Diddipoeler\Component\SportsManagement\Site\Model;
 
 \defined('_JEXEC') or die;
