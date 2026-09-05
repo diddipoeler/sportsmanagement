6c3a130b06a5c54919ee530111af28c887eda8aa
diddipoeler <diddipoeler@gmx.de>
2026-09-05 12:52:57 +0200

    Harden Joomla 5/6 age groups field bridge

--- admin/elements/agegroups.php
+++ admin/elements/agegroups.php
@@ -1,17 +1,33 @@
 <?php
-/** Legacy compatibility bridge for the Joomla 5/6 team age groups field. */
+/**
+ * Legacy compatibility bridge for the Joomla 5/6 team age groups field.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * @deprecated 5.6 Use Diddipoeler\Component\SportsManagement\Administrator\Field\AgegroupsField
+ */
 \defined('_JEXEC') or die;
 
 use Diddipoeler\Component\SportsManagement\Administrator\Field\AgegroupsField;
 
 if (!class_exists(AgegroupsField::class)) {
-    $fieldFile = JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Field/AgegroupsField.php';
-
-    if (is_file($fieldFile)) {
-        require_once $fieldFile;
+    foreach ([
+        JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Field/SportsManagementDatabaseTrait.php',
+        JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Field/SportsManagementListField.php',
+        JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Field/AgegroupsField.php',
+    ] as $nativeFile) {
+        if (is_file($nativeFile)) {
+            require_once $nativeFile;
+        }
     }
 }
 
-if (class_exists(AgegroupsField::class) && !class_exists('JFormFieldagegroups', false)) {
+if (!class_exists(AgegroupsField::class)) {
+    throw new \RuntimeException('SportsManagement native Agegroups field could not be loaded.', 500);
+}
+
+if (!class_exists('JFormFieldagegroups', false)) {
     class_alias(AgegroupsField::class, 'JFormFieldagegroups');
 }
