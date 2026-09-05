8e6fbf63806540ece982bc6b0a1d66367e9f7baf
diddipoeler <diddipoeler@gmx.de>
2026-09-05 09:49:16 +0200

    Harden Joomla 5/6 results model bridge

--- site/models/results.php
+++ site/models/results.php
@@ -1,10 +1,13 @@
 <?php
 /**
- * SportsManagement legacy compatibility bridge.
+ * Legacy compatibility bridge for the native Joomla 5/6 frontend Results model.
  *
- * The active Joomla 5/6 implementation lives in site/src/Model/ResultsModel.php.
+ * @version    5.6.0
+ * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
-defined('_JEXEC') or die('Restricted access');
+\defined('_JEXEC') or die;
 
 use Diddipoeler\Component\SportsManagement\Site\Model\ResultsAccessModel;
 use Diddipoeler\Component\SportsManagement\Site\Model\ResultsDataModel;
@@ -13,18 +16,36 @@ use Diddipoeler\Component\SportsManagement\Site\Model\ResultsModel;
 use Diddipoeler\Component\SportsManagement\Site\Pagination\JSMSportsmanagementPagination;
 
 if (!class_exists(ResultsModel::class)) {
-    require_once JPATH_SITE . '/components/com_sportsmanagement/src/Model/SportsManagementListModel.php';
-    require_once JPATH_SITE . '/components/com_sportsmanagement/src/Model/SportsManagementModel.php';
-    require_once JPATH_SITE . '/components/com_sportsmanagement/src/Model/SportsManagementProjectModel.php';
-    require_once JPATH_SITE . '/components/com_sportsmanagement/src/Model/ResultsDataModel.php';
-    require_once JPATH_SITE . '/components/com_sportsmanagement/src/Model/ResultsAccessModel.php';
-    require_once JPATH_SITE . '/components/com_sportsmanagement/src/Model/ResultsEditModel.php';
-
-    if (!class_exists(JSMSportsmanagementPagination::class)) {
-        require_once JPATH_SITE . '/components/com_sportsmanagement/src/Pagination/JSMSportsmanagementPagination.php';
+    foreach ([
+        JPATH_SITE . '/components/com_sportsmanagement/src/Model/SportsManagementListModel.php',
+        JPATH_SITE . '/components/com_sportsmanagement/src/Model/SportsManagementModel.php',
+        JPATH_SITE . '/components/com_sportsmanagement/src/Model/SportsManagementProjectModel.php',
+        JPATH_SITE . '/components/com_sportsmanagement/src/Model/ResultsDataModel.php',
+        JPATH_SITE . '/components/com_sportsmanagement/src/Model/ResultsAccessModel.php',
+        JPATH_SITE . '/components/com_sportsmanagement/src/Model/ResultsEditModel.php',
+    ] as $nativeFile) {
+        if (is_file($nativeFile)) {
+            require_once $nativeFile;
+        }
     }
 
-    require_once JPATH_SITE . '/components/com_sportsmanagement/src/Model/ResultsModel.php';
+    if (!class_exists(JSMSportsmanagementPagination::class)) {
+        $paginationFile = JPATH_SITE . '/components/com_sportsmanagement/src/Pagination/JSMSportsmanagementPagination.php';
+
+        if (is_file($paginationFile)) {
+            require_once $paginationFile;
+        }
+    }
+
+    $nativeModel = JPATH_SITE . '/components/com_sportsmanagement/src/Model/ResultsModel.php';
+
+    if (is_file($nativeModel)) {
+        require_once $nativeModel;
+    }
+}
+
+if (!class_exists(ResultsModel::class)) {
+    throw new \RuntimeException('SportsManagement native Results model could not be loaded.', 500);
 }
 
 if (!class_exists('sportsmanagementModelResults', false)) {
