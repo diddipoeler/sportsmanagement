9a212ccd213a083940448e94d1804b73e74fb518
diddipoeler <diddipoeler@gmx.de>
2026-09-05 18:43:26 +0200

    Align legacy clubicons layout metadata with Joomla 5/6

--- modules/mod_sportsmanagement_clubicons/tmpl/default2.php
+++ modules/mod_sportsmanagement_clubicons/tmpl/default2.php
@@ -1,12 +1,10 @@
 <?php
 /**
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage mod_sportsmanagement_clubicons
- * @file       default2.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * Legacy SportsManagement club icons layout retained for compatibility.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 defined('_JEXEC') or die('Restricted access');
