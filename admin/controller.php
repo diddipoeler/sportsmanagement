ce60595e156f427d4470ce49070f3f620e8a14e9
diddipoeler <diddipoeler@gmx.de>
2026-09-05 22:04:09 +0200

    Normalize Joomla 5/6 metadata for admin controller bridge

--- admin/controller.php
+++ admin/controller.php
@@ -3,8 +3,8 @@
  * Legacy administrator controller bridge for Joomla 5/6.
  *
  * @version    5.6.0
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 \defined('_JEXEC') or die;
