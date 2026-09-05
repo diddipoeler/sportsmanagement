e64584a1d8778d9ceb74f8f318e96b538fcb671b
diddipoeler <diddipoeler@gmx.de>
2026-09-05 18:16:30 +0200

    Add Joomla 5/6 metadata to calendar Zabuto alias

--- modules/mod_sportsmanagement_calendar/tmpl/default_zabuto.php
+++ modules/mod_sportsmanagement_calendar/tmpl/default_zabuto.php
@@ -4,6 +4,11 @@
  *
  * The historical Zabuto template only loaded third-party assets and did not render
  * SportsManagement events. Use the native Joomla 5/6 calendar layout instead.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 \defined('_JEXEC') or die;
 
