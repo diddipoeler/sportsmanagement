13c683d9c6c234c7f3ae3b841af065099023b4fd
diddipoeler <diddipoeler@gmx.de>
2026-09-05 19:06:42 +0200

    Normalize project map Joomla 5/6 service namespaces

--- modules/mod_sportsmanagement_projectmap/services/provider.php
+++ modules/mod_sportsmanagement_projectmap/services/provider.php
@@ -19,8 +19,8 @@ return new class () implements ServiceProviderInterface
 {
     public function register(Container $container): void
     {
-        $container->registerServiceProvider(new ModuleDispatcherFactory('\\Diddipoeler\\Module\\SportsManagementProjectMap'));
-        $container->registerServiceProvider(new HelperFactory('\\Diddipoeler\\Module\\SportsManagementProjectMap\\Site\\Helper'));
+        $container->registerServiceProvider(new ModuleDispatcherFactory('Diddipoeler\\Module\\SportsManagementProjectMap'));
+        $container->registerServiceProvider(new HelperFactory('Diddipoeler\\Module\\SportsManagementProjectMap\\Site\\Helper'));
         $container->registerServiceProvider(new Module());
     }
 };
