1bb4b255e00d943c58f0913919eb158dde89c62f
diddipoeler <diddipoeler@gmx.de>
2026-09-05 09:06:32 +0200

    Normalize Joomla 5/6 events ranking helper metadata

--- modules/mod_sportsmanagement_eventsranking/src/Helper/EventsRankingHelper.php
+++ modules/mod_sportsmanagement_eventsranking/src/Helper/EventsRankingHelper.php
@@ -2,10 +2,10 @@
 /**
  * Native Joomla 5/6 data and route helper for the events ranking module.
  *
- * @version   5.6.0
- * @author    diddipoeler
- * @copyright Copyright (C) diddipoeler
- * @license   GNU General Public License version 2 or later; see LICENSE.txt
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 namespace Diddipoeler\Module\SportsManagementEventsRanking\Site\Helper;
 
