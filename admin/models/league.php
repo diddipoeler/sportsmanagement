1b3bc138b0b8a23f615522eb19b2878af600b812
diddipoeler <diddipoeler@gmx.de>
2026-09-05 10:23:22 +0200

    Harden Joomla 5/6 league legacy bridge

--- admin/models/league.php
+++ admin/models/league.php
@@ -4,19 +4,30 @@
  *
  * The active Joomla 5/6 implementation lives in admin/src/Model/LeagueModel.php.
  *
- * @version    4.24.00
+ * @version    5.6.0
  * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
  * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
-
-defined('_JEXEC') or die('Restricted access');
+\defined('_JEXEC') or die;
 
 use Diddipoeler\Component\SportsManagement\Administrator\Model\LeagueModel;
 
 if (!class_exists(LeagueModel::class)) {
-    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Model/SportsManagementAdminModel.php';
-    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Model/LeagueModel.php';
+    $nativeFiles = [
+        JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Model/SportsManagementAdminModel.php',
+        JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Model/LeagueModel.php',
+    ];
+
+    foreach ($nativeFiles as $nativeFile) {
+        if (is_file($nativeFile)) {
+            require_once $nativeFile;
+        }
+    }
+}
+
+if (!class_exists(LeagueModel::class)) {
+    throw new \RuntimeException('SportsManagement native League model could not be loaded.', 500);
 }
 
 if (!class_exists('sportsmanagementModelleague', false)) {
