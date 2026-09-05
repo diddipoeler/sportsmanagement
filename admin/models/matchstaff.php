a55cfc88e24fdf2d16cf6505f224b42b29c11a2b
diddipoeler <diddipoeler@gmx.de>
2026-09-05 18:23:37 +0200

    Add Joomla 5/6 metadata to match staff bridge

--- admin/models/matchstaff.php
+++ admin/models/matchstaff.php
@@ -1,5 +1,12 @@
 <?php
-/** Legacy compatibility bridge for the native match staff form model. */
+/**
+ * Legacy compatibility bridge for the native match staff form model.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 defined('_JEXEC') or die('Restricted access');
 
 use Diddipoeler\Component\SportsManagement\Administrator\Model\MatchstaffModel;
