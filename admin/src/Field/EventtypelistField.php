9bfc323f15498b42e53fa1b02f8aba9c0e479aa7
diddipoeler <diddipoeler@gmx.de>
2026-09-05 13:40:01 +0200

    Use Joomla 5/6 query API in event type list field

--- admin/src/Field/EventtypelistField.php
+++ admin/src/Field/EventtypelistField.php
@@ -21,7 +21,7 @@ final class EventtypelistField extends SportsManagementListField
     protected function getOptions(): array
     {
         $db = $this->getSportsManagementDatabase();
-        $query = $db->getQuery(true)
+        $query = $db->createQuery()
             ->select([
                 $db->quoteName('id', 'value'),
                 $db->quoteName('name', 'text'),
