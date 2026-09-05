fde98f8af1d35225de8e1f25327e0c96970d7a6c
diddipoeler <diddipoeler@gmx.de>
2026-09-05 19:02:57 +0200

    Align new project default layout metadata with Joomla 5/6

--- modules/mod_sportsmanagement_new_project/tmpl/default.php
+++ modules/mod_sportsmanagement_new_project/tmpl/default.php
@@ -5,7 +5,7 @@
  * The Joomla 5/6 implementation lives in native.php so existing module
  * instances and template overrides selecting "default" remain functional.
  *
- * @version    4.24.00
+ * @version    5.6.0
  * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
  * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
