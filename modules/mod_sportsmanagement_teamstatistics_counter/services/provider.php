cbef05bf8cbd1df2950ea7689db50c9da293bc16
diddipoeler <diddipoeler@gmx.de>
2026-09-05 19:40:13 +0200

    Align team statistics counter provider with Joomla 5/6

--- modules/mod_sportsmanagement_teamstatistics_counter/services/provider.php
+++ modules/mod_sportsmanagement_teamstatistics_counter/services/provider.php
@@ -2,7 +2,7 @@
 /**
  * Joomla 5/6 service provider for the SportsManagement Team Statistics Counter module.
  *
- * @version    4.24.00
+ * @version    5.6.0
  * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
  * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
@@ -19,8 +19,8 @@ return new class implements ServiceProviderInterface
 {
     public function register(Container $container): void
     {
-        $container->registerServiceProvider(new ModuleDispatcherFactory('\\Diddipoeler\\Module\\SportsManagementTeamStatisticsCounter'));
-        $container->registerServiceProvider(new HelperFactory('\\Diddipoeler\\Module\\SportsManagementTeamStatisticsCounter\\Site\\Helper'));
+        $container->registerServiceProvider(new ModuleDispatcherFactory('Diddipoeler\\Module\\SportsManagementTeamStatisticsCounter'));
+        $container->registerServiceProvider(new HelperFactory('Diddipoeler\\Module\\SportsManagementTeamStatisticsCounter\\Site\\Helper'));
         $container->registerServiceProvider(new Module());
     }
 };
