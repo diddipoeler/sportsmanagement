a3374fc4a1cc4ba99005117f3ba73312b49e7610
diddipoeler <diddipoeler@gmx.de>
2026-09-05 13:40:30 +0200

    Modernize Joomla 5/6 league field

--- admin/src/Field/LeagueField.php
+++ admin/src/Field/LeagueField.php
@@ -1,11 +1,18 @@
 <?php
+/**
+ * Joomla 5/6-native league selector used by administrator list filters.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 namespace Diddipoeler\Component\SportsManagement\Administrator\Field;
 
 \defined('_JEXEC') or die;
 
 use Joomla\CMS\Factory;
 
-/** Joomla 5/6-native league selector used by administrator list filters. */
 final class LeagueField extends SportsManagementListField
 {
     protected $type = 'League';
@@ -19,7 +26,7 @@ final class LeagueField extends SportsManagementListField
         $association = (int) ($filter['search_associations_leagues'] ?? $app->getUserState('com_sportsmanagement.projects_search_associations_leagues', 0));
 
         $db = $this->getSportsManagementDatabase();
-        $query = $db->getQuery(true)
+        $query = $db->createQuery()
             ->select([
                 $db->quoteName('id', 'value'),
                 $db->quoteName('name', 'text'),
