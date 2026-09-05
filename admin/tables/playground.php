f7ffae87b13ec0a9da649a28914a22682e094f93
diddipoeler <diddipoeler@gmx.de>
2026-09-05 23:44:59 +0200

    Migrate playground legacy table to Joomla 5/6 bridge

--- admin/tables/playground.php
+++ admin/tables/playground.php
@@ -1,69 +1,22 @@
 <?php
 /**
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage tables
- * @file       playground.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * SportsManagement legacy compatibility bridge.
+ *
+ * The active Joomla 5/6 implementation lives in admin/src/Table/PlaygroundTable.php.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
-defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\Language\Text;
-use Joomla\CMS\Filter\OutputFilter;
+\defined('_JEXEC') or die;
 
-/**
- * sportsmanagementTablePlayground
- *
- * @package
- * @author
- * @copyright diddi
- * @version   2014
- * @access    public
- */
-class sportsmanagementTablePlayground extends JSMTable
-{
-	/**
-	 * Constructor
-	 *
-	 * @param   object Database connector object
-	 *
-	 * @since 1.0
-	 */
-	function __construct(&$db)
-	{
-		$db = sportsmanagementHelper::getDBConnection();
-		parent::__construct('#__sportsmanagement_playground', 'id', $db);
-	}
+use Diddipoeler\Component\SportsManagement\Administrator\Table\PlaygroundTable;
 
-	/**
-	 * Overloaded check method to ensure data integrity
-	 *
-	 * @access public
-	 * @return boolean True on success
-	 * @since  1.0
-	 */
-	function check()
-	{
-		if (empty($this->name))
-		{
-			$this->setError(Text::_('ERROR NAME REQUIRED'));
+if (!class_exists(PlaygroundTable::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Table/PlaygroundTable.php';
+}
 
-			return false;
-		}
-
-		// Setting alias
-		if (empty($this->alias))
-		{
-			$this->alias = OutputFilter::stringURLSafe($this->name);
-		}
-		else
-		{
-			$this->alias = OutputFilter::stringURLSafe($this->alias); // Make sure the user didn't modify it to something illegal...
-		}
-
-		return true;
-	}
-
+if (!class_exists('sportsmanagementTablePlayground', false)) {
+    class_alias(PlaygroundTable::class, 'sportsmanagementTablePlayground');
 }
