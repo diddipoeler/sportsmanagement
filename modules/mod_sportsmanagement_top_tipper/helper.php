c2eea18b4cbc0c8d4be5a14ba04bb49fc3934b39
diddipoeler <diddipoeler@gmx.de>
2026-09-05 14:43:28 +0200

    Align Top Tipper Joomla 5/6 helper metadata

--- modules/mod_sportsmanagement_top_tipper/helper.php
+++ modules/mod_sportsmanagement_top_tipper/helper.php
@@ -2,7 +2,7 @@
 /**
  * Legacy compatibility bridge for the Joomla 5/6 Top Tipper module.
  *
- * @version    4.24.00
+ * @version    5.6.0
  * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
  * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
