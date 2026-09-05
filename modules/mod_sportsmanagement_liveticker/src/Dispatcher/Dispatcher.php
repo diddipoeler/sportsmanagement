66b203e8f9444e0ae6e9d0ac554822a75b71000c
diddipoeler <diddipoeler@gmx.de>
2026-09-05 21:22:44 +0200

    Normalize Joomla 5/6 metadata for liveticker dispatcher

--- modules/mod_sportsmanagement_liveticker/src/Dispatcher/Dispatcher.php
+++ modules/mod_sportsmanagement_liveticker/src/Dispatcher/Dispatcher.php
@@ -3,8 +3,8 @@
  * Native Joomla 5/6 dispatcher for the SportsManagement liveticker module.
  *
  * @version    5.6.0
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 namespace Diddipoeler\Module\SportsManagementLiveticker\Site\Dispatcher;
