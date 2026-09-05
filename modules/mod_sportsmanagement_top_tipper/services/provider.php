e525f3a1757c095b21e42027e10af36cdff61a45
diddipoeler <diddipoeler@gmx.de>
2026-09-05 19:42:42 +0200

    Align Top Tipper Joomla 5/6 service provider

--- modules/mod_sportsmanagement_top_tipper/services/provider.php
+++ modules/mod_sportsmanagement_top_tipper/services/provider.php
@@ -2,7 +2,7 @@
 /**
  * Joomla 5/6 service provider for the SportsManagement Top Tipper module.
  *
- * @version    4.24.00
+ * @version    5.6.0
  * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
  * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
@@ -20,10 +20,10 @@ return new class implements ServiceProviderInterface
     public function register(Container $container): void
     {
         $container->registerServiceProvider(
-            new ModuleDispatcherFactory('\\Diddipoeler\\Module\\SportsManagementTopTipper')
+            new ModuleDispatcherFactory('Diddipoeler\\Module\\SportsManagementTopTipper')
         );
         $container->registerServiceProvider(
-            new HelperFactory('\\Diddipoeler\\Module\\SportsManagementTopTipper\\Site\\Helper')
+            new HelperFactory('Diddipoeler\\Module\\SportsManagementTopTipper\\Site\\Helper')
         );
         $container->registerServiceProvider(new Module());
     }
