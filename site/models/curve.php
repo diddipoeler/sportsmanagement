d0a779ad8b3de1daf2fc80a196ee331de7144dee
diddipoeler <diddipoeler@gmx.de>
2026-09-05 16:56:25 +0200

    Align Curve bridge Joomla 5/6 metadata

--- site/models/curve.php
+++ site/models/curve.php
@@ -2,7 +2,7 @@
 /**
  * Legacy compatibility bridge for the native Joomla 5/6 frontend Curve model.
  *
- * @version    4.24.00
+ * @version    5.6.0
  * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
  * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
