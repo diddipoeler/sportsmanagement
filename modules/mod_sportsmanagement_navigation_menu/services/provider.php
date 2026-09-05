6cd5fbdab3839797136f38c5909fb76bc64dc62f
diddipoeler <diddipoeler@gmx.de>
2026-09-05 19:00:27 +0200

    Normalize navigation menu Joomla 5/6 service namespaces

--- modules/mod_sportsmanagement_navigation_menu/services/provider.php
+++ modules/mod_sportsmanagement_navigation_menu/services/provider.php
@@ -20,10 +20,10 @@ return new class implements ServiceProviderInterface
     public function register(Container $container): void
     {
         $container->registerServiceProvider(
-            new ModuleDispatcherFactory('\\Diddipoeler\\Module\\SportsManagementNavigationMenu')
+            new ModuleDispatcherFactory('Diddipoeler\\Module\\SportsManagementNavigationMenu')
         );
         $container->registerServiceProvider(
-            new HelperFactory('\\Diddipoeler\\Module\\SportsManagementNavigationMenu\\Site\\Helper')
+            new HelperFactory('Diddipoeler\\Module\\SportsManagementNavigationMenu\\Site\\Helper')
         );
         $container->registerServiceProvider(new Module());
     }
