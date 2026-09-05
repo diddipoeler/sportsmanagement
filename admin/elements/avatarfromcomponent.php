146a6fe8c9b3f6fe8e257cdfa0c60f17a45a065f
diddipoeler <diddipoeler@gmx.de>
2026-09-05 12:53:07 +0200

    Harden Joomla 5/6 avatar field bridge

--- admin/elements/avatarfromcomponent.php
+++ admin/elements/avatarfromcomponent.php
@@ -1,5 +1,13 @@
 <?php
-/** Legacy compatibility bridge for the Joomla 5/6 avatar provider field. */
+/**
+ * Legacy compatibility bridge for the Joomla 5/6 avatar provider field.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * @deprecated 5.6 Use Diddipoeler\Component\SportsManagement\Administrator\Field\AvatarfromcomponentField
+ */
 \defined('_JEXEC') or die;
 
 use Diddipoeler\Component\SportsManagement\Administrator\Field\AvatarfromcomponentField;
@@ -12,6 +20,10 @@ if (!class_exists(AvatarfromcomponentField::class)) {
     }
 }
 
-if (class_exists(AvatarfromcomponentField::class) && !class_exists('JFormFieldAvatarFromComponent', false)) {
+if (!class_exists(AvatarfromcomponentField::class)) {
+    throw new \RuntimeException('SportsManagement native Avatarfromcomponent field could not be loaded.', 500);
+}
+
+if (!class_exists('JFormFieldAvatarFromComponent', false)) {
     class_alias(AvatarfromcomponentField::class, 'JFormFieldAvatarFromComponent');
 }
