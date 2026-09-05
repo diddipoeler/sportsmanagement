be772ef8859dc8a3477bf3d0518741e7c1732066
diddipoeler <diddipoeler@gmx.de>
2026-09-05 19:44:00 +0200

    Normalize TrainingsData Joomla 5/6 service provider namespace

--- modules/mod_sportsmanagement_trainingsdata/services/provider.php
+++ modules/mod_sportsmanagement_trainingsdata/services/provider.php
@@ -19,8 +19,8 @@ return new class implements ServiceProviderInterface
 {
     public function register(Container $container): void
     {
-        $container->registerServiceProvider(new ModuleDispatcherFactory('\\Diddipoeler\\Module\\SportsManagementTrainingsData'));
-        $container->registerServiceProvider(new HelperFactory('\\Diddipoeler\\Module\\SportsManagementTrainingsData\\Site\\Helper'));
+        $container->registerServiceProvider(new ModuleDispatcherFactory('Diddipoeler\\Module\\SportsManagementTrainingsData'));
+        $container->registerServiceProvider(new HelperFactory('Diddipoeler\\Module\\SportsManagementTrainingsData\\Site\\Helper'));
         $container->registerServiceProvider(new Module());
     }
 };
