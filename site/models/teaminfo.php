88527cd013647ee3d7782e2d39012313cb855836
diddipoeler <diddipoeler@gmx.de>
2026-09-05 11:02:54 +0200

    Harden Joomla 5/6 teaminfo model bridge

--- site/models/teaminfo.php
+++ site/models/teaminfo.php
@@ -1,15 +1,30 @@
 <?php
 /**
- * Legacy compatibility bridge for the native Teaminfo model.
+ * SportsManagement legacy compatibility bridge for the native Teaminfo model.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
-defined('_JEXEC') or die;
+\defined('_JEXEC') or die;
 
 use Diddipoeler\Component\SportsManagement\Site\Model\TeaminfoModel;
 
 if (!class_exists(TeaminfoModel::class)) {
-    require_once JPATH_SITE . '/components/com_sportsmanagement/src/Model/SportsManagementModel.php';
-    require_once JPATH_SITE . '/components/com_sportsmanagement/src/Model/SportsManagementProjectModel.php';
-    require_once JPATH_SITE . '/components/com_sportsmanagement/src/Model/TeaminfoModel.php';
+    foreach ([
+        JPATH_SITE . '/components/com_sportsmanagement/src/Model/SportsManagementModel.php',
+        JPATH_SITE . '/components/com_sportsmanagement/src/Model/SportsManagementProjectModel.php',
+        JPATH_SITE . '/components/com_sportsmanagement/src/Model/TeaminfoModel.php',
+    ] as $nativeFile) {
+        if (is_file($nativeFile)) {
+            require_once $nativeFile;
+        }
+    }
+}
+
+if (!class_exists(TeaminfoModel::class)) {
+    throw new \RuntimeException('SportsManagement native Teaminfo model could not be loaded.', 500);
 }
 
 if (!class_exists('sportsmanagementModelTeamInfo', false)) {
