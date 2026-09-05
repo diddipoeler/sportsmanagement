35802dd846a7ee45523d3ef074d9918e84ea0418
diddipoeler <diddipoeler@gmx.de>
2026-09-05 14:58:28 +0200

    Add Joomla 5/6 metadata to AJAX navigation wrapper

--- modules/mod_sportsmanagement_ajax_top_navigation_menu/tmpl/native.php
+++ modules/mod_sportsmanagement_ajax_top_navigation_menu/tmpl/native.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Joomla 5/6 wrapper layout for the SportsManagement AJAX top navigation module.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 \defined('_JEXEC') or die;
 
 use Joomla\CMS\Helper\ModuleHelper;
