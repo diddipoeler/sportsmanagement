e4769777e66160ad25b7f3bb6d532d174c5bdf62
diddipoeler <diddipoeler@gmx.de>
2026-09-05 23:00:25 +0200

    Normalize Joomla 5/6 metadata for randomplayer entry point

--- modules/mod_sportsmanagement_randomplayer/mod_sportsmanagement_randomplayer.php
+++ modules/mod_sportsmanagement_randomplayer/mod_sportsmanagement_randomplayer.php
@@ -1,8 +1,12 @@
 <?php
 /**
+ * Joomla 5/6 compatibility entry point for the SportsManagement Random Player module.
+ *
+ * Runtime execution is handled by services/provider.php and the namespaced module dispatcher.
+ *
  * @version    5.6.0
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 \defined('_JEXEC') or die;
