ec8171d92a1bbd0b48d3910e5c242094320924e5
diddipoeler <diddipoeler@gmx.de>
2026-09-05 11:08:35 +0200

    Harden Joomla 5/6 team statistics counter helper bridge

--- modules/mod_sportsmanagement_teamstatistics_counter/helper.php
+++ modules/mod_sportsmanagement_teamstatistics_counter/helper.php
@@ -2,7 +2,7 @@
 /**
  * Legacy helper bridge for third-party overrides.
  *
- * @version    4.24.00
+ * @version    5.6.0
  * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
  * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
@@ -15,7 +15,15 @@ use Joomla\Database\DatabaseInterface;
 use Joomla\Registry\Registry;
 
 if (!class_exists(TeamStatisticsCounterHelper::class)) {
-    require_once __DIR__ . '/src/Helper/TeamStatisticsCounterHelper.php';
+    $nativeHelper = __DIR__ . '/src/Helper/TeamStatisticsCounterHelper.php';
+
+    if (is_file($nativeHelper)) {
+        require_once $nativeHelper;
+    }
+}
+
+if (!class_exists(TeamStatisticsCounterHelper::class)) {
+    throw new \RuntimeException('SportsManagement Team Statistics Counter helper could not be loaded.', 500);
 }
 
 if (!class_exists('modJSMTeamStatisticsCounter', false)) {
