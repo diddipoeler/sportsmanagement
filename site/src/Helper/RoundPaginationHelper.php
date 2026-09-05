400ea48248be19a22efa2e1fa27f9852fc20d0cb
diddipoeler <diddipoeler@gmx.de>
2026-09-05 09:41:57 +0200

    Add Joomla 5/6 metadata to round pagination helper

--- site/src/Helper/RoundPaginationHelper.php
+++ site/src/Helper/RoundPaginationHelper.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Joomla 5/6 matchday navigation helper for results-style views.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 namespace Diddipoeler\Component\SportsManagement\Site\Helper;
 
 \defined('_JEXEC') or die;
