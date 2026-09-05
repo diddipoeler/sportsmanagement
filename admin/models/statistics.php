630dd66895a2a04c563925a3fcd32e4770a849d7
diddipoeler <diddipoeler@gmx.de>
2026-09-05 11:42:53 +0200

    Harden Joomla 5/6 statistics model bridge

--- admin/models/statistics.php
+++ admin/models/statistics.php
@@ -1,12 +1,31 @@
 <?php
-/** Legacy compatibility bridge for the native administrator Statistics model. */
-defined('_JEXEC') or die('Restricted access');
+/**
+ * Legacy compatibility bridge for the native administrator Statistics model.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
+\defined('_JEXEC') or die;
 
 use Diddipoeler\Component\SportsManagement\Administrator\Model\StatisticsModel;
 
 if (!class_exists(StatisticsModel::class)) {
-    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Model/SportsManagementListModel.php';
-    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Model/StatisticsModel.php';
+    $nativeFiles = [
+        JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Model/SportsManagementListModel.php',
+        JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Model/StatisticsModel.php',
+    ];
+
+    foreach ($nativeFiles as $nativeFile) {
+        if (is_file($nativeFile)) {
+            require_once $nativeFile;
+        }
+    }
+}
+
+if (!class_exists(StatisticsModel::class)) {
+    throw new \RuntimeException('SportsManagement native Statistics model could not be loaded.', 500);
 }
 
 if (!class_exists('sportsmanagementModelStatistics', false)) {
