7d8e5b5c34928c7a032bf163d5b02ef92873e4f3
diddipoeler <diddipoeler@gmx.de>
2026-09-05 18:16:25 +0200

    Add Joomla 5/6 metadata to calendar TOAST UI alias

--- modules/mod_sportsmanagement_calendar/tmpl/default_tuicalendar.php
+++ modules/mod_sportsmanagement_calendar/tmpl/default_tuicalendar.php
@@ -4,6 +4,11 @@
  *
  * The bundled TOAST UI v1 runtime is no longer required on Joomla 5/6.
  * Stored layout selections render through the native calendar layout instead.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 \defined('_JEXEC') or die;
 
