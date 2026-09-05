742acfc50b3a48603ecf4958db65978fcc8ea69d
diddipoeler <diddipoeler@gmx.de>
2026-09-05 18:58:48 +0200

    Normalize matches slider Joomla 5/6 service namespaces

--- modules/mod_sportsmanagement_matchesslider/services/provider.php
+++ modules/mod_sportsmanagement_matchesslider/services/provider.php
@@ -20,10 +20,10 @@ return new class () implements ServiceProviderInterface
     public function register(Container $container): void
     {
         $container->registerServiceProvider(
-            new ModuleDispatcherFactory('\\Diddipoeler\\Module\\SportsManagementMatchesSlider')
+            new ModuleDispatcherFactory('Diddipoeler\\Module\\SportsManagementMatchesSlider')
         );
         $container->registerServiceProvider(
-            new HelperFactory('\\Diddipoeler\\Module\\SportsManagementMatchesSlider\\Site\\Helper')
+            new HelperFactory('Diddipoeler\\Module\\SportsManagementMatchesSlider\\Site\\Helper')
         );
         $container->registerServiceProvider(new Module());
     }
