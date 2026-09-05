22764877bd3be36f0efd6744cab8f9f0bfdcdf9f
diddipoeler <diddipoeler@gmx.de>
2026-09-05 23:38:30 +0200

    Add Joomla 5/6 metadata to admin league table bridge

--- admin/tables/league.php
+++ admin/tables/league.php
@@ -3,6 +3,11 @@
  * SportsManagement legacy compatibility bridge.
  *
  * The active Joomla 5/6 implementation lives in admin/src/Table/LeagueTable.php.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 
 defined('_JEXEC') or die('Restricted access');
