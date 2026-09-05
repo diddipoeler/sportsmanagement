61b593dcc39077ef52da9d3b14c5ea1fbecbb84f
diddipoeler <diddipoeler@gmx.de>
2026-09-05 23:00:10 +0200

    Normalize Joomla 5/6 metadata for staff info bridge

--- site/views/staff/tmpl/default_info.php
+++ site/views/staff/tmpl/default_info.php
@@ -1,5 +1,12 @@
 <?php
-/** Legacy compatibility bridge for the native Joomla 5/6 staff info layout. */
+/**
+ * Legacy compatibility bridge for the native Joomla 5/6 staff info layout.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 \defined('_JEXEC') or die;
 
 require JPATH_SITE . '/components/com_sportsmanagement/tmpl/staff/default_info.php';
