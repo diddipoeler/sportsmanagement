02d55acc9a6bf3457507954618267c4391b711af
diddipoeler <diddipoeler@gmx.de>
2026-09-05 19:13:05 +0200

    Normalize ranking Joomla 5/6 service provider namespace

--- modules/mod_sportsmanagement_ranking/services/provider.php
+++ modules/mod_sportsmanagement_ranking/services/provider.php
@@ -19,8 +19,8 @@ return new class implements ServiceProviderInterface
 {
     public function register(Container $container): void
     {
-        $container->registerServiceProvider(new ModuleDispatcherFactory('\\Diddipoeler\\Module\\SportsManagementRanking'));
-        $container->registerServiceProvider(new HelperFactory('\\Diddipoeler\\Module\\SportsManagementRanking\\Site\\Helper'));
+        $container->registerServiceProvider(new ModuleDispatcherFactory('Diddipoeler\\Module\\SportsManagementRanking'));
+        $container->registerServiceProvider(new HelperFactory('Diddipoeler\\Module\\SportsManagementRanking\\Site\\Helper'));
         $container->registerServiceProvider(new Module());
     }
 };
