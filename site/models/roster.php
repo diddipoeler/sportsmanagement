ddfb38c8c8592f8c150d2bba6aeba5006625ed65
diddipoeler <diddipoeler@gmx.de>
2026-09-05 10:24:30 +0200

    Harden Joomla 5/6 roster legacy bridge

--- site/models/roster.php
+++ site/models/roster.php
@@ -2,7 +2,7 @@
 /**
  * Legacy compatibility bridge for the native Joomla 5/6 frontend Roster model.
  *
- * @version    4.24.00
+ * @version    5.6.0
  * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
  * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
@@ -12,9 +12,21 @@
 use Diddipoeler\Component\SportsManagement\Site\Model\RosterModel;
 
 if (!class_exists(RosterModel::class)) {
-    require_once JPATH_SITE . '/components/com_sportsmanagement/src/Model/SportsManagementModel.php';
-    require_once JPATH_SITE . '/components/com_sportsmanagement/src/Model/SportsManagementProjectModel.php';
-    require_once JPATH_SITE . '/components/com_sportsmanagement/src/Model/RosterModel.php';
+    $nativeFiles = [
+        JPATH_SITE . '/components/com_sportsmanagement/src/Model/SportsManagementModel.php',
+        JPATH_SITE . '/components/com_sportsmanagement/src/Model/SportsManagementProjectModel.php',
+        JPATH_SITE . '/components/com_sportsmanagement/src/Model/RosterModel.php',
+    ];
+
+    foreach ($nativeFiles as $nativeFile) {
+        if (is_file($nativeFile)) {
+            require_once $nativeFile;
+        }
+    }
+}
+
+if (!class_exists(RosterModel::class)) {
+    throw new \RuntimeException('SportsManagement native Roster model could not be loaded.', 500);
 }
 
 if (!class_exists('sportsmanagementModelRoster', false)) {
