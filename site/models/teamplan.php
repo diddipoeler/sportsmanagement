c48d282193f09314fe27344e3072f00bd4dafe3a
diddipoeler <diddipoeler@gmx.de>
2026-09-05 10:55:41 +0200

    Harden Joomla 5/6 team plan model bridge

--- site/models/teamplan.php
+++ site/models/teamplan.php
@@ -1,18 +1,31 @@
 <?php
 /**
- * SportsManagement legacy compatibility bridge.
+ * SportsManagement legacy compatibility bridge for the native team plan model.
  *
- * The active Joomla 5/6 implementation lives in site/src/Model/TeamplanModel.php.
+ * @version    5.6.0
+ * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
-defined('_JEXEC') or die('Restricted access');
+\defined('_JEXEC') or die;
 
 use Diddipoeler\Component\SportsManagement\Site\Model\TeamplanModel;
 
 if (!class_exists(TeamplanModel::class)) {
-    require_once JPATH_SITE . '/components/com_sportsmanagement/src/Model/SportsManagementModel.php';
-    require_once JPATH_SITE . '/components/com_sportsmanagement/src/Model/SportsManagementProjectModel.php';
-    require_once JPATH_SITE . '/components/com_sportsmanagement/src/Model/TeamplanEventDataTrait.php';
-    require_once JPATH_SITE . '/components/com_sportsmanagement/src/Model/TeamplanModel.php';
+    foreach ([
+        JPATH_SITE . '/components/com_sportsmanagement/src/Model/SportsManagementModel.php',
+        JPATH_SITE . '/components/com_sportsmanagement/src/Model/SportsManagementProjectModel.php',
+        JPATH_SITE . '/components/com_sportsmanagement/src/Model/TeamplanEventDataTrait.php',
+        JPATH_SITE . '/components/com_sportsmanagement/src/Model/TeamplanModel.php',
+    ] as $nativeFile) {
+        if (is_file($nativeFile)) {
+            require_once $nativeFile;
+        }
+    }
+}
+
+if (!class_exists(TeamplanModel::class)) {
+    throw new \RuntimeException('SportsManagement native Teamplan model could not be loaded.', 500);
 }
 
 if (!class_exists('sportsmanagementModelTeamPlan', false)) {
