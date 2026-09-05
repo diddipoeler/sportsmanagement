e5d23c8f36c3316e767a33a57726128c520f952e
diddipoeler <diddipoeler@gmx.de>
2026-09-05 18:23:52 +0200

    Add Joomla 5/6 metadata to match statistic bridge

--- admin/models/matchstatistic.php
+++ admin/models/matchstatistic.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Legacy compatibility bridge for the native match statistic model.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 defined('_JEXEC') or die('Restricted access');
 
 use Diddipoeler\Component\SportsManagement\Administrator\Model\MatchstatisticModel;
