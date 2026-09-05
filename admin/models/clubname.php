148306cdad4f710ae0a5a9870f0dc68be5aac0e7
diddipoeler <diddipoeler@gmx.de>
2026-09-05 09:43:07 +0200

    Harden Joomla 5/6 club-name model bridge

--- admin/models/clubname.php
+++ admin/models/clubname.php
@@ -2,18 +2,30 @@
 /**
  * Legacy compatibility bridge for the native administrator Clubname model.
  *
- * @version    4.24.00
+ * @version    5.6.0
  * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
  * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
-defined('_JEXEC') or die('Restricted access');
+\defined('_JEXEC') or die;
 
 use Diddipoeler\Component\SportsManagement\Administrator\Model\ClubnameModel;
 
 if (!class_exists(ClubnameModel::class)) {
-    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Model/SportsManagementAdminModel.php';
-    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Model/ClubnameModel.php';
+    $adminModel = JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Model/SportsManagementAdminModel.php';
+    $clubnameModel = JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Model/ClubnameModel.php';
+
+    if (is_file($adminModel)) {
+        require_once $adminModel;
+    }
+
+    if (is_file($clubnameModel)) {
+        require_once $clubnameModel;
+    }
+}
+
+if (!class_exists(ClubnameModel::class)) {
+    throw new \RuntimeException('SportsManagement native administrator Clubname model could not be loaded.', 500);
 }
 
 if (!class_exists('sportsmanagementModelclubname', false)) {
