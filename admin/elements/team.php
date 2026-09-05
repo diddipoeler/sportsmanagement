fe6e78bda908f0e4b98cb89becf7f2365324ed9e
diddipoeler <diddipoeler@gmx.de>
2026-09-05 12:44:29 +0200

    Harden Joomla 5/6 team field bridge

--- admin/elements/team.php
+++ admin/elements/team.php
@@ -1,15 +1,33 @@
 <?php
-/** Legacy compatibility bridge for the native Joomla 5/6 team field. */
+/**
+ * Legacy compatibility bridge for the native Joomla 5/6 team field.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * @deprecated 5.6 Use Diddipoeler\Component\SportsManagement\Administrator\Field\TeamField
+ */
 \defined('_JEXEC') or die;
 
 use Diddipoeler\Component\SportsManagement\Administrator\Field\TeamField;
 
 if (!class_exists(TeamField::class)) {
-    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Field/SportsManagementDatabaseTrait.php';
-    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Field/SportsManagementListField.php';
-    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Field/TeamField.php';
+    foreach ([
+        JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Field/SportsManagementDatabaseTrait.php',
+        JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Field/SportsManagementListField.php',
+        JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Field/TeamField.php',
+    ] as $nativeFile) {
+        if (is_file($nativeFile)) {
+            require_once $nativeFile;
+        }
+    }
 }
 
-if (class_exists(TeamField::class) && !class_exists('JFormFieldTeam', false)) {
+if (!class_exists(TeamField::class)) {
+    throw new \RuntimeException('SportsManagement native Team field could not be loaded.', 500);
+}
+
+if (!class_exists('JFormFieldTeam', false)) {
     class_alias(TeamField::class, 'JFormFieldTeam');
 }
