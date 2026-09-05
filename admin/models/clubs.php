532558812d14bfafac05dd0eb19a44ba166027e0
diddipoeler <diddipoeler@gmx.de>
2026-09-05 10:14:08 +0200

    Harden Joomla 5/6 clubs legacy bridge

--- admin/models/clubs.php
+++ admin/models/clubs.php
@@ -2,18 +2,30 @@
 /**
  * Legacy compatibility bridge for the native administrator Clubs model.
  *
- * @version    4.24.00
+ * @version    5.6.0
  * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
  * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
-defined('_JEXEC') or die('Restricted access');
+\defined('_JEXEC') or die;
 
 use Diddipoeler\Component\SportsManagement\Administrator\Model\ClubsModel;
 
 if (!class_exists(ClubsModel::class)) {
-    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Model/SportsManagementListModel.php';
-    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Model/ClubsModel.php';
+    $baseModel = JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Model/SportsManagementListModel.php';
+    $nativeModel = JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Model/ClubsModel.php';
+
+    if (is_file($baseModel)) {
+        require_once $baseModel;
+    }
+
+    if (is_file($nativeModel)) {
+        require_once $nativeModel;
+    }
+}
+
+if (!class_exists(ClubsModel::class)) {
+    throw new \RuntimeException('SportsManagement native Clubs model could not be loaded.', 500);
 }
 
 if (!class_exists('sportsmanagementModelClubs', false)) {
