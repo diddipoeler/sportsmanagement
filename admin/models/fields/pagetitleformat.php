188eed0f28dc26a162bbdba0499dda6317acbd64
diddipoeler <diddipoeler@gmx.de>
2026-09-05 08:53:41 +0200

    Harden Joomla 5/6 datetime page title and extension author fields

--- admin/models/fields/pagetitleformat.php
+++ admin/models/fields/pagetitleformat.php
@@ -1,10 +1,26 @@
 <?php
+/**
+ * Legacy compatibility bridge for the native Joomla 5/6 page-title format field.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 \defined('_JEXEC') or die;
 
 use Diddipoeler\Component\SportsManagement\Administrator\Field\PagetitleformatField;
 
 if (!class_exists(PagetitleformatField::class)) {
-    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Field/PagetitleformatField.php';
+    $nativeField = JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Field/PagetitleformatField.php';
+
+    if (is_file($nativeField)) {
+        require_once $nativeField;
+    }
+}
+
+if (!class_exists(PagetitleformatField::class)) {
+    throw new \RuntimeException('SportsManagement native Pagetitleformat field could not be loaded.', 500);
 }
 
 if (!class_exists('JFormFieldPageTitleFormat', false)) {
