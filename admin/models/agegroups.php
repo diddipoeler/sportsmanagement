a9941f6b9e4d0e00c3f4aa991f6d27c2b232f284
diddipoeler <diddipoeler@gmx.de>
2026-09-05 09:56:18 +0200

    Harden Joomla 5/6 age groups model bridge

--- admin/models/agegroups.php
+++ admin/models/agegroups.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * SportsManagement legacy compatibility bridge.
+ * SportsManagement legacy compatibility bridge for the native age-groups model.
  *
  * The active Joomla 5/6 implementation lives in admin/src/Model/AgegroupsModel.php.
  *
@@ -9,14 +9,23 @@
  * @copyright  Copyright (C) diddipoeler
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
-
-defined('_JEXEC') or die('Restricted access');
+\defined('_JEXEC') or die;
 
 use Diddipoeler\Component\SportsManagement\Administrator\Model\AgegroupsModel;
 
 if (!class_exists(AgegroupsModel::class)) {
-    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Model/SportsManagementListModel.php';
-    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Model/AgegroupsModel.php';
+    foreach ([
+        JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Model/SportsManagementListModel.php',
+        JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Model/AgegroupsModel.php',
+    ] as $nativeFile) {
+        if (is_file($nativeFile)) {
+            require_once $nativeFile;
+        }
+    }
+}
+
+if (!class_exists(AgegroupsModel::class)) {
+    throw new \RuntimeException('SportsManagement native Agegroups model could not be loaded.', 500);
 }
 
 if (!class_exists('sportsmanagementModelagegroups', false)) {
