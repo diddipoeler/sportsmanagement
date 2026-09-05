8598002bc1b2f0122eac0eb075a00fbcca109edd
diddipoeler <diddipoeler@gmx.de>
2026-09-05 20:09:52 +0200

    Migrate match event reader to Joomla 5/6 model

--- admin/src/Model/MatcheventModel.php
+++ admin/src/Model/MatcheventModel.php
@@ -1,5 +1,7 @@
 <?php
 /**
+ * Native Joomla 5/6 administrator model for match events.
+ *
  * @version    5.6.0
  * @author     diddipoeler
  * @copyright  Copyright (C) diddipoeler
@@ -27,6 +29,61 @@ final class MatcheventModel extends SportsManagementAdminModel
             'matchevent',
             ['control' => 'jform', 'load_data' => $loadData]
         );
+    }
+
+    /** @return array<int,object> */
+    public function getMatchEvents(int $matchId): array
+    {
+        if ($matchId <= 0) {
+            return [];
+        }
+
+        $db = $this->getDatabase();
+        $query = $db->getQuery(true)
+            ->select([
+                'me.*',
+                $db->quoteName('t.name', 'team'),
+                $db->quoteName('et.name', 'event'),
+                "CONCAT(t1.firstname, ' \\'', t1.nickname, '\\' ', t1.lastname) AS player1",
+            ])
+            ->from($db->quoteName('#__sportsmanagement_match_event', 'me'))
+            ->join(
+                'LEFT',
+                $db->quoteName('#__sportsmanagement_season_team_person_id', 'tp1')
+                . ' ON ' . $db->quoteName('tp1.id') . ' = ' . $db->quoteName('me.teamplayer_id')
+            )
+            ->join(
+                'LEFT',
+                $db->quoteName('#__sportsmanagement_season_team_id', 'st1')
+                . ' ON ' . $db->quoteName('st1.team_id') . ' = ' . $db->quoteName('tp1.team_id')
+                . ' AND ' . $db->quoteName('st1.season_id') . ' = ' . $db->quoteName('tp1.season_id')
+            )
+            ->join(
+                'LEFT',
+                $db->quoteName('#__sportsmanagement_person', 't1')
+                . ' ON ' . $db->quoteName('t1.id') . ' = ' . $db->quoteName('tp1.person_id')
+                . ' AND ' . $db->quoteName('t1.published') . ' = 1'
+            )
+            ->join(
+                'LEFT',
+                $db->quoteName('#__sportsmanagement_team', 't')
+                . ' ON ' . $db->quoteName('t.id') . ' = ' . $db->quoteName('st1.team_id')
+            )
+            ->join(
+                'LEFT',
+                $db->quoteName('#__sportsmanagement_eventtype', 'et')
+                . ' ON ' . $db->quoteName('et.id') . ' = ' . $db->quoteName('me.event_type_id')
+            )
+            ->where($db->quoteName('me.match_id') . ' = ' . $matchId)
+            ->order($db->quoteName('me.event_time') . ' ASC');
+
+        try {
+            $db->setQuery($query);
+            return $db->loadObjectList() ?: [];
+        } catch (\Throwable $e) {
+            $this->setError($e->getMessage());
+            return [];
+        }
     }
 
     public function saveorder($pks = null, $order = null)
