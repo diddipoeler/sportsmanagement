612124a5080b50b6ca0265ef70ebd091b9c29605
diddipoeler <diddipoeler@gmx.de>
2026-09-05 14:42:39 +0200

    Complete Joomla 5/6 Updates view metadata

--- admin/views/updates/view.html.php
+++ admin/views/updates/view.html.php
@@ -1,5 +1,12 @@
 <?php
-/** Legacy compatibility bridge for the native Joomla 5/6 administrator Updates view. */
+/**
+ * Legacy compatibility bridge for the native Joomla 5/6 administrator Updates view.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 \defined('_JEXEC') or die;
 
 use Diddipoeler\Component\SportsManagement\Administrator\View\Updates\HtmlView;
