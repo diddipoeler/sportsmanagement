577c91205279335391354267d79dd9f49ed119d1
diddipoeler <diddipoeler@gmx.de>
2026-09-05 19:44:15 +0200

    Align TrainingsData layout metadata with Joomla 5/6

--- modules/mod_sportsmanagement_trainingsdata/tmpl/default.php
+++ modules/mod_sportsmanagement_trainingsdata/tmpl/default.php
@@ -2,7 +2,7 @@
 /**
  * Joomla 5/6 layout for the SportsManagement trainings data module.
  *
- * @version    3.8.0
+ * @version    5.6.0
  * @author     diddipoeler
  * @copyright  Copyright (C) 2015 diddipoeler
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
