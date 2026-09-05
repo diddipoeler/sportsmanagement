29e2e795a7e1a2acf35f4ef1834e49da11457727
diddipoeler <diddipoeler@gmx.de>
2026-09-05 18:48:39 +0200

    Normalize first league Joomla 5/6 service namespaces

--- modules/mod_sportsmanagement_firstleagueoverview/services/provider.php
+++ modules/mod_sportsmanagement_firstleagueoverview/services/provider.php
@@ -19,8 +19,8 @@ return new class implements ServiceProviderInterface
 {
     public function register(Container $container): void
     {
-        $container->registerServiceProvider(new ModuleDispatcherFactory('\\Diddipoeler\\Module\\SportsManagementFirstLeagueOverview'));
-        $container->registerServiceProvider(new HelperFactory('\\Diddipoeler\\Module\\SportsManagementFirstLeagueOverview\\Site\\Helper'));
+        $container->registerServiceProvider(new ModuleDispatcherFactory('Diddipoeler\\Module\\SportsManagementFirstLeagueOverview'));
+        $container->registerServiceProvider(new HelperFactory('Diddipoeler\\Module\\SportsManagementFirstLeagueOverview\\Site\\Helper'));
         $container->registerServiceProvider(new Module());
     }
 };
