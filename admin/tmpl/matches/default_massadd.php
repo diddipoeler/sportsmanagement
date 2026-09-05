d3132e732341e7224f5a8b2093f3a56431904ec0
diddipoeler <diddipoeler@gmx.de>
2026-09-05 20:16:14 +0200

    Add Joomla 5/6 metadata to matches mass-add layout

--- admin/tmpl/matches/default_massadd.php
+++ admin/tmpl/matches/default_massadd.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Native Joomla 5/6 administrator mass-add layout for project matches.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 \defined('_JEXEC') or die;
 
 use Joomla\CMS\HTML\HTMLHelper;
