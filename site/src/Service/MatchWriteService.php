b75ca012c114dcc2f66d670571ab08f7a355317f
diddipoeler <diddipoeler@gmx.de>
2026-09-05 20:25:13 +0200

    Add Joomla 5/6 metadata to site match write alias

--- site/src/Service/MatchWriteService.php
+++ site/src/Service/MatchWriteService.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Joomla 5/6 frontend alias for the shared match write service.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 namespace Diddipoeler\Component\SportsManagement\Site\Service;
 
 \defined('_JEXEC') or die;
