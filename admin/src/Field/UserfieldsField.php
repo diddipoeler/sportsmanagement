c754d1e89e322b6c19918ed79307cb4d9f33aa56
diddipoeler <diddipoeler@gmx.de>
2026-09-05 13:53:14 +0200

    Modernize Joomla 5/6 user fields selector

--- admin/src/Field/UserfieldsField.php
+++ admin/src/Field/UserfieldsField.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Joomla 5/6 native user fields selector.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 namespace Diddipoeler\Component\SportsManagement\Administrator\Field;
 
 \defined('_JEXEC') or die;
@@ -23,7 +31,7 @@ final class UserfieldsField extends SportsManagementListField
     {
         $view = Factory::getApplication()->getInput()->getCmd('view');
         $db = $this->getSportsManagementDatabase();
-        $query = $db->getQuery(true)
+        $query = $db->createQuery()
             ->select([
                 $db->quoteName('id', 'value'),
                 $db->quoteName('name', 'text'),
