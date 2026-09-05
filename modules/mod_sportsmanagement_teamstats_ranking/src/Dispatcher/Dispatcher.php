08058a5af58d5a95ef8b7eda0b8f2119e8eae146
diddipoeler <diddipoeler@gmx.de>
2026-09-05 12:45:13 +0200

    Update team stats ranking dispatcher metadata

--- modules/mod_sportsmanagement_teamstats_ranking/src/Dispatcher/Dispatcher.php
+++ modules/mod_sportsmanagement_teamstats_ranking/src/Dispatcher/Dispatcher.php
@@ -2,7 +2,7 @@
 /**
  * Joomla 5/6 dispatcher for the SportsManagement Team Stats Ranking module.
  *
- * @version    4.24.00
+ * @version    5.6.0
  * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
  * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
