2be80239b072c429f1588c7f8838bbb5d248514e
diddipoeler <diddipoeler@gmx.de>
2026-09-06 09:06:18 +0200

    Standardize Joomla 5/6 all clubs bridge metadata

--- site/models/allclubs.php
+++ site/models/allclubs.php
@@ -1,12 +1,10 @@
 <?php
 /**
- * SportsManagement legacy compatibility bridge.
+ * SportsManagement legacy compatibility bridge for the native Joomla 5/6 all clubs model.
  *
- * The active Joomla 5/6 implementation lives in site/src/Model/AllclubsModel.php.
- *
- * @version    4.24.00
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 
