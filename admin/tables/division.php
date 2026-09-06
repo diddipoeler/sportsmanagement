9aef88b48fe9c3fc3b8ca6718312d480feb058c6
diddipoeler <diddipoeler@gmx.de>
2026-09-06 09:06:30 +0200

    Add Joomla 5/6 metadata to admin Division table bridge

--- admin/tables/division.php
+++ admin/tables/division.php
@@ -1,5 +1,12 @@
 <?php
-/** SportsManagement legacy compatibility bridge for the division table. */
+/**
+ * SportsManagement legacy compatibility bridge for the native administrator Division table.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 \defined('_JEXEC') or die;
 
 use Diddipoeler\Component\SportsManagement\Administrator\Table\DivisionTable;
