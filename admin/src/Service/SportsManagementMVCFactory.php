c42f3f7cd6fd6c557c76658255904adccef1ff4d
diddipoeler <diddipoeler@gmx.de>
2026-09-06 17:46:06 +0200

    Complete Joomla 5/6 metadata for MVC factory service

--- admin/src/Service/SportsManagementMVCFactory.php
+++ admin/src/Service/SportsManagementMVCFactory.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Joomla 5/6 MVC factory with controlled legacy compatibility fallbacks.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 namespace Diddipoeler\Component\SportsManagement\Administrator\Service;
 
 \defined('_JEXEC') or die;
