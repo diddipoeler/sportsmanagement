96a26b90fd21c14b98b70cc8ee2de6cef2d142e5
diddipoeler <diddipoeler@gmx.de>
2026-09-06 09:06:38 +0200

    Add Joomla 5/6 metadata to admin Eventtype table bridge

--- admin/tables/eventtype.php
+++ admin/tables/eventtype.php
@@ -1,5 +1,12 @@
 <?php
-/** SportsManagement legacy compatibility bridge for the event type table. */
+/**
+ * SportsManagement legacy compatibility bridge for the native administrator Eventtype table.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 \defined('_JEXEC') or die;
 
 use Diddipoeler\Component\SportsManagement\Administrator\Table\EventtypeTable;
