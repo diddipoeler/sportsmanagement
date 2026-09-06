23c4fa1f496c931edbcf97ee84a23cc358163e3a
diddipoeler <diddipoeler@gmx.de>
2026-09-06 18:30:26 +0200

    Complete Joomla 5/6 metadata for image and calendar import models

--- admin/src/Model/ImagehandlerModel.php
+++ admin/src/Model/ImagehandlerModel.php
@@ -1,5 +1,11 @@
 <?php
 /**
+ * Joomla 5/6 administrator image selector model.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
  * @package     SportsManagement
  * @subpackage  com_sportsmanagement
  */
