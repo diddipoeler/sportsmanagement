b3675f937eb3de7de55f7d029e9a62d6ce713db1
diddipoeler <diddipoeler@gmx.de>
2026-09-06 10:17:14 +0200

    Add Joomla 5/6 JSM calendar table bridge metadata batch 94

--- admin/tables/jsmgcalendar.php
+++ admin/tables/jsmgcalendar.php
@@ -1,5 +1,13 @@
 <?php
-defined('_JEXEC') or die('Restricted access');
+/**
+ * Legacy compatibility bridge for the native Joomla 5/6 JSM Google Calendar table.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
+\defined('_JEXEC') or die;
 
 use Diddipoeler\Component\SportsManagement\Administrator\Table\JsmgcalendarTable;
 
