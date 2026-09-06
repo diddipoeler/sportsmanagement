4e37abdfac5042e3e2e6e3c9b934471a83dc3371
diddipoeler <diddipoeler@gmx.de>
2026-09-06 11:24:20 +0200

    Normalize Joomla 5/6 allpersons items bridge metadata batch 97

--- site/views/allpersons/tmpl/default_items.php
+++ site/views/allpersons/tmpl/default_items.php
@@ -1,5 +1,12 @@
 <?php
-/** Legacy compatibility bridge for the native Joomla 5/6 all-persons items layout. */
+/**
+ * Legacy compatibility bridge for the native Joomla 5/6 all-persons items layout.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 \defined('_JEXEC') or die;
 
 require JPATH_SITE . '/components/com_sportsmanagement/tmpl/allpersons/default_items.php';
