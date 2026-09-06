d595bfef98a6ddc153e3bce761adcbad824ab7b2
diddipoeler <diddipoeler@gmx.de>
2026-09-06 09:06:51 +0200

    Add Joomla 5/6 metadata to native Division table

--- admin/src/Table/DivisionTable.php
+++ admin/src/Table/DivisionTable.php
@@ -1,9 +1,12 @@
 <?php
 /**
- * @package     SportsManagement
- * @subpackage  com_sportsmanagement
+ * Native Joomla 5/6 administrator Division table.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
-
 namespace Diddipoeler\Component\SportsManagement\Administrator\Table;
 
 \defined('_JEXEC') or die;
