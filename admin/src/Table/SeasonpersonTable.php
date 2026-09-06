9cfa01a0197c89ae8c47b573e4982dd419d1abfc
diddipoeler <diddipoeler@gmx.de>
2026-09-06 11:25:09 +0200

    Add native Joomla 5/6 season person table batch 97

---
+++ admin/src/Table/SeasonpersonTable.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Native Joomla 5/6 table implementation for SportsManagement season-person assignments.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
+namespace Diddipoeler\Component\SportsManagement\Administrator\Table;
+
+\defined('_JEXEC') or die;
+
+use Joomla\Database\DatabaseInterface;
+
+final class SeasonpersonTable extends SportsManagementTable
+{
+    public function __construct(DatabaseInterface $db)
+    {
+        parent::__construct('#__sportsmanagement_season_person_id', 'id', $db);
+    }
+}
