2914d462848d0327711bdf362626f60c3d95132b
diddipoeler <diddipoeler@gmx.de>
2026-09-06 09:06:23 +0200

    Add Joomla 5/6 metadata to admin Club table bridge

--- admin/tables/club.php
+++ admin/tables/club.php
@@ -1,8 +1,11 @@
 <?php
 /**
- * SportsManagement legacy compatibility bridge.
+ * SportsManagement legacy compatibility bridge for the native administrator Club table.
  *
- * The active Joomla 5/6 implementation lives in admin/src/Table/ClubTable.php.
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 
 defined('_JEXEC') or die('Restricted access');
