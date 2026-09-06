78a27c41900ae6b2497f8823a8460539fa1c43fd
diddipoeler <diddipoeler@gmx.de>
2026-09-06 13:24:23 +0200

    Complete Teaminfo Joomla 5/6 PHP metadata

--- site/src/View/Teaminfo/HtmlView.php
+++ site/src/View/Teaminfo/HtmlView.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Native Joomla 5/6 view for the frontend team information page.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 namespace Diddipoeler\Component\SportsManagement\Site\View\Teaminfo;
 
 \defined('_JEXEC') or die;
--- site/views/teaminfo/view.html.php
+++ site/views/teaminfo/view.html.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Legacy compatibility bridge for the native Joomla 5/6 Teaminfo view.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 \defined('_JEXEC') or die;
 
 use Diddipoeler\Component\SportsManagement\Site\View\Teaminfo\HtmlView;
