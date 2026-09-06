5cb922956881b29d8a08c8fde23f6af9ad696531
diddipoeler <diddipoeler@gmx.de>
2026-09-06 20:32:22 +0200

    Use public SportsManagement database access in site list views

--- site/src/View/Allclubs/HtmlView.php
+++ site/src/View/Allclubs/HtmlView.php
@@ -62,7 +62,7 @@ final class HtmlView extends SportsManagementHtmlView
         $this->filter = (string) $this->state->get('filter.search', '');
         $this->sortDirection = (string) $this->state->get('filter_order_Dir', 'ASC');
         $this->sortColumn = (string) $this->state->get('filter_order', 'v.name');
-        $this->user = $this->getApplication()->getIdentity();
+        $this->user = $this->app->getIdentity();
         $this->lists = $this->buildFilterLists($model);
         $this->form = (object) ['limitField' => $this->pagination->getLimitBox()];
 
@@ -75,7 +75,7 @@ final class HtmlView extends SportsManagementHtmlView
     {
         $options = [
             HTMLHelper::_('select.option', '0', Text::_('COM_SPORTSMANAGEMENT_GLOBAL_SELECT_COUNTRY')),
-            ...CountryPresentationHelper::options($model->getDatabase()),
+            ...CountryPresentationHelper::options($model->getSportsManagementDatabase()),
         ];
 
         return [
