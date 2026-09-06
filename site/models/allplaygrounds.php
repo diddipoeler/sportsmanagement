a9e5e471acf53dd9758e578d7372380ac9f9ba3d
diddipoeler <diddipoeler@gmx.de>
2026-09-06 10:03:55 +0200

    Normalize Joomla 5/6 metadata for all playgrounds bridge

--- site/models/allplaygrounds.php
+++ site/models/allplaygrounds.php
@@ -1,16 +1,15 @@
 <?php
 /**
- * SportsManagement legacy compatibility bridge.
+ * SportsManagement legacy compatibility bridge for the native Allplaygrounds model.
  *
  * The active Joomla 5/6 implementation lives in site/src/Model/AllplaygroundsModel.php.
  *
- * @version    4.24.00
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
-
-defined('_JEXEC') or die('Restricted access');
+\defined('_JEXEC') or die;
 
 use Diddipoeler\Component\SportsManagement\Site\Model\AllplaygroundsModel;
 
