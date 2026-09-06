0701cea0dfd675d12b0155fd73ad19ce043aa682
diddipoeler <diddipoeler@gmx.de>
2026-09-06 09:58:24 +0200

    Normalize Joomla 5/6 metadata for round table bridge

--- admin/tables/round.php
+++ admin/tables/round.php
@@ -1,5 +1,15 @@
 <?php
-defined('_JEXEC') or die('Restricted access');
+/**
+ * SportsManagement legacy compatibility bridge for the native Round table.
+ *
+ * The active Joomla 5/6 implementation lives in admin/src/Table/RoundTable.php.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
+\defined('_JEXEC') or die;
 
 use Diddipoeler\Component\SportsManagement\Administrator\Table\RoundTable;
 
