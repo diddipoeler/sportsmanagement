b459de4ba957db0bf66184c9eaf3df3f5a537e1a
diddipoeler <diddipoeler@gmx.de>
2026-09-06 11:24:53 +0200

    Normalize Joomla 5/6 person table bridge metadata batch 97

--- admin/tables/player.php
+++ admin/tables/player.php
@@ -1,6 +1,13 @@
 <?php
-/** Legacy compatibility bridge for the native SportsManagement person table. */
-defined('_JEXEC') or die('Restricted access');
+/**
+ * Legacy compatibility bridge for the native Joomla 5/6 SportsManagement person table.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
+\defined('_JEXEC') or die;
 
 use Diddipoeler\Component\SportsManagement\Administrator\Table\PersonTable;
 
