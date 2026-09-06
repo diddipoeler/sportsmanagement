7556b88eae8fa48d04b4119e177aa36d005b2fd4
diddipoeler <diddipoeler@gmx.de>
2026-09-06 20:03:08 +0200

    Complete Joomla 5/6 metadata for administrator field bridges

--- admin/elements/nameformat.php
+++ admin/elements/nameformat.php
@@ -1,6 +1,11 @@
 <?php
 /**
  * Legacy compatibility bridge for the SportsManagement name-format element.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 defined('_JEXEC') or die('Restricted access');
 
