a16518c37f4df636dd9ff7cf6568dea21c977f2e
diddipoeler <diddipoeler@gmx.de>
2026-09-07 12:36:30 +0200

    Modernize matchreport roster playground input access

--- site/views/matchreport/tmpl/default_rosterplayground.php
+++ site/views/matchreport/tmpl/default_rosterplayground.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- * @version    1.0.05
+ * @version    5.6.0
  * @package    Sportsmanagement
  * @subpackage matchreport
  * @file       default_rosterplayground.php
@@ -10,7 +10,6 @@
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\Factory;
 
 $startfade = $this->config['roster_playground_player_fade'];
 
@@ -265,8 +264,8 @@ for ($a = 0; $a < sizeof($favteams1); $a++)
 													if ($this->config['show_player_profile_link'])
 													{
 														$routeparameter                       = array();
-														$routeparameter['cfg_which_database'] = Factory::getApplication()->input->getInt('cfg_which_database', 0);
-														$routeparameter['s']                  = Factory::getApplication()->input->getInt('s', 0);
+														$routeparameter['cfg_which_database'] = $this->jinput->getInt('cfg_which_database', 0);
+														$routeparameter['s']                  = $this->jinput->getInt('s', 0);
 														$routeparameter['p']                  = $this->project->slug;
 														$routeparameter['tid']                = $player->team_slug;
 														$routeparameter['pid']                = $player->person_slug;
@@ -338,8 +337,8 @@ for ($a = 0; $a < sizeof($favteams1); $a++)
 													if ($this->config['show_player_profile_link'])
 													{
 														$routeparameter                       = array();
-														$routeparameter['cfg_which_database'] = Factory::getApplication()->input->getInt('cfg_which_database', 0);
-														$routeparameter['s']                  = Factory::getApplication()->input->getInt('s', 0);
+														$routeparameter['cfg_which_database'] = $this->jinput->getInt('cfg_which_database', 0);
+														$routeparameter['s']                  = $this->jinput->getInt('s', 0);
 														$routeparameter['p']                  = $this->project->slug;
 														$routeparameter['tid']                = $player->team_slug;
 														$routeparameter['pid']                = $player->person_slug;
