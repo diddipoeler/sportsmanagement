f119d7203f60923c03465dc5c54960a61936bb06
diddipoeler <diddipoeler@gmx.de>
2026-09-07 12:26:09 +0200

    Modernize matchreport details input access for Joomla 5/6

--- site/views/matchreport/tmpl/default_details.php
+++ site/views/matchreport/tmpl/default_details.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- * @version    1.0.05
+ * @version    5.6.0
  * @package    Sportsmanagement
  * @subpackage matchreport
  * @file       default_details.php
@@ -13,7 +13,6 @@ defined('_JEXEC') or die('Restricted access');
 use Diddipoeler\Component\SportsManagement\Site\Helper\SiteRouteHelper;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\HTML\HTMLHelper;
-use Joomla\CMS\Factory;
 
 ?>
 <!-- Details-->
@@ -175,8 +174,8 @@ use Joomla\CMS\Factory;
 			?>
 			<?php
 			$routeparameter                       = array();
-			$routeparameter['cfg_which_database'] = Factory::getApplication()->input->getInt('cfg_which_database', 0);
-			$routeparameter['s']                  = Factory::getApplication()->input->getInt('s', 0);
+			$routeparameter['cfg_which_database'] = $this->jinput->getInt('cfg_which_database', 0);
+			$routeparameter['s']                  = $this->jinput->getInt('s', 0);
 			$routeparameter['p']                  = $this->project->slug;
 			$routeparameter['pgid']               = $this->match->playground_slug;
 			$playground_link                      = SiteRouteHelper::view('playground', $routeparameter);
@@ -240,8 +239,8 @@ use Joomla\CMS\Factory;
 						foreach ($this->matchreferees as $referee)
 							:
 							$routeparameter                       = array();
-							$routeparameter['cfg_which_database'] = Factory::getApplication()->input->getInt('cfg_which_database', 0);
-							$routeparameter['s']                  = Factory::getApplication()->input->getInt('s', 0);
+							$routeparameter['cfg_which_database'] = $this->jinput->getInt('cfg_which_database', 0);
+							$routeparameter['s']                  = $this->jinput->getInt('s', 0);
 							$routeparameter['p']                  = $this->project->slug;
 							$routeparameter['pid']                = $referee->person_slug;
 							$referee_link                         = SiteRouteHelper::view('referee', $routeparameter);
