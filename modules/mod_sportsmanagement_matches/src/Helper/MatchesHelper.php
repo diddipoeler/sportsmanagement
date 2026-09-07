1918a5617b55213aae1573119051f0f551974a76
diddipoeler <diddipoeler@gmx.de>
2026-09-07 08:44:36 +0200

    Use module application container for Joomla 5/6 matches helper

--- modules/mod_sportsmanagement_matches/src/Helper/MatchesHelper.php
+++ modules/mod_sportsmanagement_matches/src/Helper/MatchesHelper.php
@@ -12,7 +12,6 @@ namespace Diddipoeler\Module\SportsManagementMatches\Site\Helper;
 \defined('_JEXEC') or die;
 
 use Joomla\CMS\Application\CMSApplicationInterface;
-use Joomla\CMS\Factory;
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\Database\DatabaseInterface;
 use Joomla\Registry\Registry;
@@ -37,7 +36,7 @@ final class MatchesHelper
 
         try {
             /** @var DatabaseInterface $joomlaDatabase */
-            $joomlaDatabase = Factory::getContainer()->get(DatabaseInterface::class);
+            $joomlaDatabase = $app->getContainer()->get(DatabaseInterface::class);
             $db = $this->database($params, $joomlaDatabase);
             $matches = $this->loadMatches($db, $params, $projects);
             $showReferees = (int) $params->get('show_referee', 1) === 1;
