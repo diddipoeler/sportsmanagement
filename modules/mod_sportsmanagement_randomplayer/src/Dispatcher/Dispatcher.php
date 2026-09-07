8d5a2e108a78b490f2e4f283f393648702cbcef2
diddipoeler <diddipoeler@gmx.de>
2026-09-07 16:32:07 +0200

    Load complete random player module languages

--- modules/mod_sportsmanagement_randomplayer/src/Dispatcher/Dispatcher.php
+++ modules/mod_sportsmanagement_randomplayer/src/Dispatcher/Dispatcher.php
@@ -32,7 +32,14 @@ final class Dispatcher extends AbstractModuleDispatcher implements HelperFactory
         $data['params']->set('layout', 'native');
 
         $app = $this->getApplication();
-        $app->getLanguage()->load('com_sportsmanagement', JPATH_SITE, null, true);
+        $language = $app->getLanguage();
+        $tag = $language->getTag();
+
+        $language->load('mod_sportsmanagement_randomplayer', JPATH_SITE, $tag, true);
+        $language->load('com_sportsmanagement', JPATH_ADMINISTRATOR, $tag, true);
+        $language->load('com_sportsmanagement', JPATH_SITE, $tag, true);
+        $language->load('com_sportsmanagement_countries', JPATH_ADMINISTRATOR, $tag, true);
+
         /** @var DatabaseInterface $database */
         $database = Factory::getContainer()->get(DatabaseInterface::class);
         $data['list'] = $this->getHelperFactory()
