eec934f0f9559f90de9d7bf105ad82916e231d88
diddipoeler <diddipoeler@gmx.de>
2026-09-07 11:44:32 +0200

    Fix RandomPlayer Joomla application container access

--- modules/mod_sportsmanagement_randomplayer/src/Dispatcher/Dispatcher.php
+++ modules/mod_sportsmanagement_randomplayer/src/Dispatcher/Dispatcher.php
@@ -12,6 +12,7 @@ namespace Diddipoeler\Module\SportsManagementRandomPlayer\Site\Dispatcher;
 \defined('_JEXEC') or die;
 
 use Joomla\CMS\Dispatcher\AbstractModuleDispatcher;
+use Joomla\CMS\Factory;
 use Joomla\CMS\Helper\HelperFactoryAwareInterface;
 use Joomla\CMS\Helper\HelperFactoryAwareTrait;
 use Joomla\Database\DatabaseInterface;
@@ -33,7 +34,7 @@ final class Dispatcher extends AbstractModuleDispatcher implements HelperFactory
         $app = $this->getApplication();
         $app->getLanguage()->load('com_sportsmanagement', JPATH_SITE, null, true);
         /** @var DatabaseInterface $database */
-        $database = $app->getContainer()->get(DatabaseInterface::class);
+        $database = Factory::getContainer()->get(DatabaseInterface::class);
         $data['list'] = $this->getHelperFactory()
             ->getHelper('RandomPlayerHelper')
             ->getData($data['params'], $database);
