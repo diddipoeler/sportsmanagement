d2e3f3e64134d9360c6f8e64a9ae4c201dcf4375
diddipoeler <diddipoeler@gmx.de>
2026-09-07 11:47:13 +0200

    Fix team statistics counter container access

--- modules/mod_sportsmanagement_teamstatistics_counter/src/Dispatcher/Dispatcher.php
+++ modules/mod_sportsmanagement_teamstatistics_counter/src/Dispatcher/Dispatcher.php
@@ -13,6 +13,7 @@ namespace Diddipoeler\Module\SportsManagementTeamStatisticsCounter\Site\Dispatch
 
 use Joomla\CMS\Dispatcher\AbstractModuleDispatcher;
 use Joomla\CMS\Document\HtmlDocument;
+use Joomla\CMS\Factory;
 use Joomla\CMS\Helper\HelperFactoryAwareInterface;
 use Joomla\CMS\Helper\HelperFactoryAwareTrait;
 use Joomla\Database\DatabaseInterface;
@@ -33,7 +34,7 @@ final class Dispatcher extends AbstractModuleDispatcher implements HelperFactory
         $app->getLanguage()->load('com_sportsmanagement', JPATH_ADMINISTRATOR, null, true);
 
         /** @var DatabaseInterface $joomlaDatabase */
-        $joomlaDatabase = $app->getContainer()->get(DatabaseInterface::class);
+        $joomlaDatabase = Factory::getContainer()->get(DatabaseInterface::class);
         $data['data'] = $this->getHelperFactory()
             ->getHelper('TeamStatisticsCounterHelper')
             ->getData($data['params'], $joomlaDatabase);
