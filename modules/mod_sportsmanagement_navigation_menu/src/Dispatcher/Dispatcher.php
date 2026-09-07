b6d103af7e65a5bf182a506453c03759ddcde000
diddipoeler <diddipoeler@gmx.de>
2026-09-07 16:31:54 +0200

    Load complete navigation module languages

--- modules/mod_sportsmanagement_navigation_menu/src/Dispatcher/Dispatcher.php
+++ modules/mod_sportsmanagement_navigation_menu/src/Dispatcher/Dispatcher.php
@@ -30,7 +30,14 @@ final class Dispatcher extends AbstractModuleDispatcher implements HelperFactory
         }
 
         $app = $this->getApplication();
-        $app->getLanguage()->load('com_sportsmanagement', JPATH_ADMINISTRATOR, null, true);
+        $language = $app->getLanguage();
+        $tag = $language->getTag();
+
+        $language->load('mod_sportsmanagement_navigation_menu', JPATH_SITE, $tag, true);
+        $language->load('com_sportsmanagement', JPATH_ADMINISTRATOR, $tag, true);
+        $language->load('com_sportsmanagement', JPATH_SITE, $tag, true);
+        $language->load('com_sportsmanagement_countries', JPATH_ADMINISTRATOR, $tag, true);
+
         /** @var DatabaseInterface $joomlaDatabase */
         $joomlaDatabase = Factory::getContainer()->get(DatabaseInterface::class);
 
