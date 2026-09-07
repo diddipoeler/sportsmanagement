fb4ec214b966feae76cf9ac88339187e82d7b2fd
diddipoeler <diddipoeler@gmx.de>
2026-09-07 09:18:38 +0200

    Use module application container for events ranking

--- modules/mod_sportsmanagement_eventsranking/src/Dispatcher/Dispatcher.php
+++ modules/mod_sportsmanagement_eventsranking/src/Dispatcher/Dispatcher.php
@@ -12,7 +12,6 @@ namespace Diddipoeler\Module\SportsManagementEventsRanking\Site\Dispatcher;
 \defined('_JEXEC') or die;
 
 use Joomla\CMS\Dispatcher\AbstractModuleDispatcher;
-use Joomla\CMS\Factory;
 use Joomla\CMS\Helper\HelperFactoryAwareInterface;
 use Joomla\CMS\Helper\HelperFactoryAwareTrait;
 use Joomla\Database\DatabaseInterface;
@@ -33,7 +32,7 @@ final class Dispatcher extends AbstractModuleDispatcher implements HelperFactory
         $app->getLanguage()->load('com_sportsmanagement', JPATH_ADMINISTRATOR, null, true);
 
         /** @var DatabaseInterface $database */
-        $database = Factory::getContainer()->get(DatabaseInterface::class);
+        $database = $app->getContainer()->get(DatabaseInterface::class);
         $data['rankingData'] = $this->getHelperFactory()
             ->getHelper('EventsRankingHelper')
             ->getData($data['params'], $app, $database);
