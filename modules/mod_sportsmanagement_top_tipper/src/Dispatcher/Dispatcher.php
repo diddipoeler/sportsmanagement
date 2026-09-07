5117490690b308498854fe0a69cb2f3527ed3f6b
diddipoeler <diddipoeler@gmx.de>
2026-09-07 17:05:15 +0200

    Load complete top tipper module languages

--- modules/mod_sportsmanagement_top_tipper/src/Dispatcher/Dispatcher.php
+++ modules/mod_sportsmanagement_top_tipper/src/Dispatcher/Dispatcher.php
@@ -28,8 +28,13 @@ final class Dispatcher extends AbstractModuleDispatcher implements HelperFactory
         }
 
         $app = $this->getApplication();
-        $app->getLanguage()->load('com_sportsmanagement', JPATH_SITE);
-        $app->getLanguage()->load('mod_sportsmanagement_top_tipper', JPATH_SITE . '/modules/mod_sportsmanagement_top_tipper');
+        $language = $app->getLanguage();
+        $tag = $language->getTag();
+
+        $language->load('mod_sportsmanagement_top_tipper', JPATH_SITE . '/modules/mod_sportsmanagement_top_tipper', $tag, true);
+        $language->load('com_sportsmanagement', JPATH_ADMINISTRATOR, $tag, true);
+        $language->load('com_sportsmanagement', JPATH_SITE, $tag, true);
+        $language->load('com_sportsmanagement_countries', JPATH_ADMINISTRATOR, $tag, true);
 
         $app->getDocument()->getWebAssetManager()->registerAndUseStyle(
             'mod_sportsmanagement_top_tipper',
