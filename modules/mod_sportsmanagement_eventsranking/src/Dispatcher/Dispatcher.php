ad7da21f9ad36058a7eb4ef6ea6b1d11d6b449d6
diddipoeler <diddipoeler@gmx.de>
2026-09-07 16:43:00 +0200

    Load complete events ranking module languages

--- modules/mod_sportsmanagement_eventsranking/src/Dispatcher/Dispatcher.php
+++ modules/mod_sportsmanagement_eventsranking/src/Dispatcher/Dispatcher.php
@@ -30,7 +30,13 @@ final class Dispatcher extends AbstractModuleDispatcher implements HelperFactory
         }
 
         $app = $this->getApplication();
-        $app->getLanguage()->load('com_sportsmanagement', JPATH_ADMINISTRATOR, null, true);
+        $language = $app->getLanguage();
+        $tag = $language->getTag();
+
+        $language->load('mod_sportsmanagement_eventsranking', JPATH_SITE, $tag, true);
+        $language->load('com_sportsmanagement', JPATH_ADMINISTRATOR, $tag, true);
+        $language->load('com_sportsmanagement', JPATH_SITE, $tag, true);
+        $language->load('com_sportsmanagement_countries', JPATH_ADMINISTRATOR, $tag, true);
 
         /** @var DatabaseInterface $database */
         $database = Factory::getContainer()->get(DatabaseInterface::class);
