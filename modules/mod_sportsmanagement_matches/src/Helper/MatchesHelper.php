33e83cc9446426b74c62c2c9c448ec52167aa94d
diddipoeler <diddipoeler@gmx.de>
2026-09-07 11:48:45 +0200

    Fix matches module container access on Joomla 5/6

--- modules/mod_sportsmanagement_matches/src/Helper/MatchesHelper.php
+++ modules/mod_sportsmanagement_matches/src/Helper/MatchesHelper.php
@@ -12,6 +12,7 @@ namespace Diddipoeler\Module\SportsManagementMatches\Site\Helper;
 \defined('_JEXEC') or die;
 
 use Joomla\CMS\Application\CMSApplicationInterface;
+use Joomla\CMS\Factory;
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\Database\DatabaseInterface;
 use Joomla\Registry\Registry;
@@ -36,7 +37,7 @@ final class MatchesHelper
 
         try {
             /** @var DatabaseInterface $joomlaDatabase */
-            $joomlaDatabase = $app->getContainer()->get(DatabaseInterface::class);
+            $joomlaDatabase = Factory::getContainer()->get(DatabaseInterface::class);
             $db = $this->database($params, $joomlaDatabase);
             $matches = $this->loadMatches($db, $params, $projects);
             $showReferees = (int) $params->get('show_referee', 1) === 1;
