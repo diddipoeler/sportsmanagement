485f287032daabe9582d2df382dc0e9b1d4f4e72
diddipoeler <diddipoeler@gmx.de>
2026-09-08 15:44:05 +0200

    Fix Joomla 5/6 K2 field container access

--- admin/src/Field/Categorylistk2Field.php
+++ admin/src/Field/Categorylistk2Field.php
@@ -29,7 +29,7 @@ final class Categorylistk2Field extends ListField
         }
 
         /** @var DatabaseInterface $db */
-        $db = $app->getContainer()->get(DatabaseInterface::class);
+        $db = Factory::getContainer()->get(DatabaseInterface::class);
         $query = $db->createQuery()
             ->select([
                 $db->quoteName('id', 'value'),
