18189ed6d12d494b51d0bb7c01782583b2fcb5f8
diddipoeler <diddipoeler@gmx.de>
2026-09-08 14:12:16 +0200

    Fix RandomPlayer dispatcher container access

--- modules/mod_sportsmanagement_randomplayer/src/Dispatcher/Dispatcher.php
+++ modules/mod_sportsmanagement_randomplayer/src/Dispatcher/Dispatcher.php
@@ -13,6 +13,7 @@ namespace Diddipoeler\Module\SportsManagementRandomPlayer\Site\Dispatcher;
 
 use Joomla\CMS\Application\SiteApplication;
 use Joomla\CMS\Dispatcher\AbstractModuleDispatcher;
+use Joomla\CMS\Factory;
 use Joomla\CMS\Helper\HelperFactoryAwareInterface;
 use Joomla\CMS\Helper\HelperFactoryAwareTrait;
 use Joomla\Database\DatabaseInterface;
@@ -46,7 +47,7 @@ final class Dispatcher extends AbstractModuleDispatcher implements HelperFactory
         $language->load('com_sportsmanagement_countries', JPATH_ADMINISTRATOR, $tag, true);
 
         /** @var DatabaseInterface $database */
-        $database = $app->getContainer()->get(DatabaseInterface::class);
+        $database = Factory::getContainer()->get(DatabaseInterface::class);
         $data['list'] = $this->getHelperFactory()
             ->getHelper('RandomPlayerHelper')
             ->getData($data['params'], $database);
