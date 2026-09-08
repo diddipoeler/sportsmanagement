a45172ba7800d8df3c0faff5f04585ca5d6568ce
diddipoeler <diddipoeler@gmx.de>
2026-09-08 14:39:31 +0200

    Fix protected container access in AJAX top navigation helper

--- modules/mod_sportsmanagement_ajax_top_navigation_menu/src/Helper/AjaxTopNavigationHelper.php
+++ modules/mod_sportsmanagement_ajax_top_navigation_menu/src/Helper/AjaxTopNavigationHelper.php
@@ -14,6 +14,7 @@ namespace Diddipoeler\Module\SportsManagementAjaxTopNavigationMenu\Site\Helper;
 use Joomla\CMS\Application\CMSApplicationInterface;
 use Joomla\CMS\Application\SiteApplication;
 use Joomla\CMS\Component\ComponentHelper;
+use Joomla\CMS\Factory;
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\Uri\Uri;
@@ -32,7 +33,7 @@ final class AjaxTopNavigationHelper
 
         $input = $app->getInput();
         /** @var DatabaseInterface $database */
-        $database = $app->getContainer()->get(DatabaseInterface::class);
+        $database = Factory::getContainer()->get(DatabaseInterface::class);
         $legacyHelper = new NavigationDataHelper($params, $app, $database);
         $points = $legacyHelper->getFederations() ?: [];
         $tabPoints = [];
