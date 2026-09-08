9bd4e7f825057f2dbd0feef9a219f6f430fbfdfc
diddipoeler <diddipoeler@gmx.de>
2026-09-08 13:45:52 +0200

    Use site application container in random player dispatcher

--- modules/mod_sportsmanagement_randomplayer/src/Dispatcher/Dispatcher.php
+++ modules/mod_sportsmanagement_randomplayer/src/Dispatcher/Dispatcher.php
@@ -11,8 +11,8 @@ namespace Diddipoeler\Module\SportsManagementRandomPlayer\Site\Dispatcher;
 
 \defined('_JEXEC') or die;
 
+use Joomla\CMS\Application\SiteApplication;
 use Joomla\CMS\Dispatcher\AbstractModuleDispatcher;
-use Joomla\CMS\Factory;
 use Joomla\CMS\Helper\HelperFactoryAwareInterface;
 use Joomla\CMS\Helper\HelperFactoryAwareTrait;
 use Joomla\Database\DatabaseInterface;
@@ -32,6 +32,11 @@ final class Dispatcher extends AbstractModuleDispatcher implements HelperFactory
         $data['params']->set('layout', 'native');
 
         $app = $this->getApplication();
+
+        if (!$app instanceof SiteApplication) {
+            throw new \RuntimeException('SportsManagement RandomPlayer requires the Joomla site application.', 500);
+        }
+
         $language = $app->getLanguage();
         $tag = $language->getTag();
 
@@ -41,7 +46,7 @@ final class Dispatcher extends AbstractModuleDispatcher implements HelperFactory
         $language->load('com_sportsmanagement_countries', JPATH_ADMINISTRATOR, $tag, true);
 
         /** @var DatabaseInterface $database */
-        $database = Factory::getContainer()->get(DatabaseInterface::class);
+        $database = $app->getContainer()->get(DatabaseInterface::class);
         $data['list'] = $this->getHelperFactory()
             ->getHelper('RandomPlayerHelper')
             ->getData($data['params'], $database);
