f46386e2aaa8e60983aad2b5300fcef6fa178f79
diddipoeler <diddipoeler@gmx.de>
2026-09-08 14:13:13 +0200

    Fix TeamPlayers dispatcher container access

--- modules/mod_sportsmanagement_teamplayers/src/Dispatcher/Dispatcher.php
+++ modules/mod_sportsmanagement_teamplayers/src/Dispatcher/Dispatcher.php
@@ -13,6 +13,7 @@ namespace Diddipoeler\Module\SportsManagementTeamPlayers\Site\Dispatcher;
 
 use Joomla\CMS\Application\SiteApplication;
 use Joomla\CMS\Dispatcher\AbstractModuleDispatcher;
+use Joomla\CMS\Factory;
 use Joomla\CMS\Helper\HelperFactoryAwareInterface;
 use Joomla\CMS\Helper\HelperFactoryAwareTrait;
 use Joomla\Database\DatabaseInterface;
@@ -42,7 +43,7 @@ final class Dispatcher extends AbstractModuleDispatcher implements HelperFactory
         $language->load('com_sportsmanagement', JPATH_ADMINISTRATOR, null, true);
 
         /** @var DatabaseInterface $database */
-        $database = $app->getContainer()->get(DatabaseInterface::class);
+        $database = Factory::getContainer()->get(DatabaseInterface::class);
         $payload = $this->getHelperFactory()
             ->getHelper('TeamPlayersHelper')
             ->getData($data['params'], $database);
