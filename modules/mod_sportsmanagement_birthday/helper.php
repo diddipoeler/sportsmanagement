0d56a633d98b391e4c4a23c6436e18c5c943ffbb
diddipoeler <diddipoeler@gmx.de>
2026-09-08 14:14:57 +0200

    Fix Birthday helper container access

--- modules/mod_sportsmanagement_birthday/helper.php
+++ modules/mod_sportsmanagement_birthday/helper.php
@@ -42,7 +42,7 @@ if (!class_exists('modSportsmanagementBirthdayDataHelper', false)) {
         public function getData(Registry $params, Registry $componentParams, CMSApplicationInterface $app): array
         {
             /** @var DatabaseInterface $database */
-            $database = $app->getContainer()->get(DatabaseInterface::class);
+            $database = Factory::getContainer()->get(DatabaseInterface::class);
 
             return (new BirthdayHelper())->getData($params, $componentParams, $app, $database);
         }
@@ -61,7 +61,7 @@ if (!class_exists('modSportsmanagementBirthdayHelper', false)) {
             }
 
             /** @var DatabaseInterface $database */
-            $database = $app->getContainer()->get(DatabaseInterface::class);
+            $database = Factory::getContainer()->get(DatabaseInterface::class);
 
             return (new BirthdayHelper())->getData(
                 $params,
