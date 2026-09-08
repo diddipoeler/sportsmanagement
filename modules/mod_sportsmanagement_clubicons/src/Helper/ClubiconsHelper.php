cd28b97be768ccd9e56efac78d34eab2fb0c20f2
diddipoeler <diddipoeler@gmx.de>
2026-09-08 09:00:14 +0200

    Use application container in clubicons helper

--- modules/mod_sportsmanagement_clubicons/src/Helper/ClubiconsHelper.php
+++ modules/mod_sportsmanagement_clubicons/src/Helper/ClubiconsHelper.php
@@ -16,7 +16,6 @@ use Diddipoeler\Component\SportsManagement\Site\Service\RankingEngine;
 use Diddipoeler\Component\SportsManagement\Site\Service\SportsManagementDatabaseResolver;
 use Joomla\CMS\Application\CMSApplicationInterface;
 use Joomla\CMS\Component\ComponentHelper;
-use Joomla\CMS\Factory;
 use Joomla\CMS\Uri\Uri;
 use Joomla\Database\DatabaseInterface;
 use Joomla\Registry\Registry;
@@ -37,7 +36,7 @@ final class ClubiconsHelper
         }
 
         /** @var DatabaseInterface $joomlaDatabase */
-        $joomlaDatabase = Factory::getContainer()->get(DatabaseInterface::class);
+        $joomlaDatabase = $app->getContainer()->get(DatabaseInterface::class);
         $db = $this->database($params, $joomlaDatabase);
         $divisionId = $this->firstId($params->get('division_id', 0));
         $result = (new RankingEngine($db))->calculate($projectId, $divisionId);
