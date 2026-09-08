f1eb1fd3b863b541f4a36746c8a52ad2c94fc087
diddipoeler <diddipoeler@gmx.de>
2026-09-08 21:27:20 +0200

    Resolve club birthday bridge database from site application container

--- modules/mod_sportsmanagement_club_birthday/helper.php
+++ modules/mod_sportsmanagement_club_birthday/helper.php
@@ -39,7 +39,7 @@ if (!class_exists('modSportsmanagementClubBirthdayHelper', false)) {
             }
 
             /** @var DatabaseInterface $database */
-            $database = Factory::getContainer()->get(DatabaseInterface::class);
+            $database = $app->getContainer()->get(DatabaseInterface::class);
 
             return (new ClubBirthdayHelper())->getData($params, $app, $database);
         }
