832a7977631bc28af4e26168417df42363e8f024
diddipoeler <diddipoeler@gmx.de>
2026-09-08 12:53:48 +0200

    Use site application container in first league dispatcher

--- modules/mod_sportsmanagement_firstleagueoverview/src/Dispatcher/Dispatcher.php
+++ modules/mod_sportsmanagement_firstleagueoverview/src/Dispatcher/Dispatcher.php
@@ -11,8 +11,8 @@ namespace Diddipoeler\Module\SportsManagementFirstLeagueOverview\Site\Dispatcher
 
 \defined('_JEXEC') or die;
 
+use Joomla\CMS\Application\SiteApplication;
 use Joomla\CMS\Dispatcher\AbstractModuleDispatcher;
-use Joomla\CMS\Factory;
 use Joomla\CMS\Helper\HelperFactoryAwareInterface;
 use Joomla\CMS\Helper\HelperFactoryAwareTrait;
 use Joomla\Database\DatabaseInterface;
@@ -30,6 +30,11 @@ final class Dispatcher extends AbstractModuleDispatcher implements HelperFactory
         }
 
         $app = $this->getApplication();
+
+        if (!$app instanceof SiteApplication) {
+            throw new \RuntimeException('SportsManagement First League Overview requires the Joomla site application.', 500);
+        }
+
         $language = $app->getLanguage();
         $tag = $language->getTag();
 
@@ -39,7 +44,7 @@ final class Dispatcher extends AbstractModuleDispatcher implements HelperFactory
         $language->load('com_sportsmanagement_countries', JPATH_ADMINISTRATOR, $tag, true);
 
         /** @var DatabaseInterface $database */
-        $database = Factory::getContainer()->get(DatabaseInterface::class);
+        $database = $app->getContainer()->get(DatabaseInterface::class);
         $overview = $this->getHelperFactory()
             ->getHelper('FirstLeagueOverviewHelper')
             ->getData($data['params'], $database);
