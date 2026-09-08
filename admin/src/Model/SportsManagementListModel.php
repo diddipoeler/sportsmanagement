feb97337f92b1cba1fa4c70d8835d99e10066836
diddipoeler <diddipoeler@gmx.de>
2026-09-08 21:22:04 +0200

    Use DI container for administrator list model application

--- admin/src/Model/SportsManagementListModel.php
+++ admin/src/Model/SportsManagementListModel.php
@@ -24,10 +24,10 @@ abstract class SportsManagementListModel extends ListModel
      */
     private bool $stateReadInProgress = false;
 
-    /** Resolve the active Joomla administrator application. */
+    /** Resolve the active Joomla administrator application from the DI container. */
     protected function administratorApplication(): AdministratorApplication
     {
-        $app = Factory::getApplication();
+        $app = Factory::getContainer()->get(AdministratorApplication::class);
 
         if (!$app instanceof AdministratorApplication) {
             throw new \RuntimeException('SportsManagement administrator application is unavailable.');
