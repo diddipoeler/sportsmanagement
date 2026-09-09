571acc4f673da3d4ad1dbeaf2c80a3c1a1140b6f
diddipoeler <diddipoeler@gmx.de>
2026-09-09 10:53:58 +0200

    Use native Joomla 5/6 extra-field table bridge

--- admin/tables/extrafield.php
+++ admin/tables/extrafield.php
@@ -1,38 +1,25 @@
 <?php
 /**
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage tables
- * @file       extrafield.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * Legacy compatibility bridge for the native Joomla 5/6 extra-field table.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
-defined('_JEXEC') or die('Restricted access');
+\defined('_JEXEC') or die;
 
-/**
- * sportsmanagementTableExtraField
- *
- * @package
- * @author
- * @copyright diddi
- * @version   2014
- * @access    public
- */
-class sportsmanagementTableExtraField extends JSMTable
-{
-	/**
-	 * Constructor
-	 *
-	 * @param   object Database connector object
-	 *
-	 * @since 1.0
-	 */
-	function __construct(&$db)
-	{
-		$db = sportsmanagementHelper::getDBConnection();
-		parent::__construct('#__sportsmanagement_user_extra_fields', 'id', $db);
-	}
+use Diddipoeler\Component\SportsManagement\Administrator\Table\ExtrafieldTable;
 
+if (!class_exists(ExtrafieldTable::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Table/SportsManagementTable.php';
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Table/ExtrafieldTable.php';
+}
+
+if (!class_exists(ExtrafieldTable::class)) {
+    throw new \RuntimeException('SportsManagement native Extrafield table could not be loaded.', 500);
+}
+
+if (!class_exists('sportsmanagementTableExtraField', false)) {
+    class_alias(ExtrafieldTable::class, 'sportsmanagementTableExtraField');
 }
