ff02cdc712a62b4f4af6c9b6d67e4edc5dd50c5b
diddipoeler <diddipoeler@gmx.de>
2026-09-09 14:34:44 +0200

    Use Joomla DI for administrator user list field

--- admin/src/Field/UserlistField.php
+++ admin/src/Field/UserlistField.php
@@ -22,7 +22,8 @@ final class UserlistField extends ListField
 
     protected function getOptions(): array
     {
-        $app = Factory::getApplication();
+        /** @var AdministratorApplication $app */
+        $app = Factory::getContainer()->get(AdministratorApplication::class);
 
         if (!$app instanceof AdministratorApplication) {
             throw new \RuntimeException('SportsManagement administrator application is unavailable.');
