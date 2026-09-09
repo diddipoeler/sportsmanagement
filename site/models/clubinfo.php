1b2431261db2f57ab871b7302ac5e0acfffa83d4
diddipoeler <diddipoeler@gmx.de>
2026-09-09 13:37:54 +0200

    Harden Joomla 5/6 Clubinfo model bridge

--- site/models/clubinfo.php
+++ site/models/clubinfo.php
@@ -1,20 +1,33 @@
 <?php
 /**
- * Joomla 5/6 Clubinfo migration.
+ * SportsManagement legacy compatibility bridge for the native Joomla 5/6 club info model.
  *
  * @version    5.6.0
  * @author     diddipoeler
  * @copyright  Copyright (C) diddipoeler
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
+
 defined('_JEXEC') or die;
 
 use Diddipoeler\Component\SportsManagement\Site\Model\ClubinfoModel;
 
 if (!class_exists(ClubinfoModel::class)) {
-    require_once JPATH_SITE . '/components/com_sportsmanagement/src/Model/SportsManagementModel.php';
-    require_once JPATH_SITE . '/components/com_sportsmanagement/src/Model/SportsManagementProjectModel.php';
-    require_once JPATH_SITE . '/components/com_sportsmanagement/src/Model/ClubinfoModel.php';
+    $nativeModels = [
+        JPATH_SITE . '/components/com_sportsmanagement/src/Model/SportsManagementModel.php',
+        JPATH_SITE . '/components/com_sportsmanagement/src/Model/SportsManagementProjectModel.php',
+        JPATH_SITE . '/components/com_sportsmanagement/src/Model/ClubinfoModel.php',
+    ];
+
+    foreach ($nativeModels as $nativeModel) {
+        if (is_file($nativeModel)) {
+            require_once $nativeModel;
+        }
+    }
+}
+
+if (!class_exists(ClubinfoModel::class)) {
+    throw new \RuntimeException('SportsManagement native Clubinfo model could not be loaded.', 500);
 }
 
 if (!class_exists('sportsmanagementModelClubInfo', false)) {
