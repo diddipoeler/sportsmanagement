06687c6dfb066f4c0ff84bad6455fd61449caf3c
diddipoeler <diddipoeler@gmx.de>
2026-09-09 15:56:18 +0200

    Use Joomla root URLs for quote mark assets

--- modules/mod_sportsmanagement_rquotes/tmpl/_rquote.php
+++ modules/mod_sportsmanagement_rquotes/tmpl/_rquote.php
@@ -7,11 +7,14 @@
  */
 \defined('_JEXEC') or die;
 
+use Joomla\CMS\Uri\Uri;
+
 $quotemarks = (int) $params->get('quotemarks', 0);
 $showPicture = (bool) $params->get('showpicture', 0);
 $author = htmlspecialchars((string) ($rquote->author ?? ''), ENT_QUOTES, 'UTF-8');
 $quote = (string) ($rquote->quote ?? '');
 $pictureUrl = (string) ($rquote->picture_url ?? '');
+$quoteAssetBase = rtrim(Uri::root(true), '/') . '/modules/mod_sportsmanagement_rquotes/assets/images/';
 
 if ($quotemarks > 0) {
     $quote = strip_tags($quote, '<img><br><a>');
@@ -30,9 +33,9 @@ if ($quotemarks > 0) {
             <span>&quot; <?php echo $quote; ?> &quot;</span>
         <?php elseif ($quotemarks === 2) : ?>
             <span>
-                <img src="modules/mod_sportsmanagement_rquotes/assets/images/quote1_25_start.png" width="15" height="15" alt="">
+                <img src="<?php echo htmlspecialchars($quoteAssetBase . 'quote1_25_start.png', ENT_QUOTES, 'UTF-8'); ?>" width="15" height="15" alt="">
                 <?php echo $quote; ?>
-                <img src="modules/mod_sportsmanagement_rquotes/assets/images/quote1_25_end.png" width="15" height="15" alt="">
+                <img src="<?php echo htmlspecialchars($quoteAssetBase . 'quote1_25_end.png', ENT_QUOTES, 'UTF-8'); ?>" width="15" height="15" alt="">
             </span>
         <?php elseif ($quotemarks === 3) : ?>
             <span class="mod_rquote_css"><span><?php echo $quote; ?></span></span>
