fd4359bccf0cef45b93e79b117a59683534fc391
diddipoeler <diddipoeler@gmx.de>
2026-09-09 12:08:23 +0200

    Harden Joomla 5/6 Databasetools model bridge

--- admin/models/databasetools.php
+++ admin/models/databasetools.php
@@ -12,8 +12,20 @@ defined('_JEXEC') or die('Restricted access');
 use Diddipoeler\Component\SportsManagement\Administrator\Model\DatabasetoolsModel;
 
 if (!class_exists(DatabasetoolsModel::class)) {
-    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Model/SportsManagementListModel.php';
-    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Model/DatabasetoolsModel.php';
+    $nativeModels = [
+        JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Model/SportsManagementListModel.php',
+        JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Model/DatabasetoolsModel.php',
+    ];
+
+    foreach ($nativeModels as $nativeModel) {
+        if (is_file($nativeModel)) {
+            require_once $nativeModel;
+        }
+    }
+}
+
+if (!class_exists(DatabasetoolsModel::class)) {
+    throw new \RuntimeException('SportsManagement native Databasetools model could not be loaded.', 500);
 }
 
 if (!class_exists('sportsmanagementModelDatabaseTools', false)) {
