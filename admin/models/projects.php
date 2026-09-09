c75005b99592849209a083cdffad8eb9515a9db4
diddipoeler <diddipoeler@gmx.de>
2026-09-09 13:53:48 +0200

    Harden Joomla 5/6 Projects admin model bridge

--- admin/models/projects.php
+++ admin/models/projects.php
@@ -12,8 +12,20 @@ defined('_JEXEC') or die('Restricted access');
 use Diddipoeler\Component\SportsManagement\Administrator\Model\ProjectsModel;
 
 if (!class_exists(ProjectsModel::class)) {
-    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Model/SportsManagementListModel.php';
-    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Model/ProjectsModel.php';
+    $nativeModels = [
+        JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Model/SportsManagementListModel.php',
+        JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Model/ProjectsModel.php',
+    ];
+
+    foreach ($nativeModels as $nativeModel) {
+        if (is_file($nativeModel)) {
+            require_once $nativeModel;
+        }
+    }
+}
+
+if (!class_exists(ProjectsModel::class)) {
+    throw new \RuntimeException('SportsManagement native Projects model could not be loaded.', 500);
 }
 
 if (!class_exists('sportsmanagementModelProjects', false)) {
