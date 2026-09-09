d1a2859343cd8c14e2fdc863161f22cc634eec71
diddipoeler <diddipoeler@gmx.de>
2026-09-09 12:05:59 +0200

    Harden Joomla 5/6 Allteams model bridge

--- site/models/allteams.php
+++ site/models/allteams.php
@@ -14,8 +14,20 @@
 use Diddipoeler\Component\SportsManagement\Site\Model\AllteamsModel;
 
 if (!class_exists(AllteamsModel::class)) {
-    require_once JPATH_SITE . '/components/com_sportsmanagement/src/Model/SportsManagementListModel.php';
-    require_once JPATH_SITE . '/components/com_sportsmanagement/src/Model/AllteamsModel.php';
+    $nativeModels = [
+        JPATH_SITE . '/components/com_sportsmanagement/src/Model/SportsManagementListModel.php',
+        JPATH_SITE . '/components/com_sportsmanagement/src/Model/AllteamsModel.php',
+    ];
+
+    foreach ($nativeModels as $nativeModel) {
+        if (is_file($nativeModel)) {
+            require_once $nativeModel;
+        }
+    }
+}
+
+if (!class_exists(AllteamsModel::class)) {
+    throw new \RuntimeException('SportsManagement native Allteams model could not be loaded.', 500);
 }
 
 if (!class_exists('sportsmanagementModelallteams', false)) {
