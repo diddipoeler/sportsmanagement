f755816356a4c60d50a0f542a0b8acaa05114d9d
diddipoeler <diddipoeler@gmx.de>
2026-09-09 12:06:56 +0200

    Harden Joomla 5/6 Allplaygrounds model bridge

--- site/models/allplaygrounds.php
+++ site/models/allplaygrounds.php
@@ -14,8 +14,20 @@
 use Diddipoeler\Component\SportsManagement\Site\Model\AllplaygroundsModel;
 
 if (!class_exists(AllplaygroundsModel::class)) {
-    require_once JPATH_SITE . '/components/com_sportsmanagement/src/Model/SportsManagementListModel.php';
-    require_once JPATH_SITE . '/components/com_sportsmanagement/src/Model/AllplaygroundsModel.php';
+    $nativeModels = [
+        JPATH_SITE . '/components/com_sportsmanagement/src/Model/SportsManagementListModel.php',
+        JPATH_SITE . '/components/com_sportsmanagement/src/Model/AllplaygroundsModel.php',
+    ];
+
+    foreach ($nativeModels as $nativeModel) {
+        if (is_file($nativeModel)) {
+            require_once $nativeModel;
+        }
+    }
+}
+
+if (!class_exists(AllplaygroundsModel::class)) {
+    throw new \RuntimeException('SportsManagement native Allplaygrounds model could not be loaded.', 500);
 }
 
 if (!class_exists('sportsmanagementModelallplaygrounds', false)) {
