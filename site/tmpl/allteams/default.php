ca9528e16b99d64a9cd0b99bf136d4a24ed26c91
diddipoeler <diddipoeler@gmx.de>
2026-09-09 21:28:55 +0200

    Use keepalive web asset in all teams layout

--- site/tmpl/allteams/default.php
+++ site/tmpl/allteams/default.php
@@ -4,11 +4,11 @@
  */
 defined('_JEXEC') or die('Restricted access');
 
-use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
 
-HTMLHelper::_('behavior.keepalive');
-$this->getDocument()->getWebAssetManager()->useScript('jquery');
+$this->getDocument()->getWebAssetManager()
+    ->useScript('keepalive')
+    ->useScript('jquery');
 
 $startRange = (int) $this->params->get('character_filter_start_hex', 0);
 $endRange = (int) $this->params->get('character_filter_end_hex', 0);
