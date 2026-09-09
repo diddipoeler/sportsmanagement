b19d94d067493ccda75990f52ceeb052f60e927d
diddipoeler <diddipoeler@gmx.de>
2026-09-09 11:59:12 +0200

    Harden Joomla 5/6 Cpanel model bridge

--- admin/models/cpanel.php
+++ admin/models/cpanel.php
@@ -12,8 +12,20 @@ defined('_JEXEC') or die('Restricted access');
 use Diddipoeler\Component\SportsManagement\Administrator\Model\CpanelModel;
 
 if (!class_exists(CpanelModel::class)) {
-    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Model/SportsManagementListModel.php';
-    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Model/CpanelModel.php';
+    $nativeModels = [
+        JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Model/SportsManagementListModel.php',
+        JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Model/CpanelModel.php',
+    ];
+
+    foreach ($nativeModels as $nativeModel) {
+        if (is_file($nativeModel)) {
+            require_once $nativeModel;
+        }
+    }
+}
+
+if (!class_exists(CpanelModel::class)) {
+    throw new \RuntimeException('SportsManagement native Cpanel model could not be loaded.', 500);
 }
 
 if (!class_exists('sportsmanagementModelcpanel', false)) {
