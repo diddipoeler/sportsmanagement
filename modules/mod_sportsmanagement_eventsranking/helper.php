15fe78fa27763451b1658d0b3fb4d1a0ae117f0e
diddipoeler <diddipoeler@gmx.de>
2026-09-09 12:08:37 +0200

    Harden Joomla 5/6 Events Ranking dependencies

--- modules/mod_sportsmanagement_eventsranking/helper.php
+++ modules/mod_sportsmanagement_eventsranking/helper.php
@@ -9,12 +9,25 @@
  */
 \defined('_JEXEC') or die;
 
+use Diddipoeler\Component\SportsManagement\Site\Helper\SiteRouteHelper;
+use Diddipoeler\Component\SportsManagement\Site\Service\SportsManagementDatabaseResolver;
 use Diddipoeler\Module\SportsManagementEventsRanking\Site\Helper\EventsRankingHelper;
 use Joomla\CMS\Application\SiteApplication;
 use Joomla\CMS\Factory;
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
 use Joomla\Database\DatabaseInterface;
+
+$nativeDependencies = [
+    SiteRouteHelper::class => JPATH_SITE . '/components/com_sportsmanagement/src/Helper/SiteRouteHelper.php',
+    SportsManagementDatabaseResolver::class => JPATH_SITE . '/components/com_sportsmanagement/src/Service/SportsManagementDatabaseResolver.php',
+];
+
+foreach ($nativeDependencies as $class => $file) {
+    if (!class_exists($class, false) && is_file($file)) {
+        require_once $file;
+    }
+}
 
 if (!class_exists(EventsRankingHelper::class)) {
     $nativeHelper = __DIR__ . '/src/Helper/EventsRankingHelper.php';
