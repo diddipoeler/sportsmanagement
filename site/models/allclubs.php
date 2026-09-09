fa8f98301242aa2e53cb21c12f00fb81c4851fc1
diddipoeler <diddipoeler@gmx.de>
2026-09-09 10:39:08 +0200

    Harden Joomla 5/6 allclubs model bridge

--- site/models/allclubs.php
+++ site/models/allclubs.php
@@ -8,13 +8,17 @@
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 
-defined('_JEXEC') or die('Restricted access');
+defined('_JEXEC') or die;
 
 use Diddipoeler\Component\SportsManagement\Site\Model\AllclubsModel;
 
 if (!class_exists(AllclubsModel::class)) {
     require_once JPATH_SITE . '/components/com_sportsmanagement/src/Model/SportsManagementListModel.php';
     require_once JPATH_SITE . '/components/com_sportsmanagement/src/Model/AllclubsModel.php';
+}
+
+if (!class_exists(AllclubsModel::class)) {
+    throw new \RuntimeException('SportsManagement native Allclubs model could not be loaded.', 500);
 }
 
 if (!class_exists('sportsmanagementModelallclubs', false)) {
