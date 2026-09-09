eb7ae5d76cb1fb54233f2f692204ef9d887a9466
diddipoeler <diddipoeler@gmx.de>
2026-09-09 18:46:09 +0200

    Use Joomla DI in prediction match field

--- admin/src/Field/PredictionmatchidField.php
+++ admin/src/Field/PredictionmatchidField.php
@@ -11,6 +11,7 @@ namespace Diddipoeler\Component\SportsManagement\Administrator\Field;
 
 \defined('_JEXEC') or die;
 
+use Joomla\CMS\Application\AdministratorApplication;
 use Joomla\CMS\Factory;
 
 /** Matches belonging to projects assigned to the active prediction game. */
@@ -29,7 +30,9 @@ final class PredictionmatchidField extends SportsManagementListField
 
     protected function getOptions(): array
     {
-        $predictionId = (int) Factory::getApplication()->getUserState('com_sportsmanagement.prediction_id', 0);
+        /** @var AdministratorApplication $app */
+        $app = Factory::getContainer()->get(AdministratorApplication::class);
+        $predictionId = (int) $app->getUserState('com_sportsmanagement.prediction_id', 0);
         $options = [];
 
         if ($predictionId > 0) {
