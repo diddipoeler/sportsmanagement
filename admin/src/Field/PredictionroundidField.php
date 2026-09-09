884a2da310b902ca66273b2f9671b49a68f3fd22
diddipoeler <diddipoeler@gmx.de>
2026-09-09 18:46:21 +0200

    Use Joomla DI in prediction round field

--- admin/src/Field/PredictionroundidField.php
+++ admin/src/Field/PredictionroundidField.php
@@ -11,6 +11,7 @@ namespace Diddipoeler\Component\SportsManagement\Administrator\Field;
 
 \defined('_JEXEC') or die;
 
+use Joomla\CMS\Application\AdministratorApplication;
 use Joomla\CMS\Factory;
 
 /** Rounds belonging to projects assigned to the active prediction game. */
@@ -43,7 +44,9 @@ final class PredictionroundidField extends SportsManagementListField
             return $this->roundOptions;
         }
 
-        $predictionId = (int) Factory::getApplication()->getUserState('com_sportsmanagement.prediction_id', 0);
+        /** @var AdministratorApplication $app */
+        $app = Factory::getContainer()->get(AdministratorApplication::class);
+        $predictionId = (int) $app->getUserState('com_sportsmanagement.prediction_id', 0);
         $options = [];
 
         if ($predictionId > 0) {
