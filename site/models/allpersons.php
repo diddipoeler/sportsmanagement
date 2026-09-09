e71213ea6bc823c1fc9c6736d1ffb5f09f804864
diddipoeler <diddipoeler@gmx.de>
2026-09-09 12:01:11 +0200

    Harden Joomla 5/6 Allpersons model bridge

--- site/models/allpersons.php
+++ site/models/allpersons.php
@@ -14,8 +14,20 @@
 use Diddipoeler\Component\SportsManagement\Site\Model\AllpersonsModel;
 
 if (!class_exists(AllpersonsModel::class)) {
-    require_once JPATH_SITE . '/components/com_sportsmanagement/src/Model/SportsManagementListModel.php';
-    require_once JPATH_SITE . '/components/com_sportsmanagement/src/Model/AllpersonsModel.php';
+    $nativeModels = [
+        JPATH_SITE . '/components/com_sportsmanagement/src/Model/SportsManagementListModel.php',
+        JPATH_SITE . '/components/com_sportsmanagement/src/Model/AllpersonsModel.php',
+    ];
+
+    foreach ($nativeModels as $nativeModel) {
+        if (is_file($nativeModel)) {
+            require_once $nativeModel;
+        }
+    }
+}
+
+if (!class_exists(AllpersonsModel::class)) {
+    throw new \RuntimeException('SportsManagement native Allpersons model could not be loaded.', 500);
 }
 
 if (!class_exists('sportsmanagementModelallpersons', false)) {
