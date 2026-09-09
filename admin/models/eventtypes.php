8fd25adbb8a61e799d21e22fbf1a5c6879aad1ff
diddipoeler <diddipoeler@gmx.de>
2026-09-09 12:06:06 +0200

    Harden Joomla 5/6 Eventtypes model bridge

--- admin/models/eventtypes.php
+++ admin/models/eventtypes.php
@@ -12,8 +12,20 @@ defined('_JEXEC') or die('Restricted access');
 use Diddipoeler\Component\SportsManagement\Administrator\Model\EventtypesModel;
 
 if (!class_exists(EventtypesModel::class)) {
-    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Model/SportsManagementListModel.php';
-    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Model/EventtypesModel.php';
+    $nativeModels = [
+        JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Model/SportsManagementListModel.php',
+        JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Model/EventtypesModel.php',
+    ];
+
+    foreach ($nativeModels as $nativeModel) {
+        if (is_file($nativeModel)) {
+            require_once $nativeModel;
+        }
+    }
+}
+
+if (!class_exists(EventtypesModel::class)) {
+    throw new \RuntimeException('SportsManagement native Eventtypes model could not be loaded.', 500);
 }
 
 if (!class_exists('sportsmanagementModelEventtypes', false)) {
