066ffff3859eadcdb87805b41137babd29b5185f
diddipoeler <diddipoeler@gmx.de>
2026-09-09 11:02:00 +0200

    Harden Joomla 5/6 calendar import table bridge

--- admin/tables/jsmgcalendarimport.php
+++ admin/tables/jsmgcalendarimport.php
@@ -1,14 +1,24 @@
 <?php
 /**
- * SportsManagement legacy table compatibility bridge.
+ * Legacy compatibility bridge for the native Joomla 5/6 JSM Google Calendar table.
  *
- * @package   SportsManagement
- * @license   GNU General Public License version 2 or later; see LICENSE
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
-
-defined('_JEXEC') or die;
+\defined('_JEXEC') or die;
 
 use Diddipoeler\Component\SportsManagement\Administrator\Table\JsmgcalendarTable;
+
+if (!class_exists(JsmgcalendarTable::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Table/SportsManagementTable.php';
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Table/JsmgcalendarTable.php';
+}
+
+if (!class_exists(JsmgcalendarTable::class)) {
+    throw new \RuntimeException('SportsManagement native JSM Google Calendar table could not be loaded.', 500);
+}
 
 if (!class_exists('GCalendarTableImport', false)) {
     class_alias(JsmgcalendarTable::class, 'GCalendarTableImport');
