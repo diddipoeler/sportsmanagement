2301ebda9138dde9668be488a9943cb0f9e947bd
diddipoeler <diddipoeler@gmx.de>
2026-09-09 10:48:06 +0200

    Use native Joomla 5/6 confidential table bridge

--- admin/tables/confidential.php
+++ admin/tables/confidential.php
@@ -1,38 +1,25 @@
 <?php
 /**
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage tables
- * @file       confidential.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * Legacy compatibility bridge for the native Joomla 5/6 confidential table.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
-defined('_JEXEC') or die('Restricted access');
+\defined('_JEXEC') or die;
 
-/**
- * sportsmanagementTableconfidential
- *
- * @package
- * @author
- * @copyright diddi
- * @version   2014
- * @access    public
- */
-class sportsmanagementTableconfidential extends JSMTable
-{
-	/**
-	 * Constructor
-	 *
-	 * @param   object Database connector object
-	 *
-	 * @since 1.0
-	 */
-	function __construct(&$db)
-	{
-		$db = sportsmanagementHelper::getDBConnection();
-		parent::__construct('#__sportsmanagement_confidential', 'id', $db);
-	}
+use Diddipoeler\Component\SportsManagement\Administrator\Table\ConfidentialTable;
 
+if (!class_exists(ConfidentialTable::class)) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Table/SportsManagementTable.php';
+    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Table/ConfidentialTable.php';
+}
+
+if (!class_exists(ConfidentialTable::class)) {
+    throw new \RuntimeException('SportsManagement native Confidential table could not be loaded.', 500);
+}
+
+if (!class_exists('sportsmanagementTableconfidential', false)) {
+    class_alias(ConfidentialTable::class, 'sportsmanagementTableconfidential');
 }
