640770ba07778bd3f5838424008f115d4bfc13d9
diddipoeler <diddipoeler@gmx.de>
2026-09-09 11:58:56 +0200

    Harden Joomla 5/6 Allprojects model bridge

--- site/models/allprojects.php
+++ site/models/allprojects.php
@@ -12,8 +12,20 @@ defined('_JEXEC') or die;
 use Diddipoeler\Component\SportsManagement\Site\Model\AllprojectsModel;
 
 if (!class_exists(AllprojectsModel::class)) {
-    require_once JPATH_SITE . '/components/com_sportsmanagement/src/Model/SportsManagementListModel.php';
-    require_once JPATH_SITE . '/components/com_sportsmanagement/src/Model/AllprojectsModel.php';
+    $nativeModels = [
+        JPATH_SITE . '/components/com_sportsmanagement/src/Model/SportsManagementListModel.php',
+        JPATH_SITE . '/components/com_sportsmanagement/src/Model/AllprojectsModel.php',
+    ];
+
+    foreach ($nativeModels as $nativeModel) {
+        if (is_file($nativeModel)) {
+            require_once $nativeModel;
+        }
+    }
+}
+
+if (!class_exists(AllprojectsModel::class)) {
+    throw new \RuntimeException('SportsManagement native Allprojects model could not be loaded.', 500);
 }
 
 if (!class_exists('sportsmanagementModelallprojects', false)) {
