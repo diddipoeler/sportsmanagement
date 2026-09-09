2e57d9d44e62778c8bb37ac61af9fbae24fe9216
diddipoeler <diddipoeler@gmx.de>
2026-09-09 13:46:03 +0200

    Harden Joomla 5/6 Clubplan model bridge

--- site/models/clubplan.php
+++ site/models/clubplan.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Legacy compatibility bridge for the native Clubplan model.
+ * Legacy compatibility bridge for the native Joomla 5/6 Clubplan model.
  *
- * @version    4.24.00
+ * @version    5.6.0
  * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
  * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
@@ -12,9 +12,21 @@ defined('_JEXEC') or die;
 use Diddipoeler\Component\SportsManagement\Site\Model\ClubplanModel;
 
 if (!class_exists(ClubplanModel::class)) {
-    require_once JPATH_SITE . '/components/com_sportsmanagement/src/Model/SportsManagementModel.php';
-    require_once JPATH_SITE . '/components/com_sportsmanagement/src/Model/SportsManagementProjectModel.php';
-    require_once JPATH_SITE . '/components/com_sportsmanagement/src/Model/ClubplanModel.php';
+    $nativeModels = [
+        JPATH_SITE . '/components/com_sportsmanagement/src/Model/SportsManagementModel.php',
+        JPATH_SITE . '/components/com_sportsmanagement/src/Model/SportsManagementProjectModel.php',
+        JPATH_SITE . '/components/com_sportsmanagement/src/Model/ClubplanModel.php',
+    ];
+
+    foreach ($nativeModels as $nativeModel) {
+        if (is_file($nativeModel)) {
+            require_once $nativeModel;
+        }
+    }
+}
+
+if (!class_exists(ClubplanModel::class)) {
+    throw new \RuntimeException('SportsManagement native Clubplan model could not be loaded.', 500);
 }
 
 if (!class_exists('sportsmanagementModelClubPlan', false)) {
