00675af672e8590933b68903d45c53f33026d418
diddipoeler <diddipoeler@gmx.de>
2026-09-09 12:00:20 +0200

    Harden Joomla 5/6 Currentseasons model bridge

--- admin/models/currentseasons.php
+++ admin/models/currentseasons.php
@@ -12,8 +12,20 @@ defined('_JEXEC') or die('Restricted access');
 use Diddipoeler\Component\SportsManagement\Administrator\Model\CurrentseasonsModel;
 
 if (!class_exists(CurrentseasonsModel::class)) {
-    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Model/SportsManagementListModel.php';
-    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Model/CurrentseasonsModel.php';
+    $nativeModels = [
+        JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Model/SportsManagementListModel.php',
+        JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Model/CurrentseasonsModel.php',
+    ];
+
+    foreach ($nativeModels as $nativeModel) {
+        if (is_file($nativeModel)) {
+            require_once $nativeModel;
+        }
+    }
+}
+
+if (!class_exists(CurrentseasonsModel::class)) {
+    throw new \RuntimeException('SportsManagement native Currentseasons model could not be loaded.', 500);
 }
 
 if (!class_exists('sportsmanagementModelcurrentseasons', false)) {
