f9f949173914216fa05aee5f1b51b7eff895aa06
diddipoeler <diddipoeler@gmx.de>
2026-09-10 09:35:48 +0200

    Joomla 5/6: remove legacy match event tab branches

--- admin/views/match/tmpl/editeventsbb.php
+++ admin/views/match/tmpl/editeventsbb.php
@@ -1,66 +1,39 @@
 <?php
 /**
- * SportsManagement ein Programm zur Verwaltung für Sportarten
- * @version    1.0.05
+ * SportsManagement match event tabs for Joomla 5/6.
+ *
+ * @version    5.6.0
  * @package    Sportsmanagement
  * @subpackage match
  * @file       editeventsbb.php
  * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @copyright  Copyright: © 2013-2026 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 defined('_JEXEC') or die('Restricted access');
+
 use Joomla\CMS\HTML\HTMLHelper;
-use Joomla\CMS\Language\Text;
 
-$params = $this->form->getFieldsets('params');
-
-/** welche joomla version ? */
-if (version_compare(substr(JVERSION, 0, 3), '4.0', 'ge'))
-{
-
-}
-elseif (version_compare(substr(JVERSION, 0, 3), '3.0', 'ge'))
-{
-	HTMLHelper::_('behavior.modal');
-}
+$escape   = static fn ($value): string => htmlspecialchars((string) $value, ENT_QUOTES, 'UTF-8');
+$homeTeam = $escape($this->teams->team1 ?? '');
+$awayTeam = $escape($this->teams->team2 ?? '');
 ?>
 <div id="gamesevents">
-    <form method="post" id="adminForm" class="form-validate">
-		<?php
-		// Welche joomla version
-		if (version_compare(JVERSION, '3.0.0', 'ge'))
-		{
-			// Define tabs options for version of Joomla! 3.1
-			$tabsOptionsJ31 = array(
-				"active" => "panel1" // It is the ID of the active tab.
-			);
+    <form method="post" id="adminForm" name="adminForm" class="form-validate">
+        <?php echo HTMLHelper::_('bootstrap.startTabSet', 'match-events-tabs', ['active' => 'panel1']); ?>
+        <?php echo HTMLHelper::_('bootstrap.addTab', 'match-events-tabs', 'panel1', $homeTeam); ?>
+        <?php echo $this->loadTemplate('home'); ?>
+        <?php echo HTMLHelper::_('bootstrap.endTab'); ?>
+        <?php echo HTMLHelper::_('bootstrap.addTab', 'match-events-tabs', 'panel2', $awayTeam); ?>
+        <?php echo $this->loadTemplate('away'); ?>
+        <?php echo HTMLHelper::_('bootstrap.endTab'); ?>
+        <?php echo HTMLHelper::_('bootstrap.endTabSet'); ?>
 
-			echo HTMLHelper::_('bootstrap.startTabSet', 'ID-Tabs-J31-Group', $tabsOptionsJ31);
-			echo HTMLHelper::_('bootstrap.addTab', 'ID-Tabs-J31-Group', 'panel1', Text::_($this->teams->team1));
-			echo $this->loadTemplate('home');
-			echo HTMLHelper::_('bootstrap.endTab');
-			echo HTMLHelper::_('bootstrap.addTab', 'ID-Tabs-J31-Group', 'panel2', Text::_($this->teams->team2));
-			echo $this->loadTemplate('away');
-			echo HTMLHelper::_('bootstrap.endTab');
-			echo HTMLHelper::_('bootstrap.endTabSet');
-		}
-
-		else
-		{
-			echo HTMLHelper::_('tabs.start', 'tabs', array('useCookie' => 1));
-			echo HTMLHelper::_('tabs.panel', Text::_($this->teams->team1), 'panel1');
-			echo $this->loadTemplate('home');
-			echo HTMLHelper::_('tabs.panel', Text::_($this->teams->team2), 'panel2');
-			echo $this->loadTemplate('away');
-			echo HTMLHelper::_('tabs.end');
-		}
-		?>
-        <input type="hidden" name="task" value=""/>
-        <input type="hidden" name="view" value="match"/>
-        <input type="hidden" name="option" value="" id=""/>
-        <input type="hidden" name="boxchecked" value="0"/>
-		<?php echo HTMLHelper::_('form.token'); ?>
+        <input type="hidden" name="task" value="">
+        <input type="hidden" name="view" value="match">
+        <input type="hidden" name="option" value="com_sportsmanagement">
+        <input type="hidden" name="boxchecked" value="0">
+        <?php echo HTMLHelper::_('form.token'); ?>
     </form>
 </div>
 <div style="clear: both"></div>
