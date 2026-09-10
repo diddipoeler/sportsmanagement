d6b7f287783327544c2c22f53cc7e738e407d118
diddipoeler <diddipoeler@gmx.de>
2026-09-10 09:35:38 +0200

    Joomla 5/6: modernize all leagues filters

--- site/tmpl/allleagues/default.php
+++ site/tmpl/allleagues/default.php
@@ -1,62 +1,88 @@
 <?php
 /**
  * SportsManagement all leagues template for Joomla 5/6.
+ *
+ * @version 5.6.0
  */
 defined('_JEXEC') or die('Restricted access');
 
 use Joomla\CMS\Language\Text;
 
-$this->getDocument()->getWebAssetManager()
-    ->useScript('keepalive')
-    ->useScript('jquery');
+$this->getDocument()->getWebAssetManager()->useScript('keepalive');
 
 $startRange = (int) $this->params->get('character_filter_start_hex', 0);
-$endRange = (int) $this->params->get('character_filter_end_hex', 0);
+$endRange   = (int) $this->params->get('character_filter_end_hex', 0);
 ?>
 <script>
-    function tableOrdering(order, dir) {
-        const form = document.getElementById('adminForm');
-        form.filter_order.value = order;
-        form.filter_order_Dir.value = dir;
-        form.submit();
+function tableOrdering(order, dir) {
+    const form = document.getElementById('adminForm');
+
+    if (!form) {
+        return;
     }
 
-    function searchPerson(value) {
-        const search = document.getElementById('filter_search');
-        search.value = value;
-        document.getElementById('adminForm').submit();
+    form.filter_order.value = order;
+    form.filter_order_Dir.value = dir;
+    form.submit();
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.getElementById('adminForm');
+    const search = document.getElementById('filter_search');
+
+    if (!form || !search) {
+        return;
     }
 
-    function clearLeagueSearch() {
-        document.getElementById('filter_search').value = '';
-        document.getElementById('adminForm').submit();
+    form.querySelectorAll('[data-character-filter]').forEach(function (button) {
+        button.addEventListener('click', function () {
+            search.value = button.dataset.characterFilter || '';
+            form.submit();
+        });
+    });
+
+    const clearButton = form.querySelector('[data-clear-league-search]');
+
+    if (clearButton) {
+        clearButton.addEventListener('click', function () {
+            search.value = '';
+            form.submit();
+        });
     }
+});
 </script>
 
 <div class="<?php echo $this->escape($this->divclasscontainer); ?>" id="allleagues">
-    <form name="adminForm" id="adminForm" action="<?php echo htmlspecialchars($this->uri->toString(), ENT_QUOTES, 'UTF-8'); ?>" method="post">
+    <form name="adminForm" id="adminForm"
+          action="<?php echo htmlspecialchars($this->uri->toString(), ENT_QUOTES, 'UTF-8'); ?>"
+          method="post">
         <fieldset class="filters">
             <legend class="hidelabeltxt"><?php echo Text::_('JGLOBAL_FILTER_LABEL'); ?></legend>
             <div class="filter-search">
                 <input type="text" name="filter_search" id="filter_search"
                        value="<?php echo $this->escape($this->filter); ?>" class="inputbox"
-                       onchange="this.form.submit();" />
-                <button type="submit" class="btn">
-                    <i class="icon-search" aria-hidden="true"></i><?php echo Text::_('JGLOBAL_FILTER_BUTTON'); ?>
+                       onchange="this.form.submit();">
+                <button type="submit" class="btn" title="<?php echo $this->escape(Text::_('JGLOBAL_FILTER_BUTTON')); ?>">
+                    <span class="icon-search" aria-hidden="true"></span><?php echo Text::_('JGLOBAL_FILTER_BUTTON'); ?>
                 </button>
-                <button type="button" class="btn" onclick="clearLeagueSearch();">
-                    <i class="icon-remove" aria-hidden="true"></i><?php echo Text::_('JSEARCH_FILTER_CLEAR'); ?>
+                <button type="button" class="btn" data-clear-league-search>
+                    <span class="icon-remove" aria-hidden="true"></span><?php echo Text::_('JSEARCH_FILTER_CLEAR'); ?>
                 </button>
 
                 <?php echo $this->lists['nation2'] . '&nbsp;&nbsp;'; ?>
                 <?php for ($i = $startRange; $i <= $endRange; $i++) : ?>
-                    <a href="javascript:searchPerson('<?php echo '&#' . $i . ';'; ?>')"><?php echo '&#' . $i . ';'; ?></a>&nbsp;&nbsp;&nbsp;&nbsp;
+                    <?php $character = html_entity_decode('&#' . $i . ';', ENT_QUOTES | ENT_HTML5, 'UTF-8'); ?>
+                    <button
+                        type="button"
+                        class="btn btn-sm btn-link p-0 me-2"
+                        data-character-filter="<?php echo $this->escape($character); ?>"
+                    ><?php echo $this->escape($character); ?></button>
                 <?php endfor; ?>
             </div>
 
-            <input type="hidden" name="filter_order" value="<?php echo $this->escape($this->sortColumn); ?>" />
-            <input type="hidden" name="filter_order_Dir" value="<?php echo $this->escape($this->sortDirection); ?>" />
-            <input type="hidden" name="limitstart" value="" />
+            <input type="hidden" name="filter_order" value="<?php echo $this->escape($this->sortColumn); ?>">
+            <input type="hidden" name="filter_order_Dir" value="<?php echo $this->escape($this->sortDirection); ?>">
+            <input type="hidden" name="limitstart" value="">
 
             <div class="display-limit">
                 <?php echo Text::_('JGLOBAL_DISPLAY_NUM'); ?>&#160;
