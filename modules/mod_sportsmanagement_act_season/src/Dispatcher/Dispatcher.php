6b9d166e1a35a861260726d24a3a7291c5dffee2
diddipoeler <diddipoeler@gmx.de>
2026-09-10 16:51:10 +0200

    Remove SiteApplication dependency from Act Season dispatcher

--- modules/mod_sportsmanagement_act_season/src/Dispatcher/Dispatcher.php
+++ modules/mod_sportsmanagement_act_season/src/Dispatcher/Dispatcher.php
@@ -11,7 +11,6 @@ namespace Diddipoeler\Module\SportsManagementActSeason\Site\Dispatcher;
 
 \defined('_JEXEC') or die;
 
-use Joomla\CMS\Application\SiteApplication;
 use Joomla\CMS\Component\ComponentHelper;
 use Joomla\CMS\Dispatcher\AbstractModuleDispatcher;
 use Joomla\CMS\Factory;
@@ -33,7 +32,7 @@ final class Dispatcher extends AbstractModuleDispatcher implements HelperFactory
 
         $app = $this->getApplication();
 
-        if (!$app instanceof SiteApplication) {
+        if (!$app->isClient('site')) {
             throw new \RuntimeException('SportsManagement Act Season requires the Joomla site application.', 500);
         }
 
