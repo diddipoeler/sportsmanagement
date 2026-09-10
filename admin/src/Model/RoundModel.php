0090b8b140442c2b448795110bc135b08c64b597
diddipoeler <diddipoeler@gmx.de>
2026-09-10 17:08:17 +0200

    Remove AdministratorApplication dependency from Round model

--- admin/src/Model/RoundModel.php
+++ admin/src/Model/RoundModel.php
@@ -14,7 +14,7 @@ namespace Diddipoeler\Component\SportsManagement\Administrator\Model;
 use Diddipoeler\Component\SportsManagement\Administrator\Helper\SportsManagementDatabaseResolver;
 use Diddipoeler\Component\SportsManagement\Administrator\Helper\SportsManagementDateHelper;
 use Diddipoeler\Component\SportsManagement\Administrator\Table\RoundTable;
-use Joomla\CMS\Application\AdministratorApplication;
+use Joomla\CMS\Application\CMSApplication;
 use Joomla\CMS\Factory;
 use Joomla\CMS\Filter\OutputFilter;
 use Joomla\CMS\Form\Form;
@@ -358,11 +358,11 @@ final class RoundModel extends SportsManagementAdminModel
         );
     }
 
-    private static function backendApplication(): AdministratorApplication
+    private static function backendApplication(): CMSApplication
     {
         $app = Factory::getApplication();
 
-        if (!$app instanceof AdministratorApplication) {
+        if (!$app instanceof CMSApplication || !$app->isClient('administrator')) {
             throw new \RuntimeException('SportsManagement administrator application is unavailable.');
         }
 
