f37849fee36abcee9fe1905386f4a806778c163a
diddipoeler <diddipoeler@gmx.de>
2026-09-10 18:14:32 +0200

    Bridge SIS import admin view to native Joomla view

--- admin/views/jlextsisimport/view.html.php
+++ admin/views/jlextsisimport/view.html.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * SportsManagement SIS import administrator view.
+ * Legacy compatibility bridge for the native Joomla 5/6 SIS import administrator view.
  *
  * @version    5.6.0
  * @author     diddipoeler
@@ -9,41 +9,20 @@
  */
 \defined('_JEXEC') or die('Restricted access');
 
-class sportsmanagementViewjlextsisimport extends sportsmanagementView
-{
-    public function init(): void
-    {
-        if (in_array($this->getLayout(), ['default', 'default_3', 'default_4'], true)) {
-            $this->_displayDefault();
-            return;
-        }
+use Diddipoeler\Component\SportsManagement\Administrator\View\Jlextsisimport\HtmlView;
 
-        $this->revisionDate = '2011-04-28 - 12:00';
+if (!class_exists(HtmlView::class)) {
+    $nativeView = JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/View/Jlextsisimport/HtmlView.php';
+
+    if (is_file($nativeView)) {
+        require_once $nativeView;
     }
+}
 
-    public function _displayDefault(): void
-    {
-        $input = $this->app->getInput();
-        $option = $input->getCmd('option', 'com_sportsmanagement');
+if (!class_exists(HtmlView::class)) {
+    throw new \RuntimeException('SportsManagement native SIS import administrator view could not be loaded.', 500);
+}
 
-        $this->project = $this->app->getUserState($option . 'project');
-        $this->revisionDate = '2011-04-28 - 12:00';
-        $this->import_version = 'NEW';
-    }
-
-    public function _displayDefaultUpdate(): void
-    {
-        $input = $this->app->getInput();
-        $option = $input->getCmd('option', 'com_sportsmanagement');
-        $model = $this->getModel();
-
-        $this->project = $this->app->getUserState($option . 'project');
-        $this->uploadArray = $this->app->getUserState($option . 'uploadArray', []);
-        $this->importData = $model->getUpdateData();
-    }
-
-    protected function addToolbar(): void
-    {
-        parent::addToolbar();
-    }
+if (!class_exists('sportsmanagementViewjlextsisimport', false)) {
+    class_alias(HtmlView::class, 'sportsmanagementViewjlextsisimport');
 }
