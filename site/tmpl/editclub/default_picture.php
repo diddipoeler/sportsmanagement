0a38af99b1c32015556562e8c966a6080ce7078e
diddipoeler <diddipoeler@gmx.de>
2026-09-10 10:58:39 +0200

    Migrate frontend club picture layout for Joomla 5/6

--- site/tmpl/editclub/default_picture.php
+++ site/tmpl/editclub/default_picture.php
@@ -1,31 +1,29 @@
 <?php
 /**
  * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- * @version    1.0.05
+ * @version    5.6.0
  * @package    Sportsmanagement
  * @subpackage editclub
  * @file       default_picture.php
  * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @copyright  Copyright: © 2013-2026 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 defined('_JEXEC') or die('Restricted access');
+
 use Joomla\CMS\Language\Text;
-
 ?>
-<fieldset class="adminform">
-    <legend><?php echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_CLUB_LOGO'); ?>
-    </legend>
-    <table class="admintable" border='0'>
-		<?php foreach ($this->form->getFieldset('picture') as $field)
-			:
-			?>
-            <tr>
-                <td class="key"><?php echo $field->label; ?></td>
-                <td><?php echo $field->input; ?></td>
-            </tr>
-		<?php endforeach; ?>
-    </table>
-</fieldset>
+<fieldset class="mb-4">
+    <legend class="h5"><?php echo Text::_('COM_SPORTSMANAGEMENT_ADMIN_CLUB_LOGO'); ?></legend>
 
-	  
+    <?php foreach ($this->form->getFieldset('picture') as $field) : ?>
+        <?php if (strtolower((string) $field->type) === 'hidden') : ?>
+            <?php echo $field->input; ?>
+            <?php continue; ?>
+        <?php endif; ?>
+        <div class="mb-3">
+            <div class="form-label"><?php echo $field->label; ?></div>
+            <?php echo $field->input; ?>
+        </div>
+    <?php endforeach; ?>
+</fieldset>
