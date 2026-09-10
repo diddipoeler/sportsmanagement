c60d45fc9aff51b4a5a7de36462d3c9e74e47725
diddipoeler <diddipoeler@gmx.de>
2026-09-10 13:13:23 +0200

    Remove SiteApplication dependency from first league dispatcher

--- modules/mod_sportsmanagement_firstleagueoverview/src/Dispatcher/Dispatcher.php
+++ modules/mod_sportsmanagement_firstleagueoverview/src/Dispatcher/Dispatcher.php
@@ -11,7 +11,6 @@ namespace Diddipoeler\Module\SportsManagementFirstLeagueOverview\Site\Dispatcher
 
 \defined('_JEXEC') or die;
 
-use Joomla\CMS\Application\SiteApplication;
 use Joomla\CMS\Dispatcher\AbstractModuleDispatcher;
 use Joomla\CMS\Factory;
 use Joomla\CMS\Helper\HelperFactoryAwareInterface;
@@ -32,7 +31,7 @@ final class Dispatcher extends AbstractModuleDispatcher implements HelperFactory
 
         $app = $this->getApplication();
 
-        if (!$app instanceof SiteApplication) {
+        if (!$app->isClient('site')) {
             throw new \RuntimeException('SportsManagement First League Overview requires the Joomla site application.', 500);
         }
 
