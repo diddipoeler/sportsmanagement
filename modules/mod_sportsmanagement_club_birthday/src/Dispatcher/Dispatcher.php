d41811086a194acdf9f51f4ae8a2fdcb62b456b7
diddipoeler <diddipoeler@gmx.de>
2026-09-10 16:51:58 +0200

    Remove SiteApplication dependency from Club Birthday dispatcher

--- modules/mod_sportsmanagement_club_birthday/src/Dispatcher/Dispatcher.php
+++ modules/mod_sportsmanagement_club_birthday/src/Dispatcher/Dispatcher.php
@@ -9,7 +9,6 @@ namespace Diddipoeler\Module\SportsManagementClubBirthday\Site\Dispatcher;
 
 \defined('_JEXEC') or die;
 
-use Joomla\CMS\Application\SiteApplication;
 use Joomla\CMS\Dispatcher\AbstractModuleDispatcher;
 use Joomla\CMS\Factory;
 use Joomla\CMS\Helper\HelperFactoryAwareInterface;
@@ -30,7 +29,7 @@ final class Dispatcher extends AbstractModuleDispatcher implements HelperFactory
 
         $app = $this->getApplication();
 
-        if (!$app instanceof SiteApplication) {
+        if (!$app->isClient('site')) {
             throw new \RuntimeException('SportsManagement Club Birthday requires the Joomla site application.', 500);
         }
 
