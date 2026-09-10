0d6785cc56a7d572a1cdd1136ef645e950fc1a23
diddipoeler <diddipoeler@gmx.de>
2026-09-10 10:58:30 +0200

    Migrate frontend club details layout for Joomla 5/6

--- site/tmpl/editclub/default_details.php
+++ site/tmpl/editclub/default_details.php
@@ -4,14 +4,17 @@ defined('_JEXEC') or die('Restricted access');
 
 use Joomla\CMS\Language\Text;
 ?>
-<fieldset class="adminform">
-    <legend><?php echo Text::_('COM_SPORTSMANAGEMENT_TABS_DETAILS'); ?></legend>
-    <table class="admintable">
-        <?php foreach ($this->form->getFieldset('details') as $field) : ?>
-            <tr>
-                <td class="key"><?php echo $field->label; ?></td>
-                <td><?php echo $field->input; ?></td>
-            </tr>
-        <?php endforeach; ?>
-    </table>
+<fieldset class="mb-4">
+    <legend class="h5"><?php echo Text::_('COM_SPORTSMANAGEMENT_TABS_DETAILS'); ?></legend>
+
+    <?php foreach ($this->form->getFieldset('details') as $field) : ?>
+        <?php if (strtolower((string) $field->type) === 'hidden') : ?>
+            <?php echo $field->input; ?>
+            <?php continue; ?>
+        <?php endif; ?>
+        <div class="mb-3">
+            <div class="form-label"><?php echo $field->label; ?></div>
+            <?php echo $field->input; ?>
+        </div>
+    <?php endforeach; ?>
 </fieldset>
