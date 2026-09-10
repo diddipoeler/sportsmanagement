12252d23d58be786ac4c73d997948d1aca3fe8cc
diddipoeler <diddipoeler@gmx.de>
2026-09-10 16:56:10 +0200

    Remove SiteApplication dependency from Ranking dispatcher

--- modules/mod_sportsmanagement_ranking/src/Dispatcher/Dispatcher.php
+++ modules/mod_sportsmanagement_ranking/src/Dispatcher/Dispatcher.php
@@ -11,7 +11,6 @@ namespace Diddipoeler\Module\SportsManagementRanking\Site\Dispatcher;
 
 \defined('_JEXEC') or die;
 
-use Joomla\CMS\Application\SiteApplication;
 use Joomla\CMS\Dispatcher\AbstractModuleDispatcher;
 use Joomla\CMS\Helper\HelperFactoryAwareInterface;
 use Joomla\CMS\Helper\HelperFactoryAwareTrait;
@@ -31,7 +30,7 @@ final class Dispatcher extends AbstractModuleDispatcher implements HelperFactory
         $data['params']->set('layout', 'native');
         $app = $this->getApplication();
 
-        if (!$app instanceof SiteApplication) {
+        if (!$app->isClient('site')) {
             throw new \RuntimeException('SportsManagement Ranking requires the Joomla site application.', 500);
         }
 
