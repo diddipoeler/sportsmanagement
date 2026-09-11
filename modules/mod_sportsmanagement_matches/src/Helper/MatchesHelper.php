b892adb5946be81dca807d4be8f54bed26e55631
diddipoeler <diddipoeler@gmx.de>
2026-09-11 21:37:42 +0200

    Use module application container in matches helper

--- modules/mod_sportsmanagement_matches/src/Helper/MatchesHelper.php
+++ modules/mod_sportsmanagement_matches/src/Helper/MatchesHelper.php
@@ -12,7 +12,6 @@ namespace Diddipoeler\Module\SportsManagementMatches\Site\Helper;
 \defined('_JEXEC') or die;
 
 use Joomla\CMS\Application\CMSApplicationInterface;
-use Joomla\CMS\Factory;
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\Database\DatabaseInterface;
 use Joomla\Registry\Registry;
@@ -44,7 +43,7 @@ final class MatchesHelper
                 throw new \RuntimeException('SportsManagement Matches requires the Joomla site application.', 500);
             }
 
-            $joomlaDatabase ??= Factory::getContainer()->get(DatabaseInterface::class);
+            $joomlaDatabase ??= $app->getContainer()->get(DatabaseInterface::class);
             $db = $this->database($params, $joomlaDatabase);
             $matches = $this->loadMatches($db, $params, $projects);
             $showReferees = (int) $params->get('show_referee', 1) === 1;
