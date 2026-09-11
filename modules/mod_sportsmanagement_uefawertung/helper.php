aa7319ed898c4c0199fe4dc3938be7f56238faad
diddipoeler <diddipoeler@gmx.de>
2026-09-11 08:59:16 +0200

    Use site application resolver in UEFA ranking module

--- modules/mod_sportsmanagement_uefawertung/helper.php
+++ modules/mod_sportsmanagement_uefawertung/helper.php
@@ -9,11 +9,19 @@
  */
 \defined('_JEXEC') or die;
 
+use Diddipoeler\Component\SportsManagement\Site\Service\SportsManagementSiteApplicationResolver;
 use Diddipoeler\Module\SportsManagementUefaWertung\Site\Helper\UefaWertungHelper;
-use Joomla\CMS\Application\SiteApplication;
 use Joomla\CMS\Factory;
 use Joomla\Database\DatabaseInterface;
 use Joomla\Registry\Registry;
+
+if (!class_exists(SportsManagementSiteApplicationResolver::class)) {
+    $resolverFile = JPATH_SITE . '/components/com_sportsmanagement/src/Service/SportsManagementSiteApplicationResolver.php';
+
+    if (is_file($resolverFile)) {
+        require_once $resolverFile;
+    }
+}
 
 if (!class_exists(UefaWertungHelper::class)) {
     $nativeHelper = __DIR__ . '/src/Helper/UefaWertungHelper.php';
@@ -42,8 +50,7 @@ class modJSMUefaWERTUNG
     private static function result($params): array
     {
         $registry = $params instanceof Registry ? $params : new Registry((array) $params);
-        /** @var SiteApplication $app */
-        $app = Factory::getContainer()->get(SiteApplication::class);
+        $app = SportsManagementSiteApplicationResolver::resolve();
         /** @var DatabaseInterface $database */
         $database = Factory::getContainer()->get(DatabaseInterface::class);
 
