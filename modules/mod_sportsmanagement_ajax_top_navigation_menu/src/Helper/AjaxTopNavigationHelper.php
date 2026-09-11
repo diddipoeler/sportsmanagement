21b4bc1a19a322387a51d7c83dd1694e9397147d
diddipoeler <diddipoeler@gmx.de>
2026-09-11 07:57:25 +0200

    Remove SiteApplication dependency from AJAX navigation helper

--- modules/mod_sportsmanagement_ajax_top_navigation_menu/src/Helper/AjaxTopNavigationHelper.php
+++ modules/mod_sportsmanagement_ajax_top_navigation_menu/src/Helper/AjaxTopNavigationHelper.php
@@ -12,7 +12,6 @@ namespace Diddipoeler\Module\SportsManagementAjaxTopNavigationMenu\Site\Helper;
 \defined('_JEXEC') or die;
 
 use Joomla\CMS\Application\CMSApplicationInterface;
-use Joomla\CMS\Application\SiteApplication;
 use Joomla\CMS\Component\ComponentHelper;
 use Joomla\CMS\Factory;
 use Joomla\CMS\HTML\HTMLHelper;
@@ -27,7 +26,7 @@ final class AjaxTopNavigationHelper
     {
         $this->loadNavigationDataHelper();
 
-        if (!$app instanceof SiteApplication) {
+        if (!$app->isClient('site')) {
             throw new \RuntimeException('SportsManagement AJAX Top Navigation requires the Joomla site application.', 500);
         }
 
