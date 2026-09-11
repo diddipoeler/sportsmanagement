10d3e4e1ced38611d40dc40bce154742d20c8e71
diddipoeler <diddipoeler@gmx.de>
2026-09-11 20:49:03 +0200

    Use application container in ClubBirthday dispatcher

--- modules/mod_sportsmanagement_club_birthday/src/Dispatcher/Dispatcher.php
+++ modules/mod_sportsmanagement_club_birthday/src/Dispatcher/Dispatcher.php
@@ -10,7 +10,6 @@ namespace Diddipoeler\Module\SportsManagementClubBirthday\Site\Dispatcher;
 \defined('_JEXEC') or die;
 
 use Joomla\CMS\Dispatcher\AbstractModuleDispatcher;
-use Joomla\CMS\Factory;
 use Joomla\CMS\Helper\HelperFactoryAwareInterface;
 use Joomla\CMS\Helper\HelperFactoryAwareTrait;
 use Joomla\Database\DatabaseInterface;
@@ -42,7 +41,7 @@ final class Dispatcher extends AbstractModuleDispatcher implements HelperFactory
         $language->load('com_sportsmanagement_countries', JPATH_ADMINISTRATOR, $tag, true);
 
         /** @var DatabaseInterface $database */
-        $database = Factory::getContainer()->get(DatabaseInterface::class);
+        $database = $app->getContainer()->get(DatabaseInterface::class);
         $result = $this->getHelperFactory()
             ->getHelper('ClubBirthdayHelper')
             ->getData($data['params'], $app, $database);
