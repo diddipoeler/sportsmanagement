c98922545ec508c4b6fc37a4d43283b42715ce08
diddipoeler <diddipoeler@gmx.de>
2026-09-11 21:09:41 +0200

    Use Joomla DI container in site application resolver

--- site/src/Service/SportsManagementSiteApplicationResolver.php
+++ site/src/Service/SportsManagementSiteApplicationResolver.php
@@ -11,16 +11,16 @@ namespace Diddipoeler\Component\SportsManagement\Site\Service;
 
 \defined('_JEXEC') or die;
 
-use Joomla\CMS\Application\CMSApplicationInterface;
+use Joomla\CMS\Application\SiteApplication;
 use Joomla\CMS\Factory;
 
 final class SportsManagementSiteApplicationResolver
 {
-    public static function resolve(): CMSApplicationInterface
+    public static function resolve(): SiteApplication
     {
-        $app = Factory::getApplication();
+        $app = Factory::getContainer()->get(SiteApplication::class);
 
-        if (!$app instanceof CMSApplicationInterface || !$app->isClient('site')) {
+        if (!$app->isClient('site')) {
             throw new \RuntimeException('SportsManagement site application is unavailable.', 500);
         }
 
