651ffcdfb65d31ebc5cc610f6500c0df161a201d
diddipoeler <diddipoeler@gmx.de>
2026-09-11 20:51:15 +0200

    Use application container in CountRekord dispatcher

--- modules/mod_sportsmanagement_count_rekord/src/Dispatcher/Dispatcher.php
+++ modules/mod_sportsmanagement_count_rekord/src/Dispatcher/Dispatcher.php
@@ -12,7 +12,6 @@ namespace Diddipoeler\Module\SportsManagementCountRekord\Site\Dispatcher;
 \defined('_JEXEC') or die;
 
 use Joomla\CMS\Dispatcher\AbstractModuleDispatcher;
-use Joomla\CMS\Factory;
 use Joomla\CMS\Helper\HelperFactoryAwareInterface;
 use Joomla\CMS\Helper\HelperFactoryAwareTrait;
 use Joomla\Database\DatabaseInterface;
@@ -30,7 +29,7 @@ final class Dispatcher extends AbstractModuleDispatcher implements HelperFactory
         }
 
         /** @var DatabaseInterface $database */
-        $database = Factory::getContainer()->get(DatabaseInterface::class);
+        $database = $this->getApplication()->getContainer()->get(DatabaseInterface::class);
         $data['list'] = $this->getHelperFactory()
             ->getHelper('CountRekordHelper')
             ->getData($data['params'], $data['module'], $database);
