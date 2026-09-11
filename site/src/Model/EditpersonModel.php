275c088447d57c2f73868abd6a68baeef50d511a
diddipoeler <diddipoeler@gmx.de>
2026-09-11 13:37:13 +0200

    Use site application resolver in person editor model

--- site/src/Model/EditpersonModel.php
+++ site/src/Model/EditpersonModel.php
@@ -12,9 +12,9 @@ namespace Diddipoeler\Component\SportsManagement\Site\Model;
 \defined('_JEXEC') or die;
 
 use Diddipoeler\Component\SportsManagement\Administrator\Table\PersonTable;
-use Joomla\CMS\Application\CMSApplication;
+use Diddipoeler\Component\SportsManagement\Site\Service\SportsManagementSiteApplicationResolver;
+use Joomla\CMS\Application\CMSApplicationInterface;
 use Joomla\CMS\Component\ComponentHelper;
-use Joomla\CMS\Factory;
 use Joomla\CMS\Form\FormHelper;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\Log\Log;
@@ -29,15 +29,9 @@ final class EditpersonModel extends AdminModel
 
     private ?PersonTable $person = null;
 
-    private function siteApplication(): CMSApplication
+    private function siteApplication(): CMSApplicationInterface
     {
-        $app = Factory::getApplication();
-
-        if (!$app instanceof CMSApplication || !$app->isClient('site')) {
-            throw new \RuntimeException('SportsManagement requires the Joomla site application.');
-        }
-
-        return $app;
+        return SportsManagementSiteApplicationResolver::resolve();
     }
 
     public function updItem($data): bool
