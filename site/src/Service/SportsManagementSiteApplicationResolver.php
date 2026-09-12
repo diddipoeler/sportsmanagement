1f670c1fdcb3cb02c63c154727180dd3b0450fdd
diddipoeler <diddipoeler@gmx.de>
2026-09-12 22:52:14 +0200

    Restore site application resolver interface contract

--- site/src/Service/SportsManagementSiteApplicationResolver.php
+++ site/src/Service/SportsManagementSiteApplicationResolver.php
@@ -11,16 +11,17 @@ namespace Diddipoeler\Component\SportsManagement\Site\Service;
 
 \defined('_JEXEC') or die;
 
+use Joomla\CMS\Application\CMSApplicationInterface;
 use Joomla\CMS\Application\SiteApplication;
 use Joomla\CMS\Factory;
 
 final class SportsManagementSiteApplicationResolver
 {
-    public static function resolve(): SiteApplication
+    public static function resolve(): CMSApplicationInterface
     {
         $app = Factory::getContainer()->get(SiteApplication::class);
 
-        if (!$app instanceof SiteApplication || !$app->isClient('site')) {
+        if (!$app instanceof CMSApplicationInterface || !$app->isClient('site')) {
             throw new \RuntimeException('SportsManagement site application is unavailable.', 500);
         }
 
