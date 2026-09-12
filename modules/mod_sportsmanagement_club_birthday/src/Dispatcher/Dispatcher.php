9bda9ef11b3db55e2e06bafc7db2cb4d6234960d
diddipoeler <diddipoeler@gmx.de>
2026-09-12 15:32:54 +0200

    Fix Club Birthday dispatcher container access

--- modules/mod_sportsmanagement_club_birthday/src/Dispatcher/Dispatcher.php
+++ modules/mod_sportsmanagement_club_birthday/src/Dispatcher/Dispatcher.php
@@ -10,6 +10,7 @@ namespace Diddipoeler\Module\SportsManagementClubBirthday\Site\Dispatcher;
 \defined('_JEXEC') or die;
 
 use Joomla\CMS\Dispatcher\AbstractModuleDispatcher;
+use Joomla\CMS\Factory;
 use Joomla\CMS\Helper\HelperFactoryAwareInterface;
 use Joomla\CMS\Helper\HelperFactoryAwareTrait;
 use Joomla\Database\DatabaseInterface;
@@ -41,7 +42,7 @@ final class Dispatcher extends AbstractModuleDispatcher implements HelperFactory
         $language->load('com_sportsmanagement_countries', JPATH_ADMINISTRATOR, $tag, true);
 
         /** @var DatabaseInterface $database */
-        $database = $app->getContainer()->get(DatabaseInterface::class);
+        $database = Factory::getContainer()->get(DatabaseInterface::class);
         $result = $this->getHelperFactory()
             ->getHelper('ClubBirthdayHelper')
             ->getData($data['params'], $app, $database);
