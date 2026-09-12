a520b330d50c88dbaf89a244a51c3630a5a321dc
diddipoeler <diddipoeler@gmx.de>
2026-09-12 19:40:06 +0200

    Align calendar site application guard

--- modules/mod_sportsmanagement_calendar/src/Dispatcher/Dispatcher.php
+++ modules/mod_sportsmanagement_calendar/src/Dispatcher/Dispatcher.php
@@ -11,6 +11,7 @@ namespace Diddipoeler\Module\SportsManagementCalendar\Site\Dispatcher;
 
 \defined('_JEXEC') or die;
 
+use Joomla\CMS\Application\SiteApplication;
 use Joomla\CMS\Dispatcher\AbstractModuleDispatcher;
 use Joomla\CMS\Helper\HelperFactoryAwareInterface;
 use Joomla\CMS\Helper\HelperFactoryAwareTrait;
@@ -28,6 +29,11 @@ final class Dispatcher extends AbstractModuleDispatcher implements HelperFactory
         }
 
         $app = $this->getApplication();
+
+        if (!$app instanceof SiteApplication || !$app->isClient('site')) {
+            throw new \RuntimeException('SportsManagement Calendar requires the Joomla site application.', 500);
+        }
+
         $language = $app->getLanguage();
         $tag = $language->getTag();
 
