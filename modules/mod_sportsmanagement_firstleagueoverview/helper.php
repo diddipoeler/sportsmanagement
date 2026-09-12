af945cf5f9183c92d993fd510939a8bbc5db99a5
diddipoeler <diddipoeler@gmx.de>
2026-09-12 15:35:12 +0200

    Fix First League Overview helper container access

--- modules/mod_sportsmanagement_firstleagueoverview/helper.php
+++ modules/mod_sportsmanagement_firstleagueoverview/helper.php
@@ -13,6 +13,7 @@ use Diddipoeler\Component\SportsManagement\Site\Helper\SiteRouteHelper;
 use Diddipoeler\Component\SportsManagement\Site\Service\SportsManagementDatabaseResolver;
 use Diddipoeler\Component\SportsManagement\Site\Service\SportsManagementSiteApplicationResolver;
 use Diddipoeler\Module\SportsManagementFirstLeagueOverview\Site\Helper\FirstLeagueOverviewHelper;
+use Joomla\CMS\Factory;
 use Joomla\Database\DatabaseInterface;
 use Joomla\Registry\Registry;
 
@@ -58,7 +59,7 @@ if (!class_exists('modjsmfirstleagueoverview', false)) {
             $registry = $params instanceof Registry ? $params : new Registry((array) ($params ?? []));
             $app = SportsManagementSiteApplicationResolver::resolve();
             /** @var DatabaseInterface $database */
-            $database = $app->getContainer()->get(DatabaseInterface::class);
+            $database = Factory::getContainer()->get(DatabaseInterface::class);
 
             return (new FirstLeagueOverviewHelper())->getData($registry, $database);
         }
