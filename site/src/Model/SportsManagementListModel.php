36eb607120ce6b8a9a52539c10116362890885de
diddipoeler <diddipoeler@gmx.de>
2026-09-12 14:41:34 +0200

    Use public Joomla container in site list model

--- site/src/Model/SportsManagementListModel.php
+++ site/src/Model/SportsManagementListModel.php
@@ -14,6 +14,7 @@ namespace Diddipoeler\Component\SportsManagement\Site\Model;
 use Diddipoeler\Component\SportsManagement\Site\Service\SportsManagementDatabaseResolver;
 use Diddipoeler\Component\SportsManagement\Site\Service\SportsManagementSiteApplicationResolver;
 use Joomla\CMS\Application\SiteApplication;
+use Joomla\CMS\Factory;
 use Joomla\CMS\MVC\Model\ListModel;
 use Joomla\Database\DatabaseInterface;
 
@@ -65,7 +66,7 @@ abstract class SportsManagementListModel extends ListModel
         $this->databaseSelectorOverride = $selector === 1 ? 1 : 0;
 
         /** @var DatabaseInterface $joomlaDatabase */
-        $joomlaDatabase = $this->siteApplication()->getContainer()->get(DatabaseInterface::class);
+        $joomlaDatabase = Factory::getContainer()->get(DatabaseInterface::class);
         $this->setDatabase($joomlaDatabase);
     }
 
