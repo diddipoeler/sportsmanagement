1267c2e323df6959ca6efacc9e7545d971fdc391
diddipoeler <diddipoeler@gmx.de>
2026-09-13 21:51:27 +0200

    Add Joomla 5/6 metadata to Leagues compatibility view

--- admin/views/leagues/view.html.php
+++ admin/views/leagues/view.html.php
@@ -1,5 +1,12 @@
 <?php
-/** Legacy compatibility bridge for the native administrator Leagues view. */
+/**
+ * Legacy compatibility bridge for the native Joomla 5/6 administrator Leagues view.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 \defined('_JEXEC') or die;
 
 use Diddipoeler\Component\SportsManagement\Administrator\View\Leagues\HtmlView;
