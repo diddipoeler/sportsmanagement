1b8d06ccacadf3aff53300e5b3b02ad994f67594
diddipoeler <diddipoeler@gmx.de>
2026-09-13 21:53:06 +0200

    Complete Joomla 5/6 metadata for stats ranking layout

--- site/tmpl/statsranking/default_stats.php
+++ site/tmpl/statsranking/default_stats.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage statsranking
- * @file       default_stats.php
+ * Native Joomla 5/6 SportsManagement statistics ranking layout.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 
