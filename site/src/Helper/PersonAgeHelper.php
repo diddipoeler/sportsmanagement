1749c5700c6050e040ff6541320df566633f9d34
diddipoeler <diddipoeler@gmx.de>
2026-09-13 21:56:32 +0200

    Add Joomla 5/6 metadata to PersonAgeHelper

--- site/src/Helper/PersonAgeHelper.php
+++ site/src/Helper/PersonAgeHelper.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Calculate person ages for native Joomla 5/6 site views.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 namespace Diddipoeler\Component\SportsManagement\Site\Helper;
 
 \defined('_JEXEC') or die;
