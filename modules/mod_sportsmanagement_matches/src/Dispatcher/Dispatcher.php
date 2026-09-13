e39e0e29334747c0ecce1e01afc7e200f3373d5c
diddipoeler <diddipoeler@gmx.de>
2026-09-13 18:01:54 +0200

    Fix matches dispatcher public database container access

--- modules/mod_sportsmanagement_matches/src/Dispatcher/Dispatcher.php
+++ modules/mod_sportsmanagement_matches/src/Dispatcher/Dispatcher.php
@@ -13,6 +13,7 @@ namespace Diddipoeler\Module\SportsManagementMatches\Site\Dispatcher;
 
 use Joomla\CMS\Application\SiteApplication;
 use Joomla\CMS\Dispatcher\AbstractModuleDispatcher;
+use Joomla\CMS\Factory;
 use Joomla\CMS\Helper\HelperFactoryAwareInterface;
 use Joomla\CMS\Helper\HelperFactoryAwareTrait;
 use Joomla\Database\DatabaseInterface;
@@ -44,7 +45,7 @@ final class Dispatcher extends AbstractModuleDispatcher implements HelperFactory
         $language->load('com_sportsmanagement_countries', JPATH_ADMINISTRATOR, $tag, true);
 
         /** @var DatabaseInterface $database */
-        $database = $app->getContainer()->get(DatabaseInterface::class);
+        $database = Factory::getContainer()->get(DatabaseInterface::class);
         $result = $this->getHelperFactory()
             ->getHelper('MatchesHelper')
             ->getData($data['params'], $app, $data['module'], $database);
