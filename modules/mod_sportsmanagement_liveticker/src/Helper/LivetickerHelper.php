f5af7e6413429e979bc504b3f24be1dab8c11686
diddipoeler <diddipoeler@gmx.de>
2026-09-13 18:30:50 +0200

    Fix liveticker helper database container access

--- modules/mod_sportsmanagement_liveticker/src/Helper/LivetickerHelper.php
+++ modules/mod_sportsmanagement_liveticker/src/Helper/LivetickerHelper.php
@@ -16,6 +16,7 @@ use DateTimeZone;
 use Diddipoeler\Component\SportsManagement\Site\Service\SportsManagementDatabaseResolver;
 use Diddipoeler\Component\SportsManagement\Site\Service\SportsManagementSiteApplicationResolver;
 use Joomla\CMS\Application\CMSApplicationInterface;
+use Joomla\CMS\Factory;
 use Joomla\CMS\Helper\ModuleHelper;
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Uri\Uri;
@@ -28,7 +29,7 @@ final class LivetickerHelper
     public function getData(Registry $params, object $module, CMSApplicationInterface $app): array
     {
         /** @var DatabaseInterface $joomlaDatabase */
-        $joomlaDatabase = $app->getContainer()->get(DatabaseInterface::class);
+        $joomlaDatabase = Factory::getContainer()->get(DatabaseInterface::class);
         $db = $this->database($params, $joomlaDatabase);
         $list = $this->getList($params, $app, (int) $params->get('display_num', 5), $db);
         $commentary = (bool) $params->get('display_commentary', 1)
@@ -65,7 +66,7 @@ final class LivetickerHelper
         $params = new Registry();
         $params->loadString((string) ($module->params ?? ''));
         /** @var DatabaseInterface $joomlaDatabase */
-        $joomlaDatabase = $app->getContainer()->get(DatabaseInterface::class);
+        $joomlaDatabase = Factory::getContainer()->get(DatabaseInterface::class);
         $db = $this->database($params, $joomlaDatabase);
         $list = $this->getList($params, $app, (int) $params->get('display_num', 5), $db);
         $commentary = (bool) $params->get('display_commentary', 1)
