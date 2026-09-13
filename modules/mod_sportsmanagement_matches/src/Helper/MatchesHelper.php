51d9aac7e2fc96c41fd597f3ef370f1473f7f829
diddipoeler <diddipoeler@gmx.de>
2026-09-13 18:02:10 +0200

    Fix matches helper public database container fallback

--- modules/mod_sportsmanagement_matches/src/Helper/MatchesHelper.php
+++ modules/mod_sportsmanagement_matches/src/Helper/MatchesHelper.php
@@ -13,6 +13,7 @@ namespace Diddipoeler\Module\SportsManagementMatches\Site\Helper;
 
 use Joomla\CMS\Application\CMSApplicationInterface;
 use Joomla\CMS\Application\SiteApplication;
+use Joomla\CMS\Factory;
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\Database\DatabaseInterface;
 use Joomla\Registry\Registry;
@@ -44,7 +45,7 @@ final class MatchesHelper
                 throw new \RuntimeException('SportsManagement Matches requires the Joomla site application.', 500);
             }
 
-            $joomlaDatabase ??= $app->getContainer()->get(DatabaseInterface::class);
+            $joomlaDatabase ??= Factory::getContainer()->get(DatabaseInterface::class);
             $db = $this->database($params, $joomlaDatabase);
             $matches = $this->loadMatches($db, $params, $projects);
             $showReferees = (int) $params->get('show_referee', 1) === 1;
