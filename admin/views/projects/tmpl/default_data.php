7c06391f021427d0388a0851aad7740148cbcfa6
diddipoeler <diddipoeler@gmx.de>
2026-09-13 15:27:51 +0200

    Continue Joomla 5/6 site admin and module migration

--- admin/views/projects/tmpl/default_data.php
+++ admin/views/projects/tmpl/default_data.php
@@ -1,5 +1,12 @@
 <?php
-/** Legacy compatibility bridge for the native administrator projects data layout. */
+/**
+ * Legacy compatibility bridge for the native administrator projects data layout.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 \defined('_JEXEC') or die;
 
 require JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/tmpl/projects/default_data.php';
--- site/views/nextmatch/tmpl/default_allovereventsranking.php
+++ site/views/nextmatch/tmpl/default_allovereventsranking.php
@@ -1,124 +1,113 @@
 <?php
 /**
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage nextmatch
- * @file       default_allovereventsranking.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * Joomla 5/6 overall events ranking layout for the next match view.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
-defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\Language\Text;
+\defined('_JEXEC') or die;
+
 use Joomla\CMS\HTML\HTMLHelper;
-use Joomla\CMS\Uri\Uri;
-use Joomla\CMS\Factory;
-
-//echo 'alloverevents <pre>'.print_r($this->alloverevents,true).'</pre>';
-//echo 'overallevents <pre>'.print_r($this->overallevents,true).'</pre>';
-
+use Joomla\CMS\Language\Text;
 ?>
 <div class="<?php echo $this->divclassrow; ?> table-responsive" id="nextmatchallovereventsranking">
-	<?php
-	$this->notes = array();
-	$this->notes[] = Text::_('COM_SPORTSMANAGEMENT_NEXTMATCH_ALLOVEREVENTSRANKING');
-	echo $this->loadTemplate('jsm_notes'); 
+    <?php
+    $this->notes   = [];
+    $this->notes[] = Text::_('COM_SPORTSMANAGEMENT_NEXTMATCH_ALLOVEREVENTSRANKING');
+    echo $this->loadTemplate('jsm_notes');
 
-	
-  
-echo JHtml::_('bootstrap.startTabSet', 'myTab2', array('active' => 'name1')); 
-$active = 1;
-foreach ( $this->overallevents as $overallevents => $value )
-		{
- // echo 'value <pre>'.print_r($value,true).'</pre>';
-  
-  unset($ranking);
-		/** tabelle pro ereignis */  
-		foreach ( $this->alloverevents as $alloverevents => $value2 ) if ( $value2->events[$overallevents->id]->event_sum != 0  )
-		{
-          echo 'value2 <pre>'.print_r($value2,true).'</pre>';
-			$temp = new stdclass;  
-			$temp->playerid = $alloverevents;
-			$temp->event_sum = $value2->events[$overallevents->id]->event_sum;  
-			$ranking[] = $temp; 
-		}
-		if ( is_array($ranking) )
-		{
-		/** absteigend sortieren */
-		usort($ranking, function($a, $b) { return $b->event_sum - $a->event_sum; });  
-		}
- 
-  
-  
-		$width    = 20;
-		$height   = 20;
-		$type     = 4;
-		$imgTitle = Text::_($value->name);
-		$icon     = sportsmanagementHelper::getPictureThumb($value->icon, $imgTitle, $width, $height, $type);
-echo JHtml::_('bootstrap.addTab', 'myTab2', 'name'.$active,  $icon.' '.Text::_($value->name) );  
-?>
-<table class="table table-striped">
-						<?php
-						
-						foreach ( $ranking as $rankingkey => $rankingvalue )
-								{  
-							?>
-								<tr>  
-								<td>
-							<?php
-								echo $this->alloverevents[$rankingvalue->playerid]->team_name;
-							?>
-								</td>
- 
-								<td>
-							<?php
-								echo sportsmanagementHelper::formatName(null, $this->alloverevents[$rankingvalue->playerid]->firstname1, $this->alloverevents[$rankingvalue->playerid]->nickname1, $this->alloverevents[$rankingvalue->playerid]->lastname1, $this->config["name_format"]);
-							?>
-								</td>
-  
-								<td>
-							<?php
-								echo sportsmanagementHelperHtml::getBootstrapModalImage(
-											'nextmatchalloverevents' . $this->alloverevents[$rankingvalue->playerid]->playerid ,
-											$this->alloverevents[$rankingvalue->playerid]->tppicture1,
-											$this->alloverevents[$rankingvalue->playerid]->lastname1,
-											'20',
-											'',
-											$this->modalwidth,
-											$this->modalheight,
-											$this->overallconfig['use_jquery_modal']
-										);
-							?>
-								</td>
-								<td>
-							<?php
-								echo $rankingvalue->event_sum;
-							?>
-								</td>  
-							</tr> 
-							<?php  
-  
-							}  
-							
-							?>
-				</table>  
-  <?php
-  
-  
-echo JHtml::_('bootstrap.endTab');  
-  
-$active++;  
-} 
-echo JHtml::_('bootstrap.endTabSet');  
-?>  
-	
-	
-	
-	<?php
+    echo HTMLHelper::_('bootstrap.startTabSet', 'myTab2', ['active' => 'name1']);
 
-	
-	?>	
-	
- 
+    $active = 1;
+
+    foreach ($this->overallevents as $value) {
+        $ranking = [];
+        $eventId = (int) ($value->id ?? 0);
+
+        if ($eventId <= 0) {
+            continue;
+        }
+
+        foreach ($this->alloverevents as $playerId => $player) {
+            $event = $player->events[$eventId] ?? null;
+            $eventSum = (int) ($event->event_sum ?? 0);
+
+            if ($eventSum === 0) {
+                continue;
+            }
+
+            $ranking[] = (object) [
+                'playerid' => $playerId,
+                'event_sum' => $eventSum,
+            ];
+        }
+
+        if ($ranking !== []) {
+            usort(
+                $ranking,
+                static fn($first, $second): int => $second->event_sum <=> $first->event_sum
+            );
+        }
+
+        $width    = 20;
+        $height   = 20;
+        $type     = 4;
+        $imgTitle = Text::_((string) $value->name);
+        $icon     = sportsmanagementHelper::getPictureThumb($value->icon, $imgTitle, $width, $height, $type);
+
+        echo HTMLHelper::_(
+            'bootstrap.addTab',
+            'myTab2',
+            'name' . $active,
+            $icon . ' ' . Text::_((string) $value->name)
+        );
+        ?>
+        <table class="table table-striped">
+            <?php foreach ($ranking as $rankingValue) :
+                $player = $this->alloverevents[$rankingValue->playerid] ?? null;
+
+                if ($player === null) {
+                    continue;
+                }
+                ?>
+                <tr>
+                    <td><?php echo htmlspecialchars((string) ($player->team_name ?? ''), ENT_QUOTES, 'UTF-8'); ?></td>
+                    <td>
+                        <?php
+                        echo sportsmanagementHelper::formatName(
+                            null,
+                            $player->firstname1 ?? '',
+                            $player->nickname1 ?? '',
+                            $player->lastname1 ?? '',
+                            $this->config['name_format']
+                        );
+                        ?>
+                    </td>
+                    <td>
+                        <?php
+                        echo sportsmanagementHelperHtml::getBootstrapModalImage(
+                            'nextmatchalloverevents' . ($player->playerid ?? $rankingValue->playerid),
+                            $player->tppicture1 ?? '',
+                            $player->lastname1 ?? '',
+                            '20',
+                            '',
+                            $this->modalwidth,
+                            $this->modalheight,
+                            $this->overallconfig['use_jquery_modal']
+                        );
+                        ?>
+                    </td>
+                    <td><?php echo (int) $rankingValue->event_sum; ?></td>
+                </tr>
+            <?php endforeach; ?>
+        </table>
+        <?php
+        echo HTMLHelper::_('bootstrap.endTab');
+        ++$active;
+    }
+
+    echo HTMLHelper::_('bootstrap.endTabSet');
+    ?>
 </div>
