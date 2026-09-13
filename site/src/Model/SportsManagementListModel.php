30026eccedc3722a2ed25b1426951135642616c4
diddipoeler <diddipoeler@gmx.de>
2026-09-13 18:01:41 +0200

    Fix site list model public database container access

--- site/src/Model/SportsManagementListModel.php
+++ site/src/Model/SportsManagementListModel.php
@@ -14,6 +14,7 @@ namespace Diddipoeler\Component\SportsManagement\Site\Model;
 use Diddipoeler\Component\SportsManagement\Site\Service\SportsManagementDatabaseResolver;
 use Diddipoeler\Component\SportsManagement\Site\Service\SportsManagementSiteApplicationResolver;
 use Joomla\CMS\Application\SiteApplication;
+use Joomla\CMS\Factory;
 use Joomla\CMS\MVC\Model\ListModel;
 use Joomla\Database\DatabaseInterface;
 
@@ -63,9 +64,10 @@ abstract class SportsManagementListModel extends ListModel
     public function setDatabaseSelector(int $selector): void
     {
         $this->databaseSelectorOverride = $selector === 1 ? 1 : 0;
+        $this->siteApplication();
 
         /** @var DatabaseInterface $joomlaDatabase */
-        $joomlaDatabase = $this->siteApplication()->getContainer()->get(DatabaseInterface::class);
+        $joomlaDatabase = Factory::getContainer()->get(DatabaseInterface::class);
         $this->setDatabase($joomlaDatabase);
     }
 
