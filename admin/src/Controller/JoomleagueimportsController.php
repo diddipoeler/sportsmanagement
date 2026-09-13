ab039f906e88b1efbc15f6b7d65de7bacf7e1bec
diddipoeler <diddipoeler@gmx.de>
2026-09-13 14:55:25 +0200

    Migrate JoomLeague match id step 22

--- admin/src/Controller/JoomleagueimportsController.php
+++ admin/src/Controller/JoomleagueimportsController.php
@@ -64,7 +64,7 @@ final class JoomleagueimportsController extends BaseController
 
         if ($step === '10') {
             $result = $this->runNativeStagingStep($sportsTypeId);
-        } elseif (in_array($step, ['11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21'], true)) {
+        } elseif (in_array($step, ['11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22'], true)) {
             $result = $this->runNativePostImportStep((int) $step, $sportsTypeId);
         } else {
             $model = $this->getModel();
@@ -169,7 +169,8 @@ final class JoomleagueimportsController extends BaseController
             18 => $service->remapProjectTeamTeamRelations(),
             19 => $service->remapProjectTeamAndProjectPositionRelations(),
             20 => $service->remapMatchRosterRelations(),
-            21 => $finalService->remapProjectTeamMatchRelations(),
+            21 => $finalService->remapRoundDivisionAndProjectTeamMatchRelations(),
+            22 => $finalService->remapMatchRelations(),
             default => [],
         };
 
@@ -201,6 +202,7 @@ final class JoomleagueimportsController extends BaseController
             19 => 'Update Team-Spieler/Team-Staff:',
             20 => 'Update Spiele:',
             21 => 'Update Spiele:',
+            22 => 'Update Spiele:',
             default => 'JoomLeague:',
         };
 
