9d34ffe7f33df8667f7338c242fac1e608e29329
diddipoeler <diddipoeler@gmx.de>
2026-09-13 15:44:12 +0200

    Add Joomla 5/6 metadata to rosteralltime view

--- site/src/View/Rosteralltime/HtmlView.php
+++ site/src/View/Rosteralltime/HtmlView.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Native Joomla 5/6 roster-all-time view.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 namespace Diddipoeler\Component\SportsManagement\Site\View\Rosteralltime;
 
 \defined('_JEXEC') or die;
