41d0583ac402e19413171520c09e46a1f25eb45d
diddipoeler <diddipoeler@gmx.de>
2026-09-13 16:05:24 +0200

    Use native nextmatch comments layout from legacy bridge

--- site/views/nextmatch/tmpl/default_comments.php
+++ site/views/nextmatch/tmpl/default_comments.php
@@ -1,39 +1,12 @@
 <?php
 /**
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage nextmatch
- * @file       default_preview.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * Legacy compatibility bridge for the native Joomla 5/6 next-match comments layout.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
-defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\Language\Text;
-use Joomla\CMS\HTML\HTMLHelper;
+\defined('_JEXEC') or die;
 
-/**
- * workaround to support {jcomments (off|lock)} in match preview
- * no comments are shown if {jcomments (off|lock)} is found in the match preview
- */
-$commentsDisabled = 0;
-
-if (!empty($this->match->preview) && preg_match('/{jcomments\s+(off|lock)}/is', $this->match->preview))
-{
-	$commentsDisabled = 1;
-}
-
-// Comments integration
-if (!$commentsDisabled)
-{
-	?>
-    <!-- START of match comments -->
-    <div class="<?php echo $this->divclassrow; ?> table-responsive" id="nextmatch-comments">
-		<?php
-		$commmentsInstance = sportsmanagementModelComments::CreateInstance($this->config);
-		echo $commmentsInstance->showMatchComments($this->match, $this->teams[0], $this->teams[1], $this->config, $this->project);
-		?>
-    </div>
-	<?php
-}
+require JPATH_SITE . '/components/com_sportsmanagement/tmpl/nextmatch/default_comments.php';
