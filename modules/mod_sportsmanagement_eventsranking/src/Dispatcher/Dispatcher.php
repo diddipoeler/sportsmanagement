8d9c52fb83df24c061d4b5617334bc60ef299a68
diddipoeler <diddipoeler@gmx.de>
2026-09-13 11:59:39 +0200

    Use site application container in events ranking

--- modules/mod_sportsmanagement_eventsranking/src/Dispatcher/Dispatcher.php
+++ modules/mod_sportsmanagement_eventsranking/src/Dispatcher/Dispatcher.php
@@ -11,8 +11,8 @@ namespace Diddipoeler\Module\SportsManagementEventsRanking\Site\Dispatcher;
 
 \defined('_JEXEC') or die;
 
+use Joomla\CMS\Application\SiteApplication;
 use Joomla\CMS\Dispatcher\AbstractModuleDispatcher;
-use Joomla\CMS\Factory;
 use Joomla\CMS\Helper\HelperFactoryAwareInterface;
 use Joomla\CMS\Helper\HelperFactoryAwareTrait;
 use Joomla\Database\DatabaseInterface;
@@ -30,6 +30,11 @@ final class Dispatcher extends AbstractModuleDispatcher implements HelperFactory
         }
 
         $app = $this->getApplication();
+
+        if (!$app instanceof SiteApplication || !$app->isClient('site')) {
+            throw new \RuntimeException('SportsManagement Events Ranking requires the Joomla site application.', 500);
+        }
+
         $language = $app->getLanguage();
         $tag = $language->getTag();
 
@@ -39,7 +44,7 @@ final class Dispatcher extends AbstractModuleDispatcher implements HelperFactory
         $language->load('com_sportsmanagement_countries', JPATH_ADMINISTRATOR, $tag, true);
 
         /** @var DatabaseInterface $database */
-        $database = Factory::getContainer()->get(DatabaseInterface::class);
+        $database = $app->getContainer()->get(DatabaseInterface::class);
         $data['rankingData'] = $this->getHelperFactory()
             ->getHelper('EventsRankingHelper')
             ->getData($data['params'], $app, $database);
