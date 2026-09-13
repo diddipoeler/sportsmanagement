6e231fc49dd87c2ee08e2dd9a505eff097c81567
diddipoeler <diddipoeler@gmx.de>
2026-09-13 18:23:32 +0200

    Fix playground plan legacy helper container access

--- modules/mod_sportsmanagement_playgroundplan/helper.php
+++ modules/mod_sportsmanagement_playgroundplan/helper.php
@@ -14,6 +14,7 @@ use Diddipoeler\Component\SportsManagement\Site\Service\SportsManagementSiteAppl
 use Diddipoeler\Module\SportsManagementPlaygroundPlan\Site\Helper\PlaygroundPlanHelper;
 use Joomla\CMS\Application\CMSApplicationInterface;
 use Joomla\CMS\Component\ComponentHelper;
+use Joomla\CMS\Factory;
 use Joomla\Database\DatabaseInterface;
 use Joomla\Registry\Registry;
 
@@ -107,7 +108,7 @@ if (!class_exists('modSportsmanagementPlaygroundplanHelper', false)) {
         {
             $app = self::siteApplication();
             /** @var DatabaseInterface $joomlaDatabase */
-            $joomlaDatabase = $app->getContainer()->get(DatabaseInterface::class);
+            $joomlaDatabase = Factory::getContainer()->get(DatabaseInterface::class);
             $selector = $app->getInput()->getInt(
                 'cfg_which_database',
                 (int) ComponentHelper::getParams('com_sportsmanagement')->get('cfg_which_database', 0)
