ac60db8d0eddd30c43e48bb883a0c48c1dd1e614
diddipoeler <diddipoeler@gmx.de>
2026-09-13 17:13:32 +0200

    Add Joomla 5/6 metadata to rosterpositions view

--- admin/src/View/Rosterpositions/HtmlView.php
+++ admin/src/View/Rosterpositions/HtmlView.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Native Joomla 5/6 administrator list view for roster positions.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 namespace Diddipoeler\Component\SportsManagement\Administrator\View\Rosterpositions;
 \defined('_JEXEC') or die;
 use Joomla\CMS\Language\Text; use Joomla\CMS\MVC\View\HtmlView as BaseHtmlView; use Joomla\CMS\Toolbar\ToolbarHelper;
