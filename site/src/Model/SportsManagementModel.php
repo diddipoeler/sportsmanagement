6b1569a0363b26dbd9223ead8cdf5ba13742508f
diddipoeler <diddipoeler@gmx.de>
2026-09-13 18:01:29 +0200

    Fix site base model public database container access

--- site/src/Model/SportsManagementModel.php
+++ site/src/Model/SportsManagementModel.php
@@ -14,6 +14,7 @@ namespace Diddipoeler\Component\SportsManagement\Site\Model;
 use Diddipoeler\Component\SportsManagement\Site\Service\SportsManagementDatabaseResolver;
 use Diddipoeler\Component\SportsManagement\Site\Service\SportsManagementSiteApplicationResolver;
 use Joomla\CMS\Application\SiteApplication;
+use Joomla\CMS\Factory;
 use Joomla\CMS\MVC\Model\BaseDatabaseModel;
 use Joomla\Database\DatabaseInterface;
 
@@ -35,9 +36,10 @@ abstract class SportsManagementModel extends BaseDatabaseModel
     public function setDatabaseSelector(int $selector): void
     {
         $this->databaseSelectorOverride = $selector === 1 ? 1 : 0;
+        $this->siteApplication();
 
         /** @var DatabaseInterface $joomlaDatabase */
-        $joomlaDatabase = $this->siteApplication()->getContainer()->get(DatabaseInterface::class);
+        $joomlaDatabase = Factory::getContainer()->get(DatabaseInterface::class);
         $this->setDatabase($joomlaDatabase);
     }
 
