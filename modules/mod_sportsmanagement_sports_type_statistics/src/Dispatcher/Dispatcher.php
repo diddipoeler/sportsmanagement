c680b38e44dd12fd3dcd812fd6f0e8d8988d4a58
diddipoeler <diddipoeler@gmx.de>
2026-09-13 18:25:39 +0200

    Fix sports type statistics dispatcher container access

--- modules/mod_sportsmanagement_sports_type_statistics/src/Dispatcher/Dispatcher.php
+++ modules/mod_sportsmanagement_sports_type_statistics/src/Dispatcher/Dispatcher.php
@@ -11,6 +11,7 @@ namespace Diddipoeler\Module\SportsManagementSportsTypeStatistics\Site\Dispatche
 
 use Joomla\CMS\Application\SiteApplication;
 use Joomla\CMS\Dispatcher\AbstractModuleDispatcher;
+use Joomla\CMS\Factory;
 use Joomla\CMS\Helper\HelperFactoryAwareInterface;
 use Joomla\CMS\Helper\HelperFactoryAwareTrait;
 use Joomla\Database\DatabaseInterface;
@@ -42,7 +43,7 @@ final class Dispatcher extends AbstractModuleDispatcher implements HelperFactory
         $language->load('com_sportsmanagement_countries', JPATH_ADMINISTRATOR, $tag, true);
 
         /** @var DatabaseInterface $database */
-        $database = $app->getContainer()->get(DatabaseInterface::class);
+        $database = Factory::getContainer()->get(DatabaseInterface::class);
         $data['statistics'] = $this->getHelperFactory()
             ->getHelper('SportsTypeStatisticsHelper')
             ->getData($data['params'], $database);
