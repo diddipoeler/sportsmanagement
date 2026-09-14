2c2bd0ad13dd39e82036a414afd20f1243301afa
diddipoeler <diddipoeler@gmx.de>
2026-09-14 10:14:41 +0200

    Complete referee default layout metadata

--- site/tmpl/referee/default.php
+++ site/tmpl/referee/default.php
@@ -1,5 +1,12 @@
 <?php
-/** Native Joomla 5/6 referee layout. */
+/**
+ * Native Joomla 5/6 referee layout.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright  Copyright: © 2013-2023 Fussball in Europa https://fussballineuropa.de/ All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 \defined('_JEXEC') or die;
 
 $escape = static fn ($value): string => htmlspecialchars((string) $value, ENT_QUOTES, 'UTF-8');
