200294e9f0def3f3c117da2671ed72df5b748252
diddipoeler <diddipoeler@gmx.de>
2026-09-14 10:47:56 +0200

    Bridge legacy admin listheader notes to Joomla 5/6 tmpl

--- admin/views/listheader/tmpl/default_jsm_notes.php
+++ admin/views/listheader/tmpl/default_jsm_notes.php
@@ -1,45 +1,12 @@
 <?php
 /**
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage listheader
- * @file       default_jsm_notes.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * Compatibility bridge for the Joomla 5/6 administrator listheader notes layout.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
-defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\HTML\HTMLHelper;
-use Joomla\CMS\Language\Text;
+\defined('_JEXEC') or die;
 
-if (version_compare(substr(JVERSION, 0, 3), '4.0', 'ge'))
-{
-$boxicon = 'fas fa-edit fa-2x fa-pull-left';
-}
-else	
-{
-$boxicon = 'info-tab note-icon';	
-}
-?>
-
-<?php
-if ( $this->notes )
-{
-$notes = implode("<br>",$this->notes);  
-?>
-<!--Note box blau -->
-<div class="color-box">
-<div class="shadow">
-<div class="<?php echo $boxicon;?>" title="<?php echo Text::_('COM_SPORTSMANAGEMENT_GLOBAL_NOTE'); ?>"><i></i></div>
-<div class="note-box">
-<p><strong><?php echo Text::_('COM_SPORTSMANAGEMENT_GLOBAL_NOTE'); ?></strong>
-<?php echo $notes; ?>
-</p>
-</div>
-</div>
-</div>
-<!--Note box blau -->
-
-<?php  
-}
+require JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/tmpl/listheader/default_jsm_notes.php';
