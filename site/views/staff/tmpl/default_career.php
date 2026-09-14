ba1e46c835498b2804b496d32001e1f0e8fff88d
diddipoeler <diddipoeler@gmx.de>
2026-09-14 10:21:59 +0200

    Complete staff career bridge metadata

--- site/views/staff/tmpl/default_career.php
+++ site/views/staff/tmpl/default_career.php
@@ -1,5 +1,12 @@
 <?php
-/** Legacy compatibility bridge for the native Joomla 5/6 staff career layout. */
+/**
+ * Compatibility bridge to the native Joomla 5/6 staff career layout.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright  Copyright: © 2013-2023 Fussball in Europa https://fussballineuropa.de/ All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 \defined('_JEXEC') or die;
 
 require JPATH_SITE . '/components/com_sportsmanagement/tmpl/staff/default_career.php';
