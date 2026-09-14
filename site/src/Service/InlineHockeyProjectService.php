b5d6486e1c3fe4ff30197c1d6899d0b9dd59b76e
diddipoeler <diddipoeler@gmx.de>
2026-09-14 14:01:59 +0200

    Add Joomla 5/6 metadata to Inline Hockey services

--- site/src/Service/InlineHockeyProjectService.php
+++ site/src/Service/InlineHockeyProjectService.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Native Joomla 5/6 project service for Inline Hockey imports.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 namespace Diddipoeler\Component\SportsManagement\Site\Service;
 
 \defined('_JEXEC') or die;
