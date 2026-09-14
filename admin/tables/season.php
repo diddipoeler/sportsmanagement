9b68ece57f3914fd307551fcc3dc28d58ae9655a
diddipoeler <diddipoeler@gmx.de>
2026-09-14 08:39:50 +0200

    Standardize Joomla 5/6 season table bridge metadata

--- admin/tables/season.php
+++ admin/tables/season.php
@@ -1,11 +1,15 @@
 <?php
 /**
- * SportsManagement legacy compatibility bridge.
+ * SportsManagement legacy compatibility bridge for the native Season table.
  *
  * The active Joomla 5/6 implementation lives in admin/src/Table/SeasonTable.php.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
-
-defined('_JEXEC') or die('Restricted access');
+\defined('_JEXEC') or die;
 
 use Diddipoeler\Component\SportsManagement\Administrator\Table\SeasonTable;
 
