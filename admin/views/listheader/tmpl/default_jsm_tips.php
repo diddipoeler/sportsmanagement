f1aa790f69d03d5af941f83f23a6973aa598d97a
diddipoeler <diddipoeler@gmx.de>
2026-09-14 10:48:03 +0200

    Bridge legacy admin listheader tips to Joomla 5/6 tmpl

--- admin/views/listheader/tmpl/default_jsm_tips.php
+++ admin/views/listheader/tmpl/default_jsm_tips.php
@@ -1,45 +1,12 @@
 <?php
 /**
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage listheader
- * @file       default_jsm_tips.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * Compatibility bridge for the Joomla 5/6 administrator listheader tips layout.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
-defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\HTML\HTMLHelper;
-use Joomla\CMS\Language\Text;
+\defined('_JEXEC') or die;
 
-if (version_compare(substr(JVERSION, 0, 3), '4.0', 'ge'))
-{
-$boxicon = 'fas fa-quote-right fa-2x fa-pull-left';
-}
-else	
-{
-$boxicon = 'info-tab tip-icon';	
-}
-?>
-
-<?php
-if ( $this->tips )
-{
-$tips = implode("<br>",$this->tips);  
-?>
-<!--Tip Box grün -->
-<div class="color-box">
-<div class="shadow">
-<div class="<?php echo $boxicon;?>" title="<?php echo Text::_('COM_SPORTSMANAGEMENT_GLOBAL_TIP'); ?>"><i></i></div>
-<div class="tip-box">
-<p><strong><?php echo Text::_('COM_SPORTSMANAGEMENT_GLOBAL_TIP'); ?></strong>
-<?php echo $tips; ?>
-</p>
-</div>
-</div>
-</div>
-<!--Tip Box grün -->
-
-<?php  
-}
+require JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/tmpl/listheader/default_jsm_tips.php';
