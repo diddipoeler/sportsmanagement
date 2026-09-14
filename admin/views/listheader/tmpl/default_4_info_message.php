d0362993f8a4ec2745693a1b13badd97519bf848
diddipoeler <diddipoeler@gmx.de>
2026-09-14 10:49:36 +0200

    Bridge legacy admin listheader info message to Joomla 5/6 tmpl

--- admin/views/listheader/tmpl/default_4_info_message.php
+++ admin/views/listheader/tmpl/default_4_info_message.php
@@ -1,21 +1,12 @@
 <?php
 /**
+ * Compatibility bridge for the Joomla 5/6 administrator listheader info message.
  *
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- *
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage listheader
- * @file       default_4_info_message.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
+\defined('_JEXEC') or die;
 
-defined('_JEXEC') or die('Restricted access');
-?>
-<div class="alert alert-success ">
-	<?php
-	echo $this->jsmmessage;
-	?>
-</div>
+require JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/tmpl/listheader/default_4_info_message.php';
