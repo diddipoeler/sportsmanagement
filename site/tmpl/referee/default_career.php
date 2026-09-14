375c1adf975b8225ccbaa26d428139741800eed5
diddipoeler <diddipoeler@gmx.de>
2026-09-14 10:14:55 +0200

    Complete referee career layout metadata

--- site/tmpl/referee/default_career.php
+++ site/tmpl/referee/default_career.php
@@ -1,5 +1,12 @@
 <?php
-/** Native Joomla 5/6 referee career history. */
+/**
+ * Native Joomla 5/6 referee career history.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright  Copyright: © 2013-2023 Fussball in Europa https://fussballineuropa.de/ All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 \defined('_JEXEC') or die;
 
 use Diddipoeler\Component\SportsManagement\Site\Helper\SiteRouteHelper;
