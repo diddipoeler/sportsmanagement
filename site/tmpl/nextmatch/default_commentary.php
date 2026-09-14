ce349bf1ecc0ed1eddd1f14f5d7fa1bf736b6bc4
diddipoeler <diddipoeler@gmx.de>
2026-09-14 10:40:26 +0200

    Harden nextmatch commentary layout wrapper

--- site/tmpl/nextmatch/default_commentary.php
+++ site/tmpl/nextmatch/default_commentary.php
@@ -10,49 +10,29 @@
 \defined('_JEXEC') or die;
 
 use Joomla\CMS\Language\Text;
-
 ?>
 <!-- START of match commentary -->
-<div class="<?php echo $this->divclassrow; ?> table-responsive" id="nextmatch">
-	<?php
-
-	if (!empty($this->matchcommentary))
-	{
-		?>
+<div class="<?php echo htmlspecialchars((string) $this->divclassrow, ENT_QUOTES, 'UTF-8'); ?> table-responsive" id="nextmatch">
+    <?php if (!empty($this->matchcommentary)) : ?>
         <table class="table">
             <tr>
                 <td class="contentheading">
-					<?php
-					echo '&nbsp;' . Text::_('COM_SPORTSMANAGEMENT_MATCHREPORT_MATCH_COMMENTARY');
-					?>
+                    <?php echo '&nbsp;' . Text::_('COM_SPORTSMANAGEMENT_MATCHREPORT_MATCH_COMMENTARY'); ?>
                 </td>
             </tr>
         </table>
 
         <table class="table">
-			<?php
-			foreach ($this->matchcommentary as $commentary)
-			{
-				?>
-
+            <?php foreach ($this->matchcommentary as $commentary) : ?>
                 <tr>
                     <td class="list">
-                        <dl>
-							<?php echo $commentary->event_time; ?>
-                        </dl>
+                        <dl><?php echo $commentary->event_time; ?></dl>
                     </td>
                     <td class="list">
-                        <dl>
-							<?php echo $commentary->notes; ?>
-                        </dl>
+                        <dl><?php echo $commentary->notes; ?></dl>
                     </td>
                 </tr>
-				<?php
-			}
-			?>
+            <?php endforeach; ?>
         </table>
-		<?PHP
-	}
-
-	?>
+    <?php endif; ?>
 </div>
