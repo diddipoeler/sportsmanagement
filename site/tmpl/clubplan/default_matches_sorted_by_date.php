ce291ffcbccdfc2da7703eccde9ee32ee6453ff4
diddipoeler <diddipoeler@gmx.de>
2026-09-14 10:35:31 +0200

    Add Joomla 5/6 metadata to sorted clubplan matches

--- site/tmpl/clubplan/default_matches_sorted_by_date.php
+++ site/tmpl/clubplan/default_matches_sorted_by_date.php
@@ -1,6 +1,13 @@
 <?php
-/** Joomla 5/6 club-plan match list grouped by date. */
-defined('_JEXEC') or die;
+/**
+ * Native Joomla 5/6 club-plan match list grouped by date.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
+\defined('_JEXEC') or die;
 
 use Diddipoeler\Component\SportsManagement\Site\Helper\ClubLogoHelper;
 use Diddipoeler\Component\SportsManagement\Site\Helper\MatchResultHelper;
