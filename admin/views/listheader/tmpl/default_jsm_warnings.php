174a02e09edeb5a73490c5005e1fa8bde73a5e9c
diddipoeler <diddipoeler@gmx.de>
2026-09-14 10:48:09 +0200

    Bridge legacy admin listheader warnings to Joomla 5/6 tmpl

--- admin/views/listheader/tmpl/default_jsm_warnings.php
+++ admin/views/listheader/tmpl/default_jsm_warnings.php
@@ -1,44 +1,12 @@
 <?php
 /**
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage listheader
- * @file       default_jsm_warnings.php
- * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * Compatibility bridge for the Joomla 5/6 administrator listheader warnings layout.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
-defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\HTML\HTMLHelper;
-use Joomla\CMS\Language\Text;
+\defined('_JEXEC') or die;
 
-if (version_compare(substr(JVERSION, 0, 3), '4.0', 'ge'))
-{
-$boxicon = 'fas fa-exclamation-triangle fa-2x fa-pull-left';
-}
-else	
-{
-$boxicon = 'info-tab warning-icon';	
-}
-?>
-
-<?php
-if ( $this->warnings )
-{
-$warnings = implode("<br>",$this->warnings);  
-?>
-<!--Warning box rot -->
-<div class="color-box">
-<div class="shadow">
-<div class="<?php echo $boxicon;?>" title="<?php echo Text::_('COM_SPORTSMANAGEMENT_GLOBAL_WARNING'); ?>"><i></i></div>
-<div class="warning-box">
-<p><strong><?php echo Text::_('COM_SPORTSMANAGEMENT_GLOBAL_WARNING'); ?></strong>
-<?php echo $warnings; ?>
-</p>
-</div>
-</div>
-</div>
-<!--Warning box rot -->
-<?php  
-}
+require JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/tmpl/listheader/default_jsm_warnings.php';
