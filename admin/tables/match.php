80273f6bc99d16c065c0d6f632804313d57f44da
diddipoeler <diddipoeler@gmx.de>
2026-09-14 08:39:45 +0200

    Standardize Joomla 5/6 match table bridge metadata

--- admin/tables/match.php
+++ admin/tables/match.php
@@ -1,5 +1,14 @@
 <?php
-/** SportsManagement legacy compatibility bridge for the match table. */
+/**
+ * SportsManagement legacy compatibility bridge for the native Match table.
+ *
+ * The active Joomla 5/6 implementation lives in admin/src/Table/MatchTable.php.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 \defined('_JEXEC') or die;
 
 use Diddipoeler\Component\SportsManagement\Administrator\Table\MatchTable;
