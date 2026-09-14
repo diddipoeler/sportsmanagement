cd22593696dd755935af0efe5c7fdf26114002c8
diddipoeler <diddipoeler@gmx.de>
2026-09-14 09:35:49 +0200

    Bridge legacy player description layout to Joomla 5/6 tmpl

--- site/views/player/tmpl/default_description.php
+++ site/views/player/tmpl/default_description.php
@@ -1,45 +1,12 @@
 <?php
 /**
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage player
- * @file       default_description.php
+ * Compatibility bridge to the native Joomla 5/6 player description layout.
+ *
+ * @version    5.6.0
  * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @copyright  Copyright: © 2013-2023 Fussball in Europa https://fussballineuropa.de/ All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
-defined('_JEXEC') or die('Restricted access');
-use Joomla\CMS\Language\Text;
-use Joomla\CMS\HTML\HTMLHelper;
+\defined('_JEXEC') or die;
 
-?>
-<!-- Team Player Description START -->
-<div class="<?php echo $this->divclassrow; ?> table-responsive" id="defaultplayerdescription">
-	<?php
-	$description = "";
-
-	if (isset($this->teamPlayer) && !empty($this->teamPlayer->notes))
-	{
-		$description = $this->teamPlayer->notes;
-	}
-    if (isset($this->person) && !empty($this->person->notes))
-	{
-		$description .= '<br />'.$this->person->notes;
-	}
-    
-	if (!empty($description))
-	{
-		?>
-        <h2><?php echo Text::_('COM_SPORTSMANAGEMENT_PERSON_INFO'); ?></h2>
-        <div class="personinfo">
-			<?php
-			$description = HTMLHelper::_('content.prepare', $description);
-			echo stripslashes($description);
-			?>
-        </div>
-		<?php
-	}
-	?>
-</div>
-<!-- Team Player Description END -->
+require JPATH_SITE . '/components/com_sportsmanagement/tmpl/player/default_description.php';
