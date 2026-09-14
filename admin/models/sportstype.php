b951eb40a6648f0874d486b5ca9d3b7df3d8a64e
diddipoeler <diddipoeler@gmx.de>
2026-09-14 16:51:34 +0200

    Add Joomla 5/6 metadata to sportstype bridge

--- admin/models/sportstype.php
+++ admin/models/sportstype.php
@@ -1,8 +1,13 @@
 <?php
 /**
- * SportsManagement legacy compatibility bridge.
+ * SportsManagement legacy compatibility bridge for the native Joomla 5/6 sportstype model.
  *
  * The active Joomla 5/6 implementation lives in admin/src/Model/SportstypeModel.php.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 
 defined('_JEXEC') or die('Restricted access');
