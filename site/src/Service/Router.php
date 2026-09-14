b299dccbdf58adf65d4c761bb9a05eb4cace7b00
diddipoeler <diddipoeler@gmx.de>
2026-09-14 15:27:56 +0200

    Add Joomla 5/6 metadata to site router

--- site/src/Service/Router.php
+++ site/src/Service/Router.php
@@ -4,6 +4,11 @@
  *
  * Keeps the established SportsManagement URL format while integrating the
  * component with Joomla's RouterFactory service and menu preprocessing.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 
 namespace Diddipoeler\Component\SportsManagement\Site\Service;
