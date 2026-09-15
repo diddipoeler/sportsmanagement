24a2f129b3d1c2e12886ca234ea419b4a3233f7f
diddipoeler <diddipoeler@gmx.de>
2026-09-15 14:57:25 +0200

    Add Joomla 5/6 metadata to tree-to-node bridge

--- site/models/treetonode.php
+++ site/models/treetonode.php
@@ -1,5 +1,12 @@
 <?php
-/** SportsManagement legacy compatibility bridge for the Joomla 5/6 tree-to-node model. */
+/**
+ * SportsManagement legacy compatibility bridge for the Joomla 5/6 tree-to-node model.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
+ * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 defined('_JEXEC') or die;
 
 use Diddipoeler\Component\SportsManagement\Site\Model\TreetonodeModel;
