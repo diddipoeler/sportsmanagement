896522fa396e680c154fe41688756c75fda0f516
diddipoeler <diddipoeler@gmx.de>
2026-09-16 13:06:40 +0200

    Use Joomla 5/6 date helper for associations

--- admin/src/Model/JlextassociationModel.php
+++ admin/src/Model/JlextassociationModel.php
@@ -11,6 +11,7 @@ namespace Diddipoeler\Component\SportsManagement\Administrator\Model;
 
 \defined('_JEXEC') or die;
 
+use Diddipoeler\Component\SportsManagement\Administrator\Helper\SportsManagementDateHelper;
 use Diddipoeler\Component\SportsManagement\Administrator\Table\JlextassociationTable;
 
 /** Native Joomla 5/6 administrator form model for associations. */
@@ -27,17 +28,15 @@ final class JlextassociationModel extends SportsManagementAdminModel
 
     protected function prepareSportsManagementData(array $data): array
     {
-        $this->ensureSportsManagementHelper();
-
         $founded = trim((string) ($data['founded'] ?? ''));
         $dissolved = trim((string) ($data['dissolved'] ?? ''));
 
         if ($founded !== '' && $founded !== '0000-00-00') {
-            $founded = (string) \sportsmanagementHelper::convertDate($founded, 0);
+            $founded = SportsManagementDateHelper::convertDate($founded, 0);
         }
 
         if ($dissolved !== '' && $dissolved !== '0000-00-00') {
-            $dissolved = (string) \sportsmanagementHelper::convertDate($dissolved, 0);
+            $dissolved = SportsManagementDateHelper::convertDate($dissolved, 0);
         }
 
         $data['founded'] = $founded !== '' ? $founded : '0000-00-00';
@@ -45,33 +44,16 @@ final class JlextassociationModel extends SportsManagementAdminModel
 
         if ($data['founded'] !== '0000-00-00') {
             $data['founded_year'] = date('Y', strtotime($data['founded']));
-            $data['founded_timestamp'] = \sportsmanagementHelper::getTimestamp($data['founded']);
+            $data['founded_timestamp'] = SportsManagementDateHelper::getTimestamp($data['founded']);
         } elseif (empty($data['founded_year'])) {
             $data['founded_year'] = 'kein';
         }
 
         if ($data['dissolved'] !== '0000-00-00') {
             $data['dissolved_year'] = date('Y', strtotime($data['dissolved']));
-            $data['dissolved_timestamp'] = \sportsmanagementHelper::getTimestamp($data['dissolved']);
+            $data['dissolved_timestamp'] = SportsManagementDateHelper::getTimestamp($data['dissolved']);
         }
 
         return $data;
-    }
-
-    private function ensureSportsManagementHelper(): void
-    {
-        if (class_exists('sportsmanagementHelper', false)) {
-            return;
-        }
-
-        $helperFile = JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/helpers/sportsmanagement.php';
-
-        if (is_file($helperFile)) {
-            require_once $helperFile;
-        }
-
-        if (!class_exists('sportsmanagementHelper', false)) {
-            throw new \RuntimeException('SportsManagement helper is unavailable.');
-        }
     }
 }
