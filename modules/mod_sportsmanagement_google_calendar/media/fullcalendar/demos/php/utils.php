7afe8716ce9d4dbbfebafff83179577ccd4a09e7
diddipoeler <diddipoeler@gmx.de>
2026-09-18 12:31:12 +0200

    Complete Joomla 5/6 Google Calendar PHP headers

--- modules/mod_sportsmanagement_google_calendar/media/fullcalendar/demos/php/utils.php
+++ modules/mod_sportsmanagement_google_calendar/media/fullcalendar/demos/php/utils.php
@@ -7,7 +7,12 @@
 // Requires PHP 5.2.0 or higher.
 // --------------------------------------------------------------------------------------------------
 /**
- * @license   GNU General Public License version 2 or later; see LICENSE.txt
+ * FullCalendar demo utility helpers retained for Joomla 5/6 compatibility.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 defined('_JEXEC') or die('Restricted access');
 
