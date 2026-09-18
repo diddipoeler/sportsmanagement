e47c89373f89f3995c8b610ca4c2ba7ea667cde1
diddipoeler <diddipoeler@gmx.de>
2026-09-18 20:27:56 +0200

    Harden Joomla 5/6 administrator list model bridge

--- admin/models/sportsmanagements.php
+++ admin/models/sportsmanagements.php
@@ -7,13 +7,17 @@
  * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
-defined('_JEXEC') or die('Restricted access');
+\defined('_JEXEC') or die;
 
 use Diddipoeler\Component\SportsManagement\Administrator\Model\SportsmanagementsModel;
 
 if (!class_exists(SportsmanagementsModel::class)) {
     require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Model/SportsManagementListModel.php';
     require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Model/SportsmanagementsModel.php';
+}
+
+if (!class_exists(SportsmanagementsModel::class)) {
+    throw new \RuntimeException('SportsManagement native administrator list model could not be loaded.', 500);
 }
 
 if (!class_exists('sportsmanagementModelsportsmanagements', false)) {
