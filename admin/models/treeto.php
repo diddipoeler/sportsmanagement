7713aba8b4e55a1bdfd5c87c77a97b3651efcce8
diddipoeler <diddipoeler@gmx.de>
2026-09-18 20:28:59 +0200

    Harden Joomla 5/6 treeto model bridge

--- admin/models/treeto.php
+++ admin/models/treeto.php
@@ -7,13 +7,17 @@
  * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
-defined('_JEXEC') or die('Restricted access');
+\defined('_JEXEC') or die;
 
 use Diddipoeler\Component\SportsManagement\Administrator\Model\TreetoModel;
 
 if (!class_exists(TreetoModel::class)) {
     require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Model/SportsManagementAdminModel.php';
     require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Model/TreetoModel.php';
+}
+
+if (!class_exists(TreetoModel::class)) {
+    throw new \\RuntimeException('SportsManagement native Treeto model could not be loaded.', 500);
 }
 
 if (!class_exists('sportsmanagementModelTreeto', false)) {
