9acae7dd05351967c69bec4f890c76392663ed08
diddipoeler <diddipoeler@gmx.de>
2026-09-18 19:44:54 +0200

    Align Joomla 5/6 frontend SportsManagement model bridge

--- site/models/sportsmanagement.php
+++ site/models/sportsmanagement.php
@@ -1,15 +1,13 @@
 <?php
 /**
- * SportsManagement ein Programm zur Verwaltung für alle Sportarten
- * @version    1.0.05
- * @package    Sportsmanagement
- * @subpackage models
- * @file       sportsmanagement.php
+ * Joomla 5/6 compatibility model for the historic SportsManagement frontend root view.
+ *
+ * @version    5.6.0
  * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
- * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
+ * @copyright  Copyright (C) 2013-2026 Fussball in Europa
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
-defined('_JEXEC') or die('Restricted access');
+\defined('_JEXEC') or die;
 
 use Diddipoeler\Component\SportsManagement\Administrator\Table\LegacySportsmanagementTable;
 use Joomla\CMS\Factory;
@@ -34,6 +32,10 @@ class sportsmanagementModelsportsmanagement extends ItemModel
     {
         if (!class_exists(LegacySportsmanagementTable::class)) {
             require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Table/LegacySportsmanagementTable.php';
+        }
+
+        if (!class_exists(LegacySportsmanagementTable::class)) {
+            throw new \RuntimeException('SportsManagement legacy table bridge could not be loaded.', 500);
         }
 
         /** @var DatabaseInterface $db */
