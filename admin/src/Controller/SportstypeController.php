16c2f8e5b7178d938d2a8c5f9555213d64f551df
diddipoeler <diddipoeler@gmx.de>
2026-09-18 16:17:23 +0200

    Complete Joomla 5/6 sports type controller header

--- admin/src/Controller/SportstypeController.php
+++ admin/src/Controller/SportstypeController.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Joomla 5/6 SportsManagement administrator controller.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 namespace Diddipoeler\Component\SportsManagement\Administrator\Controller;
 \defined('_JEXEC') or die;
 /** Native Joomla 5/6 standard CRUD controller for sportstype. */
