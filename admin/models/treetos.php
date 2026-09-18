acdcd82f54aab6e8ea778fba7db21f1d10042530
diddipoeler <diddipoeler@gmx.de>
2026-09-18 20:29:02 +0200

    Harden Joomla 5/6 treetos model bridge

--- admin/models/treetos.php
+++ admin/models/treetos.php
@@ -7,13 +7,17 @@
  * @copyright  Copyright (C) diddipoeler
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
-defined('_JEXEC') or die('Restricted access');
+\defined('_JEXEC') or die;
 
 use Diddipoeler\Component\SportsManagement\Administrator\Model\TreetosModel;
 
 if (!class_exists(TreetosModel::class)) {
     require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Model/SportsManagementListModel.php';
     require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Model/TreetosModel.php';
+}
+
+if (!class_exists(TreetosModel::class)) {
+    throw new \\RuntimeException('SportsManagement native Treetos model could not be loaded.', 500);
 }
 
 if (!class_exists('sportsmanagementModelTreetos', false)) {
