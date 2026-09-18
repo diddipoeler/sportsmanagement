b2fa6f3064f163965029903f1a837529892df3ea
diddipoeler <diddipoeler@gmx.de>
2026-09-18 12:31:08 +0200

    Complete Joomla 5/6 Google Calendar PHP headers

--- modules/mod_sportsmanagement_google_calendar/media/fullcalendar/demos/php/get-timezones.php
+++ modules/mod_sportsmanagement_google_calendar/media/fullcalendar/demos/php/get-timezones.php
@@ -7,7 +7,12 @@
 // Requires PHP 5.2.0 or higher.
 // --------------------------------------------------------------------------------------------------
 /**
- * @license   GNU General Public License version 2 or later; see LICENSE.txt
+ * FullCalendar timezone demo helper retained for Joomla 5/6 compatibility.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 defined('_JEXEC') or die('Restricted access');
 echo json_encode(DateTimeZone::listIdentifiers());
