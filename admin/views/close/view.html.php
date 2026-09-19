cae7d78cc716b61312ce82500efa6a6edc3ce347
diddipoeler <diddipoeler@gmx.de>
2026-09-19 16:55:07 +0200

    Harden Joomla 5/6 Close administrator view bridge

--- admin/views/close/view.html.php
+++ admin/views/close/view.html.php
@@ -1,7 +1,11 @@
 <?php
 /**
- * SportsManagement legacy administrator view bridge.
- * The active Joomla 5/6 implementation lives in admin/src/View/Close/HtmlView.php.
+ * Legacy compatibility bridge for the native Joomla 5/6 administrator Close view.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 \defined('_JEXEC') or die;
 
@@ -9,6 +13,10 @@ use Diddipoeler\Component\SportsManagement\Administrator\View\Close\HtmlView;
 
 if (!class_exists(HtmlView::class)) {
     require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/View/Close/HtmlView.php';
+}
+
+if (!class_exists(HtmlView::class)) {
+    throw new \RuntimeException('SportsManagement native administrator Close view could not be loaded.', 500);
 }
 
 if (!class_exists('sportsmanagementViewClose', false)) {
