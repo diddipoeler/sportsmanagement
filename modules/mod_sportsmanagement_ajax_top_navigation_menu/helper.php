53317418a4ebbc0740eb21e928e7648a4ea32259
diddipoeler <diddipoeler@gmx.de>
2026-09-19 13:03:42 +0200

    Harden AJAX Top Navigation Joomla 5/6 helper bridge

--- modules/mod_sportsmanagement_ajax_top_navigation_menu/helper.php
+++ modules/mod_sportsmanagement_ajax_top_navigation_menu/helper.php
@@ -12,7 +12,15 @@
 use Diddipoeler\Module\SportsManagementAjaxTopNavigationMenu\Site\Helper\NavigationDataHelper;
 
 if (!class_exists(NavigationDataHelper::class)) {
-    require_once __DIR__ . '/src/Helper/NavigationDataHelper.php';
+    $nativeHelper = __DIR__ . '/src/Helper/NavigationDataHelper.php';
+
+    if (is_file($nativeHelper)) {
+        require_once $nativeHelper;
+    }
+}
+
+if (!class_exists(NavigationDataHelper::class)) {
+    throw new \RuntimeException('SportsManagement AJAX Top Navigation helper could not be loaded.', 500);
 }
 
 if (!class_exists('modSportsmanagementAjaxTopNavigationMenuHelper', false)) {
