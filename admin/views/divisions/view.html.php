d595d7e87585d9b370677075718887742859bc79
diddipoeler <diddipoeler@gmx.de>
2026-09-19 14:58:19 +0200

    Harden Joomla 5/6 Divisions administrator view bridge

--- admin/views/divisions/view.html.php
+++ admin/views/divisions/view.html.php
@@ -1,10 +1,27 @@
 <?php
 /**
- * Joomla 5/6 compatibility bridge for the legacy administrator divisions view.
+ * Legacy compatibility bridge for the native Joomla 5/6 administrator Divisions view.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 \defined('_JEXEC') or die;
 
 use Diddipoeler\Component\SportsManagement\Administrator\View\Divisions\HtmlView;
+
+if (!class_exists(HtmlView::class)) {
+    $nativeView = JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/View/Divisions/HtmlView.php';
+
+    if (is_file($nativeView)) {
+        require_once $nativeView;
+    }
+}
+
+if (!class_exists(HtmlView::class)) {
+    throw new \RuntimeException('SportsManagement native administrator Divisions view could not be loaded.', 500);
+}
 
 if (!class_exists('sportsmanagementViewDivisions', false)) {
     class_alias(HtmlView::class, 'sportsmanagementViewDivisions');
