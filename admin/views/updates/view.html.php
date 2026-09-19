56ca807d6ad59b2dfc42380504204ba2693fc00d
diddipoeler <diddipoeler@gmx.de>
2026-09-19 16:23:57 +0200

    Harden Joomla 5/6 Updates administrator view bridge

--- admin/views/updates/view.html.php
+++ admin/views/updates/view.html.php
@@ -15,6 +15,10 @@ if (!class_exists(HtmlView::class)) {
     require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/View/Updates/HtmlView.php';
 }
 
+if (!class_exists(HtmlView::class)) {
+    throw new \RuntimeException('SportsManagement native administrator Updates view could not be loaded.', 500);
+}
+
 if (!class_exists('sportsmanagementViewUpdates', false)) {
     class_alias(HtmlView::class, 'sportsmanagementViewUpdates');
 }
