7e46c665cee492ba3f11ab49d1391675d94042d5
diddipoeler <diddipoeler@gmx.de>
2026-09-19 12:42:09 +0200

    Fix TeamPlayers Joomla 5/6 Factory import

--- modules/mod_sportsmanagement_teamplayers/src/Dispatcher/Dispatcher.php
+++ modules/mod_sportsmanagement_teamplayers/src/Dispatcher/Dispatcher.php
@@ -13,6 +13,7 @@ namespace Diddipoeler\Module\SportsManagementTeamPlayers\Site\Dispatcher;
 
 use Joomla\CMS\Application\SiteApplication;
 use Joomla\CMS\Dispatcher\AbstractModuleDispatcher;
+use Joomla\CMS\Factory;
 use Joomla\CMS\Helper\HelperFactoryAwareInterface;
 use Joomla\CMS\Helper\HelperFactoryAwareTrait;
 use Joomla\Database\DatabaseInterface;
