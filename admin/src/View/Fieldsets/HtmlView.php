ec22999121377169d40587d6ec0f1c9c4dd2046b
diddipoeler <diddipoeler@gmx.de>
2026-09-19 17:21:06 +0200

    Add native Joomla 5/6 administrator Fieldsets view

---
+++ admin/src/View/Fieldsets/HtmlView.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Native Joomla 5/6 administrator SportsManagement Fieldsets view.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
+namespace Diddipoeler\Component\SportsManagement\Administrator\View\Fieldsets;
+
+\defined('_JEXEC') or die;
+
+use Joomla\CMS\MVC\View\HtmlView as BaseHtmlView;
+
+/** Native Joomla 5/6 administrator fieldsets compatibility view. */
+final class HtmlView extends BaseHtmlView
+{
+    public function init(): void
+    {
+    }
+}
