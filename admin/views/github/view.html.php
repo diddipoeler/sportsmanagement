d77663ee92cee0d46b42120c37fc8e93d203c643
diddipoeler <diddipoeler@gmx.de>
2026-09-19 16:54:44 +0200

    Harden Joomla 5/6 GitHub administrator view bridge

--- admin/views/github/view.html.php
+++ admin/views/github/view.html.php
@@ -1,11 +1,22 @@
 <?php
-/** Legacy compatibility bridge for the native Joomla 5/6 GitHub administrator view. */
+/**
+ * Legacy compatibility bridge for the native Joomla 5/6 administrator GitHub view.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 \defined('_JEXEC') or die;
 
 use Diddipoeler\Component\SportsManagement\Administrator\View\Github\HtmlView;
 
 if (!class_exists(HtmlView::class)) {
     require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/View/Github/HtmlView.php';
+}
+
+if (!class_exists(HtmlView::class)) {
+    throw new \RuntimeException('SportsManagement native administrator GitHub view could not be loaded.', 500);
 }
 
 if (!class_exists('sportsmanagementViewgithub', false)) {
