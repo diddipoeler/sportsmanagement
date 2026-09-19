a5e4db052e99bb057c55a67de7b4b86d056fd9d9
diddipoeler <diddipoeler@gmx.de>
2026-09-19 15:59:37 +0200

    Complete Joomla 5/6 Sportstype administrator view header

--- admin/src/View/Sportstype/HtmlView.php
+++ admin/src/View/Sportstype/HtmlView.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Native Joomla 5/6 administrator SportsManagement Sportstype edit view.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 namespace Diddipoeler\Component\SportsManagement\Administrator\View\Sportstype;
 
 \defined('_JEXEC') or die;
