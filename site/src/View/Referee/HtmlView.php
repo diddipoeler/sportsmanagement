05da26a1503ce354d77b7aa788646e91b33ee2da
diddipoeler <diddipoeler@gmx.de>
2026-09-19 13:20:29 +0200

    Complete Joomla 5/6 frontend view header

--- site/src/View/Referee/HtmlView.php
+++ site/src/View/Referee/HtmlView.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Native Joomla 5/6 frontend SportsManagement Referee view.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 namespace Diddipoeler\Component\SportsManagement\Site\View\Referee;
 
 \defined('_JEXEC') or die;
