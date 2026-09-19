3c531b50da591c2057050e6e6f1d3f5016126622
diddipoeler <diddipoeler@gmx.de>
2026-09-19 12:53:49 +0200

    Use Joomla 5/6 DI container in TrainingsData dispatcher

--- modules/mod_sportsmanagement_trainingsdata/src/Dispatcher/Dispatcher.php
+++ modules/mod_sportsmanagement_trainingsdata/src/Dispatcher/Dispatcher.php
@@ -14,6 +14,7 @@ namespace Diddipoeler\Module\SportsManagementTrainingsData\Site\Dispatcher;
 use Joomla\CMS\Application\SiteApplication;
 use Joomla\CMS\Dispatcher\AbstractModuleDispatcher;
 use Joomla\CMS\Document\HtmlDocument;
+use Joomla\CMS\Factory;
 use Joomla\CMS\Helper\HelperFactoryAwareInterface;
 use Joomla\CMS\Helper\HelperFactoryAwareTrait;
 use Joomla\Database\DatabaseInterface;
@@ -39,7 +40,7 @@ final class Dispatcher extends AbstractModuleDispatcher implements HelperFactory
         $app->getLanguage()->load('com_sportsmanagement', JPATH_SITE, null, true);
 
         /** @var DatabaseInterface $database */
-        $database = $app->getContainer()->get(DatabaseInterface::class);
+        $database = Factory::getContainer()->get(DatabaseInterface::class);
         $data['trainingsdata'] = $this->getHelperFactory()
             ->getHelper('TrainingsDataHelper')
             ->getData($data['params'], $database);
