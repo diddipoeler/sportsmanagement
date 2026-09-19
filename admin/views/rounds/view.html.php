ad086d14a897c3b6d99fa603ba57e4a365c83f14
diddipoeler <diddipoeler@gmx.de>
2026-09-19 13:23:15 +0200

    Harden Joomla 5/6 Rounds administrator view bridge

--- admin/views/rounds/view.html.php
+++ admin/views/rounds/view.html.php
@@ -1,11 +1,22 @@
 <?php
-/** Legacy compatibility bridge for the native administrator rounds view. */
+/**
+ * Legacy compatibility bridge for the native Joomla 5/6 administrator Rounds view.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 \defined('_JEXEC') or die;
 
 use Diddipoeler\Component\SportsManagement\Administrator\View\Rounds\HtmlView;
 
 if (!class_exists(HtmlView::class)) {
     require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/View/Rounds/HtmlView.php';
+}
+
+if (!class_exists(HtmlView::class)) {
+    throw new \RuntimeException('SportsManagement native administrator Rounds view could not be loaded.', 500);
 }
 
 if (!class_exists('sportsmanagementViewRounds', false)) {
