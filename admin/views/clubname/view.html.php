c68e9bc03024991ebd574b19f5e0ae86ee0e9ae9
diddipoeler <diddipoeler@gmx.de>
2026-09-19 16:19:52 +0200

    Harden Joomla 5/6 Clubname administrator view bridge

--- admin/views/clubname/view.html.php
+++ admin/views/clubname/view.html.php
@@ -1,11 +1,22 @@
 <?php
-/** Legacy compatibility bridge for the native Joomla 5/6 club-name administrator view. */
+/**
+ * Legacy compatibility bridge for the native Joomla 5/6 administrator Clubname view.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 \defined('_JEXEC') or die;
 
 use Diddipoeler\Component\SportsManagement\Administrator\View\Clubname\HtmlView;
 
 if (!class_exists(HtmlView::class)) {
     require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/View/Clubname/HtmlView.php';
+}
+
+if (!class_exists(HtmlView::class)) {
+    throw new \RuntimeException('SportsManagement native administrator Clubname view could not be loaded.', 500);
 }
 
 if (!class_exists('sportsmanagementViewclubname', false)) {
