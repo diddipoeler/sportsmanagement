4c8b938558d5c500fc3b982e61dcb5d05eb50294
diddipoeler <diddipoeler@gmx.de>
2026-09-19 16:21:54 +0200

    Harden Joomla 5/6 Templates administrator view bridge

--- admin/views/templates/view.html.php
+++ admin/views/templates/view.html.php
@@ -1,11 +1,22 @@
 <?php
-/** Legacy compatibility bridge for the native Joomla 5/6 project templates view. */
+/**
+ * Legacy compatibility bridge for the native Joomla 5/6 administrator Templates view.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 \defined('_JEXEC') or die;
 
 use Diddipoeler\Component\SportsManagement\Administrator\View\Templates\HtmlView;
 
 if (!class_exists(HtmlView::class)) {
     require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/View/Templates/HtmlView.php';
+}
+
+if (!class_exists(HtmlView::class)) {
+    throw new \RuntimeException('SportsManagement native administrator Templates view could not be loaded.', 500);
 }
 
 if (!class_exists('sportsmanagementViewTemplates', false)) {
