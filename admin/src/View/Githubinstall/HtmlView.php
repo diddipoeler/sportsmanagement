cc9679fd1c2a5c8d12a444203e909f9064af2270
diddipoeler <diddipoeler@gmx.de>
2026-09-19 16:56:51 +0200

    Complete Joomla 5/6 GitHubinstall administrator view header

--- admin/src/View/Githubinstall/HtmlView.php
+++ admin/src/View/Githubinstall/HtmlView.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Native Joomla 5/6 administrator SportsManagement GitHubinstall view.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 namespace Diddipoeler\Component\SportsManagement\Administrator\View\Githubinstall;
 
 \defined('_JEXEC') or die;
