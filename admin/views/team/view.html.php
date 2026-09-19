3a2621a2c33384fd4ee6705693b55f6c5c152492
diddipoeler <diddipoeler@gmx.de>
2026-09-19 12:53:28 +0200

    Harden Joomla 5/6 Team administrator view bridge

--- admin/views/team/view.html.php
+++ admin/views/team/view.html.php
@@ -1,11 +1,22 @@
 <?php
-/** Legacy compatibility bridge for the native administrator team editor. */
+/**
+ * Legacy compatibility bridge for the native Joomla 5/6 administrator Team view.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 \defined('_JEXEC') or die;
 
 use Diddipoeler\Component\SportsManagement\Administrator\View\Team\HtmlView;
 
 if (!class_exists(HtmlView::class)) {
     require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/View/Team/HtmlView.php';
+}
+
+if (!class_exists(HtmlView::class)) {
+    throw new \RuntimeException('SportsManagement native administrator Team view could not be loaded.', 500);
 }
 
 if (!class_exists('sportsmanagementViewTeam', false)) {
