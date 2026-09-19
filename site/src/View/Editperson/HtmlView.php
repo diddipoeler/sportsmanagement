1bd820bd014a028cd252baf3f3861df9c4c436c8
diddipoeler <diddipoeler@gmx.de>
2026-09-19 16:56:25 +0200

    Complete Joomla 5/6 frontend edit view header

--- site/src/View/Editperson/HtmlView.php
+++ site/src/View/Editperson/HtmlView.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Native Joomla 5/6 frontend SportsManagement Editperson view.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 namespace Diddipoeler\Component\SportsManagement\Site\View\Editperson;
 
 \defined('_JEXEC') or die;
