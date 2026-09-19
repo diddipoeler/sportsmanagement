215a06b7a03f64dd8996023b9c591c023fce5420
diddipoeler <diddipoeler@gmx.de>
2026-09-19 12:42:14 +0200

    Fix Team Statistics Counter Joomla 5/6 Factory import

--- modules/mod_sportsmanagement_teamstatistics_counter/helper.php
+++ modules/mod_sportsmanagement_teamstatistics_counter/helper.php
@@ -11,6 +11,7 @@
 
 use Diddipoeler\Component\SportsManagement\Site\Service\SportsManagementSiteApplicationResolver;
 use Diddipoeler\Module\SportsManagementTeamStatisticsCounter\Site\Helper\TeamStatisticsCounterHelper;
+use Joomla\CMS\Factory;
 use Joomla\Database\DatabaseInterface;
 use Joomla\Registry\Registry;
 
