d73d7aae7a05a4d43353ee9d0baa9828eca663b6
diddipoeler <diddipoeler@gmx.de>
2026-09-19 16:25:47 +0200

    Complete Joomla 5/6 Update layout header

--- admin/tmpl/update/default.php
+++ admin/tmpl/update/default.php
@@ -1,3 +1,10 @@
 <?php
-/** Joomla 5/6 native administrator update layout. */
+/**
+ * Native Joomla 5/6 administrator SportsManagement Update layout.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 \defined('_JEXEC') or die;
