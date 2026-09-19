907d188c03f290abb8d128f538fd5e2ef72573b3
diddipoeler <diddipoeler@gmx.de>
2026-09-19 17:21:27 +0200

    Bridge legacy SIS handball view to native Joomla 5/6 view

--- site/views/sishandball/view.html.php
+++ site/views/sishandball/view.html.php
@@ -1,80 +1,24 @@
 <?php
 /**
- * SportsManagement SIS handball compatibility view for Joomla 5/6.
+ * Legacy compatibility bridge for the native Joomla 5/6 frontend SIS handball view.
  *
  * @version    5.6.0
  * @author     diddipoeler
  * @copyright  Copyright (C) diddipoeler
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
-defined('_JEXEC') or die('Restricted access');
+\defined('_JEXEC') or die;
 
-use Joomla\CMS\Application\SiteApplication;
-use Joomla\CMS\Component\ComponentHelper;
-use Joomla\CMS\Factory;
-use Joomla\CMS\MVC\View\HtmlView;
+use Diddipoeler\Component\SportsManagement\Site\View\Sishandball\HtmlView;
 
-class sportsmanagementViewsishandball extends HtmlView
-{
-    public $sis_getxmldatei = 0;
-    public $sis_getschiedsrichter = 1;
-    public $sis_getspielort = 1;
-    public $linkresults = '';
-    public $params;
-    public $paramscomponent;
-    public $sis_art = '';
-    public $vereinsnummer = '';
-    public $liganummer = '';
-    public $tabelle = null;
-    public $spielplan = null;
-    public $statistik = null;
-    public $article;
+if (!class_exists(HtmlView::class)) {
+    require_once JPATH_SITE . '/components/com_sportsmanagement/src/View/Sishandball/HtmlView.php';
+}
 
-    public function display($tpl = null): void
-    {
-        $app = Factory::getContainer()->get(SiteApplication::class);
-        $params = $app->getParams();
-        $paramsComponent = ComponentHelper::getParams('com_sportsmanagement');
-        $model = $this->getModel();
+if (!class_exists(HtmlView::class)) {
+    throw new \RuntimeException('SportsManagement native frontend SIS handball view could not be loaded.', 500);
+}
 
-        $xmlBaseUrl = (string) $paramsComponent->get('sis_xmllink', '');
-        $clubNumber = (string) $paramsComponent->get('sis_meinevereinsnummer', '');
-        $clubPassword = (string) $paramsComponent->get('sis_meinvereinspasswort', '');
-        $leagueNumber = (string) $params->get('sis_liganummer', '');
-        $requestedSisType = (string) $params->get('sis_art', '4');
-        $sisType = in_array($requestedSisType, ['x', '1a'], true) ? '1' : $requestedSisType;
-
-        $linkResults = $model->getLink(
-            $clubNumber,
-            $clubPassword,
-            $leagueNumber,
-            $sisType,
-            $xmlBaseUrl
-        );
-
-        $this->sis_getxmldatei = (int) $params->get('sis_getxmldatei', 0);
-        $this->sis_getschiedsrichter = (int) $params->get('sis_getschiedsrichter', 1);
-        $this->sis_getspielort = (int) $params->get('sis_getspielort', 1);
-        $this->linkresults = $linkResults;
-        $this->params = $params;
-        $this->paramscomponent = $paramsComponent;
-        $this->sis_art = $sisType;
-        $this->vereinsnummer = $clubNumber;
-        $this->liganummer = $leagueNumber;
-        $this->article = (object) ['title' => ''];
-
-        if (in_array($sisType, ['4', '6', '7'], true)) {
-            $this->tabelle = $model->getTabelle($linkResults, $leagueNumber, $sisType);
-        }
-
-        if (in_array($sisType, ['1', '2', '3', '10', '11'], true)) {
-            $this->spielplan = $model->getSpielplan($linkResults, $leagueNumber, $sisType);
-        }
-
-        if (in_array($sisType, ['12', '12a'], true)) {
-            $this->statistik = $model->getStatistik($linkResults, $leagueNumber);
-        }
-
-        parent::display($tpl);
-    }
+if (!class_exists('sportsmanagementViewsishandball', false)) {
+    class_alias(HtmlView::class, 'sportsmanagementViewsishandball');
 }
