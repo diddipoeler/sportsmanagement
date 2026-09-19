1c6e0917f9939525e4d32d5617690924254ab3f4
diddipoeler <diddipoeler@gmx.de>
2026-09-19 15:16:56 +0200

    Harden Joomla 5/6 Agegroups administrator view bridge

--- admin/views/agegroups/view.html.php
+++ admin/views/agegroups/view.html.php
@@ -1,11 +1,22 @@
 <?php
-/** Legacy compatibility bridge for the native administrator agegroups view. */
+/**
+ * Legacy compatibility bridge for the native Joomla 5/6 administrator Agegroups view.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 \defined('_JEXEC') or die;
 
 use Diddipoeler\Component\SportsManagement\Administrator\View\Agegroups\HtmlView;
 
 if (!class_exists(HtmlView::class)) {
     require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/View/Agegroups/HtmlView.php';
+}
+
+if (!class_exists(HtmlView::class)) {
+    throw new \RuntimeException('SportsManagement native administrator Agegroups view could not be loaded.', 500);
 }
 
 if (!class_exists('sportsmanagementViewagegroups', false)) {
