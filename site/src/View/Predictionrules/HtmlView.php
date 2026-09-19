645eb89c807655cfbaf8cd2c6524ec7e3ca7b41a
diddipoeler <diddipoeler@gmx.de>
2026-09-19 16:23:40 +0200

    Complete Joomla 5/6 frontend view header

--- site/src/View/Predictionrules/HtmlView.php
+++ site/src/View/Predictionrules/HtmlView.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Native Joomla 5/6 frontend SportsManagement Predictionrules view.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 namespace Diddipoeler\Component\SportsManagement\Site\View\Predictionrules;
 
 \defined('_JEXEC') or die;
