1ff46963bb9fd19b6cbe2c72368b23db56d7d239
diddipoeler <diddipoeler@gmx.de>
2026-09-19 13:19:40 +0200

    Harden Joomla 5/6 Leagues administrator view bridge

--- admin/views/leagues/view.html.php
+++ admin/views/leagues/view.html.php
@@ -15,6 +15,10 @@ if (!class_exists(HtmlView::class)) {
     require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/View/Leagues/HtmlView.php';
 }
 
+if (!class_exists(HtmlView::class)) {
+    throw new \RuntimeException('SportsManagement native administrator Leagues view could not be loaded.', 500);
+}
+
 if (!class_exists('sportsmanagementViewLeagues', false)) {
     class_alias(HtmlView::class, 'sportsmanagementViewLeagues');
 }
