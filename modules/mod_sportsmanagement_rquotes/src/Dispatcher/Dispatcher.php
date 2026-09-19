30c59d7ef147c6287ffcb5ad04e2c061f26dd12d
diddipoeler <diddipoeler@gmx.de>
2026-09-19 12:39:04 +0200

    Use Joomla 5/6 DI container in Rquotes dispatcher

--- modules/mod_sportsmanagement_rquotes/src/Dispatcher/Dispatcher.php
+++ modules/mod_sportsmanagement_rquotes/src/Dispatcher/Dispatcher.php
@@ -12,6 +12,7 @@ namespace Diddipoeler\Module\SportsManagementRquotes\Site\Dispatcher;
 use Joomla\CMS\Application\SiteApplication;
 use Joomla\CMS\Component\ComponentHelper;
 use Joomla\CMS\Dispatcher\AbstractModuleDispatcher;
+use Joomla\CMS\Factory;
 use Joomla\CMS\Helper\HelperFactoryAwareInterface;
 use Joomla\CMS\Helper\HelperFactoryAwareTrait;
 use Joomla\Database\DatabaseInterface;
@@ -37,7 +38,7 @@ final class Dispatcher extends AbstractModuleDispatcher implements HelperFactory
         $app->getLanguage()->load('com_sportsmanagement', JPATH_ADMINISTRATOR, null, true);
 
         /** @var DatabaseInterface $database */
-        $database = $app->getContainer()->get(DatabaseInterface::class);
+        $database = Factory::getContainer()->get(DatabaseInterface::class);
         $result = $this->getHelperFactory()->getHelper('RquotesHelper')->getData(
             $data['params'],
             ComponentHelper::getParams('com_sportsmanagement'),
