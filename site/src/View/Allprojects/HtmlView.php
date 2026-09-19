2173dc7f846d0b550dd56b3f86753e856e8809bf
diddipoeler <diddipoeler@gmx.de>
2026-09-19 15:02:03 +0200

    Complete Joomla 5/6 frontend view header

--- site/src/View/Allprojects/HtmlView.php
+++ site/src/View/Allprojects/HtmlView.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Native Joomla 5/6 frontend SportsManagement Allprojects view.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 namespace Diddipoeler\Component\SportsManagement\Site\View\Allprojects;
 
 \defined('_JEXEC') or die;
