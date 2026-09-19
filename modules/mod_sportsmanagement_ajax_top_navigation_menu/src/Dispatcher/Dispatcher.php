c81e3eab1dfe1436e5edfb31e94d0533e07d3211
diddipoeler <diddipoeler@gmx.de>
2026-09-19 13:03:39 +0200

    Harden AJAX Top Navigation Joomla 5/6 site dispatcher

--- modules/mod_sportsmanagement_ajax_top_navigation_menu/src/Dispatcher/Dispatcher.php
+++ modules/mod_sportsmanagement_ajax_top_navigation_menu/src/Dispatcher/Dispatcher.php
@@ -11,6 +11,7 @@ namespace Diddipoeler\Module\SportsManagementAjaxTopNavigationMenu\Site\Dispatch
 
 \defined('_JEXEC') or die;
 
+use Joomla\CMS\Application\SiteApplication;
 use Joomla\CMS\Dispatcher\AbstractModuleDispatcher;
 use Joomla\CMS\Helper\HelperFactoryAwareInterface;
 use Joomla\CMS\Helper\HelperFactoryAwareTrait;
@@ -28,6 +29,11 @@ final class Dispatcher extends AbstractModuleDispatcher implements HelperFactory
         }
 
         $app = $this->getApplication();
+
+        if (!$app instanceof SiteApplication || !$app->isClient('site')) {
+            throw new \RuntimeException('SportsManagement AJAX Top Navigation requires the Joomla site application.', 500);
+        }
+
         $app->getLanguage()->load('com_sportsmanagement', JPATH_ADMINISTRATOR, null, true);
 
         $requestedLayout = (string) $data['params']->get('layout', 'default');
