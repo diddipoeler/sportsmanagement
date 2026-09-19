10cd226c7533b6fe3421b954f668bdf6c6f12af1
diddipoeler <diddipoeler@gmx.de>
2026-09-19 15:59:34 +0200

    Harden Joomla 5/6 Sportstype administrator view bridge

--- admin/views/sportstype/view.html.php
+++ admin/views/sportstype/view.html.php
@@ -1,11 +1,22 @@
 <?php
-/** Legacy compatibility bridge for the native Joomla 5/6 sports-type administrator view. */
+/**
+ * Legacy compatibility bridge for the native Joomla 5/6 administrator Sportstype view.
+ *
+ * @version    5.6.0
+ * @author     diddipoeler
+ * @copyright  Copyright (C) diddipoeler
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 \defined('_JEXEC') or die;
 
 use Diddipoeler\Component\SportsManagement\Administrator\View\Sportstype\HtmlView;
 
 if (!class_exists(HtmlView::class)) {
     require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/View/Sportstype/HtmlView.php';
+}
+
+if (!class_exists(HtmlView::class)) {
+    throw new \RuntimeException('SportsManagement native administrator Sportstype view could not be loaded.', 500);
 }
 
 if (!class_exists('sportsmanagementViewSportsType', false)) {
