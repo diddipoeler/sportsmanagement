46992b8a93d85c6c4b739bef36fcf7f5474ebdd6
diddipoeler <diddipoeler@gmx.de>
2026-09-23 17:26:52 +0200

    Complete Joomla 5/6 legacy library metadata batch 302

--- admin/libraries/joomla/components/com_search/views/search/tmpl/default_results.php
+++ admin/libraries/joomla/components/com_search/views/search/tmpl/default_results.php
@@ -2,13 +2,16 @@
 /**
  * @package     Joomla.Site
  * @subpackage  com_search
+ * @version     5.6.0
+ * @author      Open Source Matters, Inc.
  *
  * @copyright   Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
-defined('_JEXEC') or die;
+\defined('_JEXEC') or die;
 
+use Joomla\CMS\Language\Text;
 use Joomla\CMS\Router\Route;
 
 ?>
