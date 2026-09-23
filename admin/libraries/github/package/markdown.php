071c76f321516e79ef3920b5a9844ae2a87b359a
diddipoeler <diddipoeler@gmx.de>
2026-09-23 15:42:26 +0200

    Continue Joomla 5/6 GitHub library metadata batch 294

--- admin/libraries/github/package/markdown.php
+++ admin/libraries/github/package/markdown.php
@@ -2,6 +2,8 @@
 /**
  * @package     Joomla.Platform
  * @subpackage  GitHub
+ * @version     5.6.0
+ * @author      Open Source Matters, Inc.
  *
  * @copyright   Copyright (C) 2012 - 2016 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
