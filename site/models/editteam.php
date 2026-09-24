5169b56689dc98b5aae983dca00361cf7f7c9c62
diddipoeler <diddipoeler@gmx.de>
2026-09-24 06:42:01 +0200

    Continue Joomla 5/6 site modules migration batch 314

--- site/models/editteam.php
+++ site/models/editteam.php
@@ -2,12 +2,12 @@
 /**
  * Legacy compatibility bridge for the native frontend Editteam model.
  *
- * @version    4.24.00
+ * @version    5.6.0
  * @author     diddipoeler, stony, svdoldie und donclumsy (diddipoeler@gmx.de)
  * @copyright  Copyright: © 2013-2023 Fussball in Europa http://fussballineuropa.de/ All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
-\defined('_JEXEC') or die('Restricted access');
+\defined('_JEXEC') or die;
 
 use Diddipoeler\Component\SportsManagement\Site\Model\EditteamModel;
 
