865fa58681b6f761351c8635b0bbf0dea8a7c362
diddipoeler <diddipoeler@gmx.de>
2026-09-26 07:01:14 +0200

    Continue Joomla 5/6 site referees loader batch 436

--- site/models/referees.php
+++ site/models/referees.php
@@ -12,9 +12,17 @@
 use Diddipoeler\Component\SportsManagement\Site\Model\RefereesModel;
 
 if (!class_exists(RefereesModel::class)) {
-    require_once JPATH_SITE . '/components/com_sportsmanagement/src/Model/SportsManagementModel.php';
-    require_once JPATH_SITE . '/components/com_sportsmanagement/src/Model/SportsManagementProjectModel.php';
-    require_once JPATH_SITE . '/components/com_sportsmanagement/src/Model/RefereesModel.php';
+    foreach ([
+        JPATH_SITE . '/components/com_sportsmanagement/src/Service/SportsManagementDatabaseResolver.php',
+        JPATH_SITE . '/components/com_sportsmanagement/src/Service/SportsManagementSiteApplicationResolver.php',
+        JPATH_SITE . '/components/com_sportsmanagement/src/Model/SportsManagementModel.php',
+        JPATH_SITE . '/components/com_sportsmanagement/src/Model/SportsManagementProjectModel.php',
+        JPATH_SITE . '/components/com_sportsmanagement/src/Model/RefereesModel.php',
+    ] as $nativeFile) {
+        if (is_file($nativeFile)) {
+            require_once $nativeFile;
+        }
+    }
 }
 
 if (!class_exists(RefereesModel::class)) {
