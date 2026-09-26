09a580a771e6ca10006ee0e91862002bd6bf7c5e
diddipoeler <diddipoeler@gmx.de>
2026-09-26 09:09:56 +0200

    Continue Joomla 5/6 administrator match table loaders batch 447

--- admin/tables/matchcommentary.php
+++ admin/tables/matchcommentary.php
@@ -12,8 +12,14 @@
 use Diddipoeler\Component\SportsManagement\Administrator\Table\MatchcommentaryTable;
 
 if (!class_exists(MatchcommentaryTable::class)) {
-    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Table/SportsManagementTable.php';
-    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Table/MatchcommentaryTable.php';
+    foreach ([
+        JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Table/SportsManagementTable.php',
+        JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Table/MatchcommentaryTable.php',
+    ] as $nativeTable) {
+        if (is_file($nativeTable)) {
+            require_once $nativeTable;
+        }
+    }
 }
 
 if (!class_exists(MatchcommentaryTable::class)) {
--- admin/tables/matchreferee.php
+++ admin/tables/matchreferee.php
@@ -12,8 +12,14 @@
 use Diddipoeler\Component\SportsManagement\Administrator\Table\MatchrefereeTable;
 
 if (!class_exists(MatchrefereeTable::class)) {
-    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Table/SportsManagementTable.php';
-    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Table/MatchrefereeTable.php';
+    foreach ([
+        JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Table/SportsManagementTable.php',
+        JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Table/MatchrefereeTable.php',
+    ] as $nativeTable) {
+        if (is_file($nativeTable)) {
+            require_once $nativeTable;
+        }
+    }
 }
 
 if (!class_exists(MatchrefereeTable::class)) {
