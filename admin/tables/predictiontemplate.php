e34e3f5de65ee811b916953461a91dcfb9ba0120
diddipoeler <diddipoeler@gmx.de>
2026-09-26 09:20:31 +0200

    Continue Joomla 5/6 administrator prediction table loaders batch 452

--- admin/tables/predictiontemplate.php
+++ admin/tables/predictiontemplate.php
@@ -12,8 +12,14 @@
 use Diddipoeler\Component\SportsManagement\Administrator\Table\PredictiontemplateTable;
 
 if (!class_exists(PredictiontemplateTable::class)) {
-    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Table/SportsManagementTable.php';
-    require_once JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Table/PredictiontemplateTable.php';
+    foreach ([
+        JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Table/SportsManagementTable.php',
+        JPATH_ADMINISTRATOR . '/components/com_sportsmanagement/src/Table/PredictiontemplateTable.php',
+    ] as $nativeTable) {
+        if (is_file($nativeTable)) {
+            require_once $nativeTable;
+        }
+    }
 }
 
 if (!class_exists(PredictiontemplateTable::class)) {
